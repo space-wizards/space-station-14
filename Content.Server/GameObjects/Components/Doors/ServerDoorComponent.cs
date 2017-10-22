08921e21af96318ce7ef1342a917181f83c67ec2
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-10-22 19:26:34 +0200

    Finished

--- Content.Server/GameObjects/Components/Doors/ServerDoorComponent.cs
+++ Content.Server/GameObjects/Components/Doors/ServerDoorComponent.cs
@@ -4,12 +4,15 @@ using SS14.Server.GameObjects;
 using SS14.Shared.GameObjects;
 using SS14.Shared.Interfaces.GameObjects.Components;
 using SS14.Shared.Log;
+using SS14.Shared.Maths;
 
 namespace Content.Server.GameObjects
 {
     public class ServerDoorComponent : SharedDoorComponent
     {
         public bool Opened { get; private set; }
+
+        private float OpenTimeCounter;
 
         private IInteractableComponent interactableComponent;
         private CollidableComponent collidableComponent;
@@ -32,7 +35,6 @@ namespace Content.Server.GameObjects
 
         private void OnAttackHand(object sender, AttackHandEventArgs args)
         {
-            Logger.Info("Yes!");
             if (Opened)
             {
                 Close();
@@ -43,21 +45,52 @@ namespace Content.Server.GameObjects
             }
         }
 
+        private void OnBump(object sender, BumpEventArgs args)
+        {
+            Logger.Info("Bumped!");
+        }
+
         public void Open()
         {
             Opened = true;
-            //collidableComponent.IsHardCollidable = false;
+            collidableComponent.IsHardCollidable = false;
         }
 
-        public void Close()
+        public bool Close()
         {
+            if (collidableComponent.TryCollision(Vector2.Zero))
+            {
+                // Do nothing, somebody's in the door.
+                return false;
+            }
             Opened = false;
-            //collidableComponent.IsHardCollidable = true;
+            OpenTimeCounter = 0;
+            collidableComponent.IsHardCollidable = true;
+            return true;
         }
 
         public override ComponentState GetComponentState()
         {
             return new DoorComponentState(Opened);
+        }
+
+        private const float AUTO_CLOSE_DELAY = 5;
+        public override void Update(float frameTime)
+        {
+            if (!Opened)
+            {
+                return;
+            }
+
+            OpenTimeCounter += frameTime;
+            if (OpenTimeCounter > AUTO_CLOSE_DELAY)
+            {
+                if (!Close())
+                {
+                    // Try again in 2 seconds if it's jammed or something.
+                    OpenTimeCounter -= 2;
+                }
+            }
         }
     }
 }
