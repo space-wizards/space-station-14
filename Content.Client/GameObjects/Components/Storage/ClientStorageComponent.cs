2d23c6a45ea73092b16fe173834f8e320a163407
indeano <38874984+indeano@users.noreply.github.com>
2018-06-20 16:49:13 -0400

    Some work on directing storage component's client updates (#75)
    * adds storage system, primitive client-unsubscribing of storage updates
    
    * strips out currently unneeded system
    
    * channel to actor
    
    * closestorageui todo added
    
    * thorough logging, actor to session, validates session before sending packets
    
    * offloads session status check to an event handler

--- Content.Client/GameObjects/Components/Storage/ClientStorageComponent.cs
+++ Content.Client/GameObjects/Components/Storage/ClientStorageComponent.cs
@@ -37,7 +37,6 @@ namespace Content.Client.GameObjects.Components.Storage
         public override void OnRemove()
         {
             Window.Dispose();
-
             base.OnRemove();
         }
 
@@ -52,6 +51,9 @@ namespace Content.Client.GameObjects.Components.Storage
                 //Opens the UI
                 case OpenStorageUIMessage msg:
                     OpenUI();
+                    break;
+                case CloseStorageUIMessage msg:
+                    // todo: close window/grey it out
                     break;
             }
         }
