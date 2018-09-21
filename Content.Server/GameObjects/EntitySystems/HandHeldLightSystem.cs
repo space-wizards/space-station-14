47b14eea5924ab6095074fa05dbda5df556a5416
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2018-09-21 20:50:07 +0200

    Flashlight runs off a power cell now.

---
+++ Content.Server/GameObjects/EntitySystems/HandHeldLightSystem.cs
@@ -0,0 +1,24 @@
+using Content.Server.GameObjects.Components.Interactable;
+using Content.Server.GameObjects.Components.Interactable.Tools;
+using SS14.Shared.GameObjects;
+using SS14.Shared.GameObjects.Systems;
+
+namespace Content.Server.GameObjects.EntitySystems
+{
+    public class HandHeldLightSystem : EntitySystem
+    {
+        public override void Initialize()
+        {
+            EntityQuery = new TypeEntityQuery(typeof(HandheldLightComponent));
+        }
+
+        public override void Update(float frameTime)
+        {
+            foreach (var entity in RelevantEntities)
+            {
+                var comp = entity.GetComponent<HandheldLightComponent>();
+                comp.OnUpdate(frameTime);
+            }
+        }
+    }
+}
