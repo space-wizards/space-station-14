19395638e7e18bc6c690b143d6ef78d45d9073dd
clusterfack <8516830+clusterfack@users.noreply.github.com>
2018-12-10 14:55:56 -0600

    Make overlays work in worldspace instead of screen space and not cover user interfaces

--- Content.Client/Graphics/Overlays/GradientCircleMask.cs
+++ Content.Client/Graphics/Overlays/GradientCircleMask.cs
@@ -2,6 +2,7 @@
 using SS14.Client.Graphics.Overlays;
 using SS14.Client.Graphics.Shaders;
 using SS14.Client.Interfaces.Graphics.ClientEye;
+using SS14.Client.Interfaces.Graphics.Overlays;
 using SS14.Shared.IoC;
 using SS14.Shared.Maths;
 using SS14.Shared.Prototypes;
@@ -13,6 +14,7 @@ namespace Content.Client.Graphics.Overlays
         [Dependency] private readonly IPrototypeManager _prototypeManager;
         [Dependency] private readonly IEyeManager _eyeManager;
 
+        public override OverlaySpace Space => OverlaySpace.WorldSpace;
 
         public GradientCircleMask() : base(nameof(GradientCircleMask))
         {
@@ -22,9 +24,9 @@ namespace Content.Client.Graphics.Overlays
 
         protected override void Draw(DrawingHandle handle)
         {
-            var worldHandle = (DrawingHandleScreen)handle;
+            var worldHandle = (DrawingHandleWorld)handle;
             var viewport = _eyeManager.GetWorldViewport();
-            worldHandle.DrawRect(new UIBox2(_eyeManager.WorldToScreen(viewport.TopLeft), _eyeManager.WorldToScreen(viewport.BottomRight)), Color.White);
+            worldHandle.DrawRect(viewport, Color.White);
         }
     }
 }
