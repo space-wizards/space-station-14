c17bc612fff505106bcc870642c8c760da4fdc3e
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-03-20 01:12:38 +0100

    Lights now go CLUNK when turned on.

--- Content.Server/GameObjects/Components/Power/PoweredLightComponent.cs
+++ Content.Server/GameObjects/Components/Power/PoweredLightComponent.cs
@@ -1,12 +1,22 @@
+using System;
 using SS14.Server.GameObjects;
+using SS14.Server.GameObjects.EntitySystems;
+using SS14.Shared.Audio;
 using SS14.Shared.Enums;
 using SS14.Shared.GameObjects;
+using SS14.Shared.Interfaces.GameObjects;
+using SS14.Shared.Interfaces.Timing;
+using SS14.Shared.IoC;
 
 namespace Content.Server.GameObjects.Components.Power
 {
     public class PoweredLightComponent : Component
     {
         public override string Name => "PoweredLight";
+
+        private static readonly TimeSpan _thunkDelay = TimeSpan.FromSeconds(2);
+
+        private TimeSpan _lastThunk;
 
         public override void Initialize()
         {
@@ -21,6 +31,12 @@ namespace Content.Server.GameObjects.Components.Power
                 {
                     sprite.LayerSetState(0, "on");
                     light.State = LightState.On;
+                    var time = IoCManager.Resolve<IGameTiming>().CurTime;
+                    if (time > _lastThunk + _thunkDelay)
+                    {
+                        IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<AudioSystem>()
+                            .Play("/Audio/machines/light_tube_on.ogg", Owner, AudioParams.Default.WithVolume(-10f));
+                    }
                 }
                 else
                 {
