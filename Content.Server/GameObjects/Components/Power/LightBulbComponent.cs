e6b71dcc1cbe038c9307843ed0f14a15649dbaa3
Zumorica <zddm@outlook.es>
2019-03-22 22:38:46 +0100

    Light bulb now updates its sprite when it changes.

--- Content.Server/GameObjects/Components/Power/LightBulbComponent.cs
+++ Content.Server/GameObjects/Components/Power/LightBulbComponent.cs
@@ -1,4 +1,5 @@
 using System;
+using SS14.Server.GameObjects;
 using SS14.Shared.Enums;
 using SS14.Shared.GameObjects;
 using SS14.Shared.Serialization;
@@ -31,8 +32,21 @@ namespace Content.Server.GameObjects.Components.Power
             get { return _state; }
             set
             {
+                var sprite = Owner.GetComponent<SpriteComponent>();
                 OnLightBulbStateChange?.Invoke(this, EventArgs.Empty);
                 _state = value;
+                switch (value)
+                {
+                    case LightBulbState.Normal:
+                        sprite.LayerSetState(0, "normal");
+                        break;
+                    case LightBulbState.Broken:
+                        sprite.LayerSetState(0, "broken");
+                        break;
+                    case LightBulbState.Burned:
+                        sprite.LayerSetState(0, "burned");
+                        break;
+                }
             }
         }
 
