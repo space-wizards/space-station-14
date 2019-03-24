4b1bd2173ffa685a4a01679a08c9d1a5dcb6ec05
Acruid <shatter66@gmail.com>
2019-03-24 16:10:38 -0700

    Added the ReagentPrototype class.

---
+++ Content.Shared/Chemistry/ReagentPrototype.cs
@@ -0,0 +1,25 @@
+﻿using Robust.Shared.Maths;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+using YamlDotNet.RepresentationModel;
+
+namespace Content.Shared.Chemistry
+{
+    [Prototype("reagent")]
+    public class ReagentPrototype : IPrototype, IIndexedPrototype
+    {
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Color SubstanceColor { get; private set; }
+
+        public void LoadFrom(YamlMappingNode mapping)
+        {
+            ID = mapping.GetNode("id").AsString();
+            Name = mapping.GetNode("name").ToString();
+            Description = mapping.GetNode("desc").ToString();
+
+            SubstanceColor = mapping.TryGetNode("color", out var colorNode) ? colorNode.AsHexColor(Color.White) : Color.White;
+        }
+    }
+}
