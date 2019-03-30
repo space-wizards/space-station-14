05b5946795b7caaf382d6f79e476815049e0b5c1
clusterfack <8516830+clusterfack@users.noreply.github.com>
2019-03-30 16:37:26 -0500

    Updates .bat startup files
    Makes it easier for newbs to start up server/client using the batch files

--- runclient.bat
+++ runclient.bat
@@ -1,6 +1,6 @@
 @echo off
 set PDIR=%~dp0
-cd %PDIR%Bin\Client
+cd %PDIR%RobustToolbox\Bin\Client
 start SS14.Client.exe %*
 cd %PDIR%
 set PDIR=
--- runserver.bat
+++ runserver.bat
@@ -1,6 +1,6 @@
 @echo off
 set PDIR=%~dp0
-cd %PDIR%Bin\Server
+cd %PDIR%RobustToolbox\Bin\Server
 call SS14.Server.exe %*
 cd %PDIR%
 set PDIR=
