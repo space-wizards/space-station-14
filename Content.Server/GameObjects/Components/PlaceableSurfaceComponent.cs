148be01ee7ac5372e8514ae828b70e38281ab338
Damian <damian@autistici.org>
2019-04-24 08:24:51 +0100

    Moved dropping items over PlaceableSurfaces from AfterAttack to AttackBy Added ClickLocation variable to AttackByEventArgs

--- Content.Server/GameObjects/Components/PlaceableSurfaceComponent.cs
+++ Content.Server/GameObjects/Components/PlaceableSurfaceComponent.cs
@@ -1,9 +1,10 @@
-﻿using Robust.Shared.GameObjects;
+﻿using Content.Server.GameObjects.EntitySystems;
+using Robust.Shared.GameObjects;
 using Robust.Shared.Serialization;
 
 namespace Content.Server.GameObjects.Components
 {
-    public class PlaceableSurfaceComponent : Component
+    public class PlaceableSurfaceComponent : Component, IAttackBy
     {
         public override string Name => "PlaceableSurface";
 
@@ -15,6 +16,16 @@ namespace Content.Server.GameObjects.Components
             base.ExposeData(serializer);
 
             serializer.DataField(ref _isPlaceable, "IsPlaceable", true);
+        }
+        public bool AttackBy(AttackByEventArgs eventArgs)
+        {
+            if(!eventArgs.User.TryGetComponent<HandsComponent>(out var handComponent))
+            {
+                return true;
+            }
+            handComponent.Drop(eventArgs.AttackWith);
+            eventArgs.AttackWith.Transform.WorldPosition = eventArgs.ClickLocation.Position;
+            return true;
         }
     }
 }
