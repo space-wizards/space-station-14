dd13d969b2dcd69182aa97e44742f52126141219
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-06-02 01:17:07 +0200

    Benchmarks project along with a component manager benchmark. (#251)

---
+++ Content.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace Content.Benchmarks
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ComponentManagerGetAllComponents>();
+        }
+    }
+}
