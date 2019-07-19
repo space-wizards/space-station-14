3749612a290100caa237aaa3a8a8a7cdc63bcf03
Zumorica <zddm@outlook.es>
2019-07-19 16:07:22 +0200

    Quick equip

--- Content.Server/GameObjects/Components/Items/Clothing/ClothingComponent.cs
+++ Content.Server/GameObjects/Components/Items/Clothing/ClothingComponent.cs
@@ -1,14 +1,15 @@
-﻿using Content.Shared.GameObjects;
+using Content.Shared.GameObjects;
 using Content.Shared.GameObjects.Components.Items;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Serialization;
 using System;
 using System.Collections.Generic;
+using Content.Server.GameObjects.EntitySystems;
 using static Content.Shared.GameObjects.Components.Inventory.EquipmentSlotDefines;
 
 namespace Content.Server.GameObjects
 {
-    public class ClothingComponent : ItemComponent
+    public class ClothingComponent : ItemComponent, IUse
     {
         public override string Name => "Clothing";
         public override uint? NetID => ContentNetIDs.CLOTHING;
@@ -16,6 +17,7 @@ namespace Content.Server.GameObjects
 
         public SlotFlags SlotFlags = SlotFlags.PREVENTEQUIP; //Different from None, NONE allows equips if no slot flags are required
 
+        private bool _quickEquipEnabled = true;
         private int _heatResistance;
         public int HeatResistance => _heatResistance;
 
@@ -46,12 +48,44 @@ namespace Content.Server.GameObjects
                 }
             });
 
+            serializer.DataField(ref _quickEquipEnabled, "QuickEquip", true);
+
             serializer.DataFieldCached(ref _heatResistance, "HeatResistance", 323);
         }
 
         public override ComponentState GetComponentState()
         {
             return new ClothingComponentState(ClothingEquippedPrefix, EquippedPrefix);
+        }
+
+        public bool UseEntity(UseEntityEventArgs eventArgs)
+        {
+            if (!_quickEquipEnabled) return false;
+            if (!eventArgs.User.TryGetComponent(out InventoryComponent inv)
+            ||  !eventArgs.User.TryGetComponent(out HandsComponent hands)) return false;
+
+            foreach (var pair in SlotMasks)
+            {
+                if ((SlotFlags & pair.Value) == 0) continue;
+
+
+
+                if (inv.TryGetSlotItem(pair.Key, out ItemComponent item))
+                {
+                    if (!inv.CanUnequip(pair.Key)) continue;
+                    hands.Drop(Owner);
+                    inv.Unequip(pair.Key);
+                    hands.PutInHand(item);
+                }
+                else
+                {
+                    hands.Drop(Owner);
+                }
+
+                return inv.Equip(pair.Key, this);
+            }
+
+            return false;
         }
     }
 }
