72ba0d9458462e1910a8608f55db505aa58ddbfa
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-08-13 23:43:44 +0200

    Don't let bullets fly forever. Give them 10 seconds until deleted.

---
+++ Content.Server/GameObjects/EntitySystems/ProjectileSystem.cs
@@ -0,0 +1,34 @@
+using Content.Server.GameObjects.Components.Projectiles;
+using JetBrains.Annotations;
+using Robust.Shared.GameObjects;
+using Robust.Shared.GameObjects.Systems;
+
+namespace Content.Server.GameObjects.EntitySystems
+{
+    [UsedImplicitly]
+    internal sealed class ProjectileSystem : EntitySystem
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            EntityQuery = new TypeEntityQuery(typeof(ProjectileComponent));
+        }
+
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+
+            foreach (var entity in RelevantEntities)
+            {
+                var component = entity.GetComponent<ProjectileComponent>();
+                component.TimeLeft -= frameTime;
+
+                if (component.TimeLeft <= 0)
+                {
+                    entity.Delete();
+                }
+            }
+        }
+    }
+}
