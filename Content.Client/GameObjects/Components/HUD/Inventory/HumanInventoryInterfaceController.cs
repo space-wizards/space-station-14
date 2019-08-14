6be135a137d58df51ab6f05b64808ed9d7229c8a
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-08-14 12:00:41 +0200

    Fix human inventory window ID slot using mask texture.

--- Content.Client/GameObjects/Components/HUD/Inventory/HumanInventoryInterfaceController.cs
+++ Content.Client/GameObjects/Components/HUD/Inventory/HumanInventoryInterfaceController.cs
@@ -185,7 +185,7 @@ namespace Content.Client.GameObjects
 
                 // Right column
                 AddButton(Slots.EARS, "ears", (2 * (size + sep), 0));
-                AddButton(Slots.IDCARD, "mask", (2 * (size + sep), size + sep));
+                AddButton(Slots.IDCARD, "id", (2 * (size + sep), size + sep));
                 AddButton(Slots.GLOVES, "gloves", (2 * (size + sep), 2 * (size + sep)));
 
                 // Far right column.
