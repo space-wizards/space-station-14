13077a759c65ad4c742739c42a2a80a6ec8750e9
tentekal <tanner.fink@outlook.com>
2019-08-24 14:19:39 -0400

    Init commit on Status Effect, scaffolding for the component.

---
+++ Content.Shared/GameObjects/Components/Status/SharedStatusEffectComponent.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.GameObjects;
+
+namespace Content.Shared.GameObjects.Components.Status
+{
+    public class SharedStatusEffectComponent : Component
+    {
+        public override string Name { get; }
+    }
+}
