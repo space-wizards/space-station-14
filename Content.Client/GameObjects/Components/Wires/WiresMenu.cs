eafe40236c72594fd74fb84902b4397ab40b9578
Damian <damian@autistici.org>
2019-08-25 19:38:31 +0200

    cleanup

--- Content.Client/GameObjects/Components/Wires/WiresMenu.cs
+++ Content.Client/GameObjects/Components/Wires/WiresMenu.cs
@@ -1,23 +1,14 @@
-using System;
 using System.Collections.Generic;
-using System.Data;
-using Content.Client.GameObjects.Components.Wires;
-using Content.Client.Interfaces.Chat;
-using Content.Client.VendingMachines;
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.CustomControls;
-using Robust.Shared.IoC;
 using Robust.Shared.Maths;
 using static Content.Shared.GameObjects.Components.SharedWiresComponent;
 
-namespace Content.Client.GameObjects.Components
+namespace Content.Client.GameObjects.Components.Wires
 {
     public class WiresMenu : SS14Window
     {
         protected override Vector2? CustomSize => (300, 450);
-
-        private List<ClientWire> _cachedWires;
-
         public WiresBoundUserInterface Owner { get; set; }
 
         private readonly VBoxContainer _rows;
@@ -31,7 +22,6 @@ namespace Content.Client.GameObjects.Components
 
         public void Populate(List<ClientWire> wiresList)
         {
-            _cachedWires = wiresList;
             _rows.RemoveAllChildren();
             foreach (var entry in wiresList)
             {
