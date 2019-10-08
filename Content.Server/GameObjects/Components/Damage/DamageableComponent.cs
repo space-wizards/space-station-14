8cf304927f5e5a20540ad0af7f54dc7af14f0fd6
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-10-08 15:05:21 -0400

    Add rejuvenate to right click menu

--- Content.Server/GameObjects/Components/Damage/DamageableComponent.cs
+++ Content.Server/GameObjects/Components/Damage/DamageableComponent.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using Content.Server.Interfaces;
 using Content.Server.Interfaces.GameObjects;
 using Content.Shared.GameObjects;
+using Robust.Server.Interfaces.GameObjects;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Localization;
 using Robust.Shared.Serialization;
 using Robust.Shared.ViewVariables;
+using Robust.Server.Console;
 
 namespace Content.Server.GameObjects
 {
@@ -180,6 +185,37 @@ namespace Content.Server.GameObjects
             {
                 _currentDamage.Add(damageType, 0);
                 Thresholds.Add(damageType, new List<DamageThreshold>());
+            }
+        }
+
+        /// <summary>
+        ///     Completely removes all damage from the DamageableComponent (heals the mob).
+        /// </summary>
+        [Verb]
+        private sealed class RejuvenateVerb : Verb<DamageableComponent>
+        {
+            protected override string GetText(IEntity user, DamageableComponent component)
+            {
+                var localizationManager = IoCManager.Resolve<ILocalizationManager>();
+                return localizationManager.GetString("Rejuvenate");
+            }
+
+            protected override VerbVisibility GetVisibility(IEntity user, DamageableComponent component)
+            {
+                var localizationManager = IoCManager.Resolve<ILocalizationManager>();
+                var groupController = IoCManager.Resolve<IConGroupController>();
+
+                if (user.TryGetComponent<IActorComponent>(out var player))
+                {
+                    if (groupController.CanCommand(player.playerSession, localizationManager.GetString("rejuvenate")))
+                        return VerbVisibility.Visible;
+                }
+                return VerbVisibility.Invisible;
+            }
+
+            protected override void Activate(IEntity user, DamageableComponent component)
+            {
+                component.HealAllDamage();
             }
         }
     }
