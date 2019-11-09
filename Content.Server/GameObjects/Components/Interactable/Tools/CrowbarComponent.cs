d7a7ee68476dde315cf8ef79f6e20a6c301a2337
Rok Povsic <rok.povsic@gmail.com>
2019-11-09 19:29:46 +0100

    Fix crowbar distance to work on max 1.5 tiles

--- Content.Server/GameObjects/Components/Interactable/Tools/CrowbarComponent.cs
+++ Content.Server/GameObjects/Components/Interactable/Tools/CrowbarComponent.cs
@@ -1,4 +1,5 @@
-﻿using Content.Server.GameObjects.EntitySystems;
+﻿using System;
+using Content.Server.GameObjects.EntitySystems;
 using Content.Shared.Maps;
 using Robust.Server.GameObjects.EntitySystems;
 using Robust.Shared.GameObjects;
@@ -18,6 +19,8 @@ namespace Content.Server.GameObjects.Components.Interactable.Tools
         [Dependency] private readonly IMapManager _mapManager;
 #pragma warning restore 649
 
+        private double MAX_DISTANCE = 1.5;
+
         /// <summary>
         /// Tool that can be used to crowbar things apart, such as deconstructing
         /// </summary>
@@ -27,6 +30,16 @@ namespace Content.Server.GameObjects.Components.Interactable.Tools
         {
             var mapGrid = _mapManager.GetGrid(eventArgs.ClickLocation.GridID);
             var tile = mapGrid.GetTileRef(eventArgs.ClickLocation);
+
+            var ownerLocation = Owner.Transform.GridPosition;
+            var tileCenterX = tile.X + 0.5;
+            var tileCenterY = tile.Y + 0.5;
+
+            if (Distance(ownerLocation.X, tileCenterX, ownerLocation.Y, tileCenterY) > MAX_DISTANCE)
+            {
+                return;
+            }
+
             var tileDef = (ContentTileDefinition)_tileDefinitionManager[tile.Tile.TypeId];
             if (tileDef.CanCrowbar)
             {
@@ -34,6 +47,11 @@ namespace Content.Server.GameObjects.Components.Interactable.Tools
                 mapGrid.SetTile(eventArgs.ClickLocation, new Tile(underplating.TileId));
                _entitySystemManager.GetEntitySystem<AudioSystem>().Play("/Audio/items/crowbar.ogg", Owner);
             }
+        }
+
+        private double Distance(float x1, double x2, float y1, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
         }
     }
 }
