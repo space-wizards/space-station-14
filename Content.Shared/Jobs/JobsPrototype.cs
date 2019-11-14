e7f24d5bac788b41d97c2d65b8b17203398b1a77
ZelteHonor <gabrieldionbouchard@gmail.com>
2019-11-14 14:48:32 -0500

    Add Job Prototype

---
+++ Content.Shared/Jobs/JobsPrototype.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+using YamlDotNet.RepresentationModel;
+
+namespace Content.Shared.Jobs
+{
+    [Prototype("job")]
+    public class JobsPrototype : IPrototype, IIndexedPrototype
+    {
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+        public IEnumerable<String> Department { get; private set; }
+
+        public void LoadFrom(YamlMappingNode mapping)
+        {
+            ID = mapping.GetNode("id").AsString();
+            Name = mapping.GetNode("name").ToString();
+            Department = mapping.GetNode("department").AllNodes.Select(i => i.ToString());
+        }
+    }
+}
