c4ea6e53e80a4b52335961da1d45e63bb0645b2e
DamianX <DamianX@users.noreply.github.com>
2020-01-15 15:10:18 +0100

    Use EFCore to store preferences (#506)
    * Use EFcore to store preferences
    
    * Fixed nullabilty warnings

---
+++ Content.Server.Database/Model.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Content.Server.Database
+{
+    public class PreferencesDbContext : DbContext
+    {
+        // This is used by the "dotnet ef" CLI tool.
+        public PreferencesDbContext() :
+            base(new DbContextOptionsBuilder().UseSqlite("Data Source=:memory:").Options)
+        {
+        }
+
+        public PreferencesDbContext(DbContextOptions<PreferencesDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Prefs> Preferences { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Prefs>()
+                .HasIndex(p => p.Username)
+                .IsUnique();
+        }
+    }
+
+    public class Prefs
+    {
+        public int PrefsId { get; set; }
+        public string Username { get; set; } = null!;
+        public int SelectedCharacterSlot { get; set; }
+        public List<HumanoidProfile> HumanoidProfiles { get; } = new List<HumanoidProfile>();
+    }
+
+    public class HumanoidProfile
+    {
+        public int HumanoidProfileId { get; set; }
+        public int Slot { get; set; }
+        public string SlotName { get; set; } = null!;
+        public string CharacterName { get; set; } = null!;
+        public int Age { get; set; }
+        public string Sex { get; set; } = null!;
+        public string HairName { get; set; } = null!;
+        public string HairColor { get; set; } = null!;
+        public string FacialHairName { get; set; } = null!;
+        public string FacialHairColor { get; set; } = null!;
+        public string EyeColor { get; set; } = null!;
+        public string SkinColor { get; set; } = null!;
+    }
+}
---
+++ Content.Server.Database/PrefsDb.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Content.Server.Database
+{
+    public class PrefsDb
+    {
+        private readonly PreferencesDbContext _prefsCtx;
+
+        public PrefsDb(string dbPath)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PreferencesDbContext>();
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+
+            _prefsCtx = new PreferencesDbContext(optionsBuilder.Options);
+            _prefsCtx.Database.Migrate();
+        }
+
+        public Prefs GetPlayerPreferences(string username)
+        {
+            return _prefsCtx.Preferences.SingleOrDefault(p => p.Username == username);
+        }
+
+        public void SaveSelectedCharacterIndex(string username, int slot)
+        {
+            var prefs = _prefsCtx.Preferences.SingleOrDefault(p => p.Username == username);
+            if (prefs is null)
+                _prefsCtx.Preferences.Add(new Prefs
+                {
+                    Username = username,
+                    SelectedCharacterSlot = slot
+                });
+            else
+                prefs.SelectedCharacterSlot = slot;
+            _prefsCtx.SaveChanges();
+        }
+
+        public void SaveCharacterSlot(string username, HumanoidProfile newProfile)
+        {
+            var prefs = _prefsCtx
+                .Preferences
+                .Single(p => p.Username == username);
+            var oldProfile = prefs
+                .HumanoidProfiles
+                .SingleOrDefault(h => h.Slot == newProfile.Slot);
+            if (!(oldProfile is null)) prefs.HumanoidProfiles.Remove(oldProfile);
+            prefs.HumanoidProfiles.Add(newProfile);
+        }
+
+        public void DeleteCharacterSlot(string username, int slot)
+        {
+            var profile = _prefsCtx
+                .Preferences
+                .Single(p => p.Username == username)
+                .HumanoidProfiles
+                .RemoveAll(h => h.Slot == slot);
+            _prefsCtx.SaveChanges();
+        }
+    }
+}
