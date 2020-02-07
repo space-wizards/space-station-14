064314bd5f86986dfedf48ef01b528924b917399
Tad Hardesty <tad@platymuus.com>
2020-02-06 22:41:15 -0800

    Fix powernets crossing maps

--- Content.Server/GameObjects/Components/Power/PowerProviderComponent.cs
+++ Content.Server/GameObjects/Components/Power/PowerProviderComponent.cs
@@ -10,6 +10,7 @@ using Robust.Shared.IoC;
 using Robust.Shared.Log;
 using Robust.Shared.Serialization;
 using Robust.Shared.ViewVariables;
+using Robust.Shared.Map;
 
 namespace Content.Server.GameObjects.Components.Power
 {
@@ -284,7 +285,8 @@ namespace Content.Server.GameObjects.Components.Power
             if (this == device)
                 return false;
 
-            return (device.Owner.Transform.WorldPosition - Owner.Transform.WorldPosition).Length <= _range;
+            return device.Owner.Transform.MapID == Owner.Transform.MapID &&
+                (device.Owner.Transform.WorldPosition - Owner.Transform.WorldPosition).Length <= _range;
         }
     }
 }
