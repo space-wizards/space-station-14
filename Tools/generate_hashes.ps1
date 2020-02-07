425a277b110a13931cec71ee5a2c95606deffc1c
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-02-07 20:47:23 +0100

    Good job me.

--- Tools/generate_hashes.ps1
+++ Tools/generate_hashes.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/env pwsh
 
-Get-ChildItem release/*.zip | Get-FileHash -Algorithm S | ForEach-Object {
+Get-ChildItem release/*.zip | Get-FileHash -Algorithm SHA256 | ForEach-Object {
     $_.Hash > "$($_.Path).sha256";
 }
