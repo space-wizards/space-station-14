2b089f0272f859facb62ec8acc426b76f60205b7
Sam <Sam.V_@hotmail.com>
2020-04-20 14:48:21 +1000

    Fixed https://github.com/space-wizards/space-station-14/issues/811, chat now refers to Mind object.

--- Content.Server/Chat/ChatCommands.cs
+++ Content.Server/Chat/ChatCommands.cs
@@ -1,10 +1,12 @@
 ﻿using Content.Server.GameObjects.Components.Observer;
 using Content.Server.Interfaces.Chat;
 using Content.Server.Observer;
+using Content.Server.Players;
 using Robust.Server.Interfaces.Console;
 using Robust.Server.Interfaces.Player;
 using Robust.Shared.Enums;
 using Robust.Shared.IoC;
+using Robust.Shared.Log;
 
 namespace Content.Server.Chat
 {
@@ -26,10 +28,17 @@ namespace Content.Server.Chat
 
             var message = string.Join(" ", args);
 
+
             if (player.AttachedEntity.HasComponent<GhostComponent>())
                 chat.SendDeadChat(player, message);
             else
-                chat.EntitySay(player.AttachedEntity, message);
+            {
+                // This is a hacky way of getting the Players Mind
+                var mindComponent = player.ContentData().Mind;
+                // Say the message, as the Mind of the player.
+                chat.EntitySay(mindComponent.OwnedEntity, message);
+            }
+
         }
     }
 
@@ -51,14 +60,17 @@ namespace Content.Server.Chat
 
             var action = string.Join(" ", args);
 
-            chat.EntityMe(player.AttachedEntity, action);
+            // As EntitySay, Hacky way of getting the Player's Mind
+            var mindComponent = player.ContentData().Mind;
+            // Now do the /me as the Player's Mind.
+            chat.EntityMe(mindComponent.OwnedEntity, action);
         }
     }
 
     internal class OOCCommand : IClientCommand
     {
         public string Command => "ooc";
-        public string Description => "Send Out of Character chat messages.";
+        public string Description => "Send Out Of Character chat messages.";
         public string Help => "ooc <text>";
 
         public void Execute(IConsoleShell shell, IPlayerSession player, string[] args)
