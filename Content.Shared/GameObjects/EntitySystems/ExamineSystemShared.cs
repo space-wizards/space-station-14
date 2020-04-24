4f16b68dd08e3b8318cb0471de9dedeacc938877
zumorica <zddm@outlook.es>
2020-04-24 16:21:58 +0200

    Examine now has InRangeUnobstructed test.

--- Content.Shared/GameObjects/EntitySystems/ExamineSystemShared.cs
+++ Content.Shared/GameObjects/EntitySystems/ExamineSystemShared.cs
@@ -1,13 +1,15 @@
+using Content.Server.GameObjects.EntitySystems;
 using Content.Shared.GameObjects.Components.Mobs;
 using JetBrains.Annotations;
 using Robust.Shared.GameObjects.Systems;
 using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
 
 namespace Content.Shared.GameObjects.EntitySystems
 {
     public abstract class ExamineSystemShared : EntitySystem
     {
-        public const float ExamineRange = 8f;
+        public const float ExamineRange = 16f;
         public const float ExamineRangeSquared = ExamineRange * ExamineRange;
 
         [Pure]
@@ -28,8 +30,10 @@ namespace Content.Shared.GameObjects.EntitySystems
                 return false;
             }
 
-            var delta = examined.Transform.WorldPosition - examiner.Transform.WorldPosition;
-            return delta.LengthSquared <= ExamineRangeSquared;
+            return IoCManager.Resolve<IEntitySystemManager>()
+                .GetEntitySystem<SharedInteractionSystem>()
+                .InRangeUnobstructed(examiner.Transform.MapPosition, examined.Transform.MapPosition.Position,
+                    ExamineRange, predicate: entity => entity == examiner || entity == examined, insideBlockerValid:true);
         }
     }
 }
