076fbab236105b1c9e18100e9bfe42501d3e1c5e
Lefouin <jfouin2@protonmail.com>
2020-05-01 18:28:56 +0200

    Add WithVariation.

---
+++ Content.Shared/Audio/AudioHelpers.cs
@@ -0,0 +1,17 @@
+using Content.Shared.GameObjects.Components.Sound;
+using Robust.Shared.Audio;
+using Robust.Shared.GameObjects;
+
+namespace Content.Shared.Audio
+{
+    public static class AudioHelpers{
+        /// <summary>
+        ///     Returns a random pitch.
+        /// </summary>
+        public static WithVariation(float amplitude);
+        {
+            var scale = amplitude * IoCManager.Resolve<IRobustRandom>().NextGaussian();
+            return AudioParams.Default.WithPitchScale(scale);
+        }
+    }
+}
