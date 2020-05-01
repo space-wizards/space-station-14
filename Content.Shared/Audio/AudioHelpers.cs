719b6b2b8f82c23d11a3a48dc696d8ffc46e8c3d
Lefouin <jfouin2@protonmail.com>
2020-05-01 20:21:49 +0200

    Actually making AudioHelpers work

--- Content.Shared/Audio/AudioHelpers.cs
+++ Content.Shared/Audio/AudioHelpers.cs
@@ -1,6 +1,9 @@
 using Content.Shared.GameObjects.Components.Sound;
 using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.Random;
+using Robust.Shared.IoC;
+using Robust.Shared.Random;
 
 namespace Content.Shared.Audio
 {
@@ -11,7 +14,7 @@ namespace Content.Shared.Audio
         public static AudioParams WithVariation(float amplitude)
         {
             var scale = amplitude * IoCManager.Resolve<IRobustRandom>().NextGaussian();
-            return AudioParams.Default.WithPitchScale(scale);
+            return AudioParams.Default.WithPitchScale((float)scale);
         }
     }
 }
