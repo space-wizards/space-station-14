b132e90275840b1e51e754e78cb42925f5a59084
FL-OZ <anotherscuffed@gmail.com>
2020-05-15 12:36:13 -0500

    PDA Foundations 2

--- Content.Client/GameObjects/Components/PDA/PDABoundUserInterface.cs
+++ Content.Client/GameObjects/Components/PDA/PDABoundUserInterface.cs
@@ -1,7 +1,159 @@
+using Content.Client.Utility;
+using Content.Shared.GameObjects.Components.PDA;
+using Robust.Client.GameObjects.Components.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.CustomControls;
+using Robust.Shared.GameObjects.Components.UserInterface;
+using Robust.Shared.Localization;
+using Robust.Shared.Maths;
+
 namespace Content.Client.GameObjects.Components.PDA
 {
-    public class PDABoundUserInterface
+    public class PDABoundUserInterface : BoundUserInterface
     {
-        
+
+        private PDAMenu _menu;
+        private ClientUserInterfaceComponent Owner;
+        public PDABoundUserInterface(ClientUserInterfaceComponent owner, object uiKey) : base(owner, uiKey)
+        {
+            Owner = owner;
+        }
+
+        protected override void Open()
+        {
+            base.Open();
+            _menu = new PDAMenu(this);
+            _menu.OpenToLeft();
+            _menu.OnClose += Close;
+            _menu.FlashLightToggleButton.OnToggled += args =>
+            {
+                SendMessage(new PDAToggleFlashlightMessage());
+
+            };
+
+            _menu.EjectIDButton.OnPressed += args =>
+            {
+                SendMessage(new PDAEjectIDMessage());
+            };
+        }
+
+        protected override void UpdateState(BoundUserInterfaceState state)
+        {
+            base.UpdateState(state);
+            if (!(state is PDAUpdateUserInterfaceState cstate))
+            {
+                return;
+            }
+
+            _menu.FlashLightToggleButton.Pressed = cstate.FlashlightEnabled;
+            _menu.PDAOwnerLabel.SetMarkup(Loc.GetString("Owner: [color=white]{0}[/color]",cstate.PDAOwnerInfo.ActualOwnerName));
+
+            if (cstate.PDAOwnerInfo.JobTitle == null || cstate.PDAOwnerInfo.IDOwner == null)
+            {
+                _menu.IDInfoLabel.SetMarkup(Loc.GetString("ID:"));
+            }
+            else
+            {
+                _menu.IDInfoLabel.SetMarkup(Loc.GetString("ID: [color=white]{0}[/color], [color=yellow]{1}[/color]",
+                    cstate.PDAOwnerInfo.IDOwner,
+                    cstate.PDAOwnerInfo.JobTitle));
+            }
+
+            _menu.EjectIDButton.Visible = cstate.PDAOwnerInfo.IDOwner != null ? true : false;
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _menu?.Dispose();
+
+        }
+
+
+        public class PDAMenu : SS14Window
+        {
+            protected override Vector2? CustomSize => (512, 256);
+
+            private PDABoundUserInterface _owner { get; }
+
+            public Button FlashLightToggleButton { get; }
+            public Button EjectIDButton { get; }
+
+            public RichTextLabel PDAOwnerLabel { get; }
+            public PanelContainer IDInfoContainer { get; }
+            public RichTextLabel IDInfoLabel { get; }
+
+            public PDAMenu(PDABoundUserInterface owner = null)
+            {
+                //CustomMinimumSize = (380, 128);
+                _owner = owner;
+                Title = Loc.GetString("PDA");
+
+                PDAOwnerLabel = new RichTextLabel
+                {
+
+                };
+
+                IDInfoLabel = new RichTextLabel()
+                {
+                    SizeFlagsHorizontal = SizeFlags.FillExpand,
+                };
+
+                EjectIDButton = new Button
+                {
+                    Text = Loc.GetString("Eject ID"),
+                    SizeFlagsHorizontal = SizeFlags.ShrinkCenter,
+                    SizeFlagsVertical = SizeFlags.ShrinkCenter
+
+                };
+
+                var innerHBoxContainer = new HBoxContainer
+                {
+                    Children =
+                    {
+                        IDInfoLabel,
+                        EjectIDButton
+                    }
+                };
+
+                IDInfoContainer = new PanelContainer
+                {
+                    SizeFlagsHorizontal = SizeFlags.Fill,
+                    Children =
+                    {
+                        innerHBoxContainer,
+                    }
+                };
+
+                FlashLightToggleButton = new Button
+                {
+                    Text = Loc.GetString("Toggle Flashlight"),
+                    ToggleMode = true,
+                };
+
+                var vboxContainer = new VBoxContainer
+                {
+                    SizeFlagsVertical = SizeFlags.FillExpand,
+                    SizeFlagsHorizontal = SizeFlags.FillExpand,
+                    CustomMinimumSize = (50,50),
+
+                    Children =
+                    {
+                        PDAOwnerLabel,
+                        IDInfoContainer,
+                        FlashLightToggleButton
+                    }
+                };
+
+                Contents.AddChild(vboxContainer);
+
+            }
+
+
+        }
     }
+
+
+
 }
--- Content.Server/GameObjects/Components/PDA/PDAComponent.cs
+++ Content.Server/GameObjects/Components/PDA/PDAComponent.cs
@@ -1,20 +1,75 @@
 using Content.Server.GameObjects.Components.Access;
 using Content.Server.GameObjects.EntitySystems;
-using Content.Shared.PDA;
+using Content.Shared.GameObjects;
+using Content.Shared.GameObjects.Components.PDA;
+using Robust.Server.GameObjects;
 using Robust.Server.GameObjects.Components.Container;
+using Robust.Server.GameObjects.Components.UserInterface;
+using Robust.Server.Interfaces.GameObjects;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.Network;
+using Robust.Shared.Localization;
+using Robust.Shared.Players;
 
-namespace Content.Server.PDA
+namespace Content.Server.GameObjects.Components.PDA
 {
     [RegisterComponent]
-    public class PDAComponent : SharedPDAComponent, IAttackBy
+    public class PDAComponent : SharedPDAComponent, IAttackBy, IUse
     {
         private Container _idSlot;
+        private PointLightComponent _pdaLight;
+        private bool _lightOn = false;
+        private BoundUserInterface _interface;
+        public bool IdSlotEmpty => _idSlot.ContainedEntities.Count < 1;
+        public IEntity OriginalOwner { get; private set; }
+
+        public IdCardComponent ContainedID { get; private set; }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            _idSlot = ContainerManagerComponent.Ensure<Container>("pda_entity_container", Owner, out var existed);
+            _pdaLight = Owner.GetComponent<PointLightComponent>();
+            _interface = Owner.GetComponent<ServerUserInterfaceComponent>()
+                .GetBoundUserInterface(PDAUiKey.Key);
+            _interface.OnReceiveMessage += UserInterfaceOnReceiveMessage;
+        }
+
+        private void UserInterfaceOnReceiveMessage(ServerBoundUserInterfaceMessage message)
+        {
+            switch (message.Message)
+            {
+                case PDAToggleFlashlightMessage msg:
+                {
+                    ToggleLight();
+                    break;
+                }
+
+                case PDAEjectIDMessage msg:
+                {
+                    HandleIDEjection(message.Session.AttachedEntity);
+                    break;
+                }
+            }
+        }
+
+        private void UpdatePDAUserInterface()
+        {
+            var ownerInfo = new PDAIdInfoText
+            {
+                ActualOwnerName = OriginalOwner.Name,
+                IDOwner = ContainedID?.FullName,
+                JobTitle = ContainedID?.JobTitle
+            };
+
+            _interface.SetState(new PDAUpdateUserInterfaceState(_lightOn,ownerInfo));
+        }
 
         public bool AttackBy(AttackByEventArgs eventArgs)
         {
             var item = eventArgs.AttackWith;
-            if (_idSlot.ContainedEntities.Count > 0)
+            if (!IdSlotEmpty)
             {
                 return false;
             }
@@ -22,9 +77,88 @@ namespace Content.Server.PDA
             if (item.TryGetComponent<IdCardComponent>(out var idCardComponent) && !_idSlot.Contains(item))
             {
                 _idSlot.Insert(item);
+                ContainedID = idCardComponent;
+                UpdatePDAUserInterface();
+                return true;
             }
 
             return false;
         }
+
+        public bool UseEntity(UseEntityEventArgs eventArgs)
+        {
+            if (!eventArgs.User.TryGetComponent(out IActorComponent actor))
+            {
+                return false;
+            }
+            _interface.Open(actor.playerSession);
+
+            return true;
+        }
+
+
+        public override void HandleNetworkMessage(ComponentMessage message, INetChannel netChannel, ICommonSession session = null)
+        {
+            base.HandleNetworkMessage(message, netChannel, session);
+
+        }
+
+        public void SetPDAOwner(IEntity mob)
+        {
+            if (mob == null || mob == OriginalOwner)
+            {
+                return;
+            }
+
+            OriginalOwner = mob;
+            UpdatePDAUserInterface();
+        }
+
+        protected void ToggleLight()
+        {
+            _lightOn = !_lightOn;
+            _pdaLight.Enabled = _lightOn;
+            UpdatePDAUserInterface();
+        }
+
+        protected void HandleIDEjection(IEntity pdaUser)
+        {
+            if (IdSlotEmpty)
+            {
+                return;
+            }
+
+            var cardEntity = ContainedID.Owner;
+            _idSlot.Remove(cardEntity);
+
+            var hands = pdaUser.GetComponent<HandsComponent>();
+            var cardItemComponent = cardEntity.GetComponent<ItemComponent>();
+            hands.PutInHandOrDrop(cardItemComponent);
+            ContainedID = null;
+            UpdatePDAUserInterface();
+        }
+
+
+        [Verb]
+        public sealed class EjectIDVerb : Verb<PDAComponent>
+        {
+            protected override string GetText(IEntity user, PDAComponent component)
+            {
+                return Loc.GetString("Eject ID");
+            }
+
+            protected override VerbVisibility GetVisibility(IEntity user, PDAComponent component)
+            {
+                return component.IdSlotEmpty ? VerbVisibility.Invisible : VerbVisibility.Visible;
+            }
+
+            protected override void Activate(IEntity user, PDAComponent component)
+            {
+                component.HandleIDEjection(user);
+            }
+        }
+
+
+
     }
 }
