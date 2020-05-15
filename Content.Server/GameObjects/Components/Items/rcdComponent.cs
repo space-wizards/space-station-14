6056f60eb5d0b94466cb87ddb460af6aeeed3589
Kmc2000 <bluekorben2000@yahoo.com>
2020-05-15 11:53:02 +0100

    Basic RCD that's kinda scuffed right now

---
+++ Content.Server/GameObjects/Components/Items/rcdComponent.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Content.Server.GameObjects.Components.Stack;
+using Content.Server.GameObjects.EntitySystems;
+using Content.Server.Interfaces;
+using Content.Shared.Maps;
+using Microsoft.EntityFrameworkCore.Internal;
+using Robust.Server.GameObjects.EntitySystems;
+using Robust.Server.Interfaces.GameObjects;
+using Robust.Shared.GameObjects;
+using Robust.Shared.GameObjects.Components.Transform;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.GameObjects.Components;
+using Robust.Shared.Interfaces.Map;
+using Robust.Shared.IoC;
+using Robust.Shared.Localization;
+using Robust.Shared.Map;
+using Robust.Shared.Serialization;
+using Robust.Shared.Utility;
+
+namespace Content.Server.GameObjects.Components.Items
+{
+    [RegisterComponent]
+    public class RcdComponent : Component, IAfterAttack, IUse, IExamine
+    {
+#pragma warning disable 649
+        [Dependency] private readonly ITileDefinitionManager _tileDefinitionManager;
+        [Dependency] private readonly IEntitySystemManager _entitySystemManager;
+        [Dependency] private readonly IMapManager _mapManager;
+        [Dependency] private readonly IServerEntityManager _serverEntityManager;
+#pragma warning restore 649
+
+        public override string Name => "Rapid Construction Device";
+        public string _outputTile = "floor_steel";
+        String[] modes = Enum.GetNames(typeof(RCDmodes));
+        private int mode = 0;
+        private int ammo = 5; //How much "ammo" we have left. You can refille this with RCD ammo.
+
+        private enum RCDmodes //Enum to store the different mode states for clarity.
+        {
+            Floors,
+            Walls,
+            Deconstruct
+        }
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _outputTile, "output", "floor_steel");
+        }
+
+        /**
+         *
+         *    Method called when the RCD is clicked in-hand, this will swap the RCD's mode from "floors" to "walls".
+         *
+         */
+
+        public bool UseEntity(UseEntityEventArgs eventArgs)
+        {
+            SwapMode(eventArgs);
+            return false;
+        }
+
+        public void Activate(ActivateEventArgs eventArgs)
+        {
+            return;
+        }
+
+        public void SwapMode(UseEntityEventArgs eventArgs)
+        {
+            _entitySystemManager.GetEntitySystem<AudioSystem>().Play("/Audio/items/genhit.ogg", Owner);
+            this.mode = (this.mode < modes.Length-1) ? this.mode + 1 : 0;
+            string mode = modes[this.mode];
+            switch (mode)
+            {
+                case("Floors"):
+                    _outputTile = "floor_steel";
+                    break;
+                case("Walls"):
+                    _outputTile = "base_wall";
+                    break;
+                case("Deconstruct"):
+                    _outputTile = "space";
+                    break;
+            }
+            var notify = IoCManager.Resolve<IServerNotifyManager>();
+            notify.PopupMessage(Owner, eventArgs.User, "The RCD will now place "+mode);
+        }
+
+        void IExamine.Examine(FormattedMessage message)
+        {
+            var loc = IoCManager.Resolve<ILocalizationManager>();
+            string mode = Enum.GetNames(typeof(RCDmodes))[this.mode]; //Access the string name of the mode based off of its numerical index in the enum.
+            message.AddMarkup(loc.GetString("It's currently placing "+mode+", and holds "+this.ammo+" charges."));
+        }
+
+        public void AfterAttack(AfterAttackEventArgs eventArgs)
+        {
+            var attacked = eventArgs.Attacked;
+            var mapGrid = _mapManager.GetGrid(eventArgs.ClickLocation.GridID);
+            var tile = mapGrid.GetTileRef(eventArgs.ClickLocation);
+
+            var coordinates = mapGrid.GridTileToLocal(tile.GridIndices);
+            float distance = coordinates.Distance(_mapManager, Owner.Transform.GridPosition);
+
+            if (distance > InteractionSystem.InteractionRange || ammo <= 0)
+            {
+                return;
+            }
+
+            var tileDef = (ContentTileDefinition)_tileDefinitionManager[tile.Tile.TypeId];
+
+                string mode = modes[this.mode];
+                if (mode == "Floors" || mode == "Deconstruct")
+                {
+                    if (!tileDef.IsSubFloor || attacked != null)
+                    {
+                        return;
+                    }
+                    var desiredTile = _tileDefinitionManager[_outputTile];
+                    mapGrid.SetTile(eventArgs.ClickLocation, new Tile(desiredTile.TileId));
+                }
+                else
+                {
+                    if (!_mapManager.TryGetGrid(eventArgs.ClickLocation.GridID, out var grid))
+                    {
+                        return;
+                    }
+                    var snapPos = grid.SnapGridCellFor(eventArgs.ClickLocation, SnapGridOffset.Center);
+                    GridCoordinates snapCoords = grid.GridTileToLocal(snapPos);
+                    var ent = _serverEntityManager.SpawnEntity("solid_wall", snapCoords);
+                    ent.GetComponent<ITransformComponent>().LocalRotation = Transform.LocalRotation;
+
+                }
+
+                _entitySystemManager.GetEntitySystem<AudioSystem>().Play("/Audio/items/deconstruct.ogg", Owner);
+                ammo--;
+
+
+
+        }
+
+    }
+}
