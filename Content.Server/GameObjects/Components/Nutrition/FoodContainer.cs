da61e573d42e4a318c24164392eaa814b57ab8ee
FL-OZ <anotherscuffed@gmail.com>
2020-05-26 17:56:52 -0500

    Rework drink/food/foodcontainer components completely.

---
+++ Content.Server/GameObjects/Components/Nutrition/FoodContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Content.Server.GameObjects.Components.Sound;
+using Content.Server.GameObjects.EntitySystems;
+using Content.Shared.GameObjects.Components.Nutrition;
+using Robust.Server.GameObjects;
+using Robust.Server.GameObjects.Components.Container;
+using Robust.Server.Interfaces.GameObjects;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.Random;
+using Robust.Shared.IoC;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+
+namespace Content.Server.GameObjects.Components.Nutrition
+{
+    [RegisterComponent]
+    public sealed class DrinkFoodContainerComponent : SharedDrinkFoodContainerComponent, IMapInit, IUse
+    {
+        public override string Name => "DrinkFoodContainer";
+        public void MapInit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UseEntity(UseEntityEventArgs eventArgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
