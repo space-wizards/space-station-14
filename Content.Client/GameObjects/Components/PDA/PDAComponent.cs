4c20a504a5661213e03ec3a73adb2f31a2d44f73
FL-OZ <58238103+FL-OZ@users.noreply.github.com>
2020-05-28 06:22:47 -0500

    Add basic PDA/Syndicate Uplink. (#942)
    Co-authored-by: FL-OZ <anotherscuffed@gmail.com>
    Co-authored-by: Pieter-Jan Briers <pieterjan.briers+git@gmail.com>

---
+++ Content.Client/GameObjects/Components/PDA/PDAComponent.cs
@@ -0,0 +1,11 @@
+using Content.Shared.GameObjects.Components.PDA;
+using Robust.Shared.GameObjects;
+
+namespace Content.Client.GameObjects.Components.PDA
+{
+    [RegisterComponent]
+    public class PDAComponent : SharedPDAComponent
+    {
+
+    }
+}
