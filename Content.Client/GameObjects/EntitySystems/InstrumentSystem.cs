f392a78c76f3756aea765f708eeaa6b33ed4ddfc
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-05-29 22:50:41 +0200

    Make instruments prediction-safe.

--- Content.Client/GameObjects/EntitySystems/InstrumentSystem.cs
+++ Content.Client/GameObjects/EntitySystems/InstrumentSystem.cs
@@ -1,11 +1,17 @@
 using Content.Client.GameObjects.Components.Instruments;
+using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Systems;
+using Robust.Shared.Interfaces.Timing;
+using Robust.Shared.IoC;
 
 namespace Content.Client.GameObjects.EntitySystems
 {
+    [UsedImplicitly]
     public class InstrumentSystem : EntitySystem
     {
+        [Dependency] private readonly IGameTiming _gameTiming;
+
         public override void Initialize()
         {
             base.Initialize();
@@ -15,6 +21,11 @@ namespace Content.Client.GameObjects.EntitySystems
         public override void Update(float frameTime)
         {
             base.Update(frameTime);
+
+            if (!_gameTiming.IsFirstTimePredicted)
+            {
+                return;
+            }
 
             foreach (var entity in RelevantEntities)
             {
