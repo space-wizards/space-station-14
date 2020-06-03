8ba013a168e5ae0e8c59b810a0d38f03dddf2c70
py01 <pyronetics01@gmail.com>
2020-06-03 01:17:42 -0600

    Battery Component

---
+++ Content.Server/GameObjects/Components/NodeContainer/NodeGroups/TempPowerCodeFolder/BatteryComponent.cs
@@ -0,0 +1,74 @@
+﻿using Robust.Shared.GameObjects;
+using Robust.Shared.Maths;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+using System;
+
+namespace Content.Server.GameObjects.Components.NewPower
+{
+    [RegisterComponent]
+    public class BatteryComponent : Component
+    {
+        public override string Name => "Battery";
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        public int MaxCharge { get => _maxCharge; set => SetMaxCharge(value); }
+        private int _maxCharge;
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        public float CurrentCharge { get => _currentCharge; set => SetCurrentCharge(value); }
+        private float _currentCharge;
+
+        [ViewVariables]
+        public BatteryState BatteryState { get; private set; }
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _maxCharge, "maxCharge", 1000);
+            serializer.DataField(ref _currentCharge, "startingCharge", 500);
+        }
+
+        public override void OnAdd()
+        {
+            base.OnAdd();
+            UpdateStorageState();
+        }
+
+        private void UpdateStorageState()
+        {
+            if (CurrentCharge == MaxCharge)
+            {
+                BatteryState = BatteryState.Full;
+            }
+            else if (CurrentCharge == 0)
+            {
+                BatteryState = BatteryState.Empty;
+            }
+            else
+            {
+                BatteryState = BatteryState.PartlyFull;
+            }
+        }
+
+        private void SetMaxCharge(int newMax)
+        {
+            _maxCharge = Math.Max(newMax, 0);
+            _currentCharge = Math.Min( _currentCharge, MaxCharge);
+            UpdateStorageState();
+        }
+
+        private void SetCurrentCharge(float newChargeAmount)
+        {
+            _currentCharge = FloatMath.Clamp(newChargeAmount, 0, MaxCharge);
+            UpdateStorageState();
+        }
+    }
+
+    public enum BatteryState
+    {
+        Full,
+        PartlyFull,
+        Empty
+    }
+}
