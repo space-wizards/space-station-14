3331536b779290c487a78a4c1e3f078ecceb10cd
wind <windarkata@gmail.com>
2020-06-08 19:32:56 -0500

    PLEASE NO MORE /n

--- Content.Server/Cargo/CargoOrderDataManager.cs
+++ Content.Server/Cargo/CargoOrderDataManager.cs
@@ -72,7 +72,6 @@ namespace Content.Server.Cargo
                 if (!component.ConnectedToDatabase || component.Database.Id != id)
                     continue;
                 component.Dirty();
-                
             }
         }
 
