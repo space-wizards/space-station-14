e555c2b12896418cc3f8b81fa817994908567a24
Tyler Young <tyler.young@impromptu.ninja>
2020-06-10 20:12:24 -0400

    add complete strings that aren't defined in content files to allow them to be mapped

--- Content.Server/GameObjects/Components/RandomPottedPlantComponent.cs
+++ Content.Server/GameObjects/Components/RandomPottedPlantComponent.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Robust.Server.GameObjects;
 using Robust.Server.Interfaces.GameObjects;
 using Robust.Shared.GameObjects;
@@ -20,6 +21,17 @@ namespace Content.Server.GameObjects.Components
         private string _selectedState;
         private bool _plastic;
 
+        // for shared string dict, since we don't define these anywhere in content
+        [UsedImplicitly]
+        public static readonly string[] plantIdStrings =
+        {
+            "plant-01", "plant-02", "plant-03", "plant-04", "plant-05",
+            "plant-06", "plant-07", "plant-08", "plant-09", "plant-10",
+            "plant-11", "plant-12", "plant-13", "plant-14", "plant-15",
+            "plant-16", "plant-17", "plant-18", "plant-19", "plant-20",
+            "plant-21", "plant-22", "plant-23", "plant-24", "plant-25",
+            "plant-26", "plant-27", "plant-28", "plant-29", "plant-30",
+        };
 
         static RandomPottedPlantComponent()
         {
