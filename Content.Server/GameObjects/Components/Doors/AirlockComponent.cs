4b9a6c18fef1742e3a513c140467e75446ee5c7a
Bright0 <55061890+Bright0@users.noreply.github.com>
2020-06-12 10:51:30 -0600

    Adds hacking UI pop-up when using a hacking tool on an open panel (#1100)
    Co-authored-by: Bright0 <nsmoaksmoakna@gmail.com>

--- Content.Server/GameObjects/Components/Doors/AirlockComponent.cs
+++ Content.Server/GameObjects/Components/Doors/AirlockComponent.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Threading;
 using Content.Server.GameObjects.Components.Interactable;
 using Content.Server.GameObjects.Components.Power;
@@ -224,6 +224,19 @@ namespace Content.Server.GameObjects.Components.Doors
         {
             if (!eventArgs.Using.TryGetComponent<ToolComponent>(out var tool))
                 return false;
+
+            if (tool.HasQuality(ToolQuality.Cutting)
+                || tool.HasQuality(ToolQuality.Multitool))
+            {
+                if (_wires.IsPanelOpen)
+                {
+                    if (eventArgs.User.TryGetComponent(out IActorComponent actor))
+                    {
+                        _wires.OpenInterface(actor.playerSession);
+                        return true;
+                    }
+                }
+            }
 
             if (!tool.UseTool(eventArgs.User, Owner, ToolQuality.Prying)) return false;
 
