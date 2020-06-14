6a8f2db57466f4c8b193f48d641f341116f01370
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-06-14 15:48:48 +0200

    Add break chance to utensils
    Set to 20% for plastic ones

--- Content.Server/GameObjects/Components/Utensil/UtensilComponent.cs
+++ Content.Server/GameObjects/Components/Utensil/UtensilComponent.cs
@@ -3,12 +3,42 @@ using Content.Server.GameObjects.EntitySystems;
 using Content.Server.Utility;
 using Content.Shared.GameObjects.Components.Utensil;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.Random;
+using Robust.Shared.IoC;
+using Robust.Shared.Random;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
 
 namespace Content.Server.GameObjects.Components.Utensil
 {
     [RegisterComponent]
     public class UtensilComponent : SharedUtensilComponent, IAfterInteract
     {
+#pragma warning disable 649
+        [Dependency] private readonly IRobustRandom _random;
+#pragma warning restore 649
+
+        /// <summary>
+        /// The chance that the utensil has to break with each use.
+        /// A value of 0 means that it is unbreakable.
+        /// </summary>
+        [ViewVariables(VVAccess.ReadWrite)]
+        private float _breakChance;
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _breakChance, "breakChance", 0);
+        }
+
+        private void TryBreak()
+        {
+            if (_random.Prob(_breakChance))
+            {
+                Owner.Delete();
+            }
+        }
+
         void IAfterInteract.AfterInteract(AfterInteractEventArgs eventArgs)
         {
             if (eventArgs.Target == null)
@@ -26,7 +56,10 @@ namespace Content.Server.GameObjects.Components.Utensil
                 return;
             }
 
-            food.TryUseFood(eventArgs.User, null);
+            if (food.TryUseFood(eventArgs.User, null))
+            {
+                TryBreak();
+            }
         }
     }
 }
