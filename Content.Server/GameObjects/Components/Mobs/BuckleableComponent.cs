7a56ff1ef1c1d1b904b1f7f6e77b79ced8f22ced
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-06-16 22:51:46 +0200

    Create BuckleableComponent.cs

---
+++ Content.Server/GameObjects/Components/Mobs/BuckleableComponent.cs
@@ -0,0 +1,26 @@
+﻿using Content.Server.GameObjects.Components.Movement;
+using Content.Server.GameObjects.EntitySystems;
+using Robust.Shared.GameObjects;
+using Robust.Shared.ViewVariables;
+
+namespace Content.Server.GameObjects.Components.Mobs
+{
+    public class BuckleableComponent : Component, IActionBlocker, IInteractHand, IMoveSpeedModifier
+    {
+        public override string Name => "Buckleable";
+
+        private bool _buckled = false;
+
+        [ViewVariables] public bool Buckled => _buckled;
+        public float WalkSpeedModifier => 0;
+        public float SprintSpeedModifier => 0;
+
+        public bool InteractHand(InteractHandEventArgs eventArgs)
+        {
+            _buckled = false;
+            return true;
+        }
+
+        public bool CanMove() => !Buckled;
+    }
+}
