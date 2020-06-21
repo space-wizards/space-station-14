8e23f8fd409d8fce754ffbbfda5e12595aaf66d0
Víctor Aguilera Puerto <zddm@outlook.es>
2020-06-21 23:02:58 +0200

    Food trash is optional by default. Fix FoodBase not having a FoodComponent.

--- Content.Server/GameObjects/Components/Nutrition/FoodComponent.cs
+++ Content.Server/GameObjects/Components/Nutrition/FoodComponent.cs
@@ -47,7 +47,7 @@ namespace Content.Server.GameObjects.Components.Nutrition
             base.ExposeData(serializer);
             serializer.DataField(ref _useSound, "useSound", "/Audio/items/eatfood.ogg");
             serializer.DataField(ref _transferAmount, "transferAmount", ReagentUnit.New(5));
-            serializer.DataField(ref _trashPrototype, "trash", "TrashPlate");
+            serializer.DataField(ref _trashPrototype, "trash", null);
 
             if (serializer.Reading)
             {
@@ -168,6 +168,12 @@ namespace Content.Server.GameObjects.Components.Nutrition
 
             if (UsesRemaining > 0)
             {
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(_trashPrototype))
+            {
+                Owner.Delete();
                 return true;
             }
 
