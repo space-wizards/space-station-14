2e4da57c1c2b126383ac20f226e01ba1cf441fec
py01 <pyronetics01@gmail.com>
2020-06-21 15:37:04 -0600

    Makes Power-related components not update on paused entities

--- Content.Server/GameObjects/EntitySystems/BatteryStorageSystem.cs
+++ Content.Server/GameObjects/EntitySystems/BatteryStorageSystem.cs
@@ -1,11 +1,17 @@
 ﻿using Content.Server.GameObjects.Components.Power.PowerNetComponents;
+using Robust.Server.Interfaces.Timing;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Systems;
+using Robust.Shared.IoC;
 
 namespace Content.Server.GameObjects.EntitySystems
 {
     internal class BatteryStorageSystem : EntitySystem
     {
+#pragma warning disable 649
+        [Dependency] private readonly IPauseManager _pauseManager;
+#pragma warning restore 649
+
         public override void Initialize()
         {
             EntityQuery = new TypeEntityQuery(typeof(BatteryStorageComponent));
@@ -15,6 +21,10 @@ namespace Content.Server.GameObjects.EntitySystems
         {
             foreach (var entity in RelevantEntities)
             {
+                if (_pauseManager.IsEntityPaused(entity))
+                {
+                    continue;
+                }
                 entity.GetComponent<BatteryStorageComponent>().Update(frameTime);
             }
         }
--- Content.Server/GameObjects/EntitySystems/PowerApcSystem.cs
+++ Content.Server/GameObjects/EntitySystems/PowerApcSystem.cs
@@ -3,11 +3,17 @@ using Content.Server.GameObjects.Components.NodeContainer.NodeGroups;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Systems;
 using System.Collections.Generic;
+using Robust.Shared.IoC;
+using Robust.Server.Interfaces.Timing;
 
 namespace Content.Server.GameObjects.EntitySystems
 {
     public sealed class ApcSystem : EntitySystem
     {
+#pragma warning disable 649
+        [Dependency] private readonly IPauseManager _pauseManager;
+#pragma warning restore 649
+
         public override void Initialize()
         {
             EntityQuery = new TypeEntityQuery(typeof(ApcComponent));
@@ -18,16 +24,17 @@ namespace Content.Server.GameObjects.EntitySystems
             var uniqueApcNets = new HashSet<IApcNet>(); //could be improved by maintaining set instead of getting collection every frame 
             foreach (var entity in RelevantEntities)
             {
-                uniqueApcNets.Add(entity.GetComponent<ApcComponent>().Net);
+                if (_pauseManager.IsEntityPaused(entity))
+                {
+                    continue;
+                }
+                var apc = entity.GetComponent<ApcComponent>();
+                uniqueApcNets.Add(apc.Net);
+                entity.GetComponent<ApcComponent>().Update();
             }
             foreach (var apcNet in uniqueApcNets)
             {
                 apcNet.Update(frameTime);
-            }
-
-            foreach (var entity in RelevantEntities)
-            {
-                entity.GetComponent<ApcComponent>().Update();
             }
         }
     }
