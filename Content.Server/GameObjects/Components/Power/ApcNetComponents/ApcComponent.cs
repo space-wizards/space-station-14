6dab3072cf78b114021c032918adcf8317f50094
chairbender <kwhipke1@gmail.com>
2020-06-28 16:03:29 -0700

    #200 merge in latest changes from master

--- Content.Server/GameObjects/Components/Power/ApcNetComponents/ApcComponent.cs
+++ Content.Server/GameObjects/Components/Power/ApcNetComponents/ApcComponent.cs
@@ -11,6 +11,7 @@ using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Systems;
 using Robust.Shared.ViewVariables;
 using System;
+using Content.Server.Interfaces.GameObjects.Components.Interaction;
 using Robust.Shared.IoC;
 using Robust.Shared.Interfaces.Timing;
 
