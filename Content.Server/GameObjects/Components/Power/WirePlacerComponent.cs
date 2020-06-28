33eced3112996d9f510d5d2d2707b89086c760e2
py01 <pyronetics01@gmail.com>
2020-06-27 18:32:51 -0600

    Redo of WirePlacer's check for whether or not there is already a wire on a tile

--- Content.Server/GameObjects/Components/Power/WirePlacerComponent.cs
+++ Content.Server/GameObjects/Components/Power/WirePlacerComponent.cs
@@ -1,14 +1,17 @@
-﻿using Content.Server.GameObjects.Components.Stack;
+﻿using Content.Server.GameObjects.Components.NodeContainer;
+using Content.Server.GameObjects.Components.NodeContainer.NodeGroups;
+using Content.Server.GameObjects.Components.NodeContainer.Nodes;
+using Content.Server.GameObjects.Components.Stack;
 using Content.Server.GameObjects.EntitySystems;
 using Content.Server.Utility;
-using Robust.Server.GameObjects;
 using Robust.Server.Interfaces.GameObjects;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Components.Transform;
 using Robust.Shared.Interfaces.Map;
 using Robust.Shared.IoC;
-using Robust.Shared.Map;
 using Robust.Shared.Serialization;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Content.Server.GameObjects.Components.Power
 {
@@ -25,50 +28,45 @@ namespace Content.Server.GameObjects.Components.Power
 
         private string _wirePrototypeID;
 
+        /// <summary>
+        ///     When placing a wire, if there is a <see cref="NodeContainerComponent"/> whose
+        ///     <see cref="Node"/>s have the same set of <see cref="Node.NodeGroupID"/>s
+        ///     as this, a wire will not be placed. Should generally be the same set of
+        ///     <see cref="NodeGroupID"/>s as the type of wire to be placed.
+        /// </summary>
+        private List<NodeGroupID> _blockingNodeGroupIDs;
+
         public override void ExposeData(ObjectSerializer serializer)
         {
             base.ExposeData(serializer);
             serializer.DataField(ref _wirePrototypeID, "wirePrototypeID", "HVWire");
+            serializer.DataField(ref _blockingNodeGroupIDs, "blockingNodeGroupIDs", new List<NodeGroupID> { NodeGroupID.HVPower });
         }
 
         /// <inheritdoc />
         public void AfterInteract(AfterInteractEventArgs eventArgs)
         {
             if (!InteractionChecks.InRangeUnobstructed(eventArgs)) return;
-
             if(!_mapManager.TryGetGrid(eventArgs.ClickLocation.GridID, out var grid))
                 return;
-
             var snapPos = grid.SnapGridCellFor(eventArgs.ClickLocation, SnapGridOffset.Center);
             var snapCell = grid.GetSnapGridCell(snapPos, SnapGridOffset.Center);
-
             if(grid.GetTileRef(snapPos).Tile.IsEmpty)
                 return;
-
-            var found = false;
             foreach (var snapComp in snapCell)
             {
-                if (snapComp.Owner.Prototype.ID != _wirePrototypeID)
-                    continue;
-
-                found = true;
-                break;
+                if (snapComp.Owner.TryGetComponent<NodeContainerComponent>(out var nodeContainer))
+                {
+                    var nodeGroupIDs = nodeContainer.Nodes.Select(node => node.NodeGroupID).ToList();
+                    if (_blockingNodeGroupIDs.All(nodeGroupIDs.Contains))
+                    {
+                        return;
+                    }
+                }
             }
-
-            if (found)
-                return;
-
-            bool hasItemSpriteComp = Owner.TryGetComponent(out SpriteComponent itemSpriteComp);
-
             if (Owner.TryGetComponent(out StackComponent stack) && !stack.Use(1))
                 return;
-
-            GridCoordinates coordinates = grid.GridTileToLocal(snapPos);
-            var newWire = _entityManager.SpawnEntity(_wirePrototypeID, coordinates);
-            if (newWire.TryGetComponent(out SpriteComponent wireSpriteComp) && hasItemSpriteComp)
-            {
-                wireSpriteComp.Color = itemSpriteComp.Color;
-            }
+            _entityManager.SpawnEntity(_wirePrototypeID, grid.GridTileToLocal(snapPos));
         }
     }
 }
