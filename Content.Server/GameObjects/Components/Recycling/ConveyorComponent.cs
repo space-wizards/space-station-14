0210913ce1e9073a978b733f2180c20e8fc75319
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-07-02 18:51:00 +0200

    Add ConveyorComponent

---
+++ Content.Server/GameObjects/Components/Recycling/ConveyorComponent.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.GameObjects;
+using Robust.Shared.GameObjects.Components;
+using Robust.Shared.Interfaces.GameObjects;
+
+namespace Content.Server.GameObjects.Components.Recycling
+{
+    [RegisterComponent]
+    public class ConveyorComponent : Component, ICollideBehavior
+    {
+        public override string Name => "Conveyor";
+
+        public void CollideWith(IEntity collidedWith)
+        {
+            collidedWith.Transform.LocalPosition += Owner.Transform.LocalRotation.ToVec() / 10;
+        }
+    }
+}
