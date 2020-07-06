c86c935f57e2ef1e48cda5b7cd006eb3e90827a9
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-07-06 20:30:02 +0200

    Added angle and speed properties to ConveyorComponent

--- Content.Server/GameObjects/Components/Recycling/ConveyorComponent.cs
+++ Content.Server/GameObjects/Components/Recycling/ConveyorComponent.cs
@@ -1,6 +1,8 @@
 ﻿using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Components;
 using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Maths;
+using Robust.Shared.Serialization;
 
 namespace Content.Server.GameObjects.Components.Recycling
 {
@@ -9,9 +11,38 @@ namespace Content.Server.GameObjects.Components.Recycling
     {
         public override string Name => "Conveyor";
 
-        public void CollideWith(IEntity collidedWith)
+        /// <summary>
+        ///     The angle in radians to move entities by in relation
+        ///     to the owner's rotation.
+        ///     Parsed from YAML as degrees.
+        /// </summary>
+        private double _angle;
+
+        /// <summary>
+        ///     The amount of units to move the entity by.
+        /// </summary>
+        private float _speed;
+
+        private Angle GetAngle()
         {
-            collidedWith.Transform.LocalPosition += Owner.Transform.LocalRotation.ToVec() / 10;
+            return new Angle(Owner.Transform.LocalRotation.Theta + _angle);
+        }
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+
+            double degrees = 0;
+            serializer.DataField(ref degrees, "angle", 0);
+
+            _angle = MathHelper.DegreesToRadians(degrees);
+
+            serializer.DataField(ref _speed, "speed", 0.1f);
+        }
+
+        void ICollideBehavior.CollideWith(IEntity collidedWith)
+        {
+            collidedWith.Transform.LocalPosition += GetAngle().ToVec() * _speed;
         }
     }
 }
