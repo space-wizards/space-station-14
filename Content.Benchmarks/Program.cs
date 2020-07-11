db0026a365e6c218eb063b58718b17c06165aac9
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-07-11 13:21:34 +0200

    ComponentFetchBenchmark

--- Content.Benchmarks/Program.cs
+++ Content.Benchmarks/Program.cs
@@ -6,8 +6,7 @@ namespace Content.Benchmarks
     {
         public static void Main(string[] args)
         {
-            BenchmarkRunner.Run<ComponentManagerGetAllComponents>();
-            //ComponentManagerGetAllComponents.TestRun();
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
 }
