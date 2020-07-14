818c00cc43b241e173ce2cf566fd95f96c97a3db
py01 <pyronetics01@gmail.com>
2020-07-14 15:49:53 -0600

    ConduitNode

--- Content.Server/GameObjects/Components/NodeContainer/Nodes/AdjacentNode.cs
+++ Content.Server/GameObjects/Components/NodeContainer/Nodes/AdjacentNode.cs
@@ -1,5 +1,4 @@
 ﻿using Robust.Shared.GameObjects.Components.Transform;
-using Robust.Shared.Serialization;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,17 +7,16 @@ namespace Content.Server.GameObjects.Components.NodeContainer.Nodes
     /// <summary>
     ///     A <see cref="Node"/> that can reach other <see cref="AdjacentNode"/>s that are directly adjacent to it.
     /// </summary>
-    public class AdjacentNode : Node
+    public class AdjacentNode : ConduitNode
     {
-        protected override IEnumerable<Node> GetReachableNodes()
+        protected override IEnumerable<Node> GetNodesToConsider()
         {
             return Owner.GetComponent<SnapGridComponent>()
                 .GetCardinalNeighborCells()
                 .SelectMany(sgc => sgc.GetLocal())
                 .Select(entity => entity.TryGetComponent<NodeContainerComponent>(out var container) ? container : null)
                 .Where(container => container != null)
-                .SelectMany(container => container.Nodes)
-                .Where(node => node != null && node != this);
+                .SelectMany(container => container.Nodes);
         }
     }
 }
---
+++ Content.Server/GameObjects/Components/NodeContainer/Nodes/ConduitNode.cs
@@ -0,0 +1,53 @@
+﻿using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Content.Server.GameObjects.Components.NodeContainer.Nodes
+{
+    public abstract class ConduitNode : Node
+    {
+        [ViewVariables]
+        public ConduitLayer ConduitLayer { get; private set; }
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(this, x => ConduitLayer, "conduitLayer", ConduitLayer.First);
+        }
+
+        protected override IEnumerable<Node> GetReachableNodes()
+        {
+            return GetMatchingLayerNodes(GetNodesToConsider())
+                .Where(node => node != null && node != this);
+        }
+
+        /// <summary>
+        ///     Filters out <see cref="ConduitNode"/>s with a different <see cref="ConduitNode.ConduitLayer"/> from a set of <see cref="Node"/>s.
+        ///     Does not remove non-<see cref="ConduitNode"/> <see cref="Node"/>s.
+        /// </summary>
+        protected IEnumerable<Node> GetMatchingLayerNodes(IEnumerable<Node> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                if (!(node is ConduitNode conduitNode) || (conduitNode.ConduitLayer & ConduitLayer) != ConduitLayer.None)
+                {
+                    yield return node;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Gets a set of <see cref="Node"/>s that this should consider connecting to.
+        /// </summary>
+        protected abstract IEnumerable<Node> GetNodesToConsider();
+    }
+
+    public enum ConduitLayer
+    {
+        None     = 0,
+        First    = 1 << 0,
+        Second   = 1 << 1,
+        Third    = 1 << 2,
+    }
+}
