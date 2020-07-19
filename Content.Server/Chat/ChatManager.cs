d3f8f03368e19d79190f74f2624b5e047460aae0
Bright0 <nsmoaksmoakna@gmail.com>
2020-07-19 15:48:52 -0600

    added listener component and radio prototype

--- Content.Server/Chat/ChatManager.cs
+++ Content.Server/Chat/ChatManager.cs
@@ -14,6 +14,10 @@ using Robust.Shared.Interfaces.Network;
 using Robust.Shared.IoC;
 using Robust.Shared.Localization;
 using Robust.Shared.Log;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using Content.Server.GameObjects.Components;
+using System.Collections.Generic;
 
 namespace Content.Server.Chat
 {
@@ -71,6 +75,19 @@ namespace Content.Server.Chat
             msg.MessageWrap = $"{source.Name} says, \"{{0}}\"";
             msg.SenderEntity = source.Uid;
             _netManager.ServerSendToMany(msg, clients.ToList());
+
+            var entities = source.EntityManager.GetEntitiesInRange(pos, VoiceRange);
+            if (entities.Count() > 0)
+            {
+                foreach (var entity in entities)
+                {
+                    if (entity.TryGetComponent<ListeningComponent>(out ListeningComponent listener))
+                    {
+                        listener.HeardSpeech(message);
+                    }
+                        
+                }
+            }
         }
 
         public void EntityMe(IEntity source, string action)
