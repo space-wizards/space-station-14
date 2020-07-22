1eec6467950f969c41e9b28a4ccb3b4f35794fbe
Víctor Aguilera Puerto <zddm@outlook.es>
2020-07-23 01:15:16 +0200

    Start adding stuff for gas reactions

---
+++ Content.Server/Atmos/GasReactionPrototype.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Content.Server.Interfaces;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+using YamlDotNet.RepresentationModel;
+
+namespace Content.Server.Atmos
+{
+    [Prototype("gasReaction")]
+    public class GasReactionPrototype : IPrototype, IIndexedPrototype
+    {
+        public string ID { get; }
+
+        /// <summary>
+        ///     Lower numbers are checked/react later than higher numbers.
+        ///     If two reactions have the same priority, they may happen in either order.
+        /// </summary>
+        public int Priority { get; }
+        private List<IGasReactionEffect> _effects;
+
+        public void LoadFrom(YamlMappingNode mapping)
+        {
+            var serializer = YamlObjectSerializer.NewReader(mapping);
+
+            serializer.DataField(this, x => ID, "id", string.Empty);
+            serializer.DataField(this, x => Priority, "priority", 100);
+        }
+    }
+}
---
+++ Content.Server/Interfaces/IGasReactionEffect.cs
@@ -0,0 +1,10 @@
+﻿using Content.Server.Atmos;
+using Robust.Shared.Map;
+
+namespace Content.Server.Interfaces
+{
+    public interface IGasReactionEffect
+    {
+        void React(GasMixture mixture, GridCoordinates coordinates);
+    }
+}
