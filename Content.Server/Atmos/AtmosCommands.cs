e4bcd567dc05d633dbb11c85e24944efe7066b37
Víctor Aguilera Puerto <zddm@outlook.es>
2020-07-23 02:42:40 +0200

    Fill gas command

--- Content.Server/Atmos/AtmosCommands.cs
+++ Content.Server/Atmos/AtmosCommands.cs
@@ -132,6 +132,61 @@ namespace Content.Server.Atmos
         }
     }
 
+        public class FillGas : IClientCommand
+    {
+        public string Command => "fillgas";
+        public string Description => "Adds gas to all tiles in a grid.";
+        public string Help => "fillgas <GridId> <Gas> <moles>";
+        public void Execute(IConsoleShell shell, IPlayerSession? player, string[] args)
+        {
+            var gasId = -1;
+            var gas = (Gas) (-1);
+            if (args.Length < 3) return;
+            if(!int.TryParse(args[0], out var id)
+               || !(int.TryParse(args[1], out gasId) || Enum.TryParse(args[1], out gas))
+               || !float.TryParse(args[2], out var moles)) return;
+
+            var gridId = new GridId(id);
+
+            var mapMan = IoCManager.Resolve<IMapManager>();
+
+            if (!gridId.IsValid() || !mapMan.TryGetGrid(gridId, out var gridComp))
+            {
+                shell.SendText(player, "Invalid grid ID.");
+                return;
+            }
+
+            var entMan = IoCManager.Resolve<IEntityManager>();
+
+            if (!entMan.TryGetEntity(gridComp.GridEntityId, out var grid))
+            {
+                shell.SendText(player, "Failed to get grid entity.");
+                return;
+            }
+
+            if (!grid.HasComponent<GridAtmosphereComponent>())
+            {
+                shell.SendText(player, "Grid doesn't have an atmosphere.");
+                return;
+            }
+
+            var gam = grid.GetComponent<GridAtmosphereComponent>();
+
+            foreach (var tile in gam)
+            {
+                if (gasId != -1)
+                {
+                    tile.Air?.Add(gasId, moles);
+                    gam.Invalidate(tile.GridIndices);
+                    continue;
+                }
+
+                tile.Air?.Add(gas, moles);
+                gam.Invalidate(tile.GridIndices);
+            }
+        }
+    }
+
     public class RemoveGas : IClientCommand
     {
         public string Command => "removegas";
