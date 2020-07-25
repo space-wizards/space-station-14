3e75ba4b8f2a107b57f62d907f10315b83d46ec4
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-07-25 12:47:21 +0200

    Add AddEntity method

--- Content.Tools/Map.cs
+++ Content.Tools/Map.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Robust.Shared.GameObjects.Systems;
 using Robust.Shared.Utility;
 using YamlDotNet.RepresentationModel;
 
@@ -35,6 +34,24 @@ namespace Content.Tools
 
         private uint MaxId { get; set; }
 
+        private void AddEntity(YamlMappingNode node)
+        {
+            var uid = uint.Parse(node["uid"].AsString());
+
+            if (uid > MaxId)
+            {
+                MaxId = uid;
+            }
+            else
+            {
+                uid = MaxId + 1;
+                MaxId++;
+                node.Children["uid"] = uid.ToString();
+            }
+
+            GetEntitiesNode().Add(node);
+        }
+
         private YamlSequenceNode GetEntitiesNode()
         {
             return (YamlSequenceNode) Root["entities"];
@@ -55,36 +72,27 @@ namespace Content.Tools
 
         private void Parse()
         {
+            var oldMaxId = MaxId;
+
             Entities = ParseEntities();
             MaxId = Entities.Max(entry => entry.Key);
+
+            DebugTools.Assert(oldMaxId == MaxId);
         }
 
         public void Merge(Map other)
         {
-            var maxId = MaxId;
-
             foreach (var (id, otherEntity) in other.ParseEntities())
             {
-                if (!Entities.TryGetValue(id, out var thisEntity))
+                if (!Entities.TryGetValue(id, out var thisEntity) ||
+                    !thisEntity.Equals(otherEntity))
                 {
-                    GetEntitiesNode().Add(otherEntity);
+                    AddEntity(otherEntity);
                     return;
                 }
-
-                if (thisEntity.Equals(otherEntity))
-                {
-                    continue;
-                }
-
-                otherEntity.Children["uid"] = (maxId + 1).ToString();
-                maxId++;
-
-                GetEntitiesNode().Add(otherEntity);
             }
 
             Parse();
-
-            DebugTools.Assert(maxId == MaxId);
         }
     }
 }
