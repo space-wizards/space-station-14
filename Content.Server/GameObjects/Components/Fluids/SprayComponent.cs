19d0e0dab2e3790ee42421cf0138e082f5ec7c01
Exp <theexp111@gmail.com>
2020-07-27 18:37:28 +0200

    Spray Bottle

---
+++ Content.Server/GameObjects/Components/Fluids/SprayComponent.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Content.Server.GameObjects.Components.Chemistry;
+using Content.Server.GameObjects.EntitySystems;
+using Content.Server.Interfaces;
+using Content.Server.Utility;
+using Content.Shared.Chemistry;
+using Content.Shared.GameObjects;
+using Content.Shared.GameObjects.EntitySystems;
+using Content.Shared.Interfaces.GameObjects.Components;
+using NFluidsynth;
+using Robust.Server.GameObjects.EntitySystems;
+using Robust.Shared.GameObjects;
+using Robust.Shared.GameObjects.Systems;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.Map;
+using Robust.Shared.IoC;
+using Robust.Shared.Localization;
+using Robust.Shared.Map;
+using Robust.Shared.Maths;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+
+namespace Content.Server.GameObjects.Components.Fluids
+{
+    [RegisterComponent]
+    class SprayComponent : Component, IAfterInteract
+    {
+#pragma warning disable 649
+        [Dependency] private readonly IMapManager _mapManager;
+        [Dependency] private readonly IServerNotifyManager _notifyManager = default!;
+#pragma warning restore 649
+        public override string Name => "Spray";
+
+        private ReagentUnit _transferAmount;
+        private int _sprayRange;
+        private string _spraySound;
+
+        /// <summary>
+        ///     The amount of solution to be sprayer from this solution when using it
+        /// </summary>
+        [ViewVariables]
+        public ReagentUnit TransferAmount
+        {
+            get => _transferAmount;
+            set => _transferAmount = value;
+        }
+
+        private SolutionComponent _contents;
+        public ReagentUnit CurrentVolume => _contents.CurrentVolume;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            _contents = Owner.GetComponent<SolutionComponent>();
+        }
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _transferAmount, "transferAmount", ReagentUnit.New(5.0));
+            serializer.DataField(ref _sprayRange, "sprayRange", 3);
+            serializer.DataField(ref _spraySound, "spraySound", string.Empty);
+        }
+
+        List<GridCoordinates> GetTilesBetween(GridCoordinates origin, GridCoordinates end, int range = 100)
+        {
+            var tiles = new List<GridCoordinates>();
+            int dx = Math.Abs((int)(end.X - origin.X)), sx = origin.X < end.X ? 1 : -1;
+            int dy = Math.Abs((int)(end.Y - origin.Y)), sy = origin.Y < end.Y ? 1 : -1;
+            int err = (dx > dy ? dx : -dy) / 2, e2;
+            for (int i = 0; i <= range; i++)
+            {
+                tiles.Add(origin);
+                if (origin.X == end.X && origin.Y == end.Y) break;
+                e2 = err;
+                if (e2 > -dx) { err -= dy; origin = origin.Offset(new Vector2(sx, 0)); }
+                if (e2 < dy) { err += dx; origin = origin.Offset(new Vector2(0, sy)); }
+            }
+            return tiles;
+        }
+
+        void IAfterInteract.AfterInteract(AfterInteractEventArgs eventArgs)
+        {
+            if (CurrentVolume <= 0)
+            {
+                _notifyManager.PopupMessage(Owner, eventArgs.User, Loc.GetString("Not enough left!"));
+                return;
+            }
+
+            var playerPos = eventArgs.User.Transform.GridPosition;
+            if (eventArgs.ClickLocation.GridID != playerPos.GridID)
+                return;
+
+            var mapGrid = _mapManager.GetGrid(eventArgs.ClickLocation.GridID);
+
+            var clickTile = mapGrid.GetTileRef(eventArgs.ClickLocation);
+            var clickCoordinates = mapGrid.GridTileToLocal(clickTile.GridIndices);
+
+            var playerTile = mapGrid.GetTileRef(playerPos);
+            var playerCoord = mapGrid.GridTileToLocal(playerTile.GridIndices);
+
+            // Get the tiles we want to spray things at
+            List<GridCoordinates> tiles;
+            if (clickCoordinates == playerCoord) // clicked on the tile standing on
+            {
+                tiles = new List<GridCoordinates>() { playerCoord };
+            }
+            else
+            {
+                tiles = GetTilesBetween(playerCoord, clickCoordinates, _sprayRange);
+                tiles.RemoveAt(0); // don't use the one we're standing on
+            }
+
+            //Play sound
+            EntitySystem.Get<AudioSystem>().PlayFromEntity(_spraySound, Owner);
+
+            // Spray the tiles, half the amount each step
+            var intSys = EntitySystem.Get<SharedInteractionSystem>();
+            ReagentUnit amount = TransferAmount;
+            foreach (var tile in tiles)
+            {
+                if (CurrentVolume == 0 || Math.Round(amount.Float()) == 0) // no need to create empty puddles
+                    break;
+
+                var mapCoords = tile.ToMap(_mapManager);
+
+                //FIXME: this doesn't work as wanted yet (stopped by walls, but also by open doors)
+                if (intSys.InRangeUnobstructed(eventArgs.User.Transform.MapPosition, mapCoords, range: _sprayRange, ignoredEnt: eventArgs.User))
+                {
+                    //TODO: maybe remove the sound? and maybe the puddle overflow
+                    SpillHelper.SpillAt(tile, _contents.SplitSolution(amount), "PuddleSmear"); //make non PuddleSmear?
+                    amount = amount * 0.5;
+                }
+                else // found wall and that stops the spray
+                {
+                    break; 
+                }
+            }
+        }
+    }
+}
