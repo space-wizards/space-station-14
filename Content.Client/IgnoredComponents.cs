7c5bc542e65880699e438584d5d1bd87a29819c5
py01 <pyronetics01@gmail.com>
2020-07-26 19:49:44 -0600

    ignored component

--- Content.Client/IgnoredComponents.cs
+++ Content.Client/IgnoredComponents.cs
@@ -59,7 +59,7 @@
             "IdCardConsole",
             "Airlock",
             "MedicalScanner",
-            "ConduitPlacer",
+            "WirePlacer",
             "Drink",
             "Food",
             "FoodContainer",
