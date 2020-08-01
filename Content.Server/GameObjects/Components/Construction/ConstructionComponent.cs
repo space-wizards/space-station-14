c97e004945e8391b351a1da470f2eda6695ab744
Exp <theexp111@gmail.com>
2020-08-01 14:55:36 +0200

    Construction Examine

--- Content.Server/GameObjects/Components/Construction/ConstructionComponent.cs
+++ Content.Server/GameObjects/Components/Construction/ConstructionComponent.cs
@@ -1,6 +1,10 @@
-﻿using Content.Shared.Construction;
+﻿using Content.Server.GameObjects.EntitySystems.Click;
+using Content.Shared.Construction;
 using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Localization;
 using Robust.Shared.Serialization;
+using Robust.Shared.Utility;
 using Robust.Shared.ViewVariables;
 
 namespace Content.Server.GameObjects.Components.Construction
@@ -9,8 +13,11 @@ namespace Content.Server.GameObjects.Components.Construction
     /// Holds data about an entity that is in the process of being constructed or destructed.
     /// </summary>
     [RegisterComponent]
-    public class ConstructionComponent : Component
+    public class ConstructionComponent : Component, IExamine
     {
+#pragma warning disable 649
+        [Dependency] private readonly ILocalizationManager _loc;
+#pragma warning restore 649
         /// <inheritdoc />
         public override string Name => "Construction";
 
@@ -36,6 +43,29 @@ namespace Content.Server.GameObjects.Components.Construction
 
             serializer.DataReadWriteFunction("stage", 0,
                 value => Stage = value, () => Stage);
+        }
+
+        void IExamine.Examine(FormattedMessage message, bool inDetailsRange)
+        {
+            var stages = Prototype.Stages;
+            if (Stage > 0 && Stage < stages.Count)
+            {
+                var curStage = stages[Stage];
+                if (curStage.Backward != null && curStage.Backward is ConstructionStepTool)
+                {
+                    var backward = (ConstructionStepTool) curStage.Backward;
+                    message.AddText(_loc.GetString("To deconstruct: {0}x {1} Tool", backward.Amount, backward.ToolQuality));
+                }
+                if (curStage.Forward != null && curStage.Forward is ConstructionStepMaterial)
+                {
+                    if (curStage.Backward != null)
+                    {
+                        message.AddText("\n");
+                    }
+                    var forward = (ConstructionStepMaterial) curStage.Forward;
+                    message.AddText(_loc.GetString("To construct: {0}x {1}", forward.Amount, forward.Material));
+                }
+            }
         }
     }
 }
