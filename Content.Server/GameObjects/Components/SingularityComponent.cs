35894b1941b463055cbd6e4ca24f7f4ad35670cc
unusualcrow <unusualcrow@protonmail.com>
2020-08-02 15:59:26 -0400

    progress on component

--- Content.Server/GameObjects/Components/SingularityComponent.cs
+++ Content.Server/GameObjects/Components/SingularityComponent.cs
@@ -4,21 +4,27 @@ using System.Diagnostics;
 using System.Text;
 using System.Threading;
 using Content.Server.GameObjects.Components.Power.PowerNetComponents;
+using Content.Server.GameObjects.Components.Sound;
 using Content.Server.GameObjects.EntitySystems;
 using Content.Server.Interfaces;
 using Content.Server.Interfaces.Chat;
 using Content.Server.Interfaces.GameObjects.Components.Interaction;
+using Content.Shared.GameObjects.Components.Sound;
 using Content.Shared.Interfaces.GameObjects.Components;
+using Content.Shared.Maps;
 using Content.Shared.Physics;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Robust.Server.GameObjects;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Components;
 using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.Map;
 using Robust.Shared.IoC;
 using Robust.Shared.Localization;
 using Robust.Shared.Log;
 using Robust.Shared.Maths;
 using Robust.Shared.Map;
+using Robust.Shared.Physics;
 using Robust.Shared.ViewVariables;
 using Timer = Robust.Shared.Timers.Timer;
 
@@ -28,6 +34,10 @@ namespace Content.Server.GameObjects.Components
     public class SingularityComponent : Component, ICollideBehavior
     {
         public override string Name => "Singularity";
+
+        public int Energy = 100;
+        public int Level = 1;
+
         private float _range;
 
         private Random rand = new Random();
@@ -37,35 +47,123 @@ namespace Content.Server.GameObjects.Components
 
         private ICollidableComponent _collidableComponent;
 
+        private IMapManager _mapManager;
+
+        private SpriteComponent _spriteComponent;
+
+        public List<IEntity> DeleteList = new List<IEntity>();
+
+
         public override void Initialize()
         {
             base.Initialize();
 
             _collidableComponent = Owner.GetComponent<ICollidableComponent>();
             _collidableComponent.Hard = false;
+
+            _spriteComponent = Owner.GetComponent<SpriteComponent>();
+
             _singularityController = _collidableComponent.EnsureController<SingularityController>();
             _singularityController.ControlledComponent = _collidableComponent;
+
             _entityManager = IoCManager.Resolve<IEntityManager>();
+            _mapManager = IoCManager.Resolve<IMapManager>();
+
 
         }
 
         public void Update()
         {
+            Energy--;
+
             _singularityController.Push(new Vector2((rand.Next(-10, 10)), rand.Next(-10, 10)).Normalized, 5f);
 
             foreach (var entity in _entityManager.GetEntitiesInRange(Owner.Transform.GridPosition, _range))
             {
-                if (entity.HasComponent<RadiationPanel>())
+                if (entity.IsValid())
                 {
-                    entity.GetComponent<RadiationPanel>().Radiation += 200;
+                    if (entity.HasComponent<RadiationPanel>())
+                    {
+                        entity.GetComponent<RadiationPanel>().Radiation += 200;
+                    }
                 }
             }
+
+            UpdateLevel();
+        }
+
+        public void TileUpdate()
+        {
+            IMapGrid mapGrid = _mapManager.GetGrid(Owner.Transform.GridID);
+            foreach (TileRef tile in mapGrid.GetTilesIntersecting(_collidableComponent.WorldAABB))
+            {
+                mapGrid.SetTile(tile.GridIndices, Tile.Empty);
+            }
+        }
+
+        public void UpdateLevel()
+        {
+            int prevLevel = Level;
+            float radius = 0.5f;
+
+            if (Energy == 0)
+            {
+                //collapse
+            }
+
+            if (Energy < 200)
+            {
+                Level = 1;
+            }
+
+            if (Energy >= 200)
+            {
+                Level = 2;
+                radius = 1.5f;
+            }
+
+            if (Energy >= 300)
+            {
+                Level = 3;
+                radius = 2.5f;
+            }
+
+            if (Energy >= 600)
+            {
+                Level = 4;
+                radius = 3.5f;
+            }
+
+            if (Energy >= 1000)
+            {
+                Level = 5;
+                radius = 4.5f;
+            }
+
+            if (Energy >= 1500)
+            {
+                Level = 6;
+                radius = 5.5f;
+            }
+
+            if (Level != prevLevel)
+            {
+                _spriteComponent.LayerSetRSI(0, "Effects/Singularity/singularity_" + Level.ToString() + ".rsi");
+                _spriteComponent.LayerSetState(0, "singularity_" + Level.ToString());
+
+                (_collidableComponent.PhysicsShapes[0] as PhysShapeCircle).Radius = radius;
+
+                _range = radius * 2;
+            }
+
         }
 
         void ICollideBehavior.CollideWith(IEntity entity)
         {
+            Energy++;
 
-            entity.Delete();
+            //This avoids a peculiar bug with getting components on deleted entities
+            DeleteList.Add(entity);
         }
 
     }
--- Content.Server/GameObjects/EntitySystems/SingularitySystem.cs
+++ Content.Server/GameObjects/EntitySystems/SingularitySystem.cs
@@ -1,25 +1,46 @@
 using Content.Server.GameObjects.Components;
 using Robust.Shared.GameObjects.Systems;
+using Robust.Shared.Interfaces.GameObjects;
 using Robust.Shared.Log;
 
 namespace Content.Server.GameObjects.EntitySystems
 {
     public class SingularitySystem : EntitySystem
     {
-
+        private int tick;
         float curTime;
-
         public override void Update(float frameTime)
         {
+            tick++;
             base.Update(frameTime);
 
             curTime += frameTime;
 
             if (curTime >= 1f)
             {
-                curTime = 0f;
-                foreach (var singulo in ComponentManager.EntityQuery<SingularityComponent>()) {
+                curTime -= 1f;
+                foreach (var singulo in ComponentManager.EntityQuery<SingularityComponent>())
+                {
                     singulo.Update();
+                }
+            }
+
+            foreach (var singulo in ComponentManager.EntityQuery<SingularityComponent>())
+            {
+                foreach(var entity in singulo.DeleteList)
+                {
+                    entity.Delete();
+                }
+
+                singulo.DeleteList.Clear();
+            }
+
+            if (tick == 4)
+            {
+                tick = 0;
+                foreach (var singulo in ComponentManager.EntityQuery<SingularityComponent>())
+                {
+                    singulo.TileUpdate();
                 }
             }
         }
