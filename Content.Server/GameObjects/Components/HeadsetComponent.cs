2dbf55064c3c139f8aef47f20b0533ce1b2aa538
Bright <nsmoak10@yahoo.com>
2020-08-02 20:11:59 -0600

    add headset component

---
+++ Content.Server/GameObjects/Components/HeadsetComponent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Content.Server.GameObjects.Components
+{
+    [RegisterComponent]
+    public class HeadsetComponent : Component
+    {
+        public override string Name => "Headset";
+    }
+}
