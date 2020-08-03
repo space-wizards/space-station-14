cb98fe2caf37e204b59b0ec3cc3437a157afca3b
Exp <theexp111@gmail.com>
2020-08-03 14:48:21 +0200

    Localization

--- Content.Server/GameObjects/Components/Atmos/GasAnalyzerComponent.cs
+++ Content.Server/GameObjects/Components/Atmos/GasAnalyzerComponent.cs
@@ -9,6 +9,7 @@ using Content.Shared.Utility;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Systems;
 using Robust.Shared.IoC;
+using Robust.Shared.Localization;
 using Robust.Shared.Map;
 using Robust.Shared.Utility;
 
@@ -19,6 +20,7 @@ namespace Content.Server.GameObjects.Components.Atmos
     {
 #pragma warning disable 649
         [Dependency] private IServerNotifyManager _notifyManager = default!;
+        [Dependency] private ILocalizationManager _loc = default!;
 #pragma warning restore 649
         public override string Name => "GasAnalyzer";
 
@@ -26,7 +28,7 @@ namespace Content.Server.GameObjects.Components.Atmos
         {
             if (!eventArgs.CanReach)
             {
-                _notifyManager.PopupMessage(eventArgs.ClickLocation, eventArgs.User, "Can't reach");
+                _notifyManager.PopupMessage(eventArgs.ClickLocation, eventArgs.User, _loc.GetString("You can't reach there!"));
                 return;
             }
 
@@ -36,13 +38,13 @@ namespace Content.Server.GameObjects.Components.Atmos
 
             if (tile == null)
             {
-                _notifyManager.PopupMessage(eventArgs.ClickLocation, eventArgs.User, "No atmosphere!");
+                _notifyManager.PopupMessage(eventArgs.ClickLocation, eventArgs.User, _loc.GetString("No atmosphere there!"));
                 return;
             }
 
             string message = "";
-            message += $"Pressure: {tile.Pressure:0.##} kPa\n";
-            message += $"Temperature: {tile.Temperature}K ({TemperatureHelpers.KelvinToCelsius(tile.Temperature)}°C)\n";
+            message += _loc.GetString("Pressure: {0:0.##} kPa\n", tile.Pressure);
+            message += _loc.GetString("Temperature: {0}K ({1}°C)", tile.Temperature, TemperatureHelpers.KelvinToCelsius(tile.Temperature));
 
             for (int i = 0; i < Atmospherics.TotalNumberOfGases; i++)
             {
@@ -50,9 +52,9 @@ namespace Content.Server.GameObjects.Components.Atmos
 
                 if (tile.Gases[i] <= Atmospherics.GasMinMoles) continue;
 
-                message += $"{gas.Name}: {tile.Gases[i]} mol \n";
+                message += _loc.GetString("\n{0}: {1} mol", gas.Name, tile.Gases[i]);
             }
-            message = message.TrimEnd(); // Remove annoying newline
+
             _notifyManager.PopupMessage(eventArgs.ClickLocation, eventArgs.User, message);
         }
     }
