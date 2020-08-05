c73a64b4a602180f86f43cf3cef2cb54c64ff716
Exp <theexp111@gmail.com>
2020-08-05 09:14:50 +0200

    Reworkkkk

--- Content.Server/GameObjects/Components/Chemistry/VaporComponent.cs
+++ Content.Server/GameObjects/Components/Chemistry/VaporComponent.cs
@@ -1,12 +1,21 @@
 ﻿using Content.Server.GameObjects.Components.Fluids;
 using Content.Shared.Chemistry;
+using Content.Shared.Physics;
 using Microsoft.EntityFrameworkCore.Update.Internal;
 using Robust.Shared.GameObjects;
+using Robust.Shared.GameObjects.Components;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.Map;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
+using Robust.Shared.Maths;
+using Robust.Shared.Physics;
 using Robust.Shared.Serialization;
 using Robust.Shared.Timers;
 using Robust.Shared.ViewVariables;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using Timer = Robust.Shared.Timers.Timer;
@@ -14,7 +23,7 @@ using Timer = Robust.Shared.Timers.Timer;
 namespace Content.Server.GameObjects.Components.Chemistry
 {
     [RegisterComponent]
-    class VaporComponent : Component
+    class VaporComponent : Component, ICollideBehavior
     {
         public override string Name => "Vapor";
 
@@ -23,46 +32,47 @@ namespace Content.Server.GameObjects.Components.Chemistry
         [ViewVariables]
         private ReagentUnit _transferAmount;
 
-        private float _settleTime;
-
-        private CancellationTokenSource _updateToken;
+        private bool _running;
+        private Vector2 _direction;
+        private float _velocity;
 
 
         public override void Initialize()
         {
             base.Initialize();
             _contents = Owner.GetComponent<SolutionComponent>();
-            SpawnTimer();
+        }
+
+        public void Start(Vector2 dir, float velocity)
+        {
+            _running = true;
+            _direction = dir;
+            _velocity = velocity;
+            // Set Move
+            if (Owner.TryGetComponent(out ICollidableComponent collidable))
+            {
+                var controller = collidable.EnsureController<VaporController>();
+                controller.Move(_direction, _velocity);
+            }
         }
 
         public override void ExposeData(ObjectSerializer serializer)
         {
             base.ExposeData(serializer);
-            serializer.DataField(ref _transferAmount, "transferAmount", ReagentUnit.New(5));
-            serializer.DataField(ref _settleTime, "settleTime", 0.5f);
-        }
-
-        public void SpawnTimer()
-        {
-            _updateToken = new CancellationTokenSource();
-            Timer.Spawn(TimeSpan.FromSeconds(_settleTime), Update, _updateToken.Token);
+            serializer.DataField(ref _transferAmount, "transferAmount", ReagentUnit.New(0.5));
         }
 
         public void Update()
         {
-            _updateToken?.Cancel();
-            if (Owner.Deleted) return;
+            if (!_running)
+                return;
 
-            SpillHelper.SpillAt(Owner, _contents.SplitSolution(_transferAmount), "PuddleSmear", false); //make non PuddleSmear?
+            SpillHelper.SpillAt(Owner.Transform.GridPosition, _contents.SplitSolution(_transferAmount), "PuddleSmear", false); //make non PuddleSmear?
+
             if (_contents.CurrentVolume == 0)
             {
-                //Delete this
+                // Delete this
                 Owner.Delete();
-            }
-            else
-            {
-                //Spawn another one in some time
-                SpawnTimer();
             }
         }
 
@@ -79,6 +89,22 @@ namespace Content.Server.GameObjects.Components.Chemistry
             }
 
             return true;
+        }
+
+        void ICollideBehavior.CollideWith(IEntity collidedWith)
+        {
+            // Check for collision with a impassable object (e.g. wall) and stop
+            if (collidedWith.TryGetComponent(out ICollidableComponent collidable))
+            {
+                if ((collidable.CollisionLayer & (int) CollisionGroup.Impassable) != 0 && collidable.Hard)
+                {
+                    if (Owner.TryGetComponent(out ICollidableComponent coll))
+                    {
+                        var controller = coll.EnsureController<VaporController>();
+                        controller.Stop();
+                    }
+                }
+            }
         }
     }
 }
--- Content.Server/GameObjects/Components/Fluids/SprayComponent.cs
+++ Content.Server/GameObjects/Components/Fluids/SprayComponent.cs
@@ -4,7 +4,9 @@ using Content.Server.GameObjects.Components.Chemistry;
 using Content.Server.Interfaces;
 using Content.Shared.Chemistry;
 using Content.Shared.Interfaces.GameObjects.Components;
+using Microsoft.EntityFrameworkCore.Update.Internal;
 using Robust.Server.GameObjects.EntitySystems;
+using Robust.Server.Interfaces.GameObjects;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Components;
 using Robust.Shared.GameObjects.Systems;
@@ -23,15 +25,14 @@ namespace Content.Server.GameObjects.Components.Fluids
     class SprayComponent : Component, IAfterInteract
     {
 #pragma warning disable 649
-        [Dependency] private readonly IMapManager _mapManager = default!;
         [Dependency] private readonly IServerNotifyManager _notifyManager = default!;
-        [Dependency] private readonly IEntityManager _entityManager = default!;
+        [Dependency] private readonly IServerEntityManager _serverEntityManager = default!;
 #pragma warning restore 649
         public override string Name => "Spray";
 
         private ReagentUnit _transferAmount;
-        private int _sprayRange;
         private string _spraySound;
+        private float _sprayVelocity;
 
         /// <summary>
         ///     The amount of solution to be sprayer from this solution when using it
@@ -41,6 +42,16 @@ namespace Content.Server.GameObjects.Components.Fluids
         {
             get => _transferAmount;
             set => _transferAmount = value;
+        }
+
+        /// <summary>
+        ///     The speed at which the vapor starts when sprayed
+        /// </summary>
+        [ViewVariables]
+        public float Velocity
+        {
+            get => _sprayVelocity;
+            set => _sprayVelocity = value;
         }
 
         private SolutionComponent _contents;
@@ -55,29 +66,9 @@ namespace Content.Server.GameObjects.Components.Fluids
         public override void ExposeData(ObjectSerializer serializer)
         {
             base.ExposeData(serializer);
-            serializer.DataField(ref _transferAmount, "transferAmount", ReagentUnit.New(5.0));
-            serializer.DataField(ref _sprayRange, "sprayRange", 3);
+            serializer.DataField(ref _transferAmount, "transferAmount", ReagentUnit.New(10));
+            serializer.DataField(ref _sprayVelocity, "sprayVelocity", 5.0f);
             serializer.DataField(ref _spraySound, "spraySound", string.Empty);
-        }
-
-        // Source: https://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C.23
-        // Gets all tiles in between origin and end, plus the end tile itself
-        List<GridCoordinates> GetTilesBetween(GridCoordinates origin, GridCoordinates end, int range = 100)
-        {
-            var tiles = new List<GridCoordinates>();
-            int dx = Math.Abs((int)(end.X - origin.X)), sx = origin.X < end.X ? 1 : -1;
-            int dy = Math.Abs((int)(end.Y - origin.Y)), sy = origin.Y < end.Y ? 1 : -1;
-            int err = (dx > dy ? dx : -dy) / 2, e2;
-            for (int i = 0; i < range; i++)
-            {
-                e2 = err;
-                if (e2 > -dx) { err -= dy; origin = origin.Offset(new Vector2(sx, 0)); }
-                if (e2 < dy) { err += dx; origin = origin.Offset(new Vector2(0, sy)); }
-                // Add tile to list and check if we're (prematurely) at the end
-                tiles.Add(origin);
-                if (origin.X == end.X && origin.Y == end.Y) break;
-            }
-            return tiles;
         }
 
         void IAfterInteract.AfterInteract(AfterInteractEventArgs eventArgs)
@@ -92,63 +83,18 @@ namespace Content.Server.GameObjects.Components.Fluids
             if (eventArgs.ClickLocation.GridID != playerPos.GridID)
                 return;
 
-            var mapGrid = _mapManager.GetGrid(eventArgs.ClickLocation.GridID);
+            var direction = (eventArgs.ClickLocation.Position - playerPos.Position).Normalized;
+            var solution = _contents.SplitSolution(_transferAmount);
 
-            var clickTile = mapGrid.GetTileRef(eventArgs.ClickLocation);
-            var clickCoordinates = mapGrid.GridTileToLocal(clickTile.GridIndices);
-
-            var playerTile = mapGrid.GetTileRef(playerPos);
-            var playerCoord = mapGrid.GridTileToLocal(playerTile.GridIndices);
-
-            // Get the tiles we want to spray things at
-            //TODO: if you spray directly against a wall, you should spray the ground instead?
-            List<GridCoordinates> tiles;
-            if (clickCoordinates == playerCoord) // clicked on the tile standing on
-            {
-                tiles = new List<GridCoordinates>() { playerCoord };
-            }
-            else
-            {
-                tiles = GetTilesBetween(playerCoord, clickCoordinates, _sprayRange);
-            }
+            playerPos = playerPos.Offset(direction); // Move a bit so we don't hit the
+            var vapor = _serverEntityManager.SpawnEntity("Vapor", playerPos);
+            // Add the solution to the vapor and actually send the thing
+            var vaporComponent = vapor.GetComponent<VaporComponent>();
+            vaporComponent.TryAddSolution(solution);
+            vaporComponent.Start(direction, _sprayVelocity); //TODO: maybe make the velocity depending on the distance to the click
 
             //Play sound
             EntitySystem.Get<AudioSystem>().PlayFromEntity(_spraySound, Owner);
-
-            //TODO: add spray visual effect?
-            // Spray the tiles, half the amount each step
-            ReagentUnit amount = TransferAmount;
-            foreach (var tile in tiles)
-            {
-                if (CurrentVolume == 0 || Math.Round(amount.Float()) == 0) // no need to create empty puddles
-                    break;
-
-                var mapCoords = tile.ToMap(_mapManager);
-
-                // Check if any impassable & hard entities on the tile (e.g. walls, vending machines)
-                var ents = _entityManager.GetEntitiesAt(mapCoords.MapId, mapCoords.Position, true);
-                var unobstructed = true;
-                foreach (var ent in ents)
-                {
-                    if (ent.TryGetComponent(out ICollidableComponent coll))
-                    {
-                        if ((coll.CollisionLayer & (int)Content.Shared.Physics.CollisionGroup.Impassable) != 0 && coll.Hard)
-                        {
-                            unobstructed = false;
-                            break;
-                        }
-                    }
-                }
-                if (unobstructed)
-                {
-                    VaporHelper.SpillAt(tile, _contents.SplitSolution(amount));
-                    amount = amount * 0.5;
-                }
-                else // found wall and that stops the spray
-                {
-                    break;
-                }
-            }
         }
     }
 }
