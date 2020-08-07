8f5f754c70155eb601d9f5907cb7ca906653f80b
py01 <pyronetics01@gmail.com>
2020-08-07 01:03:50 -0600

    PumpComponent

---
+++ Content.Server/GameObjects/Components/Atmos/PipeNet/PumpComponent.cs
@@ -0,0 +1,40 @@
+﻿using Content.Server.GameObjects.Components.NodeContainer;
+using Content.Server.GameObjects.Components.NodeContainer.Nodes;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization;
+using System.Linq;
+
+namespace Content.Server.GameObjects.Components.Atmos.PipeNet
+{
+    public class PumpComponent : Component
+    {
+        public override string Name => "Pump";
+
+        private PipeDirection _inletDirection;
+
+        private PipeDirection _outletDirection;
+
+        private PipeNode _inletNode;
+
+        private PipeNode _outletNode;
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _inletDirection, "inletDirection", PipeDirection.None);
+            serializer.DataField(ref _outletDirection, "outletDirection", PipeDirection.None);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            var pipeNodes = Owner.GetComponent<NodeContainerComponent>().Nodes.OfType<PipeNode>();
+            _inletNode = pipeNodes.Where(pipeNode => pipeNode.PipeDirection == _inletDirection).First();
+            _outletNode = pipeNodes.Where(pipeNode => pipeNode.PipeDirection == _outletDirection).First();
+        }
+
+        public void Update(float frameTime)
+        {
+        }
+    }
+}
