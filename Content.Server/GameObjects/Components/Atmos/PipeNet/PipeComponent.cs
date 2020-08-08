25276578a60804adb507bb6ad5a15ace54c30275
py01 <pyronetics01@gmail.com>
2020-08-07 22:08:16 -0600

    PipeComponent

---
+++ Content.Server/GameObjects/Components/Atmos/PipeNet/PipeComponent.cs
@@ -0,0 +1,24 @@
+﻿using Content.Server.GameObjects.Components.NodeContainer.NodeGroups;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+
+namespace Content.Server.GameObjects.Components.Atmos.PipeNet
+{
+    public class PipeComponent : Component
+    {
+        public override string Name => "Pipe";
+
+        [ViewVariables]
+        public IPipeNet PipeNet { get; private set; }
+
+        [ViewVariables]
+        public float Volume { get; private set; }
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(this, x => Volume, "volume", 10);
+        }
+    }
+}
