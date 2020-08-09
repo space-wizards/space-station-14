c156af9bd4b4d85f377018dfc11c72e868940853
py01 <pyronetics01@gmail.com>
2020-08-09 01:22:41 -0600

    BasePump

---
+++ Content.Server/GameObjects/Components/Atmos/Piping/BasePumpComponent.cs
@@ -0,0 +1,57 @@
+﻿using Content.Server.Atmos;
+using Content.Server.GameObjects.Components.NodeContainer.Nodes;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+using System.Linq;
+
+namespace Content.Server.GameObjects.Components.Atmos
+{
+    [RegisterComponent]
+    public abstract class BasePumpComponent : Component
+    {
+        [ViewVariables]
+        private PipeDirection _inletDirection;
+
+        [ViewVariables]
+        private PipeDirection _outletDirection;
+
+        [ViewVariables]
+        private Pipe _inletPipe;
+
+        [ViewVariables]
+        private Pipe _outletPipe;
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _inletDirection, "inletDirection", PipeDirection.None);
+            serializer.DataField(ref _outletDirection, "outletDirection", PipeDirection.None);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            var pipeContainer = Owner.GetComponent<PipeContainerComponent>();
+            _inletPipe = pipeContainer.Pipes.Where(pipe => pipe.PipeDirection == _inletDirection).First();
+            _outletPipe = pipeContainer.Pipes.Where(pipe => pipe.PipeDirection == _outletDirection).First();
+        }
+
+        public void Update(float frameTime)
+        {
+            PumpGas(_inletPipe.ContainedGas, _outletPipe.ContainedGas, frameTime);
+        }
+
+        protected abstract void PumpGas(GasMixture inletGas, GasMixture outletGas, float frameTime);
+    }
+
+    public class DebugPumpComponent : BasePumpComponent
+    {
+        public override string Name => "DebugPump";
+
+        protected override void PumpGas(GasMixture inletGas, GasMixture outletGas, float frameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Content.Server/GameObjects/EntitySystems/PumpSystem.cs
@@ -0,0 +1,17 @@
+﻿using Content.Server.GameObjects.Components.Atmos;
+using Robust.Shared.GameObjects.Systems;
+
+namespace Content.Server.GameObjects.EntitySystems
+{
+    public class PumpSystem : EntitySystem
+    {
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+            foreach (var pump in ComponentManager.EntityQuery<BasePumpComponent>())
+            {
+                pump.Update(frameTime);
+            }
+        }
+    }
+}
