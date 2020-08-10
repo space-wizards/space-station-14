b47e9082b917d9182f6808c4d7ba82a46211c215
py01 <pyronetics01@gmail.com>
2020-08-10 17:26:17 -0600

    scrubber prototype

--- Content.Server/GameObjects/Components/Atmos/Piping/DebugScrubberComponent.cs
+++ Content.Server/GameObjects/Components/Atmos/Piping/DebugScrubberComponent.cs
@@ -1,7 +1,10 @@
 ﻿using Content.Server.Atmos;
+using Robust.Shared.GameObjects;
 
 namespace Content.Server.GameObjects.Components.Atmos
 {
+    [RegisterComponent]
+    [ComponentReference(typeof(BaseScrubberComponent))]
     public class DebugScrubberComponent : BaseScrubberComponent
     {
         public override string Name => "DebugScrubber";
