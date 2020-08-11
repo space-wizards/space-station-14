ce8989b22975a17e1b1e00a13573790b5d0ab44c
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2020-08-11 00:52:15 -0700

    Fix StackComponent not getting removed from containers when empty

--- Content.Server/GameObjects/Components/Items/FloorTileItemComponent.cs
+++ Content.Server/GameObjects/Components/Items/FloorTileItemComponent.cs
@@ -50,9 +50,6 @@ namespace Content.Server.GameObjects.Components.Items
                 var desiredTile = _tileDefinitionManager[_outputTile];
                 mapGrid.SetTile(eventArgs.ClickLocation, new Tile(desiredTile.TileId));
                 EntitySystem.Get<AudioSystem>().PlayAtCoords("/Audio/Items/genhit.ogg", eventArgs.ClickLocation);
-                if(_stack.Count < 1){
-                    Owner.Delete();
-                }
             }
 
 
