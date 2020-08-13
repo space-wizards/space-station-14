15b9540e7301ee6a9b9d6f70390339968188d161
Vince <vincefvanwijk@gmail.com>
2020-08-13 17:28:51 +0200

    Split up client side and server side tests

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -19,16 +19,13 @@ namespace Content.IntegrationTests.Tests
     public class EntityTest : ContentIntegrationTest
     {
         [Test]
-        public async Task Test()
+        public async Task SpawnTest()
         {
             var server = StartServerDummyTicker();
-            var client = StartClient();
             await server.WaitIdleAsync();
-            await client.WaitIdleAsync();
             var mapMan = server.ResolveDependency<IMapManager>();
             var entityMan = server.ResolveDependency<IEntityManager>();
-            var serverPrototypeMan = server.ResolveDependency<IPrototypeManager>();
-            var clientPrototypeMan = client.ResolveDependency<IPrototypeManager>();
+            var prototypeMan = server.ResolveDependency<IPrototypeManager>();
             var mapLoader = server.ResolveDependency<IMapLoader>();
             var pauseMan = server.ResolveDependency<IPauseManager>();
             var prototypes = new List<EntityPrototype>();
@@ -43,56 +40,65 @@ namespace Content.IntegrationTests.Tests
                 grid = mapLoader.LoadBlueprint(mapId, "Maps/stationstation.yml");
             });
 
-            client.Assert(() =>
-                {
-                    foreach (var prototype in clientPrototypeMan.EnumeratePrototypes<EntityPrototype>())
-                    {
-                        if (prototype.Abstract)
-                        {
-                            continue;
-                        }
-
-                        Assert.That(prototype.Components.ContainsKey("Icon"), $"Entity {prototype.ID} does not have an Icon component, but is not abstract");
-                    }
-                });
-
             server.Assert(() =>
+            {
+                var testLocation = new GridCoordinates(new Vector2(0, 0), grid);
+
+                //Generate list of non-abstract prototypes to test
+                foreach (var prototype in prototypeMan.EnumeratePrototypes<EntityPrototype>())
                 {
-                    var testLocation = new GridCoordinates(new Vector2(0, 0), grid);
-
-                    //Generate list of non-abstract prototypes to test
-                    foreach (var prototype in serverPrototypeMan.EnumeratePrototypes<EntityPrototype>())
+                    if (prototype.Abstract)
                     {
-                        if (prototype.Abstract)
-                        {
-                            continue;
-                        }
-                        prototypes.Add(prototype);
+                        continue;
+                    }
+                    prototypes.Add(prototype);
+                }
+
+                //Iterate list of prototypes to spawn
+                foreach (var prototype in prototypes)
+                {
+                    try
+                    {
+                        Logger.LogS(LogLevel.Debug, "EntityTest", "Testing: " + prototype.ID);
+                        testEntity = entityMan.SpawnEntity(prototype.ID, testLocation);
+                        server.RunTicks(2);
+                        Assert.That(testEntity.Initialized);
+                        entityMan.DeleteEntity(testEntity.Uid);
                     }
 
-                    //Iterate list of prototypes to spawn
-                    foreach (var prototype in prototypes)
+                    //Fail any exceptions thrown on spawn
+                    catch (Exception e)
                     {
-                        try
-                        {
-                            Logger.LogS(LogLevel.Debug, "EntityTest", "Testing: " + prototype.ID);
-                            testEntity = entityMan.SpawnEntity(prototype.ID, testLocation);
-                            server.RunTicks(2);
-                            Assert.That(testEntity.Initialized);
-                            entityMan.DeleteEntity(testEntity.Uid);
-                        }
-
-                        //Fail any exceptions thrown on spawn
-                        catch (Exception e)
-                        {
-                            Logger.LogS(LogLevel.Error, "EntityTest", "Entity '" + prototype.ID + "' threw: " + e.Message);
-                            //Assert.Fail();
-                            throw;
-                        }
+                        Logger.LogS(LogLevel.Error, "EntityTest", "Entity '" + prototype.ID + "' threw: " + e.Message);
+                        //Assert.Fail();
+                        throw;
                     }
-                });
+                }
+            });
 
             await server.WaitIdleAsync();
+        }
+
+        [Test]
+        public async Task NotAbstractIconTest()
+        {
+            var client = StartClient();
+            await client.WaitIdleAsync();
+            var prototypeMan = client.ResolveDependency<IPrototypeManager>();
+
+            client.Assert(() =>
+            {
+                foreach (var prototype in prototypeMan.EnumeratePrototypes<EntityPrototype>())
+                {
+                    if (prototype.Abstract)
+                    {
+                        continue;
+                    }
+
+                    Assert.That(prototype.Components.ContainsKey("Icon"), $"Entity {prototype.ID} does not have an Icon component, but is not abstract");
+                }
+            });
+
             await client.WaitIdleAsync();
         }
     }
