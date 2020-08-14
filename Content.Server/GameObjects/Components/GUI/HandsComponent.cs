3b6f76471d4c048e1b1b580a36c194b17379b413
Víctor Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>
2020-08-14 21:49:27 +0200

    Update Content.Server/GameObjects/Components/GUI/HandsComponent.cs
    Co-authored-by: DrSmugleaf <DrSmugleaf@users.noreply.github.com>

--- Content.Server/GameObjects/Components/GUI/HandsComponent.cs
+++ Content.Server/GameObjects/Components/GUI/HandsComponent.cs
@@ -281,7 +281,7 @@ namespace Content.Server.GameObjects.Components.GUI
                 throw new ArgumentException("Entity must be held in one of our hands.", nameof(entity));
             }
 
-            return Drop(slot!, coords, doMobChecks);
+            return Drop(slot, coords, doMobChecks);
         }
 
         public bool Drop(string slot, bool doMobChecks = true)
