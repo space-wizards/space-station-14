49b18000b30f423f1a7a232edb33e772042d47d9
py01 <pyronetics01@gmail.com>
2020-08-19 17:28:39 -0600

    power tests work

--- Content.IntegrationTests/Tests/PowerTest.cs
+++ Content.IntegrationTests/Tests/PowerTest.cs
@@ -18,6 +18,10 @@ namespace Content.IntegrationTests.Tests
         {
             var server = StartServerDummyTicker();
 
+            PowerSupplierComponent supplier = null;
+            PowerConsumerComponent consumer1 = null;
+            PowerConsumerComponent consumer2 = null;
+
             server.Assert(() =>
             {
                 var mapMan = IoCManager.Resolve<IMapManager>();
@@ -29,20 +33,24 @@ namespace Content.IntegrationTests.Tests
                 var consumerEnt1 = entityMan.SpawnEntity("DebugConsumer", new GridCoordinates(new Vector2(0, 1), grid.Index));
                 var consumerEnt2 = entityMan.SpawnEntity("DebugConsumer", new GridCoordinates(new Vector2(0, 2), grid.Index));
 
-                Assert.That(generatorEnt.TryGetComponent<PowerSupplierComponent>(out var supplier));
-                Assert.That(consumerEnt1.TryGetComponent<PowerConsumerComponent>(out var consumer1));
-                Assert.That(consumerEnt2.TryGetComponent<PowerConsumerComponent>(out var consumer2));
+                Assert.That(generatorEnt.TryGetComponent(out supplier));
+                Assert.That(consumerEnt1.TryGetComponent(out consumer1));
+                Assert.That(consumerEnt2.TryGetComponent(out consumer2));
 
                 var supplyRate = 1000; //arbitrary amount of power supply
 
                 supplier.SupplyRate = supplyRate;
                 consumer1.DrawRate = supplyRate / 2; //arbitrary draw less than supply
-                consumer1.Priority = Priority.First; //power goes to this consumer first
                 consumer2.DrawRate = supplyRate * 2; //arbitrary draw greater than supply
-                consumer2.Priority = Priority.Last; //excess power should go to this consumer of lower priority
 
-                server.RunTicks(1);
+                consumer1.Priority = Priority.First; //power goes to this consumer first
+                consumer2.Priority = Priority.Last; //any excess power should go to low priority consumer
+            });
 
+            server.RunTicks(1);
+
+            server.Assert(() =>
+            {
                 Assert.That(consumer1.DrawRate, Is.EqualTo(consumer1.ReceivedPower)); //first should be fully powered
                 Assert.That(consumer2.ReceivedPower, Is.EqualTo(supplier.SupplyRate - consumer1.ReceivedPower)); //second should get remaining power
             });
@@ -52,6 +60,8 @@ namespace Content.IntegrationTests.Tests
         public async Task ApcNetTest()
         {
             var server = StartServerDummyTicker();
+
+            PowerReceiverComponent receiver = null;
 
             server.Assert(() =>
             {
@@ -66,13 +76,16 @@ namespace Content.IntegrationTests.Tests
 
                 Assert.That(apcEnt.TryGetComponent<ApcComponent>(out var apc));
                 Assert.That(apcExtensionEnt.TryGetComponent<PowerProviderComponent>(out var provider));
-                Assert.That(powerReceiverEnt.TryGetComponent<PowerReceiverComponent>(out var receiver));
+                Assert.That(powerReceiverEnt.TryGetComponent(out receiver));
 
                 apc.Battery.CurrentCharge = 10000; //arbitrary nonzero amount of charge
                 receiver.Load = 1; //arbitrary small amount of power
+            });
 
-                server.RunTicks(1);
+            server.RunTicks(1);
 
+            server.Assert(() =>
+            {
                 Assert.That(receiver.Powered);
             });
         }
