aaf3c5abf79be84933a691eec11840cf32294b2b
Víctor Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>
2020-08-20 16:19:05 +0200

    Update Content.Server/GameTicking/GameTicker.cs
    Co-authored-by: DrSmugleaf <DrSmugleaf@users.noreply.github.com>

--- Content.Server/GameTicking/GameTicker.cs
+++ Content.Server/GameTicking/GameTicker.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Threading;
 using System.Threading.Tasks;
 using Content.Server.GameObjects.Components.Access;
