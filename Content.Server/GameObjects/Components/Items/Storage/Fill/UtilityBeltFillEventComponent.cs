01370e5e6d539a36ff1a5b00178e0ef13ef97824
AJCM <AJCM@tutanota.com>
2020-08-22 10:08:51 -0400

    oops

--- Content.Server/GameObjects/Components/Items/Storage/Fill/UtilityBeltFillEventComponent.cs
+++ Content.Server/GameObjects/Components/Items/Storage/Fill/UtilityBeltFillEventComponent.cs
@@ -1,7 +1,35 @@
+using Robust.Server.Interfaces.GameObjects;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
+
 namespace Content.Server.GameObjects.Components.Items.Storage.Fill
 {
-    public class UtilityBeltFillEventComponent
+    [RegisterComponent]
+    internal sealed class UtilityBeltFillEventComponent : UtilityBeltClothingFillComponent
     {
-        
+        public override string Name => "UtilityBeltClothingFill";
+
+#pragma warning disable 649
+        [Dependency] private readonly IEntityManager _entityManager;
+#pragma warning restore 649
+
+        void IMapInit.MapInit()
+        {
+            var storage = Owner.GetComponent<IStorageComponent>();
+
+            void Spawn(string prototype)
+            {
+                storage.Insert(_entityManager.SpawnEntity(prototype, Owner.Transform.GridPosition));
+            }
+
+            Spawn("Crowbar");
+            Spawn("Wrench");
+            Spawn("Screwdriver");
+            Spawn("Wirecutter");
+            Spawn("Welder");
+            Spawn("Multitool");
+            Spawn("ApcExtensionCableStack");
+        }
     }
 }
