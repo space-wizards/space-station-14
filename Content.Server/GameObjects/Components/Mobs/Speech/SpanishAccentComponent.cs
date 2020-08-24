c166a1bd792eb26eb8626eee9e6677c882e0055b
Exp <theexp111@gmail.com>
2020-08-24 15:55:12 +0200

    Better?

--- Content.Server/GameObjects/Components/Mobs/Speech/SpanishAccentComponent.cs
+++ Content.Server/GameObjects/Components/Mobs/Speech/SpanishAccentComponent.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Content.Server.GameObjects.Components.Mobs.Speech
 {
@@ -17,79 +18,48 @@ namespace Content.Server.GameObjects.Components.Mobs.Speech
         {
             // Insert E before every S
             message = InsertS(message);
-            // Insert
+            // If a sentence ends with ?, insert a reverse ? at the beginning of the sentence
             message = ReplaceQuestionMark(message);
             return message;
         }
 
         private static string InsertS(string message)
         {
-            // Find a sentence that begins with S and add a
-            var msg = message; // We make a working copy of the message
-            var offset = 0; // As we may insert some additional chars, we need to offset the index accordingly
-            for (var i = 0; i < message.Length; i++)
-            {
-                var c = message[i];
-                var newWord = false;
-                if (c == ' ')
-                {
-                    newWord = true;
-                    i++;
-                    if (i >= message.Length)
-                        break;
-                }
+            // Replace every new Word that starts with s/S
+            var msg = message.Replace(" s", " es").Replace(" S", "Es");
 
-                // We are (most likely) at the beginning of a new word
-                if (newWord || i == 0)
-                {
-                    c = message[i];
-                    if (c == 's' || c == 'S')
-                    {
-                        // Remove the s
-                        msg = msg.Remove(i + offset, 1);
-                        // Insert the es/Es
-                        msg = msg.Insert(i + offset, c == 's' ? "es" : "Es");
-                        offset++;
-                    }
-                }
+            // Still need to check if the beginning of the message starts
+            if (msg.StartsWith("s"))
+            {
+                return msg.Remove(0, 1).Insert(0, "es");
             }
+            else if (msg.StartsWith("S"))
+            {
+                return msg.Remove(0, 1).Insert(0, "Es");
+            }
+
             return msg;
         }
 
+        private static readonly Regex SentenceRegex = new Regex(@"(?<=[\.!\?])");
         private static string ReplaceQuestionMark(string message)
         {
-            var msg = message;
-            var offset = 0;
-            for (var i = 0; i < message.Length; i++)
+            var sentences = SentenceRegex.Split(message);
+            var msg = "";
+            foreach (var s in sentences)
             {
-                var c = message[i];
-                if (c == '?')
+                if (s.EndsWith("?")) // We've got a question => add ¿ to the beginning
                 {
-                    var index = FindSentenceStart(message, i);
-                    if (index != i)
-                    {
-                        msg = msg.Insert(index + offset, "¿");
-                        offset++;
-                    }
+                    // Because we don't split by whitespace, we may have some spaces in front of the sentence.
+                    // So we add the symbol before the first non space char
+                    msg += s.Insert(s.Length - s.TrimStart().Length, "¿");
+                }
+                else
+                {
+                    msg += s;
                 }
             }
             return msg;
-        }
-        private static int FindSentenceStart(string message, int ending)
-        {
-            for (var i = ending - 1; i >= 0; i--)
-            {
-                var c = message[i];
-                if (c == '.' || c == ';' || c == '!' || c == '?')
-                {
-                    do
-                    {
-                        i++;
-                    } while (message[i] == ' ');
-                    return i;
-                }
-            }
-            return 0;
         }
     }
 }
