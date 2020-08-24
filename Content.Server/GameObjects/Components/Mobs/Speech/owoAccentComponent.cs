02ea650f8a6612123afafd3ce7c9b403f37be426
Exp <theexp111@gmail.com>
2020-08-24 12:03:41 +0200

    owo whats this

--- Content.Server/GameObjects/Components/Mobs/Speech/owoAccentComponent.cs
+++ Content.Server/GameObjects/Components/Mobs/Speech/owoAccentComponent.cs
@@ -1,4 +1,8 @@
-﻿using Robust.Shared.GameObjects;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.Random;
+using Robust.Shared.IoC;
+using Robust.Shared.Random;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,11 +14,16 @@ namespace Content.Server.GameObjects.Components.Mobs.Speech
     {
         public override string Name => "OwOAccent";
 
+        private static readonly IReadOnlyList<string> Faces = new List<string>{
+            " (・`ω´・)", " ;;w;;", " owo", " UwU", " >w<", " ^w^"
+        }.AsReadOnly();
+        private string RandomFace => IoCManager.Resolve<IRobustRandom>().Pick(Faces);
+
         public string Accentuate(string message)
         {
-            var arr = message.ToCharArray();
-            Array.Reverse(arr);
-            return new string(arr);
+            return message.Replace("!", RandomFace)
+                .Replace("r", "w").Replace("R", "W")
+                .Replace("l", "w").Replace("L", "W");
         }
     }
 }
