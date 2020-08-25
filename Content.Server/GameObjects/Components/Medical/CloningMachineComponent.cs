8f4a806aed1e400d47f283d27c99fcd8c610a790
SoulSloth <root@DESKTOP-EKBQMB8.localdomain>
2020-08-24 21:02:43 -0400

    cloning machine can now spawn the right player profile assuming the attatched entity is still correct.

--- Content.Server/GameObjects/Components/Medical/CloningMachineComponent.cs
+++ Content.Server/GameObjects/Components/Medical/CloningMachineComponent.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Content.Server.GameObjects.Components.Mobs;
 using Content.Server.GameObjects.Components.Power.ApcNetComponents;
 using Content.Server.GameObjects.EntitySystems;
+using Content.Server.Interfaces;
+using Content.Server.Interfaces.GameTicking;
+using Content.Server.Utility;
 using Content.Shared.GameObjects.Components.Medical;
 using Content.Shared.Interfaces.GameObjects.Components;
+using Content.Shared.Preferences;
 using Robust.Server.GameObjects;
 using Robust.Server.GameObjects.Components.Container;
 using Robust.Server.GameObjects.Components.UserInterface;
 using Robust.Server.Interfaces.GameObjects;
+using Robust.Server.Interfaces.Player;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
 
 namespace Content.Server.GameObjects.Components.Medical
 {
@@ -19,6 +30,7 @@ namespace Content.Server.GameObjects.Components.Medical
         private AppearanceComponent _appearance;
         private BoundUserInterface _userInterface;
         private ContainerSlot _bodyContainer;
+        [Dependency] private readonly IServerPreferencesManager _prefsManager;
 
         private PowerReceiverComponent _powerReceiver;
         private bool Powered => _powerReceiver.Powered;
@@ -54,11 +66,11 @@ namespace Content.Server.GameObjects.Components.Medical
 
 
         private static readonly CloningMachineBoundUserInterfaceState EmptyUIState =
-            new CloningMachineBoundUserInterfaceState(new List<EntityUid>(),0, false);
+            new CloningMachineBoundUserInterfaceState(new List<EntityUid>(), 0, false);
 
         private CloningMachineBoundUserInterfaceState GetUserInterfaceState()
         {
-            return new CloningMachineBoundUserInterfaceState(CloningSystem.scannedUids,0, false);
+            return new CloningMachineBoundUserInterfaceState(CloningSystem.scannedUids, 0, false);
         }
 
 
@@ -74,7 +86,7 @@ namespace Content.Server.GameObjects.Components.Medical
             _userInterface.Open(actor.playerSession);
         }
 
-        private void OnUiReceiveMessage(ServerBoundUserInterfaceMessage obj)
+        private async void OnUiReceiveMessage(ServerBoundUserInterfaceMessage obj)
         {
             if (!(obj.Message is UiButtonPressedMessage message))
             {
@@ -84,15 +96,32 @@ namespace Content.Server.GameObjects.Components.Medical
             switch (message.Button)
             {
                 case UiButton.Clone:
+                    var entityManager = IoCManager.Resolve<IEntityManager>();
+                    var _playerManager = IoCManager.Resolve<IPlayerManager>();
 
-                    /*if (_bodyContainer.ContainedEntity != null)
-                    {
-                        CloningSystem.AddToScannedUids(_bodyContainer.ContainedEntity.Uid);
-                    }*/
+                    var mob = entityManager.SpawnEntity("HumanMob_Content", Owner.Transform.MapPosition);
+
+                    //THIS CALL FAILS
+                    var list = _playerManager.GetPlayersBy(x => x.AttachedEntity != null)
+                        .Select(x => x.AttachedEntityUid);
+                    var client = _playerManager
+                        .GetPlayersBy(x => x.AttachedEntity != null
+                                           && x.AttachedEntityUid == message.ScanId).First();
+
+                    var profile = GetPlayerProfileAsync(client.Name).Result;
+                    mob.GetComponent<HumanoidAppearanceComponent>().UpdateFromProfile(profile);
+                    mob.Name = profile.Name;
+
+                    _bodyContainer.Insert(mob);
+                    _bodyContainer.Remove(mob);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
         }
+
+        private async Task<HumanoidCharacterProfile> GetPlayerProfileAsync(string username) =>
+            (HumanoidCharacterProfile) (await _prefsManager.GetPreferencesAsync(username))
+            .SelectedCharacter;
     }
 }
