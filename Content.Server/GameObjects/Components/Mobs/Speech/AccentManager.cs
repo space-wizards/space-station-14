8e0b3abf150665a3ce739042f76790bd5ad8c52e
Exp <theexp111@gmail.com>
2020-08-25 15:55:18 +0200

    InjectDependencies

--- Content.Server/GameObjects/Components/Mobs/Speech/AccentManager.cs
+++ Content.Server/GameObjects/Components/Mobs/Speech/AccentManager.cs
@@ -15,17 +15,22 @@ namespace Content.Server.GameObjects.Components.Mobs.Speech
 
     public class AccentManager : IAccentManager
     {
+        [Dependency] private readonly IChatManager _chatManager = default!;
+        [Dependency] private readonly IComponentManager _componentManager = default!;
+
         public static readonly Regex SentenceRegex = new Regex(@"(?<=[\.!\?])");
 
         public void Initialize()
         {
-            IoCManager.Resolve<IChatManager>().RegisterChatTransform(AccentHandler);
+            IoCManager.InjectDependencies(this);
+
+            _chatManager.RegisterChatTransform(AccentHandler);
         }
 
         public string AccentHandler(IEntity player, string message)
         {
             //TODO: give accents a prio?
-            var accents = IoCManager.Resolve<IComponentManager>().GetComponents<IAccentComponent>(player.Uid);
+            var accents = _componentManager.GetComponents<IAccentComponent>(player.Uid);
             foreach (var accent in accents)
             {
                 message = accent.Accentuate(message);
