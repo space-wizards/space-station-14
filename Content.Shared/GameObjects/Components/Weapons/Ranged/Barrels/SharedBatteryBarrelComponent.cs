74648ad3076fa38fca89d16bbcb69e57343d57e6
Exp <theexp111@gmail.com>
2020-08-26 11:25:47 +0200

    -ItemStatus for BatteryGuns -EjectCell Verb -Using the gun throws the battery also on the ground

---
+++ Content.Shared/GameObjects/Components/Weapons/Ranged/Barrels/SharedBatteryBarrelComponent.cs
@@ -0,0 +1,27 @@
+﻿using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Content.Shared.GameObjects.Components.Weapons.Ranged.Barrels
+{
+    [Serializable, NetSerializable]
+    public class BatteryBarrelComponentState : ComponentState
+    {
+        public FireRateSelector FireRateSelector { get; }
+        public (int count, int max)? Magazine { get; }
+        public string SoundGunshot { get; }
+
+        public BatteryBarrelComponentState(
+            FireRateSelector fireRateSelector,
+            (int count, int max)? magazine,
+            string soundGunshot) :
+            base(ContentNetIDs.BATTERY_BARREL)
+        {
+            FireRateSelector = fireRateSelector;
+            Magazine = magazine;
+            SoundGunshot = soundGunshot;
+        }
+    }
+}
--- Content.Shared/GameObjects/ContentNetIDs.cs
+++ Content.Shared/GameObjects/ContentNetIDs.cs
@@ -70,6 +70,7 @@
         public const uint REVOLVER_BARREL = 1064;
         public const uint CUFFED = 1065;
         public const uint HANDCUFFS = 1066;
+        public const uint BATTERY_BARREL = 1067;
 
         // Net IDs for integration tests.
         public const uint PREDICTION_TEST = 10001;
