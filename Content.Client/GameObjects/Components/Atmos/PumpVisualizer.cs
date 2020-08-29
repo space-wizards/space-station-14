7f4f7664617c4e6c8c8dfb85401ad099c0985e2e
py01 <pyronetics01@gmail.com>
2020-08-29 15:16:39 -0600

    pump visualizer draft 2

--- Content.Client/GameObjects/Components/Atmos/PumpVisualizer.cs
+++ Content.Client/GameObjects/Components/Atmos/PumpVisualizer.cs
@@ -1,13 +1,25 @@
 ﻿using Content.Shared.GameObjects.Atmos;
 using JetBrains.Annotations;
 using Robust.Client.GameObjects;
+using Robust.Client.Graphics;
 using Robust.Client.Interfaces.GameObjects.Components;
+using Robust.Shared.Utility;
+using YamlDotNet.RepresentationModel;
 
 namespace Content.Client.GameObjects.Components.Disposal
 {
     [UsedImplicitly]
     public class PumpVisualizer : AppearanceVisualizer
     {
+        private RSI _pumpRSI;
+
+        public override void LoadData(YamlMappingNode node)
+        {
+            base.LoadData(node);
+            var rsiString = node.GetNode("pumpRSI").ToString(); //how to load an RSI from a string? I need the appearance to take a different rsi than what the sprite component started as
+
+        }
+
         public override void OnChangeData(AppearanceComponent component)
         {
             base.OnChangeData(component);
@@ -26,7 +38,10 @@ namespace Content.Client.GameObjects.Components.Disposal
             pumpBaseState += pumpVisualState.InletConduitLayer.ToString();
             pumpBaseState += pumpVisualState.OutletConduitLayer.ToString();
 
-            sprite.LayerSetState(Layer.PumpBase, pumpBaseState);
+            var layer = sprite.AddLayerState(pumpBaseState);
+
+            sprite.LayerMapSet(Layer.PumpBase, layer);
+            sprite.LayerSetRSI(layer, _pumpRSI);
         }
 
         private enum Layer
