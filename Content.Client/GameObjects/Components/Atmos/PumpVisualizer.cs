811c2da8f29816ff0ce5df381a8509d2539900d5
py01 <pyronetics01@gmail.com>
2020-08-29 00:54:16 -0600

    pumpvisualizer simplification

--- Content.Client/GameObjects/Components/Atmos/PumpVisualizer.cs
+++ Content.Client/GameObjects/Components/Atmos/PumpVisualizer.cs
@@ -1,27 +1,13 @@
 ﻿using Content.Shared.GameObjects.Atmos;
-using Content.Shared.GameObjects.Components.Atmos;
 using JetBrains.Annotations;
 using Robust.Client.GameObjects;
 using Robust.Client.Interfaces.GameObjects.Components;
-using Robust.Shared.Interfaces.GameObjects;
 
 namespace Content.Client.GameObjects.Components.Disposal
 {
     [UsedImplicitly]
     public class PumpVisualizer : AppearanceVisualizer
     {
-        public override void InitializeEntity(IEntity entity)
-        {
-            base.InitializeEntity(entity);
-
-            if (!entity.TryGetComponent(out ISpriteComponent sprite))
-            {
-                return;
-            }
-            sprite.LayerMapSet(Layer.PumpBase, sprite.AddLayerState("pumpSouthNorth2")); //default
-            sprite.LayerSetShader(Layer.PumpBase, "unshaded");
-        }
-
         public override void OnChangeData(AppearanceComponent component)
         {
             base.OnChangeData(component);
@@ -34,7 +20,6 @@ namespace Content.Client.GameObjects.Components.Disposal
             {
                 return;
             }
-
             var pumpBaseState = "pump";
             pumpBaseState += pumpVisualState.InletDirection.ToString();
             pumpBaseState += pumpVisualState.OutletDirection.ToString();
