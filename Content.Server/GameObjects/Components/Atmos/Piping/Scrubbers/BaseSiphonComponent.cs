b288975cb7dfb13c035f93323225f2e6db5062b8
py01 <60152240+collinlunn@users.noreply.github.com>
2020-09-12 06:26:50 -0600

    Siphon and Vent Visualizers (#2062)
    * scrubber sprites
    
    * vent sprites
    
    * Vent visualstate
    
    * scrubber visual state
    
    * Vent and siphon respect being disabled
    
    * Vent and Siphon Visualizer
    
    * Fix typo
    
    Co-authored-by: py01 <pyronetics01@gmail.com>
    Co-authored-by: DrSmugleaf <DrSmugleaf@users.noreply.github.com>

--- Content.Server/GameObjects/Components/Atmos/Piping/Scrubbers/BaseSiphonComponent.cs
+++ Content.Server/GameObjects/Components/Atmos/Piping/Scrubbers/BaseSiphonComponent.cs
@@ -2,6 +2,8 @@
 using Content.Server.GameObjects.Components.NodeContainer;
 using Content.Server.GameObjects.Components.NodeContainer.Nodes;
 using Content.Server.GameObjects.EntitySystems;
+using Content.Shared.GameObjects.Atmos;
+using Robust.Server.GameObjects;
 using Robust.Shared.GameObjects.Systems;
 using Robust.Shared.Log;
 using Robust.Shared.ViewVariables;
@@ -23,6 +25,20 @@ namespace Content.Server.GameObjects.Components.Atmos.Piping
 
         private AtmosphereSystem _atmosSystem;
 
+        [ViewVariables(VVAccess.ReadWrite)]
+        public bool SiphonEnabled
+        {
+            get => _siphonEnabled;
+            set
+            {
+                _siphonEnabled = value;
+                UpdateAppearance();
+            }
+        }
+        private bool _siphonEnabled = true;
+
+        private AppearanceComponent _appearance;
+
         public override void Initialize()
         {
             base.Initialize();
@@ -40,10 +56,15 @@ namespace Content.Server.GameObjects.Components.Atmos.Piping
                 Logger.Error($"{typeof(BaseSiphonComponent)} on entity {Owner.Uid} could not find compatible {nameof(PipeNode)}s on its {nameof(NodeContainerComponent)}.");
                 return;
             }
+            Owner.TryGetComponent(out _appearance);
+            UpdateAppearance();
         }
 
         public override void Update()
         {
+            if (!SiphonEnabled)
+                return;
+
             var tileAtmos = Owner.Transform.Coordinates.GetTileAtmosphere(_entityManager);
             if (tileAtmos == null)
                 return;
@@ -52,5 +73,10 @@ namespace Content.Server.GameObjects.Components.Atmos.Piping
         }
 
         protected abstract void ScrubGas(GasMixture inletGas, GasMixture outletGas);
+
+        private void UpdateAppearance()
+        {
+            _appearance?.SetData(SiphonVisuals.VisualState, new SiphonVisualState(SiphonEnabled));
+        }
     }
 }
