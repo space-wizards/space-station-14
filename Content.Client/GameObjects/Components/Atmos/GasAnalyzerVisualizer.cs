5b7f364e7fab6ae7a1d277e265b73763d0d8fbf9
Exp <theexp111@gmail.com>
2020-09-18 12:32:18 +0200

    Gives the gas analyzer visual clues that it's open

---
+++ Content.Client/GameObjects/Components/Atmos/GasAnalyzerVisualizer.cs
@@ -0,0 +1,39 @@
+﻿using Content.Shared.GameObjects.Components.Atmos;
+using Robust.Client.GameObjects;
+using Robust.Client.Interfaces.GameObjects.Components;
+
+namespace Content.Client.GameObjects.Components.Atmos
+{
+    class GasAnalyzerVisualizer : AppearanceVisualizer
+    {
+        public override void OnChangeData(AppearanceComponent component)
+        {
+            base.OnChangeData(component);
+
+            if (component.Deleted)
+            {
+                return;
+            }
+
+            if (!component.Owner.TryGetComponent(out ISpriteComponent sprite))
+            {
+                return;
+            }
+
+            if (component.TryGetData(GasAnalyzerVisuals.VisualState, out GasAnalyzerVisualState visualState))
+            {
+                switch (visualState)
+                {
+                    case GasAnalyzerVisualState.Off:
+                        sprite.LayerSetState(0, "icon");
+                        break;
+                    case GasAnalyzerVisualState.Working:
+                        sprite.LayerSetState(0, "working");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
