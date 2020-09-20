2e29c39b2542267347eac1b5f1c6debedd4e44ad
20kdc <asdd2808@gmail.com>
2020-09-21 00:29:45 +0100

    KitchenSpike component: the kitchen spike now has basic functionality
    still placeholder sprite though

---
+++ Content.Server/GameObjects/Components/Kitchen/ButcherableComponent.cs
@@ -0,0 +1,52 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using Content.Server.GameObjects.Components.Body.Digestive;
+using Content.Server.GameObjects.Components.Chemistry;
+using Content.Server.GameObjects.Components.GUI;
+using Content.Server.GameObjects.Components.Items.Storage;
+using Content.Server.GameObjects.EntitySystems;
+using Content.Server.Utility;
+using Content.Shared.Chemistry;
+using Content.Shared.Interfaces;
+using Content.Shared.Interfaces.GameObjects.Components;
+using Content.Shared.Utility;
+using Robust.Server.GameObjects.EntitySystems;
+using Robust.Server.GameObjects;
+using Robust.Shared.Audio;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Localization;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Log;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Server.GameObjects.Components.Kitchen
+{
+    /// <summary>
+    /// Indicates that the entity can be thrown on a kitchen spike for butchering.
+    /// </summary>
+    [RegisterComponent]
+    public class ButcherableComponent : Component
+    {
+        public override string Name => "Butcherable";
+
+        [ViewVariables]
+        public string? MeatPrototype => _meatPrototype;
+
+        [ViewVariables]
+        private string? _meatPrototype;
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _meatPrototype, "meat", null);
+        }
+    }
+}
+
