8a66b61eddb2b4e84a7c8ce2469d35e5c8e0c88c
20kdc <asdd2808@gmail.com>
2020-09-21 21:10:33 +0100

    Monkey cubes: Use IReagentReaction even though it doesn't quite work yet. Butcherable: remove imports

--- Content.Server/GameObjects/Components/Chemistry/RehydratableComponent.cs
+++ Content.Server/GameObjects/Components/Chemistry/RehydratableComponent.cs
@@ -16,12 +16,11 @@ using Robust.Server.GameObjects;
 using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.GameObjects.Components;
 using Robust.Shared.IoC;
 using Robust.Shared.Localization;
 using Robust.Shared.Serialization;
 using Robust.Shared.ViewVariables;
-using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
 using Robust.Shared.Log;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
@@ -31,10 +30,10 @@ namespace Content.Server.GameObjects.Components.Chemistry
     /// <summary>
     /// Basically, monkey cubes.
     /// But specifically, this component deletes the entity and spawns in a new entity when the entity is exposed to a given reagent.
-    /// TODO: This needs support for reaction-on-touch, but that's waiting on merge of Zumorica's work on interactions between sprays and entities
     /// </summary>
     [RegisterComponent]
-    public class RehydratableComponent : Component, ISolutionChange
+    [ComponentReference(typeof(IReagentReaction))]
+    public class RehydratableComponent : Component, IReagentReaction
     {
         public override string Name => "Rehydratable";
 
@@ -43,6 +42,8 @@ namespace Content.Server.GameObjects.Components.Chemistry
         [ViewVariables]
         private string? _targetPrototype;
 
+        private bool _expanding;
+
         public override void ExposeData(ObjectSerializer serializer)
         {
             base.ExposeData(serializer);
@@ -50,18 +51,32 @@ namespace Content.Server.GameObjects.Components.Chemistry
             serializer.DataField(ref _targetPrototype, "target", null);
         }
 
-        void ISolutionChange.SolutionChanged(SolutionChangeEventArgs eventArgs)
+        ReagentUnit IReagentReaction.ReagentReactTouch(ReagentPrototype reagent, ReagentUnit volume) => Reaction(reagent, volume);
+        ReagentUnit IReagentReaction.ReagentReactInjection(ReagentPrototype reagent, ReagentUnit volume) => Reaction(reagent, volume);
+
+        private ReagentUnit Reaction(ReagentPrototype reagent, ReagentUnit volume)
         {
-            var solution = eventArgs.Owner.GetComponent<SolutionContainerComponent>();
-            if (solution.Solution.GetReagentQuantity(_catalystPrototype) > ReagentUnit.Zero)
+            if ((volume > ReagentUnit.Zero) && (reagent.ID == _catalystPrototype))
             {
-                Owner.PopupMessageEveryone(Loc.GetString("{0:TheName} expands!", Owner));
-                if (!string.IsNullOrEmpty(_targetPrototype))
-                {
-                    Owner.EntityManager.SpawnEntity(_targetPrototype, Owner.Transform.Coordinates);
-                }
-                Owner.Delete();
+                Expand();
             }
+            return ReagentUnit.Zero;
+        }
+
+        // Try not to make this public if you can help it.
+        private void Expand()
+        {
+            if (_expanding)
+            {
+                return;
+            }
+            _expanding = true;
+            Owner.PopupMessageEveryone(Loc.GetString("{0:TheName} expands!", Owner));
+            if (!string.IsNullOrEmpty(_targetPrototype))
+            {
+                Owner.EntityManager.SpawnEntity(_targetPrototype, Owner.Transform.Coordinates);
+            }
+            Owner.Delete();
         }
     }
 }
