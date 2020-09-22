81e2097fb2f32db2f891b8ff40ac8b1285e5c6e2
Víctor Aguilera Puerto <zddm@outlook.es>
2020-09-22 13:51:12 +0200

    Popup messages

--- Content.Server/GameObjects/Components/Nutrition/CreamPiedComponent.cs
+++ Content.Server/GameObjects/Components/Nutrition/CreamPiedComponent.cs
@@ -1,9 +1,13 @@
-﻿using Content.Shared.Chemistry;
+﻿using Content.Server.Utility;
+using Content.Shared.Chemistry;
 using Content.Shared.GameObjects.Components.Nutrition;
+using Content.Shared.Interfaces;
 using Content.Shared.Interfaces.GameObjects.Components;
 using Robust.Server.GameObjects;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Localization;
 using Robust.Shared.ViewVariables;
+using Serilog;
 
 namespace Content.Server.GameObjects.Components.Nutrition
 {
@@ -26,14 +30,19 @@ namespace Content.Server.GameObjects.Components.Nutrition
             }
         }
 
+        public void Wash()
+        {
+            if(CreamPied)
+                CreamPied = false;
+        }
+
         public ReagentUnit ReagentReactTouch(ReagentPrototype reagent, ReagentUnit volume)
         {
             switch (reagent.ID)
             {
                 case "chem.SpaceCleaner":
                 case "chem.H2O":
-                    if (CreamPied)
-                        CreamPied = false;
+                    Wash();
                     break;
             }
 
@@ -45,6 +54,8 @@ namespace Content.Server.GameObjects.Components.Nutrition
             if (eventArgs.Target != Owner || !eventArgs.Thrown.TryGetComponent(out CreamPieComponent creamPie) || CreamPied) return;
 
             CreamPied = true;
+            Owner.PopupMessage(Loc.GetString("You have been creamed by {0:theName}!", eventArgs.User));
+            Owner.PopupMessageOtherClients(Loc.GetString("{0:theName} has been creamed by {1:theName}!", Owner, eventArgs.User));
         }
     }
 }
