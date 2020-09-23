2a1b90a0ebc2d2ef529b44a3d7bdfb870e10279e
Paul Ritter <ritter.paul1@googlemail.com>
2020-09-23 14:30:31 +0200

    Arcade fun - Ports over /vg/s SpaceVillain arcade game (#2125)
    * arcade fun
    
    * stuff
    
    * removed unused using statements
    added sound feedback
    win check fix
    reworked ui a bit
    
    * description rework
    
    * windowopen bug fixed
    
    * adds rewards, cool!
    
    * for unusualcrow
    
    * fixes
    
    Co-authored-by: Víctor Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>

--- Content.Shared/GameObjects/ContentNetIDs.cs
+++ Content.Shared/GameObjects/ContentNetIDs.cs
@@ -75,6 +75,7 @@
         public const uint ROTATION = 1069;
         public const uint MOB_STATE_MANAGER = 1070;
         public const uint SLIP = 1071;
+        public const uint SPACE_VILLAIN_ARCADE = 1072;
 
         // Net IDs for integration tests.
         public const uint PREDICTION_TEST = 10001;
