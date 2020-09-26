84ce845ea37d54e6c0889d075d8c8c661f075002
Paul Ritter <ritter.paul1@googlemail.com>
2020-09-26 15:25:22 +0200

    Nanotrasen Block Game is here (#2131)
    * tetris!
    
    * softdropping & left,right key holding
    
    * started work on the ui
    
    * playable state
    
    * there you go exp
    
    * multiuser rework
    
    * ui update refactor
    
    * blockgame:tm:
    
    * highscores, keybindings, ui refactor
    
    * speed adjusts
    leveling
    
    * highscorebackground tweak
    speed tweak
    
    * NULLABLE
    
    * yes

--- Content.Shared/GameObjects/ContentNetIDs.cs
+++ Content.Shared/GameObjects/ContentNetIDs.cs
@@ -76,6 +76,7 @@
         public const uint MOB_STATE_MANAGER = 1070;
         public const uint SLIP = 1071;
         public const uint SPACE_VILLAIN_ARCADE = 1072;
+        public const uint BLOCKGAME_ARCADE = 1073;
 
         // Net IDs for integration tests.
         public const uint PREDICTION_TEST = 10001;
