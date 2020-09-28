408a5e52871320ca92f17d80a6dd9b16558883ff
py01 <pyronetics01@gmail.com>
2020-09-28 11:24:08 -0600

    GasCanisterPort

---
+++ Content.Server/GameObjects/Components/Atmos/Piping/GasCanisterPortComponent.cs
@@ -0,0 +1,34 @@
+﻿using Content.Server.GameObjects.Components.NodeContainer.Nodes;
+using Robust.Shared.ViewVariables;
+using System;
+
+namespace Content.Server.GameObjects.Components.Atmos.Piping
+{
+    public class GasCanisterPortComponent : PipeNetDeviceComponent
+    {
+        public override string Name => "GasCanisterPort";
+
+        [ViewVariables]
+        public bool GasCanisterConnected { get; private set; }
+
+        [ViewVariables]
+        public GasCanisterComponent ConnectedGasCanister { get; private set; }
+
+        [ViewVariables]
+        private PipeNode _gasPort;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+        }
+
+        public override void Update()
+        {
+            if (GasCanisterConnected)
+            {
+                ConnectedGasCanister.Air.Share(_gasPort.Air, 0);
+            }
+        }
+    }
+}
