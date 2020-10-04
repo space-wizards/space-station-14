6510048ad6d4461341f7ca00fbf9c9a598192d86
Julian Giebel <j.giebel@netrocks.info>
2020-10-04 18:01:18 +0200

    Implement device networking

---
+++ Content.Server/GameObjects/EntitySystems/DeviceNetwork/DeviceNetworkConnection.cs
@@ -0,0 +1,61 @@
+﻿using Robust.Shared.ViewVariables;
+using System.Collections.Generic;
+
+namespace Content.Server.GameObjects.EntitySystems.DeviceNetwork
+{
+    public class DeviceNetworkConnection
+    {
+        private readonly DeviceNetworkSystem _network;
+        [ViewVariables]
+        private readonly int _netId;
+
+        [ViewVariables]
+        public bool Open { get; internal set; }
+        [ViewVariables]
+        public string Address { get; internal set; }
+        [ViewVariables]
+        public int Frequency { get; internal set; }
+
+        [ViewVariables]
+        public bool RecieveAll
+        {
+            get => _network.GetDeviceReceiveAll(_netId, Frequency, Address);
+            set => _network.SetDeviceReceiveAll(_netId, Frequency, Address, value);
+        }
+
+        public DeviceNetworkConnection(DeviceNetworkSystem network, int netId, string address, int frequency)
+        {
+            _network = network;
+            _netId = netId;
+            Open = true;
+            Address = address;
+            Frequency = frequency;
+        }
+
+        public bool Send(int frequency, string address, IReadOnlyDictionary<string, string> payload)
+        {
+            return _network.EnqueuePackage(_netId, frequency, address, payload);
+        }
+
+        public bool Send(string address, IReadOnlyDictionary<string, string> payload)
+        {
+            return Send(0, address, payload);
+        }
+
+        public bool Broadcast(int frequency, IReadOnlyDictionary<string, string> payload)
+        {
+            return _network.EnqueuePackage(_netId, frequency, "", payload, true);
+        }
+
+        public bool Broadcast(IReadOnlyDictionary<string, string> payload)
+        {
+            return Broadcast(0, payload);
+        }
+
+        public void Close()
+        {
+            _network.RemoveDevice(_netId, Frequency, Address);
+            Open = false;
+        }
+    }
+}
---
+++ Content.Server/Interfaces/IDeviceNetwork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Content.Server.GameObjects.EntitySystems.DeviceNetwork;
+
+namespace Content.Server.Interfaces
+{
+    public interface IDeviceNetwork
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="netId"><see cref="BaseNetworks"/></param>
+        /// <param name="frequency"></param>
+        /// <param name="messageHandler"></param>
+        /// <param name="receiveAll"></param>
+        /// <returns></returns>
+        public DeviceNetworkConnection Register(int netId, int frequency, OnReceiveNetMessage messageHandler, bool receiveAll = false);
+        public DeviceNetworkConnection Register(int netId, OnReceiveNetMessage messageHandler, bool receiveAll = false);
+    }
+}
