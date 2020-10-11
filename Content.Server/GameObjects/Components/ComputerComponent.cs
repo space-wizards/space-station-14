6e33c15fd7b0ff1319509c785fc5c6cc7494f6c0
Víctor Aguilera Puerto <zddm@outlook.es>
2020-10-11 13:37:19 +0200

    Computers now spawn with circuit boards, add arcade circuitboards.

--- Content.Server/GameObjects/Components/ComputerComponent.cs
+++ Content.Server/GameObjects/Components/ComputerComponent.cs
@@ -1,13 +1,28 @@
 ﻿using Content.Server.GameObjects.Components.Power.ApcNetComponents;
 using Content.Shared.GameObjects.Components;
+using JetBrains.Annotations;
 using Robust.Server.GameObjects;
+using Robust.Server.GameObjects.Components.Container;
+using Robust.Server.Interfaces.GameObjects;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Log;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
 
 namespace Content.Server.GameObjects.Components
 {
     [RegisterComponent]
-    public sealed class ComputerComponent : SharedComputerComponent
+    public sealed class ComputerComponent : SharedComputerComponent, IMapInit
     {
+        [ViewVariables]
+        private string _boardPrototype;
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _boardPrototype, "board", string.Empty);
+        }
+
         public override void Initialize()
         {
             base.Initialize();
@@ -21,6 +36,8 @@ namespace Content.Server.GameObjects.Components
                     appearance.SetData(ComputerVisuals.Powered, powerReceiver.Powered);
                 }
             }
+
+            CreateComputerBoard();
         }
 
         public override void OnRemove()
@@ -39,6 +56,37 @@ namespace Content.Server.GameObjects.Components
             {
                 appearance.SetData(ComputerVisuals.Powered, e.Powered);
             }
+        }
+
+        /// <summary>
+        ///     Creates the corresponding computer board on the computer.
+        ///     This exists so when you deconstruct computers that were serialized with the map,
+        ///     you can retrieve the computer board.
+        /// </summary>
+        private void CreateComputerBoard()
+        {
+            // We don't do anything if this is null or empty.
+            if (string.IsNullOrEmpty(_boardPrototype))
+                return;
+
+            var container = ContainerManagerComponent.Ensure<Container>("board", Owner, out var existed);
+
+            if (existed)
+            {
+                // We already contain a board. Note: We don't check if it's the right one!
+                if (container.ContainedEntities.Count != 0)
+                    return;
+            }
+
+            var board = Owner.EntityManager.SpawnEntity(_boardPrototype, Owner.Transform.Coordinates);
+
+            if(!container.Insert(board))
+                Logger.Warning($"Couldn't insert board {board} to computer {Owner}!");
+        }
+
+        public void MapInit()
+        {
+            CreateComputerBoard();
         }
     }
 }
