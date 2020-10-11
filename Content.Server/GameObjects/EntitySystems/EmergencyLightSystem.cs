40504c25552ced08df4784cc5afa26c6db373228
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2020-10-11 22:11:18 +1100

    Event-based emergency lights (#2217)
    Co-authored-by: Metal Gear Sloth <metalgearsloth@gmail.com>

--- Content.Server/GameObjects/EntitySystems/EmergencyLightSystem.cs
+++ Content.Server/GameObjects/EntitySystems/EmergencyLightSystem.cs
@@ -1,4 +1,6 @@
-﻿using Content.Server.GameObjects.Components.Power.ApcNetComponents.PowerReceiverUsers;
+﻿using System;
+using System.Collections.Generic;
+using Content.Server.GameObjects.Components.Power.ApcNetComponents.PowerReceiverUsers;
 using JetBrains.Annotations;
 using Robust.Shared.GameObjects.Systems;
 
@@ -7,10 +9,42 @@ namespace Content.Server.GameObjects.EntitySystems
     [UsedImplicitly]
     internal sealed class EmergencyLightSystem : EntitySystem
     {
+        private List<EmergencyLightComponent> _activeLights = new List<EmergencyLightComponent>();
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<EmergencyLightMessage>(HandleEmergencyLightMessage);
+        }
+
+        private void HandleEmergencyLightMessage(EmergencyLightMessage message)
+        {
+            switch (message.State)
+            {
+                case EmergencyLightComponent.EmergencyLightState.Charging:
+                    if (_activeLights.Contains(message.Component))
+                        _activeLights.Add(message.Component);
+                    
+                    break;
+                case EmergencyLightComponent.EmergencyLightState.Full:
+                case EmergencyLightComponent.EmergencyLightState.Empty:
+                    _activeLights.Remove(message.Component);
+                    break;
+                case EmergencyLightComponent.EmergencyLightState.On:
+                    if (!_activeLights.Contains(message.Component))
+                        _activeLights.Add(message.Component);
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
         public override void Update(float frameTime)
         {
-            foreach (var comp in ComponentManager.EntityQuery<EmergencyLightComponent>())
+            for (var i = _activeLights.Count - 1; i >= 0; i--)
             {
+                var comp = _activeLights[i];
                 comp.OnUpdate(frameTime);
             }
         }
