06461ea46b977afc8229e7af71e0936b5e4653c5
Víctor Aguilera Puerto <zddm@outlook.es>
2020-10-21 15:36:39 +0200

    update plants

--- Content.Server/GameObjects/EntitySystems/PlantSystem.cs
+++ Content.Server/GameObjects/EntitySystems/PlantSystem.cs
@@ -1,14 +1,24 @@
-﻿using JetBrains.Annotations;
+﻿using Content.Server.GameObjects.Components.Botany;
+using JetBrains.Annotations;
 using Robust.Shared.GameObjects.Systems;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
 
 namespace Content.Server.GameObjects.EntitySystems
 {
     [UsedImplicitly]
     public class PlantSystem : EntitySystem
     {
+        [Dependency] private readonly IComponentManager _componentManager = default!;
+
         public override void Update(float frameTime)
         {
             base.Update(frameTime);
+
+            foreach (var plantHolder in _componentManager.EntityQuery<PlantHolderComponent>())
+            {
+                plantHolder.Update(frameTime);
+            }
         }
     }
 }
