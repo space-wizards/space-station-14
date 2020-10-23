4e47ec2dbdeec39cfcf766e0edd27e6170f5db17
brndd <burneddi@gmail.com>
2020-10-23 23:25:29 +0300

    Put the damage in the windows

---
+++ Content.Client/GameObjects/Components/WindowVisualizer.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.GameObjects.Components
+{
+    public class WindowVisualizer
+    {
+        public sealed class WindowVisualizer : AppearanceVisualizer
+        {
+
+        }
+
+    }
+}
--- Content.Server/GameObjects/Components/WindowComponent.cs
+++ Content.Server/GameObjects/Components/WindowComponent.cs
@@ -1,4 +1,6 @@
 ﻿using Content.Shared.GameObjects.Components;
+using Content.Shared.GameObjects.Components.Damage;
+using Robust.Server.GameObjects;
 using Robust.Shared.GameObjects;
 
 namespace Content.Server.GameObjects.Components
@@ -7,5 +9,28 @@ namespace Content.Server.GameObjects.Components
     [ComponentReference(typeof(SharedWindowComponent))]
     public class WindowComponent : SharedWindowComponent
     {
+        public override void Initialize()
+        {
+            base.Initialize();
+            if (Owner.TryGetComponent(out IDamageableComponent damageableComponent))
+            {
+                damageableComponent.HealthChangedEvent += OnDamage;
+            }
+        }
+
+        private void OnDamage(HealthChangedEventArgs eventArgs)
+        {
+            int current = eventArgs.Damageable.TotalDamage;
+            int max = eventArgs.Damageable.Thresholds[DamageState.Dead];
+            UpdateVisuals(current, max);
+        }
+
+        private void UpdateVisuals(int currentDamage, int maxDamage)
+        {
+            if (Owner.TryGetComponent(out AppearanceComponent appearance))
+            {
+                appearance.SetData(WindowVisuals.Damage, (float) currentDamage / maxDamage);
+            }
+        }
     }
 }
