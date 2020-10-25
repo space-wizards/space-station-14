1ac2c35b531125f8087a705537ed5d6652a2ab92
scuffedjays <yetanotherscuffed@gmail.com>
2020-10-25 07:00:26 -0500

    Add weightlessness status effect.

---
+++ Content.Server/GameObjects/Components/Mobs/WeightlessStatusComponent.cs
@@ -0,0 +1,16 @@
+using Content.Shared.GameObjects.Components.Mobs;
+using Content.Shared.GameObjects.Components.Movement;
+using Robust.Shared.GameObjects;
+
+[RegisterComponent]
+[ComponentReference(typeof(SharedWeightlessStatusComponent))]
+/// <summary>
+/// Simple component to indicate to players when they are experiencing weightlessness.
+/// </summary>
+public class WeightlessStatusComponent : SharedWeightlessStatusComponent
+{    public void Update()
+    {
+        UpdateStatus(Owner.IsWeightless());
+    }
+
+}
---
+++ Content.Shared/GameObjects/Components/Mobs/SharedWeightlessStatusComponent.cs
@@ -0,0 +1,31 @@
+using Robust.Shared.GameObjects;
+
+namespace Content.Shared.GameObjects.Components.Mobs
+{
+    public abstract class SharedWeightlessStatusComponent : Component
+    {
+        //Could probably be better.
+        public override string Name => "WeightlessStatus";
+
+        protected void UpdateStatus(bool isWeightless)
+        {
+
+            if (!Owner.TryGetComponent(out SharedStatusEffectsComponent status))
+            {
+                return;
+            }
+
+            if(isWeightless)
+            {
+                status.ChangeStatusEffect(StatusEffect.Weightless,"/Textures/Interface/StatusEffects/Weightless/weightless.png",null);
+            }
+            else
+            {
+                status.RemoveStatusEffect(StatusEffect.Weightless);
+            }
+
+        }
+
+    }
+
+}
