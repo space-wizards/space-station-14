8183550c2b69aa3aefac305301c6ed2599963e67
Víctor Aguilera Puerto <zddm@outlook.es>
2020-10-25 13:51:52 +0100

    typo

--- Content.Shared/Atmos/Atmospherics.cs
+++ Content.Shared/Atmos/Atmospherics.cs
@@ -231,7 +231,7 @@
     }
 
     /// <summary>
-    ///     Gases to Ids. Keep these updated with the prototypes!
+    ///     Gasses to Ids. Keep these updated with the prototypes!
     /// </summary>
     public enum Gas : sbyte
     {
