64c0695cd6f40975f3012b7c1ac9955de94441fc
Metal Gear Sloth <metalgearsloth@gmail.com>
2020-10-26 19:50:42 +1100

    Align storage welders with door welders
    * Spamming the button doesn't punish you.
    * Requires lit welder to start
    *

--- Content.Server/GameObjects/Components/Items/Storage/EntityStorageComponent.cs
+++ Content.Server/GameObjects/Components/Items/Storage/EntityStorageComponent.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using Content.Server.GameObjects.Components.Body;
 using Content.Server.GameObjects.Components.GUI;
 using Content.Server.GameObjects.Components.Interactable;
 using Content.Shared.GameObjects.Components.Body;
@@ -14,7 +15,6 @@ using Content.Shared.Interfaces.GameObjects.Components;
 using Robust.Server.GameObjects;
 using Robust.Server.GameObjects.Components.Container;
 using Robust.Server.GameObjects.EntitySystems;
-using Robust.Server.Interfaces.GameObjects;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Components;
 using Robust.Shared.GameObjects.Systems;
@@ -47,14 +47,14 @@ namespace Content.Server.GameObjects.Components.Items.Storage
         [ViewVariables]
         private bool _isCollidableWhenOpen;
         [ViewVariables]
-        private IEntityQuery _entityQuery;
+        private IEntityQuery? _entityQuery;
         private bool _showContents;
         private bool _occludesLight;
         private bool _open;
         private bool _isWeldedShut;
 
         [ViewVariables]
-        protected Container Contents;
+        protected Container Contents = default!;
 
         /// <summary>
         /// Determines if the container contents should be drawn when the container is closed.
@@ -96,12 +96,14 @@ namespace Content.Server.GameObjects.Components.Items.Storage
             {
                 _isWeldedShut = value;
 
-                if (Owner.TryGetComponent(out AppearanceComponent appearance))
+                if (Owner.TryGetComponent(out AppearanceComponent? appearance))
                 {
                     appearance.SetData(StorageVisuals.Welded, value);
                 }
             }
         }
+
+        private bool _beingWelded;
 
         [ViewVariables(VVAccess.ReadWrite)]
         public bool CanWeldShut { get; set; }
@@ -218,7 +220,7 @@ namespace Content.Server.GameObjects.Components.Items.Storage
                 placeableSurfaceComponent.IsPlaceable = Open;
             }
 
-            if (Owner.TryGetComponent(out AppearanceComponent appearance))
+            if (Owner.TryGetComponent(out AppearanceComponent? appearance))
             {
                 appearance.SetData(StorageVisuals.Open, Open);
             }
@@ -226,7 +228,7 @@ namespace Content.Server.GameObjects.Components.Items.Storage
 
         private bool AddToContents(IEntity entity)
         {
-            if (entity.TryGetComponent(out IPhysicsComponent entityPhysicsComponent))
+            if (entity.TryGetComponent(out IPhysicsComponent? entityPhysicsComponent))
             {
                 if(MaxSize < entityPhysicsComponent.WorldAABB.Size.X
                     || MaxSize < entityPhysicsComponent.WorldAABB.Size.Y)
@@ -331,25 +333,43 @@ namespace Content.Server.GameObjects.Components.Items.Storage
 
         async Task<bool> IInteractUsing.InteractUsing(InteractUsingEventArgs eventArgs)
         {
-
             if (Open)
+            {
+                _beingWelded = false;
                 return false;
+            }
 
             if (!CanWeldShut)
+            {
+                _beingWelded = false;
                 return false;
+            }
 
             if (Contents.Contains(eventArgs.User))
             {
+                _beingWelded = false;
                 Owner.PopupMessage(eventArgs.User, Loc.GetString("It's too Cramped!"));
                 return false;
             }
 
-            if (!eventArgs.Using.TryGetComponent(out WelderComponent tool))
+            if (!eventArgs.Using.TryGetComponent(out WelderComponent? tool) || !tool.WelderLit)
+            {
+                _beingWelded = false;
                 return false;
+            }
+
+            if (_beingWelded)
+                return false;
+
+            _beingWelded = true;
 
             if (!await tool.UseTool(eventArgs.User, Owner, 1f, ToolQuality.Welding, 1f))
+            {
+                _beingWelded = false;
                 return false;
+            }
 
+            _beingWelded = false;
             IsWeldedShut ^= true;
             return true;
         }
