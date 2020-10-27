4180d3722a1b0622ba577be9e49cfa27619ffa5d
Metal Gear Sloth <metalgearsloth@gmail.com>
2020-10-27 22:26:52 +1100

    Force it
    If no one can get this working then I'm doing impulses which I know I can do.

--- Content.Shared/Physics/MoverController.cs
+++ Content.Shared/Physics/MoverController.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
-using Content.Shared.GameObjects.Components.Movement;
+using System;
+using Robust.Shared.GameObjects.Components;
 using Robust.Shared.Maths;
 using Robust.Shared.Physics;
 
@@ -7,27 +8,52 @@ namespace Content.Shared.Physics
 {
     public class MoverController : VirtualController
     {
-        public void Move(Vector2 velocityDirection, float speed)
+        public override IPhysicsComponent? ControlledComponent { protected get; set; }
+
+        private const float MaxChange = 0.01f;
+
+        // Essentially we need to store our force somewhere so if the player doesn't do any inputs we aren't naturally resisting any new forces.
+        private Vector2 _accumulatedForce = Vector2.Zero;
+
+        private float _forceCap = 10.0f;
+
+        // Essentially do unique shit to make movement feel nicer.
+        public void Push(Vector2 velocityDirection, float speed, float frameTime)
         {
             if (ControlledComponent == null)
                 return;
 
-            if (ControlledComponent.Owner.IsWeightless())
+            var bodyVelocity = ControlledComponent.LinearVelocity;
+            var deltaV = velocityDirection - bodyVelocity;
+
+            if (deltaV.Length <= 0.0)
                 return;
 
-            if (!ControlledComponent.OnGround)
-                return;
+            var accel = deltaV; // frameTime;
 
-            if (ControlledComponent.LinearVelocity.Length > speed)
-                return;
-
-            ControlledComponent.Force += velocityDirection * speed * ControlledComponent.Mass;
+            // Just because I cbf tabbing anymore; body.LinearVelocity += body.Force * body.InvMass * deltaTime;
+            // Thus mass gets cancelled out and so velocity += force * deltaTime
+            var force = accel.Normalized * Math.Min(accel.Length, MaxChange) * ControlledComponent.Mass;
+            Force = force;
         }
 
-        public void StopMoving()
+        public void StopMoving(float frameTime)
         {
-            if (ControlledComponent == null || !ControlledComponent.OnGround) return;
-            ControlledComponent.Force += -ControlledComponent.LinearVelocity * ControlledComponent.Mass;
+            if (ControlledComponent == null)
+                return;
+
+            var bodyVelocity = ControlledComponent.LinearVelocity;
+            var deltaV = -bodyVelocity;
+
+            if (deltaV.Length <= 0.0)
+                return;
+
+            var accel = deltaV; // frameTime;
+
+            // Just because I cbf tabbing anymore; body.LinearVelocity += body.Force * body.InvMass * deltaTime;
+            // Thus mass gets cancelled out and so velocity += force * deltaTime
+            var force = accel.Normalized * Math.Min(accel.Length, MaxChange) * ControlledComponent.Mass;
+            Force = force;
         }
     }
 }
