2780ab8228625dc33081237083f8e598099d6f56
scuffedjays <yetanotherscuffed@gmail.com>
2020-10-27 19:00:47 -0500

    add client side component

---
+++ Content.Client/GameObjects/Components/Mobs/WeightlessStatusComponent.cs
@@ -0,0 +1,10 @@
+using Content.Shared.GameObjects.Components.Mobs;
+using Robust.Shared.GameObjects;
+
+
+[RegisterComponent]
+[ComponentReference(typeof(SharedWeightlessStatusComponent))]
+public class WeightlessStatusComponent : SharedWeightlessStatusComponent
+{
+
+}
