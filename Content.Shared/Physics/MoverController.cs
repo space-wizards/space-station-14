cdda2a163d1da33810d20295e73ab904f44b920f
brndd <burneddi@gmail.com>
2020-10-29 23:16:11 +0200

    I don't even know WHAT I changed

--- Content.Shared/Physics/MoverController.cs
+++ Content.Shared/Physics/MoverController.cs
@@ -1,6 +1,7 @@
 ﻿#nullable enable
 using Content.Shared.GameObjects.Components.Movement;
 using Robust.Shared.GameObjects.Components;
+using Robust.Shared.Log;
 using Robust.Shared.Maths;
 using Robust.Shared.Physics;
 
@@ -11,8 +12,9 @@ namespace Content.Shared.Physics
         public override IPhysicsComponent? ControlledComponent { protected get; set; }
 
         private float thrust = 275f; //thrust in newtons
-        private float mass = 60; //don't do this kids
-        private float friction_coeff = 5f; //friction coefficient
+        private float mass = 85f; //don't do this kids
+        private float friction_coeff = 1f; //friction coefficient
+        private float time_to_vmax = 5f;
 
         public void Move(Vector2 velocityDirection, float speed)
         {
@@ -29,14 +31,23 @@ namespace Content.Shared.Physics
             LinearVelocity = velocityDirection * speed;
         }
 
-        public void ApplyForce(Vector2 force)
+        public void ApplyForce(Vector2 force, float frameTime)
         {
             if (ControlledComponent == null) return;
-            force *= thrust;
-            Vector2 friction = -ControlledComponent.LinearVelocity * friction_coeff;
+            float friction_coeff = 5 / time_to_vmax;
+            force *= friction_coeff;
+            Vector2 friction;
+            if(ControlledComponent.LinearVelocity.Length > 0)
+            {
+                friction = -ControlledComponent.LinearVelocity * friction_coeff;
+            }
+            else
+            {
+                friction = Vector2.Zero;
+            }
             force += friction;
-            Vector2 acceleration = force / mass;
-            ControlledComponent.LinearVelocity  += acceleration;
+            Force = force * frameTime;
+
         }
 
         public void StopMoving()
