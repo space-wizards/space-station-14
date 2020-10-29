a546c864b3ac2ea5d77fac2ffb395b159469bd3b
20kdc <asdd2808@gmail.com>
2020-10-29 14:15:36 +0000

    Station Extension/Repair: Underplating recipe works now

---
+++ Content.Server/Construction/Completions/SpawnTile.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+using System.Threading.Tasks;
+using Content.Shared.Construction;
+using JetBrains.Annotations;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.Map;
+using Robust.Shared.Map;
+using Robust.Shared.IoC;
+using Robust.Shared.Serialization;
+
+namespace Content.Server.Construction.Completions
+{
+    [UsedImplicitly]
+    public class SpawnTile : IGraphAction
+    {
+        public string Tile { get; private set; } = string.Empty;
+
+        public void ExposeData(ObjectSerializer serializer)
+        {
+            serializer.DataField(this, x => x.Tile, "tile", string.Empty);
+        }
+
+        public async Task PerformAction(IEntity entity, IEntity? user)
+        {
+            if (entity.Deleted) return;
+
+            var mapManager = IoCManager.Resolve<IMapManager>();
+            var tileDefinitionManager = IoCManager.Resolve<ITileDefinitionManager>();
+
+            var grid = mapManager.GetGrid(entity.Transform.GridID);
+            grid.SetTile(entity.Transform.Coordinates, new Tile(tileDefinitionManager[Tile].TileId));
+        }
+    }
+}
---
+++ Content.Shared/Construction/ConstructionConditions/TileIsExactly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Content.Shared.GameObjects.Components;
+using Content.Shared.Maps;
+using JetBrains.Annotations;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Map;
+using Robust.Shared.Maths;
+using Robust.Shared.Serialization;
+using Robust.Shared.IoC;
+using Robust.Shared.Interfaces.Map;
+
+namespace Content.Shared.Construction.ConstructionConditions
+{
+    [UsedImplicitly]
+    public class TileIsExactly : IConstructionCondition
+    {
+        public void ExposeData(ObjectSerializer serializer)
+        {
+            serializer.DataField(this, x => x.TileName, "tile", string.Empty);
+        }
+
+        /// <summary>
+        ///     The tile ID.
+        /// </summary>
+        public string TileName { get; private set; }
+
+        public bool Condition(IEntity user, EntityCoordinates location, Direction direction)
+        {
+            var tileRef = location.GetTileRef();
+            var tileIDNum = (tileRef?.Tile ?? Tile.Empty).TypeId;
+            var tileDefinitionManager = IoCManager.Resolve<ITileDefinitionManager>();
+            return tileIDNum == tileDefinitionManager[TileName].TileId;
+        }
+    }
+}
