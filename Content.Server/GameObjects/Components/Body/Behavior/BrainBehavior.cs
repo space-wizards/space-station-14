f8f156d85b276afd82e89280fc35c5f4209601ad
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-11-01 16:46:17 +0100

    Remove outdated component ignores

--- Content.Server/GameObjects/Components/Body/Behavior/BrainBehavior.cs
+++ Content.Server/GameObjects/Components/Body/Behavior/BrainBehavior.cs
@@ -1,7 +1,6 @@
 ﻿#nullable enable
 using Content.Server.GameObjects.Components.Mobs;
 using Content.Shared.GameObjects.Components.Body;
-using Content.Shared.GameObjects.Components.Body.Behavior;
 using Content.Shared.GameObjects.Components.Body.Part;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Interfaces.GameObjects;
