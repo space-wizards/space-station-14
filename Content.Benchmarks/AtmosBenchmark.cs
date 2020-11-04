3456b847c5b9589d65df69aa69ccc9383e22ef9c
Víctor Aguilera Puerto <zddm@outlook.es>
2020-11-04 18:00:26 +0100

    Adds ContentBenchmark helper class for advanced benchmarks, adds Atmos benchmark

---
+++ Content.Benchmarks/AtmosBenchmark.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Disassemblers;
+using BenchmarkDotNet.Engines;
+using Content.Server.GameObjects.Components.Atmos;
+using Content.Shared.Atmos;
+using Content.Shared.Maps;
+using Robust.Server.Interfaces.Timing;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.Map;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
+using Robust.Shared.Maths;
+
+namespace Content.Benchmarks
+{
+    public class AtmosBenchmark : ContentBenchmark
+    {
+        private string _tileName = "floor_steel";
+        private string _wallPrototype = "reinforced_wall";
+        private int _squareSize = 32;
+        private int _ticks = 30 * 60; // 1 minute in 30 TPS
+
+        private Dictionary<Gas, float> _gases = new Dictionary<Gas, float>()
+        {
+            { Gas.Oxygen, 10000f },
+            { Gas.Phoron, 10000f }
+        };
+
+        private float _temperature = 10000f;
+        private ServerIntegrationInstance _server;
+
+        private int HalfSquareSize => _squareSize / 2;
+
+        [Benchmark]
+        public void PhoronFireBenchmark()
+        {
+            bool CoordinateWall(int x) => x == -HalfSquareSize || x == (HalfSquareSize - 1);
+
+            _server.Loop(() =>
+            {
+                var mapManager = IoCManager.Resolve<IMapManager>();
+                var entityManager = IoCManager.Resolve<IEntityManager>();
+                var tileDefinitionManager = IoCManager.Resolve<ITileDefinitionManager>();
+                var pauseManager = IoCManager.Resolve<IPauseManager>();
+
+                foreach (var mapId in mapManager.GetAllMapIds())
+                {
+                    pauseManager.SetMapPaused(mapId, true);
+                }
+
+                var newMap = mapManager.CreateMap();
+                var grid = mapManager.CreateGrid(newMap);
+                var map = mapManager.GetMapEntity(newMap);
+                var gridEnt = entityManager.GetEntity(grid.GridEntityId);
+
+                var tile = new Tile(tileDefinitionManager[_tileName].TileId);
+
+                var gridAtmos = gridEnt.AddComponent<GridAtmosphereComponent>();
+
+                for (var x = -HalfSquareSize; x < HalfSquareSize; x++)
+                {
+                    for (var y = -HalfSquareSize; y < HalfSquareSize; y++)
+                    {
+                        var vector = new Vector2i(x, y);
+                        grid.SetTile(vector, tile);
+
+                        if (CoordinateWall(x) || CoordinateWall(y))
+                        {
+                            entityManager.SpawnEntity(_wallPrototype, new EntityCoordinates(gridEnt.Uid, vector));
+                        }
+                    }
+                }
+
+                gridAtmos.RepopulateTiles();
+                gridAtmos.Update(0f);
+
+                var zeroAtmos = gridAtmos.GetTile(Vector2i.Zero);
+
+                if (zeroAtmos == null)
+                    throw new NullReferenceException("Atmosphere at (0, 0) is null!");
+
+                foreach (var (gas, amount) in _gases)
+                {
+                    zeroAtmos.Air.AdjustMoles(gas, amount);
+                }
+
+                zeroAtmos.Air.Temperature = _temperature;
+
+                gridAtmos.AddActiveTile(zeroAtmos);
+
+                gridAtmos.Invalidate(Vector2i.Zero);
+            }, _ticks);
+        }
+    }
+}
---
+++ Content.Benchmarks/ContentBenchmark.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using Content.IntegrationTests;
+
+namespace Content.Benchmarks
+{
+    public class ContentBenchmark : ContentIntegrationTest
+    {
+        private string _robustResourceDest =
+            Path.Combine(Directory.GetCurrentDirectory(), "../../RobustToolbox/Resources/");
+        private string _resourceDest = Path.Combine(Directory.GetCurrentDirectory(), "../../Resources/");
+        private string _serverDest = Path.Combine(Directory.GetCurrentDirectory(), "../../bin/Content.Server/");
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            DirectoryCopy(Path.Combine(Directory.GetCurrentDirectory(), "../../../../../../RobustToolbox/Resources"),
+                _robustResourceDest);
+            DirectoryCopy(Path.Combine(Directory.GetCurrentDirectory(), "../../../../../Content.Server"),
+                _serverDest);
+            DirectoryCopy(Path.Combine(Directory.GetCurrentDirectory(), "../../../../../../Resources"),
+                _resourceDest);
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            Directory.Delete(_robustResourceDest, true);
+            Directory.Delete(_serverDest, true);
+            Directory.Delete(_resourceDest, true);
+        }
+
+        protected override ServerIntegrationInstance StartServer(ServerIntegrationOptions options = null)
+        {
+            options ??= new ServerContentIntegrationOption();
+            options.NotThreaded = true;
+            return base.StartServer(options);
+        }
+
+        // I'm lazy and grabbed this method from here:
+        // https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs = true)
+        {
+            // Get the subdirectories for the specified directory.
+            var dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            var dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                var tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (var subdir in dirs)
+                {
+                    var tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
+    }
+}
