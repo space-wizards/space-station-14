36ceca943ae4a1992e52682887e53080c74b3c61
scuffedjays <yetanotherscuffed@gmail.com>
2020-11-09 19:31:55 -0600

    add juiceable component

---
+++ Content.Server/GameObjects/Components/Kitchen/JuiceableComponent.cs
@@ -0,0 +1,22 @@
+﻿using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization;
+
+namespace Content.Server.GameObjects.Components.Kitchen
+{
+    /// <summary>
+    /// Tag component that denotes an entity as Juiceable
+    /// </summary>
+    [RegisterComponent]
+    public class JuiceableComponent : Component
+    {
+        public override string Name => "Juicable";
+        //TODO: add juice result.
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            
+        }
+
+    }
+}
