46092cf5feaa0cb2bef901ab587cfba0be64b93a
Paul <ritter.paul1+git@googlemail.com>
2020-11-11 09:30:59 +0100

    fully implements stealcondition

--- Content.Server/Objectives/Conditions/StealCondition.cs
+++ Content.Server/Objectives/Conditions/StealCondition.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Content.Server.Mobs;
 using Content.Server.Objectives.Interfaces;
+using Content.Shared.Objectives;
+using Robust.Server.GameObjects.Components.Container;
 using Robust.Shared.Serialization;
 using Robust.Shared.Utility;
 
@@ -8,27 +10,36 @@ namespace Content.Server.Objectives.Conditions
 {
     public class StealCondition : IObjectiveCondition
     {
-        public string PrototypeID { get; private set; }
+        public string PrototypeId { get; private set; }
 
         public void ExposeData(ObjectSerializer serializer)
         {
-            serializer.DataField(this, x => x.PrototypeID, "prototype", "");
+            serializer.DataField(this, x => x.PrototypeId, "prototype", "");
         }
 
-        public string GetTitle() => $"Steal prototype {PrototypeID}";
+        public string GetTitle() => $"Steal prototype {PrototypeId}";
 
-        public string GetDescription() => $"We need you to steal prototype {PrototypeID}. Dont get caught.";
+        public string GetDescription() => $"We need you to steal prototype {PrototypeId}. Dont get caught.";
 
         public SpriteSpecifier GetIcon()
         {
-            //TODO
-            throw new NotImplementedException();
+            return new PrototypeIcon(PrototypeId);
         }
 
         public float GetProgress(Mind mind)
         {
-            //TODO
-            throw new NotImplementedException();
+            if (mind.OwnedEntity == null) return 0f;
+            if (!mind.OwnedEntity.TryGetComponent<ContainerManagerComponent>(out var containerManagerComponent)) return 0f;
+
+            foreach (var container in containerManagerComponent.GetAllContainers())
+            {
+                foreach (var entity in container.ContainedEntities)
+                {
+                    if (entity.Prototype?.ID == PrototypeId) return 1f;
+                }
+            }
+
+            return 0f;
         }
 
         public float GetDifficulty() => 1f;
---
+++ Content.Shared/Objectives/PrototypeIcon.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Objectives
+{
+    public sealed class PrototypeIcon : SpriteSpecifier
+    {
+        public readonly string PrototypeId;
+
+        public PrototypeIcon(string prototypeId)
+        {
+            PrototypeId = prototypeId;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is PrototypeIcon prototypeIcon)
+                return PrototypeId == prototypeIcon.PrototypeId;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return PrototypeId.GetHashCode();
+        }
+    }
+}
