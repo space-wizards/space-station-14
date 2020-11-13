8b6e44f1449f74d767f4958b7863e59fb588fd17
brndd <burneddi@gmail.com>
2020-11-13 05:50:41 +0200

    Clean up movercontroller + RK4 for stopping

--- Content.Shared/Physics/MoverController.cs
+++ Content.Shared/Physics/MoverController.cs
@@ -12,21 +12,20 @@ namespace Content.Shared.Physics
     {
         public override IPhysicsComponent? ControlledComponent { protected get; set; }
 
-        private float time_to_vmax = 0.2f;
-        private float max_speed = 7f;
+        private float _timeToVmax = 0.2f;
 
         public void Move(Vector2 velocityDirection, float frameTime)
         {
-            if (ControlledComponent == null || (ControlledComponent.Owner.IsWeightless()))
+            if (ControlledComponent == null || ControlledComponent.Owner.IsWeightless())
             {
                 return;
             }
             //apply a counteracting force to the standard friction between a human and a floor
             //TODO: friction should involve mass, but in the current physics system it doesn't so we can't have it here either
-            Vector2 antiFriction = velocityDirection.Normalized * (0.35f * 9.8f * frameTime);
+            //Vector2 antiFriction = velocityDirection.Normalized * (0.35f * 9.8f * frameTime);
 
             float mass = ControlledComponent.Mass;
-            float dragCoeff = 5 / time_to_vmax;
+            float dragCoeff = 5 / _timeToVmax;
             Vector2 thrustForce = velocityDirection * dragCoeff * mass;
             Vector2 linearVelocity = ControlledComponent.LinearVelocity;
 
@@ -60,34 +59,38 @@ namespace Content.Shared.Physics
 
         public void StopMoving(float frameTime)
         {
-            if (ControlledComponent == null) return;
+            if (ControlledComponent == null || (ControlledComponent.Owner.IsWeightless()))
+            {
+                return;
+            }
+
+            //apply a counteracting force to the standard friction between a human and a floor
+            //TODO: friction should involve mass, but in the current physics system it doesn't so we can't have it here either
+            //Vector2 antiFriction = velocityDirection.Normalized * (0.35f * 9.8f * frameTime);
+
+            float mass = ControlledComponent.Mass;
+            float dragCoeff = 5 / _timeToVmax;
+            var linearVelocity = ControlledComponent.LinearVelocity;
+
             Vector2 netForce = Vector2.Zero;
-            float drag_coeff = 5 / time_to_vmax;
-            netForce *= drag_coeff;
-            Vector2 dragForce;
-            if(ControlledComponent.LinearVelocity.Length > 0)
-            {
+            Vector2 a = netForce / mass;
+            Vector2 k1 = a * frameTime;
 
-                dragForce = -ControlledComponent.LinearVelocity * drag_coeff;
-            }
-            else
-            {
-                dragForce = Vector2.Zero;
-            }
-            netForce += dragForce;
-            netForce *= ControlledComponent.Mass;
+            netForce = - (linearVelocity + k1/2) * dragCoeff * mass;
+            a = netForce / mass;
+            Vector2 k2 = a * frameTime;
 
-            // //Overshoot check
-            // Vector2 deltaV = netForce * ControlledComponent.InvMass * frameTime;
-            // if (deltaV.LengthSquared > ControlledComponent.LinearVelocity.LengthSquared)
-            // {
-            //     Logger.Debug("STOP: Clamping overshoot.");
-            //     Force = -(ControlledComponent.LinearVelocity * ControlledComponent.Mass) / frameTime;
-            // }
-            // else
-            // {
-            //     Force = netForce;
-            // }
+            netForce = - (linearVelocity + k2/2) * dragCoeff * mass;
+            a = netForce / mass;
+            Vector2 k3 = a * frameTime;
+
+            netForce = - (linearVelocity + k3) * dragCoeff * mass;
+            a = netForce / mass;
+            Vector2 k4 = a * frameTime;
+
+            Vector2 deltaV = (k1 + k2*2 + k3*2 + k4) / 6;
+            a = deltaV / frameTime;
+            netForce = a * mass;
             Force = netForce;
         }
 
