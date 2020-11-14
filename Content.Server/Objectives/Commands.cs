c5ab864a08352412b9cb5fd80b29091b502ceecc
Paul <ritter.paul1+git@googlemail.com>
2020-11-14 15:09:44 +0100

    giving commands some love

--- Content.Server/Objectives/Commands.cs
+++ Content.Server/Objectives/Commands.cs
@@ -22,66 +22,70 @@ namespace Content.Server.Objectives
             }
 
             var mgr = IoCManager.Resolve<IPlayerManager>();
-            if (mgr.TryGetPlayerDataByUsername(args[0], out var data))
-            {
-                var mind = data.ContentData()?.Mind;
-                if (mind == null)
-                {
-                    shell.SendText(player, "Can't find the mind.");
-                    return;
-                }
-
-                if (!IoCManager.Resolve<IPrototypeManager>()
-                    .TryIndex<ObjectivePrototype>(args[1], out var objectivePrototype))
-                {
-                    shell.SendText(player, $"Can't find matching ObjectivePrototype {objectivePrototype}");
-                    return;
-                }
-
-                if (!mind.TryAddObjective(objectivePrototype))
-                {
-                     shell.SendText(player, "Objective requirements dont allow that objective to be added.");
-                }
-            }
-            else
+            if (!mgr.TryGetPlayerDataByUsername(args[0], out var data))
             {
                 shell.SendText(player, "Can't find the playerdata.");
+                return;
             }
+
+
+            var mind = data.ContentData()?.Mind;
+            if (mind == null)
+            {
+                shell.SendText(player, "Can't find the mind.");
+                return;
+            }
+
+            if (!IoCManager.Resolve<IPrototypeManager>()
+                .TryIndex<ObjectivePrototype>(args[1], out var objectivePrototype))
+            {
+                shell.SendText(player, $"Can't find matching ObjectivePrototype {objectivePrototype}");
+                return;
+            }
+
+            if (!mind.TryAddObjective(objectivePrototype))
+            {
+                shell.SendText(player, "Objective requirements dont allow that objective to be added.");
+            }
+
         }
     }
 
     public class ListObjectivesCommand : IClientCommand
     {
         public string Command => "lsobjectives";
-        public string Description => "Lists all objectives in a players mind";
-        public string Help => "lsobjectives <username>";
+        public string Description => "Lists all objectives in a players mind.";
+        public string Help => "lsobjectives [<username>]";
         public void Execute(IConsoleShell shell, IPlayerSession? player, string[] args)
         {
-            var mgr = IoCManager.Resolve<IPlayerManager>();
-            if (mgr.TryGetPlayerDataByUsername(args[0], out var data))
+            IPlayerData? data;
+            if (args.Length == 0 && player != null)
             {
-                var mind = data.ContentData()?.Mind;
-                if (mind == null)
-                {
-                    shell.SendText(player, "Can't find the mind.");
-                    return;
-                }
-
-                shell.SendText(player, $"Objectives for player {args[0]}:");
-                var objectives = mind.AllObjectives.ToList();
-                if (objectives.Count == 0)
-                {
-                    shell.SendText(player, "None.");
-                }
-                for (var i = 0; i < objectives.Count; i++)
-                {
-                    shell.SendText(player, $"- [{i}] {objectives[i]}");
-                }
-            }
-            else
+                data = player.Data;
+            }else if (!IoCManager.Resolve<IPlayerManager>().TryGetPlayerDataByUsername(args[0], out data))
             {
                 shell.SendText(player, "Can't find the playerdata.");
+                return;
             }
+
+            var mind = data.ContentData()?.Mind;
+            if (mind == null)
+            {
+                shell.SendText(player, "Can't find the mind.");
+                return;
+            }
+
+            shell.SendText(player, $"Objectives for player {args[0]}:");
+            var objectives = mind.AllObjectives.ToList();
+            if (objectives.Count == 0)
+            {
+                shell.SendText(player, "None.");
+            }
+            for (var i = 0; i < objectives.Count; i++)
+            {
+                shell.SendText(player, $"- [{i}] {objectives[i]}");
+            }
+
         }
     }
 
@@ -92,6 +96,12 @@ namespace Content.Server.Objectives
         public string Help => "rmobjective <username> <index>";
         public void Execute(IConsoleShell shell, IPlayerSession? player, string[] args)
         {
+            if (args.Length != 2)
+            {
+                shell.SendText(player, "Expected exactly 2 arguments.");
+                return;
+            }
+
             var mgr = IoCManager.Resolve<IPlayerManager>();
             if (mgr.TryGetPlayerDataByUsername(args[0], out var data))
             {
