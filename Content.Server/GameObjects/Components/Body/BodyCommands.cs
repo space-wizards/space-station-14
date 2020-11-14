c5c74500cdd418ef32d0920c99b45ca7677892df
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-11-14 15:58:21 +0100

    Improve hurt command feedback and usability

--- Content.Server/GameObjects/Components/Body/BodyCommands.cs
+++ Content.Server/GameObjects/Components/Body/BodyCommands.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Content.Server.Administration;
 using Content.Shared.Administration;
@@ -252,84 +253,212 @@ namespace Content.Server.GameObjects.Components.Body
     {
         public string Command => "hurt";
         public string Description => "Ouch";
-        public string Help => $"Usage: {Command} <type> <amount> (<entity uid/_>) (<ignoreResistance>)";
+        public string Help => $"Usage: {Command} <type/?> <amount> (<entity uid/_>) (<ignoreResistances>)";
 
-        private void SendDamageTypes(IConsoleShell shell, IPlayerSession? player)
+        private string DamageTypes()
         {
             var msg = "";
             foreach (var dClass in Enum.GetNames(typeof(DamageClass)))
             {
                 msg += $"\n{dClass}";
                 var types = Enum.Parse<DamageClass>(dClass).ToTypes();
-                foreach (var dType in types)
+
+                if (types.Count > 0)
                 {
-                    msg += $"\n - {dType}";
+                    msg += ": ";
                 }
+
+                msg += string.Join('|', types);
             }
-            shell.SendText(player, $"Damage Types:{msg}");
+
+            return $"Damage Types:{msg}";
+        }
+
+        private delegate void Damage(IDamageableComponent damageable, bool ignoreResistances);
+
+        private bool TryParseEntity(IConsoleShell shell, IPlayerSession? player, string arg,
+            [NotNullWhen(true)] out IEntity? entity)
+        {
+            entity = null;
+
+            if (arg == "_")
+            {
+                var playerEntity = player?.AttachedEntity;
+
+                if (playerEntity == null)
+                {
+                    shell.SendText(player, $"You must have a player entity to use this command without specifying an entity.\n{Help}");
+                    return false;
+                }
+
+                entity = playerEntity;
+                return true;
+            }
+
+            if (!EntityUid.TryParse(arg, out var entityUid))
+            {
+                shell.SendText(player, $"{arg} is not a valid entity uid.\n{Help}");
+
+                return false;
+            }
+
+            var entityManager = IoCManager.Resolve<IEntityManager>();
+
+            if (!entityManager.TryGetEntity(entityUid, out var parsedEntity))
+            {
+                shell.SendText(player, $"No entity found with uid {entityUid}");
+
+                return false;
+            }
+
+            entity = parsedEntity;
+            return true;
+        }
+
+        private bool TryParseDamageArgs(
+            IConsoleShell shell,
+            IPlayerSession? player,
+            string[] args,
+            [NotNullWhen(true)] out Damage? func)
+        {
+            if (!int.TryParse(args[1], out var amount))
+            {
+                shell.SendText(player, $"{args[1]} is not a valid damage integer.");
+
+                func = null;
+                return false;
+            }
+
+            if (Enum.TryParse<DamageClass>(args[0], true, out var damageClass))
+            {
+                func = (damageable, ignoreResistances) =>
+                {
+                    if (!damageable.DamageClasses.ContainsKey(damageClass))
+                    {
+                        shell.SendText(player, $"Entity {damageable.Owner.Name} with id {damageable.Owner.Uid} can not be damaged with damage class {damageClass}");
+
+                        return;
+                    }
+
+                    if (!damageable.ChangeDamage(damageClass, amount, ignoreResistances))
+                    {
+                        shell.SendText(player, $"Entity {damageable.Owner.Name} with id {damageable.Owner.Uid} received no damage.");
+
+                        return;
+                    }
+
+                    var response =
+                        $"Damaged entity {damageable.Owner.Name} with id {damageable.Owner.Uid} for {amount} {damageClass} damage{(ignoreResistances ? ", ignoring resistances." : ".")}";
+
+                    shell.SendText(player, response);
+                };
+
+                return true;
+            }
+            // Fall back to DamageType
+            else if (Enum.TryParse<DamageType>(args[0], true, out var damageType))
+            {
+                func = (damageable, ignoreResistances) =>
+                {
+                    if (!damageable.DamageTypes.ContainsKey(damageType))
+                    {
+                        shell.SendText(player, $"Entity {damageable.Owner.Name} with id {damageable.Owner.Uid} can not be damaged with damage class {damageType}");
+
+                        return;
+                    }
+
+                    if (!damageable.ChangeDamage(damageType, amount, ignoreResistances))
+                    {
+                        shell.SendText(player, $"Entity {damageable.Owner.Name} with id {damageable.Owner.Uid} received no damage.");
+
+                        return;
+                    }
+
+                    var response =
+                        $"Damaged entity {damageable.Owner.Name} with id {damageable.Owner.Uid} for {amount} {damageType} damage{(ignoreResistances ? ", ignoring resistances." : ".")}";
+
+                    shell.SendText(player, response);
+                };
+
+                return true;
+            }
+            else
+            {
+                shell.SendText(player, $"{args[0]} is not a valid damage class or type.");
+
+                var types = DamageTypes();
+                shell.SendText(player, types);
+
+                func = null;
+                return false;
+            }
         }
 
         public void Execute(IConsoleShell shell, IPlayerSession? player, string[] args)
         {
-            // Check if we have enough for the dmg types to show
-            if (args.Length > 0 && args[0] == "?")
+            bool ignoreResistances;
+            IEntity entity;
+            Damage? damageFunc;
+
+            switch (args.Length)
             {
-                SendDamageTypes(shell, player);
+                // Check if we have enough for the dmg types to show
+                case var n when n > 0 && (args[0] == "?" || args[0] == "¿"):
+                    var types = DamageTypes();
+
+                    if (args[0] == "¿")
+                    {
+                        types = types.Replace("e", "é");
+                    }
+
+                    shell.SendText(player, types);
+
+                    return;
+                // Not enough args
+                case var n when n < 2:
+                    shell.SendText(player, $"Invalid number of arguments.\n{Help}");
+                    return;
+                case var n when n >= 2 && n <= 4:
+                    if (!TryParseDamageArgs(shell, player, args, out damageFunc))
+                    {
+                        return;
+                    }
+
+                    var entityUid = n == 2 ? "_" : args[2];
+
+                    if (!TryParseEntity(shell, player, entityUid, out var parsedEntity))
+                    {
+                        return;
+                    }
+
+                    entity = parsedEntity;
+
+                    if (n == 4)
+                    {
+                        if (!bool.TryParse(args[3], out ignoreResistances))
+                        {
+                            shell.SendText(player, $"{args[3]} is not a valid boolean value for ignoreResistances.\n{Help}");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        ignoreResistances = false;
+                    }
+
+                    break;
+                default:
+                    shell.SendText(player, $"Invalid amount of arguments.\n{Help}");
+                    return;
+            }
+
+            if (!entity.TryGetComponent(out IDamageableComponent? damageable))
+            {
+                shell.SendText(player, $"Entity {entity.Name} with id {entity.Uid} does not have a {nameof(IDamageableComponent)}.");
                 return;
             }
 
-            // Not enough args
-            if (args.Length < 2)
-            {
-                shell.SendText(player, Help);
-                return;
-            }
-
-            var ignoreResistance = false;
-            var entityUid = player != null && player.AttachedEntityUid.HasValue ? player.AttachedEntityUid.Value : EntityUid.Invalid;
-            if (!int.TryParse(args[1], out var amount) ||
-                args.Length >= 3 && args[2] != "_" && !EntityUid.TryParse(args[2], out entityUid) ||
-                args.Length >= 4 && !bool.TryParse(args[3], out ignoreResistance))
-            {
-                shell.SendText(player, Help);
-                return;
-            }
-
-            if (entityUid == EntityUid.Invalid)
-            {
-                shell.SendText(player, "Not a valid entity.");
-                return;
-            }
-
-            if (!IoCManager.Resolve<IEntityManager>().TryGetEntity(entityUid, out var ent))
-            {
-                shell.SendText(player, "Entity couldn't be found.");
-                return;
-            }
-
-            if (!ent.TryGetComponent(out IDamageableComponent? damageable))
-            {
-                shell.SendText(player, "Entity can't be damaged.");
-                return;
-            }
-
-            if (Enum.TryParse<DamageClass>(args[0], true, out var dmgClass))
-            {
-                if (!damageable.ChangeDamage(dmgClass, amount, ignoreResistance))
-                    shell.SendText(player, "Something went wrong!");
-                return;
-            }
-            // Fall back to DamageType
-            else if (Enum.TryParse<DamageType>(args[0], true, out var dmgType))
-            {
-                if (!damageable.ChangeDamage(dmgType, amount, ignoreResistance))
-                    shell.SendText(player, "Something went wrong!");
-                return;
-            }
-            else
-            {
-                SendDamageTypes(shell, player);
-            }
+            damageFunc(damageable, ignoreResistances);
         }
     }
 }
