46d27e0aa1d5705ea5e43786d9ef61314bdacc2f
brndd <burneddi@gmail.com>
2020-11-15 08:01:34 +0200

    extra low tick leniency

--- Content.Shared/Physics/MoverController.cs
+++ Content.Shared/Physics/MoverController.cs
@@ -12,26 +12,12 @@ namespace Content.Shared.Physics
     {
         public override IPhysicsComponent? ControlledComponent { protected get; set; }
 
-        private float _timeToVmax = 0.2f;
+        private float timeToVmax = 0.2f;
+        private float minDesiredTimeStep = 1 / 30f;
 
-        public void Move(Vector2 velocityDirection, float frameTime)
+        private Vector2 StepSimulation(Vector2 velocityDirection, Vector2 linearVelocity, float dragCoeff, float mass, float frameTime)
         {
-            if (ControlledComponent == null)
-            {
-                return;
-            }
-            //apply a counteracting force to the standard friction between a human and a floor
-            //TODO: friction should involve mass, but in the current physics system it doesn't so we can't have it here either
-            //Vector2 antiFriction = velocityDirection.Normalized * (0.35f * 9.8f * frameTime);
-
-            float mass = ControlledComponent.Mass;
-            float dragCoeff = 5 / _timeToVmax;
-            if (ControlledComponent.Owner.IsWeightless())
-            {
-                dragCoeff /= 50; //ice level time
-            }
             Vector2 thrustForce = velocityDirection * dragCoeff * mass;
-            Vector2 linearVelocity = ControlledComponent.LinearVelocity;
 
             Vector2 netForce = thrustForce - linearVelocity * dragCoeff * mass;
             Vector2 a = netForce / mass;
@@ -50,12 +36,46 @@ namespace Content.Shared.Physics
             Vector2 k4 = a * frameTime;
 
             Vector2 deltaV = (k1 + k2*2 + k3*2 + k4) / 6;
+            return deltaV;
+        }
+
+        public void Move(Vector2 velocityDirection, float frameTime)
+        {
+            if (ControlledComponent == null)
+            {
+                return;
+            }
+            //apply a counteracting force to the standard friction between a human and a floor
+            //TODO: friction should involve mass, but in the current physics system it doesn't so we can't have it here either
+            //Vector2 antiFriction = velocityDirection.Normalized * (0.35f * 9.8f * frameTime);
+
+            float mass = ControlledComponent.Mass;
+            float dragCoeff = 5 / timeToVmax;
+            if (ControlledComponent.Owner.IsWeightless())
+            {
+                dragCoeff /= 50; //ice level time
+            }
+            Vector2 linearVelocity = ControlledComponent.LinearVelocity;
+
+            var multiplier = frameTime / minDesiredTimeStep;
+            var divisions = Math.Max(1, MathF.Round(multiplier, MidpointRounding.AwayFromZero));
+            if (divisions > 10)
+            {
+                return; //hah hah holy hell brother you have worse problems than players being unable to move
+            }
+
+            var timeStep = frameTime / divisions;
+            Vector2 deltaV = Vector2.Zero;
+            for (var i = 0; i < divisions; i++)
+            {
+                deltaV += StepSimulation(velocityDirection, linearVelocity + deltaV, dragCoeff, mass, timeStep);
+            }
             Impulse = deltaV * mass / frameTime;
         }
 
         public void StopMoving(float frameTime)
         {
-            if (ControlledComponent == null || (ControlledComponent.Owner.IsWeightless()))
+            if (ControlledComponent == null || ControlledComponent.Owner.IsWeightless())
             {
                 return;
             }
@@ -64,27 +84,28 @@ namespace Content.Shared.Physics
             //TODO: friction should involve mass, but in the current physics system it doesn't so we can't have it here either
             //Vector2 antiFriction = velocityDirection.Normalized * (0.35f * 9.8f * frameTime);
 
-            float mass = ControlledComponent.Mass;
-            float dragCoeff = 5 / _timeToVmax;
             var linearVelocity = ControlledComponent.LinearVelocity;
+            if (MathHelper.CloseTo(linearVelocity.LengthSquared, 0))
+            {
+                return;
+            }
 
-            Vector2 netForce = Vector2.Zero;
-            Vector2 a = netForce / mass;
-            Vector2 k1 = a * frameTime;
+            float mass = ControlledComponent.Mass;
+            float dragCoeff = 5 / timeToVmax;
 
-            netForce = - (linearVelocity + k1/2) * dragCoeff * mass;
-            a = netForce / mass;
-            Vector2 k2 = a * frameTime;
+            var multiplier = frameTime / minDesiredTimeStep;
+            var divisions = Math.Max(1, MathF.Round(multiplier, MidpointRounding.AwayFromZero));
+            if (divisions > 10)
+            {
+                return; //hah hah holy hell brother you have worse problems than players being unable to move
+            }
 
-            netForce = - (linearVelocity + k2/2) * dragCoeff * mass;
-            a = netForce / mass;
-            Vector2 k3 = a * frameTime;
-
-            netForce = - (linearVelocity + k3) * dragCoeff * mass;
-            a = netForce / mass;
-            Vector2 k4 = a * frameTime;
-
-            Vector2 deltaV = (k1 + k2*2 + k3*2 + k4) / 6;
+            var timeStep = frameTime / divisions;
+            Vector2 deltaV = Vector2.Zero;
+            for (var i = 0; i < divisions; i++)
+            {
+                deltaV += StepSimulation(Vector2.Zero, linearVelocity + deltaV, dragCoeff, mass, timeStep);
+            }
             Impulse = deltaV * mass / frameTime;
         }
     }
