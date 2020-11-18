7984f52a0a2cbdb4e7c5ae35a28e96dc412ba5c2
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-11-18 12:04:10 +0100

    Add command to delete all instances of a component

---
+++ Content.Server/Administration/Commands/DeleteComponent.cs
@@ -0,0 +1,48 @@
+﻿#nullable enable
+using Content.Shared.Administration;
+using Robust.Server.Interfaces.Console;
+using Robust.Server.Interfaces.Player;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
+
+namespace Content.Server.Administration.Commands
+{
+    [AdminCommand(AdminFlags.Admin)]
+    public class DeleteComponent : IClientCommand
+    {
+        public string Command => "deletecomponent";
+        public string Description => "Removes all instances of the specified component.";
+        public string Help => $"Usage: {Command}";
+
+        public void Execute(IConsoleShell shell, IPlayerSession? player, string[] args)
+        {
+            switch (args.Length)
+            {
+                case 0:
+                    shell.SendText(player, $"Not enough arguments.\n{Help}");
+                    break;
+                default:
+                    var name = string.Join(" ", args);
+                    var componentFactory = IoCManager.Resolve<IComponentFactory>();
+                    var entityManager = IoCManager.Resolve<IEntityManager>();
+                    var componentType = componentFactory.GetRegistration(name).Type;
+
+                    var components = entityManager.ComponentManager.GetAllComponents(componentType);
+
+                    var i = 0;
+
+                    foreach (var component in components)
+                    {
+                        var uid = component.Owner.Uid;
+                        entityManager.ComponentManager.RemoveComponent(uid, component);
+                        i++;
+                    }
+
+                    shell.SendText(player, $"Removed {i} components with name {name}.");
+
+                    break;
+            }
+        }
+    }
+}
