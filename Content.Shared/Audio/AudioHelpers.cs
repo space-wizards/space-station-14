64052461cd3ba8bd539b7c9d78a03b5204fe56e7
brndd <burneddi@gmail.com>
2020-11-22 16:00:40 +0200

    prettify

--- Content.Shared/Audio/AudioHelpers.cs
+++ Content.Shared/Audio/AudioHelpers.cs
@@ -18,33 +18,17 @@ namespace Content.Shared.Audio
             return AudioParams.Default.WithPitchScale(scale);
         }
 
-        private static float[] _semitoneMultipliers =
+        // Might as well just hardcode these because the audio system is limited to pitching up and down
+        // by 12 semitones anyway (ie. 0.5 to 2.0 multiplier).
+        private static readonly float[] SemitoneMultipliers =
         {
-            0.5f,
-            233.08f/440f,
-            246.94f/440f,
-            261.63f/440f,
-            277.18f/440f,
-            293.66f/440f,
-            311.13f/440f,
-            329.63f/440f,
-            349.23f/440f,
-            369.99f/440f,
-            392.00f/440f,
-            415.30f/440f,
+            0.5f, 233.08f/440f, 246.94f/440f, 261.63f/440f,
+            277.18f/440f, 293.66f/440f, 311.13f/440f, 329.63f/440f,
+            349.23f/440f, 369.99f/440f, 392.00f/440f, 415.30f/440f,
             1.0f,
-            466.16f/440f,
-            493.88f/440f,
-            523.25f/440f,
-            554.37f/440f,
-            587.33f/440f,
-            622.25f/440f,
-            659.26f/440f,
-            698.46f/440f,
-            739.99f/440f,
-            783.99f/440f,
-            830.61f/440f,
-            2.0f
+            466.16f/440f, 493.88f/440f, 523.25f/440f, 554.37f/440f,
+            587.33f/440f, 622.25f/440f, 659.26f/440f, 698.46f/440f,
+            739.99f/440f, 783.99f/440f, 830.61f/440f, 2.0f
         };
 
         /// <summary>
@@ -55,7 +39,7 @@ namespace Content.Shared.Audio
         public static AudioParams ShiftSemitone(int shift)
         {
             shift = MathHelper.Clamp(shift, -12, 12);
-            float pitchMult = _semitoneMultipliers[shift + 12];
+            float pitchMult = SemitoneMultipliers[shift + 12];
             return AudioParams.Default.WithPitchScale(pitchMult);
         }
 
