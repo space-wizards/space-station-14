6602c8c97224f88440b056f3614c3c7ca5b40a67
Paul Ritter <ritter.paul1@googlemail.com>
2020-11-22 08:38:07 +0100

    Objectives (#2459)
    * temp commit to save progress
    
    * adds objectives
    
    * refactors mind.addobjective a bit
    
    * better names for my testobjectives which i'll remove later on anyways
    
    * nullable errors
    
    * some misc fixes
    
    * no sorted or set, what was i thinking here?
    
    * removes unused imports
    
    * added commands
    
    * fully implements stealcondition
    
    * started uiwork
    
    * moved prototypeicon to engine
    
    * removes objective class & uiwork
    
    * refactors ui to only update when opened
    adds progresstexturerect
    
    * adds some margin
    
    * removes some testing code
    
    * ignores objectiveprototypes on clientside
    
    * fixes
    
    * removes using statements for exp
    
    * gets the job
    
    * always show issuer
    
    * locs & _
    
    * giving commands some love
    
    * Update Content.Client/GameObjects/EntitySystems/DoAfter/DoAfterBar.cs
    
    Co-authored-by: Exp <theexp111@gmail.com>
    
    * makes commands use new thingy
    
    * string interpolation
    
    * good catch exp
    
    * loc'd
    
    * linq gone
    
    * runtime
    
    * moves function from engine
    
    * oopsie
    
    * Update Content.Server/Objectives/Conditions/StealCondition.cs
    
    Co-authored-by: metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
    
    * makes messages directed
    
    * base call & validation
    
    * shuffle once
    
    * No? Money down!
    
    Co-authored-by: Paul <ritter.paul1+git@googlemail.com>
    Co-authored-by: Exp <theexp111@gmail.com>
    Co-authored-by: metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
    Co-authored-by: Metal Gear Sloth <metalgearsloth@gmail.com>

---
+++ Content.Server/GameObjects/Components/ContainerExt/ContainerExt.cs
@@ -0,0 +1,23 @@
+﻿using Robust.Server.GameObjects.Components.Container;
+
+namespace Content.Server.GameObjects.Components.ContainerExt
+{
+    public static class ContainerExt
+    {
+        public static int CountPrototypeOccurencesRecursive(this ContainerManagerComponent mgr, string prototypeId)
+        {
+            int total = 0;
+            foreach (var container in mgr.GetAllContainers())
+            {
+                foreach (var entity in container.ContainedEntities)
+                {
+                    if (entity.Prototype?.ID == prototypeId) total++;
+                    if(!entity.TryGetComponent<ContainerManagerComponent>(out var component)) continue;
+                    total += component.CountPrototypeOccurencesRecursive(prototypeId);
+                }
+            }
+
+            return total;
+        }
+    }
+}
