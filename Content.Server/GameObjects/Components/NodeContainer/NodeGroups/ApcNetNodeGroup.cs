d4af7bb596833e0afe5480f87637e0178796f60d
py01 <pyronetics01@gmail.com>
2020-11-26 02:54:56 -0600

    ApcNet disruption method

--- Content.Server/GameObjects/Components/NodeContainer/NodeGroups/ApcNetNodeGroup.cs
+++ Content.Server/GameObjects/Components/NodeContainer/NodeGroups/ApcNetNodeGroup.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using Content.Server.GameObjects.Components.Power;
+using Content.Server.GameObjects.Components.Power.ApcNetComponents;
+using Robust.Shared.Interfaces.Timing;
+using Robust.Shared.IoC;
+using Robust.Shared.ViewVariables;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using Content.Server.GameObjects.Components.Power;
-using Content.Server.GameObjects.Components.Power.ApcNetComponents;
-using Robust.Shared.ViewVariables;
 
 namespace Content.Server.GameObjects.Components.NodeContainer.NodeGroups
 {
@@ -20,6 +23,8 @@ namespace Content.Server.GameObjects.Components.NodeContainer.NodeGroups
         void UpdatePowerProviderReceivers(PowerProviderComponent provider);
 
         void Update(float frameTime);
+
+        bool DisruptPower(TimeSpan disruptionLength, TimeSpan disruptionCooldown);
     }
 
     [NodeGroup(NodeGroupID.Apc)]
@@ -30,6 +35,20 @@ namespace Content.Server.GameObjects.Components.NodeContainer.NodeGroups
 
         [ViewVariables]
         private readonly Dictionary<PowerProviderComponent, List<PowerReceiverComponent>> _providerReceivers = new Dictionary<PowerProviderComponent, List<PowerReceiverComponent>>();
+
+        [ViewVariables]
+        public TimeSpan DisruptionEnd { get; private set; } = new();
+
+        [ViewVariables]
+        public TimeSpan DisruptionCooldownEnd { get; private set; } = new();
+
+        [ViewVariables]
+        private bool Disrupted => _gameTiming.CurTime <= DisruptionEnd;
+
+        [ViewVariables]
+        private bool DisruptionOnCooldown => _gameTiming.CurTime <= DisruptionCooldownEnd;
+
+        [Dependency] private readonly IGameTiming _gameTiming = default!;
 
         //Debug property
         [ViewVariables]
@@ -84,6 +103,9 @@ namespace Content.Server.GameObjects.Components.NodeContainer.NodeGroups
 
         public void Update(float frameTime)
         {
+            if (Disrupted)
+                return;
+
             var totalCharge = 0.0;
             var totalMaxCharge = 0;
             foreach (var (apc, battery) in _apcBatteries)
@@ -122,6 +144,31 @@ namespace Content.Server.GameObjects.Components.NodeContainer.NodeGroups
             return false;
         }
 
+        public bool DisruptPower(TimeSpan disruptionLength, TimeSpan disruptionCooldown)
+        {
+            if (!DisruptionOnCooldown & !Disrupted)
+            {
+                DisruptPower();
+                var curTime = _gameTiming.CurTime;
+                DisruptionEnd = curTime + disruptionLength;
+                DisruptionCooldownEnd = DisruptionEnd + disruptionCooldown;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void DisruptPower()
+        {
+            foreach (var providerPair in _providerReceivers)
+            {
+                foreach (var receiver in providerPair.Value)
+                {
+                    receiver.HasApcPower = false;
+                }
+            }
+        }
+
         #endregion
 
         private class NullApcNet : IApcNet
@@ -132,6 +179,7 @@ namespace Content.Server.GameObjects.Components.NodeContainer.NodeGroups
             public void RemovePowerProvider(PowerProviderComponent provider) { }
             public void UpdatePowerProviderReceivers(PowerProviderComponent provider) { }
             public void Update(float frameTime) { }
+            public bool DisruptPower(TimeSpan disruptionLength, TimeSpan disruptionCooldown) { return false; }
         }
     }
 }
