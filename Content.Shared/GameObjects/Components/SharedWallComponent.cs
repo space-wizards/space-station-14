e56e561ec96a58d52daddcab2d6360a0bb232de3
Alex Evgrashin <aevgrashin@yandex.ru>
2020-11-30 00:18:45 +0300

    You can build bulbs only on walls

---
+++ Content.Shared/GameObjects/Components/SharedWallComponent.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.GameObjects;
+
+namespace Content.Shared.GameObjects.Components
+{
+    [RegisterComponent]
+    public class SharedWallComponent : Component
+    {
+        public override string Name => "Wall";
+    }
+}
