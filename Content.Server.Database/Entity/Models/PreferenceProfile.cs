7314bac8d5ac31d7ffdd5adb19c340ddb8b14a9f
Saphire <lattice@saphi.re>
2020-12-01 01:44:18 +0700

    Fix invalid table name introduced by implicit DbSet name added after latest migration

--- Content.Server.Database/Entity/Models/PreferenceProfile.cs
+++ Content.Server.Database/Entity/Models/PreferenceProfile.cs
@@ -6,6 +6,7 @@ namespace Content.Server.Database.Entity.Models
     public class PreferenceProfile : IEntityTypeConfiguration<PreferenceProfile>
     {
         public int PreferenceId { get; set; }
+
         public Preference Preference { get; set; } = null!;
 
         public int ProfileId { get; set; }
@@ -17,6 +18,9 @@ namespace Content.Server.Database.Entity.Models
             builder.HasKey(p => new { p.PreferenceId, p.ProfileId });
             builder.HasIndex(p => p.PreferenceId)
                 .IsUnique();
+
+            // Temporary until all names and migrations can be cleaned up.
+            builder.ToTable("PreferenceProfile");
         }
     }
 }
