c3341132c51a52ea25b93c1d1fccac5f970cf63f
Vera Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>
2020-12-03 22:49:00 +0100

    Upgradeable machines. (#2675)
    * Start work on upgradeable machines.
    
    * Upgradeable machines work
    
    * Component requirements for upgradeable machines
    
    * Better container handling
    
    * Remember to not push submodule updates in your PRs, kids!
    
    * Refresh parts after building a machine.
    
    * NetSync false
    
    * Address some reviews, fix some bugs
    
    * Nullable stackhelpers dependencies
    
    * Use container helper method to delete all entities in containers
    
    * Nullable string in AddContainer
    
    * Better examine for machine frame and construction in general
    
    * Machine breakage
    
    * Nullable node
    
    * nullable GraphPrototype
    
    * Re-save saltern for autolathe parts
    
    * Fix SaveLoadSave

--- Content.Server/Construction/Completions/EmptyAllContainers.cs
+++ Content.Server/Construction/Completions/EmptyAllContainers.cs
@@ -1,5 +1,4 @@
 ﻿#nullable enable
-using System.Linq;
 using System.Threading.Tasks;
 using Content.Shared.Construction;
 using JetBrains.Annotations;
@@ -11,23 +10,21 @@ using Robust.Shared.Serialization;
 namespace Content.Server.Construction.Completions
 {
     [UsedImplicitly]
-    public class EmptyContainer : IGraphAction
+    public class EmptyAllContainers : IGraphAction
     {
-        public string Container { get; private set; } = string.Empty;
-
         public void ExposeData(ObjectSerializer serializer)
         {
-            serializer.DataField(this, x => x.Container, "container", string.Empty);
         }
 
         public async Task PerformAction(IEntity entity, IEntity? user)
         {
-            if (entity.Deleted) return;
+            if (entity.Deleted || !entity.TryGetComponent<ContainerManagerComponent>(out var containerManager))
+                return;
 
-            if (!entity.TryGetComponent(out ContainerManagerComponent? containerManager) ||
-                !containerManager.TryGetContainer(Container, out var container)) return;
-
-            container.EmptyContainer(true, entity.Transform.Coordinates);
+            foreach (var container in containerManager.GetAllContainers())
+            {
+                container.EmptyContainer(true, entity.Transform.Coordinates);
+            }
         }
     }
 }
