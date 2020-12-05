8685fd3a66f6ef66f14881d17c34dd821acb84fc
Morshu32 <paulbisaccia@live.it>
2020-12-05 19:09:46 +0100

    Added bool _placeCentered to check if an entity must be placed at mouse position or center of the PlaceableSurface (+ offset) when dropped.

--- Content.Server/GameObjects/Components/PlaceableSurfaceComponent.cs
+++ Content.Server/GameObjects/Components/PlaceableSurfaceComponent.cs
@@ -3,6 +3,7 @@ using Content.Server.GameObjects.Components.GUI;
 using Content.Shared.GameObjects.Components;
 using Content.Shared.Interfaces.GameObjects.Components;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Maths;
 using Robust.Shared.Serialization;
 using Robust.Shared.ViewVariables;
 
@@ -13,6 +14,8 @@ namespace Content.Server.GameObjects.Components
     public class PlaceableSurfaceComponent : SharedPlaceableSurfaceComponent, IInteractUsing
     {
         private bool _isPlaceable;
+        private bool _placeCentered;
+        private Vector2 _positionOffset;
 
         [ViewVariables(VVAccess.ReadWrite)]
         public override bool IsPlaceable
@@ -31,6 +34,39 @@ namespace Content.Server.GameObjects.Components
             }
         }
 
+        public override bool PlaceCentered
+        {
+            get => _placeCentered;
+            set
+            {
+                if (_placeCentered == value)
+                {
+                    return;
+                }
+
+                _placeCentered = value;
+
+                Dirty();
+            }
+        }
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        public override Vector2 PositionOffset
+        {
+            get => _positionOffset;
+            set
+            {
+                if (_positionOffset == value)
+                {
+                    return;
+                }
+
+                _positionOffset = value;
+
+                Dirty();
+            }
+        }
+
         [ViewVariables]
         int IInteractUsing.Priority => -10;
 
@@ -39,6 +75,8 @@ namespace Content.Server.GameObjects.Components
             base.ExposeData(serializer);
 
             serializer.DataField(ref _isPlaceable, "IsPlaceable", true);
+            serializer.DataField(ref _placeCentered, "PlaceCentered", false);
+            serializer.DataField(ref _positionOffset, "PositionOffset", Vector2.Zero);
         }
 
         public override ComponentState GetComponentState()
@@ -56,7 +94,10 @@ namespace Content.Server.GameObjects.Components
                 return false;
             }
             handComponent.Drop(eventArgs.Using);
-            eventArgs.Using.Transform.WorldPosition = eventArgs.ClickLocation.Position;
+            if (_placeCentered)
+                eventArgs.Using.Transform.WorldPosition = eventArgs.Target.Transform.WorldPosition + _positionOffset;
+            else
+                eventArgs.Using.Transform.WorldPosition = eventArgs.ClickLocation.Position;
             return true;
         }
     }
--- Content.Shared/GameObjects/Components/SharedPlaceableSurfaceComponent.cs
+++ Content.Shared/GameObjects/Components/SharedPlaceableSurfaceComponent.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Maths;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.GameObjects.Components
@@ -8,8 +9,9 @@ namespace Content.Shared.GameObjects.Components
     {
         public override string Name => "PlaceableSurface";
         public override uint? NetID => ContentNetIDs.PLACEABLE_SURFACE;
-
         public virtual bool IsPlaceable { get; set; }
+        public virtual bool PlaceCentered { get; set; }
+        public virtual Vector2 PositionOffset { get; set; }
     }
 
     [Serializable, NetSerializable]
