4432c3974cf81d933509ba9b1f21b0bd1a3a436d
Alex Evgrashin <aevgrashin@yandex.ru>
2020-12-05 14:26:42 +0300

    Refactored code a bit

---
+++ Content.Shared/Construction/ConstructionConditions/WallmountCondition.cs
@@ -0,0 +1,59 @@
+﻿#nullable enable
+using Content.Shared.GameObjects.Components;
+using Content.Shared.Maps;
+using Content.Shared.Utility;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
+using Robust.Shared.Maths;
+using Robust.Shared.Serialization;
+using System.Linq;
+
+namespace Content.Shared.Construction.ConstructionConditions
+{
+    public class WallmountCondition : IConstructionCondition
+    {
+        public void ExposeData(ObjectSerializer serializer) { }
+
+        public bool Condition(IEntity user, EntityCoordinates location, Direction direction)
+        {
+            // check that player doesn't try to build object facing wall other side
+            bool invalidDirection = FacingWallOtherSide(user, location, direction);
+            if (invalidDirection)
+                return false;
+
+            // check that player builds inside wall
+            var wallEnity = location.GetEntitiesInTile(true)
+                .FirstOrDefault((e) => e.HasComponent<SharedWallComponent>());
+            if (wallEnity == null)
+                return false;
+
+            // check that building doesn't facing adjacent wall
+            var dirLocation = location.Offset(direction);
+            var adjWallEntity = dirLocation.GetEntitiesInTile(true)
+                .FirstOrDefault((e) => e.HasComponent<SharedWallComponent>());
+            if (adjWallEntity != null)
+                return false;
+
+            // TODO: check that we doesn't intersect other wallmount
+
+            return true;
+        }
+
+        private bool FacingWallOtherSide(IEntity user, EntityCoordinates location, Direction bpDirection)
+        {
+            var entManager = IoCManager.Resolve<IEntityManager>();
+
+            // get blueprint and user position
+            var userWorldPosition = user.Transform.WorldPosition;
+            var objWorldPosition = location.ToMap(entManager).Position;
+
+            // find direction from user to blueprint
+            var userToObject = (objWorldPosition - userWorldPosition);
+
+            // dot product will be positive if user direction and blueprint are co-directed
+            var dotProd = Vector2.Dot(bpDirection.ToVec(), userToObject);
+            return dotProd > 0;
+        }
+    }
+}
