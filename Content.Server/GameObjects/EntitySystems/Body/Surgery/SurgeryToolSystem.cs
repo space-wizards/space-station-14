1ff2bea9655bfa06f4f8f0e26ba10674bffe6390
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-12-20 03:37:42 +0100

    Remove wireless surgery

---
+++ Content.Server/GameObjects/EntitySystems/Body/Surgery/SurgeryToolSystem.cs
@@ -0,0 +1,36 @@
+﻿using Content.Server.GameObjects.Components.Body.Surgery;
+using Content.Shared.GameObjects.EntitySystems;
+using Content.Shared.Utility;
+using JetBrains.Annotations;
+using Robust.Shared.GameObjects.Systems;
+
+namespace Content.Server.GameObjects.EntitySystems.Body.Surgery
+{
+    [UsedImplicitly]
+    public class SurgeryToolSystem : EntitySystem
+    {
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+
+            foreach (var tool in ComponentManager.EntityQuery<SurgeryToolComponent>())
+            {
+                if (tool.PerformerCache == null)
+                {
+                    continue;
+                }
+
+                if (tool.BodyCache == null)
+                {
+                    continue;
+                }
+
+                if (!ActionBlockerSystem.CanInteract(tool.PerformerCache) ||
+                    !tool.PerformerCache.InRangeUnobstructed(tool.BodyCache))
+                {
+                    tool.CloseAllSurgeryUIs();
+                }
+            }
+        }
+    }
+}
