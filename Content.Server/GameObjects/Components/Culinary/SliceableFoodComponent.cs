54cbdf8c52fdbae57a0f848cf44a2e8d688ff28f
cyclowns <cyclowns@protonmail.ch>
2021-01-02 02:16:48 -0700

    sprite fixes, sounds

--- Content.Server/GameObjects/Components/Culinary/SliceableFoodComponent.cs
+++ Content.Server/GameObjects/Components/Culinary/SliceableFoodComponent.cs
@@ -4,11 +4,17 @@ using Content.Shared.Interfaces.GameObjects.Components;
 using Content.Shared.GameObjects.EntitySystems;
 using Content.Server.GameObjects.Components.Nutrition;
 using Content.Server.GameObjects.Components.Chemistry;
+using Content.Server.GameObjects.Components.GUI;
+using Content.Server.GameObjects.Components.Items.Storage;
+using Robust.Server.GameObjects.EntitySystems;
+using Robust.Shared.GameObjects.Systems;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Containers;
 using Robust.Shared.Serialization;
 using Robust.Shared.Localization;
 using Robust.Shared.ViewVariables;
 using Robust.Shared.Utility;
+using Robust.Shared.Audio;
 
 namespace Content.Server.GameObjects.Components.Culinary
 {
@@ -19,6 +25,7 @@ namespace Content.Server.GameObjects.Components.Culinary
 
         [ViewVariables(VVAccess.ReadWrite)] private string _slice;
         private uint _totalCount;
+        [ViewVariables(VVAccess.ReadWrite)] private string _sound;
 
         [ViewVariables(VVAccess.ReadWrite)] public uint Count;
 
@@ -26,6 +33,7 @@ namespace Content.Server.GameObjects.Components.Culinary
         {
             base.ExposeData(serializer);
             serializer.DataField(ref _slice, "slice", "FoodOrangeCakeSlice");
+            serializer.DataField(ref _sound, "sound", "/Audio/Items/Culinary/chop.ogg");
             serializer.DataField<uint>(ref _totalCount, "count", 5);
         }
         
@@ -34,6 +42,7 @@ namespace Content.Server.GameObjects.Components.Culinary
             base.Initialize();
             Count = _totalCount;
             Owner.EnsureComponent<FoodComponent>();
+            Owner.EnsureComponent<SolutionContainerComponent>();
         }
 
         async Task<bool> IInteractUsing.InteractUsing(InteractUsingEventArgs eventArgs)
@@ -47,14 +56,24 @@ namespace Content.Server.GameObjects.Components.Culinary
                 return false;
             }
 
-            Owner.EntityManager.SpawnEntity(_slice, Owner.Transform.Coordinates);
+            var itemToSpawn = Owner.EntityManager.SpawnEntity(_slice, Owner.Transform.Coordinates);
+            if (eventArgs.User.TryGetComponent(out HandsComponent handsComponent))
+            {
+                if (ContainerHelpers.IsInContainer(Owner))
+                {
+                    handsComponent.PutInHandOrDrop(itemToSpawn.GetComponent<ItemComponent>());
+                }
+            }
+
+            EntitySystem.Get<AudioSystem>().PlayAtCoords(_sound, Owner.Transform.Coordinates, AudioParams.Default.WithVolume(-2));
 
             Count--;
-            solution.TryRemoveReagent("chem.Nutriment", solution.CurrentVolume / ReagentUnit.New(Count));
             if (Count < 1)
             {
                 Owner.Delete();
+                return true;
             }
+            solution.TryRemoveReagent("chem.Nutriment", solution.CurrentVolume / ReagentUnit.New(Count));
             return true;
         }
 
