92ee77b577989e3228e841dcff2dbc40975df667
py01 <pyronetics01@gmail.com>
2021-01-16 03:19:44 -0600

    DummyWireComponent

---
+++ Content.Server/GameObjects/Components/Power/DummyWireComponent.cs
@@ -0,0 +1,45 @@
+#nullable enable
+using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Serialization;
+using System.Collections.Generic;
+
+namespace Content.Server.GameObjects.Components.Power
+{
+    //TODO: make these wires not visible on right-click
+    [RegisterComponent]
+    public class DummyWireComponent : Component
+    {
+        public override string Name => "DummyWire";
+
+        private List<string> _dummyWireProtos = default!;
+
+        private List<IEntity> _dummyWires = new();
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _dummyWireProtos, "prototype", new List<string> { "HVDummyWire" });
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            foreach (var proto in _dummyWireProtos)
+            {
+                var wire = Owner.EntityManager.SpawnEntity(proto, Owner.Transform.Coordinates);
+                _dummyWires.Add(wire);
+            }
+        }
+
+        public override void OnRemove()
+        {
+            foreach (var wire in _dummyWires)
+            {
+                Owner.EntityManager.DeleteEntity(wire);
+            }
+
+            base.OnRemove();
+        }
+    }
+}
