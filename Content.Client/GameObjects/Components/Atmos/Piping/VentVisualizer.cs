5b8b5a1e87a1eaabb63cee9557cfa4d867d123f4
py01 <pyronetics01@gmail.com>
2021-01-17 22:36:14 -0600

    Siphon and vent visualiser only set enabled visibility

--- Content.Client/GameObjects/Components/Atmos/Piping/VentVisualizer.cs
+++ Content.Client/GameObjects/Components/Atmos/Piping/VentVisualizer.cs
@@ -1,51 +1,35 @@
-﻿using JetBrains.Annotations;
+using JetBrains.Annotations;
 using Robust.Client.GameObjects;
-using Robust.Client.Graphics;
 using Robust.Client.Interfaces.GameObjects.Components;
-using Robust.Client.Interfaces.ResourceManagement;
-using Robust.Client.ResourceManagement;
-using Robust.Shared.GameObjects.Components.Renderable;
-using Robust.Shared.IoC;
-using Robust.Shared.Log;
-using Robust.Shared.Utility;
-using System;
 using Content.Shared.GameObjects.Components.Atmos;
 using YamlDotNet.RepresentationModel;
 using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Serialization;
 
 namespace Content.Client.GameObjects.Components.Atmos
 {
     [UsedImplicitly]
     public class VentVisualizer : AppearanceVisualizer
     {
-        private RSI _ventRSI;
+        private string _ventOnstate;
 
         public override void LoadData(YamlMappingNode node)
         {
             base.LoadData(node);
 
-            var rsiString = node.GetNode("ventRSI").ToString();
-            var rsiPath = SharedSpriteComponent.TextureRoot / rsiString;
-            try
-            {
-                var resourceCache = IoCManager.Resolve<IResourceCache>();
-                var resource = resourceCache.GetResource<RSIResource>(rsiPath);
-                _ventRSI = resource.RSI;
-            }
-            catch (Exception e)
-            {
-                Logger.ErrorS("go.ventvisualizer", "Unable to load RSI '{0}'. Trace:\n{1}", rsiPath, e);
-            }
+            var serializer = YamlObjectSerializer.NewReader(node);
+            serializer.DataField(ref _ventOnstate, "ventOnState", "ventOn");
         }
 
         public override void InitializeEntity(IEntity entity)
         {
             base.InitializeEntity(entity);
+
             if (!entity.TryGetComponent(out ISpriteComponent sprite)) return;
-            sprite.LayerMapReserveBlank(Layer.VentBase);
-            var pipeBaseLayer = sprite.LayerMapGet(Layer.VentBase);
-            sprite.LayerSetRSI(pipeBaseLayer, _ventRSI);
-            sprite.LayerSetVisible(pipeBaseLayer, true);
+
+            sprite.LayerMapReserveBlank(Layer.VentEnabled);
+            var layer = sprite.LayerMapGet(Layer.VentEnabled);
+            sprite.LayerSetState(layer, _ventOnstate);
         }
 
         public override void OnChangeData(AppearanceComponent component)
@@ -55,18 +39,13 @@ namespace Content.Client.GameObjects.Components.Atmos
             if (!component.Owner.TryGetComponent(out ISpriteComponent sprite)) return;
             if (!component.TryGetData(VentVisuals.VisualState, out VentVisualState ventVisualState)) return;
 
-            var ventBaseState = "vent";
-            ventBaseState += ventVisualState.VentEnabled ? "On" : "Off";
-
-            var baseVentLayer = sprite.LayerMapGet(Layer.VentBase);
-            sprite.LayerSetRSI(baseVentLayer, _ventRSI);
-            sprite.LayerSetState(baseVentLayer, ventBaseState);
-            sprite.LayerSetVisible(baseVentLayer, true);
+            var layer = sprite.LayerMapGet(Layer.VentEnabled);
+            sprite.LayerSetVisible(layer, ventVisualState.VentEnabled);
         }
 
         private enum Layer : byte
         {
-            VentBase,
+            VentEnabled,
         }
     }
 }
