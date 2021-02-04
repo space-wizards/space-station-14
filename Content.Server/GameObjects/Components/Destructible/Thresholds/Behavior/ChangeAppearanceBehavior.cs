d1eea77e5382e70e8a1eb87feb0c4417667e9e06
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-02-04 13:13:10 +0100

    Bring back old threshold appearance and trigger code
    WIP

---
+++ Content.Server/GameObjects/Components/Destructible/Thresholds/Behavior/ChangeAppearanceBehavior.cs
@@ -0,0 +1,47 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using Content.Server.GameObjects.EntitySystems;
+using Content.Shared.GameObjects.Components.Destructible;
+using Robust.Server.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
+
+namespace Content.Server.GameObjects.Components.Destructible.Thresholds.Behavior
+{
+    public class ChangeAppearanceBehavior : IThresholdBehavior
+    {
+        [ViewVariables] public ThresholdAppearance? Appearance;
+
+        public void ExposeData(ObjectSerializer serializer)
+        {
+            serializer.DataField(ref Appearance, "appearance", null);
+        }
+
+        public void Trigger(IEntity owner, DestructibleSystem system)
+        {
+            if (Appearance == null)
+            {
+                return;
+            }
+
+            if (!owner.TryGetComponent(out AppearanceComponent? appearanceComponent))
+            {
+                return;
+            }
+
+            // TODO Remove layers == null see https://github.com/space-wizards/RobustToolbox/pull/1461
+            if (!appearanceComponent.TryGetData(DamageVisualizerData.Layers, out Dictionary<int, ThresholdAppearance>? layers) ||
+                layers == null)
+            {
+                layers = new Dictionary<int, ThresholdAppearance>();
+            }
+
+            var appearance = Appearance.Value;
+            var layerIndex = appearance.Layer ?? 0;
+            layers[layerIndex] = appearance;
+
+            appearanceComponent.SetData(DamageVisualizerData.Layers, layers);
+        }
+    }
+}
--- Content.Server/GameObjects/Components/Destructible/Thresholds/Threshold.cs
+++ Content.Server/GameObjects/Components/Destructible/Thresholds/Threshold.cs
@@ -1,7 +1,9 @@
 ﻿#nullable enable
 using System.Collections.Generic;
+using System.Linq;
 using Content.Server.GameObjects.Components.Destructible.Thresholds.Behavior;
 using Content.Server.GameObjects.EntitySystems;
+using Content.Shared.Damage;
 using Robust.Shared.Interfaces.GameObjects;
 using Robust.Shared.Interfaces.Serialization;
 using Robust.Shared.Serialization;
@@ -11,7 +13,25 @@ namespace Content.Server.GameObjects.Components.Destructible.Thresholds
 {
     public class Threshold : IExposeData
     {
-        private List<IThresholdBehavior> _behaviors = new();
+        [ViewVariables] private List<IThresholdBehavior> _behaviors = new();
+
+        [ViewVariables] public int? Damage;
+
+        [ViewVariables] public Dictionary<DamageClass, int>? DamageClasses;
+
+        [ViewVariables] public Dictionary<DamageType, int>? DamageTypes;
+
+        [ViewVariables] public int DamageTotal =>
+            Damage +
+            DamageClasses?.Values.Sum() ?? 0 +
+            DamageTypes?.Values.Sum() ?? 0;
+
+        /// <summary>
+        ///     Whether or not <see cref="Damage"/>, <see cref="DamageClasses"/> and
+        ///     <see cref="DamageTypes"/> all have to be met in order to reach this state,
+        ///     or just one of them.
+        /// </summary>
+        [ViewVariables] public bool Inclusive = true;
 
         /// <summary>
         ///     Whether or not this threshold has already been triggered.
@@ -33,9 +53,80 @@ namespace Content.Server.GameObjects.Components.Destructible.Thresholds
 
         public void ExposeData(ObjectSerializer serializer)
         {
+            serializer.DataField(ref Damage, "damage", null);
+            serializer.DataField(ref DamageClasses, "damageClasses", null);
+            serializer.DataField(ref DamageTypes, "damageTypes", null);
+            serializer.DataField(ref Inclusive, "inclusive", true);
             serializer.DataField(this, x => x.Triggered, "triggered", false);
             serializer.DataField(this, x => x.TriggersOnce, "triggersOnce", false);
             serializer.DataField(ref _behaviors, "behaviors", new List<IThresholdBehavior>());
+        }
+
+        private bool DamageClassesReached(IReadOnlyDictionary<DamageClass, int>? classesReached)
+        {
+            if (DamageClasses == null)
+            {
+                return true;
+            }
+
+            if (classesReached == null)
+            {
+                return false;
+            }
+
+            foreach (var (@class, damageRequired) in DamageClasses)
+            {
+                if (!classesReached.TryGetValue(@class, out var damageReached) ||
+                    damageReached < damageRequired)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool DamageTypesReached(IReadOnlyDictionary<DamageType, int>? typesReached)
+        {
+            if (DamageTypes == null)
+            {
+                return true;
+            }
+
+            if (typesReached == null)
+            {
+                return false;
+            }
+
+            foreach (var (type, damageRequired) in DamageTypes)
+            {
+                if (!typesReached.TryGetValue(type, out var damageReached) ||
+                    damageReached < damageRequired)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool Reached(
+            int? damage = null,
+            IReadOnlyDictionary<DamageClass, int>? damageClasses = null,
+            IReadOnlyDictionary<DamageType, int>? damageTypes = null)
+        {
+            if (Inclusive)
+            {
+                return damage >= Damage &&
+                       DamageClassesReached(damageClasses) &&
+                       DamageTypesReached(damageTypes);
+            }
+            else
+            {
+                return damage >= Damage ||
+                       DamageClassesReached(damageClasses) ||
+                       DamageTypesReached(damageTypes);
+            }
         }
 
         /// <summary>
