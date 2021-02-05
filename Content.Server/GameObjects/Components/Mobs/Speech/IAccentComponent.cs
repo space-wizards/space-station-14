63e12525393aa8340e743d1cdaed49f378665a92
Vera Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>
2021-02-05 17:01:54 +0100

    Speech and Emoting components (#3075)
    * Add Speech and Emoting components.
    
    * makesentient gives you speech and emoting abilities.
    
    * Ghosts do *not* need a SpeechComponent.
    
    * I hate you all, and I hate shared code as well.

--- Content.Server/GameObjects/Components/Mobs/Speech/IAccentComponent.cs
+++ Content.Server/GameObjects/Components/Mobs/Speech/IAccentComponent.cs
@@ -1,4 +1,11 @@
-﻿namespace Content.Server.GameObjects.Components.Mobs.Speech
+﻿#nullable enable
+using Content.Shared.GameObjects.Components.Mobs.Speech;
+using Content.Shared.GameObjects.EntitySystems.ActionBlocker;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Serialization;
+
+namespace Content.Server.GameObjects.Components.Mobs.Speech
 {
     internal interface IAccentComponent
     {
