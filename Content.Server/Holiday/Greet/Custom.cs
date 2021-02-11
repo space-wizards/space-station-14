375bd9c438740277da57d6e8b459d8d17a8feda2
Vera Aguilera Puerto <zddm@outlook.es>
2021-02-11 15:27:19 +0100

    Add custom holiday greet

---
+++ Content.Server/Holiday/Greet/Custom.cs
@@ -0,0 +1,24 @@
+using System;
+using Content.Server.Holiday.Interfaces;
+using JetBrains.Annotations;
+using Robust.Shared.Interfaces.Serialization;
+using Robust.Shared.Serialization;
+
+namespace Content.Server.Holiday.Greet
+{
+    [UsedImplicitly]
+    public class Custom : IHolidayGreet, IExposeData
+    {
+        private string _greet;
+
+        void IExposeData.ExposeData(ObjectSerializer serializer)
+        {
+            serializer.DataField(ref _greet, "text", string.Empty);
+        }
+
+        public string Greet(HolidayPrototype holiday)
+        {
+            return _greet;
+        }
+    }
+}
--- Content.Server/Holiday/Interfaces/IHolidayGreet.cs
+++ Content.Server/Holiday/Interfaces/IHolidayGreet.cs
@@ -1,9 +1,12 @@
+using Robust.Shared.Interfaces.Serialization;
 using Robust.Shared.Localization;
+using Robust.Shared.Serialization;
 
 namespace Content.Server.Holiday.Interfaces
 {
-    public interface IHolidayGreet
+    public interface IHolidayGreet : IExposeData
     {
+        void IExposeData.ExposeData(ObjectSerializer serializer) { }
         string Greet(HolidayPrototype holiday);
     }
 }
