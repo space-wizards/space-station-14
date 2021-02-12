5af6dee724adc977ed8f3e49c96cbed9f8b136b3
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-02-12 14:50:45 +0100

    Add game preset attribute, unhardcode game presets

---
+++ Content.Server/GameTicking/GamePresets/GamePresetAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Immutable;
+using JetBrains.Annotations;
+
+namespace Content.Server.GameTicking.GamePresets
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    [BaseTypeRequired(typeof(GamePreset))]
+    [MeansImplicitUse]
+    public class GamePresetAttribute : Attribute
+    {
+        public string Id { get; }
+
+        public ImmutableList<string> Aliases { get; }
+
+        public GamePresetAttribute(string id, params string[] aliases)
+        {
+            Id = id;
+            Aliases = aliases.ToImmutableList();
+        }
+    }
+}
