dde6d687c9aa1e3e02860e1e02d728bb32c3fdd7
Vera Aguilera Puerto <zddm@outlook.es>
2021-02-12 11:46:56 +0100

    Fix build

--- Content.Server/Commands/GameTicking/FixRotationsCommand.cs
+++ Content.Server/Commands/GameTicking/FixRotationsCommand.cs
@@ -2,13 +2,8 @@
 using Content.Shared.GameObjects.Components;
 using Content.Server.GameObjects.Components;
 using Content.Shared.Administration;
-using Content.Shared.Maps;
-using Robust.Server.Interfaces.Player;
 using Robust.Shared.Console;
-using Robust.Shared.GameObjects.Components.Transform;
 using Robust.Shared.GameObjects;
-using Robust.Shared.Interfaces.GameObjects;
-using Robust.Shared.Interfaces.Map;
 using Robust.Shared.IoC;
 using Robust.Shared.Map;
 using Robust.Shared.Maths;
