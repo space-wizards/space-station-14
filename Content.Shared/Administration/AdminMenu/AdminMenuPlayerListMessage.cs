ec5b0df3a781624427e37a95f812b52abf44a1fa
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-02-15 18:28:34 +0100

    Fix the admin panel only listing your character name

---
+++ Content.Shared/Administration/AdminMenu/AdminMenuPlayerListMessage.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using Lidgren.Network;
+using Robust.Shared.Network;
+
+namespace Content.Shared.Administration.AdminMenu
+{
+    public class AdminMenuPlayerListMessage : NetMessage
+    {
+        #region REQUIRED
+        public static readonly MsgGroups GROUP = MsgGroups.Command;
+        public static readonly string NAME = nameof(AdminMenuPlayerListMessage);
+        public AdminMenuPlayerListMessage(INetChannel channel) : base(NAME, GROUP) { }
+        #endregion
+
+        public Dictionary<string, string> NamesToPlayers = default!;
+
+        public override void ReadFromBuffer(NetIncomingMessage buffer)
+        {
+            var pairs = buffer.ReadInt32();
+
+            NamesToPlayers = new Dictionary<string, string>();
+
+            for (var i = 0; i < pairs; i++)
+            {
+                var name = buffer.ReadString();
+                var player = buffer.ReadString();
+
+                NamesToPlayers.Add(name, player);
+            }
+        }
+
+        public override void WriteToBuffer(NetOutgoingMessage buffer)
+        {
+            buffer.Write(NamesToPlayers.Count);
+
+            foreach (var (name, player) in NamesToPlayers)
+            {
+                buffer.Write(name);
+                buffer.Write(player);
+            }
+        }
+    }
+}
