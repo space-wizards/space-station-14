ec2818960f7a6c4c5442b46b05daa36dcca23f73
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-02-20 00:03:57 +0100

    IndexedPrototype fixes

--- Content.Tests/Shared/Alert/AlertOrderPrototypeTests.cs
+++ Content.Tests/Shared/Alert/AlertOrderPrototypeTests.cs
@@ -23,39 +23,49 @@ namespace Content.Tests.Shared.Alert
     - category: Temperature
 
 - type: alert
+  name: AlertLowPressure
   category: Pressure
   alertType: LowPressure
 
 - type: alert
+  name: AlertOverfed
   category: Hunger
   alertType: Overfed
 
 - type: alert
+  name: AlertHighPressure
   category: Pressure
   alertType: HighPressure
 
 - type: alert
+  name: AlertPeckish
   category: Hunger
   alertType: Peckish
 
 - type: alert
+  name: AlertStun
   alertType: Stun
 
 - type: alert
+  name: AlertHandcuffed
   alertType: Handcuffed
 
 - type: alert
+  name: AlertHot
   category: Temperature
   alertType: Hot
 
 - type: alert
+  name: AlertCold
   category: Temperature
   alertType: Cold
 
 - type: alert
+  name: AlertWeightless
   alertType: Weightless
 
 - type: alert
+  name: AlertPilotingShuttle
   alertType: PilotingShuttle
 ";
 
