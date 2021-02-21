b314c3f04018fa7e5fe76932ba9de973cdef52f6
tmtmtl30 <53132901+tmtmtl30@users.noreply.github.com>
2021-02-21 05:02:23 -0800

    Adds a very basic perpetual background ambient hum. (#3150)

--- Content.Client/UserInterface/OptionsMenu.Audio.cs
+++ Content.Client/UserInterface/OptionsMenu.Audio.cs
@@ -1,4 +1,5 @@
-﻿using Content.Client.UserInterface.Stylesheets;
+using Content.Client.UserInterface.Stylesheets;
+using Content.Shared;
 using Robust.Client.Graphics;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
@@ -19,6 +20,7 @@ namespace Content.Client.UserInterface
             private readonly Button ApplyButton;
             private readonly Label MasterVolumeLabel;
             private readonly Slider MasterVolumeSlider;
+            private readonly CheckBox AmbienceCheckBox;
             private readonly Button ResetButton;
 
             public AudioControl(IConfigurationManager cfg, IClydeAudio clydeAudio)
@@ -62,6 +64,11 @@ namespace Content.Client.UserInterface
                         new Control { MinSize = (4, 0) },
                     }
                 });
+
+                // sets up ambience checkbox. i am sorry for not fixing the rest of this code.
+                AmbienceCheckBox = new CheckBox { Text = Loc.GetString("Ambient Hum") };
+                contents.AddChild(AmbienceCheckBox);
+                AmbienceCheckBox.Pressed = _cfg.GetCVar(CCVars.AmbienceBasicEnabled);
 
                 ApplyButton = new Button
                 {
@@ -112,6 +119,7 @@ namespace Content.Client.UserInterface
                 ApplyButton.OnPressed += OnApplyButtonPressed;
                 ResetButton.OnPressed += OnResetButtonPressed;
                 MasterVolumeSlider.OnValueChanged += OnMasterVolumeSliderChanged;
+                AmbienceCheckBox.OnToggled += OnAmbienceCheckToggled;
 
                 AddChild(vBox);
                 UpdateChanges();
@@ -122,6 +130,7 @@ namespace Content.Client.UserInterface
                 ApplyButton.OnPressed -= OnApplyButtonPressed;
                 ResetButton.OnPressed -= OnResetButtonPressed;
                 MasterVolumeSlider.OnValueChanged -= OnMasterVolumeSliderChanged;
+                AmbienceCheckBox.OnToggled -= OnAmbienceCheckToggled;
                 base.Dispose(disposing);
             }
 
@@ -132,9 +141,15 @@ namespace Content.Client.UserInterface
                 UpdateChanges();
             }
 
+            private void OnAmbienceCheckToggled(BaseButton.ButtonEventArgs args)
+            {
+                UpdateChanges();
+            }
+
             private void OnApplyButtonPressed(BaseButton.ButtonEventArgs args)
             {
                 _cfg.SetCVar(CVars.AudioMasterVolume, MasterVolumeSlider.Value / 100.0f);
+                _cfg.SetCVar(CCVars.AmbienceBasicEnabled, AmbienceCheckBox.Pressed);
                 _cfg.SaveToFile();
                 UpdateChanges();
             }
@@ -143,13 +158,15 @@ namespace Content.Client.UserInterface
             {
                 MasterVolumeSlider.Value = _cfg.GetCVar(CVars.AudioMasterVolume) * 100.0f;
                 MasterVolumeLabel.Text = string.Format(Loc.GetString("{0:0}%"), MasterVolumeSlider.Value);
+                AmbienceCheckBox.Pressed = _cfg.GetCVar(CCVars.AmbienceBasicEnabled);
                 UpdateChanges();
             }
 
             private void UpdateChanges()
             {
                 var isMasterVolumeSame = System.Math.Abs(MasterVolumeSlider.Value - _cfg.GetCVar(CVars.AudioMasterVolume) * 100.0f) < 0.01f;
-                var isEverythingSame = isMasterVolumeSame;
+                var isAmbienceSame = AmbienceCheckBox.Pressed == _cfg.GetCVar(CCVars.AmbienceBasicEnabled);
+                var isEverythingSame = isMasterVolumeSame && isAmbienceSame;
                 ApplyButton.Disabled = isEverythingSame;
                 ResetButton.Disabled = isEverythingSame;
             }
