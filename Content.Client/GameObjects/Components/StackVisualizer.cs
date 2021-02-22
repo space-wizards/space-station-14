916e6cb1faa262bc4dceedc3529cdb4123cff4f4
SweptWasTaken <sweptwastaken@protonmail.com>
2021-02-21 17:02:10 -0800

    Fixup

--- Content.Client/GameObjects/Components/StackVisualizer.cs
+++ Content.Client/GameObjects/Components/StackVisualizer.cs
@@ -75,6 +75,7 @@ namespace Content.Client.GameObjects.Components
         /// 
         /// </summary>
         private bool _isComposite;
+        private ResourcePath? _spritePath;
 
         public override void LoadData(YamlMappingNode mapping)
         {
@@ -93,7 +94,10 @@ namespace Content.Client.GameObjects.Components
                 _isComposite = transparent.AsBool();
             }
 
-            if (mapping.TryGetNode<YamlScalarNode>("sprite", out var rsiPath));
+            if (mapping.TryGetNode<YamlScalarNode>("sprite", out var spritePath))
+            {
+                _spritePath = spritePath.AsResourcePath();
+            }
         }
 
         public override void InitializeEntity(IEntity entity)
@@ -104,9 +108,14 @@ namespace Content.Client.GameObjects.Components
                 && _spriteLayers.Count > 0
                 && entity.TryGetComponent<ISpriteComponent>(out var spriteComponent))
             {
+                if (_spritePath == null)
+                {
+                    _spritePath = spriteComponent.BaseRSI!.Path!;
+                }
+
                 foreach (var sprite in _spriteLayers)
                 {
-                    var rsiPath = spriteComponent.BaseRSI!.Path!;
+                    var rsiPath = _spritePath;
                     spriteComponent.LayerMapReserveBlank(sprite);
                     spriteComponent.LayerSetSprite(sprite, new SpriteSpecifier.Rsi(rsiPath, sprite));
                     spriteComponent.LayerSetVisible(sprite, false);
