eddec5fcce621f3140abf2ce1acb604fff4c1d51
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-02-28 18:49:46 +0100

    Revert "Fixes a crash when an entity goes off-grid. (hehe)"
    This reverts commit 3967ecf098e16ee85ad3a8e2062cd3dcbae5cdda.

--- Content.Shared/Physics/Controllers/SharedTileFrictionController.cs
+++ Content.Shared/Physics/Controllers/SharedTileFrictionController.cs
@@ -88,10 +88,14 @@ namespace Content.Shared.Physics.Controllers
         [Pure]
         private float GetTileFriction(IPhysBody body)
         {
-            if (body.BodyStatus == BodyStatus.InAir || !_mapManager.TryGetGrid(body.Owner.Transform.GridID, out var grid))
+            if (body.BodyStatus == BodyStatus.InAir || body.Entity.Transform.GridID == GridId.Invalid)
                 return 0.0f;
 
-            var tile = grid.GetTileRef(body.Owner.Transform.Coordinates);
+            var transform = body.Owner.Transform;
+            var coords = transform.Coordinates;
+
+            var grid = _mapManager.GetGrid(coords.GetGridId(body.Owner.EntityManager));
+            var tile = grid.GetTileRef(coords);
             var tileDef = _tileDefinitionManager[tile.Tile.TypeId];
             return tileDef.Friction;
         }
