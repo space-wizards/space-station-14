d805cb1c71f54168e209dbd720711e1df03e1a44
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-01 20:23:06 +0100

    Do the thing for construction

--- Content.Shared/Construction/ConstructionGraphStepTypeSerializer.cs
+++ Content.Shared/Construction/ConstructionGraphStepTypeSerializer.cs
@@ -1,6 +1,5 @@
 ﻿#nullable enable
 using System;
-using Robust.Shared.IoC;
 using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.Serialization.Manager.Result;
@@ -11,47 +10,57 @@ namespace Content.Shared.Construction
     [TypeSerializer]
     public class ConstructionGraphStepTypeSerializer : ITypeReader<ConstructionGraphStep, MappingDataNode>
     {
-        // TODO PAUL SERV3
+        private Type? GetType(MappingDataNode node)
+        {
+            if (node.HasNode("material"))
+            {
+                return typeof(MaterialConstructionGraphStep);
+            }
+            else if (node.HasNode("tool"))
+            {
+                return typeof(ToolConstructionGraphStep);
+            }
+            else if (node.HasNode("prototype"))
+            {
+                return typeof(PrototypeConstructionGraphStep);
+            }
+            else if (node.HasNode("component"))
+            {
+                return typeof(ComponentConstructionGraphStep);
+            }
+            else if (node.HasNode("tag"))
+            {
+                return typeof(TagConstructionGraphStep);
+            }
+            else if (node.HasNode("allTags") || node.HasNode("anyTags"))
+            {
+                return typeof(MultipleTagsConstructionGraphStep);
+            }
+            else if (node.HasNode("steps"))
+            {
+                return typeof(NestedConstructionGraphStep);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
         public DeserializationResult Read(ISerializationManager serializationManager,
             MappingDataNode node,
             ISerializationContext? context = null)
         {
-            Type type;
+            var type = GetType(node) ??
+                       throw new ArgumentException(
+                           "Tried to convert invalid YAML node mapping to ConstructionGraphStep!");
 
-            if (node.HasNode("material"))
-            {
-                type = typeof(MaterialConstructionGraphStep);
-            }
-            else if (node.HasNode("tool"))
-            {
-                type = typeof(ToolConstructionGraphStep);
-            }
-            else if (node.HasNode("prototype"))
-            {
-                type = typeof(PrototypeConstructionGraphStep);
-            }
-            else if (node.HasNode("component"))
-            {
-                type = typeof(ComponentConstructionGraphStep);
-            }
-            else if (node.HasNode("tag"))
-            {
-                type = typeof(TagConstructionGraphStep);
-            }
-            else if (node.HasNode("allTags") || node.HasNode("anyTags"))
-            {
-                type = typeof(MultipleTagsConstructionGraphStep);
-            }
-            else if (node.HasNode("steps"))
-            {
-                type = typeof(NestedConstructionGraphStep);
-            }
-            else
-            {
-                throw new ArgumentException("Tried to convert invalid YAML node mapping to ConstructionGraphStep!");
-            }
+            return new DeserializedValue<ConstructionGraphStep>(serializationManager.ReadValueOrThrow<ConstructionGraphStep>(type, node, context));
+        }
 
-            return new DeserializedValue<ConstructionGraphStep>(serializationManager.ReadValueOrThrow<ConstructionGraphStep>(type, node));
+        public bool Validate(ISerializationManager serializationManager, MappingDataNode node)
+        {
+            var type = GetType(node);
+            return type != null && serializationManager.ValidateNode(type, node);
         }
     }
 }
