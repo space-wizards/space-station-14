a168cc0222197f868cd93f59c3789aa5153cebc9
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-01 14:28:57 +0100

    Fix clothing component slot flags serialization tag

--- Content.Server/GameObjects/Components/Items/Clothing/ClothingComponent.cs
+++ Content.Server/GameObjects/Components/Items/Clothing/ClothingComponent.cs
@@ -24,13 +24,15 @@ namespace Content.Server.GameObjects.Components.Items.Clothing
         public override uint? NetID => ContentNetIDs.CLOTHING;
 
         [ViewVariables]
-        [DataField("slotFlags")]
+        [DataField("Slots")]
         public SlotFlags SlotFlags = SlotFlags.PREVENTEQUIP; //Different from None, NONE allows equips if no slot flags are required
 
         [DataField("QuickEquip")]
         private bool _quickEquipEnabled = true;
+
         [DataField("HeatResistance")]
         private int _heatResistance = 323;
+
         [ViewVariables(VVAccess.ReadWrite)]
         public int HeatResistance => _heatResistance;
 
