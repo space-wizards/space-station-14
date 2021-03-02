4771adf16c9c14f08677dd450241bee15c2f4d79
Paul <ritter.paul1+git@googlemail.com>
2021-03-02 10:40:36 +0100

    monke see yaml linter

--- Content.IntegrationTests/Tests/Serialization/ValidationTest.cs
+++ Content.IntegrationTests/Tests/Serialization/ValidationTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Robust.Shared.Prototypes;
@@ -13,12 +15,32 @@ namespace Content.IntegrationTests.Tests.Serialization
             var server = StartServer();
             await server.WaitIdleAsync();
             var sprotoManager = server.ResolveDependency<IPrototypeManager>();
+            var serverErrors = new HashSet<string>();
             await server.WaitAssertion(() =>
                 {
                     var res = sprotoManager.ValidateDirectory(new ResourcePath("/Prototypes"));
-                    Assert.That(res.Count, Is.Zero);
+                    serverErrors = res.SelectMany(p =>
+                        p.Value.Where(n => !n.node.Valid)
+                            .SelectMany(n => n.node.Invalids().Select(i => $"{{{n.file}}} => {p.Key} <> {i}").ToList())).ToHashSet();
                 }
             );
+            server.Stop();
+
+            var client = StartClient();
+            await client.WaitIdleAsync();
+            var cprotoManager = client.ResolveDependency<IPrototypeManager>();
+            var clientErrors = new HashSet<string>();
+            await client.WaitAssertion(() =>
+                {
+                    var res = cprotoManager.ValidateDirectory(new ResourcePath("/Prototypes"));
+                    clientErrors = res.SelectMany(p =>
+                        p.Value.Where(n => !n.node.Valid)
+                            .SelectMany(n => n.node.Invalids().Select(i => $"{{{n.file}}} => {p.Key} <> {i}").ToList())).ToHashSet();
+                }
+            );
+
+            var actualErrors = clientErrors.Intersect(serverErrors).ToHashSet();
+            Assert.That(actualErrors.Count, Is.Zero);
         }
     }
 }
