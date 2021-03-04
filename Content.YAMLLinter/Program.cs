f9a253851ec14d99d0d9436135c48bebb1b5334a
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-04 19:42:56 +0100

    Revert "Make yaml linter 5 times faster (~80% less execution time)"
    This reverts commit 3e6091359a26252c3e98828199553de668031c63.

--- Content.YAMLLinter/Program.cs
+++ Content.YAMLLinter/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Content.IntegrationTests;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.Markdown.Validation;
+using Robust.Shared.Utility;
 
 namespace Content.YAMLLinter
 {
-    internal class Program : ContentIntegrationTest
+    class Program : ContentIntegrationTest
     {
-        private static int Main(string[] args)
+        static int Main(string[] args)
         {
             var errors = new Program().RunValidation().Result;
             if (errors.Count != 0)
@@ -30,13 +32,53 @@ namespace Content.YAMLLinter
             return 0;
         }
 
-        private async Task<Dictionary<string, HashSet<ErrorNode>>> RunValidation()
+        private async Task<Dictionary<string, HashSet<ErrorNode>>> ValidateClient()
+        {
+            var client = StartClient();
+
+            await client.WaitIdleAsync();
+
+            var cPrototypeManager = client.ResolveDependency<IPrototypeManager>();
+            var clientErrors = new Dictionary<string, HashSet<ErrorNode>>();
+
+            await client.WaitAssertion(() =>
+            {
+                clientErrors = cPrototypeManager.ValidateDirectory(new ResourcePath("/Prototypes"));
+            });
+
+            client.Stop();
+
+            return clientErrors;
+        }
+
+        private async Task<Dictionary<string, HashSet<ErrorNode>>> ValidateServer()
+        {
+            var server = StartServer();
+
+            await server.WaitIdleAsync();
+
+            var sPrototypeManager = server.ResolveDependency<IPrototypeManager>();
+            var serverErrors = new Dictionary<string, HashSet<ErrorNode>>();
+
+            await server.WaitAssertion(() =>
+            {
+                serverErrors = sPrototypeManager.ValidateDirectory(new ResourcePath("/Prototypes"));
+            });
+
+            server.Stop();
+
+            return serverErrors;
+        }
+
+        public async Task<Dictionary<string, HashSet<ErrorNode>>> RunValidation()
         {
             var allErrors = new Dictionary<string, HashSet<ErrorNode>>();
 
-            var clientErrors = await Task.Run(() => new ClientLinter().ValidateClient());
+            var tasks = await Task.WhenAll(ValidateClient(), ValidateServer());
+            var clientErrors = tasks[0];
+            var serverErrors = tasks[1];
 
-            foreach (var (key, val) in new ServerLinter().ValidateServer())
+            foreach (var (key, val) in serverErrors)
             {
                 if (clientErrors.TryGetValue(key, out var clientVal))
                 {
