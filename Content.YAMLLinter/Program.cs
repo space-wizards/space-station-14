6f1ec78591427f25c758350f1235fd6e74a0e169
Paul <ritter.paul1+git@googlemail.com>
2021-03-04 09:44:19 +0100

    adds proper info the validation

--- Content.YAMLLinter/Program.cs
+++ Content.YAMLLinter/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Content.IntegrationTests;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.Markdown.Validation;
 using Robust.Shared.Utility;
 
 namespace Content.YAMLLinter
@@ -15,31 +16,31 @@ namespace Content.YAMLLinter
             var errors = new Program().RunValidation();
             if (errors.Count != 0)
             {
-                Console.WriteLine($"Found {errors.Count} Error(s)!");
-                Console.WriteLine();
-                foreach (var error in errors)
+                foreach (var (file, errorHashset) in errors)
                 {
-                    Console.WriteLine(error);
+                    foreach (var errorNode in errorHashset)
+                    {
+                        Console.WriteLine($"({file} | L:{errorNode.Node.Start.Line}-{errorNode.Node.End.Line}) | C:{errorNode.Node.Start.Column}-{errorNode.Node.End.Column}): {errorNode.ErrorReason}");
+                    }
                 }
 
                 return -1;
             }
 
+            Console.WriteLine("No errors found!");
+
             return 0;
         }
 
-        public HashSet<string> RunValidation()
+        public Dictionary<string, HashSet<ErrorNode>> RunValidation()
         {
             var server = StartServer();
             server.WaitIdleAsync().Wait();
             var sprotoManager = server.ResolveDependency<IPrototypeManager>();
-            var serverErrors = new HashSet<string>();
+            var serverErrors = new Dictionary<string, HashSet<ErrorNode>>();
             server.WaitAssertion(() =>
                 {
-                    var res = sprotoManager.ValidateDirectory(new ResourcePath("/Prototypes"));
-                    serverErrors = res.SelectMany(p =>
-                        p.Value.Where(n => !n.node.Valid)
-                            .SelectMany(n => n.node.Invalids().Select(i => $"{{{n.file}}} => {p.Key} <> {i}").ToList())).ToHashSet();
+                    serverErrors = sprotoManager.ValidateDirectory(new ResourcePath("/Prototypes"));
                 }
             ).Wait();
             server.Stop();
@@ -47,19 +48,28 @@ namespace Content.YAMLLinter
             var client = StartClient();
             client.WaitIdleAsync().Wait();
             var cprotoManager = client.ResolveDependency<IPrototypeManager>();
-            var clientErrors = new HashSet<string>();
+            var clientErrors = new Dictionary<string, HashSet<ErrorNode>>();
             client.WaitAssertion(() =>
                 {
-                    var res = cprotoManager.ValidateDirectory(new ResourcePath("/Prototypes"));
-                    clientErrors = res.SelectMany(p =>
-                        p.Value.Where(n => !n.node.Valid)
-                            .SelectMany(n => n.node.Invalids().Select(i => $"{{{n.file}}} => {p.Key} <> {i}").ToList())).ToHashSet();
+                    clientErrors = cprotoManager.ValidateDirectory(new ResourcePath("/Prototypes"));
                 }
             ).Wait();
-
-            var res = clientErrors.Intersect(serverErrors).ToHashSet();
             client.Stop();
-            return res;
+
+            var allErrors = new Dictionary<string, HashSet<ErrorNode>>();
+            foreach (var (key, val) in serverErrors)
+            {
+                if (clientErrors.TryGetValue(key, out var clientVal))
+                {
+                    var newErrors = val.Intersect(clientVal).ToHashSet();
+                    newErrors.UnionWith(val.Where(n => n.AlwaysRelevant));
+                    newErrors.UnionWith(clientVal.Where(n => n.AlwaysRelevant));
+                    if (newErrors.Count == 0) continue;
+
+                    allErrors[key] = newErrors;
+                }
+            }
+            return allErrors;
         }
     }
 }
