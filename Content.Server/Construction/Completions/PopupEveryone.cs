7baa7fe4e89d6164c752712f0c8bedae2fc089d8
Vera Aguilera Puerto <zddm@outlook.es>
2021-03-05 06:54:43 +0100

    Improvements

--- Content.Server/Construction/Completions/PopupEveryone.cs
+++ Content.Server/Construction/Completions/PopupEveryone.cs
@@ -1,28 +1,20 @@
-﻿#nullable enable
+#nullable enable
 using System.Threading.Tasks;
+using Content.Server.Utility;
 using Content.Shared.Construction;
-using Content.Shared.Interfaces;
-using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Serialization.Manager.Attributes;
 
 namespace Content.Server.Construction.Completions
 {
-    [UsedImplicitly]
     [DataDefinition]
-    public class PopupUser : IGraphAction
+    public class PopupEveryone : IGraphAction
     {
-        [field: DataField("cursor")] public bool Cursor { get; } = false;
         [field: DataField("text")] public string Text { get; } = string.Empty;
 
         public async Task PerformAction(IEntity entity, IEntity? user)
         {
-            if (user == null) return;
-
-            if(Cursor)
-                user.PopupMessageCursor(Text);
-            else
-                entity.PopupMessage(user, Text);
+            entity.PopupMessageEveryone(Text);
         }
     }
 }
