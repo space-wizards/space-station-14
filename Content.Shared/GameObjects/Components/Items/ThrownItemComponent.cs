1293d3c2edf6fce85d684a552d47d3605212e1de
Metal Gear Sloth <metalgearsloth@gmail.com>
2021-03-08 11:01:38 +1100

    Better throwing collisions

--- Content.Shared/GameObjects/Components/Items/ThrownItemComponent.cs
+++ Content.Shared/GameObjects/Components/Items/ThrownItemComponent.cs
@@ -1,17 +1,22 @@
 #nullable enable
 using Content.Shared.GameObjects.EntitySystems;
+using Content.Shared.Interfaces.GameObjects.Components;
+using Content.Shared.Physics;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Physics;
 using Robust.Shared.Physics.Collision;
+using Robust.Shared.Physics.Dynamics;
 
 namespace Content.Shared.GameObjects.Components.Items
 {
     [RegisterComponent]
-    public class ThrownItemComponent : Component, IStartCollide, ICollideSpecial
+    public class ThrownItemComponent : Component, IStartCollide, ICollideSpecial, IThrown, ILand
     {
         public override string Name => "ThrownItem";
 
         public IEntity? Thrower { get; set; }
+
+        private Fixture? _fixture;
 
         void IStartCollide.CollideWith(IPhysBody ourBody, IPhysBody otherBody, in Manifold manifold)
         {
@@ -22,6 +27,23 @@ namespace Content.Shared.GameObjects.Components.Items
         bool ICollideSpecial.PreventCollide(IPhysBody collidedwith)
         {
             return collidedwith.Entity == Thrower;
+        }
+
+        void IThrown.Thrown(ThrownEventArgs eventArgs)
+        {
+            if (!Owner.TryGetComponent(out PhysicsComponent? physicsComponent) ||
+                physicsComponent.Fixtures.Count != 1) return;
+
+            var shape = physicsComponent.Fixtures[0].Shape;
+            _fixture = new Fixture(physicsComponent, shape) {CollisionLayer = (int) CollisionGroup.ThrownItem, Hard = false};
+            physicsComponent.AddFixture(_fixture);
+        }
+
+        void ILand.Land(LandEventArgs eventArgs)
+        {
+            if (!Owner.TryGetComponent(out PhysicsComponent? physicsComponent) || _fixture == null) return;
+
+            physicsComponent.RemoveFixture(_fixture);
         }
     }
 }
