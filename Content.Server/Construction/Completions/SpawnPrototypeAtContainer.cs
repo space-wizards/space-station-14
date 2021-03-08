13e95ac9a807de0f92b4f76996ddfff02bd4441d
Vera Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>
2021-03-08 05:09:17 +0100

    Makes construction graphs turing complete (#3516)
    * Makes construction graphs turing complete
    
    * Improvements

---
+++ Content.Server/Construction/Completions/SpawnPrototypeAtContainer.cs
@@ -0,0 +1,33 @@
+#nullable enable
+using System;
+using System.Threading.Tasks;
+using Content.Shared.Construction;
+using JetBrains.Annotations;
+using Robust.Shared.Containers;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+
+namespace Content.Server.Construction.Completions
+{
+    [UsedImplicitly]
+    [DataDefinition]
+    public class SpawnPrototypeAtContainer : IGraphAction
+    {
+        [field: DataField("prototype")] public string Prototype { get; } = string.Empty;
+        [field: DataField("container")] public string Container { get; } = string.Empty;
+        [field: DataField("amount")] public int Amount { get; } = 1;
+
+        public async Task PerformAction(IEntity entity, IEntity? user)
+        {
+            if (entity.Deleted || string.IsNullOrEmpty(Container) || string.IsNullOrEmpty(Prototype))
+                return;
+
+            var container = entity.EnsureContainer<Container>(Container);
+
+            for (var i = 0; i < Amount; i++)
+            {
+                container.Insert(entity.EntityManager.SpawnEntity(Prototype, entity.Transform.Coordinates));
+            }
+        }
+    }
+}
