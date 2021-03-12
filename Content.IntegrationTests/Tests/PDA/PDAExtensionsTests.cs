7df2cb9870dc56d0d553b3299eab9178cec56751
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-12 23:07:15 +0100

    Add extension methods to get a player's id and tests

---
+++ Content.IntegrationTests/Tests/PDA/PDAExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Content.Server.GameObjects.Components.GUI;
+using Content.Server.GameObjects.Components.PDA;
+using NUnit.Framework;
+using Robust.Server.Player;
+
+namespace Content.IntegrationTests.Tests.PDA
+{
+    public class PDAExtensionsTests : ContentIntegrationTest
+    {
+        [Test]
+        public async Task PlayerGetIdComponent()
+        {
+            var (client, server) = await StartConnectedServerClientPair();
+
+            await Task.WhenAll(client.WaitIdleAsync(), server.WaitIdleAsync());
+
+            var sPlayerManager = server.ResolveDependency<IPlayerManager>();
+
+            await server.WaitAssertion(() =>
+            {
+                var player = sPlayerManager.GetAllPlayers().Single().AttachedEntity;
+
+                Assert.NotNull(player);
+
+                // The player spawns with an id on by default
+                Assert.NotNull(player.PlayerGetId());
+                Assert.True(player.TryPlayerGetId(out var id));
+                Assert.NotNull(id);
+
+                // Remove id
+                var inventory = player.GetComponent<InventoryComponent>();
+
+                foreach (var slot in inventory.Slots)
+                {
+                    var item = inventory.GetSlotItem(slot);
+
+                    if (item == null)
+                    {
+                        continue;
+                    }
+
+                    if (item.Owner.HasComponent<PDAComponent>())
+                    {
+                        inventory.ForceUnequip(slot);
+                    }
+                }
+
+                // No id
+                Assert.Null(player.PlayerGetId());
+                Assert.False(player.TryPlayerGetId(out id));
+                Assert.Null(id);
+            });
+        }
+    }
+}
---
+++ Content.Server/GameObjects/Components/PDA/PDAExtensions.cs
@@ -0,0 +1,69 @@
+﻿#nullable enable
+using System.Diagnostics.CodeAnalysis;
+using Content.Server.GameObjects.Components.Access;
+using Content.Server.GameObjects.Components.GUI;
+using Content.Server.Interfaces.GameObjects.Components.Items;
+using Robust.Shared.GameObjects;
+
+namespace Content.Server.GameObjects.Components.PDA
+{
+    public static class PdaExtensions
+    {
+        /// <summary>
+        ///     Gets the id that a player is holding in their hands or inventory.
+        ///     Order: Hands > ID slot > PDA in ID slot
+        /// </summary>
+        /// <param name="player">The player to check in.</param>
+        /// <returns>The id card component.</returns>
+        public static IdCardComponent? PlayerGetId(this IEntity player)
+        {
+            if (player.TryGetComponent(out IHandsComponent? hands))
+            {
+                foreach (var item in hands.GetAllHeldItems())
+                {
+                    if (item.Owner.TryGetComponent(out PDAComponent? pda) &&
+                        pda.ContainedID != null)
+                    {
+                        return pda.ContainedID;
+                    }
+
+                    if (item.Owner.TryGetComponent(out IdCardComponent? card))
+                    {
+                        return card;
+                    }
+                }
+            }
+
+            if (player.TryGetComponent(out InventoryComponent? inventory))
+            {
+                foreach (var item in inventory.GetAllHeldItems())
+                {
+                    if (item.TryGetComponent(out PDAComponent? pda) &&
+                        pda.ContainedID != null)
+                    {
+                        return pda.ContainedID;
+                    }
+
+                    if (item.TryGetComponent(out IdCardComponent? card))
+                    {
+                        return card;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        ///     Gets the id that a player is holding in their hands or inventory.
+        ///     Order: Hands > ID slot > PDA in ID slot
+        /// </summary>
+        /// <param name="player">The player to check in.</param>
+        /// <param name="id">The id card component.</param>
+        /// <returns>true if found, false otherwise.</returns>
+        public static bool TryPlayerGetId(this IEntity player, [NotNullWhen(true)] out IdCardComponent? id)
+        {
+            return (id = player.PlayerGetId()) != null;
+        }
+    }
+}
