13afec5f7624ad429e8ff5e5b31adc8593321327
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-03-14 11:36:55 +0100

    Make TraitorOverlay use EnumerateRunes()

--- Content.Client/GameObjects/Components/Suspicion/TraitorOverlay.cs
+++ Content.Client/GameObjects/Components/Suspicion/TraitorOverlay.cs
@@ -100,9 +100,9 @@ namespace Content.Client.GameObjects.Components.Suspicion
         {
             var baseLine = new Vector2(pos.X, font.GetAscent(1) + pos.Y);
 
-            foreach (var chr in str)
+            foreach (var rune in str.EnumerateRunes())
             {
-                var advance = font.DrawChar(handle, chr, baseLine, 1, color);
+                var advance = font.DrawChar(handle, rune, baseLine, 1, color);
                 baseLine += new Vector2(advance, 0);
             }
         }
