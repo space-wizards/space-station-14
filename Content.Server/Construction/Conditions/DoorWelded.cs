51178b9ae7722e8715a5dd3ae2718ec331b38c07
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2021-03-17 05:07:49 -0700

    Airlock construction (#3609)
    * Add construction and deconstsruction of airlocks
    
    * Change name to door electronics
    
    * Rearrange things
    
    * Fix errors
    
    * Fix physics error from bumping
    
    * Prevent editing the map for now
    
    * Address reviews
    
    * Change switch to if
    
    * Fix newlines

--- Content.Server/Construction/Conditions/DoorWelded.cs
+++ Content.Server/Construction/Conditions/DoorWelded.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+#nullable enable
+using System.Threading.Tasks;
 using Content.Server.GameObjects.Components.Doors;
 using Content.Shared.Construction;
 using JetBrains.Annotations;
@@ -14,29 +15,30 @@ namespace Content.Server.Construction.Conditions
     [DataDefinition]
     public class DoorWelded : IEdgeCondition
     {
-        [DataField("welded")] public bool Welded { get; private set; } = true;
+        [DataField("welded")]
+        public bool Welded { get; private set; } = true;
 
         public async Task<bool> Condition(IEntity entity)
         {
             if (!entity.TryGetComponent(out ServerDoorComponent? doorComponent)) return false;
+
             return doorComponent.IsWeldedShut == Welded;
         }
 
         public bool DoExamine(IEntity entity, FormattedMessage message, bool inDetailsRange)
         {
-            if (!entity.TryGetComponent(out ServerDoorComponent? doorComponent)) return false;
+            if (!entity.TryGetComponent(out ServerDoorComponent? door)) return false;
 
-            if (doorComponent.State == DoorState.Closed && Welded)
+            if (door.IsWeldedShut != Welded)
             {
-                message.AddMarkup(Loc.GetString("First, weld the door.\n"));
+                if (Welded == true)
+                    message.AddMarkup(Loc.GetString("construction-condition-door-weld", ("entityName", entity.Name)) + "\n");
+                else
+                    message.AddMarkup(Loc.GetString("construction-condition-door-unweld", ("entityName", entity.Name)) + "\n");
                 return true;
             }
 
-            if (!doorComponent.IsWeldedShut || Welded) return false;
-
-            message.AddMarkup(Loc.GetString("First, unweld the door.\n"));
-            return true;
-
+            return false;
         }
     }
 }
