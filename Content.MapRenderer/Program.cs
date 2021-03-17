03a4f1c9537de14861720181268bbace9a6014f4
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-17 17:00:32 +0100

    Add pr number to env

--- Content.MapRenderer/Program.cs
+++ Content.MapRenderer/Program.cs
@@ -68,7 +68,7 @@ namespace Content.MapRenderer
             }
 
             var images = new List<ImgurUploadResponse>();
-            Console.WriteLine(Environment.GetEnvironmentVariable("REPOSITORY_OWNER"));
+            Console.WriteLine(Environment.GetEnvironmentVariable("github.event.number"));
 
             // foreach (var map in maps)
             // {
