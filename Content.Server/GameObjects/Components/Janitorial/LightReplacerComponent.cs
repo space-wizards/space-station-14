9d3ad46492ac4766cc7bcdd085802aa9073b193c
Alex Evgrashin <aevgrashin@yandex.ru>
2021-03-18 23:47:29 +0300

    Added text messages

--- Content.Server/GameObjects/Components/Janitorial/LightReplacerComponent.cs
+++ Content.Server/GameObjects/Components/Janitorial/LightReplacerComponent.cs
@@ -2,9 +2,11 @@
 using Content.Server.GameObjects.Components.Items.Storage;
 using Content.Server.GameObjects.Components.Power.ApcNetComponents.PowerReceiverUsers;
 using Content.Shared.GameObjects.EntitySystems.ActionBlocker;
+using Content.Shared.Interfaces;
 using Content.Shared.Interfaces.GameObjects.Components;
 using Robust.Shared.Containers;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Localization;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
@@ -18,6 +20,7 @@ namespace Content.Server.GameObjects.Components.Janitorial
 {
     /// <summary>
     ///     Device that allows user to quikly change bulbs in <see cref="PoweredLightComponent"/>
+    ///     Can be reloaded by new light tubes or light bulbs
     /// </summary>
     [RegisterComponent]
     public class LightReplacerComponent : Component, IAfterInteract, IInteractUsing
@@ -55,13 +58,13 @@ namespace Content.Server.GameObjects.Components.Janitorial
             {
                 // replace broken light in fixture?
                 if (eventArgs.Target.TryGetComponent(out PoweredLightComponent? fixture))
-                    return TryReplaceBulb(fixture);
+                    return TryReplaceBulb(fixture, eventArgs.User);
                 // add new bulb to light replacer container?
                 else if (eventArgs.Target.TryGetComponent(out LightBulbComponent? bulb))
-                    return TryInsertBulb(bulb);
+                    return TryInsertBulb(bulb, eventArgs.User, true);
                 // add bulbs from storage?
                 else if (eventArgs.Target.TryGetComponent(out ServerStorageComponent? storage))
-                    return TryInsertBulb(storage);
+                    return TryInsertBulb(storage, eventArgs.User);
             }
 
             return false;
@@ -74,14 +77,15 @@ namespace Content.Server.GameObjects.Components.Janitorial
 
             if (eventArgs.Using != null)
             {
+                // want to insert a new light bulb?
                 if (eventArgs.Using.TryGetComponent(out LightBulbComponent? bulb))
-                    return TryInsertBulb(bulb);
+                    return TryInsertBulb(bulb, eventArgs.User, true);
             }
 
             return false;
         }
 
-        private bool TryReplaceBulb(PoweredLightComponent fixture)
+        private bool TryReplaceBulb(PoweredLightComponent fixture, IEntity? user = null)
         {
             // check if light bulb is broken or missing
             if (fixture.LightBulb != null && fixture.LightBulb.State == LightBulbState.Normal) return false;
@@ -91,33 +95,59 @@ namespace Content.Server.GameObjects.Components.Janitorial
                 (e) => e.GetComponentOrNull<LightBulbComponent>()?.Type == fixture.BulbType);
 
             // try to remove it from storage
-            if (bulb == null || !_bulbsStorage.Remove(bulb)) return false;
+            if (bulb == null || !_bulbsStorage.Remove(bulb))
+            {
+                if (user != null)
+                {
+                    var msg = Loc.GetString("comp-light-replacer-missing-light", ("light-replacer", Owner));
+                    user.PopupMessage(msg);
+                }
+                return false;
+            }
+
 
             // insert it into fixture
             return fixture.ReplacBulb(bulb);
         }
 
-        private bool TryInsertBulb(LightBulbComponent bulb)
+        private bool TryInsertBulb(LightBulbComponent bulb, IEntity? user = null, bool showTooltip = false)
         {
+            // only normal lights can be inserted inside light replacer
             if (bulb.State != LightBulbState.Normal)
+            {
+                if (showTooltip && user != null)
+                {
+                    var msg = Loc.GetString("comp-light-replacer-insert-broken-light");
+                    user.PopupMessage(msg);
+                }
+
                 return false;
+            }
 
             return _bulbsStorage.Insert(bulb.Owner);
         }
 
-        private bool TryInsertBulb(ServerStorageComponent storage)
+        private bool TryInsertBulb(ServerStorageComponent storage, IEntity? user = null)
         {
             if (storage.StoredEntities == null)
                 return false;
 
             var insertedBulbs = 0;
-            foreach (var ent in storage.StoredEntities)
+            var storagedEnts = storage.StoredEntities.ToArray();
+            foreach (var ent in storagedEnts)
             {
                 if (ent.TryGetComponent(out LightBulbComponent? bulb))
                 {
                     if (TryInsertBulb(bulb))
                         insertedBulbs++;
                 }
+            }
+
+            // show some message if success
+            if (insertedBulbs > 0 && user != null)
+            {
+                var msg = Loc.GetString("comp-light-replacer-refill-from-storage", ("light-replacer", Owner));
+                user.PopupMessage(msg);
             }
 
             return insertedBulbs > 0;
