76590fc9c52f59cbe03458f06398762828aae3ca
Alex Evgrashin <aevgrashin@yandex.ru>
2021-03-18 20:33:50 +0300

    More interactions

--- Content.Server/GameObjects/Components/Janitorial/LightReplacerComponent.cs
+++ Content.Server/GameObjects/Components/Janitorial/LightReplacerComponent.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using Content.Server.GameObjects.Components.Items.Storage;
 using Content.Server.GameObjects.Components.Power.ApcNetComponents.PowerReceiverUsers;
 using Content.Shared.GameObjects.EntitySystems.ActionBlocker;
 using Content.Shared.Interfaces.GameObjects.Components;
@@ -7,6 +8,7 @@ using Robust.Shared.GameObjects;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.ViewVariables;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,13 +20,13 @@ namespace Content.Server.GameObjects.Components.Janitorial
     ///     Device that allows user to quikly change bulbs in <see cref="PoweredLightComponent"/>
     /// </summary>
     [RegisterComponent]
-    public class LightReplacerComponent : Component, IAfterInteract
+    public class LightReplacerComponent : Component, IAfterInteract, IInteractUsing
     {
         public override string Name => "LightReplacer";
 
         [DataField("contents")] private List<LightReplacerEntity> _contents = new();
 
-        private IContainer _bulbsStorage = default!;
+        [ViewVariables] private IContainer _bulbsStorage = default!;
 
         public override void Initialize()
         {
@@ -48,14 +50,44 @@ namespace Content.Server.GameObjects.Components.Janitorial
             if (!ActionBlockerSystem.CanUse(eventArgs.User)) return false;
             if (!eventArgs.CanReach) return false;
 
-            // check if it's a powered light
-            if (eventArgs.Target == null || !eventArgs.Target.TryGetComponent(out PoweredLightComponent? fixture)) return false;
+            // behaviour will depends on target type
+            if (eventArgs.Target != null)
+            {
+                // replace broken light in fixture?
+                if (eventArgs.Target.TryGetComponent(out PoweredLightComponent? fixture))
+                    return TryReplaceBulb(fixture);
+                // add new bulb to light replacer container?
+                else if (eventArgs.Target.TryGetComponent(out LightBulbComponent? bulb))
+                    return TryInsertBulb(bulb);
+                // add bulbs from storage?
+                else if (eventArgs.Target.TryGetComponent(out ServerStorageComponent? storage))
+                    return TryInsertBulb(storage);
+            }
 
-            // check if light bulb is ok and doesn't need to be replaced
+            return false;
+        }
+
+        async Task<bool> IInteractUsing.InteractUsing(InteractUsingEventArgs eventArgs)
+        {
+            // standard interaction checks
+            if (!ActionBlockerSystem.CanInteract(eventArgs.User)) return false;
+
+            if (eventArgs.Using != null)
+            {
+                if (eventArgs.Using.TryGetComponent(out LightBulbComponent? bulb))
+                    return TryInsertBulb(bulb);
+            }
+
+            return false;
+        }
+
+        private bool TryReplaceBulb(PoweredLightComponent fixture)
+        {
+            // check if light bulb is broken or missing
             if (fixture.LightBulb != null && fixture.LightBulb.State == LightBulbState.Normal) return false;
 
             // try get first bulb of the same type as targeted light fixtutre
-            var bulb =_bulbsStorage.ContainedEntities.FirstOrDefault(
+            var bulb = _bulbsStorage.ContainedEntities.FirstOrDefault(
                 (e) => e.GetComponentOrNull<LightBulbComponent>()?.Type == fixture.BulbType);
 
             // try to remove it from storage
@@ -63,6 +95,32 @@ namespace Content.Server.GameObjects.Components.Janitorial
 
             // insert it into fixture
             return fixture.ReplacBulb(bulb);
+        }
+
+        private bool TryInsertBulb(LightBulbComponent bulb)
+        {
+            if (bulb.State != LightBulbState.Normal)
+                return false;
+
+            return _bulbsStorage.Insert(bulb.Owner);
+        }
+
+        private bool TryInsertBulb(ServerStorageComponent storage)
+        {
+            if (storage.StoredEntities == null)
+                return false;
+
+            var insertedBulbs = 0;
+            foreach (var ent in storage.StoredEntities)
+            {
+                if (ent.TryGetComponent(out LightBulbComponent? bulb))
+                {
+                    if (TryInsertBulb(bulb))
+                        insertedBulbs++;
+                }
+            }
+
+            return insertedBulbs > 0;
         }
 
         [Serializable]
