6f3f04b61e9a50a4f5b0f1c1c2a0d3b0adc0fb38
Vera Aguilera Puerto <zddm@outlook.es>
2021-03-21 21:11:38 +0100

    People who get creamed while they were already creamed get stunned again.

--- Content.Server/GameObjects/Components/Nutrition/CreamPiedComponent.cs
+++ Content.Server/GameObjects/Components/Nutrition/CreamPiedComponent.cs
@@ -22,6 +22,8 @@ namespace Content.Server.GameObjects.Components.Nutrition
             get => _creamPied;
             private set
             {
+                if (value == _creamPied) return;
+
                 _creamPied = value;
                 if (Owner.TryGetComponent(out AppearanceComponent? appearance))
                 {
@@ -51,7 +53,7 @@ namespace Content.Server.GameObjects.Components.Nutrition
 
         void IThrowCollide.HitBy(ThrowCollideEventArgs eventArgs)
         {
-            if (CreamPied || eventArgs.Thrown.Deleted || !eventArgs.Thrown.TryGetComponent(out CreamPieComponent? creamPie)) return;
+            if (eventArgs.Thrown.Deleted || !eventArgs.Thrown.TryGetComponent(out CreamPieComponent? creamPie)) return;
 
             CreamPied = true;
             Owner.PopupMessage(Loc.GetString("You have been creamed by {0:theName}!", eventArgs.Thrown));
