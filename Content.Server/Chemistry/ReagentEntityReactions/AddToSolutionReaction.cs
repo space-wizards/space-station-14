9baa51a598c03ce6cd05e81149d81160a43d4db6
Vera Aguilera Puerto <zddm@outlook.es>
2021-03-25 13:00:27 +0100

    Refactors reactions to be more POWERFUL and DATA-ORIENTED

---
+++ Content.Server/Chemistry/ReagentEntityReactions/AddToSolutionReaction.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Content.Server.GameObjects.Components.Chemistry;
+using Content.Shared.Chemistry;
+using JetBrains.Annotations;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+
+namespace Content.Server.Chemistry.ReagentEntityReactions
+{
+    [UsedImplicitly]
+    [DataDefinition]
+    public class AddToSolutionReaction : ReagentEntityReaction
+    {
+        [DataField("reagents", true)]
+        // ReSharper disable once CollectionNeverUpdated.Local
+        private readonly HashSet<string> _reagents = new ();
+
+        protected override void React(IEntity entity, ReagentPrototype reagent, ReagentUnit volume, Solution? source)
+        {
+            if (!entity.TryGetComponent(out SolutionContainerComponent? solutionContainer) || (_reagents.Count > 0 && !_reagents.Contains(reagent.ID))) return;
+
+            if(solutionContainer.TryAddReagent(reagent.ID, volume, out var accepted))
+                source?.RemoveReagent(reagent.ID, accepted);
+        }
+    }
+}
