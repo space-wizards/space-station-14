f4dd10372095cbbac50aa403fbe4db71ddea7698
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2021-03-31 15:03:48 -0700

    Fix PrototypeIdSerializer imports

--- Content.Server/GameObjects/Components/Engineering/SpawnAfterInteractComponent.cs
+++ Content.Server/GameObjects/Components/Engineering/SpawnAfterInteractComponent.cs
@@ -3,7 +3,7 @@ using Content.Shared.GameObjects;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.Manager.Attributes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Robust.Shared.ViewVariables;
 
 namespace Content.Server.GameObjects.Components.Engineering
