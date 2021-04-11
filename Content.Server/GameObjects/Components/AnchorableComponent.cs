24de969f54a40084efd361c05bc44256a235d896
Vera Aguilera Puerto <zddm@outlook.es>
2021-04-11 16:39:29 +0200

    Anchorable component now raises cancellable event on (un)anchor.

--- Content.Server/GameObjects/Components/AnchorableComponent.cs
+++ Content.Server/GameObjects/Components/AnchorableComponent.cs
@@ -83,6 +83,13 @@ namespace Content.Server.GameObjects.Components
             if (_physicsComponent == null)
                 return false;
 
+            var attempt = new AnchorAttemptMessage();
+
+            Owner.EntityManager.EventBus.RaiseLocalEvent(Owner.Uid, attempt);
+
+            if (attempt.Cancelled)
+                return false;
+
             _physicsComponent.BodyType = BodyType.Static;
 
             // Snap rotation to cardinal (multiple of 90)
@@ -120,6 +127,13 @@ namespace Content.Server.GameObjects.Components
             if (_physicsComponent == null)
                 return false;
 
+            var attempt = new UnanchorAttemptMessage();
+
+            Owner.EntityManager.EventBus.RaiseLocalEvent(Owner.Uid, attempt);
+
+            if (attempt.Cancelled)
+                return false;
+
             _physicsComponent.BodyType = BodyType.Dynamic;
 
             return true;
@@ -147,4 +161,7 @@ namespace Content.Server.GameObjects.Components
             return await TryToggleAnchor(eventArgs.User, eventArgs.Using);
         }
     }
+
+    public class AnchorAttemptMessage : CancellableEntityEventArgs { }
+    public class UnanchorAttemptMessage : CancellableEntityEventArgs { }
 }
