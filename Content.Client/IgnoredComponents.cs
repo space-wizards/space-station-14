75a8833b2cbe847aa1040b30e72b08025249a3a2
Visne <39844191+Visne@users.noreply.github.com>
2021-04-21 12:00:14 +0200

    Add AdvertiseComponent and add it to vending machines (#3756)
    * WIP AdvertiseComponent
    
    * Add AdvertiseComponent and add it to vending machines
    
    * Add snacks.yml
    
    * Capitalise C in cigarette machine
    
    * Update Content.Server/GameObjects/Components/AdvertiseComponent.cs
    
    Co-authored-by: Pieter-Jan Briers <pieterjan.briers@gmail.com>
    
    * Update Content.Server/GameObjects/Components/AdvertiseComponent.cs
    
    Co-authored-by: Pieter-Jan Briers <pieterjan.briers@gmail.com>
    
    * Fix most problems
    
    * Add localisation + exception for illegal prototype values
    
    * Add ads for smart fridge, discount dan's, youtool and mining
    
    * Oops
    
    * Fix style
    
    * Make dependencies local
    
    * Remove some ads, increase wait
    
    * Increase wait, allow full initial wait width
    
    * Fix error
    
    * Update sovietsoda.ftl
    
    * Update sovietsoda.ftl
    
    Co-authored-by: Pieter-Jan Briers <pieterjan.briers@gmail.com>
    Co-authored-by: metalgearsloth <comedian_vs_clown@hotmail.com>

--- Content.Client/IgnoredComponents.cs
+++ Content.Client/IgnoredComponents.cs
@@ -254,6 +254,7 @@ namespace Content.Client
             "SpawnAfterInteract",
             "DisassembleOnActivate",
             "ExplosionLaunched",
+            "Advertise",
         };
     }
 }
