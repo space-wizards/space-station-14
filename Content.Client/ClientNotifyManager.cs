2e68efe1c87e5916e7040247479c1c00cb2a7dda
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-05-02 01:33:01 +0200

    Remove IUserInterfaceManager.UIScale to fix UIScale bugs.

--- Content.Client/ClientNotifyManager.cs
+++ Content.Client/ClientNotifyManager.cs
@@ -104,7 +104,7 @@ namespace Content.Client
             label.Measure(Vector2.Infinity);
             var minimumSize = label.DesiredSize;
 
-            label.InitialPos = (coordinates.Position / _userInterfaceManager.UIScale) - minimumSize / 2;
+            label.InitialPos = (coordinates.Position / label.UIScale) - minimumSize / 2;
             LayoutContainer.SetPosition(label, label.InitialPos);
             _aliveLabels.Add(label);
         }
