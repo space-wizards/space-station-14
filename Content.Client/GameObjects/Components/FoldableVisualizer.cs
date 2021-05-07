5732c8b64e2e1b14f3e39af66391133dd3e97b24
Clément <topy72.mine@gmail.com>
2021-05-07 19:38:55 +0200

    Removed caching from visualizers

--- Content.Client/GameObjects/Components/FoldableVisualizer.cs
+++ Content.Client/GameObjects/Components/FoldableVisualizer.cs
@@ -32,33 +32,28 @@ namespace Content.Client.GameObjects.Components
         {
             base.OnChangeData(appearance);
 
-            // TODO : Stop caching components
-            // Do nothing if the object doesn't have a SpriteComponent
-            if (_sprite == null)
-                if (appearance.Owner.TryGetComponent(out ISpriteComponent? s))
-                {
-                    this._sprite = s;    // Keep a reference to the sprite for good measure
-                } else return;
+            if (!appearance.Owner.TryGetComponent(out SpriteComponent? sprite)) return;
 
             // Get appearance data, and check if the folded state has changed
             if (!appearance.TryGetData(FoldableVisuals.FoldedState, out bool folded)) return;
 
-            // Set all the layers state to [state]_folded if it exists, or back to the default [state]
-
             _isFolded = folded;
 
+
+            // Set all the layers state to [state]_folded if it exists, or back to the default [state]
+
             var i = 0;
-            foreach (var layer in _sprite.AllLayers)
+            foreach (var layer in sprite.AllLayers)
             {
                 var newState = folded
-                    ? _sprite.LayerGetState(i) + _foldedSuffix
-                    : _sprite.LayerGetState(i).ToString()?.Replace(_foldedSuffix ?? "", "");
+                    ? sprite.LayerGetState(i) + _foldedSuffix
+                    : sprite.LayerGetState(i).ToString()?.Replace(_foldedSuffix ?? "", "");
 
                 // Check if the state actually exists in the RSI and set it on the layer
-                var stateExists = _sprite.BaseRSI?.TryGetState(newState, out var actualState);
+                var stateExists = sprite.BaseRSI?.TryGetState(newState, out var actualState);
                 if (stateExists ?? false)
                 {
-                    _sprite.LayerSetState(i, newState);
+                    sprite.LayerSetState(i, newState);
                 }
 
                 i++;
--- Content.Client/GameObjects/Components/Strap/StrapVisualizer.cs
+++ Content.Client/GameObjects/Components/Strap/StrapVisualizer.cs
@@ -28,46 +28,36 @@ namespace Content.Client.GameObjects.Components.Strap
         [ViewVariables][DataField("buckledSuffix", required: false)]
         private string? _buckledSuffix = "_buckled";
 
-        // This array keeps the original layers' states of the sprite
-        private string?[]? _defaultStates;
-
-        // This keeps a reference to the entity's SpriteComponent to avoid fetching it each time
-        private ISpriteComponent? _sprite;
-
-        private bool? _isBuckled;
+        private bool _isBuckled = false;
 
 
         public override void OnChangeData(AppearanceComponent appearance)
         {
             base.OnChangeData(appearance);
 
-            // Do nothing if the object doesn't have a SpriteComponent
-            if (_sprite == null)
-                if (appearance.Owner.TryGetComponent(out ISpriteComponent? s))
-                {
-                    this._sprite = s;    // Keep a reference to the sprite for good measure
-                } else return;
+            if (!appearance.Owner.TryGetComponent(out SpriteComponent? sprite)) return;
 
             // Get appearance data, and check if the buckled state has changed
-            if (!appearance.TryGetData(StrapVisuals.BuckledState, out bool folded)) return;
-            if (_isBuckled != null && _isBuckled == folded) return;
+            if (!appearance.TryGetData(StrapVisuals.BuckledState, out bool buckled)) return;
+            if (_isBuckled == buckled) return;
+
+            _isBuckled = buckled;
+
 
             // Set all the layers state to [state]_buckled if it exists, or back to the default [state]
 
-            _isBuckled = folded;
-
             var i = 0;
-            foreach (var layer in _sprite.AllLayers)
+            foreach (var layer in sprite.AllLayers)
             {
-                var newState = folded
-                    ? _sprite.LayerGetState(i) + _buckledSuffix
-                    : _sprite.LayerGetState(i).ToString()?.Replace(_buckledSuffix ?? "", "");
+                var newState = buckled
+                    ? sprite.LayerGetState(i) + _buckledSuffix
+                    : sprite.LayerGetState(i).ToString()?.Replace(_buckledSuffix ?? "", "");
 
                 // Check if the state actually exists in the RSI and set it on the layer
-                var stateExists = _sprite.BaseRSI?.TryGetState(newState, out var actualState);
+                var stateExists = sprite.BaseRSI?.TryGetState(newState, out var actualState);
                 if (stateExists ?? false)
                 {
-                    _sprite.LayerSetState(i, newState);
+                    sprite.LayerSetState(i, newState);
                 }
 
                 i++;
