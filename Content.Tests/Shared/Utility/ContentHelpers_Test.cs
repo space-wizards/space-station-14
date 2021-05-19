5f1e44d056def939f2dfb83547c888ec5199d430
Ygg One <y.laughing.man.y@gmail.com>
2021-05-19 20:37:13 +0200

    Fix layers displaying in cable stacks
    Fixed stackVisualizer to use another round method

--- Content.Tests/Shared/Utility/ContentHelpers_Test.cs
+++ Content.Tests/Shared/Utility/ContentHelpers_Test.cs
@@ -82,6 +82,58 @@ namespace Content.Tests.Shared.Utility
                 (320, 6, 5, 5),
             };
 
+        public static readonly IEnumerable<(double val, double max, int levels, int expected)> TestEqLvls = 
+            new (double, double, int, int)[]
+            {
+                // Testing odd max on even levels
+                (-10, 5, 2, 0),
+                (0, 5, 2, 0),
+                (1, 5, 2, 0),
+                (2, 5, 2, 0),
+                (3, 5, 2, 1),
+                (4, 5, 2, 1),
+                (5, 5, 2, 1),
+ 
+                // Testing even max on odd levels
+                (0, 6, 3, 0),
+                (1, 6, 3, 0),
+                (2, 6, 3, 1),
+                (3, 6, 3, 1),
+                (4, 6, 3, 2),
+                (5, 6, 3, 2),
+                (6, 6, 3, 2),
+                
+                // Testing even max on even levels
+                (0, 4, 2, 0),
+                (1, 4, 2, 0),
+                (2, 4, 2, 1),
+                (3, 4, 2, 1),
+                (4, 4, 2, 1),
+                
+                // Testing odd max on odd levels
+                (0, 5, 3, 0),
+                (1, 5, 3, 0),
+                (2, 5, 3, 1),
+                (3, 5, 3, 1),
+                (4, 5, 3, 2),
+                // Largers odd max on odd levels
+                (0, 7, 3, 0),
+                (1, 7, 3, 0),
+                (2, 7, 3, 0),
+                (3, 7, 3, 1),
+                (4, 7, 3, 1),
+                (5, 7, 3, 2),
+                (6, 7, 3, 2),
+                (7, 7, 3, 2),
+                
+                // Testing edge cases
+                (0.1, 6, 5, 0),
+                (-32, 6, 5, 0),
+                (2.4, 6, 5, 1),
+                (2.5, 6, 5, 2),
+                (320, 6, 5, 4),
+            };
+        
         [Parallelizable]
         [Test]
         public void Test([ValueSource(nameof(TestData))] (double val, double max, int levels, int expected) data)
@@ -96,6 +148,14 @@ namespace Content.Tests.Shared.Utility
         {
             (double val, double max, int size, int expected) = data;
             Assert.That(ContentHelpers.RoundToNearestLevels(val, max, size), Is.EqualTo(expected));
+        }
+
+        [Parallelizable]
+        [Test]
+        public void TestEqual([ValueSource(nameof(TestEqLvls))] (double val, double max, int size, int expected) data)
+        {
+            (double val, double max, int size, int expected) = data;
+            Assert.That(ContentHelpers.RoundToEqualLevels(val, max, size), Is.EqualTo(expected));
         }
     }
 }
