62044218085aebd927087a6daecba1c0f0c3ab46
20kdc <asdd2808@gmail.com>
2021-05-25 17:25:52 +0100

    Refactor/fix client-side of solar control computer (introduce ComputerBoundUserInterface & fix bugs)

--- Content.Client/GameObjects/Components/Power/SolarControlConsoleBoundUserInterface.cs
+++ Content.Client/GameObjects/Components/Power/SolarControlConsoleBoundUserInterface.cs
@@ -8,50 +8,109 @@ using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.CustomControls;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
+using Robust.Shared.Log;
+using Robust.Shared.Localization;
 using Robust.Shared.Maths;
 using Robust.Shared.Timing;
 
 namespace Content.Client.GameObjects.Components.Power
 {
     [UsedImplicitly]
-    public class SolarControlConsoleBoundUserInterface : BoundUserInterface
+    public class SolarControlConsoleBoundUserInterface : ComputerBoundUserInterface<SolarControlWindow, SolarControlConsoleBoundInterfaceState>
     {
-        [Dependency] private readonly IGameTiming _gameTiming = default!;
+        public SolarControlConsoleBoundUserInterface(ClientUserInterfaceComponent owner, object uiKey) : base(owner, uiKey) {}
+    }
 
-        private SolarControlWindow? _window;
+    public sealed class SolarControlWindow : SS14Window, IComputerWindow<SolarControlConsoleBoundInterfaceState>
+    {
+        public readonly Label OutputPower;
+        public readonly Label SunAngle;
+
+        private readonly SolarControlNotARadar _notARadar;
+
+        public readonly LineEdit PanelRotation;
+        public readonly LineEdit PanelVelocity;
+
         private SolarControlConsoleBoundInterfaceState _lastState = new(0, 0, 0, 0);
 
-        protected override void Open()
+        public SolarControlWindow()
         {
-            base.Open();
+            Title = Loc.GetString("solar-control-window-title");
 
-            _window = new SolarControlWindow(_gameTiming);
-            _window.OnClose += Close;
-            _window.PanelRotation.OnTextEntered += (text) => {
+            var rows = new GridContainer();
+            rows.Columns = 2;
+
+            // little secret: the reason I put the values
+            // in the first column is because otherwise the UI
+            // layouter autoresizes the window to be too small
+            rows.AddChild(new Label {Text = Loc.GetString("solar-control-window-output-power")});
+            rows.AddChild(new Label {Text = ""});
+
+            rows.AddChild(OutputPower = new Label {Text = "?"});
+            rows.AddChild(new Label {Text = Loc.GetString("solar-control-window-watts")});
+
+            rows.AddChild(new Label {Text = Loc.GetString("solar-control-window-sun-angle")});
+            rows.AddChild(new Label {Text = ""});
+
+            rows.AddChild(SunAngle = new Label {Text = "?"});
+            rows.AddChild(new Label {Text = Loc.GetString("solar-control-window-degrees")});
+
+            rows.AddChild(new Label {Text = Loc.GetString("solar-control-window-panel-angle")});
+            rows.AddChild(new Label {Text = ""});
+
+            rows.AddChild(PanelRotation = new LineEdit());
+            rows.AddChild(new Label {Text = Loc.GetString("solar-control-window-degrees")});
+
+            rows.AddChild(new Label {Text = Loc.GetString("solar-control-window-panel-angular-velocity")});
+            rows.AddChild(new Label {Text = ""});
+
+            rows.AddChild(PanelVelocity = new LineEdit());
+            rows.AddChild(new Label {Text = Loc.GetString("solar-control-window-degrees-per-minute")});
+
+            rows.AddChild(new Label {Text = Loc.GetString("solar-control-window-press-enter-to-confirm")});
+            rows.AddChild(new Label {Text = ""});
+
+            PanelRotation.HorizontalExpand = true;
+            PanelVelocity.HorizontalExpand = true;
+
+            _notARadar = new SolarControlNotARadar();
+
+            var outerColumns = new HBoxContainer();
+            outerColumns.AddChild(rows);
+            outerColumns.AddChild(_notARadar);
+            Contents.AddChild(outerColumns);
+            Resizable = false;
+        }
+
+        public void SetupComputerWindow(ComputerBoundUserInterfaceBase cb)
+        {
+            PanelRotation.OnTextEntered += (text) => {
                 double value;
                 if (double.TryParse(text.Text, out value))
                 {
                     SolarControlConsoleAdjustMessage msg = new SolarControlConsoleAdjustMessage();
                     msg.Rotation = Angle.FromDegrees(value);
                     msg.AngularVelocity = _lastState.AngularVelocity;
-                    SendMessage(msg);
+                    cb.SendMessage(msg);
+                    // Predict this...
+                    _lastState.Rotation = msg.Rotation;
+                    _notARadar.UpdateState(_lastState);
                 }
             };
-            _window.PanelVelocity.OnTextEntered += (text) => {
+            PanelVelocity.OnTextEntered += (text) => {
                 double value;
                 if (double.TryParse(text.Text, out value))
                 {
                     SolarControlConsoleAdjustMessage msg = new SolarControlConsoleAdjustMessage();
-                    msg.Rotation = _lastState.Rotation;
+                    msg.Rotation = _notARadar.PredictedPanelRotation;
                     msg.AngularVelocity = Angle.FromDegrees(value / 60);
-                    SendMessage(msg);
+                    cb.SendMessage(msg);
+                    // Predict this...
+                    _lastState.Rotation = _notARadar.PredictedPanelRotation;
+                    _lastState.AngularVelocity = msg.AngularVelocity;
+                    _notARadar.UpdateState(_lastState);
                 }
             };
-            _window.OpenCentered();
-        }
-
-        public SolarControlConsoleBoundUserInterface(ClientUserInterfaceComponent owner, object uiKey) : base(owner, uiKey)
-        {
         }
 
         private string FormatAngle(Angle d)
@@ -69,111 +128,33 @@ namespace Content.Client.GameObjects.Components.Power
             }
         }
 
-        protected override void UpdateState(BoundUserInterfaceState state)
+        public void UpdateState(SolarControlConsoleBoundInterfaceState scc)
         {
-            base.UpdateState(state);
-
-            if (_window == null)
-            {
-                return;
-            }
-
-            SolarControlConsoleBoundInterfaceState scc = (SolarControlConsoleBoundInterfaceState) state;
             _lastState = scc;
-            _window.NotARadar.UpdateState(scc);
-            _window.OutputPower.Text = ((int) MathF.Floor(scc.OutputPower)).ToString();
-            _window.SunAngle.Text = FormatAngle(scc.TowardsSun);
-            UpdateField(_window.PanelRotation, FormatAngle(scc.Rotation));
-            UpdateField(_window.PanelVelocity, FormatAngle(scc.AngularVelocity * 60));
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-
-            if (disposing)
-            {
-                _window?.Dispose();
-            }
-        }
-
-        private sealed class SolarControlWindow : SS14Window
-        {
-            public readonly Label OutputPower;
-            public readonly Label SunAngle;
-
-            public readonly SolarControlNotARadar NotARadar;
-
-            public readonly LineEdit PanelRotation;
-            public readonly LineEdit PanelVelocity;
-
-            public SolarControlWindow(IGameTiming igt)
-            {
-                Title = "Solar Control Window";
-
-                var rows = new GridContainer();
-                rows.Columns = 2;
-
-                // little secret: the reason I put the values
-                // in the first column is because otherwise the UI
-                // layouter autoresizes the window to be too small
-                rows.AddChild(new Label {Text = "Output Power:"});
-                rows.AddChild(new Label {Text = ""});
-
-                rows.AddChild(OutputPower = new Label {Text = "?"});
-                rows.AddChild(new Label {Text = "W"});
-
-                rows.AddChild(new Label {Text = "Sun Angle:"});
-                rows.AddChild(new Label {Text = ""});
-
-                rows.AddChild(SunAngle = new Label {Text = "?"});
-                rows.AddChild(new Label {Text = "°"});
-
-                rows.AddChild(new Label {Text = "Panel Angle:"});
-                rows.AddChild(new Label {Text = ""});
-
-                rows.AddChild(PanelRotation = new LineEdit());
-                rows.AddChild(new Label {Text = "°"});
-
-                rows.AddChild(new Label {Text = "Panel Angular Velocity:"});
-                rows.AddChild(new Label {Text = ""});
-
-                rows.AddChild(PanelVelocity = new LineEdit());
-                rows.AddChild(new Label {Text = "°/min."});
-
-                rows.AddChild(new Label {Text = "Press Enter to confirm."});
-                rows.AddChild(new Label {Text = ""});
-
-                PanelRotation.HorizontalExpand = true;
-                PanelVelocity.HorizontalExpand = true;
-
-                NotARadar = new SolarControlNotARadar(igt);
-
-                var outerColumns = new HBoxContainer();
-                outerColumns.AddChild(rows);
-                outerColumns.AddChild(NotARadar);
-                Contents.AddChild(outerColumns);
-                Resizable = false;
-            }
+            _notARadar.UpdateState(scc);
+            OutputPower.Text = ((int) MathF.Floor(scc.OutputPower)).ToString();
+            SunAngle.Text = FormatAngle(scc.TowardsSun);
+            UpdateField(PanelRotation, FormatAngle(scc.Rotation));
+            UpdateField(PanelVelocity, FormatAngle(scc.AngularVelocity * 60));
         }
 
         private sealed class SolarControlNotARadar : Control
         {
-            // IoC doesn't apply here, so it's propagated from the parent class.
             // This is used for client-side prediction of the panel rotation.
             // This makes the display feel a lot smoother.
-            private IGameTiming _gameTiming;
+            private IGameTiming _gameTiming = IoCManager.Resolve<IGameTiming>();
 
             private SolarControlConsoleBoundInterfaceState _lastState = new(0, 0, 0, 0);
 
             private TimeSpan _lastStateTime = TimeSpan.Zero;
 
-            public const int SizeFull = 290;
-            public const int RadiusCircle = 140;
+            public const int StandardSizeFull = 290;
+            public const int StandardRadiusCircle = 140;
+            public int SizeFull => (int) (StandardSizeFull * UIScale);
+            public int RadiusCircle => (int) (StandardRadiusCircle * UIScale);
 
-            public SolarControlNotARadar(IGameTiming igt)
+            public SolarControlNotARadar()
             {
-                _gameTiming = igt;
                 MinSize = (SizeFull, SizeFull);
             }
 
@@ -182,6 +163,8 @@ namespace Content.Client.GameObjects.Components.Power
                 _lastState = ls;
                 _lastStateTime = _gameTiming.CurTime;
             }
+
+            public Angle PredictedPanelRotation => _lastState.Rotation + (_lastState.AngularVelocity * ((_gameTiming.CurTime - _lastStateTime).TotalSeconds));
 
             protected override void Draw(DrawingHandleScreen handle)
             {
@@ -211,15 +194,17 @@ namespace Content.Client.GameObjects.Components.Power
 
                 // The rotations need to be adjusted because Y is inverted in Robust (like BYOND)
                 Vector2 rotMul = (1, -1);
+                // Hotfix corrections I don't understand
+                Angle rotOfs = new Angle(Math.PI * -0.5);
 
-                Angle predictedPanelRotation = _lastState.Rotation + (_lastState.AngularVelocity * ((_gameTiming.CurTime - _lastStateTime).TotalSeconds));
+                Angle predictedPanelRotation = PredictedPanelRotation;
 
-                var extent = predictedPanelRotation.ToVec() * rotMul * RadiusCircle;
+                var extent = (predictedPanelRotation + rotOfs).ToVec() * rotMul * RadiusCircle;
                 Vector2 extentOrtho = (extent.Y, -extent.X);
                 handle.DrawLine((point, point) - extentOrtho, (point, point) + extentOrtho, Color.White);
                 handle.DrawLine((point, point) + (extent / panelExtentCutback), (point, point) + extent - (extent / panelExtentCutback), Color.DarkGray);
 
-                var sunExtent = _lastState.TowardsSun.ToVec() * rotMul * RadiusCircle;
+                var sunExtent = (_lastState.TowardsSun + rotOfs).ToVec() * rotMul * RadiusCircle;
                 handle.DrawLine((point, point) + sunExtent, (point, point), Color.Yellow);
             }
         }
