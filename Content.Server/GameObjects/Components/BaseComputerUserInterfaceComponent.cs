e35326d6b010e307f01dd480485bf78b8d1f437f
20kdc <asdd2808@gmail.com>
2021-05-25 18:05:32 +0100

    Refactor server side of solar control computer (introduce BaseComputerUserInterfaceComponent)

---
+++ Content.Server/GameObjects/Components/BaseComputerUserInterfaceComponent.cs
@@ -0,0 +1,111 @@
+using Content.Server.GameObjects.Components.Construction;
+using Content.Server.GameObjects.Components.Power.ApcNetComponents;
+using Content.Server.Utility;
+using Content.Shared.GameObjects.Components;
+using Content.Shared.Interfaces.GameObjects.Components;
+using Robust.Server.GameObjects;
+using Robust.Shared.Containers;
+using Robust.Shared.GameObjects;
+using Robust.Shared.ViewVariables;
+
+namespace Content.Server.GameObjects.Components
+{
+    /// <summary>
+    /// This component is used as a base class for classes like SolarControlConsoleComponent.
+    /// These components operate the server-side logic for the "primary UI" of a computer.
+    /// That means showing the UI when a user activates it, for example.
+    /// </summary>
+    [ComponentReference(typeof(IActivate))]
+    public abstract class BaseComputerUserInterfaceComponent : Component, IActivate
+    {
+        // { get; private set; } doesn't really express this properly.
+        protected readonly object UserInterfaceKey;
+
+        [ViewVariables] protected BoundUserInterface? UserInterface => Owner.GetUIOrNull(UserInterfaceKey);
+        [ViewVariables] public bool Powered => !Owner.TryGetComponent(out PowerReceiverComponent? receiver) || receiver.Powered;
+
+        public BaseComputerUserInterfaceComponent(object key)
+        {
+            UserInterfaceKey = key;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            if (UserInterface != null)
+                UserInterface.OnReceiveMessage += OnReceiveUnfilteredUserInterfaceMessage;
+
+            Owner.EnsureComponent<PowerReceiverComponent>();
+        }
+
+        /// <summary>
+        /// Override this to handle messages from the UI before filtering them.
+        /// Calling base is necessary if you want this class to have any meaning.
+        /// </summary>
+        protected void OnReceiveUnfilteredUserInterfaceMessage(ServerBoundUserInterfaceMessage obj)
+        {
+            // For now this is the only test.
+            // Further "anti-cheats" would be put here or at some other level.
+            if (!Powered)
+                return;
+            OnReceiveUserInterfaceMessage(obj);
+        }
+
+        /// <summary>
+        /// Override this to handle messages from the UI.
+        /// Calling base is unnecessary.
+        /// These messages will automatically be blocked if the user shouldn't be able to access this computer, or if the computer has lost power.
+        /// </summary>
+        protected virtual void OnReceiveUserInterfaceMessage(ServerBoundUserInterfaceMessage obj)
+        {
+            // Nothing!
+        }
+
+        public override void HandleMessage(ComponentMessage message, IComponent? component)
+        {
+            base.HandleMessage(message, component);
+            switch (message)
+            {
+                case PowerChangedMessage powerChanged:
+                    PowerReceiverOnOnPowerStateChanged(powerChanged);
+                    break;
+            }
+        }
+
+        private void PowerReceiverOnOnPowerStateChanged(PowerChangedMessage e)
+        {
+            if (!e.Powered)
+            {
+                // We need to kick off users who are using it when it loses power.
+                UserInterface?.CloseAll();
+                // Now alert subclass.
+                ComputerLostPower();
+            }
+        }
+
+        /// <summary>
+        /// Override this if you want the computer to do something when it loses power (i.e. reset state)
+        /// All UIs should have been closed by the time this is called.
+        /// Calling base is unnecessary.
+        /// </summary>
+        public virtual void ComputerLostPower()
+        {
+        }
+
+        void IActivate.Activate(ActivateEventArgs eventArgs)
+        {
+            if (!eventArgs.User.TryGetComponent(out ActorComponent? actor))
+            {
+                return;
+            }
+
+            if (!Powered)
+            {
+                return;
+            }
+
+            UserInterface?.Open(actor.PlayerSession);
+        }
+    }
+}
--- Content.Server/GameObjects/Components/Power/PowerNetComponents/SolarControlConsoleComponent.cs
+++ Content.Server/GameObjects/Components/Power/PowerNetComponents/SolarControlConsoleComponent.cs
@@ -1,4 +1,4 @@
-﻿#nullable enable
+#nullable enable
 using Content.Server.GameObjects.Components.Power.ApcNetComponents;
 using Content.Server.GameObjects.EntitySystems;
 using Content.Server.Utility;
@@ -13,25 +13,19 @@ namespace Content.Server.GameObjects.Components.Power.PowerNetComponents
 {
     [RegisterComponent]
     [ComponentReference(typeof(IActivate))]
-    public class SolarControlConsoleComponent : SharedSolarControlConsoleComponent, IActivate
+    public class SolarControlConsoleComponent : BaseComputerUserInterfaceComponent
     {
+        public override string Name => "SolarControlConsole";
+
         [Dependency] private readonly IEntitySystemManager _entitySystemManager = default!;
 
         private PowerSolarSystem _powerSolarSystem = default!;
-        private bool Powered => !Owner.TryGetComponent(out PowerReceiverComponent? receiver) || receiver.Powered;
 
-        [ViewVariables] private BoundUserInterface? UserInterface => Owner.GetUIOrNull(SolarControlConsoleUiKey.Key);
+        public SolarControlConsoleComponent() : base(SolarControlConsoleUiKey.Key) { }
 
         public override void Initialize()
         {
             base.Initialize();
-
-            if (UserInterface != null)
-            {
-                UserInterface.OnReceiveMessage += UserInterfaceOnReceiveMessage;
-            }
-
-            Owner.EnsureComponent<PowerReceiverComponent>();
             _powerSolarSystem = _entitySystemManager.GetEntitySystem<PowerSolarSystem>();
         }
 
@@ -40,7 +34,7 @@ namespace Content.Server.GameObjects.Components.Power.PowerNetComponents
             UserInterface?.SetState(new SolarControlConsoleBoundInterfaceState(_powerSolarSystem.TargetPanelRotation, _powerSolarSystem.TargetPanelVelocity, _powerSolarSystem.TotalPanelPower, _powerSolarSystem.TowardsSun));
         }
 
-        private void UserInterfaceOnReceiveMessage(ServerBoundUserInterfaceMessage obj)
+        protected override void OnReceiveUserInterfaceMessage(ServerBoundUserInterfaceMessage obj)
         {
             switch (obj.Message)
             {
@@ -55,23 +49,6 @@ namespace Content.Server.GameObjects.Components.Power.PowerNetComponents
                     }
                     break;
             }
-        }
-
-        void IActivate.Activate(ActivateEventArgs eventArgs)
-        {
-            if (!eventArgs.User.TryGetComponent(out ActorComponent? actor))
-            {
-                return;
-            }
-
-            if (!Powered)
-            {
-                return;
-            }
-
-            // always update the UI immediately before opening, just in case
-            UpdateUIState();
-            UserInterface?.Open(actor.PlayerSession);
         }
     }
 }
