6121c034792bfd5fca60342f33ffcd90731e6890
mirrorcult <notzombiedude@gmail.com>
2021-05-25 19:06:37 -0700

    unnecessary usings

--- Content.Server/GameObjects/Components/Chemistry/SolutionTransferComponent.cs
+++ Content.Server/GameObjects/Components/Chemistry/SolutionTransferComponent.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Threading.Tasks;
 using Content.Server.Eui;
-using Content.Server.GameObjects.Components.Mobs;
 using Content.Shared.Chemistry;
 using Content.Shared.GameObjects.Components.Chemistry;
 using Content.Shared.GameObjects.EntitySystems.ActionBlocker;
@@ -10,7 +9,6 @@ using Content.Shared.GameObjects.Verbs;
 using Content.Shared.Interfaces;
 using Content.Shared.Interfaces.GameObjects.Components;
 using Robust.Server.GameObjects;
-using Robust.Server.Player;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Localization;
