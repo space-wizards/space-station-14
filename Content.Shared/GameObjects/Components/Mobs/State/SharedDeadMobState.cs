ddcb4ad204f80594aa4a9a461fccf3fe13e32c74
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-05-31 22:02:32 +1000

    Make dead bodies use CollisionWake

--- Content.Shared/GameObjects/Components/Mobs/State/SharedDeadMobState.cs
+++ Content.Shared/GameObjects/Components/Mobs/State/SharedDeadMobState.cs
@@ -1,9 +1,27 @@
-﻿#nullable enable
+﻿using Robust.Shared.GameObjects;
+
+#nullable enable
 namespace Content.Shared.GameObjects.Components.Mobs.State
 {
     public abstract class SharedDeadMobState : BaseMobState
     {
         protected override DamageState DamageState => DamageState.Dead;
+
+        public override void EnterState(IEntity entity)
+        {
+            base.EnterState(entity);
+            var wake = entity.EnsureComponent<CollisionWakeComponent>();
+            wake.Enabled = true;
+        }
+
+        public override void ExitState(IEntity entity)
+        {
+            base.ExitState(entity);
+            if (entity.HasComponent<CollisionWakeComponent>())
+            {
+                entity.RemoveComponent<CollisionWakeComponent>();
+            }
+        }
 
         public override bool CanInteract()
         {
