20243052cb7f0d54352b98cf09e3c2dc21e33612
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-06-01 11:48:24 +0200

    NodeGroups are dirtied when one of the nodes' snapgrid position changes. Adds method to add behavior to a node when its snapgrid position changes.

--- Content.Server/GameObjects/EntitySystems/NodeGroupSystem.cs
+++ Content.Server/GameObjects/EntitySystems/NodeGroupSystem.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Content.Server.GameObjects.Components.NodeContainer;
 using Content.Server.GameObjects.Components.NodeContainer.NodeGroups;
 using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
@@ -9,6 +10,22 @@ namespace Content.Server.GameObjects.EntitySystems
     public class NodeGroupSystem : EntitySystem
     {
         private readonly HashSet<INodeGroup> _dirtyNodeGroups = new();
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<NodeContainerComponent, SnapGridPositionChangedEvent>(OnSnapGridPositionChanged);
+        }
+
+        private void OnSnapGridPositionChanged(EntityUid uid, NodeContainerComponent component, SnapGridPositionChangedEvent args)
+        {
+            foreach (var node in component.Nodes.Values)
+            {
+                node.OnSnapGridMove();
+                AddDirtyNodeGroup(node.NodeGroup);
+            }
+        }
 
         public void AddDirtyNodeGroup(INodeGroup nodeGroup)
         {
