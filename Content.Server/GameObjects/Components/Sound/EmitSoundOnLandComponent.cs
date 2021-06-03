943c6a0b3e983f39426283f4e305ea796233d807
Galactic Chimp <GalacticChimpanzee@gmail.com>
2021-06-03 13:34:19 +0200

    #3935 - extracted BaseEmitSoundComponent

--- Content.Server/GameObjects/Components/Sound/EmitSoundOnLandComponent.cs
+++ Content.Server/GameObjects/Components/Sound/EmitSoundOnLandComponent.cs
@@ -1,9 +1,5 @@
 using Content.Shared.Interfaces.GameObjects.Components;
-using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
-using Robust.Shared.Player;
-using Robust.Shared.Serialization.Manager.Attributes;
-using Robust.Shared.ViewVariables;
 
 namespace Content.Server.GameObjects.Components.Sound
 {
@@ -11,20 +7,15 @@ namespace Content.Server.GameObjects.Components.Sound
     /// Simple sound emitter that emits sound on land
     /// </summary>
     [RegisterComponent]
-    public class EmitSoundOnLandComponent : Component, ILand
+    public class EmitSoundOnLandComponent : BaseEmitSoundComponent, ILand
     {
         /// <inheritdoc />
         ///
         public override string Name => "EmitSoundOnLand";
 
-        [ViewVariables(VVAccess.ReadWrite)] [DataField("sound")] public string? _soundName;
-
         void ILand.Land(LandEventArgs eventArgs)
         {
-            if (!string.IsNullOrWhiteSpace(_soundName))
-            {
-                SoundSystem.Play(Filter.Pvs(Owner), _soundName, Owner, AudioParams.Default.WithVolume(-2f));
-            }
+            PlaySoundBasedOnMode();
         }
     }
 }
