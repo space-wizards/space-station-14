d8539927f73e2c40390e14137038a0e562fcf9b0
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-06-09 19:04:26 +0200

    Re-organize all of Content.Shared

---
+++ Content.Shared/Lathe/LatheVisualState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.GameObjects.Components.Power
+{
+    [Serializable, NetSerializable]
+    public enum LatheVisualState
+    {
+        Idle,
+        Producing,
+        InsertingMetal,
+        InsertingGlass,
+        InsertingGold,
+        InsertingPlasma,
+        InsertingPlastic
+    }
+}
