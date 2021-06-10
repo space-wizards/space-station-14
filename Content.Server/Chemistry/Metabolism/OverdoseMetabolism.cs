19cd1dfcf206c51baf762f0940ab1913aeaecf8c
scaly-chimp <scalychimp@gmail.com>
2021-06-10 19:34:52 +0800

    adds an overdose metabolism with sensible default settings

---
+++ Content.Server/Chemistry/Metabolism/OverdoseMetabolism.cs
@@ -0,0 +1,64 @@
+using Content.Server.GameObjects.Components.Nutrition;
+using Content.Shared.Chemistry;
+using Content.Shared.Interfaces.Chemistry;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Content.Shared.Damage;
+using Content.Shared.GameObjects.Components.Damage;
+using Content.Server.GameObjects.Components.Chemistry;
+
+namespace Content.Server.Chemistry.Metabolism
+{
+    /// <summary>
+    /// Deals a certain amount of damage once a certain amount of reagent is ingested at once.
+    /// </summary>
+    [DataDefinition]
+    public class OverdoseMetabolism : IMetabolizable
+    {
+        /// <summary>
+        /// How much of the reagent should be metabolized each sec.
+        /// </summary> 
+        [DataField("rate")]
+        public ReagentUnit MetabolismRate { get; set; } = ReagentUnit.New(1);
+
+        /// <summary>
+        /// How much damage is changed when the overdose happenes, allowing for possible positive overdose?
+        /// </summary>
+        [DataField("healthChange")]
+        public float HealthChange { get; set; } = 500.0f;
+
+        /// <summary>
+        /// Class of damage changed, Brute, Burn, Toxin, Airloss.
+        /// </summary> 
+        [DataField("damageClass")]
+        public DamageClass DamageType { get; set; } = DamageClass.Brute;
+
+        [DataField("overdoseReagent")]
+        public string OverdoseReagent { get; set; } = "Water";
+
+        [DataField("overdoseAmount")]
+        public ReagentUnit OverdoseAmount { get; set; } = ReagentUnit.New(30.0f);
+
+        /// <summary>
+        /// Remove reagent at set rate, changes damage if a an overdose happens.
+        /// </summary>
+        /// <param name="solutionEntity"></param>
+        /// <param name="reagentId"></param>
+        /// <param name="tickTime"></param>
+        /// <returns></returns>
+        ReagentUnit IMetabolizable.Metabolize(IEntity solutionEntity, string reagentId, float tickTime)
+        {
+            if (solutionEntity.TryGetComponent(out IDamageableComponent? health) && solutionEntity.TryGetComponent(out SolutionContainerComponent? solutionComponent))
+            {
+
+                solutionComponent.Solution.ContainsReagent(OverdoseReagent, out ReagentUnit chemicalAmount);
+                    
+                if (chemicalAmount >= OverdoseAmount)
+                {
+                    health.ChangeDamage(DamageType, (int) HealthChange, true);
+                }
+            }
+            return MetabolismRate;
+        }
+    }
+}
