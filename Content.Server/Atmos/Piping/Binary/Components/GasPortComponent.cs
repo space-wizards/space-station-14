5644f4893244131e711347b02ecffabb128f1a9f
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-06-19 11:12:24 +0200

    Fix port system doing useless thing

--- Content.Server/Atmos/Piping/Binary/Components/GasPortComponent.cs
+++ Content.Server/Atmos/Piping/Binary/Components/GasPortComponent.cs
@@ -1,9 +1,8 @@
-using Content.Server.Atmos;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.ViewVariables;
 
-namespace Content.Server.GameObjects.Components.Atmos.Piping.Binary
+namespace Content.Server.Atmos.Piping.Binary.Components
 {
     [RegisterComponent]
     public class GasPortComponent : Component
@@ -12,11 +11,7 @@ namespace Content.Server.GameObjects.Components.Atmos.Piping.Binary
 
         [ViewVariables(VVAccess.ReadWrite)]
         [DataField("pipe")]
-        public string PipeName { get; set; } = "pipe";
-
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("connected")]
-        public string ConnectedName { get; set; } = "connected";
+        public string PipeName { get; set; } = "connected";
 
         [ViewVariables(VVAccess.ReadOnly)]
         public GasMixture Buffer { get; } = new();
