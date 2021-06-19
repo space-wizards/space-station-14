840180f1979a16155bdca4abdb97bb944efc9635
Galactic Chimp <GalacticChimpanzee@gmail.com>
2021-06-19 11:42:56 +0200

    #1607 partial progress

--- Content.Server/AI/Utils/Visibility.cs
+++ Content.Server/AI/Utils/Visibility.cs
@@ -58,7 +58,7 @@ namespace Content.Server.AI.Utils
         public static IEnumerable<IEntity> GetEntitiesInRange(EntityCoordinates grid, Type component, float range)
         {
             var entityManager = IoCManager.Resolve<IEntityManager>();
-            foreach (var entity in entityManager.ComponentManager.h GetEntities(new TypeEntityQuery(component)))
+            foreach (var entity in entityManager.GetEntities().Where(e => entityManager.ComponentManager.HasComponent(e.Uid, component)))
             {
                 if (entity.Transform.Coordinates.GetGridId(entityManager) != grid.GetGridId(entityManager))
                 {
