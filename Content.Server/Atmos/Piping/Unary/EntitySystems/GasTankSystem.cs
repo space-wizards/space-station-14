8fbee32ee133897a54214da25b7427dd1dcf5de1
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-06-23 11:11:42 +0200

    fix shitcode

--- Content.Server/Atmos/Piping/Unary/EntitySystems/GasTankSystem.cs
+++ Content.Server/Atmos/Piping/Unary/EntitySystems/GasTankSystem.cs
@@ -1,4 +1,5 @@
 using Content.Server.Atmos.Components;
+using Content.Server.Atmos.EntitySystems;
 using Content.Server.GameObjects.Components.NodeContainer.Nodes;
 using Content.Server.NodeContainer;
 using JetBrains.Annotations;
@@ -9,6 +10,9 @@ namespace Content.Server.Atmos.Piping.Unary.EntitySystems
     [UsedImplicitly]
     public class GasTankSystem : EntitySystem
     {
+        private const float TimerDelay = 0.5f;
+        private float _timer = 0f;
+
         public override void Initialize()
         {
             base.Initialize();
@@ -30,6 +34,25 @@ namespace Content.Server.Atmos.Piping.Unary.EntitySystems
             tankNode.Volume = tank.InitialMixture.Volume;
             tankNode.Air.Volume = tank.InitialMixture.Volume;
             tankNode.Air.Temperature = tank.InitialMixture.Temperature;
+        }
+
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+
+            _timer += frameTime;
+
+            if (_timer < TimerDelay) return;
+            _timer -= TimerDelay;
+
+            var atmosphereSystem = Get<AtmosphereSystem>();
+
+            foreach (var gasTank in EntityManager.ComponentManager.EntityQuery<GasTankComponent>(true))
+            {
+                atmosphereSystem.React(gasTank.Air, gasTank);
+                gasTank.CheckStatus();
+                gasTank.UpdateUserInterface();
+            }
         }
     }
 }
