05e31c2cd38b3b718bd1cb2e41a2c0d2f353ea31
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2021-06-28 14:17:08 +1000

    Don't pushback when throwing while buckled (#4232)
    * Don't pushback when throwing while buckled
    
    * Address reviews
    
    * Fix

--- Content.Shared/Buckle/SharedBuckleSystem.cs
+++ Content.Shared/Buckle/SharedBuckleSystem.cs
@@ -1,5 +1,6 @@
 using Content.Shared.Buckle.Components;
 using Content.Shared.Standing;
+using Content.Shared.Throwing;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Physics.Dynamics;
 
@@ -13,6 +14,7 @@ namespace Content.Shared.Buckle
             SubscribeLocalEvent<SharedBuckleComponent, PreventCollideEvent>(PreventCollision);
             SubscribeLocalEvent<SharedBuckleComponent, DownAttemptEvent>(HandleDown);
             SubscribeLocalEvent<SharedBuckleComponent, StandAttemptEvent>(HandleStand);
+            SubscribeLocalEvent<SharedBuckleComponent, ThrowPushbackAttemptEvent>(HandleThrowPushback);
         }
 
         private void HandleStand(EntityUid uid, SharedBuckleComponent component, StandAttemptEvent args)
@@ -29,6 +31,12 @@ namespace Content.Shared.Buckle
             {
                 args.Cancel();
             }
+        }
+
+        private void HandleThrowPushback(EntityUid uid, SharedBuckleComponent component, ThrowPushbackAttemptEvent args)
+        {
+            if (!component.Buckled) return;
+            args.Cancel();
         }
 
         private void PreventCollision(EntityUid uid, SharedBuckleComponent component, PreventCollideEvent args)
