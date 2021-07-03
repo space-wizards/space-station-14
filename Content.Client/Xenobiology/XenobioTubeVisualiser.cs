fec962be1093aab0595d3c982a1f6c95b493007e
DmitriyRubetskoy <blueberrypiey234@gmail.com>
2021-07-03 18:23:19 +0300

    removed Equals

--- Content.Client/Xenobiology/XenobioTubeVisualiser.cs
+++ Content.Client/Xenobiology/XenobioTubeVisualiser.cs
@@ -16,12 +16,11 @@ namespace Content.Client.Cloning
             var sprite = component.Owner.GetComponent<ISpriteComponent>();
             if (!component.TryGetData(XenoTubeStatus.Powered, out bool powered)) return;
             if (!component.TryGetData(XenoTubeStatus.Occupied, out bool occupied)) return;
-            if (powered.Equals(false)) sprite.LayerSetState(0, "tube-unpowered");
-            return;
-            if (powered.Equals(true))
+            if (powered == false) sprite.LayerSetState(0, "tube-unpowered");
+            if (powered == true)
             {
-                if (occupied.Equals(false)) sprite.LayerSetState(0, "tube-powered");
-                if (occupied.Equals(true)) sprite.LayerSetState(0, "tube-occupied");
+                if (occupied == false) sprite.LayerSetState(0, "tube-powered");
+                if (occupied == true) sprite.LayerSetState(0, "tube-occupied");
             }
         }
     }
