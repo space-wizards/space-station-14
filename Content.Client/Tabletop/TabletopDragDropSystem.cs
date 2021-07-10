e50f8e9423c484fda0b56492cdb705c9503e7484
Visne <vincefvanwijk@gmail.com>
2021-07-10 03:20:24 +0200

    Don't send coordinates every frame

--- Content.Client/Tabletop/TabletopDragDropSystem.cs
+++ Content.Client/Tabletop/TabletopDragDropSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Content.Shared.Tabletop;
 using JetBrains.Annotations;
 using Robust.Client.Graphics;
@@ -7,6 +8,7 @@ using Robust.Shared.GameObjects;
 using Robust.Shared.Input;
 using Robust.Shared.Input.Binding;
 using Robust.Shared.IoC;
+using Robust.Shared.Log;
 using Robust.Shared.Map;
 
 namespace Content.Client.Tabletop
@@ -18,7 +20,16 @@ namespace Content.Client.Tabletop
         [Dependency] private readonly IMapManager _mapManager = default!;
         [Dependency] private readonly IInputManager _inputManager = default!;
 
+        /**
+         * Time in seconds to wait until sending the location of a dragged entity to the server again.
+         */
+        private const float Delay = 0.1f;
+
+        // Entity being dragged
         private IEntity? _draggedEntity;
+
+        // Time passed since last update sent to the server.
+        private float _timePassed = 0f;
 
         public override void Initialize()
         {
@@ -34,7 +45,16 @@ namespace Content.Client.Tabletop
             var worldPos = _eyeManager.ScreenToMap(_inputManager.MouseScreenPosition);
             EntityCoordinates coords = new(_mapManager.GetMapEntityId(worldPos.MapId), worldPos.Position);
 
-            RaiseNetworkEvent(new TabletopMoveEvent(_draggedEntity.Uid, coords));
+            // Move the entity locally every update
+            _draggedEntity.Transform.Coordinates = coords;
+
+            _timePassed += frameTime;
+
+            if (_timePassed >= Delay)
+            {
+                RaiseNetworkEvent(new TabletopMoveEvent(_draggedEntity.Uid, coords));
+                _timePassed = 0f;
+            }
         }
 
         private bool OnUse(in PointerInputCmdHandler.PointerInputCmdArgs args)
