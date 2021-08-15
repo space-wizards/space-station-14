36060471de688fa61ed7733e7467703ea38487b9
scaly-chimp <scalychimp@gmail.com>
2021-08-15 19:46:26 +0800

    bounds and banished OOP

--- Content.Server/Explosion/Components/TriggerOnProximityComponent.cs
+++ Content.Server/Explosion/Components/TriggerOnProximityComponent.cs
@@ -16,23 +16,14 @@ namespace Content.Server.Explosion.Components
         public IPhysShape Shape { get; set; } = default!;
 
         private bool _enabled;
-        [DataField("Enabled")]
+        [DataField("enabled")]
         [ViewVariables(VVAccess.ReadWrite)]
         public bool Enabled
         {
             set
             {
-
-                if (value)
-                {
-                    Owner.EntityManager.EntitySysManager.GetEntitySystem<ProximityTriggerSystem>().AddProximityFixture(Owner.Uid, this);
-                }
-                else
-                {
-                    Owner.EntityManager.EntitySysManager.GetEntitySystem<ProximityTriggerSystem>().RemoveProximityFixture(Owner.Uid, this);
-                }
+                EntitySystem.Get<ProximityTriggerSystem>().SetProximityFixture(Owner.Uid, this, value);
                 _enabled = value;
-
             }
             get => _enabled;
         }
--- Content.Server/Explosion/ProximityTriggerSystem.cs
+++ Content.Server/Explosion/ProximityTriggerSystem.cs
@@ -1,28 +1,13 @@
 using Content.Server.Explosion.Components;
-using Content.Shared.Physics;
 using Robust.Shared.GameObjects;
-using Robust.Shared.Log;
 using Robust.Shared.Physics;
 using Robust.Shared.Physics.Dynamics;
-using System;
 
 namespace Content.Server.Explosion
 {
     public class ProximityTriggerSystem : EntitySystem
     {
-        public void AddProximityFixture(EntityUid uid, TriggerOnProximityComponent component)
-        {
-            var entity = EntityManager.GetEntity(uid);
-            var broadphase = Get<SharedBroadphaseSystem>();
-            
-            if (entity.TryGetComponent(out PhysicsComponent? physics))
-            {
-                var fixture = physics.GetFixture(component.ProximityFixture);
-                if (fixture == null)
-                    broadphase.CreateFixture(physics, new Fixture(physics, component.Shape) { Hard = false, ID = component.ProximityFixture });                
-            }
-        }
-        public void RemoveProximityFixture(EntityUid uid, TriggerOnProximityComponent component)
+        public void SetProximityFixture(EntityUid uid, TriggerOnProximityComponent component, bool remove)
         {
             var entity = EntityManager.GetEntity(uid);
             var broadphase = Get<SharedBroadphaseSystem>();
@@ -30,8 +15,16 @@ namespace Content.Server.Explosion
             if (entity.TryGetComponent(out PhysicsComponent? physics))
             {
                 var fixture = physics.GetFixture(component.ProximityFixture);
-                if (fixture != null)
-                    broadphase.DestroyFixture(physics, fixture);
+                if (!remove)
+                {   
+                    if (fixture != null)
+                        broadphase.DestroyFixture(physics, fixture);
+                }
+                else
+                {
+                    if (fixture == null)
+                        broadphase.CreateFixture(physics, new Fixture(physics, component.Shape) { Hard = false, ID = component.ProximityFixture });
+                }
             }
         }
     }
