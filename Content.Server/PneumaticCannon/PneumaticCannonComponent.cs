f4c85b16b78c28d5c344a2e90004e6369ca33fb8
Kara D <lunarautomaton6@gmail.com>
2021-09-02 23:42:34 -0700

    basics & visuals

---
+++ Content.Server/PneumaticCannon/PneumaticCannonComponent.cs
@@ -0,0 +1,31 @@
+﻿using Robust.Shared.Analyzers;
+using Robust.Shared.Containers;
+using Robust.Shared.GameObjects;
+using Robust.Shared.ViewVariables;
+
+namespace Content.Server.PneumaticCannon
+{
+    [RegisterComponent, Friend(typeof(PneumaticCannonSystem))]
+    public class PneumaticCannonComponent : Component
+    {
+        public override string Name { get; } = "PneumaticCannon";
+
+        [ViewVariables]
+        public ContainerSlot GasTankSlot = default!;
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        public PneumaticCannonPower Power = PneumaticCannonPower.Low;
+    }
+
+    /// <summary>
+    ///     How strong the pneumatic cannon should be.
+    ///     Each tier throws items farther and with more speed, but has drawbacks.
+    ///     The highest power knocks the player down for a considerable amount of time.
+    /// </summary>
+    public enum PneumaticCannonPower
+    {
+        Low,
+        Medium,
+        High
+    }
+}
---
+++ Content.Server/PneumaticCannon/PneumaticCannonSystem.cs
@@ -0,0 +1,89 @@
+﻿using Content.Server.Atmos.Components;
+using Content.Server.Hands.Components;
+using Content.Server.Items;
+using Content.Server.Storage.Components;
+using Content.Shared.Interaction;
+using Content.Shared.PneumaticCannon;
+using Robust.Server.GameObjects;
+using Robust.Shared.Containers;
+using Robust.Shared.GameObjects;
+
+namespace Content.Server.PneumaticCannon
+{
+    public class PneumaticCannonSystem : EntitySystem
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<PneumaticCannonComponent, ComponentInit>(OnComponentInit);
+            SubscribeLocalEvent<PneumaticCannonComponent, InteractUsingEvent>(OnInteractUsing);
+            SubscribeLocalEvent<PneumaticCannonComponent, InteractHandEvent>(OnInteractHand);
+        }
+
+        private void OnComponentInit(EntityUid uid, PneumaticCannonComponent component, ComponentInit args)
+        {
+            component.GasTankSlot = component.Owner.EnsureContainer<ContainerSlot>($"{component.Name}-gasTank");
+        }
+
+        private void OnInteractUsing(EntityUid uid, PneumaticCannonComponent component, InteractUsingEvent args)
+        {
+            if (args.Used.HasComponent<GasTankComponent>() && component.GasTankSlot.CanInsert(args.Used))
+            {
+                component.GasTankSlot.Insert(args.Used);
+                // popup
+                UpdateAppearance(component);
+                return;
+            }
+
+            if (args.Used.TryGetComponent<ItemComponent>(out var item)
+                && component.Owner.TryGetComponent<ServerStorageComponent>(out var storage))
+            {
+                if (storage.CanInsert(args.Used))
+                {
+                    storage.Insert(args.Used);
+                    // popup
+                }
+                else
+                {
+                    // popup
+                }
+            }
+        }
+
+        private void OnInteractHand(EntityUid uid, PneumaticCannonComponent component, InteractHandEvent args)
+        {
+            TryRemoveGasTank(component, args.User);
+        }
+
+        public void TryRemoveGasTank(PneumaticCannonComponent component, IEntity user)
+        {
+            if (component.GasTankSlot.ContainedEntity == null)
+            {
+                //popup
+                return;
+            }
+
+            var ent = component.GasTankSlot.ContainedEntity;
+            if (component.GasTankSlot.Remove(ent))
+            {
+                if (user.TryGetComponent<HandsComponent>(out var hands))
+                {
+                    hands.TryPickupEntityToActiveHand(ent);
+                }
+
+                //popup
+                UpdateAppearance(component);
+            }
+        }
+
+        private void UpdateAppearance(PneumaticCannonComponent component)
+        {
+            if (component.Owner.TryGetComponent<AppearanceComponent>(out var appearance))
+            {
+                appearance.SetData(PneumaticCannonVisuals.Tank,
+                    component.GasTankSlot.ContainedEntities.Count != 0);
+            }
+        }
+    }
+}
