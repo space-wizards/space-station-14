56db34567a4ef330d876da1a4e26cd64491b0cbd
ElectroJr <leonsfriedrich@gmail.com>
2021-09-06 19:16:03 +1200

    Add pickup verb

---
+++ Content.Server/Items/ItemSystem.cs
@@ -0,0 +1,35 @@
+using Content.Shared.Item;
+using Content.Shared.Verbs;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Localization;
+
+namespace Content.Server.Items
+{
+
+    public class ItemSystem : EntitySystem
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<SharedItemComponent, AssembleVerbsEvent>(AddPickupVerb);
+        }
+
+        private void AddPickupVerb(EntityUid uid, SharedItemComponent component, AssembleVerbsEvent args)
+        {
+            if (!args.DefaultInRangeUnobstructed || args.Hands == null)
+                return;
+
+            if (component.CanPickup(args.User))
+            {
+                Verb verb = new("pickup");
+                verb.Act = () => args.Hands.TryPutInActiveHandOrAny(args.Target);
+                if (args.PrepareGUI)
+                {
+                    verb.Text = Loc.GetString("pick-up-verb-get-data-text");
+                    verb.IconTexture = "/Textures/Interface/VerbIcons/pickup.svg.192dpi.png";
+                }
+                args.Verbs.Add(verb);
+            }
+        }
+    }
+}
