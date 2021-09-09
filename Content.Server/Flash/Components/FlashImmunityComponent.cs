24bac20ceb40f2f8b46f5e0022c9851539e5631f
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-09-09 13:48:13 +0200

    VV ReadWrite for FlashImmunity Enabled.

--- Content.Server/Flash/Components/FlashImmunityComponent.cs
+++ Content.Server/Flash/Components/FlashImmunityComponent.cs
@@ -1,6 +1,7 @@
 using Robust.Shared.Analyzers;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.ViewVariables;
 
 namespace Content.Server.Flash.Components
 {
@@ -9,6 +10,7 @@ namespace Content.Server.Flash.Components
     {
         public override string Name => "FlashImmunity";
 
+        [ViewVariables(VVAccess.ReadWrite)]
         [DataField("enabled")]
         public bool Enabled { get; set; } = true;
     }
