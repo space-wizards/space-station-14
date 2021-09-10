ed8bb4ae8eb86531c779640699243509e94017d1
ElectroJr <leonsfriedrich@gmail.com>
2021-09-11 01:53:12 +1200

    power cell verbs

--- Content.Server/PowerCell/PowerCellSystem.cs
+++ Content.Server/PowerCell/PowerCellSystem.cs
@@ -1,5 +1,7 @@
-﻿using Content.Server.PowerCell.Components;
+using Content.Server.PowerCell.Components;
+using Content.Shared.ActionBlocker;
 using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.Verbs;
 using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
@@ -10,12 +12,48 @@ namespace Content.Server.PowerCell
     public class PowerCellSystem  : EntitySystem
     {
         [Dependency] private readonly SolutionContainerSystem _solutionsSystem = default!;
+        [Dependency] private readonly ActionBlockerSystem _actionBlockerSystem = default!;
 
         public override void Initialize()
         {
             base.Initialize();
 
             SubscribeLocalEvent<PowerCellComponent, SolutionChangedEvent>(OnSolutionChange);
+            SubscribeLocalEvent<PowerCellSlotComponent, GetAlternativeVerbsEvent>(AddEjectVerb);
+            SubscribeLocalEvent<PowerCellSlotComponent, GetInteractionVerbsEvent>(AddInsertVerb);
+        }
+
+        // TODO VERBS EJECTABLES Standardize eject/insert verbs into a single system?
+        private void AddEjectVerb(EntityUid uid, PowerCellSlotComponent component, GetAlternativeVerbsEvent args)
+        {
+            if (args.Hands == null ||
+                !args.CanAccess ||
+                !args.CanInteract ||
+                !component.ShowVerb ||
+                !component.HasCell ||
+                !_actionBlockerSystem.CanPickup(args.User))
+                return;
+
+            Verb verb = new("cell:eject");
+            verb.Act = () => component.EjectCell(args.User);
+            verb.Category = VerbCategory.Eject;
+            args.Verbs.Add(verb);
+        }
+
+        private void AddInsertVerb(EntityUid uid, PowerCellSlotComponent component, GetInteractionVerbsEvent args)
+        {
+            if (args.Using == null ||
+                !args.CanAccess ||
+                !args.CanInteract ||
+                component.HasCell ||
+                !args.Using.HasComponent<PowerCellComponent>() ||
+                !_actionBlockerSystem.CanDrop(args.User))
+                return;
+
+            Verb verb = new("cell:Insert");
+            verb.Act = () => component.InsertCell(args.Using);
+            verb.Category = VerbCategory.Insert;
+            args.Verbs.Add(verb);
         }
 
         private void OnSolutionChange(EntityUid uid, PowerCellComponent component, SolutionChangedEvent args)
