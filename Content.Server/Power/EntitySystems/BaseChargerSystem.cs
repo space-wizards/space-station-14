65f78748d36da70dc45f6d2a7fc1d6c377b4c719
ElectroJr <leonsfriedrich@gmail.com>
2021-09-11 21:10:47 +1200

    Add charger verbs

--- Content.Server/Power/EntitySystems/BaseChargerSystem.cs
+++ Content.Server/Power/EntitySystems/BaseChargerSystem.cs
@@ -1,18 +1,70 @@
 using Content.Server.Power.Components;
+using Content.Server.PowerCell.Components;
+using Content.Server.Weapon;
+using Content.Shared.ActionBlocker;
+using Content.Shared.Verbs;
 using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
 
 namespace Content.Server.Power.EntitySystems
 {
     [UsedImplicitly]
     internal sealed class BaseChargerSystem : EntitySystem
     {
+        [Dependency] private readonly ActionBlockerSystem _actionBlockerSystem = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<PowerCellChargerComponent, GetAlternativeVerbsEvent>(AddEjectVerb);
+            SubscribeLocalEvent<PowerCellChargerComponent, GetInteractionVerbsEvent>(AddInsertVerb);
+            SubscribeLocalEvent<WeaponCapacitorChargerComponent, GetAlternativeVerbsEvent>(AddEjectVerb);
+            SubscribeLocalEvent<WeaponCapacitorChargerComponent, GetInteractionVerbsEvent>(AddInsertVerb);
+        }
+
         public override void Update(float frameTime)
         {
             foreach (var comp in ComponentManager.EntityQuery<BaseCharger>(true))
             {
                 comp.OnUpdate(frameTime);
             }
+        }
+
+        // TODO VERBS EJECTABLES Standardize eject/insert verbs into a single system?
+        // TODO QUESTION: I thought component inheritance was a no-no? These two chargers inherit from BaseCharger?
+        private void AddEjectVerb(EntityUid uid, BaseCharger component, GetAlternativeVerbsEvent args)
+        {
+            if (args.Hands == null ||
+                !args.CanAccess ||
+                !args.CanInteract ||
+                !component.HasCell ||
+                !_actionBlockerSystem.CanPickup(args.User))
+                return;
+
+            Verb verb = new("charger:eject");
+            verb.Text = component.Container.ContainedEntity!.Name;
+            verb.Category = VerbCategory.Eject;
+            verb.Act = () => component.RemoveItem(args.User);
+            args.Verbs.Add(verb);
+        }
+
+        private void AddInsertVerb(EntityUid uid, BaseCharger component, GetInteractionVerbsEvent args)
+        {
+            if (args.Using == null ||
+                !args.CanAccess ||
+                !args.CanInteract ||
+                component.HasCell ||
+                !component.IsEntityCompatible(args.Using) ||
+                !_actionBlockerSystem.CanDrop(args.User))
+                return;
+
+            Verb verb = new("charger:Insert");
+            verb.Text = args.Using.Name;
+            verb.Category = VerbCategory.Insert;
+            verb.Act = () => component.TryInsertItem(args.Using);
+            args.Verbs.Add(verb);
         }
     }
 }
