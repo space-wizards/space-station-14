53b68067f8f11084c0d95bd8af4e80cbb34a4aa7
Ygg One <y.laughing.man.y@gmail.com>
2021-09-11 13:54:46 +0200

    Revert previous commit

--- Content.Server/Destructible/Thresholds/Behaviors/SpillBehavior.cs
+++ Content.Server/Destructible/Thresholds/Behaviors/SpillBehavior.cs
@@ -1,5 +1,4 @@
 using Content.Server.Fluids.Components;
-using Content.Shared.Chemistry.Components.SolutionManager;
 using Content.Shared.Chemistry.EntitySystems;
 using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
@@ -13,15 +12,11 @@ namespace Content.Server.Destructible.Thresholds.Behaviors
     {
         public void Execute(IEntity owner, DestructibleSystem system)
         {
-            EntitySystem.TryGet(out SolutionContainerSystem? containerSystem);
-            if (!owner.HasComponent<SolutionContainerManagerComponent>() ||
-                containerSystem == null)
+            // TODO see if this is correct
+            if (!EntitySystem.Get<SolutionContainerSystem>().TryGetSolution(owner, SpillableComponent.SolutionName, out var solution))
                 return;
 
-            foreach (var solution in containerSystem.AllSolutions(owner.Uid))
-            {
-                solution.SpillAt(owner.Transform.Coordinates, "PuddleSmear", false);
-            }
+            solution.SpillAt(owner.Transform.Coordinates, "PuddleSmear", false);
         }
     }
 }
