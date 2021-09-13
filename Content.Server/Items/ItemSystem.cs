a53ae04246732cb19c2ceae1aaab36bbc7712d25
ElectroJr <leonsfriedrich@gmail.com>
2021-09-13 19:30:46 +1200

    Fix in-inventory check

--- Content.Server/Items/ItemSystem.cs
+++ Content.Server/Items/ItemSystem.cs
@@ -27,8 +27,8 @@ namespace Content.Server.Items
             verb.Act = () => args.Hands.TryPutInActiveHandOrAny(args.Target);
             verb.IconTexture = "/Textures/Interface/VerbIcons/pickup.svg.192dpi.png";
 
-            // if the item already in the user's inventory (or some other storage), change the text
-            if (args.Target.IsInContainer())
+            // if the item already in the user's inventory, change the text
+            if (args.Target.TryGetContainer(out var container) && container.Owner == args.User)
                 verb.Text = Loc.GetString("pick-up-verb-get-data-text-inventory"); 
             else
                 verb.Text = Loc.GetString("pick-up-verb-get-data-text");
