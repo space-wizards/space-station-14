942e45d6b3820d85d3593d45cef335d6edb781fc
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-09-16 11:59:39 +0200

    Improves the JobSpecial system.

---
+++ Content.Server/Jobs/GiveItemOnHolidaySpecial.cs
@@ -0,0 +1,36 @@
+using Content.Server.Hands.Components;
+using Content.Server.Holiday;
+using Content.Server.Holiday.Interfaces;
+using Content.Server.Items;
+using Content.Shared.Roles;
+using JetBrains.Annotations;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server.Jobs
+{
+    [UsedImplicitly]
+    [DataDefinition]
+    public class GiveItemOnHolidaySpecial : JobSpecial
+    {
+        [DataField("holiday", customTypeSerializer:typeof(PrototypeIdSerializer<HolidayPrototype>))]
+        public string Holiday { get; } = string.Empty;
+
+        [DataField("prototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string Prototype { get; } = string.Empty;
+
+        public override void AfterEquip(IEntity mob)
+        {
+            if (string.IsNullOrEmpty(Holiday) || string.IsNullOrEmpty(Prototype)) return;
+            if (!IoCManager.Resolve<IHolidayManager>().IsCurrentlyHoliday(Holiday)) return;
+
+            var item = mob.EntityManager.SpawnEntity(Prototype, mob.Transform.Coordinates);
+            if (!item.TryGetComponent(out ItemComponent? itemComp)) return;
+            if (!mob.TryGetComponent(out HandsComponent? handsComponent)) return;
+            handsComponent.PutInHand(itemComp, false);
+        }
+    }
+}
