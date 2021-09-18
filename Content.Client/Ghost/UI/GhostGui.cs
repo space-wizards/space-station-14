31f2f7f216cdf338002bab3b569209fb9e6dc17e
Visne <vincefvanwijk@gmail.com>
2021-09-18 21:44:03 +0200

    GhostTargetWindow to XAML

--- Content.Client/Ghost/UI/GhostGui.cs
+++ Content.Client/Ghost/UI/GhostGui.cs
@@ -1,9 +1,7 @@
-using System.Collections.Generic;
 using Content.Shared.Ghost;
 using Robust.Client.Console;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
-using Robust.Client.UserInterface.CustomControls;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Localization;
@@ -26,7 +24,7 @@ namespace Content.Client.Ghost.UI
 
             _owner = owner;
 
-            TargetWindow = new GhostTargetWindow(owner, eventBus);
+            TargetWindow = new GhostTargetWindow(eventBus);
 
             MouseFilter = MouseFilterMode.Ignore;
 
