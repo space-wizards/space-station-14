0daad6944abd92b4ff9ec3be64240c753bc4aac6
20kdc <asdd2808@gmail.com>
2021-09-22 12:44:05 +0100

    startsingularityengine now constructs PAs

--- Content.Server/Singularity/StartSingularityEngineCommand.cs
+++ Content.Server/Singularity/StartSingularityEngineCommand.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using Content.Server.Administration;
+using Content.Server.Construction.Components;
 using Content.Server.ParticleAccelerator.Components;
 using Content.Server.Singularity.Components;
 using Content.Server.Singularity.EntitySystems;
@@ -14,10 +16,10 @@ namespace Content.Server.Singularity
     public class StartSingularityEngineCommand : IConsoleCommand
     {
         public string Command => "startsingularityengine";
-        public string Description => "Automatically turns on the particle accelerator and containment field emitters.";
+        public string Description => "Automatically sets up the PA, turns on the particle accelerator and the containment field emitters.";
         public string Help => $"{Command}";
 
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        public async void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             if (args.Length != 0)
             {
@@ -26,6 +28,22 @@ namespace Content.Server.Singularity
             }
 
             var entityManager = IoCManager.Resolve<IEntityManager>();
+            // Dear code reviewers: I don't think there's a good way to do this,
+            //  and the command exists to give fast turnaround time on testing singulo,
+            //  which, given all the singulo bugs, seems necessary,
+            //  so it can't just be removed.
+            // What this code does: Constructs all PA components by forcing node changes into the completed state.
+            foreach (var comp in entityManager.ComponentManager.EntityQuery<ConstructionComponent>().ToArray())
+            {
+                // Check for valid graph
+                var graph = comp.GraphPrototype;
+                if (graph == null)
+                    continue;
+                // Construct PAs
+                if (graph.ID.StartsWith("particleAccelerator") && graph.Nodes.ContainsKey("completed"))
+                    await comp.ChangeNode("completed");
+            }
+            // Turn on emitters and collectors.
             foreach (var comp in entityManager.ComponentManager.EntityQuery<EmitterComponent>())
             {
                 EntitySystem.Get<EmitterSystem>().SwitchOn(comp);
@@ -34,6 +52,7 @@ namespace Content.Server.Singularity
             {
                 comp.Collecting = true;
             }
+            // Actually turn on the PA.
             foreach (var comp in entityManager.ComponentManager.EntityQuery<ParticleAcceleratorControlBoxComponent>())
             {
                 comp.RescanParts();
