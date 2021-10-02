1aadd3e57d1d7c3a5d4de889bff6c8ae73dec55e
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-10-02 11:02:02 +0200

    Activate now triggers UseDelay cooldown as well. Fixes #4605

--- Content.Server/Interaction/InteractionSystem.cs
+++ Content.Server/Interaction/InteractionSystem.cs
@@ -207,6 +207,14 @@ namespace Content.Server.Interaction
 
         private void InteractionActivate(IEntity user, IEntity used)
         {
+            if (used.TryGetComponent<UseDelayComponent>(out var delayComponent))
+            {
+                if (delayComponent.ActiveDelay)
+                    return;
+
+                delayComponent.BeginDelay();
+            }
+
             if (!_actionBlockerSystem.CanInteract(user) || ! _actionBlockerSystem.CanUse(user))
                 return;
 
@@ -595,8 +603,8 @@ namespace Content.Server.Interaction
             {
                 if (delayComponent.ActiveDelay)
                     return;
-                else
-                    delayComponent.BeginDelay();
+
+                delayComponent.BeginDelay();
             }
 
             var useMsg = new UseInHandEvent(user, used);
