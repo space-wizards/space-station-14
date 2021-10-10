f652a58e2d13bf27678a6ae205ee71955cfd7a40
JasperJRoth <38472993+JasperJRoth@users.noreply.github.com>
2021-10-10 19:50:39 -0400

    Added suit storage item

---
+++ Content.Client/Storage/Visualizers/SuitStorageVisualizer.cs
@@ -0,0 +1,42 @@
+using Content.Shared.Storage;
+using JetBrains.Annotations;
+using Robust.Client.GameObjects;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+
+namespace Content.Client.Storage.Visualizers
+{
+    [UsedImplicitly]
+    public sealed class SuitStorageVisualizer : AppearanceVisualizer
+    {
+
+        public override void InitializeEntity(IEntity entity)
+        {
+            if (!entity.TryGetComponent(out ISpriteComponent? sprite))
+            {
+                return;
+            }
+        }
+
+        public override void OnChangeData(AppearanceComponent component)
+        {
+            base.OnChangeData(component);
+
+            if (!component.Owner.TryGetComponent(out ISpriteComponent? sprite))
+            {
+                return;
+            }
+
+            // Checks if the door is open and if so, unhides the "open" layer
+            component.TryGetData(StorageVisuals.Open, out bool open);
+            sprite.LayerSetVisible(SuitStorageVisualLayers.Opening, open);
+
+        }
+    }
+
+    public enum SuitStorageVisualLayers : byte
+    {
+        Base,
+        Opening
+    }
+}
