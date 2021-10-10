657d98244f6a231b2d9a95a0fc4fddd1d161b144
Kara D <lunarautomaton6@gmail.com>
2021-10-09 19:58:41 -0700

    sprites and entities all work

---
+++ Content.Server/Doors/Components/ToggleDoorOnTriggerComponent.cs
@@ -0,0 +1,11 @@
+﻿using Robust.Shared.GameObjects;
+
+namespace Content.Server.Doors.Components
+{
+    public class ToggleDoorOnTriggerComponent : Component
+    {
+        public override string Name => "ToggledoorOnTrigger";
+
+
+    }
+}
---
+++ Content.Server/MachineLinking/Components/TriggerOnSignalReceivedComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameObjects;
+
+namespace Content.Server.MachineLinking.Components
+{
+    public class TriggerOnSignalReceivedComponent : Component
+    {
+        public override string Name => "TriggerOnSignalReceived";
+    }
+}
