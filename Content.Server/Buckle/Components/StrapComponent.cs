900f998e4ab59aa4f1612c93dc167835c6462df8
Clément <topy72.mine@gmail.com>
2021-10-14 23:02:50 +0200

    Fixed rotation shenanigans and buckle offset (thanks Paul)

--- Content.Server/Buckle/Components/StrapComponent.cs
+++ Content.Server/Buckle/Components/StrapComponent.cs
@@ -65,7 +65,7 @@ namespace Content.Server.Buckle.Components
         /// Don't change it unless you really have to
         /// </summary>
         [DataField("maxBuckleDistance", required: false)]
-        public float MaxBuckleDistance = 0.2f;
+        public float MaxBuckleDistance = .3f;
 
         /// <summary>
         /// You can specify the offset the entity will have after unbuckling.
