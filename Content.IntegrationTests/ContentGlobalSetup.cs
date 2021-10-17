15e0b4aa0becca34c4769a6806b8990b95db63fb
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-10-17 15:48:31 +0200

    Comment out global setup

--- Content.IntegrationTests/ContentGlobalSetup.cs
+++ Content.IntegrationTests/ContentGlobalSetup.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using NUnit.Framework;
-using Robust.UnitTesting;
-
-namespace Content.IntegrationTests
-{
-    [SetUpFixture]
-    public class ContentGlobalSetup
-    {
-        [OneTimeSetUp]
-        public void SetUp()
-        {
-            var processors = Environment.ProcessorCount;
-            var test = new DummyTest();
-
-            for (var i = 0; i < processors; i++)
-            {
-                RobustIntegrationTest.ClientsReady.Enqueue(test.CreateClient());
-                RobustIntegrationTest.ServersReady.Enqueue(test.CreateServer());
-            }
-        }
-
-        private class DummyTest : ContentIntegrationTest
-        {
-            public ClientIntegrationInstance CreateClient()
-            {
-                return StartClient();
-            }
-
-            public ServerIntegrationInstance CreateServer()
-            {
-                return StartServer();
-            }
-        }
-    }
-}
+﻿// using System;
+// using NUnit.Framework;
+// using Robust.UnitTesting;
+//
+// namespace Content.IntegrationTests
+// {
+//     [SetUpFixture]
+//     public class ContentGlobalSetup
+//     {
+//         [OneTimeSetUp]
+//         public void SetUp()
+//         {
+//             var processors = Environment.ProcessorCount;
+//             var test = new DummyTest();
+//
+//             for (var i = 0; i < 1; i++)
+//             {
+//                 RobustIntegrationTest.ClientsReady.Enqueue(test.CreateClient());
+//                 RobustIntegrationTest.ServersReady.Enqueue(test.CreateServer());
+//             }
+//         }
+//
+//         private class DummyTest : ContentIntegrationTest
+//         {
+//             public ClientIntegrationInstance CreateClient()
+//             {
+//                 return StartClient();
+//             }
+//
+//             public ServerIntegrationInstance CreateServer()
+//             {
+//                 return StartServer();
+//             }
+//         }
+//     }
+// }
