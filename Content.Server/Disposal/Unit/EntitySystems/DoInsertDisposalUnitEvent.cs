3e5a856948665b05232a907d1b32a2cbc63fa921
Javier Guardia Fernández <DrSmugleaf@users.noreply.github.com>
2021-10-20 21:12:23 +0200

    Make DisposalUnitComponent.TryInsert ECS (#4959)

---
+++ Content.Server/Disposal/Unit/EntitySystems/DoInsertDisposalUnitEvent.cs
@@ -0,0 +1,6 @@
+﻿using Robust.Shared.GameObjects;
+
+namespace Content.Server.Disposal.Unit.EntitySystems
+{
+    public record DoInsertDisposalUnitEvent(EntityUid User, EntityUid ToInsert, EntityUid Unit);
+}
