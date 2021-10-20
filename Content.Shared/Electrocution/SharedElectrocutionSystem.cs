482de4c0bf967a29d5d8b273f859a7d7089b0ce1
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-10-20 17:36:14 +0200

    events on their own file

--- Content.Shared/Electrocution/SharedElectrocutionSystem.cs
+++ Content.Shared/Electrocution/SharedElectrocutionSystem.cs
@@ -153,32 +153,4 @@ namespace Content.Shared.Electrocution
         }
 
     }
-
-    public class ElectrocutionAttemptEvent : CancellableEntityEventArgs
-    {
-        public readonly EntityUid TargetUid;
-        public readonly EntityUid? SourceUid;
-        public float SiemensCoefficient = 1f;
-
-        public ElectrocutionAttemptEvent(EntityUid targetUid, EntityUid? sourceUid, float siemensCoefficient)
-        {
-            TargetUid = targetUid;
-            SourceUid = sourceUid;
-            SiemensCoefficient = siemensCoefficient;
-        }
-    }
-
-    public class ElectrocutedEvent : EntityEventArgs
-    {
-        public readonly EntityUid TargetUid;
-        public readonly EntityUid? SourceUid;
-        public readonly float SiemensCoefficient;
-
-        public ElectrocutedEvent(EntityUid targetUid, EntityUid? sourceUid, float siemensCoefficient)
-        {
-            TargetUid = targetUid;
-            SourceUid = sourceUid;
-            SiemensCoefficient = siemensCoefficient;
-        }
-    }
 }
