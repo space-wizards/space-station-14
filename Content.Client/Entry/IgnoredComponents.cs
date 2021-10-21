343e5d1028cd61815b124771752515d15e4a8e06
20kdc <asdd2808@gmail.com>
2021-10-22 00:46:06 +0100

    PPAIs: Fix test failure by ignoring PAI component client-side

--- Content.Client/Entry/IgnoredComponents.cs
+++ Content.Client/Entry/IgnoredComponents.cs
@@ -63,6 +63,7 @@ namespace Content.Client.Entry
             "Paper",
             "Write",
             "Bloodstream",
+            "PAI",
             "TransformableContainer",
             "Mind",
             "StorageFill",
