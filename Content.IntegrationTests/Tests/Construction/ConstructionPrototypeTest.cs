b1cab69654061f0f866595292d5633b053e2104d
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-10-27 00:56:33 +1100

    Also add path test

--- Content.IntegrationTests/Tests/Construction/ConstructionPrototypeTest.cs
+++ Content.IntegrationTests/Tests/Construction/ConstructionPrototypeTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Content.Shared.Construction.Prototypes;
 using NUnit.Framework;
@@ -41,6 +42,24 @@ namespace Content.IntegrationTests.Tests.Construction
                 var graph = protoMan.Index<ConstructionGraphPrototype>(proto.Graph);
 
                 Assert.That(graph.Nodes.ContainsKey(target), $"Found no targetNode \"{target}\" on graph \"{graph.ID}\" for construction prototype \"{proto.ID}\"!");
+            }
+        }
+
+        [Test]
+        public async Task TestStartReachesTarget()
+        {
+            var server = StartServerDummyTicker();
+
+            await server.WaitIdleAsync();
+
+            var protoMan = server.ResolveDependency<IPrototypeManager>();
+
+            foreach (var proto in protoMan.EnumeratePrototypes<ConstructionPrototype>())
+            {
+                var start = proto.StartNode;
+                var target = proto.TargetNode;
+                var graph = protoMan.Index<ConstructionGraphPrototype>(proto.Graph);
+                Assert.That(graph.TryPath(start, target, out _), $"Unable to find path from \"{start}\" to \"{target}\" on graph \"{graph.ID}\"");
             }
         }
     }
