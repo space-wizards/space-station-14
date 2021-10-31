d5c8d12f60c6d605ebc5f17066961a92d7633205
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-10-31 11:33:44 +0100

    Make nullable pool settings true

--- Content.IntegrationTests/ContentIntegrationTest.cs
+++ Content.IntegrationTests/ContentIntegrationTest.cs
@@ -174,7 +174,7 @@ namespace Content.IntegrationTests
 
         private bool ShouldPool(IntegrationOptions options)
         {
-            if (!options.Pool)
+            if (!options.Pool ?? true)
             {
                 return false;
             }
