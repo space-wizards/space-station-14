012be96877987608ce38c7f39d30b04dc9e35835
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2021-11-01 23:24:00 +1100

    Network MovementSpeedModifierComponent (#5100)

--- Content.Shared/Movement/Components/MovementSpeedModifierComponent.cs
+++ Content.Shared/Movement/Components/MovementSpeedModifierComponent.cs
@@ -1,16 +1,17 @@
 using Robust.Shared.Containers;
 using Robust.Shared.GameObjects;
+using Robust.Shared.GameStates;
 using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.ViewVariables;
 
 namespace Content.Shared.Movement.Components
 {
     [RegisterComponent]
-    public class MovementSpeedModifierComponent : Component
+    [NetworkedComponent]
+    public sealed class MovementSpeedModifierComponent : Component
     {
         public const float DefaultBaseWalkSpeed = 4.0f;
         public const float DefaultBaseSprintSpeed = 7.0f;
-
 
         public override string Name => "MovementSpeedModifier";
 
@@ -36,12 +37,32 @@ namespace Content.Shared.Movement.Components
         }
 
         [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("baseWalkSpeed")]
-        public float BaseWalkSpeed { get; set; } = 4;
+        public float BaseWalkSpeedVV
+        {
+            get => BaseWalkSpeed;
+            set
+            {
+                BaseWalkSpeed = value;
+                Dirty();
+            }
+        }
 
         [ViewVariables(VVAccess.ReadWrite)]
+        public float BaseSprintSpeedVV
+        {
+            get => BaseSprintSpeed;
+            set
+            {
+                BaseSprintSpeed = value;
+                Dirty();
+            }
+        }
+
+        [DataField("baseWalkSpeed")]
+        public float BaseWalkSpeed { get; set; } = DefaultBaseWalkSpeed;
+
         [DataField("baseSprintSpeed")]
-        public float BaseSprintSpeed { get; set; } = 7;
+        public float BaseSprintSpeed { get; set; } = DefaultBaseSprintSpeed;
 
         [ViewVariables]
         public float CurrentWalkSpeed => WalkSpeedModifier * BaseWalkSpeed;
