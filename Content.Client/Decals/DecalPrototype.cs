527d260fd0908abf20de56ebace3c888fdffe4d4
Paul <ritter.paul1@googlemail.com>
2021-11-04 22:47:40 +0100

    decalstuff

---
+++ Content.Client/Decals/DecalPrototype.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Utility;
+
+namespace Content.Client.Decals
+{
+    public class DecalPrototype : IPrototype
+    {
+        [DataField("id")] public string ID { get; } = null!;
+        [DataField("sprite")] public SpriteSpecifier Sprite { get; } = SpriteSpecifier.Invalid;
+    }
+}
