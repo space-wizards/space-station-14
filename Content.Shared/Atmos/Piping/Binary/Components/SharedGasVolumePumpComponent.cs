17e1b7827edc7ff6768aad8a03e5f7f5f50abecd
ike709 <ike709@users.noreply.github.com>
2021-11-04 19:41:56 -0500

    Adds UIs for volume and pressure pumps (#5155)
    * Adds UIs for volume and pressure pumps
    
    * Update Content.Server/Atmos/Piping/Binary/EntitySystems/GasPressurePumpSystem.cs
    
    * Update Content.Server/Atmos/Piping/Binary/EntitySystems/GasVolumePumpSystem.cs
    
    Co-authored-by: ike709 <ike709@github.com>

---
+++ Content.Shared/Atmos/Piping/Binary/Components/SharedGasVolumePumpComponent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Atmos.Piping.Binary.Components
+{
+    [Serializable, NetSerializable]
+    public enum GasVolumePumpUiKey
+    {
+        Key,
+    }
+
+    [Serializable, NetSerializable]
+    public class GasVolumePumpBoundUserInterfaceState : BoundUserInterfaceState
+    {
+        public string PumpLabel { get; }
+        public float TransferRate { get; }
+        public bool Enabled { get; }
+
+        public GasVolumePumpBoundUserInterfaceState(string pumpLabel, float transferRate, bool enabled)
+        {
+            PumpLabel = pumpLabel;
+            TransferRate = transferRate;
+            Enabled = enabled;
+        }
+    }
+
+    [Serializable, NetSerializable]
+    public class GasVolumePumpToggleStatusMessage : BoundUserInterfaceMessage
+    {
+        public bool Enabled { get; }
+
+        public GasVolumePumpToggleStatusMessage(bool enabled)
+        {
+            Enabled = enabled;
+        }
+    }
+
+    [Serializable, NetSerializable]
+    public class GasVolumePumpChangeTransferRateMessage : BoundUserInterfaceMessage
+    {
+        public float TransferRate { get; }
+
+        public GasVolumePumpChangeTransferRateMessage(float transferRate)
+        {
+            TransferRate = transferRate;
+        }
+    }
+}
---
+++ Resources/Locale/en-US/components/gas-pump-component.ftl
@@ -0,0 +1,10 @@
+comp-gas-pump-ui-pump-status = Status:
+comp-gas-pump-ui-status-enabled = On
+comp-gas-pump-ui-status-disabled = Off
+
+comp-gas-pump-ui-pump-set-rate = Set
+comp-gas-pump-ui-pump-set-max = Max
+
+comp-gas-pump-ui-pump-output-pressure = Output Pressure (kPa):
+
+comp-gas-pump-ui-pump-transfer-rate = Transfer Rate (L/s):
