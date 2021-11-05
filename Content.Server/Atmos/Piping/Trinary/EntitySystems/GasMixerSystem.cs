d276d76f02146ef291a23066ab90e897e0eb0697
ike709 <ike709@users.noreply.github.com>
2021-11-04 19:34:50 -0500

    Adds a UI for gas mixers

--- Content.Server/Atmos/Piping/Trinary/EntitySystems/GasMixerSystem.cs
+++ Content.Server/Atmos/Piping/Trinary/EntitySystems/GasMixerSystem.cs
@@ -4,19 +4,29 @@ using Content.Server.Atmos.Piping.Trinary.Components;
 using Content.Server.NodeContainer;
 using Content.Server.NodeContainer.Nodes;
 using Content.Shared.Atmos;
+using Content.Shared.Atmos.Piping.Trinary.Components;
+using Content.Shared.Interaction;
 using JetBrains.Annotations;
+using Robust.Server.GameObjects;
 using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
 
 namespace Content.Server.Atmos.Piping.Trinary.EntitySystems
 {
     [UsedImplicitly]
     public class GasMixerSystem : EntitySystem
     {
+        [Dependency] private UserInterfaceSystem _userInterfaceSystem = default!;
         public override void Initialize()
         {
             base.Initialize();
 
             SubscribeLocalEvent<GasMixerComponent, AtmosDeviceUpdateEvent>(OnMixerUpdated);
+            SubscribeLocalEvent<GasMixerComponent, InteractHandEvent>(OnMixerInteractHand);
+            // Bound UI subscriptions
+            SubscribeLocalEvent<GasMixerComponent, GasMixerChangeOutputPressureMessage>(OnOutputPressureChangeMessage);
+            SubscribeLocalEvent<GasMixerComponent, GasMixerChangeNodePercentageMessage>(OnChangeNodePercentageMessage);
+            SubscribeLocalEvent<GasMixerComponent, GasMixerToggleStatusMessage>(OnToggleStatusMessage);
         }
 
         private void OnMixerUpdated(EntityUid uid, GasMixerComponent mixer, AtmosDeviceUpdateEvent args)
@@ -90,6 +100,47 @@ namespace Content.Server.Atmos.Piping.Trinary.EntitySystems
                 var removed = inletTwo.Air.Remove(transferMolesTwo);
                 outlet.AssumeAir(removed);
             }
+        }
+
+        private void OnMixerInteractHand(EntityUid uid, GasMixerComponent component, InteractHandEvent args)
+        {
+            if (!args.User.TryGetComponent(out ActorComponent? actor))
+                return;
+
+            _userInterfaceSystem.TryOpen(uid, GasMixerUiKey.Key, actor.PlayerSession);
+            DirtyUI(uid, component);
+
+            args.Handled = true;
+        }
+
+        private void DirtyUI(EntityUid uid, GasMixerComponent? mixer)
+        {
+            if (!Resolve(uid, ref mixer))
+                return;
+
+            _userInterfaceSystem.TrySetUiState(uid, GasMixerUiKey.Key,
+                new GasMixerBoundUserInterfaceState(mixer.Owner.Name, mixer.TargetPressure, mixer.Enabled, mixer.InletOneConcentration));
+        }
+
+        private void OnToggleStatusMessage(EntityUid uid, GasMixerComponent mixer, GasMixerToggleStatusMessage args)
+        {
+            mixer.Enabled = args.Enabled;
+            DirtyUI(uid, mixer);
+        }
+
+        private void OnOutputPressureChangeMessage(EntityUid uid, GasMixerComponent mixer, GasMixerChangeOutputPressureMessage args)
+        {
+            mixer.TargetPressure = Math.Clamp(args.Pressure, 0f, Atmospherics.MaxOutputPressure);
+            DirtyUI(uid, mixer);
+        }
+
+        private void OnChangeNodePercentageMessage(EntityUid uid, GasMixerComponent mixer,
+            GasMixerChangeNodePercentageMessage args)
+        {
+            float nodeOne = Math.Clamp(args.NodeOne, 0f, 100.0f) / 100.0f;
+            mixer.InletOneConcentration = nodeOne;
+            mixer.InletTwoConcentration = 1.0f - mixer.InletOneConcentration;
+            DirtyUI(uid, mixer);
         }
     }
 }
---
+++ Resources/Locale/en-US/components/gas-mixer-component.ftl
@@ -0,0 +1,10 @@
+comp-gas-mixer-ui-mixer-status = Status:
+comp-gas-mixer-ui-status-enabled = On
+comp-gas-mixer-ui-status-disabled = Off
+
+comp-gas-mixer-ui-mixer-output-pressure = Output Pressure (kPa):
+comp-gas-mixer-ui-mixer-node-one = Node 1:
+comp-gas-mixer-ui-mixer-node-two = Node 2:
+
+comp-gas-mixer-ui-mixer-set = Set
+comp-gas-mixer-ui-mixer-max = Max
