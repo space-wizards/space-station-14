9aac2cbf2b70d9d2b306a76a0b5c7716f3f7abe7
Kara D <lunarautomaton6@gmail.com>
2021-11-07 15:48:41 -0700

    Remove IMoveSpeedModifier

---
+++ Content.Shared/Clothing/SharedMagbootsSystem.cs
@@ -0,0 +1,20 @@
+﻿using Content.Shared.Movement.EntitySystems;
+using Robust.Shared.GameObjects;
+
+namespace Content.Shared.Clothing
+{
+    public class SharedMagbootsSystem : EntitySystem
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<SharedMagbootsComponent, RefreshMovementSpeedModifiersEvent>(OnRefreshMovespeed);
+        }
+
+        private void OnRefreshMovespeed(EntityUid uid, SharedMagbootsComponent component, RefreshMovementSpeedModifiersEvent args)
+        {
+            args.ModifySpeed(component.WalkSpeedModifier, component.SprintSpeedModifier);
+        }
+    }
+}
