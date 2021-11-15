5923c848d2bd8bb137663d8c58f630f73575a039
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-11-15 14:13:08 +0100

    Admins can see if other admins are AFK.

--- Content.Server/Administration/Commands/AdminWhoCommand.cs
+++ Content.Server/Administration/Commands/AdminWhoCommand.cs
@@ -1,4 +1,8 @@
-﻿using Content.Server.Administration.Managers;
+﻿using System.Text;
+using Content.Server.Administration.Managers;
+using Content.Server.Afk;
+using Content.Shared.Administration;
+using Robust.Server.Player;
 using Robust.Shared.Console;
 using Robust.Shared.IoC;
 using Robust.Shared.Utility;
@@ -15,12 +19,30 @@ public sealed class AdminWhoCommand : IConsoleCommand
     public void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         var adminMgr = IoCManager.Resolve<IAdminManager>();
+        var afk = IoCManager.Resolve<IAfkManager>();
+
+        var sb = new StringBuilder();
+        var first = true;
         foreach (var admin in adminMgr.ActiveAdmins)
         {
+            if (!first)
+                sb.Append('\n');
+            first = false;
+
             var adminData = adminMgr.GetAdminData(admin)!;
             DebugTools.AssertNotNull(adminData);
 
-            shell.WriteLine(adminData.Title != null ? $"{admin.Name}: {adminData.Title}" : admin.Name);
+            sb.Append(admin.Name);
+            if (adminData.Title is { } title)
+                sb.Append($": [{title}]");
+
+            if (shell.Player is IPlayerSession player && adminMgr.HasAdminFlag(player, AdminFlags.Admin))
+            {
+                if (afk.IsAfk(admin))
+                    sb.Append(" [AFK]");
+            }
         }
+
+        shell.WriteLine(sb.ToString());
     }
 }
