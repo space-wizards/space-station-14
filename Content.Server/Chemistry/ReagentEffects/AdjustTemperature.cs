ac8fa3fa6cd20ed9ba9219cd9b6b1c2d48f92db7
Kara D <lunarautomaton6@gmail.com>
2021-11-15 01:05:30 -0700

    implements all medicines

---
+++ Content.Server/Chemistry/ReagentEffects/AdjustTemperature.cs
@@ -0,0 +1,29 @@
+﻿using Content.Server.Temperature.Components;
+using Content.Server.Temperature.Systems;
+using Content.Shared.Chemistry.Reagent;
+using Robust.Shared.Serialization.Manager.Attributes;
+
+namespace Content.Server.Chemistry.ReagentEffects
+{
+    public class AdjustTemperature : ReagentEffect
+    {
+        [DataField("amount")]
+        public float Amount;
+
+        public override void Metabolize(ReagentEffectArgs args)
+        {
+            if (args.EntityManager.TryGetComponent(args.SolutionEntity, out TemperatureComponent temp))
+            {
+                var sys = args.EntityManager.EntitySysManager.GetEntitySystem<TemperatureSystem>();
+                if (Amount > 0)
+                {
+                    sys.ReceiveHeat(args.SolutionEntity, Amount, temp);
+                }
+                else if (Amount < 0)
+                {
+                    sys.RemoveHeat(args.SolutionEntity, Amount, temp);
+                }
+            }
+        }
+    }
+}
