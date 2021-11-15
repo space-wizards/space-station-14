b83ee83c224873f878e16b6478e0abf27646e53f
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-11-15 09:10:57 +0100

    Add adminwho command.

---
+++ Content.Server/Administration/Commands/AdminWhoCommand.cs
@@ -0,0 +1,26 @@
+﻿using Content.Server.Administration.Managers;
+using Robust.Shared.Console;
+using Robust.Shared.IoC;
+using Robust.Shared.Utility;
+
+namespace Content.Server.Administration.Commands;
+
+[AnyCommand]
+public sealed class AdminWhoCommand : IConsoleCommand
+{
+    public string Command => "adminwho";
+    public string Description => "Returns a list of all admins on the server";
+    public string Help => "Usage: adminwho";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var adminMgr = IoCManager.Resolve<IAdminManager>();
+        foreach (var admin in adminMgr.ActiveAdmins)
+        {
+            var adminData = adminMgr.GetAdminData(admin)!;
+            DebugTools.AssertNotNull(adminData);
+
+            shell.WriteLine(adminData.Title != null ? $"{admin.Name}: {adminData.Title}" : admin.Name);
+        }
+    }
+}
