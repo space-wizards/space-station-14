de28308e7b629a64d17028ec038d01c92c87cba4
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-11-20 16:39:43 +0100

    Add tests for bulk adding logs

--- Content.IntegrationTests/Tests/Administration/Logs/AdminLogTests.cs
+++ Content.IntegrationTests/Tests/Administration/Logs/AdminLogTests.cs
@@ -59,4 +59,64 @@ public class AdminLogsTests : ContentIntegrationTest
             return false;
         });
     }
+
+    [Test]
+    [TestCase(500, false)]
+    [TestCase(500, true)]
+    public async Task AddLogs(int amount, bool parallel)
+    {
+        var server = StartServer(new ServerContentIntegrationOption
+        {
+            CVarOverrides =
+            {
+                [CCVars.AdminLogsQueueSendDelay.Name] = "0"
+            }
+        });
+        await server.WaitIdleAsync();
+
+        var sEntities = server.ResolveDependency<IEntityManager>();
+        var sMaps = server.ResolveDependency<IMapManager>();
+        var sSystems = server.ResolveDependency<IEntitySystemManager>();
+
+        var sAdminLogSystem = sSystems.GetEntitySystem<AdminLogSystem>();
+        var sGameTicker = sSystems.GetEntitySystem<GameTicker>();
+
+        await server.WaitPost(() =>
+        {
+            var coordinates = GetMainEntityCoordinates(sMaps);
+            var entity = sEntities.SpawnEntity(null, coordinates);
+
+            if (parallel)
+            {
+                Parallel.For(0, amount, _ =>
+                {
+                    sAdminLogSystem.Add(LogType.Unknown, $"{entity:Entity} test log.");
+                });
+            }
+            else
+            {
+                for (var i = 0; i < amount; i++)
+                {
+                    sAdminLogSystem.Add(LogType.Unknown, $"{entity:Entity} test log.");
+                }
+            }
+        });
+
+        await WaitUntil(server, async () =>
+        {
+            var messages = sAdminLogSystem.AllMessages(new LogFilter
+            {
+                Round = sGameTicker.RoundId
+            });
+
+            var count = 0;
+
+            await foreach (var _ in messages)
+            {
+                count++;
+            }
+
+            return count >= amount;
+        });
+    }
 }
