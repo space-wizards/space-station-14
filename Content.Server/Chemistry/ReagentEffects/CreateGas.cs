640913c20204316e482908b5c6b121e0d238e35d
Kara D <lunarautomaton6@gmail.com>
2021-11-27 00:59:34 -0700

    phlogiston!

---
+++ Content.Server/Chemistry/ReagentEffects/CreateGas.cs
@@ -0,0 +1,37 @@
+﻿using Content.Server.Atmos;
+using Content.Server.Atmos.EntitySystems;
+using Content.Shared.Atmos;
+using Content.Shared.Atmos.Prototypes;
+using Content.Shared.Chemistry.Reagent;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server.Chemistry.ReagentEffects;
+
+public class CreateGas : ReagentEffect
+{
+    [DataField("gas", required: true)]
+    public Gas Gas = default!;
+
+    /// <summary>
+    ///     For each unit consumed, how many moles of gas should be created?
+    /// </summary>
+    [DataField("multiplier")]
+    public float Multiplier = 3f;
+
+    public override void Effect(ReagentEffectArgs args)
+    {
+        var atmosSys = EntitySystem.Get<AtmosphereSystem>();
+
+        var xform = args.EntityManager.GetComponent<TransformComponent>(args.SolutionEntity);
+        var tileMix = atmosSys.GetTileMixture(xform.Coordinates);
+
+        if (tileMix != null)
+        {
+            tileMix.AdjustMoles(Gas, args.Quantity.Float() * Multiplier);
+        }
+    }
+}
---
+++ Content.Server/Chemistry/ReagentEffects/Ignite.cs
@@ -0,0 +1,17 @@
+﻿using Content.Server.Atmos.EntitySystems;
+using Content.Shared.Chemistry.Reagent;
+using Robust.Shared.GameObjects;
+
+namespace Content.Server.Chemistry.ReagentEffects;
+
+/// <summary>
+///     Ignites a mob.
+/// </summary>
+public class Ignite : ReagentEffect
+{
+    public override void Effect(ReagentEffectArgs args)
+    {
+        var flamSys = EntitySystem.Get<FlammableSystem>();
+        flamSys.Ignite(args.SolutionEntity);
+    }
+}
