f244745d1521c5e38150dd22804e2a1e7f9a3db0
hubismal <hubismal@gmail.com>
2021-11-26 21:41:01 -0600

    Add new PAIMidi action that pAIs use to open their instruments

---
+++ Content.Server/Actions/Actions/PAIMidi.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+using Content.Server.Ghost;
+using Content.Shared.Actions.Behaviors;
+using Content.Shared.Actions.Components;
+using Content.Shared.Cooldown;
+using JetBrains.Annotations;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Server.GameObjects;
+using Content.Shared.Instruments;
+
+namespace Content.Server.Actions.Actions
+{
+    /// <summary>
+    ///     Pull up MIDI instrument interface for PAIs to "play themselves"
+    /// </summary>
+    [UsedImplicitly]
+    [DataDefinition]
+    public class PAIMidi : IInstantAction
+    {
+
+        public void DoInstantAction(InstantActionEventArgs args)
+        {
+            if (!args.Performer.TryGetComponent<SharedActionsComponent>(out var actions)) return;
+            if (!args.Performer.TryGetComponent<ServerUserInterfaceComponent>(out var serverUi)) return;
+            if (!args.Performer.TryGetComponent<ActorComponent>(out var actor)) return;
+			if (!serverUi.TryGetBoundUserInterface(InstrumentUiKey.Key,out var bui)) return;
+
+			bui.Toggle(actor.PlayerSession);
+
+        }
+    }
+}
