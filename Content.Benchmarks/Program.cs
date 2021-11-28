3a2e3a07c3873bcbb49e2026878658753a8e971d
wrexbe <81056464+wrexbe@users.noreply.github.com>
2021-11-28 07:37:58 -0800

    Make benchmark debuggable

--- Content.Benchmarks/Program.cs
+++ Content.Benchmarks/Program.cs
@@ -1,12 +1,21 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System;
 
 namespace Content.Benchmarks
 {
-    internal static class Program
+    internal class Program
     {
         public static void Main(string[] args)
         {
+#if DEBUG
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nWARNING: YOU ARE RUNNING A DEBUG BUILD, USE A RELEASE BUILD FOR AN ACCURATE BENCHMARK");
+            Console.WriteLine("THE DEBUG BUILD IS ONLY GOOD FOR FIXING A CRASHING BENCHMARK\n");
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+#else
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+#endif
         }
     }
 }
