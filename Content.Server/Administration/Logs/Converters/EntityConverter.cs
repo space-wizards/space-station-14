1f49df38189a797b419766103ad39f0d30b487fd
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-11-30 15:08:09 +0100

    Entity -> IEntity

--- Content.Server/Administration/Logs/Converters/EntityConverter.cs
+++ Content.Server/Administration/Logs/Converters/EntityConverter.cs
@@ -6,11 +6,11 @@ using Robust.Shared.IoC;
 namespace Content.Server.Administration.Logs.Converters;
 
 [AdminLogConverter]
-public class EntityConverter : AdminLogConverter<Entity>
+public class EntityConverter : AdminLogConverter<IEntity>
 {
     [Dependency] private readonly IEntityManager _entities = default!;
 
-    public override void Write(Utf8JsonWriter writer, Entity value, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, IEntity value, JsonSerializerOptions options)
     {
         EntityUidConverter.Write(writer, value.Uid, options, _entities);
     }
