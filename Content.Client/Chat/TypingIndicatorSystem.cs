27f7685440fda3ad894bdbad62f59d61f5ff5368
Ephememory <leetvipertm@gmail.com>
2021-11-30 18:38:29 -0600

    fix invisible format errors

--- Content.Client/Chat/TypingIndicatorSystem.cs
+++ Content.Client/Chat/TypingIndicatorSystem.cs
@@ -15,12 +15,12 @@ using Robust.Shared.Timing;
 
 namespace Content.Client.Chat
 {
-	public class TypingIndicatorSystem : SharedTypingIndicatorSystem
-	{
+    public class TypingIndicatorSystem : SharedTypingIndicatorSystem
+    {
         [Dependency] private readonly IPlayerManager _playerManager = default!;
-		[Dependency] private readonly IEyeManager _eyeManager = default!;
-		[Dependency] private readonly IConfigurationManager _cfg = default!;
-        
+        [Dependency] private readonly IEyeManager _eyeManager = default!;
+        [Dependency] private readonly IConfigurationManager _cfg = default!;
+
         private readonly Dictionary<EntityUid, TypingIndicatorGui> _guis = new();
         private IEntity? _attachedEntity;
 
@@ -35,12 +35,12 @@ namespace Content.Client.Chat
         /// to send messages informing the server they are typing.
         /// </summary>
         private bool _canTransmit = false;
-		
+
         public override void Initialize()
         {
-			base.Initialize();
+            base.Initialize();
             _canTransmit = true;
-			SubscribeNetworkEvent<RemoteClientTypingMessage>(HandleRemoteTyping);
+            SubscribeNetworkEvent<RemoteClientTypingMessage>(HandleRemoteTyping);
             SubscribeLocalEvent<PlayerAttachSysMessage>(HandlePlayerAttached);
         }
 
@@ -49,16 +49,16 @@ namespace Content.Client.Chat
             if (!EnabledLocally || !_canTransmit) return;
             RaiseNetworkEvent(new ClientTypingMessage(_playerManager?.LocalPlayer?.UserId, _attachedEntity?.Uid));
             _canTransmit = false;
-            Timer.Spawn(1000 * (int)_cfg.GetCVar(CCVars.ChatTypingIndicatorPollRate), () =>
-            {
-                _canTransmit = true;
-            });
+            Timer.Spawn(1000 * (int) _cfg.GetCVar(CCVars.ChatTypingIndicatorPollRate), () =>
+             {
+                 _canTransmit = true;
+             });
         }
 
-		private void HandleRemoteTyping(RemoteClientTypingMessage ev)
-		{
+        private void HandleRemoteTyping(RemoteClientTypingMessage ev)
+        {
             var entity = EntityManager.GetEntity(ev.EnityId.GetValueOrDefault());
-			var comp = entity.EnsureComponent<TypingIndicatorComponent>();
+            var comp = entity.EnsureComponent<TypingIndicatorComponent>();
 
             //a remote client is typing. toggle their component so we can render the overlay locally.
             comp.IsVisible = true;
@@ -68,65 +68,65 @@ namespace Content.Client.Chat
             });
         }
 
-		private void HandlePlayerAttached(PlayerAttachSysMessage message)
-		{
-			_attachedEntity = message.AttachedEntity;
-		}
+        private void HandlePlayerAttached(PlayerAttachSysMessage message)
+        {
+            _attachedEntity = message.AttachedEntity;
+        }
 
-		public override void FrameUpdate(float frameTime)
-		{
-			base.FrameUpdate(frameTime);
+        public override void FrameUpdate(float frameTime)
+        {
+            base.FrameUpdate(frameTime);
             //this is ripped from debug health overlay. not good but it works.
             if (_attachedEntity == null || _attachedEntity.Deleted)
-			{
-				return;
-			}
+            {
+                return;
+            }
 
-			var viewBox = _eyeManager.GetWorldViewport().Enlarged(2.0f);
+            var viewBox = _eyeManager.GetWorldViewport().Enlarged(2.0f);
 
-			foreach (var typingIndicatorComp in EntityManager.EntityQuery<TypingIndicatorComponent>())
-			{
+            foreach (var typingIndicatorComp in EntityManager.EntityQuery<TypingIndicatorComponent>())
+            {
                 var entity = typingIndicatorComp.Owner;
-				if (_attachedEntity.Transform.MapID != entity.Transform.MapID ||
-					!viewBox.Contains(entity.Transform.WorldPosition))
-				{
-					if (_guis.TryGetValue(entity.Uid, out var oldGui))
-					{
-						_guis.Remove(entity.Uid);
+                if (_attachedEntity.Transform.MapID != entity.Transform.MapID ||
+                    !viewBox.Contains(entity.Transform.WorldPosition))
+                {
+                    if (_guis.TryGetValue(entity.Uid, out var oldGui))
+                    {
+                        _guis.Remove(entity.Uid);
                         oldGui.Visible = false;
-						oldGui.Dispose();
-					}
+                        oldGui.Dispose();
+                    }
 
-					continue;
-				}
+                    continue;
+                }
 
-				if (_guis.ContainsKey(entity.Uid))
-				{
-					if (_guis.TryGetValue(entity.Uid, out var typGui))
+                if (_guis.ContainsKey(entity.Uid))
+                {
+                    if (_guis.TryGetValue(entity.Uid, out var typGui))
                     {
                         typGui.Visible = typingIndicatorComp.IsVisible;
                     }
 
-					continue;
-				}
+                    continue;
+                }
 
-				var gui = new TypingIndicatorGui(entity);
-				_guis.Add(entity.Uid, gui);
-			}
-		}
+                var gui = new TypingIndicatorGui(entity);
+                _guis.Add(entity.Uid, gui);
+            }
+        }
 
-		[UsedImplicitly]
-		public sealed class EnabledTypingIndicatorSystem : IConsoleCommand
-		{
-			public string Command => "typingindicator";
-			public string Description => "Enables the typing indicator for your character";
-			public string Help => "";
+        [UsedImplicitly]
+        public sealed class EnabledTypingIndicatorSystem : IConsoleCommand
+        {
+            public string Command => "typingindicator";
+            public string Description => "Enables the typing indicator for your character";
+            public string Help => "";
 
-			public void Execute(IConsoleShell shell, string argStr, string[] args)
-			{
-				var cfg = IoCManager.Resolve<IConfigurationManager>();
-				cfg.SetCVar(CCVars.ChatTypingIndicatorSystemEnabled, !cfg.GetCVar(CCVars.ChatTypingIndicatorSystemEnabled));
-			}
-		}
-	}
+            public void Execute(IConsoleShell shell, string argStr, string[] args)
+            {
+                var cfg = IoCManager.Resolve<IConfigurationManager>();
+                cfg.SetCVar(CCVars.ChatTypingIndicatorSystemEnabled, !cfg.GetCVar(CCVars.ChatTypingIndicatorSystemEnabled));
+            }
+        }
+    }
 }
