8433dfa00a74e7d3bf8231ebd4e9ffd8b694d108
StanberyTrask <StanberyTrask@gmail.com>
2021-11-30 21:40:57 -0600

    Add comments for draft PR

--- Content.Server/Reflector/ReflectorSystem.cs
+++ Content.Server/Reflector/ReflectorSystem.cs
@@ -1,44 +1,10 @@
 using System;
-using Content.Server.Popups;
-using Content.Shared.Audio;
-using Content.Shared.Examine;
-using Content.Shared.Interaction;
-using Content.Shared.Throwing;
-using JetBrains.Annotations;
-using Robust.Server.GameObjects;
-using Robust.Shared.Audio;
-using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
-using Robust.Shared.Localization;
-using Robust.Shared.Player;
-using Robust.Shared.Random;
-using Robust.Shared.Physics;
-using Robust.Shared.Physics.Dynamics;
-using System.Collections.Generic;
-using Content.Server.Alert;
-using Content.Server.Atmos.Components;
-using Content.Server.Stunnable;
-using Content.Server.Temperature.Systems;
-using Content.Shared.ActionBlocker;
-using Content.Shared.Alert;
-using Content.Shared.Atmos;
-using Content.Shared.Damage;
-using Content.Shared.Popups;
-using Content.Shared.Temperature;
-using Content.Shared.Tag;
-using Content.Shared.FixedPoint;
-using Robust.Shared.Prototypes;
-using Content.Shared.Damage.Prototypes;
-using System.Linq;
 using Robust.Shared.Maths;
-using System.Threading;
-using Content.Server.Power.EntitySystems;
-using Content.Server.Projectiles.Components;
-using Content.Server.Singularity.Components;
-using Content.Server.Storage.Components;
-using Content.Shared.Singularity.Components;
-using Robust.Shared.Log;
-using Timer = Robust.Shared.Timing.Timer;
+using Robust.Server.GameObjects;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Physics.Dynamics;
+using Content.Server.Popups;
+using Content.Shared.Interaction;
 
 
 namespace Content.Server.Reflector
@@ -50,7 +16,6 @@ namespace Content.Server.Reflector
         {
             base.Initialize();
 
-            //SubscribeLocalEvent<ReflectorComponent, EndCollideEvent>(OnEndCollide);
             SubscribeLocalEvent<ReflectorComponent, InteractHandEvent>(OnInteractHand);
             SubscribeLocalEvent<ReflectorComponent, PreventCollideEvent>(PreventCollision);
         }
@@ -73,29 +38,40 @@ namespace Content.Server.Reflector
 
         private void PreventCollision(EntityUid uid, ReflectorComponent reflector, PreventCollideEvent args)
         {
+
+            // this is actually the normal of the reflector, so a single sided reflector pointing like --^-- is 90 degrees, this is helpfull for boxes
             Angle ReflectorAngle = ToRadians(reflector.Angle);
 
-            Angle ProjectileAngle = args.BodyB.Owner.Transform.LocalRotation.ToWorldVec().ToWorldAngle();
+            // using args.BodyB.Owner.Transform.LocalRotation.ToWorldVec().ToWorldAngle() gets bolts going south saying they're pointing 0 + some tiny fraction
+            // south is 270 and this seems to get rid of the float error
+            // i also cant seem to use the velocity angle because every time i restart the server it spits out a new angle for a bolt going south
+            Angle ProjectileAngle = (args.BodyB.Owner.Transform.LocalRotation + ToRadians(270)) % Math.Tau;
 
             Angle Incidence = GetAngleOfIncidence(ReflectorAngle, ProjectileAngle);
 
+            // a special tool that will help us later
             IEntity Bolt = args.BodyB.Owner;
 
-            //this is broken, but its suposed to stop bolts from passing through side on
-            if (Math.Abs(Incidence) == ToRadians(180) && !(reflector.Type == 3))
+            //stops bolts from passing when hitting the side of the mirror
+            if (Math.Abs(Incidence) == ToRadians(90) && !(reflector.Type == 3))
             {
                 return;
             }
 
+            // we only care if its an emitter bolt, or whatever else the admeme cares to add to the whitelist
             if(reflector.Whitelist.IsValid(args.BodyB.OwnerUid))
             {
+                // the bolt gets rotated by this counterclockwise
+                // so 200 rotated by 50 gets you 150
                 Angle NewAngle = 0;
 
-                //this could probably be better
+                // this could probably be better
+                // using Incidence or Incidence * 2 for newangle gets the bolt stuck in the reflector
+                // if newangle is set to just ReflectorAngle its too shallow because we're not setting the vector to NewAngle, we're rotating it by NewAngle
                 switch(reflector.Type)
                 {
                     case 0:
-                        if (Math.Abs(Incidence) < ToRadians(180))
+                        if (Math.Abs(Incidence) < ToRadians(90))
                         {
                             args.Cancel();
                             NewAngle = ReflectorAngle * 2;
@@ -111,38 +87,43 @@ namespace Content.Server.Reflector
                         break;
                     case 2:
                         args.Cancel();
-                        NewAngle = ReflectorAngle;
+                        //i honestly have no idea why i have to add 90 degrees
+                        NewAngle = ReflectorAngle + (Math.PI / 4);
                         break;
                 }
 
-                if (Bolt.Transform.LocalRotation != ReflectorAngle + (Math.PI / 2))
+                // checks to see if we already rotated the bolt, it works when Incidence == 0 because it'll exit 180 degrees of where it entered
+                if (Bolt.Transform.WorldRotation != ReflectorAngle * 2)
                 {
-                    //just something for debugging
-                    reflector.Owner.PopupMessageEveryone($"{ProjectileAngle.Degrees} \n {Incidence.Degrees} \n {NewAngle.Degrees}");
+                    // just something for debugging
+                    reflector.Owner.PopupMessageEveryone($"{ProjectileAngle.Degrees} \n {Incidence.Degrees}");
 
-                    //center the bolt, this should be changed to the point of the mirror it would hit as if you dont do this the bolt flies off at weird angles
+                    // center the bolt around the mirror, this should be changed to the point of the mirror it would hit because if you just use the bolt's position the bolt flies off at weird angles
                     Bolt.Transform.Coordinates = EntityManager.GetEntity(uid).Transform.Coordinates;
 
-                    //rotate the veloctiy vector
-                    args.BodyB.LinearVelocity = (NewAngle).RotateVec(args.BodyB.LinearVelocity);
+                    // rotate the veloctiy vector
+                    //this works when the bolt comes in from the north but breaks when it comes in from the west
+                    // im guessing i should do something with the sign but the case of (Math.Sign(Incidence = 0) * NewAngle) makes head hurt
+                    args.BodyB.LinearVelocity = NewAngle.RotateVec(args.BodyB.LinearVelocity);
 
-                    //rotate the bolt so it flies straight
-                    Bolt.Transform.LocalRotation = ReflectorAngle + (Math.PI / 2);
+                    // rotate the bolt so it flies straight //broke
+                    Bolt.Transform.WorldRotation = NewAngle;
                 }
             }
         }
 
+        // im gonna replace everything with pi and tau
         private Angle ToRadians(float Angle)
         {
             return Angle * (Math.PI/180);
         }
 
         /// <summary>
-        ///     this is probably all wrong but its been 3 years since i've done angles
+        ///     get the difference between the bolt angle and the reflector normal
         /// <summary>
         private Angle GetAngleOfIncidence(Angle Normal, Angle Input)
         {
-            return (Normal - Input) % (Math.PI / 2);
+            return ((Input + Math.PI) % Math.Tau) - Normal;
         }
     }
 }
