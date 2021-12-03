07b2ce138d25479059b213a4169d7e6b6d634b53
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2021-12-03 21:19:53 +1100

    entitystorage fixes (#5660)

--- Content.Server/Storage/Components/IStorageComponent.cs
+++ Content.Server/Storage/Components/IStorageComponent.cs
@@ -2,7 +2,7 @@ using Robust.Shared.GameObjects;
 
 namespace Content.Server.Storage.Components
 {
-    public interface IStorageComponent
+    public interface IStorageComponent : IComponent
     {
         bool Remove(IEntity entity);
         bool Insert(IEntity entity);
