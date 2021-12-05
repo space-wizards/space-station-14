e8b2d0a844d43a4f7f87fe0ffe9e1d810951dba9
Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
2021-12-05 16:23:47 +1300

    Make BatteryBarrelComponent use item slots. (#5591)

--- Content.Server/Weapon/Ranged/Barrels/BarrelSystem.cs
+++ Content.Server/Weapon/Ranged/Barrels/BarrelSystem.cs
@@ -3,9 +3,11 @@ using Content.Server.Weapon.Ranged.Barrels.Components;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Popups;
 using Content.Shared.Verbs;
+using Robust.Shared.Containers;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Localization;
+using System;
 
 namespace Content.Server.Weapon.Ranged.Barrels
 {
@@ -19,13 +21,19 @@ namespace Content.Server.Weapon.Ranged.Barrels
 
             SubscribeLocalEvent<RevolverBarrelComponent, GetAlternativeVerbsEvent>(AddSpinVerb);
 
-            SubscribeLocalEvent<ServerBatteryBarrelComponent, GetAlternativeVerbsEvent>(AddEjectCellVerb);
-            SubscribeLocalEvent<ServerBatteryBarrelComponent, GetInteractionVerbsEvent>(AddInsertCellVerb);
+            SubscribeLocalEvent<ServerBatteryBarrelComponent, EntInsertedIntoContainerMessage>(OnCellSlotUpdated);
+            SubscribeLocalEvent<ServerBatteryBarrelComponent, EntRemovedFromContainerMessage>(OnCellSlotUpdated);
 
             SubscribeLocalEvent<BoltActionBarrelComponent, GetInteractionVerbsEvent>(AddToggleBoltVerb);
 
             SubscribeLocalEvent<ServerMagazineBarrelComponent, GetInteractionVerbsEvent>(AddMagazineInteractionVerbs);
             SubscribeLocalEvent<ServerMagazineBarrelComponent, GetAlternativeVerbsEvent>(AddEjectMagazineVerb);
+        }
+
+        private void OnCellSlotUpdated(EntityUid uid, ServerBatteryBarrelComponent component, ContainerModifiedMessage args)
+        {
+            if (args.Container.ID == component.CellSlot.ID)
+                component.UpdateAppearance();
         }
 
         private void AddSpinVerb(EntityUid uid, RevolverBarrelComponent component, GetAlternativeVerbsEvent args)
@@ -59,42 +67,6 @@ namespace Content.Server.Weapon.Ranged.Barrels
                 ? Loc.GetString("close-bolt-verb-get-data-text")
                 : Loc.GetString("open-bolt-verb-get-data-text");
             verb.Act = () => component.BoltOpen = !component.BoltOpen;
-            args.Verbs.Add(verb);
-        }
-
-        // TODO VERBS EJECTABLES Standardize eject/insert verbs into a single system?
-        // Really, why isn't this just PowerCellSlotComponent?
-        private void AddEjectCellVerb(EntityUid uid, ServerBatteryBarrelComponent component, GetAlternativeVerbsEvent args)
-        {
-            if (args.Hands == null ||
-                !args.CanAccess ||
-                !args.CanInteract ||
-                !component.PowerCellRemovable ||
-                component.PowerCell == null ||
-                !_actionBlockerSystem.CanPickup(args.User.Uid))
-                return;
-
-            Verb verb = new();
-            verb.Text = component.PowerCell.Owner.Name;
-            verb.Category = VerbCategory.Eject;
-            verb.Act = () => component.TryEjectCell(args.User);
-            args.Verbs.Add(verb);
-        }
-
-        private void AddInsertCellVerb(EntityUid uid, ServerBatteryBarrelComponent component, GetInteractionVerbsEvent args)
-        {
-            if (args.Using == null ||
-                !args.CanAccess ||
-                !args.CanInteract ||
-                component.PowerCell != null ||
-                !args.Using.HasComponent<BatteryComponent>() ||
-                !_actionBlockerSystem.CanDrop(args.User.Uid))
-                return;
-
-            Verb verb = new();
-            verb.Text = args.Using.Name;
-            verb.Category = VerbCategory.Insert;
-            verb.Act = () => component.TryInsertPowerCell(args.Using);
             args.Verbs.Add(verb);
         }
 
