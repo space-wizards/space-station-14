b595382d35f355e055a10fabd9aa501e4a7e8fc7
Pancake <Pangogie@users.noreply.github.com>
2021-12-05 04:41:54 -0800

    + Monkey Files

---
+++ Content.Server/Speech/Components/MonkeyAccentComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.GameObjects;
+
+namespace Content.Server.Speech.Components
+{
+    [RegisterComponent]
+    public class MonkeyAccentComponent : Component
+    {
+        public override string Name => "MonkeyAccent";
+    }
+}
