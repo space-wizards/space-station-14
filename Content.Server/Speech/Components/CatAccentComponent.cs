57d99ce5cdf6a7792391e5315aa35ae1d65c21e1
Pancake <Pangogie@users.noreply.github.com>
2021-12-05 04:28:16 -0800

    Cat and Dog Accent

---
+++ Content.Server/Speech/Components/CatAccentComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.GameObjects;
+
+namespace Content.Server.Speech.Components
+{
+    [RegisterComponent]
+    public class CatAccentComponent : Component
+    {
+        public override string Name => "CatAccent";
+    }
+}
