268b2602b3c0e93391d957e10c37d33bf968d821
Bingo Johnson <83136974+BingoJohnson@users.noreply.github.com>
2021-12-07 21:49:19 +0000

    Critical and urgent change to atmos.
    Extensive and comprehensive refactor spanning over 104 lines, improving performance and cleaning up the code.

--- Content.Server/Atmos/Piping/Unary/EntitySystems/GasVentPumpSystem.cs
+++ Content.Server/Atmos/Piping/Unary/EntitySystems/GasVentPumpSystem.cs
@@ -27,7 +27,7 @@ namespace Content.Server.Atmos.Piping.Unary.EntitySystems
 
         private void OnGasVentPumpUpdated(EntityUid uid, GasVentPumpComponent vent, AtmosDeviceUpdateEvent args)
         {
-            var appearance = IoCManager.Resolve<IEntityManager>().GetComponentOrNull<AppearanceComponent>(vent.Owner);
+            var appearance = EntityManager.GetComponentOrNull<AppearanceComponent>(vent.Owner); //Bingo waz here
 
             if (vent.Welded)
             {
@@ -43,7 +43,7 @@ namespace Content.Server.Atmos.Piping.Unary.EntitySystems
                 return;
             }
 
-            var environment = _atmosphereSystem.GetTileMixture(IoCManager.Resolve<IEntityManager>().GetComponent<TransformComponent>(vent.Owner).Coordinates, true);
+            var environment = _atmosphereSystem.GetTileMixture(EntityManager.GetComponent<TransformComponent>(vent.Owner).Coordinates, true);
 
             // We're in an air-blocked tile... Do nothing.
             if (environment == null)
