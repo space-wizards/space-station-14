333e94c99d46a445abc37717525cb50dd8030d58
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2021-12-06 17:47:39 -0800

    Clean up RulesManager

--- Content.Client/Info/RulesManager.cs
+++ Content.Client/Info/RulesManager.cs
@@ -11,18 +11,6 @@ namespace Content.Client.Info;
 public sealed class RulesManager
 {
     [Dependency] private readonly IClientNetManager _clientNetManager = default!;
-/*
- * Rules unread should be per server to show that the rules are different.
- * Save server id
- * Save the last read date
- *
- * Last read date is used for comparing against rules change date sent from server
- * Last read date is used for reopening the rules after some time as a reminder (2 months?)
- *
- * Add delay before allowing the player to close.
- * Require scrolling to the bottom to close.
- * Make the close button an `I Agree` button.
- */
 
     // If you fork SS14, change this to have the rules "last seen" date stored separately.
     public const string ForkId = "Wizards";
@@ -31,8 +19,11 @@ public sealed class RulesManager
 
     public event Action? OpenRulesWindow;
 
-    public void Startup()
+    private void OnConnectStateChanged(ClientConnectionState state)
     {
+        if (state != ClientConnectionState.Connected)
+            return;
+
         var path = new ResourcePath($"/rules_last_seen_{ForkId}");
         var lastReadTime = DateTime.UnixEpoch;
         if (_resource.UserData.Exists(path))
@@ -45,13 +36,6 @@ public sealed class RulesManager
         if (showRules)
             OpenRulesWindow?.Invoke();
     }
-
-    private void OnConnectStateChanged(ClientConnectionState state)
-    {
-        if (state == ClientConnectionState.Connected)
-            Startup();
-    }
-
 
     /// <summary>
     ///     Ran when the user opens ("read") the rules, stores the new ID to disk.
