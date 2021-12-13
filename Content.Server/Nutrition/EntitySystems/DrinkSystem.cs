c02de6c211cdafab024c17ca074168eea75f5e4c
ElectroJr <leonsfriedrich@gmail.com>
2021-12-13 23:25:21 +1300

    more solution pretty strings

--- Content.Server/Nutrition/EntitySystems/DrinkSystem.cs
+++ Content.Server/Nutrition/EntitySystems/DrinkSystem.cs
@@ -380,7 +380,7 @@ namespace Content.Server.Nutrition.EntitySystems
             });
 
             // logging
-            _logSystem.Add(LogType.ForceFeed, LogImpact.Medium, $"{ToPrettyString(userUid)} is forcing {ToPrettyString(targetUid)} to drink {ToPrettyString(uid)}");
+            _logSystem.Add(LogType.ForceFeed, LogImpact.Medium, $"{ToPrettyString(userUid)} is forcing {ToPrettyString(targetUid)} to drink {ToPrettyString(uid)} {SolutionContainerSystem.ToPrettyString(drinkSolution):solution}");
 
             return true;
         }
--- Content.Server/Nutrition/EntitySystems/FoodSystem.cs
+++ Content.Server/Nutrition/EntitySystems/FoodSystem.cs
@@ -325,7 +325,7 @@ namespace Content.Server.Nutrition.EntitySystems
             });
 
             // logging
-            _logSystem.Add(LogType.ForceFeed, LogImpact.Medium, $"{ToPrettyString(user)} is forcing {ToPrettyString(target)} to eat {ToPrettyString(uid)}");
+            _logSystem.Add(LogType.ForceFeed, LogImpact.Medium, $"{ToPrettyString(user)} is forcing {ToPrettyString(target)} to eat {ToPrettyString(uid)} {SolutionContainerSystem.ToPrettyString(foodSolution):solution}");
 
             return true;
         }
@@ -415,9 +415,9 @@ namespace Content.Server.Nutrition.EntitySystems
 
             // logging
             if (user == null)
-                _logSystem.Add(LogType.ForceFeed, $"{ToPrettyString(uid):food} was thrown into the mouth of {ToPrettyString(target):target}");
+                _logSystem.Add(LogType.ForceFeed, $"{ToPrettyString(uid):food} {SolutionContainerSystem.ToPrettyString(foodSolution):solution} was thrown into the mouth of {ToPrettyString(target):target}");
             else
-                _logSystem.Add(LogType.ForceFeed, $"{ToPrettyString(user.Value):user} threw {ToPrettyString(uid):food} into the mouth of {ToPrettyString(target):target}");
+                _logSystem.Add(LogType.ForceFeed, $"{ToPrettyString(user.Value):user} threw {ToPrettyString(uid):food} {SolutionContainerSystem.ToPrettyString(foodSolution):solution} into the mouth of {ToPrettyString(target):target}");
 
             var filter = user == null ? Filter.Entities(target) : Filter.Entities(target, user.Value);
             _popupSystem.PopupEntity(Loc.GetString(food.EatMessage), target, filter);
