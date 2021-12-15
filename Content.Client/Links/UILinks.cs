30773a363a5e0d87177d2a33c675ee6ca899b22b
Arthur <exstrominer@gmail.com>
2021-12-15 07:59:59 +0300

    Update ui links

--- Content.Client/Links/UILinks.cs
+++ Content.Client/Links/UILinks.cs
@@ -2,11 +2,11 @@
 {
     public static class UILinks
     {
-        public const string GitHub = "https://github.com/space-wizards/space-station-14/";
+        public const string GitHub = "https://github.com/space-syndicate/space-station-14/";
         public const string Patreon = "https://www.patreon.com/spacestation14";
 
-        public const string Discord = "https://discordapp.com/invite/t2jac3p";
-        public const string BugReport = "https://github.com/space-wizards/space-station-14/issues/new/choose";
-        public const string Website = "https://spacestation14.io";
+        public const string Discord = "https://discord.com/invite/F9bNgSYfX5";
+        public const string BugReport = "https://github.com/space-syndicate/space-station-14/issues/new/choose";
+        public const string Website = "https://station14.ru";
     }
 }
