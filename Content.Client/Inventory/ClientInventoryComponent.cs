84c6e5ac298cf2dc88ee7c653af4c8e306da9445
Paul <ritter.paul1@googlemail.com>
2021-12-16 16:41:28 +0100

    only strippable & ai left...

--- Content.Client/Inventory/ClientInventoryComponent.cs
+++ Content.Client/Inventory/ClientInventoryComponent.cs
@@ -1,21 +1,13 @@
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using Content.Client.Clothing;
 using Content.Client.Items.UI;
-using Content.Shared.CharacterAppearance;
 using Content.Shared.Inventory;
-using Content.Shared.Movement.EntitySystems;
-using Robust.Client.GameObjects;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.CustomControls;
 using Robust.Shared.Analyzers;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
-using Robust.Shared.Reflection;
 using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.ViewVariables;
-using static Content.Shared.Inventory.SharedInventoryComponent.ClientInventoryMessage;
 
 namespace Content.Client.Inventory
 {
@@ -39,28 +31,5 @@ namespace Content.Client.Inventory
 
         [ViewVariables]
         [DataField("speciesId")] public string? SpeciesId { get; set; }
-
-        public void SendEquipMessage(Slots slot)
-        {
-            var equipMessage = new ClientInventoryMessage(slot, ClientInventoryUpdate.Equip);
-#pragma warning disable 618
-            SendNetworkMessage(equipMessage);
-#pragma warning restore 618
-        }
-
-        public void SendUseMessage(Slots slot)
-        {
-            var equipmessage = new ClientInventoryMessage(slot, ClientInventoryUpdate.Use);
-#pragma warning disable 618
-            SendNetworkMessage(equipmessage);
-#pragma warning restore 618
-        }
-
-        public void SendOpenStorageUIMessage(Slots slot)
-        {
-#pragma warning disable 618
-            SendNetworkMessage(new OpenSlotStorageUIMessage(slot));
-#pragma warning restore 618
-        }
     }
 }
--- Content.Client/Inventory/StrippableBoundUserInterface.cs
+++ Content.Client/Inventory/StrippableBoundUserInterface.cs
@@ -7,7 +7,6 @@ using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Localization;
 using Robust.Shared.ViewVariables;
-using static Content.Shared.Inventory.EquipmentSlotDefines;
 
 namespace Content.Client.Inventory
 {
