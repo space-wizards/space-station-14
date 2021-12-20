00b165848f212fcb8bbdc3f0c1f5b66d363436a7
pointer-to-null <91910481+pointer-to-null@users.noreply.github.com>
2021-12-20 21:48:20 +0300

    Displays the name of the map in the lobby.

--- Content.Server/GameTicking/GameTicker.Lobby.cs
+++ Content.Server/GameTicking/GameTicker.Lobby.cs
@@ -42,9 +42,16 @@ namespace Content.Server.GameTicking
                 return string.Empty;
             }
 
+            var mapName = Loc.GetString("game-ticker-no-map-selected");
+            var map = _gameMapManager.GetSelectedMap();
+            if(map != null)
+            {
+                mapName = map.MapName;
+            }
+
             var gmTitle = Preset.ModeTitle;
             var desc = Preset.Description;
-            return Loc.GetString("game-ticker-get-info-text",("gmTitle", gmTitle),("desc", desc));
+            return Loc.GetString("game-ticker-get-info-text",("mapName", mapName),("gmTitle", gmTitle),("desc", desc));
         }
 
         private TickerLobbyReadyEvent GetStatusSingle(ICommonSession player, LobbyPlayerStatus status)
