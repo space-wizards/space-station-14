b1f1b2f11fafcee3b98521248bab4a7e4b5729b9
moonheart08 <moonheart08@users.noreply.github.com>
2021-12-22 20:03:34 -0600

    Conditional map support

---
+++ Content.Server/Maps/Conditions/HolidayMapCondition.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using Content.Server.Holiday;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Log;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
+
+namespace Content.Server.Maps.Conditions;
+
+public class HolidayMapCondition : GameMapCondition
+{
+    [DataField("holidays")]
+    public string[] Holidays { get; } = default!;
+
+    public override bool Check(GameMapPrototype map)
+    {
+        var holidaySystem = EntitySystem.Get<HolidaySystem>();
+        Logger.Debug($"{Holidays.Any(holiday => holidaySystem.IsCurrentlyHoliday(holiday)) ^ Inverted}");
+
+        return Holidays.Any(holiday => holidaySystem.IsCurrentlyHoliday(holiday)) ^ Inverted;
+    }
+}
