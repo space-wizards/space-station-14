b54bb3a538b73973902b33c2bc8e1096a80fb8ac
FoLoKe <36813380+FoLoKe@users.noreply.github.com>
2021-12-26 05:32:01 +0300

    Moves spike to ECS (#5706)

--- Content.Shared/Nutrition/Components/SharedButcherableComponent.cs
+++ Content.Shared/Nutrition/Components/SharedButcherableComponent.cs
@@ -1,6 +1,8 @@
 using Content.Shared.DragDrop;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Robust.Shared.ViewVariables;
 
 namespace Content.Shared.Nutrition.Components
@@ -13,13 +15,23 @@ namespace Content.Shared.Nutrition.Components
     {
         public override string Name => "Butcherable";
 
+        //TODO: List for sub-products like animal-hides, organs and etc?
         [ViewVariables]
-        public string? MeatPrototype => _meatPrototype;
+        [DataField("meat", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string MeatPrototype = "FoodMeat";
 
         [ViewVariables]
-        [DataField("meat")]
-        private string? _meatPrototype;
+        [DataField("pieces")]
+        public int Pieces = 5;
 
+        /// <summary>
+        /// Prevents butchering same entity on two and more spikes simultaneously and multiple doAfters on the same Spike
+        /// </summary>
+        public bool BeingButchered;
+
+        // TODO: ECS this out!, my guess CanDropEvent should be client side only and then "ValidDragDrop" in the DragDropSystem needs a little touch
+        // But this may lead to creating client-side systems for every Draggable component subbed to CanDrop. Actually those systems could control
+        // CanDropOn behaviors as well (IDragDropOn)
         bool IDraggable.CanDrop(CanDropEvent args)
         {
             return true;
