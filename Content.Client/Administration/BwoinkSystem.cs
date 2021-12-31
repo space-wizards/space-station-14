043c9a3e613778dce497164ee64110c4f1c228a0
Efruit <602406+Efruit@users.noreply.github.com>
2021-12-30 22:47:23 -0500

    Administration: Add user tabs to the AHelp window

--- Content.Client/Administration/BwoinkSystem.cs
+++ Content.Client/Administration/BwoinkSystem.cs
@@ -1,12 +1,16 @@
 ﻿#nullable enable
 using System.Collections.Generic;
+using System.Linq;
+using Content.Client.Administration.Managers;
 using Content.Client.Administration.UI;
+using Content.Client.Administration.UI.CustomControls;
 using Content.Shared.Administration;
 using JetBrains.Annotations;
 using Robust.Client.Graphics;
 using Robust.Client.Player;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Player;
+using Robust.Shared.Localization;
 using Robust.Shared.Audio;
 using Robust.Shared.IoC;
 using Robust.Shared.Network;
@@ -16,17 +20,20 @@ namespace Content.Client.Administration
     [UsedImplicitly]
     public class BwoinkSystem : SharedBwoinkSystem
     {
+        [Dependency] private readonly IClientAdminManager _adminManager = default!;
         [Dependency] private readonly IPlayerManager _playerManager = default!;
         [Dependency] private readonly IClyde _clyde = default!;
 
-        private readonly Dictionary<NetUserId, BwoinkWindow> _activeWindowMap = new();
+        private BwoinkWindow? _adminWindow;
+        private BwoinkedWindow? _plainWindow;
+        private readonly Dictionary<NetUserId, BwoinkPanel> _activePanelMap = new();
 
         protected override void OnBwoinkTextMessage(BwoinkTextMessage message, EntitySessionEventArgs eventArgs)
         {
             base.OnBwoinkTextMessage(message, eventArgs);
             LogBwoink(message);
             // Actual line
-            var window = EnsureWindow(message.ChannelId);
+            var window = EnsurePanel(message.ChannelId);
             window.ReceiveLine(message.Text);
             // Play a sound if we didn't send it
             var localPlayer = _playerManager.LocalPlayer;
@@ -37,25 +44,57 @@ namespace Content.Client.Administration
             }
         }
 
-        public BwoinkWindow EnsureWindow(NetUserId channelId)
+        private BwoinkPanel EnsureAdmin(NetUserId channelId)
         {
-            if (!_activeWindowMap.TryGetValue(channelId, out var existingWindow))
+            if (_adminWindow is null)
+                _adminWindow = new BwoinkWindow(this);
+
+            if (!_activePanelMap.TryGetValue(channelId, out var existingPanel))
             {
-                _activeWindowMap[channelId] = existingWindow = new BwoinkWindow(channelId,
-                    _playerManager.SessionsDict.TryGetValue(channelId, out var otherSession)
-                        ? otherSession.Name
-                        : channelId.ToString());
+                _playerManager.SessionsDict.TryGetValue(channelId, out var otherSession);
+                existingPanel = new BwoinkPanel(this, channelId, otherSession?.Name ?? channelId.ToString());
+                _activePanelMap[channelId] = existingPanel;
             }
 
-            existingWindow.Open();
-            return existingWindow;
+            if (!_adminWindow.IsOpen)
+            {
+                _adminWindow.Open();
+
+                var csl = _adminWindow.ChannelSelector.PlayerItemList;
+                csl.ClearSelected();
+
+                var pi = csl.FirstOrDefault(i => ((PlayerInfo) i.Metadata!).SessionId == channelId);
+                if (pi is not null)
+                    pi.Selected = true;
+            }
+            return existingPanel;
         }
 
-        public void EnsureWindowForLocalPlayer()
+        private BwoinkPanel EnsurePlain(NetUserId channelId)
+        {
+            if (_plainWindow is null)
+            {
+                var bp = new BwoinkPanel(this, channelId, Loc.GetString("bwoink-user-title"));
+                _plainWindow = new BwoinkedWindow(bp);
+            }
+
+            _plainWindow.Open();
+            return _plainWindow.Bwoink!;
+        }
+
+        public BwoinkPanel EnsurePanel(NetUserId channelId)
+        {
+            if (_adminManager.HasFlag(AdminFlags.Adminhelp))
+                return EnsureAdmin(channelId);
+
+            return EnsurePlain(channelId);
+        }
+
+        public void EnsurePanelForLocalPlayer()
         {
             var localPlayer = _playerManager.LocalPlayer;
             if (localPlayer != null)
-                EnsureWindow(localPlayer.UserId);
+                EnsurePanel(localPlayer.UserId);
         }
 
         public void Send(NetUserId channelId, string text)
