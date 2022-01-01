73ef674e7a383b0da709380e5f9a20fd005a21cd
moonheart08 <moonheart08@users.noreply.github.com>
2022-01-01 00:04:44 -0600

    fix links again

--- Content.Client/Links/UILinks.cs
+++ Content.Client/Links/UILinks.cs
@@ -5,7 +5,7 @@
         public const string GitHub = "https://github.com/moonheart08/outer-rim-14/";
         public const string Patreon = "https://www.patreon.com/spacestation14";
 
-        public const string Discord = "https://discordapp.com/invite/t2jac3p";
+        public const string Discord = "https://discord.gg/jBPuBVkX";
         public const string BugReport = "https://github.com/moonheart08/outer-rim-14/issues/new/choose";
         public const string Website = "https://spacestation14.io";
     }
