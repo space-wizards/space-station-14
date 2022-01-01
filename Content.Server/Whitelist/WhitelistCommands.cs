de714072cb6db44b82f2e3be87828b2fdf31b0ce
Kara D <lunarautomaton6@gmail.com>
2021-12-31 20:09:46 -0700

    commands

--- Content.Server/Whitelist/WhitelistCommands.cs
+++ Content.Server/Whitelist/WhitelistCommands.cs
@@ -1,8 +1,14 @@
 ﻿using Content.Server.Administration;
 using Content.Server.Database;
 using Content.Shared.Administration;
+using Content.Shared.CCVar;
+using Robust.Server.Player;
+using Robust.Shared.Configuration;
 using Robust.Shared.Console;
 using Robust.Shared.IoC;
+using Robust.Shared.Localization;
+using Robust.Shared.Network;
+using Robust.Shared.Player;
 
 namespace Content.Server.Whitelist;
 
@@ -10,8 +16,8 @@ namespace Content.Server.Whitelist;
 public class AddWhitelistCommand : IConsoleCommand
 {
     public string Command => "whitelistadd";
-    public string Description => "a";
-    public string Help => "a";
+    public string Description => Loc.GetString("command-whitelistadd-description");
+    public string Help => Loc.GetString("command-whitelistadd-help");
     public async void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         if (args.Length != 1)
@@ -39,8 +45,8 @@ public class AddWhitelistCommand : IConsoleCommand
 public class RemoveWhitelistCommand : IConsoleCommand
 {
     public string Command => "whitelistremove";
-    public string Description => "a";
-    public string Help => "a";
+    public string Description => Loc.GetString("command-whitelistremove-description");
+    public string Help => Loc.GetString("command-whitelistremove-help");
     public async void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         if (args.Length != 1)
@@ -61,5 +67,39 @@ public class RemoveWhitelistCommand : IConsoleCommand
 
             await db.RemoveFromWhitelistAsync(guid);
         }
+    }
+}
+
+[AdminCommand(AdminFlags.Ban)]
+public class KickNonWhitelistedCommand : IConsoleCommand
+{
+    public string Command => "kicknonwhitelisted";
+    public string Description => Loc.GetString("command-kicknonwhitelisted-description");
+    public string Help => Loc.GetString("command-kicknonwhitelisted-help");
+    public async void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length != 0)
+            return;
+
+        var cfg = IoCManager.Resolve<IConfigurationManager>();
+
+        if (!cfg.GetCVar(CCVars.WhitelistEnabled))
+            return;
+
+        var player = IoCManager.Resolve<IPlayerManager>();
+        var db = IoCManager.Resolve<IServerDbManager>();
+        var net = IoCManager.Resolve<IServerNetManager>();
+
+        foreach (var session in player.NetworkedSessions)
+        {
+            if (await db.GetAdminDataForAsync(session.UserId) is not null)
+                continue;
+            
+            if (!await db.GetWhitelistStatusAsync(session.UserId.UserId))
+            {
+                net.DisconnectChannel(session.ConnectedClient, Loc.GetString("whitelist-not-whitelisted"));
+            }
+        }
+
     }
 }
