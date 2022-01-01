148f27f004281ef68a8be16bc8b15eb86e8807dc
moonheart08 <moonheart08@users.noreply.github.com>
2021-12-31 22:54:38 -0600

    Update the UI links.

--- Content.Client/Links/UILinks.cs
+++ Content.Client/Links/UILinks.cs
@@ -2,11 +2,11 @@
 {
     public static class UILinks
     {
-        public const string GitHub = "https://github.com/space-wizards/space-station-14/";
+        public const string GitHub = "https://github.com/moonheart08/outer-rim-14/";
         public const string Patreon = "https://www.patreon.com/spacestation14";
 
         public const string Discord = "https://discordapp.com/invite/t2jac3p";
-        public const string BugReport = "https://github.com/space-wizards/space-station-14/issues/new/choose";
+        public const string BugReport = "https://github.com/moonheart08/outer-rim-14/issues/new/choose";
         public const string Website = "https://spacestation14.io";
     }
 }
