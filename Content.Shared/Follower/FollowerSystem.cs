6cbd579f1ec8bb1f73f0894558b9aaa3b3699f76
Kara D <lunarautomaton6@gmail.com>
2022-01-03 14:12:08 -0700

    Follow verb for ghosts

---
+++ Content.Shared/Follower/FollowerSystem.cs
@@ -0,0 +1,59 @@
+﻿using Content.Shared.Database;
+using Content.Shared.Ghost;
+using Content.Shared.Movement.EntitySystems;
+using Content.Shared.Verbs;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Localization;
+using Robust.Shared.Log;
+
+namespace Content.Shared.Follower;
+
+public class FollowerSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<GetAlternativeVerbsEvent>(OnGetAlternativeVerbs);
+        SubscribeLocalEvent<FollowerComponent, RelayMoveInputEvent>(OnFollowerMove);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        foreach (var follower in EntityManager.EntityQuery<FollowerComponent>())
+        {
+            Logger.Debug($"Entity {follower} on {follower.Following}");
+            var xform = Transform(follower.Owner);
+            var targetXform = Transform(follower.Following);
+            xform.Coordinates = targetXform.Coordinates;
+        }
+    }
+
+    private void OnGetAlternativeVerbs(GetAlternativeVerbsEvent ev)
+    {
+        if (!HasComp<SharedGhostComponent>(ev.User))
+            return;
+
+        var verb = new Verb
+        {
+            Priority = 10,
+            Act = (() =>
+            {
+                var follower = EnsureComp<FollowerComponent>(ev.User);
+                follower.Following = ev.Target;
+            }),
+            Impact = LogImpact.Low,
+            Text = Loc.GetString("verb-follow-text"),
+            IconTexture = "/Textures/Interface/VerbIcons/open.svg.192dpi.png",
+        };
+
+        ev.Verbs.Add(verb);
+    }
+
+    private void OnFollowerMove(EntityUid uid, FollowerComponent component, RelayMoveInputEvent args)
+    {
+        RemComp<FollowerComponent>(uid);
+    }
+}
