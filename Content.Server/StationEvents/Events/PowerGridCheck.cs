9199f1a7ecd7f5ac8c9268088886c5c2cb6b378a
Efruit <602406+Efruit@users.noreply.github.com>
2022-01-03 01:51:59 -0500

    Events/PowerGridCheck: Stagger the shutdown/reboot
    Closes #3900

--- Content.Server/StationEvents/Events/PowerGridCheck.cs
+++ Content.Server/StationEvents/Events/PowerGridCheck.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Content.Server.Power.Components;
+using Content.Server.NodeContainer;
+using Content.Server.NodeContainer.NodeGroups;
 using JetBrains.Annotations;
 using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
@@ -28,7 +31,10 @@ namespace Content.Server.StationEvents.Events
 
         private CancellationTokenSource? _announceCancelToken;
 
-        private readonly List<EntityUid> _powered = new();
+        private readonly List<HashSet<NodeContainerComponent>> _powerStages = new();
+
+        [Dependency] private readonly IEntityManager _entityManager = default!;
+        [Dependency] private readonly IRobustRandom _random = default!;
 
         public override void Announce()
         {
@@ -36,32 +42,146 @@ namespace Content.Server.StationEvents.Events
             EndAfter = IoCManager.Resolve<IRobustRandom>().Next(60, 120);
         }
 
+        private const int SHUTOFF_TARGET_MS = 8000;
+        private const int REBOOT_TARGET_MS = 4000;
+
+        private static bool IsAcceptableNode(NodeGroupID i) => i is NodeGroupID.HVPower or NodeGroupID.MVPower or NodeGroupID.Apc;
+
         public override void Startup()
         {
-            var entityManager = IoCManager.Resolve<IEntityManager>();
+            // Everything we've ever seen. Used to avoid loops.
+            var seen = new HashSet<NodeContainerComponent>();
+            // Current sources. Not like electrical current, just the ones we're using right now.
+            var curr = new HashSet<NodeContainerComponent>();
+            // Current children, i.e. the next curr
+            var next = new HashSet<NodeContainerComponent>();
 
-            foreach (var component in entityManager.EntityQuery<ApcPowerReceiverComponent>(true))
+            // First we get the seed units
+            foreach (var (ngComp, pSupComp) in _entityManager.EntityQuery<NodeContainerComponent, PowerSupplierComponent>(true))
             {
-                component.PowerDisabled = true;
-                _powered.Add(component.Owner);
+                // We start with the HV networks
+                if (ngComp.Nodes.Values.Any(n => n.NodeGroupID == NodeGroupID.HVPower))
+                {
+                    curr.Add(ngComp);
+                }
             }
+
+            // Break 'em in to tiers
+            while (curr.Count > 0)
+            {
+                foreach (var cncc in curr)
+                {
+                    // Seen 'em? Skip em.
+                    if (!seen.Add(cncc))
+                        continue;
+
+                    // No breaking atmos nets, pls
+                    foreach (var n in cncc.Nodes.Values.Where(nn => IsAcceptableNode(nn.NodeGroupID)))
+                    {
+                        foreach (var rn in n.ReachableNodes)
+                        {
+                            // WHY DO WE HAVE TO PUT THIS SO MANY LAYERS DOWN
+                            // FUCK
+                            if (_entityManager.TryGetComponent<NodeContainerComponent>(rn.Owner, out var ncc))
+                                next.Add(ncc);
+                        }
+                    }
+                }
+
+                _powerStages.Add(curr);
+                curr = next;
+                next = new();
+            }
+
+            if (next.Count > 0)
+                _powerStages.Add(next);
+
+            DoShutoff(0);
 
             base.Startup();
         }
 
+        private void Toggle(EntityUid owner, bool off)
+        {
+            if (_entityManager.Deleted(owner))
+                return;
+
+            // We try to be as component-agnostic as possible here, so we try everything.
+
+            if (_entityManager.TryGetComponent<PowerSupplierComponent>(owner, out var psc))
+                psc.Enabled = !off;
+
+            // Nothing to shut off?
+            /*
+            if (_entityManager.TryGetComponent<PowerConsumerComponent>(pns.Owner, out var pcc))
+                pcc.
+            */
+
+            if (_entityManager.TryGetComponent<PowerNetworkBatteryComponent>(owner, out var pnbc))
+                pnbc.Enabled = !off;
+
+            // Nothing to shut off here either, I guess
+            /*
+            if (_entityManager.TryGetComponent<ApcPowerProviderComponent>(pns.Owner, out var appc))
+                appc.
+            */
+
+            if (_entityManager.TryGetComponent<ApcPowerReceiverComponent>(owner, out var aprc))
+                aprc.PowerDisabled = off;
+        }
+
+        private void DoShutoff(int tier, bool fast = false)
+        {
+            if (tier >= _powerStages.Count)
+                return;
+
+            foreach (var pns in _powerStages[tier])
+                Toggle(pns.Owner, true);
+
+            if (fast)
+                DoShutoff(tier+1);
+            else
+            {
+                // The (rough) goal for how long each stage should take to shut down
+                var tgt = SHUTOFF_TARGET_MS / _powerStages.Count;
+                // Half the target
+                var htgt = (int) (0.5f * tgt);
+
+                // The actual next time will be fudged ±50% of the target.
+                var nt = _random.Next(tgt - htgt, tgt + htgt);
+                Timer.Spawn(nt, () => DoShutoff(tier+1));
+            }
+        }
+
+        // NOTE: Fast is provided in case we ever add a way to abort an event.
+        // It's mostly relevant for this, but I added it to DoShutoff too just for good measure.
+        private void DoReboot(int tier, bool fast = false)
+        {
+            if (tier < 0)
+            {
+                _powerStages.Clear();
+                return;
+            }
+
+            foreach (var pns in _powerStages[tier])
+                Toggle(pns.Owner, false);
+
+            if (fast)
+                DoReboot(tier - 1);
+            else
+            {
+                // Same as the DoShutoff version.
+                var tgt = REBOOT_TARGET_MS / _powerStages.Count;
+                var htgt = (int) (0.5f * tgt);
+
+                var nt = _random.Next(tgt - htgt, tgt + htgt);
+                Timer.Spawn(nt, () => DoReboot(tier-1));
+            }
+        }
+
         public override void Shutdown()
         {
-            var entMan = IoCManager.Resolve<IEntityManager>();
-
-            foreach (var entity in _powered)
-            {
-                if (entMan.Deleted(entity)) continue;
-
-                if (entMan.TryGetComponent(entity, out ApcPowerReceiverComponent? powerReceiverComponent))
-                {
-                    powerReceiverComponent.PowerDisabled = false;
-                }
-            }
+            DoReboot(_powerStages.Count - 1);
 
             // Can't use the default EndAudio
             _announceCancelToken?.Cancel();
@@ -70,7 +190,6 @@ namespace Content.Server.StationEvents.Events
             {
                 SoundSystem.Play(Filter.Broadcast(), "/Audio/Announcements/power_on.ogg");
             }, _announceCancelToken.Token);
-            _powered.Clear();
 
             base.Shutdown();
         }
