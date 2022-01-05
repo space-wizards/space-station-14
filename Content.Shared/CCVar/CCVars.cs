4a3d5c01a09988065da23c81f8df612c705b63ba
moonheart08 <moonheart08@users.noreply.github.com>
2022-01-05 11:17:58 -0600

    Disable the pre-round lobby.

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -73,7 +73,7 @@ namespace Content.Shared.CCVar
         ///     Controls the duration of the lobby timer in seconds. Defaults to 2 minutes and 30 seconds.
         /// </summary>
         public static readonly CVarDef<int>
-            GameLobbyDuration = CVarDef.Create("game.lobbyduration", 150, CVar.ARCHIVE);
+            GameLobbyDuration = CVarDef.Create("game.lobbyduration", 0, CVar.ARCHIVE);
 
         /// <summary>
         ///     Controls if players can latejoin at all.
