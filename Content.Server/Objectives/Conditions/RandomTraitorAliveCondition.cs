2a90441d1d90b172d1619381d426b467ba0d5e85
Elijah Evanoff <elijahrane@gmail.com>
2022-01-04 22:44:48 -0500

    Removed redundant file

--- Content.Server/Objectives/Conditions/RandomTraitorAliveCondition.cs
+++ Content.Server/Objectives/Conditions/RandomTraitorAliveCondition.cs
@@ -1,22 +1,23 @@
 ﻿using System.Collections.Generic;
 using Content.Server.Mind.Components;
 using Content.Server.Objectives.Interfaces;
-using Content.Shared.MobState.Components;
 using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Random;
+using Robust.Shared.Localization;
+using Robust.Shared.Utility;
 using Robust.Shared.Serialization.Manager.Attributes;
-using Content.Server.Objectives.Interfaces;
 using Content.Server.Traitor;
 
 namespace Content.Server.Objectives.Conditions
 {
-    [UsedImplicitly]
     [DataDefinition]
-    public class RandomTraitorAliveCondition : OtherTraitorAliveCondition
+    public class RandomTraitorAliveCondition : IObjectiveCondition
     {
-        public override IObjectiveCondition GetAssigned(Mind.Mind mind)
+        protected Mind.Mind? Target;
+        
+        public IObjectiveCondition GetAssigned(Mind.Mind mind)
         {
             var entityMgr = IoCManager.Resolve<IEntityManager>();
             List<Mind.Mind> _allOtherTraitors = new List<Mind.Mind>();
@@ -30,6 +31,50 @@ namespace Content.Server.Objectives.Conditions
             }
  
             return new RandomTraitorAliveCondition {Target = IoCManager.Resolve<IRobustRandom>().Pick(_allOtherTraitors)};
+        }
+
+        public string Title
+        {
+            get
+            {
+                var targetName = string.Empty;
+
+                if (Target == null)
+                    return Loc.GetString("objective-condition-other-traitor-alive-title", ("targetName", targetName));
+
+                if (Target.CharacterName != null)
+                    targetName = Target.CharacterName;
+                else if (Target.OwnedEntity is {Valid: true} owned)
+                    targetName = IoCManager.Resolve<IEntityManager>().GetComponent<MetaDataComponent>(owned).EntityName;
+
+                return Loc.GetString("objective-condition-other-traitor-alive-title", ("targetName", targetName));
+            }
+        }
+
+        public string Description => Loc.GetString("objective-condition-other-traitor-alive-description");
+
+        public SpriteSpecifier Icon => new SpriteSpecifier.Rsi(new ResourcePath("Objects/Misc/bureaucracy.rsi"), "folder_red");
+
+        public float Progress => (!Target?.CharacterDeadIC ?? true) ? 1f : 0f;
+
+        public float Difficulty => 1.75f;
+
+        public bool Equals(IObjectiveCondition? other)
+        {
+            return other is RandomTraitorAliveCondition kpc && Equals(Target, kpc.Target);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((RandomTraitorAliveCondition) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Target?.GetHashCode() ?? 0;
         }
     }
 }
