b187002b85d1cbeb54bbf53106993d205a9906b5
ElectroJr <leonsfriedrich@gmail.com>
2022-01-07 15:20:07 +1300

    well if githubs gonna ignore the git mv, might as well use file-namespaces

--- Content.Shared/Foldable/FoldableComponent.cs
+++ Content.Shared/Foldable/FoldableComponent.cs
@@ -6,37 +6,35 @@ using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.ViewVariables;
 using System;
 
-namespace Content.Shared.Foldable
+namespace Content.Shared.Foldable;
+
+/// <summary>
+/// Used to create "foldable structures" that you can pickup like an item when folded. Used for rollerbeds and wheelchairs
+/// </summary>
+[RegisterComponent]
+[NetworkedComponent]
+[Friend(typeof(SharedFoldableSystem))]
+public class FoldableComponent : Component
 {
-    /// <summary>
-    /// Used to create "foldable structures" that you can pickup like an item when folded. Used for rollerbeds and wheelchairs
-    /// </summary>
-    [RegisterComponent]
-    [NetworkedComponent]
-    [Friend(typeof(SharedFoldableSystem))]
-    public class FoldableComponent : Component
+    public override string Name => "Foldable";
+
+    [DataField("folded")]
+    [ViewVariables]
+    public bool IsFolded = false;
+
+    public bool CanBeFolded = true;
+}
+
+// ahhh, the ol' "state thats just a copy of the component".
+[Serializable, NetSerializable]
+public class FoldableComponentState : ComponentState
+{
+    public readonly bool IsFolded;
+    public readonly bool CanBeFolded;
+
+    public FoldableComponentState(bool isFolded, bool canBeFolded)
     {
-        public override string Name => "Foldable";
-
-        [DataField("folded")]
-        [ViewVariables]
-        public bool IsFolded = false;
-
-        public bool CanBeFolded = true;
-    }
-
-    // ahhh, the ol' "state thats just a copy of the component".
-    [Serializable, NetSerializable]
-    public class FoldableComponentState : ComponentState
-    {
-
-        public readonly bool IsFolded;
-        public readonly bool CanBeFolded;
-
-        public FoldableComponentState(bool isFolded, bool canBeFolded)
-        {
-            IsFolded = isFolded;
-            CanBeFolded = canBeFolded;
-        }
+        IsFolded = isFolded;
+        CanBeFolded = canBeFolded;
     }
 }
