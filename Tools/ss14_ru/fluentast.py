17448e044de6031f00d39b3b32d9ce8a2020008e
SpaceCarp <admin@spacestation14.ru>
2022-01-07 22:43:16 +0300

    add: Добавлены переводы от Lekar, ga add: Добавлен файл defaultwindow.ftl из core fix: Актуализированы ключи русской локали после пулла основного репозитория

--- Tools/ss14_ru/fluentast.py
+++ Tools/ss14_ru/fluentast.py
@@ -1,7 +1,12 @@
+import typing
+
 from fluent.syntax import ast, FluentParser, FluentSerializer
+from lokalisemodels import LokaliseKey
 from pydash import py_
 
+
 class FluentAstAbstract:
+    element = None
     @classmethod
     def get_id_name(cls, element):
         if isinstance(element, ast.Junk):
@@ -11,18 +16,32 @@ class FluentAstAbstract:
         else:
             return None
 
+    @classmethod
+    def create_element(cls, element):
+        if isinstance(element, ast.Junk):
+            cls.element = FluentAstJunk(element)
+            return cls.element
+        elif isinstance(element, ast.Message):
+            cls.element = FluentAstMessage(element)
+            return cls.element
+        else:
+            return None
+
+
 class FluentAstMessage:
     def __init__(self, message: ast.Message):
         self.message = message
+        self.element = message
 
     def get_id_name(self):
         return self.message.id.name
 
 
 class FluentAstAttribute:
-    def __init__(self, id, value):
+    def __init__(self, id, value, parent_key = None):
         self.id = id
         self.value = value
+        self.parent_key = parent_key
 
 
 class FluentAstAttributeFactory:
@@ -40,9 +59,11 @@ class FluentAstAttributeFactory:
 
         return attrs
 
+
 class FluentAstJunk:
     def __init__(self, junk: ast.Junk):
         self.junk = junk
+        self.element = junk
 
     def get_id_name(self):
         return self.junk.content.split('=')[0].strip()
@@ -50,7 +71,7 @@ class FluentAstJunk:
 
 class FluentSerializedMessage:
     @classmethod
-    def from_yaml_element(cls, id, value, attributes, parent_id = None):
+    def from_yaml_element(cls, id, value, attributes, parent_id = None, raw_key = False):
         if not value and not id and not parent_id:
             return None
 
@@ -60,13 +81,13 @@ class FluentSerializedMessage:
             else:
                 return None
 
-        message = f'{cls.get_key(id)} = {cls.get_value(value, parent_id)}\n'
+        message = f'{cls.get_key(id, raw_key)} = {cls.get_value(value, parent_id)}\n'
 
         if attributes and len(attributes):
             full_message = message
 
             for attr in attributes:
-                full_message = cls.add_attr(full_message, attr.id, attr.value)
+                full_message = cls.add_attr(full_message, attr.id, attr.value, raw_key=raw_key)
 
             desc_attr = py_.find(attributes, lambda a: a.id == 'desc')
             if not desc_attr and parent_id:
@@ -75,6 +96,46 @@ class FluentSerializedMessage:
             return full_message
 
         return cls.to_serialized_message(message)
+
+    @classmethod
+    def from_lokalise_keys(cls, keys: typing.List[LokaliseKey]):
+        attributes_keys = list(filter(lambda k: k.is_attr, keys))
+        attributes = list(map(lambda k: FluentAstAttribute(id='.{name}'.format(name=k.get_key_last_name(k.key_name)),
+                                                           value=FluentSerializedMessage.get_attr(k, k.get_key_last_name(k.key_name)), parent_key=k.get_parent_key()),
+                              attributes_keys))
+        attributes_group = py_.group_by(attributes, 'parent_key')
+
+        serialized_message = ''
+        for key in keys:
+            if key.is_attr:
+                continue
+            key_name = key.get_key_last_name(key.key_name)
+            key_value = key.get_translation('ru').data['translation']
+            key_attributes = []
+
+            if len(attributes_group):
+                k = f'{key.get_key_base_name(key.key_name)}.{key_name}'
+                key_attributes = attributes_group[k] if k in attributes_group else []
+
+            message = key.serialize_message()
+            full_message = cls.from_yaml_element(key_name, key_value, key_attributes, key.get_parent_key(), True)
+
+            if full_message:
+                serialized_message = serialized_message + '\n' + full_message
+            elif message:
+                serialized_message = serialized_message + '\n' + message
+            else:
+                raise Exception('Что-то пошло не так')
+
+        return serialized_message
+
+    @staticmethod
+    def get_attr(k, name, parent_id = None):
+        if parent_id:
+            return "{ " + parent_id + f'.{name}' + " }"
+        else:
+            return k.get_translation('ru').data['translation']
+
 
     @staticmethod
     def to_serialized_message(string_message):
@@ -87,8 +148,9 @@ class FluentSerializedMessage:
         return serialized if serialized else ''
 
     @staticmethod
-    def add_attr(message_str, attr_key, attr_value):
-        return f'{message_str}\n  .{attr_key} = {attr_value}'
+    def add_attr(message_str, attr_key, attr_value, raw_key = False):
+        prefix = '' if raw_key else '.'
+        return f'{message_str}\n  {prefix}{attr_key} = {attr_value}'
 
     @staticmethod
     def get_value(value, parent_id):
@@ -100,6 +162,8 @@ class FluentSerializedMessage:
             return '{ "" }'
 
     @staticmethod
-    def get_key(id):
-        return f'ent-{id}'
-
+    def get_key(id, raw = False):
+        if raw:
+            return f'{id}'
+        else:
+            return f'ent-{id}'
