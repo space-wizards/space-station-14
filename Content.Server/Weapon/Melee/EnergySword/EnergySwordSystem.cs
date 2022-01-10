72df9119f3814f33119a4fb0249f4b7d29263a16
Júlio César Ueti <j.cesarueti@yahoo.com.br>
2022-01-10 00:54:43 -0300

    Code clean up and removal of extra PointLight component in yaml

--- Content.Server/Weapon/Melee/EnergySword/EnergySwordSystem.cs
+++ Content.Server/Weapon/Melee/EnergySword/EnergySwordSystem.cs
@@ -125,23 +125,20 @@ namespace Content.Server.Weapon.Melee.Esword
             if (comp.Hacked == true || !_blockerSystem.CanInteract(args.User))
                 return;
 
-            if (TryComp(args.Used, out ToolComponent? tool))
+            if (TryComp(args.Used, out ToolComponent? tool) && tool.Qualities.ContainsAny("Pulsing"))
             {
-                if (tool.Qualities.ContainsAny("Pulsing"))
+                comp.Hacked = true;
+
+                if (comp.Activated == true && TryComp(comp.Owner, out SpriteComponent? sprite)
+                    && TryComp(comp.Owner, out SharedItemComponent? item))
                 {
-                    comp.Hacked = true;
+                    sprite.LayerSetVisible(1, false);
+                    sprite.LayerSetColor(1, Color.White);
+                    //todo: figure out how to use the RGBLightControllerSystem to phase out the rainbow sprite AND add lights.
+                    sprite.LayerSetState(0, "e_sword_rainbow_on");
 
-                    if (comp.Activated == true && TryComp(comp.Owner, out SpriteComponent? sprite)
-                        && TryComp(comp.Owner, out SharedItemComponent? item))
-                    {
-                        sprite.LayerSetVisible(1, false);
-                        sprite.LayerSetColor(1, Color.White);
-                        //todo: figure out how to use the RGBLightControllerSystem to phase out the rainbow sprite AND add lights.
-                        sprite.LayerSetState(0, "e_sword_rainbow_on");
-
-                        item.Color = Color.White;
-                        item.EquippedPrefix = "on-rainbow";
-                    }
+                    item.Color = Color.White;
+                    item.EquippedPrefix = "on-rainbow";
                 }
             }
         }
