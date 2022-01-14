0d6cd7ba2cd8aa179666452c4756fbe527e0900d
ElectroJr <leonsfriedrich@gmail.com>
2022-01-14 21:44:16 +1300

    fix delete-spawn

--- Content.Server/Power/EntitySystems/CableSystem.cs
+++ Content.Server/Power/EntitySystems/CableSystem.cs
@@ -45,6 +45,11 @@ public class CableSystem : EntitySystem
         if (args.Anchored)
             return; // huh? it wasn't anchored?
 
+        // anchor state can change as a result of deletion (detach to null).
+        // We don't want to spawn an entity when deleted.
+        if (!TryLifeStage(uid, out var life) || life >= EntityLifeStage.Terminating)
+            return;
+
         // This entity should not be un-anchorable. But this can happen if the grid-tile is deleted (RCD, explosion,
         // etc). In that case: behave as if the cable had been cut.
         Spawn(cable.CableDroppedOnCutPrototype, Transform(uid).Coordinates);
