e3be84b5f89c7aac4fead1e168724cc7380ac114
mirrorcult <lunarautomaton6@gmail.com>
2022-01-15 15:43:14 -0700

    Multi-map and multi-station gameticker loading (#6167)

---
+++ Content.Server/Station/Components/PartOfStationComponent.cs
@@ -0,0 +1,18 @@
+﻿using Content.Server.GameTicking;
+using Robust.Shared.Analyzers;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+
+namespace Content.Server.Station;
+
+/// <summary>
+///     Added to grids saved in maps to designate them as 'part of a station' and not main grids. I.e. ancillary
+///     shuttles for multi-grid stations.
+/// </summary>
+[RegisterComponent, ComponentProtoName("PartOfStation")]
+[Friend(typeof(GameTicker))]
+public class PartOfStationComponent : Component
+{
+    [DataField("id", required: true)] // does yamllinter even lint maps for required fields?
+    public string Id = default!;
+}
