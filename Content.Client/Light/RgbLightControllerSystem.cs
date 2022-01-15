cf4336d959e88affe577a218cc6c470532eb6cbc
ElectroJr <leonsfriedrich@gmail.com>
2022-01-15 21:07:04 +1300

    Improved gamer gear

--- Content.Client/Light/RgbLightControllerSystem.cs
+++ Content.Client/Light/RgbLightControllerSystem.cs
@@ -1,15 +1,17 @@
 using System;
+using Content.Shared.Item;
 using Content.Shared.Light;
 using Content.Shared.Light.Component;
 using Robust.Client.GameObjects;
 using Robust.Shared.GameObjects;
+using Robust.Shared.GameStates;
 using Robust.Shared.IoC;
 using Robust.Shared.Maths;
 using Robust.Shared.Timing;
 
 namespace Content.Client.Light
 {
-    public class RgbLightControllerSystem : EntitySystem
+    public sealed class RgbLightControllerSystem : SharedRgbLightControllerSystem
     {
         [Dependency] private IGameTiming _gameTiming = default!;
 
@@ -17,24 +19,78 @@ namespace Content.Client.Light
         {
             base.Initialize();
 
-            UpdatesOutsidePrediction = true;
+            SubscribeLocalEvent<RgbLightControllerComponent, ComponentHandleState>(OnHandleState);
+            SubscribeLocalEvent<RgbLightControllerComponent, ComponentRemove>(OnComponentRemoved);
         }
 
-        public override void Update(float frameTime)
+        private void OnComponentRemoved(EntityUid uid, RgbLightControllerComponent rgb, ComponentRemove args)
         {
-            base.Update(frameTime);
-            var curTime = _gameTiming.CurTime;
+            if (TryComp(uid, out PointLightComponent? light))
+                light.Color = Color.White;
+
+            if (TryComp(uid, out SharedItemComponent? item))
+                item.Color = Color.White;
+
+            ResetSpriteColors(uid, rgb);
+        }
+
+        private void OnHandleState(EntityUid uid, RgbLightControllerComponent rgb, ref ComponentHandleState args)
+        {
+            if (args.Current is not RgbLightControllerState state)
+                return;
+
+            // just to be safe, un-colour the layers so they don't get stuck with some mid-transition one.
+            ResetSpriteColors(uid, rgb);
+
+            rgb.CycleRate = state.CycleRate;
+            rgb.Layers = state.Layers;
+        }
+
+        private void ResetSpriteColors(EntityUid uid, RgbLightControllerComponent? rgb = null, SpriteComponent? sprite = null)
+        {
+            if (!Resolve(uid, ref rgb, ref sprite))
+                return;
+
+            if (rgb.Layers == null)
+            {
+                sprite.Color = Color.White;
+                return;
+            }
+
+            foreach (var layer in rgb.Layers)
+            {
+                sprite.LayerSetColor(layer, Color.White);
+            }
+        }
+
+        public override void FrameUpdate(float frameTime)
+        {
             foreach (var (rgb, light, sprite) in EntityManager.EntityQuery<RgbLightControllerComponent, PointLightComponent, SpriteComponent>())
             {
-                light.Color = GetCurrentRgbColor(curTime, TimeSpan.FromSeconds(rgb.CreationTick.Value * _gameTiming.TickPeriod.TotalSeconds), rgb);
-                sprite.Color = GetCurrentRgbColor(curTime, TimeSpan.FromSeconds(rgb.CreationTick.Value * _gameTiming.TickPeriod.TotalSeconds), rgb);
+                var colour = GetCurrentRgbColor(_gameTiming.RealTime, rgb.CreationTick.Value * _gameTiming.TickPeriod, rgb);
+
+                light.Color = colour;
+
+                if (rgb.Layers == null)
+                    sprite.Color = colour;
+                else
+                {
+                    foreach (var layer in rgb.Layers)
+                    {
+                        sprite.LayerSetColor(layer, colour);
+                    }
+                }
+
+                // not all rgb is hand-held (Hence, not part of EntityQuery)
+                if (TryComp(rgb.Owner, out SharedItemComponent? item))
+                    item.Color = colour;
             }
         }
 
         public static Color GetCurrentRgbColor(TimeSpan curTime, TimeSpan offset, RgbLightControllerComponent rgb)
         {
             return Color.FromHsv(new Vector4(
-                (float) (((curTime.TotalSeconds - offset.TotalSeconds) / rgb.CycleRate + Math.Abs(rgb.Owner.GetHashCode() * 0.1)) % 1),
+                (float) (((curTime.TotalSeconds - offset.TotalSeconds) * rgb.CycleRate + Math.Abs(rgb.Owner.GetHashCode() * 0.1)) % 1),
                 1.0f,
                 1.0f,
                 1.0f
---
+++ Content.Shared/Light/SharedRgbLightControllerSystem.cs
@@ -0,0 +1,38 @@
+using Content.Shared.Light.Component;
+using Robust.Shared.GameObjects;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Light;
+
+public abstract class SharedRgbLightControllerSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<RgbLightControllerComponent, ComponentGetState>(OnGetState);
+    }
+
+    private void OnGetState(EntityUid uid, RgbLightControllerComponent component, ref ComponentGetState args)
+    {
+        args.State = new RgbLightControllerState(component.CycleRate, component.Layers);
+    }
+
+    public void SetLayers(EntityUid uid, int[] layers,  RgbLightControllerComponent? rgb = null)
+    {
+        if (!Resolve(uid, ref rgb))
+            return;
+
+        rgb.Layers = layers;
+        rgb.Dirty();
+    }
+
+    public void SetCycleRate(EntityUid uid, float rate, RgbLightControllerComponent? rgb = null)
+    {
+        if (!Resolve(uid, ref rgb))
+            return;
+
+        rgb.CycleRate = rate;
+        rgb.Dirty();
+    }
+}
