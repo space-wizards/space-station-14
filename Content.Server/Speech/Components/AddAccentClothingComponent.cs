16a532137d9b1fefd1c5c6b0efde581721d8a88f
Alex Evgrashin <aevgrashin@yandex.ru>
2022-01-19 01:18:22 +0300

    Muzzle (#6194)
    Co-authored-by: mirrorcult <lunarautomaton6@gmail.com>

---
+++ Content.Server/Speech/Components/AddAccentClothingComponent.cs
@@ -0,0 +1,32 @@
+﻿using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server.Speech.Components;
+
+/// <summary>
+///     Applies accent to user while they wear entity as a clothing.
+/// </summary>
+[RegisterComponent]
+public class AddAccentClothingComponent : Component
+{
+    public override string Name => "AddAccentClothing";
+
+    /// <summary>
+    ///     Component name for accent that will be applied.
+    /// </summary>
+    [DataField("accent", required: true)]
+    public string Accent = default!;
+
+    /// <summary>
+    ///     What <see cref="ReplacementAccentPrototype"/> to use.
+    ///     Will be applied only with <see cref="ReplacementAccentComponent"/>.
+    /// </summary>
+    [DataField("replacement", customTypeSerializer: typeof(PrototypeIdSerializer<ReplacementAccentPrototype>))]
+    public string? ReplacementPrototype;
+
+    /// <summary>
+    ///     Is that clothing is worn and affecting someones accent?
+    /// </summary>
+    public bool IsActive = false;
+}
