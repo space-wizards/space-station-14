bb00c2307a0b9daabf533594f3d946bd755a5795
Alexander Evgrashin <aevgrashin@yandex.ru>
2022-01-23 00:54:57 +0300

    Add radiation artifacts

---
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Components/RadiateArtifactComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameObjects;
+
+namespace Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
+
+[RegisterComponent]
+public class RadiateArtifactComponent : Component
+{
+    public override string Name => "RadiateArtifact";
+}
---
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/RadiateArtifactSystem.cs
@@ -0,0 +1,23 @@
+﻿using Content.Server.Radiation;
+using Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
+using Content.Server.Xenoarchaeology.XenoArtifacts.Events;
+using Robust.Shared.GameObjects;
+
+namespace Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Systems;
+
+public class RadiateArtifactSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<RadiateArtifactComponent, ArtifactActivatedEvent>(OnActivate);
+    }
+
+    private void OnActivate(EntityUid uid, RadiateArtifactComponent component, ArtifactActivatedEvent args)
+    {
+        var transform = Transform(uid);
+
+        var pulse = EntityManager.SpawnEntity("RadiationPulse", transform.Coordinates);
+        EntityManager.GetComponent<RadiationPulseComponent>(pulse).DoPulse();
+    }
+}
