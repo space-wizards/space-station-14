a1d5db7be80e184b4a5ce84fba019ddf2a4983e5
Alexander Evgrashin <aevgrashin@yandex.ru>
2022-01-22 04:20:44 +0300

    Windows knock

--- Content.Server/Window/WindowComponent.cs
+++ Content.Server/Window/WindowComponent.cs
@@ -1,34 +1,16 @@
 using System;
-using Content.Server.Destructible;
-using Content.Server.Destructible.Thresholds.Triggers;
-using Content.Server.Popups;
-using Content.Shared.Audio;
-using Content.Shared.Damage;
-using Content.Shared.Examine;
-using Content.Shared.Interaction;
-using Content.Shared.Rounding;
 using Content.Shared.Sound;
 using Content.Shared.Window;
-using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
-using Robust.Shared.Localization;
-using Robust.Shared.Player;
 using Robust.Shared.Serialization.Manager.Attributes;
-using Robust.Shared.Timing;
-using Robust.Shared.Utility;
 using Robust.Shared.ViewVariables;
 
 namespace Content.Server.Window
 {
     [RegisterComponent]
     [ComponentReference(typeof(SharedWindowComponent))]
-#pragma warning disable 618
-    public class WindowComponent : SharedWindowComponent, IExamine, IInteractHand
-#pragma warning restore 618
+    public class WindowComponent : SharedWindowComponent
     {
-        [ViewVariables(VVAccess.ReadWrite)] private TimeSpan _lastKnockTime;
-
         [DataField("knockDelay")]
         [ViewVariables(VVAccess.ReadWrite)]
         public TimeSpan KnockDelay = TimeSpan.FromSeconds(0.5);
@@ -40,25 +22,7 @@ namespace Content.Server.Window
         [DataField("knockSound")]
         public SoundSpecifier KnockSound = new SoundPathSpecifier("/Audio/Effects/glass_knock.ogg");
 
-        void IExamine.Examine(FormattedMessage message, bool inDetailsRange)
-        {
-        }
-
-        bool IInteractHand.InteractHand(InteractHandEventArgs eventArgs)
-        {
-            /*if (RateLimitedKnocking && _gameTiming.CurTime < _lastKnockTime + KnockDelay)
-            {
-                return false;
-            }
-
-            SoundSystem.Play(
-                Filter.Pvs(eventArgs.Target), KnockSound.GetSound(),
-                _entMan.GetComponent<TransformComponent>(eventArgs.Target).Coordinates, AudioHelpers.WithVariation(0.05f));
-            eventArgs.Target.PopupMessageEveryone(Loc.GetString("comp-window-knock"));
-
-            _lastKnockTime = _gameTiming.CurTime;*/
-
-            return true;
-        }
+        [ViewVariables(VVAccess.ReadWrite)]
+        public TimeSpan LastKnockTime;
     }
 }
--- Content.Server/Window/WindowSystem.cs
+++ Content.Server/Window/WindowSystem.cs
@@ -1,33 +1,39 @@
-﻿using System;
-using System.Linq;
-using Content.Server.Destructible;
-using Content.Server.Destructible.Thresholds.Triggers;
-using Content.Shared.Damage;
-using Content.Shared.Examine;
-using Content.Shared.Rounding;
+﻿using Content.Server.Popups;
+using Content.Shared.Audio;
+using Content.Shared.Interaction;
+using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
+using Robust.Shared.Localization;
+using Robust.Shared.Player;
 using Robust.Shared.Timing;
 
 namespace Content.Server.Window;
 
 public class WindowSystem : EntitySystem
 {
-    [Dependency] private readonly IEntityManager _entMan = default!;
     [Dependency] private readonly IGameTiming _gameTiming = default!;
 
     public override void Initialize()
     {
         base.Initialize();
+        SubscribeLocalEvent<WindowComponent, InteractHandEvent>(OnInteractHand);
     }
 
-    private void OnExamine(EntityUid uid, WindowComponent component, ExaminedEvent args)
+    private void OnInteractHand(EntityUid uid, WindowComponent component, InteractHandEvent args)
     {
-        if (!TryComp(uid, out DamageableComponent? damageable) ||
-            !TryComp(uid, out DestructibleComponent? destructible))
-        {
+        if (args.Handled)
             return;
-        }
 
+        if (component.RateLimitedKnocking && _gameTiming.CurTime < component.LastKnockTime + component.KnockDelay)
+            return;
+
+        SoundSystem.Play(Filter.Pvs(args.Target), component.KnockSound.GetSound(),
+            Transform(args.Target).Coordinates, AudioHelpers.WithVariation(0.05f));
+
+        args.Target.PopupMessageEveryone(Loc.GetString("comp-window-knock"));
+
+        component.LastKnockTime = _gameTiming.CurTime;
+        args.Handled = true;
     }
 }
