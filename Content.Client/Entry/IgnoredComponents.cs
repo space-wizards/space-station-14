b7d797ea1d07e46ab9c295a953eafea98ace2245
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-01-29 17:25:38 +1100

    ECS cluster nades

--- Content.Client/Entry/IgnoredComponents.cs
+++ Content.Client/Entry/IgnoredComponents.cs
@@ -236,7 +236,7 @@ namespace Content.Client.Entry
             "LightReplacer",
             "SecretStash",
             "Toilet",
-            "ClusterFlash",
+            "ClusterGrenade",
             "Repairable",
             "SolutionTransfer",
             "Evaporation",
