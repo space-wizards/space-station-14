b5724feb275060955d36fc4d36b78cb9d5a0dee0
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2022-01-30 17:39:46 +1100

    ECS guns (#6229)
    Co-authored-by: ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>

--- Content.Server/Weapon/Ranged/Barrels/Components/RevolverBarrelComponent.cs
+++ Content.Server/Weapon/Ranged/Barrels/Components/RevolverBarrelComponent.cs
@@ -1,20 +1,13 @@
 using System;
-using System.Threading.Tasks;
 using Content.Server.Weapon.Ranged.Ammunition.Components;
-using Content.Shared.Interaction;
-using Content.Shared.Popups;
 using Content.Shared.Sound;
 using Content.Shared.Weapons.Ranged.Barrels.Components;
-using Robust.Shared.Audio;
+using Robust.Shared.Analyzers;
 using Robust.Shared.Containers;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameStates;
-using Robust.Shared.IoC;
-using Robust.Shared.Localization;
 using Robust.Shared.Map;
-using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Random;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
@@ -22,248 +15,53 @@ using Robust.Shared.ViewVariables;
 
 namespace Content.Server.Weapon.Ranged.Barrels.Components
 {
-    [RegisterComponent]
-    [NetworkedComponent()]
-    public sealed class RevolverBarrelComponent : ServerRangedBarrelComponent, IUse, IInteractUsing, ISerializationHooks
+    [RegisterComponent, ComponentProtoName("RevolverBarrel"), NetworkedComponent, ComponentReference(typeof(ServerRangedBarrelComponent))]
+    public sealed class RevolverBarrelComponent : ServerRangedBarrelComponent, ISerializationHooks
     {
-        [Dependency] private readonly IRobustRandom _random = default!;
-
-        public override string Name => "RevolverBarrel";
-
         [ViewVariables]
         [DataField("caliber")]
-        private BallisticCaliber _caliber = BallisticCaliber.Unspecified;
+        public BallisticCaliber Caliber = BallisticCaliber.Unspecified;
 
-        private Container _ammoContainer = default!;
+        public Container AmmoContainer = default!;
 
         [ViewVariables]
-        private int _currentSlot;
+        public int CurrentSlot;
 
-        public override int Capacity => _ammoSlots.Length;
+        public override int Capacity => AmmoSlots.Length;
 
         [DataField("capacity")]
         private int _serializedCapacity = 6;
 
         [DataField("ammoSlots", readOnly: true)]
-        private EntityUid[] _ammoSlots = Array.Empty<EntityUid>();
+        public EntityUid?[] AmmoSlots = Array.Empty<EntityUid?>();
 
-        public override int ShotsLeft => _ammoContainer.ContainedEntities.Count;
+        public override int ShotsLeft => AmmoContainer.ContainedEntities.Count;
 
         [ViewVariables]
         [DataField("fillPrototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
-        private string? _fillPrototype;
+        public string? FillPrototype;
 
         [ViewVariables]
-        private int _unspawnedCount;
+        public int UnspawnedCount;
 
         // Sounds
         [DataField("soundEject")]
-        private SoundSpecifier _soundEject = new SoundPathSpecifier("/Audio/Weapons/Guns/MagOut/revolver_magout.ogg");
+        public SoundSpecifier SoundEject = new SoundPathSpecifier("/Audio/Weapons/Guns/MagOut/revolver_magout.ogg");
 
         [DataField("soundInsert")]
-        private SoundSpecifier _soundInsert = new SoundPathSpecifier("/Audio/Weapons/Guns/MagIn/revolver_magin.ogg");
+        public SoundSpecifier SoundInsert = new SoundPathSpecifier("/Audio/Weapons/Guns/MagIn/revolver_magin.ogg");
 
         [DataField("soundSpin")]
-        private SoundSpecifier _soundSpin = new SoundPathSpecifier("/Audio/Weapons/Guns/Misc/revolver_spin.ogg");
+        public SoundSpecifier SoundSpin = new SoundPathSpecifier("/Audio/Weapons/Guns/Misc/revolver_spin.ogg");
 
         void ISerializationHooks.BeforeSerialization()
         {
-            _serializedCapacity = _ammoSlots.Length;
+            _serializedCapacity = AmmoSlots.Length;
         }
 
         void ISerializationHooks.AfterDeserialization()
         {
-            _ammoSlots = new EntityUid[_serializedCapacity];
-        }
-
-        public override ComponentState GetComponentState()
-        {
-            var slotsSpent = new bool?[Capacity];
-            for (var i = 0; i < Capacity; i++)
-            {
-                slotsSpent[i] = null;
-                var ammoEntity = _ammoSlots[i];
-                if (ammoEntity != default && Entities.TryGetComponent(ammoEntity, out AmmoComponent? ammo))
-                {
-                    slotsSpent[i] = ammo.Spent;
-                }
-            }
-
-            //TODO: make yaml var to not sent currentSlot/UI? (for russian roulette)
-            return new RevolverBarrelComponentState(
-                _currentSlot,
-                FireRateSelector,
-                slotsSpent,
-                SoundGunshot.GetSound());
-        }
-
-        protected override void Initialize()
-        {
-            base.Initialize();
-            _unspawnedCount = Capacity;
-            int idx = 0;
-            _ammoContainer = ContainerHelpers.EnsureContainer<Container>(Owner, $"{Name}-ammoContainer", out var existing);
-            if (existing)
-            {
-                foreach (var entity in _ammoContainer.ContainedEntities)
-                {
-                    _unspawnedCount--;
-                    _ammoSlots[idx] = entity;
-                    idx++;
-                }
-            }
-
-            for (var i = 0; i < _unspawnedCount; i++)
-            {
-                var entity = Entities.SpawnEntity(_fillPrototype, Entities.GetComponent<TransformComponent>(Owner).Coordinates);
-                _ammoSlots[idx] = entity;
-                _ammoContainer.Insert(entity);
-                idx++;
-            }
-
-            UpdateAppearance();
-            Dirty();
-        }
-
-        private void UpdateAppearance()
-        {
-            if (!Entities.TryGetComponent(Owner, out AppearanceComponent? appearance))
-            {
-                return;
-            }
-
-            // Placeholder, at this stage it's just here for the RPG
-            appearance.SetData(MagazineBarrelVisuals.MagLoaded, ShotsLeft > 0);
-            appearance.SetData(AmmoVisuals.AmmoCount, ShotsLeft);
-            appearance.SetData(AmmoVisuals.AmmoMax, Capacity);
-        }
-
-        public bool TryInsertBullet(EntityUid user, EntityUid entity)
-        {
-            if (!Entities.TryGetComponent(entity, out AmmoComponent? ammoComponent))
-            {
-                return false;
-            }
-
-            if (ammoComponent.Caliber != _caliber)
-            {
-                Owner.PopupMessage(user, Loc.GetString("revolver-barrel-component-try-inser-bullet-wrong-caliber"));
-                return false;
-            }
-
-            // Functions like a stack
-            // These are inserted in reverse order but then when fired Cycle will go through in order
-            // The reason we don't just use an actual stack is because spin can select a random slot to point at
-            for (var i = _ammoSlots.Length - 1; i >= 0; i--)
-            {
-                var slot = _ammoSlots[i];
-                if (slot == default)
-                {
-                    _currentSlot = i;
-                    _ammoSlots[i] = entity;
-                    _ammoContainer.Insert(entity);
-                    SoundSystem.Play(Filter.Pvs(Owner), _soundInsert.GetSound(), Owner, AudioParams.Default.WithVolume(-2));
-
-                    Dirty();
-                    UpdateAppearance();
-                    return true;
-                }
-            }
-
-            Owner.PopupMessage(user, Loc.GetString("revolver-barrel-component-try-inser-bullet-ammo-full"));
-            return false;
-        }
-
-        public void Cycle()
-        {
-            // Move up a slot
-            _currentSlot = (_currentSlot + 1) % _ammoSlots.Length;
-            Dirty();
-            UpdateAppearance();
-        }
-
-        /// <summary>
-        /// Russian Roulette
-        /// </summary>
-        public void Spin()
-        {
-            var random = _random.Next(_ammoSlots.Length - 1);
-            _currentSlot = random;
-            SoundSystem.Play(Filter.Pvs(Owner), _soundSpin.GetSound(), Owner, AudioParams.Default.WithVolume(-2));
-            Dirty();
-        }
-
-        public override EntityUid? PeekAmmo()
-        {
-            return _ammoSlots[_currentSlot];
-        }
-
-        /// <summary>
-        /// Takes a projectile out if possible
-        /// IEnumerable just to make supporting shotguns saner
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public override EntityUid? TakeProjectile(EntityCoordinates spawnAt)
-        {
-            var ammo = _ammoSlots[_currentSlot];
-            EntityUid? bullet = null;
-            if (ammo != default)
-            {
-                var ammoComponent = Entities.GetComponent<AmmoComponent>(ammo);
-                bullet = EntitySystem.Get<GunSystem>().TakeBullet(ammoComponent, spawnAt);
-                if (ammoComponent.Caseless)
-                {
-                    _ammoSlots[_currentSlot] = default;
-                    _ammoContainer.Remove(ammo);
-                }
-            }
-            Cycle();
-            UpdateAppearance();
-            return bullet;
-        }
-
-        private void EjectAllSlots()
-        {
-            for (var i = 0; i < _ammoSlots.Length; i++)
-            {
-                var entity = _ammoSlots[i];
-                if (entity == default)
-                {
-                    continue;
-                }
-
-                _ammoContainer.Remove(entity);
-                EjectCasing(entity);
-                _ammoSlots[i] = default;
-            }
-
-            if (_ammoContainer.ContainedEntities.Count > 0)
-            {
-                SoundSystem.Play(Filter.Pvs(Owner), _soundEject.GetSound(), Owner, AudioParams.Default.WithVolume(-1));
-            }
-
-            // May as well point back at the end?
-            _currentSlot = _ammoSlots.Length - 1;
-        }
-
-        /// <summary>
-        /// Eject all casings
-        /// </summary>
-        /// <param name="eventArgs"></param>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public bool UseEntity(UseEntityEventArgs eventArgs)
-        {
-            EjectAllSlots();
-            Dirty();
-            UpdateAppearance();
-            return true;
-        }
-
-        public async Task<bool> InteractUsing(InteractUsingEventArgs eventArgs)
-        {
-            return TryInsertBullet(eventArgs.User, eventArgs.Using);
+            AmmoSlots = new EntityUid?[_serializedCapacity];
         }
     }
 }
