4535296a2fed3efa1900a9c97ae33a91151e0666
Kara D <lunarautomaton6@gmail.com>
2022-02-05 00:07:15 -0700

    Global usings

---
+++ Content.Client/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿// Global usings for Content.Client
+
+global using Robust.Shared.Log;
+global using Robust.Shared.GameObjects;
+global using Robust.Shared.IoC;
+global using Robust.Shared.ViewVariables;
+global using Robust.Shared.Serialization.Manager.Attributes;
---
+++ Content.Shared/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿// There isn't really a 'default place' to put these,
+// so a file in the project top level directory it is
+
+global using Robust.Shared.Log;
+global using Robust.Shared.GameObjects;
+global using Robust.Shared.IoC;
+global using Robust.Shared.ViewVariables;
+global using Robust.Shared.Serialization.Manager.Attributes;
+
