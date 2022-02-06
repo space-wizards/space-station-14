84245f37ebccd83b6172f9831f33936ba0fd6d66
ElectroJr <leonsfriedrich@gmail.com>
2022-02-06 16:27:32 +1300

    adjust logs for physics-damage

--- Content.Server/Damage/Systems/DamageOnLandSystem.cs
+++ Content.Server/Damage/Systems/DamageOnLandSystem.cs
@@ -1,18 +1,12 @@
-using Content.Server.Administration.Logs;
 using Content.Server.Damage.Components;
-using Content.Shared.Administration.Logs;
 using Content.Shared.Damage;
-using Content.Shared.Database;
 using Content.Shared.Throwing;
-using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
 
 namespace Content.Server.Damage.Systems
 {
     public sealed class DamageOnLandSystem : EntitySystem
     {
         [Dependency] private readonly DamageableSystem _damageableSystem = default!;
-        [Dependency] private readonly AdminLogSystem _logSystem = default!;
 
         public override void Initialize()
         {
@@ -22,11 +16,7 @@ namespace Content.Server.Damage.Systems
 
         private void DamageOnLand(EntityUid uid, DamageOnLandComponent component, LandEvent args)
         {
-            var dmg = _damageableSystem.TryChangeDamage(uid, component.Damage, component.IgnoreResistances);
-            if (dmg == null)
-                return;
-
-            _logSystem.Add(LogType.Landed, $"{ToPrettyString(component.Owner):entity} received {dmg.Total:damage} damage from landing");
+            _damageableSystem.TryChangeDamage(uid, component.Damage, component.IgnoreResistances);
         }
     }
 }
