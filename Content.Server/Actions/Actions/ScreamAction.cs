486092058fab569494b1de643f3e191432c1aaa2
hubismal <hubismal@gmail.com>
2022-02-06 02:29:22 -0600

    ScreamAction now supports data fields for variation/volume, and works with non-humanoids (defaults to male)

--- Content.Server/Actions/Actions/ScreamAction.cs
+++ Content.Server/Actions/Actions/ScreamAction.cs
@@ -22,15 +22,15 @@ namespace Content.Server.Actions.Actions
     [DataDefinition]
     public class ScreamAction : IInstantAction, ISerializationHooks
     {
-        private const float Variation = 0.125f;
-        private const float Volume = 4f;
-
         [Dependency] private readonly IRobustRandom _random = default!;
         [Dependency] private readonly IEntityManager _entMan = default!;
 
         [DataField("male", required: true)] private SoundSpecifier _male = default!;
         [DataField("female", required: true)] private SoundSpecifier _female = default!;
         [DataField("wilhelm", required: true)] private SoundSpecifier _wilhelm = default!;
+
+        [DataField("volume")] private float Volume = 4f;
+        [DataField("variation")] private float Variation = 0.125f;
 
         /// seconds
         [DataField("cooldown")] private float _cooldown = 10;
@@ -43,26 +43,31 @@ namespace Content.Server.Actions.Actions
         public void DoInstantAction(InstantActionEventArgs args)
         {
             if (!EntitySystem.Get<ActionBlockerSystem>().CanSpeak(args.Performer)) return;
-            if (!_entMan.TryGetComponent<HumanoidAppearanceComponent?>(args.Performer, out var humanoid)) return;
             if (!_entMan.TryGetComponent<SharedActionsComponent?>(args.Performer, out var actions)) return;
-
-            if (_random.Prob(.01f))
+            if (_entMan.TryGetComponent<HumanoidAppearanceComponent?>(args.Performer, out var humanoid))
             {
-                SoundSystem.Play(Filter.Pvs(args.Performer), _wilhelm.GetSound(), args.Performer, AudioParams.Default.WithVolume(Volume));
-            }
-            else
-            {
-                switch (humanoid.Sex)
+                if (_random.Prob(.01f))
                 {
-                    case Sex.Male:
-                        SoundSystem.Play(Filter.Pvs(args.Performer), _male.GetSound(), args.Performer, AudioHelpers.WithVariation(Variation).WithVolume(Volume));
-                        break;
-                    case Sex.Female:
-                        SoundSystem.Play(Filter.Pvs(args.Performer), _female.GetSound(), args.Performer, AudioHelpers.WithVariation(Variation).WithVolume(Volume));
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
+                    SoundSystem.Play(Filter.Pvs(args.Performer), _wilhelm.GetSound(), args.Performer, AudioParams.Default.WithVolume(Volume));
                 }
+                else
+                {
+                    switch (humanoid.Sex)
+                    {
+                        case Sex.Male:
+                            SoundSystem.Play(Filter.Pvs(args.Performer), _male.GetSound(), args.Performer, AudioHelpers.WithVariation(Variation).WithVolume(Volume));
+                            break;
+                        case Sex.Female:
+                            SoundSystem.Play(Filter.Pvs(args.Performer), _female.GetSound(), args.Performer, AudioHelpers.WithVariation(Variation).WithVolume(Volume));
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+            else //non-humanoids (sexless creatures) use male screams by default
+            {
+                SoundSystem.Play(Filter.Pvs(args.Performer), _male.GetSound(), args.Performer, AudioHelpers.WithVariation(Variation).WithVolume(Volume));
             }
 
             actions.Cooldown(args.ActionType, Cooldowns.SecondsFromNow(_cooldown));
