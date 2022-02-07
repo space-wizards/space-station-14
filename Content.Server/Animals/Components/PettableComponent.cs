f0a20737f7ef62c19c0b74f388b4025b8eb7df8c
Willhelm53 <97707302+Willhelm53@users.noreply.github.com>
2022-02-06 21:30:29 -0600

    Added PettableComponent, PettingSystem, and various SFX. Added PettableComponent to dogs, cats, chickens, ducks, and others. Gave GrammarComponent and pronounds to all pets and certain animals.

---
+++ Content.Server/Animals/Components/PettableComponent.cs
@@ -0,0 +1,32 @@
+using System;
+using Content.Shared.Sound;
+using Content.Server.Animals.Systems;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.ViewVariables;
+
+namespace Content.Server.Animals.Components
+{
+    [RegisterComponent, Friend(typeof(PettingSystem))]
+    public class PettableComponent : Component
+    {
+        [DataField("petDelay")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public TimeSpan PetDelay = TimeSpan.FromSeconds(1.0);
+
+        [DataField("petSound")]
+        public SoundSpecifier? PetSound; // Nullable in case no path is specified by yaml
+
+        /// <summary> Chance that a petting attempt will succeed.
+        /// 0 = never (always display failure popup, play no sound.)
+        /// 0.5 = 50% chance (e.g. finnicky cats who don't always like to be pet.)
+        /// 1 = always (e.g. friendly dogs)
+        /// -1 = invalid (does not play a popup, e.g. if the petting target is deceased.)
+        /// </summary>
+        [DataField("petSuccessChance")]
+        public float PetSuccessChance = 1.0f; // Always succeed by default, e.g. friendly dogs. A different value can be specified in the yaml.
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        public TimeSpan LastPetTime;
+    }
+}
---
+++ Resources/Locale/en-US/animals/petting/petting-system.ftl
@@ -0,0 +1,4 @@
+### Petting system
+
+petting-system-success = You pet { THE($target) } on {POSS-ADJ($target)} soft floofy head.
+petting-system-failure = You reach out to pet { THE($target) }, but {SUBJECT($target)} { CONJUGATE-BE($target) } aloof towards you.
