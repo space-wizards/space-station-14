49a13e2b4cf416ff7abf5abe4b754523b5d558bb
fishfish458 <fishfish458>
2022-02-07 19:19:53 -0600

    [REMOVE] more cleanup

--- Content.Server/Medical/CloningConsoleComponent.cs
+++ Content.Server/Medical/CloningConsoleComponent.cs
@@ -1,12 +1,8 @@
-using System;
 using Content.Server.UserInterface;
 using Content.Shared.CloningConsole;
 using Robust.Server.GameObjects;
-using Robust.Shared.GameObjects;
-using Robust.Shared.ViewVariables;
 using Content.Server.Medical.GeneticScanner;
 using Content.Server.Cloning.Components;
-using System.Collections.Generic;
 
 namespace Content.Server.Medical.Components
 {
--- Content.Shared/MedicalScanner/SharedGeneticScannerSystem.cs
+++ Content.Shared/MedicalScanner/SharedGeneticScannerSystem.cs
@@ -1,21 +1,12 @@
-using System;
 using Content.Shared.Body.Components;
 using Content.Shared.DragDrop;
 using JetBrains.Annotations;
-using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
-using Robust.Shared.Timing;
-using Robust.Shared.Log;
 
 namespace Content.Shared.GeneticScanner
 {
     [UsedImplicitly]
     public abstract class SharedGeneticScannerSystem : EntitySystem
     {
-        [Dependency] protected readonly IGameTiming GameTiming = default!;
-
-        protected static TimeSpan ExitAttemptDelay = TimeSpan.FromSeconds(0.5);
-
         public override void Initialize()
         {
             base.Initialize();
@@ -24,7 +15,6 @@ namespace Content.Shared.GeneticScanner
 
         private void OnCanDragDropOn(EntityUid uid, SharedGeneticScannerComponent component, CanDragDropOnEvent args)
         {
-            Logger.Debug("can drop on");
             if (args.Handled) return;
 
             args.CanDrop = CanInsert(component, args.Dragged);
