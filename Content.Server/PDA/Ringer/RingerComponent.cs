acb0c8d3bb7a5979e9815c11fa6b992380ce2048
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2022-02-08 22:15:06 +1100

    Nerf ringers (#6558)

--- Content.Server/PDA/Ringer/RingerComponent.cs
+++ Content.Server/PDA/Ringer/RingerComponent.cs
@@ -1,11 +1,8 @@
 using Content.Shared.PDA;
-using Robust.Shared.GameObjects;
-using Robust.Shared.Serialization.Manager.Attributes;
-using Robust.Shared.ViewVariables;
 
 namespace Content.Server.PDA.Ringer
 {
-    [RegisterComponent, ComponentProtoName("Ringer")]
+    [RegisterComponent]
     public sealed class RingerComponent : Component
     {
         [ViewVariables]
@@ -23,5 +20,16 @@ namespace Content.Server.PDA.Ringer
 
         [DataField("isPlaying")]
         public bool IsPlaying = false;
+
+        /// <summary>
+        /// How far the sound projects in metres.
+        /// </summary>
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField("range")]
+        public float Range = 3f;
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField("volume")]
+        public float Volume = -4f;
     }
 }
