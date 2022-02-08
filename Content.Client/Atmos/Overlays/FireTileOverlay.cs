1c6df07086e33fd015451e3fbd09a237d95e70f0
hubismal <47284081+hubismal@users.noreply.github.com>
2022-02-08 04:43:33 -0600

    Fires burn bright (#6516)
    Co-authored-by: metalgearsloth <comedian_vs_clown@hotmail.com>

--- Content.Client/Atmos/Overlays/FireTileOverlay.cs
+++ Content.Client/Atmos/Overlays/FireTileOverlay.cs
@@ -1,27 +1,28 @@
 using Content.Client.Atmos.EntitySystems;
 using Robust.Client.Graphics;
 using Robust.Shared.Enums;
-using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Map;
-using Robust.Shared.Maths;
 
 namespace Content.Client.Atmos.Overlays
 {
-    public sealed class GasTileOverlay : Overlay
+    public sealed class FireTileOverlay : Overlay
     {
         private readonly GasTileOverlaySystem _gasTileOverlaySystem;
 
         [Dependency] private readonly IMapManager _mapManager = default!;
+        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
 
         public override OverlaySpace Space => OverlaySpace.WorldSpaceBelowFOV;
+        private readonly ShaderInstance _shader;
 
-        public GasTileOverlay()
+        public FireTileOverlay()
         {
             IoCManager.InjectDependencies(this);
 
             _gasTileOverlaySystem = EntitySystem.Get<GasTileOverlaySystem>();
-            ZIndex = GasTileOverlaySystem.GasOverlayZIndex;
+            _shader = _prototypeManager.Index<ShaderPrototype>("unshaded").Instance().Duplicate();
+            ZIndex = GasTileOverlaySystem.GasOverlayZIndex + 1;
         }
 
         protected override void Draw(in OverlayDrawArgs args)
@@ -30,6 +31,8 @@ namespace Content.Client.Atmos.Overlays
 
             var mapId = args.Viewport.Eye!.Position.MapId;
             var worldBounds = args.WorldBounds;
+
+            drawHandle.UseShader(_shader);
 
             foreach (var mapGrid in _mapManager.FindGridsIntersecting(mapId, worldBounds))
             {
@@ -40,13 +43,15 @@ namespace Content.Client.Atmos.Overlays
 
                 foreach (var tile in mapGrid.GetTilesIntersecting(worldBounds))
                 {
-                    var enumerator = _gasTileOverlaySystem.GetOverlays(mapGrid.Index, tile.GridIndices);
+                    var enumerator = _gasTileOverlaySystem.GetFireOverlays(mapGrid.Index, tile.GridIndices);
                     while (enumerator.MoveNext(out var tuple))
                     {
                         drawHandle.DrawTexture(tuple.Texture, new Vector2(tile.X, tile.Y), tuple.Color);
                     }
                 }
             }
+
+            drawHandle.SetTransform(Matrix3.Identity);
         }
     }
 }
