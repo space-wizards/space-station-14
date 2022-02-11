0eaa39a29445f3db637ee36990fb14943206224b
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-02-11 20:57:12 +1100

    Make recyclers great again

--- Content.Server/Recycling/RecyclerSystem.cs
+++ Content.Server/Recycling/RecyclerSystem.cs
@@ -2,31 +2,42 @@ using Content.Server.Power.Components;
 using Content.Server.Recycling.Components;
 using Content.Shared.Body.Components;
 using Content.Shared.Recycling;
+using Content.Shared.Tag;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Physics.Dynamics;
 
 namespace Content.Server.Recycling
 {
-    internal sealed class RecyclerSystem : EntitySystem
+    public sealed class RecyclerSystem : EntitySystem
     {
+        [Dependency] private readonly TagSystem _tags = default!;
+
         public override void Initialize()
         {
             base.Initialize();
-            SubscribeLocalEvent<RecyclerComponent, StartCollideEvent>(HandleCollide);
+            SubscribeLocalEvent<RecyclerComponent, StartCollideEvent>(OnCollide);
         }
 
-        private void HandleCollide(EntityUid uid, RecyclerComponent component, StartCollideEvent args)
+        private void OnCollide(EntityUid uid, RecyclerComponent component, StartCollideEvent args)
         {
+            if (args.OurFixture.ID != "brrt") return;
+
             Recycle(component, args.OtherFixture.Body.Owner);
         }
 
         private void Recycle(RecyclerComponent component, EntityUid entity)
         {
-            // TODO: Prevent collision with recycled items
+            RecyclableComponent? recyclable = null;
 
             // Can only recycle things that are recyclable... And also check the safety of the thing to recycle.
-            if (!EntityManager.TryGetComponent(entity, out RecyclableComponent? recyclable) || !recyclable.Safe && component.Safe) return;
+            if (!_tags.HasTag(entity, "Recyclable") &&
+                (!EntityManager.TryGetComponent(entity, out recyclable) || !recyclable.Safe && component.Safe))
+            {
+                return;
+            }
+
+            // TODO: Prevent collision with recycled items
 
             // Mobs are a special case!
             if (CanGib(component, entity))
@@ -36,14 +47,17 @@ namespace Content.Server.Recycling
                 return;
             }
 
-            recyclable.Recycle(component.Efficiency);
+            if (recyclable == null)
+                QueueDel(entity);
+            else
+                Recycle(recyclable, component.Efficiency);
         }
 
         private bool CanGib(RecyclerComponent component, EntityUid entity)
         {
-            // We suppose this entity has a Recyclable component.
-            return EntityManager.HasComponent<SharedBodyComponent>(entity) && !component.Safe &&
-                   EntityManager.TryGetComponent(component.Owner, out ApcPowerReceiverComponent? receiver) && receiver.Powered;
+            // TODO: Power needs a helper for this jeez
+            return HasComp<SharedBodyComponent>(entity) && !component.Safe &&
+                   TryComp<ApcPowerReceiverComponent>(component.Owner, out var receiver) && receiver.Powered;
         }
 
         public void Bloodstain(RecyclerComponent component)
@@ -52,6 +66,22 @@ namespace Content.Server.Recycling
             {
                 appearance.SetData(RecyclerVisuals.Bloody, true);
             }
+        }
+
+        private void Recycle(RecyclableComponent component, float efficiency = 1f)
+        {
+            if (!string.IsNullOrEmpty(component.Prototype))
+            {
+                var xform = Transform(component.Owner);
+
+                for (var i = 0; i < Math.Max(component.Amount * efficiency, 1); i++)
+                {
+                    EntityManager.SpawnEntity(component.Prototype, xform.Coordinates);
+                }
+
+            }
+
+            EntityManager.QueueDeleteEntity(component.Owner);
         }
     }
 }
