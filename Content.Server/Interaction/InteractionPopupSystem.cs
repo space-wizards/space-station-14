97b7fcb7028daaec86c5ca43073508493ec01e8b
Willhelm53 <97707302+Willhelm53@users.noreply.github.com>
2022-02-10 23:05:59 -0600

    Herp derp

--- Content.Server/Interaction/InteractionPopupSystem.cs
+++ Content.Server/Interaction/InteractionPopupSystem.cs
@@ -32,30 +32,29 @@ public sealed class InteractionPopupSystem : EntitySystem
         if (curTime < component.LastInteractTime + component.InteractDelay)
             return;
 
+        if (TryComp<MobStateComponent>(uid, out var state) // if it has a MobStateComponent,
+            && !state.IsAlive())                           // AND if that state is not Alive (e.g. dead/incapacitated/critical)
+            return;
+
         string msg = ""; // Stores the text to be shown in the popup message
         string sfx = ""; // Stores the filepath of the sound to be played
 
-        if (!TryComp<MobStateComponent>(uid, out var state) // if it doesn't have a MobStateComponent, e.g. for a window.
-            || state.IsAlive())                             // OR if its state is Alive (not dead/incapacitated/critical).
+        if (_random.Prob(component.SuccessChance))
         {
-            if (_random.Prob(component.SuccessChance))
-            {
-                if (component.InteractSuccessString != null)
-                    msg = Loc.GetString(component.InteractSuccessString, ("target", uid)); // Success message (localized).
+            if (component.InteractSuccessString != null)
+                msg = Loc.GetString(component.InteractSuccessString, ("target", uid)); // Success message (localized).
 
-                if (component.InteractSuccessSound != null)
-                    sfx = component.InteractSuccessSound.GetSound();
-            }
-            else
-            {
-                if (component.InteractFailureString != null)
-                    msg = Loc.GetString(component.InteractFailureString, ("target", uid)); // Failure message (localized).
-
-                if (component.InteractFailureSound != null)
-                    sfx = component.InteractFailureSound.GetSound();
-            }
+            if (component.InteractSuccessSound != null)
+                sfx = component.InteractSuccessSound.GetSound();
         }
-        else return;
+        else
+        {
+            if (component.InteractFailureString != null)
+                msg = Loc.GetString(component.InteractFailureString, ("target", uid)); // Failure message (localized).
+
+            if (component.InteractFailureSound != null)
+                sfx = component.InteractFailureSound.GetSound();
+        }
 
         if (component.PopupPerceivedByOthers)
             _popupSystem.PopupEntity(msg, uid, Filter.Pvs(uid)); //play for everyone in range
