f205dea7fbec1b8a917b60167af367daf4410f5a
Alexander Evgrashin <aevgrashin@yandex.ru>
2022-02-12 13:46:37 +0300

    Modify interaction trigger

--- Content.Server/Xenoarchaeology/XenoArtifacts/Triggers/Components/ArtifactInteractionTriggerComponent.cs
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Triggers/Components/ArtifactInteractionTriggerComponent.cs
@@ -3,9 +3,26 @@ using Robust.Shared.GameObjects;
 namespace Content.Server.Xenoarchaeology.XenoArtifacts.Triggers.Components;
 
 /// <summary>
-///     Activate artifact just by touching it.
+///     Activate artifact by touching, attacking or pulling it.
 /// </summary>
 [RegisterComponent]
 public class ArtifactInteractionTriggerComponent : Component
 {
+    /// <summary>
+    ///     Should artifact be activated just by touching with empty hand?
+    /// </summary>
+    [DataField("emptyHandActivation")]
+    public bool EmptyHandActivation = true;
+
+    /// <summary>
+    ///     Should artifact be activated by melee attacking?
+    /// </summary>
+    [DataField("attackActivation")]
+    public bool AttackActivation = true;
+
+    /// <summary>
+    ///     Should artifact be activated by starting pulling it?
+    /// </summary>
+    [DataField("pullActivation")]
+    public bool PullActivation = true;
 }
--- Content.Server/Xenoarchaeology/XenoArtifacts/Triggers/Systems/ArtifactInteractionTriggerSystem.cs
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Triggers/Systems/ArtifactInteractionTriggerSystem.cs
@@ -1,7 +1,10 @@
 using Content.Server.Xenoarchaeology.XenoArtifacts.Triggers.Components;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
 using Content.Shared.Interaction.Helpers;
+using Content.Shared.Physics.Pull;
+using Content.Shared.Weapons.Melee;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 
@@ -14,14 +17,33 @@ public class ArtifactInteractionTriggerSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
+        SubscribeLocalEvent<ArtifactInteractionTriggerComponent, PullStartedMessage>(OnPull);
+        SubscribeLocalEvent<ArtifactInteractionTriggerComponent, AttackedEvent>(OnAttack);
         SubscribeLocalEvent<ArtifactInteractionTriggerComponent, InteractHandEvent>(OnInteract);
+    }
+
+    private void OnPull(EntityUid uid, ArtifactInteractionTriggerComponent component, PullStartedMessage args)
+    {
+        if (!component.PullActivation)
+            return;
+
+        _artifactSystem.TryActivateArtifact(uid, args.Puller.Owner);
+    }
+
+    private void OnAttack(EntityUid uid, ArtifactInteractionTriggerComponent component, AttackedEvent args)
+    {
+        if (!component.AttackActivation)
+            return;
+
+        _artifactSystem.TryActivateArtifact(uid, args.User);
     }
 
     private void OnInteract(EntityUid uid, ArtifactInteractionTriggerComponent component, InteractHandEvent args)
     {
-        if (args.Handled)
+        if (args.Handled || !args.InRangeUnobstructed())
             return;
-        if (!args.InRangeUnobstructed())
+
+        if (!component.EmptyHandActivation)
             return;
 
         args.Handled = _artifactSystem.TryActivateArtifact(uid, args.User);
