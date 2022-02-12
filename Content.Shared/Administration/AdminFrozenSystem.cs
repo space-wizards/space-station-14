307e642a6d11d636009c31e2788db78f5554244d
mirrorcult <lunarautomaton6@gmail.com>
2022-02-11 19:01:37 -0700

    Admin freeze (#6668)

---
+++ Content.Shared/Administration/AdminFrozenSystem.cs
@@ -0,0 +1,21 @@
+﻿using Content.Shared.Interaction.Events;
+using Content.Shared.Item;
+using Content.Shared.Movement;
+using Content.Shared.Throwing;
+
+namespace Content.Shared.Administration;
+
+public sealed class AdminFrozenSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<AdminFrozenComponent, UseAttemptEvent>((_, _, args) => args.Cancel());
+        SubscribeLocalEvent<AdminFrozenComponent, PickupAttemptEvent>((_, _, args) => args.Cancel());
+        SubscribeLocalEvent<AdminFrozenComponent, ThrowAttemptEvent>((_, _, args) => args.Cancel());
+        SubscribeLocalEvent<AdminFrozenComponent, AttackAttemptEvent>((_, _, args) => args.Cancel());
+        SubscribeLocalEvent<AdminFrozenComponent, MovementAttemptEvent>((_, _, args) => args.Cancel());
+        SubscribeLocalEvent<AdminFrozenComponent, InteractionAttemptEvent>((_, _, args) => args.Cancel());
+    }
+}
