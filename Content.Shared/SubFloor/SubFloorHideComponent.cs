8ef9187b4f5b57c41dcd4e5c14da7ee9061563be
ElectroJr <leonsfriedrich@gmail.com>
2022-02-16 13:26:00 +1300

    english

--- Content.Shared/SubFloor/SubFloorHideComponent.cs
+++ Content.Shared/SubFloor/SubFloorHideComponent.cs
@@ -24,7 +24,7 @@ namespace Content.Shared.SubFloor
         ///     Whether interactions with this entity should be blocked while it is under floor tiles.
         /// </summary>
         /// <remarks>
-        ///     Useful for entities like vents, which have exposed parts despite still partially hidden.
+        ///     Useful for entities like vents, which are only partially hidden.
         /// </remarks>
         [DataField("blockInteractions")]
         public bool BlockInteractions { get; set; } = true;
