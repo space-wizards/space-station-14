d4ed927b6b2d5ea53830b6cd7d5a39c6ad704901
Rane <elijahrane@gmail.com>
2022-02-16 02:00:52 -0500

    Basic instructions comment

--- Content.Shared/Emag/Systems/SharedEmagSystem.cs
+++ Content.Shared/Emag/Systems/SharedEmagSystem.cs
@@ -4,6 +4,11 @@ using Content.Shared.Examine;
 
 namespace Content.Shared.Emag.Systems
 {
+    /// How to add an emag interaction:
+    /// 1. Go to the system for the component you want the interaction with
+    /// 2. Subscribe to the GotEmaggedEvent
+    /// 3. Have some check for if this actually needs to be emagged or is already emagged (to stop charge waste)
+    /// 4. Past the check, add all the effects you desire and HANDLE THE EVENT ARGUMENT so a charge is spent
     public sealed class SharedEmagSystem : EntitySystem
     {
         public override void Initialize()
