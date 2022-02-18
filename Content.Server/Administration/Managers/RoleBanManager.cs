562ed1463863f364be20d9b9eb6903309518c0b7
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2022-02-17 16:03:48 -0800

    Fix expired role bans getting counted

--- Content.Server/Administration/Managers/RoleBanManager.cs
+++ Content.Server/Administration/Managers/RoleBanManager.cs
@@ -54,7 +54,7 @@ public sealed class RoleBanManager
 
     private async Task CacheDbRoleBans(NetUserId userId, IPAddress? address = null, ImmutableArray<byte>? hwId = null)
     {
-        var roleBans = await _db.GetServerRoleBansAsync(address, userId, hwId);
+        var roleBans = await _db.GetServerRoleBansAsync(address, userId, hwId, false);
 
         var userRoleBans = new HashSet<string>();
         foreach (var ban in roleBans)
