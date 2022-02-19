44cc0e2c4550854aae2f614d09561c8d8e5b12a7
Kara D <lunarautomaton6@gmail.com>
2022-02-18 21:37:36 -0700

    added dynamically

--- Content.Shared/Follower/Components/OrbitVisualsComponent.cs
+++ Content.Shared/Follower/Components/OrbitVisualsComponent.cs
@@ -8,19 +8,16 @@ public sealed class OrbitVisualsComponent : Component
     /// <summary>
     ///     How long should the orbit animation last in seconds, before being randomized?
     /// </summary>
-    [DataField("orbitLengthBase")]
     public float OrbitLength = 2.0f;
 
     /// <summary>
     ///     How far away from the entity should the orbit be, before being randomized?
     /// </summary>
-    [DataField("orbitDistanceBase")]
     public float OrbitDistance = 1.0f;
 
     /// <summary>
     ///     How long should the orbit stop animation last in seconds?
     /// </summary>
-    [DataField("orbitStopLength")]
     public float OrbitStopLength = 1.0f;
 
     /// <summary>
