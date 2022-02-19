462ad6ab59aa20733ca7dceeee3a49b4baf939d3
Alex Evgrashin <aevgrashin@yandex.ru>
2022-02-19 13:25:09 +0300

    Update Content.Server/Power/Events/PowerPulseEvent.cs
    Co-authored-by: mirrorcult <lunarautomaton6@gmail.com>

--- Content.Server/Power/Events/PowerPulseEvent.cs
+++ Content.Server/Power/Events/PowerPulseEvent.cs
@@ -4,7 +4,7 @@
 ///     Invoked on a target entity, when it was pulsed with an energy.
 ///     For instance, interacted with an active stun baton.
 /// </summary>
-public sealed class PowerPulseEvent : EventArgs
+public sealed class PowerPulseEvent : EntityEventArgs
 {
     public EntityUid? User;
     public EntityUid? Used;
