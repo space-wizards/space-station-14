76de03c49a2c09e7c51cf7ed5282132379b63829
Kara D <lunarautomaton6@gmail.com>
2022-02-20 20:27:33 -0700

    i hate ui coding i hate ui coding

---
+++ Content.Shared/Atmos/Piping/Unary/Components/SharedGasThermomachineComponent.cs
@@ -0,0 +1,46 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Atmos.Piping.Unary.Components;
+
+[Serializable]
+[NetSerializable]
+public enum ThermomachineUiKey
+{
+    Key
+}
+
+[Serializable]
+[NetSerializable]
+public sealed class GasThermomachineToggleMessage : BoundUserInterfaceMessage
+{
+}
+
+[Serializable]
+[NetSerializable]
+public sealed class GasThermomachineChangeTemperatureMessage : BoundUserInterfaceMessage
+{
+    public float Temperature { get; }
+
+    public GasThermomachineChangeTemperatureMessage(float temperature)
+    {
+        Temperature = temperature;
+    }
+}
+
+[Serializable]
+[NetSerializable]
+public sealed class GasThermomachineBoundUserInterfaceState : BoundUserInterfaceState
+{
+    public float MinTemperature { get; }
+    public float MaxTemperature { get; }
+    public float Temperature { get; }
+    public bool Enabled { get; }
+
+    public GasThermomachineBoundUserInterfaceState(float minTemperature, float maxTemperature, float temperature, bool enabled)
+    {
+        MinTemperature = minTemperature;
+        MaxTemperature = maxTemperature;
+        Temperature = temperature;
+        Enabled = enabled;
+    }
+}
---
+++ Resources/Locale/en-US/components/gas-thermomachine-component.ftl
@@ -0,0 +1,5 @@
+﻿comp-gas-thermomachine-ui-title = Thermomachine
+
+comp-gas-thermomachine-ui-toggle =  Toggle
+comp-gas-thermomachine-ui-status-disabled = Off
+comp-gas-thermomachine-ui-status-enabled = On
