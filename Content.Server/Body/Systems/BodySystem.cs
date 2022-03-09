01db72e30931d39718e310163d16bb4a6b718901
Rane <elijahrane@gmail.com>
2022-03-09 05:10:09 -0500

    Restore relay

--- Content.Server/Body/Systems/BodySystem.cs
+++ Content.Server/Body/Systems/BodySystem.cs
@@ -19,6 +19,7 @@ namespace Content.Server.Body.Systems
         {
             base.Initialize();
             SubscribeLocalEvent<BodyComponent, RelayMoveInputEvent>(OnRelayMoveInput);
+            SubscribeLocalEvent<BodyComponent, ApplyMetabolicMultiplierEvent>(OnApplyMetabolicMultiplier);
         }
 
         private void OnRelayMoveInput(EntityUid uid, BodyComponent component, RelayMoveInputEvent args)
@@ -34,6 +35,15 @@ namespace Content.Server.Body.Systems
                 }
 
                 _ticker.OnGhostAttempt(mind.Mind!, true);
+            }
+        }
+
+        private void OnApplyMetabolicMultiplier(EntityUid uid, BodyComponent component, ApplyMetabolicMultiplierEvent args)
+        {
+            foreach (var (part, _) in component.Parts)
+            foreach (var mechanism in part.Mechanisms)
+            {
+                RaiseLocalEvent(mechanism.Owner, args, false);
             }
         }
 
