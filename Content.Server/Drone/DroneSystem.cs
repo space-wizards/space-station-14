ec20171f504b60c0a53d1fcf95a5b21d88216b7b
HoofedEar <HoofedEar@users.noreply.github.com>
2022-03-10 17:43:21 -0800

    Revert "Satchel dropped on death"
    This reverts commit 5d54076e5ebeab9149224d6435891b99ad102dd1.

--- Content.Server/Drone/DroneSystem.cs
+++ Content.Server/Drone/DroneSystem.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using Content.Shared.Drone;
 using Content.Server.Drone.Components;
 using Content.Shared.MobState;
@@ -70,16 +69,9 @@ namespace Content.Server.Drone
             {
                 var body = Comp<SharedBodyComponent>(uid); //There's no way something can have a mobstate but not a body...
 
-                foreach (var item in drone.ToolUids.Select((value, i) => ( value, i )))
+                foreach (var item in drone.ToolUids)
                 {
-                    if (drone.Tools[item.i].PrototypeId == "ClothingBackpackSatchelDrone")
-                    {
-                        RemComp<UnremoveableComponent>(item.value);
-                    }
-                    else
-                    {
-                        EntityManager.DeleteEntity(item.value);
-                    }
+                    EntityManager.DeleteEntity(item);
                 }
                 body.Gib();
                 EntityManager.DeleteEntity(uid);
@@ -101,13 +93,12 @@ namespace Content.Server.Drone
                 if (TryComp<HandsComponent>(uid, out var hands) && hands.Count >= drone.Tools.Count)
                 {
                    foreach (var entry in drone.Tools)
-                   {
-                       var item = EntityManager.SpawnEntity(entry.PrototypeId, spawnCoord);
-                       if (item.ToString() != "ClothingBackpackSatchelDrone")
-                       {AddComp<UnremoveableComponent>(item);}
-                       hands.PutInHand(item);
-                       drone.ToolUids.Add(item);
-                   }
+                    {
+                        var item = EntityManager.SpawnEntity(entry.PrototypeId, spawnCoord);
+                        AddComp<UnremoveableComponent>(item);
+                        hands.PutInHand(item);
+                        drone.ToolUids.Add(item);
+                    }
                 }
 
                 drone.AlreadyAwoken = true;
