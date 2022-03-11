40a767ab08a47c4ed006d7db18c45da6ff25ed3c
Rane <elijahrane@gmail.com>
2022-03-11 00:03:22 -0500

    Temperature cure

---
+++ Content.Server/Disease/Cures/DiseaseBodyTemperature.cs
@@ -0,0 +1,28 @@
+using Content.Server.Temperature.Components;
+using Content.Shared.Disease;
+
+namespace Content.Server.Disease.Cures
+{
+    /// <summary>
+    ///     Requires the solution entity to be above or below a certain temperature.
+    ///     Used for things like cryoxadone and pyroxadone.
+    /// </summary>
+    public sealed class DiseaseBodyTemperature : DiseaseCure
+    {
+        [DataField("min")]
+        public float Min = 0;
+
+        [DataField("max")]
+        public float Max = float.MaxValue;
+        public override bool Cure(DiseaseEffectArgs args)
+        {
+            if (args.EntityManager.TryGetComponent(args.DiseasedEntity, out TemperatureComponent temp))
+            {
+                if (temp.CurrentTemperature > Min && temp.CurrentTemperature < Max)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
