8d81e6cb3770049e76fca20d2fbed5c242da6eff
ElectroJr <leonsfriedrich@gmail.com>
2022-03-14 00:25:16 +1300

    add shutdown subscription

--- Content.Client/Audio/AmbientSoundSystem.cs
+++ Content.Client/Audio/AmbientSoundSystem.cs
@@ -51,6 +51,14 @@ namespace Content.Client.Audio
             configManager.OnValueChanged(CCVars.AmbientCooldown, SetCooldown, true);
             configManager.OnValueChanged(CCVars.MaxAmbientSources, SetAmbientCount, true);
             configManager.OnValueChanged(CCVars.AmbientRange, SetAmbientRange, true);
+
+            SubscribeLocalEvent<AmbientSoundComponent, ComponentShutdown>(OnShutdown);
+        }
+
+        private void OnShutdown(EntityUid uid, AmbientSoundComponent component, ComponentShutdown args)
+        {
+            if (_playingSounds.Remove(component, out var sound))
+                sound.Stream?.Stop();
         }
 
         private void SetCooldown(float value) => _cooldown = value;
@@ -232,7 +240,7 @@ namespace Content.Client.Audio
                         .TryDistance(EntityManager, coordinates, out var range) ||
                     range > comp.Range)
                 {
-                    _playingSounds[comp].Stream?.Stop();
+                    sound.Stream?.Stop();
                     _playingSounds.Remove(comp);
                 }
             }
