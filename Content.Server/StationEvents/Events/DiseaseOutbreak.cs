bb9ad4259cf9e000c1d5ba7bd64fc721b191039e
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-03-13 21:02:55 -0400

    Diseases (#7057)
    * Disease system first pass
    
    * Renamed HealthChange
    
    * First working version of diseases (wtf???)
    
    * Fix the cursed yaml initialization
    
    * Pop-Up effect
    
    * Generic status effect
    
    * Create copy of prototype
    
    * CureDiseaseEffect
    
    * Disease resistance
    
    * Spaceacillin
    
    * Nerf spaceacillin now that we know it works
    
    * Sneezing, Coughing, Snoughing
    
    * Fix queuing, prevent future issues
    
    * Disease protection
    
    * Disease outbreak event
    
    * Disease Reagent Cure
    
    * Chem cause disease effect
    
    * Disease artifacts
    
    * Try infect when interacting with diseased
    
    * Diseases don't have to be infectious
    
    * Talking without a mask does a snough
    
    * Temperature cure
    
    * Bedrest
    
    * DiseaseAdjustReagent
    
    * Tweak how disease statuses work to be a bit less shit
    
    * A few more diseases
    
    * Natural immunity (can't get the same disease twice)
    
    * Polished up some diseases, touched up spaceacillin production
    
    * Rebalanced transmission
    
    * Edit a few diseases, make disease cures support a minimum value
    
    * Nitrile gloves, more disease protection sources
    
    * Health scanner shows diseased status
    
    * Clean up disease system
    
    * Traitor item
    
    * Mouth swabs
    
    * Disease diagnoser machine
    
    * Support for clean samples
    
    * Vaccines + fixes
    
    * Pass on disease resistant clothes
    
    * More work on non-infectious diseases & vaccines
    
    * Handle dead bodies
    
    * Added the relatively CBT visualizer
    
    * Pass over diseases and their populators
    
    * Comment stuff
    
    * Readability cleanup
    
    * Add printing sound to diagnoser, fix printing bug
    
    * vaccinator sound, seal up some classes
    
    * Make disease protection equip detection not shit (thanks whoever wrote addaccentcomponent)
    
    * Mirror review
    
    * More review stuff
    
    * More mirror review stuff
    
    * Refactor snoughing
    
    * Redid report creator
    
    * Fix snough messages, new vaccinator sound
    
    * Mirror review naming
    
    * Woops, forgot the artifact
    
    * Add recipes and fills
    
    * Rebalance space cold and robovirus
    
    * Give lizarb disease interaction stuff
    
    * Tweak some stuff and move things around
    
    * Add diseases to mice (since animal vectors are interesting and can be used to make vaccines)
    
    * Remove unused reagent

---
+++ Content.Server/StationEvents/Events/DiseaseOutbreak.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+using Content.Server.Chat.Managers;
+using Content.Server.Disease.Components;
+using Content.Server.Disease;
+using Content.Shared.Disease;
+using Robust.Shared.Random;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.StationEvents.Events;
+/// <summary>
+/// Infects a couple people
+/// with a random disease that isn't super deadly
+/// </summary>
+public sealed class DiseaseOutbreak : StationEvent
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly IChatManager _chatManager = default!;
+
+    /// <summary>
+    /// Disease prototypes I decided were not too deadly for a random event
+    /// </summary>
+    public readonly IReadOnlyList<string> NotTooSeriousDiseases = new[]
+    {
+        "SpaceCold",
+        "VanAusdallsRobovirus",
+        "VentCough",
+        "AMIV"
+    };
+    public override string Name => "DiseaseOutbreak";
+    public override float Weight => WeightNormal;
+    protected override float EndAfter => 1.0f;
+    /// <summary>
+    /// Finds 2-5 random entities that can host diseases
+    /// and gives them a randomly selected disease.
+    /// They all get the same disease.
+    /// </summary>
+    public override void Startup()
+    {
+        base.Startup();
+
+        var targetList = _entityManager.EntityQuery<DiseaseCarrierComponent>().ToList();
+        _random.Shuffle(targetList);
+
+        var toInfect = _random.Next(2, 5);
+
+        var diseaseName = _random.Pick(NotTooSeriousDiseases);
+
+        if (!_prototypeManager.TryIndex(diseaseName, out DiseasePrototype? disease) || disease == null)
+            return;
+
+        foreach (var target in targetList)
+        {
+            if (toInfect-- == 0)
+                break;
+
+            EntitySystem.Get<DiseaseSystem>().TryAddDisease(target, disease);
+        }
+        _chatManager.DispatchStationAnnouncement(Loc.GetString("station-event-disease-outbreak-announcement"));
+    }
+}
