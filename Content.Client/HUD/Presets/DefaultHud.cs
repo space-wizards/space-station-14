708505704418a962d03606f0990a871b214b184c
Jezithyr <jmaster9999@gmail.com>
2022-03-16 20:57:38 -0700

    Adding usedImplicitly tag

--- Content.Client/HUD/Presets/DefaultHud.cs
+++ Content.Client/HUD/Presets/DefaultHud.cs
@@ -1,8 +1,10 @@
 ﻿using Content.Client.CombatMode.UI;
 using Content.Client.HUD.Widgets;
+using JetBrains.Annotations;
 
 namespace Content.Client.HUD.Presets;
 
+[UsedImplicitly]
 public sealed class DefaultHud : HudPreset
 {
     protected override void DefineWidgets()
