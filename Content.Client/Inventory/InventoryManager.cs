0e6fe92d72548b35e268b23cc7c5caccc8032aa0
Jezithyr <jmaster9999@gmail.com>
2022-03-25 17:25:16 -0700

    test

--- Content.Client/Inventory/InventoryManager.cs
+++ Content.Client/Inventory/InventoryManager.cs
@@ -3,23 +3,39 @@ using Content.Client.Items.Managers;
 using Content.Client.UserInterface;
 using Content.Client.UserInterface.Controls;
 using Content.Shared.Inventory;
+using OpenToolkit.Graphics.OpenGL;
 using Robust.Client.Player;
 using Robust.Client.UserInterface;
 using Robust.Shared.Prototypes;
+using Serilog;
 
 namespace Content.Client.Inventory;
 
-public sealed class InventoryManager : UIController
+public sealed class InventoryUIController : UIController
 {
     [Dependency] private EntityManager _entityManager = default!;
     [UISystemDependency] private InventorySystem? _inventorySystem = null;
     private PlayerInventoryData _playerData = new PlayerInventoryData();
     private Dictionary<string, ItemSlotUIContainer> _slotGroups = new();
 
-    public InventoryManager()
+    public InventoryUIController()
     {
         IoCManager.InjectDependencies(this);
+        Test();
     }
+
+
+    public void Test()
+    {
+        if (_inventorySystem == null)
+        {
+            Logger.Debug("Null RIP");
+            return;
+        }
+        Logger.Debug("FOund");
+    }
+
+
     public readonly struct PlayerInventoryData
     {
         public readonly EntityUid? PlayerEntity;
@@ -54,6 +70,5 @@ public sealed class InventoryManager : UIController
             entityManager.TryGetComponent<HandsComponent>(playerEntity, out HandsComponent);
             entityManager.TryGetComponent<ClientInventoryComponent>(playerEntity, out InventoryComponent);
         }
-
     }
 }
