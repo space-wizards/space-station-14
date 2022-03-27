66ecf61b2e8c90aa65cbed15032261772279c246
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2022-03-27 18:16:08 +0200

    Break test

---
+++ Content.Tests/FullReleaseTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Content.Tests;
+
+[TestFixture]
+public class FullReleaseTest
+{
+    [Test]
+    public void Test()
+    {
+#if FULL_RELEASE
+        Assert.Fail();
+#else
+        Assert.Pass();
+#endif
+    }
+}
