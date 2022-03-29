1c80a787da6319375a1f379d87f46cf91cb9a83b
Rane <elijahrane@gmail.com>
2022-03-28 20:31:43 -0400

    Removable keys

--- Content.Server/Vehicle/VehicleSystem.cs
+++ Content.Server/Vehicle/VehicleSystem.cs
@@ -1,17 +1,21 @@
 using Content.Shared.Vehicle.Components;
 using Content.Shared.Buckle.Components;
 using Content.Shared.Interaction;
+using Content.Shared.Verbs;
 using Content.Server.Hands.Components;
+using Content.Shared.Hands.EntitySystems;
 
 namespace Content.Server.Vehicle
 {
     public sealed class VehicleSystem : EntitySystem
     {
+        [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
         public override void Initialize()
         {
             base.Initialize();
             SubscribeLocalEvent<VehicleComponent, BuckleChangeEvent>(OnBuckleChange);
             SubscribeLocalEvent<VehicleComponent, AfterInteractUsingEvent>(OnAfterInteractUsing);
+            SubscribeLocalEvent<VehicleComponent, GetVerbsEvent<AlternativeVerb>>(AddKeysVerb);
         }
         /// <summary>
         /// Give the user the rider component if they're buckling to the vehicle,
@@ -51,6 +55,27 @@ namespace Content.Server.Vehicle
 
             component.HasKey = true;
             EntityManager.DeleteEntity(args.Used);
+        }
+        private void AddKeysVerb(EntityUid uid, VehicleComponent component, GetVerbsEvent<AlternativeVerb> args)
+        {
+            if (!args.CanInteract || !args.CanAccess || !HasComp<HandsComponent>(args.User) || !component.HasKey || component.Key == string.Empty)
+                return;
+            // If someone is riding, let only that person take out the keys.
+            if (component.HasRider && !TryComp<RiderComponent>(args.User, out var rider) && rider?.Vehicle?.Owner != component.Owner)
+                return;
+
+            AlternativeVerb verb = new()
+            {
+                Act = () =>
+                {
+                    var key = EntityManager.SpawnEntity(component.Key, Transform(args.User).Coordinates);
+                    _handsSystem.PickupOrDrop(args.User, key);
+                    component.HasKey = false;
+                },
+                Text = Loc.GetString("vehicle-remove-keys-verb"),
+                Priority = 2
+            };
+            args.Verbs.Add(verb);
         }
     }
 }
---
+++ Resources/Locale/en-US/vehicle/vehicle.ftl
@@ -0,0 +1,1 @@
+vehicle-remove-keys-verb = Remove keys
