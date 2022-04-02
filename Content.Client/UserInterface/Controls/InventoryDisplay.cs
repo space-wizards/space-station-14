48427c05726195ec1957b2c4912fa85ec4ac23c8
Jezithyr <jmaster9999@gmail.com>
2022-04-01 20:07:57 -0700

    added inventory window stubs

---
+++ Content.Client/UserInterface/Controls/InventoryDisplay.cs
@@ -0,0 +1,9 @@
+﻿using Content.Shared.Inventory;
+using Robust.Client.UserInterface.Controls;
+
+namespace Content.Client.UserInterface.Controls;
+
+public sealed class InventoryDisplay : BoxContainer
+{
+    //private Dictionary<string, SlotDefinition>
+}
