7900abb8880f08c93f2efb73683a923d3856ae95
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-04-08 17:17:25 -0400

    Literally Murder IExamine (#7352)

--- Content.Server/Paper/PaperSystem.cs
+++ Content.Server/Paper/PaperSystem.cs
@@ -1,4 +1,5 @@
 using Content.Server.UserInterface;
+using Content.Shared.Examine;
 using Content.Shared.Paper;
 
 namespace Content.Server.Paper
@@ -9,12 +10,27 @@ namespace Content.Server.Paper
         {
             base.Initialize();
             SubscribeLocalEvent<PaperComponent, BeforeActivatableUIOpenEvent>(AfterUIOpen);
+            SubscribeLocalEvent<PaperComponent, ExaminedEvent>(OnExamined);
         }
 
         private void AfterUIOpen(EntityUid uid, PaperComponent component, BeforeActivatableUIOpenEvent args)
         {
             component.Mode = SharedPaperComponent.PaperAction.Read;
             component.UpdateUserInterface();
+        }
+
+        private void OnExamined(EntityUid uid, PaperComponent component, ExaminedEvent args)
+        {
+            if (!args.IsInDetailsRange)
+                return;
+            if (component.Content == "")
+                return;
+
+            args.PushMarkup(
+                Loc.GetString(
+                    "paper-component-examine-detail-has-words"
+                )
+            );
         }
     }
 }
