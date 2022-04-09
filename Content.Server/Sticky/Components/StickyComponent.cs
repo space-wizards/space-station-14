5a5bcacaaf84a55bb55bac20107d8cc02f446d72
Alexander Evgrashin <aevgrashin@yandex.ru>
2022-04-09 13:52:22 +0300

    Add sticky

---
+++ Content.Server/Sticky/Components/StickyComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Content.Server.Sticky.Components;
+
+/// <summary>
+///     Items that can be stick to other structures or entities.
+///     For example paper stickers or C4 charges.
+/// </summary>
+[RegisterComponent]
+public sealed class StickyComponent : Component
+{
+    /// <summary>
+    ///     How much time does it take to place entity.
+    /// </summary>
+    public TimeSpan Delay = TimeSpan.FromSeconds(0.1);
+}
---
+++ Content.Server/Sticky/Systems/StickySystem.cs
@@ -0,0 +1,76 @@
+﻿using Content.Server.DoAfter;
+using Content.Server.Popups;
+using Content.Server.Sticky.Components;
+using Content.Shared.Interaction;
+using Robust.Shared.Containers;
+using Robust.Shared.Player;
+
+namespace Content.Server.Sticky.Systems;
+
+public sealed class StickySystem : EntitySystem
+{
+    [Dependency] private readonly DoAfterSystem _doAfterSystem = default!;
+    [Dependency] private readonly PopupSystem _popupSystem = default!;
+    [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
+
+    private const string StickerSlotId = "stickers_container";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<StickySuccessfulEvent>(OnStickSuccessful);
+        SubscribeLocalEvent<StickyComponent, AfterInteractEvent>(OnAfterInteract);
+    }
+
+    private void OnAfterInteract(EntityUid uid, StickyComponent component, AfterInteractEvent args)
+    {
+        if (args.Handled || args.Target == null)
+            return;
+
+        // show message to user
+        _popupSystem.PopupEntity("You start planting the bomb...", args.User, Filter.Entities(args.User));
+
+        // start sticking object to target
+        var delay = (float) component.Delay.TotalSeconds;
+        _doAfterSystem.DoAfter(new DoAfterEventArgs(args.User, delay, target: args.Target)
+        {
+            BroadcastFinishedEvent = new StickySuccessfulEvent(args.User, args.Target.Value, component),
+            BreakOnStun = true,
+            BreakOnTargetMove = true,
+            BreakOnUserMove = true,
+            NeedHand = true
+        });
+    }
+
+    private void OnStickSuccessful(StickySuccessfulEvent ev)
+    {
+        StickToEntity(ev.Component.Owner, ev.Target, ev.User, ev.Component);
+    }
+
+    public void StickToEntity(EntityUid uid, EntityUid target, EntityUid user, StickyComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        var container = _containerSystem.EnsureContainer<Container>(target, StickerSlotId);
+        container.ShowContents = true;
+        if (container.Insert(uid))
+        {
+            _popupSystem.PopupEntity("You planted the bomb", user, Filter.Entities(user));
+        }
+    }
+
+    private sealed class StickySuccessfulEvent : EntityEventArgs
+    {
+        public readonly EntityUid User;
+        public readonly EntityUid Target;
+        public readonly StickyComponent Component;
+
+        public StickySuccessfulEvent(EntityUid user, EntityUid target, StickyComponent component)
+        {
+            User = user;
+            Target = target;
+            Component = component;
+        }
+    }
+}
