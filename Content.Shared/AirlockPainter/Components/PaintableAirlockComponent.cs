475d2359b0fab757fc7f6d05b88c1171f23a2741
Joosep Jääger <joosep.jaager@gmail.com>
2022-04-13 14:39:24 +0300

    Update Content.Shared/AirlockPainter/Components/PaintableAirlockComponent.cs
    Co-authored-by: Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>

--- Content.Shared/AirlockPainter/Components/PaintableAirlockComponent.cs
+++ Content.Shared/AirlockPainter/Components/PaintableAirlockComponent.cs
@@ -3,7 +3,7 @@ namespace Content.Server.AirlockPainter
     [RegisterComponent]
     public sealed class PaintableAirlockComponent : Component
     {
-        [DataField("group")]
+        [DataField("group", customTypeSerializer:typeof(PrototypeIdSerializer<AirlockGroupPrototype>))]
         public string Group = default!;
     }
 }
