11b1c554da95b2ed945f28ef08051b08452611ac
ElectroJr <leonsfriedrich@gmail.com>
2022-04-14 21:24:04 +1200

    Toggleable Hardsuit Helmets

--- Content.Shared/Item/SharedItemComponent.cs
+++ Content.Shared/Item/SharedItemComponent.cs
@@ -38,6 +38,20 @@ namespace Content.Shared.Item
         public Dictionary<string, List<PrototypeLayerData>> ClothingVisuals = new();
 
         /// <summary>
+        ///     Whether or not this item can be picked up.
+        /// </summary>
+        /// <remarks>
+        ///     This should almost always be true for items. But in some special cases, an item can be equipped but not
+        ///     picked up. E.g., hardsuit helmets are attached to the suit, so we want to disable things like the pickup
+        ///     verb.
+        /// </remarks>
+        [DataField("canPickup")]
+        public bool CanPickup = true;
+
+        [DataField("quickEquip")]
+        public bool QuickEquip = true;
+
+        /// <summary>
         ///     Part of the state of the sprite shown on the player when this item is in their hands or inventory.
         /// </summary>
         /// <remarks>
@@ -61,8 +75,11 @@ namespace Content.Shared.Item
         [DataField("Slots")]
         public SlotFlags SlotFlags = SlotFlags.PREVENTEQUIP; //Different from None, NONE allows equips if no slot flags are required
 
-        [DataField("EquipSound")]
+        [DataField("equipSound")]
         public SoundSpecifier? EquipSound { get; set; } = default!;
+
+        [DataField("unequipSound")]
+        public SoundSpecifier? UnequipSound = default!;
 
         // TODO REMOVE. Currently nonfunctional and only used by RGB system. #6253 Fixes this but requires #6252
         /// <summary>
