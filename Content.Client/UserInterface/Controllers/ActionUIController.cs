fda562b457051d49d4e8b0bf007e443ca6b4878b
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2022-04-19 21:00:19 +0200

    WIP actions window implementation

--- Content.Client/UserInterface/Controllers/ActionUIController.cs
+++ Content.Client/UserInterface/Controllers/ActionUIController.cs
@@ -1,4 +1,6 @@
-﻿using Content.Client.Actions;
+﻿using System.Linq;
+using Content.Client.Actions;
+using Content.Client.DragDrop;
 using Content.Client.Gameplay;
 using Content.Client.HUD;
 using Content.Client.HUD.Widgets;
@@ -9,25 +11,36 @@ using Content.Shared.Actions;
 using Content.Shared.Actions.ActionTypes;
 using Content.Shared.Input;
 using Robust.Client.UserInterface;
+using Robust.Shared.Input;
 using Robust.Shared.Input.Binding;
 using static Robust.Client.UserInterface.Controls.BaseButton;
+using static Robust.Client.UserInterface.Controls.LineEdit;
+using static Robust.Client.UserInterface.Controls.MultiselectOptionButton<string>;
 
 namespace Content.Client.UserInterface.Controllers;
 
 public sealed class ActionUIController : UIController, IOnStateChanged<GameplayState>
 {
+    [Dependency] private readonly IEntityManager _entities = default!;
     [Dependency] private readonly IHudManager _hud = default!;
     [Dependency] private readonly IUIWindowManager _uiWindows = default!;
 
     [UISystemDependency] private readonly ActionsSystem _actionsSystem = default!;
     [UISystemDependency] private readonly InteractionOutlineSystem _interactionOutlineSystem = default!;
     [UISystemDependency] private readonly TargetOutlineSystem _targetOutlineSystem = default!;
+
     private ActionButtonContainer? actionContainer;
     private ActionPage? _defaultPage;
     private List<ActionPage> _actionPages = new();
+    private DragDropHelper<ActionButton> _dragDropHelper;
 
     private ActionsWindow? _window;
     private MenuButton ActionButton => _hud.GetUIWidget<MenuBar>().ActionButton;
+
+    public ActionUIController()
+    {
+        _dragDropHelper = new DragDropHelper<ActionButton>(OnBeginDrag, OnContinueDrag, OnEndDrag);
+    }
 
     public void OnStateChanged(GameplayState state)
     {
@@ -51,18 +64,21 @@ public sealed class ActionUIController : UIController, IOnStateChanged<GameplayS
         if (_window == null)
             return;
 
+        _window.OnClose += OnWindowClosed;
+        _window.ClearButton.OnPressed += OnClearPressed;
+        _window.SearchBar.OnTextChanged += OnSearchChanged;
+        _window.FilterButton.OnItemSelected += OnFilterSelected;
+
         _window.OpenCentered();
+
+        UpdateFilterLabel();
+
         ActionButton.Pressed = true;
     }
 
     private void CloseWindow()
     {
-        if (_window == null)
-            return;
-
-        _window.Dispose();
-        _window = null;
-        ActionButton.Pressed = false;
+        _window?.Close();
     }
 
     private void ToggleWindow()
@@ -74,6 +90,155 @@ public sealed class ActionUIController : UIController, IOnStateChanged<GameplayS
         }
 
         CloseWindow();
+    }
+
+    private void UpdateFilterLabel()
+    {
+        if (_window == null)
+            return;
+
+        if (_window.FilterButton.SelectedKeys.Count == 0)
+        {
+            _window.FilterLabel.Visible = false;
+        }
+        else
+        {
+            _window.FilterLabel.Visible = true;
+            _window.FilterLabel.Text = Loc.GetString("ui-actionmenu-filter-label",
+                ("selectedLabels", string.Join(", ", _window.FilterButton.SelectedLabels)));
+        }
+    }
+
+    private bool MatchesFilter(ActionType action, Filters filter)
+    {
+        return filter switch
+        {
+            Filters.Enabled => action.Enabled,
+            Filters.Item => action.Provider != null && action.Provider != _actionsSystem.PlayerActions?.Owner,
+            Filters.Innate => action.Provider == null || action.Provider == _actionsSystem.PlayerActions?.Owner,
+            Filters.Instant => action is InstantAction,
+            Filters.Targeted => action is TargetedAction,
+            _ => throw new ArgumentOutOfRangeException(nameof(filter), filter, null)
+        };
+    }
+
+    private void ClearList()
+    {
+        _window?.ResultsGrid.RemoveAllChildren();
+    }
+
+    private void PopulateActions(IEnumerable<ActionType> actions)
+    {
+        ClearList();
+
+        foreach (var action in actions)
+        {
+            var actionItem = new ActionButton();
+            actionItem.UpdateButtonData(_entities, action);
+            actionItem.OnKeyBindDown += args => ActionKeyBindDown(args, actionItem);
+            actionItem.OnKeyBindUp += ActionKeyBindUp;
+            actionItem.OnPressed += ActionPressed;
+        }
+    }
+
+    private void SearchAndDisplay()
+    {
+        if (_window == null)
+            return;
+
+        var search = _window.SearchBar.Text;
+        var filters = _window.FilterButton.SelectedKeys.Select(Enum.Parse<Filters>).ToArray();
+        if (filters.Length == 0 && string.IsNullOrWhiteSpace(search))
+        {
+            ClearList();
+            return;
+        }
+
+        IEnumerable<ActionType>? actions = _actionsSystem.PlayerActions?.Actions;
+        if (actions == null)
+        {
+            actions = Array.Empty<ActionType>();
+        }
+
+        actions = actions.Where(action =>
+        {
+            if (filters.Length > 0 && filters.Any(filter => !MatchesFilter(action, filter)))
+                return false;
+
+            if (action.Keywords.Any(keyword => search.Contains(keyword, StringComparison.OrdinalIgnoreCase)))
+                return true;
+
+            if (action.DisplayName.Contains(search, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            if (action.Provider == null || action.Provider == _actionsSystem.PlayerActions?.Owner)
+                return false;
+
+            var name = _entities.GetComponent<MetaDataComponent>(action.Provider.Value).EntityName;
+            return name.Contains(search, StringComparison.OrdinalIgnoreCase);
+        });
+
+        PopulateActions(actions);
+    }
+
+    private void OnWindowClosed()
+    {
+        _window = null;
+        ActionButton.Pressed = false;
+    }
+
+    private void OnClearPressed(ButtonEventArgs args)
+    {
+        if (_window == null)
+            return;
+
+        _window.SearchBar.Clear();
+        _window.FilterButton.DeselectAll();
+        UpdateFilterLabel();
+        SearchAndDisplay();
+    }
+
+    private void OnSearchChanged(LineEditEventArgs args)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnFilterSelected(ItemPressedEventArgs args)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ActionKeyBindUp(GUIBoundKeyEventArgs args)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ActionKeyBindDown(GUIBoundKeyEventArgs args, ActionButton action)
+    {
+        if (args.Function != EngineKeyFunctions.UIClick)
+            return;
+
+        _dragDropHelper.MouseDown(action);
+    }
+
+    private void ActionPressed(GUIBoundKeyEventArgs args, SlotControl control)
+    {
+        throw new NotImplementedException();
+    }
+
+    private bool OnBeginDrag()
+    {
+        throw new NotImplementedException();
+    }
+
+    private bool OnContinueDrag(float frametime)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnEndDrag()
+    {
+        throw new NotImplementedException();
     }
 
     public void RegisterActionContainer(ActionButtonContainer actionBar)
@@ -177,4 +342,12 @@ public sealed class ActionUIController : UIController, IOnStateChanged<GameplayS
         }
     }
 
+    public enum Filters
+    {
+        Enabled,
+        Item,
+        Innate,
+        Instant,
+        Targeted
+    }
 }
