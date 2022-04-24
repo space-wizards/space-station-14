ef7464f258504c6a82438c53b7b1354314c00b2a
Alexander Evgrashin <aevgrashin@yandex.ru>
2022-04-24 18:02:16 +0300

    Move radiation collector to ECS

---
+++ Content.Server/Radiation/Events/OnIrradiatedEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Content.Server.Radiation.Events;
+
+/// <summary>
+///     Raised on entity when it was irradiated
+///     by some radiation source.
+/// </summary>
+public sealed class OnIrradiatedEvent : EntityEventArgs
+{
+    public readonly float FrameTime;
+    public readonly float RadsPerSecond;
+}
