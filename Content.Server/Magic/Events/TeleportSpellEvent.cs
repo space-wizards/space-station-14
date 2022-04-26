f5ec0b89d15cb48021f391640d547d50e055ddbb
keronshb <keronshb@live.com>
2022-04-26 16:19:43 -0400

    Magic WIP

---
+++ Content.Server/Magic/Events/TeleportSpellEvent.cs
@@ -0,0 +1,5 @@
+﻿using Content.Shared.Actions;
+
+namespace Content.Server.Magic.Events;
+
+public sealed class TeleportSpellEvent : WorldTargetActionEvent { }
---
+++ Content.Server/Magic/MagicSystem.cs
@@ -0,0 +1,15 @@
+﻿using Content.Server.Magic.Events;
+
+namespace Content.Server.Magic;
+
+public sealed class MagicSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<MagicComponent>;
+
+    }
+
+}
