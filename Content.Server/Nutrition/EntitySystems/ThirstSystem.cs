9432cb6a65905954a5e208ddc0641c8e711042c9
MLGTASTICa <ak9bc01d@yahoo.com>
2022-04-26 19:41:17 +0300

    Cache Everything , Death check.

--- Content.Server/Nutrition/EntitySystems/ThirstSystem.cs
+++ Content.Server/Nutrition/EntitySystems/ThirstSystem.cs
@@ -6,7 +6,6 @@ using Content.Shared.Nutrition.Components;
 using Robust.Shared.Random;
 using Content.Shared.MobState.Components;
 using Content.Shared.Movement.Components;
-using Content.Shared.Movement.EntitySystems;
 using Content.Shared.Alert;
 using Content.Server.Administration.Logs;
 using Content.Shared.Administration.Logs;
@@ -19,8 +18,11 @@ namespace Content.Server.Nutrition.EntitySystems
     public sealed class ThirstSystem : EntitySystem
     {
 
-        [Dependency] private readonly IEntityManager _entMan = default!;
         [Dependency] private readonly IRobustRandom _random = default!;
+        [Dependency] private readonly AdminLogSystem _adminlog = default!;
+        [Dependency] private readonly ILogManager _debuglog = default!;
+        [Dependency] private readonly MovementSpeedModifierSystem _movement = default!;
+        [Dependency] private readonly AlertsSystem _alerts = default!;
         private float _accumulatedFrameTime;
 
         public override void Initialize()
@@ -43,6 +45,13 @@ namespace Content.Server.Nutrition.EntitySystems
 
         private void OnRefreshMovespeed(EntityUid uid, ThirstComponent component, RefreshMovementSpeedModifiersEvent args)
         {
+            if(EntityManager.TryGetComponent(uid,out MobStateComponent? mobState))
+            {
+                if(mobState.IsDead())
+                {
+                    return;
+                }
+            }
             float mod = component.CurrentThirstThreshold == ThirstThreshold.Parched ? 0.75f : 1.0f;
             args.ModifySpeed(mod, mod);
         }
@@ -76,19 +85,19 @@ namespace Content.Server.Nutrition.EntitySystems
         private void UpdateEffects(ThirstComponent component)
         {
             if (component.LastThirstThreshold == ThirstThreshold.Parched && component.CurrentThirstThreshold != ThirstThreshold.Dead &&
-                    _entMan.TryGetComponent(component.Owner, out MovementSpeedModifierComponent? movementSlowdownComponent))
+                    EntityManager.TryGetComponent(component.Owner, out MovementSpeedModifierComponent? movementSlowdownComponent))
             {
-                EntitySystem.Get<MovementSpeedModifierSystem>().RefreshMovementSpeedModifiers(component.Owner);
+                _movement.RefreshMovementSpeedModifiers(component.Owner);
             }
 
             // Update UI
             if (ThirstComponent.ThirstThresholdAlertTypes.TryGetValue(component.CurrentThirstThreshold, out var alertId))
             {
-                EntitySystem.Get<AlertsSystem>().ShowAlert(component.Owner, alertId);
+                _alerts.ShowAlert(component.Owner, alertId);
             }
             else
             {
-                EntitySystem.Get<AlertsSystem>().ClearAlertCategory(component.Owner, AlertCategory.Thirst);
+                _alerts.ClearAlertCategory(component.Owner, AlertCategory.Thirst);
             }
 
             switch (component.CurrentThirstThreshold)
@@ -109,7 +118,7 @@ namespace Content.Server.Nutrition.EntitySystems
                     component.ActualDecayRate = component.BaseDecayRate * 0.8f;
                     return;
                 case ThirstThreshold.Parched:
-                    EntitySystem.Get<MovementSpeedModifierSystem>().RefreshMovementSpeedModifiers(component.Owner);
+                    _movement.RefreshMovementSpeedModifiers(component.Owner);
                     component.LastThirstThreshold = component.CurrentThirstThreshold;
                     component.ActualDecayRate = component.BaseDecayRate * 0.6f;
                     return;
@@ -117,7 +126,7 @@ namespace Content.Server.Nutrition.EntitySystems
                 case ThirstThreshold.Dead:
                     return;
                 default:
-                    Logger.ErrorS("thirst", $"No thirst threshold found for {component.CurrentThirstThreshold}");
+                    _debuglog.GetSawmill("thirst").Error($"No thirst threshold found for {component.CurrentThirstThreshold}");
                     throw new ArgumentOutOfRangeException($"No thirst threshold found for {component.CurrentThirstThreshold}");
             }
         }
@@ -134,16 +143,16 @@ namespace Content.Server.Nutrition.EntitySystems
                     if (calculatedThirstThreshold != component.CurrentThirstThreshold)
                     {
                         if (component.CurrentThirstThreshold == ThirstThreshold.Dead)
-                            EntitySystem.Get<AdminLogSystem>().Add(LogType.Thirst, $"{_entMan.ToPrettyString(component.Owner):entity} has stopped taking dehydration damage");
+                            _adminlog.Add(LogType.Thirst, $"{EntityManager.ToPrettyString(component.Owner):entity} has stopped taking dehydration damage");
                         else if (calculatedThirstThreshold == ThirstThreshold.Dead)
-                            EntitySystem.Get<AdminLogSystem>().Add(LogType.Thirst, $"{_entMan.ToPrettyString(component.Owner):entity} has started taking dehydration damage");
+                           _adminlog.Add(LogType.Thirst, $"{EntityManager.ToPrettyString(component.Owner):entity} has started taking dehydration damage");
 
                         component.CurrentThirstThreshold = calculatedThirstThreshold;
                         UpdateEffects(component);
                     }
                     if (component.CurrentThirstThreshold == ThirstThreshold.Dead)
                     {
-                        if (!_entMan.TryGetComponent(component.Owner, out MobStateComponent? mobState))
+                        if (!EntityManager.TryGetComponent(component.Owner, out MobStateComponent? mobState))
                             return;
 
                         if (!mobState.IsDead())
