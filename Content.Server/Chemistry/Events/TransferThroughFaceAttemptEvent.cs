9e31b9c8cde2ad051218dc314a80e19ac4a8f47e
UKNOWH <60119809+UKNOWH@users.noreply.github.com>
2022-04-27 09:05:42 +0800

    Added comment in TransferThroughFaceAttemptEvent.cs

--- Content.Server/Chemistry/Events/TransferThroughFaceAttemptEvent.cs
+++ Content.Server/Chemistry/Events/TransferThroughFaceAttemptEvent.cs
@@ -2,6 +2,9 @@ namespace Content.Server.Chemistry.Events;
 
 public sealed class TransferThroughFaceAttemptEvent : CancellableEntityEventArgs
 {
+    /// <summary>
+    /// The Target that is going to have the reagent transferred to
+    /// </summary>
     public EntityUid Uid { get; }
 
     public TransferThroughFaceAttemptEvent(EntityUid uid)
