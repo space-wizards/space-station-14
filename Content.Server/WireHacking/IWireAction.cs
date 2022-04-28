df3472e6dfbc399bf120cfa4bb4c0afc3a97d02e
vulppine <vulppine@gmail.com>
2022-04-27 20:36:55 -0700

    documentation for IWireAction

--- Content.Server/WireHacking/IWireAction.cs
+++ Content.Server/WireHacking/IWireAction.cs
@@ -3,64 +3,70 @@ using Robust.Shared.GameObjects;
 
 namespace Content.Server.Wires;
 
-// a replacement to all the repetitive wire fuckery
-// hidden in all components that used it:
-//
-// this is basically a set of things that occur when
-// a wire tied to a shared 'type' (e.g., power) is
-// activated/acted on
-//
-// takes the entity that was used, and the entity of
-// the user (to be sent into the relevant systems
-// that use it)
-//
-// for example, cutting a wire could toggle power
-// on the used entity, but if the user doesn't have
-// the correct protective equipment, then they should
-// recieve a shock and the entire thing is cancelled
-//
-// all of these return a bool to indicate if the
-// given action was actually performed or not
-//
-// this can also be serialized in YAML hopefully,
-// avoiding any further issues with repetitive code
-//
-// this should be expected to be flyweighted;
-// do NOT store per-entity state inside any class that
-// inherits IWireAction (prefer changing state inside
-// components instead)
+/// <summary>
+///     An interface used by WiresSystem to allow compositional wiresets.
+///     This is expected to be flyweighted, do not store per-entity state
+///     within an object/class that implements IWireAction.
+/// </summary>
 public interface IWireAction
 {
-    // This is to link the wire's status with
-    // its corresponding UI key. If this is null,
-    // GetStatusLightData MUST also return null,
-    // otherwise nothing happens.
+    /// <summary>
+    ///     This is to link the wire's status with
+    ///     its corresponding UI key. If this is null,
+    ///     GetStatusLightData MUST also return null,
+    ///     otherwise nothing happens.
+    /// </summary>
     public object? StatusKey { get; }
 
-    // Called when the wire in the layout
-    // is created for the first time. Ensures
-    // that the referenced action has all
-    // the correct system references (plus
-    // other information if needed,
-    // but wire actions should NOT be stateful!)
+    /// <summary>
+    ///     Called when the wire in the layout
+    ///     is created for the first time. Ensures
+    ///     that the referenced action has all
+    ///     the correct system references (plus
+    ///     other information if needed,
+    ///     but wire actions should NOT be stateful!)
+    /// </summary>
     public void Initialize();
 
-    // Called when a wire is finally processed
-    // by WiresSystem upon wire layout
-    // creation. Use this to set specific details
-    // about the state of the entity in question.
-    //
-    // If this returns false, this will convert
-    // the given wire into a 'dummy' wire instead.
+    /// <summary>
+    ///     Called when a wire is finally processed
+    ///     by WiresSystem upon wire layout
+    ///     creation. Use this to set specific details
+    ///     about the state of the entity in question.
+    ///
+    ///     If this returns false, this will convert
+    ///     the given wire into a 'dummy' wire instead.
+    /// </summary>
     public bool AddWire(Wire wire, int count);
 
+    /// <summary>
+    ///     What happens when this wire is cut.
+    /// </summary>
+    /// <returns>true if successful, false otherwise.</summary>
     public bool Cut(EntityUid user, Wire wire);
 
+    /// <summary>
+    ///     What happens when this wire is mended.
+    /// </summary>
+    /// <returns>true if successful, false otherwise.</summary>
     public bool Mend(EntityUid user, Wire wire);
 
+    /// <summary>
+    ///     What happens when this wire is pulsed.
+    /// </summary>
+    /// <returns>true if successful, false otherwise.</summary>
     public bool Pulse(EntityUid user, Wire wire);
 
+    /// <summary>
+    ///     Used when a wire's state on an entity needs to be updated.
+    ///     Mostly for things related to entity events, e.g., power.
+    /// </summary>
     public void Update(Wire wire);
 
+    /// <summary>
+    ///     Used for when WiresSystem requires the status light data
+    ///     for display on the client.
+    /// </summary>
+    /// <returns>StatusLightData to display light data, null to have no status light.</returns>
     public StatusLightData? GetStatusLightData(Wire wire);
 }
