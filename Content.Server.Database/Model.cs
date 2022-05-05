192fd2c8637f525dd418b4a3c70ca0dcaf924a38
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2022-05-05 10:56:17 +0200

    Make Profile.Markings db column jsonb

--- Content.Server.Database/Model.cs
+++ Content.Server.Database/Model.cs
@@ -199,7 +199,7 @@ namespace Content.Server.Database
         public string Sex { get; set; } = null!;
         public string Gender { get; set; } = null!;
         public string Species { get; set; } = null!;
-        public string Markings { get; set; } = null!;
+        [Column(TypeName = "jsonb")] public JsonDocument? Markings { get; set; } = null!;
         public string HairName { get; set; } = null!;
         public string HairColor { get; set; } = null!;
         public string FacialHairName { get; set; } = null!;
