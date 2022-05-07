c0e040b09f8feee9ffbebb505b9ac06442d8fdb7
hubismal <hubismal@gmail.com>
2022-05-07 14:23:30 -0500

    whitespace fix

--- Content.Server/Speech/SpeechNoiseSystem.cs
+++ Content.Server/Speech/SpeechNoiseSystem.cs
@@ -12,7 +12,7 @@ namespace Content.Server.Speech
     public sealed class SpeechSoundSystem : EntitySystem
     {
 
-		[Dependency] private readonly IPrototypeManager _proto = default!;
+        [Dependency] private readonly IPrototypeManager _proto = default!;
 
         public override void Initialize()
         {
@@ -22,52 +22,52 @@ namespace Content.Server.Speech
             SubscribeLocalEvent<SharedSpeechComponent, ComponentStartup>(OnStartup);
         }
 
-		private void OnStartup(EntityUid uid, SharedSpeechComponent component, ComponentStartup args)
-		{
-			//Cache prototype on component startup.
-			if (_proto.TryIndex(component.SpeechSoundsId, out SpeechSoundsPrototype? speechsnds))
-			{
-				if (speechsnds != null)
-				{
-					component.SpeechSoundsCache = speechsnds;
-				}
-			}
-		}
+        private void OnStartup(EntityUid uid, SharedSpeechComponent component, ComponentStartup args)
+        {
+            //Cache prototype on component startup.
+            if (_proto.TryIndex(component.SpeechSoundsId, out SpeechSoundsPrototype? speechsnds))
+            {
+                if (speechsnds != null)
+                {
+                    component.SpeechSoundsCache = speechsnds;
+                }
+            }
+        }
 
 
         private void OnEntitySpoke(EntityUid uid, SharedSpeechComponent component, EntitySpokeEvent args)
         {
-			if (!component.PlaySpeechSound) return;
+            if (!component.PlaySpeechSound) return;
 
             //Play speech sound
 
             if (component.SpeechSoundsCache != null)
             {
-				//Re-Index sounds prototype if cached proto ID is outdated, allows VV and changing the voice.
-				if (component.SpeechSoundsCache.ID != component.SpeechSoundsId)
-				{
-					if (_proto.TryIndex(component.SpeechSoundsId, out SpeechSoundsPrototype? speechsnds))
-					{
-						if (speechsnds != null)
-						{
-							component.SpeechSoundsCache = speechsnds;
-						}
-					}
-				}
-				var contextSound = component.SpeechSoundsCache.SaySound;
-				//Different sounds for ask/exclaim based on last character
-				switch (args.Message[args.Message.Length-1])
-				{
-					case '?':
-						contextSound = component.SpeechSoundsCache.AskSound;
-						break;
-					case '!':
-						contextSound = component.SpeechSoundsCache.ExclaimSound;
-						break;
+                //Re-Index sounds prototype if cached proto ID is outdated, allows VV and changing the voice.
+                if (component.SpeechSoundsCache.ID != component.SpeechSoundsId)
+                {
+                    if (_proto.TryIndex(component.SpeechSoundsId, out SpeechSoundsPrototype? speechsnds))
+                    {
+                        if (speechsnds != null)
+                        {
+                            component.SpeechSoundsCache = speechsnds;
+                        }
+                    }
+                }
+                var contextSound = component.SpeechSoundsCache.SaySound;
+                //Different sounds for ask/exclaim based on last character
+                switch (args.Message[args.Message.Length-1])
+                {
+                    case '?':
+                        contextSound = component.SpeechSoundsCache.AskSound;
+                        break;
+                    case '!':
+                        contextSound = component.SpeechSoundsCache.ExclaimSound;
+                        break;
 
-				}
-				SoundSystem.Play(Filter.Pvs(uid), contextSound.GetSound(), uid, component.AudioParams);
-			}
+                }
+                SoundSystem.Play(Filter.Pvs(uid), contextSound.GetSound(), uid, component.AudioParams);
+            }
         }
 
     }
