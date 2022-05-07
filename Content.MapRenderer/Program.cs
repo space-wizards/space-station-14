689ccc86b7e6ba173ef72a4f38fc8368809cf65a
5150Con <104950325+5150Con@users.noreply.github.com>
2022-05-07 13:53:53 -0600

    Update Program.cs

--- Content.MapRenderer/Program.cs
+++ Content.MapRenderer/Program.cs
@@ -12,7 +12,7 @@ namespace Content.MapRenderer
 {
     internal class Program
     {
-        private const string MapsAddedEnvKey = "FILES_ADDED";
+        private const string MapsAddedEnvKey = MyStation.yml ;
         private const string MapsModifiedEnvKey = "FILES_MODIFIED";
 
         private static readonly MapPainter MapPainter = new();
