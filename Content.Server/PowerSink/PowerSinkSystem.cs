a14895f01bc554d70816a72cdd40216beee841f9
Veritius <veritiusgaming@gmail.com>
2022-05-08 18:21:08 +1000

    Add examine stuff

--- Content.Server/PowerSink/PowerSinkSystem.cs
+++ Content.Server/PowerSink/PowerSinkSystem.cs
@@ -1,11 +1,48 @@
 ﻿using Content.Server.Explosion.EntitySystems;
 using Content.Server.Power.Components;
+using Content.Shared.Body.Events;
+using Content.Shared.Examine;
 
 namespace Content.Server.PowerSink
 {
     public sealed class PowerSinkSystem : EntitySystem
     {
         [Dependency] private readonly ExplosionSystem _explosionSystem = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<PowerSinkComponent, ExaminedEvent>(OnExamine);
+        }
+
+        private void OnExamine(EntityUid uid, PowerSinkComponent component, ExaminedEvent args)
+        {
+            if (!TryComp<PowerSinkComponent>(uid, out var batteryComponent))
+                return;
+            if (args.IsInDetailsRange)
+            {
+                var effectiveMax = component.Capacity;
+                if (effectiveMax == 0) {effectiveMax = 1;}
+                var chargeFraction = batteryComponent.Charge / effectiveMax;
+                var chargePercentRounded = (int) (chargeFraction * 100);
+                args.PushMarkup(
+                    Loc.GetString(
+                        "powersink-examine-charge-percent",
+                        ("percent", chargePercentRounded),
+                        ("markupPercentColor", "green")
+                    )
+                );
+                var drainAmount = (int) Comp<PowerConsumerComponent>(component.Owner).NetworkLoad.ReceivingPower / 1000;
+                args.PushMarkup(
+                    Loc.GetString(
+                        "powersink-examine-drain-amount",
+                        ("amount", drainAmount),
+                        ("markupDrainColor", "orange")
+                    )
+                    );
+            }
+        }
 
         public override void Update(float frameTime)
         {
---
+++ Resources/Locale/en-US/powersink/powersink.ftl
@@ -0,0 +1,2 @@
+powersink-examine-charge-percent = The power sink is [color={$markupPercentColor}]{$percent}%[/color] charged.
+powersink-examine-drain-amount = The power sink is draining [color={$markupDrainColor}]{$amount} kW[/color].
