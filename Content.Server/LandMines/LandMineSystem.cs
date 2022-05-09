8453baf31fd3cb8c31cef50fa2e06995b47fa438
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-05-09 17:40:33 +0200

    Make mines use step trigger.

--- Content.Server/LandMines/LandMineSystem.cs
+++ Content.Server/LandMines/LandMineSystem.cs
@@ -1,20 +1,36 @@
-﻿using Robust.Shared.Physics.Dynamics;
+﻿using Content.Shared.Popups;
+using Content.Shared.StepTrigger;
+using Robust.Shared.Player;
 
 namespace Content.Server.LandMines;
 
 public sealed class LandMineSystem : EntitySystem
 {
+    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
+
+
     public override void Initialize()
     {
-        SubscribeLocalEvent<LandMineComponent, StartCollideEvent>(HandleCollide);
-
+        SubscribeLocalEvent<LandMineComponent, StepTriggeredEvent>(HandleTriggered);
+        SubscribeLocalEvent<LandMineComponent, StepTriggerAttemptEvent>(HandleTriggerAttempt);
     }
 
-    private void HandleCollide(EntityUid uid, LandMineComponent component, StartCollideEvent args)
+    private static void HandleTriggerAttempt(
+        EntityUid uid,
+        LandMineComponent component,
+        ref StepTriggerAttemptEvent args)
     {
-        var ent = args.OtherFixture.Body.Owner;
+        args.Continue = true;
+    }
 
-        RaiseLocalEvent(uid, new MineTriggeredEvent { Tripper = ent });
+    private void HandleTriggered(EntityUid uid, LandMineComponent component, ref StepTriggeredEvent args)
+    {
+        _popupSystem.PopupCoordinates(
+            Loc.GetString("land-mine-triggered", ("mine", uid)),
+            Transform(uid).Coordinates,
+            Filter.Entities(args.Tripper));
+
+        RaiseLocalEvent(uid, new MineTriggeredEvent { Tripper = args.Tripper });
 
         QueueDel(uid);
     }
