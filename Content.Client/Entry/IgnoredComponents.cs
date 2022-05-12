e2ac92c3b5e2172a00adbfe4fc74e9929f8a4587
keronshb <54602815+keronshb@users.noreply.github.com>
2022-05-12 03:58:27 -0400

    Allows pickuppable animals to resist out of inventory (#7545)
    Co-authored-by: ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>

--- Content.Client/Entry/IgnoredComponents.cs
+++ Content.Client/Entry/IgnoredComponents.cs
@@ -348,6 +348,7 @@ namespace Content.Client.Entry
             "InteractionPopup",
             "HealthAnalyzer",
             "Thirst",
+            "CanEscapeInventory",
             "Wires"
         };
     }
