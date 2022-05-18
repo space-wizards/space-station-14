8a10107616b54a454a867ae598d5ef86748402fc
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2022-05-19 07:53:37 +1000

    Lots of pathfinder bugfixes (#8248)

--- Content.Server/AI/Pathfinding/PathfindingNode.cs
+++ Content.Server/AI/Pathfinding/PathfindingNode.cs
@@ -263,7 +263,8 @@ namespace Content.Server.AI.Pathfinding
 
             DebugTools.Assert((PathfindingSystem.TrackedCollisionLayers & physicsComponent.CollisionLayer) != 0);
 
-            if (physicsComponent.BodyType != BodyType.Static)
+            if (physicsComponent.BodyType != BodyType.Static ||
+                !physicsComponent.Hard)
             {
                 _physicsLayers.TryAdd(entity, physicsComponent.CollisionLayer);
             }
@@ -285,18 +286,19 @@ namespace Content.Server.AI.Pathfinding
             // There's no guarantee that the entity isn't deleted
             // 90% of updates are probably entities moving around
             // Entity can't be under multiple categories so just checking each once is fine.
-            if (_physicsLayers.ContainsKey(entity))
+            if (_physicsLayers.Remove(entity))
             {
-                _physicsLayers.Remove(entity);
+                return;
             }
-            else if (_accessReaders.ContainsKey(entity))
+
+            if (_accessReaders.Remove(entity))
             {
-                _accessReaders.Remove(entity);
                 ParentChunk.Dirty();
+                return;
             }
-            else if (_blockedCollidables.ContainsKey(entity))
+
+            if (_blockedCollidables.Remove(entity))
             {
-                _blockedCollidables.Remove(entity);
                 GenerateMask();
                 ParentChunk.Dirty();
             }
