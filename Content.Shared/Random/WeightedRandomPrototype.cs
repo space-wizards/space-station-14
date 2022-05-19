c7356e1bef83baecd2dee96aba3790700e053dc3
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-05-19 12:59:26 +1000

    a

--- Content.Shared/Random/WeightedRandomPrototype.cs
+++ Content.Shared/Random/WeightedRandomPrototype.cs
@@ -2,6 +2,9 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Random;
 
+/// <summary>
+/// Generic random weighting dataset to use.
+/// </summary>
 [Prototype("weightedRandom")]
 public sealed class WeightedRandomPrototype : IPrototype
 {
