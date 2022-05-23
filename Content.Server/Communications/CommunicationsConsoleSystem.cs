37089d9ce63ba36e6923141a1ab604074181bb67
Veritius <veritiusgaming@gmail.com>
2022-05-23 14:43:23 +1000

    Revert "c"
    This reverts commit 0047f530a407fa0cb394f0239e0377394f773e6f.

--- Content.Server/Communications/CommunicationsConsoleSystem.cs
+++ Content.Server/Communications/CommunicationsConsoleSystem.cs
@@ -30,10 +30,10 @@ namespace Content.Server.Communications
         public override void Initialize()
         {
             // All events that refresh the BUI
-            SubscribeLocalEvent<CommunicationsConsoleComponent, ComponentInit>((uid, comp, _) => UpdateBoundUserInterface(uid, comp));
-            SubscribeLocalEvent<CommunicationsConsoleComponent, RoundEndSystemChangedEvent>((uid, comp, _) => UpdateBoundUserInterface(uid, comp));
-            SubscribeLocalEvent<CommunicationsConsoleComponent, AlertLevelChangedEvent>((uid, comp, _) => UpdateBoundUserInterface(uid, comp));
-            SubscribeLocalEvent<CommunicationsConsoleComponent, AlertLevelDelayFinishedEvent>((uid, comp, _) => UpdateBoundUserInterface(uid, comp));
+            SubscribeLocalEvent<CommunicationsConsoleComponent, ComponentInit>((_, comp, _) => UpdateBoundUserInterface(comp));
+            SubscribeLocalEvent<CommunicationsConsoleComponent, RoundEndSystemChangedEvent>((_, comp, _) => UpdateBoundUserInterface(comp));
+            SubscribeLocalEvent<CommunicationsConsoleComponent, AlertLevelChangedEvent>((_, comp, _) => UpdateBoundUserInterface(comp));
+            SubscribeLocalEvent<CommunicationsConsoleComponent, AlertLevelDelayFinishedEvent>((_, comp, _) => UpdateBoundUserInterface(comp));
 
             // Messages from the BUI
             SubscribeLocalEvent<CommunicationsConsoleComponent, CommunicationsConsoleSelectAlertLevelMessage>(OnSelectAlertLevelMessage);
@@ -50,7 +50,7 @@ namespace Content.Server.Communications
                 {
                     // TODO: Find a less ass way of refreshing the UI
                     if (!comp.AlreadyRefreshed) return;
-                    UpdateBoundUserInterface(comp.Owner, comp);
+                    UpdateBoundUserInterface(comp);
                     comp.AlreadyRefreshed = true;
                     return;
                 }
@@ -60,8 +60,10 @@ namespace Content.Server.Communications
             base.Update(frameTime);
         }
 
-        private void UpdateBoundUserInterface(EntityUid uid, CommunicationsConsoleComponent comp)
+        private void UpdateBoundUserInterface(CommunicationsConsoleComponent comp)
         {
+            var uid = comp.Owner;
+
             var stationUid = _stationSystem.GetOwningStation(uid);
             List<string>? levels = null;
             string currentLevel = default!;
@@ -158,7 +160,7 @@ namespace Content.Server.Communications
 
             comp.AnnouncementCooldownRemaining = comp.DelayBetweenAnnouncements;
             comp.AlreadyRefreshed = false;
-            UpdateBoundUserInterface(uid, comp);
+            UpdateBoundUserInterface(comp);
 
             msg += "\n" + Loc.GetString("communicationsconsole-announcement-sent-by") + " " + author;
             _chatManager.DispatchStationAnnouncement(msg, Loc.GetString(comp.AnnouncementDisplayName), colorOverride: comp.AnnouncementColor);
