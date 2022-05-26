711cd3b5772c3e4b869010222a5fe7510df71b0b
EmoGarbage404 <retron404@gmail.com>
2022-05-26 16:58:30 -0400

    ez gg dubskis

--- Content.Server/Explosion/Components/TriggerOnCollideComponent.cs
+++ Content.Server/Explosion/Components/TriggerOnCollideComponent.cs
@@ -3,5 +3,7 @@ namespace Content.Server.Explosion.Components
     [RegisterComponent]
     public sealed class TriggerOnCollideComponent : Component
     {
+		[DataField("fixtureID")]
+		public string FixtureID = String.Empty;
     }
 }
