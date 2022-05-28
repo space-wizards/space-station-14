81419cfe10e4f58918c4d09bad9b243e9ff9aeb2
wrexbe <wrexbe@protonmail.com>
2022-05-28 10:08:44 -0700

    This

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -69,6 +69,7 @@ namespace Content.Client.Entry
 
             factory.DoAutoRegistrations();
 
+            factory.IgnoreMissingComponents();
 
             factory.RegisterClass<SharedResearchConsoleComponent>();
             factory.RegisterClass<SharedLatheComponent>();
