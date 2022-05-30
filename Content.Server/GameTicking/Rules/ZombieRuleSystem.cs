67e5625af30a3962d0c96f880b5a34226813dfa4
EmoGarbage404 <retron404@gmail.com>
2022-05-29 23:59:20 -0400

    shuttle spawning

--- Content.Server/GameTicking/Rules/ZombieRuleSystem.cs
+++ Content.Server/GameTicking/Rules/ZombieRuleSystem.cs
@@ -3,10 +3,12 @@ using Content.Server.Chat.Managers;
 using Content.Server.Disease;
 using Content.Server.Nuke;
 using Content.Server.Players;
+using Content.Server.Roles;
 using Content.Server.RoundEnd;
 using Content.Server.Spawners.Components;
 using Content.Server.Station.Components;
 using Content.Server.Station.Systems;
+using Content.Server.Traitor;
 using Content.Shared.CCVar;
 using Content.Shared.MobState;
 using Content.Shared.Roles;
@@ -29,6 +31,7 @@ public sealed class ZombieRuleSystem : GameRuleSystem
     [Dependency] private readonly IChatManager _chatManager = default!;
     [Dependency] private readonly IMapLoader _mapLoader = default!;
     [Dependency] private readonly IMapManager _mapManager = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
     [Dependency] private readonly StationSpawningSystem _stationSpawningSystem = default!;
     [Dependency] private readonly StationSystem _stationSystem = default!;
     [Dependency] private readonly RoundEndSystem _roundEndSystem = default!;
@@ -75,70 +78,7 @@ public sealed class ZombieRuleSystem : GameRuleSystem
 
     private void OnJobAssigned(RulePlayerJobsAssignedEvent ev)
     {
-        if (!Enabled)
-            return;
-
-        var list = new List<IPlayerSession>(ev.Players);
-        var prefList = new List<IPlayerSession>();
-
-        foreach (var player in list)
-        {
-            if (!ev.Profiles.ContainsKey(player.UserId))
-            {
-                continue;
-            }
-            prefList.Add(player); //by default, zombies don't care whether or not you are command or nonantag crew
-        }
-
-        var playersPerInfected = _cfg.GetCVar(CCVars.ZombiePlayersPerInfected);
-        var maxInfected = _cfg.GetCVar(CCVars.ZombieMaxInfected);
-
-        var numInfected = Math.Max(1,
-            (int) Math.Min(
-                Math.Floor((double) ev.Players.Length / playersPerInfected), maxInfected));
-
-        for (var i = 0; i < numInfected; i++)
-        {
-            IPlayerSession zombie; //this isn't actually a zombie, but rather a patient 0. sue me for naming it shitty -emo
-            if (prefList.Count == 0)
-            {
-                if (list.Count == 0)
-                {
-                    Logger.InfoS("preset", "Insufficient ready players to fill up with patient 0, stopping the selection.");
-                    break;
-                }
-                zombie = _random.PickAndTake(list);
-                list.Remove(zombie);
-                Logger.InfoS("preset", "Insufficient preferred patient 0, picking at random.");
-            }
-            else
-            {
-                zombie = _random.PickAndTake(prefList);
-                list.Remove(zombie);
-                Logger.InfoS("preset", "Selected a preferred patient 0.");
-            }
-
-            var mind = zombie.Data.ContentData()?.Mind;
-            if (mind == null)
-            {
-                Logger.ErrorS("preset", "Failed getting mind for picked patient 0.");
-                continue;
-            }
-
-            DebugTools.AssertNotNull(mind.OwnedEntity);
-
-            if (mind.OwnedEntity != null)
-                _diseaseSystem.TryAddDisease(mind.OwnedEntity.Value, InitialZombieVirusPrototype); //change this once zombie refactor is in.
-
-            if (mind.Session != null)
-            {
-                var messageWrapper = Loc.GetString("chat-manager-server-wrap-message");
-
-                // I went all the way to ChatManager.cs and all i got was this lousy T-shirt
-                _chatManager.ChatMessageToOne(Shared.Chat.ChatChannel.Server, Loc.GetString("zombie-patientzero-role-greeting"),
-                   messageWrapper, default, false, mind.Session.ConnectedClient, Color.Plum);
-            }
-        }
+        InfectInitialPlayers();
     }
 
     private void OnPlayersSpawning(RulePlayerSpawningEvent ev)
@@ -147,41 +87,7 @@ public sealed class ZombieRuleSystem : GameRuleSystem
             return;
         //these are staying here because i want the code for reference
 
-        // Between 1 and <max op count>: needs at least n players per op.
-        /*var numInfected = Math.Max(1,
-            (int)Math.Min(
-                Math.Floor((double)ev.PlayerPool.Count / _cfg.GetCVar(CCVars.ZombiePlayersPerInfected)), _cfg.GetCVar(CCVars.ZombieMaxInfected)));
-
-        _random.PickAndTake(ev.PlayerPool);
-        */
-
         /*
-        var map = "/Maps/infiltrator.yml";
-
-        var aabbs = _stationSystem.Stations.SelectMany(x =>
-            Comp<StationDataComponent>(x).Grids.Select(x => _mapManager.GetGridComp(x).Grid.WorldAABB)).ToArray();
-        var aabb = aabbs[0];
-        for (int i = 1; i < aabbs.Length; i++)
-        {
-            aabb.Union(aabbs[i]);
-        }
-
-        var (_, gridId) = _mapLoader.LoadBlueprint(GameTicker.DefaultMap, map, new MapLoadOptions
-        {
-            Offset = aabb.Center + MathF.Max(aabb.Height / 2f, aabb.Width / 2f) * 2.5f
-        });
-
-        if (!gridId.HasValue)
-        {
-            Logger.ErrorS("NUKEOPS", $"Gridid was null when loading \"{map}\", aborting.");
-            foreach (var session in ops)
-            {
-                ev.PlayerPool.Add(session);
-            }
-            return;
-        }
-
-        var gridUid = _mapManager.GetGridEuid(gridId.Value);
         
         // TODO: Loot table or something
         var commanderGear = _prototypeManager.Index<StartingGearPrototype>("SyndicateCommanderGearFull");
@@ -271,22 +177,106 @@ public sealed class ZombieRuleSystem : GameRuleSystem
 
     public override void Started()
     {
-        _opsWon = false;
+        InfectInitialPlayers();
     }
 
     public override void Ended() { }
 
+    /// <summary>
+    /// Infects the first players with the passive zombie virus.
+    /// </summary>
+    private void InfectInitialPlayers()
+    {
+        var playerList = _playerManager.ServerSessions.ToList();
+
+        if (playerList.Count == 0)
+            return;
+
+        var playersPerInfected = _cfg.GetCVar(CCVars.ZombiePlayersPerInfected);
+        var maxInfected = _cfg.GetCVar(CCVars.ZombieMaxInfected);
+
+        var numInfected = Math.Max(1,
+            (int) Math.Min(
+                Math.Floor((double) playerList.Count / playersPerInfected), maxInfected));
+
+        for (var i = 0; i < numInfected; i++)
+        {
+            if (playerList.Count == 0)
+            {
+                Logger.InfoS("preset", "Insufficient number of players. stopping selection.");
+                break;
+            }
+            IPlayerSession zombie = _random.PickAndTake(playerList);
+            playerList.Remove(zombie);
+            Logger.InfoS("preset", "Selected a patient 0.");
+
+            var mind = zombie.Data.ContentData()?.Mind;
+            if (mind == null)
+            {
+                Logger.ErrorS("preset", "Failed getting mind for picked patient 0.");
+                continue;
+            }
+            
+            DebugTools.AssertNotNull(mind.OwnedEntity);
+
+            mind.AddRole(new TraitorRole(mind, _prototypeManager.Index<AntagPrototype>(PatientZeroPrototypeID)));
+            if (mind.OwnedEntity != null)
+                _diseaseSystem.TryAddDisease(mind.OwnedEntity.Value, InitialZombieVirusPrototype); //change this once zombie refactor is in.
+
+            if (mind.Session != null)
+            {
+                var messageWrapper = Loc.GetString("chat-manager-server-wrap-message");
+
+                // I went all the way to ChatManager.cs and all i got was this lousy T-shirt
+                _chatManager.ChatMessageToOne(Shared.Chat.ChatChannel.Server, Loc.GetString("zombie-patientzero-role-greeting"),
+                   messageWrapper, default, false, mind.Session.ConnectedClient, Color.Plum);
+            }
+        }
+    }
+
     /// EVENTS
     /// These are all the functions that handle midround events
     /// that can occur during zombie mode
-
+    
+    /// <summary>
+    /// This event announces the nuke code and spawns
+    /// the disk inside of it. This is for round end
+    /// so the duplicated disks shouldn't really matter.
+    /// </summary>
     private void UnlockNuke()
     {
         _chatManager.DispatchStationAnnouncement(Loc.GetString("zombie-nuke-armed-event", ("code",_nukeCodeSystem.Code)), "Centcomm", default, Color.Crimson);
         foreach (var nuke in EntityManager.EntityQuery<NukeComponent>().ToList())
         {
             if (nuke.DiskSlot.ContainerSlot != null)
+            {
                 nuke.DiskSlot.ContainerSlot.Insert(Spawn("NukeDisk", Transform(nuke.Owner).Coordinates));
+            }
+                
         }
+    }
+
+    /// <summary>
+    /// Spawns a shuttle a distance away from the main station
+    /// This code is 100% canibalized from nukies
+    /// idk how it works. -emo
+    /// </summary>
+    /// <param name="map">The file path to the map file</param>
+    private void SpawnShuttle(string map)
+    {
+        _chatManager.DispatchStationAnnouncement(Loc.GetString("zombie-shuttle-call-event", ("code", _nukeCodeSystem.Code)), "Centcomm", default, Color.Crimson);
+
+        var aabbs = _stationSystem.Stations.SelectMany(x =>
+            Comp<StationDataComponent>(x).Grids.Select(x => _mapManager.GetGridComp(x).Grid.WorldAABB)).ToArray();
+        var aabb = aabbs[0];
+        for (int i = 1; i < aabbs.Length; i++)
+        {
+            aabb.Union(aabbs[i]);
+        }
+
+        var (_, gridId) = _mapLoader.LoadBlueprint(GameTicker.DefaultMap, map, new MapLoadOptions
+        {
+            Offset = aabb.Center + MathF.Max(aabb.Height / 2f, aabb.Width / 2f) * 2.5f
+        });
     }
 }
