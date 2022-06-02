d54158881bfbcb6e2d8cfa5e3cefc429d6e8afc2
Veritius <veritiusgaming@gmail.com>
2022-06-02 20:49:11 +1000

    manager stuff

--- Content.Server/RoleTimer/RoleTimerManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Content.Server.Database;
-
-namespace Content.Server.RoleTimer.Managers
-{
-    public sealed class RoleTimerManager
-    {
-        [Dependency] private readonly IServerDbManager _db = default!;
-    }
-}
---
+++ Content.Server/RoleTimers/RoleTimerManagers.cs
@@ -0,0 +1,25 @@
+﻿using Content.Server.Database;
+using Robust.Shared.Network;
+
+namespace Content.Server.RoleTimers
+{
+    public sealed class RoleTimerManager
+    {
+        [Dependency] private readonly IServerDbManager _db = default!;
+
+        private RoleTimer GetRoleTimer(NetUserId userId, string role)
+        {
+            return _db.GetRoleTimer(userId.UserId, role).Result;
+        }
+
+        private List<RoleTimer> GetRoleTimers(NetUserId userId)
+        {
+            return _db.GetRoleTimers(userId.UserId).Result;
+        }
+
+        private void EditRoleTimer(int id, TimeSpan time)
+        {
+            _db.EditRoleTimer(id, time);
+        }
+    }
+}
