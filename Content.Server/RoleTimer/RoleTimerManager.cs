d2554dd54e6dff168aa428a42f2567ceee82c9a1
Veritius <veritiusgaming@gmail.com>
2022-06-02 20:08:12 +1000

    Revert "Hopefully that's all the database stuff done"
    This reverts commit 880a52960e7686f28241443c92cb91d50e9d6cf2.

--- Content.Server/RoleTimer/RoleTimerManager.cs
+++ Content.Server/RoleTimer/RoleTimerManager.cs
@@ -1,26 +1,9 @@
-﻿using System.Threading.Tasks;
-using Content.Server.Database;
-using Robust.Shared.Network;
+﻿using Content.Server.Database;
 
-namespace Content.Server.RoleTimer
+namespace Content.Server.RoleTimer.Managers
 {
-    public sealed class RoleTimerManager : IRoleTimerManager
+    public sealed class RoleTimerManager
     {
         [Dependency] private readonly IServerDbManager _db = default!;
-
-        public async Task SetPlaytimeForRole(NetUserId player, string role, TimeSpan time)
-        {
-            await _db.SetPlaytimeForRole(player, role, time);
-        }
-
-        public async Task<TimeSpan?> GetPlaytimeForRole(NetUserId player, string role)
-        {
-            return await _db.GetPlaytimeForRole(player, role);
-        }
-
-        public async Task<Dictionary<string, TimeSpan>?> GetPlaytimeAllRoles(NetUserId player)
-        {
-            return await _db.GetPlaytimeAllRoles(player);
-        }
     }
 }
