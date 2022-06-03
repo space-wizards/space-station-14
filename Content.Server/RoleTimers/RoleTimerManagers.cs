7f99e6f4f961ec69aab426723e15ce35af5a5f44
Veritius <veritiusgaming@gmail.com>
2022-06-03 11:02:38 +1000

    Manager stuff

--- Content.Server/RoleTimers/RoleTimerManagers.cs
+++ Content.Server/RoleTimers/RoleTimerManagers.cs
@@ -1,24 +1,48 @@
 ﻿using System.Threading.Tasks;
 using Content.Server.Database;
+using Robust.Server.Player;
 using Robust.Shared.Network;
 
 namespace Content.Server.RoleTimers
 {
-    public sealed class RoleTimerManager
+    public interface IRoleTimerManager
+    {
+        Task<RoleTimer> GetRoleTimer(NetUserId userId, string role);
+
+        Task<List<RoleTimer>> GetRoleTimers(NetUserId userId);
+
+        Task EditRoleTimer(int id, TimeSpan time);
+    }
+
+    public sealed class RoleTimerManager : IRoleTimerManager, IEntityEventSubscriber
     {
         [Dependency] private readonly IServerDbManager _db = default!;
+        [Dependency] private readonly IPlayerManager _playerManager = default!;
 
-        private async Task<RoleTimer> GetRoleTimer(NetUserId userId, string role)
+        private const int StateCheckTime = 90;
+        private Dictionary<IPlayerSession, Tuple<string, DateTime>> _playerData = new();
+
+        public void Initialize()
+        {
+            _playerManager.PlayerStatusChanged += OnPlayerStatusChanged;
+        }
+
+        private void OnPlayerStatusChanged(object? sender, SessionStatusEventArgs args)
+        {
+
+        }
+
+        public async Task<RoleTimer> GetRoleTimer(NetUserId userId, string role)
         {
             return await _db.GetRoleTimer(userId.UserId, role);
         }
 
-        private async Task<List<RoleTimer>> GetRoleTimers(NetUserId userId)
+        public async Task<List<RoleTimer>> GetRoleTimers(NetUserId userId)
         {
             return await _db.GetRoleTimers(userId.UserId);
         }
 
-        private async Task EditRoleTimer(int id, TimeSpan time)
+        public async Task EditRoleTimer(int id, TimeSpan time)
         {
             await _db.EditRoleTimer(id, time);
         }
