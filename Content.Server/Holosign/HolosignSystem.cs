37c5005522b37af1b336ef2b5da32b769cffc82a
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-06-03 21:45:36 +1000

    holo dis

--- Content.Server/Holosign/HolosignSystem.cs
+++ Content.Server/Holosign/HolosignSystem.cs
@@ -1,30 +1,14 @@
 using Content.Shared.Interaction.Events;
 using Content.Shared.Examine;
 using Content.Server.Coordinates.Helpers;
+using Robust.Shared.Timing;
 
 namespace Content.Server.Holosign
 {
     public sealed class HolosignSystem : EntitySystem
     {
+        [Dependency] private readonly IGameTiming _timing = default!;
 
-        public override void Update(float frameTime)
-        {
-            base.Update(frameTime);
-            foreach (var projector in EntityQuery<HolosignProjectorComponent>())
-            {
-                if (projector.CurrentCharges == projector.Charges)
-                    continue;
-
-                projector.Accumulator += frameTime;
-                if (projector.Accumulator < projector.RechargeTime.TotalSeconds)
-                {
-                    continue;
-                }
-
-                projector.Accumulator = 0;
-                projector.CurrentCharges++;
-            }
-        }
         public override void Initialize()
         {
             base.Initialize();
@@ -32,11 +16,20 @@ namespace Content.Server.Holosign
             SubscribeLocalEvent<HolosignProjectorComponent, ExaminedEvent>(OnExamine);
         }
 
+        private int GetCharges(HolosignProjectorComponent component)
+        {
+            return component.CurrentCharges + (int) ((_timing.CurTime - component.LastUsed).TotalSeconds / component.RechargeTime.TotalSeconds);
+        }
+
         private void OnExamine(EntityUid uid, HolosignProjectorComponent component, ExaminedEvent args)
         {
-            float timeRemaining = ((float) component.RechargeTime.TotalSeconds - component.Accumulator);
-            args.PushMarkup(Loc.GetString("emag-charges-remaining", ("charges", component.CurrentCharges)));
-            if (component.CurrentCharges == component.Charges)
+            // TODO: This should probably be using an itemstatus
+            // TODO: I'm too lazy to do this rn but it's literally copy-paste from emag.
+            var timeRemaining = (component.LastUsed + component.RechargeTime * (component.MaxCharges - component.CurrentCharges) - _timing.CurTime).TotalSeconds % component.RechargeTime.TotalSeconds;
+            var charges = GetCharges(component);
+
+            args.PushMarkup(Loc.GetString("emag-charges-remaining", ("charges", charges)));
+            if (charges == component.MaxCharges)
             {
                 args.PushMarkup(Loc.GetString("emag-max-charges"));
                 return;
@@ -46,11 +39,19 @@ namespace Content.Server.Holosign
 
         private void OnUse(EntityUid uid, HolosignProjectorComponent component, UseInHandEvent args)
         {
-            if (component.CurrentCharges == 0)
+            if (component.CurrentCharges == 0 || args.Handled)
                 return;
 
-            EntityManager.SpawnEntity(component.SignProto, Transform(args.User).Coordinates.SnapToGrid());
+            // TODO: Too tired to deal
+            var holo = EntityManager.SpawnEntity(component.SignProto, Transform(args.User).Coordinates.SnapToGrid(EntityManager));
+            Transform(holo).Anchored = true;
+
+            // Don't reset last use time if it's already accumulating.
+            if (component.CurrentCharges == component.MaxCharges)
+                component.LastUsed = _timing.CurTime;
+
             component.CurrentCharges--;
+            args.Handled = true;
         }
     }
 }
