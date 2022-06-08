2f826fda9c1dc5de6b74010606bd179de57a07a4
LudwigVonChesterfield <17705613+LudwigVonChesterfield@users.noreply.github.com>
2022-06-08 18:50:36 -0400

    fixes due to rename

--- Content.Server/Abilities/Mime/MimePowersComponent.cs
+++ Content.Server/Abilities/Mime/MimePowersComponent.cs
@@ -28,7 +28,7 @@ namespace Content.Server.Abilities.Mime
         public InstantAction InvisibleWallAction = new()
         {
             UseDelay = TimeSpan.FromSeconds(30),
-            Icon = new SpriteSpecifier.Texture(new ResourcePath("Structures/Walls/solid.rsi/full.png")),
+            Icon = new SpriteSpecifier.Texture(new ResourcePath("Structures/Walls/solid.rsi/solidfull.png")),
             Name = "mime-invisible-wall",
             Description = "mime-invisible-wall-desc",
             Priority = -1,
