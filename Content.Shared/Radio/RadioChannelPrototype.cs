a309b60dee51e9d93a89c619d6c285a5a6881ff4
Kevin Zheng <kevinz5000@gmail.com>
2022-06-10 17:33:52 -0700

    Document prototype fields

--- Content.Shared/Radio/RadioChannelPrototype.cs
+++ Content.Shared/Radio/RadioChannelPrototype.cs
@@ -5,8 +5,13 @@ namespace Content.Shared.Radio
     [Prototype("radioChannel")]
     public sealed class RadioChannelPrototype : IPrototype
     {
+        // Human-readable name for the channel.
         [ViewVariables] [DataField("name")] public string Name { get; private set; } = string.Empty;
+
+        // Single-character prefix to determine what channel a message should be sent to.
         [ViewVariables] [DataField("keycode")] public char KeyCode { get; private set; } = '\0';
+
+        // Integer frequency of this channel.
         [ViewVariables] [DataField("channel")] public int Channel { get; private set; } = 0;
 
         [ViewVariables]
