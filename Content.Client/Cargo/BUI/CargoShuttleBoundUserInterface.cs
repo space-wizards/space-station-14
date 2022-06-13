7d1c7b26b7a56c76deb5f3784c5776e21ce67b89
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-06-13 15:14:23 +1000

    Console working again

--- Content.Client/Cargo/BUI/CargoShuttleBoundUserInterface.cs
+++
@@ -1,8 +0,0 @@
-using Robust.Client.GameObjects;
-
-namespace Content.Client.Cargo.BUI;
-
-public abstract class CargoShuttleBoundUserInterface : BoundUserInterface
-{
-    protected CargoShuttleBoundUserInterface(ClientUserInterfaceComponent owner, object uiKey) : base(owner, uiKey) {}
-}
---
+++ Content.Client/Cargo/BUI/CargoShuttleConsoleBoundUserInterface.cs
@@ -0,0 +1,8 @@
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Cargo.BUI;
+
+public abstract class CargoShuttleConsoleBoundUserInterface : BoundUserInterface
+{
+    public CargoShuttleConsoleBoundUserInterface(ClientUserInterfaceComponent owner, object uiKey) : base(owner, uiKey) {}
+}
