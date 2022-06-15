291a758f9293ceee9cb9c1a43af90c9c75f06db2
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-06-15 23:22:29 +1000

    Telepad re-implement

--- Content.Server/Cargo/Components/CargoShuttleConsoleComponent.cs
+++ Content.Server/Cargo/Components/CargoShuttleConsoleComponent.cs
@@ -1,9 +1,4 @@
-using Robust.Shared.Map;
-
 namespace Content.Server.Cargo.Components;
 
 [RegisterComponent]
-public sealed class CargoShuttleConsoleComponent : Component
-{
-
-}
+public sealed class CargoShuttleConsoleComponent : Component {}
