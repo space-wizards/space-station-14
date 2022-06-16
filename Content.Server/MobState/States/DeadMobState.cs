69fc37e552a6b39ffb4cb4b2bc14da8aa8b1a6f3
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-06-16 22:33:08 +0800

    migrate my code to this thingy

--- Content.Server/MobState/States/DeadMobState.cs
+++ Content.Server/MobState/States/DeadMobState.cs
@@ -1,16 +1,26 @@
 using Content.Shared.Alert;
 using Content.Shared.MobState.State;
 using Content.Shared.StatusEffect;
+using Content.Shared.Popups;
+using Robust.Shared.Player;
 
 namespace Content.Server.MobState.States
 {
     public sealed class DeadMobState : SharedDeadMobState
     {
+        [Dependency] private readonly SharedPopupSystem _popup = default!;
+
         public override void EnterState(EntityUid uid, IEntityManager entityManager)
         {
             base.EnterState(uid, entityManager);
 
             EntitySystem.Get<AlertsSystem>().ShowAlert(uid, AlertType.HumanDead);
+            var popup = entityManager.EntitySysManager.GetEntitySystem<SharedPopupSystem>();
+
+            // Create the popup (Disabled for now due to popups not being obscured by LOS)
+
+            //popup.PopupEntity(Loc.GetString("chat-manager-entity-death-message",
+            //    ("entityName", uid)), uid, Filter.Pvs(uid, entityManager: entityManager));
 
             if (entityManager.TryGetComponent(uid, out StatusEffectsComponent? stun))
             {
