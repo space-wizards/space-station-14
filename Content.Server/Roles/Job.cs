33a0900cf071f1771935ee66936b7a4a2dc88f3e
Arthur <14136326+Morb0@users.noreply.github.com>
2022-06-20 09:08:02 +0300

    Localize job supervisors

--- Content.Server/Roles/Job.cs
+++ Content.Server/Roles/Job.cs
@@ -39,7 +39,7 @@ namespace Content.Server.Roles
                 if(Prototype.RequireAdminNotify)
                     chatMgr.DispatchServerMessage(session, Loc.GetString("job-greet-important-disconnect-admin-notify"));
 
-                chatMgr.DispatchServerMessage(session, Loc.GetString("job-greet-supervisors-warning", ("jobName", Name), ("supervisors", Prototype.Supervisors)));
+                chatMgr.DispatchServerMessage(session, Loc.GetString("job-greet-supervisors-warning", ("jobName", Name), ("supervisors", Loc.GetString(Prototype.Supervisors))));
 
                 if(Prototype.JoinNotifyCrew && Mind.CharacterName != null)
                 {
