86669eadcc7f8de2c35879a63cb427b0d2932bc2
jessicamaybe <jessica@jessicamaybe.com>
2022-06-21 15:52:32 -0700

    cleanup

--- Content.Client/Kudzu/KudzuVisualizerSystem.cs
+++ Content.Client/Kudzu/KudzuVisualizerSystem.cs
@@ -1,8 +1,5 @@
 using Content.Shared.Kudzu;
 using Robust.Client.GameObjects;
-using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
-using Robust.Shared.Serialization.Manager.Attributes;
 
 namespace Content.Client.Kudzu
 {
