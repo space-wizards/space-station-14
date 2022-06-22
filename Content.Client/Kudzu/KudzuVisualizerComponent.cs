7c84a5e51a1f75711c445001912a21a106f11d62
jessicamaybe <jessica@jessicamaybe.com>
2022-06-22 14:12:49 -0700

    Suggested changes

--- Content.Client/Kudzu/KudzuVisualizerComponent.cs
+++ Content.Client/Kudzu/KudzuVisualizerComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Client.Kudzu
 {
     [RegisterComponent]
-    public sealed class KudzuVisualizerComponent : Component
+    public sealed class KudzuVisualsComponent : Component
     {
         [DataField("layer")]
         public int Layer { get; } = 0;
--- Content.Client/Kudzu/KudzuVisualizerSystem.cs
+++ Content.Client/Kudzu/KudzuVisualizerSystem.cs
@@ -4,20 +4,18 @@ using Robust.Client.GameObjects;
 namespace Content.Client.Kudzu
 {
 
-    public sealed class KudzuVisualsSystem : VisualizerSystem<KudzuVisualizerComponent>
+    public sealed class KudzuVisualsSystem : VisualizerSystem<KudzuVisualsComponent>
     {
-        protected override void OnAppearanceChange(EntityUid uid, KudzuVisualizerComponent component, ref AppearanceChangeEvent args)
+        protected override void OnAppearanceChange(EntityUid uid, KudzuVisualsComponent component, ref AppearanceChangeEvent args)
         {
 
-            if (!TryComp(uid, out SpriteComponent? sprite))
-            {
+            if (args.Sprite == null)
                 return;
-            }
             if (args.Component.TryGetData(KudzuVisuals.Variant, out int var)
                 && args.Component.TryGetData(KudzuVisuals.GrowthLevel, out int level))
             {
-                sprite.LayerMapReserveBlank(component.Layer);
-                sprite.LayerSetState(0, $"kudzu_{level}{var}");
+                var index = args.Sprite.LayerMapReserveBlank(component.Layer);
+                args.Sprite.LayerSetState(index, $"kudzu_{level}{var}");
             }
         }
     }
