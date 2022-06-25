909f16237c40eab394284cc2de2b9cd106788aa0
keronshb <keronshb@live.com>
2022-06-25 19:35:24 -0400

    Thieving component and gloves

---
+++ Content.Shared/Strip/Components/ThievingComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Shared.Strip.Components;
+
+[RegisterComponent]
+public sealed class ThievingComponent : Component
+{
+    [ViewVariables]
+    [DataField("stealTime")]
+    public float StealTime = 0.5f;
+}
