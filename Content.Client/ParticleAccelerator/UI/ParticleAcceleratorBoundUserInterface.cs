f7c63ae5c15c6ea0a4c9da15488406e0a5f931eb
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-06-29 19:15:45 +1000

    Make PA 40% less jank
    I just blocked the buttons until the state comes in as I was too lazy to fix spam clicking properly. The main issue is the engine problem + the fact that the console can't reach level 4 anyway.

--- Content.Client/ParticleAccelerator/UI/ParticleAcceleratorBoundUserInterface.cs
+++ Content.Client/ParticleAccelerator/UI/ParticleAcceleratorBoundUserInterface.cs
@@ -45,7 +45,8 @@ namespace Content.Client.ParticleAccelerator.UI
         {
             base.Dispose(disposing);
 
-            _menu?.Close();
+            _menu?.Dispose();
+            _menu = null;
         }
     }
 }
