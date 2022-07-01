cb903bf1a15b2279be29290a21e6cf672f55176a
EmoGarbage404 <retron404@gmail.com>
2022-06-30 21:54:01 -0400

    cursed is still cursed

--- Content.Server/Morgue/Components/MorgueComponent.cs
+++ Content.Server/Morgue/Components/MorgueComponent.cs
@@ -10,14 +10,23 @@ namespace Content.Server.Morgue.Components;
 [RegisterComponent]
 public sealed class MorgueComponent : Component
 {
+    /// <summary>
+    ///     Whether or not the morgue slab itself is open
+    /// </summary>
     [ViewVariables]
     public bool Open = false;
 
     public readonly CollisionGroup TrayCanOpenMask = CollisionGroup.Impassable | CollisionGroup.MidImpassable;
 
+    /// <summary>
+    ///     The tray entity that holds the contents
+    /// </summary>
     [ViewVariables]
     public EntityUid Tray;
 
+    /// <summary>
+    ///     The container for the tray. evil.
+    /// </summary>
     [ViewVariables]
     public ContainerSlot TrayContainer = new();
 
@@ -25,6 +34,9 @@ public sealed class MorgueComponent : Component
     [DataField("trayPrototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
     public string TrayPrototypeId = string.Empty;
 
+    /// <summary>
+    ///     Whether or not the morgue beeps if a living player is inside.
+    /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("doSoulBeep")]
     public bool DoSoulBeep = true;
@@ -32,6 +44,9 @@ public sealed class MorgueComponent : Component
     [ViewVariables]
     public float AccumulatedFrameTime = 0f;
 
+    /// <summary>
+    ///     The amount of time between each beep.
+    /// </summary>
     [ViewVariables]
     public float BeepTime = 10f;
 
@@ -39,12 +54,22 @@ public sealed class MorgueComponent : Component
     public SoundSpecifier OccupantHasSoulAlarmSound = new SoundPathSpecifier("/Audio/Weapons/Guns/EmptyAlarm/smg_empty_alarm.ogg");
 
     //Crematorium specific stuff
+
+    /// <summary>
+    ///     Whether or not this is a crematorium. self explanatory
+    /// </summary>
     [DataField("isCrematorium")]
     public bool IsCrematorium = false;
 
+    /// <summary>
+    ///     Whether or not the crematorium is currently cooking
+    /// </summary>
     [ViewVariables]
     public bool Cooking;
 
+    /// <summary>
+    ///     The time it takes to cook
+    /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     public int BurnMilis = 5000;
 
---
+++ Content.Server/Storage/EntitySystems/CursedEntityStorageSystem.cs
@@ -0,0 +1,53 @@
+using Content.Server.Storage.Components;
+using Content.Shared.Audio;
+using Content.Shared.Interaction;
+using Robust.Shared.Audio;
+using Robust.Shared.Player;
+using Robust.Shared.Random;
+using System.Linq;
+
+namespace Content.Server.Storage.EntitySystems
+{
+    public sealed class CursedEntityStorageSystem : EntitySystem
+    {
+        [Dependency] private readonly IRobustRandom _random = default!;
+        [Dependency] private readonly EntityStorageSystem _entityStorage = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<CursedEntityStorageComponent, ActivateInWorldEvent>(OnActivate,
+                after: new[] { typeof(EntityStorageSystem) });
+        }
+
+        private void OnActivate(EntityUid uid, CursedEntityStorageComponent component, ActivateInWorldEvent args)
+        {
+            if (!TryComp<EntityStorageComponent>(uid, out var storage))
+                return;
+
+            if (storage.Open || storage.Contents.ContainedEntities.Count <= 0)
+                return;
+
+            var lockers = EntityQuery<EntityStorageComponent>().Select(c => c.Owner).ToList();
+
+            if (lockers.Contains(uid))
+                lockers.Remove(uid);
+
+            if (lockers.Count == 0)
+                return;
+
+            var lockerEnt = _random.Pick(lockers);
+
+            var locker = EntityManager.GetComponent<EntityStorageComponent>(lockerEnt);
+
+            foreach (var entity in storage.Contents.ContainedEntities.ToArray())
+            {
+                storage.Contents.ForceRemove(entity);
+                _entityStorage.Insert(entity, lockerEnt, locker);
+            }
+
+            SoundSystem.Play(component.CursedSound.GetSound(), Filter.Pvs(uid), uid, AudioHelpers.WithVariation(0.125f, _random));
+        }
+    }
+}
