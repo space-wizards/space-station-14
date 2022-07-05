2dbabff1aa8734c29f678f101a4fa2e3d0de0190
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-07-05 15:28:17 +0800

    some minor refactoring

--- Content.Server/CommandReport/CommandReportSystem.cs
+++ Content.Server/CommandReport/CommandReportSystem.cs
@@ -29,29 +29,29 @@ namespace Content.Server.CommandReport
         {
             // Such copypaste code... So sorry...
             var wasSent = false;
-            var chat = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<ChatSystem>();
             var messageWrap = Loc.GetString("command-reports-sender-announcement-wrap-message");
 
             SoundSystem.Play(AnnouncementSound, Filter.Broadcast(), AudioParams.Default.WithVolume(-2f));  // Play the "New Command Report Created" sound effect
 
-
             _chatManager.ChatMessageToAll(ChatChannel.Radio, broadcastToRadio == true ? message : Loc.GetString("command-reports-confidential-announcement-message"), messageWrap, colorOverride: Color.Gold);
 
             // some copypaste code from NukeCodeSystem.cs, to the person making fax, don't forget to convert this too!
-            var consoles = EntityManager.EntityQuery<CommunicationsConsoleComponent>();
+            var consoles = EntityManager.EntityQuery<CommunicationsConsoleComponent>(); // get all entities with the CommunicationConsoleComponent
             foreach (var console in consoles)
             {
                 if (!EntityManager.TryGetComponent((console).Owner, out TransformComponent? transform))
-                    continue;
+                    continue; // if no transform component, then ignore and continue
+
                 var consolePos = transform.MapPosition;
-                var paper = EntityManager.SpawnEntity("Paper", consolePos);
-                var papercomp = EntityManager.GetComponent<PaperComponent>(paper);
-                var papermeta = EntityManager.GetComponent<MetaDataComponent>(paper);
-                papercomp.Content = $"Central Command Report\n{message}";
-                papercomp.Mode = SharedPaperComponent.PaperAction.Read;
-                papermeta.EntityName = Loc.GetString("command-reports-paper-name");
-                papermeta.EntityDescription = Loc.GetString("command-reports-paper-description");
-                wasSent = true;
+                var paper = EntityManager.SpawnEntity("Paper", consolePos); // the paper that will be sent to the console
+
+                var papercomp = EntityManager.GetComponent<PaperComponent>(paper); // the PaperComponent of the paper
+                var papermeta = EntityManager.GetComponent<MetaDataComponent>(paper); // the metadata of the paper
+
+                papercomp.Content = $"Central Command Report\n{message}"; // header + contents
+                papermeta.EntityName = Loc.GetString("command-reports-paper-name"); // set the name of the paper to something special
+
+                wasSent = true; // fax has been sent at least once
             }
 
             _adminLogger.Add(LogType.Chat, LogImpact.Low, $"Command report sent: {message}");
