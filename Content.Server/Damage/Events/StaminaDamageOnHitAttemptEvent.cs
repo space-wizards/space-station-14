40eecdd78ac3b76322d8d95a8f4a7ef81b6fb98d
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2022-07-06 18:06:12 +1000

    Stamina damage (#9230)

---
+++ Content.Server/Damage/Events/StaminaDamageOnHitAttemptEvent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server.Damage.Events;
+
+/// <summary>
+/// Attempting to apply stamina damage on a melee hit to an entity.
+/// </summary>
+[ByRefEvent]
+public struct StaminaDamageOnHitAttemptEvent
+{
+    public bool Cancelled;
+}
