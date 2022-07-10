07689309350ca174c43166cb3a2c5c2b30fdcfe5
Kara <lunarautomaton6@gmail.com>
2022-07-10 01:32:43 -0700

    `ZombieOutbreak`

--- Content.Server/StationEvents/Events/ZombieOutbreak.cs
+++ Content.Server/StationEvents/Events/ZombieOutbreak.cs
@@ -1,10 +1,4 @@
-using Content.Server.Chat;
-using Robust.Shared.Random;
-using Content.Server.Chat.Managers;
-using Content.Server.Chat.Systems;
-using Content.Server.Station.Systems;
 using Content.Shared.MobState.Components;
-using Content.Shared.Sound;
 using Content.Server.Zombies;
 
 namespace Content.Server.StationEvents.Events
@@ -14,25 +8,17 @@ namespace Content.Server.StationEvents.Events
     /// </summary>
     public sealed class ZombieOutbreak : StationEventSystem
     {
-        [Dependency] private readonly IRobustRandom RobustRandom = default!;
-        [Dependency] private readonly IEntityManager EntityManager = default!;
+        [Dependency] private readonly ZombifyOnDeathSystem _zombify = default!;
 
         public override string Prototype => "ZombieOutbreak";
-        public override int EarliestStart => 50;
-        public override float Weight => WeightLow / 2;
-        public override SoundSpecifier? StartAudio => new SoundPathSpecifier("/Audio/Announcements/bloblarm.ogg");
-        protected override float EndAfter => 1.0f;
-        public override int? MaxOccurrences => 1;
-        public override bool AnnounceEvent => false;
 
         /// <summary>
-        /// Finds 1-3 random, dead entities accross the station
+        /// Finds 1-3 random, dead entities across the station
         /// and turns them into zombies.
         /// </summary>
         public override void Started()
         {
             base.Started();
-            HashSet<EntityUid> stationsToNotify = new();
             List<MobStateComponent> deadList = new();
             foreach (var mobState in EntityManager.EntityQuery<MobStateComponent>())
             {
@@ -43,31 +29,12 @@ namespace Content.Server.StationEvents.Events
 
             var toInfect = RobustRandom.Next(1, 3);
 
-            var zombifysys = EntityManager.EntitySysManager.GetEntitySystem<ZombifyOnDeathSystem>();
-
-            // Now we give it to people in the list of dead entities earlier.
-            var entSysMgr = IoCManager.Resolve<IEntitySystemManager>();
-            var stationSystem = entSysMgr.GetEntitySystem<StationSystem>();
-            var chatSystem = entSysMgr.GetEntitySystem<ChatSystem>();
-
             foreach (var target in deadList)
             {
                 if (toInfect-- == 0)
                     break;
 
-                zombifysys.ZombifyEntity(target.Owner);
-
-                var station = stationSystem.GetOwningStation(target.Owner);
-                if(station == null) continue;
-                stationsToNotify.Add((EntityUid) station);
-            }
-
-            if (!AnnounceEvent)
-                return;
-            foreach (var station in stationsToNotify)
-            {
-                chatSystem.DispatchStationAnnouncement((EntityUid) station, Loc.GetString("station-event-zombie-outbreak-announcement"),
-                    playDefaultSound: false, colorOverride: Color.DarkMagenta);
+                _zombify.ZombifyEntity(target.Owner);
             }
         }
     }
