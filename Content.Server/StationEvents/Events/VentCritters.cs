fe8489d1cbf9421142b51d30d0c1b30b4ef622b1
Kara <lunarautomaton6@gmail.com>
2022-07-10 01:31:08 -0700

    `VentCritters`

--- Content.Server/StationEvents/Events/VentCritters.cs
+++ Content.Server/StationEvents/Events/VentCritters.cs
@@ -7,32 +7,10 @@ namespace Content.Server.StationEvents.Events;
 
 public sealed class VentCritters : StationEventSystem
 {
-    [Dependency] private readonly IRobustRandom RobustRandom = default!;
-    [Dependency] private readonly IEntityManager EntityManager = default!;
-
     public static List<string> SpawnedPrototypeChoices = new List<string>()
         {"MobGiantSpiderAngry", "MobMouse", "MobMouse1", "MobMouse2"};
 
     public override string Prototype => "VentCritters";
-
-    public override string? StartAnnouncement =>
-        Loc.GetString("station-event-vent-spiders-start-announcement", ("data", Loc.GetString(Loc.GetString($"random-sentience-event-data-{RobustRandom.Next(1, 6)}"))));
-
-    public override SoundSpecifier? StartAudio => new SoundPathSpecifier("/Audio/Announcements/aliens.ogg");
-
-    public override int EarliestStart => 15;
-
-    public override int MinimumPlayers => 15;
-
-    public override float Weight => WeightLow;
-
-    public override int? MaxOccurrences => 2;
-
-    protected override float StartAfter => 30f;
-
-    protected override float EndAfter => 60;
-
-    public override bool AnnounceEvent => false;
 
     public override void Started()
     {
