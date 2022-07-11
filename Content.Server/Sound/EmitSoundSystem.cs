c4e40f0fa8f623f40ace1118e3add568424724e7
ElectroJr <leonsfriedrich@gmail.com>
2022-07-11 18:23:31 -0400

    Update EmitSoundSystem

--- Content.Server/Sound/EmitSoundSystem.cs
+++ Content.Server/Sound/EmitSoundSystem.cs
@@ -8,10 +8,7 @@ using Content.Shared.Interaction.Events;
 using Content.Shared.Maps;
 using Content.Shared.Throwing;
 using JetBrains.Annotations;
-using Robust.Shared.Audio;
 using Robust.Shared.Map;
-using Robust.Shared.Player;
-using Robust.Shared.Random;
 
 namespace Content.Server.Sound
 {
@@ -22,8 +19,8 @@ namespace Content.Server.Sound
     public sealed class EmitSoundSystem : EntitySystem
     {
         [Dependency] private readonly IMapManager _mapManager = default!;
-        [Dependency] private readonly IRobustRandom _random = default!;
         [Dependency] private readonly ITileDefinitionManager _tileDefMan = default!;
+        [Dependency] private readonly SharedAudioSystem _autioSys = default!;
 
         /// <inheritdoc />
         public override void Initialize()
@@ -85,8 +82,7 @@ namespace Content.Server.Sound
 
         private void TryEmitSound(BaseEmitSoundComponent component)
         {
-            var audioParams = component.AudioParams.WithPitchScale((float) _random.NextGaussian(1, component.PitchVariation));
-            SoundSystem.Play(component.Sound.GetSound(), Filter.Pvs(component.Owner, entityManager: EntityManager), component.Owner, audioParams);
+            _autioSys.PlayPvs(component.Sound, component.Owner, component.Sound.Params.AddVolume(-2f));
         }
     }
 }
