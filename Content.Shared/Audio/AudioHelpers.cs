9f2309920a6f5b982806600abea95c32d21341e1
ElectroJr <leonsfriedrich@gmail.com>
2022-07-11 16:53:05 -0400

    Move specifier to robust

--- Content.Shared/Audio/AudioHelpers.cs
+++ Content.Shared/Audio/AudioHelpers.cs
@@ -8,7 +8,7 @@ namespace Content.Shared.Audio
         /// <summary>
         ///     Returns a random pitch.
         /// </summary>
-        [Obsolete("Use variant that takes in IRobustRandom instead.")]
+        [Obsolete("Use variation datafield.")]
         public static AudioParams WithVariation(float amplitude)
         {
             return WithVariation(amplitude, null);
@@ -58,13 +58,6 @@ namespace Content.Shared.Audio
             IoCManager.Resolve(ref rand);
             variation = Math.Clamp(variation, 0, 12);
             return ShiftSemitone(rand.Next(-variation, variation));
-        }
-
-        public static string GetRandomFileFromSoundCollection(string name, IRobustRandom? rand, IPrototypeManager? proto)
-        {
-            IoCManager.Resolve(ref rand, ref proto);
-            var soundCollection = proto.Index<SoundCollectionPrototype>(name);
-            return rand.Pick(soundCollection.PickFiles).ToString();
         }
     }
 }
