6bc83b0d48c167fe30437fa94272a00ed1633c5d
Themias <themias5@gmail.com>
2022-07-11 23:42:58 -0400

    Update to use time instead of index

--- Content.Server/Disease/DiseaseSystem.cs
+++ Content.Server/Disease/DiseaseSystem.cs
@@ -116,7 +116,7 @@ namespace Content.Server.Disease
                             disease.Stages[j] > lastThreshold)
                         {
                             lastThreshold = disease.Stages[j];
-                            stage = j;
+                            stage = (int) lastThreshold;
                         }
                     }
 
