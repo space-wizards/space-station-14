728baf4d3b66f7e7f5671589938dbad72f86e7bb
jessicamaybe <jessica@jessicamaybe.com>
2022-07-12 15:04:10 -0700

    Add current round time to game lobby

--- Content.Server/GameTicking/GameTicker.Lobby.cs
+++ Content.Server/GameTicking/GameTicker.Lobby.cs
@@ -45,7 +45,8 @@ namespace Content.Server.GameTicking
             var mapName = map?.MapName ?? Loc.GetString("game-ticker-no-map-selected");
             var gmTitle = Loc.GetString(Preset.ModeTitle);
             var desc = Loc.GetString(Preset.Description);
-            return Loc.GetString("game-ticker-get-info-text",("roundId", RoundId), ("playerCount", playerCount),("mapName", mapName),("gmTitle", gmTitle),("desc", desc));
+            var roundTime = _gameTiming.RealTime.ToString("hh\\:mm");
+            return Loc.GetString("game-ticker-get-info-text",("roundId", RoundId), ("playerCount", playerCount),("mapName", mapName), ("roundTime", roundTime), ("gmTitle", gmTitle),("desc", desc));
         }
 
         private TickerLobbyReadyEvent GetStatusSingle(ICommonSession player, LobbyPlayerStatus status)
