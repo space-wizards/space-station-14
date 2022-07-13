0ffe16cb402df57d5b38ccc4a0e21ac4a5d0b13b
keronshb <keronshb@live.com>
2022-07-13 16:21:50 -0400

    Fixes Singulo Crash, cleans up client gen component

--- Content.Client/Singularity/Components/ContainmentFieldGeneratorComponent.cs
+++ Content.Client/Singularity/Components/ContainmentFieldGeneratorComponent.cs
@@ -1,11 +1,6 @@
 using Content.Shared.Singularity.Components;
 
-namespace Content.Client.Singularity.Components
-{
-    [RegisterComponent]
-    [ComponentReference(typeof(SharedContainmentFieldGeneratorComponent))]
-    public sealed class ContainmentFieldGeneratorComponent : SharedContainmentFieldGeneratorComponent
-    {
-
-    }
-}
+namespace Content.Client.Singularity.Components;
+[RegisterComponent]
+[ComponentReference(typeof(SharedContainmentFieldGeneratorComponent))]
+public sealed class ContainmentFieldGeneratorComponent : SharedContainmentFieldGeneratorComponent { }
