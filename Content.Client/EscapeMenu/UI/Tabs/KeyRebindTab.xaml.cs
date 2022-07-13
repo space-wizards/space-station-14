6de1a6eced67bcd193720751de68fad77d0046a8
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-07-13 16:34:45 +1000

    Keybinds

--- Content.Client/EscapeMenu/UI/Tabs/KeyRebindTab.xaml.cs
+++ Content.Client/EscapeMenu/UI/Tabs/KeyRebindTab.xaml.cs
@@ -172,6 +172,15 @@ namespace Content.Client.EscapeMenu.UI.Tabs
             AddButton(ContentKeyFunctions.Loadout8);
             AddButton(ContentKeyFunctions.Loadout9);
 
+            AddHeader("ui-options-header-shuttle");
+            AddButton(ContentKeyFunctions.ShuttleStrafeUp);
+            AddButton(ContentKeyFunctions.ShuttleStrafeRight);
+            AddButton(ContentKeyFunctions.ShuttleStrafeLeft);
+            AddButton(ContentKeyFunctions.ShuttleStrafeDown);
+            AddButton(ContentKeyFunctions.ShuttleRotateLeft);
+            AddButton(ContentKeyFunctions.ShuttleRotateRight);
+            AddButton(ContentKeyFunctions.ShuttleBrake);
+
             AddHeader("ui-options-header-map-editor");
             AddButton(EngineKeyFunctions.EditorPlaceObject);
             AddButton(EngineKeyFunctions.EditorCancelPlace);
