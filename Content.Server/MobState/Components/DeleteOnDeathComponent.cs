7da301853949d2316bf063deb4b2177370fca8aa
Rane <elijahrane@gmail.com>
2022-07-13 23:13:07 -0400

    Port honkbots from nyano

---
+++ Content.Server/MobState/Components/DeleteOnDeathComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Server.MobState.Components
+{
+    /// <summary>
+    /// For shrimple mobs that do not leave a corpse
+    /// </summary>
+    [RegisterComponent]
+    public sealed class DeleteOnDeathComponent : Component
+    {}
+}
