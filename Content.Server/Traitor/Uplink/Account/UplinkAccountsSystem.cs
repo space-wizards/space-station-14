7fbf431ff0aa7c9a031a299fe721610f02b6fbdf
EmoGarbage404 <retron404@gmail.com>
2022-07-15 13:09:00 -0400

    quickfix nukie uplinks

--- Content.Server/Traitor/Uplink/Account/UplinkAccountsSystem.cs
+++ Content.Server/Traitor/Uplink/Account/UplinkAccountsSystem.cs
@@ -1,10 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Content.Server.Traitor.Uplink.Components;
 using Content.Shared.Stacks;
 using Content.Shared.Traitor.Uplink;
 using Robust.Shared.Map;
-using Robust.Shared.Prototypes;
 
 namespace Content.Server.Traitor.Uplink.Account
 {
@@ -70,28 +68,18 @@ namespace Content.Server.Traitor.Uplink.Account
 
         }
 
-        public bool TryPurchaseItem(UplinkComponent component, string itemId, EntityCoordinates spawnCoords, [NotNullWhen(true)] out EntityUid? purchasedItem)
+        public bool TryPurchaseItem(UplinkAccount acc, string itemId, EntityCoordinates spawnCoords, [NotNullWhen(true)] out EntityUid? purchasedItem)
         {
-            var acc = component.UplinkAccount;
             purchasedItem = null;
 
-            if (acc == null) return false;
-            if (acc.AccountHolder == null) return false;
-
             if (!_listingSystem.TryGetListing(itemId, out var listing))
-            {
                 return false;
-            }
 
             if (acc.Balance < listing.Price)
-            {
                 return false;
-            }
 
             if (!RemoveFromBalance(acc, listing.Price))
-            {
                 return false;
-            }
 
             purchasedItem = EntityManager.SpawnEntity(listing.ItemId, spawnCoords);
             return true;
