d070350c4c22d85384a6a0e7fbe82952059c9612
Rane <elijahrane@gmail.com>
2022-07-15 03:44:17 -0400

    mirror review

--- Content.Server/MobState/Components/DeleteOnDeathComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Server.MobState.Components
-{
-    /// <summary>
-    /// For shrimple mobs that do not leave a corpse
-    /// </summary>
-    [RegisterComponent]
-    public sealed class DeleteOnDeathComponent : Component
-    {}
-}
