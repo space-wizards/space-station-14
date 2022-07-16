f6282222509fd5d9f2ad2f456f675d4e7a9253f1
keronshb <keronshb@live.com>
2022-07-16 13:08:45 -0400

    Cleanup and refactor part ???

--- Content.Server/Singularity/EntitySystems/ContainmentFieldGeneratorSystem.cs
+++ Content.Server/Singularity/EntitySystems/ContainmentFieldGeneratorSystem.cs
@@ -127,7 +127,7 @@ public sealed class ContainmentFieldGeneratorSystem : EntitySystem
         }
     }
 
-    //TODO: Get rid of this
+    //TODO: Rework this to player bouncing
     private void HandleFieldCollide(EntityUid uid, ContainmentFieldComponent component, StartCollideEvent args)
     {
 
@@ -146,7 +146,7 @@ public sealed class ContainmentFieldGeneratorSystem : EntitySystem
                 if (component.Connections.ContainsKey(dir))
                     continue; // This direction already has an active connection
 
-                TryFieldConnection(dir, component);
+                TryGenerateFieldConnection(dir, component);
             }
         }
     }
@@ -155,7 +155,7 @@ public sealed class ContainmentFieldGeneratorSystem : EntitySystem
     {
         if (EntityManager.TryGetComponent<PointLightComponent>(component.Owner, out var pointLightComponent))
         {
-            bool hasAnyConnection = component.Connection1 != null || component.Connection2 != null;
+            bool hasAnyConnection = component.Connections != null;
             pointLightComponent.Enabled = hasAnyConnection;
         }
     }
@@ -181,7 +181,7 @@ public sealed class ContainmentFieldGeneratorSystem : EntitySystem
         }
     }
 
-    private bool TryFieldConnection(Direction dir, ContainmentFieldGeneratorComponent component)
+    private bool TryGenerateFieldConnection(Direction dir, ContainmentFieldGeneratorComponent component)
     {
         if (!component.Enabled) return false;
 
@@ -210,108 +210,30 @@ public sealed class ContainmentFieldGeneratorSystem : EntitySystem
 
         var ent = closestResult.Value.HitEntity;
 
-        if (!TryComp<ContainmentFieldGeneratorComponent?>(ent, out var otherFieldGeneratorComponent))
+        if (!TryComp<ContainmentFieldGeneratorComponent?>(ent, out var otherFieldGeneratorComponent) ||
+            otherFieldGeneratorComponent == component ||
+            !TryComp<PhysicsComponent>(ent, out var collidableComponent) ||
+            collidableComponent.BodyType != BodyType.Static)
         {
             return false;
         }
 
-        var fields = GenCon(component, otherFieldGeneratorComponent);
+        var fields = GenerateFieldConnection(component, otherFieldGeneratorComponent);
 
         component.Connections[dir] = (otherFieldGeneratorComponent, fields);
         component.Connections[dir.GetOpposite()] = (component, fields);
 
+        if (!component.IsConnected)
+            component.IsConnected = true;
+
+        if (!otherFieldGeneratorComponent.IsConnected)
+            otherFieldGeneratorComponent.IsConnected = true;
+
+        UpdateConnectionLights(component);
         return true;
     }
 
-    private bool TryGenerateFieldConnection([NotNullWhen(true)] ref Tuple<Angle, List<EntityUid>>? propertyFieldTuple,ContainmentFieldGeneratorComponent component)
-    {
-        if (propertyFieldTuple != null) return false;
-        if (!component.Enabled) return false; //don't gen a field unless it's on
-
-        var gen1XForm = Transform(component.Owner);
-        if (!gen1XForm.Anchored) return false;
-        var gen1CardinalDirAngle = gen1XForm.WorldRotation;
-        var rad1 = Direction.South.ToAngle() + gen1CardinalDirAngle; //This fires east
-        var rad2 = Direction.West.ToAngle() + gen1CardinalDirAngle; //This fires south
-        var rad3 = Direction.North.ToAngle() + gen1CardinalDirAngle; //This fires west
-        var rad4 = Direction.East.ToAngle() + gen1CardinalDirAngle; //This fires north
-
-        //TODO: Finding the generators seems fine, need to prevent it from going through a wall, however.
-        //TODO: There is still an issue with x2 fields spawning on the generator that has null generators, but has connections
-        //Though I just double checked with a 1x1 and there were no field gens
-        //TODO: Also extra issue with the last node (D) despawning the connection between A-B.
-        //TODO: Maybe change from the spawn each to the singular spawn with texture wrapper.
-        foreach (var direction in new[] { rad1, rad2, rad3, rad4 })
-        {
-            if (component.Connection1?.Item1 == direction || component.Connection2?.Item1 == direction) continue;
-
-            var ray = new CollisionRay(gen1XForm.MapPosition.Position, direction.ToVec(), component.CollisionMask);
-            var rayCastResults = _physics.IntersectRay(gen1XForm.MapID, ray, component.MaxLength, component.Owner, false).ToList();
-
-            if (!rayCastResults.Any()) continue;
-
-            RayCastResults? closestResult = null;
-            foreach (var res in rayCastResults)
-            {
-                if (HasComp<ContainmentFieldGeneratorComponent>(res.HitEntity))
-                {
-                    closestResult = res;
-                }
-                break;
-            }
-            if (closestResult == null) continue;
-
-            var ent = closestResult.Value.HitEntity;
-
-            //TODO: Something could be wrong with this
-            if (!TryComp<ContainmentFieldGeneratorComponent?>(ent, out var fieldGeneratorComponent) ||
-                fieldGeneratorComponent.Owner == component.Owner ||
-                !HasFreeConnections(fieldGeneratorComponent) ||
-                IsConnectedWith(component, fieldGeneratorComponent) ||
-                !TryComp<PhysicsComponent?>(ent, out var collidableComponent) ||
-                collidableComponent.BodyType != BodyType.Static)
-            {
-                continue;
-            }
-
-            //rework this I don't think it will work out
-            //TODO: One gen always appears with no connections
-            component.Generator1 = component.Owner;
-            if (component.Generator2 == fieldGeneratorComponent.Owner) //new addition, come back to this
-                continue;
-
-            component.Generator2 = fieldGeneratorComponent.Owner;
-
-            //The spawning works fine
-            GenerateConnection(component);
-            //TODO: Really need to reapproach the tuple approach
-            propertyFieldTuple = new Tuple<Angle, List<EntityUid>>(direction, component.Fields);
-            if (fieldGeneratorComponent.Connection1 == null)
-            {
-                fieldGeneratorComponent.Connection1 = new Tuple<Angle, List<EntityUid>>(direction.Opposite(), component.Fields);
-                if (!fieldGeneratorComponent.IsConnected)
-                    fieldGeneratorComponent.IsConnected = true;
-            }
-            else if (fieldGeneratorComponent.Connection2 == null)
-            {
-                fieldGeneratorComponent.Connection2 = new Tuple<Angle, List<EntityUid>>(direction.Opposite(), component.Fields);
-                if (!fieldGeneratorComponent.IsConnected)
-                    fieldGeneratorComponent.IsConnected = true;
-            }
-            else
-            {
-                Logger.Error("When trying to connect two Containment Field Generators, the second one already had two connection but the check didn't catch it");
-            }
-
-            component.IsConnected = true;
-            UpdateConnectionLights(component);
-            return true;
-        }
-
-        return false;
-    }
-
-    private List<EntityUid> GenCon(ContainmentFieldGeneratorComponent firstGenComp, ContainmentFieldGeneratorComponent secondGenComp)
+    private List<EntityUid> GenerateFieldConnection(ContainmentFieldGeneratorComponent firstGenComp, ContainmentFieldGeneratorComponent secondGenComp)
     {
         var fieldList = new List<EntityUid>();
         var gen1Coords = Transform(firstGenComp.Owner).Coordinates;
@@ -341,42 +263,6 @@ public sealed class ContainmentFieldGeneratorSystem : EntitySystem
             currentOffset += dirVec;
         }
         return fieldList;
-    }
-
-    private void GenerateConnection(ContainmentFieldGeneratorComponent component)
-    {
-        if (component.Generator1 == null || component.Generator2 == null)
-            return;
-
-        var gen1Coords = Transform(component.Generator1.Value).Coordinates;
-        var gen2Coords = Transform(component.Generator2.Value).Coordinates;
-
-        if (gen1Coords == gen2Coords)
-            return;
-
-        var delta = (gen2Coords - gen1Coords).Position;
-        var dirVec = delta.Normalized;
-        var stopDist = delta.Length;
-        var currentOffset = dirVec;
-        while (currentOffset.Length < stopDist)
-        {
-            var currentCoords = gen1Coords.Offset(currentOffset);
-            var newField = Spawn(component.CreatedField, currentCoords);
-
-            var fieldXForm = Transform(newField);
-            fieldXForm.AttachParent(component.Generator1.Value);
-            if (dirVec.GetDir() == Direction.East || dirVec.GetDir() == Direction.West)
-            {
-                var angle = fieldXForm.LocalPosition.ToAngle();
-                var rotateBy90 = angle.Degrees + 90;
-                var rotatedAngle = Angle.FromDegrees(rotateBy90);
-
-                fieldXForm.LocalRotation = rotatedAngle;
-            }
-
-            component.Fields.Add(newField);
-            currentOffset += dirVec;
-        }
     }
 
     private void DeleteFields(ContainmentFieldGeneratorComponent component)
@@ -422,10 +308,5 @@ public sealed class ContainmentFieldGeneratorSystem : EntitySystem
     {
         return connectedGen == mainGen || mainGen.Generator1 == connectedGen.Owner ||
                mainGen.Generator2 == connectedGen.Owner;
-    }
-
-    public bool HasFreeConnections(ContainmentFieldGeneratorComponent component)
-    {
-        return component.Connection1 == null || component.Connection2 == null;
     }
 }
