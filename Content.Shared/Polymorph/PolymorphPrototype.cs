972f758535adf9a1f59fb046d6b5089811e7099d
EmoGarbage404 <retron404@gmail.com>
2022-07-17 14:26:52 -0400

    disallow recursive polymorphs by default

--- Content.Shared/Polymorph/PolymorphPrototype.cs
+++ Content.Shared/Polymorph/PolymorphPrototype.cs
@@ -87,6 +87,9 @@ namespace Content.Shared.Polymorph
         /// </summary>
         [DataField("revertOnDeath", serverOnly: true)]
         public bool RevertOnDeath = true;
+
+        [DataField("allowRepeatedMorphs", serverOnly: true)]
+        public bool AllowRepeatedMorphs = false;
     }
 
     public enum PolymorphInventoryChange : byte
