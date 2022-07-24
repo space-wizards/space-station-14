9e7b84c4eedb0f664a384675b1938081367dae43
Rane <elijahrane@gmail.com>
2022-07-24 18:50:11 -0400

    change to right cvar

--- Content.Server/Chat/Systems/ChatSystem.cs
+++ Content.Server/Chat/Systems/ChatSystem.cs
@@ -156,8 +156,8 @@ public sealed partial class ChatSystem : SharedChatSystem
         message = SanitizeInGameOOCMessage(message);
 
         var sendType = type;
-        // If we have OOC disabled for players during the round, redirect dead/ghost LOOC to dead chat.
-        if (!_configurationManager.GetCVar(CCVars.OocEnableDuringRound) && (HasComp<GhostComponent>(source) || _mobStateSystem.IsDead(source)))
+        // If we have OOC disabled for players, redirect dead/ghost LOOC to dead chat.
+        if (!_configurationManager.GetCVar(CCVars.OocEnabled) && (HasComp<GhostComponent>(source) || _mobStateSystem.IsDead(source)))
             sendType = InGameOOCChatType.Dead;
 
         switch (sendType)
