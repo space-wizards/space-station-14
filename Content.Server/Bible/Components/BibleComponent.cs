3b7391d47b96a1dacf8d207e8675ffaf78324897
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-07-26 01:56:37 +1000

    Nerf bible
    70 damage per 5 seconds is absurd.

--- Content.Server/Bible/Components/BibleComponent.cs
+++ Content.Server/Bible/Components/BibleComponent.cs
@@ -6,28 +6,33 @@ namespace Content.Server.Bible.Components
     [RegisterComponent]
     public sealed class BibleComponent : Component
     {
-        // Damage that will be healed on a success
+        /// <summary>
+        /// Damage that will be healed on a success
+        /// </summary>
         [DataField("damage", required: true)]
         [ViewVariables(VVAccess.ReadWrite)]
         public DamageSpecifier Damage = default!;
-        // Damage that will be dealt on a failure
+
+        /// <summary>
+        /// Damage that will be dealt on a failure
+        /// </summary>
         [DataField("damageOnFail", required: true)]
         [ViewVariables(VVAccess.ReadWrite)]
         public DamageSpecifier DamageOnFail = default!;
-        // Damage that will be dealt when a non-chaplain attempts to heal
+
+        /// <summary>
+        /// Damage that will be dealt when a non-chaplain attempts to heal
+        /// </summary>
         [DataField("damageOnUntrainedUse", required: true)]
         [ViewVariables(VVAccess.ReadWrite)]
         public DamageSpecifier DamageOnUntrainedUse = default!;
 
-        //Chance the bible will fail to heal someone with no helmet
+        /// <summary>
+        /// Chance the bible will fail to heal someone with no helmet
+        /// </summary>
         [DataField("failChance")]
         [ViewVariables(VVAccess.ReadWrite)]
         public float FailChance = 0.34f;
-
-        public TimeSpan LastAttackTime;
-        public TimeSpan CooldownEnd;
-        [DataField("cooldownTime")]
-        public float CooldownTime { get; } = 5f;
 
         [DataField("sizzleSound")]
         public SoundSpecifier SizzleSoundPath = new SoundPathSpecifier("/Audio/Effects/lightburn.ogg");
