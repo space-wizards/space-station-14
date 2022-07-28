8bd2be7ea5abcf67761c8ce25360259384c09648
Steven Karatisoglou <lkytiger@outlook.com>
2022-07-28 13:43:32 -0700

    Converted initialization of HydroTrayVisualizer under PlantHolderVisualizerSystem to it's own GenericVisualizer.

--- Content.Client/Botany/PlantHolderVisualizerSystem.cs
+++ Content.Client/Botany/PlantHolderVisualizerSystem.cs
@@ -19,39 +19,7 @@ public sealed class PlantHolderVisualizerSystem : VisualizerSystem<PlantHolderVi
             return;
 
         sprite.LayerMapReserveBlank(PlantHolderLayers.Plant);
-        sprite.LayerMapReserveBlank(PlantHolderLayers.HealthLight);
-        sprite.LayerMapReserveBlank(PlantHolderLayers.WaterLight);
-        sprite.LayerMapReserveBlank(PlantHolderLayers.NutritionLight);
-        sprite.LayerMapReserveBlank(PlantHolderLayers.AlertLight);
-        sprite.LayerMapReserveBlank(PlantHolderLayers.HarvestLight);
-
-        var hydroTools = new ResourcePath("Structures/Hydroponics/overlays.rsi");
-
-        sprite.LayerSetSprite(PlantHolderLayers.HealthLight,
-            new SpriteSpecifier.Rsi(hydroTools, "lowhealth3"));
-        sprite.LayerSetSprite(PlantHolderLayers.WaterLight,
-            new SpriteSpecifier.Rsi(hydroTools, "lowwater3"));
-        sprite.LayerSetSprite(PlantHolderLayers.NutritionLight,
-            new SpriteSpecifier.Rsi(hydroTools, "lownutri3"));
-        sprite.LayerSetSprite(PlantHolderLayers.AlertLight,
-            new SpriteSpecifier.Rsi(hydroTools, "alert3"));
-        sprite.LayerSetSprite(PlantHolderLayers.HarvestLight,
-            new SpriteSpecifier.Rsi(hydroTools, "harvest3"));
-
-        // Let's make those invisible for now.
         sprite.LayerSetVisible(PlantHolderLayers.Plant, false);
-        sprite.LayerSetVisible(PlantHolderLayers.HealthLight, false);
-        sprite.LayerSetVisible(PlantHolderLayers.WaterLight, false);
-        sprite.LayerSetVisible(PlantHolderLayers.NutritionLight, false);
-        sprite.LayerSetVisible(PlantHolderLayers.AlertLight, false);
-        sprite.LayerSetVisible(PlantHolderLayers.HarvestLight, false);
-
-        // Pretty unshaded lights!
-        sprite.LayerSetShader(PlantHolderLayers.HealthLight, "unshaded");
-        sprite.LayerSetShader(PlantHolderLayers.WaterLight, "unshaded");
-        sprite.LayerSetShader(PlantHolderLayers.NutritionLight, "unshaded");
-        sprite.LayerSetShader(PlantHolderLayers.AlertLight, "unshaded");
-        sprite.LayerSetShader(PlantHolderLayers.HarvestLight, "unshaded");
     }
 
     protected override void OnAppearanceChange(EntityUid uid, PlantHolderVisualsComponent component, ref AppearanceChangeEvent args)
@@ -71,31 +39,6 @@ public sealed class PlantHolderVisualizerSystem : VisualizerSystem<PlantHolderVi
                 args.Sprite.LayerSetRSI(PlantHolderLayers.Plant, rsi);
                 args.Sprite.LayerSetState(PlantHolderLayers.Plant, state);
             }
-        }
-
-        if (args.Component.TryGetData<bool>(PlantHolderVisuals.HealthLight, out var health))
-        {
-            args.Sprite.LayerSetVisible(PlantHolderLayers.HealthLight, health);
-        }
-
-        if (args.Component.TryGetData<bool>(PlantHolderVisuals.WaterLight, out var water))
-        {
-            args.Sprite.LayerSetVisible(PlantHolderLayers.WaterLight, water);
-        }
-
-        if (args.Component.TryGetData<bool>(PlantHolderVisuals.NutritionLight, out var nutrition))
-        {
-            args.Sprite.LayerSetVisible(PlantHolderLayers.NutritionLight, nutrition);
-        }
-
-        if (args.Component.TryGetData<bool>(PlantHolderVisuals.AlertLight, out var alert))
-        {
-            args.Sprite.LayerSetVisible(PlantHolderLayers.AlertLight, alert);
-        }
-
-        if (args.Component.TryGetData<bool>(PlantHolderVisuals.HarvestLight, out var harvest))
-        {
-            args.Sprite.LayerSetVisible(PlantHolderLayers.HarvestLight, harvest);
         }
     }
 }
