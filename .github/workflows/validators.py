8f266ed6a03e4f15981e771fe642d92faca72e18
Paul Ritter <ritter.paul1@gmail.com>
2022-07-29 11:57:20 +0200

    finished testing

--- .github/workflows/validators.py
+++
@@ -1,9 +0,0 @@
-from yamale.validators import Validator
-import validators
-
-class URL(Validator):
-    """ Custom URL validator for testing """
-    tag = 'url'
-
-    def _is_valid(self, value):
-        return validators.url(value)
