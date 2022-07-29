1a9ca577a0ec1ace3569dbda6ecd0f2ce46cac6e
Paul Ritter <ritter.paul1@gmail.com>
2022-07-29 11:28:51 +0200

    tests requirements

--- .github/workflows/validators.py
+++ .github/workflows/validators.py
@@ -1,8 +1,9 @@
 from yamale.validators import Validator
+import validators
 
 class URL(Validator):
     """ Custom URL validator for testing """
     tag = 'url'
 
     def _is_valid(self, value):
-        return False
+        return validators.url(value)
