55d85be767a0af7e37328c4ceda2a576d78dd349
jessicamaybe <jessica@jessicamaybe.com>
2022-07-30 13:45:44 -0700

    comments

--- Content.Server/Nutrition/Components/TrashOnEmptyComponent.cs
+++ Content.Server/Nutrition/Components/TrashOnEmptyComponent.cs
@@ -1,9 +1,11 @@
 namespace Content.Server.Nutrition.Components
 {
-
+    /// <summary>
+    /// Component that tags solution containers as trash when their contents have been emptied.
+    /// Used for things like used ketchup packets or used syringes.
+    /// </summary>
     [RegisterComponent]
     public sealed class TrashOnEmptyComponent : Component
     {
-
     }
 }
