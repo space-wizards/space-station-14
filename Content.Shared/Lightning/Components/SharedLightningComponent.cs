0ad112adcd4b2cadd139a99a556685d8d20c8b29
keronshb <keronshb@live.com>
2022-07-31 12:14:56 -0400

    LightningDraft1

---
+++ Content.Shared/Lightning/Components/SharedLightningComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace Content.Shared.Lightning.Components;
+[RegisterComponent]
+public class SharedLightningComponent : Component
+{
+    /// <summary>
+    /// Can this lightning arc to something else?
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("canArc")]
+    public bool CanArc;
+
+    /// <summary>
+    /// How many arcs should this produce/how far should it go?
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("maxArc")]
+    public int MaxArc;
+}
---
+++ Content.Shared/Lightning/SharedLightningSystem.cs
@@ -0,0 +1,21 @@
+﻿namespace Content.Shared.Lightning;
+
+public class SharedLightningSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    //TODO: Add way to form the lightning (sprites/spawning)
+
+    //TODO: Make fixture with shape (edge/AABB/poly) so the body and impact can shock
+
+    //TODO: Scale the body of the sprite and the fixture.
+
+    //TODO: Add Electrocution Component
+
+    //TODO: Use TimedDespawn to handle the deletion
+
+    //TODO: Add way to arc/chain lightning
+}
