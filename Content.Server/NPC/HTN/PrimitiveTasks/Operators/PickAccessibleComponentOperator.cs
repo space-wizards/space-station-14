5ea8adcbb1fdf151b5c27c0a56d62fc60a4d8fbf
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-08-02 00:45:34 +1000

    walter blanco

--- Content.Server/NPC/HTN/PrimitiveTasks/Operators/PickAccessibleComponentOperator.cs
+++ Content.Server/NPC/HTN/PrimitiveTasks/Operators/PickAccessibleComponentOperator.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Content.Server.NPC.Pathfinding;
 using Content.Server.NPC.Pathfinding.Accessible;
+using Robust.Shared.Map;
 using Robust.Shared.Random;
 
 namespace Content.Server.NPC.HTN.PrimitiveTasks.Operators;
@@ -10,15 +11,20 @@ namespace Content.Server.NPC.HTN.PrimitiveTasks.Operators;
 /// </summary>
 public sealed class PickAccessibleComponentOperator : HTNOperator
 {
+    [Dependency] private readonly IComponentFactory _factory = default!;
     [Dependency] private readonly IEntityManager _entManager = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     private AiReachableSystem _reachable = default!;
+    private EntityLookupSystem _lookup = default!;
 
-    [DataField("idleRangeKey", required: true)]
-    public string IdleRangeKey = string.Empty;
+    [DataField("rangeKey", required: true)]
+    public string RangeKey = string.Empty;
 
     [ViewVariables, DataField("targetKey", required: true)]
     public string TargetKey = string.Empty;
+
+    [ViewVariables, DataField("component", required: true)]
+    public string Component = string.Empty;
 
     public override void Initialize()
     {
@@ -29,15 +35,42 @@ public sealed class PickAccessibleComponentOperator : HTNOperator
     /// <inheritdoc/>
     public override async Task<(bool Valid, Dictionary<string, object>? Effects)> Plan(NPCBlackboard blackboard)
     {
+        // Check if the component exists
+        if (!_factory.TryGetRegistration(Component, out var registration))
+        {
+            return (false, null);
+        }
+
+        var range = blackboard.GetValueOrDefault<float>(RangeKey);
+        var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
+
+        if (!blackboard.TryGetValue<EntityCoordinates>(NPCBlackboard.OwnerCoordinates, out var coordinates))
+        {
+            return (false, null);
+        }
+
+        var comp = registration.Type;
+
+        // TODO: Need to get ones that are accessible.
+        // TODO: Look at unreal HTN to see repeatable ones maybe?
+        foreach (var entity in _lookup.GetEntitiesInRange(coordinates, range))
+        {
+            // if (entity == owner || !_entManager)
+                // continue;
+        }
+
         // TODO: Copy over GoToPuddleSystem here.
 
         // Very inefficient (should weight each region by its node count) but better than the old system
-        var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
 
-        if (!_entManager.TryGetComponent(_entManager.GetComponent<TransformComponent>(owner).GridUid, out IMapGridComponent? grid))
+
+        if (!_entManager.TryGetComponent(_entManager.GetComponent<TransformComponent>(owner).GridUid,
+                out IMapGridComponent? grid))
+        {
             return (false, null);
+        }
 
-        var reachableArgs = ReachableArgs.GetArgs(owner, blackboard.GetValueOrDefault<float>(IdleRangeKey));
+        var reachableArgs = ReachableArgs.GetArgs(owner, blackboard.GetValueOrDefault<float>(RangeKey));
         var entityRegion = _reachable.GetRegion(owner);
         var reachableRegions = _reachable.GetReachableRegions(reachableArgs, entityRegion);
 
@@ -61,4 +94,5 @@ public sealed class PickAccessibleComponentOperator : HTNOperator
         {
             { TargetKey, target },
         });
+    }
 }
