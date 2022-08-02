61db69aecc8df81707f4bffaa95f1997555afb29
Rinkashikachi <15rinkashikachi15@gmail.com>
2022-08-02 11:48:13 +0300

    Added localization for Lathe menu

--- Content.Client/Lathe/UI/LatheMenu.cs
+++ Content.Client/Lathe/UI/LatheMenu.cs
@@ -36,7 +36,7 @@ namespace Content.Client.Lathe.UI
 
             Owner = owner;
 
-            Title = "Lathe Menu"; // TODO Replace this with the name of the lathe itself
+            Title = Loc.GetString("lathe-menu-title"); // TODO Replace this with the name of the lathe itself
 
             var vBox = new BoxContainer
             {
@@ -55,21 +55,21 @@ namespace Content.Client.Lathe.UI
 
             QueueButton = new Button()
             {
-                Text = "Queue",
+                Text = Loc.GetString("lathe-menu-queue"),
                 TextAlign = Label.AlignMode.Center,
                 SizeFlagsStretchRatio = 1,
             };
 
             ServerConnectButton = new Button()
             {
-                Text = "Server list",
+                Text = Loc.GetString("lathe-menu-server-list"),
                 TextAlign = Label.AlignMode.Center,
                 SizeFlagsStretchRatio = 1,
             };
 
             ServerSyncButton  = new Button()
             {
-                Text = "Sync",
+                Text = Loc.GetString("lathe-menu-sync"),
                 TextAlign = Label.AlignMode.Center,
                 SizeFlagsStretchRatio = 1,
             };
@@ -90,7 +90,7 @@ namespace Content.Client.Lathe.UI
 
             _searchBar = new LineEdit()
             {
-                PlaceHolder = "Search Designs",
+                PlaceHolder = Loc.GetString("lathe-menu-search-designs"),
                 HorizontalExpand = true,
                 SizeFlagsStretchRatio = 3
             };
@@ -99,7 +99,7 @@ namespace Content.Client.Lathe.UI
 
             var filterButton = new Button()
             {
-                Text = "Filter",
+                Text = Loc.GetString("lathe-menu-search-filter"),
                 TextAlign = Label.AlignMode.Center,
                 SizeFlagsStretchRatio = 1,
                 Disabled = true,
@@ -116,7 +116,7 @@ namespace Content.Client.Lathe.UI
 
             _amountLineEdit = new LineEdit()
             {
-                PlaceHolder = "Amount",
+                PlaceHolder = Loc.GetString("lathe-menu-search-amount"),
                 Text = "1",
                 HorizontalExpand = true,
             };
