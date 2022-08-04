065f14a999918d5449adc31ce231de26b53a43ad
jessicamaybe <jessica@jessicamaybe.com>
2022-08-03 22:24:14 -0700

    bat

--- Content.Shared/Throwing/BaseballBatSystem.cs
+++ Content.Shared/Throwing/BaseballBatSystem.cs
@@ -1,8 +1,13 @@
 using System.Linq;
+using Content.Shared.Item;
 using Content.Shared.Physics;
 using Content.Shared.Weapons.Melee;
+using Robust.Shared.Audio;
 using Robust.Shared.Map;
 using Robust.Shared.Physics;
+using Robust.Shared.Player;
+using Robust.Shared.Random;
+using Robust.Shared.Utility;
 
 namespace Content.Shared.Throwing
 {
@@ -13,6 +18,9 @@ namespace Content.Shared.Throwing
     {
         [Dependency] private readonly ThrowingSystem _throwingSystem = default!;
         [Dependency] private readonly EntityLookupSystem _entityLookupSystem = default!;
+        [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
+        [Dependency] private readonly IRobustRandom _random = default!;
+
         /// <inheritdoc/>
         public override void Initialize()
         {
@@ -25,18 +33,23 @@ namespace Content.Shared.Throwing
 
             var location = EntityManager.GetComponent<TransformComponent>(args.User).Coordinates;
             var diff = args.ClickLocation.ToMapPos(EntityManager) - location.ToMapPos(EntityManager);
-            var angle = Angle.FromWorldVec(diff);
+
 
             //if (!EntityManager.TryGetComponent<ThrownItemComponent>(args.User, out var thrownItemComponent))
             //    return;
 
             //EntityManager.RemoveComponent<ThrownItemComponent>(args.User);
 
-            var dir = (args.ClickLocation.ToMapPos(EntityManager) - EntityManager.GetComponent<TransformComponent>(args.User).WorldPosition).Normalized;
-
-            foreach (var entity in _entityLookupSystem.GetEntitiesInArc(location, 0.5f, diff.ToAngle(), 45f))
+            foreach (var entity in _entityLookupSystem.GetEntitiesInArc(location, 0.5f, diff.ToAngle(), 50f, LookupFlags.None))
             {
-                _throwingSystem.TryThrow(entity, dir * 2f, 50f, uid);
+                if (EntityManager.HasComponent<ItemComponent>(entity))
+                {
+                    var dir = args.ClickLocation.ToMapPos(EntityManager) - EntityManager.GetComponent<TransformComponent>(args.User).WorldPosition.Normalized;
+                    _throwingSystem.TryThrow(entity, dir * _random.NextFloat(2f, 6f), 2f * component.WackForceMultiplier, uid);
+                    if (EntityManager.HasComponent<ThrownItemComponent>(entity))
+                        _audioSystem.Play("/Audio/Effects/hit_kick.ogg", Filter.Pvs(args.User), entity, AudioParams.Default);
+                }
+
             }
         }
     }
