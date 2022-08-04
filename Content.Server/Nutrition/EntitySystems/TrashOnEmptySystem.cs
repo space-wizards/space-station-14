da2d02a0f41fc144f68ae3ac21fbd3960f8994e3
jessicamaybe <jessica@jessicamaybe.com>
2022-08-04 01:03:46 -0700

    syringes are weird creatures

--- Content.Server/Nutrition/EntitySystems/TrashOnEmptySystem.cs
+++ Content.Server/Nutrition/EntitySystems/TrashOnEmptySystem.cs
@@ -1,7 +1,11 @@
 using Content.Server.Chemistry.Components.SolutionManager;
 using Content.Server.Chemistry.EntitySystems;
 using Content.Server.Nutrition.Components;
+using Content.Server.Popups;
+using Content.Shared.Chemistry.Components;
+using Content.Shared.Popups;
 using Content.Shared.Tag;
+using Robust.Shared.Player;
 
 namespace Content.Server.Nutrition.EntitySystems
 {
@@ -9,6 +13,8 @@ namespace Content.Server.Nutrition.EntitySystems
     {
         [Dependency] private readonly SolutionContainerSystem _solutionContainerSystem = default!;
         [Dependency] private readonly TagSystem _tagSystem = default!;
+        [Dependency] private readonly PopupSystem _popupSystem = default!;
+
         public override void Initialize()
         {
             base.Initialize();
@@ -17,15 +23,27 @@ namespace Content.Server.Nutrition.EntitySystems
 
         private void OnSolutionChange(EntityUid uid, TrashOnEmptyComponent component, SolutionChangedEvent args)
         {
+            EntityManager.EnsureComponent<TagComponent>(component.Owner);
+
             if (!EntityManager.HasComponent<SolutionContainerManagerComponent>((component).Owner))
                 return;
-            if (_tagSystem.HasTag(component.Owner, "Trash"))
-                return;
-            if (!_solutionContainerSystem.TryGetDrainableSolution(component.Owner, out var drinkSolution) || drinkSolution.DrainAvailable <= 0)
+
+            if (_solutionContainerSystem.TryGetDrainableSolution(component.Owner, out var solution))
+                UpdateTags(component, solution);
+            // have to do this because syringes don't have drainable solution OR injectable solution for some reason :(
+            if (_solutionContainerSystem.TryGetSolution(component.Owner, "injector", out var injectableSolution))
+                UpdateTags(component, injectableSolution);
+        }
+
+        private void UpdateTags(TrashOnEmptyComponent component, Solution solution)
+        {
+            if (solution.DrainAvailable <= 0)
             {
-                EntityManager.EnsureComponent<TagComponent>(component.Owner);
                 _tagSystem.AddTag(component.Owner, "Trash");
+                return;
             }
+            if (_tagSystem.HasTag(component.Owner, "Trash"))
+                _tagSystem.RemoveTag(component.Owner, "Trash");
         }
     }
 }
