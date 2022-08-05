92d0ca5a05463109ec1402e613e451925bfc2b9d
Rane <elijahrane@gmail.com>
2022-08-04 23:40:17 -0400

    ship it

--- Content.Server/Medical/BiomassReclaimer/BiomassReclaimerSystem.cs
+++ Content.Server/Medical/BiomassReclaimer/BiomassReclaimerSystem.cs
@@ -13,15 +13,8 @@ using Content.Server.Body.Components;
 using Content.Shared.Throwing;
 using Content.Shared.Nutrition.Components;
 using Content.Shared.Administration.Logs;
-using Content.Shared.Administration.Logs;
 using Content.Shared.Database;
-using Content.Shared.Emag.Components;
-using Content.Shared.Emag.Systems;
-using Content.Shared.IdentityManagement;
-using Content.Shared.Interaction;
-using Content.Shared.Popups;
-using Content.Shared.Tag;
-using Robust.Shared.Player;
+using Content.Server.Climbing;
 
 namespace Content.Server.Medical.BiomassReclaimer
 {
@@ -85,7 +78,7 @@ namespace Content.Server.Medical.BiomassReclaimer
             base.Initialize();
             SubscribeLocalEvent<ActiveBiomassReclaimerComponent, ComponentInit>(OnInit);
             SubscribeLocalEvent<ActiveBiomassReclaimerComponent, ComponentShutdown>(OnShutdown);
-            SubscribeLocalEvent<BiomassReclaimerComponent, DragDropEvent>(OnDragDrop);
+            SubscribeLocalEvent<BiomassReclaimerComponent, ClimbedOnEvent>(OnClimbedOn);
         }
 
         private void OnInit(EntityUid uid, ActiveBiomassReclaimerComponent component, ComponentInit args)
@@ -101,32 +94,37 @@ namespace Content.Server.Medical.BiomassReclaimer
             _ambientSoundSystem.SetAmbience(uid, false);
         }
 
-        private void OnDragDrop(EntityUid uid, BiomassReclaimerComponent component, DragDropEvent args)
+        private bool CanGib(EntityUid uid, EntityUid dragged, BiomassReclaimerComponent component)
         {
-            if (args.Handled)
-                return;
-
-            args.Handled = true;
-
             if (HasComp<ActiveBiomassReclaimerComponent>(uid))
-                return;
+                return false;
 
-            if (!HasComp<MobStateComponent>(args.Dragged))
-                return;
+            if (!HasComp<MobStateComponent>(dragged))
+                return false;
 
             if (!Transform(uid).Anchored)
-                return;
+                return false;
 
             if (TryComp<ApcPowerReceiverComponent>(uid, out var power) && !power.Powered)
+                return false;
+
+            if (component.SafetyEnabled && !_mobState.IsDead(dragged))
+                return false;
+
+            return true;
+        }
+
+        private void OnClimbedOn(EntityUid uid, BiomassReclaimerComponent component, ClimbedOnEvent args)
+        {
+            if (!CanGib(uid, args.Climber, component))
+            {
+                Vector2 direction = (_robustRandom.Next(-2, 2), _robustRandom.Next(-2, 2));
+                _throwing.TryThrow(args.Climber, direction, 0.5f);
                 return;
+            }
+            _adminLogger.Add(LogType.Action, LogImpact.Extreme, $"{ToPrettyString(args.Instigator):player} used a biomass reclaimer to gib {ToPrettyString(args.Climber):target} in {ToPrettyString(uid):reclaimer}");
 
-            if (component.SafetyEnabled && !_mobState.IsDead(args.Dragged)
-                && args.User != args.Dragged)
-                return;
-
-                _adminLogger.Add(LogType.Action, LogImpact.Extreme, $"{ToPrettyString(args.User):player} used a biomass reclaimer to gib {ToPrettyString(args.Dragged):target} in {ToPrettyString(uid):reclaimer}");
-
-            StartProcessing(args.Dragged, component);
+            StartProcessing(args.Climber, component);
         }
         private void StartProcessing(EntityUid toProcess, BiomassReclaimerComponent component)
         {
