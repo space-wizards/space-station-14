92d262d6e758f0ee494f4c4798f2a812e9dd45b4
EmoGarbage404 <retron404@gmail.com>
2022-08-05 14:31:53 -0400

    newkie oversight

--- Content.Server/Traitor/Uplink/UplinkSystem.cs
+++ Content.Server/Traitor/Uplink/UplinkSystem.cs
@@ -76,7 +76,7 @@ namespace Content.Server.Traitor.Uplink
                 {
                     if (!pdaUid.ContainedEntity.HasValue) continue;
 
-                    if (HasComp<PDAComponent>(pdaUid.ContainedEntity.Value))
+                    if (HasComp<PDAComponent>(pdaUid.ContainedEntity.Value) || HasComp<StoreComponent>(pdaUid.ContainedEntity.Value))
                         return pdaUid.ContainedEntity.Value;
                 }
             }
@@ -84,7 +84,7 @@ namespace Content.Server.Traitor.Uplink
             // Also check hands
             foreach (var item in _handsSystem.EnumerateHeld(user))
             {
-                if (HasComp<PDAComponent>(item))
+                if (HasComp<PDAComponent>(item) || HasComp<StoreComponent>(item))
                     return item;
             }
 
