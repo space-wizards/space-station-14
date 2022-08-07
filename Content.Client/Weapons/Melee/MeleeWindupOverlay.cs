d3ef3a65524536dc62e9cd62cde02e4167321566
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-08-08 08:38:37 +1000

    a

--- Content.Client/Weapons/Melee/MeleeWindupOverlay.cs
+++ Content.Client/Weapons/Melee/MeleeWindupOverlay.cs
@@ -7,23 +7,31 @@ namespace Content.Client.Weapons.Melee;
 public sealed class MeleeWindupOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _entManager = default!;
+    private readonly SharedTransformSystem _transform = default!;
 
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
 
     protected override void Draw(in OverlayDrawArgs args)
     {
         var xformQuery = _entManager.GetEntityQuery<TransformComponent>();
+        var handle = args.WorldHandle;
 
         foreach (var comp in _entManager.EntityQuery<ActiveNewMeleeWeaponComponent>())
         {
             if (!xformQuery.TryGetComponent(comp.Owner, out var xform) ||
-                xform.MapID != args.MapId ||
-                args.WorldAABB.Contains(xform.WorldPosition))
+                xform.MapID != args.MapId)
             {
                 continue;
             }
 
+            var worldPos = _transform.GetWorldPosition(xform, xformQuery);
 
+            if (!args.WorldAABB.Contains(worldPos))
+            {
+                continue;
+            }
+
+            handle.DrawCircle(worldPos, 0.25f, Color.White.WithAlpha(0.25f));
         }
     }
 }
