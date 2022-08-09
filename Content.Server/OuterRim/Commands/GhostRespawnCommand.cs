2ab66de82e9f2cd54c2c341147c619e777e94361
moonheart08 <moonheart08@users.noreply.github.com>
2022-08-09 16:37:42 -0500

    adjust gen balance, fix respawn button.

---
+++ Content.Server/OuterRim/Commands/GhostRespawnCommand.cs
@@ -0,0 +1,39 @@
+﻿using Content.Server.GameTicking;
+using Content.Server.Ghost.Components;
+using Content.Shared.CCVar;
+using Robust.Server.Player;
+using Robust.Shared.Configuration;
+using Robust.Shared.Console;
+using Robust.Shared.Timing;
+
+namespace Content.Server.OuterRim.Commands;
+
+public sealed class GhostRespawnCommand : IConsoleCommand
+{
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+    [Dependency] private readonly IConfigurationManager _configurationManager = default!;
+
+    public string Command => "ghostrespawn";
+    public string Description => "";
+    public string Help => "";
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (shell.Player is null)
+            return;
+        if (shell.Player.AttachedEntity is null)
+            return;
+        if (!_entityManager.TryGetComponent<GhostComponent>(shell.Player.AttachedEntity, out var ghost))
+            return;
+
+        var time = ( _gameTiming.ServerTime - ghost.TimeOfDeath);
+        var respawnTime = _configurationManager.GetCVar(CCVars.RespawnTime);
+
+        if (respawnTime > time.TotalSeconds)
+            return;
+
+        var gameTicker = _entityManager.EntitySysManager.GetEntitySystem<GameTicker>();
+
+        gameTicker.Respawn((IPlayerSession)shell.Player);
+    }
+}
