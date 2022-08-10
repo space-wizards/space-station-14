c6ac52babe6ecd4afc031c1e27b3960cadf7699b
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-08-10 16:04:28 +1000

    Run MapsLoadableTest cases in parallel
    Each test takes several seconds so.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -99,7 +99,7 @@ namespace Content.IntegrationTests.Tests
             return task.GetAwaiter().GetResult();
         }
 
-        [Test, TestCaseSource(nameof(GetMapNames))]
+        [Test, TestCaseSource(nameof(GetMapNames)), Parallelizable(ParallelScope.Children)]
         public async Task MapsLoadableTest(string mapName)
         {
             await using var pairTracker = await PoolManager.GetServerClient(new PoolSettings{NoClient = true});
