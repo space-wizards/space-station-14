9d8c4c42d38b044b413600146d3084c5f6c3df11
moonheart08 <moonheart08@users.noreply.github.com>
2022-08-10 00:44:48 -0500

    clean.

--- Tools/setup_secrets.ps1
+++ Tools/setup_secrets.ps1
@@ -1,2 +1,3 @@
-﻿New-Item -ItemType SymbolicLink -Path "Content.ServerSecret\Code" -Target "Secrets\ServerSecret"
+﻿git submodule add https://github.com/moonheart08/outer-rim-14-secrets Secrets
+New-Item -ItemType SymbolicLink -Path "Content.ServerSecret\Code" -Target "Secrets\ServerSecret"
 New-Item -ItemType SymbolicLink -Path "Resources\ServerPrototypes" -Target "Secrets\ServerPrototypes"
