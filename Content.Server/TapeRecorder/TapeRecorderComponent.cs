83daee6f4bca46c5ac9d4bf9ebe5b8bc10cfa824
jessicamaybe <jessica@jessicamaybe.com>
2022-08-11 13:47:33 -0700

    comments

--- Content.Server/TapeRecorder/TapeRecorderComponent.cs
+++ Content.Server/TapeRecorder/TapeRecorderComponent.cs
@@ -3,28 +3,34 @@ namespace Content.Server.TapeRecorder
     [RegisterComponent]
     public class TapeRecorderComponent : Component
     {
+        //A list of all recorded messages
         public List<string> MessageList = new();
-
+        //A list of the timestamps for each recorded message
         public List<float> MessageTimeStamps = new();
 
-        public bool Recording = false;
 
+
+        public bool Recording = false;
         public bool Playing = false;
 
-        public float AccumulatedTime;
 
+        public float AccumulatedTime;
         public float TapeStartTime;
 
-        public float TimeStamp;
-        public int PlaybackCurrentMessage;
 
-        public int NextMessage;
+        //Our current position in the "tape"
+        public float TimeStamp;
+        //During playback, the message we are currently on
+        public int PlaybackCurrentMessage;
 
         public float RecordingStartTime;
 
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField("tapeMaxTime")]
         public float TapeMaxTime;
 
 
+        //stuff for cooldown of using in hand
         public TimeSpan LastUseTime;
         public TimeSpan CooldownEnd;
         [DataField("cooldownTime")]
