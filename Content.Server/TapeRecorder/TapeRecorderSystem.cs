3ffdd6c9665314ff09f635327d31ac6db93f7cce
jessicamaybe <jessica@jessicamaybe.com>
2022-08-11 02:18:59 -0700

    proper recording after stopping

--- Content.Server/TapeRecorder/TapeRecorderSystem.cs
+++ Content.Server/TapeRecorder/TapeRecorderSystem.cs
@@ -1,5 +1,6 @@
 using Content.Server.Chat.Systems;
 using Content.Server.Popups;
+using Content.Shared.Chat;
 using Content.Shared.Interaction.Events;
 using Robust.Shared.Player;
 using Robust.Shared.Timing;
@@ -33,7 +34,6 @@ namespace Content.Server.TapeRecorder
             {
 
                 tape.AccumulatedTime += frameTime;
-
                 if (tape.Playing)
                 {
                     for (int i = 0; i < tape.MessageTimeStamps.Count; i++)
@@ -44,13 +44,35 @@ namespace Content.Server.TapeRecorder
                             tape.PlaybackCurrentMessage++;
                         }
 
-                        if (tape.PlaybackCurrentMessage > tape.MessageList.Count)
-                            tape.Playing = false;
+                        if (tape.PlaybackCurrentMessage == tape.MessageList.Count)
+                            StopPlaying(tape);
                     }
-
                     tape.TimeStamp = tape.AccumulatedTime - tape.TapeStartTime;
                 }
             }
+        }
+
+        public void StartRecording(TapeRecorderComponent component)
+        {
+            component.Recording = true;
+            component.RecordingStartTime = component.AccumulatedTime - component.TimeStamp;
+            _popupSystem.PopupEntity("now recording", component.Owner, Filter.Pvs(component.Owner));
+        }
+
+        public void StartPlaying(TapeRecorderComponent component)
+        {
+            component.Recording = false;
+            component.Playing = true;
+            component.TimeStamp = 0;
+            component.PlaybackCurrentMessage = 0;
+            component.TapeStartTime = component.AccumulatedTime;
+            _popupSystem.PopupEntity("now playing", component.Owner, Filter.Pvs(component.Owner));
+        }
+
+        public void StopPlaying(TapeRecorderComponent component)
+        {
+            component.Playing = false;
+            _popupSystem.PopupEntity("stopped playback", component.Owner, Filter.Pvs(component.Owner));
         }
 
         private void OnUseInHand(EntityUid uid, TapeRecorderComponent component, UseInHandEvent args)
@@ -63,36 +85,29 @@ namespace Content.Server.TapeRecorder
             component.LastUseTime = currentTime;
             component.CooldownEnd = component.LastUseTime + TimeSpan.FromSeconds(component.CooldownTime);
 
-
             if (!component.Recording && !component.Playing)
             {
-                component.Recording = true;
-                component.RecordingStartTime = component.AccumulatedTime;
-                _popupSystem.PopupEntity("now recording", args.User, Filter.Pvs(args.User));
+                StartRecording(component);
                 return;
             }
 
             if (component.Recording && !component.Playing)
             {
-                component.Recording = false;
-                component.Playing = true;
-                component.TimeStamp = 0;
-                component.PlaybackCurrentMessage = 0;
-                component.TapeStartTime = component.AccumulatedTime;
-                _popupSystem.PopupEntity("now playing", args.User, Filter.Pvs(args.User));
+                StartPlaying(component);
                 return;
             }
 
             if (component.Playing)
             {
-                component.Playing = false;
-                _popupSystem.PopupEntity("stopped playback", args.User, Filter.Pvs(args.User));
-                return;
+                StopPlaying(component);
             }
         }
 
         private void OnChatMessageHeard(EntityUid uid, TapeRecorderComponent component, ChatMessageHeardNearbyEvent args)
         {
+
+            if (args.Channel != ChatChannel.Local)
+                return;
 
             if (component.Recording)
             {
