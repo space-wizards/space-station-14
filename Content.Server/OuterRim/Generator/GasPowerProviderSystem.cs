28a509f761493bf7a414618dc93994ae64d2cae5
moonheart08 <moonheart08@users.noreply.github.com>
2022-08-11 20:12:18 -0500

    New thruster type (gas powered.)

---
+++ Content.Server/OuterRim/Generator/GasPowerProviderSystem.cs
@@ -0,0 +1,87 @@
+﻿using Content.Server.Atmos.EntitySystems;
+using Content.Server.Atmos.Piping.Components;
+using Content.Server.NodeContainer;
+using Content.Server.NodeContainer.Nodes;
+using Content.Server.Power.Components;
+using Content.Shared.Atmos;
+using Robust.Shared.Timing;
+
+namespace Content.Server.OuterRim.Generator;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class GasPowerProviderSystem : EntitySystem
+{
+    [Dependency] private readonly AtmosphereSystem _atmosphereSystem = default!;
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<GasPowerProviderComponent, AtmosDeviceUpdateEvent>(OnDeviceUpdated);
+        //SubscribeLocalEvent<GasPowerProviderComponent, AtmosDeviceDisabledEvent>(OnDeviceDisabled);
+    }
+
+    private void OnDeviceDisabled(EntityUid uid, GasPowerProviderComponent component, AtmosDeviceDisabledEvent args)
+    {
+        RaiseLocalEvent(uid, new PowerChangedEvent(true, 0));
+    }
+
+    private void OnDeviceUpdated(EntityUid uid, GasPowerProviderComponent component, AtmosDeviceUpdateEvent args)
+    {
+        if (component.LastProcess == TimeSpan.Zero)
+        {
+            component.LastProcess = _gameTiming.CurTime;
+            return; // Skip tick 0.
+        }
+
+        var timeDelta =  (float)(_gameTiming.CurTime - component.LastProcess).TotalSeconds;
+        component.LastProcess = _gameTiming.CurTime;
+
+        if (!TryComp(uid, out AtmosDeviceComponent? device)
+            || !TryComp(uid, out NodeContainerComponent? nodeContainer)
+            || !nodeContainer.TryGetNode("pipe", out PipeNode? pipe))
+        {
+            return;
+        }
+
+        if (pipe.Air.Temperature <= component.MaxTemperature)
+        {
+            if (pipe.Air.Moles[(int) Gas.Plasma] > component.PlasmaMolesConsumedSec * timeDelta)
+            {
+                pipe.Air.AdjustMoles(Gas.Plasma, -component.PlasmaMolesConsumedSec * timeDelta);
+                Logger.Debug($"{-component.PlasmaMolesConsumedSec * timeDelta}");
+                SetPowered(uid, component, true);
+            }
+            else
+            {
+                SetPowered(uid, component, false);
+            }
+        }
+        else
+        {
+            var pres = component.PressureConsumedSec * timeDelta;
+            if (pipe.Air.Pressure >= pres)
+            {
+                pipe.Air.Remove((pres * 100.0f) / (Atmospherics.R * pipe.Air.Temperature));
+                SetPowered(uid, component, true);
+            }
+            else
+            {
+                SetPowered(uid, component, false);
+            }
+        }
+    }
+
+    private void SetPowered(EntityUid uid, GasPowerProviderComponent comp, bool state)
+    {
+        if (state != comp.Powered)
+        {
+            comp.Powered = state;
+            RaiseLocalEvent(uid, new PowerChangedEvent(state, 0));
+        }
+
+        return;
+    }
+}
--- Content.Server/Power/EntitySystems/StaticPowerSystem.cs
+++ Content.Server/Power/EntitySystems/StaticPowerSystem.cs
@@ -1,3 +1,4 @@
+using Content.Server.OuterRim.Generator;
 using Content.Server.Power.Components;
 
 namespace Content.Server.Power.EntitySystems;
@@ -8,6 +9,7 @@ public static class StaticPowerSystem
     // ReSharper disable once UnusedParameter.Global
     public static bool IsPowered(this EntitySystem system, EntityUid uid, IEntityManager entManager, ApcPowerReceiverComponent? receiver = null)
     {
-        return entManager.TryGetComponent<ApcPowerReceiverComponent>(uid, out receiver) && receiver.Powered;
+        return (entManager.TryGetComponent<ApcPowerReceiverComponent>(uid, out receiver) && receiver.Powered)
+            || (entManager.TryGetComponent<GasPowerProviderComponent>(uid, out var gasPower) && gasPower.Powered);
     }
 }
