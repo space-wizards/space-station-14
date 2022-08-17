be0fdef2b49c62ea12859748e0c27562f32dfcc0
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-08-17 14:01:24 +1000

    Hotfix storage arbitrage
    There's still many more with whitelists

--- Content.IntegrationTests/Tests/StorageTest.cs
+++ Content.IntegrationTests/Tests/StorageTest.cs
@@ -1,14 +1,42 @@
 #nullable enable
 using System.Threading.Tasks;
 using Content.Server.Storage.Components;
+using Content.Shared.Item;
+using Content.Shared.Storage;
 using NUnit.Framework;
 using Robust.Shared.Prototypes;
 
 namespace Content.IntegrationTests.Tests
 {
     [TestFixture]
-    public sealed class StorageFillTest
+    public sealed class StorageTest
     {
+        /// <summary>
+        /// Can an item store more than itself weighs.
+        /// In an ideal world this test wouldn't need to exist because sizes would be recursive.
+        /// </summary>
+        [Test]
+        public async Task StorageSizeArbitrageTest()
+        {
+            await using var pairTracker = await PoolManager.GetServerClient(new PoolSettings{NoClient = true});
+            var server = pairTracker.Pair.Server;
+
+            var protoManager = server.ResolveDependency<IPrototypeManager>();
+
+            await server.WaitAssertion(() =>
+            {
+                foreach (var proto in protoManager.EnumeratePrototypes<EntityPrototype>())
+                {
+                    if (!proto.TryGetComponent<ServerStorageComponent>("Storage", out var storage) ||
+                        storage.Whitelist != null ||
+                        !proto.TryGetComponent<ItemComponent>("Item", out var item)) continue;
+
+                    Assert.That(storage.StorageCapacityMax, Is.LessThanOrEqualTo(item.Size), $"Found storage arbitrage on {proto.ID}");
+                }
+            });
+            await pairTracker.CleanReturnAsync();
+        }
+
         [Test]
         public async Task TestStorageFillPrototypes()
         {
