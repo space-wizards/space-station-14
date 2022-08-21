fb85f80b92ef9d48601b5b5a3de3d295f9db003e
Alexander Evgrashin <aevgrashin@yandex.ru>
2022-08-21 22:38:46 +0200

    Start refactoring

---
+++ Content.Shared/Radiation/Systems/SharedRadiationSystem.Blocker.cs
@@ -0,0 +1,130 @@
+using Content.Shared.Radiation.Components;
+
+namespace Content.Shared.Radiation.Systems;
+
+// Process RadiationBlocker component to create rad resistance map
+// When blocker is anchored it's cached in resistance map by occupied tile
+public partial class SharedRadiationSystem
+{
+    public Dictionary<EntityUid, Dictionary<Vector2i, float>> _resistancePerTile = new();
+
+    private void InitRadBlocking()
+    {
+        SubscribeLocalEvent<RadiationBlockerComponent, ComponentInit>(OnInit);
+        SubscribeLocalEvent<RadiationBlockerComponent, ComponentShutdown>(OnShutdown);
+        SubscribeLocalEvent<RadiationBlockerComponent, AnchorStateChangedEvent>(OnAnchorChanged);
+        SubscribeLocalEvent<RadiationBlockerComponent, ReAnchorEvent>(OnReAnchor);
+        SubscribeLocalEvent<GridRemovalEvent>(OnGridRemoved);
+    }
+
+    private void OnInit(EntityUid uid, RadiationBlockerComponent component, ComponentInit args)
+    {
+        AddTile(uid, component);
+    }
+
+    private void OnShutdown(EntityUid uid, RadiationBlockerComponent component, ComponentShutdown args)
+    {
+        RemoveTile(uid, component);
+    }
+
+    private void OnAnchorChanged(EntityUid uid, RadiationBlockerComponent component, ref AnchorStateChangedEvent args)
+    {
+        if (args.Anchored)
+        {
+            AddTile(uid, component);
+        }
+        else
+        {
+            RemoveTile(uid, component);
+        }
+    }
+
+    private void OnReAnchor(EntityUid uid, RadiationBlockerComponent component, ref ReAnchorEvent args)
+    {
+        // probably grid was split
+        // we need to remove entity from old resistance map
+        RemoveTile(uid, component);
+        // and move it to the new one
+        AddTile(uid, component);
+    }
+
+    private void OnGridRemoved(GridRemovalEvent ev)
+    {
+        _resistancePerTile.Remove(ev.EntityUid);
+    }
+
+    private void AddTile(EntityUid uid, RadiationBlockerComponent component)
+    {
+        // check that last position was removed
+        if (component.LastPosition != null)
+        {
+            RemoveTile(uid, component);
+        }
+
+        // check if entity even provide some rad protection
+        if (!component.Enabled || component.RadResistance <= 0)
+            return;
+
+        // check if it's on a grid
+        var trs = Transform(uid);
+        if (!trs.Anchored || trs.GridUid == null || !TryComp(trs.GridUid, out IMapGridComponent? grid))
+            return;
+
+        // save resistance into rad protection grid
+        var gridId = trs.GridUid.Value;
+        var tilePos = grid.Grid.TileIndicesFor(trs.Coordinates);
+        AddToTile(gridId, tilePos, component.RadResistance);
+
+        // and remember it as last valid position
+        component.LastPosition = (gridId, tilePos);
+    }
+
+    private void RemoveTile(EntityUid uid, RadiationBlockerComponent component)
+    {
+        // check if blocker was placed on grid before component was removed
+        if (component.LastPosition == null)
+            return;
+        var (gridId, tilePos) = component.LastPosition.Value;
+
+        // try to remove
+        RemoveFromTile(gridId, tilePos, component.RadResistance);
+        component.LastPosition = null;
+    }
+
+    private void AddToTile(EntityUid gridId, Vector2i tilePos, float radResistance)
+    {
+        // get existing rad resistance grid or create it if it doesn't exist
+        if (!_resistancePerTile.ContainsKey(gridId))
+        {
+            _resistancePerTile.Add(gridId, new Dictionary<Vector2i, float>());
+        }
+        var grid = _resistancePerTile[gridId];
+
+        // add to existing cell more rad resistance
+        var newResistance = radResistance;
+        if (grid.TryGetValue(tilePos, out var existingResistance))
+        {
+            newResistance += existingResistance;
+        }
+        grid[tilePos] = newResistance;
+    }
+
+    private void RemoveFromTile(EntityUid gridId, Vector2i tilePos, float radResistance)
+    {
+        // get grid
+        if (!_resistancePerTile.ContainsKey(gridId))
+            return;
+        var grid = _resistancePerTile[gridId];
+
+        // subtract resistance from tile
+        if (!grid.TryGetValue(tilePos, out var existingResistance))
+            return;
+        existingResistance -= radResistance;
+
+        // remove tile from grid if no resistance left
+        if (existingResistance > 0)
+            grid[tilePos] = existingResistance;
+        else
+            grid.Remove(tilePos);
+    }
+}
--- Content.Shared/Radiation/Systems/SharedRadiationSystem.cs
+++ Content.Shared/Radiation/Systems/SharedRadiationSystem.cs
@@ -3,7 +3,7 @@ using Robust.Shared.Map;
 
 namespace Content.Shared.Radiation.Systems;
 
-public abstract class SharedRadiationSystem : EntitySystem
+public abstract partial class SharedRadiationSystem : EntitySystem
 {
     [Dependency] private readonly IMapManager _mapManager = default!;
 
@@ -21,71 +21,18 @@ public abstract class SharedRadiationSystem : EntitySystem
     public EntityUid gridUid;
     public Dictionary<Vector2i, float> visitedTiles = new();
 
+    public override void Initialize()
+    {
+        base.Initialize();
+        InitRadBlocking();
+    }
+
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
+        UpdateRadSources();
 
-        foreach (var comp in EntityManager.EntityQuery<RadiationSourceComponent>())
-        {
-            var ent = comp.Owner;
-            var cords = Transform(ent).MapPosition;
-            CalculateRadiationMap(cords, comp.RadsPerSecond);
-        }
     }
 
-    public void CalculateRadiationMap(MapCoordinates epicenter, float radsPerSecond)
-    {
-        MapId = epicenter.MapId;
 
-        Vector2i initialTile;
-        if (_mapManager.TryFindGridAt(epicenter, out var candidateGrid) &&
-            candidateGrid.TryGetTileRef(candidateGrid.WorldToTile(epicenter.Position), out var tileRef) )
-        {
-            gridUid = tileRef.GridUid;
-            initialTile = tileRef.GridIndices;
-        }
-        else
-        {
-            return;
-        }
-
-        var query = GetEntityQuery<RadiationBlockerComponent>();
-
-        visitedTiles.Clear();
-        var visitNext = new Queue<(Vector2i, float)>();
-        visitNext.Enqueue((initialTile, radsPerSecond));
-
-        do
-        {
-            var (current, incomingRads) = visitNext.Dequeue();
-            if (visitedTiles.ContainsKey(current) || incomingRads <= 0f)
-                continue;
-
-            visitedTiles.Add(current, incomingRads);
-
-            // here is material absorption
-            var nextRad = incomingRads;
-            var ents = candidateGrid.GetAnchoredEntities(current);
-            foreach (var uid in ents)
-            {
-                if (!query.TryGetComponent(uid, out var blocker))
-                    continue;
-                nextRad -= blocker.RadResistance;
-            }
-
-
-            // and also remove by distance
-            nextRad -= 1f;
-            // if no radiation power left - don't propagate further
-            if (nextRad <= 0)
-                continue;
-
-            foreach (var dir in _directions)
-            {
-                var next = current.Offset(dir);
-                visitNext.Enqueue((next, nextRad));
-            }
-
-        } while (visitNext.Count != 0);
-    }
 }
