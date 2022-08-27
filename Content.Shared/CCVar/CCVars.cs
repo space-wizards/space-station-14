7e1d35f0bdb21863c635b32b758e442693073241
moonheart08 <moonheart08@users.noreply.github.com>
2022-08-27 01:16:47 -0500

    need you back on.

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -1128,7 +1128,7 @@ namespace Content.Shared.CCVar
          */
 
         public static readonly CVarDef<bool> WorldGenEnabled =
-            CVarDef.Create("worldgen.enabled", false, CVar.SERVERONLY);
+            CVarDef.Create("worldgen.enabled", true, CVar.SERVERONLY);
 
         public static readonly CVarDef<string> SpawnBiome =
             CVarDef.Create("worldgen.spawn_biome", "SpawnBiome", CVar.SERVERONLY);
