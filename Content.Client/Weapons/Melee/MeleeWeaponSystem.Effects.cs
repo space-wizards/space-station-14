6f3cc788ceb214b62b0503f6ebda4bfa073a548c
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-08-30 18:06:57 +1000

    Use original colour for lerping

--- Content.Client/Weapons/Melee/MeleeWeaponSystem.Effects.cs
+++ Content.Client/Weapons/Melee/MeleeWeaponSystem.Effects.cs
@@ -6,10 +6,7 @@ namespace Content.Client.Weapons.Melee;
 
 public sealed partial class MeleeWeaponSystem
 {
-    /// <summary>
-    /// Plays the red effect whenever the server confirms something is hit
-    /// </summary>
-    public static readonly Animation DamageAnimation = new()
+    private static readonly Animation DefaultDamageAnimation = new()
     {
         Length = TimeSpan.FromSeconds(0.3),
         AnimationTracks =
@@ -29,12 +26,50 @@ public sealed partial class MeleeWeaponSystem
 
     private const string DamageAnimationKey = "damage-effect";
 
+    /// <summary>
+    /// Gets the red effect animation whenever the server confirms something is hit
+    /// </summary>
+    public Animation? GetDamageAnimation(EntityUid uid)
+    {
+        if (!TryComp<SpriteComponent>(uid, out var sprite))
+            return null;
+
+        // 90% of them are going to be this so why allocate a new class.
+        if (sprite.Color.Equals(Color.White))
+            return DefaultDamageAnimation;
+
+        return new Animation
+        {
+            Length = TimeSpan.FromSeconds(0.3),
+            AnimationTracks =
+            {
+                new AnimationTrackComponentProperty()
+                {
+                    ComponentType = typeof(SpriteComponent),
+                    Property = nameof(SpriteComponent.Color),
+                    KeyFrames =
+                    {
+                        new AnimationTrackProperty.KeyFrame(Color.Red, 0f),
+                        new AnimationTrackProperty.KeyFrame(Color.White, 0.3f)
+                    }
+                }
+            }
+        };
+    }
+
     private void OnDamageEffect(DamageEffectEvent ev)
     {
         if (Deleted(ev.Entity))
             return;
 
+        // Need to stop the existing animation first to ensure the sprite color is fixed.
+        // Otherwise we might lerp to a red colour instead.
         _animation.Stop(ev.Entity, DamageAnimationKey);
-        _animation.Play(ev.Entity, DamageAnimation, DamageAnimationKey);
+        var animation = GetDamageAnimation(ev.Entity);
+
+        if (animation == null)
+            return;
+
+        _animation.Play(ev.Entity, DefaultDamageAnimation, DamageAnimationKey);
     }
 }
