8f2b27cab1b8355db973303c9725e8f8c283f443
CommieFlowers <rasmus.cedergren@hotmail.com>
2022-08-31 23:38:33 +0200

    APC construction and deconstruction Construction action GivePrototype

---
+++ Content.Server/Power/Components/ApcElectronicsComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Server.Power.Components
+{
+    [RegisterComponent]
+    /// <summary>
+    /// APC electronics, used for constructing APC:s
+    /// </summary>
+    public sealed class ApcElectronicsComponent : Component
+    { }
+}
