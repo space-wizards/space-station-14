42ebb21cc8f9d63d85055f12132a484f4adb6a19
CommieFlowers <rasmus.cedergren@hotmail.com>
2022-09-03 04:12:38 +0200

    removed comments

--- Content.Server/Power/EntitySystems/ApcSystem.cs
+++ Content.Server/Power/EntitySystems/ApcSystem.cs
@@ -53,8 +53,6 @@ namespace Content.Server.Power.EntitySystems
             UpdateApcState(uid, component);
         }
 
-        // TODO: Make APC:s need Screwing to update their sprite
-
         private void OnApcInit(EntityUid uid, ApcComponent component, MapInitEvent args)
         {
             UpdateApcState(uid, component);
