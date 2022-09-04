acaf0ec8cde9922c37ad9f54c3b95e3cc03c6f36
vulppine <vulppine@gmail.com>
2022-09-03 22:36:44 -0700

    removes a comment (it was wrong)

--- Content.Shared/Atmos/Monitor/AtmosAlarmType.cs
+++ Content.Shared/Atmos/Monitor/AtmosAlarmType.cs
@@ -8,6 +8,6 @@ public enum AtmosAlarmType : sbyte
     Invalid = 0,
     Normal = 1,
     Warning = 2,
-    Danger = 3, // 1 << 1 is the exact same thing and we're not really doing **bitmasking** are we?
+    Danger = 3,
     Emagged = 4,
 }
