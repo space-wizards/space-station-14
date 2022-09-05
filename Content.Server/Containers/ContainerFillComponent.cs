f2eb0763d06470ed904982c5e91b790cc53e617d
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2022-09-05 01:03:39 -0700

    Add ContainerFill to spawn items in containers on MapInit

---
+++ Content.Server/Containers/ContainerFillComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace Content.Server.Containers;
+
+[RegisterComponent]
+public sealed class ContainerFillComponent : Component
+{
+    [ViewVariables]
+    [DataField("construction")]
+    public bool Construction;
+
+    [ViewVariables]
+    [DataField("containers")]
+    public Dictionary<string, List<string>> Containers = new();
+}
---
+++ Content.Server/Containers/ContainerFillSystem.cs
@@ -0,0 +1,48 @@
+﻿using Content.Server.Construction;
+using Content.Server.Construction.Components;
+using Robust.Server.Containers;
+using Robust.Shared.Containers;
+
+namespace Content.Server.Containers;
+
+public sealed class ContainerFillSystem : EntitySystem
+{
+    [Dependency] private readonly ContainerSystem _containerSystem = default!;
+    [Dependency] private readonly ConstructionSystem _constructionSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ContainerFillComponent, MapInitEvent>(OnContainerFillMapInit);
+    }
+
+    private void OnContainerFillMapInit(EntityUid uid, ContainerFillComponent component, MapInitEvent args)
+    {
+        if (component.Containers.Count == 0) return;
+
+        if (!TryComp<ContainerManagerComponent>(uid, out var containerManagerComponent))
+            return;
+
+        ConstructionComponent? construction = null;
+        if (component.Construction && !TryComp(uid, out construction))
+            Logger.Warning($"{ToPrettyString(uid)} doesn't have a construction component even though Construction is true!");
+
+        var transform = Transform(uid);
+        var coordinates = transform.Coordinates;
+
+        foreach (var (containerId, contents) in component.Containers)
+        {
+            var container = _containerSystem.EnsureContainer<IContainer>(uid, containerId, containerManagerComponent);
+            foreach (var prototypeName in contents)
+            {
+                var entity = EntityManager.SpawnEntity(prototypeName, coordinates);
+                if (!container.Insert(entity, EntityManager, ownerTransform: transform))
+                    Logger.Warning($"Couldn't insert {ToPrettyString(entity)} into {ToPrettyString(uid)}!");
+            }
+
+            if (construction != null)
+                _constructionSystem.AddContainer(uid, containerId, construction);
+        }
+    }
+}
