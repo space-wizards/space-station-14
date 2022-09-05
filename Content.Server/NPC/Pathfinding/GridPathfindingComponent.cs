0101f481bedb2a578b5ebac8446055e6a4a408e0
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-09-05 23:15:04 +1000

    rev

--- Content.Server/NPC/Pathfinding/GridPathfindingComponent.cs
+++ Content.Server/NPC/Pathfinding/GridPathfindingComponent.cs
@@ -2,7 +2,7 @@ namespace Content.Server.NPC.Pathfinding;
 
 [RegisterComponent]
 [Access(typeof(PathfindingSystem))]
-public sealed class GridPathfindingComponent : Component, IPathfindingGraph
+public sealed class GridPathfindingComponent : Component
 {
     public readonly Dictionary<Vector2i, PathfindingChunk> Graph = new();
 }
--- Content.Server/NPC/Pathfinding/IPathfindingGraph.cs
+++
@@ -1,6 +0,0 @@
-namespace Content.Server.NPC.Pathfinding;
-
-public interface IPathfindingGraph
-{
-
-}
