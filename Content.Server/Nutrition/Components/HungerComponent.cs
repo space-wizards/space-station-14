9f40c175952d05e632a27f0dfd916b1af24eff3a
Visne <39844191+Visne@users.noreply.github.com>
2022-09-10 01:46:17 +0200

    die

--- Content.Server/Nutrition/Components/HungerComponent.cs
+++ Content.Server/Nutrition/Components/HungerComponent.cs
@@ -2,7 +2,6 @@ using Content.Shared.Alert;
 using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Systems;
 using Content.Shared.Nutrition.Components;
-using Content.Shared.Rejuvenate;
 using Robust.Shared.Random;
 
 namespace Content.Server.Nutrition.Components
