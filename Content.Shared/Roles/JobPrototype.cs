18c3678b50ba270afd5c5d8e60f7b4c27f1afdd6
Rane <elijahrane@gmail.com>
2022-09-09 10:27:11 -0400

    reviews

--- Content.Shared/Roles/JobPrototype.cs
+++ Content.Shared/Roles/JobPrototype.cs
@@ -57,8 +57,8 @@ namespace Content.Shared.Roles
         ///     A numerical score for how much easier this job is for antagonists.
         ///     For traitors, reduces starting TC by this amount. Other gamemodes can use it for whatever they find fitting.
         /// </summary>
-        [DataField("difficultyAdjustment")]
-        public int DifficultyAdjustment = 0;
+        [DataField("antagAdvantage")]
+        public int AntagAdvantage = 0;
 
         [DataField("startingGear", customTypeSerializer: typeof(PrototypeIdSerializer<StartingGearPrototype>))]
         public string? StartingGear { get; private set; }
