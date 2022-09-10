d72e4a603927007e1415100e206a51bfffb4f5b4
CommieFlowers <rasmus.cedergren@hotmail.com>
2022-09-10 10:07:09 +0200

    Melee update, splitting MeleeHitEvent into MeleeHitEvent and GetMeleeDamageEvent

--- Content.Server/Weapon/Melee/EnergySword/Components/EnergySwordComponent.cs
+++ Content.Server/Weapon/Melee/EnergySword/Components/EnergySwordComponent.cs
@@ -16,6 +16,12 @@ namespace Content.Server.Weapon.Melee.EnergySword
         public bool IsSharp = true;
 
         /// <summary>
+        ///     Does this become hidden when deactivated
+        /// </summary>
+        [DataField("secret")]
+        public bool Secret { get; set; } = false;
+
+        /// <summary>
         ///     RGB cycle rate for hacked e-swords.
         /// </summary>
         [DataField("cycleRate")]
