e4872336d78358f55d91cd726728c4529651ca3e
Ashley <theashtronaut@protonmail.ch>
2022-09-15 16:19:57 -0700

    remove todo comment because I did it lol

--- Content.Server/Lock/LockSystem.cs
+++ Content.Server/Lock/LockSystem.cs
@@ -194,7 +194,6 @@ namespace Content.Server.Lock
                 () => TryLock(uid, args.User, component);
             verb.Text = Loc.GetString(component.Locked ? "toggle-lock-verb-unlock" : "toggle-lock-verb-lock");
             verb.IconTexture = component.Locked ? "/Textures/Interface/VerbIcons/unlock.svg.192dpi.png" : "/Textures/Interface/VerbIcons/lock.svg.192dpi.png";
-            // TODO VERB ICONS need padlock open/close icons.
             args.Verbs.Add(verb);
         }
 
