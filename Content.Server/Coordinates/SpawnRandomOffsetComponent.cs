83eede4a29bc94a5f2b019505d6cbfbe0fb8e01d
wrexbe <wrexbe@protonmail.com>
2022-09-16 12:56:38 -0700

    Set random offset with VV

--- Content.Server/Coordinates/SpawnRandomOffsetComponent.cs
+++ Content.Server/Coordinates/SpawnRandomOffsetComponent.cs
@@ -3,5 +3,6 @@ namespace Content.Server.Coordinates;
 [RegisterComponent]
 public sealed class SpawnRandomOffsetComponent : Component
 {
+    [ViewVariables(VVAccess.ReadWrite)]
     [DataField("offset")] public float Offset = 0.5f;
 }
