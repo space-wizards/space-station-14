14d48f4306d7de70cd2e4ed3e20328c4d695b580
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2022-09-17 00:37:15 +1000

    Pointing arrow changes (#11097)

--- Content.Client/Pointing/PointingSystem.cs
+++ Content.Client/Pointing/PointingSystem.cs
@@ -2,14 +2,52 @@ using Content.Client.Pointing.Components;
 using Content.Shared.MobState.EntitySystems;
 using Content.Shared.Pointing;
 using Content.Shared.Verbs;
+using Robust.Client.Animations;
 using Robust.Client.GameObjects;
+using Robust.Shared.Animations;
 using DrawDepth = Content.Shared.DrawDepth.DrawDepth;
 
 namespace Content.Client.Pointing;
 
-public sealed class PointingSystem : EntitySystem
+public sealed class PointingSystem : SharedPointingSystem
 {
+    [Dependency] private readonly AnimationPlayerSystem _player = default!;
     [Dependency] private readonly SharedMobStateSystem _mobState = default!;
+
+    private const string AnimationKey = "pointingarrow";
+
+    /// <summary>
+    /// How far it goes in any direction.
+    /// </summary>
+    private const float Offset = 0.25f;
+
+    /// <summary>
+    /// How long it takes to go from the bottom of the animation to the top.
+    /// </summary>
+    private const float UpTime = 0.5f;
+
+    /// <summary>
+    /// Starts at the bottom then goes up and comes back down. Seems to look nicer than starting in the middle.
+    /// </summary>
+    private static readonly Animation PointingAnimation = new Animation()
+    {
+        Length = TimeSpan.FromSeconds(2 * UpTime),
+        AnimationTracks =
+        {
+            new AnimationTrackComponentProperty()
+            {
+                ComponentType = typeof(SpriteComponent),
+                Property = nameof(SpriteComponent.Offset),
+                InterpolationMode = AnimationInterpolationMode.Linear,
+                KeyFrames =
+                {
+                    new AnimationTrackProperty.KeyFrame(Vector2.Zero, 0f),
+                    new AnimationTrackProperty.KeyFrame(new Vector2(0f, Offset), UpTime),
+                    new AnimationTrackProperty.KeyFrame(Vector2.Zero, UpTime),
+                }
+            }
+        }
+    };
 
     public override void Initialize()
     {
@@ -17,8 +55,13 @@ public sealed class PointingSystem : EntitySystem
 
         SubscribeLocalEvent<GetVerbsEvent<Verb>>(AddPointingVerb);
         SubscribeLocalEvent<PointingArrowComponent, ComponentStartup>(OnArrowStartup);
+        SubscribeLocalEvent<PointingArrowComponent, AnimationCompletedEvent>(OnArrowAnimation);
         SubscribeLocalEvent<RoguePointingArrowComponent, ComponentStartup>(OnRogueArrowStartup);
+    }
 
+    private void OnArrowAnimation(EntityUid uid, PointingArrowComponent component, AnimationCompletedEvent args)
+    {
+        _player.Play(uid, PointingAnimation, AnimationKey);
     }
 
     private void AddPointingVerb(GetVerbsEvent<Verb> args)
@@ -58,6 +101,8 @@ public sealed class PointingSystem : EntitySystem
         {
             sprite.DrawDepth = (int) DrawDepth.Overlays;
         }
+
+        _player.Play(uid, PointingAnimation, AnimationKey);
     }
 
     private void OnRogueArrowStartup(EntityUid uid, RoguePointingArrowComponent arrow, ComponentStartup args)
@@ -65,6 +110,7 @@ public sealed class PointingSystem : EntitySystem
         if (EntityManager.TryGetComponent(uid, out SpriteComponent? sprite))
         {
             sprite.DrawDepth = (int) DrawDepth.Overlays;
+            sprite.NoRotation = false;
         }
     }
 }
