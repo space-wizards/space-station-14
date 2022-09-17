41176212a141ed7ec6c39dfe4fc0c3071a788931
CommieFlowers <rasmus.cedergren@hotmail.com>
2022-09-17 14:27:44 +0200

    armor pricing v1

--- Content.Server/Armor/ArmorSystem.cs
+++ Content.Server/Armor/ArmorSystem.cs
@@ -2,6 +2,7 @@
 using Content.Server.Examine;
 using Content.Shared.Verbs;
 using Robust.Shared.Utility;
+using Content.Server.Cargo.Systems;
 
 namespace Content.Server.Armor
 {
@@ -16,8 +17,83 @@ namespace Content.Server.Armor
 
             SubscribeLocalEvent<ArmorComponent, DamageModifyEvent>(OnDamageModify);
             SubscribeLocalEvent<ArmorComponent, GetVerbsEvent<ExamineVerb>>(OnArmorVerbExamine);
+            SubscribeLocalEvent<ArmorComponent, PriceCalculationEvent>(GetArmorPrice);
         }
 
+        private void GetArmorPrice(EntityUid uid, ArmorComponent component, ref PriceCalculationEvent args)
+        {
+            if (component.Modifiers == null)
+                return;
+
+            double price = 0;
+
+            double coefDefaultPrice = 1; //default price of 1% protection against a certain type of damage
+            var coefPriceList = new Dictionary<string, double>()
+            {
+                {"Brute", 3 }, //Group: Blunt, Slash, Piercing
+                {"Blunt", 1 },
+                {"Slash", 1 },
+                {"Piercing", 1 },
+                {"Burn", 3.6 }, //Group: Heat, Cold, Shock
+                {"Heat", 1.2 },
+                {"Cold", 1.2 },
+                {"Shock", 1.2 },
+                {"Toxin", 6.7 }, //Group: Radiation, Poison
+                {"Radiation", 1.2 }, 
+                {"Poison", 5 }, //  Armor stopping poison damage? Sounds strong
+                {"Genetic", 2 }, //Group: Cellular
+                {"Cellular", 2 }, //idk
+                {"Airloss", 6 }, //Group: Bloodloss, Asphyxiation
+                {"Bloodloss", 3 }, 
+                {"Asphyxiation", 3 },
+                {"Caustic", 6.2 } //Group: Heat, Poison
+            };
+            double flatDefaultPrice = 5; //default price of 1 damage protection against a certain type of damage
+            var flatPriceList = new Dictionary<string, double>()
+            {
+                {"Brute", 15 }, //Group: Blunt, Slash, Piercing
+                {"Blunt", 5 },
+                {"Slash", 5 },
+                {"Piercing", 5 },
+                {"Burn", 30 }, //Group: Heat, Cold, Shock
+                {"Heat", 10 },
+                {"Cold", 10 },
+                {"Shock", 10 },
+                {"Toxin", 62 }, //Group: Radiation, Poison
+                {"Radiation", 12 },
+                {"Poison", 50 }, //  Armor stopping poison damage? Sounds strong
+                {"Genetic", 20 }, //Group: Cellular
+                {"Cellular", 20 }, //idk
+                {"Airloss", 60 }, //Group: Bloodloss, Asphyxiation
+                {"Bloodloss", 30 },
+                {"Asphyxiation", 30 },
+                {"Caustic", 60 } //Group: Heat, Poison
+            };
+
+            foreach (var modifier in component.Modifiers.Coefficients)
+            {
+                if (coefPriceList.ContainsKey(modifier.Key))
+                {
+                    price += coefPriceList[modifier.Key] * 100 * (1-modifier.Value);
+                }
+                else
+                {
+                    price += coefDefaultPrice * 100 * (1 - modifier.Value);
+                }
+            }
+            foreach (var modifier in component.Modifiers.FlatReduction)
+            {
+                if (flatPriceList.ContainsKey(modifier.Key))
+                {
+                    price += flatPriceList[modifier.Key] * modifier.Value;
+                }
+                else
+                {
+                    price += flatDefaultPrice * modifier.Value;
+                }
+            }
+            args.Price += price;
+        }
         private void OnDamageModify(EntityUid uid, ArmorComponent component, DamageModifyEvent args)
         {
             args.Damage = DamageSpecifier.ApplyModifierSet(args.Damage, component.Modifiers);
