f1bafd5a87fcf53c0e5096a8bfa4725dcb4ba632
brainfood1183 <newman1183@googlemail.com>
2022-09-18 03:57:08 +0100

    another prawn push attempt

---
+++ Content.Server/Speech/Components/PrawnAccentComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Server.Speech.Components;
+
+[RegisterComponent]
+public sealed class PrawnAccentComponent : Component
+{
+    [DataField("isFishy")]
+    public bool IsFishy = true;
+}
