fa5a3adf19a31ff6f81082217e3bd613cea9d8d5
vulppine <vulppine@gmail.com>
2022-09-17 21:20:13 -0700

    adds a requires power component for device networked entities

---
+++ Content.Server/DeviceNetwork/Components/DeviceNetworkRequiresPowerComponent.cs
@@ -0,0 +1,6 @@
+namespace Content.Server.DeviceNetwork.Components;
+
+[RegisterComponent]
+public sealed class DeviceNetworkRequiresPowerComponent : Component
+{
+}
---
+++ Content.Server/DeviceNetwork/Systems/DeviceNetworkRequiresPowerSystem.cs
@@ -0,0 +1,22 @@
+using Content.Server.DeviceNetwork.Components;
+using Content.Server.Power.Components;
+using Content.Server.Power.EntitySystems;
+
+namespace Content.Server.DeviceNetwork.Systems;
+
+public sealed class DeviceNetworkRequiresPowerSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<DeviceNetworkRequiresPowerComponent, BeforePacketSentEvent>(OnBeforePacketSent);
+    }
+
+    private void OnBeforePacketSent(EntityUid uid, DeviceNetworkRequiresPowerComponent component,
+        BeforePacketSentEvent args)
+    {
+        if (!this.IsPowered(uid, EntityManager))
+        {
+            args.Cancel();
+        }
+    }
+}
