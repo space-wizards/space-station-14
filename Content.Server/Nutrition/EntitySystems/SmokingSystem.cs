d8dc54f83619a79cc59d4b2db3734343ed113907
themias <89101928+themias@users.noreply.github.com>
2022-09-19 19:54:33 -0400

    Add smoking pipe (#11419)
    * Add smoking pipe
    
    * fix access issue
    
    * try fix init issue
    
    * fixed for real

--- Content.Server/Nutrition/EntitySystems/SmokingSystem.cs
+++ Content.Server/Nutrition/EntitySystems/SmokingSystem.cs
@@ -44,6 +44,7 @@ namespace Content.Server.Nutrition.EntitySystems
             SubscribeLocalEvent<SmokableComponent, ComponentShutdown>(OnSmokableShutdownEvent);
 
             InitializeCigars();
+            InitializePipes();
         }
 
         public void SetSmokableState(EntityUid uid, SmokableState state, SmokableComponent? smokable = null,
