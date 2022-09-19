3998f75bec3b83d9c9eea62fc83e4feabe094eff
brainfood1183 <newman1183@googlemail.com>
2022-09-19 12:44:52 +0100

    more pirate stuff for pirates

---
+++ Content.Server/Speech/Components/PirateAccentComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace Content.Server.Speech.Components;
+
+/// <summary>
+///     Nyehh, my gabagool, see?
+///     Etc etc.
+/// </summary>
+[RegisterComponent]
+public sealed class PirateAccentComponent : Component
+{
+    /// <summary>
+    ///     Do you make all the rules?
+    /// </summary>
+    [DataField("isPirate")]
+    public bool IsPirate = true;
+}
