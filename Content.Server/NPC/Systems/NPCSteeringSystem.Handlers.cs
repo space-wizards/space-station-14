1ca8b5c825227919cf1ff1fc94c0995d0365e293
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-09-21 20:31:48 +1000

    TODOS

--- Content.Server/NPC/Systems/NPCSteeringSystem.Handlers.cs
+++ Content.Server/NPC/Systems/NPCSteeringSystem.Handlers.cs
@@ -10,6 +10,21 @@ public sealed partial class NPCSteeringSystem
      * For any custom path handlers, e.g. destroying walls, opening airlocks, etc.
      */
 
+
+    /*
+     * TODO:
+     * - Add "break obstacles to target" below the melee combat one. Make melee combat one require clear path
+     * - Add path cap
+     * - Circle cast BFS in LOS to determine targets.
+     * - Store last known coordinates of X targets.
+     * - Require line of sight for melee
+     * - Add new behavior where they move to melee target's last known position (diffing theirs and current)
+     *  then do the thing like from dishonored where it gets passed to a search system that opens random stuff.
+     *
+     * Also need to make sure it picks nearest obstacle path so it starts smashing in front of it.
+     */
+
+
     private bool TryHandleFlags(NPCSteeringComponent component, PathPoly poly, PhysicsComponent? body = null)
     {
         if (component.Flags == PathFlags.None)
