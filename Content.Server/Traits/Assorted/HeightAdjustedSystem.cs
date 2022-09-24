b026859783a8ddc13a728dda787b00daa51f3d9a
moonheart08 <moonheart08@users.noreply.github.com>
2022-09-24 00:58:23 -0500

    Taller and Smaller traits.

---
+++ Content.Server/Traits/Assorted/HeightAdjustedSystem.cs
@@ -0,0 +1,27 @@
+﻿using Robust.Server.GameObjects;
+
+namespace Content.Server.Traits.Assorted;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class HeightAdjustedSystem : EntitySystem
+{
+    [Dependency] private readonly AppearanceSystem _appearance = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<HeightAdjustedComponent, ComponentStartup>(SetupHeight);
+    }
+
+    private void SetupHeight(EntityUid uid, HeightAdjustedComponent component, ComponentStartup args)
+    {
+        EnsureComp<ScaleVisualsComponent>(uid);
+        EnsureComp<ServerAppearanceComponent>(uid);
+        if (!_appearance.TryGetData(uid, ScaleVisuals.Scale, out var oldScale))
+            oldScale = Vector2.One;
+
+        _appearance.SetData(uid, ScaleVisuals.Scale, (Vector2)oldScale * new Vector2(component.Height, 1.0f));
+    }
+}
