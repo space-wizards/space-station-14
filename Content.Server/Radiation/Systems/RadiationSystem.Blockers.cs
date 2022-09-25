5dd13ecf0780466a0c268a5a6dc018243fdc8d10
Alexander Evgrashin <aevgrashin@yandex.ru>
2022-09-25 23:19:25 +0200

    Remove null check

--- Content.Server/Radiation/Systems/RadiationSystem.Blockers.cs
+++ Content.Server/Radiation/Systems/RadiationSystem.Blockers.cs
@@ -102,7 +102,7 @@ public partial class RadiationSystem
 
         // check if it's on a grid
         var trs = Transform(uid);
-        if (!trs.Anchored || trs.GridUid == null || !TryComp(trs.GridUid, out IMapGridComponent? grid))
+        if (!trs.Anchored || !TryComp(trs.GridUid, out IMapGridComponent? grid))
             return;
 
         // save resistance into rad protection grid
