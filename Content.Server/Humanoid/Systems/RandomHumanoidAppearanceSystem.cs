1ef433e647a18936f077bcd1990e243e9ff72136
vulppine <vulppine@gmail.com>
2022-09-25 12:57:58 -0700

    fixes default species appearances with no profile, tweaks randomization to no longer randomize species

--- Content.Server/Humanoid/Systems/RandomHumanoidAppearanceSystem.cs
+++ Content.Server/Humanoid/Systems/RandomHumanoidAppearanceSystem.cs
@@ -17,19 +17,24 @@ public sealed class RandomHumanoidAppearanceSystem : EntitySystem
     private void OnMapInit(EntityUid uid, RandomHumanoidAppearanceComponent component, MapInitEvent args)
     {
         // If we have an initial profile/base layer set, do not randomize this humanoid.
-        if (TryComp(uid, out HumanoidComponent? humanoid) && !string.IsNullOrEmpty(humanoid.Initial))
+        if (!TryComp(uid, out HumanoidComponent? humanoid) || !string.IsNullOrEmpty(humanoid.Initial))
         {
             return;
         }
 
-        var profile = HumanoidCharacterProfile.Random(component.IgnoredSpecies);
+        var profile = HumanoidCharacterProfile.RandomWithSpecies(humanoid.Species);
 
-        _humanoid.LoadProfile(uid, profile);
+        _humanoid.LoadProfile(uid, profile, humanoid);
 
         if (component.RandomizeName)
         {
             var meta = MetaData(uid);
             meta.EntityName = profile.Name;
         }
+    }
+
+    private void A(string a, string a2 = "2", string b = "somethng")
+    {
+        A("test", b: "something else");
     }
 }
