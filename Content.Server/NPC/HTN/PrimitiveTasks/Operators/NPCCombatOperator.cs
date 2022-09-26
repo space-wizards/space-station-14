63ace034455d69b43f838f6bc114428d380f9231
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-09-26 15:14:23 +1000

    Require LOS for melee

--- Content.Server/NPC/HTN/PrimitiveTasks/Operators/NPCCombatOperator.cs
+++ Content.Server/NPC/HTN/PrimitiveTasks/Operators/NPCCombatOperator.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Content.Server.Interaction;
 using Content.Server.NPC.Pathfinding;
 using Content.Server.NPC.Systems;
+using Content.Shared.Examine;
 using Content.Shared.Interaction;
 using Content.Shared.MobState;
 using Content.Shared.MobState.Components;
@@ -13,9 +14,9 @@ namespace Content.Server.NPC.HTN.PrimitiveTasks.Operators;
 public abstract class NPCCombatOperator : HTNOperator
 {
     [Dependency] protected readonly IEntityManager EntManager = default!;
-    private FactionSystem _tags = default!;
+    private FactionSystem _factions = default!;
     protected InteractionSystem Interaction = default!;
-    protected PathfindingSystem Pathfinding = default!;
+    private PathfindingSystem _pathfinding = default!;
 
     [ViewVariables, DataField("key")] public string Key = "CombatTarget";
 
@@ -25,14 +26,19 @@ public abstract class NPCCombatOperator : HTNOperator
     [ViewVariables, DataField("keyCoordinates")]
     public string KeyCoordinates = "CombatTargetCoordinates";
 
+    /// <summary>
+    /// Regardless of pathfinding or LOS these are the max we'll check
+    /// </summary>
+    private const int MaxConsideredTargets = 10;
     private const int MaxTargetCount = 5;
 
     public override void Initialize(IEntitySystemManager sysManager)
     {
         base.Initialize(sysManager);
-        _tags = sysManager.GetEntitySystem<FactionSystem>();
+        sysManager.GetEntitySystem<ExamineSystemShared>();
+        _factions = sysManager.GetEntitySystem<FactionSystem>();
         Interaction = sysManager.GetEntitySystem<InteractionSystem>();
-        Pathfinding = sysManager.GetEntitySystem<PathfindingSystem>();
+        _pathfinding = sysManager.GetEntitySystem<PathfindingSystem>();
     }
 
     public override async Task<(bool Valid, Dictionary<string, object>? Effects)> Plan(NPCBlackboard blackboard)
@@ -69,19 +75,40 @@ public abstract class NPCCombatOperator : HTNOperator
         var cancelToken = new CancellationTokenSource();
         var count = 0;
 
+        if (xformQuery.TryGetComponent(existingTarget, out var targetXform))
+        {
+            var distance = await _pathfinding.GetPathDistance(owner, targetXform.Coordinates,
+                SharedInteractionSystem.InteractionRange, cancelToken.Token);
+
+            if (distance != null)
+            {
+                targets.Add((existingTarget, GetRating(blackboard, existingTarget, existingTarget, distance.Value, canMove, xformQuery), distance.Value));
+            }
+        }
+
         // TODO: Need a perception system instead
-        foreach (var target in _tags
+        // TODO: This will be expensive so will be good to optimise and cut corners.
+        foreach (var target in _factions
                      .GetNearbyHostiles(owner, radius))
         {
             if (mobQuery.TryGetComponent(target, out var mobState) &&
                 mobState.CurrentState > DamageState.Alive ||
-                !xformQuery.TryGetComponent(target, out var targetXform))
+                !xformQuery.TryGetComponent(target, out targetXform))
             {
                 continue;
             }
 
             count++;
-            var distance = await Pathfinding.GetPathDistance(owner, targetXform.Coordinates,
+
+            if (count >= MaxConsideredTargets)
+                break;
+
+            if (!ExamineSystemShared.InRangeUnOccluded(owner, target, radius, null))
+            {
+                continue;
+            }
+
+            var distance = await _pathfinding.GetPathDistance(owner, targetXform.Coordinates,
                 SharedInteractionSystem.InteractionRange, cancelToken.Token);
 
             if (distance == null)
@@ -89,7 +116,7 @@ public abstract class NPCCombatOperator : HTNOperator
 
             targets.Add((target, GetRating(blackboard, target, existingTarget, distance.Value, canMove, xformQuery), distance.Value));
 
-            if (count >= MaxTargetCount)
+            if (targets.Count >= MaxTargetCount)
                 break;
         }
 
