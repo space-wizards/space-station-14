0961689f564279bb639a370ffa124236e5171d20
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-09-28 00:28:34 +1000

    Fix tests

--- Content.Server/NPC/HTN/PrimitiveTasks/Operators/Ranged/PickRangedTargetOperator.cs
+++ Content.Server/NPC/HTN/PrimitiveTasks/Operators/Ranged/PickRangedTargetOperator.cs
@@ -5,24 +5,14 @@ namespace Content.Server.NPC.HTN.PrimitiveTasks.Operators.Ranged;
 /// <summary>
 /// Selects a target for ranged combat.
 /// </summary>
-[MeansImplicitUse]
+[UsedImplicitly]
 public sealed class PickRangedTargetOperator : NPCCombatOperator
 {
     protected override float GetRating(NPCBlackboard blackboard, EntityUid uid, EntityUid existingTarget, float distance, bool canMove, EntityQuery<TransformComponent> xformQuery)
     {
-        // TODO: Uhh make this better with penetration or something.
-        var inLOS = Interaction.InRangeUnobstructed(blackboard.GetValue<EntityUid>(NPCBlackboard.Owner),
-            uid, distance + 0.1f);
-
-        if (!canMove && !inLOS)
-            return -1f;
-
         // Yeah look I just came up with values that seemed okay but they will need a lot of tweaking.
         // Having a debug overlay just to project these would be very useful when finetuning in future.
         var rating = 0f;
-
-        if (inLOS)
-            rating += 4f;
 
         if (existingTarget == uid)
         {
