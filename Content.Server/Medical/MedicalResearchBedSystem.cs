0cf240bc3520bca8875da2fa11e5d4435c345397
AWF <you@example.com>
2022-09-28 22:52:43 +0800

    Update: made medical research power dependent, can now track health healed on bed for buckled entity

--- Content.Server/Medical/MedicalResearchBedSystem.cs
+++ Content.Server/Medical/MedicalResearchBedSystem.cs
@@ -3,6 +3,8 @@ using Content.Server.DoAfter;
 using Content.Server.Medical.Components;
 using Content.Server.Buckle.Components;
 using Content.Server.Chemistry.Components.SolutionManager;
+using Content.Server.Station.Systems;
+using Content.Server.Power.EntitySystems;
 using Content.Shared.Buckle.Components;
 using Content.Shared.Damage;
 using Content.Shared.IdentityManagement;
@@ -23,38 +25,105 @@ namespace Content.Server.Medical
             base.Initialize();
             SubscribeLocalEvent<MedicalResearchBedComponent, ActivateInWorldEvent>(HandleActivateEvent);
             SubscribeLocalEvent<MedicalResearchBedComponent, GetVerbsEvent<ActivationVerb>>(AddOpenUiVerb);
+            SubscribeLocalEvent<MedicalResearchBedComponent, BuckleChangeEvent>(OnBuckleChange);
         }
 
+        private void OnBuckleChange(EntityUid uid, MedicalResearchBedComponent medicalResearchBed, BuckleChangeEvent args)
+        {
+            var entities = IoCManager.Resolve<IEntityManager>();
+            if (entities.TryGetComponent<MedicalResearchBedServerComponent>(uid, out var server))
+                server.bedChange = true;
+
+        }
+
+        //probably should just be one function...
         private void HandleActivateEvent(EntityUid uid, MedicalResearchBedComponent medicalResearchBed, ActivateInWorldEvent args)
         {
-            OpenUserInterface(args.User, medicalResearchBed);
+            if (!this.IsPowered(uid, EntityManager))
+                return;
 
-            //TODO check power
+            var entities = IoCManager.Resolve<IEntityManager>();
+            OpenUserInterface(args.User, medicalResearchBed);
 
             var strap = Comp<StrapComponent>(args.Target);
             foreach (var buckledEntity in strap.BuckledEntities)
             {
                 if (TryComp<SolutionContainerManagerComponent>(buckledEntity, out var solutions))
-                    medicalResearchBed.UserInterface?.SendMessage(new MedicalResearchBedScannedUserMessage(buckledEntity, solutions.Solutions["chemicals"].Contents));
-            }         
+                {
+                    if (entities.TryGetComponent<DamageableComponent>(buckledEntity, out var damageable))
+                    {
+                        if (entities.TryGetComponent<MedicalResearchBedServerComponent>(args.Target, out var server))
+                        {
+                           
+                            if (server.bedChange)
+                            {
+                                server.bedChange = false;
+                                server.lastHealthRecording = damageable.TotalDamage;
+                            }
+                            else
+                            {
+                                if (damageable.TotalDamage < server.lastHealthRecording)
+                                {
+                                    server.healthChanges += server.lastHealthRecording - damageable.TotalDamage;
+                                    server.lastHealthRecording = damageable.TotalDamage;
+                                } else
+                                {
+                                    server.lastHealthRecording = damageable.TotalDamage;
+                                }
+                            }
+
+                            Console.WriteLine(server.healthChanges);
+                            medicalResearchBed.UserInterface?.SendMessage(new MedicalResearchBedScannedUserMessage(buckledEntity, solutions.Solutions["chemicals"].Contents));
+                        }
+
+                       
+                    }
+                }
+            }
         }
 
         private void HandleActivateVerb(EntityUid uid, MedicalResearchBedComponent medicalResearchBed, GetVerbsEvent<ActivationVerb> args)
         {
-            OpenUserInterface(args.User, medicalResearchBed);
+            if (!this.IsPowered(uid, EntityManager))
+                return;
 
-            //TODO check power
+            var entities = IoCManager.Resolve<IEntityManager>();
+            OpenUserInterface(args.User, medicalResearchBed);
 
             var strap = Comp<StrapComponent>(args.Target);
             foreach (var buckledEntity in strap.BuckledEntities)
             {
-                //Console.WriteLine(buckledEntity);
                 if (TryComp<SolutionContainerManagerComponent>(buckledEntity, out var solutions))
                 {
+                    if (entities.TryGetComponent<DamageableComponent>(buckledEntity, out var damageable))
+                    {
+                        if (entities.TryGetComponent<MedicalResearchBedServerComponent>(args.Target, out var server))
+                        {
+                            
+                            if (server.bedChange)
+                                {
+                                    server.bedChange = false;
+                                    server.lastHealthRecording = damageable.TotalDamage;
+                                }
+                                else
+                                {
+                                    if (damageable.TotalDamage < server.lastHealthRecording)
+                                    {
+                                        server.healthChanges += server.lastHealthRecording - damageable.TotalDamage;
+                                        server.lastHealthRecording = damageable.TotalDamage;
+                                    } else
+                                    {
+                                        server.lastHealthRecording = damageable.TotalDamage;
+                                    }   
+                                }
+                            
+                            Console.WriteLine(server.healthChanges);
+                            medicalResearchBed.UserInterface?.SendMessage(new MedicalResearchBedScannedUserMessage(buckledEntity, solutions.Solutions["chemicals"].Contents));
+                        }
 
-                    medicalResearchBed.UserInterface?.SendMessage(new MedicalResearchBedScannedUserMessage(buckledEntity, solutions.Solutions["chemicals"].Contents));
+                        
+                    }
                 }
-                    
             }
         }
 
@@ -68,10 +137,12 @@ namespace Content.Server.Medical
 
         private void AddOpenUiVerb(EntityUid uid, MedicalResearchBedComponent component, GetVerbsEvent<ActivationVerb> args)
         {
+            if (!this.IsPowered(uid, EntityManager))
+                return;
 
             ActivationVerb verb = new();
             verb.Act = () => HandleActivateVerb(uid, component, args);
-            verb.Text = "Toggle UI";
+            verb.Text = "Open UI";
             args.Verbs.Add(verb);
         }
 
