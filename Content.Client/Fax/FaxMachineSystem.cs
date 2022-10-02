fd4ecd06d2d6155d0c55989877506933025823e4
Morbo <14136326+Morb0@users.noreply.github.com>
2022-10-02 15:27:41 +0300

    Use switch for client visuals state

--- Content.Client/Fax/FaxMachineSystem.cs
+++ Content.Client/Fax/FaxMachineSystem.cs
@@ -21,11 +21,12 @@ public sealed class FaxMachineSystem : VisualizerSystem<FaxMachineVisualsCompone
         if (args.Component.TryGetData(FaxMachineVisuals.BaseState, out FaxMachineVisualState baseState) &&
             args.Sprite.LayerMapTryGet(FaxMachineVisualLayers.Base, out var baseLayer))
         {
-            string state = component.NormalState;
-            if (baseState == FaxMachineVisualState.Inserting)
-                state = component.InsertingState;
-            if (baseState == FaxMachineVisualState.Printing)
-                state = component.PrintState;
+            var state = baseState switch
+            {
+                FaxMachineVisualState.Inserting => component.InsertingState,
+                FaxMachineVisualState.Printing => component.PrintState,
+                _ => component.NormalState,
+            };
 
             args.Sprite.LayerSetAnimationTime(baseLayer, 0f);
             args.Sprite.LayerSetState(baseLayer, state);
