382c964a43606caf7a884d50735559ac483141ec
Kevin Zheng <kevinz5000@gmail.com>
2022-10-04 14:11:37 -0700

    Add comment

--- Content.Server/Power/Components/MaintenanceOilComponent.cs
+++ Content.Server/Power/Components/MaintenanceOilComponent.cs
@@ -1,5 +1,11 @@
 namespace Content.Server.Power.Components;
 
+/// <summary>
+/// Add this to a substation so that it requires maintenance (by replenishing
+/// with oil). Also needs a SolutionContainerManager, a
+/// BatteryDischargerComponent, and a DamagableComponent in order for this to
+/// actually do anything.
+/// </summary>
 [RegisterComponent]
 public sealed class MaintenanceOilComponent : Component
 {
