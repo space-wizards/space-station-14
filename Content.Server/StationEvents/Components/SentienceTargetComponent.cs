701eb2cd9c5fddcc5cc1f6fd3b7799f9624e3ba1
Morbo <14136326+Morb0@users.noreply.github.com>
2022-10-06 11:23:03 +0300

    Localize sentience event target flavors

--- Content.Server/StationEvents/Components/SentienceTargetComponent.cs
+++ Content.Server/StationEvents/Components/SentienceTargetComponent.cs
@@ -3,6 +3,12 @@
 [RegisterComponent]
 public sealed class SentienceTargetComponent : Component
 {
+    private string _flavorKind = string.Empty;
+    
     [DataField("flavorKind", required: true)]
-    public string FlavorKind = default!;
+    public string FlavorKind
+    {
+        get => _flavorKind;
+        private set => _flavorKind = Loc.GetString(value);
+    }
 }
