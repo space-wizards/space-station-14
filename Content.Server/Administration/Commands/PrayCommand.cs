cb56a4a3cbf888417e63f792b789301633575cf9
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-07 12:45:51 +0800

    Revert "praying + cleanup"
    This reverts commit e8ee90f9f0be9a2eeb4d660359f0913c9e82aba3.

--- Content.Server/Administration/Commands/PrayCommand.cs
+++ Content.Server/Administration/Commands/PrayCommand.cs
@@ -1,28 +1,18 @@
-using Content.Server.Administration.Systems;
+using Content.Server.Body.Components;
 using Content.Shared.Administration;
 using Robust.Shared.Console;
 
 namespace Content.Server.Administration.Commands
 {
-    [AdminCommand(AdminFlags.None)]
     public sealed class PrayCommand : IConsoleCommand
     {
-
         public string Command => "pray";
         public string Description => Loc.GetString("prayer-command-description");
         public string Help => Loc.GetString("prayer-command-help");
 
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
-            // TODO: Actually make this work
-            var prayerSystem = IoCManager.Resolve<PrayerSystem>();
-
-            if (shell.Player == null)
-            {
-                shell.WriteLine("You have no player, are you running this on the server?");
-                return;
-            }
-            prayerSystem.Pray(shell.Player, argStr);
+            Logger.Debug(argStr);
         }
     }
 }
