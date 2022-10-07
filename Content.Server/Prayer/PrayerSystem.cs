cbcea5d134b38ea932a70ea31944525428b2c8e8
eclips_e <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2022-10-07 15:16:40 +0800

    slight fixes

--- Content.Server/Prayer/PrayerSystem.cs
+++ Content.Server/Prayer/PrayerSystem.cs
@@ -25,12 +25,12 @@ namespace Content.Server.Prayer
         {
             base.Initialize();
 
-            // error is here
             SubscribeLocalEvent<PrayableComponent, GetVerbsEvent<ActivationVerb>>(AddPrayVerb);
         }
 
         private void AddPrayVerb(EntityUid uid, PrayableComponent comp, GetVerbsEvent<ActivationVerb> args)
         {
+            // if it doesn't have an actor and we can't reach it then don't add the verb
             if (!EntityManager.TryGetComponent<ActorComponent?>(args.User, out var actor))
                 return;
             if (!args.CanAccess)
@@ -41,13 +41,14 @@ namespace Content.Server.Prayer
             prayerVerb.IconTexture = "/Textures/Interface/pray.svg.png";
             prayerVerb.Act = () =>
             {
+                if (comp.BibleUserOnly && !EntityManager.TryGetComponent<BibleUserComponent>(args.User, out var bibleUser))
+                {
+                    _popupSystem.PopupEntity(Loc.GetString("prayer-popup-notify-locked"), uid, Filter.Empty().AddPlayer(actor.PlayerSession), PopupType.Large);
+                    return;
+                }
+                
                 _quickDialog.OpenDialog(actor.PlayerSession, "Pray", "Message", (string message) =>
                 {
-                    if (comp.BibleUserOnly && !EntityManager.TryGetComponent<BibleUserComponent>(args.User, out var bibleUser))
-                    {
-                        _popupSystem.PopupEntity(Loc.GetString("prayer-popup-notify-locked"), uid, Filter.Empty().AddPlayer(actor.PlayerSession), PopupType.Large);
-                        return;
-                    }
                     Pray(actor.PlayerSession, message);
                 });
             };
@@ -65,6 +66,7 @@ namespace Content.Server.Prayer
         {
             if (target.AttachedEntity == null)
                 return;
+            
             if (popupMessage == String.Empty)
                 popupMessage = Loc.GetString("prayer-popup-subtle-default");
             _popupSystem.PopupEntity(popupMessage, target.AttachedEntity.Value, Filter.Empty().AddPlayer(target), PopupType.Large);
