aff00e94adcac01ecdd7820b2be8cde2a2c2885b
Morbo <14136326+Morb0@users.noreply.github.com>
2022-10-08 15:33:32 +0300

    Localize in event start

--- Content.Server/StationEvents/Components/SentienceTargetComponent.cs
+++ Content.Server/StationEvents/Components/SentienceTargetComponent.cs
@@ -3,12 +3,6 @@
 [RegisterComponent]
 public sealed class SentienceTargetComponent : Component
 {
-    private string _flavorKind = string.Empty;
-    
     [DataField("flavorKind", required: true)]
-    public string FlavorKind
-    {
-        get => _flavorKind;
-        private set => _flavorKind = Loc.GetString(value);
-    }
+    public string FlavorKind = default!;
 }
