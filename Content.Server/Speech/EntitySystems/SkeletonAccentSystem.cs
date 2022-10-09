a170a15c005237c6912b6f663c59e4a291283b37
Willhelm53 <wmerrick@live.com>
2022-10-09 17:26:09 -0500

    We're so sorry Skeletons, you're so misunderstood!

---
+++ Content.Server/Speech/EntitySystems/SkeletonAccentSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+using Content.Server.Speech.Components;
+using Robust.Shared.Random;
+
+namespace Content.Server.Speech.EntitySystems;
+
+public sealed class SkeletonAccentSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    private static readonly Dictionary<string, string> DirectReplacements = new()
+    {
+        { "fuck", "rattle rattle" },
+        { "shit", "rattle rattle" },
+        { "fuck you", "I've got a BONE to pick with you" },
+        { "definitely", "make no bones about it" },
+        { "absolutely", "make no bones about it" },
+    };
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<SkeletonAccentComponent, AccentGetEvent>(OnAccentGet);
+    }
+
+    public string Accentuate(string message, SkeletonAccentComponent component)
+    {
+        // Order:
+        // Do replacements first
+        // Then prefix/suffix
+
+        var msg = message;
+
+        foreach (var (first, replace) in DirectReplacements)
+        {
+            msg = Regex.Replace(msg, $@"(?<!\w){first}(?!\w)", replace, RegexOptions.IgnoreCase);
+        }
+
+        // Suffix
+        if (_random.Prob(component.ackChance))
+        {
+            msg += " ACK ACK!";
+        }
+        return msg;
+    }
+
+    private void OnAccentGet(EntityUid uid, SkeletonAccentComponent component, AccentGetEvent args)
+    {
+        args.Message = Accentuate(args.Message, component);
+    }
+}
