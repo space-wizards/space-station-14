ee7580a37143b4e1763d0897945b3a337b54ba66
EmoGarbage404 <retron404@gmail.com>
2022-10-09 20:20:05 -0400

    future proof this one teeny thing

--- Content.Server/Bed/BedSystem.cs
+++ Content.Server/Bed/BedSystem.cs
@@ -138,6 +138,8 @@ namespace Content.Server.Bed
         {
             var metabolismRating = args.PartRatings[component.MachinePartMetabolismModifier];
             component.Multiplier = component.BaseMultiplier * metabolismRating; //linear scaling so it's not OP
+            if (component.Emagged)
+                component.Multiplier = 1f / component.Multiplier;
         }
     }
 }
