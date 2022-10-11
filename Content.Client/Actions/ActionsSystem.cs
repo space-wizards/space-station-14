8b77ff2f831cc4fca8ce4747a39c0f2c9da58591
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2022-10-11 20:59:37 +0200

    Clone new actions from the server

--- Content.Client/Actions/ActionsSystem.cs
+++ Content.Client/Actions/ActionsSystem.cs
@@ -60,7 +60,7 @@ namespace Content.Client.Actions
             {
                 if (!component.Actions.TryGetValue(serverAction, out var clientAction))
                 {
-                    added.Add(serverAction);
+                    added.Add((ActionType) serverAction.Clone());
                 }
                 else
                 {
