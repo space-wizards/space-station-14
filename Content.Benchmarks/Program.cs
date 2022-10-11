945f6e75efaa877ef2aca066fa122be362423a56
Paul <ritter.paul1@googlemail.com>
2022-10-11 20:28:57 +0200

    argh

--- Content.Benchmarks/Program.cs
+++ Content.Benchmarks/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using Content.IntegrationTests;
 using Content.Server.Maps;
