9a3940a2efde933160746c1b33ede6a90875d343
Ygg01 <y.laughing.man.y@gmail.com>
2022-10-13 01:18:26 +0200

    Adds fluid overlay

---
+++ Content.Client/Fluids/PuddleDebugOverlaySystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using Content.Shared.Fluids;
+using Robust.Client.Graphics;
+
+namespace Content.Client.Fluids;
+
+public sealed class PuddleDebugOverlaySystem : SharedPuddleDebugOverlaySystem
+{
+    [Dependency] private readonly IOverlayManager _overlayManager = default!;
+
+    public readonly Dictionary<EntityUid, PuddleOverlayDebugMessage> TileData = new();
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeNetworkEvent<PuddleOverlayDisableMessage>(DisableOverlay);
+        SubscribeNetworkEvent<PuddleOverlayDebugMessage>(RenderDebugData);
+
+        if (!_overlayManager.HasOverlay<PuddleOverlay>())
+        {
+            _overlayManager.AddOverlay(new PuddleOverlay());
+        }
+    }
+
+    private void RenderDebugData(PuddleOverlayDebugMessage message)
+    {
+        TileData[message.GridUid] = message;
+    }
+
+    private void DisableOverlay(PuddleOverlayDisableMessage message)
+    {
+        TileData.Clear();
+    }
+
+    public bool HasData(EntityUid gridId)
+    {
+        return TileData.ContainsKey(gridId);
+    }
+
+    public PuddleDebugOverlayData[] GetData(EntityUid mapGridGridEntityId)
+    {
+        return TileData[mapGridGridEntityId].OverlayData;
+    }
+}
