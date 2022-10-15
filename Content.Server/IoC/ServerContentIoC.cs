288eac4c0b642fd1aa933f2abf21862e17fd8f5e
Kevin Zheng <kevinz5000@gmail.com>
2022-10-15 15:13:11 -0700

    Drop unused namespace

--- Content.Server/IoC/ServerContentIoC.cs
+++ Content.Server/IoC/ServerContentIoC.cs
@@ -3,7 +3,6 @@ using Content.Server.Administration.Logs;
 using Content.Server.Administration.Managers;
 using Content.Server.Administration.Notes;
 using Content.Server.Afk;
-using Content.Server.Botany;
 using Content.Server.Chat.Managers;
 using Content.Server.Connection;
 using Content.Server.Database;
