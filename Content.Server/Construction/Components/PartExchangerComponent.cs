f0ca67bd4c00f90d79be1bdce62ce6d14351d17b
EmoGarbage404 <retron404@gmail.com>
2022-10-17 19:26:32 -0400

    RPED

---
+++ Content.Server/Construction/Components/PartExchangerComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+
+namespace Content.Server.Construction.Components;
+
+[RegisterComponent]
+public sealed class PartExchangerComponent : Component
+{
+    /// <summary>
+    /// How long it takes to exchange the parts
+    /// </summary>
+    [DataField("exchangeDuration")]
+    public float ExchangeDuration = 3;
+
+    /// <summary>
+    /// Whether or not the distance check is needed.
+    /// Good for BRPED.
+    /// </summary>
+    /// <remarks>
+    /// I fucking hate BRPED and if you ever add it
+    /// i will personally kill your dog.
+    /// </remarks>
+    [DataField("doDistanceCheck")]
+    public bool DoDistanceCheck = true;
+
+    public CancellationTokenSource? Token;
+}
