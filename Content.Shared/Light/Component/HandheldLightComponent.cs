756891a3e2814ded3141292d0428c05ba77d06bb
Carbonhell <frafonia@gmail.com>
2022-10-19 12:13:20 +0200

    refactor: blinkingBehaviourID -> blinkingBehaviourId, radiatingBehaviourID -> radiatingBehaviourId

--- Content.Shared/Light/Component/HandheldLightComponent.cs
+++ Content.Shared/Light/Component/HandheldLightComponent.cs
@@ -49,14 +49,14 @@ namespace Content.Shared.Light
         /// Specify the ID of the light behaviour to use when the state of the light is Dying
         /// </summary>
         [ViewVariables]
-        [DataField("blinkingBehaviourID")]
+        [DataField("blinkingBehaviourId")]
         public string BlinkingBehaviourId { get; set; } = string.Empty;
 
         /// <summary>
         /// Specify the ID of the light behaviour to use when the state of the light is LowPower
         /// </summary>
         [ViewVariables]
-        [DataField("radiatingBehaviourID")]
+        [DataField("radiatingBehaviourId")]
         public string RadiatingBehaviourId { get; set; } = string.Empty;
 
         [Serializable, NetSerializable]
