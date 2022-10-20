7293663fa43d48ad5d10cca5f0aba74868fc5e2f
Alexander Evgrashin <aevgrashin@yandex.ru>
2022-10-20 23:18:00 +0200

    Added a lot of doc

--- Content.Server/Radiation/Components/GeigerComponent.cs
+++ Content.Server/Radiation/Components/GeigerComponent.cs
@@ -3,10 +3,14 @@ using Robust.Shared.Audio;
 
 namespace Content.Server.Radiation.Components;
 
+/// <inheritdoc/>
 [RegisterComponent]
 [ComponentReference(typeof(SharedGeigerComponent))]
 public sealed class GeigerComponent : SharedGeigerComponent
 {
+    /// <summary>
+    ///     Map of sounds that should be play on loop for different radiation levels.
+    /// </summary>
     [DataField("sounds")]
     public Dictionary<GeigerDangerLevel, SoundSpecifier> Sounds = new()
     {
@@ -16,11 +20,23 @@ public sealed class GeigerComponent : SharedGeigerComponent
         {GeigerDangerLevel.Extreme, new SoundPathSpecifier("/Audio/Items/Geiger/ext.ogg")}
     };
 
+    /// <summary>
+    ///     If true it will be active only when player equipped it.
+    /// </summary>
     [DataField("attachedToSuit")]
     public bool AttachedToSuit;
 
+    /// <summary>
+    ///     Current player that equipped geiger counter.
+    ///     Because sound is annoying, geiger counter clicks will play
+    ///     only for player that equipped it.
+    /// </summary>
     [ViewVariables(VVAccess.ReadOnly)]
     public EntityUid? User;
 
+    /// <summary>
+    ///     Current stream of geiger counter audio.
+    ///     Played only for current user.
+    /// </summary>
     public IPlayingAudioStream? Stream;
 }
--- Content.Shared/Radiation/Components/SharedGeigerComponent.cs
+++ Content.Shared/Radiation/Components/SharedGeigerComponent.cs
@@ -1,21 +1,40 @@
+using Content.Shared.Radiation.Systems;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.Radiation.Components;
 
+/// <summary>
+///     Geiger counter that shows current radiation level.
+///     Can be added as a component to clothes.
+/// </summary>
 [NetworkedComponent]
+[Access(typeof(SharedGeigerSystem))]
 public abstract class SharedGeigerComponent : Component
 {
+    /// <summary>
+    ///     Is geiger counter currently active?
+    ///     If false attached entity will ignore any radiation rays.
+    /// </summary>
     [DataField("isEnabled")]
     public bool IsEnabled;
 
+    /// <summary>
+    ///     Should it shows examine message with current radiation level?
+    /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("showExamine")]
     public bool ShowExamine;
 
+    /// <summary>
+    ///     Current radiation level in rad per second.
+    /// </summary>
     [ViewVariables(VVAccess.ReadOnly)]
     public float CurrentRadiation;
 
+    /// <summary>
+    ///     Estimated radiation danger level.
+    /// </summary>
     [ViewVariables(VVAccess.ReadOnly)]
     public GeigerDangerLevel DangerLevel = GeigerDangerLevel.None;
 }
