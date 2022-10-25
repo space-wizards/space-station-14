478e4d106bb9569364deb254de8f17f7c44cd621
keronshb <keronshb@live.com>
2022-10-25 17:26:02 -0400

    Adds to uplink, adjusts stealth

--- Content.Shared/SimpleMob/SimpleMobComponent.cs
+++ Content.Shared/SimpleMob/SimpleMobComponent.cs
@@ -1,4 +1,7 @@
 ﻿namespace Content.Shared.SimpleMob;
+/// <summary>
+/// Use this where you want simplemobs to be able to bypass some checks like open containers
+/// </summary>
 [RegisterComponent]
 public sealed class SimpleMobComponent : Component
 {
