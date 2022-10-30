55f21d5987d4d2bcd630c9e986dafa1aa8340540
EmoGarbage404 <retron404@gmail.com>
2022-10-29 21:57:49 -0400

    some changes

---
+++ Content.Server/Xenoarchaeology/Equipment/Components/ArtifactAnalyzerConsoleComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Server.Xenoarchaeology.Equipment.Components;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed class ArtifactAnalyzerConsoleComponent : Component
+{
+    
+}
