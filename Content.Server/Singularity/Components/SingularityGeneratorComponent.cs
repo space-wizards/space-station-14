7966c26ad2a8780e46fe620df080254e6f2e07a3
TemporalOroboros <TemporalOroboros@gmail.com>
2022-10-29 23:02:44 -0700

    Testing containment and fake VV setters for SingularityGeneratorComponent

--- Content.Server/Singularity/Components/SingularityGeneratorComponent.cs
+++ Content.Server/Singularity/Components/SingularityGeneratorComponent.cs
@@ -5,28 +5,21 @@ namespace Content.Server.Singularity.Components;
 [RegisterComponent]
 public sealed class SingularityGeneratorComponent : Component
 {
-    [Dependency] private readonly IEntityManager _entMan = default!;
-
     /// <summary>
-    ///     The amount of power this generator has accumulated.
+    /// The amount of power this generator has accumulated.
+    /// If you want to set this use <see  cref="SingularityGeneratorSystem.SetPower"/>
     /// </summary>
-    [Access(friends:typeof(SingularityGeneratorSystem))]
-    public int _power;
-
     [DataField("power")]
-    [ViewVariables(VVAccess.ReadWrite)]
-    public int Power
-    {
-        get => _power;
-        set { IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<SingularityGeneratorSystem>().SetPower(this, value); }
-    }
+    [Access(friends:typeof(SingularityGeneratorSystem), Self=AccessPermissions.Read, Other=AccessPermissions.Read)]
+    public float Power = 0;
 
     /// <summary>
-    ///     The power threshold at which this generator will spawn a singularity.
+    /// The power threshold at which this generator will spawn a singularity.
+    /// If you want to set this use <see  cref="SingularityGeneratorSystem.SetThreshold"/>
     /// </summary>
     [DataField("threshold")]
-    [ViewVariables(VVAccess.ReadWrite)]
-    public int Threshold = 16;
+    [Access(friends:typeof(SingularityGeneratorSystem), Self=AccessPermissions.Read, Other=AccessPermissions.Read)]
+    public float Threshold = 16;
 
     /// <summary>
     ///     The prototype ID used to spawn a singularity.
