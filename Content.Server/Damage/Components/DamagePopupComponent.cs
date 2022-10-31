412a793b9e1a8ba88e1c4318a48a1acb65a7ae0e
ZeroDayDaemon <60460608+ZeroDayDaemon@users.noreply.github.com>
2022-10-31 03:47:57 -0400

    Add DamagePopup and Target Entities

---
+++ Content.Server/Damage/Components/DamagePopupComponent.cs
@@ -0,0 +1,14 @@
+﻿using Content.Server.Damage.Systems;
+
+namespace Content.Server.Damage.Components;
+
+[RegisterComponent, Access(typeof(DamagePopupSystem))]
+public sealed class DamagePopupComponent : Component
+{
+    /// <summary>
+    /// String will be used to determine the type of damage popup displayed.
+    /// </summary>
+    [DataField("damagePopupTypeString")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string? DamagePopupTypeString;
+}
---
+++ Content.Server/Damage/Systems/DamagePopupSystem.cs
@@ -0,0 +1,45 @@
+﻿using Content.Server.Damage.Components;
+using Content.Server.Popups;
+using Content.Shared.Damage;
+using Content.Shared.FixedPoint;
+using Robust.Shared.Player;
+
+namespace Content.Server.Damage.Systems;
+
+public sealed class DamagePopupSystem : EntitySystem
+{
+    [Dependency] private readonly PopupSystem _popupSystem = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<DamagePopupComponent, DamageChangedEvent>(OnDamageChange);
+    }
+
+    private void OnDamageChange(EntityUid uid, DamagePopupComponent component, DamageChangedEvent args)
+    {
+        if (args.DamageIncreased && args.DamageDelta != null)
+        {
+            string msg = ""; // Stores the text to be shown in the popup message
+            FixedPoint2 damageTotal = args.Damageable.TotalDamage;
+            FixedPoint2 damageDelta = args.DamageDelta.Total;
+
+            switch(component.DamagePopupTypeString)
+            {
+                case "damageDelta":
+                    msg = damageDelta.ToString();
+                    break;
+                case "damageTotal":
+                    msg = damageTotal.ToString();
+                    break;
+                case "damageCombined":
+                    msg = damageDelta + " | " + damageTotal;
+                    break;
+                default:
+                    msg = "Invalid type";
+                    break;
+            }
+
+            _popupSystem.PopupEntity(msg, uid, Filter.Pvs(uid, 2F, EntityManager));
+        }
+    }
+}
