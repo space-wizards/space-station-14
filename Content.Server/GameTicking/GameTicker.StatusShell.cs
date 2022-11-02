575ea70d0df0d859fddb1c2de5c1a636133ef2f9
Rane <elijahrane@gmail.com>
2022-11-01 21:48:36 -0400

    expose forkID as part of status

--- Content.Server/GameTicking/GameTicker.StatusShell.cs
+++ Content.Server/GameTicking/GameTicker.StatusShell.cs
@@ -34,6 +34,7 @@ namespace Content.Server.GameTicking
             lock (_statusShellLock)
             {
                 jObject["name"] = _baseServer.ServerName;
+                jObject["fork_id"] = _baseServer.ForkID;
                 jObject["players"] = _playerManager.PlayerCount;
                 jObject["soft_max_players"] = _cfg.GetCVar(CCVars.SoftMaxPlayers);
                 jObject["run_level"] = (int) _runLevel;
