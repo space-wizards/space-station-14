ce7a8083b450a1d359bf84091764fea8b201e1c3
Morb <14136326+Morb0@users.noreply.github.com>
2022-11-04 18:12:23 +0300

    Replace Patreon with Boosty (#514)

--- Content.Client/Links/UILinks.cs
+++ Content.Client/Links/UILinks.cs
@@ -3,7 +3,7 @@
     public static class UILinks
     {
         public const string GitHub = "https://github.com/space-syndicate/space-station-14/";
-        public const string Patreon = "https://www.patreon.com/spacestation14";
+        public const string Patreon = "https://boosty.to/corvax";
 
         public const string Discord = "https://discord.com/invite/F9bNgSYfX5";
         public const string BugReport = "https://github.com/space-syndicate/space-station-14/issues/new/choose";
