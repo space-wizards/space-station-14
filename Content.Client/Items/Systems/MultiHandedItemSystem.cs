41d3c005bae4bf60038f9d67f3898a2f5e341781
EmoGarbage404 <retron404@gmail.com>
2022-11-09 21:35:05 -0500

    multi-handed item component

---
+++ Content.Client/Items/Systems/MultiHandedItemSystem.cs
@@ -0,0 +1,7 @@
+﻿using Content.Shared.Item;
+
+namespace Content.Client.Items.Systems;
+
+public sealed class MultiHandedItemSystem : SharedMultiHandedItemSystem
+{
+}
---
+++ Content.Shared/Item/SharedMultiHandedItemSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Content.Shared.Hands;
+using Content.Shared.Hands.Components;
+using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Popups;
+using Robust.Shared.Player;
+
+namespace Content.Shared.Item;
+
+public abstract class SharedMultiHandedItemSystem : EntitySystem
+{
+    [Dependency] private readonly SharedHandsSystem _hands = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<MultiHandedItemComponent, GettingPickedUpAttemptEvent>((e,c,a) => OnAttemptPickup(e,c,a));
+        SubscribeLocalEvent<MultiHandedItemComponent, GotEquippedHandEvent>(OnEquipped);
+        SubscribeLocalEvent<MultiHandedItemComponent, GotUnequippedHandEvent>(OnUnequipped);
+    }
+
+    protected virtual void OnEquipped(EntityUid uid, MultiHandedItemComponent component, GotEquippedHandEvent args)
+    {
+
+    }
+
+    protected virtual void OnUnequipped(EntityUid uid, MultiHandedItemComponent component, GotUnequippedHandEvent args)
+    {
+
+    }
+
+    protected virtual bool OnAttemptPickup(EntityUid uid, MultiHandedItemComponent component, GettingPickedUpAttemptEvent args)
+    {
+        if (!TryComp<SharedHandsComponent>(args.User, out var hands))
+        {
+            args.Cancel();
+            return false;
+        }
+
+        var freeHands = _hands.EnumerateHands(args.User, hands).Where(x => x.IsEmpty);
+        if (freeHands.Count() < component.HandsNeeded)
+        {
+            args.Cancel();
+            return false;
+        }
+
+        return true;
+    }
+}
