0e79714bb12428d1b8657d63de3c3bb4a9d7a54b
Rane <elijahrane@gmail.com>
2022-11-09 23:05:12 -0500

    initial setup

---
+++ Content.Server/Disease/Components/VaccineMachineComponent.cs
@@ -0,0 +1,13 @@
+using Content.Server.Research.Components;
+
+namespace Content.Server.Disease.Components
+{
+    /// <summary>
+    ///     As of now, the disease server and R&D server are one and the same.
+    ///     So, this is a research client that can look for DiseaseServer
+    ///     on its connected server and print vaccines of the diseases stored there.
+    /// </summary>
+    [RegisterComponent]
+    public sealed class VaccineMachineComponent : ResearchClientComponent
+    {}
+}
