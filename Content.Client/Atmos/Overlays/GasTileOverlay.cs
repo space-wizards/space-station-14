ff5cf1694a0a4de8329a48f8c7048176a0194c68
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2022-11-15 21:28:46 +0100

    Remove byte[] allocations for GasTileOverlaySystem.UpdateChunkTile

--- Content.Client/Atmos/Overlays/GasTileOverlay.cs
+++ Content.Client/Atmos/Overlays/GasTileOverlay.cs
@@ -166,16 +166,13 @@ namespace Content.Client.Atmos.Overlays
 
                     while (enumerator.MoveNext(out var gas))
                     {
-                        if (gas.Value.Opacity == null)
-                            continue;
-
                         var tilePosition = chunk.Origin + (enumerator.X, enumerator.Y);
                         if (!localBounds.Contains(tilePosition))
                             continue;
 
                         for (var i = 0; i < _gasCount; i++)
                         {
-                            var opacity = gas.Value.Opacity[i];
+                            var opacity = gas.Opacity[i];
                             if (opacity > 0)
                                 drawHandle.DrawTexture(_frames[i][_frameCounter[i]], tilePosition, Color.White.WithAlpha(opacity));
                         }
@@ -190,14 +187,14 @@ namespace Content.Client.Atmos.Overlays
 
                     while (enumerator.MoveNext(out var gas))
                     {
-                        if (gas.Value.FireState == 0)
+                        if (gas.FireState == 0)
                             continue;
 
                         var index = chunk.Origin + (enumerator.X, enumerator.Y);
                         if (!localBounds.Contains(index))
                             continue;
 
-                        var state = gas.Value.FireState - 1;
+                        var state = gas.FireState - 1;
                         var texture = _fireFrames[state][_fireFrameCounter[state]];
                         drawHandle.DrawTexture(texture, index);
                     }
