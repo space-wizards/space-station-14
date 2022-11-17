4ee6c8e57d3fe8dca0448c86ed75246a5f666cf9
Kara <lunarautomaton6@gmail.com>
2022-11-17 10:05:12 -0600

    Merge `RandomArtifactVisualizer` into `RandomArtifactSpriteSystem/Component`

---
+++ Content.Client/Xenoarchaeology/XenoArtifacts/RandomArtifactSpriteSystem.cs
@@ -0,0 +1,45 @@
+﻿using Content.Shared.Xenoarchaeology.XenoArtifacts;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Xenoarchaeology.XenoArtifacts;
+
+public sealed class RandomArtifactSpriteSystem : VisualizerSystem<RandomArtifactSpriteComponent>
+{
+    protected override void OnAppearanceChange(EntityUid uid, RandomArtifactSpriteComponent component, ref AppearanceChangeEvent args)
+    {
+        if (args.Sprite == null)
+            return;
+
+        if (!AppearanceSystem.TryGetData(uid, SharedArtifactsVisuals.SpriteIndex, out var ind, args.Component)
+            || ind is not int spriteIndex)
+            return;
+
+        if (!AppearanceSystem.TryGetData(uid, SharedArtifactsVisuals.IsActivated, out var act, args.Component)
+            || act is not bool isActivated)
+            isActivated = false;
+
+        var spriteIndexStr = spriteIndex.ToString("D2");
+        var spritePrefix = isActivated ? "_on" : "";
+
+        // layered artifact sprite
+        if (args.Sprite.LayerMapTryGet(SharedArtifactsVisualLayers.Effect, out var layer))
+        {
+            var spriteState = "ano" + spriteIndexStr + "_on";
+            args.Sprite.LayerSetState(layer, spriteState);
+            args.Sprite.LayerSetVisible(layer, isActivated);
+        }
+        // non-layered
+        else
+        {
+            var spriteState = "ano" + spriteIndexStr + spritePrefix;
+            args.Sprite.LayerSetState(SharedArtifactsVisualLayers.Base, spriteState);
+        }
+
+    }
+}
+
+public enum SharedArtifactsVisualLayers
+{
+    Base,
+    Effect // doesn't have to use this
+}
