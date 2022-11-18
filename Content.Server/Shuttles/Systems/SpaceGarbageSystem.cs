855e2537bbd0d02705926f40ac3c9442601fab28
Vordenburg <114301317+Vordenburg@users.noreply.github.com>
2022-11-18 13:43:04 -0500

    Make Entities with SpaceGarbageComponent delay CanCollide until Startup.

--- Content.Server/Shuttles/Systems/SpaceGarbageSystem.cs
+++ Content.Server/Shuttles/Systems/SpaceGarbageSystem.cs
@@ -1,6 +1,8 @@
 using Content.Server.Shuttles.Components;
 using Robust.Shared.Physics;
+using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Events;
+using Robust.Shared.Physics.Systems;
 
 namespace Content.Server.Shuttles.Systems;
 
@@ -9,10 +11,29 @@ namespace Content.Server.Shuttles.Systems;
 /// </summary>
 public sealed class SpaceGarbageSystem : EntitySystem
 {
+    [Dependency] private readonly SharedPhysicsSystem _physicsSystem = default!;
+
     public override void Initialize()
     {
         base.Initialize();
+        SubscribeLocalEvent<SpaceGarbageComponent, ComponentInit>(OnComponentInit);
+        SubscribeLocalEvent<SpaceGarbageComponent, ComponentStartup>(OnComponentStartup);
         SubscribeLocalEvent<SpaceGarbageComponent, StartCollideEvent>(OnCollide);
+    }
+
+    private void OnComponentInit(EntityUid uid, SpaceGarbageComponent component, ComponentInit args)
+    {
+        // StartCollideEvents can accumulate before TransformSystem has had a
+        // chance to adequately assign a proper GridUid, if one is available.
+        // Delay those until everything is ready.
+        if (TryComp(uid, out PhysicsComponent? physicsComponent))
+            _physicsSystem.SetCanCollide(physicsComponent, false);
+    }
+
+    private void OnComponentStartup(EntityUid uid, SpaceGarbageComponent component, ComponentStartup args)
+    {
+        if (TryComp(uid, out PhysicsComponent? physicsComponent))
+            _physicsSystem.SetCanCollide(physicsComponent, true);
     }
 
     private void OnCollide(EntityUid uid, SpaceGarbageComponent component, ref StartCollideEvent args)
