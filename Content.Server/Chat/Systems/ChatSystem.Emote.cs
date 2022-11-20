2479151369074eb03899c8f1744abccd4f712c9d
Alexander Evgrashin <aevgrashin@yandex.ru>
2022-11-20 22:28:51 +0100

    More work

--- Content.Server/Chat/Systems/ChatSystem.Emote.cs
+++ Content.Server/Chat/Systems/ChatSystem.Emote.cs
@@ -23,24 +23,44 @@ public partial class ChatSystem
         CacheEmotes();
     }
 
-    private void CacheEmotes()
+    public void CacheEmotes()
     {
         var emotes = _prototypeManager.EnumeratePrototypes<EmotePrototype>();
         foreach (var emote in emotes)
         {
             foreach (var word in emote.Words)
             {
+                if (_wordEmoteDict.ContainsKey(word))
+                {
+                    var existingId = _wordEmoteDict[word].ID;
+                    Sawmill.Error($"Duplicate of emote word {word} in emotes {emote.ID} and {existingId}");
+                    continue;
+                }
+
                 _wordEmoteDict.Add(word, emote);
             }
         }
     }
 
-    public bool TryEmote(EntityUid source, string action)
+    public bool TryEmote(EntityUid uid, string action)
     {
         var actionLower = action.ToLower();
         if (!_wordEmoteDict.TryGetValue(actionLower, out var emote))
             return false;
 
+        var ev = new EmoteEvent(emote);
+        RaiseLocalEvent(uid, ev);
+
         return true;
+    }
+}
+
+public struct EmoteEvent
+{
+    public readonly EmotePrototype Emote;
+
+    public EmoteEvent(EmotePrototype emote)
+    {
+        Emote = emote;
     }
 }
--- Content.Shared/Chat/SharedChatSystem.cs
+++ Content.Shared/Chat/SharedChatSystem.cs
@@ -1,3 +1,12 @@
 namespace Content.Shared.Chat;
 
-public abstract class SharedChatSystem : EntitySystem {}
+public abstract class SharedChatSystem : EntitySystem
+{
+    protected ISawmill Sawmill = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        Sawmill = Logger.GetSawmill("chat");
+    }
+}
