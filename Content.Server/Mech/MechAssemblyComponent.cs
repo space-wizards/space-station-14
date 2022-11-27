3aeda89588cc6a39e2c9d550aaf2fc66e949bcb1
EmoGarbage404 <retron404@gmail.com>
2022-11-26 21:45:32 -0500

    construction yo mamma

---
+++ Content.Server/Mech/MechAssemblyComponent.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.Tag;
+using Content.Shared.Tools;
+using Robust.Shared.Containers;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Dictionary;
+
+namespace Content.Server.Mech;
+
+[RegisterComponent]
+public sealed class MechAssemblyComponent : Component
+{
+    [DataField("requiredParts", required: true, customTypeSerializer: typeof(PrototypeIdDictionarySerializer<bool, TagPrototype>))]
+    public Dictionary<string, bool> RequiredParts = new();
+
+    [DataField("finishedPrototype", required: true, customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string FinishedPrototype = default!;
+
+    [ViewVariables]
+    public Container PartsContainer = default!;
+
+    [DataField("qualityNeeded", customTypeSerializer: typeof(PrototypeIdSerializer<ToolQualityPrototype>))]
+    public string QualityNeeded = "Prying";
+}
