4844bc409dae41c7836c6aaab943404806946f6a
EmoGarbage404 <retron404@gmail.com>
2022-11-29 22:16:29 -0500

    genericize ui fragments

---
+++ Content.Client/CartridgeLoader/UiFragmentComponent.cs
@@ -0,0 +1,18 @@
+﻿using Content.Client.UserInterface.Fragments;
+
+namespace Content.Client.CartridgeLoader;
+
+/// <summary>
+/// The component used for defining a ui fragment to attach to an entity
+/// </summary>
+/// <remarks>
+/// This is used primarily for PDA cartridges.
+/// </remarks>
+/// <seealso cref="UIFragment"/>
+/// <seealso cref="UIFragmentSerializer"/>
+[RegisterComponent]
+public sealed class UIFragmentComponent : Component
+{
+    [DataField("ui", true, customTypeSerializer: typeof(UIFragmentSerializer))]
+    public UIFragment? Ui;
+}
--- Content.Client/UserInterface/Fragments/UIFragmentSerializer.cs
+++ Content.Client/UserInterface/Fragments/UIFragmentSerializer.cs
@@ -4,39 +4,39 @@ using Robust.Shared.Serialization.Markdown.Validation;
 using Robust.Shared.Serialization.Markdown.Value;
 using Robust.Shared.Serialization.TypeSerializers.Interfaces;
 
-namespace Content.Client.CartridgeLoader;
+namespace Content.Client.UserInterface.Fragments;
 
 /// <summary>
-/// Boilerplate serializer for defining the ui fragment used for a cartridge in yaml
+/// Boilerplate serializer for defining the ui fragment in yaml
 /// </summary>
 /// <example>
-/// This is an example from the yaml definition from the notekeeper ui
+/// This is an example from the yaml definition from the notekeeper cartridge ui
 /// <code>
 /// - type: CartridgeUi
 ///     ui: !type:NotekeeperUi
 /// </code>
 /// </example>
-public sealed class CartridgeUISerializer : ITypeSerializer<CartridgeUI, ValueDataNode>
+public sealed class UIFragmentSerializer : ITypeSerializer<UIFragment, ValueDataNode>
 {
     public ValidationNode Validate(ISerializationManager serializationManager, ValueDataNode node,
         IDependencyCollection dependencies, ISerializationContext? context = null)
     {
-        return serializationManager.ValidateNode<CartridgeUI>(node, context);
+        return serializationManager.ValidateNode<UIFragment>(node, context);
     }
 
-    public CartridgeUI Read(ISerializationManager serializationManager, ValueDataNode node, IDependencyCollection dependencies,
-        bool skipHook, ISerializationContext? context = null, CartridgeUI? value = default)
+    public UIFragment Read(ISerializationManager serializationManager, ValueDataNode node, IDependencyCollection dependencies,
+        bool skipHook, ISerializationContext? context = null, UIFragment? value = default)
     {
         return serializationManager.Read(node, context, skipHook, value);
     }
 
-    public CartridgeUI Copy(ISerializationManager serializationManager, CartridgeUI source, CartridgeUI target, bool skipHook,
+    public UIFragment Copy(ISerializationManager serializationManager, UIFragment source, UIFragment target, bool skipHook,
         ISerializationContext? context = null)
     {
         return serializationManager.Copy(source, context, skipHook);
     }
 
-    public DataNode Write(ISerializationManager serializationManager, CartridgeUI value, IDependencyCollection dependencies,
+    public DataNode Write(ISerializationManager serializationManager, UIFragment value, IDependencyCollection dependencies,
         bool alwaysWrite = false, ISerializationContext? context = null)
     {
         return serializationManager.WriteValue(value, alwaysWrite, context);
