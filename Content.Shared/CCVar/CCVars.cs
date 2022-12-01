70d5655952336e7ecf8e2f5081b9ccb289c08c6d
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-12-01 23:38:30 +0100

    Replace UILinks with new cvars.
    All the UI links can now be empty and the relevant buttons will be hidden on the UI.

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -1339,30 +1339,42 @@ namespace Content.Shared.CCVar
         /// Link to Discord server to show in the launcher.
         /// </summary>
         public static readonly CVarDef<string> InfoLinksDiscord =
-            CVarDef.Create("infolinks.discord", "", CVar.SERVERONLY);
+            CVarDef.Create("infolinks.discord", "", CVar.SERVER | CVar.REPLICATED);
 
         /// <summary>
         /// Link to website to show in the launcher.
         /// </summary>
         public static readonly CVarDef<string> InfoLinksForum =
-            CVarDef.Create("infolinks.forum", "", CVar.SERVERONLY);
+            CVarDef.Create("infolinks.forum", "", CVar.SERVER | CVar.REPLICATED);
 
         /// <summary>
         /// Link to GitHub page to show in the launcher.
         /// </summary>
         public static readonly CVarDef<string> InfoLinksGithub =
-            CVarDef.Create("infolinks.github", "", CVar.SERVERONLY);
+            CVarDef.Create("infolinks.github", "", CVar.SERVER | CVar.REPLICATED);
 
         /// <summary>
         /// Link to website to show in the launcher.
         /// </summary>
         public static readonly CVarDef<string> InfoLinksWebsite =
-            CVarDef.Create("infolinks.website", "", CVar.SERVERONLY);
+            CVarDef.Create("infolinks.website", "", CVar.SERVER | CVar.REPLICATED);
 
         /// <summary>
         /// Link to wiki to show in the launcher.
         /// </summary>
         public static readonly CVarDef<string> InfoLinksWiki =
-            CVarDef.Create("infolinks.wiki", "", CVar.SERVERONLY);
+            CVarDef.Create("infolinks.wiki", "", CVar.SERVER | CVar.REPLICATED);
+
+        /// <summary>
+        /// Link to Patreon. Not shown in the launcher currently.
+        /// </summary>
+        public static readonly CVarDef<string> InfoLinksPatreon =
+            CVarDef.Create("infolinks.patreon", "", CVar.SERVER | CVar.REPLICATED);
+
+        /// <summary>
+        /// Link to the bug report form.
+        /// </summary>
+        public static readonly CVarDef<string> InfoLinksBugReport =
+            CVarDef.Create("infolinks.bug_report", "", CVar.SERVER | CVar.REPLICATED);
     }
 }
