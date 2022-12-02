91eea958361bc36999a42e3a7535210f15d5e4b4
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-12-02 15:06:49 +0800

    Gr3y.T1d3

---
+++ Content.Server/StationEvents/Events/GreytideVirus.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using Content.Server.Doors.Components;
+using Content.Server.Doors.Systems;
+using Content.Server.GameTicking.Rules.Configurations;
+using Content.Shared.Doors.Components;
+using Content.Shared.Doors.Systems;
+using Robust.Shared.Audio;
+using Robust.Shared.Player;
+using Robust.Shared.Random;
+
+namespace Content.Server.StationEvents.Events;
+
+public sealed class GreytideVirus : StationEventSystem
+{
+    [Dependency] private readonly DoorSystem _doorSystem = default!;
+
+    public override string Prototype => "GreytideVirus";
+
+    public override void Started()
+    {
+        base.Started();
+
+        var modifier = GetSeverityModifier();
+
+        var airlocks = EntityQuery<AirlockComponent, DoorComponent>().ToList();
+        RobustRandom.Shuffle(airlocks);
+
+        var airlockAmount = (int) (RobustRandom.Next(2, 4) * Math.Sqrt(modifier)); // A small colony of critters.
+
+        for (var i = 0; i < airlockAmount && i < airlocks.Count - 1; i++)
+        {
+                var airlock = RobustRandom.Pick(airlocks);
+                Sawmill.Info($"Bolting and opening {airlock}");
+
+                _doorSystem.TryOpen(airlock.Item1.Owner, airlock.Item2);
+                airlock.Item1.SetBoltsWithAudio(true);
+        }
+    }
+}
---
+++ Resources/Locale/en-US/station-events/events/greytide-virus.ftl
@@ -0,0 +1,1 @@
+greytide-virus-event-announcement = Gr3y.T1d3 virus detected on-station imprisonment subroutines. Recommend engineering involvement.
