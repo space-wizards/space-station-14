19c283f554100b2e3dc4e13fae6c364110475a1d
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-12-02 15:06:59 +0800

    might as well clean this up

--- Content.Server/StationEvents/Events/FalseAlarm.cs
+++ Content.Server/StationEvents/Events/FalseAlarm.cs
@@ -3,31 +3,30 @@ using JetBrains.Annotations;
 using Robust.Shared.Audio;
 using Robust.Shared.Player;
 
-namespace Content.Server.StationEvents.Events
+namespace Content.Server.StationEvents.Events;
+
+[UsedImplicitly]
+public sealed class FalseAlarm : StationEventSystem
 {
-    [UsedImplicitly]
-    public sealed class FalseAlarm : StationEventSystem
+    public override string Prototype => "FalseAlarm";
+
+    public override void Started()
     {
-        public override string Prototype => "FalseAlarm";
+        base.Started();
 
-        public override void Started()
+        var ev = GetRandomEventUnweighted(PrototypeManager, RobustRandom);
+
+        if (ev.Configuration is not StationEventRuleConfiguration cfg)
+            return;
+
+        if (cfg.StartAnnouncement != null)
         {
-            base.Started();
+            ChatSystem.DispatchGlobalAnnouncement(Loc.GetString(cfg.StartAnnouncement), playSound: false, colorOverride: Color.Gold);
+        }
 
-            var ev = GetRandomEventUnweighted(PrototypeManager, RobustRandom);
-
-            if (ev.Configuration is not StationEventRuleConfiguration cfg)
-                return;
-
-            if (cfg.StartAnnouncement != null)
-            {
-                ChatSystem.DispatchGlobalAnnouncement(Loc.GetString(cfg.StartAnnouncement), playSound: false, colorOverride: Color.Gold);
-            }
-
-            if (cfg.StartAudio != null)
-            {
-                SoundSystem.Play(cfg.StartAudio.GetSound(), Filter.Broadcast(), cfg.StartAudio.Params);
-            }
+        if (cfg.StartAudio != null)
+        {
+            SoundSystem.Play(cfg.StartAudio.GetSound(), Filter.Broadcast(), cfg.StartAudio.Params);
         }
     }
 }
