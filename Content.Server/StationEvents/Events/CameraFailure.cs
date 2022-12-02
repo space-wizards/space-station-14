e0edcf7fc6bd9f1acb1044b0d3263e34463b2cdc
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-12-02 15:20:12 +0800

    Camera Failure

---
+++ Content.Server/StationEvents/Events/CameraFailure.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Content.Server.SurveillanceCamera;
+using Robust.Shared.Random;
+
+namespace Content.Server.StationEvents.Events;
+
+public sealed class CameraFailure : StationEventSystem
+{
+    [Dependency] private readonly SurveillanceCameraSystem _cameraSystem = default!;
+
+    public override string Prototype => "CameraFailure";
+
+    public override void Started()
+    {
+        base.Started();
+
+        var modifier = GetSeverityModifier();
+
+        var cameras = EntityQuery<SurveillanceCameraComponent>().ToList();
+        RobustRandom.Shuffle(cameras);
+
+        var cameraAmount = (int) (RobustRandom.Next(2, 4) * Math.Sqrt(modifier));
+
+        for (var i = 0; i < cameraAmount && i < cameras.Count - 1; i++)
+        {
+            var camera = RobustRandom.Pick(cameras);
+            Sawmill.Info($"Disabling {camera}");
+
+            _cameraSystem.SetActive(camera.Owner, false);
+        }
+    }
+}
