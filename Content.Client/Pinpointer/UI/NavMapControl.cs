da368ba89a7164734657c2c8a2bcd3764ff614d1
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-12-14 20:42:02 +1100

    a

--- Content.Client/Pinpointer/UI/NavMapControl.cs
+++ Content.Client/Pinpointer/UI/NavMapControl.cs
@@ -44,6 +44,7 @@ public sealed class NavMapControl : MapGridControl
 
         var area = new Box2(-WorldRange, -WorldRange, WorldRange, WorldRange).Translated(offset);
         var tileSize = new Vector2(grid.TileSize, -grid.TileSize);
+        Span<Vector2> verts = stackalloc Vector2[4];
 
         for (var x = Math.Floor(area.Left); x <= Math.Ceiling(area.Right + 1); x += SharedNavMapSystem.ChunkSize * grid.TileSize)
         {
@@ -66,10 +67,87 @@ public sealed class NavMapControl : MapGridControl
                     if (mask == 0x0)
                         continue;
 
-                    var tile = (chunk.Origin * SharedNavMapSystem.ChunkSize + SharedNavMapSystem.GetTile(mask)) * grid.TileSize - offset;
+                    // Alright now we'll work out our edges
+                    var relativeTile = SharedNavMapSystem.GetTile(mask);
+                    var tile = (chunk.Origin * SharedNavMapSystem.ChunkSize + relativeTile) * grid.TileSize - offset;
                     var position = new Vector2(tile.X, -tile.Y);
+                    NavMapChunk? neighborChunk;
+                    bool neighbor;
 
-                    handle.DrawRect(new UIBox2(Scale(position), Scale(position + tileSize)), Color.Aqua, false);
+                    // North edge
+                    if (relativeTile.Y == SharedNavMapSystem.ChunkSize - 1)
+                    {
+                        neighbor = navMap.Chunks.TryGetValue(chunkOrigin + new Vector2i(0, 1), out neighborChunk) &&
+                                      (neighborChunk.TileData &
+                                       SharedNavMapSystem.GetFlag(new Vector2i(relativeTile.X, 0))) != 0x0;
+                    }
+                    else
+                    {
+                        var flag = SharedNavMapSystem.GetFlag(relativeTile + new Vector2i(0, 1));
+                        neighbor = (chunk.TileData & flag) != 0x0;
+                    }
+
+                    if (!neighbor)
+                    {
+                        handle.DrawLine(Scale(position + new Vector2(0f, -grid.TileSize)), Scale(position + tileSize), Color.Aqua);
+                    }
+
+                    // East edge
+                    if (relativeTile.X == SharedNavMapSystem.ChunkSize - 1)
+                    {
+                        neighbor = navMap.Chunks.TryGetValue(chunkOrigin + new Vector2i(1, 0), out neighborChunk) &&
+                                   (neighborChunk.TileData &
+                                    SharedNavMapSystem.GetFlag(new Vector2i(0, relativeTile.Y))) != 0x0;
+                    }
+                    else
+                    {
+                        var flag = SharedNavMapSystem.GetFlag(relativeTile + new Vector2i(1, 0));
+                        neighbor = (chunk.TileData & flag) != 0x0;
+                    }
+
+                    if (!neighbor)
+                    {
+                        handle.DrawLine(Scale(position + tileSize), Scale(position + new Vector2(grid.TileSize, 0f)), Color.Aqua);
+                    }
+
+                    // South edge
+                    if (relativeTile.Y == 0)
+                    {
+                        neighbor = navMap.Chunks.TryGetValue(chunkOrigin + new Vector2i(0, -1), out neighborChunk) &&
+                                   (neighborChunk.TileData &
+                                    SharedNavMapSystem.GetFlag(new Vector2i(relativeTile.X, SharedNavMapSystem.ChunkSize - 1))) != 0x0;
+                    }
+                    else
+                    {
+                        var flag = SharedNavMapSystem.GetFlag(relativeTile + new Vector2i(0, -1));
+                        neighbor = (chunk.TileData & flag) != 0x0;
+                    }
+
+                    if (!neighbor)
+                    {
+                        handle.DrawLine(Scale(position + new Vector2(grid.TileSize, 0f)), Scale(position), Color.Aqua);
+                    }
+
+                    // West edge
+                    if (relativeTile.X == 0)
+                    {
+                        neighbor = navMap.Chunks.TryGetValue(chunkOrigin + new Vector2i(-1, 0), out neighborChunk) &&
+                                   (neighborChunk.TileData &
+                                    SharedNavMapSystem.GetFlag(new Vector2i(SharedNavMapSystem.ChunkSize - 1, relativeTile.Y))) != 0x0;
+                    }
+                    else
+                    {
+                        var flag = SharedNavMapSystem.GetFlag(relativeTile + new Vector2i(-1, 0));
+                        neighbor = (chunk.TileData & flag) != 0x0;
+                    }
+
+                    if (!neighbor)
+                    {
+                        handle.DrawLine(Scale(position), Scale(position + new Vector2(0f, -grid.TileSize)), Color.Aqua);
+                    }
+
+
+                    // handle.DrawRect(new UIBox2(Scale(position), Scale(position + tileSize)), Color.Aqua, false);
                 }
             }
         }
