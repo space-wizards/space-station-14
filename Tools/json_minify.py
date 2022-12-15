9c02147a27faba740ad646925bcf4de5263a00a7
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-12-15 13:57:53 +1100

    Minify meta.json files
    These were always supposed to be minified we just got lazy.

---
+++ Tools/json_minify.py
@@ -0,0 +1,29 @@
+from json import dump, load
+from json.decoder import JSONDecodeError
+from pathlib import Path
+from glob import iglob
+
+
+def main():
+    resources: Path = Path("../Resources")
+    a = resources.resolve()
+    errors = []
+
+    for fn in a.rglob("**/meta.json"):
+        try:
+            with fn.open("r+", encoding="utf-8-sig") as f:
+                loaded = load(f)
+                f.seek(0)
+                dump(loaded, f, separators=(",", ":"))
+                f.truncate()
+        except JSONDecodeError:
+            errors.append(fn)
+
+        print(f"Minified {fn}")
+
+    for error in errors:
+        print(f"JSON decode error minifying {error}")
+
+
+if __name__ == "__main__":
+    main()
