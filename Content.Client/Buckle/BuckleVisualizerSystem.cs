91141ba31914cfcd4cef9b88ef4d5d2e88c59761
AJCM-git <60196617+AJCM-git@users.noreply.github.com>
2022-12-15 14:39:34 -0400

    ECS Buckle and Rotation visualizers (#13024)
    Co-authored-by: DrSmugleaf <DrSmugleaf@users.noreply.github.com>

--- Content.Client/Buckle/BuckleVisualizerSystem.cs
+++ Content.Client/Buckle/BuckleVisualizerSystem.cs
@@ -1,5 +1,5 @@
-﻿using Content.Client.Rotation;
 using Content.Shared.Buckle.Components;
+using Content.Client.Rotation;
 using Robust.Client.GameObjects;
 
 namespace Content.Client.Buckle;
@@ -9,7 +9,8 @@ public sealed class BuckleVisualizer : VisualizerSystem<BuckleComponent>
     protected override void OnAppearanceChange(EntityUid uid, BuckleComponent component, ref AppearanceChangeEvent args)
     {
         if (!args.Component.TryGetData<int>(StrapVisuals.RotationAngle, out var angle) ||
-            !args.Component.TryGetData<bool>(BuckleVisuals.Buckled, out var buckled) || !buckled ||
+            !args.Component.TryGetData<bool>(BuckleVisuals.Buckled, out var buckled) ||
+            !buckled ||
             args.Sprite == null)
         {
             return;
@@ -19,3 +20,4 @@ public sealed class BuckleVisualizer : VisualizerSystem<BuckleComponent>
             .AnimateSpriteRotation(args.Sprite, Angle.FromDegrees(angle), 0.125f);
     }
 }
+
