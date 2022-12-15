99c5dcc027da28d3d302a49d26b1cd4c69c37381
Moony <moony@hellomouse.net>
2022-12-15 15:27:17 -0600

    Update DamageSpecifier.cs

--- Content.Shared/Damage/DamageSpecifier.cs
+++ Content.Shared/Damage/DamageSpecifier.cs
@@ -16,6 +16,15 @@ namespace Content.Shared.Damage
     [DataDefinition]
     public sealed class DamageSpecifier : IEquatable<DamageSpecifier>
     {
+        // These exist solely so the wiki works. Please do not touch them or use them.
+        [JsonPropertyName("types")]
+        [DataField("types", customTypeSerializer: typeof(PrototypeIdDictionarySerializer<FixedPoint2, DamageTypePrototype>))]
+        private readonly Dictionary<string,FixedPoint2>? _damageTypeDictionary;
+
+        [JsonPropertyName("groups")]
+        [DataField("groups", customTypeSerializer: typeof(PrototypeIdDictionarySerializer<FixedPoint2, DamageGroupPrototype>))]
+        private readonly Dictionary<string, FixedPoint2>? _damageGroupDictionary;
+    
         /// <summary>
         ///     Main DamageSpecifier dictionary. Most DamageSpecifier functions exist to somehow modifying this.
         /// </summary>
