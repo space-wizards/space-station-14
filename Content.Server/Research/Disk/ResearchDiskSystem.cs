9bda74458a575c9b05c4763a2b39f6fcfc254536
EmoGarbage404 <retron404@gmail.com>
2022-12-17 22:15:21 -0500

    research disk tweaks

--- Content.Server/Research/Disk/ResearchDiskSystem.cs
+++ Content.Server/Research/Disk/ResearchDiskSystem.cs
@@ -1,17 +1,22 @@
+using System.Linq;
 using Content.Shared.Interaction;
 using Content.Server.Research.Components;
 using Content.Server.Popups;
+using Content.Shared.Research.Prototypes;
 using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server.Research.Disk
 {
     public sealed class ResearchDiskSystem : EntitySystem
     {
+        [Dependency] private readonly IPrototypeManager _prototype = default!;
         [Dependency] private readonly PopupSystem _popupSystem = default!;
         public override void Initialize()
         {
             base.Initialize();
             SubscribeLocalEvent<ResearchDiskComponent, AfterInteractEvent>(OnAfterInteract);
+            SubscribeLocalEvent<ResearchDiskComponent, MapInitEvent>(OnMapInit);
         }
 
         private void OnAfterInteract(EntityUid uid, ResearchDiskComponent component, AfterInteractEvent args)
@@ -25,6 +30,15 @@ namespace Content.Server.Research.Disk
             server.Points += component.Points;
             _popupSystem.PopupEntity(Loc.GetString("research-disk-inserted", ("points", component.Points)), args.Target.Value, Filter.Entities(args.User));
             EntityManager.QueueDeleteEntity(uid);
+        }
+
+        private void OnMapInit(EntityUid uid, ResearchDiskComponent component, MapInitEvent args)
+        {
+            if (!component.UnlockAllTech)
+                return;
+
+            component.Points = _prototype.EnumeratePrototypes<TechnologyPrototype>()
+                .Sum(tech => tech.RequiredPoints);
         }
     }
 }
