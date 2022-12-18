39e07a9f718cce19f286cd02ca370a5ac8708e57
EmoGarbage404 <retron404@gmail.com>
2022-12-17 20:31:17 -0500

    hot plates

---
+++ Content.Server/Chemistry/Components/SolutionHeaterComponent.cs
@@ -0,0 +1,22 @@
+﻿namespace Content.Server.Chemistry.Components;
+
+[RegisterComponent]
+public sealed class SolutionHeaterComponent : Component
+{
+    public readonly string BeakerSlotId = "beakerSlot";
+
+    [DataField("heatPerSecond")]
+    public float HeatPerSecond = 80;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float HeatMultiplier = 1;
+
+    [ViewVariables]
+    public TimeSpan NextHeat = TimeSpan.Zero;
+
+    [DataField("machinePartHeatPerSecond")]
+    public string MachinePartHeatPerSecond = "Laser";
+
+    [DataField("partRatingHeatMultiplier")]
+    public float PartRatingHeatMultiplier = 1.5f;
+}
---
+++ Content.Server/Chemistry/EntitySystems/SolutionHeaterSystem.cs
@@ -0,0 +1,50 @@
+﻿using Content.Server.Chemistry.Components;
+using Content.Server.Chemistry.Components.SolutionManager;
+using Content.Server.Construction;
+using Content.Shared.Containers.ItemSlots;
+using Robust.Shared.Timing;
+
+namespace Content.Server.Chemistry.EntitySystems;
+
+public sealed class SolutionHeaterSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly ItemSlotsSystem _itemSlots = default!;
+    [Dependency] private readonly SolutionContainerSystem _solution = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<SolutionHeaterComponent, RefreshPartsEvent>(OnRefreshParts);
+    }
+
+    private void OnRefreshParts(EntityUid uid, SolutionHeaterComponent component, RefreshPartsEvent args)
+    {
+        var heatRating = args.PartRatings[component.MachinePartHeatPerSecond] - 1;
+
+        component.HeatMultiplier = MathF.Pow(component.PartRatingHeatMultiplier, heatRating);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        foreach (var heater in EntityQuery<SolutionHeaterComponent>())
+        {
+            if (heater.NextHeat > _timing.CurTime)
+                continue;
+            heater.NextHeat = _timing.CurTime + TimeSpan.FromSeconds(1);
+
+            if (_itemSlots.GetItemOrNull(heater.Owner, heater.BeakerSlotId) is not { } item)
+                continue;
+
+            if (!TryComp<SolutionContainerManagerComponent>(item, out var solution))
+                continue;
+
+            foreach (var s in solution.Solutions.Values)
+            {
+                _solution.AddThermalEnergy(solution.Owner, s, heater.HeatPerSecond * heater.HeatMultiplier);
+            }
+        }
+    }
+}
