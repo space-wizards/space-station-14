88f2e1cf8e254b93f907c0b5d6870e9ee0ce2b91
EmoGarbage404 <retron404@gmail.com>
2022-12-18 12:48:40 -0500

    clean up a bunch of R&D code

--- Content.Server/Research/Components/ResearchServerComponent.cs
+++ Content.Server/Research/Components/ResearchServerComponent.cs
@@ -1,5 +1,9 @@
+using Content.Server.Research.Systems;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
 namespace Content.Server.Research.Components
 {
+    [Access(typeof(ResearchSystem))]
     [RegisterComponent]
     public sealed class ResearchServerComponent : Component
     {
@@ -11,12 +15,33 @@ namespace Content.Server.Research.Components
         [ViewVariables(VVAccess.ReadWrite)] [DataField("points")]
         public int Points = 0;
 
-        [ViewVariables(VVAccess.ReadOnly)] public int Id { get; set; }
-
         [ViewVariables(VVAccess.ReadOnly)]
-        public List<ResearchPointSourceComponent> PointSources { get; } = new();
+        public int Id { get; set; }
 
         [ViewVariables(VVAccess.ReadOnly)]
         public List<ResearchClientComponent> Clients { get; } = new();
+
+        [DataField("nextUpdateTime", customTypeSerializer: typeof(TimeOffsetSerializer))]
+        public TimeSpan NextUpdateTime = TimeSpan.Zero;
+
+        [DataField("researchConsoleUpdateTime"), ViewVariables(VVAccess.ReadWrite)]
+        public readonly TimeSpan ResearchConsoleUpdateTime = TimeSpan.FromSeconds(1);
     }
+
+    /// <summary>
+    /// Event raised on a server's clients when the point value of the server is changed.
+    /// </summary>
+    /// <param name="Server"></param>
+    /// <param name="Total"></param>
+    /// <param name="Delta"></param>
+    [ByRefEvent]
+    public record struct ResearchServerPointsChangedEvent(EntityUid Server, int Total, int Delta);
+
+    /// <summary>
+    /// Event raised every second to calculate the amount of points added to the server.
+    /// </summary>
+    /// <param name="Server"></param>
+    /// <param name="Points"></param>
+    [ByRefEvent]
+    public record struct ResearchServerGetPointsPerSecondEvent(EntityUid Server, int Points);
 }
--- Content.Server/Research/Systems/ResearchSystem.Client.cs
+++ Content.Server/Research/Systems/ResearchSystem.Client.cs
@@ -1,7 +1,8 @@
+using System.Linq;
 using Content.Server.Research.Components;
 using Content.Shared.Research.Components;
 
-namespace Content.Server.Research;
+namespace Content.Server.Research.Systems;
 
 public sealed partial class ResearchSystem
 {
@@ -50,8 +51,9 @@ public sealed partial class ResearchSystem
 
     private void OnClientStartup(EntityUid uid, ResearchClientComponent component, ComponentStartup args)
     {
-        if (Servers.Count > 0)
-            RegisterClientServer(component, Servers[0]);
+        var allServers = EntityQuery<ResearchServerComponent>().ToArray();
+        if (allServers.Length > 0)
+            RegisterClientServer(component, allServers[0]);
     }
 
     private void OnClientShutdown(EntityUid uid, ResearchClientComponent component, ComponentShutdown args)
@@ -66,21 +68,22 @@ public sealed partial class ResearchSystem
 
     private void UpdateClientInterface(ResearchClientComponent component)
     {
-        var state = new ResearchClientBoundInterfaceState(Servers.Count, GetServerNames(),
+        var names = GetServerNames();
+        var state = new ResearchClientBoundInterfaceState(names.Length, names,
             GetServerIds(), component.ConnectedToServer ? component.Server!.Id : -1);
 
-        _uiSystem.GetUiOrNull(component.Owner, ResearchClientUiKey.Key)?.SetState(state);
+        _uiSystem.TrySetUiState(component.Owner, ResearchClientUiKey.Key, state);
     }
 
     private bool RegisterClientServer(ResearchClientComponent component, ResearchServerComponent? server = null)
     {
-        if (server == null) return false;
-        return RegisterServerClient(server, component);
+        return server != null && RegisterServerClient(server, component);
     }
 
     private void UnregisterClientServer(ResearchClientComponent component)
     {
-        if (component.Server == null) return;
+        if (component.Server == null)
+            return;
 
         UnregisterServerClient(component.Server, component);
     }
