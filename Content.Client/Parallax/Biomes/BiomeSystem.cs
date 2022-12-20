86597d42ac76c17d3058f96666d5b2e7253e37b5
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-12-20 22:22:57 +1100

    a

--- Content.Client/Parallax/Biomes/BiomeSystem.cs
+++ Content.Client/Parallax/Biomes/BiomeSystem.cs
@@ -1,11 +1,21 @@
+using Content.Shared.Maps;
+using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
+using Robust.Client.ResourceManagement;
+using Robust.Shared.ContentPack;
 using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
 
 namespace Content.Client.Parallax.Biomes;
 
 public sealed class BiomeSystem : EntitySystem
 {
     [Dependency] private readonly IOverlayManager _overlay = default!;
+    [Dependency] private readonly IPrototypeManager _protoManager = default!;
+    [Dependency] private readonly IResourceCache _resourceManager = default!;
+    [Dependency] private readonly SpriteSystem _sprite = default!;
+
+    private const int ChunkSize = 4;
 
     public override void Initialize()
     {
@@ -19,7 +29,7 @@ public sealed class BiomeSystem : EntitySystem
         _overlay.RemoveOverlay<BiomeOverlay>();
     }
 
-    // For now we'll just use 
+    // For now we'll just use
 
     public Tile GetTile(Vector2i indices, BiomePrototype prototype, int seed)
     {
@@ -28,6 +38,31 @@ public sealed class BiomeSystem : EntitySystem
 
     public Texture GetTileTexture(Vector2i indices, BiomePrototype prototype, int seed)
     {
+        // TODO: Should do this per chunk and just not render the tiles unneeded.
+        var random = new Random(seed);
+        var chunk = SharedMapSystem.GetChunkIndices(indices, ChunkSize);
+        var relative = SharedMapSystem.GetChunkRelative(indices, ChunkSize);
 
+        for (var x = 0; x < ChunkSize; x++)
+        {
+            for (var y = 0; y < ChunkSize; y++)
+            {
+                if (x != relative.X && y != relative.Y)
+                {
+                    random.Next();
+                    continue;
+                }
+
+                var resource = _protoManager.Index<ContentTileDefinition>("Grass").Sprite;
+
+                if (resource == null)
+                    continue;
+
+                // TODO: Use the weighted random stuff.
+                return _resourceManager.GetResource<TextureResource>(resource).Texture;
+            }
+        }
+
+        throw new InvalidOperationException();
     }
 }
