764ac77206e7f31b8b5e166bc1f79aa51e84a7ae
Eoin Mcloughlin <helloworld@eoinrul.es>
2022-12-21 21:30:04 +0000

    Add empty-paper message

--- Content.Client/Paper/UI/PaperWindow.xaml.cs
+++ Content.Client/Paper/UI/PaperWindow.xaml.cs
@@ -17,13 +17,15 @@ namespace Content.Client.Paper.UI
 
         public void Populate(SharedPaperComponent.PaperBoundUserInterfaceState state)
         {
-            if (state.Mode == SharedPaperComponent.PaperAction.Write)
-            {
-                Input.Visible = true;
-            }
+            bool isEditing = state.Mode == SharedPaperComponent.PaperAction.Write;
+            Input.Visible = isEditing;
+
             var msg = new FormattedMessage();
             msg.AddMarkupPermissive(state.Text);
             Label.SetMessage(msg);
+
+            BlankPaperIndicator.Visible = !isEditing && state.Text.Length == 0;
+            BlankPaperPlaceholder.Visible = !BlankPaperIndicator.Visible;
 
             StampDisplay.RemoveAllChildren();
             foreach(var stamper in state.StampedBy)
