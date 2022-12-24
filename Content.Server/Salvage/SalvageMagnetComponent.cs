53d1770b89743cb07b3a41514a0aa526cf096f5a
dmnct <asp1@tuta.io>
2022-12-24 10:56:52 +0100

    Changed variables back to TimeSpan

--- Content.Server/Salvage/SalvageMagnetComponent.cs
+++ Content.Server/Salvage/SalvageMagnetComponent.cs
@@ -52,28 +52,28 @@ namespace Content.Server.Salvage
         /// </summary>
         [ViewVariables(VVAccess.ReadWrite)]
         [DataField("attachingTime")]
-        public int AttachingTime;
+        public TimeSpan AttachingTime = TimeSpan.FromSeconds(10);
 
         /// <summary>
         ///     How long can the magnet hold the debris until it starts losing the lock (in seconds)
         /// </summary>
         [ViewVariables(VVAccess.ReadWrite)]
         [DataField("holdTime")]
-        public int HoldTime;
+        public TimeSpan HoldTime = TimeSpan.FromSeconds(10);
 
         /// <summary>
         ///     How long can the magnet hold the debris while losing the lock (in seconds)
         /// </summary>
         [ViewVariables(VVAccess.ReadWrite)]
         [DataField("detachingTime")]
-        public int DetachingTime;
+        public TimeSpan DetachingTime = TimeSpan.FromSeconds(10);
 
         /// <summary>
         ///     How long the magnet has to cool down after use (in seconds)
         /// </summary>
         [ViewVariables(VVAccess.ReadWrite)]
         [DataField("cooldownTime")]
-        public int CooldownTime;
+        public TimeSpan CooldownTime = TimeSpan.FromSeconds(10);
 
         [DataField("salvageChannel", customTypeSerializer: typeof(PrototypeIdSerializer<RadioChannelPrototype>))]
         public string SalvageChannel = "Supply";
