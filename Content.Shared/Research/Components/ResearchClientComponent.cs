4eee1ee9b247b66eb8958728d8995819d3bd88f8
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2022-12-25 16:22:23 -0500

    clean up infinite researchsystem shitcode (#13136)
    * clean up infinite researchsystem shitcode
    
    * fml some more shit
    
    * make syncing work logically
    
    * naming naming naming

--- Content.Shared/Research/Components/ResearchClientComponent.cs
+++ Content.Shared/Research/Components/ResearchClientComponent.cs
@@ -3,14 +3,33 @@
 namespace Content.Shared.Research.Components
 {
     /// <summary>
+    /// This is an entity that is able to connect to a <see cref="ResearchServerComponent"/>
+    /// </summary>
+    [RegisterComponent]
+    public sealed class ResearchClientComponent : Component
+    {
+        public bool ConnectedToServer => Server != null;
+
+        /// <summary>
+        /// The server the client is connected to
+        /// </summary>
+        [ViewVariables(VVAccess.ReadOnly)]
+        public EntityUid? Server { get; set; }
+    }
+
+    /// <summary>
+    /// Raised on the client whenever its server is changed
+    /// </summary>
+    /// <param name="Server">Its new server</param>
+    [ByRefEvent]
+    public readonly record struct ResearchRegistrationChangedEvent(EntityUid? Server);
+
+    /// <summary>
     ///     Sent to the server when the client deselects a research server.
     /// </summary>
     [Serializable, NetSerializable]
     public sealed class ResearchClientServerDeselectedMessage : BoundUserInterfaceMessage
     {
-        public ResearchClientServerDeselectedMessage()
-        {
-        }
     }
 
     /// <summary>
@@ -33,10 +52,6 @@ namespace Content.Shared.Research.Components
     [Serializable, NetSerializable]
     public sealed class ResearchClientSyncMessage : BoundUserInterfaceMessage
     {
-
-        public ResearchClientSyncMessage()
-        {
-        }
     }
 
     [NetSerializable, Serializable]
