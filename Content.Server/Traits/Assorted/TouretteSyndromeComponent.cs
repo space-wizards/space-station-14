9b36cddb515b2012966ebe26e543d59b8a2f1ea4
gnomee1337 <alexeynshah@gmail.com>
2022-12-28 02:12:01 +0200

    Created Tourette System which chooses a random syndrom event. Events: scream, drop_item, use_item

--- Content.Server/Traits/Assorted/TouretteSyndromeComponent.cs
+++ Content.Server/Traits/Assorted/TouretteSyndromeComponent.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Content.Server.Traits.Assorted;
 
 
@@ -8,10 +10,28 @@ namespace Content.Server.Traits.Assorted;
 public sealed class TouretteSyndromeComponent : Component
 {
     /// <summary>
+    /// Message to play when using item in active hand.
+    /// </summary>
+    [DataField("wristSpasmMessage", required: true)]
+    public string TouretteUseItemMessage { get; } = default!;
+
+    /// <summary>
+    /// Message to play when drops item from active hand.
+    /// </summary>
+    [DataField("armTwitchingMessage", required: true)]
+    public string TouretteDropItemMessage { get; } = default!;
+
+    /// <summary>
     /// The random time between incidents, (min, max).
     /// </summary>
     [DataField("timeBetweenIncidents", required: true)]
     public Vector2 TimeBetweenIncidents { get; }
+
+    /// <summary>
+    /// The random number to choose symptom, (first, last).
+    /// </summary>
+    [DataField("touretteSymptoms", required: true)]
+    public List<string> TouretteSymptoms { get; } = default!;
 
     public float NextIncidentTime;
 
--- Content.Server/Traits/Assorted/TouretteSyndromeSystem.cs
+++ Content.Server/Traits/Assorted/TouretteSyndromeSystem.cs
@@ -1,8 +1,22 @@
 using Content.Server.Speech;
 using Robust.Shared.Random;
 using Content.Server.Chat.Systems;
+using Content.Shared.Hands.EntitySystems;
 
 namespace Content.Server.Traits.Assorted;
+
+// ToDO:
+//+1. After timer expired generate from 1 to N number to choose random event.
+// Events:
+//+2. Random Scream
+// 3. Random Phrase:
+//   3.1) Barking
+//   3.2) Grunting
+//   3.3) Vulgar words
+// 4. Random PVP mode
+// 5. Character twitching
+//+6. Drop item from hand
+//+7. Use item in hand
 
 /// <summary>
 /// This handles making character randomly scream/speak with no reason.
@@ -12,6 +26,7 @@ public sealed class TouretteSyndromeSystem : EntitySystem
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly VocalSystem _vocalSystem = default!;
     [Dependency] private readonly ChatSystem _chat = default!;
+    [Dependency] private readonly SharedHandsSystem _sharedHands = default!;
 
     /// <inheritdoc/>
     public override void Initialize()
@@ -36,21 +51,42 @@ public sealed class TouretteSyndromeSystem : EntitySystem
             if (tourette.NextIncidentTime >= 0)
                 continue;
 
-            // Set the new time.
+            // Set the new time
             tourette.NextIncidentTime +=
                 _random.NextFloat(tourette.TimeBetweenIncidents.X, tourette.TimeBetweenIncidents.Y);
 
-            if (tourette.NextIncidentTime <= 3)
-            {
-                
-                _chat.TrySendInGameICMessage(tourette.Owner, "test message", InGameICChatType.Speak, hideChat: false, hideGlobalGhostChat: true);
-            }
-            else
-            {
-                _vocalSystem.TryScream(tourette.Owner);
-            }
+            // Generate random value in range of TouretteSymptoms list size
+            int random_value = _random.Next(tourette.TouretteSymptoms.Count);
 
+            // Get random value from TouretteSymptoms
+            string tourette_event = tourette.TouretteSymptoms[random_value];
+
+            switch (tourette_event)
+            {
+                case "Scream":
+                    _vocalSystem.TryScream(tourette.Owner);
+                    break;
+
+                case "Phrase":
+                    _chat.TrySendInGameICMessage(tourette.Owner, "test message", InGameICChatType.Speak, hideChat: false, hideGlobalGhostChat: true);
+                    break;
+
+                case "DropItemInHand":
+                    _chat.TrySendInGameICMessage(tourette.Owner, tourette.TouretteDropItemMessage, InGameICChatType.Emote, hideChat: false, hideGlobalGhostChat: true);
+                    // Try drop item from active hand
+                    _sharedHands.TryDrop(tourette.Owner);
+                    break;
+
+                case "UseItemInHand":
+                    _chat.TrySendInGameICMessage(tourette.Owner, tourette.TouretteUseItemMessage, InGameICChatType.Emote, hideChat: false, hideGlobalGhostChat: true);
+                    // Try use item in active hand
+                    _sharedHands.TryUseItemInHand(tourette.Owner);
+                    break;
+
+                default:
+                    throw new InvalidOperationException(
+                        $"{nameof(tourette.TouretteSymptoms)} switch doesn't have behavior for {tourette.TouretteSymptoms[random_value]} block");
+            }
         }
     }
-
 }
