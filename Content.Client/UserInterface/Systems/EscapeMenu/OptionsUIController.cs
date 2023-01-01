af4fd93246f9b557cc8715b5ac49b29470190df7
ElectroJr <leonsfriedrich@gmail.com>
2023-01-01 17:49:53 -0500

    replace controls button with command

--- Content.Client/UserInterface/Systems/EscapeMenu/OptionsUIController.cs
+++ Content.Client/UserInterface/Systems/EscapeMenu/OptionsUIController.cs
@@ -1,13 +1,38 @@
 using Content.Client.Options.UI;
 using JetBrains.Annotations;
-using Robust.Client.State;
 using Robust.Client.UserInterface.Controllers;
+using Robust.Shared.Console;
 
 namespace Content.Client.UserInterface.Systems.EscapeMenu;
 
 [UsedImplicitly]
 public sealed class OptionsUIController : UIController
 {
+    [Dependency] private readonly IConsoleHost _con = default!;
+
+    public override void Initialize()
+    {
+        _con.RegisterCommand("options", Loc.GetString("cmd-options-desc"), Loc.GetString("cmd-options-help"), OptionsCommand);
+    }
+
+    private void OptionsCommand(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length == 0)
+        {
+            ToggleWindow();
+            return;
+        }
+        OpenWindow();
+
+        if (!int.TryParse(args[0], out var tab))
+        {
+            shell.WriteError(Loc.GetString("cmd-parse-failure-int", ("arg", args[0])));
+            return;
+        }
+
+        _optionsWindow.Tabs.CurrentTab = tab;
+    }
+
     private OptionsMenu _optionsWindow = default!;
 
     private void EnsureWindow()
