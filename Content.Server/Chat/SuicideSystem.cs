e792916f322d049a95ae5588b036bf327c3d905f
Chief-Engineer <119664036+Chief-Engineer@users.noreply.github.com>
2023-01-03 05:49:07 -0600

    reword attempt log again

--- Content.Server/Chat/SuicideSystem.cs
+++ Content.Server/Chat/SuicideSystem.cs
@@ -33,7 +33,7 @@ namespace Content.Server.Chat
             if (!TryComp<MobStateComponent>(victim, out var mobState) || _mobState.IsDead(victim, mobState))
                 return false;
 
-            _adminLogger.Add(LogType.Mind, $"{EntityManager.ToPrettyString(victim):player} is attempting suicide");
+            _adminLogger.Add(LogType.Mind, $"{EntityManager.ToPrettyString(victim):player} is attempting to suicide");
 
             var suicideEvent = new SuicideEvent(victim);
 
