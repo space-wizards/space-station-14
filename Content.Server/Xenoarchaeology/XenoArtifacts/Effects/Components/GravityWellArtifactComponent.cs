dc947a22d185f9650e9b2a7f430e9192e5a4453c
TemporalOroboros <TemporalOroboros@gmail.com>
2023-01-03 00:53:33 -0800

    Comments and review

--- Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Components/GravityWellArtifactComponent.cs
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Components/GravityWellArtifactComponent.cs
@@ -1,40 +1,50 @@
+using Content.Server.Singularity.Components;
+
 namespace Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
 
+/// <summary>
+/// A component used to apply variation to the properties of a gravity well produced by an artifact.
+/// The most of the properties of this component correspond 1:1 with properties of an actual <see cref="GravityWellComponent"/>
+/// and define the range of possible values the corresponding property may have.
+/// </summary>
 [RegisterComponent]
 public sealed class GravityWellArtifactComponent : Component
 {
     /// <summary>
     /// The bounds on the outer range of the produced gravity well.
+    /// The upper bound should be lower than the lower bound for <see cref="MaxRange"/>.
+    /// See <see cref="GravityWellComponent.MaxRange"/> for the property this corresponds to.
     /// </summary>
     [DataField("minRange")]
     public (float min, float max) MinRange = (0f, 0f);
 
     /// <summary>
-    ///  The bounds on the inner range of the produced gravity well.
+    /// The bounds on the inner range of the produced gravity well.
+    /// The lower bound should be higher than the upper bound for <see cref="MinRange"/>.
+    /// See <see cref="GravityWellComponent.MinRange"/> for the property this corresponds to.
     /// </summary>
     [DataField("maxRange")]
     public (float min, float max) MaxRange = (0f, 0f);
 
     /// <summary>
     /// The bounds on the amount of radial acceleration the produced gravity well causes.
+    /// See <see cref="GravityWellComponent.BaseRadialAcceleration"/> for the property this corresponds to.
     /// </summary>
     [DataField("radialAcceleration")]
     public (float min, float max) RadialAcceleration = (0f, 0f);
 
     /// <summary>
     /// The bounds on the amount of tangential acceleration the produced gravity well causes.
+    /// See <see cref="GravityWellComponent.BaseTangentialAcceleration"/> for the property this corresponds to.
     /// </summary>
     [DataField("tangentialAcceleration")]
     public (float min, float max) TangentialAcceleration = (0f, 0f);
 
     /// <summary>
     /// The bounds on the amount of time between gravitational pulses produced by the artifact.
+    /// This also scales the amount of impulse applied to everything in range upon pulsing.
+    /// See <see cref="GravityWellComponent.TargetPulsePeriod"/> for the property this corresponds to.
     /// </summary>
     [DataField("pulsePeriod")]
     public (TimeSpan min, TimeSpan max) PulsePeriod = (TimeSpan.FromSeconds(.5f), TimeSpan.FromSeconds(.5f));
-
-    /// <summary>
-    /// Whether the artifact was already a gravity well when this was applied.
-    /// </summary>
-    public bool EntityWasAlreadyAGravityWell = false;
 }
--- Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/GravityWellArtifactSystem.cs
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/GravityWellArtifactSystem.cs
@@ -1,6 +1,7 @@
 using Content.Server.Singularity.Components;
 using Content.Server.Singularity.EntitySystems;
 using Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
+using Content.Server.Xenoarchaeology.XenoArtifacts.Events;
 using Robust.Shared.Random;
 
 namespace Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Systems;
@@ -16,19 +17,21 @@ public sealed class GravityWellArtifactSystem : EntitySystem
     {
         base.Initialize();
 
-        SubscribeLocalEvent<GravityWellArtifactComponent, ComponentStartup>(SetupGravityWell);
-        SubscribeLocalEvent<GravityWellArtifactComponent, ComponentShutdown>(ShutdownGravityWell);
+        SubscribeLocalEvent<GravityWellArtifactComponent, ArtifactNodeEnteredEvent>(SetupGravityWell);
     }
 
-    private void SetupGravityWell(EntityUid uid, GravityWellArtifactComponent comp, ComponentStartup args)
+    /// <summary>
+    /// Sets up the state of the <see cref="GravityWellComponent"/> involved in granting an artifact a gravity well as an effect.
+    /// </summary>
+    /// <param name="uid">The uid of the artifact being given a gravity well.</param>
+    /// <param name="comp">The state of the gravity well effect the artifact has.</param>
+    /// <param name="args">The prompt to initialize the effects of the current node.</param>
+    private void SetupGravityWell(EntityUid uid, GravityWellArtifactComponent comp, ArtifactNodeEnteredEvent args)
     {
-        if (HasComp<GravityWellComponent>(uid))
-        {
-            comp.EntityWasAlreadyAGravityWell = true;
+        GravityWellComponent? gravWell = null;
+        if(!Resolve(uid, ref gravWell))
             return;
-        }
 
-        var gravWell = AddComp<GravityWellComponent>(uid);
         gravWell.MinRange = _random.NextFloat(comp.MinRange.min, comp.MinRange.max);
         gravWell.MaxRange = _random.NextFloat(comp.MaxRange.min, comp.MaxRange.max);
         gravWell.BaseRadialAcceleration = _random.NextFloat(comp.RadialAcceleration.min, comp.RadialAcceleration.max);
@@ -38,14 +41,5 @@ public sealed class GravityWellArtifactSystem : EntitySystem
             TimeSpan.FromSeconds(_random.NextFloat((float)comp.PulsePeriod.min.TotalSeconds, (float)comp.PulsePeriod.max.TotalSeconds)),
             gravWell
         );
-    }
-
-    private void ShutdownGravityWell(EntityUid uid, GravityWellArtifactComponent comp, ComponentShutdown args)
-    {
-        if (comp.EntityWasAlreadyAGravityWell)
-            return;
-
-        /// Can't source-track so just assume that we are reponsible for making the entity a gravity well.
-        RemComp<GravityWellComponent>(uid);
     }
 }
