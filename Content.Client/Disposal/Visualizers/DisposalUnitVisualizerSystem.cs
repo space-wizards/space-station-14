549cfbd159e312ad4fc824460e387976de58e035
Eoin Mcloughlin <helloworld@eoinrul.es>
2023-01-06 18:20:18 +0000

    Remove warnings from DisposalUnitVisualizer
    Break up DisposalUnitVisualizer into a VisualizerSystem; add the
    previous visualizer's settings to DisposalUnitComponent.

---
+++ Content.Client/Disposal/Visualizers/DisposalUnitVisualizerSystem.cs
@@ -0,0 +1,141 @@
+using Content.Client.Disposal.Components;
+using Robust.Client.Animations;
+using Robust.Client.GameObjects;
+using static Content.Shared.Disposal.Components.SharedDisposalUnitComponent;
+
+namespace Content.Client.Disposal.Visualizers
+{
+    public sealed class DisposalUnitVisualizerSystem : VisualizerSystem<DisposalUnitComponent>
+    {
+        [Dependency] private readonly SharedAudioSystem SoundSystem = default!;
+        private const string AnimationKey = "disposal_unit_animation";
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<DisposalUnitComponent, ComponentInit>(OnComponentInit);
+        }
+
+        private void OnComponentInit(EntityUid uid, DisposalUnitComponent disposalUnit, ComponentInit args)
+        {
+            disposalUnit.FlushAnimation = new Animation {
+                Length = TimeSpan.FromSeconds(disposalUnit.FlushTime)
+            };
+
+            var flick = new AnimationTrackSpriteFlick();
+            disposalUnit.FlushAnimation.AnimationTracks.Add(flick);
+            flick.LayerKey = DisposalUnitVisualLayers.Base;
+            flick.KeyFrames.Add(new AnimationTrackSpriteFlick.KeyFrame(disposalUnit.StateFlush, 0));
+
+            var sound = new AnimationTrackPlaySound();
+            disposalUnit.FlushAnimation.AnimationTracks.Add(sound);
+
+            sound.KeyFrames.Add(new AnimationTrackPlaySound.KeyFrame(SoundSystem.GetSound(disposalUnit.FlushSound), 0));
+
+            var entities = IoCManager.Resolve<IEntityManager>();
+            entities.EnsureComponent<AnimationPlayerComponent>(uid);
+            var appearance = entities.EnsureComponent<ClientAppearanceComponent>(uid);
+
+            if (entities.TryGetComponent(uid, out ISpriteComponent? sprite) && sprite != null)
+            {
+                UpdateState(uid, disposalUnit, sprite);
+            }
+        }
+
+        protected override void OnAppearanceChange(EntityUid uid, DisposalUnitComponent unit, ref AppearanceChangeEvent args)
+        {
+            if (args.Sprite == null)
+            {
+                return;
+            }
+
+            UpdateState(uid, unit, args.Sprite);
+        }
+
+        // Update visuals and tick animation
+        private void UpdateState(EntityUid uid, DisposalUnitComponent unit, ISpriteComponent sprite)
+        {
+            if (!AppearanceSystem.TryGetData(uid, Visuals.VisualState, out VisualState state))
+            {
+                return;
+            }
+
+            switch (state)
+            {
+                case VisualState.UnAnchored:
+                    sprite.LayerSetState(DisposalUnitVisualLayers.Base, unit.StateUnAnchored);
+                    break;
+                case VisualState.Anchored:
+                    sprite.LayerSetState(DisposalUnitVisualLayers.Base, unit.StateAnchored);
+                    break;
+                case VisualState.Charging:
+                    sprite.LayerSetState(DisposalUnitVisualLayers.Base, unit.StateCharging);
+                    break;
+                case VisualState.Flushing:
+                    sprite.LayerSetState(DisposalUnitVisualLayers.Base, unit.StateAnchored);
+
+                    var entities = IoCManager.Resolve<IEntityManager>();
+                    var animPlayer = entities.GetComponent<AnimationPlayerComponent>(uid);
+
+                    if (!AnimationSystem.HasRunningAnimation(uid, AnimationKey))
+                    {
+                        AnimationSystem.Play(uid, unit.FlushAnimation, AnimationKey);
+                    }
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            if (!AppearanceSystem.TryGetData(uid, Visuals.Handle, out HandleState handleState))
+            {
+                handleState = HandleState.Normal;
+            }
+
+            sprite.LayerSetVisible(DisposalUnitVisualLayers.Handle, handleState != HandleState.Normal);
+
+            switch (handleState)
+            {
+                case HandleState.Normal:
+                    break;
+                case HandleState.Engaged:
+                    sprite.LayerSetState(DisposalUnitVisualLayers.Handle, unit.OverlayEngaged);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            if (!AppearanceSystem.TryGetData(uid, Visuals.Light, out LightState lightState))
+            {
+                lightState = LightState.Off;
+            }
+
+            sprite.LayerSetVisible(DisposalUnitVisualLayers.Light, lightState != LightState.Off);
+
+            switch (lightState)
+            {
+                case LightState.Off:
+                    break;
+                case LightState.Charging:
+                    sprite.LayerSetState(DisposalUnitVisualLayers.Light, unit.OverlayCharging);
+                    break;
+                case LightState.Full:
+                    sprite.LayerSetState(DisposalUnitVisualLayers.Light, unit.OverlayFull);
+                    break;
+                case LightState.Ready:
+                    sprite.LayerSetState(DisposalUnitVisualLayers.Light, unit.OverlayReady);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public enum DisposalUnitVisualLayers : byte
+    {
+        Base,
+        Handle,
+        Light
+    }
+}
