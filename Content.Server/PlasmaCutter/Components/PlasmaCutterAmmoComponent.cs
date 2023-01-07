12a2375be79687c5848a542b272fdb9bd4ee8ed7
Abobadadada <98908861+Abobadadada@users.noreply.github.com>
2023-01-07 08:36:24 +0200

    dsad

--- Content.Server/PlasmaCutter/Components/PlasmaCutterAmmoComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Server.PlasmaCutter.Components
-{
-    [RegisterComponent]
-    public sealed class PlasmaCutterAmmoComponent : Component
-    {
-        //How much ammo we refill
-        [ViewVariables(VVAccess.ReadWrite)][DataField("refillAmmo")] public int RefillAmmo = 10;
-    }
-}
