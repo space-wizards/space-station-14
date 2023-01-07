43ee7a6786e4960712f71d39d7d5cfde9e8b443e
nikthechampiongr <nikvog4@gmail.com>
2023-01-07 16:54:43 +0200

    Changed InCrit into DamageStateCondition

--- Content.Server/Chemistry/ReagentEffectConditions/DamageStateCondition.cs
+++ Content.Server/Chemistry/ReagentEffectConditions/DamageStateCondition.cs
@@ -5,16 +5,19 @@ using Content.Shared.MobState.EntitySystems;
 
 namespace Content.Server.Chemistry.ReagentEffectConditions
 {
-    public sealed class InCrit : ReagentEffectCondition
+    public sealed class DamageStateCondition : ReagentEffectCondition
     {
 
         [Dependency] private readonly SharedMobStateSystem _mobState = default!;
+
+        [DataField("damagestate")]
+        public DamageState DamageState = DamageState.Alive;
 
         public override bool Condition(ReagentEffectArgs args)
         {
             if (args.EntityManager.TryGetComponent(args.SolutionEntity, out MobStateComponent? mobState))
             {
-                if (mobState.CurrentState == DamageState.Critical)
+                if (mobState.CurrentState == DamageState)
                     return true;
             }
 
