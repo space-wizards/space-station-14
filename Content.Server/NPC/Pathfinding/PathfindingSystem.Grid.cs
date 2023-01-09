243fd73db4dcacfdb125159f43e5a331742977db
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2023-01-09 13:45:21 +1100

    Update tilechangedevent to ref structs (#13383)

--- Content.Server/NPC/Pathfinding/PathfindingSystem.Grid.cs
+++ Content.Server/NPC/Pathfinding/PathfindingSystem.Grid.cs
@@ -53,7 +53,7 @@ public sealed partial class PathfindingSystem
         SubscribeLocalEvent<MoveEvent>(OnMoveEvent);
     }
 
-    private void OnTileChange(TileChangedEvent ev)
+    private void OnTileChange(ref TileChangedEvent ev)
     {
         if (ev.OldTile.IsEmpty == ev.NewTile.Tile.IsEmpty)
             return;
