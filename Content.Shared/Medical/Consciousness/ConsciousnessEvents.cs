51f3cee46204e08a649ae795ff83e9c347dd56e7
Jezithyr <Jezithyr@gmail.com>
2023-01-10 10:38:14 -0800

    Cleanup/simplification of consciousness update event

--- Content.Shared/Medical/Consciousness/ConsciousnessEvents.cs
+++ Content.Shared/Medical/Consciousness/ConsciousnessEvents.cs
@@ -1,18 +1,10 @@
 ﻿using Content.Shared.FixedPoint;
+using Content.Shared.Medical.Consciousness.Components;
 
 namespace Content.Shared.Medical.Consciousness;
 
 [ByRefEvent]
-public record struct UpdateBaseConsciousnessEvent(FixedPoint2 Base);
-
-[ByRefEvent]
-public record struct UpdateConsciousnessModifierEvent(FixedPoint2 Modifier);
-
-[ByRefEvent]
-public record struct UpdateConsciousnessOffsetEvent(FixedPoint2 Offset);
-
-[ByRefEvent]
-public record struct UpdateConsciousnessCapEvent(FixedPoint2 Cap);
+public record struct UpdateConsciousnessValuesEvent(ConsciousnessComponent Component);
 
 [ByRefEvent]
 public record struct ConsciousnessUpdateEvent(bool IsConscious, FixedPoint2 Consciousness);
--- Content.Shared/Medical/Consciousness/Systems/ConsciousnessSystem.cs
+++ Content.Shared/Medical/Consciousness/Systems/ConsciousnessSystem.cs
@@ -23,11 +23,11 @@ public sealed class ConsciousnessSystem : EntitySystem
         {
             if (args.Component.CurrentState == MobState.Alive && args.Component.CurrentState != MobState.Dead)
             {
-                args.Component.CurrentState = MobState.Critical;
+                args.State = MobState.Critical;
             }
             return;
         }
-        args.Component.CurrentState = MobState.Alive;
+        args.State = MobState.Alive;
     }
 
     private void OnComponentGetState(EntityUid uid, ConsciousnessComponent component, ref ComponentGetState args)
@@ -76,54 +76,16 @@ public sealed class ConsciousnessSystem : EntitySystem
         return consciousnessValue > consciousness.PassOutThreshold;
     }
 
-    public void UpdateBaseConsciousness(EntityUid entity, FixedPoint2 baseValue,
+    public void UpdateConsciousnessValues(EntityUid entity,
         ConsciousnessComponent? consciousness = null)
     {
         if (!Resolve(entity, ref consciousness))
             return;
 
-        var ev = new UpdateBaseConsciousnessEvent(baseValue);
+        var ev = new UpdateConsciousnessValuesEvent(consciousness);
         RaiseLocalEvent(entity, ref ev);
-        consciousness.Base = ev.Base;
         CheckConsciousness(entity, consciousness);
     }
-
-    public void UpdateConsciousnessModifier(EntityUid entity, FixedPoint2 modifier,
-        ConsciousnessComponent? consciousness = null)
-    {
-        if (!Resolve(entity, ref consciousness))
-            return;
-
-        var ev = new UpdateConsciousnessModifierEvent(modifier);
-        RaiseLocalEvent(entity, ref ev);
-        consciousness.Modifier = ev.Modifier;
-        CheckConsciousness(entity, consciousness);
-    }
-
-    public void UpdateConsciousnessOffset(EntityUid entity, FixedPoint2 offset,
-        ConsciousnessComponent? consciousness = null)
-    {
-        if (!Resolve(entity, ref consciousness))
-            return;
-
-        var ev = new UpdateConsciousnessOffsetEvent(offset);
-        RaiseLocalEvent(entity, ref ev);
-        consciousness.Offset = ev.Offset;
-        CheckConsciousness(entity, consciousness);
-    }
-
-    public void UpdateConsciousnessCap(EntityUid entity, FixedPoint2 cap,
-        ConsciousnessComponent? consciousness = null)
-    {
-        if (!Resolve(entity, ref consciousness))
-            return;
-
-        var ev = new UpdateConsciousnessCapEvent(cap);
-        RaiseLocalEvent(entity, ref ev);
-        consciousness.Cap = ev.Cap;
-        CheckConsciousness(entity, consciousness);
-    }
-
     private void CheckConsciousness(EntityUid entity, ConsciousnessComponent consciousness)
     {
         var isConscious = IsConscious(entity, out var consciousnessValue, consciousness);
