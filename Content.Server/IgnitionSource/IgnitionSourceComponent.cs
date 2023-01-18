2aad589f6decfa90d38da1ae7342f87d9a491f35
Gus <august.eymann@gmail.com>
2023-01-18 00:30:22 -0800

    stupid typos

--- Content.Server/IgnitionSource/IgnitionSourceComponent.cs
+++ Content.Server/IgnitionSource/IgnitionSourceComponent.cs
@@ -10,6 +10,6 @@ public sealed class IgnitionSourceComponent : Component
     [DataField("ignited")]
     public bool Ignited = false;
 
-    [DataField("temperature", required = true)]
+    [DataField("temperature", required: true)]
     public int Temperature;
 }
