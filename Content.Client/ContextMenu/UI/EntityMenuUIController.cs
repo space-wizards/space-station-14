64dac5f6252e0d287b2cf60f6b5f89ba69f8ab9b
ElectroJr <leonsfriedrich@gmail.com>
2023-01-18 00:49:33 -0500

    Fix nested context menu popup

--- Content.Client/ContextMenu/UI/EntityMenuUIController.cs
+++ Content.Client/ContextMenu/UI/EntityMenuUIController.cs
@@ -260,6 +260,8 @@ namespace Content.Client.ContextMenu.UI
             {
                 var subElement = new EntityMenuElement(entity);
                 subElement.SubMenu = new ContextMenuPopup(_context, subElement);
+                subElement.SubMenu.OnPopupOpen += () => _verb.OpenVerbMenu(group[0], popup: subElement.SubMenu);
+                subElement.SubMenu.OnPopupHide += subElement.SubMenu.MenuBody.DisposeAllChildren;
                 _context.AddElement(subMenu, subElement);
                 Elements.TryAdd(entity, subElement);
             }
