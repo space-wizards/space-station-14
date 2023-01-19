e772eeaed9f7d7ee27afed3ce36222f9056b345b
Mirino97 <j.cesarueti@yahoo.com.br>
2023-01-18 22:10:19 -0300

    Adds toggle aghost command file and visflag for aghosts

---
+++ Content.Server/Administration/Commands/ToggleAghostCommand.cs
@@ -0,0 +1,38 @@
+﻿using Content.Server.Ghost.Components;
+using Content.Server.Visible;
+using Content.Shared.Administration;
+using Robust.Server.GameObjects;
+using Robust.Shared.Console;
+
+namespace Content.Server.Administration.Commands;
+
+[AdminCommand(AdminFlags.Admin)]
+public sealed class ToggleAghostVisibility : IConsoleCommand
+{
+    public string Command => "toggleaghost";
+    public string Description => "Shows or hides your aghost from other players";
+    public string Help => $"{Command}";
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (shell.Player == null)
+            shell.WriteLine("You can only toggle ghost visibility on a client.");
+
+        var entityManager = IoCManager.Resolve<IEntityManager>();
+
+        var uid = shell.Player?.AttachedEntity;
+        if (uid == null
+            || !entityManager.HasComponent<GhostComponent>(uid)
+            || !entityManager.TryGetComponent<EyeComponent>(uid, out var eyeComponent)
+            || !entityManager.TryGetComponent(uid, out VisibilityComponent? visibility))
+            return;
+
+        var _visibilitySystem = entityManager.EntitySysManager.GetEntitySystem<VisibilitySystem>();
+
+        if (visibility.Layer == (int) VisibilityFlags.Ghost)
+            _visibilitySystem.SetLayer(visibility, (int) VisibilityFlags.Aghost);
+        else
+            _visibilitySystem.SetLayer(visibility, (int) VisibilityFlags.Ghost);
+
+
+    }
+}
--- Content.Server/Visible/VisibilityFlags.cs
+++ Content.Server/Visible/VisibilityFlags.cs
@@ -6,5 +6,6 @@ namespace Content.Server.Visible
         None   = 0,
         Normal = 1 << 0,
         Ghost  = 1 << 1,
+        Aghost = 1 << 2,
     }
 }
