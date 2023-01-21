09ec9ff0014d1e8f2e10f4fd4f0b1ba33ab47f69
ElectroJr <leonsfriedrich@gmail.com>
2023-01-21 02:17:38 -0500

    do the other log too

--- Content.Shared/Hands/EntitySystems/SharedHandsSystem.Pickup.cs
+++ Content.Shared/Hands/EntitySystems/SharedHandsSystem.Pickup.cs
@@ -139,7 +139,7 @@ public abstract partial class SharedHandsSystem : EntitySystem
 
         if (!handContainer.Insert(entity, EntityManager))
         {
-            Logger.Error($"Failed to insert {ToPrettyString(entity)} into users hand container. User: {ToPrettyString(uid)}. Hand: {hand.Name}.");
+            Logger.Error($"Failed to insert {ToPrettyString(entity)} into users hand container when picking up. User: {ToPrettyString(uid)}. Hand: {hand.Name}.");
             return;
         }
 
