c9736a500f1e12e771c9e0e0beb20a993418d76f
Morbo <14136326+Morb0@users.noreply.github.com>
2023-01-21 16:37:44 +0300

    Add space, question & exclamation mark

--- Content.Server/Corvax/TTS/TTSSystem.Sanitize.cs
+++ Content.Server/Corvax/TTS/TTSSystem.Sanitize.cs
@@ -16,7 +16,7 @@ public sealed partial class TTSSystem
     private string Sanitize(string text)
     {
         text = text.Trim();
-        text = Regex.Replace(text, @"[^a-zA-Zа-яА-ЯёЁ0-9,\-,+]", "");
+        text = Regex.Replace(text, @"[^a-zA-Zа-яА-ЯёЁ0-9,\-,+, ,?,!]", "");
         text = Regex.Replace(text, @"[a-zA-Z]", ReplaceLat2Cyr, RegexOptions.Multiline | RegexOptions.IgnoreCase);
         text = Regex.Replace(text, @"(?<![a-zA-Zа-яёА-ЯЁ])[a-zA-Zа-яёА-ЯЁ]+?(?![a-zA-Zа-яёА-ЯЁ])", ReplaceMatchedWord, RegexOptions.Multiline | RegexOptions.IgnoreCase);
         text = Regex.Replace(text, @"(?<=[1-90])(\.|,)(?=[1-90])", " целых ");
