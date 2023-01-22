cac5f4cb4c796e89d7dcb3fda53342ee0d5ab4cb
AlexMorgan3817 <Ivan.Zvereff@gmail.com>
2023-01-22 12:58:03 +0700

    Moving to Shared

--- Content.Server/Radio/EntitySystems/HeadsetSystem.cs
+++ Content.Server/Radio/EntitySystems/HeadsetSystem.cs
@@ -114,35 +114,13 @@ public sealed class HeadsetSystem : EntitySystem
         else if (component.Channels.Count > 0)
         {
             args.PushMarkup(Loc.GetString("examine-headset-channels-prefix"));
-            HeadsetSystem.GetChannelsExamine(component.Channels, args, _protoManager, "examine-headset-channel");
+            EncryptionKeySystem.GetChannelsExamine(component.Channels, args, _protoManager, "examine-headset-channel");
             args.PushMarkup(Loc.GetString("examine-headset-chat-prefix", ("prefix", ":h")));
             if (component.DefaultChannel != null)
             {
                 var proto = _protoManager.Index<RadioChannelPrototype>(component.DefaultChannel);
                 args.PushMarkup(Loc.GetString("examine-headset-default-channel", ("channel", component.DefaultChannel), ("color", proto.Color)));
             }
-        }
-    }
-
-    /// <summary>
-    ///     A static method for formating list of radio channels for examine events.
-    /// </summary>
-    /// <param name="channels">HashSet of channels in headset, encryptionkey or etc.</param>
-    /// <param name="protoManager">IPrototypeManager for getting prototypes of channels with their variables.</param>
-    /// <param name="channelFTLPattern">String that provide id of pattern in .ftl files to format channel with variables of it.</param>
-    public static void GetChannelsExamine(HashSet<string> channels, ExaminedEvent examineEvent, IPrototypeManager protoManager, string channelFTLPattern)
-    {
-        foreach (var id in channels)
-        {
-            var proto = protoManager.Index<RadioChannelPrototype>(id);
-            string keyCode = "" + proto.KeyCode;
-            if (id != "Common")
-                keyCode = ":" + keyCode;
-            examineEvent.PushMarkup(Loc.GetString(channelFTLPattern,
-                ("color", proto.Color),
-                ("key", keyCode),
-                ("id", proto.LocalizedName),
-                ("freq", proto.Frequency)));
         }
     }
 
