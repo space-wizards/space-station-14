a837d44a1e8fe05d5da948cb4793e2977c8636bf
Rane <elijahrane@gmail.com>
2023-01-23 18:52:14 -0500

    update wording

--- Content.Client/Chemistry/Visualizers/SolutionContainerVisualsSystem.cs
+++ Content.Client/Chemistry/Visualizers/SolutionContainerVisualsSystem.cs
@@ -23,7 +23,7 @@ public sealed class SolutionContainerVisualsSystem : VisualizerSystem<SolutionCo
         // a giant error sign and error for debug.
         if (fraction > 1f)
         {
-            Logger.Error("Attempted to set solution container visuals volume ratio on " + ToPrettyString(uid) + " to a value greater than 100% of it's max volume.");
+            Logger.Error("Attempted to set solution container visuals volume ratio on " + ToPrettyString(uid) + " to a value greater than 1. Volume should never be greater than max volume!");
             fraction = 1f;
         }
 
