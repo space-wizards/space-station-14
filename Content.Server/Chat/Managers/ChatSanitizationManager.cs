0fb1889dfc1487323baefb71378b0303815cca37
lapatison <100279397+lapatison@users.noreply.github.com>
2023-01-30 05:17:06 +0300

    Add more chat sanitization (#704)

--- Content.Server/Chat/Managers/ChatSanitizationManager.cs
+++ Content.Server/Chat/Managers/ChatSanitizationManager.cs
@@ -19,7 +19,9 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
         { "о/", "chatsan-waves" }, // cyrillic о
         { "о7", "chatsan-salutes" }, // cyrillic о
         { "0_o", "chatsan-wide-eyed" },
-        { "лол", "chatsan-laughs" },
+        { "лмао", "chatsan-laughs" },
+        { "рофл", "chatsan-laughs" },
+        { "яхз", "chatsan-shrugs" },
         // Corvax-Localization-End
         // I could've done this with regex, but felt it wasn't the right idea.
         { ":)", "chatsan-smiles" },
