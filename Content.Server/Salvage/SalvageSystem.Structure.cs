719717182e4082e3b2806c3fc22ef53b706bc73b
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-02-02 00:14:45 +1100

    meth

--- Content.Server/Salvage/SalvageSystem.Structure.cs
+++ Content.Server/Salvage/SalvageSystem.Structure.cs
@@ -1,5 +1,6 @@
 using Content.Server.Procedural;
 using Content.Server.Salvage.Expeditions;
+using Content.Server.Salvage.Expeditions.Structure;
 using Robust.Shared.Map.Components;
 
 namespace Content.Server.Salvage;
@@ -11,6 +12,18 @@ public sealed partial class SalvageSystem
         // TODO: Move this to the main method
         // TODO: Pass in the pre-built rooms and shit and then spawn via that.
         // TODO: Dungeon likely needs a start room or smth.
+        switch (mission)
+        {
+            case SalvageStructure structure:
+                SetupMission(structure, dungeon, grid, random);
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+    }
 
+    private void SetupMission(SalvageStructure structure, Dungeon dungeon, MapGridComponent grid, Random random)
+    {
+        // TODO: Landing around dungeon + pathfind into it.
     }
 }
