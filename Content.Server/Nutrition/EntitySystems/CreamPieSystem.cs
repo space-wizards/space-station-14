c60ca70bc8fa6b69dfbe2a140eca2b1d5ca32c71
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-02-02 21:43:07 +0300

    refactor CreamPieSystem (no obsolete warnings)

--- Content.Server/Nutrition/EntitySystems/CreamPieSystem.cs
+++ Content.Server/Nutrition/EntitySystems/CreamPieSystem.cs
@@ -4,15 +4,14 @@ using Content.Server.Explosion.EntitySystems;
 using Content.Server.Fluids.EntitySystems;
 using Content.Server.Nutrition.Components;
 using Content.Server.Popups;
-using Content.Shared.Audio;
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Interaction;
 using Content.Shared.Nutrition.Components;
 using Content.Shared.Nutrition.EntitySystems;
-using Content.Shared.Popups;
 using Content.Shared.Rejuvenate;
 using Content.Shared.Throwing;
 using JetBrains.Annotations;
+using Robust.Server.GameObjects;
 using Robust.Shared.Audio;
 using Robust.Shared.Player;
 
@@ -25,6 +24,8 @@ namespace Content.Server.Nutrition.EntitySystems
         [Dependency] private readonly SpillableSystem _spillable = default!;
         [Dependency] private readonly ItemSlotsSystem _itemSlots = default!;
         [Dependency] private readonly TriggerSystem _trigger = default!;
+        [Dependency] private readonly SharedAudioSystem _audio = default!;
+        [Dependency] private readonly PopupSystem _popup = default!;
 
         public override void Initialize()
         {
@@ -36,11 +37,11 @@ namespace Content.Server.Nutrition.EntitySystems
 
         protected override void SplattedCreamPie(EntityUid uid, CreamPieComponent creamPie)
         {
-            SoundSystem.Play(creamPie.Sound.GetSound(), Filter.Pvs(creamPie.Owner), creamPie.Owner, AudioHelpers.WithVariation(0.125f));
+            _audio.Play(_audio.GetSound(creamPie.Sound), Filter.Pvs(uid), uid, false, new AudioParams().WithVariation(0.125f));
 
-            if (EntityManager.TryGetComponent<FoodComponent?>(creamPie.Owner, out var foodComp) && _solutions.TryGetSolution(creamPie.Owner, foodComp.SolutionName, out var solution))
+            if (EntityManager.TryGetComponent<FoodComponent?>(uid, out var foodComp) && _solutions.TryGetSolution(uid, foodComp.SolutionName, out var solution))
             {
-                _spillable.SpillAt(creamPie.Owner, solution, "PuddleSmear", false);
+                _spillable.SpillAt(uid, solution, "PuddleSmear", false);
             }
             ActivatePayload(uid);
 
@@ -74,8 +75,13 @@ namespace Content.Server.Nutrition.EntitySystems
 
         protected override void CreamedEntity(EntityUid uid, CreamPiedComponent creamPied, ThrowHitByEvent args)
         {
-            creamPied.Owner.PopupMessage(Loc.GetString("cream-pied-component-on-hit-by-message",("thrower", args.Thrown)));
-            creamPied.Owner.PopupMessageOtherClients(Loc.GetString("cream-pied-component-on-hit-by-message-others", ("owner", creamPied.Owner),("thrower", args.Thrown)));
+            _popup.PopupEntity(Loc.GetString("cream-pied-component-on-hit-by-message",("thrower", args.Thrown)), uid, args.Target);
+            var otherPlayers = Filter.Empty().AddPlayersByPvs(uid);
+            if (TryComp<ActorComponent>(args.Target, out var actor)) 
+            {
+                otherPlayers.RemovePlayer(actor.PlayerSession);
+            }
+            _popup.PopupEntity(Loc.GetString("cream-pied-component-on-hit-by-message-others", ("owner", uid),("thrower", args.Thrown)), uid, otherPlayers, false);
         }
 
         private void OnRejuvenate(EntityUid uid, CreamPiedComponent component, RejuvenateEvent args)
