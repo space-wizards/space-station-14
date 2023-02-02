e0bc870a3cb4b841261a560622be2ffef32f8357
deltanedas <user@zenith>
2023-02-02 20:10:23 +0000

    emagged the fax while i was there

--- Content.Server/Fax/FaxSystem.cs
+++ Content.Server/Fax/FaxSystem.cs
@@ -25,6 +25,7 @@ namespace Content.Server.Fax;
 public sealed class FaxSystem : EntitySystem
 {
     [Dependency] private readonly IChatManager _chat = default!;
+    [Dependency] private readonly EmagSystem _emagSystem = default!;
     [Dependency] private readonly IAdminManager _adminManager = default!;
     [Dependency] private readonly ItemSlotsSystem _itemSlotsSystem = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearanceSystem = default!;
@@ -210,7 +211,7 @@ public sealed class FaxSystem : EntitySystem
                 return;
             }
 
-            if (component.KnownFaxes.ContainsValue(newName) && !component.Emagged) // Allow exist names if emagged for fun
+            if (component.KnownFaxes.ContainsValue(newName) && !_emagSystem.IsEmagged(uid)) // Allow existing names if emagged for fun
             {
                 _popupSystem.PopupEntity(Loc.GetString("fax-machine-popup-name-exist"), uid);
                 return;
@@ -228,11 +229,7 @@ public sealed class FaxSystem : EntitySystem
 
     private void OnEmagged(EntityUid uid, FaxMachineComponent component, ref GotEmaggedEvent args)
     {
-        if (component.Emagged)
-            return;
-
         _audioSystem.PlayPvs(component.EmagSound, uid);
-        component.Emagged = true;
         args.Handled = true;
     }
 
@@ -246,7 +243,7 @@ public sealed class FaxSystem : EntitySystem
             switch (command)
             {
                 case FaxConstants.FaxPingCommand:
-                    var isForSyndie = component.Emagged &&
+                    var isForSyndie = _emagSystem.IsEmagged(uid) &&
                                       args.Data.ContainsKey(FaxConstants.FaxSyndicateData);
                     if (!isForSyndie && !component.ResponsePings)
                         return;
@@ -362,7 +359,7 @@ public sealed class FaxSystem : EntitySystem
             { DeviceNetworkConstants.Command, FaxConstants.FaxPingCommand }
         };
 
-        if (component.Emagged)
+        if (_emagSystem.IsEmagged(uid))
             payload.Add(FaxConstants.FaxSyndicateData, true);
 
         _deviceNetworkSystem.QueuePacket(uid, null, payload);
