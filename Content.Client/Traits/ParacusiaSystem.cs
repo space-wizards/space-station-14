8a245a4924b01b2ad02f3ab933cd321b212d21ea
Yasumi <scribbles0@outlook.com>
2023-02-03 00:45:39 -0800

    finally network paracusia

--- Content.Client/Traits/ParacusiaSystem.cs
+++ Content.Client/Traits/ParacusiaSystem.cs
@@ -20,34 +20,12 @@ public sealed class ParacusiaSystem : EntitySystem
     public override void Initialize()
     {
         SubscribeLocalEvent<ParacusiaComponent, ComponentStartup>(SetupParacusia);
-        SubscribeLocalEvent<ParacusiaComponent, ComponentGetState>(GetCompState);
-        SubscribeLocalEvent<ParacusiaComponent, ComponentHandleState>(HandleCompState);
     }
 
     private void SetupParacusia(EntityUid uid, ParacusiaComponent component, ComponentStartup args)
     {
         component.NextIncidentTime =
             _random.NextFloat(component.MinTimeBetweenIncidents, component.MaxTimeBetweenIncidents);
-    }
-
-    private void GetCompState(EntityUid uid, ParacusiaComponent component, ref ComponentGetState args)
-    {
-        args.State = new ParacusiaComponentState
-        {
-            MaxTimeBetweenIncidents = component.MaxTimeBetweenIncidents,
-            MinTimeBetweenIncidents = component.MinTimeBetweenIncidents,
-            MaxSoundDistance = component.MaxSoundDistance,
-            Sounds = component.Sounds,
-        };
-    }
-
-    private void HandleCompState(EntityUid uid, ParacusiaComponent component, ref ComponentHandleState args)
-    {
-        if (args.Current is not ParacusiaComponentState state) return;
-        component.MaxTimeBetweenIncidents = state.MaxTimeBetweenIncidents;
-        component.MinTimeBetweenIncidents = state.MinTimeBetweenIncidents;
-        component.MaxSoundDistance = state.MaxSoundDistance;
-        component.Sounds = state.Sounds;
     }
 
     public override void Update(float frameTime)
---
+++ Content.Shared/Traits/Assorted/ParacusiaSystem.cs
@@ -0,0 +1,32 @@
+using Robust.Shared.GameStates;
+
+
+namespace Content.Shared.Traits.Assorted;
+public sealed class ParacusiaSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ParacusiaComponent, ComponentGetState>(GetCompState);
+        SubscribeLocalEvent<ParacusiaComponent, ComponentHandleState>(HandleCompState);
+    }
+    private void GetCompState(EntityUid uid, ParacusiaComponent component, ref ComponentGetState args)
+    {
+        args.State = new ParacusiaComponentState
+        {
+            MaxTimeBetweenIncidents = component.MaxTimeBetweenIncidents,
+            MinTimeBetweenIncidents = component.MinTimeBetweenIncidents,
+            MaxSoundDistance = component.MaxSoundDistance,
+            Sounds = component.Sounds,
+        };
+        Dirty(component);
+    }
+
+    private void HandleCompState(EntityUid uid, ParacusiaComponent component, ref ComponentHandleState args)
+    {
+        if (args.Current is not ParacusiaComponentState state) return;
+        component.MaxTimeBetweenIncidents = state.MaxTimeBetweenIncidents;
+        component.MinTimeBetweenIncidents = state.MinTimeBetweenIncidents;
+        component.MaxSoundDistance = state.MaxSoundDistance;
+        component.Sounds = state.Sounds;
+    }
+}
