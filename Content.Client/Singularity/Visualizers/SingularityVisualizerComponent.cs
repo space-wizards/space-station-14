95c7e02b8d7cbcfca314533c7ce2ea099d2d44b2
TemporalOroboros <TemporalOroboros@gmail.com>
2023-02-02 22:02:27 -0800

    Split SingularityVisualizer into Component/System pair

---
+++ Content.Client/Singularity/Visualizers/SingularityVisualizerComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Client.Singularity.Visualizers;
+
+[RegisterComponent]
+[Access(typeof(SingularityVisualizerSystem))]
+public sealed class SingularityVisualizerComponent : Component
+{
+    [DataField("layer")]
+    public int Layer { get; } = 0;
+}
