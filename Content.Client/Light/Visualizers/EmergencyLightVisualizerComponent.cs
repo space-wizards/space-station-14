201219b0f0212fe887bc849578a0961768c1ebfc
TemporalOroboros <TemporalOroboros@gmail.com>
2023-02-02 21:03:26 -0800

    Split EmergencyLightVisualizer into Component/System pair

---
+++ Content.Client/Light/Visualizers/EmergencyLightVisualizerComponent.cs
@@ -0,0 +1,6 @@
+﻿namespace Content.Client.Light.Visualizers;
+
+[RegisterComponent]
+[Access(typeof(EmergencyLightVisualizerSystem))]
+public sealed class EmergencyLightVisualizerComponent : Component
+{}
---
+++ Content.Client/Light/Visualizers/EmergencyLightVisualizerSystem.cs
@@ -0,0 +1,30 @@
+﻿using Content.Shared.Light.Component;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Light.Visualizers;
+
+public sealed class EmergencyLightVisualizerSystem : VisualizerSystem<EmergencyLightVisualizerComponent>
+{
+    protected override void OnAppearanceChange(EntityUid uid, EmergencyLightVisualizerComponent comp, ref AppearanceChangeEvent args)
+    {
+        if (args.Sprite == null)
+            return;
+
+        if (!AppearanceSystem.TryGetData(uid, EmergencyLightVisuals.On, out bool on, args.Component))
+            on = false;
+
+        args.Sprite.LayerSetState(EmergencyLightVisualLayers.Light, on ? "emergency_light_on" : "emergency_light_off");
+        args.Sprite.LayerSetShader(EmergencyLightVisualLayers.Light, on ? "unshaded" : "shaded");
+
+        if (AppearanceSystem.TryGetData(uid, EmergencyLightVisuals.Color, out Color color, args.Component))
+        {
+            args.Sprite.LayerSetColor(EmergencyLightVisualLayers.Light, color);
+        }
+    }
+}
+
+public enum EmergencyLightVisualLayers
+{
+    Base,
+    Light
+}
