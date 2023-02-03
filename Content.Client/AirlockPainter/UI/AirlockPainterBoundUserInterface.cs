d6f04b594387d50a70b9b0c2fd71472c6c68a9cf
jicksaw <jicksaw@pm.me>
2023-02-03 03:01:15 +0200

    Show the selected style after reopening the menu

--- Content.Client/AirlockPainter/UI/AirlockPainterBoundUserInterface.cs
+++ Content.Client/AirlockPainter/UI/AirlockPainterBoundUserInterface.cs
@@ -1,11 +1,14 @@
 using Content.Shared.AirlockPainter;
 using Robust.Client.GameObjects;
+using Robust.Client.UserInterface.Controls;
 
 namespace Content.Client.AirlockPainter.UI
 {
     public sealed class AirlockPainterBoundUserInterface : BoundUserInterface
     {
         private AirlockPainterWindow? _window;
+        private AirlockPainterSystem? _painter;
+
         public List<string> Styles = new();
 
         [Dependency] private readonly IEntitySystemManager _entitySystems = default!;
@@ -19,22 +22,33 @@ namespace Content.Client.AirlockPainter.UI
             base.Open();
 
             _window = new AirlockPainterWindow();
-            if (State != null)
-                UpdateState(State);
 
-            // Add styles
-            var painterSystem = _entitySystems.GetEntitySystem<AirlockPainterSystem>();
-            _window.Populate(painterSystem.Entries);
+            _painter = _entitySystems.GetEntitySystem<AirlockPainterSystem>();
 
             _window.OpenCentered();
-
             _window.OnClose += Close;
-            _window.OnSpritePicked += OnSpritePicked;
+            _window.OnSpritePicked = OnSpritePicked;
         }
 
-        private void OnSpritePicked(int index)
+        protected override void UpdateState(BoundUserInterfaceState state)
         {
-            SendMessage(new AirlockPainterSpritePickedMessage(index));
+            base.UpdateState(state);
+
+            if (_window == null)
+                return;
+
+            if (_painter == null)
+                return;
+
+            if (state is not AirlockPainterBoundUserInterfaceState stateCast)
+                return;
+
+            _window.Populate(_painter.Entries, stateCast.SelectedStyle);
+        }
+
+        private void OnSpritePicked(ItemList.ItemListSelectedEventArgs args)
+        {
+            SendMessage(new AirlockPainterSpritePickedMessage(args.ItemIndex));
         }
     }
 }
