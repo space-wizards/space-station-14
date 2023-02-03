8165bc964c3f0f1533f02f1181c58d4070e0d538
TemporalOroboros <TemporalOroboros@gmail.com>
2023-02-02 21:26:51 -0800

    Split CreamPiedVisualizer into Component/System pair

---
+++ Content.Client/Nutrition/Visualizers/CreamPiedVisualizerComponent.cs
@@ -0,0 +1,12 @@
+﻿using Content.Shared.Nutrition.Components;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Nutrition.Visualizers;
+
+[RegisterComponent]
+[Access(typeof(CreamPiedVisualizerSystem))]
+public sealed class CreamPiedVisualizerComponent : Component
+{
+    [DataField("state")]
+    public string? State;
+}
---
+++ Content.Client/Nutrition/Visualizers/CreamPiedVisualizerSystem.cs
@@ -0,0 +1,39 @@
+﻿using Content.Shared.Nutrition.Components;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Nutrition.Visualizers;
+
+public sealed class CreamPiedVisualizerSystem : VisualizerSystem<CreamPiedVisualizerComponent>
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<CreamPiedVisualizerComponent, ComponentInit>(OnInit);
+    }
+
+    private void OnInit(EntityUid uid, CreamPiedVisualizerComponent comp, ComponentInit args)
+    {
+        if(!TryComp<SpriteComponent>(uid, out var sprite))
+            return;
+
+        sprite.LayerMapReserveBlank(CreamPiedVisualLayers.Pie);
+        sprite.LayerSetRSI(CreamPiedVisualLayers.Pie, "Effects/creampie.rsi");
+        sprite.LayerSetVisible(CreamPiedVisualLayers.Pie, false);
+    }
+
+    protected override void OnAppearanceChange(EntityUid uid, CreamPiedVisualizerComponent comp, ref AppearanceChangeEvent args)
+    {
+        if (args.Sprite == null)
+            return;
+        if(!AppearanceSystem.TryGetData<bool>(uid, CreamPiedVisuals.Creamed, out var pied, args.Component))
+            return;
+        
+        args.Sprite.LayerSetVisible(CreamPiedVisualLayers.Pie, pied);
+        args.Sprite.LayerSetState(CreamPiedVisualLayers.Pie, comp.State);
+    }
+}
+
+public enum CreamPiedVisualLayers : byte
+{
+    Pie,
+}
