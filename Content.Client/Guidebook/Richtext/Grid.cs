06ae3ec0c33f33fe3c4e4b4986361169a222fa58
Naive817 <lyx2020@sjtu.edu.cn>
2023-02-04 10:05:06 +0800

    fixed grid.cs?

--- Content.Client/Guidebook/Richtext/Grid.cs
+++ Content.Client/Guidebook/Richtext/Grid.cs
@@ -8,22 +8,5 @@ public sealed class Grid : GridContainer, IDocumentTag
 {
     public bool TryParseTag(Dictionary<string, string> args, [NotNullWhen(true)] out Control? control)
     {
-        HorizontalExpand = true;
-        control = this;
-
-        if (args.TryGetValue("Orientation", out var orientation))
-            Orientation = Enum.Parse<LayoutOrientation>(orientation);
-        else
-            Orientation = LayoutOrientation.Horizontal;
-
-        if (args.TryGetValue("HorizontalAlignment", out var halign))
-            HorizontalAlignment = Enum.Parse<HAlignment>(halign);
-        else
-            HorizontalAlignment = HAlignment.Center;
-
-        if (args.TryGetValue("VerticalAlignment", out var valign))
-            VerticalAlignment = Enum.Parse<VAlignment>(valign);
-
-        return true;
     }
 }
