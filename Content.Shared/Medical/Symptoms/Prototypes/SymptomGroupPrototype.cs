7b9eb15ffeb67cd39cf1e330ec764bd2e7afc454
DrSmugleaf <drsmugleaf@gmail.com>
2023-02-04 02:33:17 +0100

    Add missing custom type serializer to symptom group symptom

--- Content.Shared/Medical/Symptoms/Prototypes/SymptomGroupPrototype.cs
+++ Content.Shared/Medical/Symptoms/Prototypes/SymptomGroupPrototype.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Immutable;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Set;
 
 namespace Content.Shared.Medical.Symptoms.Prototypes;
 
@@ -8,6 +9,6 @@ public sealed class SymptomGroupPrototype : IPrototype
 {
     [IdDataField] public string ID { get; init; } = string.Empty;
 
-    [DataField("symptoms", required: true)]
+    [DataField("symptoms", required: true, customTypeSerializer: typeof(PrototypeIdHashSetSerializer<EntityPrototype>))]
     public readonly ImmutableHashSet<string> Symptoms = ImmutableHashSet<string>.Empty;
 }
