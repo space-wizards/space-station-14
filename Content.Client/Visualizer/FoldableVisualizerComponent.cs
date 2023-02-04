396bdbde7df702d50ad1edbae12084ebbf014ef4
TemporalOroboros <TemporalOroboros@gmail.com>
2023-02-03 20:28:17 -0800

    GenericVisualizer

--- Content.Client/Visualizer/FoldableVisualizerComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Client.Visualizer;
-
-[RegisterComponent]
-[Access(typeof(FoldableVisualizerSystem))]
-public sealed class FoldableVisualizerComponent : Component
-{
-    [DataField("key")]
-    public string Key = default!;
-}
