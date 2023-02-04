0ab09d0d3e8e7522d3de8238db02c4606e7923a2
TemporalOroboros <TemporalOroboros@gmail.com>
2023-02-03 20:33:15 -0800

    GenericVisualizer

--- Content.Client/Visualizer/RollerbedVisualizerComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Client.Visualizer;
-
-[RegisterComponent]
-[Access(typeof(RollerbedVisualizerSystem))]
-public sealed class RollerbedVisualizerComponent : Component
-{
-    [DataField("key")]
-    public string Key = default!;
-}
