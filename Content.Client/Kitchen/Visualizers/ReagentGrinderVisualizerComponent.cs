4f70b7a1120c5e39911f1796cabed343e850fba6
TemporalOroboros <TemporalOroboros@gmail.com>
2023-02-04 14:02:39 -0800

    Generic visualizer

--- Content.Client/Kitchen/Visualizers/ReagentGrinderVisualizerComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Robust.Client.GameObjects;
-using Content.Shared.Kitchen;
-
-namespace Content.Client.Kitchen.Visualizers;
-
-[RegisterComponent]
-[Access(typeof(ReagentGrinderVisualizerSystem))]
-public sealed class ReagentGrinderVisualizerComponent : Component
-{}
