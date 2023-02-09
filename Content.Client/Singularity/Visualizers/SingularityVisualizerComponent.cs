db2e5168c66a8550f861db22f67f8d6e60897dd5
TemporalOroboros <TemporalOroboros@gmail.com>
2023-02-08 22:31:30 -0800

    Move to SingularityComponent

--- Content.Client/Singularity/Visualizers/SingularityVisualizerComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Content.Client.Singularity.Visualizers;
-
-[RegisterComponent]
-[Access(typeof(SingularityVisualizerSystem))]
-public sealed class SingularityVisualizerComponent : Component
-{
-    [DataField("layer")]
-    public int Layer { get; } = 0;
-}
