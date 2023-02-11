927eae3c64d6c92152441a1522bccbf0df51903b
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-02-11 11:47:11 +0300

    use file namespace

--- Content.Server/StationEvents/Events/SolarFlare.cs
+++ Content.Server/StationEvents/Events/SolarFlare.cs
@@ -5,82 +5,80 @@ using Robust.Shared.Random;
 using Content.Server.Light.EntitySystems;
 using Content.Server.Light.Components;
 
-namespace Content.Server.StationEvents.Events
+namespace Content.Server.StationEvents.Events;
+
+public sealed class SolarFlare : StationEventSystem
 {
-    public sealed class SolarFlare : StationEventSystem
+    [Dependency] private readonly PoweredLightSystem _poweredLight = default!;
+
+    public override string Prototype => "SolarFlare";
+
+    private bool _onlyJamHeadsets = true;
+    private HashSet<string> _affectedChannels = new();
+    private float _endAfter = 0.0f;
+    private bool _running = false;
+    private float _lightBurnChance = 0.0f;
+    private float _lightChangeColorChance = 0.0f;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly PoweredLightSystem _poweredLight = default!;
+        base.Initialize();
+        SubscribeLocalEvent<ActiveRadioComponent, RadioReceiveAttemptEvent>(OnRadioSendAttempt);
+    }
 
-        public override string Prototype => "SolarFlare";
+    public override void Added()
+    {
+        if (Configuration is not SolarFlareEventRuleConfiguration ev)
+            return;
+        base.Added();
+        _onlyJamHeadsets = ev.OnlyJamHeadsets;
+        _endAfter = RobustRandom.Next(ev.MinEndAfter, ev.MaxEndAfter);
+        _affectedChannels = ev.AffectedChannels;
+        _lightBurnChance = ev.LightBreakChance;
+    }
 
-        private bool _onlyJamHeadsets = true;
-        private HashSet<string> _affectedChannels = new();
-        private float _endAfter = 0.0f;
-        private bool _running = false;
-        private float _lightBurnChance = 0.0f;
-        private float _lightChangeColorChance = 0.0f;
+    public override void Started()
+    {
+        base.Started();
+        _running = true;
+        MessLights();
+    }
 
-        public override void Initialize()
+    private void MessLights()
+    {
+        foreach (var comp in EntityQuery<PoweredLightComponent>())
         {
-            base.Initialize();
-            SubscribeLocalEvent<ActiveRadioComponent, RadioReceiveAttemptEvent>(OnRadioSendAttempt);
-        }
-
-        public override void Added()
-        {
-            if (Configuration is not SolarFlareEventRuleConfiguration ev)
-                return;
-            base.Added();
-            _onlyJamHeadsets = ev.OnlyJamHeadsets;
-            _endAfter = RobustRandom.Next(ev.MinEndAfter, ev.MaxEndAfter);
-            _affectedChannels = ev.AffectedChannels;
-            _lightBurnChance = ev.LightBreakChance;
-        }
-
-        public override void Started()
-        {
-            base.Started();
-            _running = true;
-            MessLights();
-        }
-
-        private void MessLights() 
-        {
-            foreach (var comp in EntityQuery<PoweredLightComponent>()) 
+            if (RobustRandom.Prob(_lightBurnChance))
             {
-                if (RobustRandom.Prob(_lightBurnChance))
-                {
-                    _poweredLight.TryDestroyBulb(comp.Owner, comp);
-                }
+                _poweredLight.TryDestroyBulb(comp.Owner, comp);
             }
         }
+    }
 
-        public override void Update(float frameTime)
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        if (!RuleStarted)
+            return;
+
+        if (Elapsed > _endAfter)
         {
-            base.Update(frameTime);
-
-            if (!RuleStarted)
-                return;
-
-            if (Elapsed > _endAfter)
-            {
-                ForceEndSelf();
-                return;
-            }
+            ForceEndSelf();
+            return;
         }
+    }
 
-        public override void Ended()
-        {
-            base.Ended();
-            _running = false;
-        }
+    public override void Ended()
+    {
+        base.Ended();
+        _running = false;
+    }
 
-        private void OnRadioSendAttempt(EntityUid uid, ActiveRadioComponent component, RadioReceiveAttemptEvent args)
-        {
-            if (_running && _affectedChannels.Contains(args.Channel.ID))
-                if (!_onlyJamHeadsets || (HasComp<HeadsetComponent>(uid) || HasComp<HeadsetComponent>(args.RadioSource)))
-                    args.Cancel();
-        }
-
+    private void OnRadioSendAttempt(EntityUid uid, ActiveRadioComponent component, RadioReceiveAttemptEvent args)
+    {
+        if (_running && _affectedChannels.Contains(args.Channel.ID))
+            if (!_onlyJamHeadsets || (HasComp<HeadsetComponent>(uid) || HasComp<HeadsetComponent>(args.RadioSource)))
+                args.Cancel();
     }
 }
