42745b1c6e143279c2fb439bdfbbf929f2568d7e
keronshb <54602815+keronshb@users.noreply.github.com>
2023-02-13 07:20:39 -0500

    Conveyor Belt optimization and prediction (#12929)
    * belt multithreading
    
    * moves away from multithreading and changes setting awake directly to physics system method
    
    * prediction for conveyors
    
    * Fixes missing reference in FaxSystem
    
    * Fixes oddities
    
    * Adds networked to conveyor components
    
    * Some more cleanup.
    
    * reverts power change event
    
    * Removes the event, fixes a file
    
    * Should fix the rest of the weird additions
    
    * More cleanup to fix extra files
    
    * Fixes again
    
    * fix
    
    * fixes fax system
    
    * Adds component state, cleans up the dependencies
    
    * Checks for prediction
    
    * Merge conflicts
    
    * powa
    
    ---------
    
    Co-authored-by: metalgearsloth <comedian_vs_clown@hotmail.com>

--- Content.Shared/Conveyor/ActiveConveyorComponent.cs
+++ Content.Shared/Conveyor/ActiveConveyorComponent.cs
@@ -1,4 +1,6 @@
-namespace Content.Server.Conveyor;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Conveyor;
 
 /// <summary>
 /// Used to track which conveyors are relevant in case there's a lot of them.
