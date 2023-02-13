478e33c497c322ec1e6497ff0714cb8f2384a1f5
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-02-13 23:55:52 +1100

    uid changes

--- Content.Server/Weapons/Melee/MeleeWeaponSystem.cs
+++ Content.Server/Weapons/Melee/MeleeWeaponSystem.cs
@@ -91,14 +91,14 @@ public sealed class MeleeWeaponSystem : SharedMeleeWeaponSystem
             return;
 
         if (user == null)
-            PopupSystem.PopupEntity(message, uid.Value); 
+            PopupSystem.PopupEntity(message, uid.Value);
         else
             PopupSystem.PopupEntity(message, uid.Value, Filter.PvsExcept(user.Value, entityManager: EntityManager), true);
     }
 
-    protected override bool DoDisarm(EntityUid user, DisarmAttackEvent ev, MeleeWeaponComponent component, ICommonSession? session)
+    protected override bool DoDisarm(EntityUid user, DisarmAttackEvent ev, EntityUid meleeUid, MeleeWeaponComponent component, ICommonSession? session)
     {
-        if (!base.DoDisarm(user, ev, component, session))
+        if (!base.DoDisarm(user, ev, meleeUid, component, session))
             return false;
 
         if (!TryComp<CombatModeComponent>(user, out var combatMode) ||
