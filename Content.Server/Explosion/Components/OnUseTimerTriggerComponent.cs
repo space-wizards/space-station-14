ae0a088fdc1e452ae3fba27239d1f7613096ae27
deltanedas <user@zenith>
2023-02-16 11:47:27 +0000

    add Automated field to OnUseTimerTrigger

--- Content.Server/Explosion/Components/OnUseTimerTriggerComponent.cs
+++ Content.Server/Explosion/Components/OnUseTimerTriggerComponent.cs
@@ -44,5 +44,11 @@ namespace Content.Server.Explosion.Components
         /// </summary>
         [DataField("canToggleStartOnStick")]
         public bool AllowToggleStartOnStick;
+
+		/// <summary>
+        ///     Disallows starting the timer by hand, must be stuck or triggered by a system.
+		/// </summary>
+        [DataField("automated")]
+        public bool Automated;
     }
 }
