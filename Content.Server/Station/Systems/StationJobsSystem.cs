d956702e463b9c52f7df6395a3539eaea6dc3654
Chief-Engineer <119664036+Chief-Engineer@users.noreply.github.com>
2023-02-17 14:55:04 -0600

    fix TrySetJobSlot() on infinite slot jobs

--- Content.Server/Station/Systems/StationJobsSystem.cs
+++ Content.Server/Station/Systems/StationJobsSystem.cs
@@ -224,7 +224,7 @@ public sealed partial class StationJobsSystem : EntitySystem
                 UpdateJobsAvailable();
                 return true;
             case true:
-                stationJobs.TotalJobs += amount - (int)jobList[jobPrototypeId]!.Value;
+                stationJobs.TotalJobs += amount - (int) (jobList[jobPrototypeId]!.HasValue ? jobList[jobPrototypeId]!.Value : 0);
 
                 jobList[jobPrototypeId] = (uint)amount;
                 UpdateJobsAvailable();
