6adaaaed9d6ccc000ae6d05286de69560231f359
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-02-21 13:59:55 +0300

    it does work

--- Content.Server/HotPotato/HotPotatoComponent.cs
+++ Content.Server/HotPotato/HotPotatoComponent.cs
@@ -3,4 +3,6 @@ namespace Content.Server.HotPotato;
 [RegisterComponent]
 public sealed class HotPotatoComponent : Component
 {
+    [DataField("canTransfer")]
+    public bool CanTransfer = true;
 }
--- Content.Server/HotPotato/HotPotatoSystem.cs
+++ Content.Server/HotPotato/HotPotatoSystem.cs
@@ -1,6 +1,10 @@
 using Content.Shared.Hands.EntitySystems;
-using Content.Shared.Interaction.Components;
+using Content.Shared.Hands.Components;
 using Content.Shared.Interaction.Events;
+using Content.Shared.Weapons.Melee.Events;
+using Content.Shared.Hands;
+using Content.Server.Explosion.Components;
+using Robust.Shared.Containers;
 
 namespace Content.Server.HotPotato;
 
@@ -11,10 +15,46 @@ public sealed class HotPotatoSystem : EntitySystem
     public override void Initialize()
     {
         SubscribeLocalEvent<HotPotatoComponent, UseInHandEvent>(OnUseInHand);
+        SubscribeLocalEvent<HotPotatoComponent, GotEquippedHandEvent>(OnGotEquipped);
+        SubscribeLocalEvent<HotPotatoComponent, MeleeHitEvent>(TransferItem);
+        SubscribeLocalEvent<HotPotatoComponent, ContainerGettingRemovedAttemptEvent>(OnRemoveAttempt);
     }
 
-    private void OnUseInHand(EntityUid uid, HotPotatoComponent component, UseInHandEvent args)
+    private void OnUseInHand(EntityUid uid, HotPotatoComponent comp, UseInHandEvent args)
     {
-        EntityManager.EnsureComponent<UnremoveableComponent>(uid);
+        comp.CanTransfer = false;
+    }
+
+    private void OnGotEquipped(EntityUid uid, HotPotatoComponent comp, GotEquippedHandEvent args)
+    {
+        if (HasComp<ActiveTimerTriggerComponent>(uid))
+            comp.CanTransfer = false;
+    }
+
+    private void TransferItem(EntityUid uid, HotPotatoComponent comp, MeleeHitEvent args)
+    {
+        comp.CanTransfer = true;
+        TryTransferItem(uid, args);
+        comp.CanTransfer = false;
+    }
+
+    private void TryTransferItem(EntityUid uid, MeleeHitEvent args)
+    {
+        foreach (var hitEntity in args.HitEntities) {
+            if (TryComp<SharedHandsComponent>(hitEntity, out var hands)) 
+            {
+                if (_hands.TryPickupAnyHand(hitEntity, uid))
+                    return;
+                foreach (var hand in hands.Hands.Values)
+                    if (_hands.TryDrop(hitEntity, hand) && _hands.TryPickup(hitEntity, uid, hand.Name))
+                        return;
+            }
+        }
+    }
+
+    private void OnRemoveAttempt(EntityUid uid, HotPotatoComponent comp, ContainerGettingRemovedAttemptEvent args)
+    {
+        if (!comp.CanTransfer)
+            args.Cancel();
     }
 }
