8865418e3a89ec3d855a7a74925d8301635146b9
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-02-22 19:56:25 +0300

    today i learned networking

--- Content.Shared/HotPotato/SharedHotPotatoSystem.cs
+++ Content.Shared/HotPotato/SharedHotPotatoSystem.cs
@@ -1,4 +1,6 @@
 using Robust.Shared.Containers;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.HotPotato;
 
@@ -9,11 +11,35 @@ public abstract class SharedHotPotatoSystem : EntitySystem
     {
         base.Initialize();
         SubscribeLocalEvent<HotPotatoComponent, ContainerGettingRemovedAttemptEvent>(OnRemoveAttempt);
+
+        SubscribeLocalEvent<HotPotatoComponent, ComponentGetState>(GetCompState);
+        SubscribeLocalEvent<HotPotatoComponent, ComponentHandleState>(HandleCompState);
     }
 
     private void OnRemoveAttempt(EntityUid uid, HotPotatoComponent comp, ContainerGettingRemovedAttemptEvent args)
     {
         if (!comp.CanTransfer)
             args.Cancel();
+    }
+
+    private void GetCompState(EntityUid uid, HotPotatoComponent comp, ref ComponentGetState args)
+    {
+        args.State = new HotPotatoComponentState
+        {
+            CanTransfer = comp.CanTransfer,
+        };
+    }
+
+    private void HandleCompState(EntityUid uid, HotPotatoComponent comp, ref ComponentHandleState args)
+    {
+        if (args.Current is not HotPotatoComponentState state)
+            return;
+        comp.CanTransfer = state.CanTransfer;
+    }
+
+    [Serializable, NetSerializable]
+    public sealed class HotPotatoComponentState : ComponentState
+    {
+        public bool CanTransfer { get; init; }
     }
 }
