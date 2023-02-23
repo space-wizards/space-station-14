fe14831a0e435fa6f12d5bf9c7cd35ab821f0fe9
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-02-23 14:11:49 +0300

    hot potato comment + datafield + access

--- Content.Shared/HotPotato/HotPotatoComponent.cs
+++ Content.Shared/HotPotato/HotPotatoComponent.cs
@@ -2,8 +2,16 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.HotPotato;
 
+/// <summary>
+/// Similar to <see cref="Content.Shared.Interaction.Components.UnremoveableComponent"/>
+/// except entities with this component can be removed in specific case: <see cref="CanTransfer"/>
+/// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed class HotPotatoComponent : Component
 {
+    /// <summary>
+    /// If set to true entity can be removed by hitting entities if they have hands
+    /// </summary>
+    [DataField("canTransfer"), ViewVariables(VVAccess.ReadWrite)]
     public bool CanTransfer = true;
 }
