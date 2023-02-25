0e12b73c1e7c6ef1cb93541f427a64c7f33d7c9a
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-02-25 13:53:54 +1100

    Revert "guh"
    This reverts commit e7c0bb36f5637241a15f11dcfdad1f2238d04241.

--- Content.Server/Atmos/Serialization/TileAtmosCollectionSerializer.cs
+++ Content.Server/Atmos/Serialization/TileAtmosCollectionSerializer.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using Robust.Shared.Serialization;
+﻿using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Serialization.Markdown;
 using Robust.Shared.Serialization.Markdown.Mapping;
@@ -66,98 +65,22 @@ public sealed class TileAtmosCollectionSerializer : ITypeSerializer<Dictionary<V
             tiles[indices] = newIndex;
         }
 
-        var tileChunks = new Dictionary<Vector2i, int>();
-        var checkedChunks = new HashSet<Vector2i>();
-        var chunkSize = 2;
-
-        // If there's ChunkSize x ChunkSize area of common tiles then compress it
-        foreach (var chunk in tiles.Keys)
-        {
-            var origin = SharedMapSystem.GetChunkIndices(chunk, chunkSize);
-
-            if (!checkedChunks.Add(origin))
-                continue;
-
-            var matches = true;
-            int chunkData = -1;
-
-            for (var x = 0; x < chunkSize; x++)
-            {
-                for (var y = 0; y < chunkSize; y++)
-                {
-                    var indices = new Vector2i(x + chunkSize * origin.X, y + chunkSize * origin.Y);
-
-                    if (!tiles.TryGetValue(indices, out var data))
-                    {
-                        matches = false;
-                        break;
-                    }
-
-                    if (chunkData != -1 && chunkData != data)
-                    {
-                        matches = false;
-                        break;
-                    }
-
-                    chunkData = data;
-                }
-
-                if (!matches)
-                {
-                    break;
-                }
-            }
-
-            if (!matches || chunkData == -1)
-                continue;
-
-            tileChunks.Add(origin, chunkData);
-        }
-
-        // Remove tile data
-        foreach (var origin in tileChunks.Keys)
-        {
-            var chunkOrigin = origin * chunkSize;
-
-            for (var x = 0; x < chunkSize; x++)
-            {
-                for (var y = 0; y < chunkSize; y++)
-                {
-                    var indices = new Vector2i(x + chunkOrigin.X, y + chunkOrigin.Y);
-                    tiles.Remove(indices);
-                }
-            }
-        }
-
-        if (tileChunks.Count == 0)
-            tileChunks = null;
-        if (uniqueMixes.Count == 0)
-            uniqueMixes = null;
-        if (tiles.Count == 0)
-            tiles = null;
+        if (uniqueMixes.Count == 0) uniqueMixes = null;
+        if (tiles.Count == 0) tiles = null;
 
         return serializationManager.WriteValue(new TileAtmosData
         {
-            ChunkSize = chunkSize,
             UniqueMixes = uniqueMixes,
-            TilesUniqueMixes = tiles,
-            TileChunksUniqueMixes = tileChunks,
+            TilesUniqueMixes = tiles
         }, alwaysWrite, context);
     }
 
     [DataDefinition]
     private struct TileAtmosData
     {
-        [DataField("chunkSize")] public int ChunkSize;
-
         [DataField("uniqueMixes")] public List<GasMixture>? UniqueMixes;
 
         [DataField("tiles")] public Dictionary<Vector2i, int>? TilesUniqueMixes;
-
-        /// <summary>
-        /// Stores bulk tile data, saves (ChunkSize x ChunkSize) - 1 lines.
-        /// </summary>
-        [DataField("tileChunks")] public Dictionary<Vector2i, int>? TileChunksUniqueMixes;
     }
 
     public void CopyTo(ISerializationManager serializationManager, Dictionary<Vector2i, TileAtmosphere> source, ref Dictionary<Vector2i, TileAtmosphere> target, SerializationHookContext hookCtx,
