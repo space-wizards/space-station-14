912cbb5c495ba3a66466c12c3f9f2c3b83817222
Alexis Ehret <git@08a.re>
2023-02-26 23:19:43 +0100

    Removed unused using in AnchorableSystem.cs

--- Content.Server/Construction/AnchorableSystem.cs
+++ Content.Server/Construction/AnchorableSystem.cs
@@ -11,9 +11,7 @@ using Content.Shared.Examine;
 using Content.Shared.Pulling.Components;
 using Content.Shared.Tools.Components;
 using Robust.Shared.Map;
-using Robust.Shared.Physics;
 using Robust.Shared.Physics.Components;
-using Robust.Shared.Player;
 
 namespace Content.Server.Construction
 {
