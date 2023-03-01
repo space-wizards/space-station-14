671edbf350acfe274ed9c577809246b89ac71b1e
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-03-01 10:10:05 +0300

    component fields tweaks

--- Content.Server/Reflect/ReflectComponent.cs
+++ Content.Server/Reflect/ReflectComponent.cs
@@ -11,15 +11,15 @@ public sealed class ReflectComponent : Component
     /// <summary>
     ///     Can only reflect when enabled
     /// </summary>
-    [DataField("enabled")]
+    [DataField("enabled"), ViewVariables(VVAccess.ReadWrite)]
     public bool Enabled;
 
-    [DataField("chance")]
+    [DataField("chance"), ViewVariables(VVAccess.ReadWrite)]
     public float Chance;
 
-    [DataField("spread")]
+    [DataField("spread"), ViewVariables(VVAccess.ReadWrite)]
     public Angle Spread = Angle.FromDegrees(5);
 
     [DataField("onReflect")]
-    public SoundSpecifier? OnReflect { get; set; }
+    public SoundSpecifier? OnReflect;
 }
