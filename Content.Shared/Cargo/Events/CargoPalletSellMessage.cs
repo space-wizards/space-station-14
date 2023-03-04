7f3c299d82ade4acb438ccfc4e390d53d9d83a5c
Cheackraze <71046427+Cheackraze@users.noreply.github.com>
2023-03-04 01:38:26 -0500

    cleanup

--- Content.Shared/Cargo/Events/CargoPalletSellMessage.cs
+++ Content.Shared/Cargo/Events/CargoPalletSellMessage.cs
@@ -3,7 +3,7 @@ using Robust.Shared.Serialization;
 namespace Content.Shared.Cargo.Events;
 
 /// <summary>
-/// Raised on a client request cargo shuttle recall
+/// Raised on a client request pallet sale
 /// </summary>
 [Serializable, NetSerializable]
 public sealed class CargoPalletSellMessage : BoundUserInterfaceMessage
