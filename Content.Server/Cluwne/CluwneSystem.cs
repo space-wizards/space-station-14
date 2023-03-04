8a30030d7a94b06f07ef98ebabe17840c50665af
brainfood1183 <newman1183@googlemail.com>
2023-03-04 16:44:43 +0000

    adds autoemote

--- Content.Server/Cluwne/CluwneSystem.cs
+++ Content.Server/Cluwne/CluwneSystem.cs
@@ -2,15 +2,14 @@ using Content.Server.Administration.Commands;
 using Content.Server.Popups;
 using Content.Shared.Popups;
 using Content.Shared.Mobs;
+using Content.Server.Chat;
 using Content.Server.Chat.Systems;
 using Content.Shared.Chat.Prototypes;
 using Robust.Shared.Random;
-using Content.Shared.Emoting;
 using Content.Shared.Stunnable;
 using Content.Shared.Damage.Prototypes;
 using Content.Shared.Damage;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Timing;
 using Content.Server.Emoting.Systems;
 using Content.Server.Speech.EntitySystems;
 using Content.Shared.Cluwne;
@@ -26,9 +25,8 @@ public sealed class CluwneSystem : EntitySystem
     [Dependency] private readonly SharedStunSystem _stunSystem = default!;
     [Dependency] private readonly DamageableSystem _damageableSystem = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly ChatSystem _chat = default!;
-    [Dependency] private readonly EmoteSystem _emote = default!;
+    [Dependency] private readonly AutoEmoteSystem _autoEmote = default!;
 
     public override void Initialize()
     {
@@ -40,24 +38,25 @@ public sealed class CluwneSystem : EntitySystem
             new[] { typeof(VocalSystem), typeof(BodyEmotesSystem) });
     }
 
-    public override void
-
-     Update(float frameTime)
+    /// <summary>
+    /// Removes autoemotecomp and cluwnecomp on death.
+    /// </summary>
+    private void OnMobState(EntityUid uid, CluwneComponent component, MobStateChangedEvent args)
     {
-        base.Update(frameTime);
+        if (args.NewMobState == MobState.Dead)
 
-        foreach (var cluwnecomp in EntityQuery<CluwneComponent>())
-        {
-            cluwnecomp.LastGiggleCooldown -= frameTime;
-            RandomEmote(cluwnecomp.Owner, cluwnecomp);
-        }
+            RemComp<CluwneComponent>(uid);
+            RemComp<AutoEmoteComponent>(uid);
+            _autoEmote.RemoveEmote(uid, "CluwneGiggle");
+            var damageSpec = new DamageSpecifier(_prototypeManager.Index<DamageGroupPrototype>("Genetic"), 300);
+            _damageableSystem.TryChangeDamage(uid, damageSpec);
     }
 
-    /// <summary>
-    /// Cluwne will cluwne laugh when emoting.
-    /// </summary>
     public EmoteSoundsPrototype? EmoteSounds;
 
+    /// <summary>
+    /// OnStartup gives the cluwne outfit, ensures clumsy,gives name prefix and makes sure emote sounds are laugh.
+    /// </summary>
     private void OnComponentStartup(EntityUid uid, CluwneComponent component, ComponentStartup args)
     {
         if (component.EmoteSoundsId == null)
@@ -67,6 +66,8 @@ public sealed class CluwneSystem : EntitySystem
         var meta = MetaData(uid);
         var name = meta.EntityName;
 
+        EnsureComp<AutoEmoteComponent>(uid);
+        _autoEmote.AddEmote(uid, "CluwneGiggle");
         EnsureComp<ClumsyComponent>(uid);
 
         _popupSystem.PopupEntity(Loc.GetString("cluwne-transform", ("target", uid)), uid, PopupType.LargeCaution);
@@ -78,53 +79,25 @@ public sealed class CluwneSystem : EntitySystem
     }
 
     /// <summary>
-    /// Makes sure the cluwne emits a cluwnelaugh on an emote that has a sound such as lol.
+    /// Handles the timing on autoemote as well as falling over and honking.
     /// </summary>
     private void OnEmote(EntityUid uid, CluwneComponent component, ref EmoteEvent args)
     {
         if (args.Handled)
             return;
-        args.Handled = _chat.TryPlayEmoteSound(uid, EmoteSounds, args.Emote);
-    }
+            args.Handled = _chat.TryPlayEmoteSound(uid, EmoteSounds, args.Emote);
 
-    /// <summary>
-    /// If mob is dead then the cluwne mechanic gets removed from mob.
-    /// </summary>
-    private void OnMobState(EntityUid uid, CluwneComponent component, MobStateChangedEvent args)
-    {
-        if (args.NewMobState == MobState.Dead)
-    
-            RemComp<CluwneComponent>(uid);
-            var damageSpec = new DamageSpecifier(_prototypeManager.Index<DamageGroupPrototype>("Genetic"), 300);
-            _damageableSystem.TryChangeDamage(uid, damageSpec);
-    }
-
-    /// <summary>
-    /// Makes cluwne do a random emote. Falldown and horn, twitch and honk, giggle.
-    /// </summary>
-    private void RandomEmote(EntityUid uid, CluwneComponent component)
-    {
-        if (component.LastGiggleCooldown > 0)
-            return;
-
-        if (_robustRandom.Prob(component.KnockChance))
-        {
-            _audio.PlayPvs(component.KnockSound, uid);
-            _stunSystem.TryParalyze(uid, TimeSpan.FromSeconds(component.ParalyzeTime), true);
-            _chat.TrySendInGameICMessage(uid, "spasms", InGameICChatType.Emote, false, false);
-        }
-
-        else if (_robustRandom.Prob(component.GiggleRandomChance))
-             _chat.TrySendInGameICMessage(uid, "giggles", InGameICChatType.Emote, false, false);
-
-        else if (_robustRandom.Prob(component.GiggleRandomChance))
+        if (_robustRandom.Prob(component.GiggleRandomChance))
         {
             _audio.PlayPvs(component.SpawnSound, uid);
             _chat.TrySendInGameICMessage(uid, "honks", InGameICChatType.Emote, false, false);
         }
 
-        else
-            component.LastGiggleCooldown = component.GiggleCooldown;
+        else if (_robustRandom.Prob(component.KnockChance))
+        {
+            _audio.PlayPvs(component.KnockSound, uid);
+            _stunSystem.TryParalyze(uid, TimeSpan.FromSeconds(component.ParalyzeTime), true);
+            _chat.TrySendInGameICMessage(uid, "spasms", InGameICChatType.Emote, false, false);
+        }
     }
 }
-
--- Content.Shared/Cluwne/CluwneComponent.cs
+++ Content.Shared/Cluwne/CluwneComponent.cs
@@ -1,8 +1,6 @@
 using Robust.Shared.Audio;
 using Content.Shared.Chat.Prototypes;
-using Robust.Shared.Prototypes;
 using Robust.Shared.GameStates;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Shared.Cluwne;
@@ -11,44 +9,29 @@ namespace Content.Shared.Cluwne;
 [NetworkedComponent]
 public sealed class CluwneComponent : Component
 {
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string GiggleEmoteId = "Scream";
+
     /// <summary>
-    /// Giggle emote chances.
+    /// timings for giggles and knocks.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    public float GiggleChance = 0.2f;
+    public TimeSpan DamageGiggleCooldown = TimeSpan.FromSeconds(2);
 
     [ViewVariables(VVAccess.ReadWrite)]
     public float KnockChance = 0.05f;
 
     [ViewVariables(VVAccess.ReadWrite)]
-    public float GiggleRandomChance = 0.08f;
-
-    [ViewVariables(VVAccess.ReadWrite)]
-    public float GiggleCooldown = 5;
-
-    [ViewVariables(VVAccess.ReadWrite)]
-    public float RandomGiggleAttempt = 5;
-
-    [ViewVariables(VVAccess.ReadWrite)]
-    public float LastGiggleCooldown = 0f;
+    public float GiggleRandomChance = 0.1f;
 
     /// <summary>
-    /// Cluwne Emotes
+    /// The last time the zombie groaned from taking damage.
     /// </summary>
-    [DataField("scream", customTypeSerializer: typeof(PrototypeIdSerializer<EmoteSoundsPrototype>))]
-    public string GiggleEmoteId = "Scream";
-
-    [DataField("twitch", customTypeSerializer: typeof(PrototypeIdSerializer<EmoteSoundsPrototype>))]
-    public string TwitchEmoteId = "Twitch";
+    [ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan LastDamageGiggle = TimeSpan.Zero;
 
     [DataField("emoteId", customTypeSerializer: typeof(PrototypeIdSerializer<EmoteSoundsPrototype>))]
     public string? EmoteSoundsId = "Cluwne";
-
-    /// <summary>
-    ///Giggle emote timespan.
-    /// </summary>
-    [DataField("giggleGoChance", customTypeSerializer: typeof(TimeOffsetSerializer))]
-    public TimeSpan GiggleGoChance = TimeSpan.Zero;
 
     /// <summary>
     /// Amount of time cluwne is paralyzed for when falling over.
@@ -57,11 +40,8 @@ public sealed class CluwneComponent : Component
     public float ParalyzeTime = 2f;
 
     /// <summary>
-    /// Sound specifiers for giggles and noises.
+    /// Sound specifiers for honk and knock.
     /// </summary>
-    [DataField("giggle")]
-    public SoundSpecifier Giggle = new SoundCollectionSpecifier("CluwneScreams");
-
     [DataField("spawnsound")]
     public SoundSpecifier SpawnSound = new SoundPathSpecifier("/Audio/Items/bikehorn.ogg");
 
