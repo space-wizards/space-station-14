9c9a51a12e286556e7270946d6a15ca823ee1bf6
pofitlo <107665898+pofitlo@users.noreply.github.com>
2023-03-06 00:16:57 +0400

    add new product and new component
    I created a new component and system so that a message about hacking could be displayed

---
+++ Content.Shared/Emag/Components/BeEmaggedComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Shared.Emag.Components;
+	[RegisterComponent]
+     public sealed class BeEmaggedComponent : Component
+     {
+
+      [ViewVariables(VVAccess.ReadWrite)]
+      public bool Enabled = true;
+		
+
+     }
