0f25a6fc6859b36631564ff3305f869386ddd7a9
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-03-11 22:44:59 +0300

    forgot hitscanreflect desc

--- Content.Shared/Weapons/Ranged/HitScanReflectAttempt.cs
+++ Content.Shared/Weapons/Ranged/HitScanReflectAttempt.cs
@@ -1,7 +1,7 @@
 namespace Content.Shared.Weapons.Ranged;
 
 /// <summary>
-/// Shot may be redirected by changing <see cref="Target"/> variable
+/// Shot may be reflected by setting <see cref="Reflected"/> to true
 /// </summary>
 [ByRefEvent]
 public record struct HitScanReflectAttempt(bool Reflected);
