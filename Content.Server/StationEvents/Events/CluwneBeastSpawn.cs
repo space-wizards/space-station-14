a430532092672293d23d3542f3016744d9297eaf
brainfood1183 <newman1183@googlemail.com>
2023-03-12 01:56:23 +0000

    changes spawn event

--- Content.Server/StationEvents/Events/CluwneBeastSpawn.cs
+++ Content.Server/StationEvents/Events/CluwneBeastSpawn.cs
@@ -1,35 +1,30 @@
 using System.Linq;
 using Content.Server.StationEvents.Components;
-using Robust.Shared.Random;
 
 namespace Content.Server.StationEvents.Events;
 
 public sealed class CluwneBeastSpawn : StationEventSystem
 {
-    public static List<string> SpawnedPrototypeChoices = new List<string>()
-        {"MobMouse", "MobMouse1", "MobMouse2"};
     public override string Prototype => "CluwneBeastSpawn";
 
     public override void Started()
     {
         base.Started();
-
-        var modifier = GetSeverityModifier();
-
-        var spawnLocations = EntityManager.EntityQuery<VentCritterSpawnLocationComponent, TransformComponent>().ToList();
+        var spawnLocations = EntityManager.EntityQuery<VentCritterSpawnLocationComponent>().ToList();
         RobustRandom.Shuffle(spawnLocations);
 
-        var spawnAmount = (int) (RobustRandom.Next(1, 3) * Math.Sqrt(modifier));
+        var mod = Math.Sqrt(GetSeverityModifier());
 
-        for (int i = 0; i < spawnAmount && i < spawnLocations.Count - 1; i++)
+        var spawnAmount = (1);
+        Sawmill.Info($"Spawning {spawnAmount} of cluwnebeasts");
+        foreach (var location in spawnLocations)
         {
-            var spawnChoice = RobustRandom.Pick(SpawnedPrototypeChoices);
-            if (RobustRandom.Prob(Math.Min(0.01f * modifier, 1.0f)) || i == 0)
-                spawnChoice = "SpawnPointGhostCluwneBeast";
+            if (spawnAmount-- == 0)
+                break;
 
-            var coords = spawnLocations[i].Item2.Coordinates;
-            Sawmill.Info($"Spawning cluwnebeast {spawnChoice} at {coords}");
-            EntityManager.SpawnEntity(spawnChoice, coords);
+            var coords = EntityManager.GetComponent<TransformComponent>(location.Owner);
+
+            EntityManager.SpawnEntity("SpawnPointGhostCluwneBeast", coords.Coordinates);
         }
     }
 }
