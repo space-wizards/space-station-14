422e4166ebe9582cd4e408cd920835afbbf1fda9
Naive817 <lyx2020@sjtu.edu.cn>
2023-03-14 10:46:05 +0800

    adds mech drill

---
+++ Content.Server/Mech/Equipment/Components/MechDrillComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using Robust.Shared.Audio;
+
+namespace Content.Server.Mech.Equipment.Components;
+
+/// <summary>
+/// A piece of mech equipment that drills entities and stores them
+/// inside of a container so large objects can be moved.
+/// </summary>
+[RegisterComponent]
+public sealed class MechDrillComponent : Component
+{
+    /// <summary>
+    /// The change in energy after each drill.
+    /// </summary>
+    [DataField("drillEnergyDelta")]
+    public float drillEnergyDelta = -30;
+
+    /// <summary>
+    /// How long does it take to drill something?
+    /// </summary>
+    [DataField("drillDelay")]
+    public float drillDelay = 0.25f;
+
+
+    /// <summary>
+    /// The sound played when a mech is drillbing something
+    /// </summary>
+    [DataField("drillSound")]
+    public SoundSpecifier drillSound = new SoundPathSpecifier("/Audio/Mecha/sound_mecha_hydraulic.ogg");
+
+    public IPlayingAudioStream? AudioStream;
+
+}
---
+++ Content.Server/Mech/Equipment/EntitySystems/MechDrillSystem.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using Content.Server.DoAfter;
+using Content.Server.Gatherable.Components;
+using Content.Server.Interaction;
+using Content.Server.Mech.Components;
+using Content.Server.Mech.Equipment.Components;
+using Content.Server.Mech.Systems;
+using Content.Shared.DoAfter;
+using Content.Shared.Construction.Components;
+using Content.Shared.Interaction;
+using Content.Shared.Mech;
+using Content.Shared.Mech.Equipment.Components;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Wall;
+using Robust.Server.GameObjects;
+using Robust.Shared.Containers;
+using Robust.Shared.Map;
+using Robust.Shared.Physics;
+using Robust.Shared.Physics.Components;
+
+namespace Content.Server.Mech.Equipment.EntitySystems;
+
+/// <summary>
+/// Handles <see cref="MechDrillComponent"/> and all related UI logic
+/// </summary>
+public sealed class MechDrillSystem : EntitySystem
+{
+    [Dependency] private readonly MechSystem _mech = default!;
+    [Dependency] private readonly DoAfterSystem _doAfter = default!;
+    [Dependency] private readonly InteractionSystem _interaction = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly TransformSystem _transform = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<MechDrillComponent, InteractNoHandEvent>(OnInteract);
+        SubscribeLocalEvent<MechDrillComponent, DoAfterEvent>(OnMechdrill);
+    }
+
+    private void OnInteract(EntityUid uid, MechDrillComponent component, InteractNoHandEvent args)
+    {
+        if (args.Handled || args.Target is not {} target)
+            return;
+
+        if (!HasComp<GatherableComponent>(target))
+        {
+            return;
+        }
+
+        if (!TryComp<MechComponent>(args.User, out var mech) || mech.PilotSlot.ContainedEntity == target)
+            return;
+
+        if (mech.Energy + component.drillEnergyDelta < 0)
+            return;
+
+        if (!_interaction.InRangeUnobstructed(args.User, target))
+            return;
+
+        args.Handled = true;
+        component.AudioStream = _audio.PlayPvs(component.drillSound, uid);
+        _doAfter.DoAfter(new DoAfterEventArgs(args.User, component.drillDelay, target:target, used:uid)
+        {
+            BreakOnTargetMove = true,
+            BreakOnUserMove = true
+        });
+    }
+
+    private void OnMechdrill(EntityUid uid, MechDrillComponent component, DoAfterEvent args)
+    {
+        if (args.Cancelled)
+        {
+            component.AudioStream?.Stop();
+            return;
+        }
+
+        if (args.Handled || args.Args.Target == null)
+            return;
+
+        if (!TryComp<MechEquipmentComponent>(uid, out var equipmentComponent) || equipmentComponent.EquipmentOwner == null)
+            return;
+        if (!_mech.TryChangeEnergy(equipmentComponent.EquipmentOwner.Value, component.drillEnergyDelta))
+            return;
+
+        _mech.UpdateUserInterface(equipmentComponent.EquipmentOwner.Value);
+
+        args.Handled = true;
+    }
+}
