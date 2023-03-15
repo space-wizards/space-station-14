4cbe4524a2b369896cb83328982caf7930a7f589
deltanedas <@deltanedas:kde.org>
2023-03-15 18:20:35 +0000

    restructure cognizine effect so creatures that have minds can talk

--- Content.Server/Chemistry/ReagentEffects/MakeSentient.cs
+++ Content.Server/Chemistry/ReagentEffects/MakeSentient.cs
@@ -13,17 +13,17 @@ public sealed class MakeSentient : ReagentEffect
         var entityManager = args.EntityManager;
         var uid = args.SolutionEntity;
 
-        // This makes it so it doesn't affect things that are already sentient
+        // This piece of code makes things able to speak "normally". One thing of note is that monkeys have a unique accent and won't be affected by this.
+        entityManager.RemoveComponent<ReplacementAccentComponent>(uid);
+
+        // Monke talk. This makes cognizine a cure to AMIV's long term damage funnily enough, do with this information what you will.
+        entityManager.RemoveComponent<MonkeyAccentComponent>(uid);
+
+        // This makes it so it doesn't add a ghost role to things that are already sentient
         if (entityManager.HasComponent<MindComponent>(uid))
         {
             return;
         }
-
-        // This piece of code makes things able to speak "normally". One thing of note is that monkeys have a unique accent and won't be affected by this.
-        entityManager.RemoveComponent<ReplacementAccentComponent>(uid);
-
-        // Monke talk
-        entityManager.RemoveComponent<MonkeyAccentComponent>(uid);
 
         // No idea what anything past this point does
         if (entityManager.TryGetComponent(uid, out GhostTakeoverAvailableComponent? takeOver))
