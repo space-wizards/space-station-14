11fd97f88eabce403cdc65e6c12aae4f6f617a43
brainfood1183 <newman1183@googlemail.com>
2023-03-16 04:01:24 +0000

    pirate accent

--- Content.Server/Speech/Components/PirateAccentComponent.cs
+++ Content.Server/Speech/Components/PirateAccentComponent.cs
@@ -1,4 +1,16 @@
-﻿namespace Content.Server.Speech.Components;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using System.Text.RegularExpressions;
+using Content.Server.Speech.Components;
+using Content.Server.Speech.EntitySystems;
+
+namespace Content.Server.Speech.Components;
 
 [RegisterComponent]
-public sealed class PirateAccentComponent : Component {}
+public sealed class PirateAccentComponent : Component
+{
+    [DataField("yarChance")]
+    public float YarChance = 1f;
+
+    [DataField("pirateAccent", customTypeSerializer: typeof(PrototypeIdSerializer<ReplacementAccentPrototype>))]
+    public readonly string PirateAccent = "pirate";
+}
--- Content.Server/Speech/EntitySystems/PirateAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/PirateAccentSystem.cs
@@ -1,54 +1,55 @@
-using System.Globalization;
-using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
+using Robust.Shared.Profiling;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+using System.Text.RegularExpressions;
 
 namespace Content.Server.Speech.EntitySystems;
 
 public sealed class PirateAccentSystem : EntitySystem
 {
-    private static readonly Dictionary<string, string> DirectReplacements = new()
-    {
-        { (Loc.GetString($"accent-pirate-word-1")), (Loc.GetString($"accent-pirate-response-1")) },
-        { (Loc.GetString($"accent-pirate-word-2")), (Loc.GetString($"accent-pirate-response-1")) },
-        { (Loc.GetString($"accent-pirate-word-3")), (Loc.GetString($"accent-pirate-response-2")) },
-        { (Loc.GetString($"accent-pirate-word-4")), (Loc.GetString($"accent-pirate-response-3")) },
-        { (Loc.GetString($"accent-pirate-word-5")), (Loc.GetString($"accent-pirate-response-4")) },
-        { (Loc.GetString($"accent-pirate-word-6")), (Loc.GetString($"accent-pirate-response-5")) },
-        { (Loc.GetString($"accent-pirate-word-7")), (Loc.GetString($"accent-pirate-response-6")) },
-        { (Loc.GetString($"accent-pirate-word-8")), (Loc.GetString($"accent-pirate-response-7")) },
-        { (Loc.GetString($"accent-pirate-word-9")), (Loc.GetString($"accent-pirate-response-8")) },
-        { (Loc.GetString($"accent-pirate-word-10")), (Loc.GetString($"accent-pirate-response-9")) },
-        { (Loc.GetString($"accent-pirate-word-11")), (Loc.GetString($"accent-pirate-response-10")) },
-        { (Loc.GetString($"accent-pirate-word-12")), (Loc.GetString($"accent-pirate-response-11")) },
-        { (Loc.GetString($"accent-pirate-word-13")), (Loc.GetString($"accent-pirate-response-12")) },
-        { (Loc.GetString($"accent-pirate-word-14")), (Loc.GetString($"accent-pirate-response-12")) },
-        { (Loc.GetString($"accent-pirate-word-15")), (Loc.GetString($"accent-pirate-response-13")) },
-        { (Loc.GetString($"accent-pirate-word-16")), (Loc.GetString($"accent-pirate-response-19")) },
-        { (Loc.GetString($"accent-pirate-word-17")), (Loc.GetString($"accent-pirate-response-15")) },
-        { (Loc.GetString($"accent-pirate-word-18")), (Loc.GetString($"accent-pirate-response-14")) },
-        { (Loc.GetString($"accent-pirate-word-19")), (Loc.GetString($"accent-pirate-response-16")) },
-        { (Loc.GetString($"accent-pirate-word-20")), (Loc.GetString($"accent-pirate-response-17")) },
-        { (Loc.GetString($"accent-pirate-word-21")), (Loc.GetString($"accent-pirate-response-18")) },
-        { (Loc.GetString($"accent-pirate-word-22")), (Loc.GetString($"accent-pirate-response-20")) },
-        { (Loc.GetString($"accent-pirate-word-23")), (Loc.GetString($"accent-pirate-response-21")) },
-        { (Loc.GetString($"accent-pirate-word-24")), (Loc.GetString($"accent-pirate-response-22")) },
-        { (Loc.GetString($"accent-pirate-word-25")), (Loc.GetString($"accent-pirate-response-23")) },
-        { (Loc.GetString($"accent-pirate-word-26")), (Loc.GetString($"accent-pirate-response-24")) },
-        { (Loc.GetString($"accent-pirate-word-27")), (Loc.GetString($"accent-pirate-response-25")) },
-        { (Loc.GetString($"accent-pirate-word-28")), (Loc.GetString($"accent-pirate-response-26")) },
-    };
-
+    [Dependency] private readonly IRobustRandom _random = default!;
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<PirateAccentComponent, AccentGetEvent>(OnAccentGet);
     }
-    private void OnAccentGet(EntityUid uid, PirateAccentComponent component, AccentGetEvent args)
+
+    // converts left word when typed into the right word. For example typing you becomes ye.
+    private static readonly Dictionary<string, string> DirectReplacements = new()
     {
+        { Loc.GetString($"accent-pirate-word-1"), Loc.GetString($"accent-pirate-word-2") },
+        { Loc.GetString($"accent-pirate-word-3"), Loc.GetString($"accent-pirate-word-4") },
+        { Loc.GetString($"accent-pirate-word-5"), Loc.GetString($"accent-pirate-word-6") },
+        { Loc.GetString($"accent-pirate-word-7"), Loc.GetString($"accent-pirate-word-8") },
+    };
+
+    public string Accentuate(string message, PirateAccentComponent component)
+    {
+
+        var msg = message;
+
         foreach (var (first, replace) in DirectReplacements)
         {
-            args.Message = Regex.Replace(args.Message, $@"(?<!\w){first}(?!\w)", replace, RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, $@"(?<!\w){first}(?!\w)", replace, RegexOptions.IgnoreCase);
         }
+
+        // Suffix:
+        if (_random.Prob(component.YarChance))
+        {
+            var pick = _random.Next(1, 4);
+
+            // Reverse sanitize capital
+            msg = msg[0].ToString().ToLower() + msg.Remove(0, 1);
+            msg = Loc.GetString($"accent-pirate-prefix-{pick}") + " " + msg;
+        }
+
+        return msg;
+    }
+    private void OnAccentGet(EntityUid uid, PirateAccentComponent component, AccentGetEvent args)
+    {
+        args.Message = Accentuate(args.Message, component);
     }
 }
+
