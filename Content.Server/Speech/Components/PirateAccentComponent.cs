32c24957152db2e3b0b7ac82a6c937c456faae75
AJCM <AJCM@tutanota.com>
2023-03-22 22:50:49 -0400

    a

--- Content.Server/Speech/Components/PirateAccentComponent.cs
+++ Content.Server/Speech/Components/PirateAccentComponent.cs
@@ -7,14 +7,15 @@ namespace Content.Server.Speech.Components;
 public sealed class PirateAccentComponent : Component
 {
     [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("yarrChance")]
     public readonly float YarrChance = 0.5f;
 
     [ViewVariables]
     public readonly List<string> PirateWords = new()
     {
-        { "accent-pirate-prefix-1" },
-        { "accent-pirate-prefix-2" },
-        { "accent-pirate-prefix-3" }
+        "accent-pirate-prefix-1",
+        "accent-pirate-prefix-2",
+        "accent-pirate-prefix-3"
     };
 
     [ViewVariables]
