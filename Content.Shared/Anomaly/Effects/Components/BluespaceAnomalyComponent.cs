d5d9046fb609db6fd99076a6938b6db32e495c8f
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-03-23 01:53:32 -0400

    Anomaly cleanup (#14781)

--- Content.Shared/Anomaly/Effects/Components/BluespaceAnomalyComponent.cs
+++ Content.Shared/Anomaly/Effects/Components/BluespaceAnomalyComponent.cs
@@ -3,8 +3,7 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.Anomaly.Effects.Components;
 
-[RegisterComponent, NetworkedComponent]
-[Access(typeof(BluespaceAnomalySystem))]
+[RegisterComponent, NetworkedComponent, Access(typeof(BluespaceAnomalySystem))]
 public sealed class BluespaceAnomalyComponent : Component
 {
     /// <summary>
