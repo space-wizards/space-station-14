1a64eb4c427d7abcb269e2b48292cd029ea467ca
deltanedas <@deltanedas:kde.org>
2023-03-24 09:40:46 +0000

    signaller can be used as a payload

--- Content.Server/MachineLinking/System/SignallerSystem.cs
+++ Content.Server/MachineLinking/System/SignallerSystem.cs
@@ -1,31 +1,55 @@
+using Content.Server.Explosion.EntitySystems;
 using Content.Server.MachineLinking.Components;
-using JetBrains.Annotations;
 using Content.Shared.Interaction.Events;
+using System.Linq;
 
-namespace Content.Server.MachineLinking.System
+namespace Content.Server.MachineLinking.System;
+
+public sealed class SignallerSystem : EntitySystem
 {
-    [UsedImplicitly]
-    public sealed class SignallerSystem : EntitySystem
+    [Dependency] private readonly SignalLinkerSystem _signal = default!;
+
+    private HashSet<(EntityUid, String)> _triggering = new();
+
+    public override void Initialize()
     {
-        [Dependency] private readonly SignalLinkerSystem _signalSystem = default!;
-        public override void Initialize()
-        {
-            base.Initialize();
-            SubscribeLocalEvent<SignallerComponent, ComponentInit>(OnInit);
-            SubscribeLocalEvent<SignallerComponent, UseInHandEvent>(OnUseInHand);
-        }
+        base.Initialize();
 
-        private void OnInit(EntityUid uid, SignallerComponent component, ComponentInit args)
-        {
-            _signalSystem.EnsureTransmitterPorts(uid, component.Port);
-        }
+        SubscribeLocalEvent<SignallerComponent, ComponentInit>(OnInit);
+        SubscribeLocalEvent<SignallerComponent, UseInHandEvent>(OnUseInHand);
+        SubscribeLocalEvent<SignallerComponent, TriggerEvent>(OnTrigger);
+    }
 
-        private void OnUseInHand(EntityUid uid, SignallerComponent component, UseInHandEvent args)
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        // do any deferred triggers for this tick
+        var list = _triggering.ToList();
+        _triggering.Clear();
+        foreach (var (uid, port) in list)
         {
-            if (args.Handled)
-                return;
-            _signalSystem.InvokePort(uid, component.Port);
-            args.Handled = true;
-        }   
+            _signal.InvokePort(uid, port);
+        }
+    }
+
+    private void OnInit(EntityUid uid, SignallerComponent component, ComponentInit args)
+    {
+        _signal.EnsureTransmitterPorts(uid, component.Port);
+    }
+
+    private void OnUseInHand(EntityUid uid, SignallerComponent component, UseInHandEvent args)
+    {
+        if (args.Handled)
+            return;
+        _signal.InvokePort(uid, component.Port);
+        args.Handled = true;
+    }
+
+    private void OnTrigger(EntityUid uid, SignallerComponent component, TriggerEvent args)
+    {
+        // defer it to the next tick to prevent stack overflow
+        _triggering.Add((uid, component.Port));
+        args.Handled = true;
     }
 }
