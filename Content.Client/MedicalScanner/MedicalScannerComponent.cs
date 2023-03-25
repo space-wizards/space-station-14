c952a56838517024d3b5b9c7dd7b1a21abd68267
TemporalOroboros <TemporalOroboros@gmail.com>
2023-03-25 15:01:56 -0700

    Use GenericVisualizerComponent + Make Yellow MedicalScannerVisualizer state actually reachable

--- Content.Client/MedicalScanner/MedicalScannerComponent.cs
+++ Content.Client/MedicalScanner/MedicalScannerComponent.cs
@@ -5,29 +5,4 @@ namespace Content.Client.MedicalScanner;
 [RegisterComponent]
 public sealed class MedicalScannerComponent : SharedMedicalScannerComponent
 {
-    /// <summary>
-    /// A map of the base machine sprite states indexed by which machine states they correspond to.
-    /// </summary>
-    public readonly Dictionary<MedicalScannerStatus, string> MachineStateStatusMap = new()
-    {
-        [MedicalScannerStatus.Off] = "closed",
-        [MedicalScannerStatus.Open] = "open",
-        [MedicalScannerStatus.Red] = "occupied",
-        [MedicalScannerStatus.Death] = "occupied",
-        [MedicalScannerStatus.Green] = "occupied",
-        [MedicalScannerStatus.Yellow] = "occupied",
-    };
-
-    /// <summary>
-    /// A map of the machine terminal sprite states indexed by which machine states they correspond to.
-    /// </summary>
-    public readonly Dictionary<MedicalScannerStatus, string> TerminalStateStatusMap = new()
-    {
-        [MedicalScannerStatus.Off] = "off_unlit",
-        [MedicalScannerStatus.Open] = "idle_unlit",
-        [MedicalScannerStatus.Red] = "red_unlit",
-        [MedicalScannerStatus.Death] = "off_unlit",
-        [MedicalScannerStatus.Green] = "idle_unlit",
-        [MedicalScannerStatus.Yellow] = "maint_unlit",
-    };
 }
