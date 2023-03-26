c8d0670f71efab843fd1f5d75ae2c22da2abd0a8
Kara <lunarautomaton6@gmail.com>
2023-03-25 20:53:02 -0500

    Buff bullet impulses a wee bit

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -402,7 +402,7 @@ public abstract partial class SharedGunSystem : EntitySystem
         var toMap = toCoordinates.ToMapPos(EntityManager, Transform);
         var shotDirection = (toMap - fromMap).Normalized;
 
-        const float impulseStrength = 5.0f;
+        const float impulseStrength = 25.0f;
         var impulseVector =  shotDirection * impulseStrength;
         Physics.ApplyLinearImpulse(user, -impulseVector, body: userPhysics);
     }
