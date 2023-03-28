18e0995589440dd0d2c062e31d8bdc691e475fdb
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2023-03-27 18:59:16 -0700

    Basic mind ghost and transfer test

--- Content.IntegrationTests/Tests/Minds/MindTests.cs
+++ Content.IntegrationTests/Tests/Minds/MindTests.cs
@@ -1,8 +1,12 @@
 ﻿#nullable enable
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Content.Server.Ghost;
 using Content.Server.Mind;
+using Content.Server.Mind.Commands;
 using Content.Server.Mind.Components;
+using Content.Server.Players;
 using Content.Server.Roles;
 using Content.Server.Traitor;
 using Content.Shared.Damage;
@@ -10,11 +14,15 @@ using Content.Shared.Damage.Prototypes;
 using Content.Shared.FixedPoint;
 using Content.Shared.Roles;
 using NUnit.Framework;
+using Robust.Client.Player;
 using Robust.Server.GameObjects;
+using Robust.Server.Player;
 using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
 using Robust.Shared.Map;
 using Robust.Shared.Network;
 using Robust.Shared.Prototypes;
+using IPlayerManager = Robust.Server.Player.IPlayerManager;
 
 namespace Content.IntegrationTests.Tests.Minds;
 
@@ -291,5 +299,85 @@ public sealed class MindTests
         });
 
         await pairTracker.CleanReturnAsync();
+    }
+
+    [Test]
+    public async Task TestPlayerCanGhost()
+    {
+        // Client is needed to spawn session
+        await using var pairTracker = await PoolManager.GetServerClient();
+        var server = pairTracker.Pair.Server;
+
+        var entMan = server.ResolveDependency<IServerEntityManager>();
+        var playerMan = server.ResolveDependency<IPlayerManager>();
+
+        var mindSystem = entMan.EntitySysManager.GetEntitySystem<MindSystem>();
+        var ghostSystem = entMan.EntitySysManager.GetEntitySystem<GhostSystem>();
+
+        EntityUid entity = default!;
+        Mind mind = default!;
+        IPlayerSession player = playerMan.ServerSessions.Single();
+
+        await server.WaitAssertion(() =>
+        {
+            entity = entMan.SpawnEntity(null, new MapCoordinates());
+            var mindComp = entMan.EnsureComponent<MindContainerComponent>(entity);
+
+            mind = mindSystem.CreateMind(player.UserId, "Mindy McThinker");
+
+            Assert.That(mind.UserId, Is.EqualTo(player.UserId));
+
+            mindSystem.TransferTo(mind, entity);
+            Assert.That(mindSystem.GetMind(entity, mindComp), Is.EqualTo(mind));
+        });
+
+        await PoolManager.RunTicksSync(pairTracker.Pair, 5);
+
+        await server.WaitAssertion(() =>
+        {
+            entMan.DeleteEntity(entity);
+        });
+
+        await PoolManager.RunTicksSync(pairTracker.Pair, 5);
+
+        EntityUid mob = default!;
+        Mind mobMind = default!;
+
+        await server.WaitAssertion(() =>
+        {
+            Assert.That(mind.OwnedEntity != null);
+
+            mob = entMan.SpawnEntity(null, new MapCoordinates());
+
+            MakeSentientCommand.MakeSentient(mob, IoCManager.Resolve<IEntityManager>());
+            mobMind = mindSystem.CreateMind(player.UserId, "Mindy McThinker the Second");
+
+            mindSystem.ChangeOwningPlayer(mobMind, player.UserId);
+            mindSystem.TransferTo(mobMind, mob);
+        });
+
+        await PoolManager.RunTicksSync(pairTracker.Pair, 5);
+
+        await server.WaitAssertion(() =>
+        {
+            var m = player.ContentData()?.Mind;
+            Assert.That(m, Is.Not.EqualTo(null));
+            Assert.That(m!.OwnedEntity, Is.EqualTo(mob));
+            Assert.That(m, Is.Not.EqualTo(mind));
+        });
+
+        await pairTracker.CleanReturnAsync();
+    }
+
+    [Test]
+    public async Task TestPlayerCanReturnFromGhostWhenDead()
+    {
+        // TODO Implement
+    }
+
+    [Test]
+    public async Task TestGhostDoesNotInfiniteLoop()
+    {
+        // TODO Implement
     }
 }
