0f5805fe806ec8ab14671dd4047405c3172ae2b8
deltanedas <@deltanedas:kde.org>
2023-03-29 07:51:47 +0100

    inject popup

--- Content.Server/Weapons/Melee/MeleeWeaponSystem.cs
+++ Content.Server/Weapons/Melee/MeleeWeaponSystem.cs
@@ -251,7 +251,10 @@ public sealed class MeleeWeaponSystem : SharedMeleeWeaponSystem
 
             // prevent deathnettles injecting through hardsuits
             if (!comp.PierceArmor && _inventory.TryGetSlotEntity(entity, "outerClothing", out var suit) && _tag.HasTag(suit.Value, "Hardsuit"))
+            {
+                _popup.PopupEntity(Loc.GetString("melee-inject-failed-hardsuit", ("weapon", owner))
                 continue;
+            }
 
             if (bloodQuery.TryGetComponent(entity, out var bloodstream))
                 hitBloodstreams.Add((entity, bloodstream));
