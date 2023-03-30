3f5afc048ea56ceb6b9c414b229eb406b3e65783
EmoGarbage404 <retron404@gmail.com>
2023-03-30 13:06:14 -0400

    VV and restore cloth to glory

--- Content.Shared/Materials/MaterialReclaimerComponent.cs
+++ Content.Shared/Materials/MaterialReclaimerComponent.cs
@@ -15,52 +15,52 @@ public sealed class MaterialReclaimerComponent : Component
     /// Whether or not the machine has power. We put it here
     /// so we can network and predict it.
     /// </summary>
-    [DataField("powered")]
+    [DataField("powered"), ViewVariables(VVAccess.ReadWrite)]
     public bool Powered;
 
     /// <summary>
     /// How quickly it takes to consume X amount of materials per second.
     /// For example, with a rate of 50, an entity with 100 total material takes 2 seconds to process.
     /// </summary>
-    [DataField("baseMaterialProcessRate")]
+    [DataField("baseMaterialProcessRate"), ViewVariables(VVAccess.ReadWrite)]
     public float BaseMaterialProcessRate = 50f;
 
     /// <summary>
     /// How quickly it takes to consume X amount of materials per second.
     /// For example, with a rate of 50, an entity with 100 total material takes 2 seconds to process.
     /// </summary>
-    [DataField("materialProcessRate")]
+    [DataField("materialProcessRate"), ViewVariables(VVAccess.ReadWrite)]
     public float MaterialProcessRate;
 
     /// <summary>
     /// Machine part whose rating modifies <see cref="MaterialProcessRate"/>
     /// </summary>
-    [DataField("machinePartProcessRate", customTypeSerializer: typeof(PrototypeIdSerializer<MachinePartPrototype>))]
+    [DataField("machinePartProcessRate", customTypeSerializer: typeof(PrototypeIdSerializer<MachinePartPrototype>)), ViewVariables(VVAccess.ReadWrite)]
     public string MachinePartProcessRate = "Manipulator";
 
     /// <summary>
     /// How much the machine part quality affects the <see cref="MaterialProcessRate"/>
     /// </summary>
-    [DataField("partRatingProcessRateMultiplier")]
+    [DataField("partRatingProcessRateMultiplier"), ViewVariables(VVAccess.ReadWrite)]
     public float PartRatingProcessRateMultiplier = 1.5f;
 
     /// <summary>
     /// The minimum amount fo time it can take to process an entity.
     /// this value supercedes the calculated one using <see cref="MaterialProcessRate"/>
     /// </summary>
-    [DataField("minimumProcessDuration")]
+    [DataField("minimumProcessDuration"), ViewVariables(VVAccess.ReadWrite)]
     public TimeSpan MinimumProcessDuration = TimeSpan.FromSeconds(0.5f);
 
     /// <summary>
     /// The id of our output solution
     /// </summary>
-    [DataField("solutionContainerId")]
+    [DataField("solutionContainerId"), ViewVariables(VVAccess.ReadWrite)]
     public string SolutionContainerId = "output";
 
     /// <summary>
     /// The prototype for the puddle
     /// </summary>
-    [DataField("puddleId", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    [DataField("puddleId", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>)), ViewVariables(VVAccess.ReadWrite)]
     public string PuddleId = "PuddleSmear";
 
     /// <summary>
