4434b3752c844d6744a7ddee8e958ee3bb31d179
GoodWheatley <109803540+GoodWheatley@users.noreply.github.com>
2023-04-01 17:47:21 -0400

    Chat Filter (April Fools) (#14999)

---
+++ Content.Server/Speech/EntitySystems/ChatFilterAccentSystem.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.RegularExpressions;
+using Content.Server.Speech.Components;
+using Content.Shared.CCVar;
+using Robust.Shared.Configuration;
+
+namespace Content.Server.Speech.EntitySystems;
+
+public sealed class ChatFilterAccentSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    private static readonly Dictionary<string, string> DirectReplacements = new()
+    {
+        { "fuck", "####" },
+        { "shit", "####" },
+        { "ass", "###" },
+        { "dick", "####" },
+        { "bitch", "#####" },
+        { "piss", "####" },
+        { "damn", "####" },
+        { "kill", "####" },
+        { "hurt", "####" },
+        { "god", "###" },
+        { "hell", "####" },
+        { "nuke", "####" },
+        { "dang", "####" },
+        { "lol", "###" },
+        { "nukies", "#####" },
+        { "nukie", "#####" },
+        { "gun", "###" },
+        { "ammo", "####" },
+        { "bridge", "#####" },
+        { "med", "###" },
+        { "clown", "#####" },
+        { "admin", "#####" },
+        { "badmin", "######" },
+        { "admeme", "######" },
+        { "badmeme", "######" },
+        { "centcom", "#######" },
+        { "singulo", "#######" },
+        { "singuloose", "##########" },
+        { "sword", "#####" },
+        { "dead", "####" },
+        { "died", "####" },
+        { "sus", "###" },
+        { "aos", "###" },
+        { "kos", "###" },
+        { "anomaly", "#######" },
+        { "shuttle", "#######" },
+        { "hos", "###" },
+        { "drunk", "#####" },
+        { "bar", "###" },
+        { "arrest", "#####" },
+        { "bomb", "####" },
+        { "I", "#" },
+        { "traitor", "#######" },
+        { "tator", "#####" },
+        { "bad", "###" },
+    };
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ChatFilterAccentComponent, AccentGetEvent>(OnAccentGet);
+    }
+
+    public string Accentuate(string message)
+    {
+        var msg = message;
+
+        if (_cfg.GetCVar(CCVars.GameChatFilter) == true)
+        {
+
+            foreach (var (first, replace) in DirectReplacements)
+            {
+                msg = Regex.Replace(msg, $@"{first}", replace, RegexOptions.IgnoreCase);
+            }
+
+            return msg;
+        }
+
+        return msg;
+    }
+
+    private void OnAccentGet(EntityUid uid, ChatFilterAccentComponent component, AccentGetEvent args)
+    {
+        args.Message = Accentuate(args.Message);
+    }
+}
