8e08e3efdb24478d12ce36942ff9d9a59407defe
moonheart08 <moonheart08@users.noreply.github.com>
2023-04-04 00:25:27 -0500

    o

--- Content.IntegrationTests/PoolManager.cs
+++ Content.IntegrationTests/PoolManager.cs
@@ -57,6 +57,7 @@ public static class PoolManager
         (CCVars.ArrivalsShuttles.Name,        "false"),
         (CCVars.EmergencyShuttleEnabled.Name, "false"),
         (CCVars.ProcgenPreload.Name,          "false"),
+        (CCVars.WorldgenEnabled.Name,         "false"),
         // @formatter:on
     };
 
