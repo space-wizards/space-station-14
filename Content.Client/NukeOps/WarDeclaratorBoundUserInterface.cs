0ea3f489495ca182ad89074ba75c25d143a77641
CaptainSqrBeard <iliya.11.levchenko@gmail.com>
2023-04-06 19:36:21 +0600

    UI things

--- Content.Client/NukeOps/WarDeclaratorBoundUserInterface.cs
+++ Content.Client/NukeOps/WarDeclaratorBoundUserInterface.cs
@@ -18,9 +18,6 @@ namespace Content.Client.NukeOps
 
         private WarDeclaratorWindow? _window;
         public WarDeclaratorBoundUserInterface(ClientUserInterfaceComponent owner, Enum uiKey) : base(owner, uiKey) {}
-        private TimeSpan _endTime;
-        private TimeSpan _timeStamp;
-        private WarConditionStatus _status;
 
         protected override void Open()
         {
@@ -35,7 +32,6 @@ namespace Content.Client.NukeOps
             _window.OnClose += Close;
             _window.OnMessageEntered += OnMessageChanged;
             _window.OnWarButtonPressed += OnWarButtonPressed;
-            _window.OnDraw += UpdateTimer;
         }
 
         private void OnMessageChanged(string newMsg)
@@ -48,54 +44,6 @@ namespace Content.Client.NukeOps
             SendMessage(new WarDeclaratorPressedWarButton(_window?.MessageLineEdit.Text));
         }
 
-        public void UpdateTimer()
-        {
-            if (_window == null)
-            {
-                return;
-            }
-
-            switch(_status)
-            {
-                case WarConditionStatus.YES_WAR:
-                    var gameruleTime = _gameTiming.CurTime.Subtract(_timeStamp);
-                    var timeLeft = _endTime.Subtract(gameruleTime);
-
-                    if (timeLeft > TimeSpan.Zero)
-                    {
-                        _window.InfoLabel.Text = Loc.GetString("war-declarator-boost-timer", ("minutes", timeLeft.Minutes), ("seconds", timeLeft.Seconds));
-                    }
-                    else
-                    {
-                        _status = WarConditionStatus.NO_WAR_TIMEOUT;
-                        _window.StatusLabel.Text = Loc.GetString("war-declarator-boost-impossible");
-                        _window.InfoLabel.Text = Loc.GetString("war-declarator-conditions-time-out");
-                        _window.StatusLabel.SetOnlyStyleClass(StyleNano.StyleClassPowerStateNone);
-                        _window.WarButton.Disabled = true;
-                    }
-                    break;
-                case WarConditionStatus.WAR_DELAY:
-                    var timeAfterDeclaration = _gameTiming.CurTime.Subtract(_timeStamp);
-                    var timeRemain = _endTime.Subtract(timeAfterDeclaration);
-
-                    if (timeRemain > TimeSpan.Zero)
-                    {
-                        _window.InfoLabel.Text = Loc.GetString("war-declarator-boost-timer", ("minutes", timeRemain.Minutes), ("seconds", timeRemain.Seconds));
-                    }
-                    else
-                    {
-                        _status = WarConditionStatus.WAR_READY;
-                        _window.StatusLabel.Text = Loc.GetString("war-declarator-boost-declared");
-                        _window.InfoLabel.Text = Loc.GetString("war-declarator-conditions-ready");
-                        _window.StatusLabel.SetOnlyStyleClass(StyleNano.StyleClassPowerStateLow);
-                        _window.WarButton.Disabled = true;
-                    }
-                    break;
-                default:
-                    return;
-            }
-        }
-
         /// <summary>
         /// Update the UI state based on server-sent info
         /// </summary>
@@ -106,51 +54,7 @@ namespace Content.Client.NukeOps
             if (_window == null || state is not WarDeclaratorBoundUserInterfaceState cast)
                 return;
 
-            _window.WarButton.Disabled = cast.Status != WarConditionStatus.YES_WAR;
-            _timeStamp = cast.TimeStamp;
-            _endTime = cast.EndTime;
-            _status = cast.Status;
-
-            switch(cast.Status)
-            {
-                case WarConditionStatus.WAR_READY:
-                    _window.StatusLabel.Text = Loc.GetString("war-declarator-boost-declared");
-                    _window.InfoLabel.Text = Loc.GetString("war-declarator-conditions-ready");
-                    _window.StatusLabel.SetOnlyStyleClass(StyleNano.StyleClassPowerStateLow);
-                    break;
-                case WarConditionStatus.WAR_DELAY:
-                    _window.StatusLabel.Text = Loc.GetString("war-declarator-boost-declared-delay");
-                    UpdateTimer();
-                    _window.StatusLabel.SetOnlyStyleClass(StyleNano.StyleClassPowerStateLow);
-                    break;
-                case WarConditionStatus.YES_WAR:
-                    _window.StatusLabel.Text = Loc.GetString("war-declarator-boost-possible");
-                    UpdateTimer();
-                    _window.StatusLabel.SetOnlyStyleClass(StyleNano.StyleClassPowerStateGood);
-                    break;
-                case WarConditionStatus.NO_WAR_SMALL_CREW:
-                    _window.StatusLabel.Text = Loc.GetString("war-declarator-boost-impossible");
-                    _window.InfoLabel.Text = Loc.GetString("war-declarator-conditions-small-crew", ("min_size", cast.MinCrew));
-                    _window.StatusLabel.SetOnlyStyleClass(StyleNano.StyleClassPowerStateNone);
-                    break;
-                case WarConditionStatus.NO_WAR_SHUTTLE_DEPARTED:
-                    _window.StatusLabel.Text = Loc.GetString("war-declarator-boost-impossible");
-                    _window.InfoLabel.Text = Loc.GetString("war-declarator-conditions-left-outpost");
-                    _window.StatusLabel.SetOnlyStyleClass(StyleNano.StyleClassPowerStateNone);
-                    break;
-                case WarConditionStatus.NO_WAR_TIMEOUT:
-                    _window.StatusLabel.Text = Loc.GetString("war-declarator-boost-impossible");
-                    _window.InfoLabel.Text = Loc.GetString("war-declarator-conditions-time-out");
-                    _window.StatusLabel.SetOnlyStyleClass(StyleNano.StyleClassPowerStateNone);
-                    break;
-                default:
-                    _window.StatusLabel.Text = Loc.GetString("war-declarator-boost-impossible");
-                    _window.InfoLabel.Text = Loc.GetString("war-declarator-conditions-unknown");
-                    _window.StatusLabel.SetOnlyStyleClass(StyleNano.StyleClassPowerStateNone);
-                    break;
-            }
-
-            _window.SetMessage(cast.Message);
+            _window?.UpdateStatus(cast);
         }
 
         protected override void Dispose(bool disposing)
--- Content.Server/GameTicking/Rules/Components/NukeOpsShuttleComponent.cs
+++ Content.Server/GameTicking/Rules/Components/NukeOpsShuttleComponent.cs
@@ -1,10 +1,9 @@
 namespace Content.Server.GameTicking.Rules.Components;
+
+/// <summary>
+/// Tags grid as nuke ops shuttle
+/// </summary>
+[RegisterComponent]
+public sealed class NukeOpsShuttleComponent : Component
 {
-    /// <summary>
-    /// Tags grid as nuke ops shuttle
-    /// </summary>
-    [RegisterComponent]
-    public sealed class NukeOpsShuttleComponent : Component
-    {
-    }
 }
