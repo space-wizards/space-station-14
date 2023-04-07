376324a1826cc703a8777f2ac1411924daf77e5b
TemporalOroboros <TemporalOroboros@gmail.com>
2023-04-07 04:55:11 -0700

    Resolves reviews

--- Content.Client/Power/SMES/SmesVisualizerComponent.cs
+++ Content.Client/Power/SMES/SmesVisualizerComponent.cs
@@ -1,5 +1,26 @@
 ﻿namespace Content.Client.Power.SMES;
 
 [RegisterComponent]
-public sealed class SmesVisualizerComponent : Component
-{}
+public sealed class SmesVisualsComponent : Component
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataField("chargeOverlayPrefix")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string ChargeOverlayPrefix = "smes-og";
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataField("inputOverlayPrefix")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string InputOverlayPrefix = "smes-oc";
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataField("outputOverlayPrefix")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string OutputOverlayPrefix = "smes-op";
+}
--- Content.Client/Power/SMES/SmesVisualizerSystem.cs
+++ Content.Client/Power/SMES/SmesVisualizerSystem.cs
@@ -4,70 +4,45 @@ using Robust.Client.GameObjects;
 
 namespace Content.Client.Power.SMES;
 
-public sealed class SmesVisualizerSystem : VisualizerSystem<SmesVisualizerComponent>
+public sealed class SmesVisualizerSystem : VisualizerSystem<SmesVisualsComponent>
 {
-    public override void Initialize()
-    {
-        base.Initialize();
-        SubscribeLocalEvent<SmesVisualizerComponent, ComponentInit>(OnInit);
-    }
-
-    private void OnInit(EntityUid uid, SmesVisualizerComponent comp, ComponentInit args)
-    {
-        if(!TryComp<SpriteComponent>(uid, out var sprite))
-            return;
-
-        sprite.LayerMapSet(Layers.Input, sprite.AddLayerState("smes-oc0"));
-        sprite.LayerSetShader(Layers.Input, "unshaded");
-        sprite.LayerMapSet(Layers.Charge, sprite.AddLayerState("smes-og1"));
-        sprite.LayerSetShader(Layers.Charge, "unshaded");
-        sprite.LayerSetVisible(Layers.Charge, false);
-        sprite.LayerMapSet(Layers.Output, sprite.AddLayerState("smes-op0"));
-        sprite.LayerSetShader(Layers.Output, "unshaded");
-    }
-
-    protected override void OnAppearanceChange(EntityUid uid, SmesVisualizerComponent comp, ref AppearanceChangeEvent args)
+    protected override void OnAppearanceChange(EntityUid uid, SmesVisualsComponent comp, ref AppearanceChangeEvent args)
     {
         if (args.Sprite == null)
             return;
 
         if (!AppearanceSystem.TryGetData<int>(uid, SmesVisuals.LastChargeLevel, out var level, args.Component) || level == 0)
         {
-            args.Sprite.LayerSetVisible(Layers.Charge, false);
+            args.Sprite.LayerSetVisible(SmesVisualLayers.Charge, false);
         }
         else
         {
-            args.Sprite.LayerSetVisible(Layers.Charge, true);
-            args.Sprite.LayerSetState(Layers.Charge, $"smes-og{level}");
+            args.Sprite.LayerSetVisible(SmesVisualLayers.Charge, true);
+            args.Sprite.LayerSetState(SmesVisualLayers.Charge, $"{comp.ChargeOverlayPrefix}{level}");
         }
 
-        if (AppearanceSystem.TryGetData<ChargeState>(uid, SmesVisuals.LastChargeState, out var state, args.Component))
+        if (!AppearanceSystem.TryGetData<ChargeState>(uid, SmesVisuals.LastChargeState, out var state, args.Component))
+            state = ChargeState.Still;
+
+        switch (state)
         {
-            switch (state)
-            {
-                case ChargeState.Still:
-                    args.Sprite.LayerSetState(Layers.Input, "smes-oc0");
-                    args.Sprite.LayerSetState(Layers.Output, "smes-op1");
-                    break;
-                case ChargeState.Charging:
-                    args.Sprite.LayerSetState(Layers.Input, "smes-oc1");
-                    args.Sprite.LayerSetState(Layers.Output, "smes-op1");
-                    break;
-                case ChargeState.Discharging:
-                    args.Sprite.LayerSetState(Layers.Input, "smes-oc0");
-                    args.Sprite.LayerSetState(Layers.Output, "smes-op2");
-                    break;
-            }
-        }
-        else
-        {
-            args.Sprite.LayerSetState(Layers.Input, "smes-oc0");
-            args.Sprite.LayerSetState(Layers.Output, "smes-op1");
+            case ChargeState.Still:
+                args.Sprite.LayerSetState(SmesVisualLayers.Input, $"{comp.InputOverlayPrefix}0");
+                args.Sprite.LayerSetState(SmesVisualLayers.Output, $"{comp.OutputOverlayPrefix}1");
+                break;
+            case ChargeState.Charging:
+                args.Sprite.LayerSetState(SmesVisualLayers.Input, $"{comp.InputOverlayPrefix}1");
+                args.Sprite.LayerSetState(SmesVisualLayers.Output, $"{comp.OutputOverlayPrefix}1");
+                break;
+            case ChargeState.Discharging:
+                args.Sprite.LayerSetState(SmesVisualLayers.Input, $"{comp.InputOverlayPrefix}0");
+                args.Sprite.LayerSetState(SmesVisualLayers.Output, $"{comp.OutputOverlayPrefix}2");
+                break;
         }
     }
 }
 
-enum Layers : byte
+enum SmesVisualLayers : byte
 {
     Input,
     Charge,
