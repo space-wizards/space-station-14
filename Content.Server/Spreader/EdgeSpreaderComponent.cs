5d32e0bb867a9408214cf83651a526aa374d445d
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-04-10 15:26:56 +1000

    snek

--- Content.Server/Spreader/EdgeSpreaderComponent.cs
+++ Content.Server/Spreader/EdgeSpreaderComponent.cs
@@ -1,5 +1,9 @@
 namespace Content.Server.Spreader;
 
+/// <summary>
+/// Added to entities being considered for spreading via <see cref="SpreaderSystem"/>.
+/// This needs to be manually added and removed.
+/// </summary>
 [RegisterComponent]
 public sealed class EdgeSpreaderComponent : Component
 {
