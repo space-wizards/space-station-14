2d8997fd7514b2112dca097860a7d872f90fdbb9
TemporalOroboros <TemporalOroboros@gmail.com>
2023-04-11 01:52:20 -0700

    Resolves reviews

--- Content.Client/PowerCell/PowerChargerVisualizerSystem.cs
+++ Content.Client/PowerCell/PowerChargerVisualizerSystem.cs
@@ -3,73 +3,38 @@ using Robust.Client.GameObjects;
 
 namespace Content.Client.PowerCell;
 
-public sealed class PowerChargerVisualizerSystem : VisualizerSystem<PowerChargerVisualizerComponent>
+public sealed class PowerChargerVisualizerSystem : VisualizerSystem<PowerChargerVisualsComponent>
 {
-    public override void Initialize()
-    {
-        base.Initialize();
-        SubscribeLocalEvent<PowerChargerVisualizerComponent, ComponentInit>(OnInit);
-    }
-
-    private void OnInit(EntityUid uid, PowerChargerVisualizerComponent comp, ComponentInit args)
-    {
-        if(!TryComp<SpriteComponent>(uid, out var sprite))
-            return;
-
-        // Base item
-        sprite.LayerMapSet(Layers.Base, sprite.AddLayerState("empty"));
-
-        // Light
-        sprite.LayerMapSet(Layers.Light, sprite.AddLayerState("light-off"));
-        sprite.LayerSetShader(Layers.Light, "unshaded");
-    }
-
-    protected override void OnAppearanceChange(EntityUid uid, PowerChargerVisualizerComponent comp, ref AppearanceChangeEvent args)
+    protected override void OnAppearanceChange(EntityUid uid, PowerChargerVisualsComponent comp, ref AppearanceChangeEvent args)
     {
         if (args.Sprite == null)
             return;
 
         // Update base item
-        if (AppearanceSystem.TryGetData(uid, CellVisual.Occupied, out bool occupied, args.Component))
+        if (AppearanceSystem.TryGetData<bool>(uid, CellVisual.Occupied, out var occupied, args.Component) && occupied)
         {
             // TODO: don't throw if it doesn't have a full state
-            args.Sprite.LayerSetState(Layers.Base, occupied ? "full" : "empty");
+            args.Sprite.LayerSetState(PowerChargerVisualLayers.Base, comp.OccupiedState);
         }
         else
         {
-            args.Sprite.LayerSetState(Layers.Base, "empty");
+            args.Sprite.LayerSetState(PowerChargerVisualLayers.Base, comp.EmptyState);
         }
 
         // Update lighting
-        if (AppearanceSystem.TryGetData(uid, CellVisual.Light, out CellChargerStatus status, args.Component))
+        if (AppearanceSystem.TryGetData<CellChargerStatus>(uid, CellVisual.Light, out var status, args.Component)
+        &&  comp.LightStates.TryGetValue(status, out var lightState))
         {
-            switch (status)
-            {
-                case CellChargerStatus.Off:
-                    args.Sprite.LayerSetState(Layers.Light, "light-off");
-                    break;
-                case CellChargerStatus.Empty:
-                    args.Sprite.LayerSetState(Layers.Light, "light-empty");
-                    break;
-                case CellChargerStatus.Charging:
-                    args.Sprite.LayerSetState(Layers.Light, "light-charging");
-                    break;
-                case CellChargerStatus.Charged:
-                    args.Sprite.LayerSetState(Layers.Light, "light-charged");
-                    break;
-                default:
-                    args.Sprite.LayerSetState(Layers.Light, "light-off");
-                    break;
-            }
+            args.Sprite.LayerSetState(PowerChargerVisualLayers.Light, lightState);
+            args.Sprite.LayerSetVisible(PowerChargerVisualLayers.Light, true);
         }
         else
-        {
-            args.Sprite.LayerSetState(Layers.Light, "light-off");
-        }
+            // 
+            args.Sprite.LayerSetVisible(PowerChargerVisualLayers.Light, false);
     }
 }
 
-enum Layers : byte
+enum PowerChargerVisualLayers : byte
 {
     Base,
     Light,
