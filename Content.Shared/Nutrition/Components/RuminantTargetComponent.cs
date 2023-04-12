e13d8a0565e6821aec3f113312b0e528d1f7cf36
Kara <lunarautomaton6@gmail.com>
2023-04-12 00:04:11 -0500

    yaml

---
+++ Content.Shared/Nutrition/Components/RuminantTargetComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Shared.Nutrition.Components;
+
+/// <summary>
+///     Specifies an entity that ruminants (goats, cows, etc) will automatically try to eat.
+///     Like Kudzu.
+/// </summary>
+[RegisterComponent]
+public sealed class RuminantTargetComponent : Component
+{
+}
