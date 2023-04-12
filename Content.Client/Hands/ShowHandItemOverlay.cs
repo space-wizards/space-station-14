57e357463265f75157edda5ce995f860bc31facd
Artjom <artjombebenin@gmail.com>
2023-04-12 21:24:02 +0300

    set weapon sight

--- Content.Client/Hands/ShowHandItemOverlay.cs
+++ Content.Client/Hands/ShowHandItemOverlay.cs
@@ -18,6 +18,8 @@ namespace Content.Client.Hands
 
         private readonly IRenderTexture _renderBackbuffer;
 
+        private float _handIconOffset;
+
         public override OverlaySpace Space => OverlaySpace.ScreenSpace;
 
         public Texture? IconOverride;
@@ -27,8 +29,10 @@ namespace Content.Client.Hands
         {
             IoCManager.InjectDependencies(this);
 
+            _handIconOffset = _cfg.GetCVar(CCVars.HudHeldItemOffset);
+
             _renderBackbuffer = _clyde.CreateRenderTarget(
-                (64, 64),
+                (120, 120),
                 new RenderTargetFormatParameters(RenderTargetColorFormat.Rgba8Srgb, true),
                 new TextureSampleParameters
                 {
@@ -49,29 +53,63 @@ namespace Content.Client.Hands
                 return;
 
             var screen = args.ScreenHandle;
-            var offset = _cfg.GetCVar(CCVars.HudHeldItemOffset);
             var mousePos = _inputManager.MouseScreenPosition.Position;
 
-            if (IconOverride != null)
-            {
-                screen.DrawTexture(IconOverride, mousePos - IconOverride.Size / 2 + offset, Color.White.WithAlpha(0.75f));
+            if (IsIfDrawIconOverride(screen, mousePos))
                 return;
-            }
 
             var handEntity = EntityOverride ?? EntitySystem.Get<HandsSystem>().GetActiveHandEntity();
 
             if (handEntity == null || !_entMan.HasComponent<SpriteComponent>(handEntity))
                 return;
 
-            var halfSize = _renderBackbuffer.Size / 2;
+            // if (handEntity != null) {
+            //     _entMan.TryGetComponent<MetaDataComponent>(handEntity, out var metaDat);
+            // }
+
             var uiScale = (args.ViewportControl as Control)?.UIScale ?? 1f;
+            var bufferCenterOffset = _renderBackbuffer.Size / 2;
 
             screen.RenderInRenderTarget(_renderBackbuffer, () =>
             {
-                screen.DrawEntity(handEntity.Value, halfSize, new Vector2(1f, 1f) * uiScale, Direction.South);
+                DrawWeaponSight(screen, bufferCenterOffset, uiScale);
+                DrawHandEntityIcon(screen, handEntity.Value, bufferCenterOffset, uiScale);
             }, Color.Transparent);
 
-            screen.DrawTexture(_renderBackbuffer.Texture, mousePos - halfSize + offset, Color.White.WithAlpha(0.75f));
+            screen.DrawTexture(_renderBackbuffer.Texture,
+                mousePos - bufferCenterOffset, Color.White.WithAlpha(0.8f));
+        }
+
+        private bool IsIfDrawIconOverride(DrawingHandleScreen screen, Vector2 mousePos)
+        {
+            if (IconOverride != null)
+            {
+                screen.DrawTexture(IconOverride, mousePos - IconOverride.Size / 2 + _handIconOffset,
+                    Color.White.WithAlpha(0.75f));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void DrawHandEntityIcon(DrawingHandleScreen screen, EntityUid handEntity,
+            Vector2 centerPos, float scale)
+        {
+            Vector2 offset = centerPos + _handIconOffset;
+            screen.DrawCircle(offset, offset.X / 4.5f * scale, Color.White.WithAlpha(0.02f));
+            screen.DrawEntity(handEntity, offset, new Vector2(1f, 1f) * scale, Direction.South);
+        }
+
+        private void DrawWeaponSight(DrawingHandleScreen screen, Vector2 centerPos, float scale)
+        {
+            float circleRadious = centerPos.X / 3f * scale;
+            screen.DrawCircle(centerPos, circleRadious, Color.Purple, false);
+            screen.DrawCircle(centerPos, (circleRadious) + 1, Color.Purple, false);
+            screen.DrawCircle(centerPos, (circleRadious) + 2, Color.Purple, false);
+            screen.DrawCircle(centerPos, (circleRadious) + 3, Color.Purple, false);
+            screen.DrawCircle(centerPos, (circleRadious) + 4, Color.Red, false);
         }
     }
 }
