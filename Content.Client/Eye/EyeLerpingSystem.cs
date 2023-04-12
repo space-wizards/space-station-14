742aa24b276475baffe68829d88a44633c9eb778
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-04-12 21:52:20 +1000

    feex

--- Content.Client/Eye/EyeLerpingSystem.cs
+++ Content.Client/Eye/EyeLerpingSystem.cs
@@ -28,9 +28,6 @@ public sealed class EyeLerpingSystem : EntitySystem
         SubscribeLocalEvent<EyeComponent, ComponentShutdown>(OnEyeShutdown);
         SubscribeLocalEvent<EyeComponent, PlayerAttachedEvent>(OnAttached);
 
-        SubscribeLocalEvent<FollowerComponent, ComponentStartup>(OnFollowerStartup);
-        SubscribeLocalEvent<FollowedComponent, EntParentChangedMessage>(OnFollowerParentChange);
-
         SubscribeLocalEvent<LerpingEyeComponent, EntParentChangedMessage>(HandleMapChange);
         SubscribeLocalEvent<LerpingEyeComponent, PlayerDetachedEvent>(OnDetached);
 
@@ -38,28 +35,6 @@ public sealed class EyeLerpingSystem : EntitySystem
         UpdatesAfter.Add(typeof(PhysicsSystem));
         UpdatesBefore.Add(typeof(EyeUpdateSystem));
         UpdatesOutsidePrediction = true;
-    }
-
-    private void OnFollowerStartup(EntityUid uid, FollowerComponent component, ComponentStartup args)
-    {
-        if (!TryComp<LerpingEyeComponent>(uid, out var lerping) || !TryComp<TransformComponent>(uid, out var xform))
-            return;
-
-        lerping.LastRotation = GetRotation(uid, xform);
-    }
-
-    private void OnFollowerParentChange(EntityUid uid, FollowedComponent component, ref EntParentChangedMessage args)
-    {
-        foreach (var foll in component.Following)
-        {
-            // Want to make sure eye rotation is correct if our follower changes parents.
-            if (!TryComp<LerpingEyeComponent>(foll, out var lerping))
-                continue;
-
-            // Is this actually a map change? If yes, stop any lerps
-            if (args.OldMapId != args.Transform.MapID)
-                lerping.LastRotation = GetRotation(foll, Transform(foll));
-        }
     }
 
     private void OnEyeStartup(EntityUid uid, EyeComponent component, ComponentStartup args)
