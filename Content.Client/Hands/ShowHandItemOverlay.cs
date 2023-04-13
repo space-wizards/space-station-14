f4ead5db314564375429a247da919b6d08d04e23
Artjom <artjombebenin@gmail.com>
2023-04-13 16:24:08 +0300

    setings for scale

--- Content.Client/Hands/ShowHandItemOverlay.cs
+++ Content.Client/Hands/ShowHandItemOverlay.cs
@@ -33,7 +33,7 @@ namespace Content.Client.Hands
             _handIconOffset = _cfg.GetCVar(CCVars.HudHeldItemOffset);
 
             _renderBackbuffer = _clyde.CreateRenderTarget(
-                (120, 120),
+                (100, 100),
                 new RenderTargetFormatParameters(RenderTargetColorFormat.Rgba8Srgb, true),
                 new TextureSampleParameters
                 {
@@ -66,29 +66,29 @@ namespace Content.Client.Hands
             bool isHandItemSprite = false;
             bool isHandGunItem = false;
 
-            if (handEntity != null) {
-                isHandItemSprite = _entMan.HasComponent<SpriteComponent>(handEntity);
-                isHandGunItem = _entMan.HasComponent<GunComponent>(handEntity);
-            }
+            isHandItemSprite = _entMan.HasComponent<SpriteComponent>(handEntity);
+            isHandGunItem = _entMan.HasComponent<GunComponent>(handEntity);
 
             if (isHandGunItem) {
-                Logger.Debug(" is is gun! ++++++++++++");
+                Logger.Debug(" it is gun! ++++++++++++");
             }
 
             var uiScale = (args.ViewportControl as Control)?.UIScale ?? 1f;
-            var bufferCenterOffset = _renderBackbuffer.Size / 2;
+            float limetedScale = uiScale > 1.25f ? 1.25f : uiScale;
+
+            var halfBufferSize = _renderBackbuffer.Size / 2;
 
             screen.RenderInRenderTarget(_renderBackbuffer, () =>
             {
-                DrawWeaponSight(screen, bufferCenterOffset, uiScale);
+                DrawWeaponSight(screen, halfBufferSize, limetedScale);
 
                 if (handEntity != null && isHandItemSprite) {
-                    DrawHandEntityIcon(screen, handEntity.Value, bufferCenterOffset, uiScale);
+                    DrawHandEntityIcon(screen, handEntity.Value, halfBufferSize, limetedScale);
                 }
             }, Color.Transparent);
 
             screen.DrawTexture(_renderBackbuffer.Texture,
-                mousePos - bufferCenterOffset, Color.White.WithAlpha(0.8f));
+                mousePos - halfBufferSize, Color.White.WithAlpha(0.8f));
         }
 
         private bool IsIfDrawIconOverride(DrawingHandleScreen screen, Vector2 mousePos)
@@ -108,7 +108,7 @@ namespace Content.Client.Hands
         private void DrawHandEntityIcon(DrawingHandleScreen screen, EntityUid handEntity,
             Vector2 centerPos, float scale)
         {
-            Vector2 offset = centerPos + _handIconOffset;
+            Vector2 offset = (centerPos + _handIconOffset);
             screen.DrawCircle(offset, offset.X / 4.5f * scale, Color.White.WithAlpha(0.02f));
             screen.DrawEntity(handEntity, offset, new Vector2(1f, 1f) * scale, Direction.South);
         }
