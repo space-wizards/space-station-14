01662dc168da8313835f9b8a486241ec68d4030f
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2023-04-13 18:10:44 -0700

    Add EscapeContext keybind (#15301)
    * Add EscapeContext
    
    Escape context input closes windows if there are any open. If there are not any windows it opens the game menu.
    
    * Add fluent for Escape Context
    
    * Move EngineContext keybind to content
    
    * Readd WindowCloseAll
    
    * Fix EscapeContext not opening the game menu after using WindowCloseAll
    
    WindowCloseAll does not clear the CloseRecentWindowUIController.recentlyInteractedWindows, which caused HasClosableWindow to return true because the list still had items.
    
    Changed HasClosableWindow to check if windows in the list are still open and clear them if they aren't.
    
    * Clean up EscapeContextUIController

--- Content.Client/Options/UI/Tabs/KeyRebindTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/KeyRebindTab.xaml.cs
@@ -148,6 +148,7 @@ namespace Content.Client.Options.UI.Tabs
             AddButton(EngineKeyFunctions.WindowCloseAll);
             AddButton(EngineKeyFunctions.WindowCloseRecent);
             AddButton(EngineKeyFunctions.EscapeMenu);
+            AddButton(ContentKeyFunctions.EscapeContext);
 
             AddHeader("ui-options-header-misc");
             AddButton(ContentKeyFunctions.TakeScreenshot);
