a4eead17dc854360984ad9b0008cceb9754b8dfc
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-04-16 11:59:09 +0300

    use file namespace

--- Content.Client/Pinpointer/PinpointerSystem.cs
+++ Content.Client/Pinpointer/PinpointerSystem.cs
@@ -2,77 +2,76 @@ using Content.Shared.Pinpointer;
 using Robust.Client.Graphics;
 using Robust.Shared.GameStates;
 
-namespace Content.Client.Pinpointer
+namespace Content.Client.Pinpointer;
+
+public sealed class PinpointerSystem : SharedPinpointerSystem
 {
-    public sealed class PinpointerSystem : SharedPinpointerSystem
+    [Dependency] private readonly IEyeManager _eyeManager = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly IEyeManager _eyeManager = default!;
-        [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+        base.Initialize();
+        SubscribeLocalEvent<PinpointerComponent, ComponentHandleState>(HandleCompState);
+    }
 
-        public override void Initialize()
+    public override void FrameUpdate(float frameTime)
+    {
+        base.FrameUpdate(frameTime);
+
+        // we want to show pinpointers arrow direction relative
+        // to players eye rotation (like it was in SS13)
+
+        // because eye can change it rotation anytime
+        // we need to update this arrow in a update loop
+        var query = EntityQueryEnumerator<PinpointerComponent>();
+        while (query.MoveNext(out var uid, out var pinpointer))
         {
-            base.Initialize();
-            SubscribeLocalEvent<PinpointerComponent, ComponentHandleState>(HandleCompState);
+            UpdateAppearance(uid, pinpointer);
+            UpdateEyeDir(uid, pinpointer);
         }
+    }
 
-        public override void FrameUpdate(float frameTime)
-        {
-            base.FrameUpdate(frameTime);
+    private void HandleCompState(EntityUid uid, PinpointerComponent pinpointer, ref ComponentHandleState args)
+    {
+        if (args.Current is not PinpointerComponentState state)
+            return;
 
-            // we want to show pinpointers arrow direction relative
-            // to players eye rotation (like it was in SS13)
+        pinpointer.IsActive = state.IsActive;
+        pinpointer.ArrowAngle = state.ArrowAngle;
+        pinpointer.DistanceToTarget = state.DistanceToTarget;
+    }
 
-            // because eye can change it rotation anytime
-            // we need to update this arrow in a update loop
-            var query = EntityQueryEnumerator<PinpointerComponent>();
-            while (query.MoveNext(out var uid, out var pinpointer))
-            {
-                UpdateAppearance(uid, pinpointer);
-                UpdateEyeDir(uid, pinpointer);
-            }
-        }
+    private void UpdateAppearance(EntityUid uid, PinpointerComponent? pinpointer = null,
+        AppearanceComponent? appearance = null)
+    {
+        if (!Resolve(uid, ref pinpointer, ref appearance))
+            return;
 
-        private void HandleCompState(EntityUid uid, PinpointerComponent pinpointer, ref ComponentHandleState args)
-        {
-            if (args.Current is not PinpointerComponentState state)
-                return;
+        _appearance.SetData(uid, PinpointerVisuals.IsActive, pinpointer.IsActive, appearance);
+        _appearance.SetData(uid, PinpointerVisuals.TargetDistance, pinpointer.DistanceToTarget, appearance);
+    }
 
-            pinpointer.IsActive = state.IsActive;
-            pinpointer.ArrowAngle = state.ArrowAngle;
-            pinpointer.DistanceToTarget = state.DistanceToTarget;
-        }
+    private void UpdateArrowAngle(EntityUid uid, Angle angle, PinpointerComponent? pinpointer = null,
+        AppearanceComponent? appearance = null)
+    {
+        if (!Resolve(uid, ref pinpointer, ref appearance))
+            return;
 
-        private void UpdateAppearance(EntityUid uid, PinpointerComponent? pinpointer = null,
-            AppearanceComponent? appearance = null)
-        {
-            if (!Resolve(uid, ref pinpointer, ref appearance))
-                return;
+        _appearance.SetData(uid, PinpointerVisuals.ArrowAngle, angle, appearance);
+    }
 
-            _appearance.SetData(uid, PinpointerVisuals.IsActive, pinpointer.IsActive, appearance);
-            _appearance.SetData(uid, PinpointerVisuals.TargetDistance, pinpointer.DistanceToTarget, appearance);
-        }
+    /// <summary>
+    ///     Transform pinpointer arrow from world space to eye space
+    ///     And send it to the appearance component
+    /// </summary>
+    private void UpdateEyeDir(EntityUid uid, PinpointerComponent? pinpointer = null)
+    {
+        if (!Resolve(uid, ref pinpointer) || !pinpointer.HasTarget)
+            return;
 
-        private void UpdateArrowAngle(EntityUid uid, Angle angle, PinpointerComponent? pinpointer = null,
-            AppearanceComponent? appearance = null)
-        {
-            if (!Resolve(uid, ref pinpointer, ref appearance))
-                return;
-
-            _appearance.SetData(uid, PinpointerVisuals.ArrowAngle, angle, appearance);
-        }
-
-        /// <summary>
-        ///     Transform pinpointer arrow from world space to eye space
-        ///     And send it to the appearance component
-        /// </summary>
-        private void UpdateEyeDir(EntityUid uid, PinpointerComponent? pinpointer = null)
-        {
-            if (!Resolve(uid, ref pinpointer) || !pinpointer.HasTarget)
-                return;
-
-            var eye = _eyeManager.CurrentEye;
-            var angle = pinpointer.ArrowAngle + eye.Rotation;
-            UpdateArrowAngle(uid, angle, pinpointer);
-        }
+        var eye = _eyeManager.CurrentEye;
+        var angle = pinpointer.ArrowAngle + eye.Rotation;
+        UpdateArrowAngle(uid, angle, pinpointer);
     }
 }
