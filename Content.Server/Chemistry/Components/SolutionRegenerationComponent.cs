faeee1c8a79a7f243c7ac6f99a429c822d5fc709
deltanedas <@deltanedas:kde.org>
2023-04-17 08:07:45 +0100

    solution regeneration system

---
+++ Content.Server/Chemistry/Components/SolutionRegenerationComponent.cs
@@ -0,0 +1,45 @@
+using Content.Server.Chemistry.EntitySystems;
+using Content.Shared.Chemistry.Reagent;
+using Content.Shared.FixedPoint;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server.Chemistry.Components;
+
+/// <summary>
+/// Passively increases a solution's quantity of a reagent.
+/// </summary>
+[RegisterComponent]
+[Access(typeof(SolutionRegenerationSystem))]
+public sealed class SolutionRegenerationComponent : Component
+{
+    /// <summary>
+    /// The solution to have reagent added to.
+    /// </summary>
+    [DataField("solution", required: true), ViewVariables(VVAccess.ReadWrite)]
+    public string Solution = string.Empty;
+
+    /// <summary>
+    /// The reagent to be regenerated in the solution.
+    /// </summary>
+    [DataField("reagent", required: true, customTypeSerializer: typeof(PrototypeIdSerializer<ReagentPrototype>)), ViewVariables(VVAccess.ReadWrite)]
+    public string Reagent = string.Empty;
+
+    /// <summary>
+    /// Quantity of the reagent regenerated every time.
+    /// </summary>
+    [DataField("quantity"), ViewVariables(VVAccess.ReadWrite)]
+    public FixedPoint2 Quantity = FixedPoint2.New(1f);
+
+    /// <summary>
+    /// How long it takes to regenerate once.
+    /// </summary>
+    [DataField("duration"), ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan Duration = TimeSpan.FromSeconds(1);
+
+    /// <summary>
+    /// The time when the next regeneration will occur.
+    /// </summary>
+    [DataField("nextChargeTime", customTypeSerializer: typeof(TimeOffsetSerializer)), ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan NextRegenTime = TimeSpan.MaxValue;
+}
