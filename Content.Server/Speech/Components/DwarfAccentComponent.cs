6da1dff47082dd91fa9fb71042054eb6b8518aca
Konstantin <k.r.ilyin@gmail.com>
2023-04-17 13:51:38 +0000

    fix: Fixed error with build, updated RobustToolbox submodule

--- Content.Server/Speech/Components/DwarfAccentComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Content.Server.Speech.Components;
-
-/// <summary>
-/// Used for replacing words for dwarves (pseudo-scottish)
-/// </summary>
-[RegisterComponent]
-public sealed class DwarfAccentComponent : Component
-{
-}
