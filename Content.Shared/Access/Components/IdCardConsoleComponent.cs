c3e31bfa151966b81161b46575deb4d7b0a87e65
MagnusCrowe <whiterider1988@yahoo.com>
2023-04-17 19:41:29 -0500

    Removed paramedic access and handheld crew monitor

--- Content.Shared/Access/Components/IdCardConsoleComponent.cs
+++ Content.Shared/Access/Components/IdCardConsoleComponent.cs
@@ -65,7 +65,6 @@ public sealed class IdCardConsoleComponent : Component
         "Kitchen",
         "Maintenance",
         "Medical",
-        "Paramedic",
         "Quartermaster",
         "Research",
         "ResearchDirector",
