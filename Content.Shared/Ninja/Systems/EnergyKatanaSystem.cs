e916a37230400f29f2a23ecfa590a8af419a3e8e
deltanedas <@deltanedas:kde.org>
2023-04-19 14:52:08 +0100

    use charges system for katana, cleanup

--- Content.Shared/Ninja/Systems/EnergyKatanaSystem.cs
+++ Content.Shared/Ninja/Systems/EnergyKatanaSystem.cs
@@ -1,3 +1,5 @@
+using Content.Shared.Charges.Components;
+using Content.Shared.Charges.Systems;
 using Content.Shared.Examine;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Interaction;
@@ -7,19 +9,21 @@ using Content.Shared.Physics;
 using Content.Shared.Popups;
 using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Network;
 using Robust.Shared.Timing;
 
 namespace Content.Shared.Ninja.Systems;
 
 /// <summary>
-/// System for katana dashing, recharging and what not.
+/// System for katana dashing and binding
 /// </summary>
-// TODO: move all recharging stuff into its own system and use for emag too
 public sealed class EnergyKatanaSystem : EntitySystem
 {
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedChargesSystem _charges = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
     [Dependency] private readonly SharedInteractionSystem _interaction = default!;
+    [Dependency] private readonly INetManager _net = default!;
     [Dependency] private readonly SharedNinjaSystem _ninja = default!;
     [Dependency] private readonly SharedPopupSystem _popups = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
@@ -30,9 +34,7 @@ public sealed class EnergyKatanaSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<EnergyKatanaComponent, GotEquippedEvent>(OnEquipped);
-        SubscribeLocalEvent<EnergyKatanaComponent, ExaminedEvent>(OnExamine);
         SubscribeLocalEvent<NinjaSuitComponent, KatanaDashEvent>(OnDash);
-        SubscribeLocalEvent<EnergyKatanaComponent, EntityUnpausedEvent>(OnUnpaused);
     }
 
     private void OnEquipped(EntityUid uid, EnergyKatanaComponent comp, GotEquippedEvent args)
@@ -51,45 +53,11 @@ public sealed class EnergyKatanaSystem : EntitySystem
         _ninja.BindKatana(ninja, uid);
     }
 
-    private void OnUnpaused(EntityUid uid, EnergyKatanaComponent component, ref EntityUnpausedEvent args)
-    {
-        component.NextChargeTime += args.PausedTime;
-    }
-
-    private void OnExamine(EntityUid uid, EnergyKatanaComponent component, ExaminedEvent args)
-    {
-        args.PushMarkup(Loc.GetString("emag-charges-remaining", ("charges", component.Charges)));
-        if (component.Charges == component.MaxCharges)
-        {
-            args.PushMarkup(Loc.GetString("emag-max-charges"));
-            return;
-        }
-        var timeRemaining = Math.Round((component.NextChargeTime - _timing.CurTime).TotalSeconds);
-        args.PushMarkup(Loc.GetString("emag-recharging", ("seconds", timeRemaining)));
-    }
-
-    // TODO: remove and use LimitedCharges+AutoRecharge
-    public override void Update(float frameTime)
-    {
-        base.Update(frameTime);
-
-        foreach (var comp in EntityQuery<EnergyKatanaComponent>())
-        {
-            if (!comp.AutoRecharge)
-                continue;
-
-            if (comp.Charges == comp.MaxCharges)
-                continue;
-
-            if (_timing.CurTime < comp.NextChargeTime)
-                continue;
-
-            ChangeCharge(comp.Owner, 1, true, comp);
-        }
-    }
-
     public void OnDash(EntityUid suit, NinjaSuitComponent comp, KatanaDashEvent args)
     {
+        if (!_timing.IsFirstTimePredicted)
+            return;
+
         var user = args.Performer;
         args.Handled = true;
         if (!TryComp<NinjaComponent>(user, out var ninja) || ninja.Katana == null)
@@ -98,50 +66,37 @@ public sealed class EnergyKatanaSystem : EntitySystem
         var uid = ninja.Katana.Value;
         if (!TryComp<EnergyKatanaComponent>(uid, out var katana) || !_hands.IsHolding(user, uid, out var _))
         {
-            _popups.PopupEntity(Loc.GetString("ninja-katana-not-held"), user, user);
+            ClientPopup("ninja-katana-not-held", user);
             return;
         }
 
-        if (katana.Charges <= 0)
+        TryComp<LimitedChargesComponent>(uid, out var charges);
+        if (_charges.IsEmpty(uid, charges))
         {
-            _popups.PopupEntity(Loc.GetString("emag-no-charges"), user, user);
+            ClientPopup("ninja-katana-no-charges", user);
             return;
         }
 
-        // TODO: check that target is not dense
         var origin = Transform(user).MapPosition;
         var target = args.Target.ToMap(EntityManager, _transform);
         // prevent collision with the user duh
         if (!_interaction.InRangeUnobstructed(origin, target, 0f, CollisionGroup.Opaque, uid => uid == user))
         {
             // can only dash if the destination is visible on screen
-            _popups.PopupEntity(Loc.GetString("ninja-katana-cant-see"), user, user);
+            ClientPopup("ninja-katana-cant-see", user);
             return;
         }
 
         _transform.SetCoordinates(user, args.Target);
         _transform.AttachToGridOrMap(user);
-        _audio.PlayPvs(katana.BlinkSound, user, AudioParams.Default.WithVolume(katana.BlinkVolume));
-        // TODO: show the funny green man thing
-        ChangeCharge(uid, -1, false, katana);
+        _audio.PlayPredicted(katana.BlinkSound, user, user, AudioParams.Default.WithVolume(katana.BlinkVolume));
+        if (charges != null)
+            _charges.UseCharge(uid, charges);
     }
 
-    /// <summary>
-    /// Changes the charge on an energy katana.
-    /// </summary>
-    public bool ChangeCharge(EntityUid uid, int change, bool resetTimer, EnergyKatanaComponent? katana = null)
+    private void ClientPopup(string msg, EntityUid user)
     {
-        if (!Resolve(uid, ref katana))
-            return false;
-
-        if (katana.Charges + change < 0 || katana.Charges + change > katana.MaxCharges)
-            return false;
-
-        if (resetTimer || katana.Charges == katana.MaxCharges)
-            katana.NextChargeTime = _timing.CurTime + katana.RechargeDuration;
-
-        katana.Charges += change;
-        Dirty(katana);
-        return true;
+        if (_net.IsClient)
+            _popups.PopupEntity(Loc.GetString(msg), user);
     }
 }
--- Resources/Locale/en-US/ninja/katana.ftl
+++ Resources/Locale/en-US/ninja/katana.ftl
@@ -1,4 +1,5 @@
 ninja-katana-recalled = Your Energy Katana teleports into your hand!
 ninja-katana-not-held = You aren't holding your katana!
+ninja-katana-no-charges = No charges left!
 ninja-katana-cant-see = You can't see that!
 ninja-hands-full = Your hands are full!
