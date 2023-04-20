65b2545db2fac9f89deee6be01aef7ece63be830
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-04-21 01:02:11 +1000

    Fix server crash for name identifiers

--- Content.Server/NameIdentifier/NameIdentifierComponent.cs
+++ Content.Server/NameIdentifier/NameIdentifierComponent.cs
@@ -8,4 +8,10 @@ public sealed class NameIdentifierComponent : Component
 {
     [DataField("group", required: true, customTypeSerializer:typeof(PrototypeIdSerializer<NameIdentifierGroupPrototype>))]
     public string Group = string.Empty;
+
+    /// <summary>
+    /// The randomly generated ID for this entity.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("identifier")]
+    public int Identifier = -1;
 }
