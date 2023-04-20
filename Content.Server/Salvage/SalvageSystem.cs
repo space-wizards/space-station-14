d956b5fdac91edad50f7edad28e1feb875339b37
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-04-20 22:58:15 +1000

    oop

--- Content.Server/Salvage/SalvageSystem.cs
+++ Content.Server/Salvage/SalvageSystem.cs
@@ -41,6 +41,7 @@ namespace Content.Server.Salvage
         [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
         [Dependency] private readonly RadioSystem _radioSystem = default!;
         [Dependency] private readonly SharedAppearanceSystem _appearanceSystem = default!;
+        [Dependency] private readonly SharedAudioSystem _audio = default!;
         [Dependency] private readonly SharedTransformSystem _transform = default!;
         [Dependency] private readonly ShuttleSystem _shuttle = default!;
         [Dependency] private readonly ShuttleConsoleSystem _shuttleConsoles = default!;
