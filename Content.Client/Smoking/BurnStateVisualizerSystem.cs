422cd90f3b88a6a20cb21ecbe0f3da8f4b727f60
Sean Bikkes <bixkitts@gmail.com>
2023-04-22 23:53:02 +0200

    Implemented requested changes

--- Content.Client/Smoking/BurnStateVisualizerSystem.cs
+++ Content.Client/Smoking/BurnStateVisualizerSystem.cs
@@ -5,38 +5,19 @@ namespace Content.Client.Smoking;
 
 public sealed class BurnStateVisualizerSystem : VisualizerSystem<BurnStateVisualsComponent>
 {
-
-    [Dependency] private readonly IEntityManager _entMan = default!;
-    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
-
-    public override void Initialize()
-    {
-        base.Initialize();
-        SubscribeLocalEvent<BurnStateVisualsComponent, ComponentInit>(OnComponentInit);
-    }
-
-    private void OnComponentInit(EntityUid uid, BurnStateVisualsComponent component, ComponentInit args)
-    {
-        IoCManager.InjectDependencies(this);
-    }
-
     protected override void OnAppearanceChange(EntityUid uid, BurnStateVisualsComponent component, ref AppearanceChangeEvent args)
     {
-
-        if (!_entMan.TryGetComponent(uid, out SpriteComponent? sprite))
-            return;
-
-        if (!_appearance.TryGetData<SmokableState>(uid, SmokingVisuals.Smoking, out var burnState))
+        if(!args.AppearanceData.TryGetValue(SmokingVisuals.Smoking, out var burnState))
             return;
 
         var state = burnState switch
         {
             SmokableState.Lit => component._litIcon,
-                SmokableState.Burnt => component._burntIcon,
-                _ => component._unlitIcon
+            SmokableState.Burnt => component._burntIcon,
+            _ => component._unlitIcon
         };
 
-        sprite?.LayerSetState(0, state);
+        args.Sprite?.LayerSetState(0, state);
     }
 }
 
--- Content.Client/Smoking/BurnStateVisualsComponent.cs
+++ Content.Client/Smoking/BurnStateVisualsComponent.cs
@@ -1,13 +1,13 @@
 namespace Content.Client.Smoking;
 
-[RegisterComponent]
-public sealed class BurnStateVisualsComponent : Component
-{
-    [DataField("burntIcon")]
-    public string _burntIcon = "burnt-icon";
-    [DataField("litIcon")]
-    public string _litIcon = "lit-icon";
-    [DataField("unlitIcon")]
-    public string _unlitIcon = "icon";
-}
+    [RegisterComponent]
+    public sealed class BurnStateVisualsComponent : Component
+    {
+        [DataField("burntIcon")]
+        public string _burntIcon = "burnt-icon";
+        [DataField("litIcon")]
+        public string _litIcon = "lit-icon";
+        [DataField("unlitIcon")]
+        public string _unlitIcon = "icon";
+    }
 
