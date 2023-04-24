05b32c59f4b649d3cfd8c6cab5a72042e99f6b0e
ElectroJr <leonsfriedrich@gmail.com>
2023-04-24 14:54:32 -0400

    Fix action icons not showing up

--- Content.Client/Actions/ActionsSystem.cs
+++ Content.Client/Actions/ActionsSystem.cs
@@ -50,6 +50,15 @@ namespace Content.Client.Actions
             SubscribeLocalEvent<ActionsComponent, ComponentHandleState>(HandleComponentState);
         }
 
+        public override void Dirty(ActionType action)
+        {
+            if (_playerManager.LocalPlayer?.ControlledEntity != action.AttachedEntity)
+                return;
+
+            base.Dirty(action);
+            ActionsUpdated?.Invoke();
+        }
+
         private void HandleComponentState(EntityUid uid, ActionsComponent component, ref ComponentHandleState args)
         {
             if (args.Current is not ActionsComponentState state)
@@ -119,31 +128,36 @@ namespace Content.Client.Actions
 
         public override void AddAction(EntityUid uid, ActionType action, EntityUid? provider, ActionsComponent? comp = null, bool dirty = true)
         {
+            if (uid != _playerManager.LocalPlayer?.ControlledEntity)
+                return;
+
+            if (GameTiming.ApplyingState && !action.ClientExclusive)
+                return;
+
             if (!Resolve(uid, ref comp, false))
                 return;
 
+            dirty &= !action.ClientExclusive;
             base.AddAction(uid, action, provider, comp, dirty);
-
-            if (uid == _playerManager.LocalPlayer?.ControlledEntity)
-                ActionAdded?.Invoke(action);
+            ActionAdded?.Invoke(action);
         }
 
-        public override void RemoveActions(EntityUid uid, IEnumerable<ActionType> actions, ActionsComponent? comp = null, bool dirty = true)
+        public override void RemoveAction(EntityUid uid, ActionType action, ActionsComponent? comp = null, bool dirty = true)
         {
             if (uid != _playerManager.LocalPlayer?.ControlledEntity)
                 return;
 
+            if (GameTiming.ApplyingState && !action.ClientExclusive)
+                return;
+
             if (!Resolve(uid, ref comp, false))
                 return;
 
-            var actionList = actions.ToList();
-            base.RemoveActions(uid, actionList, comp, dirty);
+            dirty &= !action.ClientExclusive;
+            base.RemoveAction(uid, action, comp, dirty);
 
-            foreach (var act in actionList)
-            {
-                if (act.AutoRemove)
-                    ActionRemoved?.Invoke(act);
-            }
+            if (action.AutoRemove)
+                ActionRemoved?.Invoke(action);
         }
 
         /// <summary>
