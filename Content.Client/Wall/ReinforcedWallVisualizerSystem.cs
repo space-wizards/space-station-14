26e518fdf4b6280614a1ef831204a9be9fa08ffe
Sean Bikkes <bixkitts@gmail.com>
2023-04-24 12:08:18 +0200

    Refactored Reinforced Walls Visuals to ECS

---
+++ Content.Client/Wall/ReinforcedWallVisualizerSystem.cs
@@ -0,0 +1,32 @@
+using Content.Shared.Wall;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Wall
+{
+    public sealed class ReinforcedWallVisualizerSystem : VisualizerSystem<ReinforcedWallVisualsComponent>
+    {
+        protected override void OnAppearanceChange(EntityUid uid, ReinforcedWallVisualsComponent component, ref AppearanceChangeEvent args)
+        {
+            if(args.Sprite == null)
+                return;
+            if (!args.AppearanceData.TryGetValue(ReinforcedWallVisuals.DeconstructionStage, out var deconStage))
+                return;
+            int stage = (int)deconStage;
+            SetDeconstructionStage(stage, args.Sprite);
+        }
+
+        private void SetDeconstructionStage(int stage, SpriteComponent sprite)
+        {
+            object index = sprite.LayerMapReserveBlank(ReinforcedWallVisualLayers.Deconstruction);
+
+            if (stage < 0)
+            {
+                sprite.LayerSetVisible(index, false);
+                return;
+            }
+
+            sprite.LayerSetVisible(index, true);
+            sprite.LayerSetState(index, $"reinf_construct-{stage}");
+        }
+    }
+}
---
+++ Content.Client/Wall/ReinforcedWallVisualsComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Client.Wall;
+
+public sealed class ReinforcedWallVisualsComponent : Component
+{
+    public enum ReinforcedWallVisualLayers : byte
+    {
+        Deconstruction,
+    }
+}
