a2f29bac7de7295f4b26bb72d454a1b2b7850411
brainfood1183 <newman1183@googlemail.com>
2023-04-26 03:14:15 +0100

    changes

--- Content.Server/Cluwne/CluwneSystem.cs
+++ Content.Server/Cluwne/CluwneSystem.cs
@@ -14,6 +14,18 @@ using Content.Server.Emoting.Systems;
 using Content.Server.Speech.EntitySystems;
 using Content.Shared.Cluwne;
 using Content.Shared.Interaction.Components;
+using Content.Shared.Stealth.Components;
+using Content.Server.Abilities.Mime;
+using Content.Shared.Humanoid;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.Weapons.Melee.Events;
+using Content.Shared.Zombies;
+using Content.Server.Mind.Components;
+using Content.Server.GameTicking.Rules.Configurations;
+using Content.Server.GameTicking.Rules;
+using Robust.Shared.Audio;
+using Robust.Shared.Player;
+using Content.Server.Chat.Managers;
 
 namespace Content.Server.Cluwne;
 
@@ -27,6 +39,8 @@ public sealed class CluwneSystem : EntitySystem
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly ChatSystem _chat = default!;
     [Dependency] private readonly AutoEmoteSystem _autoEmote = default!;
+    [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
+    [Dependency] private readonly IChatManager _chatMan = default!;
 
     public override void Initialize()
     {
@@ -34,6 +48,8 @@ public sealed class CluwneSystem : EntitySystem
 
         SubscribeLocalEvent<CluwneComponent, ComponentStartup>(OnComponentStartup);
         SubscribeLocalEvent<CluwneComponent, MobStateChangedEvent>(OnMobState);
+        SubscribeLocalEvent<CluwneComponent, MeleeHitEvent>(OnMeleeHit);
+        SubscribeLocalEvent<CluwneComponent, MindAddedMessage>(OnCluwneBeastMindAdded);
         SubscribeLocalEvent<CluwneComponent, EmoteEvent>(OnEmote, before:
         new[] { typeof(VocalSystem), typeof(BodyEmotesSystem) });
     }
@@ -43,14 +59,32 @@ public sealed class CluwneSystem : EntitySystem
     /// </summary>
     private void OnMobState(EntityUid uid, CluwneComponent component, MobStateChangedEvent args)
     {
-        if (args.NewMobState == MobState.Dead)
+
+        if (args.NewMobState == MobState.Alive && component.IsBeast == true)
+        {
+            EnsureComp<StealthOnMoveComponent>(uid);
+        }
+
+        if (args.NewMobState == MobState.Critical && component.IsBeast == true)
+        {
+            RemComp<StealthOnMoveComponent>(uid);
+        }
+
+        if (args.NewMobState == MobState.Dead && component.IsBeast == true)
+        {
+            RemComp<StealthOnMoveComponent>(uid);
+            RemComp<AutoEmoteComponent>(uid);
+        }
+
+
+        if (args.NewMobState == MobState.Dead && component.IsCluwne)
 		{
             RemComp<CluwneComponent>(uid);
             RemComp<ClumsyComponent>(uid);
             RemComp<AutoEmoteComponent>(uid);
             var damageSpec = new DamageSpecifier(_prototypeManager.Index<DamageGroupPrototype>("Genetic"), 300);
             _damageableSystem.TryChangeDamage(uid, damageSpec);
-		}
+        }
     }
 
     public EmoteSoundsPrototype? EmoteSounds;
@@ -68,15 +102,16 @@ public sealed class CluwneSystem : EntitySystem
         var name = meta.EntityName;
 
         EnsureComp<AutoEmoteComponent>(uid);
-        _autoEmote.AddEmote(uid, "CluwneGiggle");
+        _autoEmote.AddEmote(uid, component.AutoEmoteSound);
         EnsureComp<ClumsyComponent>(uid);
 
-        _popupSystem.PopupEntity(Loc.GetString("cluwne-transform", ("target", uid)), uid, PopupType.LargeCaution);
-        _audio.PlayPvs(component.SpawnSound, uid);
-
-        meta.EntityName = Loc.GetString("cluwne-name-prefix", ("target", name));
-
-        SetOutfitCommand.SetOutfit(uid, "CluwneGear", EntityManager);
+        if (component.IsCluwne)
+        {
+            _popupSystem.PopupEntity(Loc.GetString("cluwne-transform", ("target", uid)), uid, PopupType.LargeCaution);
+            _audio.PlayPvs(component.SpawnSound, uid);
+            meta.EntityName = Loc.GetString("cluwne-name-prefix", ("target", name));
+            SetOutfitCommand.SetOutfit(uid, "CluwneGear", EntityManager);
+        }
     }
 
     /// <summary>
@@ -99,6 +134,51 @@ public sealed class CluwneSystem : EntitySystem
             _audio.PlayPvs(component.KnockSound, uid);
             _stunSystem.TryParalyze(uid, TimeSpan.FromSeconds(component.ParalyzeTime), true);
             _chat.TrySendInGameICMessage(uid, "spasms", InGameICChatType.Emote, false, false);
+        }
+    }
+
+    public CluwneBeastRuleConfiguration RuleConfig()
+    {
+            return (CluwneBeastRuleConfiguration) _prototypeManager.Index<GameRulePrototype>("CluwneBeastSpawn").Configuration;
+    }
+
+    private void OnCluwneBeastMindAdded(EntityUid uid, CluwneComponent comp, MindAddedMessage args)
+    {
+        if (comp.IsBeast == true)
+        {
+            if (TryComp<MindComponent>(uid, out var mind) && mind.Mind != null)
+                HelloBeast(mind.Mind);
+        }
+    }
+
+    private void HelloBeast(Mind.Mind mind)
+    {
+        if (!mind.TryGetSession(out var session))
+            return;
+
+        var config = RuleConfig();
+        _audio.PlayGlobal(config.GreetingSound, Filter.Empty().AddPlayer(session), false, AudioParams.Default);
+        _chatMan.DispatchServerMessage(session, Loc.GetString("cluwne-beast-greeting"));
+    }
+
+    private void OnMeleeHit(EntityUid uid, CluwneComponent component, MeleeHitEvent args)
+    {
+        if (component.CluwneOnMelee == true)
+        {
+
+            foreach (var entity in args.HitEntities)
+            {
+                if (HasComp<HumanoidAppearanceComponent>(entity)
+                    && !_mobStateSystem.IsDead(entity)
+                    && _robustRandom.Prob(component.Cluwinification)
+                    && !HasComp<ClumsyComponent>(entity)
+                    && !HasComp<ZombieComponent>(entity)
+                    && !HasComp<MimePowersComponent>(entity))
+                {
+                    _audio.PlayPvs(component.CluwneSound, uid);
+                    EnsureComp<CluwneComponent>(entity);
+                }
+            }
         }
     }
 }
