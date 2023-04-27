96049bcbb32c98bff6296fd40e3b3c0d77318e30
Sean Bikkes <bixkitts@gmail.com>
2023-04-27 10:40:41 +0200

    Implemented code style and attributes

--- Content.Client/Wall/ReinforcedWallVisualizerSystem.cs
+++ Content.Client/Wall/ReinforcedWallVisualizerSystem.cs
@@ -1,32 +1,31 @@
 using Content.Shared.Wall;
 using Robust.Client.GameObjects;
 
-namespace Content.Client.Wall
+namespace Content.Client.Wall;
+
+public sealed class ReinforcedWallVisualizerSystem : VisualizerSystem<ReinforcedWallVisualsComponent>
 {
-    public sealed class ReinforcedWallVisualizerSystem : VisualizerSystem<ReinforcedWallVisualsComponent>
+    protected override void OnAppearanceChange(EntityUid uid, ReinforcedWallVisualsComponent component, ref AppearanceChangeEvent args)
     {
-        protected override void OnAppearanceChange(EntityUid uid, ReinforcedWallVisualsComponent component, ref AppearanceChangeEvent args)
+        if(args.Sprite == null)
+            return;
+        if (!args.AppearanceData.TryGetValue(ReinforcedWallVisuals.DeconstructionStage, out var deconStage))
+            return;
+        var stage = (int)deconStage;
+        SetDeconstructionStage(stage, args.Sprite);
+    }
+
+    private void SetDeconstructionStage(int stage, SpriteComponent sprite)
+    {
+        var index = sprite.LayerMapReserveBlank(ReinforcedWallVisualsComponent.ReinforcedWallVisualLayers.Deconstruction);
+
+        if (stage < 0)
         {
-            if(args.Sprite == null)
-                return;
-            if (!args.AppearanceData.TryGetValue(ReinforcedWallVisuals.DeconstructionStage, out var deconStage))
-                return;
-            int stage = (int)deconStage;
-            SetDeconstructionStage(stage, args.Sprite);
+            sprite.LayerSetVisible(index, false);
+            return;
         }
 
-        private void SetDeconstructionStage(int stage, SpriteComponent sprite)
-        {
-            object index = sprite.LayerMapReserveBlank(ReinforcedWallVisualsComponent.ReinforcedWallVisualLayers.Deconstruction);
-
-            if (stage < 0)
-            {
-                sprite.LayerSetVisible(index, false);
-                return;
-            }
-
-            sprite.LayerSetVisible(index, true);
-            sprite.LayerSetState(index, $"reinf_construct-{stage}");
-        }
+        sprite.LayerSetVisible(index, true);
+        sprite.LayerSetState(index, $"reinf_construct-{stage}");
     }
 }
--- Content.Client/Wall/ReinforcedWallVisualsComponent.cs
+++ Content.Client/Wall/ReinforcedWallVisualsComponent.cs
@@ -1,5 +1,6 @@
 namespace Content.Client.Wall;
 
+[RegisterComponent]
 public sealed class ReinforcedWallVisualsComponent : Component
 {
     public enum ReinforcedWallVisualLayers : byte
