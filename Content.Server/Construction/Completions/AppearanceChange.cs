151ab9f6c40db9bc103c69a77515cbafdb99f9db
ElectroJr <leonsfriedrich@gmail.com>
2023-04-29 15:28:58 -0400

    fix construction

--- Content.Server/Construction/Completions/AppearanceChange.cs
+++ Content.Server/Construction/Completions/AppearanceChange.cs
@@ -3,41 +3,40 @@ using Content.Shared.Construction;
 using JetBrains.Annotations;
 using Robust.Server.GameObjects;
 
-namespace Content.Server.Construction.Completions
+namespace Content.Server.Construction.Completions;
+
+[UsedImplicitly]
+[DataDefinition]
+public sealed class AppearanceChange : IGraphAction
 {
-    [UsedImplicitly]
-    [DataDefinition]
-    public sealed class AppearanceChange : IGraphAction
+    /// <summary>
+    /// The appearance key to use.
+    /// </summary>
+    [DataField("key")]
+    public Enum Key = ConstructionVisuals.Key;
+
+    /// <summary>
+    /// The enum data to set. If not specified, will set the data to the name of the current edges' target node
+    /// (or the current node). This is because appearance changes are usually associated with reaching a new node.
+    /// </summary>
+    [DataField("data")]
+    public Enum? Data;
+
+    public void PerformAction(EntityUid uid, EntityUid? userUid, IEntityManager entityManager)
     {
-        /// <summary>
-        /// The appearance key to use.
-        /// </summary>
-        [DataField("key")]
-        public Enum Key = ConstructionVisuals.Key;
+        if (!entityManager.TryGetComponent(uid, out AppearanceComponent? appearance))
+            return;
 
-        /// <summary>
-        /// The enum data to set. If not specified, will set the data to the name of the current edges' target node
-        /// (or the current node). This is because appearance changes are usually associated with reaching a new node.
-        /// </summary>
-        [DataField("data")]
-        public Enum? Data;
-
-        public void PerformAction(EntityUid uid, EntityUid? userUid, IEntityManager entityManager)
+        if (Data != null)
         {
-            if (!entityManager.TryGetComponent(uid, out AppearanceComponent? appearance))
-                return;
-
-            if (Data != null)
-            {
-                entityManager.System<AppearanceSystem>().SetData(uid, Key, Data, appearance);
-                return;
-            }
-
-            var (node, edge) = entityManager.System<ConstructionSystem>().GetCurrentNodeAndEdge(uid);
-            var nodeName = edge?.Target ?? node?.Name;
-
-            if (nodeName != null)
-                entityManager.System<AppearanceSystem>().SetData(uid, Key, nodeName, appearance);
+            entityManager.System<AppearanceSystem>().SetData(uid, Key, Data, appearance);
+            return;
         }
+
+        var (node, edge) = entityManager.System<ConstructionSystem>().GetCurrentNodeAndEdge(uid);
+        var nodeName = edge?.Target ?? node?.Name;
+
+        if (nodeName != null)
+            entityManager.System<AppearanceSystem>().SetData(uid, Key, nodeName, appearance);
     }
 }
