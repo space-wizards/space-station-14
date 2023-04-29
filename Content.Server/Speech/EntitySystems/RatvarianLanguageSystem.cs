6454327db2c8fa42fb0cd5774e084428d8446525
keronshb <keronshb@live.com>
2023-04-28 20:36:23 -0400

    Translation to rot13

--- Content.Server/Speech/EntitySystems/RatvarianLanguageSystem.cs
+++ Content.Server/Speech/EntitySystems/RatvarianLanguageSystem.cs
@@ -1,8 +1,10 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using Content.Shared.Speech.Components;
 using Content.Shared.Speech.EntitySystems;
 using Content.Shared.StatusEffect;
+using Microsoft.Extensions.Primitives;
 
 namespace Content.Server.Speech.EntitySystems;
 
@@ -27,24 +29,21 @@ public sealed class RatvarianLanguageSystem : SharedRatvarianLanguageSystem
 
     //TODO: Make a class of or put the Regex options into the component
     private const string RatvarianKey = "RatvarianLanguage";
-    //@"(?!ratvar\b)\b[a-zA-Z]+"
-    private static Regex Ratvarian = new Regex(@"(?!ratvar\b)\b[a-zA-Z]+",
-        RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
     private static Regex THPattern = new Regex(@"th\w\B", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-    private static Regex TEPattern = new Regex(@"\Bet", RegexOptions.Compiled);
-    private static Regex ETPattern = new Regex(@"te\B",RegexOptions.Compiled);
+    private static Regex ETPattern = new Regex(@"\Bet", RegexOptions.Compiled);
+    private static Regex TEPattern = new Regex(@"te\B",RegexOptions.Compiled);
     private static Regex OFPattern = new Regex(@"(\s)(of)");
     private static Regex TIPattern = new Regex(@"ti\B", RegexOptions.Compiled | RegexOptions.IgnoreCase);
     private static Regex GUAPattern = new Regex(@"(gu)(a)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
     private static Regex ANDPattern = new Regex(@"\b(\s)(and)(\s)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
     private static Regex TOMYPattern = new Regex(@"(to|my)\s", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-    private readonly HashSet<Regex> RatvarianTranslationRules = new() {THPattern, TEPattern, ETPattern, OFPattern, TIPattern, GUAPattern, ANDPattern, TOMYPattern};
+    private static Regex ProperNouns = new Regex(@"(ratvar)|(nezbere)|(sevtuq)|(nzcrentr)|(inath-neq)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
     // see if you can run the replacements in the regex
     // regex [th] th\w\B IgnoreCase, ($&` Replace) OR (th\w)(\w) ($1`$2)
-    // regex [te] \Bet -$&
-    // regex [et] te\B  $&-
+    // regex [et] \Bet -$&
+    // regex [te] te\B  $&-
     // regex [of] (\s)(of) -$2 Replace
     // regex [ti] ti\B Ignore Case ($&` Replace)
     // regex [gua] (gu)(a) ignore case $1-$2
@@ -86,96 +85,60 @@ public sealed class RatvarianLanguageSystem : SharedRatvarianLanguageSystem
 
     private string Translate(string message)
     {
-        //TODO: split by space
-        var finalMessage = message;
-        //var finalMessage = new StringBuilder();
+        var ruleTranslation = message;
+        var finalMessage = new StringBuilder();
+        var newWord = new StringBuilder();
 
-        finalMessage = THPattern.Replace(finalMessage, "$&`");
-        finalMessage = TEPattern.Replace(finalMessage, "-$&");
-        finalMessage = ETPattern.Replace(finalMessage, "$&-");
-        finalMessage = OFPattern.Replace(finalMessage, "-$2");
-        finalMessage = TIPattern.Replace(finalMessage, "$&`");
-        finalMessage = GUAPattern.Replace(finalMessage, "$1-$2");
-        finalMessage = ANDPattern.Replace(finalMessage, "-$2-");
-        finalMessage = TOMYPattern.Replace(finalMessage, "$1-");
+        ruleTranslation = THPattern.Replace(ruleTranslation, "$&`");
+        ruleTranslation = TEPattern.Replace(ruleTranslation, "$&-");
+        ruleTranslation = ETPattern.Replace(ruleTranslation, "-$&");
+        ruleTranslation = OFPattern.Replace(ruleTranslation, "-$2");
+        ruleTranslation = TIPattern.Replace(ruleTranslation, "$&`");
+        ruleTranslation = GUAPattern.Replace(ruleTranslation, "$1-$2");
+        ruleTranslation = ANDPattern.Replace(ruleTranslation, "-$2-");
+        ruleTranslation = TOMYPattern.Replace(ruleTranslation, "$1-");
 
-        return finalMessage;
+        var temp = ruleTranslation.Split(' ');
 
-        //return THPattern.Replace(message, "$&`");
-
-        //THPattern.Replace(message, "$&`");
-
-        //var nonExcludedWords = Ratvarian.Matches(originalMessage);
-
-        //TODO: iterate through each word then iterate through each character
-        //  Needed because of different hyphening and rules
-        /*for (int i = 0; i < message.Length; i++)
+        foreach (var word in temp)
         {
-            //TODO: This didn't work
-            if (Ratvarian.IsMatch(originalMessage))
+            newWord.Clear();
+
+            if (ProperNouns.IsMatch(word))
+                newWord.Append(word);
+
+            else
             {
-                var letter = originalMessage[i];
-
-                //TODO: Replace with something other than the magic number
-                // Letters are much more readable than numbers
-                // Easier to RNG predict too? Still good to know what each char is number wise
-                if (letter >= 97 && letter <= 122)
+                for (int i = 0; i < word.Length; i++)
                 {
-                    var letterRot = letter + 13;
+                    var letter = word[i];
 
-                    if (letterRot > 122)
-                        letterRot -= 26;
+                    if (letter >= 97 && letter <= 122)
+                    {
+                        var letterRot = letter + 13;
 
-                    newLetter = (char)letterRot;
-                }
-                else if (letter >= 65 && letter <= 90)
-                {
-                    var letterRot = letter + 13;
+                        if (letterRot > 122)
+                            letterRot -= 26;
 
-                    if (letterRot > 90)
-                        letterRot -= 26;
+                        newWord.Append((char) letterRot);
+                    }
+                    else if (letter >= 65 && letter <= 90)
+                    {
+                        var letterRot = letter + 13;
 
-                    newLetter = (char)letterRot;
-                }
-                else
-                {
-                    newLetter = letter;
+                        if (letterRot > 90)
+                            letterRot -= 26;
+
+                        newWord.Append((char) letterRot);
+                    }
+                    else
+                    {
+                        newWord.Append(word[i]);
+                    }
                 }
             }
-
-            if (Ratvarian.IsMatch(originalMessage[i].ToString()))
-            {
-                var letter = originalMessage[i];
-
-                if (letter >= 97 && letter <= 122)
-                {
-                    var letterRot = letter + 13;
-
-                    if (letterRot > 122)
-                        letterRot -= 26;
-
-                    newLetter = (char)letterRot;
-                }
-                else if (letter >= 65 && letter <= 90)
-                {
-                    var letterRot = letter + 13;
-
-                    if (letterRot > 90)
-                        letterRot -= 26;
-
-                    newLetter = (char)letterRot;
-                }
-                else
-                {
-                    newLetter = letter;
-                }
-            }
-
-            else if (originalMessage[i] == ' ')
-                newLetter = ' ';
-
-            finalMessage.Append(newLetter);
-        }*/
-        //return finalMessage.ToString();
+            finalMessage.Append(newWord + " ");
+        }
+        return finalMessage.ToString().Trim();
     }
 }
