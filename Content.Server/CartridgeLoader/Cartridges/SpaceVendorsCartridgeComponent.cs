101904ca636373dda31a3cfb7a039fb9c86b7d78
Lomcastar <sasha13rudi@gmail.com>
2023-04-29 03:26:21 +0300

    Added UI for the program

--- Content.Server/CartridgeLoader/Cartridges/SpaceVendorsCartridgeComponent.cs
+++ Content.Server/CartridgeLoader/Cartridges/SpaceVendorsCartridgeComponent.cs
@@ -1,8 +1,12 @@
 
+using Content.Shared.CartridgeLoader.Cartridges;
+
 namespace Content.Server.CartridgeLoader.Cartridges
 {
     [RegisterComponent]
     public sealed class SpaceVendorsCartridgeComponent : Component
     {
+        [DataField("appraisedItems")]
+        public List<AppraisedItem> AppraisedItems = new();
     }
 }
--- Resources/Locale/en-US/cartridge-loader/cartridges.ftl
+++ Resources/Locale/en-US/cartridge-loader/cartridges.ftl
@@ -7,3 +7,8 @@ net-probe-label-name = Name
 net-probe-label-address = Address
 net-probe-label-frequency = Frequency
 net-probe-label-network = Network
+
+space-vendors-program-name = SpaceVendors
+space-vendors-label-name = Name
+space-vendors-label-price = Price
+space-vendors-label-minutes = Minute
