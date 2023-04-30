f12a24b818c4e8f8c19c19140d3837c97c7983ae
Nim <128169402+Nimfar11@users.noreply.github.com>
2023-04-30 14:00:34 +0300

    spawnrule

---
+++ Content.Server/StationEvents/Components/SpawnRuleComponent.cs
@@ -0,0 +1,25 @@
+﻿using Content.Server.StationEvents.Events;
+
+namespace Content.Server.StationEvents.Components;
+
+/// <summary>
+/// Tarantula spawning component
+/// <summary>
+[RegisterComponent, Access(typeof(SpiderSpawnRule))]
+public sealed class SpiderSpawnRuleComponent : Component
+{
+
+}
+
+/// <summary>
+/// Slime spawning component
+/// <summary>
+[RegisterComponent, Access(typeof(SlimesSpawnRule))]
+public sealed class SlimesSpawnRuleComponent : Component
+{
+    [DataField("spawnedPrototypeChoices")]
+    public List<string> SpawnedPrototypeChoices = new()
+    {
+
+    };
+}
