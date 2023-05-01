6766d9063165bc73432095eb323dcb5192705cb5
Skye <22365940+Skyedra@users.noreply.github.com>
2023-05-01 01:45:54 -0700

    accept whitespace suggestion
    Co-authored-by: metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>

--- Content.Server/PowerSink/PowerSinkSystem.cs
+++ Content.Server/PowerSink/PowerSinkSystem.cs
@@ -95,7 +95,9 @@ namespace Content.Server.PowerSink
 
                     // Wind-up SFX
                     _audio.PlayPvs(component.ChargeFireSound, entity); // Play SFX
-                } else if (_gameTiming.CurTime >= component.ExplosionTime) {
+                } 
+                else if (_gameTiming.CurTime >= component.ExplosionTime)
+                {
                     // Explode!
                     toRemove.Add((entity, component, battery));
                 }
