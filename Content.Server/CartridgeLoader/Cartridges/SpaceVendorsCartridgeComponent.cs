7f81585952b7d79b0d097c2774ec8b4bf80b330b
Lomcastar <sasha13rudi@gmail.com>
2023-05-02 18:53:53 +0300

    Code cleaning

--- Content.Server/CartridgeLoader/Cartridges/SpaceVendorsCartridgeComponent.cs
+++ Content.Server/CartridgeLoader/Cartridges/SpaceVendorsCartridgeComponent.cs
@@ -1,15 +1,19 @@
-
 using Content.Shared.CartridgeLoader.Cartridges;
 
-namespace Content.Server.CartridgeLoader.Cartridges
-{
-    [RegisterComponent]
-    public sealed class SpaceVendorsCartridgeComponent : Component
-    {
-        [DataField("appraisedItems")]
-        public List<AppraisedItem> AppraisedItems = new();
+namespace Content.Server.CartridgeLoader.Cartridges;
 
-        [DataField("maxSavedItems")]
-        public int MaxSavedItems { get; set; } = 9;
-    }
+[RegisterComponent, Access(typeof(SpaceVendorsCartridgeSystem))]
+public sealed class SpaceVendorsCartridgeComponent : Component
+{
+    /// <summary>
+    /// List of valuable items
+    /// </summary>
+    [DataField("appraisedItems"), ViewVariables(VVAccess.ReadWrite)]
+    public List<AppraisedItem> AppraisedItems = new();
+
+    /// <summary>
+    /// Limit on the number of saves
+    /// </summary>
+    [DataField("maxSavedItems"), ViewVariables(VVAccess.ReadWrite)]
+    public int MaxSavedItems = 9;
 }
--- Content.Server/CartridgeLoader/Cartridges/SpaceVendorsCartridgeSystem.cs
+++ Content.Server/CartridgeLoader/Cartridges/SpaceVendorsCartridgeSystem.cs
@@ -4,62 +4,61 @@ using Content.Shared.CartridgeLoader.Cartridges;
 using Content.Shared.CartridgeLoader;
 using Content.Shared.IdentityManagement;
 
-namespace Content.Server.CartridgeLoader.Cartridges
+namespace Content.Server.CartridgeLoader.Cartridges;
+
+public sealed class SpaceVendorsCartridgeSystem : EntitySystem
 {
-    public sealed class SpaceVendorsCartridgeSystem : EntitySystem
+    [Dependency] private readonly CartridgeLoaderSystem? _cartridgeLoaderSystem = default!;
+    [Dependency] private readonly PricingSystem _pricingSystem = default!;
+    [Dependency] private readonly PopupSystem _popupSystem = default!;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly CartridgeLoaderSystem? _cartridgeLoaderSystem = default!;
-        [Dependency] private readonly PricingSystem _pricingSystem = default!;
-        [Dependency] private readonly PopupSystem _popupSystem = default!;
+        base.Initialize();
+        SubscribeLocalEvent<SpaceVendorsCartridgeComponent, CartridgeUiReadyEvent>(OnUiReady);
+        SubscribeLocalEvent<SpaceVendorsCartridgeComponent, CartridgeAfterInteractEvent>(OnAfterInteract);
+    }
 
-        public override void Initialize()
+    private void OnAfterInteract(EntityUid uid, SpaceVendorsCartridgeComponent component, CartridgeAfterInteractEvent args)
+    {
+        if (args.InteractEvent.Handled || !args.InteractEvent.CanReach || !args.InteractEvent.Target.HasValue)
+            return;
+
+        var price = _pricingSystem.GetPrice(args.InteractEvent.Target.Value);
+
+        _popupSystem.PopupEntity(Loc.GetString("price-gun-pricing-result",
+            ("object", Identity.Entity(args.InteractEvent.Target.Value, EntityManager)),
+            ("price", $"{price:F2}")), args.InteractEvent.User, args.InteractEvent.User);
+        args.InteractEvent.Handled = true;
+
+        if (component.AppraisedItems.Count >= component.MaxSavedItems)
+            component.AppraisedItems.RemoveAt(0);
+
+        var item = new AppraisedItem(Name(args.InteractEvent.Target.Value), $"{price:F2}");
+
+        component.AppraisedItems.Add(item);
+        UpdateUiState(uid,args.Loader,component);
+    }
+
+    private void OnUiReady(EntityUid uid, SpaceVendorsCartridgeComponent component, CartridgeUiReadyEvent args)
+    {
+        UpdateUiState(uid, args.Loader, component);
+    }
+
+    private void UpdateUiState(EntityUid uid, EntityUid loaderUid, SpaceVendorsCartridgeComponent? component)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+        UpdateElapsedTimeData(component);
+        var state = new SpaceVendorsUiState(component.AppraisedItems);
+        _cartridgeLoaderSystem?.UpdateCartridgeUiState(loaderUid, state);
+    }
+
+    private void UpdateElapsedTimeData(SpaceVendorsCartridgeComponent component)
+    {
+        foreach (var item in component.AppraisedItems)
         {
-            base.Initialize();
-            SubscribeLocalEvent<SpaceVendorsCartridgeComponent, CartridgeUiReadyEvent>(OnUiReady);
-            SubscribeLocalEvent<SpaceVendorsCartridgeComponent, CartridgeAfterInteractEvent>(OnAfterInteract);
-        }
-
-        private void OnAfterInteract(EntityUid uid, SpaceVendorsCartridgeComponent component, CartridgeAfterInteractEvent args)
-        {
-            if (args.InteractEvent.Handled || !args.InteractEvent.CanReach || !args.InteractEvent.Target.HasValue)
-                return;
-
-            var price = _pricingSystem.GetPrice(args.InteractEvent.Target.Value);
-
-            _popupSystem.PopupEntity(Loc.GetString("price-gun-pricing-result",
-                ("object", Identity.Entity(args.InteractEvent.Target.Value, EntityManager)),
-                ("price", $"{price:F2}")), args.InteractEvent.User, args.InteractEvent.User);
-            args.InteractEvent.Handled = true;
-
-            if (component.AppraisedItems.Count >= component.MaxSavedItems)
-                component.AppraisedItems.RemoveAt(0);
-
-            var item = new AppraisedItem(Name(args.InteractEvent.Target.Value), $"{price:F2}");
-
-            component.AppraisedItems.Add(item);
-            UpdateUiState(uid,args.Loader,component);
-        }
-
-        private void OnUiReady(EntityUid uid, SpaceVendorsCartridgeComponent component, CartridgeUiReadyEvent args)
-        {
-            UpdateUiState(uid, args.Loader, component);
-        }
-
-        private void UpdateUiState(EntityUid uid, EntityUid loaderUid, SpaceVendorsCartridgeComponent? component)
-        {
-            if (!Resolve(uid, ref component))
-                return;
-            UpdateElapsedTimeData(component);
-            var state = new SpaceVendorsUiState(component.AppraisedItems);
-            _cartridgeLoaderSystem?.UpdateCartridgeUiState(loaderUid, state);
-        }
-
-        private void UpdateElapsedTimeData(SpaceVendorsCartridgeComponent component)
-        {
-            foreach (var item in component.AppraisedItems)
-            {
-                item.UpdateElapsedTimeData();
-            }
+            item.UpdateElapsedTimeData();
         }
     }
 }
