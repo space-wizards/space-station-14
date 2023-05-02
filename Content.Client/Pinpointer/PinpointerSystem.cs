442018cc6112a9efc5282d74722d85ece19369b9
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-05-02 15:53:32 +0300

    don't update appearance in update loop (animation plays properly)

--- Content.Client/Pinpointer/PinpointerSystem.cs
+++ Content.Client/Pinpointer/PinpointerSystem.cs
@@ -8,13 +8,6 @@ public sealed class PinpointerSystem : SharedPinpointerSystem
 {
     [Dependency] private readonly IEyeManager _eyeManager = default!;
 
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SubscribeLocalEvent<PinpointerComponent, AppearanceChangeEvent>(OnAppearanceChange);
-    }
-
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
@@ -24,48 +17,25 @@ public sealed class PinpointerSystem : SharedPinpointerSystem
 
         // because eye can change it rotation anytime
         // we need to update this arrow in a update loop
-        var query = EntityQueryEnumerator<PinpointerComponent, AppearanceComponent>();
-        while (query.MoveNext(out var uid, out var pinpointer, out var appearance))
+        var query = EntityQueryEnumerator<PinpointerComponent, SpriteComponent>();
+        while (query.MoveNext(out var _, out var pinpointer, out var sprite))
         {
-            UpdateArrowAngle(uid, pinpointer, appearance);
-        }
-    }
+            if (!pinpointer.HasTarget)
+                return;
+            var eye = _eyeManager.CurrentEye;
+            var angle = pinpointer.ArrowAngle + eye.Rotation;
 
-    /// <summary>
-    ///     Transform pinpointer arrow from world space to eye space
-    ///     And send it to the appearance component
-    /// </summary>
-    private void UpdateArrowAngle(EntityUid uid, PinpointerComponent pinpointer, AppearanceComponent appearance)
-    {
-        if (!pinpointer.HasTarget)
-            return;
-        var eye = _eyeManager.CurrentEye;
-        var angle = pinpointer.ArrowAngle + eye.Rotation;
-        Appearance.SetData(uid, PinpointerVisuals.ArrowAngle, angle, appearance);
-    }
-
-    private void OnAppearanceChange(EntityUid uid, PinpointerComponent component, ref AppearanceChangeEvent args)
-    {
-        if (!TryComp(uid, out SpriteComponent? sprite))
-            return;
-
-        if (!Appearance.TryGetData<Distance>(uid, PinpointerVisuals.TargetDistance, out var distance, args.Component) ||
-            !Appearance.TryGetData<Angle>(uid, PinpointerVisuals.ArrowAngle, out var angle, args.Component))
-        {
-            sprite.LayerSetRotation(PinpointerLayers.Screen, Angle.Zero);
-            return;
-        }
-
-        switch (distance)
-        {
-            case Distance.Close:
-            case Distance.Medium:
-            case Distance.Far:
-                sprite.LayerSetRotation(PinpointerLayers.Screen, angle);
-                break;
-            default:
-                sprite.LayerSetRotation(PinpointerLayers.Screen, Angle.Zero);
-                break;
+            switch (pinpointer.DistanceToTarget)
+            {
+                case Distance.Close:
+                case Distance.Medium:
+                case Distance.Far:
+                    sprite.LayerSetRotation(PinpointerLayers.Screen, angle);
+                    break;
+                default:
+                    sprite.LayerSetRotation(PinpointerLayers.Screen, Angle.Zero);
+                    break;
+            }
         }
     }
 }
