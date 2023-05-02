eebba3e58e4adf177727ad12548889f58effbef3
Artjom <artjombebenin@gmail.com>
2023-05-02 18:56:00 +0300

    add settings for the indicators

--- Content.Client/CombatMode/ShowCombatModeIndicatorsOverlay.cs
+++ Content.Client/CombatMode/ShowCombatModeIndicatorsOverlay.cs
@@ -10,95 +10,113 @@ using Robust.Shared.Enums;
 using Robust.Shared.Utility;
 using Robust.Shared.Map;
 
-namespace Content.Client.CombatMode
+namespace Content.Client.CombatMode;
+
+/// <summary>
+///   This shows something like crosshairs for the combat mode next to the mouse cursor.
+///   For weapons with the gun class, a crosshair of one type is displayed,
+///   while for all other types of weapons and items in hand, as well as for an empty hand,
+///   a crosshair of a different type is displayed. These crosshairs simply show the state of combat mode (on|off).
+/// </summary>
+public sealed class ShowCombatModeIndicatorsOverlay : Overlay
 {
-    /// <summary>
-    ///   This shows something like crosshairs for the combat mode next to the mouse cursor.
-    ///   For weapons with the gun class, a crosshair of one type is displayed,
-    ///   while for all other types of weapons and items in hand, as well as for an empty hand,
-    ///   a crosshair of a different type is displayed. These crosshairs simply show the state of combat mode (on|off).
-    /// </summary>
-    public sealed class ShowCombatModeIndicatorsOverlay : Overlay
+    private IConfigurationManager _cfg;
+    private IInputManager _inputManager;
+    private IEntityManager _entMan;
+    private IEyeManager _eye;
+    private CombatModeSystem _combatSystem;
+
+    public override OverlaySpace Space => OverlaySpace.ScreenSpace;
+
+    public CombatModeOverlaySetting _settings = new(
+        Color.White.WithAlpha(0.3f),
+        Color.Black.WithAlpha(0.5f),
+        // 1 is a little big
+        0.8f
+    );
+
+    private Texture _gunSight;
+    private Texture _meleeSight;
+    private bool _isShowIndicators;
+
+    public ShowCombatModeIndicatorsOverlay(IConfigurationManager cfg, IInputManager input, IEntityManager entMan,
+            IEyeManager eye, CombatModeSystem combatSys)
     {
-        private IConfigurationManager _cfg;
-        private IInputManager _inputManager;
-        private IEntityManager _entMan;
-        private IEyeManager _eye;
-        private CombatModeSystem _combatSystem;
+        IoCManager.InjectDependencies(this);
 
-        public override OverlaySpace Space => OverlaySpace.ScreenSpace;
+        _cfg = cfg;
+        _inputManager = input;
+        _entMan = entMan;
+        _eye = eye;
+        _combatSystem = combatSys;
 
-        private Texture _gunSight;
-        private Texture _meleeSight;
-        private bool _isShowIndicators;
+        var spriteSys = _entMan.EntitySysManager.GetEntitySystem<SpriteSystem>();
+        _gunSight = spriteSys.Frame0(new SpriteSpecifier.Texture(
+            new($"/Textures/Interface/Misc/crosshair_pointers.rsi/gun_sight.png")));
+        _meleeSight = spriteSys.Frame0(new SpriteSpecifier.Texture(
+            new($"/Textures/Interface/Misc/crosshair_pointers.rsi/melee_sight.png")));
 
-        public ShowCombatModeIndicatorsOverlay(IConfigurationManager cfg, IInputManager input, IEntityManager entMan,
-                IEyeManager eye, CombatModeSystem combatSys)
-        {
-            IoCManager.InjectDependencies(this);
+        _isShowIndicators = _cfg.GetCVar(CCVars.HudHeldItemShow);
 
-            _cfg = cfg;
-            _inputManager = input;
-            _entMan = entMan;
-            _eye = eye;
-            _combatSystem = combatSys;
+        _cfg.OnValueChanged(CCVars.HudHeldItemShow,
+            isShow => _isShowIndicators = isShow, true);
+    }
 
-            var spriteSys = _entMan.EntitySysManager.GetEntitySystem<SpriteSystem>();
-            _gunSight = spriteSys.Frame0(new SpriteSpecifier.Texture(
-                new($"/Textures/Interface/Misc/crosshair_pointers.rsi/gun_sight.png")));
-            _meleeSight = spriteSys.Frame0(new SpriteSpecifier.Texture(
-                new($"/Textures/Interface/Misc/crosshair_pointers.rsi/melee_sight.png")));
+    protected override void Draw(in OverlayDrawArgs args)
+    {
+        if (!_isShowIndicators)
+            return;
 
-            _isShowIndicators = _cfg.GetCVar(CCVars.HudHeldItemShow);
+        if (!_combatSystem.IsInCombatMode())
+            return;
 
-            _cfg.OnValueChanged(CCVars.HudHeldItemShow,
-                isShow => _isShowIndicators = isShow, true);
-        }
+        var mouseScreen = _inputManager.MouseScreenPosition;
+        var mousePosMap = _eye.ScreenToMap(mouseScreen);
 
-        protected override void Draw(in OverlayDrawArgs args)
-        {
-            if (!_isShowIndicators)
-                return;
+        if (mousePosMap.MapId == MapId.Nullspace)
+            return;
 
-            if (!_combatSystem.IsInCombatMode())
-                return;
+        EntityUid? handEntity = _entMan.System<HandsSystem>().GetActiveHandEntity();
+        bool isHandGunItem = _entMan.HasComponent<GunComponent>(handEntity);
 
-            var mouseScreen = _inputManager.MouseScreenPosition;
-            var mousePosMap = _eye.ScreenToMap(mouseScreen);
+        var screen = args.ScreenHandle;
+        var mousePos = mouseScreen.Position;
+        var uiScale = (args.ViewportControl as Control)?.UIScale ?? 1f;
+        float limitedScale = uiScale > 1.25f ? 1.25f : uiScale;
 
-            if (mousePosMap.MapId == MapId.Nullspace)
-                return;
+        var sight = isHandGunItem ? _gunSight : _meleeSight;
+        DrawSight(sight, screen, mousePos, limitedScale * _settings.Scale);
+    }
+    private void DrawSight(Texture sight, DrawingHandleScreen screen, Vector2 centerPos, float scale)
+    {
+        screen.DrawTextureRect(sight,
+            GetRectForTexture(sight.Size, centerPos, scale), _settings.StrokeColor);
+        screen.DrawTextureRect(sight,
+            GetRectForTexture(sight.Size, centerPos, scale, true), _settings.MainColor);
+    }
 
-            EntityUid? handEntity = _entMan.System<HandsSystem>().GetActiveHandEntity();
-            bool isHandGunItem = _entMan.HasComponent<GunComponent>(handEntity);
+    static private UIBox2 GetRectForTexture(Vector2 textureSize, Vector2 pos, float scale,
+        bool expanded = false)
+    {
+        Vector2 sightSize = textureSize * scale;
 
-            var screen = args.ScreenHandle;
-            var mousePos = mouseScreen.Position;
-            var uiScale = (args.ViewportControl as Control)?.UIScale ?? 1f;
-            float limitedScale = uiScale > 1.25f ? 1.25f : uiScale;
+        if (expanded)
+            sightSize += 7;
 
-            var sight = isHandGunItem ? _gunSight : _meleeSight;
-            DrawSight(sight, screen, mousePos, limitedScale * 0.8f);
-        }
-        private void DrawSight(Texture sight, DrawingHandleScreen screen, Vector2 centerPos, float scale)
-        {
-            float transparency = 0.4f;
+        return UIBox2.FromDimensions(pos - (sightSize / 2), sightSize);
+    }
+}
 
-            screen.DrawTextureRect(sight,
-                GetRectForTexture(sight.Size, centerPos, scale), Color.Black.WithAlpha(transparency));
-            screen.DrawTextureRect(sight,
-                GetRectForTexture(sight.Size, centerPos, scale, true), Color.White.WithAlpha(transparency));
-        }
+public struct CombatModeOverlaySetting
+{
+    public Color MainColor;
+    public Color StrokeColor;
+    public float Scale;
 
-        static private UIBox2 GetRectForTexture(Vector2 textureSize, Vector2 pos, float scale,
-            bool expanded = false)
-        {
-            Vector2 sightSize = textureSize * scale;
-
-            if (expanded)
-                sightSize += 7;
-
-            return UIBox2.FromDimensions(pos - (sightSize / 2), sightSize);
-        }
+    public CombatModeOverlaySetting(Color color, Color stroke, float scale)
+    {
+        MainColor = color;
+        StrokeColor = stroke;
+        Scale = scale;
     }
 }
