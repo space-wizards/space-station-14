31bf4d3dc48d5635b47afa5cfce339544436609e
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-05-05 18:02:21 +0200

    Remove glibc, set soundfont environment variable. - glibc removal: not needed, would freeze some shells otherwise - soundfont addition: It's a surprise tool that will help us later!

--- shell.nix
+++ shell.nix
@@ -5,11 +5,11 @@ let
     dotnetCorePackages.sdk_7_0
     glfw
     SDL2
-    glibc
     libGL
     openal
     freetype
     fluidsynth
+    soundfont-fluid
     gtk3
     pango
     cairo
@@ -24,6 +24,7 @@ in pkgs.mkShell {
   inputsFrom = dependencies;
   shellHook = ''
     export GLIBC_TUNABLES=glibc.rtld.dynamic_sort=1
+    export ROBUST_SOUNDFONT_OVERRIDE=${pkgs.soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2
     export XDG_DATA_DIRS=$GSETTINGS_SCHEMAS_PATH
     export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath dependencies}
   '';
