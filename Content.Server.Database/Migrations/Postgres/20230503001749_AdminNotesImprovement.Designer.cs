c4597da91e18a8ed5178375e1fc2d219af79c869
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2023-05-05 05:18:08 +0200

    Migrations

--- Content.Server.Database/Migrations/Postgres/20230503001749_AdminNotesImprovement.Designer.cs
+++ Content.Server.Database/Migrations/Postgres/20230503001749_AdminNotesImprovement.Designer.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using Content.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Content.Server.Database.Migrations.Postgres
 {
     [DbContext(typeof(PostgresServerDbContext))]
-    partial class PostgresServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230503001749_AdminNotesImprovement")]
+    partial class AdminNotesImprovement
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
