baa479fb795709eedf4930d8e225af74effb3ffd
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-05-06 15:05:56 +1000

    changies

--- Content.Client/CombatMode/CombatModeIndicatorsOverlay.cs
+++ Content.Client/CombatMode/CombatModeIndicatorsOverlay.cs
@@ -1,4 +1,3 @@
-
 using Content.Client.Hands.Systems;
 using Content.Shared.Weapons.Ranged.Components;
 using Robust.Client.GameObjects;
@@ -16,12 +15,13 @@ namespace Content.Client.CombatMode;
 ///   while for all other types of weapons and items in hand, as well as for an empty hand,
 ///   a crosshair of a different type is displayed. These crosshairs simply show the state of combat mode (on|off).
 /// </summary>
-public sealed class ShowCombatModeIndicatorsOverlay : Overlay
+public sealed class CombatModeIndicatorsOverlay : Overlay
 {
     private readonly IInputManager _inputManager;
     private readonly IEntityManager _entMan;
     private readonly IEyeManager _eye;
-    private readonly CombatModeSystem _combatSystem;
+    private readonly CombatModeSystem _combat;
+    private readonly HandsSystem _hands = default!;
 
     private readonly Texture _gunSight;
     private readonly Texture _meleeSight;
@@ -32,34 +32,38 @@ public sealed class ShowCombatModeIndicatorsOverlay : Overlay
     public Color StrokeColor = Color.Black.WithAlpha(0.5f);
     public float Scale = 0.8f;  // 1 is a little big
 
-    public ShowCombatModeIndicatorsOverlay(IInputManager input, IEntityManager entMan,
-            IEyeManager eye, CombatModeSystem combatSys)
+    public CombatModeIndicatorsOverlay(IInputManager input, IEntityManager entMan,
+            IEyeManager eye, CombatModeSystem combatSys, HandsSystem hands)
     {
-        IoCManager.InjectDependencies(this);
-
         _inputManager = input;
         _entMan = entMan;
         _eye = eye;
-        _combatSystem = combatSys;
+        _combat = combatSys;
+        _hands = hands;
 
         var spriteSys = _entMan.EntitySysManager.GetEntitySystem<SpriteSystem>();
-        _gunSight = spriteSys.Frame0(new SpriteSpecifier.Rsi(new($"/Textures/Interface/Misc/crosshair_pointers.rsi"),
+        _gunSight = spriteSys.Frame0(new SpriteSpecifier.Rsi(new ResPath("/Textures/Interface/Misc/crosshair_pointers.rsi"),
             "gun_sight"));
-        _meleeSight = spriteSys.Frame0(new SpriteSpecifier.Rsi(new($"/Textures/Interface/Misc/crosshair_pointers.rsi"),
+        _meleeSight = spriteSys.Frame0(new SpriteSpecifier.Rsi(new ResPath("/Textures/Interface/Misc/crosshair_pointers.rsi"),
              "melee_sight"));
+    }
+
+    protected override bool BeforeDraw(in OverlayDrawArgs args)
+    {
+        if (!_combat.IsInCombatMode())
+            return false;
+
+        return base.BeforeDraw(in args);
     }
 
     protected override void Draw(in OverlayDrawArgs args)
     {
-        if (!_combatSystem.IsInCombatMode())
-            return;
-
         var mouseScreenPosition = _inputManager.MouseScreenPosition;
         var mousePosMap = _eye.ScreenToMap(mouseScreenPosition);
         if (mousePosMap.MapId != args.MapId)
             return;
 
-        var handEntity = _entMan.System<HandsSystem>().GetActiveHandEntity();
+        var handEntity = _hands.GetActiveHandEntity();
         var isHandGunItem = _entMan.HasComponent<GunComponent>(handEntity);
 
         var mousePos = mouseScreenPosition.Position;
