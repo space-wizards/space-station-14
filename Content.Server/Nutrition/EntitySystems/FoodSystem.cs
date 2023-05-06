8cf77c9578ca5e454338d235363fb3349a15f7a5
ElectroJr <leonsfriedrich@gmail.com>
2023-05-06 15:42:11 -0400

    Just make it return two values

--- Content.Server/Nutrition/EntitySystems/FoodSystem.cs
+++ Content.Server/Nutrition/EntitySystems/FoodSystem.cs
@@ -73,7 +73,8 @@ namespace Content.Server.Nutrition.EntitySystems
             if (ev.Handled)
                 return;
 
-            ev.Handled = Feed(ev.User, ev.User, uid, foodComponent);
+            var result = TryFeed(ev.User, ev.User, uid, foodComponent);
+            ev.Handled = result.Handled;
         }
 
         /// <summary>
@@ -84,27 +85,25 @@ namespace Content.Server.Nutrition.EntitySystems
             if (args.Handled || args.Target == null || !args.CanReach)
                 return;
 
-            args.Handled = Feed(args.User, args.Target.Value, uid, foodComponent);
+            var result = TryFeed(args.User, args.Target.Value, uid, foodComponent);
+            args.Handled = result.Handled;
         }
 
-        /// <summary>
-        ///     Attempt make some entity eat something. Returns true if ANY interaction occurs (not necessarily successful).
-        /// </summary>
-        public bool Feed(EntityUid user, EntityUid target, EntityUid food, FoodComponent foodComp)
+        public (bool Success, bool Handled) TryFeed(EntityUid user, EntityUid target, EntityUid food, FoodComponent foodComp)
         {
             //Suppresses self-eating
             if (food == user || TryComp<MobStateComponent>(food, out var mobState) && _mobStateSystem.IsAlive(food, mobState)) // Suppresses eating alive mobs
-                return false;
+                return (false, false);
 
             // Target can't be fed or they're already eating
             if (!TryComp<BodyComponent>(target, out var body))
-                return false;
+                return (false, false);
 
             if (!_solutionContainerSystem.TryGetSolution(food, foodComp.SolutionName, out var foodSolution) || foodSolution.Name == null)
-                return false;
+                return (false, false);
 
             if (!_bodySystem.TryGetBodyOrganComponents<StomachComponent>(target, out var stomachs, body))
-                return false;
+                return (false, false);
 
             var forceFeed = user != target;
 
@@ -114,7 +113,7 @@ namespace Content.Server.Nutrition.EntitySystems
                     forceFeed
                         ? Loc.GetString("food-system-cant-digest-other", ("entity", food))
                         : Loc.GetString("food-system-cant-digest", ("entity", food)), user, user);
-                return true;
+                return (false, true);
             }
 
             var flavors = _flavorProfileSystem.GetLocalizedFlavorsMessage(food, user, foodSolution);
@@ -123,20 +122,20 @@ namespace Content.Server.Nutrition.EntitySystems
             {
                 _popupSystem.PopupEntity(Loc.GetString("food-system-try-use-food-is-empty", ("entity", food)), user, user);
                 DeleteAndSpawnTrash(foodComp, food, user);
-                return true;
+                return (false, true);
             }
 
             if (IsMouthBlocked(target, user))
-                return true;
+                return (false, true);
 
             if (!_interactionSystem.InRangeUnobstructed(user, food, popup: true))
-                return true;
+                return (false, true);
 
             if (!_interactionSystem.InRangeUnobstructed(user, target, MaxFeedDistance, popup: true))
-                return true;
+                return (false, true);
 
             if (!TryGetRequiredUtensils(user, foodComp, out _))
-                return true;
+                return (false, true);
 
             if (forceFeed)
             {
@@ -172,7 +171,7 @@ namespace Content.Server.Nutrition.EntitySystems
             };
 
             _doAfterSystem.TryStartDoAfter(doAfterArgs);
-            return true;
+            return (true, true);
         }
 
         private void OnDoAfter(EntityUid uid, FoodComponent component, ConsumeDoAfterEvent args)
@@ -328,7 +327,7 @@ namespace Content.Server.Nutrition.EntitySystems
             {
                 Act = () =>
                 {
-                    Feed(ev.User, ev.User, uid, component);
+                    TryFeed(ev.User, ev.User, uid, component);
                 },
                 Icon = new SpriteSpecifier.Texture(new ("/Textures/Interface/VerbIcons/cutlery.svg.192dpi.png")),
                 Text = Loc.GetString("food-system-verb-eat"),
