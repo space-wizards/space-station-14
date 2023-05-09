348523028faaa06b051f77d047065b395be69cc3
Dawid Blaszyk <blaszykdawid@gmail.com>
2023-05-09 21:19:18 +0200

    Requested change

--- Content.Shared/Doors/Systems/SharedDoorSystem.cs
+++ Content.Shared/Doors/Systems/SharedDoorSystem.cs
@@ -5,11 +5,11 @@ using Content.Shared.DoAfter;
 using Content.Shared.Doors.Components;
 using Content.Shared.Hands.Components;
 using Content.Shared.Interaction;
+using Content.Shared.Physics;
 using Content.Shared.Stunnable;
 using Content.Shared.Tag;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
-using Robust.Shared.Physics;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Physics.Systems;
@@ -446,9 +446,8 @@ public abstract class SharedDoorSystem : EntitySystem
             if (!otherPhysics.CanCollide)
                 continue;
 
-            //If the colliding entity is a slippable item (banana peel, soap, liquids, magic runes) ignore it by the airlock
-            // CollisionLayer 20 = SlipLayer, CollionMask 10 = ItemMask
-            if (otherPhysics.CollisionLayer == 20 && otherPhysics.CollisionMask == 10)
+            //If the colliding entity is a slippable item ignore it by the airlock
+            if (otherPhysics.CollisionLayer == (int) CollisionGroup.SlipLayer && otherPhysics.CollisionMask == (int) CollisionGroup.ItemMask)
                 continue;
 
             if ((physics.CollisionMask & otherPhysics.CollisionLayer) == 0 && (otherPhysics.CollisionMask & physics.CollisionLayer) == 0)
