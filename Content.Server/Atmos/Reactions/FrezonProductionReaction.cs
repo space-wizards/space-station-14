85e39ddfa7214504f864b7e82dce353b17cef615
Tom Leys <tom@crump-leys.com>
2023-05-10 14:37:26 +1200

    Frezon reaction requires 50:1 Oxy:Trit ratio
    - Reaction will now scale to always take place, using whatever catalyst
      (N2) and Tritium is available.

--- Content.Server/Atmos/Reactions/FrezonProductionReaction.cs
+++ Content.Server/Atmos/Reactions/FrezonProductionReaction.cs
@@ -20,14 +20,18 @@ public sealed class FrezonProductionReaction : IGasReactionEffect
         var efficiency = mixture.Temperature / Atmospherics.FrezonProductionMaxEfficiencyTemperature;
         var loss = 1 - efficiency;
 
+        // How much the catalyst (N2) will allow us to produce
         // Less N2 is required the more efficient it is.
-        var minimumN2 = (initialOxy + initialTrit) / (Atmospherics.FrezonProductionNitrogenRatio * efficiency);
+        var catalystLimit = initialN2 * (Atmospherics.FrezonProductionNitrogenRatio / efficiency);
+        var oxyLimit = Math.Min(initialOxy, catalystLimit) / Atmospherics.FrezonProductionTritRatio;
 
-        if (initialN2 < minimumN2)
-            return ReactionResult.NoReaction;
+        // Amount of tritium & oxygen that are reacting
+        var tritBurned = Math.Min(oxyLimit, initialTrit);
+        var oxyBurned = tritBurned * Atmospherics.FrezonProductionTritRatio;
+        var burnRatio = tritBurned / initialTrit;
 
-        var oxyConversion = initialOxy / Atmospherics.FrezonProductionConversionRate;
-        var tritConversion = initialTrit / Atmospherics.FrezonProductionConversionRate;
+        var oxyConversion = oxyBurned / Atmospherics.FrezonProductionConversionRate;
+        var tritConversion = tritBurned / Atmospherics.FrezonProductionConversionRate;
         var total = oxyConversion + tritConversion;
 
         mixture.AdjustMoles(Gas.Oxygen, -oxyConversion);
