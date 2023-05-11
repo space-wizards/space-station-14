241aefa7503265e3f509736ac63827e3b3cadf95
Tom Leys <tom@crump-leys.com>
2023-05-11 17:10:42 +1200

    Cannot stack binary and trinary Atmos pumps and devices
    - Filters now have a 5x max volume to compensate for no more stacking
    - Add flipped versions of mixers and filters to the list of constructables

---
+++ Content.Shared/Construction/Conditions/NoUnstackableInTile.cs
@@ -0,0 +1,33 @@
+﻿using Content.Shared.Maps;
+using Content.Shared.Tag;
+using JetBrains.Annotations;
+using Robust.Shared.Map;
+
+namespace Content.Shared.Construction.Conditions
+{
+    [UsedImplicitly]
+    [DataDefinition]
+    public sealed class NoUnstackableInTile : IConstructionCondition
+    {
+        public bool Condition(EntityUid user, EntityCoordinates location, Direction direction)
+        {
+            var tagSystem = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<TagSystem>();
+
+            foreach (var entity in location.GetEntitiesInTile(LookupFlags.Approximate | LookupFlags.Static | LookupFlags.Sundries))
+            {
+                if (tagSystem.HasTag(entity, "Unstackable"))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public ConstructionGuideEntry GenerateGuideEntry()
+        {
+            return new ConstructionGuideEntry
+            {
+                Localization = "construction-step-condition-no-unstackable-in-tile"
+            };
+        }
+    }
+}
