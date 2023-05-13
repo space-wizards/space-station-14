dcff40d9374315ad00ed48c5c0ce1093e86b0abf
TemporalOroboros <TemporalOroboros@gmail.com>
2023-05-13 16:10:25 -0700

    Github reviews

--- Content.Client/Storage/Visualizers/StorageVisualizerComponent.cs
+++ Content.Client/Storage/Visualizers/StorageVisualizerComponent.cs
@@ -5,26 +5,44 @@ namespace Content.Client.Storage.Visualizers;
 public sealed class EntityStorageVisualsComponent : Component
 {
     /// <summary>
-    /// 
+    /// The RSI state used for the base layer of the storage entity sprite while the storage is closed.
     /// </summary>
-    [DataField("state")]
+    [DataField("stateBase")]
+    [ViewVariables(VVAccess.ReadWrite)]
     public string? StateBase;
-    
+
     /// <summary>
-    /// 
+    /// The RSI state used for the base layer of the storage entity sprite while the storage is open.
     /// </summary>
-    [DataField("stateAlt")]
-    public string? StateBaseAlt;
-    
+    [DataField("stateBaseOpen")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string? StateBaseOpen;
+
     /// <summary>
-    /// 
+    /// The RSI state used for the door/lid while the storage is open.
     /// </summary>
     [DataField("stateOpen")]
+    [ViewVariables(VVAccess.ReadWrite)]
     public string? StateOpen;
-    
+
     /// <summary>
-    /// 
+    /// The RSI state used for the door/lid while the storage is closed.
     /// </summary>
     [DataField("stateClosed")]
+    [ViewVariables(VVAccess.ReadWrite)]
     public string? StateClosed;
+
+    /// <summary>
+    /// The RSI state used for the lock indicator while the storage is locked.
+    /// </summary>
+    [DataField("stateLocked")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string? StateLocked = "locked";
+    
+    /// <summary>
+    /// The RSI state used for the lock indicator while the storage is unlocked.
+    /// </summary>
+    [DataField("stateUnlocked")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string? StateUnlocked = "unlocked";
 }
--- Content.Client/Storage/Visualizers/StorageVisualizerSystem.cs
+++ Content.Client/Storage/Visualizers/StorageVisualizerSystem.cs
@@ -8,79 +8,67 @@ public sealed class EntityStorageVisualizerSystem : VisualizerSystem<EntityStora
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<EntityStorageVisualsComponent, ComponentInit>(OnInit);
+        SubscribeLocalEvent<EntityStorageVisualsComponent, ComponentInit>(OnComponentInit);
     }
 
     /// <summary>
     /// Sets the base sprite to this layer. Exists to make the inheritance tree less boilerplate-y.
     /// </summary>
-    private void OnInit(EntityUid uid, EntityStorageVisualsComponent comp, ComponentInit args)
+    private void OnComponentInit(EntityUid uid, EntityStorageVisualsComponent comp, ComponentInit args)
     {
+        if (comp.StateBase == null)
+            return;
+
+        comp.StateBaseOpen ??= comp.StateBase;
         if (!TryComp<SpriteComponent>(uid, out var sprite))
             return;
 
-        if (comp.StateBase != null)
-        {
-            sprite.LayerSetState(0, comp.StateBase);
-        }
-
-        if (comp.StateBaseAlt == null)
-        {
-            comp.StateBaseAlt ??= comp.StateBase;
-        }
+        sprite.LayerSetState(StorageVisualLayers.Base, comp.StateBase);
     }
 
     protected override void OnAppearanceChange(EntityUid uid, EntityStorageVisualsComponent comp, ref AppearanceChangeEvent args)
     {
-        if (args.Sprite == null)
-            return;
-        if (!AppearanceSystem.TryGetData(uid, StorageVisuals.Open, out bool open, args.Component))
+        if (args.Sprite == null
+        || !AppearanceSystem.TryGetData<bool>(uid, StorageVisuals.Open, out var open, args.Component))
             return;
 
+        // Open/Closed state for the storage entity.
         if (args.Sprite.LayerMapTryGet(StorageVisualLayers.Door, out _))
         {
-            args.Sprite.LayerSetVisible(StorageVisualLayers.Door, true);
-
             if (open)
             {
+                args.Sprite.LayerSetVisible(StorageVisualLayers.Door, true);
                 if (comp.StateOpen != null)
-                {
                     args.Sprite.LayerSetState(StorageVisualLayers.Door, comp.StateOpen);
-                    args.Sprite.LayerSetVisible(StorageVisualLayers.Door, true);
-                }
 
-                if (comp.StateBaseAlt != null)
-                    args.Sprite.LayerSetState(0, comp.StateBaseAlt);
-            }
-            else if (!open)
-            {
-                if (comp.StateClosed != null)
-                    args.Sprite.LayerSetState(StorageVisualLayers.Door, comp.StateClosed);
-                else
-                {
-                    args.Sprite.LayerSetVisible(StorageVisualLayers.Door, false);
-                }
-
-                if (comp.StateBase != null)
-                    args.Sprite.LayerSetState(0, comp.StateBase);
+                if (comp.StateBaseOpen != null)
+                    args.Sprite.LayerSetState(StorageVisualLayers.Base, comp.StateBaseOpen);
             }
             else
             {
-                args.Sprite.LayerSetVisible(StorageVisualLayers.Door, false);
+                if (comp.StateClosed != null)
+                {
+                    args.Sprite.LayerSetState(StorageVisualLayers.Door, comp.StateClosed);
+                    args.Sprite.LayerSetVisible(StorageVisualLayers.Door, true);
+                }
+                else
+                    args.Sprite.LayerSetVisible(StorageVisualLayers.Door, false);
+
+                if (comp.StateBase != null)
+                    args.Sprite.LayerSetState(StorageVisualLayers.Base, comp.StateBase);
             }
         }
 
-        if (AppearanceSystem.TryGetData(uid, StorageVisuals.CanLock, out bool canLock, args.Component) && canLock)
+        // Lock state for the storage entity. TODO: Split into its own visualizer.
+        if (AppearanceSystem.TryGetData<bool>(uid, StorageVisuals.CanLock, out var canLock, args.Component) && canLock)
         {
-            if (!AppearanceSystem.TryGetData(uid, StorageVisuals.Locked, out bool locked, args.Component))
-            {
+            if (!AppearanceSystem.TryGetData<bool>(uid, StorageVisuals.Locked, out var locked, args.Component))
                 locked = true;
-            }
 
             args.Sprite.LayerSetVisible(StorageVisualLayers.Lock, !open);
             if (!open)
             {
-                args.Sprite.LayerSetState(StorageVisualLayers.Lock, locked ? "locked" : "unlocked");
+                args.Sprite.LayerSetState(StorageVisualLayers.Lock, locked ? comp.StateLocked : comp.StateUnlocked);
             }
         }
     }
@@ -88,6 +76,7 @@ public sealed class EntityStorageVisualizerSystem : VisualizerSystem<EntityStora
 
 public enum StorageVisualLayers : byte
 {
+    Base,
     Door,
     Lock
 }
