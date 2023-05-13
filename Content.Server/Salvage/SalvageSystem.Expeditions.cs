2c43b718ec34e1da5c75d440500e72cf8e0c7c83
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-05-13 17:04:40 +1000

    Fix salvage dungeon spawn offset + remove mining
    Mining sucks and needs more design and I just simplified the dungeon spawn so it won't spawn on the landing pad anymore.

--- Content.Server/Salvage/SalvageSystem.Expeditions.cs
+++ Content.Server/Salvage/SalvageSystem.Expeditions.cs
@@ -232,6 +232,9 @@ public sealed partial class SalvageSystem
         if (configs.Count == 0)
             return;
 
+        // Temporarily removed coz it SUCKS
+        configs.Remove(SalvageMissionType.Mining);
+
         for (var i = 0; i < MissionLimit; i++)
         {
             _random.Shuffle(configs);
