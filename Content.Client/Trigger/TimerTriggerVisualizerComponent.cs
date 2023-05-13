736182b7f62a6a984f0283ef16e0b4768631ab21
TemporalOroboros <TemporalOroboros@gmail.com>
2023-05-13 12:41:17 -0700

    Resolves github requests

--- Content.Client/Trigger/TimerTriggerVisualizerComponent.cs
+++ Content.Client/Trigger/TimerTriggerVisualizerComponent.cs
@@ -5,12 +5,39 @@ namespace Content.Client.Trigger;
 
 [RegisterComponent]
 [Access(typeof(TimerTriggerVisualizerSystem))]
-public sealed class TimerTriggerVisualizerComponent : Component
+public sealed class TimerTriggerVisualsComponent : Component
 {
+    /// <summary>
+    /// The key used to index the priming animation.
+    /// </summary>
+    [ViewVariables]
     public const string AnimationKey = "priming_animation";
 
-    [DataField("countdown_sound")]
-    public SoundSpecifier? CountdownSound;
+    /// <summary>
+    /// The RSI state used while the device has not been primed.
+    /// </summary>
+    [DataField("unprimedSprite")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string UnprimedSprite = "icon";
 
+    /// <summary>
+    /// The RSI state used when the device is primed.
+    /// Not VVWrite-able because it's only used at component init to construct the priming animation.
+    /// </summary>
+    [DataField("primingSprite")]
+    public string PrimingSprite = "primed";
+
+    /// <summary>
+    /// The sound played when the device is primed.
+    /// Not VVWrite-able because it's only used at component init to construct the priming animation.
+    /// </summary>
+    [DataField("primingSound")]
+    public SoundSpecifier? PrimingSound;
+
+    /// <summary>
+    /// The actual priming animation.
+    /// Constructed at component init from the sprite and sound.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
     public Animation PrimingAnimation = default!;
 }
--- Content.Client/Trigger/TimerTriggerVisualizerSystem.cs
+++ Content.Client/Trigger/TimerTriggerVisualizerSystem.cs
@@ -1,57 +1,59 @@
 using Content.Shared.Trigger;
 using Robust.Client.Animations;
 using Robust.Client.GameObjects;
+using Robust.Shared.GameObjects;
 
 namespace Content.Client.Trigger;
 
-public sealed class TimerTriggerVisualizerSystem : VisualizerSystem<TimerTriggerVisualizerComponent>
+public sealed class TimerTriggerVisualizerSystem : VisualizerSystem<TimerTriggerVisualsComponent>
 {
+    [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
+
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<TimerTriggerVisualizerComponent, ComponentInit>(OnInit);
+        SubscribeLocalEvent<TimerTriggerVisualsComponent, ComponentInit>(OnComponentInit);
     }
 
-    private void OnInit(EntityUid uid, TimerTriggerVisualizerComponent comp, ComponentInit args)
+    private void OnComponentInit(EntityUid uid, TimerTriggerVisualsComponent comp, ComponentInit args)
     {
-        comp.PrimingAnimation = new Animation { Length = TimeSpan.MaxValue };
-        {
-            var flick = new AnimationTrackSpriteFlick();
-            comp.PrimingAnimation.AnimationTracks.Add(flick);
-            flick.LayerKey = TriggerVisualLayers.Base;
-            flick.KeyFrames.Add(new AnimationTrackSpriteFlick.KeyFrame("primed", 0f));
+        comp.PrimingAnimation = new Animation {
+            Length = TimeSpan.MaxValue,
+            AnimationTracks = {
+                new AnimationTrackSpriteFlick() {
+                    LayerKey = TriggerVisualLayers.Base,
+                    KeyFrames = { new AnimationTrackSpriteFlick.KeyFrame(comp.PrimingSprite, 0f) }
+                }
+            },
+        };
 
-            if (comp.CountdownSound != null)
-            {
-                var sound = new AnimationTrackPlaySound();
-                comp.PrimingAnimation.AnimationTracks.Add(sound);
-                sound.KeyFrames.Add(new AnimationTrackPlaySound.KeyFrame(comp.CountdownSound.GetSound(), 0));
-            }
+        if (comp.PrimingSound != null)
+        {
+            comp.PrimingAnimation.AnimationTracks.Add(
+                new AnimationTrackPlaySound() {
+                    KeyFrames = { new AnimationTrackPlaySound.KeyFrame(_audioSystem.GetSound(comp.PrimingSound), 0) }
+                }
+            );
         }
     }
 
-    protected override void OnAppearanceChange(EntityUid uid, TimerTriggerVisualizerComponent comp, ref AppearanceChangeEvent args)
+    protected override void OnAppearanceChange(EntityUid uid, TimerTriggerVisualsComponent comp, ref AppearanceChangeEvent args)
     {
-        if (args.Sprite == null)
-            return;
-        if(!TryComp<AnimationPlayerComponent>(uid, out var animPlayer))
+        if (args.Sprite == null
+        ||  !TryComp<AnimationPlayerComponent>(uid, out var animPlayer))
             return;
 
-        if (!AppearanceSystem.TryGetData(uid, TriggerVisuals.VisualState, out TriggerVisualState state, args.Component))
-        {
+        if (!AppearanceSystem.TryGetData<TriggerVisualState>(uid, TriggerVisuals.VisualState, out var state, args.Component))
             state = TriggerVisualState.Unprimed;
-        }
 
         switch (state)
         {
             case TriggerVisualState.Primed:
-                if (!AnimationSystem.HasRunningAnimation(uid, animPlayer, TimerTriggerVisualizerComponent.AnimationKey))
-                {
-                    AnimationSystem.Play(uid, animPlayer, comp.PrimingAnimation, TimerTriggerVisualizerComponent.AnimationKey);
-                }
+                if (!AnimationSystem.HasRunningAnimation(uid, animPlayer, TimerTriggerVisualsComponent.AnimationKey))
+                    AnimationSystem.Play(uid, animPlayer, comp.PrimingAnimation, TimerTriggerVisualsComponent.AnimationKey);
                 break;
             case TriggerVisualState.Unprimed:
-                args.Sprite.LayerSetState(0, "icon");
+                args.Sprite.LayerSetState(TriggerVisualLayers.Base, comp.UnprimedSprite);
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
