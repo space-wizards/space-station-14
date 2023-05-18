d816fab746c0244d9be0d3fa1800b570b54a1db1
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-05-18 23:47:12 +1000

    Adds grappling gun

---
+++ Content.Shared/Weapons/Misc/GrapplingProjectileSystem.cs
@@ -0,0 +1,36 @@
+using Content.Shared.Projectiles;
+using Robust.Shared.Physics;
+using Robust.Shared.Physics.Events;
+using Robust.Shared.Physics.Systems;
+
+namespace Content.Shared.Weapons.Misc;
+
+public sealed class GrapplingProjectileSystem : EntitySystem
+{
+    [Dependency] private readonly SharedJointSystem _joints = default!;
+    [Dependency] private readonly SharedPhysicsSystem _physics = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+
+    // TODO: Make this a generic embed component.
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<GrapplingProjectileComponent, StartCollideEvent>(OnGrappleCollide);
+    }
+
+    private void OnGrappleCollide(EntityUid uid, GrapplingProjectileComponent component, ref StartCollideEvent args)
+    {
+        if (!TryComp<ProjectileComponent>(uid, out var projectile))
+            return;
+
+        _physics.SetLinearVelocity(uid, Vector2.Zero, body: args.OurBody);
+        _physics.SetBodyType(uid, BodyType.Static, body: args.OurBody);
+        _transform.SetParent(uid, args.OtherEntity);
+
+        var joint = _joints.CreateDistanceJoint(uid, projectile.Shooter, anchorA: new Vector2(0f, -0.5f));
+        joint.MaxLength = joint.Length + 0.5f;
+        joint.Stiffness = 1f;
+        joint.MinLength = 0.1f;
+    }
+}
