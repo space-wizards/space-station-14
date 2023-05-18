8e8ba6cda3e334445d90819637dbf4c6184f9844
Dawid Blaszyk <blaszykdawid@gmail.com>
2023-05-18 22:03:46 +0200

    Romerol unzombify effect

--- Content.Shared/Zombies/UnzombifyComponent.cs
+++ Content.Shared/Zombies/UnzombifyComponent.cs
@@ -1,19 +1,7 @@
-using Content.Shared.Chat.Prototypes;
-using Content.Shared.Damage;
-using Content.Shared.Roles;
-using Content.Shared.Humanoid;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
-using static Content.Shared.Humanoid.HumanoidAppearanceState;
 
 namespace Content.Shared.Zombies
 {
     [RegisterComponent, NetworkedComponent]
-    public sealed class UnzombifyComponent : Component
-    {
-        [DataField("unzombify")]
-        public bool Unzombify = true;
-    }
+    public sealed class UnzombifyComponent : Component { }
 }
