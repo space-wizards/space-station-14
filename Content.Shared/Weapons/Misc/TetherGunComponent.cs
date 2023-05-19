36cd2046a876d86adeecf4b7e02503a6f87c0de6
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2023-05-20 00:31:15 +1000

    Fix tethergun (#16612)

--- Content.Shared/Weapons/Misc/TetherGunComponent.cs
+++ Content.Shared/Weapons/Misc/TetherGunComponent.cs
@@ -9,6 +9,12 @@ public sealed partial class TetherGunComponent : BaseForceGunComponent
     public float MaxDistance = 10f;
 
     /// <summary>
+    /// The entity the tethered target has a joint to.
+    /// </summary>
+    [DataField("tetherEntity"), AutoNetworkedField]
+    public override EntityUid? TetherEntity { get; set; }
+
+    /// <summary>
     /// The entity currently tethered.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite), DataField("tethered"), AutoNetworkedField]
