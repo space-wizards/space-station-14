11bcad2d002dea43dc72f445d558cedcd8454834
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-05-20 16:02:48 +1000

    a

--- Content.Shared/Weapons/Misc/GrapplingProjectileSystem.cs
+++ Content.Shared/Weapons/Misc/GrapplingProjectileSystem.cs
@@ -2,12 +2,14 @@ using Content.Shared.Physics;
 using Content.Shared.Projectiles;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Physics.Systems;
+using Robust.Shared.Timing;
 using Robust.Shared.Utility;
 
 namespace Content.Shared.Weapons.Misc;
 
 public sealed class GrapplingProjectileSystem : EntitySystem
 {
+    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly SharedJointSystem _joints = default!;
 
     public const string GrapplingJoint = "grappling";
@@ -20,6 +22,9 @@ public sealed class GrapplingProjectileSystem : EntitySystem
 
     private void OnGrappleCollide(EntityUid uid, GrapplingProjectileComponent component, ref ProjectileEmbedEvent args)
     {
+        if (!_timing.IsFirstTimePredicted)
+            return;
+
         var joint = _joints.CreateDistanceJoint(uid, args.Weapon, anchorA: new Vector2(0f, -0.5f), id: GrapplingJoint);
         joint.MaxLength = joint.Length + 0.5f;
         joint.Stiffness = 1f;
