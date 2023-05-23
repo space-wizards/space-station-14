67e53ca64f0f085be72aa8b64391f2368397b1e3
faint <46868845+ficcialfaint@users.noreply.github.com>
2023-05-24 00:49:30 +0300

    forgor

---
+++ Content.Server/StationRecords/Components/GeneralStationRecordConsoleComponent.cs
@@ -0,0 +1,10 @@
+using Content.Shared.StationRecords;
+
+namespace Content.Server.StationRecords;
+
+[RegisterComponent]
+public sealed class GeneralStationRecordConsoleComponent : Component
+{
+    public StationRecordKey? ActiveKey { get; set; }
+    public GeneralStationRecordsFilter? Filter { get; set; }
+}
