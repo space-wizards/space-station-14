630775756866fb313ca90f9a784da48723c70024
EmoGarbage404 <retron404@gmail.com>
2023-05-24 22:49:39 -0400

    Make weapon arcs follow the player

--- Content.Client/Weapons/Melee/MeleeWeaponSystem.Effects.cs
+++ Content.Client/Weapons/Melee/MeleeWeaponSystem.Effects.cs
@@ -140,23 +140,26 @@ public sealed partial class MeleeWeaponSystem
         sprite.Rotation = localPos.ToWorldAngle();
         var distance = Math.Clamp(localPos.Length / 2f, 0.2f, 1f);
 
+        var xformQuery = GetEntityQuery<TransformComponent>();
+        var xform = xformQuery.GetComponent(animationUid);
+
         switch (arcComponent.Animation)
         {
             case WeaponArcAnimation.Slash:
                 _animation.Play(animationUid, GetSlashAnimation(sprite, angle), SlashAnimationKey);
+                TransformSystem.SetParent(animationUid, xform, user, userXform);
                 if (arcComponent.Fadeout)
                     _animation.Play(animationUid, GetFadeAnimation(sprite, 0.065f, 0.065f + 0.05f), FadeAnimationKey);
                 break;
             case WeaponArcAnimation.Thrust:
                 _animation.Play(animationUid, GetThrustAnimation(sprite, distance), ThrustAnimationKey);
+                TransformSystem.SetParent(animationUid, xform, user, userXform);
                 if (arcComponent.Fadeout)
                     _animation.Play(animationUid, GetFadeAnimation(sprite, 0.05f, 0.15f), FadeAnimationKey);
                 break;
             case WeaponArcAnimation.None:
-                var xformQuery = GetEntityQuery<TransformComponent>();
                 var (mapPos, mapRot) = TransformSystem.GetWorldPositionRotation(userXform, xformQuery);
-                var xform = xformQuery.GetComponent(animationUid);
-                xform.AttachToGridOrMap();
+                TransformSystem.AttachToGridOrMap(animationUid, xform);
                 var worldPos = mapPos + (mapRot - userXform.LocalRotation).RotateVec(localPos);
                 var newLocalPos = TransformSystem.GetInvWorldMatrix(xform.ParentUid, xformQuery).Transform(worldPos);
                 TransformSystem.SetLocalPositionNoLerp(xform, newLocalPos);
