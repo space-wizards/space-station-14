5bb841dc1f89b4e8c02fdccb8eee8470ae4b6c6c
Morb <14136326+Morb0@users.noreply.github.com>
2023-05-26 13:41:42 +0300

    Fixed door connection crash bug (#1097)
    Co-authored-by: Canvas123 <lunarfury6@gmail.com>

--- Content.Server/DeviceLinking/Systems/DoorSignalControlSystem.cs
+++ Content.Server/DeviceLinking/Systems/DoorSignalControlSystem.cs
@@ -90,9 +90,7 @@ namespace Content.Server.DeviceLinking.Systems
                 _signalSystem.InvokePort(uid, door.OutOpen, data);
             }
             else if (args.State == DoorState.Open
-                  || args.State == DoorState.Opening
-                  || args.State == DoorState.Closing
-                  || args.State == DoorState.Emagging)
+                     || args.State == DoorState.Emagging)
             {
                 data[DeviceNetworkConstants.LogicState] = SignalState.High;
                 _signalSystem.InvokePort(uid, door.OutOpen, data);
