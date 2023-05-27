794555e0f6fcc2fb998c3488481a73e3baa425ce
ElectroJr <leonsfriedrich@gmail.com>
2023-05-27 17:31:08 -0400

    Better version info

--- Content.Replay/UI/Menu/ReplayMainMenu.cs
+++ Content.Replay/UI/Menu/ReplayMainMenu.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using Content.Client.Message;
 using Content.Client.UserInterface.Systems.EscapeMenu;
+using Content.Replay.Manager;
 using Robust.Client;
 using Robust.Client.Replays.Loading;
 using Robust.Client.ResourceManagement;
@@ -8,6 +9,8 @@ using Robust.Client.Serialization;
 using Robust.Client.State;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
+using Robust.Shared;
+using Robust.Shared.Configuration;
 using Robust.Shared.ContentPack;
 using Robust.Shared.Serialization.Markdown.Value;
 using Robust.Shared.Utility;
@@ -21,9 +24,10 @@ namespace Content.Replay.UI.Menu;
 public sealed class ReplayMainScreen : State
 {
     [Dependency] private readonly IResourceManager _resMan = default!;
-    [Dependency] private readonly IResourceCache _resourceCache = default!;
-    [Dependency] private readonly Manager.ReplayManager _replayMan = default!;
+    [Dependency] private readonly ReplayManager _replayMan = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
     [Dependency] private readonly IReplayLoadManager _loadMan = default!;
+    [Dependency] private readonly IResourceCache _resourceCache = default!;
     [Dependency] private readonly IGameController _controllerProxy = default!;
     [Dependency] private readonly IClientRobustSerializer _serializer = default!;
     [Dependency] private readonly IUserInterfaceManager _userInterfaceManager = default!;
@@ -88,25 +92,50 @@ public sealed class ReplayMainScreen : State
         data.TryGet<ValueDataNode>(Time, out var timeNode);
         data.TryGet<ValueDataNode>(Duration, out var durationNode);
         data.TryGet<ValueDataNode>("roundId", out var roundIdNode);
-        data.TryGet<ValueDataNode>(Engine, out var engineNode);
-        data.TryGet<ValueDataNode>(Fork, out var forkNode);
-        data.TryGet<ValueDataNode>(ForkVersion, out var versionNode);
         data.TryGet<ValueDataNode>(Hash, out var hashNode);
-        var forkVersion = versionNode?.Value ?? Error;
         DateTime.TryParse(timeNode?.Value, out var time);
         TimeSpan.TryParse(durationNode?.Value, out var duration);
 
-        // Why does this not have a try-convert function???
-        try
+        var forkId = string.Empty;
+        if (data.TryGet<ValueDataNode>(Fork, out var forkNode))
         {
-            Convert.FromHexString(forkVersion);
-            // version is a probably some GH hash. Crop it to keep the info box small.
-            forkVersion = forkVersion[..16];
+            // TODO REPLAYS somehow distribute and load from build.json?
+            var clientFork = _cfg.GetCVar(CVars.BuildForkId);
+            if (string.IsNullOrWhiteSpace(clientFork))
+                forkId = forkNode.Value;
+            else if (forkNode.Value == clientFork)
+                forkId = $"[color=green]{forkNode.Value}[/color]";
+            else
+                forkId = $"[color=yellow]{forkNode.Value}[/color]";
         }
-        catch
+
+        var forkVersion = string.Empty;
+        if (data.TryGet<ValueDataNode>(ForkVersion, out var versionNode))
         {
-            // ignored
+            forkVersion = versionNode.Value;
+            // Why does this not have a try-convert function? I just want to check if it looks like a hash code.
+            try
+            {
+                Convert.FromHexString(forkVersion);
+                // version is a probably some GH hash. Crop it to keep the info box small.
+                forkVersion = forkVersion[..16];
+            }
+            catch
+            {
+                // ignored
+            }
+
+            // TODO REPLAYS somehow distribute and load from build.json?
+            var clientVer = _cfg.GetCVar(CVars.BuildVersion);
+            if (!string.IsNullOrWhiteSpace(clientVer))
+            {
+                if (versionNode.Value == clientVer)
+                    forkVersion = $"[color=green]{forkVersion}[/color]";
+                else
+                    forkVersion = $"[color=yellow]{forkVersion}[/color]";
+            }
         }
+
 
         var typeHash = hashNode?.Value ?? Error;
         if (Convert.FromHexString(typeHash).SequenceEqual(_serializer.GetSerializableTypesHash()))
@@ -120,10 +149,21 @@ public sealed class ReplayMainScreen : State
             _mainMenuControl.LoadButton.Disabled = true;
         }
 
-        // TODO REPLAYS version information
-        // Include engine, forkId, & forkVersion data with the client.
-        // Currently these are server-exclusive CVARS.
-        // If they differ from the current file, highlight the text in yellow (file may be loadable, but may cause issues).
+        var engineVersion = string.Empty;
+        if (data.TryGet<ValueDataNode>(Engine, out var engineNode))
+        {
+            var clientVer = _cfg.GetCVar(CVars.BuildEngineVersion);
+            if (string.IsNullOrWhiteSpace(clientVer))
+                engineVersion = engineNode.Value;
+            else if (engineNode.Value == clientVer)
+                engineVersion = $"[color=green]{engineNode.Value}[/color]";
+            else
+                engineVersion = $"[color=yellow]{engineNode.Value}[/color]";
+        }
+
+        // Strip milliseconds.
+        // why the fuck isn't there a general format string that suppresses milliseconds.
+        duration = new((int)Math.Floor(duration.TotalDays), duration.Hours, duration.Minutes, duration.Seconds);
 
         info.HorizontalAlignment = Control.HAlignment.Left;
         info.VerticalAlignment = Control.VAlignment.Top;
@@ -135,7 +175,7 @@ public sealed class ReplayMainScreen : State
             ("duration", duration),
             ("forkId", forkNode?.Value ?? Error),
             ("version", forkVersion),
-            ("engVersion", engineNode?.Value ?? Error),
+            ("engVersion", engineVersion),
             ("hash", typeHash)));
     }
 
