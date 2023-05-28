dd044f4a91effc6cf9b0100db27221768cf72e11
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-05-28 03:03:25 -0400

    Make melee damage not go through MeleeHitEvent.cs (#16881)
    Co-authored-by: metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>

---
+++ Content.Shared/Weapons/Melee/Components/BonusMeleeDamageComponent.cs
@@ -0,0 +1,17 @@
+﻿using Content.Shared.Damage;
+
+namespace Content.Shared.Weapons.Melee.Components;
+
+/// <summary>
+/// This is used for adding in bonus damage via <see cref="GetMeleeWeaponEvent"/>
+/// This exists only for event relays and doing entity shenanigans.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed class BonusMeleeDamageComponent : Component
+{
+    /// <summary>
+    /// The damage that will be applied.
+    /// </summary>
+    [DataField("bonusDamage", required: true)]
+    public DamageSpecifier BonusDamage = default!;
+}
