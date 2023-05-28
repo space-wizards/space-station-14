ade08755411482a42598d1979241578a5dbb290e
Alexey Maksimov <ya.apexman@ya.ru>
2023-05-28 18:35:56 +0300

    Admin, joined to server as ghost, can spawn as aghost and back to ghost (#39)
    Co-authored-by: Alexey Maksimov <alemak@sourcemap.pro>

--- Content.Server/Administration/Commands/AGhost.cs
+++ Content.Server/Administration/Commands/AGhost.cs
@@ -1,10 +1,13 @@
 ﻿using Content.Server.GameTicking;
 using Content.Server.Ghost.Components;
+using Content.Server.Hands.Systems;
 using Content.Server.Players;
 using Content.Shared.Administration;
 using Content.Shared.Ghost;
+using Content.Shared.Hands.Components;
 using Robust.Server.Player;
 using Robust.Shared.Console;
+using Robust.Shared.Map;
 
 namespace Content.Server.Administration.Commands
 {
@@ -14,7 +17,7 @@ namespace Content.Server.Administration.Commands
         [Dependency] private readonly IEntityManager _entities = default!;
 
         public string Command => "aghost";
-        public string Description => "Makes you an admin ghost.";
+        public string Description => "Makes you an admin ghost. If you already are - simple ghost";
         public string Help => "aghost";
 
         public void Execute(IConsoleShell shell, string argStr, string[] args)
@@ -22,7 +25,7 @@ namespace Content.Server.Administration.Commands
             var player = shell.Player as IPlayerSession;
             if (player == null)
             {
-                shell.WriteLine("Nah");
+                shell.WriteLine($"Nah: could not find player's session, skipping command {Command}");
                 return;
             }
 
@@ -30,10 +33,11 @@ namespace Content.Server.Administration.Commands
 
             if (mind == null)
             {
-                shell.WriteLine("You can't ghost here!");
+                shell.WriteLine("You can't ghost here! Could not find 'mind'");
                 return;
             }
 
+            //try to find entity, where we can return player's mind
             if (mind.VisitingEntity != default && _entities.HasComponent<GhostComponent>(mind.VisitingEntity))
             {
                 player.ContentData()!.Mind?.UnVisit();
@@ -44,14 +48,14 @@ namespace Content.Server.Administration.Commands
                             && !_entities.HasComponent<GhostComponent>(mind.CurrentEntity);
             var coordinates = player.AttachedEntity != null
                 ? _entities.GetComponent<TransformComponent>(player.AttachedEntity.Value).Coordinates
-                : EntitySystem.Get<GameTicker>().GetObserverSpawnPoint();
-            var ghost = _entities.SpawnEntity("AdminObserver", coordinates);
+                : _entities.System<GameTicker>().GetObserverSpawnPoint();
+            var ghost = SpawnGhost(coordinates, player, canReturn);
             _entities.GetComponent<TransformComponent>(ghost).AttachToGridOrMap();
 
             if (canReturn)
             {
                 // TODO: Remove duplication between all this and "GamePreset.OnGhostAttempt()"...
-                if(!string.IsNullOrWhiteSpace(mind.CharacterName))
+                if (!string.IsNullOrWhiteSpace(mind.CharacterName))
                     _entities.GetComponent<MetaDataComponent>(ghost).EntityName = mind.CharacterName;
                 else if (!string.IsNullOrWhiteSpace(mind.Session?.Name))
                     _entities.GetComponent<MetaDataComponent>(ghost).EntityName = mind.Session.Name;
@@ -65,7 +69,42 @@ namespace Content.Server.Administration.Commands
             }
 
             var comp = _entities.GetComponent<GhostComponent>(ghost);
-            EntitySystem.Get<SharedGhostSystem>().SetCanReturnToBody(comp, canReturn);
+            _entities.System<SharedGhostSystem>().SetCanReturnToBody(comp, canReturn);
+        }
+
+        /**
+         * Choose ghost prototype based on current player's state:
+         * - if player can return back to body -> aghost
+         * - else if player is aghost -> ghost
+         * - else -> aghost
+         */
+        private EntityUid SpawnGhost(EntityCoordinates coordinates, IPlayerSession player, bool canReturn)
+        {
+            if (canReturn)
+            {
+                return _entities.SpawnEntity("AdminObserver", coordinates);
+            }
+
+            //check if current player is aghost
+            var playerAttachedEntity = player.AttachedEntity;
+            if (playerAttachedEntity is { Valid: true } playerEntity &&
+                _entities.GetComponent<MetaDataComponent>(playerEntity).EntityPrototype?.ID == "AdminObserver")
+            {
+                EmptyHands(playerAttachedEntity);
+                return _entities.SpawnEntity("MobObserver", coordinates);
+            }
+
+            return _entities.SpawnEntity("AdminObserver", coordinates);
+        }
+
+        private void EmptyHands(EntityUid? playerAttachedEntity)
+        {
+            if (playerAttachedEntity == null)
+                return;
+            var handsSystem = _entities.System<HandsSystem>();
+            var handsComponent = _entities.GetComponent<HandsComponent>(playerAttachedEntity.Value);
+            handsSystem.TryDrop(playerAttachedEntity.Value, checkActionBlocker: false, doDropInteraction: false,
+                handsComp: handsComponent);
         }
     }
 }
