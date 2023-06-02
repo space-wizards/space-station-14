419b7aa90830dffcb8558dc06de179595cc30545
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-06-02 13:59:14 +1000

    Disallow tile placement close to grids
    Probably prevents some finnicky interactions.

--- Content.Shared/Tiles/FloorTileSystem.cs
+++ Content.Shared/Tiles/FloorTileSystem.cs
@@ -57,13 +57,36 @@ public sealed class FloorTileSystem : EntitySystem
         var physicQuery = GetEntityQuery<PhysicsComponent>();
         var transformQuery = GetEntityQuery<TransformComponent>();
 
-        var tilePos = location.ToMapPos(EntityManager, _transform);
+        var map = location.ToMap(EntityManager, _transform);
+
+        // Disallow placement close to grids.
+        // FTLing close is okay but this makes alignment too finnicky.
+        const bool weh = true;
+        var state = (weh, location.EntityId);
+        _mapManager.FindGridsIntersecting(map.MapId, new Box2(map.Position - 1f, map.Position + 1f), ref state,
+            static (EntityUid entityUid, MapGridComponent grid, ref (bool weh, EntityUid EntityId) tuple) =>
+            {
+                if (tuple.EntityId == entityUid)
+                    return true;
+
+                tuple.weh = false;
+                return false;
+            });
+
+        if (!state.weh)
+        {
+            if (_netManager.IsClient && _timing.IsFirstTimePredicted)
+                _popup.PopupEntity(Loc.GetString("invalid-floor-placement"), args.User);
+
+            return;
+        }
+
         var userPos = transformQuery.GetComponent(args.User).Coordinates.ToMapPos(EntityManager, _transform);
-        var dir = userPos - tilePos;
+        var dir = userPos - map.Position;
         var canAccessCenter = false;
         if (dir.LengthSquared > 0.01)
         {
-            var ray = new CollisionRay(tilePos, dir.Normalized, (int) CollisionGroup.Impassable);
+            var ray = new CollisionRay(map.Position, dir.Normalized, (int) CollisionGroup.Impassable);
             var results = _physics.IntersectRay(locationMap.MapId, ray, dir.Length, returnOnFirstHit: true);
             canAccessCenter = !results.Any();
         }
