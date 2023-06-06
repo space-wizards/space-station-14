ed291734d66a8b4380b1757af1a442db1f112b91
John-Doom <98620794+John-Doom@users.noreply.github.com>
2023-06-06 09:26:54 +0200

    Update ElectrocutionSystem.cs
    reset to original

--- Content.Server/Electrocution/ElectrocutionSystem.cs
+++ Content.Server/Electrocution/ElectrocutionSystem.cs
@@ -1,5 +1,4 @@
 using Content.Server.Administration.Logs;
-using Content.Server.AME;
 using Content.Server.NodeContainer;
 using Content.Server.NodeContainer.EntitySystems;
 using Content.Server.NodeContainer.NodeGroups;
@@ -267,7 +266,7 @@ public sealed class ElectrocutionSystem : SharedElectrocutionSystem
         {
             if (id != null &&
                 nodeContainer.TryGetNode<Node>(id, out var tryNode) &&
-                (tryNode.NodeGroupID == NodeGroupID.AMEngine || tryNode.NodeGroup is IBasePowerNet { NetworkNode: { LastCombinedSupply: > 0 } }))
+                tryNode.NodeGroup is IBasePowerNet { NetworkNode: { LastCombinedSupply: > 0 } })
             {
                 return tryNode;
             }
