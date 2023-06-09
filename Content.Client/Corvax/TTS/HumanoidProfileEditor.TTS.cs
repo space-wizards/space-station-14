18b7a1c650f234e2f9b7d48ae8097e84689ef961
mirton <mirton14@gmail.com>
2023-06-09 23:17:23 +0300

    transfer Corvax folder Server Client Shared

---
+++ Content.Client/Corvax/TTS/HumanoidProfileEditor.TTS.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using Content.Client.Corvax.Sponsors;
+using Content.Client.Corvax.TTS;
+using Content.Shared.Corvax.TTS;
+using Content.Shared.Preferences;
+using Robust.Shared.Random;
+
+namespace Content.Client.Preferences.UI;
+
+public sealed partial class HumanoidProfileEditor
+{
+    private TTSManager _ttsMgr = default!;
+    private TTSSystem _ttsSys = default!;
+    private List<TTSVoicePrototype> _voiceList = default!;
+    private readonly List<string> _sampleText = new()
+    {
+        "Съешь же ещё этих мягких французских булок, да выпей чаю.",
+        "Клоун, прекрати разбрасывать банановые кожурки офицерам под ноги!",
+        "Капитан, вы уверены что хотите назначить клоуна на должность главы персонала?",
+        "Эс Бэ! Тут человек в сером костюме, с тулбоксом и в маске! Помогите!!"
+    };
+
+    private void InitializeVoice()
+    {
+        _ttsMgr = IoCManager.Resolve<TTSManager>();
+        _ttsSys = _entMan.System<TTSSystem>();
+        _voiceList = _prototypeManager
+            .EnumeratePrototypes<TTSVoicePrototype>()
+            .Where(o => o.RoundStart)
+            .OrderBy(o => Loc.GetString(o.Name))
+            .ToList();
+
+        _voiceButton.OnItemSelected += args =>
+        {
+            _voiceButton.SelectId(args.Id);
+            SetVoice(_voiceList[args.Id].ID);
+        };
+            
+        _voicePlayButton.OnPressed += _ => { PlayTTS(); };
+    }
+
+    private void UpdateTTSVoicesControls()
+    {
+        if (Profile is null)
+            return;
+
+        _voiceButton.Clear();
+
+        var firstVoiceChoiceId = 1;
+        for (var i = 0; i < _voiceList.Count; i++)
+        {
+            var voice = _voiceList[i];
+            if (!HumanoidCharacterProfile.CanHaveVoice(voice, Profile.Sex))
+                continue;
+                
+            var name = Loc.GetString(voice.Name);
+            _voiceButton.AddItem(name, i);
+
+            if (firstVoiceChoiceId == 1)
+                firstVoiceChoiceId = i;
+
+            if (voice.SponsorOnly &&
+                IoCManager.Resolve<SponsorsManager>().TryGetInfo(out var sponsor) &&
+                !sponsor.AllowedMarkings.Contains(voice.ID))
+            {
+                _voiceButton.SetItemDisabled(_voiceButton.GetIdx(i), true);
+            }
+        }
+
+        var voiceChoiceId = _voiceList.FindIndex(x => x.ID == Profile.Voice);
+        if (!_voiceButton.TrySelectId(voiceChoiceId) &&
+            _voiceButton.TrySelectId(firstVoiceChoiceId))
+        {
+            SetVoice(_voiceList[firstVoiceChoiceId].ID);
+        }
+    }
+
+    private void PlayTTS()
+    {
+        if (_previewDummy is null || Profile is null)
+            return;
+
+        _ttsSys.StopAllStreams();
+        _ttsMgr.RequestTTS(_previewDummy.Value, _random.Pick(_sampleText), Profile.Voice);
+    }
+}
