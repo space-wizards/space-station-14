433983c2013eb5bc75b2e8ba32b51ac46b06e6fc
mirton <mirton14@gmail.com>
2023-06-10 02:36:16 +0300

    transfer TTS VoiceMask server

--- Content.Server/VoiceMask/VoiceMaskSystem.cs
+++ Content.Server/VoiceMask/VoiceMaskSystem.cs
@@ -26,6 +26,7 @@ public sealed partial class VoiceMaskSystem : EntitySystem
         SubscribeLocalEvent<VoiceMaskerComponent, GotUnequippedEvent>(OnUnequip);
         SubscribeLocalEvent<VoiceMaskSetNameEvent>(OnSetName);
         // SubscribeLocalEvent<VoiceMaskerComponent, GetVerbsEvent<AlternativeVerb>>(GetVerbs);
+        InitializeTTS(); // Corvax-TTS
     }
 
     private void OnSetName(VoiceMaskSetNameEvent ev)
@@ -86,7 +87,7 @@ public sealed partial class VoiceMaskSystem : EntitySystem
             return;
         }
 
-        _uiSystem.GetUiOrNull(owner, VoiceMaskUIKey.Key)?.SetState(new VoiceMaskBuiState(component.VoiceName));
+        _uiSystem.GetUiOrNull(owner, VoiceMaskUIKey.Key)?.SetState(new VoiceMaskBuiState(component.VoiceName, component.VoiceId)); // Corvax-TTS
     }
 }
 
