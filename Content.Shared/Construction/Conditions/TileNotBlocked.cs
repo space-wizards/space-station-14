69c35101a967ac627cdb3a89ce2a13fefc6a453a
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-06-09 22:33:35 +0300

    Revert "allow grilles to be placed on lattice"
    This reverts commit 5c536686cdcd85d49681f934d9abaf69076e2bc2.

--- Content.Shared/Construction/Conditions/TileNotBlocked.cs
+++ Content.Shared/Construction/Conditions/TileNotBlocked.cs
@@ -9,22 +9,16 @@ namespace Content.Shared.Construction.Conditions
     public sealed class TileNotBlocked : IConstructionCondition
     {
         [DataField("filterMobs")] private bool _filterMobs = false;
-        [DataField("failIfNotSturdy")] private bool _failIfNotSturdy = true;
+        [DataField("failIfSpace")] private bool _failIfSpace = true;
 
         public bool Condition(EntityUid user, EntityCoordinates location, Direction direction)
         {
             var tileRef = location.GetTileRef();
 
-            if (tileRef == null)
-                return false;
+            if (tileRef == null || tileRef.Value.IsSpace())
+                return !_failIfSpace;
 
-            if (_failIfNotSturdy && !tileRef.Value.GetContentTileDefinition().Sturdy)
-                return false;
-
-            if (tileRef.Value.IsBlockedTurf(_filterMobs))
-                return false;
-
-            return true;
+            return !tileRef.Value.IsBlockedTurf(_filterMobs);
         }
 
         public ConstructionGuideEntry GenerateGuideEntry()
