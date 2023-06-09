df1533585e696a49f9f8d4adb1376e859bb9cc3a
mirton <mirton14@gmail.com>
2023-06-10 01:25:13 +0300

    transfer ICharacterProfile Shared

--- Content.Shared/Preferences/ICharacterProfile.cs
+++ Content.Shared/Preferences/ICharacterProfile.cs
@@ -13,6 +13,6 @@ namespace Content.Shared.Preferences
         /// <summary>
         ///     Makes this profile valid so there's no bad data like negative ages.
         /// </summary>
-        void EnsureValid();
+        void EnsureValid(string[] sponsorMarkings); // Corvax-Sponsors: Integrated filtering for sponsor markings
     }
 }
