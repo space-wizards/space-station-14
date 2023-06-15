1a1a5d9c997674b6163640b20a1e03d6622d598d
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2023-06-15 22:34:52 +1000

    Update Content.Server/Nutrition/Components/BadFoodComponent.cs

--- Content.Server/Nutrition/Components/BadFoodComponent.cs
+++ Content.Server/Nutrition/Components/BadFoodComponent.cs
@@ -3,7 +3,7 @@ using Content.Server.Nutrition.EntitySystems;
 namespace Content.Server.Nutrition.Components;
 
 /// <summary>
-/// This component prevents ai mobs like mice from wanting to eat something that is edible but is not exactly food.
+/// This component prevents NPC mobs like mice from wanting to eat something that is edible but is not exactly food.
 /// Including but not limited to: uranium, death pills, insulation
 /// </summary>
 [RegisterComponent, Access(typeof(FoodSystem))]
