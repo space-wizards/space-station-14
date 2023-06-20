4cb550c681f5e19049de98ba2ac2e36646e39fc5
ElectroJr <leonsfriedrich@gmail.com>
2023-06-19 20:00:50 -0400

    More tests

--- Content.IntegrationTests/Tests/Minds/GhostReconnectTests.cs
+++ Content.IntegrationTests/Tests/Minds/GhostReconnectTests.cs
@@ -1,84 +1,278 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Content.Server.Ghost.Components;
 using Content.Server.Mind;
-using Content.Server.Mind.Components;
 using Content.Server.Players;
 using NUnit.Framework;
 using Robust.Server.GameObjects;
 using Robust.Server.Player;
+using Robust.Shared.Enums;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Map;
 using Robust.Shared.Network;
 
 namespace Content.IntegrationTests.Tests.Minds;
 
 [TestFixture]
-public sealed class GhostTests
+public sealed class GhostReconnectTests
 {
+    private const string Prototypes = @"
+- type: entity
+  id: GhostReconnectTestEntity
+  components:
+  - type: MindContainer
+";
+
+    // This test will do the following:
+    // - attach a player to a ghost (not visiting)
+    // - disconnect
+    // - reconnect
+    // - spawned in as a new entity
     [Test]
-    public async Task TestPlayerCanGhostThenDisconnectAndReconnect()
+    public async Task TestGhostsCanReconnect()
     {
-        // Client is needed to spawn session
         await using var pairTracker = await PoolManager.GetServerClient();
-        var server = pairTracker.Pair.Server;
-        var client = pairTracker.Pair.Client;
+        var pair = pairTracker.Pair;
 
-        var netManager = client.ResolveDependency<IClientNetManager>();
+        var entMan = pair.Server.ResolveDependency<IEntityManager>();
+        await PoolManager.RunTicksSync(pair, 5);
+        var mind = GetMind(pair);
 
-        var entMan = server.ResolveDependency<IServerEntityManager>();
-        var playerMan = server.ResolveDependency<IPlayerManager>();
+        var ghost = await BecomeGhost(pair);
+        await DisconnectReconnect(pair);
 
-        EntityUid entity = default!;
+        // Player in control of a NEW entity
+        var newMind = GetMind(pair);
+        Assert.That(newMind != mind);
+        Assert.That(entMan.Deleted(ghost));
+        Assert.Null(newMind.VisitingEntity);
+
+        await pairTracker.CleanReturnAsync();
+    }
+
+    // This test will do the following:
+    // - disconnect a player
+    // - delete their original entity
+    // - reconnect
+    // - assert that they spawned in as a new entity
+    [Test]
+    public async Task TestDeletedCanReconnect()
+    {
+        await using var pairTracker = await PoolManager.GetServerClient();
+        var pair = pairTracker.Pair;
+
+        var entMan = pair.Server.ResolveDependency<IEntityManager>();
+        await PoolManager.RunTicksSync(pair, 5);
+        var mind = GetMind(pair);
+
+        var playerMan = pair.Server.ResolveDependency<IPlayerManager>();
+        var player = playerMan.ServerSessions.Single();
+        var name = player.Name;
+        var user = player.UserId;
+        var entity = mind.OwnedEntity.Value;
+
+        // Player is not a ghost
+        Assert.That(!entMan.HasComponent<GhostComponent>(mind.CurrentEntity));
+
+        // Disconnect
+        await Disconnect(pair);
+
+        // Delete entity
+        Assert.That(entMan.EntityExists(entity));
+        await pair.Server.WaitPost(() => entMan.DeleteEntity(entity));
+        Assert.That(entMan.Deleted(entity));
+        Assert.IsNull(mind.OwnedEntity);
+
+        // Reconnect
+        await Connect(pair, name);
+        player = playerMan.ServerSessions.Single();
+        Assert.That(user == player.UserId);
+
+        // Player is now a new entity
+        var newMind = GetMind(pair);
+        Assert.That(newMind != mind);
+        Assert.Null(mind.UserId);
+        Assert.Null(mind.CurrentEntity);
+        Assert.NotNull(newMind.OwnedEntity);
+        Assert.That(entMan.EntityExists(newMind.OwnedEntity));
+
+        await pairTracker.CleanReturnAsync();
+    }
+
+    // This test will do the following:
+    // - visit a ghost
+    // - disconnect
+    // - reconnect
+    // - assert that they return to their original entity
+    [Test]
+    public async Task TestVisitingGhostReconnect()
+    {
+        await using var pairTracker = await PoolManager.GetServerClient();
+        var pair = pairTracker.Pair;
+
+        var entMan = pair.Server.ResolveDependency<IEntityManager>();
+        await PoolManager.RunTicksSync(pair, 5);
+        var mind = GetMind(pair);
+
+        var original = mind.CurrentEntity;
+        var ghost = await BecomeGhost(pair, visit: true);
+        await DisconnectReconnect(pair);
+
+        // Player now controls their original mob, mind was preserved
+        Assert.That(mind == GetMind(pair));
+        Assert.That(mind.CurrentEntity == original);
+        Assert.That(!entMan.Deleted(original));
+        Assert.That(entMan.Deleted(ghost));
+
+        await pairTracker.CleanReturnAsync();
+    }
+
+    // This test will do the following:
+    // - visit a normal (non-ghost) entity,
+    // - disconnect
+    // - reconnect
+    // - assert that they return to the visited entity.
+    [Test]
+    public async Task TestVisitingReconnect()
+    {
+        await using var pairTracker = await PoolManager.GetServerClient(new PoolSettings{ ExtraPrototypes = Prototypes });
+        var pair = pairTracker.Pair;
+
+        var entMan = pair.Server.ResolveDependency<IEntityManager>();
+        var mindSys = entMan.System<MindSystem>();
+        await PoolManager.RunTicksSync(pair, 5);
+        var mind = GetMind(pair);
+
+        // Make player visit a new mob
+        var original = mind.CurrentEntity;
+        EntityUid visiting = default;
+        await pair.Server.WaitAssertion(() =>
+        {
+            visiting = entMan.SpawnEntity("GhostReconnectTestEntity", MapCoordinates.Nullspace);
+            mindSys.Visit(mind, visiting);
+        });
+        await PoolManager.RunTicksSync(pair, 5);
+
+        await DisconnectReconnect(pair);
+
+        // Player is back in control of the visited mob, mind was preserved
+        Assert.That(mind == GetMind(pair));
+        Assert.That(!entMan.Deleted(original));
+        Assert.That(!entMan.Deleted(visiting));
+        Assert.That(mind.CurrentEntity == visiting);
+        Assert.That(mind.CurrentEntity == visiting);
+
+        await pairTracker.CleanReturnAsync();
+    }
+
+    public async Task<EntityUid> BecomeGhost(Pair pair, bool visit = false)
+    {
+        var entMan = pair.Server.ResolveDependency<IServerEntityManager>();
+        var playerMan = pair.Server.ResolveDependency<IPlayerManager>();
+        var mindSys = entMan.System<MindSystem>();
+        EntityUid ghostUid = default;
         Mind mind = default!;
-        IPlayerSession player = playerMan.ServerSessions.Single();
 
-        await PoolManager.RunTicksSync(pairTracker.Pair, 5);
-
-        await server.WaitAssertion(() =>
+        var player = playerMan.ServerSessions.Single();
+        await pair.Server.WaitAssertion(() =>
         {
-            Assert.That(player.AttachedEntity != null);
-            entity = player.AttachedEntity.Value;
-            Assert.That(entMan.TryGetComponent(entity, out MindContainerComponent mindContainerComponent));
-            Assert.That(mindContainerComponent.HasMind);
-            mind = mindContainerComponent.Mind;
-            entMan.DeleteEntity(entity);
-        });
+            var oldUid = player.AttachedEntity;
+            ghostUid = entMan.SpawnEntity("MobObserver", MapCoordinates.Nullspace);
+            mind = mindSys.GetMind(player.UserId);
+            Assert.NotNull(mind);
 
-        await PoolManager.RunTicksSync(pairTracker.Pair, 5);
+            if (visit)
+            {
+                mindSys.Visit(mind, ghostUid);
+                return;
+            }
 
-        EntityUid mob = default!;
-
-        await server.WaitAssertion(() =>
-        {
-            Assert.That(mind.OwnedEntity != null);
-            Assert.That(entity != mind.OwnedEntity);
-            mob = mind.OwnedEntity.Value;
+            mindSys.TransferTo(mind, ghostUid);
+            if (oldUid != null)
+                entMan.DeleteEntity(oldUid.Value);
 
         });
 
-        await client.WaitAssertion(() =>
+        await PoolManager.RunTicksSync(pair, 5);
+        Assert.That(entMan.HasComponent<GhostComponent>(ghostUid));
+        Assert.That(player.AttachedEntity == ghostUid);
+        Assert.That(mind.CurrentEntity == ghostUid);
+
+        if (!visit)
+            Assert.Null(mind.VisitingEntity);
+
+        return ghostUid;
+    }
+
+    /// <summary>
+    /// Check that the player exists and the mind has been properly set up.
+    /// </summary>
+    public Mind GetMind(Pair pair)
+    {
+        var playerMan = pair.Server.ResolveDependency<IPlayerManager>();
+        var entMan = pair.Server.ResolveDependency<IEntityManager>();
+
+        var player = playerMan.ServerSessions.SingleOrDefault();
+        Assert.NotNull(player);
+
+        var mind = player.ContentData()!.Mind;
+        Assert.NotNull(mind);
+        Assert.That(player.AttachedEntity == mind.CurrentEntity);
+        Assert.That(entMan.EntityExists(mind.OwnedEntity));
+        Assert.That(entMan.EntityExists(mind.CurrentEntity));
+
+        return mind;
+    }
+
+    public async Task Disconnect(Pair pair)
+    {
+        var netManager = pair.Client.ResolveDependency<IClientNetManager>();
+        var playerMan = pair.Server.ResolveDependency<IPlayerManager>();
+        var player = playerMan.ServerSessions.Single();
+        var mind = player.ContentData()!.Mind;
+
+        await pair.Client.WaitAssertion(() =>
         {
             netManager.ClientDisconnect("Disconnect command used.");
         });
+        await PoolManager.RunTicksSync(pair, 5);
 
-        await PoolManager.RunTicksSync(pairTracker.Pair, 5);
+        Assert.That(player.Status == SessionStatus.Disconnected);
+        Assert.NotNull(mind.UserId);
+        Assert.Null(mind.Session);
+    }
 
-        await Task.WhenAll(client.WaitIdleAsync(), server.WaitIdleAsync());
-        client.SetConnectTarget(server);
-        await client.WaitPost(() => netManager.ClientConnect(null!, 0, null!));
-        await PoolManager.RunTicksSync(pairTracker.Pair, 10);
+    public async Task Connect(Pair pair, string username)
+    {
+        var netManager = pair.Client.ResolveDependency<IClientNetManager>();
+        var playerMan = pair.Server.ResolveDependency<IPlayerManager>();
+        Assert.That(!playerMan.ServerSessions.Any());
 
-        await server.WaitAssertion(() =>
-        {
-            // New ghost is created to attach old mind to.
-            // Make sure that session is set correctly
-            // Mind still exists
-            var m = player.ContentData()?.Mind;
-            Assert.That(m, Is.Not.EqualTo(null));
+        await Task.WhenAll(pair.Client.WaitIdleAsync(), pair.Client.WaitIdleAsync());
+        pair.Client.SetConnectTarget(pair.Server);
+        await pair.Client.WaitPost(() => netManager.ClientConnect(null!, 0, username));
+        await PoolManager.RunTicksSync(pair, 5);
 
-            Assert.That(m!.OwnedEntity, Is.Not.EqualTo(mob));
-            Assert.That(m, Is.EqualTo(mind));
-        });
+        var player = playerMan.ServerSessions.Single();
+        Assert.That(player.Status == SessionStatus.InGame);
+    }
 
-        await pairTracker.CleanReturnAsync();
+    public async Task<IPlayerSession> DisconnectReconnect(Pair pair)
+    {
+        var playerMan = pair.Server.ResolveDependency<IPlayerManager>();
+        var player = playerMan.ServerSessions.Single();
+        var mind = player.ContentData()!.Mind;
+        var name = player.Name;
+        var id = player.UserId;
+
+        await Disconnect(pair);
+        await Connect(pair, name);
+
+        // Session has changed
+        var newSession = playerMan.ServerSessions.Single();
+        Assert.That(newSession != player);
+        Assert.That(newSession.UserId == id);
+
+        return newSession;
     }
 }
