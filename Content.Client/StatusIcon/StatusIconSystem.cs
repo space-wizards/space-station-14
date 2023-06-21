2a3f63a934470f7e9beb9dd4e4c61c9f8af312c3
EmoGarbage404 <retron404@gmail.com>
2023-06-21 11:02:08 -0400

    Status Icons

---
+++ Content.Client/StatusIcon/StatusIconSystem.cs
@@ -0,0 +1,31 @@
+﻿using Content.Shared.StatusIcon;
+using Content.Shared.StatusIcon.Components;
+using Robust.Client.Graphics;
+
+namespace Content.Client.StatusIcon;
+
+/// <summary>
+/// This handles rendering gathering and rendering icons on entities.
+/// </summary>
+public sealed class StatusIconSystem : EntitySystem
+{
+    [Dependency] private readonly IOverlayManager _overlay = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        if (!_overlay.HasOverlay<StatusIconOverlay>())
+            _overlay.AddOverlay(new StatusIconOverlay());
+    }
+
+    public List<StatusIconData> GetStatusIcons(EntityUid uid)
+    {
+        if (!Exists(uid) || Terminating(uid))
+            return new();
+
+        var ev = new GetStatusIconsEvent(new());
+        RaiseLocalEvent(uid, ref ev);
+        return ev.StatusIcons;
+    }
+}
+
---
+++ Content.Shared/StatusIcon/Components/StatusIconComponent.cs
@@ -0,0 +1,26 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.StatusIcon.Components;
+
+/// <summary>
+/// This is used for noting if an entity is able to
+/// have StatusIcons displayed on them and inherent icons. (debug purposes)
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed class StatusIconComponent : Component
+{
+    /// <summary>
+    /// Optional bounds for where the icons are laid out.
+    /// If null, the sprite bounds will be used.
+    /// </summary>
+    [DataField("bounds")]
+    public Box2? Bounds;
+}
+
+/// <summary>
+/// Event raised directed on an entity CLIENT-SIDE ONLY
+/// in order to get what status icons an entity has.
+/// </summary>
+/// <param name="StatusIcons"></param>
+[ByRefEvent]
+public record struct GetStatusIconsEvent(List<StatusIconData> StatusIcons);
