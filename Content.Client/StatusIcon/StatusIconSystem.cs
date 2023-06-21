3a90ce686008ee9708196aa99a0e609ccc0cc3f9
EmoGarbage404 <retron404@gmail.com>
2023-06-21 11:31:03 -0400

    Cvars

--- Content.Client/StatusIcon/StatusIconSystem.cs
+++ Content.Client/StatusIcon/StatusIconSystem.cs
@@ -1,6 +1,9 @@
-﻿using Content.Shared.StatusIcon;
+﻿using Content.Shared.CCVar;
+using Content.Shared.StatusIcon;
 using Content.Shared.StatusIcon.Components;
 using Robust.Client.Graphics;
+using Robust.Shared.Configuration;
+using Robust.Shared.Prototypes;
 
 namespace Content.Client.StatusIcon;
 
@@ -9,14 +12,45 @@ namespace Content.Client.StatusIcon;
 /// </summary>
 public sealed class StatusIconSystem : EntitySystem
 {
+    [Dependency] private readonly IConfigurationManager _configuration = default!;
     [Dependency] private readonly IOverlayManager _overlay = default!;
+
+    private bool _globalEnabled;
+    private bool _localEnabled;
 
     /// <inheritdoc/>
     public override void Initialize()
     {
-        if (!_overlay.HasOverlay<StatusIconOverlay>())
-            _overlay.AddOverlay(new StatusIconOverlay());
+        _configuration.OnValueChanged(CCVars.LocalStatusIconsEnabled, OnLocalStatusIconChanged, true);
+        _configuration.OnValueChanged(CCVars.GlobalStatusIconsEnabled, OnGlobalStatusIconChanged, true);
     }
+    private void OnLocalStatusIconChanged(bool obj)
+    {
+        _localEnabled = obj;
+        UpdateOverlayVisible();
+    }
+
+    private void OnGlobalStatusIconChanged(bool obj)
+    {
+        _globalEnabled = obj;
+        UpdateOverlayVisible();
+    }
+
+    private void UpdateOverlayVisible()
+    {
+        if (_globalEnabled && _localEnabled)
+        {
+            if (!_overlay.HasOverlay<StatusIconOverlay>())
+                _overlay.AddOverlay(new StatusIconOverlay());
+        }
+        else
+        {
+            if (_overlay.HasOverlay<StatusIconOverlay>())
+                _overlay.RemoveOverlay<StatusIconOverlay>();
+        }
+    }
+
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
 
     public List<StatusIconData> GetStatusIcons(EntityUid uid)
     {
@@ -25,6 +59,8 @@ public sealed class StatusIconSystem : EntitySystem
 
         var ev = new GetStatusIconsEvent(new());
         RaiseLocalEvent(uid, ref ev);
+
+        ev.StatusIcons.Add(_prototype.Index<StatusIconPrototype>("DebugStatus2"));
         return ev.StatusIcons;
     }
 }
