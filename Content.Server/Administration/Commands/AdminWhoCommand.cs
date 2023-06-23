295430ef1edfa25262e3d9550117df2233811c6d
mirton <mirton14@gmail.com>
2023-06-23 13:19:09 +0300

    update command adminwho now the command is available to everyone

--- Content.Server/Administration/Commands/AdminWhoCommand.cs
+++ Content.Server/Administration/Commands/AdminWhoCommand.cs
@@ -8,7 +8,7 @@ using Robust.Shared.Utility;
 
 namespace Content.Server.Administration.Commands;
 
-[AdminCommand(AdminFlags.Admin)]
+[AnyCommand]
 public sealed class AdminWhoCommand : IConsoleCommand
 {
     public string Command => "adminwho";
