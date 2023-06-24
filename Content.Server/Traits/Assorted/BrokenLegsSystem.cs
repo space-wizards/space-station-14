bbfdfef634a3dc3fac27dd0eaf84b72563e88da0
Ray <vigersray@gmail.com>
2023-06-24 05:19:35 +0300

    maybe that

---
+++ Content.Server/Traits/Assorted/BrokenLegsSystem.cs
@@ -0,0 +1,26 @@
+﻿using Content.Server.Buckle.Systems;
+using Content.Shared.Movement.Components;
+using Content.Shared.Movement.Systems;
+using Content.Shared.Traits.Assorted;
+
+namespace Content.Server.Traits.Assorted;
+
+public sealed class BrokenLegsSystem : SharedBrokenLegsSystem
+{
+    [Dependency] private readonly BuckleSystem _buckleSystem = default!;
+    [Dependency] private readonly MovementSpeedModifierSystem _movementSpeedModifierSystem = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<BrokenLegsComponent, ComponentStartup>(SetupBrokenLegs);
+    }
+
+    private void SetupBrokenLegs(EntityUid uid, BrokenLegsComponent component, ComponentStartup args)
+    {
+        var movementSpeed = EnsureComp<MovementSpeedModifierComponent>(uid);
+        // TODO: Is there a better way to break legs? I don't know him, so for now, that's the way it is.
+        _movementSpeedModifierSystem?.ChangeBaseSpeed(uid, 0, 0, 20, movementSpeed);
+        var carriage = Spawn("Carriage", Transform(uid).Coordinates);
+        _buckleSystem.TryBuckle(uid, uid, carriage);
+    }
+}
--- Content.Shared/Traits/Assorted/BrokenLegsSystem.cs
+++ Content.Shared/Traits/Assorted/BrokenLegsSystem.cs
@@ -1,22 +1,5 @@
-﻿using Content.Shared.Movement.Components;
-using Content.Shared.Movement.Systems;
+﻿namespace Content.Shared.Traits.Assorted;
 
-namespace Content.Shared.Traits.Assorted;
-
-public sealed class BrokenLegsSystem : EntitySystem
+public abstract class SharedBrokenLegsSystem : EntitySystem
 {
-    [Dependency] private readonly MovementSpeedModifierSystem _movementSpeedModifierSystem = default!;
-
-    public override void Initialize()
-    {
-        SubscribeLocalEvent<BrokenLegsComponent, ComponentStartup>(SetupBrokenLegs);
-    }
-
-    private void SetupBrokenLegs(EntityUid uid, BrokenLegsComponent component, ComponentStartup args)
-    {
-        var movementSpeed = EnsureComp<MovementSpeedModifierComponent>(uid);
-        // TODO: Is there a better way to break legs? I don't know him, so for now, that's the way it is.
-        _movementSpeedModifierSystem?.ChangeBaseSpeed(uid, 0, 0, 20, movementSpeed);
-        Spawn("Carriage", Transform(uid).Coordinates);
-    }
 }
