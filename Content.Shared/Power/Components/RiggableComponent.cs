29ee24ea3381b0738fe0d461e5387af2289c8d1b
Sailor <109166122+Equivocateur@users.noreply.github.com>
2023-06-25 17:35:56 +0300

    Pull suggestion into the commit
    Co-authored-by: keronshb <54602815+keronshb@users.noreply.github.com>

--- Content.Shared/Power/Components/RiggableComponent.cs
+++ Content.Shared/Power/Components/RiggableComponent.cs
@@ -3,6 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Power.Components;
 
 [RegisterComponent, NetworkedComponent]
+[Access(typeof(RiggableSystem))]
 public sealed class RiggableComponent : Component
 {
     public const string SolutionName = "battery";
