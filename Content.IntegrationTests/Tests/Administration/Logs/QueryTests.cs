a633dc2a4082186a1ee58c7e8095a6de852c8274
TemporalOroboros <TemporalOroboros@gmail.com>
2023-07-01 07:33:03 -0700

    Fix QueryTests warnings

--- Content.IntegrationTests/Tests/Administration/Logs/QueryTests.cs
+++ Content.IntegrationTests/Tests/Administration/Logs/QueryTests.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Content.Server.Administration.Logs;
 using Content.Server.GameTicking;
-using Content.Shared.CCVar;
 using Content.Shared.Database;
 using NUnit.Framework;
 using Robust.Server.Player;
@@ -44,9 +43,9 @@ public sealed class QueryTests
         {
             Round = sGamerTicker.RoundId,
             Search = guid.ToString(),
-            Types = new HashSet<LogType> {LogType.Unknown},
+            Types = new HashSet<LogType> { LogType.Unknown },
             After = date,
-            AnyPlayers = new[] {player.UserId.UserId}
+            AnyPlayers = new[] { player.UserId.UserId }
         };
 
         await PoolManager.WaitUntil(server, async () =>
