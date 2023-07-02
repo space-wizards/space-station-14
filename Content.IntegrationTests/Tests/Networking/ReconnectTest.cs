eca0d69488e5488fc6ac6aab68abb15adc80e239
TemporalOroboros <TemporalOroboros@gmail.com>
2023-07-02 13:31:29 -0700

    Fix ReconnectTest warnings

--- Content.IntegrationTests/Tests/Networking/ReconnectTest.cs
+++ Content.IntegrationTests/Tests/Networking/ReconnectTest.cs
@@ -1,7 +1,6 @@
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Robust.Client.Console;
-using Robust.Shared.IoC;
 using Robust.Shared.Network;
 
 namespace Content.IntegrationTests.Tests.Networking
