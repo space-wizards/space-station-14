71340151d43b82d72347ea16dd0fcae2a7331bc1
LankLTE <twlowe06@gmail.com>
2023-07-01 19:27:08 -0700

    Change guidebook descriptions.

--- Content.Server/Chemistry/ReagentEffects/CureZombieInfection.cs
+++ Content.Server/Chemistry/ReagentEffects/CureZombieInfection.cs
@@ -10,7 +10,7 @@ namespace Content.Server.Chemistry.ReagentEffects;
 public sealed class CureZombieInfection : ReagentEffect
 {
     protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-        => Loc.GetString("reagent-effect-guidebook-paralyze"); 
+        => Loc.GetString("reagent-effect-guidebook-cure-zombie-infection"); 
 
     // Removes the Zombie Infection Components
     public override void Effect(ReagentEffectArgs args)
