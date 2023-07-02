782df2ba5215c680e29da1bcc88f4e364fbc6340
TemporalOroboros <TemporalOroboros@gmail.com>
2023-07-02 04:15:49 -0700

    Fix RemoveEncryptionKeysTest warnings

--- Content.IntegrationTests/Tests/EncryptionKeys/RemoveEncryptionKeys.cs
+++ Content.IntegrationTests/Tests/EncryptionKeys/RemoveEncryptionKeys.cs
@@ -15,26 +15,35 @@ public sealed class RemoveEncryptionKeys : InteractionTest
         await SpawnTarget("ClothingHeadsetGrey");
         var comp = Comp<EncryptionKeyHolderComponent>();
 
-        Assert.That(comp.KeyContainer.ContainedEntities.Count, Is.EqualTo(1));
-        Assert.That(comp.DefaultChannel, Is.EqualTo("Common"));
-        Assert.That(comp.Channels.Count, Is.EqualTo(1));
-        Assert.That(comp.Channels.First(), Is.EqualTo("Common"));
+        Assert.Multiple(() =>
+        {
+            Assert.That(comp.KeyContainer.ContainedEntities, Has.Count.EqualTo(1));
+            Assert.That(comp.DefaultChannel, Is.EqualTo("Common"));
+            Assert.That(comp.Channels, Has.Count.EqualTo(1));
+            Assert.That(comp.Channels.First(), Is.EqualTo("Common"));
+        });
 
         // Remove the key
         await Interact(Screw);
-        Assert.That(comp.KeyContainer.ContainedEntities.Count, Is.EqualTo(0));
-        Assert.IsNull(comp.DefaultChannel);
-        Assert.That(comp.Channels.Count, Is.EqualTo(0));
+        Assert.Multiple(() =>
+        {
+            Assert.That(comp.KeyContainer.ContainedEntities, Has.Count.EqualTo(0));
+            Assert.That(comp.DefaultChannel, Is.Null);
+            Assert.That(comp.Channels, Has.Count.EqualTo(0));
+        });
 
         // Checkl that the key was ejected and not just deleted or something.
         await AssertEntityLookup(("EncryptionKeyCommon", 1));
 
         // Re-insert a key.
         await Interact("EncryptionKeyCentCom");
-        Assert.That(comp.KeyContainer.ContainedEntities.Count, Is.EqualTo(1));
-        Assert.That(comp.DefaultChannel, Is.EqualTo("CentCom"));
-        Assert.That(comp.Channels.Count, Is.EqualTo(1));
-        Assert.That(comp.Channels.First(), Is.EqualTo("CentCom"));
+        Assert.Multiple(() =>
+        {
+            Assert.That(comp.KeyContainer.ContainedEntities, Has.Count.EqualTo(1));
+            Assert.That(comp.DefaultChannel, Is.EqualTo("CentCom"));
+            Assert.That(comp.Channels, Has.Count.EqualTo(1));
+            Assert.That(comp.Channels.First(), Is.EqualTo("CentCom"));
+        });
     }
 
     [Test]
@@ -44,40 +53,58 @@ public sealed class RemoveEncryptionKeys : InteractionTest
         var comp = Comp<EncryptionKeyHolderComponent>();
         var panel = Comp<WiresPanelComponent>();
 
-        Assert.That(comp.KeyContainer.ContainedEntities.Count, Is.GreaterThan(0));
-        Assert.That(comp.Channels.Count, Is.GreaterThan(0));
-        Assert.That(panel.Open, Is.False);
+        Assert.Multiple(() =>
+        {
+            Assert.That(comp.KeyContainer.ContainedEntities, Has.Count.GreaterThan(0));
+            Assert.That(comp.Channels, Has.Count.GreaterThan(0));
+            Assert.That(panel.Open, Is.False);
+        });
 
         // cannot remove keys without opening panel
         await Interact(Pry);
-        Assert.That(comp.KeyContainer.ContainedEntities.Count, Is.GreaterThan(0));
-        Assert.That(comp.Channels.Count, Is.GreaterThan(0));
-        Assert.That(panel.Open, Is.False);
+        Assert.Multiple(() =>
+        {
+            Assert.That(comp.KeyContainer.ContainedEntities, Has.Count.GreaterThan(0));
+            Assert.That(comp.Channels, Has.Count.GreaterThan(0));
+            Assert.That(panel.Open, Is.False);
+        });
 
         // Open panel
         await Interact(Screw);
-        Assert.That(panel.Open, Is.True);
+        Assert.Multiple(() =>
+        {
+            Assert.That(panel.Open, Is.True);
 
-        // Keys are still here
-        Assert.That(comp.KeyContainer.ContainedEntities.Count, Is.GreaterThan(0));
-        Assert.That(comp.Channels.Count, Is.GreaterThan(0));
+            // Keys are still here
+            Assert.That(comp.KeyContainer.ContainedEntities, Has.Count.GreaterThan(0));
+            Assert.That(comp.Channels, Has.Count.GreaterThan(0));
+        });
 
         // Now remove the keys
         await Interact(Pry);
-        Assert.That(comp.KeyContainer.ContainedEntities.Count, Is.EqualTo(0));
-        Assert.That(comp.Channels.Count, Is.EqualTo(0));
+        Assert.Multiple(() =>
+        {
+            Assert.That(comp.KeyContainer.ContainedEntities, Has.Count.EqualTo(0));
+            Assert.That(comp.Channels, Has.Count.EqualTo(0));
+        });
 
         // Reinsert a key
         await Interact("EncryptionKeyCentCom");
-        Assert.That(comp.KeyContainer.ContainedEntities.Count, Is.EqualTo(1));
-        Assert.That(comp.DefaultChannel, Is.EqualTo("CentCom"));
-        Assert.That(comp.Channels.Count, Is.EqualTo(1));
-        Assert.That(comp.Channels.First(), Is.EqualTo("CentCom"));
+        Assert.Multiple(() =>
+        {
+            Assert.That(comp.KeyContainer.ContainedEntities, Has.Count.EqualTo(1));
+            Assert.That(comp.DefaultChannel, Is.EqualTo("CentCom"));
+            Assert.That(comp.Channels, Has.Count.EqualTo(1));
+            Assert.That(comp.Channels.First(), Is.EqualTo("CentCom"));
+        });
 
         // Remove it again
         await Interact(Pry);
-        Assert.That(comp.KeyContainer.ContainedEntities.Count, Is.EqualTo(0));
-        Assert.That(comp.Channels.Count, Is.EqualTo(0));
+        Assert.Multiple(() =>
+        {
+            Assert.That(comp.KeyContainer.ContainedEntities, Has.Count.EqualTo(0));
+            Assert.That(comp.Channels, Has.Count.EqualTo(0));
+        });
 
         // Prying again will start deconstructing the machine.
         AssertPrototype("TelecomServerFilled");
@@ -85,4 +112,3 @@ public sealed class RemoveEncryptionKeys : InteractionTest
         AssertPrototype("MachineFrame");
     }
 }
-
