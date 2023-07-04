4c175154068e1345e4b7bc091e8d74b3849730fd
Саша Рудик <sasha13rudi@gmail.com>
2023-07-04 18:20:01 +0300

    Fix

--- Content.Client/Medical/CrewMonitoring/CrewMonitoringWindow.xaml.cs
+++ Content.Client/Medical/CrewMonitoring/CrewMonitoringWindow.xaml.cs
@@ -9,7 +9,6 @@ using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Map;
 using Robust.Shared.Timing;
-using TerraFX.Interop.Windows;
 using static Robust.Client.UserInterface.Controls.BoxContainer;
 
 namespace Content.Client.Medical.CrewMonitoring
