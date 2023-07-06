51e869a3ddb069ed58863dddd145877035e46f3f
TemporalOroboros <TemporalOroboros@gmail.com>
2023-07-06 14:25:18 -0700

    Fix FixedPoint2Tests warnings

--- Content.Tests/Shared/Chemistry/FixedPoint2_Tests.cs
+++ Content.Tests/Shared/Chemistry/FixedPoint2_Tests.cs
@@ -127,10 +127,10 @@ namespace Content.Tests.Shared.Chemistry
 
         [Test]
         [TestCase(10.1f, 2.5f, "25.25")]
-        public void FloatMultiply (float aFloat, float b, string expected)
+        public void FloatMultiply(float aFloat, float b, string expected)
         {
             var a = FixedPoint2.New(aFloat);
-            var result = a*b;
+            var result = a * b;
             Assert.That($"{result}", Is.EqualTo(expected));
         }
 
@@ -172,8 +172,11 @@ namespace Content.Tests.Shared.Chemistry
         {
             var parameter = FixedPoint2.New(a);
             var comparison = FixedPoint2.New(b);
-            Assert.That(parameter.Equals(comparison), Is.EqualTo(comparison.Equals(parameter)));
-            Assert.That(comparison.Equals(parameter), Is.EqualTo(expected));
+            Assert.Multiple(() =>
+            {
+                Assert.That(parameter.Equals(comparison), Is.EqualTo(comparison.Equals(parameter)));
+                Assert.That(comparison.Equals(parameter), Is.EqualTo(expected));
+            });
         }
     }
 }
