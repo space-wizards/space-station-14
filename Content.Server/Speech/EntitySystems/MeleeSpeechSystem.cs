91739409279489d18ff10ae389a18c80b0b0bd45
HerCoyote23 <131214189+HerCoyote23@users.noreply.github.com>
2023-07-06 17:50:13 -0700

    before I ruin it?

--- Content.Server/Speech/EntitySystems/MeleeSpeechSystem.cs
+++ Content.Server/Speech/EntitySystems/MeleeSpeechSystem.cs
@@ -1,29 +1,50 @@
+using Content.Server.Actions;
 using Content.Server.Administration.Logs;
+using Content.Shared.Actions;
 using Content.Shared.Speech.Components;
 using Content.Shared.Speech.EntitySystems;
 using Content.Shared.Database;
-
+//using Content.Client.Weapons.Melee.UI;
+using Robust.Server.GameObjects;
+using Robust.Server.Player;
+using Robust.Shared.GameObjects;
+using JetBrains.Annotations;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Players;
+using Content.Server.Store.Components;
+using Content.Shared.Store;
+using Robust.Shared.Serialization;
 
 namespace Content.Server.Speech.EntitySystems;
 
 public sealed class MeleeSpeechSystem : SharedMeleeSpeechSystem
 {
 
-	[Dependency] private readonly IAdminLogManager _adminLogger = default!;
-
-
-	public override void Initialize()
+    [Dependency] private readonly IAdminLogManager _adminLogger = default!;
+    [Dependency] private readonly ActionsSystem _actions = default!;
+    [Dependency] private readonly SharedActionsSystem _actionSystem = default!;
+    [Dependency] private readonly UserInterfaceSystem _uiSystem = default!;
+    [Dependency] private readonly ISharedPlayerManager _playermanager = default!;
+    public override void Initialize()
 	{
 		base.Initialize();
 		SubscribeLocalEvent<MeleeSpeechComponent, MeleeSpeechBattlecryChangedMessage>(OnBattlecryChanged);
-	}
+        SubscribeLocalEvent<MeleeSpeechComponent, MeleeSpeechConfigureActionEvent>(OnConfigureAction);
+        SubscribeLocalEvent<MeleeSpeechComponent, GetItemActionsEvent>(OnGetActions);
+    }
 
+    private void OnGetActions(EntityUid uid, MeleeSpeechComponent component, GetItemActionsEvent args)
+    {
+        if (component.ConfigureAction != null)
+            args.Actions.Add(component.ConfigureAction);
+    }
 
 	private void OnBattlecryChanged(EntityUid uid, MeleeSpeechComponent comp, MeleeSpeechBattlecryChangedMessage args)
 	{
 
         if (!TryComp<MeleeSpeechComponent>(uid, out var meleeSpeechUser))
-			return;
+            return;
 
         string battlecry = args.Battlecry;
 
@@ -32,18 +53,46 @@ public sealed class MeleeSpeechSystem : SharedMeleeSpeechSystem
 
         TryChangeBattlecry(uid, battlecry, meleeSpeechUser);
 	}
+    /// <summary>
+    /// Attempts to open the Battlecry UI.
+    /// </summary>
+    private void OnConfigureAction(EntityUid uid, MeleeSpeechComponent comp, MeleeSpeechConfigureActionEvent args)
+    {
+
+        TryOpenUi(uid, args.User, comp);
+    }
 
 
+    public void TryOpenUi(EntityUid user, EntityUid storeEnt, MeleeSpeechComponent? component = null)
+    {
+        if (!Resolve(storeEnt, ref component))
+            return;
 
+        if (!TryComp<ActorComponent>(user, out var actor))
+            return;
 
-	/// <summary>
-	/// Attempts to change the battlecry of an entity.
-	/// Returns true/false.
-	/// </summary>
-	/// <remarks>
-	/// If provided with a player's EntityUid to the player parameter, adds the change to the admin logs.
-	/// </remarks>
-	public bool TryChangeBattlecry(EntityUid uid, string? battlecry, MeleeSpeechComponent? meleeSpeechComp = null)
+        if (!_uiSystem.TryToggleUi(storeEnt, MeleeSpeechUiKey.Key, actor.PlayerSession))
+            return;
+
+        //UpdateUserInterface(user, storeEnt, component);
+    }
+    /* private bool TryOpenUi(EntityUid uid, MeleeSpeechComponent comp, MeleeSpeechConfigureActionEvent args)
+     {
+
+         if (!EntityManager.TryGetComponent(args.User, out ActorComponent? actor)) return false;
+
+         return _uiSystem.TryToggleUi(uid, comp.MeleeSpeechUiKey, actor.PlayerSession);
+     }
+    */
+
+    /// <summary>
+    /// Attempts to change the battlecry of an entity.
+    /// Returns true/false.
+    /// </summary>
+    /// <remarks>
+    /// If provided with a player's EntityUid to the player parameter, adds the change to the admin logs.
+    /// </remarks>
+    public bool TryChangeBattlecry(EntityUid uid, string? battlecry, MeleeSpeechComponent? meleeSpeechComp = null)
 	{
 
         if (!Resolve(uid, ref meleeSpeechComp))
@@ -58,13 +107,11 @@ public sealed class MeleeSpeechSystem : SharedMeleeSpeechSystem
 		else
 		{
 			battlecry = null;
-		}
+        }
 
-		if (meleeSpeechComp.Battlecry == battlecry)
+        if (meleeSpeechComp.Battlecry == battlecry)
 
 			return true;
-
-
 
 		meleeSpeechComp.Battlecry = battlecry;
 		Dirty(meleeSpeechComp);
@@ -72,5 +119,18 @@ public sealed class MeleeSpeechSystem : SharedMeleeSpeechSystem
 		_adminLogger.Add(LogType.ItemConfigure, LogImpact.Medium, $" {ToPrettyString(uid):entity}'s battlecry has been changed to {battlecry}");
 		return true;
 	}
+
+    [Serializable]
+    public sealed class MeleeSpeechConfigureActionEvent : InstantActionEvent
+    {
+        public EntityUid User { get; }
+        public EntityUid Target { get; }
+
+        public MeleeSpeechConfigureActionEvent(EntityUid who, EntityUid target)
+        {
+            User = who;
+            Target = target;
+        }
+    }
 }
 
--- Content.Shared/Speech/Components/MeleeSpeechComponent.cs
+++ Content.Shared/Speech/Components/MeleeSpeechComponent.cs
@@ -1,3 +1,5 @@
+using Content.Shared.Actions;
+using Content.Shared.Actions.ActionTypes;
 using Content.Shared.Speech.EntitySystems;
 using Robust.Shared.Serialization;
 
@@ -16,17 +18,36 @@ public sealed partial class MeleeSpeechComponent : Component
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("MaxBattlecryLength")]
     public int MaxBattlecryLength = 12;
+    /*
+    [DataField("configureAction", customTypeSerializer: typeof(PrototypeIdSerializer<InstantActionPrototype>))]
+    public string configureActionId = "Set Battlecry";
+    */
+    [DataField("configureAction")] 
+    public InstantAction? ConfigureAction = null;
 }
 
 /// <summary>
 /// Key representing which <see cref="BoundUserInterface"/> is currently open.
 /// Useful when there are multiple UI for an object. Here it's future-proofing only.
-/// </summary>
+/// </summary>/
 [Serializable, NetSerializable]
 public enum MeleeSpeechUiKey : byte
 {
     Key,
 }
+
+/*[Serializable, NetSerializable]
+public sealed class MeleeSpeechConfigureActionEvent : InstantActionEvent
+{
+    public EntityUid User { get; }
+    public EntityUid Target { get; }
+
+    public MeleeSpeechConfigureActionEvent(EntityUid who, EntityUid target)
+    {
+        User = who;
+        Target = target;
+    }
+}*/
 
 /// <summary>
 /// Represents an <see cref="MeleeSpeechComponent"/> state that can be sent to the client
@@ -49,5 +70,16 @@ public sealed class MeleeSpeechBattlecryChangedMessage : BoundUserInterfaceMessa
     public MeleeSpeechBattlecryChangedMessage(string battlecry)
     {
         Battlecry = battlecry;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class MeleeSpeechConfigureActionMessage : BoundUserInterfaceMessage
+{
+    public string CurrentBattlecry { get; }
+
+    public MeleeSpeechConfigureActionMessage(string currentBattlecry)
+    {
+        CurrentBattlecry = currentBattlecry;
     }
 }
