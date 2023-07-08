ba90af87697bc876f2bd76d10211e37278387ed0
faint <46868845+ficcialfaint@users.noreply.github.com>
2023-07-08 08:50:10 +0300

    Criminal Records Console component comments + missing attributes

--- Content.Server/CriminalRecords/Components/GeneralCriminalRecordConsoleComponent.cs
+++ Content.Server/CriminalRecords/Components/GeneralCriminalRecordConsoleComponent.cs
@@ -1,10 +1,24 @@
+using Content.Server.CriminalRecords.Systems;
 using Content.Shared.StationRecords;
 
 namespace Content.Server.CriminalRecords.Components;
 
+/// <summary>
+/// A component for Criminal Record Console storing an active station record key and a currently applied filter
+/// </summary>
 [RegisterComponent]
+[Access(typeof(CriminalRecordsSystem))]
 public sealed class GeneralCriminalRecordConsoleComponent : Component
 {
-    public StationRecordKey? ActiveKey { get; set; }
-    public GeneralStationRecordsFilter? Filter { get; set; }
+    /// <summary>
+    /// Currently active station record key
+    /// </summary>
+    [DataField("activeKey"), ViewVariables]
+    public StationRecordKey? ActiveKey;
+
+    /// <summary>
+    /// Currently applied filter
+    /// </summary>
+    [DataField("filter"), ViewVariables]
+    public GeneralStationRecordsFilter? Filter;
 }
