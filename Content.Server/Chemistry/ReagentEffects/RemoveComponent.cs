69b7f2105d3dc42189cbbe1ded5a04ed5420c4fe
unknown <shadowjayofdarkness@gmail.com>
2023-07-17 11:28:20 -0700

    file-scoped namespace

--- Content.Server/Chemistry/ReagentEffects/RemoveComponent.cs
+++ Content.Server/Chemistry/ReagentEffects/RemoveComponent.cs
@@ -8,41 +8,40 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Robust.Shared.IoC;
 
-namespace Content.Server.Chemistry.ReagentEffects
+namespace Content.Server.Chemistry.ReagentEffects;
+
+/// <summary>
+/// Removes designated component.
+/// You can apply this effect to the same reagent multiple times for multiple components and it'll still work.
+/// </summary>
+[UsedImplicitly]
+public sealed class RemoveComponent : ReagentEffect
 {
     /// <summary>
-    /// Removes designated component.
-    /// You can apply this effect to the same reagent multiple times for multiple components and it'll still work.
+    /// Name of component to remove, as a string
+    /// Note: component name shouldn't have the "component" postfix
+    /// WRONG: [component: ReplacementAccentComponent]
+    /// RIGHT: [component: ReplacementAccent]
     /// </summary>
-    [UsedImplicitly]
-    public sealed class RemoveComponent : ReagentEffect
+    [DataField("component")]
+    public string Component = String.Empty;
+
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+        => Loc.GetString("reagent-effect-guidebook-remove-component", ("chance", Probability), ("component", Component));
+
+    public override void Effect(ReagentEffectArgs args)
     {
-        /// <summary>
-        /// Name of component to remove, as a string
-        /// Note: component name shouldn't have the "component" postfix
-        /// WRONG: [component: ReplacementAccentComponent]
-        /// RIGHT: [component: ReplacementAccent]
-        /// </summary>
-        [DataField("component")]
-        public string Component = String.Empty;
+        var entityManager = args.EntityManager;
+        var uid = args.SolutionEntity;
+        // TODO: This is hot garbage (allegedly) and probably needs an engine change to not be a POS. Allegedly.
+        var _compFactory = IoCManager.Resolve<IComponentFactory>();
 
-        protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-            => Loc.GetString("reagent-effect-guidebook-remove-component", ("chance", Probability), ("component", Component));
-
-        public override void Effect(ReagentEffectArgs args)
+        if (!_compFactory.TryGetRegistration(Component, out var registration, true))
         {
-            var entityManager = args.EntityManager;
-            var uid = args.SolutionEntity;
-            // TODO: This is hot garbage (allegedly) and probably needs an engine change to not be a POS. Allegedly.
-            var _compFactory = IoCManager.Resolve<IComponentFactory>();
-
-            if (!_compFactory.TryGetRegistration(Component, out var registration, true))
-            {
-                System.Console.WriteLine("component '{0}' doesn't exist", Component);
-                return;
-            }
-
-            entityManager.RemoveComponent(uid, registration.Type);
+            System.Console.WriteLine("component '{0}' doesn't exist", Component);
+            return;
         }
+
+        entityManager.RemoveComponent(uid, registration.Type);
     }
 }
