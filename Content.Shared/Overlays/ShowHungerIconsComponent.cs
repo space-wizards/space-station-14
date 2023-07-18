258b93325097a0d4701b095e0beeb7571775755d
Visne <39844191+Visne@users.noreply.github.com>
2023-07-18 17:27:22 +0200

    Revert "Hunger and thirst HUDs (#18066)"
    This reverts commit 67e4b626c1c490f98950add60f42c4465cf049a1.

--- Content.Shared/Overlays/ShowHungerIconsComponent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Content.Shared.Overlays
-{
-    /// <summary>
-    /// This component allows you to see the hungriness of mobs.
-    /// </summary>
-    [RegisterComponent]
-    public sealed class ShowHungerIconsComponent : Component { }
-}
