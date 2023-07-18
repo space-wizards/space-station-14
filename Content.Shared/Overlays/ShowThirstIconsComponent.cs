67e4b626c1c490f98950add60f42c4465cf049a1
PrPleGoo <PrPleGoo@users.noreply.github.com>
2023-07-18 08:01:22 +0200

    Hunger and thirst HUDs (#18066)
    * security HUD now shows a job icon on entities with a body
    
    * thirst goggles
    
    * set starting hud gear

---
+++ Content.Shared/Overlays/ShowThirstIconsComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace Content.Shared.Overlays
+{
+    /// <summary>
+    /// This component allows you to see the thirstiness of mobs.
+    /// </summary>
+    [RegisterComponent]
+    public sealed class ShowThirstIconsComponent : Component { }
+}
