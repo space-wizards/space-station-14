c8b1f3d56863edbd146a53e4368478537a7db95f
eclips_e <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2023-07-21 22:29:35 +0800

    Fix secrets (#92)
    * Secrets... oooohhhhoohohohoh so scary....
    
    * Secrets actually work
    
    * uhhh more stuff
    
    * Secret prototypes actually work now

--- Tools/setup_secrets.ps1
+++ Tools/setup_secrets.ps1
@@ -1,4 +1,4 @@
 git submodule add -f https://github.com/ekrixi-14/secrets Secrets
 New-Item -ItemType SymbolicLink -Path "Content.ServerSecret\Code" -Target "Secrets\ServerCode"
-New-Item -ItemType SymbolicLink -Path "Resources\SecretResources" -Target "Secrets\SecretResources"
+New-Item -ItemType SymbolicLink -Path "Resources\Prototypes\SecretPrototypes" -Target "Secrets\SecretResources\Prototypes"
 pause
