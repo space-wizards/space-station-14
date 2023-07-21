e5b108337e7f7620a2518a3b22c710724825812b
Onoira <onoira@psiko.zone>
2023-07-21 22:44:48 +0200

    refactor

--- Content.Server/Tabletop/TabletopCheckerSetup.cs
+++ Content.Server/Tabletop/TabletopCheckerSetup.cs
@@ -1,15 +1,12 @@
 using JetBrains.Annotations;
 using Robust.Shared.Map;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Server.Tabletop
 {
     [UsedImplicitly]
     public sealed class TabletopCheckerSetup : TabletopSetup
     {
-
-        // TODO: Un-hardcode the rest of entity prototype IDs, probably.
+        private const float Separation = 1f;
 
         public override void SetupTabletop(TabletopSession session, IEntityManager entityManager)
         {
@@ -20,63 +17,64 @@ namespace Content.Server.Tabletop
             SpawnPieces(session, entityManager, session.Position.Offset(-4.5f, 3.5f));
         }
 
-        private void SpawnPieces(TabletopSession session, IEntityManager entityManager, MapCoordinates topLeft, float separation = 1f)
+        private static void SpawnPieces(TabletopSession session, IEntityManager entityManager, MapCoordinates topLeft)
         {
             var (mapId, x, y) = topLeft;
+            var pieces = new EntityUid[42];
+            var pieceIndex = 0;
 
-            // Spawn all black pieces
-            SpawnPieces(session, entityManager, "Black", topLeft, separation);
-
-            // Spawn all white pieces
-            SpawnPieces(session, entityManager, "White", new MapCoordinates(x, y - 7 * separation, mapId), separation);
-
-            // Queens
-            for (int i = 1; i < 4; i++)
+            void SpawnPieces(string color, MapCoordinates left)
             {
-                EntityUid tempQualifier = entityManager.SpawnEntity("BlackCheckerQueen", new MapCoordinates(x + 9 * separation + 9f / 32, y - (i - 1) * separation, mapId));
-                session.Entities.Add(tempQualifier);
+                var (mapId, x, y) = left;
+                var reversed = ("White" == color) ? 1 : -1;
 
-                EntityUid tempQualifier1 = entityManager.SpawnEntity("WhiteCheckerQueen", new MapCoordinates(x + 8 * separation + 9f / 32, y - (i - 1) * separation, mapId));
-                session.Entities.Add(tempQualifier1);
+                for (var i = 0; i < 3; i++)
+                {
+                    var x_offset = i % 2;
+                    if (reversed == -1) x_offset = 1 - x_offset; // Flips it
+
+                    for (var j = 0; j < 8; j += 2)
+                    {
+                        // Prevents an extra piece on the middle row
+                        if (x_offset + j > 8) continue;
+
+                        pieces[pieceIndex] = entityManager.SpawnEntity($"{color}CheckerPiece",
+                            new MapCoordinates(x + (j + x_offset) * Separation, y + i * reversed * Separation, mapId)
+                        );
+                        pieceIndex++;
+                    }
+                }
             }
 
-            var spares = new List<EntityUid>();
+            SpawnPieces("Black", topLeft);
+            SpawnPieces("White", new MapCoordinates(x, y - 7 * Separation, mapId));
+
+            // Queens
+            for (var i = 1; i < 4; i++)
+            {
+                pieces[pieceIndex] = entityManager.SpawnEntity("BlackCheckerQueen",
+                    new MapCoordinates(x + 9 * Separation + 9f / 32, y - (i - 1) * Separation, mapId)
+                );
+                pieces[pieceIndex + 1] = entityManager.SpawnEntity("WhiteCheckerQueen",
+                    new MapCoordinates(x + 8 * Separation + 9f / 32, y - (i - 1) * Separation, mapId)
+                );
+                pieceIndex += 2;
+            }
+
+            // Spares
             for (var i = 1; i < 7; i++)
             {
                 var step = 3 + 0.25f * (i - 1);
-                spares.Add(
-                  entityManager.SpawnEntity("BlackCheckerPiece",
-                  new MapCoordinates(x + 9 * separation + 9f / 32, y - step * separation, mapId)
-                ));
-
-                spares.Add(
-                  entityManager.SpawnEntity("WhiteCheckerPiece",
-                  new MapCoordinates(x + 8 * separation + 9f / 32, y - step * separation, mapId)
-                ));
+                pieces[pieceIndex] = entityManager.SpawnEntity("BlackCheckerPiece",
+                  new MapCoordinates(x + 9 * Separation + 9f / 32, y - step * Separation, mapId)
+                );
+                pieces[pieceIndex] = entityManager.SpawnEntity("WhiteCheckerPiece",
+                  new MapCoordinates(x + 8 * Separation + 9f / 32, y - step * Separation, mapId)
+                );
+                pieceIndex += 2;
             }
-            session.Entities.UnionWith(spares);
-        }
 
-        private void SpawnPieces(TabletopSession session, IEntityManager entityManager, string color, MapCoordinates left, float separation = 1f)
-        {
-          var (mapId, x, y) = left;
-          // If white is being placed it must go from bottom->up
-          var reversed = (color == "White") ? 1 : -1;
-
-          for (int i = 0; i < 3; i++)
-            {
-            var x_offset = i % 2;
-            if (reversed == -1) x_offset = 1 - x_offset; // Flips it
-
-            for (int j = 0; j < 8; j += 2)
-            {
-              // Prevents an extra piece on the middle row
-              if (x_offset + j > 8) continue;
-
-              EntityUid tempQualifier4 = entityManager.SpawnEntity(color + "CheckerPiece", new MapCoordinates(x + (j + x_offset) * separation, y + i * reversed * separation, mapId));
-              session.Entities.Add(tempQualifier4);
-            }
-          }
+            session.Entities.UnionWith(pieces);
         }
     }
 }
