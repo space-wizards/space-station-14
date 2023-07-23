0d4bc2f6ef995b717c24965705ef95c1ab881079
Hebi <spiritbreakz@gmail.com>
2023-07-23 17:17:36 +0200

    Changed to file scoped namespaces.

--- Content.Client/UserInterface/ControlExtensions/ControlExtension.cs
+++ Content.Client/UserInterface/ControlExtensions/ControlExtension.cs
@@ -1,98 +1,96 @@
 using Content.Client.Guidebook.Controls;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
-using System.Linq;
 
-namespace Content.Client.UserInterface.ControlExtensions
+namespace Content.Client.UserInterface.ControlExtensions;
+
+public static class ControlExtension
 {
-    public static class ControlExtension
+    public static List<T> GetControlOfType<T>(this Control parent) where T : Control
     {
-        public static List<T> GetControlOfType<T>(this Control parent) where T : Control
-        {
-            return parent.GetControlOfType<T>(typeof(T).Name, false);
-        }
-        public static List<T> GetControlOfType<T>(this Control parent, string childType) where T : Control
-        {
-            return parent.GetControlOfType<T>(childType, false);
-        }
+        return parent.GetControlOfType<T>(typeof(T).Name, false);
+    }
+    public static List<T> GetControlOfType<T>(this Control parent, string childType) where T : Control
+    {
+        return parent.GetControlOfType<T>(childType, false);
+    }
 
-        public static List<T> GetControlOfType<T>(this Control parent, bool fullTreeSearch) where T : Control
-        {
-            return parent.GetControlOfType<T>(typeof(T).Name, fullTreeSearch);
-        }
+    public static List<T> GetControlOfType<T>(this Control parent, bool fullTreeSearch) where T : Control
+    {
+        return parent.GetControlOfType<T>(typeof(T).Name, fullTreeSearch);
+    }
 
-        public static List<T> GetControlOfType<T>(this Control parent, string childType, bool fullTreeSearch) where T : Control
-        {
-            List<T> controlList = new List<T>();
+    public static List<T> GetControlOfType<T>(this Control parent, string childType, bool fullTreeSearch) where T : Control
+    {
+        List<T> controlList = new List<T>();
 
-            foreach (var child in parent.Children)
+        foreach (var child in parent.Children)
+        {
+            var isType = child.GetType().Name == childType;
+            var hasChildren = child.ChildCount > 0;
+
+            var searchDeeper = hasChildren && !isType;
+
+            if (isType)
             {
-                var isType = child.GetType().Name == childType;
-                var hasChildren = child.ChildCount > 0;
-
-                var searchDeeper = hasChildren && !isType;
-
-                if (isType)
-                {
-                    controlList.Add((T) child);
-                }
-
-                if (fullTreeSearch || searchDeeper)
-                {
-                    controlList.AddRange(child.GetControlOfType<T>(childType, fullTreeSearch));
-                }
+                controlList.Add((T) child);
             }
 
-            return controlList;
+            if (fullTreeSearch || searchDeeper)
+            {
+                controlList.AddRange(child.GetControlOfType<T>(childType, fullTreeSearch));
+            }
         }
 
-        public static List<ISearchableControl> GetSearchableControls(this Control parent, bool fullTreeSearch = false)
+        return controlList;
+    }
+
+    public static List<ISearchableControl> GetSearchableControls(this Control parent, bool fullTreeSearch = false)
+    {
+        List<ISearchableControl> controlList = new List<ISearchableControl>();
+
+        foreach (var child in parent.Children)
         {
-            List<ISearchableControl> controlList = new List<ISearchableControl>();
+            var hasChildren = child.ChildCount > 0;
+            var searchDeeper = hasChildren && child is not ISearchableControl;
 
-            foreach (var child in parent.Children)
+            if (child is ISearchableControl searchableChild)
             {
-                var hasChildren = child.ChildCount > 0;
-                var searchDeeper = hasChildren && child is not ISearchableControl;
-
-                if (child is ISearchableControl searchableChild)
-                {
-                    controlList.Add(searchableChild);
-                }
-
-                if (fullTreeSearch || searchDeeper)
-                {
-                    controlList.AddRange(child.GetSearchableControls(fullTreeSearch));
-                }
+                controlList.Add(searchableChild);
             }
 
-            return controlList;
+            if (fullTreeSearch || searchDeeper)
+            {
+                controlList.AddRange(child.GetSearchableControls(fullTreeSearch));
+            }
         }
 
-        public static bool ChildrenContainText(this Control parent, string search)
+        return controlList;
+    }
+
+    public static bool ChildrenContainText(this Control parent, string search)
+    {
+        var labels = parent.GetControlOfType<Label>();
+        var richTextLabels = parent.GetControlOfType<RichTextLabel>();
+
+        foreach (var label in labels)
         {
-            var labels = parent.GetControlOfType<Label>();
-            var richTextLabels = parent.GetControlOfType<RichTextLabel>();
-
-            foreach (var label in labels)
+            if (label.Text != null && label.Text.Contains(search, StringComparison.OrdinalIgnoreCase))
             {
-                if (label.Text != null && label.Text.Contains(search, StringComparison.OrdinalIgnoreCase))
-                {
-                    return true;
-                }
+                return true;
             }
-
-            foreach (var label in richTextLabels)
-            {
-                var text = label.GetMessage();
-
-                if (text != null && text.Contains(search, StringComparison.OrdinalIgnoreCase))
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
+
+        foreach (var label in richTextLabels)
+        {
+            var text = label.GetMessage();
+
+            if (text != null && text.Contains(search, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
