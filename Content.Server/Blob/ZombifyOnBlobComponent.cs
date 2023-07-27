a74a219d857645a745ed9eed29a1d2f2c99ccb63
Ray <vigersray@gmail.com>
2023-07-27 17:37:14 +0300

    HTN for zombie blob
    (cherry picked from commit 112e606df2ff8872ce0de0507a8121690731c458)

--- Content.Server/Blob/ZombifyOnBlobComponent.cs
+++ Content.Server/Blob/ZombifyOnBlobComponent.cs
@@ -1,7 +1,12 @@
-﻿[RegisterComponent]
+﻿using Robust.Shared.Audio;
+
+[RegisterComponent]
 public sealed class ZombieBlobComponent : Component
 {
     public List<string> OldFations = new();
 
     public EntityUid BlobPodUid = default!;
+
+    [DataField("greetSoundNotification")]
+    public SoundSpecifier GreetSoundNotification = new SoundPathSpecifier("/Audio/Ambience/Antag/zombie_start.ogg");
 }
