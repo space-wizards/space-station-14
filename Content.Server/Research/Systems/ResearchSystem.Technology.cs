6f4dbab550b95ac3222a140170779e8253c4f21d
EmoGarbage404 <retron404@gmail.com>
2023-07-28 18:03:47 -0400

    wacka wacka

--- Content.Server/Research/Systems/ResearchSystem.Technology.cs
+++ Content.Server/Research/Systems/ResearchSystem.Technology.cs
@@ -84,7 +84,7 @@ public sealed partial class ResearchSystem
         ModifyServerPoints(serverEnt.Value, -prototype.Cost);
         UpdateTechnologyCards(serverEnt.Value);
 
-        _adminLog.Add(LogType.Research, LogImpact.Medium,
+        _adminLog.Add(LogType.Action, LogImpact.Medium,
             $"{ToPrettyString(user):player} unlocked {prototype.ID} (discipline: {prototype.Discipline}, tier: {prototype.Tier}) at {ToPrettyString(client)}, for server {ToPrettyString(serverEnt.Value)}.");
         return true;
     }
