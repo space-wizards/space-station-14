7c9d7423d2c626165b88b775ff73fe2f9e21c11f
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2023-07-29 08:44:28 +0200

    Remove autogenerated from admin log key. (#18377)

--- Content.Server.Database/Migrations/Postgres/20230727190902_AdminLogCompoundKey.Designer.cs
+++ Content.Server.Database/Migrations/Postgres/20230727190902_AdminLogCompoundKey.Designer.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using Content.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Content.Server.Database.Migrations.Postgres
 {
     [DbContext(typeof(PostgresServerDbContext))]
-    partial class PostgresServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230727190902_AdminLogCompoundKey")]
+    partial class AdminLogCompoundKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
