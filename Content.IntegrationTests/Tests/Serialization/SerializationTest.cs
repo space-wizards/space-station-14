b609e810421ec3eb96ac2819c5c92b43351ad283
ElectroJr <leonsfriedrich@gmail.com>
2023-07-29 22:56:15 -0400

    Add validation checks.

--- Content.IntegrationTests/Tests/Serialization/SerializationTest.cs
+++ Content.IntegrationTests/Tests/Serialization/SerializationTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Robust.Shared.Reflection;
 using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Serialization.Manager.Attributes;
@@ -30,6 +31,9 @@ public sealed class SerializationTest
         var expected = refMan.GetEnumReference(value);
         Assert.That(valueNode!.Value, Is.EqualTo(expected));
 
+        var errors = seriMan.ValidateNode<Enum>(valueNode).GetErrors();
+        Assert.That(errors.Any(), Is.False);
+
         var deserialized = seriMan.Read<Enum>(node, notNullableOverride:true);
         Assert.That(deserialized, Is.EqualTo(value));
 
@@ -41,12 +45,28 @@ public sealed class SerializationTest
         };
 
         node = seriMan.WriteValue(data, notNullableOverride:true);
+
+        errors = seriMan.ValidateNode<TestData>(node).GetErrors();
+        Assert.That(errors.Any(), Is.False);
+
         var deserializedData = seriMan.Read<TestData>(node, notNullableOverride:false);
 
         Assert.That(deserializedData.Value, Is.EqualTo(data.Value));
         Assert.That(deserializedData.Sequence.Count, Is.EqualTo(data.Sequence.Count));
         Assert.That(deserializedData.Sequence[0], Is.EqualTo(data.Sequence[0]));
         Assert.That(deserializedData.Sequence[1], Is.EqualTo(data.Sequence[1]));
+
+        // Check that Generic & non-generic serializers are incompativle.
+        Enum genericValue = TestEnum.Bb;
+        TestEnum typedValue = TestEnum.Bb;
+
+        var genericNode = seriMan.WriteValue(genericValue, notNullableOverride:true);
+        var typedNode = seriMan.WriteValue(typedValue);
+
+        Assert.That(seriMan.ValidateNode<Enum>(genericNode).GetErrors().Any(), Is.False);
+        Assert.That(seriMan.ValidateNode<TestEnum>(genericNode).GetErrors().Any(), Is.True);
+        Assert.That(seriMan.ValidateNode<Enum>(typedNode).GetErrors().Any(), Is.True);
+        Assert.That(seriMan.ValidateNode<TestEnum>(typedNode).GetErrors().Any(), Is.False);
     }
 
     private enum TestEnum : byte { Aa, Bb, Cc, Dd }
