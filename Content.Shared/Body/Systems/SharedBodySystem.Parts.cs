93969041ef80c3b3d1b5399398de8466185ae570
ElectroJr <leonsfriedrich@gmail.com>
2023-07-30 22:09:46 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Content.Shared/Body/Systems/SharedBodySystem.Parts.cs
+++ Content.Shared/Body/Systems/SharedBodySystem.Parts.cs
@@ -42,8 +42,8 @@ public partial class SharedBodySystem
             return;
 
         part.Body = state.Body;
-        part.ParentSlot = state.ParentSlot;
-        part.Children = state.Children;
+        part.ParentSlot = state.ParentSlot; // TODO use containers. This is broken and does not work.
+        part.Children = state.Children; // TODO use containers. This is broken and does not work.
         part.Organs = state.Organs;
         part.PartType = state.PartType;
         part.IsVital = state.IsVital;
