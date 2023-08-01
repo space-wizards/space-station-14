8664b255064408692ddc83421ab9b4b17b5cea8f
ubis1 <140386474+ubis1@users.noreply.github.com>
2023-08-01 22:59:26 +0300

    deleted some stuff

--- Content.Shared/Lathe/LatheComponent.cs
+++ Content.Shared/Lathe/LatheComponent.cs
@@ -23,18 +23,6 @@ namespace Content.Shared.Lathe
         public readonly List<string> DynamicRecipes = new();
 
         /// <summary>
-        /// All of the dynamic recipes that the lathe is capable to get using EMAG
-        /// </summary>
-        [DataField("emagDynamicRecipes", customTypeSerializer: typeof(PrototypeIdListSerializer<LatheRecipePrototype>))]
-        public readonly List<string> EmagDynamicRecipes = new();
-
-        /// <summary>
-        /// All of the static recipes that the lathe is capable to get using EMAG
-        /// </summary>
-        [DataField("emagStaticRecipes", customTypeSerializer: typeof(PrototypeIdListSerializer<LatheRecipePrototype>))]
-        public readonly List<string> EmagStaticRecipes = new();
-
-        /// <summary>
         /// The lathe's construction queue
         /// </summary>
         [DataField("queue")]
