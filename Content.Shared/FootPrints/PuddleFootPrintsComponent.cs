2df123e26eecbcb458f718481791797732aa0646
mirton96 <mirton14@gmail.com>
2023-08-01 13:07:39 +0000

    Revert "foot prints update"
    This reverts commit e1f3f6a6663fea9843650237119ae3af3ba2bc4f

---
+++ Content.Shared/FootPrints/PuddleFootPrintsComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Shared.FootPrints;
+
+[RegisterComponent]
+public sealed class PuddleFootPrintsComponent : Component
+{
+    public float SizeRatio = 0.3f;
+    public float OffPercent = 80f;
+}
