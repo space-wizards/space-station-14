0a9a24ef5dc302fd3e2af82997f3328974f4b6b7
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-08-02 17:56:54 +0300

    i hate namespaces i hate namespaces

--- Content.Server/Damage/Systems/DamageOtherOnHitSystem.cs
+++ Content.Server/Damage/Systems/DamageOtherOnHitSystem.cs
@@ -12,54 +12,55 @@ using Content.Shared.Verbs;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Player;
 
-namespace Content.Server.Damage.Systems;
-
-public sealed class DamageOtherOnHitSystem : EntitySystem
+namespace Content.Server.Damage.Systems
 {
-    [Dependency] private readonly DamageableSystem _damageableSystem = default!;
-    [Dependency] private readonly IAdminLogManager _adminLogger = default!;
-    [Dependency] private readonly GunSystem _guns = default!;
-    [Dependency] private readonly SharedCameraRecoilSystem _sharedCameraRecoil = default!;
-    [Dependency] private readonly ThrownItemSystem _thrownItem = default!;
-    [Dependency] private readonly ExamineSystem _examine = default!;
-    [Dependency] private readonly DamageableSystem _damageable = default!;
-
-    public override void Initialize()
+    public sealed class DamageOtherOnHitSystem : EntitySystem
     {
-        SubscribeLocalEvent<DamageOtherOnHitComponent, ThrowDoHitEvent>(OnDoHit);
-        SubscribeLocalEvent<DamageOtherOnHitComponent, GetVerbsEvent<ExamineVerb>>(OnExamine);
-    }
+        [Dependency] private readonly DamageableSystem _damageableSystem = default!;
+        [Dependency] private readonly IAdminLogManager _adminLogger = default!;
+        [Dependency] private readonly GunSystem _guns = default!;
+        [Dependency] private readonly SharedCameraRecoilSystem _sharedCameraRecoil = default!;
+        [Dependency] private readonly ThrownItemSystem _thrownItem = default!;
+        [Dependency] private readonly ExamineSystem _examine = default!;
+        [Dependency] private readonly DamageableSystem _damageable = default!;
 
-    private void OnDoHit(EntityUid uid, DamageOtherOnHitComponent component, ThrowDoHitEvent args)
-    {
-        var dmg = _damageableSystem.TryChangeDamage(args.Target, component.Damage, component.IgnoreResistances, origin: args.Component.Thrower);
-
-        // Log damage only for mobs. Useful for when people throw spears at each other, but also avoids log-spam when explosions send glass shards flying.
-        if (dmg != null && HasComp<MobStateComponent>(args.Target))
-            _adminLogger.Add(LogType.ThrowHit, $"{ToPrettyString(args.Target):target} received {dmg.Total:damage} damage from collision");
-
-        RaiseNetworkEvent(new ColorFlashEffectEvent(Color.Red, new List<EntityUid> { args.Target }), Filter.Pvs(args.Target, entityManager: EntityManager));
-        _guns.PlayImpactSound(args.Target, dmg, null, false);
-        if (TryComp<PhysicsComponent>(uid, out var body) && body.LinearVelocity.LengthSquared() > 0f)
+        public override void Initialize()
         {
-            var direction = body.LinearVelocity.Normalized();
-            _sharedCameraRecoil.KickCamera(args.Target, direction);
+            SubscribeLocalEvent<DamageOtherOnHitComponent, ThrowDoHitEvent>(OnDoHit);
+            SubscribeLocalEvent<DamageOtherOnHitComponent, GetVerbsEvent<ExamineVerb>>(OnExamine);
         }
 
-        _thrownItem.LandComponent(args.Thrown, args.Component, playSound: false);
-    }
+        private void OnDoHit(EntityUid uid, DamageOtherOnHitComponent component, ThrowDoHitEvent args)
+        {
+            var dmg = _damageableSystem.TryChangeDamage(args.Target, component.Damage, component.IgnoreResistances, origin: args.Component.Thrower);
 
-    private void OnExamine(EntityUid uid, DamageOtherOnHitComponent component, GetVerbsEvent<ExamineVerb> args)
-    {
-        if (!args.CanInteract || !args.CanAccess)
-            return;
+            // Log damage only for mobs. Useful for when people throw spears at each other, but also avoids log-spam when explosions send glass shards flying.
+            if (dmg != null && HasComp<MobStateComponent>(args.Target))
+                _adminLogger.Add(LogType.ThrowHit, $"{ToPrettyString(args.Target):target} received {dmg.Total:damage} damage from collision");
 
-        var type = Loc.GetString("damage-throw");
-        var markup = _damageable.GetDamageExamine(component.Damage, type);
-        _examine.AddDetailedExamineVerb(args, component, markup,
-            Loc.GetString("damage-examinable-verb-text", ("type", type)),
-            "/Textures/Interface/VerbIcons/rotate_cw.svg.192dpi.png",
-            Loc.GetString("damage-examinable-verb-message")
-        );
+            RaiseNetworkEvent(new ColorFlashEffectEvent(Color.Red, new List<EntityUid> { args.Target }), Filter.Pvs(args.Target, entityManager: EntityManager));
+            _guns.PlayImpactSound(args.Target, dmg, null, false);
+            if (TryComp<PhysicsComponent>(uid, out var body) && body.LinearVelocity.LengthSquared() > 0f)
+            {
+                var direction = body.LinearVelocity.Normalized();
+                _sharedCameraRecoil.KickCamera(args.Target, direction);
+            }
+
+            _thrownItem.LandComponent(args.Thrown, args.Component, playSound: false);
+        }
+
+        private void OnExamine(EntityUid uid, DamageOtherOnHitComponent component, GetVerbsEvent<ExamineVerb> args)
+        {
+            if (!args.CanInteract || !args.CanAccess)
+                return;
+
+            var type = Loc.GetString("damage-throw");
+            var markup = _damageable.GetDamageExamine(component.Damage, type);
+            _examine.AddDetailedExamineVerb(args, component, markup,
+                Loc.GetString("damage-examinable-verb-text", ("type", type)),
+                "/Textures/Interface/VerbIcons/rotate_cw.svg.192dpi.png",
+                Loc.GetString("damage-examinable-verb-message")
+            );
+        }
     }
 }
