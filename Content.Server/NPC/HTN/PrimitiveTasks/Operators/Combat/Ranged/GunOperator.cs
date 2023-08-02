c31c848afdb253428fac0fe15a49a8a7172333ff
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2023-08-02 10:48:56 +1000

    Shooting NPCs and more (#18042)
    * Add pirate shooting
    
    * Shooting working
    
    * Basics working
    
    * Refactor time
    
    * More conversion
    
    * Update primitives
    
    * Update yml
    
    * weh
    
    * Building again
    
    * Draft
    
    * weh
    
    * b
    
    * Start shutdown
    
    * Starting to take form
    
    * Code side done
    
    * is it worky
    
    * Fix prototypes
    
    * stuff
    
    * Shitty working
    
    * Juke events working
    
    * Even more cleanup
    
    * RTX
    
    * Fix interaction combat mode and compquery
    
    * GetAmmoCount relays
    
    * Fix rotation speed
    
    * Juke fixes
    
    * fixes
    
    * weh
    
    * The collision avoidance never ends
    
    * Fixes
    
    * Pause support
    
    * framework
    
    * lazy
    
    * Fix idling
    
    * Fix drip
    
    * goobed
    
    * Fix takeover shutdown bug
    
    * Merge fixes
    
    * shitter
    
    * Fix carpos

--- Content.Server/NPC/HTN/PrimitiveTasks/Operators/Combat/Ranged/GunOperator.cs
+++ Content.Server/NPC/HTN/PrimitiveTasks/Operators/Combat/Ranged/GunOperator.cs
@@ -1,17 +1,19 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Content.Server.NPC.Components;
+using Content.Shared.CombatMode;
 using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
+using Robust.Shared.Audio;
 
-namespace Content.Server.NPC.HTN.PrimitiveTasks.Operators.Melee;
+namespace Content.Server.NPC.HTN.PrimitiveTasks.Operators.Combat.Ranged;
 
-/// <summary>
-/// Attacks the specified key in melee combat.
-/// </summary>
-public sealed class MeleeOperator : HTNOperator
+public sealed class GunOperator : HTNOperator, IHtnConditionalShutdown
 {
     [Dependency] private readonly IEntityManager _entManager = default!;
+
+    [DataField("shutdownState")]
+    public HTNPlanState ShutdownState { get; } = HTNPlanState.TaskFinished;
 
     /// <summary>
     /// Key that contains the target entity.
@@ -25,15 +27,13 @@ public sealed class MeleeOperator : HTNOperator
     [DataField("targetState")]
     public MobState TargetState = MobState.Alive;
 
-    // Like movement we add a component and pass it off to the dedicated system.
+    /// <summary>
+    /// Do we require line of sight of the target before failing.
+    /// </summary>
+    [DataField("requireLOS")]
+    public bool RequireLOS = false;
 
-    public override void Startup(NPCBlackboard blackboard)
-    {
-        base.Startup(blackboard);
-        var melee = _entManager.EnsureComponent<NPCMeleeCombatComponent>(blackboard.GetValue<EntityUid>(NPCBlackboard.Owner));
-        melee.MissChance = blackboard.GetValueOrDefault<float>(NPCBlackboard.MeleeMissChance, _entManager);
-        melee.Target = blackboard.GetValue<EntityUid>(TargetKey);
-    }
+    // Like movement we add a component and pass it off to the dedicated system.
 
     public override async Task<(bool Valid, Dictionary<string, object>? Effects)> Plan(NPCBlackboard blackboard,
         CancellationToken cancelToken)
@@ -53,10 +53,28 @@ public sealed class MeleeOperator : HTNOperator
         return (true, null);
     }
 
-    public override void Shutdown(NPCBlackboard blackboard, HTNOperatorStatus status)
+    public override void Startup(NPCBlackboard blackboard)
     {
-        base.Shutdown(blackboard, status);
-        _entManager.RemoveComponent<NPCMeleeCombatComponent>(blackboard.GetValue<EntityUid>(NPCBlackboard.Owner));
+        base.Startup(blackboard);
+        var ranged = _entManager.EnsureComponent<NPCRangedCombatComponent>(blackboard.GetValue<EntityUid>(NPCBlackboard.Owner));
+        ranged.Target = blackboard.GetValue<EntityUid>(TargetKey);
+
+        if (blackboard.TryGetValue<float>(NPCBlackboard.RotateSpeed, out var rotSpeed, _entManager))
+        {
+            ranged.RotationSpeed = new Angle(rotSpeed);
+        }
+
+        if (blackboard.TryGetValue<SoundSpecifier>("SoundTargetInLOS", out var losSound, _entManager))
+        {
+            ranged.SoundTargetInLOS = losSound;
+        }
+    }
+
+    public void ConditionalShutdown(NPCBlackboard blackboard)
+    {
+        var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
+        _entManager.System<SharedCombatModeSystem>().SetInCombatMode(owner, false);
+        _entManager.RemoveComponent<NPCRangedCombatComponent>(owner);
         blackboard.Remove<EntityUid>(TargetKey);
     }
 
@@ -66,13 +84,13 @@ public sealed class MeleeOperator : HTNOperator
         var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
         HTNOperatorStatus status;
 
-        if (_entManager.TryGetComponent<NPCMeleeCombatComponent>(owner, out var combat) &&
+        if (_entManager.TryGetComponent<NPCRangedCombatComponent>(owner, out var combat) &&
             blackboard.TryGetValue<EntityUid>(TargetKey, out var target, _entManager))
         {
             combat.Target = target;
 
             // Success
-            if (_entManager.TryGetComponent<MobStateComponent>(target, out var mobState) &&
+            if (_entManager.TryGetComponent<MobStateComponent>(combat.Target, out var mobState) &&
                 mobState.CurrentState > TargetState)
             {
                 status = HTNOperatorStatus.Finished;
@@ -81,7 +99,15 @@ public sealed class MeleeOperator : HTNOperator
             {
                 switch (combat.Status)
                 {
-                    case CombatStatus.TargetOutOfRange:
+                    case CombatStatus.TargetUnreachable:
+                        status = HTNOperatorStatus.Failed;
+                        break;
+                    case CombatStatus.NotInSight:
+                        if (RequireLOS)
+                            status = HTNOperatorStatus.Failed;
+                        else
+                            status = HTNOperatorStatus.Continuing;
+                        break;
                     case CombatStatus.Normal:
                         status = HTNOperatorStatus.Continuing;
                         break;
@@ -96,9 +122,10 @@ public sealed class MeleeOperator : HTNOperator
             status = HTNOperatorStatus.Failed;
         }
 
-        if (status != HTNOperatorStatus.Continuing)
+        // Mark it as finished to continue the plan.
+        if (status == HTNOperatorStatus.Continuing && ShutdownState == HTNPlanState.PlanFinished)
         {
-            _entManager.RemoveComponent<NPCMeleeCombatComponent>(owner);
+            status = HTNOperatorStatus.Finished;
         }
 
         return status;
