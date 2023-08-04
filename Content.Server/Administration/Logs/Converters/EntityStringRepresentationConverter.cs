8ffa1ed518a97ebdf14903d169760cc61fe8fef9
Chief-Engineer <119664036+Chief-Engineer@users.noreply.github.com>
2023-08-04 18:43:15 -0500

    add more info to entity json in logs

--- Content.Server/Administration/Logs/Converters/EntityStringRepresentationConverter.cs
+++ Content.Server/Administration/Logs/Converters/EntityStringRepresentationConverter.cs
@@ -1,10 +1,19 @@
 using System.Text.Json;
+using Content.Server.Administration.Managers;
+using Content.Server.Atmos.Components;
+using Content.Shared.Glue;
+using Content.Shared.Lube;
+using Robust.Server.Player;
 
 namespace Content.Server.Administration.Logs.Converters;
 
 [AdminLogConverter]
 public sealed class EntityStringRepresentationConverter : AdminLogConverter<EntityStringRepresentation>
 {
+    [Dependency] private readonly IAdminManager _adminManager = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] private readonly EntityManager _entityManager = default!;
+
     public override void Write(Utf8JsonWriter writer, EntityStringRepresentation value, JsonSerializerOptions options)
     {
         writer.WriteStartObject();
@@ -19,6 +28,38 @@ public sealed class EntityStringRepresentationConverter : AdminLogConverter<Enti
         if (value.Session != null)
         {
             writer.WriteString("player", value.Session.UserId.UserId);
+
+            if (_playerManager.TryGetSessionById(value.Session.UserId, out var playerSession) &&
+                _adminManager.IsAdmin(playerSession))
+            {
+                writer.WriteBoolean("admin", true);
+            }
+        }
+
+        if (value.Prototype != null)
+        {
+            writer.WriteString("prototype", value.Prototype);
+        }
+
+        if (value.Deleted)
+        {
+            writer.WriteBoolean("deleted", true);
+        }
+
+        if (_entityManager.TryGetComponent<LubedComponent>(value.Uid, out var lubedComponent))
+        {
+            writer.WriteNumber("lubeSlipStrength", lubedComponent.SlipStrength);
+            writer.WriteNumber("lubeSlipsLeft", lubedComponent.SlipsLeft);
+        }
+
+        if (_entityManager.TryGetComponent<GluedComponent>(value.Uid, out var gluedComponent))
+        {
+            writer.WriteNumber("gluedDuration", gluedComponent.Duration.TotalSeconds);
+        }
+
+        if (_entityManager.TryGetComponent<FlammableComponent>(value.Uid, out var flammableComponent) && flammableComponent.OnFire)
+        {
+            writer.WriteNumber("fireStacks", flammableComponent.FireStacks);
         }
 
         writer.WriteEndObject();
