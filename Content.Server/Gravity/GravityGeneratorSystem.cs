226a05cea22c09d122d2d40f3152552143d32645
Арт <123451459+JustArt1m@users.noreply.github.com>
2023-08-04 12:44:08 +0300

    Revert "Test without OnRefreshParts"
    This reverts commit d319757aa15a8ebec1237e552213e80eb8042790.

--- Content.Server/Gravity/GravityGeneratorSystem.cs
+++ Content.Server/Gravity/GravityGeneratorSystem.cs
@@ -27,7 +27,7 @@ namespace Content.Server.Gravity
             SubscribeLocalEvent<GravityGeneratorComponent, ComponentShutdown>(OnComponentShutdown);
             SubscribeLocalEvent<GravityGeneratorComponent, EntParentChangedMessage>(OnParentChanged); // Or just anchor changed?
             SubscribeLocalEvent<GravityGeneratorComponent, InteractHandEvent>(OnInteractHand);
-            //SubscribeLocalEvent<GravityGeneratorComponent, RefreshPartsEvent>(OnRefreshParts);
+            SubscribeLocalEvent<GravityGeneratorComponent, RefreshPartsEvent>(OnRefreshParts);
             SubscribeLocalEvent<GravityGeneratorComponent, SharedGravityGeneratorComponent.SwitchGeneratorMessage>(
                 OnSwitchGenerator);
         }
@@ -257,11 +257,11 @@ namespace Content.Server.Gravity
             }
         }
 
-        /*private void OnRefreshParts(EntityUid uid, GravityGeneratorComponent component, RefreshPartsEvent args)
+        private void OnRefreshParts(EntityUid uid, GravityGeneratorComponent component, RefreshPartsEvent args)
         {
             var maxChargeMultipler = args.PartRatings[component.MachinePartMaxChargeMultiplier];
             component.MaxCharge = maxChargeMultipler * 1;
-        }*/
+        }
 
         private void MakeBroken(EntityUid uid, GravityGeneratorComponent component, AppearanceComponent? appearance)
         {
