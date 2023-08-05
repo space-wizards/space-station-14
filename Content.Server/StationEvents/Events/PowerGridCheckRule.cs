fe4ab255a649b45b806e0fac7482b220cc41d820
deltanedas <@deltanedas:kde.org>
2023-08-05 01:05:19 +0100

    file scope

--- Content.Server/StationEvents/Events/PowerGridCheckRule.cs
+++ Content.Server/StationEvents/Events/PowerGridCheckRule.cs
@@ -10,99 +10,98 @@ using Content.Server.GameTicking.Rules.Components;
 using Content.Server.Station.Components;
 using Content.Server.StationEvents.Components;
 
-namespace Content.Server.StationEvents.Events
+namespace Content.Server.StationEvents.Events;
+
+[UsedImplicitly]
+public sealed class PowerGridCheckRule : StationEventSystem<PowerGridCheckRuleComponent>
 {
-    [UsedImplicitly]
-    public sealed class PowerGridCheckRule : StationEventSystem<PowerGridCheckRuleComponent>
+    [Dependency] private readonly ApcSystem _apcSystem = default!;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly ApcSystem _apcSystem = default!;
+        base.Initialize();
 
-        public override void Initialize()
+        SubscribeLocalEvent<PowerGridCheckDisabledComponent, ApcToggleMainBreakerAttemptEvent>(OnApcToggleMainBreaker);
+    }
+
+    protected override void Started(EntityUid uid, PowerGridCheckRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+    {
+        base.Started(uid, component, gameRule, args);
+
+        if (!TryGetRandomStation(out var chosenStation))
+            return;
+
+        var query = AllEntityQuery<ApcComponent, TransformComponent>();
+        while (query.MoveNext(out var target, out var apc, out var transform))
         {
-            base.Initialize();
-
-            SubscribeLocalEvent<PowerGridCheckDisabledComponent, ApcToggleMainBreakerAttemptEvent>(OnApcToggleMainBreaker);
+            if (apc.MainBreakerEnabled && CompOrNull<StationMemberComponent>(transform.GridUid)?.Station == chosenStation)
+                component.Powered.Add(target);
         }
 
-        protected override void Started(EntityUid uid, PowerGridCheckRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+        RobustRandom.Shuffle(component.Powered);
+
+        component.NumberPerSecond = Math.Max(1, (int)(component.Powered.Count / component.SecondsUntilOff)); // Number of APCs to turn off every second. At least one.
+    }
+
+    protected override void Ended(EntityUid uid, PowerGridCheckRuleComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
+    {
+        base.Ended(uid, component, gameRule, args);
+
+        foreach (var entity in component.Unpowered)
         {
-            base.Started(uid, component, gameRule, args);
+            if (Deleted(entity))
+                continue;
 
-            if (!TryGetRandomStation(out var chosenStation))
-                return;
-
-            var query = AllEntityQuery<ApcComponent, TransformComponent>();
-            while (query.MoveNext(out var target, out var apc, out var transform))
+            if (TryComp(entity, out ApcComponent? apcComponent))
             {
-                if (apc.MainBreakerEnabled && CompOrNull<StationMemberComponent>(transform.GridUid)?.Station == chosenStation)
-                    component.Powered.Add(target);
+                if (!apcComponent.MainBreakerEnabled)
+                    _apcSystem.ApcToggleBreaker(entity, apcComponent);
             }
-
-            RobustRandom.Shuffle(component.Powered);
-
-            component.NumberPerSecond = Math.Max(1, (int)(component.Powered.Count / component.SecondsUntilOff)); // Number of APCs to turn off every second. At least one.
+            RemComp<PowerGridCheckDisabledComponent>(entity);
         }
 
-        protected override void Ended(EntityUid uid, PowerGridCheckRuleComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
+        // Can't use the default EndAudio
+        component.AnnounceCancelToken?.Cancel();
+        component.AnnounceCancelToken = new CancellationTokenSource();
+        Timer.Spawn(3000, () =>
         {
-            base.Ended(uid, component, gameRule, args);
+            Audio.PlayGlobal("/Audio/Announcements/power_on.ogg", Filter.Broadcast(), true, AudioParams.Default.WithVolume(-4f));
+        }, component.AnnounceCancelToken.Token);
+        component.Unpowered.Clear();
+    }
 
-            foreach (var entity in component.Unpowered)
-            {
-                if (Deleted(entity))
-                    continue;
+    protected override void ActiveTick(EntityUid uid, PowerGridCheckRuleComponent component, GameRuleComponent gameRule, float frameTime)
+    {
+        base.ActiveTick(uid, component, gameRule, frameTime);
 
-                if (TryComp(entity, out ApcComponent? apcComponent))
-                {
-                    if (!apcComponent.MainBreakerEnabled)
-                        _apcSystem.ApcToggleBreaker(entity, apcComponent);
-                }
-                RemComp<PowerGridCheckDisabledComponent>(entity);
-            }
-
-            // Can't use the default EndAudio
-            component.AnnounceCancelToken?.Cancel();
-            component.AnnounceCancelToken = new CancellationTokenSource();
-            Timer.Spawn(3000, () =>
-            {
-                Audio.PlayGlobal("/Audio/Announcements/power_on.ogg", Filter.Broadcast(), true, AudioParams.Default.WithVolume(-4f));
-            }, component.AnnounceCancelToken.Token);
-            component.Unpowered.Clear();
-        }
-
-        protected override void ActiveTick(EntityUid uid, PowerGridCheckRuleComponent component, GameRuleComponent gameRule, float frameTime)
+        var updates = 0;
+        component.FrameTimeAccumulator += frameTime;
+        if (component.FrameTimeAccumulator > component.UpdateRate)
         {
-            base.ActiveTick(uid, component, gameRule, frameTime);
-
-            var updates = 0;
-            component.FrameTimeAccumulator += frameTime;
-            if (component.FrameTimeAccumulator > component.UpdateRate)
-            {
-                updates = (int) (component.FrameTimeAccumulator / component.UpdateRate);
-                component.FrameTimeAccumulator -= component.UpdateRate * updates;
-            }
-
-            for (var i = 0; i < updates; i++)
-            {
-                if (component.Powered.Count == 0)
-                    break;
-
-                var selected = component.Powered.Pop();
-                if (Deleted(selected))
-                    continue;
-                if (TryComp<ApcComponent>(selected, out var apcComponent))
-                {
-                    if (apcComponent.MainBreakerEnabled)
-                        _apcSystem.ApcToggleBreaker(selected, apcComponent);
-                }
-                EnsureComp<PowerGridCheckDisabledComponent>(selected);
-                component.Unpowered.Add(selected);
-            }
+            updates = (int) (component.FrameTimeAccumulator / component.UpdateRate);
+            component.FrameTimeAccumulator -= component.UpdateRate * updates;
         }
 
-        private void OnApcToggleMainBreaker(EntityUid uid, PowerGridCheckDisabledComponent component, ref ApcToggleMainBreakerAttemptEvent args)
+        for (var i = 0; i < updates; i++)
         {
-            args.Cancelled = true;
+            if (component.Powered.Count == 0)
+                break;
+
+            var selected = component.Powered.Pop();
+            if (Deleted(selected))
+                continue;
+            if (TryComp<ApcComponent>(selected, out var apcComponent))
+            {
+                if (apcComponent.MainBreakerEnabled)
+                    _apcSystem.ApcToggleBreaker(selected, apcComponent);
+            }
+            EnsureComp<PowerGridCheckDisabledComponent>(selected);
+            component.Unpowered.Add(selected);
         }
+    }
+
+    private void OnApcToggleMainBreaker(EntityUid uid, PowerGridCheckDisabledComponent component, ref ApcToggleMainBreakerAttemptEvent args)
+    {
+        args.Cancelled = true;
     }
 }
