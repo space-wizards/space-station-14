4192ca15aae0cf5a240a98537458b88a1100dedb
deltanedas <@deltanedas:kde.org>
2023-08-05 11:04:46 +0100

    file scope

--- Content.Shared/Projectiles/SharedProjectileSystem.cs
+++ Content.Shared/Projectiles/SharedProjectileSystem.cs
@@ -14,141 +14,140 @@ using Robust.Shared.Physics.Events;
 using Robust.Shared.Physics.Systems;
 using Robust.Shared.Serialization;
 
-namespace Content.Shared.Projectiles
+namespace Content.Shared.Projectiles;
+
+public abstract class SharedProjectileSystem : EntitySystem
 {
-    public abstract class SharedProjectileSystem : EntitySystem
+    public const string ProjectileFixture = "projectile";
+
+    [Dependency] private readonly INetManager _netManager = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
+    [Dependency] private readonly SharedPhysicsSystem _physics = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+
+    public override void Initialize()
     {
-        public const string ProjectileFixture = "projectile";
+        base.Initialize();
 
-        [Dependency] private readonly INetManager _netManager = default!;
-        [Dependency] private readonly SharedAudioSystem _audio = default!;
-        [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
-        [Dependency] private readonly SharedPhysicsSystem _physics = default!;
-        [Dependency] private readonly SharedTransformSystem _transform = default!;
+        SubscribeLocalEvent<ProjectileComponent, PreventCollideEvent>(PreventCollision);
+        SubscribeLocalEvent<EmbeddableProjectileComponent, ProjectileCollideEvent>(OnEmbedProjectileCollide);
+        SubscribeLocalEvent<EmbeddableProjectileComponent, ThrowDoHitEvent>(OnEmbedThrowDoHit);
+        SubscribeLocalEvent<EmbeddableProjectileComponent, ActivateInWorldEvent>(OnEmbedActivate);
+        SubscribeLocalEvent<EmbeddableProjectileComponent, RemoveEmbeddedProjectileEvent>(OnEmbedRemove);
+    }
 
-        public override void Initialize()
+    private void OnEmbedActivate(EntityUid uid, EmbeddableProjectileComponent component, ActivateInWorldEvent args)
+    {
+        // Nuh uh
+        if (component.RemovalTime == null)
+            return;
+
+        if (args.Handled || !TryComp<PhysicsComponent>(uid, out var physics) || physics.BodyType != BodyType.Static)
+            return;
+
+        args.Handled = true;
+
+        _doAfter.TryStartDoAfter(new DoAfterArgs(args.User, component.RemovalTime.Value,
+            new RemoveEmbeddedProjectileEvent(), eventTarget: uid, target: uid)
         {
-            base.Initialize();
+            DistanceThreshold = SharedInteractionSystem.InteractionRange,
+        });
+    }
 
-            SubscribeLocalEvent<ProjectileComponent, PreventCollideEvent>(PreventCollision);
-            SubscribeLocalEvent<EmbeddableProjectileComponent, ProjectileCollideEvent>(OnEmbedProjectileCollide);
-            SubscribeLocalEvent<EmbeddableProjectileComponent, ThrowDoHitEvent>(OnEmbedThrowDoHit);
-            SubscribeLocalEvent<EmbeddableProjectileComponent, ActivateInWorldEvent>(OnEmbedActivate);
-            SubscribeLocalEvent<EmbeddableProjectileComponent, RemoveEmbeddedProjectileEvent>(OnEmbedRemove);
+    private void OnEmbedRemove(EntityUid uid, EmbeddableProjectileComponent component, RemoveEmbeddedProjectileEvent args)
+    {
+        // Whacky prediction issues.
+        if (args.Cancelled || _netManager.IsClient)
+            return;
+
+        if (component.DeleteOnRemove)
+        {
+            QueueDel(uid);
+            return;
         }
 
-        private void OnEmbedActivate(EntityUid uid, EmbeddableProjectileComponent component, ActivateInWorldEvent args)
+        var xform = Transform(uid);
+        TryComp<PhysicsComponent>(uid, out var physics);
+        _physics.SetBodyType(uid, BodyType.Dynamic, body: physics, xform: xform);
+        _transform.AttachToGridOrMap(uid, xform);
+
+        // Land it just coz uhhh yeah
+        var landEv = new LandEvent(args.User, true);
+        RaiseLocalEvent(uid, ref landEv);
+        _physics.WakeBody(uid, body: physics);
+    }
+
+    private void OnEmbedThrowDoHit(EntityUid uid, EmbeddableProjectileComponent component, ThrowDoHitEvent args)
+    {
+        Embed(uid, args.Target, component);
+    }
+
+    private void OnEmbedProjectileCollide(EntityUid uid, EmbeddableProjectileComponent component, ref ProjectileCollideEvent args)
+    {
+        Embed(uid, args.OtherEntity, component);
+
+        // Raise a specific event for projectiles.
+        if (TryComp<ProjectileComponent>(uid, out var projectile))
         {
-            // Nuh uh
-            if (component.RemovalTime == null)
-                return;
-
-            if (args.Handled || !TryComp<PhysicsComponent>(uid, out var physics) || physics.BodyType != BodyType.Static)
-                return;
-
-            args.Handled = true;
-
-            _doAfter.TryStartDoAfter(new DoAfterArgs(args.User, component.RemovalTime.Value,
-                new RemoveEmbeddedProjectileEvent(), eventTarget: uid, target: uid)
-            {
-                DistanceThreshold = SharedInteractionSystem.InteractionRange,
-            });
-        }
-
-        private void OnEmbedRemove(EntityUid uid, EmbeddableProjectileComponent component, RemoveEmbeddedProjectileEvent args)
-        {
-            // Whacky prediction issues.
-            if (args.Cancelled || _netManager.IsClient)
-                return;
-
-            if (component.DeleteOnRemove)
-            {
-                QueueDel(uid);
-                return;
-            }
-
-            var xform = Transform(uid);
-            TryComp<PhysicsComponent>(uid, out var physics);
-            _physics.SetBodyType(uid, BodyType.Dynamic, body: physics, xform: xform);
-            _transform.AttachToGridOrMap(uid, xform);
-
-            // Land it just coz uhhh yeah
-            var landEv = new LandEvent(args.User, true);
-            RaiseLocalEvent(uid, ref landEv);
-            _physics.WakeBody(uid, body: physics);
-        }
-
-        private void OnEmbedThrowDoHit(EntityUid uid, EmbeddableProjectileComponent component, ThrowDoHitEvent args)
-        {
-            Embed(uid, args.Target, component);
-        }
-
-        private void OnEmbedProjectileCollide(EntityUid uid, EmbeddableProjectileComponent component, ref ProjectileCollideEvent args)
-        {
-            Embed(uid, args.OtherEntity, component);
-
-            // Raise a specific event for projectiles.
-            if (TryComp<ProjectileComponent>(uid, out var projectile))
-            {
-                var ev = new ProjectileEmbedEvent(projectile.Shooter, projectile.Weapon, args.OtherEntity);
-                RaiseLocalEvent(uid, ref ev);
-            }
-        }
-
-        private void Embed(EntityUid uid, EntityUid target, EmbeddableProjectileComponent component)
-        {
-            TryComp<PhysicsComponent>(uid, out var physics);
-            _physics.SetLinearVelocity(uid, Vector2.Zero, body: physics);
-            _physics.SetBodyType(uid, BodyType.Static, body: physics);
-            var xform = Transform(uid);
-            _transform.SetParent(uid, xform, target);
-
-            if (component.Offset != Vector2.Zero)
-            {
-                _transform.SetLocalPosition(xform, xform.LocalPosition + xform.LocalRotation.RotateVec(component.Offset));
-            }
-
-            if (component.Sound != null)
-            {
-                _audio.PlayPredicted(component.Sound, uid, null);
-            }
-        }
-
-        private void PreventCollision(EntityUid uid, ProjectileComponent component, ref PreventCollideEvent args)
-        {
-            if (component.IgnoreShooter && args.OtherEntity == component.Shooter)
-            {
-                args.Cancelled = true;
-            }
-        }
-
-        public void SetShooter(ProjectileComponent component, EntityUid uid)
-        {
-            if (component.Shooter == uid)
-                return;
-
-            component.Shooter = uid;
-            Dirty(uid, component);
-        }
-
-        [Serializable, NetSerializable]
-        private sealed class RemoveEmbeddedProjectileEvent : DoAfterEvent
-        {
-            public override DoAfterEvent Clone() => this;
+            var ev = new ProjectileEmbedEvent(projectile.Shooter, projectile.Weapon, args.OtherEntity);
+            RaiseLocalEvent(uid, ref ev);
         }
     }
 
-    [Serializable, NetSerializable]
-    public sealed class ImpactEffectEvent : EntityEventArgs
+    private void Embed(EntityUid uid, EntityUid target, EmbeddableProjectileComponent component)
     {
-        public string Prototype;
-        public EntityCoordinates Coordinates;
+        TryComp<PhysicsComponent>(uid, out var physics);
+        _physics.SetLinearVelocity(uid, Vector2.Zero, body: physics);
+        _physics.SetBodyType(uid, BodyType.Static, body: physics);
+        var xform = Transform(uid);
+        _transform.SetParent(uid, xform, target);
 
-        public ImpactEffectEvent(string prototype, EntityCoordinates coordinates)
+        if (component.Offset != Vector2.Zero)
         {
-            Prototype = prototype;
-            Coordinates = coordinates;
+            _transform.SetLocalPosition(xform, xform.LocalPosition + xform.LocalRotation.RotateVec(component.Offset));
         }
+
+        if (component.Sound != null)
+        {
+            _audio.PlayPredicted(component.Sound, uid, null);
+        }
+    }
+
+    private void PreventCollision(EntityUid uid, ProjectileComponent component, ref PreventCollideEvent args)
+    {
+        if (component.IgnoreShooter && args.OtherEntity == component.Shooter)
+        {
+            args.Cancelled = true;
+        }
+    }
+
+    public void SetShooter(ProjectileComponent component, EntityUid uid)
+    {
+        if (component.Shooter == uid)
+            return;
+
+        component.Shooter = uid;
+        Dirty(uid, component);
+    }
+
+    [Serializable, NetSerializable]
+    private sealed class RemoveEmbeddedProjectileEvent : DoAfterEvent
+    {
+        public override DoAfterEvent Clone() => this;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class ImpactEffectEvent : EntityEventArgs
+{
+    public string Prototype;
+    public EntityCoordinates Coordinates;
+
+    public ImpactEffectEvent(string prototype, EntityCoordinates coordinates)
+    {
+        Prototype = prototype;
+        Coordinates = coordinates;
     }
 }
 
