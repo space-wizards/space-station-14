e2a73a622b15ab3ec08b420d973d3d818748ca02
Kevin Zheng <kevinz5000@gmail.com>
2023-08-05 17:45:19 -0700

    Adjust datafield names

--- Content.Server/Atmos/Components/HeatExchangerComponent.cs
+++ Content.Server/Atmos/Components/HeatExchangerComponent.cs
@@ -15,14 +15,14 @@ public sealed class HeatExchangerComponent : Component
     /// Pipe conductivity (mols/kPa/sec).
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("g")]
+    [DataField("conductivity")]
     public float G { get; set; } = 1f;
 
     /// <summary>
     /// Thermal convection coefficient (J/degK/sec).
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("k")]
+    [DataField("convectionCoefficient")]
     public float K { get; set; } = 8000f;
 
     /// <summary>
@@ -30,7 +30,7 @@ public sealed class HeatExchangerComponent : Component
     /// radiator radiates compared to superconductivity tile losses.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("alpha")]
+    [DataField("radiationCoefficient")]
     public float alpha { get; set; } = 400f;
 
     [ViewVariables(VVAccess.ReadWrite)]
