d89c6b46b103c277a54c14b0fd8a690b70dc646b
deltanedas <@deltanedas:kde.org>
2023-08-06 11:55:06 +0100

    fix updating :trollface:

--- Content.Server/Atmos/Miasma/RottingSystem.cs
+++ Content.Server/Atmos/Miasma/RottingSystem.cs
@@ -29,6 +29,7 @@ public sealed class RottingSystem : EntitySystem
     {
         base.Initialize();
 
+        SubscribeLocalEvent<PerishableComponent, ComponentStartup>(OnPerishableStartup);
         SubscribeLocalEvent<PerishableComponent, EntityUnpausedEvent>(OnPerishableUnpaused);
         SubscribeLocalEvent<PerishableComponent, MobStateChangedEvent>(OnMobStateChanged);
 
@@ -40,6 +41,11 @@ public sealed class RottingSystem : EntitySystem
         SubscribeLocalEvent<RottingComponent, RejuvenateEvent>(OnRejuvenate);
 
         SubscribeLocalEvent<TemperatureComponent, IsRottingEvent>(OnTempIsRotting);
+    }
+
+    private void OnPerishableStartup(EntityUid uid, PerishableComponent component, ComponentStartup args)
+    {
+        component.NextPerishUpdate = _timing.CurTime + component.PerishUpdateRate;
     }
 
     private void OnPerishableUnpaused(EntityUid uid, PerishableComponent component, ref EntityUnpausedEvent args)
