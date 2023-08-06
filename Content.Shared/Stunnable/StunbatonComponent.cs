3b99f9edc158d845db7e360e94e0549da1de099d
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-08-06 20:49:20 +0300

    predict stunbaton activation

--- Content.Shared/Stunnable/StunbatonComponent.cs
+++ Content.Shared/Stunnable/StunbatonComponent.cs
@@ -1,15 +1,20 @@
-using Content.Server.Stunnable.Systems;
+using Content.Shared.Stunnable;
 using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
 
 namespace Content.Server.Stunnable.Components;
 
-[RegisterComponent, Access(typeof(StunbatonSystem))]
-public sealed class StunbatonComponent : Component
+[RegisterComponent, NetworkedComponent]
+[AutoGenerateComponentState]
+[Access(typeof(SharedStunbatonSystem))]
+public sealed partial class StunbatonComponent : Component
 {
+    [DataField("activated"), ViewVariables(VVAccess.ReadWrite)]
+    [AutoNetworkedField]
     public bool Activated = false;
 
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("energyPerUse")]
+    [DataField("energyPerUse"), ViewVariables(VVAccess.ReadWrite)]
+    [AutoNetworkedField]
     public float EnergyPerUse = 350;
 
     [DataField("stunSound", required: true)]
