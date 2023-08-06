a47800f3298b4f05525ad77742787ac97d43581f
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-08-07 00:35:41 +0300

    Add standing & speed reset on component remove

--- Content.Server/Traits/Assorted/WheelchairBoundSystem.cs
+++ Content.Server/Traits/Assorted/WheelchairBoundSystem.cs
@@ -1,4 +1,5 @@
-﻿using Content.Shared.Buckle;
+﻿using Content.Shared.Body.Systems;
+using Content.Shared.Buckle;
 using Content.Shared.Buckle.Components;
 using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Systems;
@@ -12,10 +13,12 @@ public sealed class WheelchairBoundSystem : EntitySystem
     [Dependency] private readonly SharedBuckleSystem _buckleSystem = default!;
     [Dependency] private readonly MovementSpeedModifierSystem _movementSpeedModifierSystem = default!;
     [Dependency] private readonly StandingStateSystem _standingSystem = default!;
+    [Dependency] private readonly SharedBodySystem _bodySystem = default!;
 
     public override void Initialize()
     {
         SubscribeLocalEvent<WheelchairBoundComponent, ComponentStartup>(OnStartup);
+        SubscribeLocalEvent<WheelchairBoundComponent, ComponentShutdown>(OnShutdown);
         SubscribeLocalEvent<WheelchairBoundComponent, BuckleChangeEvent>(OnBuckleChange);
     }
 
@@ -25,8 +28,13 @@ public sealed class WheelchairBoundSystem : EntitySystem
         // TODO: Is there a better way to break legs? I don't know him, so for now, that's the way it is.
         _movementSpeedModifierSystem.ChangeBaseSpeed(uid, 0, 0, 20, movementSpeed);
         var wheelchair = Spawn(component.WheelchairPrototype, Transform(uid).Coordinates);
-        _standingSystem.Down(uid);
         _buckleSystem.TryBuckle(uid, uid, wheelchair);
+    }
+
+    private void OnShutdown(EntityUid uid, WheelchairBoundComponent component, ComponentShutdown args)
+    {
+        _standingSystem.Stand(uid);
+        _bodySystem.UpdateMovementSpeed(uid);
     }
 
     private void OnBuckleChange(EntityUid uid, WheelchairBoundComponent component, ref BuckleChangeEvent args)
