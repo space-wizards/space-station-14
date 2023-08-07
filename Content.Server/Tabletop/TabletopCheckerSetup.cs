839609d9fd22db41236f079a8b4f6655b2704097
Onoira <onoira@psiko.zone>
2023-08-07 16:14:12 +0200

    softcode proto ids + rename crowns

--- Content.Server/Tabletop/TabletopCheckerSetup.cs
+++ Content.Server/Tabletop/TabletopCheckerSetup.cs
@@ -1,11 +1,26 @@
 using JetBrains.Annotations;
 using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Server.Tabletop
 {
     [UsedImplicitly]
     public sealed class TabletopCheckerSetup : TabletopSetup
     {
+
+        [DataField("prototypePieceWhite", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string PrototypePieceWhite = default!;
+
+        [DataField("prototypeCrownWhite", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string PrototypeCrownWhite = default!;
+
+        [DataField("prototypePieceBlack", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string PrototypePieceBlack = default!;
+
+        [DataField("prototypeCrownBlack", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string PrototypeCrownBlack = default!;
+
         private const float Separation = 1f;
 
         public override void SetupTabletop(TabletopSession session, IEntityManager entityManager)
@@ -17,16 +32,16 @@ namespace Content.Server.Tabletop
             SpawnPieces(session, entityManager, session.Position.Offset(-4.5f, 3.5f));
         }
 
-        private static void SpawnPieces(TabletopSession session, IEntityManager entityManager, MapCoordinates topLeft)
+        private void SpawnPieces(TabletopSession session, IEntityManager entityManager, MapCoordinates topLeft)
         {
             var (mapId, x, y) = topLeft;
             var pieces = new EntityUid[42];
             var pieceIndex = 0;
 
-            void SpawnPieces(string color, MapCoordinates left)
+            void SpawnPieces(string protoId, MapCoordinates left)
             {
                 var (mapId, x, y) = left;
-                var reversed = ("White" == color) ? 1 : -1;
+                var reversed = (PrototypePieceWhite == protoId) ? 1 : -1;
 
                 for (var i = 0; i < 3; i++)
                 {
@@ -38,7 +53,7 @@ namespace Content.Server.Tabletop
                         // Prevents an extra piece on the middle row
                         if (x_offset + j > 8) continue;
 
-                        pieces[pieceIndex] = entityManager.SpawnEntity($"{color}CheckerPiece",
+                        pieces[pieceIndex] = entityManager.SpawnEntity(protoId,
                             new MapCoordinates(x + (j + x_offset) * Separation, y + i * reversed * Separation, mapId)
                         );
                         pieceIndex++;
@@ -46,16 +61,16 @@ namespace Content.Server.Tabletop
                 }
             }
 
-            SpawnPieces("Black", topLeft);
-            SpawnPieces("White", new MapCoordinates(x, y - 7 * Separation, mapId));
+            SpawnPieces(PrototypePieceBlack, topLeft);
+            SpawnPieces(PrototypePieceWhite, new MapCoordinates(x, y - 7 * Separation, mapId));
 
-            // Queens
+            // Crowns
             for (var i = 1; i < 4; i++)
             {
-                pieces[pieceIndex] = entityManager.SpawnEntity("BlackCheckerQueen",
+                pieces[pieceIndex] = entityManager.SpawnEntity(PrototypeCrownBlack,
                     new MapCoordinates(x + 9 * Separation + 9f / 32, y - (i - 1) * Separation, mapId)
                 );
-                pieces[pieceIndex + 1] = entityManager.SpawnEntity("WhiteCheckerQueen",
+                pieces[pieceIndex + 1] = entityManager.SpawnEntity(PrototypeCrownWhite,
                     new MapCoordinates(x + 8 * Separation + 9f / 32, y - (i - 1) * Separation, mapId)
                 );
                 pieceIndex += 2;
@@ -65,10 +80,10 @@ namespace Content.Server.Tabletop
             for (var i = 1; i < 7; i++)
             {
                 var step = 3 + 0.25f * (i - 1);
-                pieces[pieceIndex] = entityManager.SpawnEntity("BlackCheckerPiece",
+                pieces[pieceIndex] = entityManager.SpawnEntity(PrototypePieceBlack,
                   new MapCoordinates(x + 9 * Separation + 9f / 32, y - step * Separation, mapId)
                 );
-                pieces[pieceIndex] = entityManager.SpawnEntity("WhiteCheckerPiece",
+                pieces[pieceIndex] = entityManager.SpawnEntity(PrototypePieceWhite,
                   new MapCoordinates(x + 8 * Separation + 9f / 32, y - step * Separation, mapId)
                 );
                 pieceIndex += 2;
