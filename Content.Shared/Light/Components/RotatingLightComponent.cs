88c648041c58fec4e3c05252b1db9aecea73516e
deltanedas <@deltanedas:kde.org>
2023-08-08 11:03:07 +0100

    what SCHIZO named the dir Component not Components

---
+++ Content.Shared/Light/Components/RotatingLightComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Light.Components;
+
+/// <summary>
+/// Animates a point light's rotation while this component is on the light.
+/// All animation is done in the client system.
+/// </summary>
+[RegisterComponent]
+public sealed class RotatingLightComponent : Component
+{
+}
---
+++ Content.Shared/Light/Components/SharedEmergencyLightComponent.cs
@@ -0,0 +1,25 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Light.Components;
+
+/// <summary>
+/// Handles station alert level and power changes for emergency lights.
+/// All logic is serverside, animation is handled by <see cref="RotatingLightComponent"/>.
+/// </summary>
+public abstract class SharedEmergencyLightComponent : Component
+{
+}
+
+[Serializable, NetSerializable]
+public enum EmergencyLightVisuals
+{
+    On,
+    Color
+}
+
+public enum EmergencyLightVisualLayers
+{
+    Base,
+    LightOff,
+    LightOn,
+}
