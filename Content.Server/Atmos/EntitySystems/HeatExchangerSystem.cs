00b1107485958763d9afe3d39c40e24d1a8f676a
Ilya246 <ilyukarno@gmail.com>
2023-08-08 11:11:48 +0400

    clarity

--- Content.Server/Atmos/EntitySystems/HeatExchangerSystem.cs
+++ Content.Server/Atmos/EntitySystems/HeatExchangerSystem.cs
@@ -58,8 +58,11 @@ public sealed class HeatExchangerSystem : EntitySystem
         // Positive dN flows from inlet to outlet
         var dt = (float)(_gameTiming.CurTime - device.LastProcess).TotalSeconds;
         var dP = inlet.Air.Pressure - outlet.Air.Pressure;
-        var dN = dP * Math.Min(comp.G*dt, 1/Atmospherics.R/outlet.Air.Temperature/(1/inlet.Air.Volume + 1/outlet.Air.Volume));
-        // will not transfer any more mols of gas than what would equalise pressure
+        var transferLimit = 1/Atmospherics.R/outlet.Air.Temperature/(1/inlet.Air.Volume + 1/outlet.Air.Volume);
+        var dN = dP*Math.Min(comp.G*dt, transferLimit);
+        // Will not transfer any more mols of gas than what would equalise pressure:
+        // transferLimit = (n2T2/V2 - n1T1/V1)/(T2/V1 + T2/V2) = Δp/R/T2/(1/V1 + 1/V2)
+        // Note that Δp is factored out; this will not have issues with negative Δp
 
         GasMixture xfer;
         if (dN > 0)
