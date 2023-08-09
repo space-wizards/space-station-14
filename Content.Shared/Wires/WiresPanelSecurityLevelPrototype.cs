62b2f8158a79d4430771cb91a9e60b21efd59055
chromiumboy <50505512+chromiumboy@users.noreply.github.com>
2023-08-09 14:59:35 -0500

    Update WiresPanelSecurityLevelPrototype.cs

--- Content.Shared/Wires/WiresPanelSecurityLevelPrototype.cs
+++ Content.Shared/Wires/WiresPanelSecurityLevelPrototype.cs
@@ -1,17 +1,16 @@
 using Robust.Shared.Prototypes;
 
-namespace Content.Shared.Wires
+namespace Content.Shared.Wires;
+
+[Prototype("WiresPanelSecurityLevel")]
+public sealed class WiresPanelSecurityLevelPrototype : IPrototype
 {
-    [Prototype("WiresPanelSecurityLevel")]
-    public sealed class WiresPanelSecurityLevelPrototype : IPrototype
-    {
-        [IdDataField]
-        public string ID { get; } = default!;
+    [IdDataField]
+    public string ID { get; } = default!;
 
-        [DataField("examine")]
-        public string? Examine = default!;
+    [DataField("examine")]
+    public string? Examine = default!;
 
-        [DataField("wiresAccessible")]
-        public bool WiresAccessible = true;
-    }
+    [DataField("wiresAccessible")]
+    public bool WiresAccessible = true;
 }
