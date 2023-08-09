cff9e5d80974b976460904145b4fa9877b5135a5
chromiumboy <50505512+chromiumboy@users.noreply.github.com>
2023-08-09 15:34:07 -0500

    Update AttemptElectrocute.cs
    File scoped namespace

--- Content.Server/Construction/Completions/AttemptElectrocute.cs
+++ Content.Server/Construction/Completions/AttemptElectrocute.cs
@@ -1,25 +1,24 @@
 using Content.Server.Electrocution;
 using Content.Shared.Construction;
 
-namespace Content.Server.Construction.Completions
+namespace Content.Server.Construction.Completions;
+
+[DataDefinition]
+public sealed class AttemptElectrocute : IGraphAction
 {
-    [DataDefinition]
-    public sealed class AttemptElectrocute : IGraphAction
+    public void PerformAction(EntityUid uid, EntityUid? userUid, IEntityManager entityManager)
     {
-        public void PerformAction(EntityUid uid, EntityUid? userUid, IEntityManager entityManager)
-        {
-            if (userUid == null)
-                return;
+        if (userUid == null)
+            return;
 
-            if (!entityManager.TryGetComponent<ElectrifiedComponent>(uid, out var electrified))
-                return;
+        if (!entityManager.TryGetComponent<ElectrifiedComponent>(uid, out var electrified))
+            return;
 
-            bool currentValue = electrified.Enabled;
-            electrified.Enabled = true;
+        bool currentValue = electrified.Enabled;
+        electrified.Enabled = true;
 
-            entityManager.EntitySysManager.GetEntitySystem<ElectrocutionSystem>().TryDoElectrifiedAct(uid, userUid.Value);
+        entityManager.EntitySysManager.GetEntitySystem<ElectrocutionSystem>().TryDoElectrifiedAct(uid, userUid.Value);
 
-            electrified.Enabled = currentValue;
-        }
+        electrified.Enabled = currentValue;
     }
 }
