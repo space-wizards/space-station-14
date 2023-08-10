219677b95d2977a59c37cb72f6eb08918ec5aef4
DrSmugleaf <drsmugleaf@gmail.com>
2023-08-10 02:01:04 -0700

    Add ahelp typing indicator for admins

--- Content.Client/Administration/Systems/BwoinkSystem.cs
+++ Content.Client/Administration/Systems/BwoinkSystem.cs
@@ -2,13 +2,17 @@
 using Content.Shared.Administration;
 using JetBrains.Annotations;
 using Robust.Shared.Network;
+using Robust.Shared.Timing;
 
 namespace Content.Client.Administration.Systems
 {
     [UsedImplicitly]
     public sealed class BwoinkSystem : SharedBwoinkSystem
     {
+        [Dependency] private readonly IGameTiming _timing = default!;
+
         public event EventHandler<BwoinkTextMessage>? OnBwoinkTextMessageRecieved;
+        private TimeSpan _lastTypingUpdateSent;
 
         protected override void OnBwoinkTextMessage(BwoinkTextMessage message, EntitySessionEventArgs eventArgs)
         {
@@ -20,6 +24,15 @@ namespace Content.Client.Administration.Systems
             // Reuse the channel ID as the 'true sender'.
             // Server will ignore this and if someone makes it not ignore this (which is bad, allows impersonation!!!), that will help.
             RaiseNetworkEvent(new BwoinkTextMessage(channelId, channelId, text));
+        }
+
+        public void SendInputTextUpdated(NetUserId channel, string text)
+        {
+            if (_lastTypingUpdateSent + TimeSpan.FromSeconds(3) > _timing.RealTime)
+                return;
+
+            _lastTypingUpdateSent = _timing.RealTime;
+            RaiseNetworkEvent(new BwoinkClientTypingUpdated(channel, text.Length > 0));
         }
     }
 }
