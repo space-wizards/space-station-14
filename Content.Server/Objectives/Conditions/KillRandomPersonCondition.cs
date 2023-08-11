f027851017f2036100919f18ceade6e13fda98ed
deltanedas <@deltanedas:kde.org>
2023-08-11 15:08:41 +0100

    file scope

--- Content.Server/Objectives/Conditions/KillRandomPersonCondition.cs
+++ Content.Server/Objectives/Conditions/KillRandomPersonCondition.cs
@@ -2,37 +2,34 @@ using Content.Server.Mind.Components;
 using Content.Server.Objectives.Interfaces;
 using Content.Shared.Humanoid;
 using Content.Shared.Mobs.Components;
-using JetBrains.Annotations;
 using Robust.Shared.Random;
 
-namespace Content.Server.Objectives.Conditions
+namespace Content.Server.Objectives.Conditions;
+
+[DataDefinition]
+public sealed class KillRandomPersonCondition : KillPersonCondition
 {
-    [UsedImplicitly]
-    [DataDefinition]
-    public sealed class KillRandomPersonCondition : KillPersonCondition
+    public override IObjectiveCondition GetAssigned(Mind.Mind mind)
     {
-        public override IObjectiveCondition GetAssigned(Mind.Mind mind)
+        var allHumans = new List<Mind.Mind>();
+        var query = EntityManager.EntityQuery<MindContainerComponent, HumanoidAppearanceComponent>(true);
+        while (query.MoveNext(out var mc, out _))
         {
-            var allHumans = new List<Mind.Mind>();
-            var query = EntityManager.EntityQuery<MindContainerComponent, HumanoidAppearanceComponent>(true);
-            while (query.MoveNext(out var mc, out _))
+            var entity = mc.Mind?.OwnedEntity;
+            if (entity == default)
+                return false;
+
+            if (EntityManager.TryGetComponent(entity, out MobStateComponent? mobState) &&
+                MobStateSystem.IsAlive(entity.Value, mobState) &&
+                mc.Mind != mind)
             {
-                var entity = mc.Mind?.OwnedEntity;
-                if (entity == default)
-                    return false;
-
-                if (EntityManager.TryGetComponent(entity, out MobStateComponent? mobState) &&
-                    MobStateSystem.IsAlive(entity.Value, mobState) &&
-                    mc.Mind != mind)
-                {
-                    allHumans.Add(mc.Mind);
-                }
+                allHumans.Add(mc.Mind);
             }
-
-            if (allHumans.Count == 0)
-                return new DieCondition(); // I guess I'll die
-
-            return new KillRandomPersonCondition {Target = IoCManager.Resolve<IRobustRandom>().Pick(allHumans)};
         }
+
+        if (allHumans.Count == 0)
+            return new DieCondition(); // I guess I'll die
+
+        return new KillRandomPersonCondition {Target = IoCManager.Resolve<IRobustRandom>().Pick(allHumans)};
     }
 }
