86306319b801092f5b50d20d062f7cb3838e5f38
unknown <shadowjayofdarkness@gmail.com>
2023-08-13 14:30:32 -0700

    Added missing spaces to the component names for the guidebook

--- Content.Server/Chemistry/ReagentEffects/RemoveComponent.cs
+++ Content.Server/Chemistry/ReagentEffects/RemoveComponent.cs
@@ -2,6 +2,7 @@ using Content.Shared.Chemistry.Reagent;
 using Content.Shared.Localizations;
 using JetBrains.Annotations;
 using Robust.Shared.Prototypes;
+using System.Text;
 
 namespace Content.Server.Chemistry.ReagentEffects;
 
@@ -24,7 +25,18 @@ public sealed class RemoveComponent : ReagentEffect
 
         foreach (var entry in Components)
         {
-            guidebookComponents.Add(entry.ToString().ToLower() + 's');
+            var entryText = entry.ToString(); // Not really sure if this is necessary but just to be safe
+            StringBuilder formattedText = new StringBuilder(entryText.Length * 2);
+            formattedText.Append(entryText[0]);
+            for (int i = 1;
+                i < entryText.Length;
+                i++)
+            {
+                if (char.IsUpper(entryText[i]) && entryText[i - 1] != ' ')
+                    formattedText.Append(' ');
+                formattedText.Append(entryText[i]);
+            }
+            guidebookComponents.Add(formattedText.ToString().ToLower() + 's');
         }
         return Loc.GetString("reagent-effect-guidebook-remove-component",
             ("chance", Probability),
