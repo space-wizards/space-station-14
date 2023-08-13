425c5d093e1d5c0656598da62a0c324aeaa0916e
Fahasor <rad.nikita@list.ru>
2023-08-13 19:25:05 +0300

    Documented LabelComponent

--- Content.Server/Labels/Label/Components/LabelComponent.cs
+++ Content.Server/Labels/Label/Components/LabelComponent.cs
@@ -1,12 +1,21 @@
 namespace Content.Server.Labels.Components
 {
+    /// <summary>
+    /// Gives opportunity to add labels by entities with the <see cref="HandLabelerComponent"/>
+    /// </summary>
     [RegisterComponent]
     public sealed class LabelComponent : Component
     {
+        /// <summary>
+        /// Label text. If you using that field in your prototype, you must set originalName field too, and set name of entity like it with label
+        /// </summary>
         [ViewVariables(VVAccess.ReadWrite)]
         [DataField("currentLabel")]
         public string? CurrentLabel { get; set; }
 
+        /// <summary>
+        /// Name of entity without label
+        /// </summary>
         [DataField("originalName")]
         public string? OriginalName { get; set; }
     }
