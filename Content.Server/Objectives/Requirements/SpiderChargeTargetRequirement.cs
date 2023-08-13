ba01acd0076ba7a8591b4f4e8e03452a82fa4518
deltanedas <@deltanedas:kde.org>
2023-08-13 16:08:51 +0100

    remove linq 1

--- Content.Server/Objectives/Requirements/SpiderChargeTargetRequirement.cs
+++ Content.Server/Objectives/Requirements/SpiderChargeTargetRequirement.cs
@@ -1,7 +1,6 @@
 ﻿using Content.Server.Mind;
 using Content.Server.Objectives.Interfaces;
 using Content.Server.Roles;
-using System.Linq;
 
 namespace Content.Server.Objectives.Requirements;
 
@@ -10,12 +9,16 @@ public sealed class SpiderChargeTargetRequirement : IObjectiveRequirement
 {
     public bool CanBeAssigned(Mind.Mind mind)
     {
-        var role = mind.Roles.Where(role => role is NinjaRole).FirstOrDefault();
-        if (role == null)
-            return false;
+        foreach (var role in mind.Roles)
+        {
+            if (role is NinjaRole ninja)
+            {
+                // if ninja is on dev (no warps) dont tell it to blow up... somewhere?
+                // the charge can still be used but its not an obj
+                return ninja.SpiderChargeTarget != null;
+            }
+        }
 
-        // if ninja is on dev (no warps) dont tell it to blow up... somewhere?
-        // the charge can still be used but its not an obj
-        return ((NinjaRole) role).SpiderChargeTarget != null;
+        return false;
     }
 }
