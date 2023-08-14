444e1e21e91991933d9fb4a23240649b599e1a32
deltanedas <@deltanedas:kde.org>
2023-08-14 20:50:29 +0100

    mech intake/filtering

---
+++ Content.Server/Mech/Components/MechAirFilterComponent.cs
@@ -0,0 +1,32 @@
+﻿using Content.Shared.Atmos;
+
+namespace Content.Server.Mech.Components;
+
+/// <summary>
+/// This is basically a reverse scrubber for MechAir
+/// </summary>
+[RegisterComponent]
+public sealed class MechAirFilterComponent : Component
+{
+    /// <summary>
+    /// Gases that will be filtered out of internal air
+    /// </summary>
+    [DataField("gases")]
+    public HashSet<Gas> Gases = new HashSet<Gas>
+    {
+        Gas.CarbonDioxide,
+        Gas.Plasma,
+        Gas.Tritium,
+        Gas.WaterVapor,
+        Gas.Miasma,
+        Gas.NitrousOxide,
+        Gas.Frezon
+        //Gas.Helium4
+    };
+
+    /// <summary>
+    /// Target volume to transfer every second.
+    /// </summary>
+    [DataField("transferRate")]
+    public float TransferRate = Atmospherics.MaxTransferRate;
+}
