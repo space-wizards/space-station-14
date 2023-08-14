1ae3e3cdee3aadb275bb10e0134caa4a8b2c3c66
mhamster <coshatnik2006@mail.ru>
2023-08-14 11:29:33 +0700

    Change datafields
    + Datafield "fireOnIgnite" => "firestacksOnIgnite"
    + Datafield "extinguishable" => "canExtinguish"
    + "extinguishable" => "canExtinguish"

--- Content.Server/Atmos/Components/FlammableComponent.cs
+++ Content.Server/Atmos/Components/FlammableComponent.cs
@@ -47,14 +47,14 @@ namespace Content.Server.Atmos.Components
         ///     Can the component anyhow lose its FireStacks?
         /// </summary>
         [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("extinguishable")]
+        [DataField("canExtinguish")]
         public bool CanExtinguish = true;
 
         /// <summary>
         ///     How many firestacks should be applied to component when being set on fire?
         /// </summary>
         [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("fireOnIgnite")]
+        [DataField("firestacksOnIgnite")]
         public float FirestacksOnIgnite = 2.0f;
     }
 }
