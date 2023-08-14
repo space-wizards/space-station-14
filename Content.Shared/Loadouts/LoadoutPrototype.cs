6bc8c4ab4010f2a071eafca8ce4c9ba2334bec68
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-08-14 16:36:04 -0700

    reorganize prototype

--- Content.Shared/Loadouts/LoadoutPrototype.cs
+++ Content.Shared/Loadouts/LoadoutPrototype.cs
@@ -17,37 +17,39 @@ namespace Content.Shared.Loadouts
         ///     The name of this loadout.
         /// </summary>
         [DataField("name")]
-        public string Name { get; private set; } = "";
+        public string Name = "";
 
         /// <summary>
         ///     The description of this loadout.
         /// </summary>
         [DataField("description")]
-        public string? Description { get; private set; }
-
-        /// <summary>
-        ///     Need one of these jobs to give loadout.
-        /// </summary>
-        [DataField("jobWhitelist")]
-        public List<string>? JobWhitelist { get; private set; }
-
-        /// <summary>
-        ///     Need none of these jobs to give loadout.
-        /// </summary>
-        [DataField("jobBlacklist")]
-        public List<string>? JobBlacklist { get; private set; }
+        public string? Description;
 
         /// <summary>
         ///     Which tab category to put this under.
         /// </summary>
         [DataField("category")]
-        public string Category { get; private set; } = "Uncategorized";
+        public string Category = "Uncategorized";
+
+        /// <summary>
+        ///     The item to give.
+        /// </summary>
+        [DataField("item")]
+        public string? Item;
+
 
         /// <summary>
         ///     The point cost of this loadout.
         /// </summary>
         [DataField("cost")]
         public int Cost = 1;
+
+        /// <summary>
+        ///     Should this item override other items in the same slot?
+        /// </summary>
+        [DataField("exclusive")]
+        public bool Exclusive;
+
 
         /// <summary>
         ///     Don't apply this loadout to entities this whitelist IS NOT valid for.
@@ -62,12 +64,27 @@ namespace Content.Shared.Loadouts
         public EntityWhitelist? Blacklist;
 
         /// <summary>
-        ///     Should this item override other items in the same slot?
+        ///     Need one of these jobs to give loadout.
         /// </summary>
-        [DataField("exclusive")]
-        public bool Exclusive = false;
+        [DataField("jobWhitelist")]
+        public List<string>? JobWhitelist;
 
-        [DataField("item")]
-        public string? Item;
+        /// <summary>
+        ///     Need none of these jobs to give loadout.
+        /// </summary>
+        [DataField("jobBlacklist")]
+        public List<string>? JobBlacklist;
+
+        /// <summary>
+        ///     Don't apply this loadout to entities this whitelist IS NOT valid for.
+        /// </summary>
+        [DataField("speciesWhitelist")]
+        public List<string>? SpeciesWhitelist;
+
+        /// <summary>
+        ///     Don't apply this loadout to entities this whitelist IS valid for. (hence, a blacklist)
+        /// </summary>
+        [DataField("speciesBlacklist")]
+        public List<string>? SpeciesBlacklist;
     }
 }
