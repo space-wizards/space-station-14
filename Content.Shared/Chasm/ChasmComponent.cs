96cfc10544dcf7c480d9b78e0577e7584be96aa5
Kara <lunarautomaton6@gmail.com>
2023-08-14 14:16:47 -0500

    Animation + deletion logic

---
+++ Content.Shared/Chasm/ChasmComponent.cs
@@ -0,0 +1,11 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Chasm;
+
+/// <summary>
+///     Marks a component that will cause entities to fall into them on a step trigger activation
+/// </summary>
+[NetworkedComponent, RegisterComponent, Access(typeof(ChasmSystem))]
+public sealed class ChasmComponent : Component
+{
+}
