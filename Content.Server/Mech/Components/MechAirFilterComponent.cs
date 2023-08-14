6efe6f4820fb5d69045195f5f2a99de7ba04bd6d
deltanedas <@deltanedas:kde.org>
2023-08-14 21:21:56 +0100

    filter slower

--- Content.Server/Mech/Components/MechAirFilterComponent.cs
+++ Content.Server/Mech/Components/MechAirFilterComponent.cs
@@ -28,5 +28,5 @@ public sealed class MechAirFilterComponent : Component
     /// Target volume to transfer every second.
     /// </summary>
     [DataField("transferRate")]
-    public float TransferRate = Atmospherics.MaxTransferRate;
+    public float TransferRate = MechAirComponent.GasMixVolume * 0.1f;
 }
