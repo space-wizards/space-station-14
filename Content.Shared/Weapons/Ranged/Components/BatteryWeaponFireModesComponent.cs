bf2fa75ca22fe68c3ef535f8c99ec0ff4fabb77a
chromiumboy <chromium.boy@gmail.com>
2023-08-14 23:22:47 -0500

    Added comments

--- Content.Shared/Weapons/Ranged/Components/BatteryWeaponFireModesComponent.cs
+++ Content.Shared/Weapons/Ranged/Components/BatteryWeaponFireModesComponent.cs
@@ -2,32 +2,45 @@ using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace Content.Shared.Weapons.Ranged.Components;
 
+/// <summary>
+/// Allows battery weapons to fire different types of projectiles
+/// </summary>
 [RegisterComponent, NetworkedComponent]
 [AutoGenerateComponentState]
 public sealed partial class BatteryWeaponFireModesComponent : Component
 {
+    /// <summary>
+    /// A list of the different firing modes the weapon can switch between
+    /// </summary>
     [DataField("fireModes", required: true)]
     [AutoNetworkedField]
     public List<BatteryWeaponFireMode> FireModes = new();
 
+    /// <summary>
+    /// The currently selected firing mode
+    /// </summary>
     [DataField("currentFireMode")]
     [AutoNetworkedField]
-    public BatteryWeaponFireMode CurrentFireMode = default!;
+    public BatteryWeaponFireMode? CurrentFireMode = default!;
 }
 
 [DataDefinition]
 [Serializable, NetSerializable]
 public sealed class BatteryWeaponFireMode
 {
+    /// <summary>
+    /// The projectile prototype associated with this firing mode 
+    /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("proto", required: true, customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
     public string Prototype = default!;
 
+    /// <summary>
+    /// The battery cost to fire the projectile associated with this firing mode
+    /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("fireCost")]
     public float FireCost = 100;
