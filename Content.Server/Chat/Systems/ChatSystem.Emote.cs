6222f6caba46a9e41185dca1404be51b7faab89e
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-08-18 00:15:00 +1000

    Fix emote chirps

--- Content.Server/Chat/Systems/ChatSystem.Emote.cs
+++ Content.Server/Chat/Systems/ChatSystem.Emote.cs
@@ -92,7 +92,7 @@ public partial class ChatSystem
         {
             // not all emotes are loc'd, but for the ones that are we pass in entity
             var action = Loc.GetString(_random.Pick(emote.ChatMessages), ("entity", source));
-            SendEntityEmote(source, action, range, nameOverride, false, hideLog, ignoreActionBlocker);
+            SendEntityEmote(source, action, range, nameOverride, hideLog: hideLog, ignoreActionBlocker);
         }
 
         // do the rest of emote event logic here
