a8b36b924aab4bbb872731377cdec4a8283b8b3c
Jeff <velcroboy333@hotmail.com>
2023-08-17 10:41:24 -0500

    Make shuttle walls/windows constructable/deconstructable

---
+++ Content.Shared/Wall/ShuttleWallVisuals.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Wall
+{
+    [Serializable, NetSerializable]
+    public enum ShuttleWallVisuals
+    {
+        DeconstructionStage,
+    }
+}
