a77bd77c932c0c097c69d389724c4e648b6cff43
faint <46868845+ficcialfaint@users.noreply.github.com>
2023-08-18 07:13:04 +0300

    Tabs

--- Content.Client/Message/RichTextLabelExt.cs
+++ Content.Client/Message/RichTextLabelExt.cs
@@ -5,9 +5,9 @@ namespace Content.Client.Message;
 
 public static class RichTextLabelExt
 {
-	public static RichTextLabel SetMarkup(this RichTextLabel label, string markup)
-	{
-		label.SetMessage(FormattedMessage.FromMarkup(markup));
-		return label;
+    public static RichTextLabel SetMarkup(this RichTextLabel label, string markup)
+    {
+        label.SetMessage(FormattedMessage.FromMarkup(markup));
+        return label;
 	}
 }
