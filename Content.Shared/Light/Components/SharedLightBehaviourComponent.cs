dfbc3f741f25e3dd8067aa2162a17acde108cac9
deltanedas <@deltanedas:kde.org>
2023-08-19 20:20:00 +0100

    rename namespace

---
+++ Content.Shared/Light/Components/SharedLightBehaviourComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Shared.Light.Components;
+/// <summary>
+/// A component which applies a specific behaviour to a PointLightComponent on its owner.
+/// </summary>
+public abstract class SharedLightBehaviourComponent : Component
+{
+}
