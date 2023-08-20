61457fa2e5831189042152620214a3d164c96b5b
ThunderBear2006 <bearthunder06@gmail.com>
2023-08-20 14:46:06 -0400

    pow pow

--- Content.Server/Mech/Equipment/EntitySystems/MechGunSystem.cs
+++ Content.Server/Mech/Equipment/EntitySystems/MechGunSystem.cs
@@ -1,5 +1,8 @@
 using System.Numerics;
 using Content.Server.Mech.Equipment.Components;
+using Content.Server.Mech.Systems;
+using Content.Server.Power.Components;
+using Content.Server.Power.EntitySystems;
 using Content.Server.Weapons.Ranged.Systems;
 using Content.Shared.Mech.Components;
 using Content.Shared.Mech.Equipment.Components;
@@ -14,8 +17,9 @@ namespace Content.Server.Mech.Equipment.EntitySystems;
 public sealed class MechGunSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _random = default!;
-    [Dependency] public readonly IPrototypeManager PrototypeManager = default!;
     [Dependency] private readonly ThrowingSystem _throwing = default!;
+    [Dependency] private readonly MechSystem _mech = default!;
+    [Dependency] private readonly BatterySystem _battery = default!;
 
     public override void Initialize()
     {
@@ -31,6 +35,12 @@ public sealed class MechGunSystem : EntitySystem
         if (!TryComp<MechComponent>(component.EquipmentOwner.Value, out var mech))
             return;
 
+        if (TryComp<BatteryComponent>(uid, out var battery))
+        {
+            ChargeGunBattery(uid, battery);
+            return;
+        }
+
         var equipment = new List<EntityUid>(mech.EquipmentContainer.ContainedEntities);
         foreach (var ent in equipment)
         {
@@ -40,5 +50,27 @@ public sealed class MechGunSystem : EntitySystem
                 _throwing.TryThrow(ent, _random.NextVector2(), _random.Next(5));
             }
         }
+    }
+
+    private void ChargeGunBattery(EntityUid uid, BatteryComponent component)
+    {
+        if (!TryComp<MechEquipmentComponent>(uid, out var mechEquipment) || !mechEquipment.EquipmentOwner.HasValue)
+            return;
+
+        if (!TryComp<MechComponent>(mechEquipment.EquipmentOwner.Value, out var mech))
+            return;
+
+        var maxCharge = component.MaxCharge;
+        var currentCharge = component.CurrentCharge;
+
+        var chargeDelta = maxCharge - currentCharge;
+
+        if (chargeDelta < 0 || mech.Energy - chargeDelta < 0)
+            return;
+
+        if (!_mech.TryChangeEnergy(mechEquipment.EquipmentOwner.Value, -chargeDelta, mech))
+            return;
+
+        _battery.SetCharge(uid, component.MaxCharge, component);
     }
 }
