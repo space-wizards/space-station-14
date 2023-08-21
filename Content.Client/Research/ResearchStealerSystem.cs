d45296cc6475f51d747acde0293865f3a35fa1b5
deltanedas <@deltanedas:kde.org>
2023-08-21 17:12:57 +0100

    client system

---
+++ Content.Client/Research/ResearchStealerSystem.cs
@@ -0,0 +1,10 @@
+using Content.Shared.Research.Systems;
+
+namespace Content.Client.Research;
+
+/// <summary>
+/// Does nothing special, only exists to provide a client implementation.
+/// </summary>
+public sealed class ResearchStealerSystem : EntitySystem
+{
+}
