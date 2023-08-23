a86972480bd46bab9473c3a0306ae39ffc84bc12
Kaede <malchanceux@protonmail.com>
2023-08-24 01:05:52 +0300

    Review

--- Content.Server/Chemistry/ReagentEffects/Polymorph.cs
+++ Content.Server/Chemistry/ReagentEffects/Polymorph.cs
@@ -2,8 +2,10 @@ using Content.Server.Mind.Components;
 using Content.Server.Polymorph.Components;
 using Content.Server.Polymorph.Systems;
 using Content.Shared.Chemistry.Reagent;
+using Content.Shared.Item;
 using Content.Shared.Polymorph;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Server.Chemistry.ReagentEffects;
 
@@ -12,23 +14,19 @@ public sealed partial class Polymorph : ReagentEffect
     /// <summary>
     ///     What polymorph prototype is used on effect
     /// </summary>
-    [DataField("prototype")] public string PolymorphPrototype { get; set; }
+    [DataField("prototype", customTypeSerializer:typeof(PrototypeIdSerializer<PolymorphPrototype>))]
+    public string PolymorphPrototype { get; set; }
 
     protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-        => Loc.GetString("reagent-effect-guidebook-make-polymorph",
-            ("chance", Probability), ("protoname", prototype.Index<PolymorphPrototype>(PolymorphPrototype)));
+    => Loc.GetString("reagent-effect-guidebook-make-polymorph",
+            ("chance", Probability), ("protoname",
+                prototype.Index<EntityPrototype>(prototype.Index<PolymorphPrototype>(PolymorphPrototype).Entity).Name));
 
-    public override void Effect(ReagentEffectArgs args)
+            public override void Effect(ReagentEffectArgs args)
     {
         var entityManager = args.EntityManager;
         var uid = args.SolutionEntity;
-        var polySystem = entityManager.EntitySysManager.GetEntitySystem<PolymorphSystem>();
-
-        // Check if the thing is actually sentient so we dont turn actual plants into trees
-        if (!entityManager.HasComponent<MindContainerComponent>(uid))
-        {
-            return;
-        }
+        var polySystem = entityManager.System<PolymorphSystem>();
 
         // Make it into a prototype
         entityManager.EnsureComponent<PolymorphableComponent>(uid);
