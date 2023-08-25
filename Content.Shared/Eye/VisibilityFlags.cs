280fc10a9991ae8bf81693b97e07db3798f10204
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-08-26 00:09:34 +1000

    Fix vis flags

---
+++ Content.Shared/Eye/VisibilityFlags.cs
@@ -0,0 +1,13 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Eye
+{
+    [Flags]
+    [FlagsFor(typeof(VisibilityMaskLayer))]
+    public enum VisibilityFlags : int
+    {
+        None   = 0,
+        Normal = 1 << 0,
+        Ghost  = 1 << 1,
+    }
+}
