d5cd6b3729c12b456d1622cdbf44203af2d7fcab
EmoGarbage404 <retron404@gmail.com>
2023-08-26 16:21:40 -0400

    Add custom control support for the character menu

--- Content.Client/CharacterInfo/CharacterInfoSystem.cs
+++ Content.Client/CharacterInfo/CharacterInfoSystem.cs
@@ -1,7 +1,11 @@
 ﻿using Content.Shared.CharacterInfo;
 using Content.Shared.Objectives;
+using Content.Shared.Zombies;
 using Robust.Client.GameObjects;
 using Robust.Client.Player;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Containers;
 
 namespace Content.Client.CharacterInfo;
 
@@ -19,6 +23,26 @@ public sealed class CharacterInfoSystem : EntitySystem
         SubscribeLocalEvent<PlayerAttachSysMessage>(OnPlayerAttached);
 
         SubscribeNetworkEvent<CharacterInfoEvent>(OnCharacterInfoEvent);
+
+        //debug tests
+        SubscribeLocalEvent<ZombieComponent, GetCharacterInfoControlsEvent>(ZombieTest);
+    }
+
+    private void ZombieTest(EntityUid uid, ZombieComponent component, ref GetCharacterInfoControlsEvent args)
+    {
+        var c = new BoxContainer()
+        {
+            Margin = new Thickness(5),
+            Children =
+            {
+                new Button()
+                {
+                    Text ="BITCH"
+                }
+            }
+        };
+
+        args.Controls.Add(c);
     }
 
     public void RequestCharacterInfo()
@@ -42,17 +66,33 @@ public sealed class CharacterInfoSystem : EntitySystem
 
     private void OnCharacterInfoEvent(CharacterInfoEvent msg, EntitySessionEventArgs args)
     {
-        var sprite = CompOrNull<SpriteComponent>(msg.EntityUid);
-        var data = new CharacterData(msg.JobTitle, msg.Objectives, msg.Briefing, sprite, Name(msg.EntityUid));
-
+        var data = new CharacterData(msg.EntityUid, msg.JobTitle, msg.Objectives, msg.Briefing, Name(msg.EntityUid));
         OnCharacterUpdate?.Invoke(data);
     }
 
+    public List<Control> GetCharacterInfoControls(EntityUid uid)
+    {
+        var ev = new GetCharacterInfoControlsEvent(uid);
+        RaiseLocalEvent(uid, ref ev, true);
+        return ev.Controls;
+    }
+
     public readonly record struct CharacterData(
+        EntityUid Entity,
         string Job,
         Dictionary<string, List<ConditionInfo>> Objectives,
         string Briefing,
-        SpriteComponent? Sprite,
         string EntityName
     );
+
+    /// <summary>
+    /// Event raised to get additional controls to display in the character info menu.
+    /// </summary>
+    [ByRefEvent]
+    public readonly record struct GetCharacterInfoControlsEvent(EntityUid Entity)
+    {
+        public readonly List<Control> Controls = new();
+
+        public readonly EntityUid Entity = Entity;
+    }
 }
