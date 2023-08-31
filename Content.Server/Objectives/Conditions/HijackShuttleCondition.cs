3469c0f69102b6990ed4b4030371d25ff6453d65
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-08-31 20:14:55 +0300

    Update

--- Content.Server/Objectives/Conditions/HijackShuttleCondition.cs
+++ Content.Server/Objectives/Conditions/HijackShuttleCondition.cs
@@ -1,10 +1,11 @@
 ﻿using Content.Server.Mind;
-using Content.Server.Objectives.Interfaces;
 using Content.Server.Roles;
 using Content.Server.Shuttles.Components;
 using Content.Shared.Cuffs.Components;
 using Content.Shared.Humanoid;
+using Content.Shared.Mind;
 using Content.Shared.Mobs.Systems;
+using Content.Shared.Objectives.Interfaces;
 using Robust.Shared.Player;
 using Robust.Shared.Utility;
 
