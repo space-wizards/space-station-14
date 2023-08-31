ef5a4ec3065ab6857c15f9bdee5cf1bd0711edd6
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2023-08-31 22:29:11 +1000

    More ent storage ref (#19693)

--- Content.Server/Storage/EntitySystems/EntityStorageSystem.cs
+++ Content.Server/Storage/EntitySystems/EntityStorageSystem.cs
@@ -4,9 +4,15 @@ using Content.Server.Construction;
 using Content.Server.Construction.Components;
 using Content.Server.Storage.Components;
 using Content.Server.Tools.Systems;
+using Content.Shared.Destructible;
+using Content.Shared.Interaction;
+using Content.Shared.Lock;
+using Content.Shared.Movement.Events;
 using Content.Shared.Storage.Components;
 using Content.Shared.Storage.EntitySystems;
+using Content.Shared.Verbs;
 using Robust.Shared.Containers;
+using Robust.Shared.GameStates;
 using Robust.Shared.Map;
 
 namespace Content.Server.Storage.EntitySystems;
@@ -20,6 +26,19 @@ public sealed class EntityStorageSystem : SharedEntityStorageSystem
     public override void Initialize()
     {
         base.Initialize();
+
+        /* CompRef things */
+        SubscribeLocalEvent<EntityStorageComponent, ComponentInit>(OnComponentInit);
+        SubscribeLocalEvent<EntityStorageComponent, ComponentStartup>(OnComponentStartup);
+        SubscribeLocalEvent<EntityStorageComponent, ActivateInWorldEvent>(OnInteract, after: new[] { typeof(LockSystem) });
+        SubscribeLocalEvent<EntityStorageComponent, LockToggleAttemptEvent>(OnLockToggleAttempt);
+        SubscribeLocalEvent<EntityStorageComponent, DestructionEventArgs>(OnDestruction);
+        SubscribeLocalEvent<EntityStorageComponent, GetVerbsEvent<InteractionVerb>>(AddToggleOpenVerb);
+        SubscribeLocalEvent<EntityStorageComponent, ContainerRelayMovementEntityEvent>(OnRelayMovement);
+
+        SubscribeLocalEvent<EntityStorageComponent, ComponentGetState>(OnGetState);
+        SubscribeLocalEvent<EntityStorageComponent, ComponentHandleState>(OnHandleState);
+        /* CompRef things */
 
         SubscribeLocalEvent<EntityStorageComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<EntityStorageComponent, WeldableAttemptEvent>(OnWeldableAttempt);
@@ -52,7 +71,12 @@ public sealed class EntityStorageSystem : SharedEntityStorageSystem
 
     public override bool ResolveStorage(EntityUid uid, [NotNullWhen(true)] ref SharedEntityStorageComponent? component)
     {
-        return Resolve(uid, ref component);
+        if (component != null)
+            return true;
+
+        TryComp<EntityStorageComponent>(uid, out var storage);
+        component = storage;
+        return component != null;
     }
 
     private void OnWeldableAttempt(EntityUid uid, EntityStorageComponent component, WeldableAttemptEvent args)
