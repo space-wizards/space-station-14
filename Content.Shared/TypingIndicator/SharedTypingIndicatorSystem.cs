d294867456a9abcf6bd096fc32959f8cc9fdf390
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-08-31 18:16:03 +0300

    Add clothing change indicator

--- Content.Shared/TypingIndicator/SharedTypingIndicatorSystem.cs
+++ Content.Shared/TypingIndicator/SharedTypingIndicatorSystem.cs
@@ -1,4 +1,5 @@
-﻿using Content.Shared.StatusIcon;
+﻿using Content.Shared.Clothing.Components;
+using Content.Shared.Inventory.Events;
 
 namespace Content.Shared.TypingIndicator;
 
@@ -8,13 +9,36 @@ namespace Content.Shared.TypingIndicator;
 public abstract class SharedTypingIndicatorSystem : EntitySystem
 {
     /// <summary>
-    ///     Default ID of typing indicator icon <see cref="StatusIconPrototype"/>
+    ///     Default ID of typing indicator icon <see cref="TypingIndicatorPrototype"/>
     /// </summary>
     [ValidatePrototypeId<TypingIndicatorPrototype>]
     public const string InitialIndicatorId = "Default";
 
     public override void Initialize()
     {
+        base.Initialize();
+        SubscribeLocalEvent<TypingIndicatorClothingComponent, GotEquippedEvent>(OnGotEquipped);
+        SubscribeLocalEvent<TypingIndicatorClothingComponent, GotUnequippedEvent>(OnGotUnequipped);
+    }
 
+    private void OnGotEquipped(EntityUid uid, TypingIndicatorClothingComponent component, GotEquippedEvent args)
+    {
+        if (!TryComp<ClothingComponent>(uid, out var clothing) ||
+            !TryComp<TypingIndicatorComponent>(args.Equipee, out var indicator))
+            return;
+
+        var isCorrectSlot = clothing.Slots.HasFlag(args.SlotFlags);
+        if (!isCorrectSlot)
+            return;
+
+        indicator.Prototype = component.Prototype;
+    }
+
+    private void OnGotUnequipped(EntityUid uid, TypingIndicatorClothingComponent component, GotUnequippedEvent args)
+    {
+        if (!TryComp<TypingIndicatorComponent>(args.Equipee, out var indicator))
+            return;
+
+        indicator.Prototype = InitialIndicatorId;
     }
 }
---
+++ Content.Shared/TypingIndicator/TypingIndicatorClotingComponent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Shared.TypingIndicator;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(SharedTypingIndicatorSystem))]
+public sealed partial class TypingIndicatorClothingComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("prototype", required: true, customTypeSerializer: typeof(PrototypeIdSerializer<TypingIndicatorPrototype>))]
+    public string Prototype = default!;
+}
