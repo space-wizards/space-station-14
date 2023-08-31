8243825ccaa60f3cc10ac62a47abafc1fd8a9718
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-08-31 13:30:39 +0300

    wip

--- Content.Client/StatusIcon/StatusIconOverlay.cs
+++ Content.Client/StatusIcon/StatusIconOverlay.cs
@@ -1,9 +1,12 @@
+using System.Linq;
 using Content.Shared.StatusIcon;
 using Content.Shared.StatusIcon.Components;
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
 using Robust.Shared.Enums;
 using System.Numerics;
+using Robust.Client.ResourceManagement;
+using Robust.Client.Utility;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
@@ -14,6 +17,8 @@ public sealed class StatusIconOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _entity = default!;
     [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly IResourceCache _resourceCache = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
 
     private readonly SpriteSystem _sprite;
     private readonly TransformSystem _transform;
@@ -22,10 +27,6 @@ public sealed class StatusIconOverlay : Overlay
     private readonly ShaderInstance _shader;
 
     public override OverlaySpace Space => OverlaySpace.WorldSpaceBelowFOV;
-
-    private readonly float[] _timer;
-    private readonly float[][] _frameDelays;
-    private readonly int[] _frameCounter;
 
     internal StatusIconOverlay()
     {
@@ -36,6 +37,7 @@ public sealed class StatusIconOverlay : Overlay
         _statusIcon = _entity.System<StatusIconSystem>();
 
         _shader = _prototype.Index<ShaderPrototype>("unshaded").Instance();
+
     }
 
     protected override void Draw(in OverlayDrawArgs args)
@@ -78,54 +80,73 @@ public sealed class StatusIconOverlay : Overlay
             var accOffsetR = 0;
             icons.Sort();
 
+            Texture? texture = null;
             foreach (var proto in icons)
             {
                 switch (proto.Icon)
                 {
-                    case SpriteSpecifier.Rsi animated:
+                    case SpriteSpecifier.Rsi rsi:
+                        var rsiActual = _resourceCache.GetResource<RSIResource>(rsi.RsiPath).RSI;
+                        rsiActual.TryGetState(rsi.RsiState, out var state);
+                        var frames = state!.GetFrames(RSI.State.Direction.South);
+                        var delays = state.GetDelays();
+                        var totalDelay = delays.Sum();
+                        var time = _timing.RealTime.TotalSeconds % totalDelay;
+                        var delaySum = 0f;
+
+                        for (var i = 0; i < delays.Length; i++)
+                        {
+                            var delay = delays[i];
+                            delaySum += delay;
+
+                            if (time > delaySum)
+                                continue;
+
+                            texture = frames[i];
+                            break;
+                        }
+
+                        texture ??= _sprite.Frame0(proto.Icon);
                         break;
                     case SpriteSpecifier.Texture rsiTexture:
-                        var texture = _sprite.Frame0(rsiTexture);
-
-                        float yOffset;
-                        float xOffset;
-
-                        // the icons are ordered left to right, top to bottom.
-                        // extra icons that don't fit are just cut off.
-                        if (proto.LocationPreference == StatusIconLocationPreference.Left ||
-                            proto.LocationPreference == StatusIconLocationPreference.None && countL <= countR)
-                        {
-                            if (accOffsetL + texture.Height > sprite.Bounds.Height * EyeManager.PixelsPerMeter)
-                                break;
-                            accOffsetL += texture.Height;
-                            yOffset = (bounds.Height + sprite.Offset.Y) / 2f - (float) accOffsetL / EyeManager.PixelsPerMeter;
-                            xOffset = -(bounds.Width + sprite.Offset.X) / 2f;
-
-                            countL++;
-                        }
-                        else
-                        {
-                            if (accOffsetR + texture.Height > sprite.Bounds.Height * EyeManager.PixelsPerMeter)
-                                break;
-                            accOffsetR += texture.Height;
-                            yOffset = (bounds.Height + sprite.Offset.Y) / 2f - (float) accOffsetR / EyeManager.PixelsPerMeter;
-                            xOffset = (bounds.Width + sprite.Offset.X) / 2f - (float) texture.Width / EyeManager.PixelsPerMeter;
-
-                            countR++;
-                        }
-
-                        var position = new Vector2(xOffset, yOffset);
-                        handle.DrawTexture(texture, position);
+                        texture = rsiTexture.GetTexture(_resourceCache);
                         break;
+                    default:
+                        throw new NotImplementedException();
                 }
+
+                float yOffset;
+                float xOffset;
+
+                // the icons are ordered left to right, top to bottom.
+                // extra icons that don't fit are just cut off.
+                if (proto.LocationPreference == StatusIconLocationPreference.Left ||
+                    proto.LocationPreference == StatusIconLocationPreference.None && countL <= countR)
+                {
+                    if (accOffsetL + texture.Height > sprite.Bounds.Height * EyeManager.PixelsPerMeter)
+                        break;
+                    accOffsetL += texture.Height;
+                    yOffset = (bounds.Height + sprite.Offset.Y) / 2f - (float) accOffsetL / EyeManager.PixelsPerMeter;
+                    xOffset = -(bounds.Width + sprite.Offset.X) / 2f;
+
+                    countL++;
+                }
+                else
+                {
+                    if (accOffsetR + texture.Height > sprite.Bounds.Height * EyeManager.PixelsPerMeter)
+                        break;
+                    accOffsetR += texture.Height;
+                    yOffset = (bounds.Height + sprite.Offset.Y) / 2f - (float) accOffsetR / EyeManager.PixelsPerMeter;
+                    xOffset = (bounds.Width + sprite.Offset.X) / 2f - (float) texture.Width / EyeManager.PixelsPerMeter;
+
+                    countR++;
+                }
+
+                var position = new Vector2(xOffset, yOffset);
+                handle.DrawTexture(texture, position);
             }
         }
 
         handle.UseShader(null);
-    }
-
-    protected override void FrameUpdate(FrameEventArgs args)
-    {
-        base.FrameUpdate(args);
     }
 }
