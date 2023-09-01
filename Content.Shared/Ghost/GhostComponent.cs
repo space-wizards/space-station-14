39e497485a8fd253b0f6b3adb13a22cd319bb243
Kara <lunarautomaton6@gmail.com>
2023-08-31 21:35:07 -0500

    Convert ghost actions to YML

--- Content.Shared/Ghost/GhostComponent.cs
+++ Content.Shared/Ghost/GhostComponent.cs
@@ -3,6 +3,7 @@ using Content.Shared.Actions.ActionTypes;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Robust.Shared.Utility;
 
 namespace Content.Shared.Ghost;
@@ -15,38 +16,22 @@ public sealed partial class GhostComponent : Component
     [ViewVariables]
     public bool IsAttached;
 
-    public InstantAction ToggleLightingAction = new()
-    {
-        Icon = new SpriteSpecifier.Texture(new ("Interface/VerbIcons/light.svg.192dpi.png")),
-        DisplayName = "ghost-gui-toggle-lighting-manager-name",
-        Description = "ghost-gui-toggle-lighting-manager-desc",
-        ClientExclusive = true,
-        CheckCanInteract = false,
-        Event = new ToggleLightingActionEvent(),
-    };
+    #region Ghost Actions
 
-    public InstantAction ToggleFoVAction = new()
-    {
-        Icon = new SpriteSpecifier.Texture(new ("Interface/VerbIcons/vv.svg.192dpi.png")),
-        DisplayName = "ghost-gui-toggle-fov-name",
-        Description = "ghost-gui-toggle-fov-desc",
-        ClientExclusive = true,
-        CheckCanInteract = false,
-        Event = new ToggleFoVActionEvent(),
-    };
+    [DataField("toggleLightingAction", customTypeSerializer: typeof(PrototypeIdSerializer<InstantActionPrototype>))]
+    public string ToggleLightingAction = "GhostToggleLighting";
 
-    public InstantAction ToggleGhostsAction = new()
-    {
-        Icon = new SpriteSpecifier.Rsi(new ("Mobs/Ghosts/ghost_human.rsi"), "icon"),
-        DisplayName = "ghost-gui-toggle-ghost-visibility-name",
-        Description = "ghost-gui-toggle-ghost-visibility-desc",
-        ClientExclusive = true,
-        CheckCanInteract = false,
-        Event = new ToggleGhostsActionEvent(),
-    };
+    [DataField("toggleFoVAction", customTypeSerializer: typeof(PrototypeIdSerializer<InstantActionPrototype>))]
+    public string ToggleFoVAction = "GhostToggleFoV";
 
-    [ViewVariables(VVAccess.ReadWrite), DataField("timeOfDeath", customTypeSerializer:typeof(TimeOffsetSerializer))]
-    public TimeSpan TimeOfDeath = TimeSpan.Zero;
+    [DataField("toggleGhostsAction", customTypeSerializer: typeof(PrototypeIdSerializer<InstantActionPrototype>))]
+    public string ToggleGhostsAction = "GhostToggleGhostVisibility";
+
+    [DataField("toggleGhostHearingAction", customTypeSerializer: typeof(PrototypeIdSerializer<InstantActionPrototype>))]
+    public string ToggleGhostHearingAction = "GhostToggleHearing";
+
+    [DataField("booAction", customTypeSerializer:typeof(PrototypeIdSerializer<InstantActionPrototype>))]
+    public string BooAction = "GhostBoo";
 
     [DataField("booRadius")]
     public float BooRadius = 3;
@@ -54,16 +39,10 @@ public sealed partial class GhostComponent : Component
     [DataField("booMaxTargets")]
     public int BooMaxTargets = 3;
 
-    [DataField("action")]
-    public InstantAction Action = new()
-    {
-        UseDelay = TimeSpan.FromSeconds(120),
-        Icon = new SpriteSpecifier.Texture(new ("Interface/Actions/scream.png")),
-        DisplayName = "action-name-boo",
-        Description = "action-description-boo",
-        CheckCanInteract = false,
-        Event = new BooActionEvent(),
-    };
+    #endregion
+
+    [ViewVariables(VVAccess.ReadWrite), DataField("timeOfDeath", customTypeSerializer:typeof(TimeOffsetSerializer))]
+    public TimeSpan TimeOfDeath = TimeSpan.Zero;
 
     // TODO: instead of this funny stuff just give it access and update in system dirtying when needed
     [ViewVariables(VVAccess.ReadWrite)]
@@ -109,10 +88,12 @@ public sealed partial class GhostComponent : Component
     private bool _canReturnToBody;
 }
 
+public sealed partial class ToggleFoVActionEvent : InstantActionEvent { }
+
+public sealed partial class ToggleGhostsActionEvent : InstantActionEvent { }
+
+public sealed partial class ToggleLightingActionEvent : InstantActionEvent { }
+
+public sealed partial class ToggleGhostHearingActionEvent : InstantActionEvent { }
+
 public sealed partial class BooActionEvent : InstantActionEvent { }
-
-public sealed partial class ToggleFoVActionEvent : InstantActionEvent { };
-
-public sealed partial class ToggleGhostsActionEvent : InstantActionEvent { };
-
-public sealed partial class ToggleLightingActionEvent : InstantActionEvent { };
