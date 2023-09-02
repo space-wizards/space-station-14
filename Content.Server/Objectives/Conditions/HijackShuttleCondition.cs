06c6019610097a1cde2722ed365e8ec9c5e0d1ba
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-09-02 20:43:02 +0300

    Code formatting

--- Content.Server/Objectives/Conditions/HijackShuttleCondition.cs
+++ Content.Server/Objectives/Conditions/HijackShuttleCondition.cs
@@ -9,122 +9,122 @@ using Content.Shared.Objectives.Interfaces;
 using Robust.Shared.Player;
 using Robust.Shared.Utility;
 
-namespace Content.Server.Objectives.Conditions
+namespace Content.Server.Objectives.Conditions;
+
+[DataDefinition]
+public sealed partial class HijackShuttleCondition : IObjectiveCondition
 {
-    [DataDefinition]
-    public sealed partial class HijackShuttleCondition : IObjectiveCondition
+    private MindComponent _mind;
+    private EntityUid _mindId;
+
+    public IObjectiveCondition GetAssigned(EntityUid mindId, MindComponent mind)
     {
-        private MindComponent _mind;
-        private EntityUid _mindId;
-
-        public IObjectiveCondition GetAssigned(EntityUid mindId, MindComponent mind)
+        return new HijackShuttleCondition
         {
-            return new HijackShuttleCondition
+            _mind = mind,
+            _mindId = mindId,
+        };
+    }
+
+    public string Title => Loc.GetString("objective-condition-hijack-shuttle-title");
+
+    public string Description => Loc.GetString("objective-condition-hijack-shuttle-description");
+
+    public SpriteSpecifier Icon => new SpriteSpecifier.Rsi(new ResPath("Objects/Tools/emag.rsi"), "icon");
+
+    private bool IsShuttleHijacked(EntityUid shuttleGridId)
+    {
+        var entMan = IoCManager.Resolve<IEntityManager>();
+        var mindSystem = entMan.System<MindSystem>();
+        var roleSystem = entMan.System<RoleSystem>();
+        var mobStateSystem = entMan.System<MobStateSystem>();
+
+        var agentOnShuttle = false;
+        var gridPlayers = Filter.BroadcastGrid(shuttleGridId).Recipients;
+        foreach (var player in gridPlayers)
+        {
+            if (!player.AttachedEntity.HasValue ||
+                !mindSystem.TryGetMind(player.AttachedEntity.Value, out var mindId, out _))
+                continue;
+
+            if (mindId == _mindId)
             {
-                _mind = mind,
-                _mindId = mindId,
-            };
+                agentOnShuttle = true;
+                continue;
+            }
+
+            var isHumanoid = entMan.HasComponent<HumanoidAppearanceComponent>(player.AttachedEntity.Value);
+            if (!isHumanoid) // Only humanoids count as enemies
+                continue;
+
+            var isPersonTraitor = roleSystem.MindHasRole<TraitorRoleComponent>(mindId);
+            if (isPersonTraitor) // Allow traitors
+                continue;
+
+            var isPersonIncapacitated = mobStateSystem.IsIncapacitated(player.AttachedEntity.Value);
+            if (isPersonIncapacitated) // Allow dead and crit
+                continue;
+
+            var isPersonCuffed =
+                entMan.TryGetComponent<CuffableComponent>(player.AttachedEntity.Value, out var cuffed)
+                && cuffed.CuffedHandCount > 0;
+            if (isPersonCuffed) // Allow handcuffed
+                continue;
+
+            return false;
         }
+        // TODO: Allow pets?
 
-        public string Title => Loc.GetString("objective-condition-hijack-shuttle-title");
+        return agentOnShuttle;
+    }
 
-        public string Description => Loc.GetString("objective-condition-hijack-shuttle-description");
-
-        public SpriteSpecifier Icon => new SpriteSpecifier.Rsi(new ResPath("Objects/Tools/emag.rsi"), "icon");
-
-        private bool IsShuttleHijacked(EntityUid shuttleGridId)
+    public float Progress
+    {
+        get
         {
             var entMan = IoCManager.Resolve<IEntityManager>();
             var mindSystem = entMan.System<MindSystem>();
-            var roleSystem = entMan.System<RoleSystem>();
-            var mobStateSystem = entMan.System<MobStateSystem>();
 
-            var agentOnShuttle = false;
-            var gridPlayers = Filter.BroadcastGrid(shuttleGridId).Recipients;
-            foreach (var player in gridPlayers)
+            var shuttleHijacked = false;
+            var agentIsAlive = !mindSystem.IsCharacterDeadIc(_mind);
+            var agentIsFree = !(entMan.TryGetComponent<CuffableComponent>(_mind.OwnedEntity, out var cuffed)
+                                && cuffed.CuffedHandCount > 0); // You're not escaping if you're restrained!
+
+            // Any emergency shuttle counts for this objective.
+            var query = entMan.AllEntityQueryEnumerator<StationEmergencyShuttleComponent>();
+            while (query.MoveNext(out var comp))
             {
-                if (!player.AttachedEntity.HasValue ||
-                    !mindSystem.TryGetMind(player.AttachedEntity.Value, out var mindId, out _))
+                if (comp.EmergencyShuttle == null)
                     continue;
 
-                if (mindId == _mindId)
+                if (IsShuttleHijacked(comp.EmergencyShuttle.Value))
                 {
-                    agentOnShuttle = true;
-                    continue;
+                    shuttleHijacked = true;
+                    break;
                 }
-
-                var isHumanoid = entMan.HasComponent<HumanoidAppearanceComponent>(player.AttachedEntity.Value);
-                if (!isHumanoid) // Only humanoids count as enemies
-                    continue;
-
-                var isPersonTraitor = roleSystem.MindHasRole<TraitorRoleComponent>(mindId);
-                if (isPersonTraitor) // Allow traitors
-                    continue;
-
-                var isPersonIncapacitated = mobStateSystem.IsIncapacitated(player.AttachedEntity.Value);
-                if (isPersonIncapacitated) // Allow dead and crit
-                    continue;
-
-                var isPersonCuffed =
-                    entMan.TryGetComponent<CuffableComponent>(player.AttachedEntity.Value, out var cuffed)
-                    && cuffed.CuffedHandCount > 0;
-                if (isPersonCuffed) // Allow handcuffed
-                    continue;
-
-                return false;
             }
-            // TODO: Allow pets?
 
-            return agentOnShuttle;
+            return (shuttleHijacked && agentIsAlive && agentIsFree) ? 1f : 0f;
         }
+    }
 
-        public float Progress
-        {
-            get {
-                var entMan = IoCManager.Resolve<IEntityManager>();
-                var mindSystem = entMan.System<MindSystem>();
+    public float Difficulty => 2.75f;
 
-                var shuttleHijacked = false;
-                var agentIsAlive = !mindSystem.IsCharacterDeadIc(_mind);
-                var agentIsFree = !(entMan.TryGetComponent<CuffableComponent>(_mind.OwnedEntity, out var cuffed)
-                                     && cuffed.CuffedHandCount > 0); // You're not escaping if you're restrained!
+    public bool Equals(IObjectiveCondition? other)
+    {
+        return other is HijackShuttleCondition esc && Equals(_mind, esc._mind);
+    }
 
-                // Any emergency shuttle counts for this objective.
-                var query = entMan.AllEntityQueryEnumerator<StationEmergencyShuttleComponent>();
-                while (query.MoveNext(out var comp))
-                {
-                    if (comp.EmergencyShuttle == null)
-                        continue;
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((HijackShuttleCondition) obj);
+    }
 
-                    if (IsShuttleHijacked(comp.EmergencyShuttle.Value))
-                    {
-                        shuttleHijacked = true;
-                        break;
-                    }
-                }
-
-                return (shuttleHijacked && agentIsAlive && agentIsFree) ? 1f : 0f;
-            }
-        }
-
-        public float Difficulty => 2.75f;
-
-        public bool Equals(IObjectiveCondition? other)
-        {
-            return other is HijackShuttleCondition esc && Equals(_mind, esc._mind);
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
-            return Equals((HijackShuttleCondition) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return _mind != null ? _mind.GetHashCode() : 0;
-        }
+    public override int GetHashCode()
+    {
+        return _mind != null ? _mind.GetHashCode() : 0;
     }
 }
