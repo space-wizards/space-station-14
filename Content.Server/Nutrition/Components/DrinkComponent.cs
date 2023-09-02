1d433f631d1a72f92c0043b177c38fc36d79627d
deltanedas <@deltanedas:kde.org>
2023-09-02 11:12:36 +0100

    refactor drink component, vvrw, file scope

--- Content.Server/Nutrition/Components/DrinkComponent.cs
+++ Content.Server/Nutrition/Components/DrinkComponent.cs
@@ -1,39 +1,34 @@
 using Content.Server.Nutrition.EntitySystems;
-using Content.Shared.DoAfter;
 using Content.Shared.FixedPoint;
-using JetBrains.Annotations;
 using Robust.Shared.Audio;
 
-namespace Content.Server.Nutrition.Components
+namespace Content.Server.Nutrition.Components;
+
+[RegisterComponent, Access(typeof(DrinkSystem))]
+public sealed partial class DrinkComponent : Component
 {
-    [RegisterComponent]
-    [Access(typeof(DrinkSystem))]
-    public sealed partial class DrinkComponent : Component
-    {
-        [DataField("solution")]
-        public string SolutionName { get; set; } = DefaultSolutionName;
-        public const string DefaultSolutionName = "drink";
+    [ViewVariables(VVAccess.ReadWrite), DataField("solution")]
+    public string SolutionName = DefaultSolutionName;
+    public const string DefaultSolutionName = "drink";
 
-        [DataField("useSound")]
-        public SoundSpecifier UseSound = new SoundPathSpecifier("/Audio/Items/drink.ogg");
+    [DataField("useSound")]
+    public SoundSpecifier UseSound = new SoundPathSpecifier("/Audio/Items/drink.ogg");
 
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("transferAmount")]
-        public FixedPoint2 TransferAmount { get; [UsedImplicitly] private set; } = FixedPoint2.New(5);
+    [ViewVariables(VVAccess.ReadWrite), DataField("transferAmount")]
+    public FixedPoint2 TransferAmount = FixedPoint2.New(5);
 
-        /// <summary>
-        /// How long it takes to drink this yourself.
-        /// </summary>
-        [DataField("delay")]
-        public float Delay = 1;
+    /// <summary>
+    /// How long it takes to drink this yourself.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("delay")]
+    public float Delay = 1;
 
-        [DataField("examinable")]
-        public bool Examinable = true;
+    [ViewVariables(VVAccess.ReadWrite), DataField("examinable")]
+    public bool Examinable = true;
 
-        /// <summary>
-        ///     This is how many seconds it takes to force feed someone this drink.
-        /// </summary>
-        [DataField("forceFeedDelay")]
-        public float ForceFeedDelay = 3;
-    }
+    /// <summary>
+    ///     This is how many seconds it takes to force feed someone this drink.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("forceFeedDelay")]
+    public float ForceFeedDelay = 3;
 }
