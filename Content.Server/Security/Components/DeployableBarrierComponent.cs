bc381f63e8e5968fd06639aa9fedc9097d1551aa
DrSmugleaf <drsmugleaf@gmail.com>
2023-09-02 21:34:31 -0700

    Fix barriers not working

--- Content.Server/Security/Components/DeployableBarrierComponent.cs
+++ Content.Server/Security/Components/DeployableBarrierComponent.cs
@@ -3,5 +3,9 @@ namespace Content.Server.Security.Components;
 [RegisterComponent]
 public sealed partial class DeployableBarrierComponent : Component
 {
+    /// <summary>
+    ///     The fixture to change collision on.
+    /// </summary>
+    [DataField("fixture", required: true)] public string FixtureId = string.Empty;
 }
 
--- Content.Server/Security/Systems/DeployableBarrierSystem.cs
+++ Content.Server/Security/Systems/DeployableBarrierSystem.cs
@@ -1,16 +1,21 @@
-using Content.Server.Pulling;
 using Content.Server.Security.Components;
 using Content.Shared.Lock;
+using Content.Shared.Pulling;
 using Content.Shared.Pulling.Components;
 using Content.Shared.Security;
 using Robust.Server.GameObjects;
+using Robust.Shared.Physics.Systems;
 
 namespace Content.Server.Security.Systems
 {
     public sealed class DeployableBarrierSystem : EntitySystem
     {
         [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
-        [Dependency] private readonly PullingSystem _pulling = default!;
+        [Dependency] private readonly FixtureSystem _fixtures = default!;
+        [Dependency] private readonly SharedPointLightSystem _pointLight = default!;
+        [Dependency] private readonly SharedPhysicsSystem _physics = default!;
+        [Dependency] private readonly SharedPullingSystem _pulling = default!;
+        [Dependency] private readonly SharedTransformSystem _transform = default!;
 
         public override void Initialize()
         {
@@ -24,26 +29,43 @@ namespace Content.Server.Security.Systems
             if (!TryComp(uid, out LockComponent? lockComponent))
                 return;
 
-            ToggleBarrierDeploy(uid, lockComponent.Locked);
+            ToggleBarrierDeploy(uid, lockComponent.Locked, component);
         }
 
         private void OnLockToggled(EntityUid uid, DeployableBarrierComponent component, ref LockToggledEvent args)
         {
-            ToggleBarrierDeploy(uid, args.Locked);
+            ToggleBarrierDeploy(uid, args.Locked, component);
         }
 
-        private void ToggleBarrierDeploy(EntityUid uid, bool isDeployed)
+        private void ToggleBarrierDeploy(EntityUid uid, bool isDeployed, DeployableBarrierComponent? component)
         {
-            Transform(uid).Anchored = isDeployed;
+            if (!Resolve(uid, ref component))
+                return;
+
+            var transform = Transform(uid);
+            var fixture = _fixtures.GetFixtureOrNull(uid, component.FixtureId);
+
+            if (isDeployed && transform.GridUid != null)
+            {
+                _transform.AnchorEntity(uid, transform);
+                if (fixture != null)
+                    _physics.SetHard(uid, fixture, true);
+            }
+            else
+            {
+                _transform.Unanchor(uid, transform);
+                if (fixture != null)
+                    _physics.SetHard(uid, fixture, false);
+            }
 
             var state = isDeployed ? DeployableBarrierState.Deployed : DeployableBarrierState.Idle;
             _appearance.SetData(uid, DeployableBarrierVisuals.State, state);
 
-            if (TryComp<SharedPullableComponent>(uid, out var pullable))
+            if (TryComp(uid, out SharedPullableComponent? pullable))
                 _pulling.TryStopPull(pullable);
 
             if (TryComp(uid, out PointLightComponent? light))
-                light.Enabled = isDeployed;
+                _pointLight.SetEnabled(uid, isDeployed, light);
         }
     }
 }
