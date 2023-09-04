109ec4575b8d0aa79b1dc9de7a0b54bcb7feb6e0
chromiumboy <chromium.boy@gmail.com>
2023-09-04 12:09:48 -0500

    The operation of the radiation collector no longer produces tritium, plus added file scoped name spaces

--- Content.Server/Singularity/EntitySystems/RadiationCollectorSystem.cs
+++ Content.Server/Singularity/EntitySystems/RadiationCollectorSystem.cs
@@ -11,151 +11,150 @@ using Content.Shared.Examine;
 using Content.Server.Atmos;
 using System.Diagnostics.CodeAnalysis;
 
-namespace Content.Server.Singularity.EntitySystems
+namespace Content.Server.Singularity.EntitySystems;
+
+public sealed class RadiationCollectorSystem : EntitySystem
 {
-    public sealed class RadiationCollectorSystem : EntitySystem
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly PopupSystem _popupSystem = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly IGameTiming _gameTiming = default!;
-        [Dependency] private readonly PopupSystem _popupSystem = default!;
-        [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
-        [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
+        base.Initialize();
+        SubscribeLocalEvent<RadiationCollectorComponent, InteractHandEvent>(OnInteractHand);
+        SubscribeLocalEvent<RadiationCollectorComponent, OnIrradiatedEvent>(OnRadiation);
+        SubscribeLocalEvent<RadiationCollectorComponent, ExaminedEvent>(OnExamined);
+        SubscribeLocalEvent<RadiationCollectorComponent, GasAnalyzerScanEvent>(OnAnalyzed);
+    }
 
-        public override void Initialize()
+    private bool TryGetLoadedGasTank(EntityUid uid, [NotNullWhen(true)] out GasTankComponent? gasTankComponent)
+    {
+        gasTankComponent = null;
+        var container = _containerSystem.EnsureContainer<ContainerSlot>(uid, "GasTank");
+
+        if (container.ContainedEntity == null)
+            return false;
+
+        if (!EntityManager.TryGetComponent(container.ContainedEntity, out gasTankComponent))
+            return false;
+
+        return true;
+    }
+
+    private void OnInteractHand(EntityUid uid, RadiationCollectorComponent component, InteractHandEvent args)
+    {
+        var curTime = _gameTiming.CurTime;
+
+        if (curTime < component.CoolDownEnd)
+            return;
+
+        ToggleCollector(uid, args.User, component);
+        component.CoolDownEnd = curTime + component.Cooldown;
+    }
+
+    private void OnRadiation(EntityUid uid, RadiationCollectorComponent component, OnIrradiatedEvent args)
+    {
+        if (!component.Enabled || component.RadiationReactiveGases == null)
+            return;
+
+        if (!TryGetLoadedGasTank(uid, out var gasTankComponent))
+            return;
+
+        var charge = 0f;
+
+        foreach (var gas in component.RadiationReactiveGases)
         {
-            base.Initialize();
-            SubscribeLocalEvent<RadiationCollectorComponent, InteractHandEvent>(OnInteractHand);
-            SubscribeLocalEvent<RadiationCollectorComponent, OnIrradiatedEvent>(OnRadiation);
-            SubscribeLocalEvent<RadiationCollectorComponent, ExaminedEvent>(OnExamined);
-            SubscribeLocalEvent<RadiationCollectorComponent, GasAnalyzerScanEvent>(OnAnalyzed);
-        }
+            float reactantMol = gasTankComponent.Air.GetMoles(gas.Reactant);
+            float delta = args.TotalRads * reactantMol * gas.ReactantBreakdownRate;
 
-        private bool TryGetLoadedGasTank(EntityUid uid, [NotNullWhen(true)] out GasTankComponent? gasTankComponent)
-        {
-            gasTankComponent = null;
-            var container = _containerSystem.EnsureContainer<ContainerSlot>(uid, "GasTank");
+            // We need to offset the huge power gains possible when using very cold gases
+            // (they allow you to have a much higher molar concentrations of gas in the tank).
+            // Hence power output is modified using the Michaelis-Menten equation,
+            // it will heavily penalise the power output of low temperature reactions:
+            // 300K = 100% power output, 73K = 49% power output, 1K = 1% power output
+            float temperatureMod = 1.5f * gasTankComponent.Air.Temperature / (150f + gasTankComponent.Air.Temperature);
+            charge += args.TotalRads * reactantMol * component.ChargeModifier * gas.PowerGenerationEfficiency * temperatureMod;
 
-            if (container.ContainedEntity == null)
-                return false;
-
-            if (!EntityManager.TryGetComponent(container.ContainedEntity, out gasTankComponent))
-                return false;
-
-            return true;
-        }
-
-        private void OnInteractHand(EntityUid uid, RadiationCollectorComponent component, InteractHandEvent args)
-        {
-            var curTime = _gameTiming.CurTime;
-
-            if (curTime < component.CoolDownEnd)
-                return;
-
-            ToggleCollector(uid, args.User, component);
-            component.CoolDownEnd = curTime + component.Cooldown;
-        }
-
-        private void OnRadiation(EntityUid uid, RadiationCollectorComponent component, OnIrradiatedEvent args)
-        {
-            if (!component.Enabled || component.RadiationReactiveGases == null)
-                return;
-
-            if (!TryGetLoadedGasTank(uid, out var gasTankComponent))
-                return;
-
-            var charge = 0f;
-
-            foreach (var gas in component.RadiationReactiveGases)
+            if (delta > 0)
             {
-                float reactantMol = gasTankComponent.Air.GetMoles(gas.Reactant);
-                float delta = args.TotalRads * reactantMol * gas.ReactantBreakdownRate;
-
-                // We need to offset the huge power gains possible when using very cold gases
-                // (they allow you to have a much higher molar concentrations of gas in the tank).
-                // Hence power output is modified using the Michaelis-Menten equation,
-                // it will heavily penalise the power output of low temperature reactions:
-                // 300K = 100% power output, 73K = 49% power output, 1K = 1% power output
-                float temperatureMod = 1.5f * gasTankComponent.Air.Temperature / (150f + gasTankComponent.Air.Temperature);
-                charge += args.TotalRads * reactantMol * component.ChargeModifier * gas.PowerGenerationEfficiency * temperatureMod;
-
-                if (delta > 0)
-                {
-                    gasTankComponent.Air.AdjustMoles(gas.Reactant, -Math.Min(delta, reactantMol));
-                }
-
-                if (gas.Byproduct != null)
-                {
-                    gasTankComponent.Air.AdjustMoles((int) gas.Byproduct, delta * gas.MolarRatio);
-                }
+                gasTankComponent.Air.AdjustMoles(gas.Reactant, -Math.Min(delta, reactantMol));
             }
 
-            // No idea if this is even vaguely accurate to the previous logic.
-            // The maths is copied from that logic even though it works differently.
-            // But the previous logic would also make the radiation collectors never ever stop providing energy.
-            // And since frameTime was used there, I'm assuming that this is what the intent was.
-            // This still won't stop things being potentially hilariously unbalanced though.
-            if (TryComp<BatteryComponent>(uid, out var batteryComponent))
+            if (gas.Byproduct != null)
             {
-                batteryComponent.CurrentCharge += charge;
+                gasTankComponent.Air.AdjustMoles((int) gas.Byproduct, delta * gas.MolarRatio);
             }
         }
 
-        private void OnExamined(EntityUid uid, RadiationCollectorComponent component, ExaminedEvent args)
+        // No idea if this is even vaguely accurate to the previous logic.
+        // The maths is copied from that logic even though it works differently.
+        // But the previous logic would also make the radiation collectors never ever stop providing energy.
+        // And since frameTime was used there, I'm assuming that this is what the intent was.
+        // This still won't stop things being potentially hilariously unbalanced though.
+        if (TryComp<BatteryComponent>(uid, out var batteryComponent))
         {
-            if (!TryGetLoadedGasTank(uid, out var gasTankComponent))
-            {
-                args.PushMarkup(Loc.GetString("power-radiation-collector-gas-tank-missing"));
-                return;
-            }
+            batteryComponent.CurrentCharge += charge;
+        }
+    }
 
-            args.PushMarkup(Loc.GetString("power-radiation-collector-gas-tank-present"));
-
-            if (gasTankComponent.IsLowPressure)
-            {
-                args.PushMarkup(Loc.GetString("power-radiation-collector-gas-tank-low-pressure"));
-            }
+    private void OnExamined(EntityUid uid, RadiationCollectorComponent component, ExaminedEvent args)
+    {
+        if (!TryGetLoadedGasTank(uid, out var gasTankComponent))
+        {
+            args.PushMarkup(Loc.GetString("power-radiation-collector-gas-tank-missing"));
+            return;
         }
 
-        private void OnAnalyzed(EntityUid uid, RadiationCollectorComponent component, GasAnalyzerScanEvent args)
-        {
-            if (!TryGetLoadedGasTank(uid, out var gasTankComponent))
-                return;
+        args.PushMarkup(Loc.GetString("power-radiation-collector-gas-tank-present"));
 
-            args.GasMixtures = new Dictionary<string, GasMixture?> { { Name(uid), gasTankComponent.Air } };
+        if (gasTankComponent.IsLowPressure)
+        {
+            args.PushMarkup(Loc.GetString("power-radiation-collector-gas-tank-low-pressure"));
+        }
+    }
+
+    private void OnAnalyzed(EntityUid uid, RadiationCollectorComponent component, GasAnalyzerScanEvent args)
+    {
+        if (!TryGetLoadedGasTank(uid, out var gasTankComponent))
+            return;
+
+        args.GasMixtures = new Dictionary<string, GasMixture?> { { Name(uid), gasTankComponent.Air } };
+    }
+
+    public void ToggleCollector(EntityUid uid, EntityUid? user = null, RadiationCollectorComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        SetCollectorEnabled(uid, !component.Enabled, user, component);
+    }
+
+    public void SetCollectorEnabled(EntityUid uid, bool enabled, EntityUid? user = null, RadiationCollectorComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        component.Enabled = enabled;
+
+        // Show message to the player
+        if (user != null)
+        {
+            var msg = component.Enabled ? "radiation-collector-component-use-on" : "radiation-collector-component-use-off";
+            _popupSystem.PopupEntity(Loc.GetString(msg), uid);
         }
 
-        public void ToggleCollector(EntityUid uid, EntityUid? user = null, RadiationCollectorComponent? component = null)
-        {
-            if (!Resolve(uid, ref component))
-                return;
+        // Update appearance
+        UpdateAppearance(uid, component);
+    }
 
-            SetCollectorEnabled(uid, !component.Enabled, user, component);
-        }
+    private void UpdateAppearance(EntityUid uid, RadiationCollectorComponent? component, AppearanceComponent? appearance = null)
+    {
+        if (!Resolve(uid, ref component, ref appearance))
+            return;
 
-        public void SetCollectorEnabled(EntityUid uid, bool enabled, EntityUid? user = null, RadiationCollectorComponent? component = null)
-        {
-            if (!Resolve(uid, ref component))
-                return;
-
-            component.Enabled = enabled;
-
-            // Show message to the player
-            if (user != null)
-            {
-                var msg = component.Enabled ? "radiation-collector-component-use-on" : "radiation-collector-component-use-off";
-                _popupSystem.PopupEntity(Loc.GetString(msg), uid);
-            }
-
-            // Update appearance
-            UpdateAppearance(uid, component);
-        }
-
-        private void UpdateAppearance(EntityUid uid, RadiationCollectorComponent? component, AppearanceComponent? appearance = null)
-        {
-            if (!Resolve(uid, ref component, ref appearance))
-                return;
-
-            var state = component.Enabled ? RadiationCollectorVisualState.Active : RadiationCollectorVisualState.Deactive;
-            _appearance.SetData(uid, RadiationCollectorVisuals.VisualState, state, appearance);
-        }
+        var state = component.Enabled ? RadiationCollectorVisualState.Active : RadiationCollectorVisualState.Deactive;
+        _appearance.SetData(uid, RadiationCollectorVisuals.VisualState, state, appearance);
     }
 }
