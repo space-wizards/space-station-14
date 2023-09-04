fee71259c8066da81be9c45ec197363882fa953f
Doru991 <75124791+Doru991@users.noreply.github.com>
2023-09-04 22:33:05 +0300

    Clean up PlantPhalanximine.cs 2/2

--- Content.Server/Chemistry/ReagentEffects/PlantMetabolism/PlantPhalanximine.cs
+++ Content.Server/Chemistry/ReagentEffects/PlantMetabolism/PlantPhalanximine.cs
@@ -16,14 +16,7 @@ namespace Content.Server.Chemistry.ReagentEffects.PlantMetabolism
                                     plantHolderComp.Seed.Immutable)
                 return;
 
-            if (args.Source == null) return;
-
-            var source = args.Source;
-            //Need at least 5u, had to do it like this because plant metabolism is weird
-            if (source.TryGetReagent("Phalanximine", out var quantity) && quantity >= 4.00)
-            {
-                plantHolderComp.Seed.Viable = true;
-            }
+            plantHolderComp.Seed.Viable = true;
         }
 
         protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys) => Loc.GetString("reagent-effect-guidebook-missing", ("chance", Probability));
