5f90eafa23b84a7bd4c951cff9cd0c0b8394c44b
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-09-05 12:37:02 +0300

    unified system names

--- Content.Shared/Buckle/SharedBuckleSystem.cs
+++ Content.Shared/Buckle/SharedBuckleSystem.cs
@@ -23,19 +23,19 @@ public abstract partial class SharedBuckleSystem : EntitySystem
     [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
     [Dependency] private readonly ISharedPlayerManager _playerManager = default!;
 
-    [Dependency] protected readonly ActionBlockerSystem ActionBlockerSystem = default!;
-    [Dependency] protected readonly SharedAppearanceSystem AppearanceSystem = default!;
+    [Dependency] protected readonly ActionBlockerSystem ActionBlocker = default!;
+    [Dependency] protected readonly SharedAppearanceSystem Appearance = default!;
 
-    [Dependency] private readonly AlertsSystem _alertsSystem = default!;
-    [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
-    [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
-    [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
-    [Dependency] private readonly SharedInteractionSystem _interactionSystem = default!;
+    [Dependency] private readonly AlertsSystem _alerts = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedContainerSystem _container = default!;
+    [Dependency] private readonly SharedInteractionSystem _interaction = default!;
     [Dependency] private readonly SharedJointSystem _joints = default!;
-    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
-    [Dependency] private readonly SharedPullingSystem _pullingSystem = default!;
-    [Dependency] private readonly SharedTransformSystem _transformSystem = default!;
-    [Dependency] private readonly StandingStateSystem _standingSystem = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedPullingSystem _pulling = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly StandingStateSystem _standing = default!;
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
 
     /// <inheritdoc/>
@@ -74,7 +74,7 @@ public abstract partial class SharedBuckleSystem : EntitySystem
         if (buckleTransform.ParentUid != strapUid)
             return;
 
-        _transformSystem.SetLocalRotation(buckleUid, Angle.Zero, buckleTransform);
+        _transform.SetLocalRotation(buckleUid, Angle.Zero, buckleTransform);
         _joints.RefreshRelay(buckleUid, strapUid);
 
         switch (strapComp.Position)
@@ -82,10 +82,10 @@ public abstract partial class SharedBuckleSystem : EntitySystem
             case StrapPosition.None:
                 break;
             case StrapPosition.Stand:
-                _standingSystem.Stand(buckleUid);
+                _standing.Stand(buckleUid);
                 break;
             case StrapPosition.Down:
-                _standingSystem.Down(buckleUid, false, false);
+                _standing.Down(buckleUid, false, false);
                 break;
         }
     }
