5c3bef531dedc55c6193a778381fa6071b7a983c
deltanedas <@deltanedas:kde.org>
2023-09-06 09:48:22 +0100

    vvrw and datafield for everything

--- Content.Server/Mech/Components/MechAirFilterComponent.cs
+++ Content.Server/Mech/Components/MechAirFilterComponent.cs
@@ -40,6 +40,6 @@ public sealed partial class MechAirFilterComponent : Component
     /// <summary>
     /// Target volume to transfer every second.
     /// </summary>
-    [DataField("transferRate")]
+    [DataField("transferRate"), ViewVariables(VVAccess.ReadWrite)]
     public float TransferRate = MechAirComponent.GasMixVolume * 0.1f;
 }
