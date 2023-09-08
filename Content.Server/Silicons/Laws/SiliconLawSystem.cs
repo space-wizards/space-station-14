7c54b1b13bff6b4053bb31c006853a54858cd716
ps3moira <113228053+ps3moira@users.noreply.github.com>
2023-09-08 14:02:57 -0700

    Adding logic

--- Content.Server/Silicons/Laws/SiliconLawSystem.cs
+++ Content.Server/Silicons/Laws/SiliconLawSystem.cs
@@ -37,11 +37,13 @@ public sealed class SiliconLawSystem : SharedSiliconLawSystem
     [Dependency] private readonly SharedStunSystem _stunSystem = default!;
     [Dependency] private readonly IEntityManager _entityManager = default!;
     [Dependency] private readonly SharedRoleSystem _roles = default!;
+    [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
 
     /// <inheritdoc/>
     public override void Initialize()
     {
         base.Initialize();
+
 
         SubscribeLocalEvent<SiliconLawBoundComponent, ComponentStartup>(OnComponentStartup);
         SubscribeLocalEvent<SiliconLawBoundComponent, ComponentShutdown>(OnComponentShutdown);
