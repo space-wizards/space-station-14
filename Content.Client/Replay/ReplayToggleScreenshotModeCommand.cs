8c9df63d57d4aaee1df6d950d3799603acf1c7a7
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2023-09-07 23:47:06 -0700

    Add a command to hide replay UI

---
+++ Content.Client/Replay/ReplayToggleScreenshotModeCommand.cs
@@ -0,0 +1,32 @@
+﻿using Content.Client.UserInterface.Systems.Chat;
+using Robust.Client.Replays.Commands;
+using Robust.Client.Replays.UI;
+using Robust.Client.UserInterface;
+using Robust.Shared.Console;
+
+namespace Content.Client.Replay;
+
+public sealed class ReplayToggleScreenshotModeCommand : BaseReplayCommand
+{
+    [Dependency] private readonly IUserInterfaceManager _userInterfaceManager = default!;
+
+    public override string Command => "replay_toggle_screenshot_mode";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var screen = _userInterfaceManager.ActiveScreen;
+        if (screen == null)
+            return;
+
+        var replayWidget = screen.GetOrAddWidget<ReplayControlWidget>();
+
+        var targetVisibility = !replayWidget.Visible;
+
+        replayWidget.Visible = targetVisibility;
+
+        foreach (var chatBox in _userInterfaceManager.GetUIController<ChatUIController>().Chats)
+        {
+            chatBox.ChatInput.Visible = !targetVisibility;
+        }
+    }
+}
