bef86312b8326c8c6ee11f143e883a5f680f00f3
deltanedas <@deltanedas:kde.org>
2023-09-09 00:28:15 +0100

    condition file scope + fix

--- Content.Shared/Objectives/Objective.cs
+++ Content.Shared/Objectives/Objective.cs
@@ -1,52 +1,51 @@
 ﻿using Content.Shared.Mind;
 using Content.Shared.Objectives.Components;
 
-namespace Content.Shared.Objectives
+namespace Content.Shared.Objectives;
+
+public sealed class Objective : IEquatable<Objective>
 {
-    public sealed class Objective : IEquatable<Objective>
+    [ViewVariables]
+    public readonly EntityUid MindId;
+    [ViewVariables]
+    public readonly ObjectivePrototype Prototype;
+    [ViewVariables]
+    public readonly List<EntityUid> Conditions = new();
+
+    public Objective(EntityUid mindId, ObjectivePrototype prototype, List<EntityUid> conditions)
     {
-        [ViewVariables]
-        public readonly EntityUid MindId;
-        [ViewVariables]
-        public readonly ObjectivePrototype Prototype;
-        [ViewVariables]
-        public readonly List<EntityUid> Conditions = new();
+        MindId = mindId;
+        Prototype = prototype;
+        Conditions = conditions;
+    }
 
-        public Objective(EntityUid mindId, ObjectivePrototype prototype, List<EntityUid> conditions)
+    public bool Equals(Objective? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        if (!Equals(MindId, other.MindId) || !Equals(Prototype, other.Prototype)) return false;
+        if (Conditions.Count != other.Conditions.Count) return false;
+
+        for (var i = 0; i < Conditions.Count; i++)
         {
-            MindId = mindId;
-            Prototype = prototype;
-            Conditions = conditions;
+            // just comparing condition entity ids here
+            // ideally compare the entities components...
+            if (Conditions[i] != other.Conditions[i]) return false;
         }
 
-        public bool Equals(Objective? other)
-        {
-            if (other is null) return false;
-            if (ReferenceEquals(this, other)) return true;
-            if (!Equals(MindId, other.MindId) || !Equals(Prototype, other.Prototype)) return false;
-            if (_conditions.Count != other._conditions.Count) return false;
+        return true;
+    }
 
-            for (var i = 0; i < _conditions.Count; i++)
-            {
-                // just comparing condition entity ids here
-                // ideally compare the entities...
-                if (_conditions[i] != other._conditions[i]) return false;
-            }
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Objective) obj);
+    }
 
-            return true;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
-            return Equals((Objective) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Mind, Prototype, _conditions);
-        }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(MindId, Prototype, Conditions);
     }
 }
