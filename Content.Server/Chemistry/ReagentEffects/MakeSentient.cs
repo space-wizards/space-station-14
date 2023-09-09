189ecf880bd0f880b09898a8c6ad6073b8c62eab
unknown <shadowjayofdarkness@gmail.com>
2023-09-09 09:49:30 -0700

    fixed cognizine accent removal not working on entities that already have a mind

--- Content.Server/Chemistry/ReagentEffects/MakeSentient.cs
+++ Content.Server/Chemistry/ReagentEffects/MakeSentient.cs
@@ -16,12 +16,6 @@ public sealed partial class MakeSentient : ReagentEffect
         var entityManager = args.EntityManager;
         var uid = args.SolutionEntity;
 
-        // This makes it so it doesn't affect things that are already sentient
-        if (entityManager.HasComponent<MindContainerComponent>(uid))
-        {
-            return;
-        }
-
         // This piece of code makes things able to speak "normally". One thing of note is that monkeys have a unique accent and won't be affected by this.
         entityManager.RemoveComponent<ReplacementAccentComponent>(uid);
 
