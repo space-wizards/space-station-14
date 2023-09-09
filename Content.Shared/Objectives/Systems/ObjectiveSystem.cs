de4b299833eab0d8b86168821f27044906861528
deltanedas <@deltanedas:kde.org>
2023-09-09 04:20:27 +0100

    remove ObjectPrototype and conditions, making requirements ecs

--- Content.Shared/Objectives/Systems/ObjectiveSystem.cs
+++ Content.Shared/Objectives/Systems/ObjectiveSystem.cs
@@ -5,57 +5,100 @@ using Robust.Shared.Utility;
 
 namespace Content.Shared.Objectives.Systems;
 
+/// <summary>
+/// Provides API for creating and interacting with objectives.
+/// Adds default info from <see cref="ObjectiveComponent"/> to <see cref="ObjectiveGetInfoEvent"/>.
+/// </summary>
 public sealed class ObjectiveSystem : EntitySystem
 {
     [Dependency] private readonly SharedMindSystem _mind = default!;
 
-    /// <summary>
-    /// Creates a new Objective instance for storing on the mind.
-    /// </summary>
-    /// <remarks>
-    /// In the future this could be changed to an entity, with an objective component.
-    /// </remarks>
-    public Objective CreateObjective(EntityUid mindId, MindComponent mind, ObjectivePrototype proto)
+    public override void Initialize()
     {
-        // spawn all the condition entities from the prototype
-        var conditions = new List<EntityUid>(proto.Conditions.Count);
-        foreach (var id in proto.Conditions)
-        {
-            var cond = Spawn(id);
-            if (!HasComp<ObjectiveConditionComponent>(cond))
-            {
-                Del(cond);
-                Log.Error($"Invalid objective condition prototype {id} from {proto.ID}");
-                continue;
-            }
+        base.Initialize();
 
-            var ev = new ConditionAssignedEvent(mindId, mind);
-            RaiseLocalEvent(cond, ref ev);
-            if (ev.Cancelled)
-            {
-                Del(cond);
-                Log.Warning($"Could not assign objective condition {id} from {proto.ID}");
-                continue;
-            }
+        SubscribeLocalEvent<ObjectiveComponent, ObjectiveGetInfoEvent>(OnGetInfo);
+    }
 
-            conditions.Add(cond);
-        }
-
-        return new Objective(mindId, proto, conditions);
+    private void OnGetInfo(EntityUid uid, ObjectiveComponent comp, ref ObjectiveGetInfoEvent args)
+    {
+        if (comp.Title != null)
+            args.Info.Title = comp.Title;
+        if (comp.Description != null)
+            args.Info.Description = comp.Description;
+        if (comp.Icon != null)
+            args.Info.Icon = comp.Icon;
     }
 
     /// <summary>
-    /// Get the title, description and progress of an objective condition using <see cref="ConditionGetInfoEvent"/>.
+    /// Checks requirements and duplicate objectives to see if an objective can be assigned.
+    /// </summary>
+    public bool CanBeAssigned(EntityUid uid, EntityUid mindId, MindComponent mind, ObjectiveComponent? comp = null)
+    {
+        if (!Resolve(uid, ref comp))
+            return false;
+
+        var ev = new RequirementCheckEvent(mindId, mind);
+        RaiseLocalEvent(uid, ref ev);
+        if (ev.Cancelled)
+            return false;
+
+        // only check for duplicate prototypes if it's unique
+        if (comp.Unique)
+        {
+            foreach (var objective in mind.AllObjectives)
+            {
+                if (objective.Prototype.ID == ID)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Spawns and assigns an objective to the mind.
+    /// If the objective could not be assigned the objective is deleted and false is returned.
+    /// </summary>
+    public bool TryAddObjective(string proto, EntityUid mindId, MindComponent mind)
+    {
+        var uid = Spawn(proto);
+        if (!HasComp<ObjectiveComponent>(uid))
+        {
+            Del(uid);
+            Log.Error($"Invalid objective prototype {proto}: missing ObjectiveComponent");
+            return null;
+        }
+
+        Log.Debug($"Created objective {proto} ({uid}");
+
+        var ev = new ObjectiveAssignedEvent(mindId, mind);
+        RaiseLocalEvent(uid, ref ev);
+        if (ev.Cancelled)
+        {
+            Del(uid);
+            Log.Warning($"Could not assign objective {uid}, deleted it");
+            return false;
+        }
+
+        mind.AllObjectives.Add(uid);
+
+        return true;
+    }
+
+    /// <summary>
+    /// Get the title, description, icon and progress of an objective condition using <see cref="ObjectiveGetInfoEvent"/>.
+    /// Any null fields are logged and replaced with fallbacks so they will never be null.
     /// </summary>
     /// <param name="uid"/>ID of the condition entity</param>
     /// <param name="mindId"/>ID of the player's mind entity</param>
     /// <param name="mind"/>Mind component of the player's mind</param>
-    public ConditionInfo GetConditionInfo(EntityUid uid, EntityUid mindId, MindComponent? mind = null)
+    public ObjectiveInfo GetObjectiveInfo(EntityUid uid, EntityUid mindId, MindComponent? mind = null)
     {
         if (!Resolve(mindId, ref mind))
-            return new ConditionInfo(null, null, null, null);
+            return new ObjectiveInfo(null, null, null, null);
 
-        var ev = new ConditionGetInfoEvent(mindId, mind, new ConditionInfo(null, null, null, null));
+        var ev = new ObjectiveGetInfoEvent(mindId, mind, new ObjectiveInfo(null, null, null, null));
         RaiseLocalEvent(uid, ref ev);
 
         var info = ev.Info;
