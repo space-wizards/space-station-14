f0964a5eed49f31d0cc01c0530cbbd2b04e75287
deltanedas <@deltanedas:kde.org>
2023-09-09 02:00:27 +0100

    new condition info

--- Content.Shared/Objectives/ConditionInfo.cs
+++ Content.Shared/Objectives/ConditionInfo.cs
@@ -1,22 +1,17 @@
-﻿using Robust.Shared.Serialization;
+using Robust.Shared.Serialization;
 using Robust.Shared.Utility;
 
-namespace Content.Shared.Objectives
-{
-    [Serializable, NetSerializable]
-    public sealed class ConditionInfo
-    {
-        public string Title { get; }
-        public string Description { get; }
-        public SpriteSpecifier SpriteSpecifier { get; }
-        public float Progress { get; }
+namespace Content.Shared.Objectives;
 
-        public ConditionInfo(string title, string description, SpriteSpecifier spriteSpecifier, float progress)
-        {
-            Title = title;
-            Description = description;
-            SpriteSpecifier = spriteSpecifier;
-            Progress = progress;
-        }
-    }
-}
+/// <summary>
+/// Info about objectives visible in the character menu and on round end.
+/// Description and icon are displayed only in the character menu.
+/// Progress is a percentage from 0.0 to 1.0.
+/// </summary>
+/// <remarks>
+/// All of these fields must eventually be set by condition event handlers.
+/// Everything but progress can be set to static data in yaml on <see cref="ObjectiveConditionComponent"/>.
+/// If anything is null it will fallback to a very noticable fallback string/empty progress or the error sprite.
+/// </remarks>
+[Serializable, NetSerializable]
+public record struct ConditionInfo(string? Title, string? Description, SpriteSpecifier? Icon, float? Progress);
