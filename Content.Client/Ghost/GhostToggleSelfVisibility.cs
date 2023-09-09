01a5883bd86f92304a3bca5766d9e9ba0f5da6d5
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2023-09-09 00:48:06 -0700

    Add a command that toggles self ghost visibility

---
+++ Content.Client/Ghost/GhostToggleSelfVisibility.cs
@@ -0,0 +1,30 @@
+﻿using Content.Shared.Ghost;
+using Robust.Client.GameObjects;
+using Robust.Shared.Console;
+
+namespace Content.Client.Ghost;
+
+public sealed class GhostToggleSelfVisibility : IConsoleCommand
+{
+    public string Command => "toggle_self_visibility";
+    public string Description => "Toggles visibility of self ghost on your own view.";
+    public string Help => "toggle_self_visibility";
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var attachedEntity = shell.Player?.AttachedEntity;
+        if (!attachedEntity.HasValue)
+            return;
+
+        var entityManager = IoCManager.Resolve<IEntityManager>();
+        if (!entityManager.HasComponent<GhostComponent>(attachedEntity))
+        {
+            shell.WriteError("Entity must be a ghost.");
+            return;
+        }
+
+        if (!entityManager.TryGetComponent(attachedEntity, out SpriteComponent? spriteComponent))
+            return;
+
+        spriteComponent.Visible = !spriteComponent.Visible;
+    }
+}
