96a0f2bd615909cbf4dbd3ac86349b5089c9abed
deltanedas <@deltanedas:kde.org>
2023-09-10 15:57:58 +0100

    pai system file scope

--- Content.Server/PAI/PAISystem.cs
+++ Content.Server/PAI/PAISystem.cs
@@ -9,119 +9,118 @@ using Robust.Server.GameObjects;
 using Robust.Shared.Random;
 using System.Text;
 
-namespace Content.Server.PAI
+namespace Content.Server.PAI;
+
+public sealed class PAISystem : SharedPAISystem
 {
-    public sealed class PAISystem : SharedPAISystem
+    [Dependency] private readonly InstrumentSystem _instrumentSystem = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly MetaDataSystem _metaData = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly ToggleableGhostRoleSystem _toggleableGhostRole = default!;
+
+    /// <summary>
+    /// Possible symbols that can replace characters in the pai's owner name when microwaved.
+    /// </summary>
+    private static readonly char[] SYMBOLS = new[] { '#', '~', '-', '@', '&', '^', '%', '$', '*'};
+
+    public override void Initialize()
     {
-        [Dependency] private readonly InstrumentSystem _instrumentSystem = default!;
-        [Dependency] private readonly IRobustRandom _random = default!;
-        [Dependency] private readonly MetaDataSystem _metaData = default!;
-        [Dependency] private readonly SharedPopupSystem _popup = default!;
-        [Dependency] private readonly ToggleableGhostRoleSystem _toggleableGhostRole = default!;
+        base.Initialize();
 
-        /// <summary>
-        /// Possible symbols that can replace characters in the pai's owner name when microwaved.
-        /// </summary>
-        private static readonly char[] SYMBOLS = new[] { '#', '~', '-', '@', '&', '^', '%', '$', '*'};
+        SubscribeLocalEvent<PAIComponent, UseInHandEvent>(OnUseInHand);
+        SubscribeLocalEvent<PAIComponent, MindAddedMessage>(OnMindAdded);
+        SubscribeLocalEvent<PAIComponent, MindRemovedMessage>(OnMindRemoved);
+        SubscribeLocalEvent<PAIComponent, BeingMicrowavedEvent>(OnMicrowaved);
+    }
 
-        public override void Initialize()
+    private void OnUseInHand(EntityUid uid, PAIComponent component, UseInHandEvent args)
+    {
+        if (!TryComp<MindContainerComponent>(uid, out var mind) || !mind.HasMind)
+            component.LastUser = args.User;
+    }
+
+    private void OnMindAdded(EntityUid uid, PAIComponent component, MindAddedMessage args)
+    {
+        if (component.LastUser == null)
+            return;
+
+        // Ownership tag
+        var val = Loc.GetString("pai-system-pai-name", ("owner", component.LastUser));
+
+        // TODO Identity? People shouldn't dox-themselves by carrying around a PAI.
+        // But having the pda's name permanently be "old lady's PAI" is weird.
+        // Changing the PAI's identity in a way that ties it to the owner's identity also seems weird.
+        // Cause then you could remotely figure out information about the owner's equipped items.
+
+        _metaData.SetEntityName(uid, val);
+    }
+
+    private void OnMindRemoved(EntityUid uid, PAIComponent component, MindRemovedMessage args)
+    {
+        // Mind was removed, shutdown the PAI.
+        PAITurningOff(uid);
+    }
+
+    private void OnMicrowaved(EntityUid uid, PAIComponent comp, BeingMicrowavedEvent args)
+    {
+        // name will always be scrambled whether it gets bricked or not, this is the reward
+        ScrambleName(uid, comp);
+
+        // randomly brick it
+        if (_random.Prob(comp.BrickChance))
         {
-            base.Initialize();
-
-            SubscribeLocalEvent<PAIComponent, UseInHandEvent>(OnUseInHand);
-            SubscribeLocalEvent<PAIComponent, MindAddedMessage>(OnMindAdded);
-            SubscribeLocalEvent<PAIComponent, MindRemovedMessage>(OnMindRemoved);
-            SubscribeLocalEvent<PAIComponent, BeingMicrowavedEvent>(OnMicrowaved);
+            _popup.PopupEntity(Loc.GetString(comp.BrickPopup), uid, PopupType.LargeCaution);
+            RemComp<PAIComponent>(uid);
+            _toggleableGhostRole.Wipe(uid);
         }
-
-        private void OnUseInHand(EntityUid uid, PAIComponent component, UseInHandEvent args)
+        else
         {
-            if (!TryComp<MindContainerComponent>(uid, out var mind) || !mind.HasMind)
-                component.LastUser = args.User;
+            // you are lucky...
+            _popup.PopupEntity(Loc.GetString(comp.ScramblePopup), uid, PopupType.Large);
         }
+    }
 
-        private void OnMindAdded(EntityUid uid, PAIComponent component, MindAddedMessage args)
+    private void ScrambleName(EntityUid uid, PAIComponent comp)
+    {
+        // randomly replace random characters from the old name
+        var oldName = Name(uid);
+        var name = new StringBuilder(oldName.Length);
+        foreach (var character in oldName)
         {
-            if (component.LastUser == null)
-                return;
+            // only scramble the owner name, don't scramble "'s pAI"
+            if (character == '\'')
+                break;
 
-            // Ownership tag
-            var val = Loc.GetString("pai-system-pai-name", ("owner", component.LastUser));
-
-            // TODO Identity? People shouldn't dox-themselves by carrying around a PAI.
-            // But having the pda's name permanently be "old lady's PAI" is weird.
-            // Changing the PAI's identity in a way that ties it to the owner's identity also seems weird.
-            // Cause then you could remotely figure out information about the owner's equipped items.
-
-            _metaData.SetEntityName(uid, val);
-        }
-
-        private void OnMindRemoved(EntityUid uid, PAIComponent component, MindRemovedMessage args)
-        {
-            // Mind was removed, shutdown the PAI.
-            PAITurningOff(uid);
-        }
-
-        private void OnMicrowaved(EntityUid uid, PAIComponent comp, BeingMicrowavedEvent args)
-        {
-            // name will always be scrambled whether it gets bricked or not, this is the reward
-            ScrambleName(uid, comp);
-
-            // randomly brick it
-            if (_random.Prob(comp.BrickChance))
+            if (_random.Prob(comp.CharScrambleChance))
             {
-                _popup.PopupEntity(Loc.GetString(comp.BrickPopup), uid, PopupType.LargeCaution);
-                RemComp<PAIComponent>(uid);
-                _toggleableGhostRole.Wipe(uid);
+                name.Append(_random.Pick(SYMBOLS));
             }
             else
             {
-                // you are lucky...
-                _popup.PopupEntity(Loc.GetString(comp.ScramblePopup), uid, PopupType.Large);
+                name.Append(character);
             }
         }
 
-        private void ScrambleName(EntityUid uid, PAIComponent comp)
+        _metaData.SetEntityName(uid, name.ToString());
+    }
+
+    public void PAITurningOff(EntityUid uid)
+    {
+        //  Close the instrument interface if it was open
+        //  before closing
+        if (HasComp<ActiveInstrumentComponent>(uid) && TryComp<ActorComponent>(uid, out var actor))
         {
-            // randomly replace random characters from the old name
-            var oldName = Name(uid);
-            var name = new StringBuilder(oldName.Length);
-            foreach (var character in oldName)
-            {
-                // only scramble the owner name, don't scramble "'s pAI"
-                if (character == '\'')
-                    break;
-
-                if (_random.Prob(comp.CharScrambleChance))
-                {
-                    name.Append(_random.Pick(SYMBOLS));
-                }
-                else
-                {
-                    name.Append(character);
-                }
-            }
-
-            _metaData.SetEntityName(uid, name.ToString());
+            _instrumentSystem.ToggleInstrumentUi(uid, actor.PlayerSession);
         }
 
-        public void PAITurningOff(EntityUid uid)
+        //  Stop instrument
+        if (TryComp<InstrumentComponent>(uid, out var instrument)) _instrumentSystem.Clean(uid, instrument);
+        if (TryComp<MetaDataComponent>(uid, out var metadata))
         {
-            //  Close the instrument interface if it was open
-            //  before closing
-            if (HasComp<ActiveInstrumentComponent>(uid) && TryComp<ActorComponent>(uid, out var actor))
-            {
-                _instrumentSystem.ToggleInstrumentUi(uid, actor.PlayerSession);
-            }
-
-            //  Stop instrument
-            if (TryComp<InstrumentComponent>(uid, out var instrument)) _instrumentSystem.Clean(uid, instrument);
-            if (TryComp<MetaDataComponent>(uid, out var metadata))
-            {
-                var proto = metadata.EntityPrototype;
-                if (proto != null)
-                    _metaData.SetEntityName(uid, proto.Name);
-            }
+            var proto = metadata.EntityPrototype;
+            if (proto != null)
+                _metaData.SetEntityName(uid, proto.Name);
         }
     }
 }
