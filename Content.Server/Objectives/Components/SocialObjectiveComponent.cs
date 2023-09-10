7d0e27ac33548401aaa67ea6a9f01bc7cd905cd4
deltanedas <@deltanedas:kde.org>
2023-09-10 03:57:05 +0100

    smart

--- Content.Server/Objectives/Components/SocialObjectiveComponent.cs
+++ Content.Server/Objectives/Components/SocialObjectiveComponent.cs
@@ -3,6 +3,7 @@ namespace Content.Server.Objectives.Components;
 /// <summary>
 /// Marker component for social objectives and kill objectives to be mutually exclusive.
 /// </summary>
+[RegisterComponent]
 public sealed partial class SocialObjectiveComponent : Component
 {
 }
