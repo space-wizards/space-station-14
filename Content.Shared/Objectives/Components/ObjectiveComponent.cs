d57d3b984d65c19d3c2906ad5c945ce2d59d5bf2
deltanedas <@deltanedas:kde.org>
2023-09-10 03:11:02 +0100

    objective component and system

---
+++ Content.Shared/Objectives/Components/ObjectiveComponent.cs
@@ -0,0 +1,91 @@
+using Content.Shared.Mind;
+using Content.Shared.Objectives;
+using Content.Shared.Objectives.Systems;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Objectives.Components;
+
+/// <summary>
+/// Required component for an objective entity prototype.
+/// Mostly it provides optional static data for the objective info event.
+/// Progress cannot be provided this way, another system has to do that.
+/// </summary>
+[RegisterComponent, Access(typeof(ObjectiveSystem))]
+public sealed partial class ObjectiveComponent : Component
+{
+    /// <summary>
+    /// Difficulty rating used to avoid assigning too many difficult objectives.
+    /// </summary>
+    [DataField("difficulty", required: true), ViewVariables(VVAccess.ReadWrite)]
+    public float Difficulty;
+
+    /// <summary>
+    /// Organisation that issued this objective, used for grouping and as a header above common objectives.
+    /// </summary>
+    [DataField("issuer", required: true), ViewVariables(VVAccess.ReadWrite)]
+    public string Issuer = string.Empty;
+
+    /// <summary>
+    /// Unique objectives can only have 1 per prototype id.
+    /// Set this to false if you want multiple objectives of the same prototype.
+    /// </summary>
+    [DataField("unique"), ViewVariables(VVAccess.ReadWrite)]
+    public bool Unique = true;
+
+    /// <summary>
+    /// Optional locale id to handle <see cref="ObjectiveGetInfoEvent"/>'s title.
+    /// If another component provides it do not use this.
+    /// </summary>
+    [DataField("title"), ViewVariables(VVAccess.ReadWrite)]
+    public string? Title;
+
+    /// <summary>
+    /// Optional locale id to handle <see cref="ObjectiveGetInfoEvent"/>'s description.
+    /// If another component provides it do not use this.
+    /// </summary>
+    [DataField("description"), ViewVariables(VVAccess.ReadWrite)]
+    public string? Description;
+
+    /// <summary>
+    /// Optional icon to handle <see cref="ObjectiveGetInfoEvent"/>'s icon.
+    /// If another component provides it (should only be done if it depends on per-objective data) do not use this.
+    /// </summary>
+    [DataField("icon"), ViewVariables(VVAccess.ReadWrite)]
+    public SpriteSpecifier? Icon;
+}
+
+/// <summary>
+/// Event raised on an objective after spawning it to see if it meets all the requirements.
+/// Requirement components should have subscriptions and cancel if the requirements are not met.
+/// If a requirement is not met then the objective is deleted.
+/// </summary>
+[ByRefEvent]
+public record struct RequirementCheckEvent(EntityUid MindId, MindComponent Mind, bool Cancelled = false);
+
+/// <summary>
+/// Event raised on an objective after its requirements have been checked.
+/// If <see cref="Cancelled"/> is set to true, the objective is deleted.
+/// Use this if the objective cannot be used, like a kill objective with no people alive.
+/// </summary>
+[ByRefEvent]
+public record struct ObjectiveAssignedEvent(EntityUid MindId, MindComponent Mind, bool Cancelled = false);
+
+/// <summary>
+/// Event raised on an objective to get info about it.
+/// In handlers set fields of <see cref="Info"/>.
+/// To use this yourself call <see cref="ObjectiveSystem.GetObjectiveInfo"/> with the mind.
+/// </summary>
+[ByRefEvent]
+public class ObjectiveGetInfoEvent
+{
+    public EntityUid MindId;
+    public MindComponent Mind;
+    public ObjectiveInfo Info;
+
+    public ObjectiveGetInfoEvent(EntityUid mindId, MindComponent mind, ObjectiveInfo info)
+    {
+        MindId = mindId;
+        Mind = mind;
+        Info = info;
+    }
+}
---
+++ Content.Shared/Objectives/Systems/ObjectiveSystem.cs
@@ -0,0 +1,135 @@
+using Content.Shared.Mind;
+using Content.Shared.Objectives;
+using Content.Shared.Objectives.Components;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Objectives.Systems;
+
+/// <summary>
+/// Provides API for creating and interacting with objectives.
+/// Adds default info from <see cref="ObjectiveComponent"/> to <see cref="ObjectiveGetInfoEvent"/>.
+/// </summary>
+public sealed class ObjectiveSystem : EntitySystem
+{
+    [Dependency] private readonly SharedMindSystem _mind = default!;
+
+    private EntityQuery<MetaDataComponent> _metaQuery;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _metaQuery = GetEntityQuery<MetaDataComponent>();
+
+        SubscribeLocalEvent<ObjectiveComponent, ObjectiveGetInfoEvent>(OnGetInfo);
+    }
+
+    private void OnGetInfo(EntityUid uid, ObjectiveComponent comp, ref ObjectiveGetInfoEvent args)
+    {
+        if (comp.Title != null)
+            args.Info.Title = comp.Title;
+        if (comp.Description != null)
+            args.Info.Description = comp.Description;
+        if (comp.Icon != null)
+            args.Info.Icon = comp.Icon;
+    }
+
+    /// <summary>
+    /// Checks requirements and duplicate objectives to see if an objective can be assigned.
+    /// </summary>
+    public bool CanBeAssigned(EntityUid uid, EntityUid mindId, MindComponent mind, ObjectiveComponent? comp = null)
+    {
+        if (!Resolve(uid, ref comp))
+            return false;
+
+        var ev = new RequirementCheckEvent(mindId, mind);
+        RaiseLocalEvent(uid, ref ev);
+        if (ev.Cancelled)
+            return false;
+
+        // only check for duplicate prototypes if it's unique
+        if (comp.Unique)
+        {
+            var proto = _metaQuery.GetComponent(uid).EntityPrototype?.ID;
+            foreach (var objective in mind.AllObjectives)
+            {
+                if (_metaQuery.GetComponent(objective).EntityPrototype?.ID == proto)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Spawns and assigns an objective for a mind.
+    /// The objective is not added to the mind's objectives, mind system does that in TryAddObjective.
+    /// If the objective could not be assigned the objective is deleted and null is returned.
+    /// </summary>
+    public EntityUid? TryCreateObjective(EntityUid mindId, MindComponent mind, string proto)
+    {
+        var uid = Spawn(proto);
+        if (!TryComp<ObjectiveComponent>(uid, out var comp))
+        {
+            Del(uid);
+            Log.Error($"Invalid objective prototype {proto}, missing ObjectiveComponent");
+            return null;
+        }
+
+        Log.Debug($"Created objective {proto} ({uid}");
+
+        if (!CanBeAssigned(uid, mindId, mind, comp))
+        {
+            Del(uid);
+            Log.Warning($"Objective {uid} did not match the requirements for {_mind.MindOwnerLoggingString(mind)}, deleted it");
+            return null;
+        }
+
+        var ev = new ObjectiveAssignedEvent(mindId, mind);
+        RaiseLocalEvent(uid, ref ev);
+        if (ev.Cancelled)
+        {
+            Del(uid);
+            Log.Warning($"Could not assign objective {uid}, deleted it");
+            return null;
+        }
+
+        return uid;
+    }
+
+    /// <summary>
+    /// Get the title, description, icon and progress of an objective using <see cref="ObjectiveGetInfoEvent"/>.
+    /// Any null fields are logged and replaced with fallbacks so they will never be null.
+    /// </summary>
+    /// <param name="uid"/>ID of the condition entity</param>
+    /// <param name="mindId"/>ID of the player's mind entity</param>
+    /// <param name="mind"/>Mind component of the player's mind</param>
+    public ObjectiveInfo GetInfo(EntityUid uid, EntityUid mindId, MindComponent? mind = null)
+    {
+        if (!Resolve(mindId, ref mind))
+            return new ObjectiveInfo(null, null, null, null);
+
+        var ev = new ObjectiveGetInfoEvent(mindId, mind, new ObjectiveInfo(null, null, null, null));
+        RaiseLocalEvent(uid, ref ev);
+
+        var info = ev.Info;
+        if (info.Title == null || info.Description == null || info.Icon == null || info.Progress == null)
+        {
+            Log.Error($"An objective {uid} of {_mind.MindOwnerLoggingString(mind)} has incomplete info: {info.Title} {info.Description} {info.Progress}");
+            info.Title ??= "!!!BROKEN OBJECTIVE!!!";
+            info.Description ??= "!!! BROKEN OBJECTIVE DESCRIPTION!!!";
+            info.Icon ??= new SpriteSpecifier.Rsi(new ("error.rsi"), "error.rsi");
+            info.Progress ??= 0f;
+        }
+
+        return info;
+    }
+
+    /// <summary>
+    /// Helper for mind to get a objective's title easily
+    /// </summary>
+    public string GetTitle(EntityUid uid, EntityUid mindId, MindComponent? mind = null)
+    {
+        return GetInfo(uid, mindId, mind).Title!;
+    }
+}
