68ad6d75ec68882bba4b4a1cb3da3b6e67491ba0
deltanedas <@deltanedas:kde.org>
2023-09-14 02:59:44 +0100

    file scope bui

--- Content.Client/Power/SMES/SmesBoundUserInterface.cs
+++ Content.Client/Power/SMES/SmesBoundUserInterface.cs
@@ -1,49 +1,43 @@
 using Content.Client.Power.SMES.UI;
 using Content.Shared.SMES;
-using JetBrains.Annotations;
 using Robust.Client.GameObjects;
 using Robust.Shared.GameObjects;
 using Robust.Shared.ViewVariables;
 
-namespace Content.Client.Power.SMES
+namespace Content.Client.Power.SMES;
+
+public sealed class SmesBoundUserInterface : BoundUserInterface
 {
-    [UsedImplicitly]
-    public sealed class SmesBoundUserInterface : BoundUserInterface
+    [ViewVariables] private SmesMenu? _menu;
+
+    public SmesBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
-        [ViewVariables] private SmesMenu? _menu;
+    }
 
-        protected override void Open()
-        {
-            base.Open();
+    protected override void Open()
+    {
+        base.Open();
 
-            _menu = new SmesMenu(this, Owner);
-            _menu.OnClose += Close;
-            _menu.OpenCentered();
-        }
+        _menu = new SmesMenu(this, Owner);
+        _menu.OnClose += Close;
+        _menu.OpenCentered();
+    }
 
-        public SmesBoundUserInterface(ClientUserInterfaceComponent owner, Enum uiKey) : base(owner, uiKey)
-        {
-        }
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
 
-        protected override void UpdateState(BoundUserInterfaceState state)
-        {
-            base.UpdateState(state);
+        if (state is not SmesBoundInterfaceState cast || _menu == null)
+            return;
 
-            if (state is not SmesBoundInterfaceState cast || _menu == null)
-            {
-                return;
-            }
-            _menu.UpdateState(cast);
-        }
+        _menu.UpdateState(cast);
+    }
 
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
 
-            if (disposing)
-            {
-                _menu?.Dispose();
-            }
-        }
+        if (disposing)
+            _menu?.Dispose();
     }
 }
