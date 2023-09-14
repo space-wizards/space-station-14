5127819aa6160e8b8e82761735b80c95752a9b60
deltanedas <@deltanedas:kde.org>
2023-09-14 17:25:50 +0100

    ?

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -46,6 +46,7 @@ namespace Content.IntegrationTests.Tests
         {
             "Dev",
             "TestTeg",
+            "Gateways",
             "Fland",
             "Meta",
             "Packed",
