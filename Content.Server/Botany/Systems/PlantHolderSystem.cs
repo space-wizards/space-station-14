3f51795cc75956ee1b872e508c82fa0ce0ac1a53
Doru991 <75124791+Doru991@users.noreply.github.com>
2023-09-14 07:56:24 +0300

    Add plant species mutations (#19960)

--- Content.Server/Botany/Systems/PlantHolderSystem.cs
+++ Content.Server/Botany/Systems/PlantHolderSystem.cs
@@ -347,6 +347,7 @@ public sealed class PlantHolderSystem : EntitySystem
         if (component.MutationLevel > 0)
         {
             Mutate(uid, Math.Min(component.MutationLevel, 25), component);
+            component.UpdateSpriteAfterUpdate = true;
             component.MutationLevel = 0;
         }
 
@@ -844,7 +845,7 @@ public sealed class PlantHolderSystem : EntitySystem
         if (component.Seed != null)
         {
             EnsureUniqueSeed(uid, component);
-            _mutation.MutateSeed(component.Seed, severity);
+            _mutation.MutateSeed(ref component.Seed, severity);
         }
     }
 
