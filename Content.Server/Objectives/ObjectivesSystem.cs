f7c732fdf5e1493ee5c98501c7d8c829b3bd102e
deltanedas <@deltanedas:kde.org>
2023-09-15 16:42:42 +0100

    group rules by agent name and prepended text

--- Content.Server/Objectives/ObjectivesSystem.cs
+++ Content.Server/Objectives/ObjectivesSystem.cs
@@ -31,96 +31,136 @@ public sealed class ObjectivesSystem : SharedObjectivesSystem
     /// </summary>
     private void OnRoundEndText(RoundEndTextAppendEvent ev)
     {
+        // go through each gamerule getting data for the roundend summary.
+        var summaries = new Dictionary<string, Dictionary<string, List<EntityUid>>>();
         var query = EntityQueryEnumerator<GameRuleComponent>();
         while (query.MoveNext(out var uid, out var gameRule))
         {
             if (!_gameTicker.IsGameRuleAdded(uid, gameRule))
                 continue;
 
-            // TODO: rework to combine the same agents from different rules if no prepend
-            var summary = new ObjectivesTextGetInfoEvent(new List<EntityUid>(), string.Empty);
-            RaiseLocalEvent(uid, ref summary);
-            if (summary.Minds.Count == 0)
+            var info = new ObjectivesTextGetInfoEvent(new List<EntityUid>(), string.Empty);
+            RaiseLocalEvent(uid, ref info);
+            if (info.Minds.Count == 0)
                 continue;
 
-            var agent = summary.AgentName;
-            var result = Loc.GetString("objectives-round-end-result", ("count", summary.Minds.Count), ("agent", agent));
-            var prepend = new ObjectivesTextPrependEvent(result);
+            // first group the gamerules by their agents, for example 2 different dragons
+            var agent = info.AgentName;
+            if (!summaries.ContainsKey(agent))
+                summaries[agent] = new Dictionary<string, List<EntityUid>>();
+
+            var prepend = new ObjectivesTextPrependEvent("");
             RaiseLocalEvent(uid, ref prepend);
-            // space between the start text and player list
-            result = prepend.Text + "\n";
 
-            foreach (var mindId in summary.Minds)
+            // next group them by their prepended texts
+            // for example with traitor rule, group them by the codewords they share
+            var summary = summaries[agent];
+            if (summary.ContainsKey(prepend.Text))
             {
-                if (!TryComp(mindId, out MindComponent? mind))
-                    continue;
+                // same prepended text (usually empty) so combine them
+                summary[prepend.Text].AddRange(info.Minds);
+            }
+            else
+            {
+                summary[prepend.Text] = info.Minds;
+            }
+        }
 
-                var name = mind.CharacterName;
-                _mind.TryGetSession(mindId, out var session);
-                var username = session?.Name;
+        // convert the data into summary text
+        foreach (var (agent, summary) in summaries)
+        {
+            // first get the total number of players that were in these game rules combined
+            var total = 0;
+            foreach (var (_, minds) in summary)
+            {
+                total += minds.Count;
+            }
 
-                string title;
-                if (username != null)
-                {
-                    if (name != null)
-                        title = Loc.GetString("objectives-player-user-named", ("user", username), ("name", name));
-                    else
-                        title = Loc.GetString("objectives-player-user", ("user", username));
-                }
-                else
-                {
-                    // nothing to identify the player by, just give up
-                    if (name == null)
-                        continue;
+            var result = Loc.GetString("objectives-round-end-result", ("count", total), ("agent", agent));
+            // next add all the players with its own prepended text
+            foreach (var (prepend, minds) in summary)
+            {
+                // add space between the start text and player list
+                if (prepend != string.Empty)
+                    result += prepend + "\n";
 
-                    title = Loc.GetString("objectives-player-named", ("name", name));
-                }
-
-                result += "\n";
-
-                var objectives = mind.AllObjectives.ToArray();
-                if (objectives.Length == 0)
-                {
-                    result += Loc.GetString("objectives-no-objectives", ("title", title), ("agent", agent));
-                    continue;
-                }
-
-                result += Loc.GetString("objectives-with-objectives", ("title", title), ("agent", agent));
-
-                foreach (var objectiveGroup in objectives.GroupBy(o => Comp<ObjectiveComponent>(o).Issuer))
-                {
-                    result += "\n" + Loc.GetString($"objective-issuer-{objectiveGroup.Key}");
-
-                    foreach (var objective in objectiveGroup)
-                    {
-                        var info = GetInfo(objective, mindId, mind);
-                        if (info == null)
-                            continue;
-
-                        var objectiveTitle = info.Value.Title;
-                        var progress = info.Value.Progress;
-                        if (progress > 0.99f)
-                        {
-                            result += "\n- " + Loc.GetString(
-                                "objectives-objective-success",
-                                ("objective", objectiveTitle),
-                                ("markupColor", "green")
-                            );
-                        }
-                        else
-                        {
-                            result += "\n- " + Loc.GetString(
-                                "objectives-objective-fail",
-                                ("objective", objectiveTitle),
-                                ("progress", (int) (progress * 100)),
-                                ("markupColor", "red")
-                            );
-                        }
-                    }
-                }
+                AddSummary(ref result, agent, minds);
             }
 
             ev.AddLine(result + "\n");
+        }
+    }
+
+    private void AddSummary(ref string result, string agent, List<EntityUid> minds)
+    {
+        foreach (var mindId in minds)
+        {
+            if (!TryComp(mindId, out MindComponent? mind))
+                continue;
+
+            var name = mind.CharacterName;
+            _mind.TryGetSession(mindId, out var session);
+            var username = session?.Name;
+
+            string title;
+            if (username != null)
+            {
+                if (name != null)
+                    title = Loc.GetString("objectives-player-user-named", ("user", username), ("name", name));
+                else
+                    title = Loc.GetString("objectives-player-user", ("user", username));
+            }
+            else
+            {
+                // nothing to identify the player by, just give up
+                if (name == null)
+                    continue;
+
+                title = Loc.GetString("objectives-player-named", ("name", name));
+            }
+
+            result += "\n";
+
+            var objectives = mind.AllObjectives.ToArray();
+            if (objectives.Length == 0)
+            {
+                result += Loc.GetString("objectives-no-objectives", ("title", title), ("agent", agent));
+                continue;
+            }
+
+            result += Loc.GetString("objectives-with-objectives", ("title", title), ("agent", agent));
+
+            foreach (var objectiveGroup in objectives.GroupBy(o => Comp<ObjectiveComponent>(o).Issuer))
+            {
+                result += "\n" + Loc.GetString($"objective-issuer-{objectiveGroup.Key}");
+
+                foreach (var objective in objectiveGroup)
+                {
+                    var info = GetInfo(objective, mindId, mind);
+                    if (info == null)
+                        continue;
+
+                    var objectiveTitle = info.Value.Title;
+                    var progress = info.Value.Progress;
+                    if (progress > 0.99f)
+                    {
+                        result += "\n- " + Loc.GetString(
+                            "objectives-objective-success",
+                            ("objective", objectiveTitle),
+                            ("markupColor", "green")
+                        );
+                    }
+                    else
+                    {
+                        result += "\n- " + Loc.GetString(
+                            "objectives-objective-fail",
+                            ("objective", objectiveTitle),
+                            ("progress", (int) (progress * 100)),
+                            ("markupColor", "red")
+                        );
+                    }
+                }
+            }
         }
     }
 
