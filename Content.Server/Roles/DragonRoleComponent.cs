46470aab6324d85f51613fec6555863e51971453
deltanedas <@deltanedas:kde.org>
2023-09-15 09:45:14 +0100

    dragon role

---
+++ Content.Server/Roles/DragonRoleComponent.cs
@@ -0,0 +1,17 @@
+using Content.Server.Dragon;
+using Content.Shared.Roles;
+
+namespace Content.Server.Roles;
+
+/// <summary>
+/// Role used to keep track of space dragons for greentext and tracking total rifts charged.
+/// </summary>
+[RegisterComponent, Access(typeof(DragonSystem))]
+public sealed partial class DragonRoleComponent : AntagonistRoleComponent
+{
+    /// <summary>
+    /// Number of carp rifts that are fully charged.
+    /// </summary>
+    [DataField("riftsCharged")]
+    public int RiftsCharged;
+}
