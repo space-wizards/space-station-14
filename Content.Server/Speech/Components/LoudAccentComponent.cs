de9f4a554f437ba68499d595f7378f500958345e
Raitononai <raitononai@gmail.com>
2023-09-16 20:33:54 +0100

    ADDS THE NEW TRAIT AND ALL ITS FILESdsf  GRAAAHdsf

---
+++ Content.Server/Speech/Components/LoudAccentComponent.cs
@@ -0,0 +1,21 @@
+using Content.Server.Speech.EntitySystems;
+
+namespace Content.Server.Speech.Components;
+
+[RegisterComponent]
+[Access(typeof(LoudSystem))]
+public sealed partial class LoudAccentComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("yellChance")]
+    public float YellChance = 0.2f;
+
+    [ViewVariables]
+    public readonly List<string> YellSuffixes = new()
+    {
+        "accent-loud-suffix-1",
+        "accent-loud-suffix-2",
+        "accent-loud-suffix-3",
+        "accent-loud-suffix-4"
+    };
+}
---
+++ Content.Server/Speech/EntitySystems/LoudSystem.cs
@@ -0,0 +1,48 @@
+using Content.Server.Speech.Components;
+using Robust.Shared.Random;
+using System.Text.RegularExpressions;
+
+namespace Content.Server.Speech.EntitySystems;
+
+public sealed class LoudSystem : EntitySystem
+{
+    [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<LoudAccentComponent, AccentGetEvent>(OnAccentGet);
+    }
+    public string Accentuate(string message, LoudAccentComponent component)
+    {
+
+        var msg = message;
+
+        //Makes everything you say caps
+
+        msg = msg.ToString().ToUpper();
+
+        //Appends !! so that you yell and chat is bold
+        msg = msg + "!!";
+
+        //Get .ftl file with suffixes
+        msg = _replacement.ApplyReplacements(msg, "loud");
+
+        //Apply random suffix to var if random chance succeeds
+        if (!_random.Prob(component.YellChance))
+            return msg;
+
+        var pick = _random.Pick(component.YellSuffixes);
+
+        msg = msg + " " + Loc.GetString(pick);
+ 
+        return msg;
+    }
+
+    private void OnAccentGet(EntityUid uid, LoudAccentComponent component, AccentGetEvent args)
+    {
+        args.Message = Accentuate(args.Message, component);
+    }
+}
