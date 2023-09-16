57ed47b0d2d226d931c4c910abaa482c0b5dd38c
brainfood1183 <newman1183@googlemail.com>
2023-09-16 19:22:51 +0100

    sorted dependencies

--- Content.Server/Clothing/Systems/SkaterSystem.cs
+++ Content.Server/Clothing/Systems/SkaterSystem.cs
@@ -2,14 +2,15 @@ using Robust.Shared.Random;
 using Content.Shared.Movement.Events;
 using Content.Shared.Stunnable;
 using Content.Shared.Clothing;
+using Content.Shared.Movement.Systems;
 
 namespace Content.Server.Clothing;
 
 public sealed class SkaterSystem : EntitySystem
 {
-    [Dependency] private readonly SharedStunSystem _stun = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedStunSystem _stun = default!;
 
     public override void Initialize()
     {
@@ -20,6 +21,7 @@ public sealed class SkaterSystem : EntitySystem
 
     public void OnMove(EntityUid uid, SkaterComponent component, ref MoveInputEvent args)
     {
+
         if (_random.Prob(component.KnockChance))
         {
             _audio.PlayPvs(component.KnockSound, uid);
