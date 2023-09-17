9c5ff2d3d74eda160c11fbb8c72f2bf2f81a9a00
brainfood1183 <newman1183@googlemail.com>
2023-09-17 20:35:17 +0100

    fix

--- Content.Server/Abilities/Firestarter/FirestarterSystem.cs
+++ Content.Server/Abilities/Firestarter/FirestarterSystem.cs
@@ -10,59 +10,58 @@ using Content.Shared.Damage;
 /// Adds an action ability that will cause all flammable targets in a radius to ignite, also heals the owner
 /// of the component when used.
 /// </summary>
-namespace Content.Server.Abilities.Firestarter
+namespace Content.Server.Abilities.Firestarter;
+
+public sealed class FirestarterSystem : EntitySystem
 {
-    public sealed class FirestarterSystem : EntitySystem
+    [Dependency] private readonly DamageableSystem _damageable = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly FlammableSystem _flammable = default!;
+    [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedContainerSystem _container = default!;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly DamageableSystem _damageable = default!;
-        [Dependency] private readonly EntityLookupSystem _lookup = default!;
-        [Dependency] private readonly FlammableSystem _flammable = default!;
-        [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
-        [Dependency] private readonly SharedAudioSystem _audio = default!;
-        [Dependency] private readonly SharedContainerSystem _container = default!;
+        base.Initialize();
+        SubscribeLocalEvent<FirestarterComponent, ComponentInit>(OnComponentInit);
+        SubscribeLocalEvent<FirestarterComponent, FireStarterActionEvent>(OnStartFire);
+    }
 
-        public override void Initialize()
+    /// <summary>
+    /// Adds the firestarter action.
+    /// </summary>
+    private void OnComponentInit(EntityUid uid, FirestarterComponent component, ComponentInit args)
+    {
+        _actionsSystem.AddAction(uid, ref component.FireStarterActionEntity, component.FireStarterAction, uid);
+    }
+
+    /// <summary>
+    /// Ignites nearby flammable objects.
+    /// </summary>
+    private void OnStartFire(EntityUid uid, FirestarterComponent component, FireStarterActionEvent args)
+    {
+
+        if (_container.IsEntityOrParentInContainer(uid))
+            return;
+
+        var xform = Transform(uid);
+        var ignitionRadius = component.IgnitionRadius;
+        IgniteNearby(uid, xform.Coordinates, args.Severity, ignitionRadius);
+        _damageable.TryChangeDamage(uid, component.HealingOnFire, true, false);
+        _audio.PlayPvs(component.IgniteSound, uid);
+
+        args.Handled = true;
+    }
+
+    public void IgniteNearby(EntityUid uid, EntityCoordinates coordinates, float severity, float radius)
+    {
+        foreach (var flammable in _lookup.GetComponentsInRange<FlammableComponent>(coordinates, radius))
         {
-            base.Initialize();
-            SubscribeLocalEvent<FirestarterComponent, ComponentInit>(OnComponentInit);
-            SubscribeLocalEvent<FirestarterComponent, FireStarterActionEvent>(OnStartFire);
-        }
-
-        /// <summary>
-        /// Adds the firestarter action.
-        /// </summary>
-        private void OnComponentInit(EntityUid uid, FirestarterComponent component, ComponentInit args)
-        {
-            _actionsSystem.AddAction(uid, ref component.FireStarterActionEntity, component.FireStarterAction, uid);
-        }
-
-        /// <summary>
-        /// Ignites nearby flammable objects.
-        /// </summary>
-        private void OnStartFire(EntityUid uid, FirestarterComponent component, FireStarterActionEvent args)
-        {
-
-            if (_container.IsEntityOrParentInContainer(uid))
-                return;
-
-            var xform = Transform(uid);
-            var ignitionRadius = component.IgnitionRadius;
-            IgniteNearby(uid, xform.Coordinates, args.Severity, ignitionRadius);
-            _damageable.TryChangeDamage(uid, component.HealingOnFire, true, false);
-            _audio.PlayPvs(component.IgniteSound, uid);
-
-            args.Handled = true;
-        }
-
-        public void IgniteNearby(EntityUid uid, EntityCoordinates coordinates, float severity, float radius)
-        {
-            foreach (var flammable in _lookup.GetComponentsInRange<FlammableComponent>(coordinates, radius))
-            {
-                var ent = flammable.Owner;
-                var stackAmount = 2 + (int) (severity / 0.15f);
-                _flammable.AdjustFireStacks(ent, stackAmount, flammable);
-                _flammable.Ignite(ent, uid, flammable);
-            }
+            var ent = flammable.Owner;
+            var stackAmount = 2 + (int) (severity / 0.15f);
+            _flammable.AdjustFireStacks(ent, stackAmount, flammable);
+            _flammable.Ignite(ent, uid, flammable);
         }
     }
 }
