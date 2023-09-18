fd3320768f170fb1c8cd541bb8b1e352708d58b0
Fahasor <rad.nikita@list.ru>
2023-09-18 12:30:26 +0300

    Add documentation to WhistleComponent

--- Content.Shared/Whistle/Components/WhistleComponent.cs
+++ Content.Shared/Whistle/Components/WhistleComponent.cs
@@ -1,11 +1,22 @@
+
+
 namespace Content.Shared.Whistle.Components
 {
+    /// <summary>
+    /// Spawn attached entity for entities in range with MobMoverComponent.
+    /// </summary>
     [RegisterComponent]
     public sealed partial class WhistleComponent : Component
     {
+        /// <summary>
+        /// Entity prototype to spawn
+        /// </summary>
         [DataField("effect")]
-        public string? effect = "WhistleExclamation";
+        public string? effect = "WhistleExclamation"; 
 
+        /// <summary>
+        /// Range value.
+        /// </summary>
         [DataField("distance")]
         public float Distance = 0;
     }
