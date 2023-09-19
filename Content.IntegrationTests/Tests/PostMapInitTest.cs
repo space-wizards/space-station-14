00a22fb3777226ee3431e47045314905164280ed
spo9k <135381384+spo9k@users.noreply.github.com>
2023-09-20 01:30:11 +0300

    Обновил три карты до сборки SS220 (#344)
    * Обновил 3 карты для ротации.
    
    Bagel, Ishimura, Fland
    
    * Update bagelfix220.yml
    
    changed the yml name
    
    * Update flandss220.yml
    
    changed the yml name
    
    * map update ss220
    
    Map update ss220-mods
    
    * Update bagelfix220.yml
    
    update 220bagel.yml
    
    * Update bagelfix220.yml
    
    * Update flandss220.yml
    
    update
    
    * Update ishimura220.yml
    
    update
    
    * Update ishimura220.yml
    
    * Update bagelfix220.yml
    
    * Update flandss220.yml
    
    * add integration tests
    
    * Update flandss220.yml
    
    * Update bagelfix220.yml
    
    * Update PostMapInitTest.cs
    
    * pool update
    
    ---------
    
    Co-authored-by: Artur <thearturzh@gmail.com>

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -67,7 +67,10 @@ namespace Content.IntegrationTests.Tests
             "CorvaxIshimura",
             "220Delta",
             "Frankenstein",
-            "MeteorArena"
+            "MeteorArena",
+            "220Bagel",
+            "220Fland",
+            "220Ishimura"
         };
 
         /// <summary>
