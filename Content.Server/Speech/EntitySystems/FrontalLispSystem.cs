f27cacc8efaaaaffce792c6297d2f6b6f108b28e
dahnte <cardenasdanten@gmail.com>
2023-09-19 14:30:57 -0700

    Fixed lisp with lizard problem

--- Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
+++ Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
@@ -16,9 +16,12 @@ public sealed class FrontalLispSystem : EntitySystem
         var message = args.Message;
 
         // handles uppercase
-        message = Regex.Replace(message, @"[S][C]|[T]?[S]+|[C](?=[IEY])|[Z]+|[P][S]+", "TH");
+        message = Regex.Replace(message, @"[S][C]|[T]?[S]+|[C](?=[IEY])|[Z]+|[P][S]+|[T](?=[I])", "TH");
         // handles lowercase
-        message = Regex.Replace(message, @"[Ss][Cc]|[Tt]?[Ss]+|[Cc](?=[IiEeYy])|[Zz]+|[Pp][Ss]+", "th");
+        message = Regex.Replace(message, @"[Ss][Cc]|[Tt]?[Ss]+|[Cc](?=[IiEeYy])|[Zz]+|[Pp][Ss]+|[Tt](?=[Ii])", "th");
+        // handles 'ex'
+        message = Regex.Replace(message, @"(?![E])[X]", "KTH");
+        message = Regex.Replace(message, @"(?![Ee])[Xx]", "kth");
 
         args.Message = message;
     }
