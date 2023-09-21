5573536f46f8e7b90172b00aa3d5bc8b4cdae3a8
chromiumboy <chromium.boy@gmail.com>
2023-09-21 13:11:44 -0500

    Further clean up

--- Content.Server/Power/NodeGroups/ApcNet.cs
+++ Content.Server/Power/NodeGroups/ApcNet.cs
@@ -31,7 +31,7 @@ namespace Content.Server.Power.NodeGroups
         [ViewVariables] private int TotalReceivers => Providers.Sum(provider => provider.LinkedReceivers.Count);
 
         [ViewVariables]
-        public IEnumerable<ApcPowerReceiverComponent> AllReceivers =>
+        private IEnumerable<ApcPowerReceiverComponent> AllReceivers =>
             Providers.SelectMany(provider => provider.LinkedReceivers);
 
         public override void Initialize(Node sourceNode, IEntityManager entMan)
