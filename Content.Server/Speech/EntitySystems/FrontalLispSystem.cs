014a8de3dabbcd1d90486b740db8247daccac6ca
dahnte <70238020+dahnte@users.noreply.github.com>
2023-09-21 10:41:30 -0700

    Clarity lisp update (st, chi, ex sounds)

--- Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
+++ Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
@@ -15,12 +15,12 @@ public sealed class FrontalLispSystem : EntitySystem
     {
         var message = args.Message;
 
-        // handles ts, sc(i|e|y), c(i|e|y), ps, t(ion), ch(i|e), z, s
-        message = Regex.Replace(message, @"[T]+[Ss]+|[S]+[Cc]+(?=[IiEeYy]+)|[C]+(?=[IiEeYy]+)|[P][Ss]+|[T]+(?=[Ii]+[Oo]+[Uu]*[Nn]*)|[C]+[Hh]+(?=[Ii]+|[Ee]+)|[Z]+|[S]+", "TH");
-        message = Regex.Replace(message, @"[t]+[s]+|[s]+[c]+(?=[iey]+)|[c]+(?=[iey]+)|[p][s]+|[t]+(?=[i]+[o]+[u]*[n]*)|[c]+[h]+(?=[i]+|[e]+)|[z]+|[s]+", "th");
-        // handles x
-        message = Regex.Replace(message, @"[X]+", "KTH");
-        message = Regex.Replace(message, @"[x]+", "kth");
+        // handles ts, sc(i|e|y), c(i|e|y), ps, st(io(u|n)), ch(i|e), z, s
+        message = Regex.Replace(message, @"[T]+[Ss]+|[S]+[Cc]+(?=[IiEeYy]+)|[C]+(?=[IiEeYy]+)|[P][Ss]+|([S]+[Tt]+|[T]+)(?=[Ii]+[Oo]+[Uu]*[Nn]*)|[C]+[Hh]+(?=[Ii]*[Ee]*)|[Z]+|[S]+", "TH");
+        message = Regex.Replace(message, @"[t]+[s]+|[s]+[c]+(?=[iey]+)|[c]+(?=[iey]+)|[p][s]+|([s]+[t]+|[t]+)(?=[i]+[o]+[u]*[n]*)|[c]+[h]+(?=[i]*[e]*)|[z]+|[s]+", "th");
+        // handles ex(c), x
+        message = Regex.Replace(message, @"[E]+[Xx]+[Cc]*|[X]+", "EKTH");
+        message = Regex.Replace(message, @"[e]+[x]+[c]*|[x]+", "ekth");
 
         args.Message = message;
     }
