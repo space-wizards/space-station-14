ac13bb3b8d2280bce7dd83b8b7033a391c0b0825
deltanedas <@deltanedas:kde.org>
2023-09-22 03:48:16 +0100

    PowerSensor component and system

---
+++ Content.Server/DeviceLinking/Components/PowerSensorComponent.cs
@@ -0,0 +1,76 @@
+using Content.Server.DeviceLinking.Systems;
+using Content.Shared.DeviceLinking;
+using Content.Shared.Tools;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Server.DeviceLinking.Components;
+
+/// <summary>
+/// A power sensor checks the power network it's anchored to.
+/// Has 2 ports for when it is charging or discharging. They should never both be high.
+/// Requires <see cref="PowerSwitchableComponent"/> to function.
+/// </summary>
+[RegisterComponent, Access(typeof(PowerSensorSystem))]
+public sealed partial class PowerSensorComponent : Component
+{
+    /// <summary>
+    /// Whether to check the power network's input or output battery stats.
+    /// Useful when working with SMESes where input and output can both be important.
+    /// Or with APCs where there is no output and only input.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public bool Output;
+
+    /// <summary>
+    /// Tool quality to use for switching between input and output.
+    /// Cannot be pulsing since linking uses that.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public ProtoId<ToolQualityPrototype> SwitchQuality = "Screwing";
+
+    /// <summary>
+    /// Sound played when switching between input and output.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier SwitchSound = new SoundPathSpecifier("/Audio/Machines/lightswitch.ogg");
+
+    /// <summary>
+    /// Name of the port set when the network is charging power.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public ProtoId<SourcePortPrototype> ChargingPort = "PowerCharging";
+
+    /// <summary>
+    /// Name of the port set when the network is discharging power.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public ProtoId<SourcePortPrototype> DischargingPort = "PowerDischarging";
+
+    /// <summary>
+    /// How long to wait before checking the power network.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan CheckDelay = TimeSpan.FromSeconds(1);
+
+    /// <summary>
+    /// Time at which power will be checked.
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan NextCheck = TimeSpan.Zero;
+
+    /// <summary>
+    /// Charge the network was at, at the last check.
+    /// Charging/discharging is derived from this.
+    /// </summary>
+    [DataField]
+    public float LastCharge;
+
+    // Initial state
+    [DataField]
+    public bool ChargingState;
+
+    [DataField]
+    public bool DischargingState;
+}
---
+++ Content.Server/DeviceLinking/Systems/PowerSensorSystem.cs
@@ -0,0 +1,122 @@
+using Content.Server.DeviceLinking.Components;
+using Content.Server.DeviceNetwork;
+using Content.Server.NodeContainer;
+using Content.Server.Power.EntitySystems;
+using Content.Server.Power.Nodes;
+using Content.Server.Power.NodeGroups;
+using Content.Shared.Examine;
+using Content.Shared.Interaction;
+using Content.Shared.Popups;
+using Content.Shared.Power.Generator;
+using Content.Shared.Timing;
+using Content.Shared.Tools;
+using Robust.Shared.Timing;
+
+namespace Content.Server.DeviceLinking.Systems;
+
+public sealed class PowerSensorSystem : EntitySystem
+{
+    [Dependency] private readonly DeviceLinkSystem _deviceLink = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly PowerNetSystem _powerNet = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedToolSystem _tool = default!;
+    [Dependency] private readonly UseDelaySystem _useDelay = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PowerSensorComponent, ComponentInit>(OnInit);
+        SubscribeLocalEvent<PowerSensorComponent, ExaminedEvent>(OnExamined);
+        SubscribeLocalEvent<PowerSensorComponent, InteractUsingEvent>(OnInteractUsing);
+    }
+
+    public override void Update(float deltaTime)
+    {
+        var query = EntityQueryEnumerator<PowerSensorComponent>();
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            var now = _timing.CurTime;
+            if (comp.NextCheck < now)
+                continue;
+
+            comp.NextCheck = now + comp.CheckDelay;
+            UpdateOutputs(uid, comp);
+        }
+    }
+
+    private void OnInit(EntityUid uid, PowerSensorComponent comp, ComponentInit args)
+    {
+        _deviceLink.EnsureSourcePorts(uid, comp.ChargingPort, comp.DischargingPort);
+    }
+
+    private void OnExamined(EntityUid uid, PowerSensorComponent comp, ExaminedEvent args)
+    {
+        if (!args.IsInDetailsRange)
+            return;
+
+        args.PushMarkup(Loc.GetString("power-sensor-examine", ("output", comp.Output)));
+    }
+
+    private void OnInteractUsing(EntityUid uid, PowerSensorComponent comp, InteractUsingEvent args)
+    {
+        if (args.Handled || !_tool.HasQuality(args.Used, comp.SwitchQuality))
+            return;
+
+        // no sound spamming
+        if (TryComp<UseDelayComponent>(uid, out var useDelay) && _useDelay.ActiveDelay(uid, useDelay))
+            return;
+
+        // switch between input and output mode.
+        comp.Output = !comp.Output;
+
+        // since the battery to be checked changed the output probably has too, update it
+        UpdateOutputs(uid, comp);
+
+        // notify the user
+        _audio.PlayPvs(comp.SwitchSound, uid);
+        var msg = Loc.GetString("power-sensor-switch", ("output", comp.Output));
+        _popup.PopupEntity(msg, uid, args.User);
+
+        _useDelay.BeginDelay(uid, useDelay);
+    }
+
+    private void UpdateOutputs(EntityUid uid, PowerSensorComponent comp)
+    {
+        // get power stats on the power network that's been switched to
+        var powerSwitchable = Comp<PowerSwitchableComponent>(uid);
+        var cable = powerSwitchable.Cables[powerSwitchable.ActiveIndex];
+        var nodeContainer = Comp<NodeContainerComponent>(uid);
+        var node = (CableDeviceNode) nodeContainer.Nodes[cable.Node];
+
+        // update state based on the power stats retrieved
+        var charge = 0f;
+        var chargingState = false;
+        var dischargingState = false;
+        if (node.NodeGroup != null)
+        {
+            var group = (IBasePowerNet) node.NodeGroup;
+            var stats = _powerNet.GetNetworkStatistics(group.NetworkNode);
+            charge = comp.Output ? stats.OutStorageCurrent : stats.InStorageCurrent;
+            chargingState = comp.LastCharge > charge;
+            dischargingState = comp.LastCharge < charge;
+        }
+
+        comp.LastCharge = charge;
+
+        // send new signals if changed
+        if (comp.ChargingState != chargingState)
+        {
+            comp.ChargingState = chargingState;
+            _deviceLink.SendSignal(uid, comp.ChargingPort, chargingState);
+        }
+
+        if (comp.DischargingState != dischargingState)
+        {
+            comp.DischargingState = dischargingState;
+            _deviceLink.SendSignal(uid, comp.DischargingPort, dischargingState);
+        }
+    }
+}
