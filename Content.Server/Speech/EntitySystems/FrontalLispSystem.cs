ebd04c1cbb0185e0bd6c4454996192e864407fcc
lzk <124214523+lzk228@users.noreply.github.com>
2023-09-25 13:01:48 +0200

    Localization of lisp  (#1451)

--- Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
+++ Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
@@ -1,10 +1,12 @@
 using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
+using Robust.Shared.Random; // Corvax-Localization
 
 namespace Content.Server.Speech.EntitySystems;
 
 public sealed class FrontalLispSystem : EntitySystem
 {
+    [Dependency] private readonly IRobustRandom _random = default!; // Corvax-Localization
     public override void Initialize()
     {
         base.Initialize();
@@ -21,7 +23,15 @@ public sealed class FrontalLispSystem : EntitySystem
         // handles ex(c), x
         message = Regex.Replace(message, @"[E]+[Xx]+[Cc]*|[X]+", "EKTH");
         message = Regex.Replace(message, @"[e]+[x]+[c]*|[x]+", "ekth");
-
+        // Corvax-Localization Start
+        // с, в, ч, т in ф or ш
+        message = Regex.Replace(message, @"\B[СВЧТ]\B", _random.Prob(0.5f) ? "Ф" : "Ш");
+        message = Regex.Replace(message, @"\B[свчт]\B", _random.Prob(0.5f) ? "ф" : "ш");
+        // д in ф
+        message = Regex.Replace(message, @"\b[Д](?![ИЕЁЮЯЬ])\b|\B[Д]\B", "Ф");
+        message = Regex.Replace(message, @"\b[Дд](?![ИиЕеЁёЮюЯяЬь])\b|\B[Дд]\B", "ф");
+        // Corvax-Localization End
+        
         args.Message = message;
     }
 }
