9f30ed76800d29d7058962953cf0d916388696ba
deltanedas <@deltanedas:kde.org>
2023-09-26 08:29:20 +0100

    minor heat exchanger cleanup

--- Content.Server/Atmos/Piping/Unary/Components/HeatExchangerComponent.cs
+++ Content.Server/Atmos/Piping/Unary/Components/HeatExchangerComponent.cs
@@ -3,34 +3,29 @@ namespace Content.Server.Atmos.Piping.Unary.Components;
 [RegisterComponent]
 public sealed partial class HeatExchangerComponent : Component
 {
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("inlet")]
-    public string InletName { get; set; } = "inlet";
-
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("outlet")]
-    public string OutletName { get; set; } = "outlet";
+    /// <summary>
+    /// Name of the pipe node to exchange heat to.
+    /// </summary>
+    [DataField("inlet"), ViewVariables(VVAccess.ReadWrite)]
+    public string InletName = "inlet";
 
     /// <summary>
     /// Pipe conductivity (mols/kPa/sec).
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("conductivity")]
-    public float G { get; set; } = 1f;
+    [DataField("conductivity"), ViewVariables(VVAccess.ReadWrite)]
+    public float G = 1f;
 
     /// <summary>
     /// Thermal convection coefficient (J/degK/sec).
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("convectionCoefficient")]
-    public float K { get; set; } = 8000f;
+    [DataField("convectionCoefficient"), ViewVariables(VVAccess.ReadWrite)]
+    public float K = 8000f;
 
     /// <summary>
     /// Thermal radiation coefficient. Number of "effective" tiles this
     /// radiator radiates compared to superconductivity tile losses.
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("radiationCoefficient")]
-    public float alpha { get; set; } = 140f;
+    [DataField("radiationCoefficient"), ViewVariables(VVAccess.ReadWrite)]
+    public float alpha = 140f;
 }
 
