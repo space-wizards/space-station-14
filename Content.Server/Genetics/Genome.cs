90c771f24f39703bf64e9c53000dcdbf6b66d487
deltanedas <@deltanedas:kde.org>
2023-09-29 08:25:42 +0100

    add Genome and GenomeLayout

---
+++ Content.Server/Genetics/Genome.cs
@@ -0,0 +1,120 @@
+using Robust.Shared.Serialization;
+using System.Collections;
+using System.Text;
+
+namespace Content.Server.Genetics;
+
+/// <summary>
+/// Genome for an organism.
+/// Internally represented as a bit array, shown to players as bases using <see cref="GetBases"/>.
+/// Other systems can get data using <see cref="GetBool"/> and <see cref="GetInt"/>.
+/// Each bit can either be a boolean or be part of a number, which has its bits stored sequentially.
+/// Each species has its own unique genome layout that maps bits to data, which is randomized roundstart.
+/// Variable length information such as a list of reagents cannot be stored here.
+/// </summary>
+[DataDefinition]
+public sealed partial class Genome
+{
+    [ViewVariables]
+    public BitArray Bits = new BitArray(0);
+
+    private static char[] Bases = new[] { 'A', 'C', 'G', 'T'};
+
+    /// <summary>
+    /// Creates a new genome from all zeroes.
+    /// </summary>
+    public Genome(int count = 0)
+    {
+        Bits = new BitArray(count);
+    }
+
+    /// <summary>
+    /// Copy this genome's bits to another one.
+    /// </summary>
+    public void CopyTo(Genome other)
+    {
+        other.Bits = new BitArray(Bits);
+    }
+
+    /// <summary>
+    /// Get the value of a single bool at an index.
+    /// </summary>
+    /// <param name="index">Bit index to get from</param>
+    public bool GetBool(int index)
+    {
+        return Bits[index];
+    }
+
+    /// <summary>
+    /// Get the value of an integer with multiple bits.
+    /// It should be clamped to a reasonable number afterwards.
+    /// </summary>
+    /// <param name="index">Starting bit index to get from</param>
+    /// <param name="bits">Number of bits to read</param>
+    public int GetInt(int index, int bits)
+    {
+        var value = 0;
+        for (int i = 0; i < bits; i++)
+        {
+            var bit = 1 << i;
+            if (GetBool(index + i))
+            {
+                value |= bit;
+            }
+        }
+
+        return value;
+    }
+
+    /// <summary>
+    /// Return a base pair string for a range of bits in the genome.
+    /// </summary>
+    /// <param name="index">Starting bit index to get from</param>
+    /// <param name="bases">Number of bases to include in the string</param>
+    public string GetBases(int index, int bases)
+    {
+        var builder = new StringBuilder(bases);
+        for (int i = 0; i < bases; i++)
+        {
+            // 2 bits makes a base
+            var c = Bases[GetInt(index + i * 2, 2)];
+            builder.Append(c);
+        }
+
+        return builder.ToString();
+    }
+
+    /// <summary>
+    /// Sets a boolean value at a bit index to a value.
+    /// </summary>
+    /// <param name="index">Bit index to set</param>
+    public void SetBool(int index, bool value)
+    {
+        Bits[index] = value;
+    }
+
+    /// <summary>
+    /// Sets the bits of an integer to a value.
+    /// This will truncate the integer to fit inside the possible values.
+    /// </summary>
+    /// <param name="index">Starting bit index to set</param>
+    /// <param name="bits">Number of bits to set</param>
+    /// <param name="value">Number to set</param>
+    public void SetInt(int index, int bits, int value)
+    {
+        for (int i = 0; i < bits; i++)
+        {
+            var bit = 1 << i;
+            Bits[index + i] = (value & bit) != 0;
+        }
+    }
+
+    /// <summary>
+    /// Flips a bit at an index from a 0 to a 1.
+    /// </summary>
+    /// <param name="index">Bit index to flip</param>
+    public void FlipBit(int index)
+    {
+        Bits[index] = !Bits[index];
+    }
+}
---
+++ Content.Server/Genetics/GenomeLayout.cs
@@ -0,0 +1,81 @@
+using Robust.Shared.Utility;
+
+namespace Content.Server.Genetics;
+
+/// <summary>
+/// Maps a <see cref="Genome"/>'s bits to bools and ints.
+/// Stores indices for every bool and int that can be retrieved.
+/// </summary>
+public sealed class GenomeLayout
+{
+    /// <summary>
+    /// Indices and bit lengths of every value stored in the genome.
+    /// For <see cref="GetBool"/> bit length must be 1.
+    /// </summary>
+    [DataField]
+    public Dictionary<string, (int, int)> Values = new();
+
+    /// <summary>
+    /// How many bits this genome has total
+    /// </summary>
+    [DataField]
+    public int TotalBits;
+
+    /// <summary>
+    /// Get a bool from the genome by name.
+    /// </summary>
+    public bool GetBool(Genome genome, string name)
+    {
+        var (index, bits) = Values[name];
+        DebugTools.Assert(bits == 1, "Do not use GetBool for int genome values");
+
+        return genome.GetBool(index);
+    }
+
+    /// <summary>
+    /// Get an int from the genome by name.
+    /// </summary>
+    public int GetInt(Genome genome, string name)
+    {
+        var (index, bits) = Values[name];
+        return genome.GetInt(index, bits);
+    }
+
+    /// <summary>
+    /// Sets a bool value on the genome by name.
+    /// </summary>
+    public void SetBool(Genome genome, string name, bool value)
+    {
+        var (index, bits) = Values[name];
+        DebugTools.Assert(bits == 1, "Do not use SetBool for int genome values");
+
+        genome.SetBool(index, value);
+    }
+
+    /// <summary>
+    /// Sets an int on the genome by name.
+    /// </summary>
+    /// <remarks>
+    /// Unused bits are silently ignored.
+    /// </remarks>
+    public void SetInt(Genome genome, string name, int value)
+    {
+        var (index, bits) = Values[name];
+        genome.SetInt(index, bits: bits, value: value);
+    }
+
+    /// <summary>
+    /// Add a mapping to the layout.
+    /// If length is 1 it will be a bool, int otherwise.
+    /// </summary>
+    /// <param name="name">Name of the value to add</param>
+    /// <param name="bits">Number of bits the value has</param>
+    public void Add(string name, int bits)
+    {
+        DebugTools.Assert(bits > 0, "Genome value bit count must be positive");
+
+        var index = TotalBits;
+        Values.Add(name, (index, bits));
+        TotalBits += bits;
+    }
+}
