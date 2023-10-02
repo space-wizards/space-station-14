217b967ee2796f54ad00b41ec352c98bcbcb3254
brainfood1183 <newman1183@googlemail.com>
2023-10-02 16:52:49 +0100

    poly effect now will not poly zombies and will not poly dead or crit crewmembers, poly effect now has a range of 4x4 instead of 6x6.

--- Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Components/PolyArtifactComponent.cs
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Components/PolyArtifactComponent.cs
@@ -3,13 +3,13 @@ using Robust.Shared.Audio;
 namespace Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
 
 /// <summary>
-/// Artifact that ignites surrounding entities when triggered.
+/// Artifact that polymorphs surrounding entities when triggered, Range handles the range of effect and sound is played on polymorph.
 /// </summary>
 [RegisterComponent]
 public sealed partial class PolyArtifactComponent : Component
 {
     [DataField("range")]
-    public float Range = 3f;
+    public float Range = 2f;
 
 
     [DataField("polySound")]
--- Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/PolyArtifactSystem.cs
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/PolyArtifactSystem.cs
@@ -2,28 +2,38 @@ using Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
 using Content.Server.Xenoarchaeology.XenoArtifacts.Events;
 using Content.Shared.Humanoid;
 using Content.Server.Polymorph.Systems;
+using Content.Shared.Zombies;
+using Content.Shared.Mobs.Systems;
 
 namespace Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Systems;
 
 public sealed class PolyArtifactSystem : EntitySystem
 {
     [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly MobStateSystem _mob = default!;
     [Dependency] private readonly PolymorphSystem _poly = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
-
-    /// <inheritdoc/>
     public override void Initialize()
     {
         SubscribeLocalEvent<PolyArtifactComponent, ArtifactActivatedEvent>(OnActivate);
     }
 
+    /// <summary>
+    /// Provided target is alive and is not a zombie, polymorphs the target.
+    /// </summary>
     private void OnActivate(EntityUid uid, PolyArtifactComponent component, ArtifactActivatedEvent args)
     {
         foreach (var target in _lookup.GetEntitiesInRange(uid, component.Range))
         {
-            if (HasComp<HumanoidAppearanceComponent>(target))
+            if (_mob.IsDead(target) || _mob.IsCritical(target))
+                return;
+
+
+            else if (HasComp<HumanoidAppearanceComponent>(target) && !HasComp<ZombieComponent>(target))
+            {
                 _poly.PolymorphEntity(target, "ArtifactMonkey");
                 _audio.PlayPvs(component.PolySound, uid);
+            }
         }
     }
 }
