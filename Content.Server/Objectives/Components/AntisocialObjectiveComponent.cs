4071357d501e0e8b533872ace93aa3a2b7ba61d5
deltanedas <@deltanedas:kde.org>
2023-10-02 18:00:35 +0100

    antisocial base objective, similar to social base objective

---
+++ Content.Server/Objectives/Components/AntisocialObjectiveComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Server.Objectives.Components;
+
+/// <summary>
+/// Marker component for antisocial objectives and social objectives to be mutually exclusive.
+/// </summary>
+[RegisterComponent]
+public sealed partial class AntiSocialObjectiveComponent : Component
+{
+}
--- Content.Server/Objectives/Components/SocialObjectiveComponent.cs
+++ Content.Server/Objectives/Components/SocialObjectiveComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Server.Objectives.Components;
 
 /// <summary>
-/// Marker component for social objectives and kill objectives to be mutually exclusive.
+/// Marker component for social objectives and antisocial objectives to be mutually exclusive.
 /// </summary>
 [RegisterComponent]
 public sealed partial class SocialObjectiveComponent : Component
