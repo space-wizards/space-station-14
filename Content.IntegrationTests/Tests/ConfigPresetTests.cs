535ccbb6a1f93f4c7c257d1defcfde7bc300ccdd
DrSmugleaf <drsmugleaf@gmail.com>
2023-10-03 05:03:24 -0700

    Add test to load all config presets

---
+++ Content.IntegrationTests/Tests/ConfigPresetTests.cs
@@ -0,0 +1,30 @@
+﻿using Content.Server.Entry;
+using Robust.Shared.Configuration;
+using Robust.Shared.ContentPack;
+
+namespace Content.IntegrationTests.Tests;
+
+[TestFixture]
+public sealed class ConfigPresetTests
+{
+    [Test]
+    public async Task TestLoadAll()
+    {
+        var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
+
+        var resources = server.ResolveDependency<IResourceManager>();
+        var config = server.ResolveDependency<IConfigurationManager>();
+
+        await server.WaitPost(() =>
+        {
+            var presets = resources.ContentFindFiles(EntryPoint.ConfigPresetsDir);
+
+            foreach (var preset in presets)
+            {
+                var stream = resources.ContentFileRead(preset);
+                config.LoadDefaultsFromTomlStream(stream);
+            }
+        });
+    }
+}
