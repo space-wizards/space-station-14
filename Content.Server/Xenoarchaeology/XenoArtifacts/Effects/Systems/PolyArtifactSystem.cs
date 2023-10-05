3299ca0c65d4dd34504705aa42fd2fa7139d9ff2
brainfood1183 <newman1183@googlemail.com>
2023-10-06 00:04:48 +0100

    zombiecomp removed.

--- Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/PolyArtifactSystem.cs
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/PolyArtifactSystem.cs
@@ -2,7 +2,6 @@ using Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
 using Content.Server.Xenoarchaeology.XenoArtifacts.Events;
 using Content.Shared.Humanoid;
 using Content.Server.Polymorph.Systems;
-using Content.Shared.Zombies;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Polymorph;
 
@@ -35,7 +34,7 @@ public sealed class PolyArtifactSystem : EntitySystem
         foreach (var comp in _lookup.GetComponentsInRange<HumanoidAppearanceComponent>(xform.Coordinates, component.Range))
         {
             var target = comp.Owner;
-            if (_mob.IsAlive(target) && !HasComp<ZombieComponent>(target))
+            if (_mob.IsAlive(target))
             {
                 _poly.PolymorphEntity(target, PolymorphPrototypeName);
                 _audio.PlayPvs(component.PolySound, uid);
