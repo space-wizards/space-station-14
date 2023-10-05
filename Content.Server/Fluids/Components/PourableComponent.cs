e81ff61ae7307ff629cbe7df56f110e160ab6209
Artjom <artjombebenin@gmail.com>
2023-10-05 21:55:47 +0400

    added summary

--- Content.Server/Fluids/Components/PourableComponent.cs
+++ Content.Server/Fluids/Components/PourableComponent.cs
@@ -4,6 +4,8 @@ using Robust.Shared.Audio;
 namespace Content.Shared.Fluids.Components;
 
 /// <summary>
+/// Adds a "Pour Liquid" option to the context menu.
+/// This allows both filling a bucket from this container and topping it up with any liquid.
 /// </summary>
 [RegisterComponent, Access(typeof(PourableSystem))]
 public sealed partial class PourableComponent : Component
