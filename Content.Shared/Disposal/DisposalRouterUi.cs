1c86762dd625a089608830449420247e0370e3d1
deltanedas <@deltanedas:kde.org>
2023-10-07 02:48:57 +0100

    clean up shared router/tagger ui stuff

---
+++ Content.Shared/Disposal/DisposalRouterUi.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Disposal;
+
+[Serializable, NetSerializable]
+public sealed class DisposalRouterUserInterfaceState : BoundUserInterfaceState
+{
+    public readonly string Tags;
+
+    public DisposalRouterUserInterfaceState(string tags)
+    {
+        Tags = tags;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class RouterSetTagsMessage : BoundUserInterfaceMessage
+{
+    public static readonly Regex TagsRegex = new("^[a-zA-Z0-9, ]*$", RegexOptions.Compiled);
+
+    public readonly string Tags;
+
+    public RouterSetTagsMessage(string tags)
+    {
+        Tags = tags.Substring(0, Math.Min(tags.Length, 150));
+    }
+}
+
+[Serializable, NetSerializable]
+public enum DisposalRouterUiKey
+{
+    Key
+}
---
+++ Content.Shared/Disposal/DisposalTaggerUi.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Disposal;
+
+[Serializable, NetSerializable]
+public sealed class DisposalTaggerUserInterfaceState : BoundUserInterfaceState
+{
+    public readonly string Tag;
+
+    public DisposalTaggerUserInterfaceState(string tag)
+    {
+        Tag = tag;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class TaggerSetTagMessage : BoundUserInterfaceMessage
+{
+    public static readonly Regex TagRegex = new("^[a-zA-Z0-9 ]$", RegexOptions.Compiled);
+
+    public readonly string Tag;
+
+    public TaggerSetTagMessage(string tag)
+    {
+        Tag = tag.Substring(0, Math.Min(tag.Length, 30));
+    }
+}
+
+[Serializable, NetSerializable]
+public enum DisposalTaggerUiKey
+{
+    Key
+}
