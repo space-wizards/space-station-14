eb27d286cd1ce2f80147ed7cbecaafb5723198f5
Vasilis The Pikachu <vasilis@pikachu.systems>
2023-10-07 18:09:41 +0200

    Component magik

--- Content.Server/Speech/Components/StutteringAccentComponent.cs
+++ Content.Server/Speech/Components/StutteringAccentComponent.cs
@@ -3,5 +3,20 @@ namespace Content.Server.Speech.Components
     [RegisterComponent]
     public sealed partial class StutteringAccentComponent : Component
     {
+        [DataField("matchRandomProb")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public float MatchRandomProb = 0.8f;
+
+        [DataField("fourRandomProb")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public float FourRandomProb = 0.1f;
+
+        [DataField("threeRandomProb")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public float ThreeRandomProb = 0.2f;
+
+        [DataField("cutRandomProb")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public float CutRandomProb = 0.05f;
     }
 }
