071ec822ad3066c5f2f45f317f1ba52389821d4b
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-10-08 14:53:29 -0700

    file scoped namespace

--- Content.Shared/Loadouts/LoadoutPrototype.cs
+++ Content.Shared/Loadouts/LoadoutPrototype.cs
@@ -2,90 +2,89 @@ using Content.Shared.Whitelist;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
 
-namespace Content.Shared.Loadouts
+namespace Content.Shared.Loadouts;
+
+/// <summary>
+///     Describes a loadout.
+/// </summary>
+[Prototype("loadout")]
+public sealed class LoadoutPrototype : IPrototype
 {
+    [ViewVariables]
+    [IdDataField]
+    public string ID { get; } = default!;
+
     /// <summary>
-    ///     Describes a loadout.
+    ///     The name of this loadout.
     /// </summary>
-    [Prototype("loadout")]
-    public sealed class LoadoutPrototype : IPrototype
-    {
-        [ViewVariables]
-        [IdDataField]
-        public string ID { get; } = default!;
+    [DataField("name")]
+    public string? Name;
 
-        /// <summary>
-        ///     The name of this loadout.
-        /// </summary>
-        [DataField("name")]
-        public string Name = "";
+    /// <summary>
+    ///     The description of this loadout.
+    /// </summary>
+    [DataField("description")]
+    public string? Description;
 
-        /// <summary>
-        ///     The description of this loadout.
-        /// </summary>
-        [DataField("description")]
-        public string? Description;
+    /// <summary>
+    ///     Which tab category to put this under.
+    /// </summary>
+    [DataField("category")]
+    public string Category = "Uncategorized";
 
-        /// <summary>
-        ///     Which tab category to put this under.
-        /// </summary>
-        [DataField("category")]
-        public string Category = "Uncategorized";
-
-        /// <summary>
-        ///     The item to give.
-        /// </summary>
-        [DataField("items", customTypeSerializer: typeof(PrototypeIdListSerializer<EntityPrototype>))]
-        public List<string> Items = new();
+    /// <summary>
+    ///     The item to give.
+    /// </summary>
+    [DataField("items", customTypeSerializer: typeof(PrototypeIdListSerializer<EntityPrototype>))]
+    public List<string> Items = new();
 
 
-        /// <summary>
-        ///     The point cost of this loadout.
-        /// </summary>
-        [DataField("cost")]
-        public int Cost = 1;
+    /// <summary>
+    ///     The point cost of this loadout.
+    /// </summary>
+    [DataField("cost")]
+    public int Cost = 1;
 
-        /// <summary>
-        ///     Should this item override other items in the same slot?
-        /// </summary>
-        [DataField("exclusive")]
-        public bool Exclusive;
+    /// <summary>
+    ///     Should this item override other items in the same slot?
+    /// </summary>
+    [DataField("exclusive")]
+    public bool Exclusive;
 
 
-        /// <summary>
-        ///     Don't apply this loadout to entities this whitelist IS NOT valid for.
-        /// </summary>
-        [DataField("entityWhitelist")]
-        public EntityWhitelist? EntityWhitelist;
+    /// <summary>
+    ///     Don't apply this loadout to entities this whitelist IS NOT valid for.
+    /// </summary>
+    [DataField("entityWhitelist")]
+    public EntityWhitelist? EntityWhitelist;
 
-        /// <summary>
-        ///     Don't apply this loadout to entities this whitelist IS valid for. (hence, a blacklist)
-        /// </summary>
-        [DataField("entityBlacklist")]
-        public EntityWhitelist? EntityBlacklist;
+    /// <summary>
+    ///     Don't apply this loadout to entities this whitelist IS valid for. (hence, a blacklist)
+    /// </summary>
+    [DataField("entityBlacklist")]
+    public EntityWhitelist? EntityBlacklist;
 
-        /// <summary>
-        ///     Need one of these jobs to give loadout.
-        /// </summary>
-        [DataField("jobWhitelist")]
-        public List<string>? JobWhitelist;
+    /// <summary>
+    ///     Need one of these jobs to give loadout.
+    /// </summary>
+    [DataField("jobWhitelist")]
+    public List<string>? JobWhitelist;
 
-        /// <summary>
-        ///     Need none of these jobs to give loadout.
-        /// </summary>
-        [DataField("jobBlacklist")]
-        public List<string>? JobBlacklist;
+    /// <summary>
+    ///     Need none of these jobs to give loadout.
+    /// </summary>
+    [DataField("jobBlacklist")]
+    public List<string>? JobBlacklist;
 
-        /// <summary>
-        ///     Don't apply this loadout to entities this whitelist IS NOT valid for.
-        /// </summary>
-        [DataField("speciesWhitelist")]
-        public List<string>? SpeciesWhitelist;
+    /// <summary>
+    ///     Don't apply this loadout to entities this whitelist IS NOT valid for.
+    /// </summary>
+    [DataField("speciesWhitelist")]
+    public List<string>? SpeciesWhitelist;
 
-        /// <summary>
-        ///     Don't apply this loadout to entities this whitelist IS valid for. (hence, a blacklist)
-        /// </summary>
-        [DataField("speciesBlacklist")]
-        public List<string>? SpeciesBlacklist;
-    }
+    /// <summary>
+    ///     Don't apply this loadout to entities this whitelist IS valid for. (hence, a blacklist)
+    /// </summary>
+    [DataField("speciesBlacklist")]
+    public List<string>? SpeciesBlacklist;
 }
