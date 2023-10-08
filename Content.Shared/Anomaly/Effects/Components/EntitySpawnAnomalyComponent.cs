3e0bd525dfbf2dc21221c5f59a55fd1afa7ad8f3
Ed <96445749+TheShuEd@users.noreply.github.com>
2023-10-08 21:23:57 +0300

    Tweaks EntitySpawnAnomalyComponent

--- Content.Shared/Anomaly/Effects/Components/EntitySpawnAnomalyComponent.cs
+++ Content.Shared/Anomaly/Effects/Components/EntitySpawnAnomalyComponent.cs
@@ -1,7 +1,6 @@
 using Content.Shared.Maps;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.Anomaly.Effects.Components;
 
@@ -21,23 +20,40 @@ public sealed partial class EntitySpawnAnomalyComponent : Component
     public List<EntProtoId> SuperCriticalSpawns = new();
 
     /// <summary>
+    /// The minimum number of entities that spawn per pulse
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public int MinSpawnAmount;
+    /// <summary>
     /// The maximum number of entities that spawn per pulse
     /// scales with severity.
     /// </summary>
-    [DataField("maxSpawnAmount"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public int MaxSpawnAmount = 7;
 
+    /// <summary>
+    /// The maximum number of entities that spawn in supercritical.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public int SuperCriticalMaxSpawnAmount = 7;
     /// <summary>
     /// The maximum radius the entities will spawn in.
     /// Also governs the maximum reach of flesh tiles
     /// scales with stability
     /// </summary>
-    [DataField("spawnRange"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float SpawnRange = 5f;
 
     /// <summary>
+    /// The maximum radius the entities will spawn in supercritical.
+    /// scales with stability
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float SuperCriticalSpawnRange = 8f;
+    
+    /// <summary>
     /// The tile that is spawned by the anomaly's effect
     /// </summary>
-    [DataField("floorTileId", customTypeSerializer: typeof(PrototypeIdSerializer<ContentTileDefinition>)), ViewVariables(VVAccess.ReadWrite)]
-    public string FloorTileId = "FloorFlesh";
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public ProtoId<ContentTileDefinition> FloorTileId = "FloorFlesh";
 }
