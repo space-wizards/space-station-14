6d9dad29550606f61bfea9b92ea46e944ef114cb
Chief-Engineer <119664036+Chief-Engineer@users.noreply.github.com>
2023-10-08 17:18:17 -0500

    I've discovered that my original plans may be bad for performance

--- Content.Server/Administration/Logs/Converters/EntityStringRepresentationConverter.cs
+++ Content.Server/Administration/Logs/Converters/EntityStringRepresentationConverter.cs
@@ -1,8 +1,5 @@
 using System.Text.Json;
 using Content.Server.Administration.Managers;
-using Content.Server.Atmos.Components;
-using Content.Shared.Glue;
-using Content.Shared.Lube;
 using Robust.Server.Player;
 
 namespace Content.Server.Administration.Logs.Converters;
@@ -11,7 +8,6 @@ namespace Content.Server.Administration.Logs.Converters;
 public sealed class EntityStringRepresentationConverter : AdminLogConverter<EntityStringRepresentation>
 {
     [Dependency] private readonly IAdminManager _adminManager = default!;
-    [Dependency] private readonly EntityManager _entityManager = default!;
 
     public override void Write(Utf8JsonWriter writer, EntityStringRepresentation value, JsonSerializerOptions options)
     {
@@ -42,28 +38,6 @@ public sealed class EntityStringRepresentationConverter : AdminLogConverter<Enti
         if (value.Deleted)
         {
             writer.WriteBoolean("deleted", true);
-        }
-
-        if (_entityManager.TryGetComponent<FlammableComponent>(value.Uid, out var flammableComponent) && flammableComponent.OnFire)
-        {
-            writer.WriteStartObject("flammable");
-            writer.WriteNumber("fireStacks", flammableComponent.FireStacks);
-            writer.WriteEndObject();
-        }
-
-        if (_entityManager.TryGetComponent<GluedComponent>(value.Uid, out var gluedComponent))
-        {
-            writer.WriteStartObject("glued");
-            writer.WriteNumber("duration", gluedComponent.Duration.TotalSeconds);
-            writer.WriteEndObject();
-        }
-
-        if (_entityManager.TryGetComponent<LubedComponent>(value.Uid, out var lubedComponent))
-        {
-            writer.WriteStartObject("lubed");
-            writer.WriteNumber("slipStrength", lubedComponent.SlipStrength);
-            writer.WriteNumber("slipsLeft", lubedComponent.SlipsLeft);
-            writer.WriteEndObject();
         }
 
         writer.WriteEndObject();
