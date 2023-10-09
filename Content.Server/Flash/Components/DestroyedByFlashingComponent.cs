f49ec0b7110a149d5ff0b134dc6376f8f81dfa5e
Ed <edwardxperia2000@gmail.com>
2023-10-09 16:18:13 +0300

    add terraforming assets add nice visual

---
+++ Content.Server/Flash/Components/DestroyedByFlashingComponent.cs
@@ -0,0 +1,21 @@
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server;
+// Also needed FlashableComponent on entity to work
+[RegisterComponent]
+public sealed partial class DestroyedByFlashingComponent : Component
+{
+    /// <summary>
+    /// how many flashes can this entity survive
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public int LifeCount = 1;
+
+    /// <summary>
+    /// The name of the prototype that appears on the destroyed shadow kudzu
+    /// </summary>
+    // (according to the idea - a visual effect)
+    [DataField, ViewVariables(VVAccess.ReadOnly)]
+    public EntProtoId RemoveEffect = "EffectDarknessPulse";
+}
