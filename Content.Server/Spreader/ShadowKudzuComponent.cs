2ca091318effcd300a728a124ee587bf11dfbe66
Ed <edwardxperia2000@gmail.com>
2023-10-09 14:44:58 +0300

    add shadowstone remove studid ShadowKudzu component.

--- Content.Server/Spreader/ShadowKudzuComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace Content.Server.Spreader;
-
-/// <summary>
-/// Allows the ShadowKudzuSystem to determine where objects can be created inside the darkness
-/// </summary>
-[RegisterComponent]
-public sealed partial class ShadowKudzuComponent : Component {}
