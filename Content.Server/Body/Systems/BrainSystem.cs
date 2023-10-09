3b1b612bbce970b5b1d85652e15797a6cb9688a2
EmoGarbage404 <retron404@gmail.com>
2023-10-08 23:16:32 -0400

    even mre!

--- Content.Server/Body/Systems/BrainSystem.cs
+++ Content.Server/Body/Systems/BrainSystem.cs
@@ -4,14 +4,11 @@ using Content.Shared.Body.Components;
 using Content.Shared.Body.Events;
 using Content.Shared.Mind;
 using Content.Shared.Mind.Components;
-using Content.Shared.Movement.Components;
-using Content.Shared.Movement.Systems;
 
 namespace Content.Server.Body.Systems
 {
     public sealed class BrainSystem : EntitySystem
     {
-        [Dependency] private readonly MovementSpeedModifierSystem _movementSpeed = default!;
         [Dependency] private readonly SharedMindSystem _mindSystem = default!;
 
         public override void Initialize()
@@ -33,16 +30,6 @@ namespace Content.Server.Body.Systems
             var ghostOnMove = EnsureComp<GhostOnMoveComponent>(newEntity);
             if (HasComp<BodyComponent>(newEntity))
                 ghostOnMove.MustBeDead = true;
-
-            // TODO: This is an awful solution.
-            // Our greatest minds still can't figure out how to allow brains/heads to ghost without giving them the
-            // ability to move first. I hate this with a passion.
-            if (!HasComp<InputMoverComponent>(newEntity))
-            {
-                AddComp<InputMoverComponent>(newEntity);
-                var move = EnsureComp<MovementSpeedModifierComponent>(newEntity);
-                _movementSpeed.ChangeBaseSpeed(newEntity, 0, 0 , 0, move);
-            }
 
             if (!_mindSystem.TryGetMind(oldEntity, out var mindId, out var mind))
                 return;
