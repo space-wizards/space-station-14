277aa58be1e0935b428800b53b9ac91df155d290
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-10-10 17:53:53 +0300

    remove ninja role comment

--- Content.Server/Roles/NinjaRoleComponent.cs
+++ Content.Server/Roles/NinjaRoleComponent.cs
@@ -2,9 +2,6 @@ using Content.Shared.Roles;
 
 namespace Content.Server.Roles;
 
-/// <summary>
-/// Stores the ninja's objectives on the mind so if they die the rest of the greentext persists.
-/// </summary>
 [RegisterComponent]
 public sealed partial class NinjaRoleComponent : AntagonistRoleComponent
 {
