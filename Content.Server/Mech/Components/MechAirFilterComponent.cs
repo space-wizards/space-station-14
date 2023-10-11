2ae77ee1b816cca9dbe3df8d202a00fa1a7bd387
deltanedas <@deltanedas:kde.org>
2023-10-11 16:54:23 +0100

    tagless chicken

--- Content.Server/Mech/Components/MechAirFilterComponent.cs
+++ Content.Server/Mech/Components/MechAirFilterComponent.cs
@@ -11,20 +11,20 @@ public sealed partial class MechAirFilterComponent : Component
     /// <summary>
     /// Gases that will be filtered out of internal air
     /// </summary>
-    [DataField("gases", required: true)]
+    [DataField(required: true)]
     public HashSet<Gas> Gases = new();
 
     /// <summary>
     /// Gases that will be filtered out of internal air to maintain oxygen ratio.
     /// When oxygen is below <see cref="TargetOxygen"/>, these gases will be filtered instead of <see cref="Gases"/>.
     /// </summary>
-    [DataField("overflowGases", required: true)]
+    [DataField(required: true)]
     public HashSet<Gas> OverflowGases = new();
 
     /// <summary>
     /// Minimum oxygen fraction before it will start removing <see cref="OverflowGases"/>.
     /// </summary>
-    [DataField("targetOxygen"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float TargetOxygen = 0.21f;
 
     /// <summary>
@@ -34,12 +34,12 @@ public sealed partial class MechAirFilterComponent : Component
     /// If you make a slime mech you might want to change this to be nitrogen, and overflowgases to remove oxygen.
     /// However theres still no real danger since standard atmos is mostly nitrogen so nitrogen tends to 100% anyway.
     /// </remarks>
-    [DataField("oxygen"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public Gas Oxygen = Gas.Oxygen;
 
     /// <summary>
     /// Target volume to transfer every second.
     /// </summary>
-    [DataField("transferRate"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float TransferRate = MechAirComponent.GasMixVolume * 0.1f;
 }
--- Content.Server/Mech/Components/MechAirIntakeComponent.cs
+++ Content.Server/Mech/Components/MechAirIntakeComponent.cs
@@ -11,18 +11,18 @@ public sealed partial class MechAirIntakeComponent : Component
     /// <summary>
     /// Target pressure change for a single atmos tick
     /// </summary>
-    [DataField("targetPressureChange"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float TargetPressureChange = 5f;
 
     /// <summary>
     /// How strong the intake pump is, it will be able to replenish air from lower pressure areas.
     /// </summary>
-    [DataField("pumpPower"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float PumpPower = 2f;
 
     /// <summary>
     /// Pressure to intake gases up to, maintains MechAir pressure.
     /// </summary>
-    [DataField("pressure"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float Pressure = Atmospherics.OneAtmosphere;
 }
