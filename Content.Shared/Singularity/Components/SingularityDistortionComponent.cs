148870889d6f152baf899c2928698f2ac3cd5113
Kara <lunarautomaton6@gmail.com>
2023-10-11 02:41:32 -0500

    Kill `ComponentExt`

--- Content.Shared/Singularity/Components/SingularityDistortionComponent.cs
+++ Content.Shared/Singularity/Components/SingularityDistortionComponent.cs
@@ -1,31 +1,17 @@
+using Content.Shared.Singularity.EntitySystems;
 using Robust.Shared.GameStates;
-using Robust.Shared.Serialization;
 
 namespace Content.Shared.Singularity.Components
 {
     [RegisterComponent, NetworkedComponent]
     [AutoGenerateComponentState]
+    [Access(typeof(SharedSingularitySystem))]
     public sealed partial class SingularityDistortionComponent : Component
     {
-        // TODO: use access and remove this funny stuff
-        [DataField("intensity")]
-        private float _intensity = 31.25f;
+        [DataField]
+        public float Intensity = 31.25f;
 
-        [DataField("falloffPower")]
-        private float _falloffPower = MathF.Sqrt(2f);
-
-        [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
-        public float Intensity
-        {
-            get => _intensity;
-            set => this.SetAndDirtyIfChanged(ref _intensity, value);
-        }
-
-        [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
-        public float FalloffPower
-        {
-            get => _falloffPower;
-            set => this.SetAndDirtyIfChanged(ref _falloffPower, value);
-        }
+        [DataField]
+        public float FalloffPower = MathF.Sqrt(2f);
     }
 }
