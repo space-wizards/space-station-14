3b5d4b2bb35511387d11333142af8ba81f66cb9c
DrSmugleaf <drsmugleaf@gmail.com>
2023-10-14 02:46:17 -0700

    Add pop sound effect when erasing

--- Content.Server/Administration/Systems/AdminSystem.cs
+++ Content.Server/Administration/Systems/AdminSystem.cs
@@ -25,6 +25,7 @@ using Content.Shared.StationRecords;
 using Content.Shared.Throwing;
 using Robust.Server.GameObjects;
 using Robust.Server.Player;
+using Robust.Shared.Audio;
 using Robust.Shared.Configuration;
 using Robust.Shared.Enums;
 using Robust.Shared.Network;
@@ -38,6 +39,7 @@ namespace Content.Server.Administration.Systems
         [Dependency] private readonly IChatManager _chat = default!;
         [Dependency] private readonly IConfigurationManager _config = default!;
         [Dependency] private readonly IPlayerManager _playerManager = default!;
+        [Dependency] private readonly AudioSystem _audio = default!;
         [Dependency] private readonly HandsSystem _hands = default!;
         [Dependency] private readonly SharedJobSystem _jobs = default!;
         [Dependency] private readonly InventorySystem _inventory = default!;
@@ -336,6 +338,9 @@ namespace Content.Server.Administration.Systems
                     var coordinates = _transform.GetMoverCoordinates(entity.Value, transform);
                     var name = Identity.Entity(entity.Value, EntityManager);
                     _popup.PopupCoordinates(Loc.GetString("admin-erase-popup", ("user", name)), coordinates, PopupType.LargeCaution);
+                    var filter = Filter.Pvs(coordinates, 1, EntityManager, _playerManager);
+                    var audioParams = new AudioParams().WithVolume(3);
+                    _audio.Play("/Audio/Effects/pop_high.ogg", filter, coordinates, true, audioParams);
                 }
 
                 foreach (var item in _inventory.GetHandOrInventoryEntities(entity.Value))
