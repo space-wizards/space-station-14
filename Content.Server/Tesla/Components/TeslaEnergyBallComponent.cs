957e754019697ac10adc2513ec781620794ca04a
Ed <edwardxperia2000@gmail.com>
2023-10-19 02:06:50 +0300

    Added Tesla energy logic

---
+++ Content.Server/Tesla/Components/TeslaEnergyBallComponent.cs
@@ -0,0 +1,48 @@
+using Robust.Shared.GameStates;
+
+using Content.Shared.Singularity.EntitySystems;
+using Robust.Shared.Audio;
+using Robust.Shared.Utility;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Tesla.Components;
+
+/// <summary>
+/// A component that makes the associated entity accumulate energy when an associated event horizon consumes things.
+/// Energy management is server-side.
+/// </summary>
+[RegisterComponent]
+public sealed partial class TeslaEnergyBallComponent : Component
+{
+
+    public float AccumulatedFrametime = 0.0f;
+
+    [DataField]
+    public float UpdateInterval = 3.0f;
+
+    /// <summary>
+    /// The amount of energy this tesla contains. Once the limit is reached, the energy will be spent to spawn mini-energy balls
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float Energy;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float NeedEnergyToSpawn = 100f;
+
+    /// <summary>
+    /// how much energy the tesla uses per second.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float EnergyLoss = 3f;
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float EnergyToDespawn = -1000f;
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public ProtoId<EntityPrototype>? SpawnProto = "TeslaMiniEnergyBall";
+}
---
+++ Content.Server/Tesla/EntitySystem/TeslaEnergyBallSystem.cs
@@ -0,0 +1,60 @@
+using Content.Server.Singularity.Components;
+using Content.Shared.Singularity.EntitySystems;
+using Content.Shared.Tesla.Components;
+using Robust.Shared.Physics.Events;
+
+namespace Content.Server.Tesla.EntitySystems;
+
+/// <summary>
+/// A component that takes energy and spends it to spawn mini energy balls
+/// </summary>
+public sealed class TeslaEnergyBallSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<TeslaEnergyBallComponent, StartCollideEvent>(HandleParticleCollide);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<TeslaEnergyBallComponent>();
+        while (query.MoveNext(out var uid, out var teslaEnergyBall))
+        {
+            teslaEnergyBall.AccumulatedFrametime += frameTime;
+
+            if (teslaEnergyBall.AccumulatedFrametime < teslaEnergyBall.UpdateInterval)
+                continue;
+
+            AdjustEnergy(uid, teslaEnergyBall, -teslaEnergyBall.EnergyLoss * teslaEnergyBall.AccumulatedFrametime);
+            Log.Debug("Текущая энергия: " + teslaEnergyBall.Energy);
+            teslaEnergyBall.AccumulatedFrametime = 0f;
+        }
+    }
+
+    private void HandleParticleCollide(EntityUid uid, TeslaEnergyBallComponent component, ref StartCollideEvent args)
+    {
+        if (!TryComp<SinguloFoodComponent>(args.OtherEntity, out var singuloFood))
+            return;
+
+        AdjustEnergy(uid, component, singuloFood.Energy);
+        EntityManager.QueueDeleteEntity(args.OtherEntity);
+
+    }
+    public void AdjustEnergy(EntityUid uid, TeslaEnergyBallComponent component, float delta)
+    {
+        component.Energy += delta;
+
+        if (component.Energy > component.NeedEnergyToSpawn) {
+            component.Energy -= component.NeedEnergyToSpawn;
+            Spawn(component.SpawnProto, Transform(uid).Coordinates);
+        }
+        if (component.Energy < component.EnergyToDespawn)
+        {
+            QueueDel(uid);
+        }
+    }
+}
