636f16ded5e075838c144187a53fcc90a7f7b13d
unknown <randomushkarytp@gmail.com>
2023-10-19 16:33:54 +0300

    Fix part

--- Content.Shared/FootPrints/PuddleFootPrintsComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Shared.FootPrints;
-
-[RegisterComponent]
-public sealed partial class PuddleFootPrintsComponent : Component
-{
-    public float SizeRatio = 0.3f;
-    public float OffPercent = 80f;
-}
