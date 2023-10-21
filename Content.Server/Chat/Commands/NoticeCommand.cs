e6a2acb36d8726b8d7cbd400d209aebde54c4846
Onoira <onoira@psiko.zone>
2023-10-21 21:16:02 +0200

    style + add localisation, dependency injection

--- Content.Server/Chat/Commands/NoticeCommand.cs
+++ Content.Server/Chat/Commands/NoticeCommand.cs
@@ -5,40 +5,41 @@ using Robust.Server.Player;
 using Robust.Shared.Console;
 using Robust.Shared.Enums;
 
-namespace Content.Server.Chat.Commands
+namespace Content.Server.Chat.Commands;
+
+[AnyCommand]
+internal sealed class NoticeCommand : IConsoleCommand
 {
-    [AnyCommand]
-    internal sealed class NoticeCommand : IConsoleCommand
+    [Dependency] private readonly IChatManager _chatManager = default!;
+
+    public string Command => "notice";
+    public string Description => Loc.GetString("notice-command-description");
+    public string Help => Loc.GetString("notice-command-help", ("command", Command));
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        public string Command => "notice";
-        public string Description => "Send a chat message to self.";
-        public string Help => "notice <text>";
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        if (shell.Player is not IPlayerSession player)
         {
-            if (shell.Player is not IPlayerSession player)
-            {
-                shell.WriteError("This command cannot be run from the server.");
-                return;
-            }
-
-            if (player.Status != SessionStatus.InGame)
-                return;
-
-            if (player.AttachedEntity is not { } playerEntity)
-            {
-                shell.WriteError("You don't have an entity!");
-                return;
-            }
-
-            if (args.Length < 1)
-                return;
-
-            var message = string.Join(" ", args).Trim();
-            if (string.IsNullOrEmpty(message))
-                return;
-
-            IoCManager.Resolve<IChatManager>().ChatMessageToOne(ChatChannel.Visual, message, message, EntityUid.Invalid, false, player.ConnectedClient, recordReplay: false);
+            shell.WriteError("This command cannot be run from the server.");
+            return;
         }
+
+        if (player.Status != SessionStatus.InGame)
+            return;
+
+        if (player.AttachedEntity is not { })
+        {
+            shell.WriteError("You don't have an entity!");
+            return;
+        }
+
+        if (args.Length < 1)
+            return;
+
+        var message = string.Join(" ", args).Trim();
+        if (string.IsNullOrEmpty(message))
+            return;
+
+        _chatManager.ChatMessageToOne(ChatChannel.Visual, message, message, EntityUid.Invalid, false, player.ConnectedClient, recordReplay: false);
     }
 }
---
+++ Resources/Locale/en-US/administration/commands/notice-command.ftl
@@ -0,0 +1,2 @@
+notice-command-description = Send a message to self in the Visual channel
+notice-command-help = Usage: {$command} <message>…
