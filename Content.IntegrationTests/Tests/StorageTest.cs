41795720da7eb92ebfc755c8bc86ae3825ad2d45
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-10-25 18:53:38 -0400

    [TEST MERGE] Slot-based Storage (#21212)

--- Content.IntegrationTests/Tests/StorageTest.cs
+++ Content.IntegrationTests/Tests/StorageTest.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using Content.Server.Storage.Components;
 using Content.Shared.Item;
+using Content.Shared.Prototypes;
 using Content.Shared.Storage;
 using Content.Shared.Storage.Components;
+using Content.Shared.Storage.EntitySystems;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Prototypes;
-using Robust.UnitTesting;
 
 namespace Content.IntegrationTests.Tests
 {
@@ -32,9 +33,11 @@ namespace Content.IntegrationTests.Tests
                 {
                     if (!proto.TryGetComponent<StorageComponent>("Storage", out var storage) ||
                         storage.Whitelist != null ||
-                        !proto.TryGetComponent<ItemComponent>("Item", out var item)) continue;
+                        storage.MaxItemSize == null ||
+                        !proto.TryGetComponent<ItemComponent>("Item", out var item))
+                        continue;
 
-                    Assert.That(storage.StorageCapacityMax, Is.LessThanOrEqualTo(item.Size), $"Found storage arbitrage on {proto.ID}");
+                    Assert.That(storage.MaxItemSize.Value, Is.LessThan(item.Size), $"Found storage arbitrage on {proto.ID}");
                 }
             });
             await pair.CleanReturnAsync();
@@ -82,69 +85,120 @@ namespace Content.IntegrationTests.Tests
             {
                 foreach (var proto in PoolManager.GetPrototypesWithComponent<StorageFillComponent>(server))
                 {
-                    int capacity;
-                    var isEntStorage = false;
+                    if (proto.HasComponent<EntityStorageComponent>(compFact))
+                        continue;
 
-                    if (proto.TryGetComponent<StorageComponent>("Storage", out var storage))
+                    if (!proto.TryGetComponent<StorageComponent>("Storage", out var storage))
                     {
-                        capacity = storage.StorageCapacityMax;
+                        Assert.Fail($"Entity {proto.ID} has storage-fill without a storage component!");
+                        continue;
                     }
-                    else if (proto.TryGetComponent<EntityStorageComponent>("EntityStorage", out var entStorage))
+
+                    proto.TryGetComponent<ItemComponent>("Item", out var item);
+
+                    var maxSize = storage.MaxItemSize ??
+                                  (item?.Size == null
+                                      ? SharedStorageSystem.DefaultStorageMaxItemSize
+                                      : (ItemSize) Math.Max(0, (int) item.Size - 1));
+
+                    var capacity = storage.MaxTotalWeight ??
+                                   storage.MaxSlots * SharedItemSystem.GetItemSizeWeight(storage.MaxItemSize ?? maxSize);
+
+                    var fill = (StorageFillComponent) proto.Components[id].Component;
+                    var size = GetFillSize(fill, false, protoMan);
+
+                    Assert.That(size, Is.LessThanOrEqualTo(capacity), $"{proto.ID} storage fill is too large.");
+                    Assert.That(GetFillSize(fill, true, protoMan), Is.LessThanOrEqualTo(storage.MaxSlots), $"{proto.ID} storage fill has too many items.");
+
+                    foreach (var entry in fill.Contents)
                     {
-                        capacity = entStorage.Capacity;
-                        isEntStorage = true;
+                        if (entry.PrototypeId == null)
+                            continue;
+
+                        if (!protoMan.TryIndex<EntityPrototype>(entry.PrototypeId, out var fillItem))
+                            continue;
+
+                        if (!fillItem.TryGetComponent<ItemComponent>("Item", out var entryItem))
+                            continue;
+
+                        Assert.That(entryItem.Size, Is.LessThanOrEqualTo(maxSize),
+                            $"Entity {proto.ID} has storage-fill item, {entry.PrototypeId}, that is too large");
                     }
-                    else
+                }
+            });
+
+            await pair.CleanReturnAsync();
+
+        }
+
+        [Test]
+        public async Task TestSufficientSpaceForEntityStorageFill()
+        {
+            await using var pair = await PoolManager.GetServerClient();
+            var server = pair.Server;
+
+            var protoMan = server.ResolveDependency<IPrototypeManager>();
+            var compFact = server.ResolveDependency<IComponentFactory>();
+            var id = compFact.GetComponentName(typeof(StorageFillComponent));
+
+            Assert.Multiple(() =>
+            {
+                foreach (var proto in PoolManager.GetPrototypesWithComponent<StorageFillComponent>(server))
+                {
+                    if (proto.HasComponent<StorageComponent>(compFact))
+                        continue;
+
+                    if (!proto.TryGetComponent<EntityStorageComponent>("EntityStorage", out var entStorage))
                     {
                         Assert.Fail($"Entity {proto.ID} has storage-fill without a storage component!");
                         continue;
                     }
 
                     var fill = (StorageFillComponent) proto.Components[id].Component;
-                    var size = GetFillSize(fill, isEntStorage);
-                    Assert.That(size, Is.LessThanOrEqualTo(capacity), $"{proto.ID} storage fill is too large.");
+                    var size = GetFillSize(fill, true, protoMan);
+                    Assert.That(size, Is.LessThanOrEqualTo(entStorage.Capacity),
+                        $"{proto.ID} storage fill is too large.");
                 }
             });
+            await pair.CleanReturnAsync();
+        }
 
-            int GetEntrySize(EntitySpawnEntry entry, bool isEntStorage)
+        private int GetEntrySize(EntitySpawnEntry entry, bool getCount, IPrototypeManager protoMan)
+        {
+            if (entry.PrototypeId == null)
+                return 0;
+
+            if (!protoMan.TryIndex<EntityPrototype>(entry.PrototypeId, out var proto))
             {
-                if (entry.PrototypeId == null)
-                    return 0;
-
-                if (!protoMan.TryIndex<EntityPrototype>(entry.PrototypeId, out var proto))
-                {
-                    Assert.Fail($"Unknown prototype: {entry.PrototypeId}");
-                    return 0;
-                }
-
-                if (isEntStorage)
-                    return entry.Amount;
-
-                if (proto.TryGetComponent<ItemComponent>("Item", out var item))
-                    return item.Size * entry.Amount;
-
-                Assert.Fail($"Prototype is missing item comp: {entry.PrototypeId}");
+                Assert.Fail($"Unknown prototype: {entry.PrototypeId}");
                 return 0;
             }
 
-            int GetFillSize(StorageFillComponent fill, bool isEntStorage)
+            if (getCount)
+                return entry.Amount;
+
+            if (proto.TryGetComponent<ItemComponent>("Item", out var item))
+                return SharedItemSystem.GetItemSizeWeight(item.Size) * entry.Amount;
+
+            Assert.Fail($"Prototype is missing item comp: {entry.PrototypeId}");
+            return 0;
+        }
+
+        private int GetFillSize(StorageFillComponent fill, bool getCount, IPrototypeManager protoMan)
+        {
+            var totalSize = 0;
+            var groups = new Dictionary<string, int>();
+            foreach (var entry in fill.Contents)
             {
-                var totalSize = 0;
-                var groups = new Dictionary<string, int>();
-                foreach (var entry in fill.Contents)
-                {
-                    var size = GetEntrySize(entry, isEntStorage);
+                var size = GetEntrySize(entry, getCount, protoMan);
 
-                    if (entry.GroupId == null)
-                        totalSize += size;
-                    else
-                        groups[entry.GroupId] = Math.Max(size, groups.GetValueOrDefault(entry.GroupId));
-                }
-
-                return totalSize + groups.Values.Sum();
+                if (entry.GroupId == null)
+                    totalSize += size;
+                else
+                    groups[entry.GroupId] = Math.Max(size, groups.GetValueOrDefault(entry.GroupId));
             }
 
-            await pair.CleanReturnAsync();
+            return totalSize + groups.Values.Sum();
         }
     }
 }
