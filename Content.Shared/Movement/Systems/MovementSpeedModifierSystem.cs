ee86516eb4436dbdc4f80ce13f603f46eafb839b
Yurii Kis <yurii.kis@smartteksas.com>
2023-10-27 14:38:48 +0300

    updated sprite alpha, removed appearance component (not used)

--- Content.Shared/Movement/Systems/MovementSpeedModifierSystem.cs
+++ Content.Shared/Movement/Systems/MovementSpeedModifierSystem.cs
@@ -39,6 +39,7 @@ namespace Content.Shared.Movement.Systems
             Dirty(uid, move);
         }
 
+        // We might want to create separate RefreshMovementFrictionModifiersEvent and RefreshMovementFrictionModifiers function that will call it
         public void ChangeFriction(EntityUid uid, float friction, float? frictionNoInput, float acceleration, MovementSpeedModifierComponent? move = null)
         {
             if (!Resolve(uid, ref move, false))
