5b2ded95fa90a04a652f8d9a7cc7e1a87b8fa035
avery <51971268+graevy@users.noreply.github.com>
2023-10-30 01:57:48 -0700

    textscreen documentation

--- Content.Server/TextScreen/Events/TextScreenTextEvent.cs
+++ Content.Server/TextScreen/Events/TextScreenTextEvent.cs
@@ -1,5 +1,8 @@
 namespace Content.Server.TextScreen.Events;
 
+/// <summary>
+/// Sets the Label on a TextScreen, and sets its display mode to Text.
+/// </summary>
 [ByRefEvent]
 public readonly record struct TextScreenTextEvent(string Label)
 {
--- Content.Server/TextScreen/Events/TextScreenTimerEvent.cs
+++ Content.Server/TextScreen/Events/TextScreenTimerEvent.cs
@@ -1,5 +1,8 @@
 namespace Content.Server.TextScreen.Events;
 
+/// <summary>
+/// Sets the Duration on a TextScreen, and sets its display mode to Timer, counting down to zero.
+/// </summary>
 [ByRefEvent]
 public readonly record struct TextScreenTimerEvent(TimeSpan Duration)
 {
