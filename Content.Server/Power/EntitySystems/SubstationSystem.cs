670358d35d5f5f303d6861edd3a79434bb076c50
joshepvodka <guilherme.ornel@gmail.com>
2023-10-31 23:30:39 -0300

    removed emag

--- Content.Server/Power/EntitySystems/SubstationSystem.cs
+++ Content.Server/Power/EntitySystems/SubstationSystem.cs
@@ -1,28 +1,21 @@
-using System;
-using System.Diagnostics.SymbolStore;
-using System.Security.Cryptography;
-using System.Security.Permissions;
 using Content.Server.Power.Components;
-using Content.Server.Power.Pow3r;
-using Content.Shared.Access.Components;
 using Content.Shared.Access.Systems;
-using Content.Shared.Emag.Components;
-using Content.Shared.Emag.Systems;
-using Content.Shared.Examine;
 using Robust.Server.GameObjects;
-using Robust.Shared.GameObjects;
-using Robust.Shared.Timing;
+using Robust.Shared.Timing; 
+using Content.Shared.Examine;
+
 
 namespace Content.Server.Power.EntitySystems;
 
 public sealed class SubstationSystem : EntitySystem 
 {
 
+    [Dependency] private readonly AccessReaderSystem _accessReader = default!;
     [Dependency] private readonly IGameTiming _gameTiming = default!;
 
     private bool _substationWearEnabled = true;
     private const int _defaultSubstationWearTimeout = 300000; //5 minutes
-    private float _substationWearCoeficient = 1 / 300000;
+    private float _substationWearCoeficient = 300000;
 
     public override void Initialize()
     {
@@ -32,11 +25,13 @@ public sealed class SubstationSystem : EntitySystem
 
         SubscribeLocalEvent<SubstationComponent, MapInitEvent>(OnSubstationInit);
         SubscribeLocalEvent<SubstationComponent, ExaminedEvent>(OnExamine);
-        SubscribeLocalEvent<SubstationComponent, GotEmaggedEvent>(OnEmagged);
     }
 
     private void OnSubstationInit(EntityUid uid, SubstationComponent component, MapInitEvent args) 
-    {}
+    {
+        component.Enabled = true;
+        component.Integrity = 100.0f;
+    }
 
     private void OnExamine(EntityUid uid, SubstationComponent component, ExaminedEvent args) 
     {
@@ -66,21 +61,19 @@ public sealed class SubstationSystem : EntitySystem
 
     public override void Update(float deltaTime)
     {
-        if(!_substationWearEnabled)
-            return;
-        
         var query = EntityQueryEnumerator<SubstationComponent, PowerNetworkBatteryComponent>();
         while (query.MoveNext(out var uid, out var subs, out var battery))
         {
-
-            if(subs.Integrity > 0.0f && subs.Enabled) {
-                var wear = battery.CurrentSupply * deltaTime * _substationWearCoeficient;
+            if(subs.Integrity > 0.0f && subs.Enabled)
+            {
+                var wear = battery.CurrentSupply * deltaTime / _substationWearCoeficient;
                 subs.Integrity -= wear;
 
-                if(subs.Integrity <= 0.0f) {
+                if(subs.Integrity <= 0.0f) 
+                {
                     ShutdownSubstation(uid, subs, battery);
                     _substationWearEnabled = false;
-                    Timer.Spawn(_defaultSubstationWearTimeout, EnableSubstationWear);
+                    //Timer.Spawn(_defaultSubstationWearTimeout, EnableSubstationWear);
                 }
             }
         }
@@ -89,13 +82,6 @@ public sealed class SubstationSystem : EntitySystem
     private void EnableSubstationWear() 
     {
         _substationWearEnabled = true;
-    }}
-
-    private void OnEmagged(EntityUid uid, SubstationComponent comp, ref GotEmaggedEvent args)
-    {
-        args.Handled = true;
-        TryComp<PowerNetworkBatteryComponent>(uid, out var battery);
-        ShutdownSubstation(uid, comp, battery);
     }
 
     private void ShutdownSubstation(EntityUid uid, SubstationComponent? subs=null, PowerNetworkBatteryComponent? battery=null)
---
+++ Resources/Locale/en-US/power/components/substation-component.ftl
@@ -0,0 +1,7 @@
+
+### UI
+
+# Shown when the substation is examined in details range
+substation-component-examine-integrity = The substation is at [color={$markupPercentColor}]{$percent}%[/color] integrity.
+substation-component-examine-malfunction = The substation is [color=red]malfunctioning[/color].
+
