3c708c2b0cbdeeb37b6316f459839dbffc3b87b2
Ed <edwardxperia2000@gmail.com>
2023-11-01 17:37:32 +0300

    finish

--- Content.Server/Anomaly/Components/AnomalyCoreComponent.cs
+++ Content.Server/Anomaly/Components/AnomalyCoreComponent.cs
@@ -1,10 +1,11 @@
 using Content.Server.Anomaly.Effects;
+using Robust.Shared.Audio;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Server.Anomaly.Components;
 
 /// <summary>
-/// This component exists for a limited time, and after it expires it modifies the entity, greatly reducing its value and changing its visuals
+/// 
 /// </summary>
 [RegisterComponent, Access(typeof(AnomalyCoreSystem))]
 public sealed partial class AnomalyCoreComponent : Component
@@ -14,12 +15,33 @@ public sealed partial class AnomalyCoreComponent : Component
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
     public double TimeToDecay = 600;
-
     /// <summary>
     /// The moment of core decay. It is set during entity initialization.
     /// </summary>
     [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), ViewVariables(VVAccess.ReadWrite)]
     public TimeSpan DecayMoment;
+    /// <summary>
+    /// Has the core decayed?
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public bool IsDecayed;
+
+    /// <summary>
+    /// Amount of time required for the core to collapse after interaction
+    /// corresponds to the length of the <see cref="ChargingSound"></see>
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public double TimeToCollapse = 3;
+    /// <summary>
+    /// The moment the core explodes after activation.
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan CollapseMoment;
+    /// <summary>
+    /// Core collapsing now?
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public bool IsCollapsing;
 
     /// <summary>
     /// The starting value of the entity.
@@ -34,8 +56,14 @@ public sealed partial class AnomalyCoreComponent : Component
     public double EndPrice = 200;
 
     /// <summary>
-    /// Has the core decayed?
+    /// the sound made at the onset of core collapse
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public bool IsDecayed;
+    public SoundSpecifier ChargingSound = new SoundPathSpecifier("/Audio/Effects/anomaly_core_charge.ogg");
+
+    /// <summary>
+    /// the sound of the core collapsing.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public SoundSpecifier CollapseSound = new SoundCollectionSpecifier("RadiationPulse");
 }
--- Content.Server/Anomaly/Effects/AnomalyCoreSystem.cs
+++ Content.Server/Anomaly/Effects/AnomalyCoreSystem.cs
@@ -4,16 +4,18 @@ using Content.Shared.Anomaly;
 using Content.Shared.Anomaly.Components;
 using Content.Shared.Interaction;
 using Content.Shared.Interaction.Events;
+using Robust.Server.GameObjects;
 using Robust.Shared.Timing;
 
 namespace Content.Server.Anomaly.Effects;
 
 /// <summary>
-/// This component reduces the value of the entity during decay
+/// <see cref="AnomalyCoreComponent""/>
 /// </summary>
 public sealed class AnomalyCoreSystem : EntitySystem
 {
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly AudioSystem _audio = default!;
     [Dependency] private readonly IGameTiming _gameTiming = default!;
 
     public override void Initialize()
@@ -35,16 +37,27 @@ public sealed class AnomalyCoreSystem : EntitySystem
         var query = EntityQueryEnumerator<AnomalyCoreComponent>();
         while (query.MoveNext(out var uid, out var component))
         {
-            if (component.IsDecayed)
-                continue;
-
-            //When time runs out, we completely decompose
-            if (component.DecayMoment < _gameTiming.CurTime)
-                Decay(uid, component);
+            if (!component.IsDecayed)
+            {
+                //When time runs out, we completely decompose
+                if (component.DecayMoment < _gameTiming.CurTime)
+                    Decay(uid, component);
+            }
+            if (component.IsCollapsing)
+            {
+                if (component.CollapseMoment < _gameTiming.CurTime)
+                    Collapse(uid, component);
+            }
         }
     }
     private void OnGetPrice(Entity<AnomalyCoreComponent> core, ref PriceCalculationEvent args)
     {
+        if (core.Comp.IsDecayed)
+        {
+            args.Price = core.Comp.EndPrice;
+            return;
+        }
+
         var timeLeft = core.Comp.DecayMoment - _gameTiming.CurTime;
         var lerp = (double) (timeLeft.TotalSeconds / core.Comp.TimeToDecay);
         lerp = Math.Clamp(lerp, 0, 1);
@@ -61,23 +74,29 @@ public sealed class AnomalyCoreSystem : EntitySystem
     #region Collapsing
     private void OnActivateInWorld(Entity<AnomalyCoreComponent> core, ref ActivateInWorldEvent args)
     {
-        TryCollapse(core);
+        StartCollapsing(core);
     }
 
     private void OnUseInHand(Entity<AnomalyCoreComponent> core, ref UseInHandEvent args)
     {
-        TryCollapse(core);
+        StartCollapsing(core);
     }
 
-    private void TryCollapse(Entity<AnomalyCoreComponent> core)
+    private void StartCollapsing(Entity<AnomalyCoreComponent> core)
     {
-        if (core.Comp.IsDecayed)
+        if (core.Comp.IsDecayed || core.Comp.IsCollapsing)
             return;
-        Log.Debug("BOOM BOOM DETKA");
-        var ev = new AnomalyPulseEvent(0.5f, 0.5f);
-        RaiseLocalEvent(core.Owner, ref ev);
-        QueueDel(core);
-    }
 
+        _audio.PlayPvs(core.Comp.ChargingSound, core);
+        core.Comp.IsCollapsing = true;
+        core.Comp.CollapseMoment = _gameTiming.CurTime + TimeSpan.FromSeconds(core.Comp.TimeToCollapse);
+    }
+    private void Collapse(EntityUid uid, AnomalyCoreComponent component)
+    {
+        _audio.PlayPvs(component.CollapseSound, uid);
+        var ev = new AnomalyPulseEvent(0.5f, 0.5f);
+        RaiseLocalEvent(uid, ref ev);
+        QueueDel(uid);
+    }
     #endregion
 }
