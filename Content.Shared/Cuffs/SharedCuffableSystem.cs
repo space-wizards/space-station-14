4dc27a7c0afc1e89a7b8afeac725a802f7fe5295
Avery Dobbins <avery.dobbins@gmail.com>
2023-10-31 23:41:14 -0400

    Address the mere mortals fears of the studded leather rod.

--- Content.Shared/Cuffs/SharedCuffableSystem.cs
+++ Content.Shared/Cuffs/SharedCuffableSystem.cs
@@ -455,6 +455,7 @@ namespace Content.Shared.Cuffs
                     if (_net.IsServer) /// let the server spawn because client mispredicts
                     {
                         var pos = Transform(target).Coordinates;
+                        ///var prototype = _prototypeManager.TryIndex<StackPrototype>(stack.StackTypeId, out var stackType)
                         handcuffsplit = Spawn("Zipties", pos); /// This should somehow get the proto ID instead of zipties, but fuck if I know how.
                         return true;
                     }
