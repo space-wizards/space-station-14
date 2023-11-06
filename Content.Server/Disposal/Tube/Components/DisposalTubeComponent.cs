44c00463e56ff53adfc5953071e05fac7b1bd3b2
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-06 17:08:07 +0300

    tube comp clean up

--- Content.Server/Disposal/Tube/Components/DisposalTubeComponent.cs
+++ Content.Server/Disposal/Tube/Components/DisposalTubeComponent.cs
@@ -1,30 +1,31 @@
-using System.Linq;
-using Content.Server.Disposal.Unit.Components;
 using Content.Server.Disposal.Unit.EntitySystems;
-using Content.Shared.Construction.Components;
-using Content.Shared.Popups;
 using Robust.Shared.Audio;
 using Robust.Shared.Containers;
 
-namespace Content.Server.Disposal.Tube.Components
+namespace Content.Server.Disposal.Tube.Components;
+
+[RegisterComponent]
+[Access(typeof(DisposalTubeSystem), typeof(DisposableSystem))]
+public sealed partial class DisposalTubeComponent : Component
 {
-    [RegisterComponent]
-    [Access(typeof(DisposalTubeSystem), typeof(DisposableSystem))]
-    public sealed partial class DisposalTubeComponent : Component
-    {
-        [DataField("containerId")] public string ContainerId { get; set; } = "DisposalTube";
+    [DataField]
+    public string ContainerId = "DisposalTube";
 
-        public static readonly TimeSpan ClangDelay = TimeSpan.FromSeconds(0.5);
-        public TimeSpan LastClang;
+    [ViewVariables]
+    public readonly TimeSpan ClangDelay = TimeSpan.FromSeconds(0.5);
 
-        public bool Connected;
-        [DataField("clangSound")] public SoundSpecifier ClangSound = new SoundPathSpecifier("/Audio/Effects/clang.ogg");
+    [ViewVariables]
+    public TimeSpan LastClang;
 
-        /// <summary>
-        ///     Container of entities that are currently inside this tube
-        /// </summary>
-        [ViewVariables]
-        [Access(typeof(DisposalTubeSystem), typeof(DisposableSystem))]
-        public Container Contents { get; set; } = default!;
-    }
+    [ViewVariables]
+    public bool Connected;
+
+    [DataField]
+    public SoundSpecifier ClangSound = new SoundPathSpecifier("/Audio/Effects/clang.ogg");
+
+    /// <summary>
+    ///     Container of entities that are currently inside this tube
+    /// </summary>
+    [ViewVariables]
+    public Container Contents = default!;
 }
