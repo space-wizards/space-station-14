84627209daace685b9de89473488a578f0a36a08
NeuroDevil <leshasgusch@gmail.com>
2023-11-07 20:47:48 +0300

    Added handheld radio UI, and fixed issue#18022

---
+++ Content.Client/Radio/Ui/HandheldRadioBoundUserInterface.cs
@@ -0,0 +1,58 @@
+using Content.Shared.Radio;
+using JetBrains.Annotations;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Radio.Ui;
+
+[UsedImplicitly]
+public sealed class HandheldRadioBoundUserInterface : BoundUserInterface
+{
+    [ViewVariables]
+    private HandheldRadioMenu? _menu;
+
+    public HandheldRadioBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _menu = new();
+
+        _menu.OnMicPressed += enabled =>
+        {
+            SendMessage(new ToggleHandheldRadioMicMessage(enabled));
+        };
+        _menu.OnSpeakerPressed += enabled =>
+        {
+            SendMessage(new ToggleHandheldRadioSpeakerMessage(enabled));
+        };
+        _menu.OnChannelSelected += channel =>
+        {
+            SendMessage(new SelectHandheldRadioChannelMessage(channel));
+        };
+
+        _menu.OnClose += Close;
+        _menu.OpenCentered();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (!disposing)
+            return;
+        _menu?.Close();
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+
+        if (state is not HandheldRadioBoundUIState msg)
+            return;
+
+        _menu?.Update(msg);
+    }
+}
---
+++ Content.Shared/Radio/SharedHandheldRadio.cs
@@ -0,0 +1,59 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Radio;
+
+[Serializable, NetSerializable]
+public enum HandheldRadioUiKey
+{
+    Key,
+}
+
+[Serializable, NetSerializable]
+public sealed class HandheldRadioBoundUIState : BoundUserInterfaceState
+{
+    public bool MicEnabled;
+    public bool SpeakerEnabled;
+    public List<string> AvailableChannels;
+    public string SelectedChannel;
+
+    public HandheldRadioBoundUIState(bool micEnabled, bool speakerEnabled, List<string> availableChannels, string selectedChannel)
+    {
+        MicEnabled = micEnabled;
+        SpeakerEnabled = speakerEnabled;
+        AvailableChannels = availableChannels;
+        SelectedChannel = selectedChannel;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class ToggleHandheldRadioMicMessage : BoundUserInterfaceMessage
+{
+    public bool Enabled;
+
+    public ToggleHandheldRadioMicMessage(bool enabled)
+    {
+        Enabled = enabled;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class ToggleHandheldRadioSpeakerMessage : BoundUserInterfaceMessage
+{
+    public bool Enabled;
+
+    public ToggleHandheldRadioSpeakerMessage(bool enabled)
+    {
+        Enabled = enabled;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class SelectHandheldRadioChannelMessage : BoundUserInterfaceMessage
+{
+    public int Channel;
+
+    public SelectHandheldRadioChannelMessage(int channel)
+    {
+        Channel = channel;
+    }
+}
