e9670fb04943f5c8a5ea7ed8aacbd4ee21b0e22c
temporaldarkness <94239354+temporaldarkness@users.noreply.github.com>
2023-11-10 03:07:11 +0300

    Фикс принтера книг
    Забыл добавить миграцию базы данных для постгреса, упс.

--- Content.Server.Database/Migrations/Postgres/20231110000509_BookTerminal.Designer.cs
+++ Content.Server.Database/Migrations/Postgres/20231110000509_BookTerminal.Designer.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using Content.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Content.Server.Database.Migrations.Postgres
 {
     [DbContext(typeof(PostgresServerDbContext))]
-    partial class PostgresServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231110000509_BookTerminal")]
+    partial class BookTerminal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Content.Server.Database/Migrations/Postgres/20231110000509_BookTerminal.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class BookTerminal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "book_terminal_entry",
+                columns: table => new
+                {
+                    book_terminal_entry_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    stamp_state = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_book_terminal_entry", x => x.book_terminal_entry_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "stamped_data",
+                columns: table => new
+                {
+                    stamped_data_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    color = table.Column<string>(type: "text", nullable: false),
+                    book_terminal_entry_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_stamped_data", x => x.stamped_data_id);
+                    table.ForeignKey(
+                        name: "FK_stamped_data_book_terminal_entry_book_terminal_entry_id",
+                        column: x => x.book_terminal_entry_id,
+                        principalTable: "book_terminal_entry",
+                        principalColumn: "book_terminal_entry_id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_book_terminal_entry_book_terminal_entry_id",
+                table: "book_terminal_entry",
+                column: "book_terminal_entry_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_stamped_data_book_terminal_entry_id",
+                table: "stamped_data",
+                column: "book_terminal_entry_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "stamped_data");
+
+            migrationBuilder.DropTable(
+                name: "book_terminal_entry");
+        }
+    }
+}
