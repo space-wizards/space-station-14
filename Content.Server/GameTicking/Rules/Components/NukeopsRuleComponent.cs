ac6419c1d1e36b9c5787292b87ee0d5db5e32a52
Robert Hardy <robertblake.hardy@outlook.com>
2023-11-17 06:01:05 +0900

    Changed NukeOps Shuttle Call Time from 10 Minutes to 3 Minutes (#21690)
    * Changed NukeOps Round End Behavior to Instant Round End
    
    * Changed RoundEndBehavior and EvacShuttleTime

--- Content.Server/GameTicking/Rules/Components/NukeopsRuleComponent.cs
+++ Content.Server/GameTicking/Rules/Components/NukeopsRuleComponent.cs
@@ -59,7 +59,7 @@ public sealed partial class NukeopsRuleComponent : Component
     /// Time to emergency shuttle to arrive if RoundEndBehavior is ShuttleCall.
     /// </summary>
     [DataField]
-    public TimeSpan EvacShuttleTime = TimeSpan.FromMinutes(10);
+    public TimeSpan EvacShuttleTime = TimeSpan.FromMinutes(3);
 
     /// <summary>
     /// Whether or not to spawn the nuclear operative outpost. Used by LoneOpsSpawn event.
