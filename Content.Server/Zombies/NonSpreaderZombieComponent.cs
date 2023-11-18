981390f24c25bb50516b7c7d28285d52a4097236
nikthechampiongr <nikvog4@gmail.com>
2023-11-18 20:31:04 +0200

    no anything
    This reverts f391ff28 and implements an alternate messure where mice and other small animals can no longer infect people as zombies.
    This is done through a component which if present cancels the check that would cause zombie components to be added on people that get infected due to a bite.
    This still allows other special stuff that happens in that function that may affect already infected individuals.
    This is a compromise between what's discussed in discord which would much rather see mice and other animals just die from the infection and people on github which would see no change happen.

---
+++ Content.Server/Zombies/NonSpreaderZombieComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server.Zombies;
+
+/// <summary>
+/// This is used for Zombies that cannot infect by biting
+/// </summary>
+[RegisterComponent]
+public sealed partial class NonSpreaderZombieComponent: Component
+{
+
+}
