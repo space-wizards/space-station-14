dd60ee818500b4053a230875f412f5d4ddaca0e9
keronshb <keronshb@live.com>
2023-11-25 17:05:35 -0500

    adds action commands and a command to upgrade an action

---
+++ Content.Server/Commands/ActionCommands.cs
@@ -0,0 +1,66 @@
+﻿using Content.Server.Administration;
+using Content.Shared.Actions;
+using Content.Shared.Administration;
+using Robust.Shared.Console;
+
+namespace Content.Server.Commands;
+
+[AdminCommand(AdminFlags.Fun)]
+internal sealed class UpgradeActionCommand : IConsoleCommand
+{
+    [Dependency] private readonly IEntityManager _entMan = default!;
+
+    public string Command => "upgradeaction";
+    public string Description => Loc.GetString("upgradeaction-command-description");
+    public string Help => Loc.GetString("upgradeaction-command-help");
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length < 1)
+        {
+            shell.WriteLine(Loc.GetString("upgradeaction-command-need-one-argument"));
+            return;
+        }
+
+        if (args.Length > 2)
+        {
+            shell.WriteLine(Loc.GetString("upgradeaction-command-max-two-arguments"));
+            return;
+        }
+
+        // var minds = IoCManager.Resolve<IEntityManager>().System<SharedMindSystem>();
+        // var gameTicker = EntitySystem.Get<GameTicker>();
+        // var suicideSystem = EntitySystem.Get<SuicideSystem>();
+
+        var actionUpgrade = _entMan.EntitySysManager.GetEntitySystem<ActionUpgradeSystem>();
+
+        // TODO: Check if the entity is valid first before assigning
+        // TODO: Also check if it has the action upgrade component
+        var entityUid = args[0];
+
+        // TODO: If only one arg, increment level
+
+        // TODO: If 2 args, check if 2nd arg is a number then upgrade action system can set level
+        if (args.Length == 2)
+        {
+            var levelArg = args[1];
+
+            if (!int.TryParse(levelArg, out var level))
+            {
+                shell.WriteLine(Loc.GetString("upgradeaction-command-second-argument-not-number"));
+                return;
+            }
+        }
+    }
+
+    /*public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        if (args.Length == 1)
+        {
+
+        }
+
+        return CompletionResult.Empty;
+    }*/
+
+}
---
+++ Resources/Locale/en-US/actions/actions/actions-commands.ftl
@@ -0,0 +1,9 @@
+﻿## Actions Commands loc
+
+## Upgradeaction command loc
+upgradeaction-command-need-one-argument = upgradeaction needs at least one argument, the action entity uid. The second optional argument is a specified level
+upgradeaction-command-max-two-arguments = upgradeaction has a maximum of two arguments, the action entity uid and the (optional) level to set
+upgradeaction-command-second-argument-not-number = upgradeaction's second argument can only be a number
+upgradeaction-command-description = Upgrades an action by one level, or to the specified level, if applicable
+upgradeaction-command-help = upgradeaction <entity Uid>
+upgradeaction-command-arg-entity = <entity Uid>
