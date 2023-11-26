06f7d8f56c3fbf7b5b2af6c0a097e8f2465ac2db
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-11-26 13:01:14 +1100

    Revert "Revert submodule to 180.2.1 (#21870)"
    This reverts commit 15052bf7c21b31f255fb7b2e7b06d9305237f4c3.
    
    # Conflicts:
    #	RobustToolbox

--- Content.IntegrationTests/Tests/PrototypeSaveTest.cs
+++ Content.IntegrationTests/Tests/PrototypeSaveTest.cs
@@ -217,7 +217,7 @@ public sealed class PrototypeSaveTest
             SerializationHookContext hookCtx,
             ISerializationContext? context, ISerializationManager.InstantiationDelegate<EntityUid>? instanceProvider)
         {
-            return EntityUid.Parse(node.Value);
+            return EntityUid.Parse(node.Value, "0");
         }
     }
 }
