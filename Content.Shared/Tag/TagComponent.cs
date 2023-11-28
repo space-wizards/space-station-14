76952352bdacaf5d90648ae5038faec61a16bb5b
PixelTheKermit <pixelmontrainer8967@gmail.com>
2023-11-28 23:11:13 +0000

    Network Tags.

--- Content.Shared/Tag/TagComponent.cs
+++ Content.Shared/Tag/TagComponent.cs
@@ -1,10 +1,12 @@
-﻿using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Set;
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Set;
 
 namespace Content.Shared.Tag
 {
-    [RegisterComponent, Access(typeof(TagSystem))]
+    [RegisterComponent, NetworkedComponent, Access(typeof(TagSystem))]
     public sealed partial class TagComponent : Component
     {
+        [AutoNetworkedField]
         [DataField("tags", customTypeSerializer: typeof(PrototypeIdHashSetSerializer<TagPrototype>))]
         [Access(typeof(TagSystem), Other = AccessPermissions.ReadExecute)] // FIXME Friends
         public HashSet<string> Tags = new();
