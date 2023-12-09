ec7810a14eadfa062210e13fdcfdfe6cbfbd1ea9
KP <13428215+nok-ko@users.noreply.github.com>
2023-12-09 02:47:55 -0800

    Remove unnecessary `tag` parameter passed to DataField attribute, add `VV.ReadWrite` back in

--- Content.Shared/CombatMode/Pacification/PacifiedComponent.cs
+++ Content.Shared/CombatMode/Pacification/PacifiedComponent.cs
@@ -14,7 +14,7 @@ public sealed partial class PacifiedComponent : Component
     /// <summary>
     ///     A blacklist specifying entities that the player will refuse to throw.
     /// </summary>
-    [DataField("throwBlacklist")]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public EntityWhitelist ThrowBlacklist = new()
     {
         Components = new[]
