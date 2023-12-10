be971bbd2d5471956a1b726d54a80a86988c336b
darksaiyanis@gmail.com <darksaiyanis@gmail.com>
2023-12-10 03:10:39 +0200

    Removed the unused ItemToggle from the serverside and created a new shared ItemToggleComponent and System, now used for the e-blade family of items. Also added e-blade hum and swing sounds. Thanks Sloth for the initial code!

--- Content.Shared/Item/ItemToggleComponent.cs
+++ Content.Shared/Item/ItemToggleComponent.cs
@@ -1,34 +1,133 @@
+using Content.Shared.Damage;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
 
-namespace Content.Server.Weapons.Melee.ItemToggle;
+namespace Content.Shared.Item;
 
-[RegisterComponent, NetworkedComponent]
+/// <summary>
+/// Handles generic item toggles, like a welder turning on and off, or an e-sword.
+/// </summary>
+/// <remarks>
+/// If you need extended functionality (e.g. requiring power) then add a new component and use events.
+/// </remarks>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class ItemToggleComponent : Component
 {
-    public bool Activated = false;
+    [ViewVariables(VVAccess.ReadWrite), DataField("activated"), AutoNetworkedField]
+    public bool Activated;
 
-    [DataField("activateSound")]
-    public SoundSpecifier ActivateSound { get; set; } = default!;
+    /// <summary>
+    ///     The noise this item makes when it is toggled on.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("soundActivate"), AutoNetworkedField]
+    public SoundSpecifier? ActivateSound;
 
-    [DataField("deActivateSound")]
-    public SoundSpecifier DeActivateSound { get; set; } = default!;
+    /// <summary>
+    ///     The noise this item makes when it is toggled off.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("soundDeactivate"), AutoNetworkedField]
+    public SoundSpecifier? DeactivateSound;
+
+    /// <summary>
+    ///     The noise this item makes when hitting something with it on.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("activatedSoundOnHit"), AutoNetworkedField]
+    public SoundSpecifier? ActivatedSoundOnHit;
+
+    /// <summary>
+    ///     The noise this item makes when hitting something with it off.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("deactivatedSoundOnHit"), AutoNetworkedField]
+    public SoundSpecifier? DeactivatedSoundOnHit;
+
+    /// <summary>
+    ///     The noise this item makes when hitting something with it off.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("activeSound"), AutoNetworkedField]
+    public SoundSpecifier? ActiveSound;
+
+    /// <summary>
+    ///     The noise this item makes when swinging at nothing while activated.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("activatedSoundOnSwing"), AutoNetworkedField]
+    public SoundSpecifier ActivatedSoundOnSwing = new SoundPathSpecifier("/Audio/Weapons/punchmiss.ogg");
+
+    /// <summary>
+    ///     The noise this item makes when swinging at nothing while not activated.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("deactivatedSoundOnSwing"), AutoNetworkedField]
+    public SoundSpecifier DeactivatedSoundOnSwing = new SoundPathSpecifier("/Audio/Weapons/punchmiss.ogg");
+
+    /// <summary>
+    ///     Damage done by this item when activated.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("activatedDamage")]
+    public DamageSpecifier ActivatedDamage = new();
+
+    /// <summary>
+    ///     Damage done by this item when deactivated.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("deactivatedDamage")]
+    public DamageSpecifier DeactivatedDamage = new();
+
 
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("activatedDisarmMalus")]
     public float ActivatedDisarmMalus = 0.6f;
 
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("offSize")]
-    public int OffSize = 1;
 
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("onSize")]
-    public int OnSize = 9999;
+    [DataField("activatedSharp")]
+    public bool ActivatedSharp = false;
+
+    /// <summary>
+    ///     Does this become hidden when deactivated
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("deactivatedSecret")]
+    public bool DeactivatedSecret = false;
+
+    /// <summary>
+    ///     Item's size increase when activated
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("activatedSizeModifier"), AutoNetworkedField]
+    public int ActivatedSizeModifier = 0;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("isHotWhenActivated"), AutoNetworkedField]
+    public bool IsHotWhenActivated = false;
+
+    /// <summary>
+    ///     Used when the item emits sound while active.
+    /// </summary>
+    public IPlayingAudioStream? Stream;
 }
 
+/// <summary>
+/// Raised directed on an entity when its ItemToggle is attempted to be activated.
+/// </summary>
 [ByRefEvent]
-public readonly record struct ItemToggleActivatedEvent();
+public readonly record struct ItemToggleActivateAttemptEvent(bool Cancelled = false);
 
+/// <summary>
+/// Raised directed on an entity when its ItemToggle is activated.
+/// </summary>
 [ByRefEvent]
-public readonly record struct ItemToggleDeactivatedEvent();
+public readonly record struct ItemToggleActivatedEvent;
+
+/// <summary>
+/// Raised directed on an entity when its ItemToggle is attempted to be deactivated.
+/// </summary>
+[ByRefEvent]
+public readonly record struct ItemToggleDeactivateAttemptEvent(bool Cancelled = false);
+
+/// <summary>
+/// Raised directed on an entity when its ItemToggle is deactivated.
+/// </summary>
+[ByRefEvent]
+public readonly record struct ItemToggleDeactivatedEvent;
+
+
---
+++ Content.Shared/Item/ItemToggleSystem.cs
@@ -0,0 +1,206 @@
+using Content.Shared.Interaction.Events;
+using Content.Shared.Toggleable;
+using Content.Shared.Weapons.Melee;
+using Content.Shared.Temperature;
+using Content.Shared.Wieldable;
+using Content.Shared.Wieldable.Components;
+using Robust.Shared.Random;
+using Robust.Shared.Audio;
+
+namespace Content.Shared.Item;
+/// <summary>
+/// Handles generic item toggles, like a welder turning on and off, or an e-sword.
+/// </summary>
+/// <remarks>
+/// If you need extended functionality (e.g. requiring power) then add a new component and use events.
+/// </remarks>
+public sealed class ItemToggleSystem : EntitySystem
+{
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly SharedItemSystem _item = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ItemToggleComponent, UseInHandEvent>(OnUseInHand);
+        SubscribeLocalEvent<ItemToggleComponent, IsHotEvent>(OnIsHotEvent);
+        SubscribeLocalEvent<ItemToggleComponent, ItemUnwieldedEvent>(TurnOffonUnwielded);
+        SubscribeLocalEvent<ItemToggleComponent, ItemWieldedEvent>(TurnOnonWielded);
+    }
+
+
+    public void Toggle(EntityUid uid, ItemToggleComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        if (component.Activated)
+        {
+            TryDeactivate(uid, component: component);
+        }
+        else
+        {
+            TryActivate(uid, component: component);
+        }
+    }
+
+    public bool TryActivate(EntityUid uid, EntityUid? user = null, ItemToggleComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return false;
+
+        if (component.Activated)
+            return true;
+
+        var attempt = new ItemToggleActivateAttemptEvent();
+
+        RaiseLocalEvent(uid, ref attempt);
+
+        if (attempt.Cancelled)
+            return false;
+
+        component.Activated = true;
+        var ev = new ItemToggleActivatedEvent();
+        RaiseLocalEvent(uid, ref ev);
+
+        UpdateItemComponent(uid, component);
+        UpdateWeaponComponent(uid, component);
+        UpdateAppearance(uid, component);
+        Dirty(uid, component);
+
+        //Added in order to supress the client side item from making noise.
+        if (uid.Id == GetNetEntity(uid).Id)
+        {
+            _audio.PlayPredicted(component.ActivateSound, uid, user);
+            //Starts the active sound (like humming).
+            component.Stream = _audio.PlayPredicted(component.ActiveSound, uid, user, AudioParams.Default.WithLoop(true));
+        }
+        return true;
+    }
+
+    public bool TryDeactivate(EntityUid uid, EntityUid? user = null, ItemToggleComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return false;
+
+        if (!component.Activated)
+            return true;
+
+        var attempt = new ItemToggleDeactivateAttemptEvent();
+
+        RaiseLocalEvent(uid, ref attempt);
+
+        if (attempt.Cancelled)
+            return false;
+
+        component.Activated = false;
+        var ev = new ItemToggleDeactivatedEvent();
+        RaiseLocalEvent(uid, ref ev);
+
+        UpdateItemComponent(uid, component);
+        UpdateWeaponComponent(uid, component);
+        UpdateAppearance(uid, component);
+        Dirty(uid, component);
+
+        //Added in order to supress the client side item from making noise.
+        if (uid.Id == GetNetEntity(uid).Id)
+        {
+            _audio.PlayPredicted(component.DeactivateSound, uid, user);
+            //Stops the active sound (like humming).
+            component.Stream?.Stop();
+            component.Stream = null;
+        }
+        return true;
+    }
+
+    private void OnUseInHand(EntityUid uid, ItemToggleComponent component, UseInHandEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.Handled = true;
+
+        if (TryComp<WieldableComponent>(uid, out var wieldableComp))
+            return;
+
+        Toggle(uid, component);
+    }
+
+    private void TurnOffonUnwielded(EntityUid uid, ItemToggleComponent component, ItemUnwieldedEvent args)
+    {
+        if (component.Activated)
+            TryDeactivate(uid, component: component);
+    }
+
+    private void TurnOnonWielded(EntityUid uid, ItemToggleComponent component, ref ItemWieldedEvent args)
+    {
+        if (!component.Activated)
+            TryActivate(uid, component: component);
+    }
+
+
+    /// <summary>
+    /// Used to update item appearance.
+    /// </summary>
+    private void UpdateAppearance(EntityUid uid, ItemToggleComponent component)
+    {
+        if (!TryComp(uid, out AppearanceComponent? appearanceComponent))
+            return;
+
+        _appearance.SetData(uid, ToggleableLightVisuals.Enabled, component.Activated, appearanceComponent);
+    }
+
+    /// <summary>
+    /// Used to update weapon component aspects, like hit sounds and damage values.
+    /// </summary>
+    private void UpdateWeaponComponent(EntityUid uid, ItemToggleComponent component)
+    {
+        if (!TryComp(uid, out MeleeWeaponComponent? weaponComp))
+            return;
+
+        if (component.Activated)
+        {
+            weaponComp.HitSound = component.ActivatedSoundOnHit;
+            weaponComp.SwingSound = component.ActivatedSoundOnSwing;
+            weaponComp.Damage = component.ActivatedDamage;
+
+            if (component.DeactivatedSecret)
+                weaponComp.Hidden = false;
+        }
+        else
+        {
+            weaponComp.HitSound = component.DeactivatedSoundOnHit;
+            weaponComp.SwingSound = component.DeactivatedSoundOnSwing;
+            weaponComp.Damage = component.DeactivatedDamage;
+
+            if (component.DeactivatedSecret)
+                weaponComp.Hidden = true;
+        }
+        Dirty(uid, weaponComp);
+    }
+
+    /// <summary>
+    /// Used to update item component aspects, like size values for items that expand when activated (heh).
+    /// </summary>
+    private void UpdateItemComponent(EntityUid uid, ItemToggleComponent component)
+    {
+        if (!TryComp(uid, out ItemComponent? item))
+            return;
+
+        if (component.Activated)
+            _item.SetSize(uid, item.Size + component.ActivatedSizeModifier, item);
+        else
+            _item.SetSize(uid, item.Size - component.ActivatedSizeModifier, item);
+    }
+
+    /// <summary>
+    /// Used to make the item hot when activated.
+    /// </summary>
+    private void OnIsHotEvent(EntityUid uid, ItemToggleComponent component, IsHotEvent args)
+    {
+        if (component.IsHotWhenActivated)
+            args.IsHot = component.Activated;
+    }
+}
