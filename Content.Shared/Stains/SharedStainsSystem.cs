5f6b3d438f0ebde320324f8aac38fda85deb4abe
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-12-11 13:53:44 +0300

    add doc to `SqueezeDoAfterEvent`

--- Content.Shared/Stains/SharedStainsSystem.cs
+++ Content.Shared/Stains/SharedStainsSystem.cs
@@ -7,6 +7,9 @@ public abstract class SharedStainsSystem : EntitySystem
 {
 }
 
+/// <summary>
+/// Do after event for squeezing clothes, removing the solution from <see cref="StainableComponent"/>.
+/// </summary>
 [Serializable, NetSerializable]
 public sealed partial class SqueezeDoAfterEvent : SimpleDoAfterEvent
 {
