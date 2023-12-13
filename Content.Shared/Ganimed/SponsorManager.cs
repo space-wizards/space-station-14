21d0afbb81f5055b35356267bb9fcb4268bbf075
temporaldarkness <94239354+temporaldarkness@users.noreply.github.com>
2023-12-13 23:42:48 +0300

    Update SponsorManager.cs

--- Content.Shared/Ganimed/SponsorManager.cs
+++ Content.Shared/Ganimed/SponsorManager.cs
@@ -9,8 +9,8 @@ public sealed class SponsorManager
 {
     private Dictionary<string, DateTime> sponsors =
 		new Dictionary<string, DateTime>(){
-			{"JustHuman", new DateTime(2023, 12, 12)},
-			{"EugeneOM", new DateTime(2023, 12, 12)},
+			{"JustHuman", new DateTime(2023, 12, 14)},
+			{"EugeneOM", new DateTime(2023, 12, 14)},
 		};
 
     public bool IsSponsor(ICommonSession? session)
