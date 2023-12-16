0e5302cc340764a2ff2bef647830837630420601
whateverusername0 <whateveremail>
2023-12-16 21:24:21 +1000

    fixed n2o decomp

--- Content.Server/Atmos/Reactions/N2ODecompositionReaction.cs
+++ Content.Server/Atmos/Reactions/N2ODecompositionReaction.cs
@@ -13,21 +13,14 @@ public sealed partial class N2ODecompositionReaction : IGasReactionEffect
     public ReactionResult React(GasMixture mixture, IGasMixtureHolder? holder, AtmosphereSystem atmosphereSystem)
     {
         var cacheN2O = mixture.GetMoles(Gas.NitrousOxide);
-        var cacheNitrogen = mixture.GetMoles(Gas.Nitrogen);
-        var cacheOxygen = mixture.GetMoles(Gas.Oxygen);
 
         var burnedFuel = cacheN2O * Atmospherics.N2ODecompositionRate * (mixture.Temperature - Atmospherics.N2ODecompositionMinScaleTemp) * (mixture.Temperature - Atmospherics.N2ODecompositionMaxScaleTemp) / Atmospherics.N2ODecompositionScaleDivisor;
 
-        if (burnedFuel <= 0 || cacheN2O - burnedFuel < 0) return ReactionResult.NoReaction;
+        //if (burnedFuel <= 0 || cacheN2O - burnedFuel < 0) return ReactionResult.NoReaction;
 
-        mixture.AdjustMoles(Gas.NitrousOxide, cacheN2O -= burnedFuel);
-        mixture.AdjustMoles(Gas.Nitrogen, cacheNitrogen += burnedFuel);
-        mixture.AdjustMoles(Gas.Oxygen, cacheOxygen += burnedFuel / 2);
-
-        var heatCap = atmosphereSystem.GetHeatCapacity(mixture);
-        var energyReleased = Atmospherics.N2ODecompositionEnergy * burnedFuel;
-        if (heatCap > Atmospherics.MinimumHeatCapacity)
-            mixture.Temperature = (mixture.Temperature * heatCap + energyReleased) / heatCap;
+        mixture.AdjustMoles(Gas.NitrousOxide, burnedFuel * Atmospherics.N2ODecompositionRate);
+        mixture.AdjustMoles(Gas.Nitrogen, -burnedFuel * Atmospherics.N2ODecompositionRate);
+        mixture.AdjustMoles(Gas.Oxygen, (-burnedFuel / 2) * Atmospherics.N2ODecompositionRate);
 
         return ReactionResult.Reacting;
     }
