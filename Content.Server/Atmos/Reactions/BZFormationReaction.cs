e365b88c062881efaf2f511816127e61fe25f7d5
whateverusername0 <whateveremail>
2023-12-17 01:52:11 +1000

    small formulas refactor (moved numerical multipliers to the right)

--- Content.Server/Atmos/Reactions/BZFormationReaction.cs
+++ Content.Server/Atmos/Reactions/BZFormationReaction.cs
@@ -18,11 +18,11 @@ public sealed partial class BZFormationReaction : IGasReactionEffect
         var volume = mixture.Volume;
         var environmentEfficiency = volume / pressure; // more volume and less pressure gives better rates
         var ratioEfficiency = Math.Min(initN2O / initPlasma, 1); // less n2o than plasma gives lower rates
-        var n2oDecomposeFactor = Math.Max(4 * (initPlasma / initN2O + initPlasma - .75f), 0); // n2o decomposes when there are more than 3 plasma parts per n2o
+        var n2oDecomposeFactor = Math.Max((initPlasma / initN2O + initPlasma - .75f) * 4, 0); // n2o decomposes when there are more than 3 plasma parts per n2o
 
         var n2oRemoved = initN2O * (1 / .4f);
         var plasmaRemoved = initPlasma * (1 / (.8f * (1 - n2oDecomposeFactor)));
-        var bzFormed = Math.Min(Math.Min(.01f * ratioEfficiency * environmentEfficiency, n2oRemoved), plasmaRemoved) / Atmospherics.BZFormationRate;
+        var bzFormed = Math.Min(Math.Min((ratioEfficiency * environmentEfficiency) / 100, n2oRemoved), plasmaRemoved) / Atmospherics.BZFormationRate;
 
         /* If n2o-plasma ratio is less than 1:3 start decomposing n2o.
 	     * Rate of decomposition vs BZ production increases as n2o concentration gets lower
@@ -30,15 +30,15 @@ public sealed partial class BZFormationReaction : IGasReactionEffect
 	     * N2O decomposes with its normal decomposition energy */
         if (n2oDecomposeFactor > 0)
         {
-            var amountDecomposed = .4f * bzFormed * n2oDecomposeFactor;
+            var amountDecomposed = bzFormed * n2oDecomposeFactor * .4f;
             mixture.AdjustMoles(Gas.Nitrogen, -amountDecomposed);
-            mixture.AdjustMoles(Gas.Oxygen, -amountDecomposed * .5f);
+            mixture.AdjustMoles(Gas.Oxygen, -amountDecomposed / 2);
             mixture.AdjustMoles(Gas.NitrousOxide, amountDecomposed * 1.5f);
         }
 
         mixture.AdjustMoles(Gas.BZ, bzFormed * (1 - n2oDecomposeFactor));
-        mixture.AdjustMoles(Gas.NitrousOxide, -(.4f * bzFormed));
-        mixture.AdjustMoles(Gas.Plasma, -(.8f * bzFormed * (1 - n2oDecomposeFactor)));
+        mixture.AdjustMoles(Gas.NitrousOxide, -(bzFormed * .4f));
+        mixture.AdjustMoles(Gas.Plasma, -(bzFormed * (1 - n2oDecomposeFactor) * .8f));
 
         return ReactionResult.Reacting;
     }
