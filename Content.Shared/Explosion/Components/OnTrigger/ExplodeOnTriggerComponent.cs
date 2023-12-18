3ddd8434f458e18fdf4efde322f51da11ad04ef9
Aexxie <codyfox.077@gmail.com>
2023-12-17 23:26:29 -0600

    Update ExplodeOnTriggerComponent.cs

--- Content.Shared/Explosion/Components/OnTrigger/ExplodeOnTriggerComponent.cs
+++ Content.Shared/Explosion/Components/OnTrigger/ExplodeOnTriggerComponent.cs
@@ -3,7 +3,7 @@ namespace Content.Shared.Explosion.Components.OnTrigger;
 /// <summary>
 /// Explode using the entity's <see cref="ExplosiveComponent"/> if Triggered.
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class ExplodeOnTriggerComponent : Component
 {
 }
