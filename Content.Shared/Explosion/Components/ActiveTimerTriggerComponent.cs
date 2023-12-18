4fd39b7ea0f0f7f766ca550771df8cf4bb0e5353
Aexxie <codyfox.077@gmail.com>
2023-12-17 23:25:26 -0600

    Update Content.Shared/Explosion/Components/ActiveTimerTriggerComponent.cs
    Co-authored-by: DrSmugleaf <DrSmugleaf@users.noreply.github.com>

--- Content.Shared/Explosion/Components/ActiveTimerTriggerComponent.cs
+++ Content.Shared/Explosion/Components/ActiveTimerTriggerComponent.cs
@@ -1,11 +1,12 @@
 using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
 
 namespace Content.Shared.Explosion.Components;
 
 /// <summary>
 ///     Component for tracking active trigger timers. A timers can activated by some other component, e.g. <see cref="OnUseTimerTriggerComponent"/>.
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class ActiveTimerTriggerComponent : Component
 {
     [DataField("timeRemaining")] public float TimeRemaining;
