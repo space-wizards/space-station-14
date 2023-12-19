9dfee201a6885ffa99c450cdcc7296f6357a3002
whateverusername0 <whateveremail>
2023-12-19 20:05:25 +1000

    placeholder

---
+++ Content.Server/Atmos/Reactions/MiasmaOxygenReaction.cs
@@ -0,0 +1,16 @@
+using Content.Server.Atmos.EntitySystems;
+using Content.Shared.Atmos;
+using JetBrains.Annotations;
+
+namespace Content.Server.Atmos.Reactions;
+
+[UsedImplicitly]
+public sealed partial class MiasmaOxygenReaction : IGasReactionEffect
+{
+    public ReactionResult React(GasMixture mixture, IGasMixtureHolder? holder, AtmosphereSystem atmosphereSystem, float heatScale)
+    {
+
+
+        return ReactionResult.Reacting;
+    }
+}
