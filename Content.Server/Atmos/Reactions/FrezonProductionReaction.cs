9adf54e5882d0ed5974ef84335b5bb70d866a430
whateverusername0 <whateveremail>
2023-12-19 16:42:57 +1000

    change trit to bz in frezon form reaction

--- Content.Server/Atmos/Reactions/FrezonProductionReaction.cs
+++ Content.Server/Atmos/Reactions/FrezonProductionReaction.cs
@@ -1,11 +1,11 @@
-﻿using Content.Server.Atmos.EntitySystems;
+using Content.Server.Atmos.EntitySystems;
 using Content.Shared.Atmos;
 using JetBrains.Annotations;
 
 namespace Content.Server.Atmos.Reactions;
 
 /// <summary>
-///     Produces frezon from oxygen and tritium, with nitrogen as a catalyst that also acts as a stopper if too much is present.
+///     Produces frezon from oxygen and bz, with nitrogen as a catalyst that also acts as a stopper if too much is present.
 ///     Has a max temperature, but paradoxically gets more efficient the hotter it is.
 /// </summary>
 [UsedImplicitly]
@@ -15,7 +15,7 @@ public sealed partial class FrezonProductionReaction : IGasReactionEffect
     {
         var initialN2 = mixture.GetMoles(Gas.Nitrogen);
         var initialOxy = mixture.GetMoles(Gas.Oxygen);
-        var initialTrit = mixture.GetMoles(Gas.Tritium);
+        var initialBZ = mixture.GetMoles(Gas.BZ);
 
         var efficiency = mixture.Temperature / Atmospherics.FrezonProductionMaxEfficiencyTemperature;
         var loss = 1 - efficiency;
@@ -23,18 +23,18 @@ public sealed partial class FrezonProductionReaction : IGasReactionEffect
         // How much the catalyst (N2) will allow us to produce
         // Less N2 is required the more efficient it is.
         var catalystLimit = initialN2 * (Atmospherics.FrezonProductionNitrogenRatio / efficiency);
-        var oxyLimit = Math.Min(initialOxy, catalystLimit) / Atmospherics.FrezonProductionTritRatio;
+        var oxyLimit = Math.Min(initialOxy, catalystLimit) / Atmospherics.FrezonProductionBZRatio;
 
-        // Amount of tritium & oxygen that are reacting
-        var tritBurned = Math.Min(oxyLimit, initialTrit);
-        var oxyBurned = tritBurned * Atmospherics.FrezonProductionTritRatio;
+        // Amount of bz & oxygen that are reacting
+        var bzBurned = Math.Min(oxyLimit, initialBZ);
+        var oxyBurned = bzBurned * Atmospherics.FrezonProductionBZRatio;
 
         var oxyConversion = oxyBurned / Atmospherics.FrezonProductionConversionRate;
-        var tritConversion = tritBurned / Atmospherics.FrezonProductionConversionRate;
-        var total = oxyConversion + tritConversion;
+        var bzConversion = bzBurned / Atmospherics.FrezonProductionConversionRate;
+        var total = oxyConversion + bzConversion;
 
         mixture.AdjustMoles(Gas.Oxygen, -oxyConversion);
-        mixture.AdjustMoles(Gas.Tritium, -tritConversion);
+        mixture.AdjustMoles(Gas.BZ, -bzConversion);
         mixture.AdjustMoles(Gas.Frezon, total * efficiency);
         mixture.AdjustMoles(Gas.Nitrogen, total * loss);
 
