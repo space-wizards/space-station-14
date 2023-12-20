28d0e2970c3058fbd6b3b56d5bd5b48641664a85
whateverusername0 <whateveremail>
2023-12-20 18:12:30 +1000

    filled it out

--- Content.Server/Atmos/Reactions/MiasmaOxygenReaction.cs
+++ Content.Server/Atmos/Reactions/MiasmaOxygenReaction.cs
@@ -9,7 +9,17 @@ public sealed partial class MiasmaOxygenReaction : IGasReactionEffect
 {
     public ReactionResult React(GasMixture mixture, IGasMixtureHolder? holder, AtmosphereSystem atmosphereSystem, float heatScale)
     {
+        var cacheMiasma = mixture.GetMoles(Gas.Miasma);
 
+        var deltaMoles = cacheMiasma / Atmospherics.MiasmaOxygenReactionRate * 2;
+
+        if (deltaMoles <= 0 || cacheMiasma - deltaMoles < 0)
+            return ReactionResult.NoReaction;
+
+        mixture.AdjustMoles(Gas.Miasma, -deltaMoles);
+        mixture.AdjustMoles(Gas.Oxygen, -deltaMoles);
+        mixture.AdjustMoles(Gas.NitrousOxide, deltaMoles / 2);
+        mixture.AdjustMoles(Gas.WaterVapor, deltaMoles * 1.5f);
 
         return ReactionResult.Reacting;
     }
