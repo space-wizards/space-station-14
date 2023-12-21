750dbc91756b9b1fb1a951924e46d6be62643976
Kara <lunarautomaton6@gmail.com>
2023-12-20 21:37:47 -0600

    Rename Miasma to Ammonia

--- Content.Server/Atmos/Reactions/AmmoniaOxygenReaction.cs
+++ Content.Server/Atmos/Reactions/AmmoniaOxygenReaction.cs
@@ -5,25 +5,25 @@ using JetBrains.Annotations;
 namespace Content.Server.Atmos.Reactions;
 
 [UsedImplicitly]
-public sealed partial class MiasmaOxygenReaction : IGasReactionEffect
+public sealed partial class AmmoniaOxygenReaction : IGasReactionEffect
 {
     public ReactionResult React(GasMixture mixture, IGasMixtureHolder? holder, AtmosphereSystem atmosphereSystem, float heatScale)
     {
-        var nMiasma = mixture.GetMoles(Gas.Miasma);
+        var nAmmonia = mixture.GetMoles(Gas.Ammonia);
         var nOxygen = mixture.GetMoles(Gas.Oxygen);
         var nTotal = mixture.TotalMoles;
 
         // Concentration-dependent reaction rate
-        var fMiasma = nMiasma/nTotal;
+        var fAmmonia = nAmmonia/nTotal;
         var fOxygen = nOxygen/nTotal;
-        var rate = MathF.Pow(fMiasma, 2) * MathF.Pow(fOxygen, 2);
+        var rate = MathF.Pow(fAmmonia, 2) * MathF.Pow(fOxygen, 2);
 
-        var deltaMoles = nMiasma / Atmospherics.MiasmaOxygenReactionRate * 2 * rate;
+        var deltaMoles = nAmmonia / Atmospherics.AmmoniaOxygenReactionRate * 2 * rate;
 
-        if (deltaMoles <= 0 || nMiasma - deltaMoles < 0)
+        if (deltaMoles <= 0 || nAmmonia - deltaMoles < 0)
             return ReactionResult.NoReaction;
 
-        mixture.AdjustMoles(Gas.Miasma, -deltaMoles);
+        mixture.AdjustMoles(Gas.Ammonia, -deltaMoles);
         mixture.AdjustMoles(Gas.Oxygen, -deltaMoles);
         mixture.AdjustMoles(Gas.NitrousOxide, deltaMoles / 2);
         mixture.AdjustMoles(Gas.WaterVapor, deltaMoles * 1.5f);
