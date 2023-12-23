0b0a859adfe161516bbf2f51da3bcd1d2294d35f
PrPleGoo <felix.leeuwen@gmail.com>
2023-12-23 17:23:08 +0100

    toggle health overlay command

--- Content.Client/Commands/ToggleHealthOverlayCommand.cs
+++ Content.Client/Commands/ToggleHealthOverlayCommand.cs
@@ -1,21 +1,20 @@
-﻿using Content.Client.HealthOverlay;
+using Content.Client.HealthOverlay;
 using Robust.Shared.Console;
-using Robust.Shared.GameObjects;
 
 namespace Content.Client.Commands
 {
     public sealed class ToggleHealthOverlayCommand : IConsoleCommand
     {
         public string Command => "togglehealthoverlay";
-        public string Description => "Toggles a health bar above mobs.";
-        public string Help => $"Usage: {Command}";
+        public string Description => Loc.GetString("toggle-health-overlay-command-description");
+        public string Help => Loc.GetString("toggle-health-overlay-command-help", ("command", Command));
 
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             var system = EntitySystem.Get<HealthOverlaySystem>();
             system.Enabled = !system.Enabled;
 
-            shell.WriteLine($"Health overlay system {(system.Enabled ? "enabled" : "disabled")}.");
+            shell.WriteLine(Loc.GetString("toggle-health-overlay-command-notify", ("state", (system.Enabled ? "enabled" : "disabled"))));
         }
     }
 }
---
+++ Resources/Locale/en-US/commands/toggle-health-overlay-command.ftl
@@ -0,0 +1,3 @@
+toggle-health-overlay-command-description = Toggles a health bar above mobs.
+toggle-health-overlay-command-help = Usage: {$command}
+toggle-health-overlay-command-notify = Health overlay system {$state}.
