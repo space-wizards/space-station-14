f06e69d4d7834ef08f5626395ae479a3b7f87b2d
PrPleGoo <felix.leeuwen@gmail.com>
2023-12-23 11:27:12 +0100

    grouping entity menu command

--- Content.Client/Commands/GroupingEntityMenuCommand.cs
+++ Content.Client/Commands/GroupingEntityMenuCommand.cs
@@ -2,17 +2,15 @@ using Content.Client.ContextMenu.UI;
 using Content.Shared.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.Console;
-using Robust.Shared.IoC;
 
 namespace Content.Client.Commands
 {
     public sealed class GroupingEntityMenuCommand : IConsoleCommand
     {
         public string Command => "entitymenug";
+        public string Description => Loc.GetString("grouping-entity-menu-command-description");
+        public string Help => Loc.GetString("grouping-entity-menu-command-help", ("command", Command), ("groupingTypesCount", EntityMenuUIController.GroupingTypesCount));
 
-        public string Description => "Sets the entity menu grouping type.";
-
-        public string Help => $"Usage: entitymenug <0:{EntityMenuUIController.GroupingTypesCount}>";
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             if (args.Length != 1)
@@ -23,13 +21,13 @@ namespace Content.Client.Commands
 
             if (!int.TryParse(args[0], out var id))
             {
-                shell.WriteLine($"{args[0]} is not a valid integer.");
+                shell.WriteError(Loc.GetString("grouping-entity-menu-command-error", ("arg", args[0])));
                 return;
             }
 
-            if (id < 0 ||id > EntityMenuUIController.GroupingTypesCount - 1)
+            if (id < 0 || id > EntityMenuUIController.GroupingTypesCount - 1)
             {
-                shell.WriteLine($"{args[0]} is not a valid integer.");
+                shell.WriteError(Loc.GetString("grouping-entity-menu-command-error", ("arg", args[0])));
                 return;
             }
 
@@ -37,7 +35,7 @@ namespace Content.Client.Commands
             var cvar = CCVars.EntityMenuGroupingType;
 
             configurationManager.SetCVar(cvar, id);
-            shell.WriteLine($"Context Menu Grouping set to type: {configurationManager.GetCVar(cvar)}");
+            shell.WriteLine(Loc.GetString("grouping-entity-menu-command-notify", ("cvar", configurationManager.GetCVar(cvar))));
         }
     }
 }
---
+++ Resources/Locale/en-US/commands/grouping-entity-menu-command.ftl
@@ -0,0 +1,4 @@
+grouping-entity-menu-command-description = Sets the entity menu grouping type.
+grouping-entity-menu-command-help = Usage: {$command} <0:{$groupingTypesCount}>
+grouping-entity-menu-command-error = {$arg} is not a valid integer.
+grouping-entity-menu-command-notify = Context Menu Grouping set to type: {$cvar}
