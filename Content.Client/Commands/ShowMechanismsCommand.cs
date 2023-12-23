fffb578801eb0870573149a93df264bcfa269da3
PrPleGoo <felix.leeuwen@gmail.com>
2023-12-23 17:21:11 +0100

    show mechanisms command

--- Content.Client/Commands/ShowMechanismsCommand.cs
+++ Content.Client/Commands/ShowMechanismsCommand.cs
@@ -1,4 +1,4 @@
-﻿using Content.Shared.Body.Organ;
+using Content.Shared.Body.Organ;
 using Robust.Client.Console;
 using Robust.Client.GameObjects;
 using Robust.Shared.Console;
@@ -7,12 +7,9 @@ namespace Content.Client.Commands
 {
     public sealed class ShowMechanismsCommand : IConsoleCommand
     {
-        public const string CommandName = "showmechanisms";
-
-        // ReSharper disable once StringLiteralTypo
-        public string Command => CommandName;
-        public string Description => "Makes mechanisms visible, even when they shouldn't be.";
-        public string Help => $"{Command}";
+        public string Command => "showmechanisms";
+        public string Description => Loc.GetString("show-mechanisms-command-description");
+        public string Help => Loc.GetString("show-mechanisms-command-help", ("command", Command));
 
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
---
+++ Resources/Locale/en-US/commands/show-mechanisms-command.ftl
@@ -0,0 +1,2 @@
+show-mechanisms-command-description = Makes mechanisms visible, even when they shouldn't be.
+show-mechanisms-command-help = Usage: {$command}
