25bf32c4d5945412eb103ef29edd30f548fa51ab
PrPleGoo <felix.leeuwen@gmail.com>
2023-12-23 17:24:50 +0100

    toggle outline command

--- Content.Client/Commands/ToggleOutlineCommand.cs
+++ Content.Client/Commands/ToggleOutlineCommand.cs
@@ -2,7 +2,6 @@ using Content.Shared.Administration;
 using Content.Shared.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.Console;
-using Robust.Shared.IoC;
 
 namespace Content.Client.Commands
 {
@@ -10,10 +9,8 @@ namespace Content.Client.Commands
     public sealed class ToggleOutlineCommand : IConsoleCommand
     {
         public string Command => "toggleoutline";
-
-        public string Description => "Toggles outline drawing on entities.";
-
-        public string Help => "";
+        public string Description => Loc.GetString("toggle-outline-command-description");
+        public string Help => Loc.GetString("toggle-outline-command-help", ("command", Command));
 
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
@@ -22,7 +19,7 @@ namespace Content.Client.Commands
             var old = configurationManager.GetCVar(cvar);
 
             configurationManager.SetCVar(cvar, !old);
-            shell.WriteLine($"Draw outlines set to: {configurationManager.GetCVar(cvar)}");
+            shell.WriteLine(Loc.GetString("toggle-outline-command-notify", ("state", configurationManager.GetCVar(cvar))));
         }
     }
 }
---
+++ Resources/Locale/en-US/commands/toggle-outline-command.ftl
@@ -0,0 +1,3 @@
+toggle-outline-command-description = Toggles outline drawing on entities.
+toggle-outline-command-help = Usage: {$command}
+toggle-outline-command-notify = Draw outlines set to: {$cvar}
