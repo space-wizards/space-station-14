fb4426a68838bb97f02db8fce40fd18acfe839a0
sampletestboy2 <pashakotik123123123123123@gmail.com>
2023-12-23 23:51:20 +0300

    randomevent + some fixes

---
+++ Content.Server/Imperial/Psychosis/MassPsychosisRuleSystem.cs
@@ -0,0 +1,37 @@
+using Content.Server.GameTicking.Rules.Components;
+using Content.Server.StationEvents.Components;
+using Content.Server.Traits.Assorted;
+using Content.Shared.Mind.Components;
+using Content.Shared.Traits.Assorted;
+using FastAccessors;
+using Robust.Shared.Random;
+
+namespace Content.Server.StationEvents.Events;
+
+public sealed class MassPsychosisRule : StationEventSystem<MassPsychosisRuleComponent>
+{
+    [Dependency] private readonly PsychosisSystem _psychosis = default!;
+
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    protected override void Started(EntityUid uid, MassPsychosisRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+    {
+        base.Started(uid, component, gameRule, args);
+        var query = EntityQueryEnumerator<PsychosisGainComponent>();
+        var list = new List<EntityUid>();
+        while (query.MoveNext(out var uidthing, out var gain))
+        {
+            list.Add(uidthing);
+        }
+        var pickedlist = new List<EntityUid>();
+        for (var str = 0; str < _random.Next(component.From, component.To); str++)
+            pickedlist.Add(_random.PickAndTake(list));
+        foreach (var entuid in pickedlist)
+        {
+            if (TryComp<PsychosisComponent>(entuid, out var psychosis))
+                continue;
+            AddComp<PsychosisComponent>(entuid);
+        }
+    }
+}
+
