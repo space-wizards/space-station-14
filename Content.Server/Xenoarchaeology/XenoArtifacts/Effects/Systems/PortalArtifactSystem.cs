751e0420e96e9c7776014e7bade40b18977990f8
Ed <edwardxperia2000@gmail.com>
2023-12-28 12:29:50 +0300

    portal fix, thx deltanedas

--- Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/PortalArtifactSystem.cs
+++ Content.Server/Xenoarchaeology/XenoArtifacts/Effects/Systems/PortalArtifactSystem.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
 using Content.Server.Xenoarchaeology.XenoArtifacts.Events;
 using Content.Shared.Mind.Components;
@@ -11,6 +10,7 @@ public sealed class PortalArtifactSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly LinkedEntitySystem _link = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
 
     public override void Initialize()
     {
@@ -21,24 +21,23 @@ public sealed class PortalArtifactSystem : EntitySystem
     private void OnActivate(Entity<PortalArtifactComponent> artifact, ref ArtifactActivatedEvent args)
     {
         var map = Transform(artifact).MapID;
-        var firstPortal = Spawn(artifact.Comp.PortalProto, Transform(artifact).MapPosition);
+        var firstPortal = Spawn(artifact.Comp.PortalProto, _transform.GetMapCoordinates(artifact));
 
+        var minds = new List<EntityUid>();
         var mindQuery = EntityQueryEnumerator<MindContainerComponent, TransformComponent>();
-        while (mindQuery.MoveNext(out var uid, out var mind))
+        while (mindQuery.MoveNext(out var uid, out var mc, out var xform))
         {
-            
+            if (mc.HasMind && xform.MapID == map)
+                minds.Add(uid);
         }
 
-        var target = _random.Pick(mindQuery);
-        var secondPortal = Spawn(artifact.Comp.PortalProto, Transform(target.Owner).MapPosition);
+        var target = _random.Pick(minds);
+        var secondPortal = Spawn(artifact.Comp.PortalProto, _transform.GetMapCoordinates(target));
+
+        //Manual position swapping, because the portal that opens doesn't trigger a collision, and doesn't teleport targets the first time.
+        _transform.SetCoordinates(artifact, Transform(secondPortal).Coordinates);
+        _transform.SetCoordinates(target, Transform(firstPortal).Coordinates);
 
         _link.TryLink(firstPortal, secondPortal, true);
-
-        //var query = EntityQueryEnumerator();
-        //while (query.MoveNext(out var uid, out var egg))
-        //{
-        //    Hatch(uid, egg);
-        //}
-        //.Where((uid, mc, xform) => mc.HasMind && xform.MapID == map)
     }
 }
