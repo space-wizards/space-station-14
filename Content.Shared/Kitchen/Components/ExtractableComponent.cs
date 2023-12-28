1fdfe6950b3fffedde1d621707fbd992e8117b73
EmoGarbage404 <retron404@gmail.com>
2023-12-27 21:09:01 -0500

    network that bitch, baby

--- Content.Shared/Kitchen/Components/ExtractableComponent.cs
+++ Content.Shared/Kitchen/Components/ExtractableComponent.cs
@@ -1,11 +1,12 @@
 ﻿using Content.Shared.Chemistry.Components;
+using Robust.Shared.GameStates;
 
 namespace Content.Shared.Kitchen.Components;
 
 /// <summary>
 /// Tag component that denotes an entity as Extractable
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class ExtractableComponent : Component
 {
     [DataField("juiceSolution")]
