9bc11b90c1d01c3602cfec57fb2c405f0d65def2
Ilya246 <ilyukarno@gmail.com>
2023-12-28 20:27:53 +0400

    implement

--- Content.Server/Implants/SubdermalImplantSystem.cs
+++ Content.Server/Implants/SubdermalImplantSystem.cs
@@ -1,6 +1,7 @@
 ﻿using Content.Server.Cuffs;
 using Content.Server.Forensics;
 using Content.Server.Humanoid;
+using Content.Server.Implants.Components;
 using Content.Server.Store.Components;
 using Content.Server.Store.Systems;
 using Content.Shared.Cuffs.Components;
@@ -8,8 +9,16 @@ using Content.Shared.Humanoid;
 using Content.Shared.Implants;
 using Content.Shared.Implants.Components;
 using Content.Shared.Interaction;
+using Content.Shared.Physics;
 using Content.Shared.Popups;
 using Content.Shared.Preferences;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Map;
+using Robust.Shared.Maths;
+using Robust.Shared.Physics;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Random;
+using System.Numerics;
 
 namespace Content.Server.Implants;
 
@@ -17,9 +26,13 @@ public sealed class SubdermalImplantSystem : SharedSubdermalImplantSystem
 {
     [Dependency] private readonly CuffableSystem _cuffable = default!;
     [Dependency] private readonly HumanoidAppearanceSystem _humanoidAppearance = default!;
+    [Dependency] private readonly IMapManager _mapManager = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly MetaDataSystem _metaData = default!;
     [Dependency] private readonly StoreSystem _store = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedTransformSystem _xform = default!;
     [Dependency] private readonly ForensicsSystem _forensicsSystem = default!;
 
     public override void Initialize()
@@ -29,6 +42,7 @@ public sealed class SubdermalImplantSystem : SharedSubdermalImplantSystem
         SubscribeLocalEvent<SubdermalImplantComponent, UseFreedomImplantEvent>(OnFreedomImplant);
         SubscribeLocalEvent<StoreComponent, ImplantRelayEvent<AfterInteractUsingEvent>>(OnStoreRelay);
         SubscribeLocalEvent<SubdermalImplantComponent, ActivateImplantEvent>(OnActivateImplantEvent);
+        SubscribeLocalEvent<SubdermalImplantComponent, UseScramImplantEvent>(OnScramImplant);
         SubscribeLocalEvent<SubdermalImplantComponent, UseDnaScramblerImplantEvent>(OnDnaScramblerImplant);
 
     }
@@ -69,6 +83,53 @@ public sealed class SubdermalImplantSystem : SharedSubdermalImplantSystem
 
     private void OnActivateImplantEvent(EntityUid uid, SubdermalImplantComponent component, ActivateImplantEvent args)
     {
+        args.Handled = true;
+    }
+
+    private void OnScramImplant(EntityUid uid, SubdermalImplantComponent component, UseScramImplantEvent args)
+    {
+        if (component.ImplantedEntity is not { } ent)
+            return;
+
+        if (!TryComp<ScramImplantComponent>(uid, out var implant))
+            return;
+
+        var xform = Transform(ent);
+        var entityCoords = xform.Coordinates.ToMap(EntityManager);
+
+        // try to find a valid position to teleport to, teleport to whatever works if we can't
+        var targetCoords = new MapCoordinates();
+        for (var i = 0; i < implant.TeleportAttempts; i++)
+        {
+            targetCoords = entityCoords.Offset(_random.NextAngle().ToVec() * _random.NextFloat(implant.TeleportRadius));
+
+            // prefer teleporting to grids
+            if (!_mapManager.TryFindGridAt(targetCoords, out var gridUid, out var grid))
+                continue;
+
+            // the implant user probably does not want to be in your walls
+            var physQuery = GetEntityQuery<PhysicsComponent>();
+            var valid = true;
+            foreach (var entity in grid.GetAnchoredEntities(targetCoords))
+            {
+                if (!physQuery.TryGetComponent(entity, out var body))
+                    continue;
+
+                if (body.BodyType != BodyType.Static ||
+                    !body.Hard ||
+                    (body.CollisionLayer & (int) CollisionGroup.Impassable) == 0)
+                    continue;
+
+                valid = false;
+                break;
+            }
+            if (!valid)
+                continue;
+            break;
+        }
+        _xform.SetWorldPosition(ent, targetCoords.Position);
+        _audio.PlayPvs(component.TeleportSound, ent);
+
         args.Handled = true;
     }
 
