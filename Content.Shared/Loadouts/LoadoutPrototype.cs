31598886c0c3ff020f11f2609c5839a5724380af
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-12-31 03:57:12 -0800

    remove datafield names on loadoutPrototype

--- Content.Shared/Loadouts/LoadoutPrototype.cs
+++ Content.Shared/Loadouts/LoadoutPrototype.cs
@@ -1,7 +1,10 @@
+using Content.Shared.Humanoid.Prototypes;
+using Content.Shared.Roles;
 using Content.Shared.Whitelist;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Set;
 
 namespace Content.Shared.Loadouts;
 
@@ -12,64 +15,64 @@ public sealed class LoadoutPrototype : IPrototype
     public string ID { get; } = default!;
 
     /// <summary>
-    ///     Which tab category to put this under.
+    ///     Which tab category to put this under
     /// </summary>
-    [DataField("category", customTypeSerializer:typeof(PrototypeIdSerializer<LoadoutCategoryPrototype>))]
+    [DataField(customTypeSerializer:typeof(PrototypeIdSerializer<LoadoutCategoryPrototype>))]
     public string Category = "Uncategorized";
 
     /// <summary>
-    ///     The item to give.
+    ///     The item to give
     /// </summary>
-    [DataField("items", customTypeSerializer: typeof(PrototypeIdListSerializer<EntityPrototype>), required: true)]
+    [DataField(customTypeSerializer: typeof(PrototypeIdListSerializer<EntityPrototype>), required: true)]
     public List<string> Items = new();
 
 
     /// <summary>
-    ///     The point cost of this loadout.
+    ///     The point cost of this loadout
     /// </summary>
-    [DataField("cost")]
+    [DataField]
     public int Cost = 1;
 
     /// <summary>
     ///     Should this item override other items in the same slot?
     /// </summary>
-    [DataField("exclusive")]
+    [DataField]
     public bool Exclusive;
 
 
     /// <summary>
-    ///     Don't apply this loadout to entities this whitelist IS NOT valid for.
+    ///     Don't apply this loadout to entities this whitelist IS NOT valid for
     /// </summary>
-    [DataField("entityWhitelist")]
+    [DataField]
     public EntityWhitelist? EntityWhitelist;
 
     /// <summary>
-    ///     Don't apply this loadout to entities this whitelist IS valid for. (hence, a blacklist)
+    ///     Don't apply this loadout to entities this whitelist IS valid for (hence, a blacklist)
     /// </summary>
-    [DataField("entityBlacklist")]
+    [DataField]
     public EntityWhitelist? EntityBlacklist;
 
     /// <summary>
-    ///     Need one of these jobs to give loadout.
+    ///     Need one of these jobs to give loadout
     /// </summary>
-    [DataField("jobWhitelist")]
+    [DataField(customTypeSerializer: typeof(PrototypeIdListSerializer<JobPrototype>))]
     public List<string>? JobWhitelist;
 
     /// <summary>
-    ///     Need none of these jobs to give loadout.
+    ///     Need none of these jobs to give loadout
     /// </summary>
-    [DataField("jobBlacklist")]
+    [DataField(customTypeSerializer: typeof(PrototypeIdListSerializer<JobPrototype>))]
     public List<string>? JobBlacklist;
 
     /// <summary>
-    ///     Don't apply this loadout to entities this whitelist IS NOT valid for.
+    ///     Don't apply this loadout to entities this whitelist IS NOT valid for
     /// </summary>
-    [DataField("speciesWhitelist")]
+    [DataField(customTypeSerializer: typeof(PrototypeIdListSerializer<SpeciesPrototype>))]
     public List<string>? SpeciesWhitelist;
 
     /// <summary>
-    ///     Don't apply this loadout to entities this whitelist IS valid for. (hence, a blacklist)
+    ///     Don't apply this loadout to entities this whitelist IS valid for
     /// </summary>
-    [DataField("speciesBlacklist")]
+    [DataField(customTypeSerializer: typeof(PrototypeIdListSerializer<SpeciesPrototype>))]
     public List<string>? SpeciesBlacklist;
 }
