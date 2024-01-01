e8f2c28164dff1d08800d9e61533637e9d88e897
ElectroJr <leonsfriedrich@gmail.com>
2023-12-31 22:39:55 -0500

    Add CycleTick() benchmark

--- Content.Benchmarks/PvsBenchmark.cs
+++ Content.Benchmarks/PvsBenchmark.cs
@@ -39,6 +39,9 @@ public class PvsBenchmark
     private MapId _mapId = new(10);
     private ICommonSession[] _players = default!;
     private EntityCoordinates[] _spawns = default!;
+    public int _cycleOffset = 0;
+    private SharedTransformSystem _sys = default!;
+    private EntityCoordinates[] _locations = default!;
 
     [GlobalSetup]
     public void Setup()
@@ -52,6 +55,7 @@ public class PvsBenchmark
         _entMan = _pair.Server.ResolveDependency<IEntityManager>();
         _pair.Server.CfgMan.SetCVar(CVars.NetPVS, true);
         _pair.Server.CfgMan.SetCVar(CVars.ThreadParallelCount, 0);
+        _sys = _entMan.System<SharedTransformSystem>();
 
         // Spawn the map
         _pair.Server.ResolveDependency<IRobustRandom>().SetSeed(42);
@@ -83,15 +87,16 @@ public class PvsBenchmark
             }
         }).Wait();
 
-        PvsTick();
-
         // Repeatedly move players around so that they "explore" the map and see lots of entities.
         // This will populate their PVS data with out-of-view entities.
         var rng = new Random(42);
         ShufflePlayers(rng, 100);
 
-        PvsTick();
-        PvsTick();
+        _pair.Server.PvsTick(_players);
+        _pair.Server.PvsTick(_players);
+
+        var ents = _players.Select(x => x.AttachedEntity!.Value).ToArray();
+        _locations = ents.Select(x => _entMan.GetComponent<TransformComponent>(x).Coordinates).ToArray();
     }
 
     private void ShufflePlayers(Random rng, int count)
@@ -105,7 +110,7 @@ public class PvsBenchmark
 
     private void ShufflePlayers(Random rng)
     {
-        PvsTick();
+        _pair.Server.PvsTick(_players);
 
         var ents = _players.Select(x => x.AttachedEntity!.Value).ToArray();
         var locations = ents.Select(x => _entMan.GetComponent<TransformComponent>(x).Coordinates).ToArray();
@@ -119,29 +124,47 @@ public class PvsBenchmark
             (locations[k], locations[n]) = (locations[n], locations[k]);
         }
 
-        var sys = _entMan.System<SharedTransformSystem>();
         _pair.Server.WaitPost(() =>
         {
             for (var i = 0; i < PlayerCount; i++)
             {
-                sys.SetCoordinates(ents[i], locations[i]);
+                _sys.SetCoordinates(ents[i], locations[i]);
             }
         }).Wait();
 
-        PvsTick();
+        _pair.Server.PvsTick(_players);
     }
 
     /// <summary>
     /// Basic benchmark for PVS in a static situation where nothing moves or gets dirtied..
-    /// This effectively provides a lower bound on "real" pvs tick time, as itt is missing:
+    /// This effectively provides a lower bound on "real" pvs tick time, as it is missing:
     /// - PVS chunks getting dirtied and needing to be rebuilt
     /// - Fetching component states for dirty components
     /// - Compressing & sending network messages
     /// - Sending PVS leave messages
     /// </summary>
     [Benchmark]
-    public void PvsTick()
+    public void StaticTick()
     {
+        _pair.Server.PvsTick(_players);
+    }
+
+    /// <summary>
+    /// Basic benchmark for PVS in a situation where players are teleporting all over the place. This isn't very
+    /// realistic, but unlike <see cref="StaticTick"/> this will actually also measure the speed of processing dirty
+    /// chunks and sending PVS leave messages.
+    /// </summary>
+    [Benchmark]
+    public void CycleTick()
+    {
+        _cycleOffset = (_cycleOffset + 1) % _players.Length;
+        _pair.Server.WaitPost(() =>
+        {
+            for (var i = 0; i < PlayerCount; i++)
+            {
+                _sys.SetCoordinates(_players[i].AttachedEntity!.Value, _locations[(i + _cycleOffset) % _players.Length]);
+            }
+        }).Wait();
         _pair.Server.PvsTick(_players);
     }
 
