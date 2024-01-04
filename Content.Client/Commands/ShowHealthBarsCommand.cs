32080b3285db8a1b8e5561047d6f9418fc0faae9
PrPleGoo <felix.leeuwen@gmail.com>
2024-01-04 11:43:32 +0100

    update command

--- Content.Client/Commands/ShowHealthBarsCommand.cs
+++ Content.Client/Commands/ShowHealthBarsCommand.cs
@@ -5,28 +5,28 @@ using System.Linq;
 
 namespace Content.Client.Commands;
 
-public sealed class ShowHealthBarsCommand : IConsoleCommand
+public sealed class ShowHealthBarsCommand : LocalizedCommands
 {
     [Dependency] private readonly IPlayerManager _playerManager = default!;
     [Dependency] private readonly IEntityManager _entityManager = default!;
 
-    public string Command => "showhealthbars";
-    public string Description => "Toggles health bars above mobs.";
-    public string Help => $"Usage: {Command} [<DamageContainerId>]";
+    public override string Command => "showhealthbars";
 
-    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    public override string Help => LocalizationManager.GetString($"cmd-{Command}-help", ("command", Command));
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         var player = _playerManager.LocalSession;
         if (player == null)
         {
-            shell.WriteLine("You aren't a player.");
+            shell.WriteError(LocalizationManager.GetString($"cmd-{Command}-error-not-player"));
             return;
         }
 
         var playerEntity = player?.AttachedEntity;
         if (!playerEntity.HasValue)
         {
-            shell.WriteLine("You do not have an attached entity.");
+            shell.WriteError(LocalizationManager.GetString($"cmd-{Command}-error-no-entity"));
             return;
         }
 
@@ -40,13 +40,13 @@ public sealed class ShowHealthBarsCommand : IConsoleCommand
 
             _entityManager.AddComponent(playerEntity.Value, showHealthBarsComponent, true);
 
-            shell.WriteLine($"Enabled health overlay for DamageContainers: {string.Join(", ", args)}.");
+            shell.WriteLine(LocalizationManager.GetString($"cmd-{Command}-notify-enabled", ("args", string.Join(", ", args))));
             return;
         }
         else
         {
             _entityManager.RemoveComponentDeferred<ShowHealthBarsComponent>(playerEntity.Value);
-            shell.WriteLine("Disabled health overlay.");
+            shell.WriteLine(LocalizationManager.GetString($"cmd-{Command}-notify-disabled"));
         }
 
         return;
