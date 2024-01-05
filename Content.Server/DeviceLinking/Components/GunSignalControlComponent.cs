4a320d6bbe5fe81747db8a3fcc35901ca812eea8
Ed <edwardxperia2000@gmail.com>
2024-01-06 01:38:20 +0300

    make auto-fire, but its broken

--- Content.Server/DeviceLinking/Components/GunSignalControlComponent.cs
+++ Content.Server/DeviceLinking/Components/GunSignalControlComponent.cs
@@ -1,12 +1,30 @@
+using Content.Server.DeviceLinking.Systems;
 using Content.Shared.DeviceLinking;
 using Robust.Shared.Prototypes;
 
-namespace Content.Server.DeviceLinking.Components
+namespace Content.Server.DeviceLinking.Components;
+
+/// <summary>
+/// A system that allows you to fire GunComponent + AmmoProvider by receiving signals from DeviceLinking
+/// </summary>
+[RegisterComponent, Access(typeof(GunSignalControlSystem))]
+public sealed partial class GunSignalControlComponent : Component
 {
-    [RegisterComponent]
-    public sealed partial class GunSignalControlComponent : Component
-    {
-        [DataField]
-        public ProtoId<SinkPortPrototype> ShootPort = "Trigger";
-    }
+    /// <summary>
+    /// for automatic weapons. Determines if the gun is attempting to fire without stopping
+    /// </summary>
+    [DataField]
+    public bool Enabled = false;
+
+    [DataField]
+    public float AccumulatedFrame = 0f;
+
+    [DataField]
+    public ProtoId<SinkPortPrototype> TriggerPort = "Trigger";
+
+    [DataField]
+    public ProtoId<SinkPortPrototype> OnPort = "On";
+
+    [DataField]
+    public ProtoId<SinkPortPrototype> OffPort = "Off";
 }
--- Content.Server/DeviceLinking/Systems/GunSignalControlSystem.cs
+++ Content.Server/DeviceLinking/Systems/GunSignalControlSystem.cs
@@ -25,15 +25,59 @@ public sealed partial class GunSignalControlSystem : EntitySystem
 
     private void OnInit(Entity<GunSignalControlComponent> gunControl, ref ComponentInit args)
     {
-        _signalSystem.EnsureSinkPorts(gunControl, gunControl.Comp.ShootPort);
+        _signalSystem.EnsureSinkPorts(gunControl, gunControl.Comp.TriggerPort, gunControl.Comp.OnPort, gunControl.Comp.OffPort);
     }
 
     private void OnSignalReceived(Entity<GunSignalControlComponent> gunControl, ref SignalReceivedEvent args)
     {
-        if (!TryComp<GunComponent>(gunControl, out var gun))
+        if (args.Port == gunControl.Comp.TriggerPort)
+        {
+            Fire(gunControl);
+        }
+        if (args.Port == gunControl.Comp.OnPort)
+        {
+            SwitchOn(gunControl);
+        }
+        if (args.Port == gunControl.Comp.OffPort)
+        {
+            SwitchOff(gunControl);
+        }
+    }
+    private void Fire(EntityUid uid)
+    {
+        if (!TryComp<GunComponent>(uid, out var gun))
             return;
 
-        var targetPos = new EntityCoordinates(gunControl, new Vector2(0, -1));
-        _gun.AttemptShoot(null, gunControl, gun, targetPos, false);
+        var targetPos = new EntityCoordinates(uid, new Vector2(0, -1));
+        _gun.AttemptShoot(null, uid, gun, targetPos, false);
+    }
+
+    private void SwitchOn(Entity<GunSignalControlComponent> gunControl)
+    {
+        gunControl.Comp.Enabled = true;
+    }
+
+    private void SwitchOff(Entity<GunSignalControlComponent> gunControl)
+    {
+        gunControl.Comp.Enabled = false;
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<GunSignalControlComponent, GunComponent>();
+        while (query.MoveNext(out var uid, out var gunControl, out var gun))
+        {
+            if (!gunControl.Enabled) continue;
+
+            gunControl.AccumulatedFrame += frameTime;
+
+            if (gunControl.AccumulatedFrame > gun.FireRate)
+            {
+                Fire(uid);
+                gunControl.AccumulatedFrame = 0f;
+            }
+        }
     }
 }
