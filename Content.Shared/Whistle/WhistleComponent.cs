466b44849a7ce5cef56b8928e1aaa98e96a97f39
Fahasor <rad.nikita@list.ru>
2024-01-05 23:40:34 +0300

    WhistleComponent now use file-scoped namespace

--- Content.Shared/Whistle/WhistleComponent.cs
+++ Content.Shared/Whistle/WhistleComponent.cs
@@ -3,24 +3,23 @@ using Content.Shared.Humanoid;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Robust.Shared.Prototypes;
 
-namespace Content.Shared.Whistle
+namespace Content.Shared.Whistle;
+
+/// <summary>
+/// Spawn attached entity for entities in range with <see cref="HumanoidAppearanceComponent"/>.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class WhistleComponent : Component
 {
     /// <summary>
-    /// Spawn attached entity for entities in range with <see cref="HumanoidAppearanceComponent"/>.
+    /// Entity prototype to spawn
     /// </summary>
-    [RegisterComponent, NetworkedComponent]
-    public sealed partial class WhistleComponent : Component
-    {
-        /// <summary>
-        /// Entity prototype to spawn
-        /// </summary>
-        [DataField("effect", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-        public string? Effect = "WhistleExclamation";
+    [DataField("effect", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string? Effect = "WhistleExclamation";
 
-        /// <summary>
-        /// Range value.
-        /// </summary>
-        [DataField("distance")]
-        public float Distance = 0;
-    }
+    /// <summary>
+    /// Range value.
+    /// </summary>
+    [DataField("distance")]
+    public float Distance = 0;
 }
