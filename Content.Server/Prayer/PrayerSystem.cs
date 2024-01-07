dd9c31ea5684fddab5bc55ecf2c66127347ca481
Avery Dobbins <avery.dobbins@gmail.com>
2024-01-07 09:19:47 -0500

    Address comments

--- Content.Server/Prayer/PrayerSystem.cs
+++ Content.Server/Prayer/PrayerSystem.cs
@@ -115,7 +115,7 @@ public sealed class PrayerSystem : EntitySystem
         /// Admin sound
         if (_gameTiming.CurTime > comp.NextRing) {
             _audioSystem.PlayGlobal(comp.SoundForGod,
-                Filter.Empty().AddPlayers(_adminManager.ActiveAdmins), false, AudioParams.Default.WithVolume(comp.VolumeForGod));
+                Filter.Empty().AddPlayers(_adminManager.ActiveAdmins), false);
             comp.NextRing = _gameTiming.CurTime + comp.RingPeriod;
         }
         
--- Content.Shared/Prayer/PrayableComponent.cs
+++ Content.Shared/Prayer/PrayableComponent.cs
@@ -1,5 +1,6 @@
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Robust.Shared.Utility;
 
 namespace Content.Shared.Prayer;
@@ -13,63 +14,59 @@ public sealed partial class PrayableComponent : Component
     /// <summary>
     /// If bible users are only allowed to use this prayable entity
     /// </summary>
-    [DataField("bibleUserOnly")]
+    [DataField]
     [ViewVariables(VVAccess.ReadWrite)]
     public bool BibleUserOnly;
 
     /// <summary>
     /// Message given to user to notify them a message was sent
     /// </summary>
-    [DataField("sentMessage")]
+    [DataField]
     [ViewVariables(VVAccess.ReadWrite)]
     public string SentMessage = "prayer-popup-notify-pray-sent";
 
     /// <summary>
     /// Prefix used in the notification to admins
     /// </summary>
-    [DataField("notifiactionPrefix")]
+    [DataField]
     [ViewVariables(VVAccess.ReadWrite)]
     public string NotificationPrefix = "prayer-chat-notify-pray";
 
     /// <summary>
     /// The sound played for admins when a prayer is sent
-	/// </summary>
-    [DataField("soundForGod")]
+    /// </summary>
+    [DataField]
     [ViewVariables(VVAccess.ReadWrite)]
-    public SoundSpecifier SoundForGod = new SoundPathSpecifier("/Audio/Effects/plehnimda.ogg");
+    public SoundSpecifier SoundForGod = new SoundPathSpecifier("/Audio/Effects/plehnimda.ogg")
+    {
+        Params = AudioParams.Default.WithVolume(5f)
+    };
 
     /// <summary>
-    /// The volume of the sound played for admins when a prayer is sent
-	/// </summary>
-    [DataField("volumeForGod")]
-    [ViewVariables(VVAccess.ReadWrite)]
-    public int VolumeForGod = 0;
-	
-    /// <summary>
     /// The last time a sound was played for admins
-	/// </summary>
-    [DataField("nextRing")]
+    /// </summary>
+    [DataField(customTypeSerializer:typeof(TimeOffsetSerializer))]
     [ViewVariables(VVAccess.ReadOnly)]
     public TimeSpan NextRing;
 
     /// <summary>
     /// The cooldown on playing sounds for admins
-	/// </summary>
-    [DataField("ringPeriod")]
+    /// </summary>
+    [DataField]
     [ViewVariables(VVAccess.ReadWrite)]
     public TimeSpan RingPeriod = TimeSpan.FromSeconds(10f);
 
     /// <summary>
     /// Used in window title and context menu
     /// </summary>
-    [DataField("verb")]
+    [DataField]
     [ViewVariables(VVAccess.ReadOnly)]
     public string Verb = "prayer-verbs-pray";
 
     /// <summary>
     /// Context menu image
     /// </summary>
-    [DataField("verbImage")]
+    [DataField]
     [ViewVariables(VVAccess.ReadOnly)]
     public SpriteSpecifier? VerbImage = new SpriteSpecifier.Texture(new ("/Textures/Interface/pray.svg.png"));
 }
