0f4dba0ae45088ce2facd8756545a38999ebb938
emisse <99158783+Emisse@users.noreply.github.com>
2024-01-08 15:56:59 -0700

    worst test ive ever seen in my life

--- Content.IntegrationTests/Tests/Interaction/InteractionTest.Constants.cs
+++ Content.IntegrationTests/Tests/Interaction/InteractionTest.Constants.cs
@@ -28,11 +28,8 @@ public abstract partial class InteractionTest
 
     // Parts
     protected const string Bin1 = "MatterBinStockPart";
-    protected const string Bin4 = "BluespaceMatterBinStockPart";
     protected const string Cap1 = "CapacitorStockPart";
-    protected const string Cap4 = "QuadraticCapacitorStockPart";
     protected const string Manipulator1 = "MicroManipulatorStockPart";
-    protected const string Manipulator4 = "FemtoManipulatorStockPart";
     protected const string Battery1 = "PowerCellSmall";
     protected const string Battery4 = "PowerCellHyper";
 }
