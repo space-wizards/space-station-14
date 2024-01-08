c724df6d1df71a4a461f75e02d6fe579cc8e5b87
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2024-01-08 13:43:38 +0100

    Update NuGet packages and fix code for changes.
    Notable:
    
    Changes to ILVerify.
    Npgsql doesn't need hacks for inet anymore, now we need hacks to make the old code work with this new reality.
    NUnit's analyzers are already complaining and I didn't even update it to 4.x yet.
    TerraFX changed to GetLastSystemError so error handling had to be changed.
    Buncha APIs have more NRT annotations.

--- Content.Server.Database/Model.cs
+++ Content.Server.Database/Model.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Text.Json;
 using Content.Shared.Database;
 using Microsoft.EntityFrameworkCore;
+using NpgsqlTypes;
 
 namespace Content.Server.Database
 {
@@ -545,7 +546,7 @@ namespace Content.Server.Database
     {
         int Id { get; set; }
         Guid? PlayerUserId { get; set; }
-        (IPAddress, int)? Address { get; set; }
+        NpgsqlInet? Address { get; set; }
         byte[]? HWId { get; set; }
         DateTime BanTime { get; set; }
         DateTime? ExpirationTime { get; set; }
@@ -613,8 +614,7 @@ namespace Content.Server.Database
         /// <summary>
         /// CIDR IP address range of the ban. The whole range can match the ban.
         /// </summary>
-        [Column(TypeName = "inet")]
-        public (IPAddress, int)? Address { get; set; }
+        public NpgsqlInet? Address { get; set; }
 
         /// <summary>
         /// Hardware ID of the banned player.
@@ -803,7 +803,7 @@ namespace Content.Server.Database
         public Round? Round { get; set; }
         public Guid? PlayerUserId { get; set; }
         [Required] public TimeSpan PlaytimeAtNote { get; set; }
-        [Column(TypeName = "inet")] public (IPAddress, int)? Address { get; set; }
+        public NpgsqlInet? Address { get; set; }
         public byte[]? HWId { get; set; }
 
         public DateTime BanTime { get; set; }
