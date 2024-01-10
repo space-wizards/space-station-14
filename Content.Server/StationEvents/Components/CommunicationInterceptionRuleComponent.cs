a93825d50119de1ef9a007d573eedaf037580480
Vasilis The Pikachu <vasilis@pikachu.systems>
2024-01-10 23:23:31 +0100

    Add new game rule

---
+++ Content.Server/StationEvents/Components/CommunicationInterceptionRuleComponent.cs
@@ -0,0 +1,14 @@
+﻿using Content.Server.StationEvents.Events;
+using Content.Server.AlertLevel;
+
+namespace Content.Server.StationEvents.Components;
+
+[RegisterComponent, Access(typeof(CommunicationInterceptionRule))]
+public sealed partial class CommunicationInterceptionRuleComponent : Component
+{
+    /// <summary>
+    /// Alert level to set the station to when the event starts.
+    /// </summary>
+
+    public readonly string AlertLevel = "blue";
+}
