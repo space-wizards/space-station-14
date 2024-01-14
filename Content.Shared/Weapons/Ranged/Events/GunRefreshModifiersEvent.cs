2c4d740835a0c3c50fe8ba9765b1385347037c16
DrSmugleaf <drsmugleaf@gmail.com>
2024-01-14 03:39:26 -0800

    Add docs

--- Content.Shared/Weapons/Ranged/Events/GunRefreshModifiersEvent.cs
+++ Content.Shared/Weapons/Ranged/Events/GunRefreshModifiersEvent.cs
@@ -1,7 +1,13 @@
-﻿using Robust.Shared.Audio;
+﻿using Content.Shared.Weapons.Ranged.Components;
+using Content.Shared.Weapons.Ranged.Systems;
+using Robust.Shared.Audio;
 
 namespace Content.Shared.Weapons.Ranged.Events;
 
+/// <summary>
+///     Raised directed on the gun entity when <see cref="SharedGunSystem.RefreshModifiers"/>
+///     is called, to update the values of <see cref="GunComponent"/> from other systems.
+/// </summary>
 [ByRefEvent]
 public record struct GunRefreshModifiersEvent(
     SoundSpecifier? SoundGunshot,
