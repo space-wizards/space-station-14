b912eb105d98e292fd10171887815c16121bb516
deltanedas <@deltanedas:kde.org>
2024-01-14 13:39:05 +0000

    vv

--- Content.Shared/CriminalRecords/CriminalRecord.cs
+++ Content.Shared/CriminalRecords/CriminalRecord.cs
@@ -13,17 +13,20 @@ public sealed record CriminalRecord
     /// <summary>
     /// Status of the person (None, Wanted, Detained).
     /// </summary>
+    [DataField]
     public SecurityStatus Status = SecurityStatus.None;
 
     /// <summary>
     /// Reason of the current status.
     /// </summary>
+    [DataField]
     public string Reason = string.Empty;
 
     /// <summary>
     /// Criminal history of the person.
     /// This should have charges and time served added after someone is detained.
     /// </summary>
+    [DataField]
     public List<CrimeHistory> History = new();
 }
 
