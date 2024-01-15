03faad52be04f3662282077ea086d85e9e9c103d
chromiumboy <chromium.boy@gmail.com>
2024-01-15 00:38:38 -0600

    Moved input context definition to content

--- Content.Client/RCD/RCDPlacementSystem.cs
+++ Content.Client/RCD/RCDPlacementSystem.cs
@@ -3,6 +3,7 @@ using Content.Shared.RCD.Components;
 using Robust.Client.GameObjects;
 using Robust.Client.Input;
 using Robust.Client.Player;
+using Robust.Shared.Input;
 
 namespace Content.Client.RCD;
 
@@ -12,17 +13,25 @@ public sealed class RCDPlacementSystem : EntitySystem
     [Dependency] private readonly IEntitySystemManager _entitySystemManager = default!;
     [Dependency] private readonly IPlayerManager _player = default!;
 
-    bool _rotationKeyDown = false;
-
     public override void Initialize()
     {
         base.Initialize();
+
+        InitializeNewInputContext();
+    }
+
+    private void InitializeNewInputContext()
+    {
+        var human = _inputManager.Contexts.GetContext("human");
+        var rcd = _inputManager.Contexts.New("rcd", human);
+        rcd.AddFunction(EngineKeyFunctions.EditorRotateObject);
     }
 
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
 
+        // Determine if player is carrying an RCD in their active hand
         var player = _player.LocalSession?.AttachedEntity;
 
         if (!TryComp<HandsComponent>(player, out var hands))
@@ -35,10 +44,13 @@ public sealed class RCDPlacementSystem : EntitySystem
 
         var hasRCD = HasComp<RCDComponent>(uid);
 
+        // Update the input context
         if (hasRCD && _inputManager.Contexts.ActiveContext != _inputManager.Contexts.GetContext("rcd"))
             _inputManager.Contexts.SetActiveContext("rcd");
 
         else if (!hasRCD && _inputManager.Contexts.ActiveContext == _inputManager.Contexts.GetContext("rcd"))
             _entitySystemManager.GetEntitySystem<InputSystem>().SetEntityContextActive();
+
+        // Update construction ghost
     }
 }
