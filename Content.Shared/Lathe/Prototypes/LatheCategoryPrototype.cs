4572b5f68b384009fa34502c44983123ec586aa9
EmoGarbage404 <retron404@gmail.com>
2024-01-18 17:25:43 -0500

    Lathe Categories

---
+++ Content.Shared/Lathe/Prototypes/LatheCategoryPrototype.cs
@@ -0,0 +1,21 @@
+using Content.Shared.Research.Prototypes;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Lathe.Prototypes;
+
+/// <summary>
+/// This is a prototype for a category for <see cref="LatheRecipePrototype"/>
+/// </summary>
+[Prototype]
+public sealed partial class LatheCategoryPrototype : IPrototype
+{
+    /// <inheritdoc/>
+    [IdDataField]
+    public string ID { get; } = default!;
+
+    /// <summary>
+    /// A localized string used in the UI
+    /// </summary>
+    [DataField]
+    public LocId Name;
+}
