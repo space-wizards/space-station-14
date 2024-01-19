bc215f804f75c7e479e93dedafb66dd99d44fc62
Tayrtahn <tayrtahn@gmail.com>
2024-01-19 10:57:08 -0500

    Documentation good.

--- Content.Server/Sound/Components/SpamEmitSoundComponent.cs
+++ Content.Server/Sound/Components/SpamEmitSoundComponent.cs
@@ -11,12 +11,23 @@ namespace Content.Server.Sound.Components
         [DataField]
         public float Accumulator = 0f;
 
+        /// <summary>
+        /// Time in seconds between each roll.
+        /// </summary>
         [DataField]
         public float RollInterval = 2f;
 
+        /// <summary>
+        /// The maximum amount of extra time (in seconds) that can be
+        /// randomly added to the RollInterval after each roll.
+        /// </summary>
         [DataField]
         public float MaxExtraInterval = 2f;
 
+        /// <summary>
+        /// Chance that the sound will play on each roll.
+        /// 0 = never, 1 = always
+        /// </summary>
         [DataField]
         public float PlayChance = 0.5f;
 
