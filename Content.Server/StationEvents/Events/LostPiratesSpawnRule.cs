79d764de83725f577d64a511fb33d5634b2f1d1f
Agoichi <agoichim@gmail.com>
2024-01-21 20:58:23 +0200

    Update LostPiratesSpawnRule.cs

--- Content.Server/StationEvents/Events/LostPiratesSpawnRule.cs
+++ Content.Server/StationEvents/Events/LostPiratesSpawnRule.cs
@@ -19,11 +19,6 @@ public sealed class LostPiratesSpawnRule : StationEventSystem<LostPiratesSpawnRu
     protected override void Started(EntityUid uid, LostPiratesSpawnRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
         base.Started(uid, component, gameRule, args);
-        if (!_piratesRuleSystem.CheckLoneOpsSpawn())
-        {
-            ForceEndSelf(uid, gameRule);
-            return;
-        }
 
         var shuttleMap = _mapManager.CreateMap();
         var options = new MapLoadOptions
