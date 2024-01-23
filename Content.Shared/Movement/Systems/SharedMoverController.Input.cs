3918d5645f561a5641cd10aa5931dc22330c3e19
Tayrtahn <tayrtahn@gmail.com>
2024-01-23 14:43:21 -0500

    Suggested changes

--- Content.Shared/Movement/Systems/SharedMoverController.Input.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.Input.cs
@@ -626,7 +626,7 @@ namespace Content.Shared.Movement.Systems
         Left = 4,
         Right = 8,
         Walk = 16,
-        AllDirections = Up | Down | Left | Right,
+        AnyDirection = Up | Down | Left | Right,
     }
 
     [Flags]
