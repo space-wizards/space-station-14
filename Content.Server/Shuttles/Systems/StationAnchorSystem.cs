eae1861e58f3eb6f088584f72395b15533f3aacd
juliangiebel <juliangiebel@live.de>
2024-01-25 18:11:20 +0100

    Work on abstracting out chargeup functionality/ui from grav gen

---
+++ Content.Server/Shuttles/Systems/StationAnchorSystem.cs
@@ -0,0 +1,54 @@
+﻿using Content.Server.Shuttles.Components;
+using Content.Shared.Verbs;
+using Robust.Shared.Utility;
+
+namespace Content.Server.Shuttles.Systems;
+
+public sealed class StationAnchorSystem : EntitySystem
+{
+    [Dependency] private readonly ShuttleSystem _shuttleSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<StationAnchorComponent, AnchorStateChangedEvent>(OnAnchorStationChange);
+        SubscribeLocalEvent<StationAnchorComponent, GetVerbsEvent<Verb>>(OnGetVerbs);
+    }
+
+    private void OnGetVerbs(EntityUid uid, StationAnchorComponent component, GetVerbsEvent<Verb> args)
+    {
+        // add debug verb to toggle power requirements
+        args.Verbs.Add(new()
+        {
+            Text = "Enable",
+            Category = VerbCategory.Debug,
+            Icon = new SpriteSpecifier.Texture(new ("/Textures/Interface/VerbIcons/smite.svg.192dpi.png")), // "smite" is a lightning bolt
+            Act = () => SetStatus(uid, true)
+        });
+    }
+
+    private void OnAnchorStationChange(Entity<StationAnchorComponent> ent, ref AnchorStateChangedEvent args)
+    {
+        if (!args.Anchored)
+            SetStatus(ent, false);
+    }
+
+    private void SetStatus(EntityUid uid, bool enabled, ShuttleComponent? shuttleComponent = default)
+    {
+        var transform = Transform(uid);
+        var grid = transform.GridUid;
+        if (!grid.HasValue || !transform.Anchored && enabled || !Resolve(grid.Value, ref shuttleComponent))
+            return;
+
+        if (enabled)
+        {
+            _shuttleSystem.Disable(grid.Value);
+        }
+        else
+        {
+            _shuttleSystem.Enable(grid.Value);
+        }
+
+        shuttleComponent.Enabled = !enabled;
+    }
+}
---
+++ Content.Shared/Power/SharedMachinePowerCharge.cs
@@ -0,0 +1,76 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Power;
+
+/// <summary>
+///     Sent to the server to set whether the machine should be on or off
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class SwitchChargingMachineMessage : BoundUserInterfaceMessage
+{
+    public bool On;
+
+    public SwitchChargingMachineMessage(bool on)
+    {
+        On = on;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class ChargingMachineState : BoundUserInterfaceState
+{
+    public bool On;
+    // 0 -> 255
+    public byte Charge;
+    public MachinePowerChargePowerStatus PowerStatus;
+    public short PowerDraw;
+    public short PowerDrawMax;
+    public short EtaSeconds;
+
+    public ChargingMachineState(
+        bool on,
+        byte charge,
+        MachinePowerChargePowerStatus powerStatus,
+        short powerDraw,
+        short powerDrawMax,
+        short etaSeconds)
+    {
+        On = on;
+        Charge = charge;
+        PowerStatus = powerStatus;
+        PowerDraw = powerDraw;
+        PowerDrawMax = powerDrawMax;
+        EtaSeconds = etaSeconds;
+    }
+}
+
+[Serializable, NetSerializable]
+public enum MachinePowerChargeUiKey
+{
+    Key
+}
+
+[Serializable, NetSerializable]
+public enum MachinePowerChargeVisuals
+{
+    State,
+    Charge
+}
+
+[Serializable, NetSerializable]
+public enum MachinePowerChargeStatus
+{
+    Broken,
+    Unpowered,
+    Off,
+    On
+}
+
+[Serializable, NetSerializable]
+public enum MachinePowerChargePowerStatus : byte
+{
+    Off,
+    Discharging,
+    Charging,
+    FullyCharged
+}
