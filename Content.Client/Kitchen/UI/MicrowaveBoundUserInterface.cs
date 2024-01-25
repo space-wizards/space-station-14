465e3040f6ddf8f951cc50af8a205e31effdf7d5
James Simonson <jamessimo89@gmail.com>
2024-01-25 12:26:15 +0800

    Facelift Microwave UI
    Includes new background light in UI, Uses predictive input, UI now properly disables buttons when microwave is active

--- Content.Client/Kitchen/UI/MicrowaveBoundUserInterface.cs
+++ Content.Client/Kitchen/UI/MicrowaveBoundUserInterface.cs
@@ -32,17 +32,39 @@ namespace Content.Client.Kitchen.UI
             _menu = new MicrowaveMenu(this);
             _menu.OpenCentered();
             _menu.OnClose += Close;
-            _menu.StartButton.OnPressed += _ => SendMessage(new MicrowaveStartCookMessage());
-            _menu.EjectButton.OnPressed += _ => SendMessage(new MicrowaveEjectMessage());
+            _menu.StartButton.OnPressed += _ => SendPredictedMessage(new MicrowaveStartCookMessage());
+            _menu.EjectButton.OnPressed += _ => SendPredictedMessage(new MicrowaveEjectMessage());
             _menu.IngredientsList.OnItemSelected += args =>
             {
-                SendMessage(new MicrowaveEjectSolidIndexedMessage(EntMan.GetNetEntity(_solids[args.ItemIndex])));
+                SendPredictedMessage(new MicrowaveEjectSolidIndexedMessage(EntMan.GetNetEntity(_solids[args.ItemIndex])));
             };
 
             _menu.OnCookTimeSelected += (args, buttonIndex) =>
             {
-                var actualButton = (MicrowaveMenu.MicrowaveCookTimeButton) args.Button;
-                SendMessage(new MicrowaveSelectCookTimeMessage(buttonIndex, actualButton.CookTime));
+                var selectedCookTime = (uint) 0;
+
+                if (args.Button is MicrowaveMenu.MicrowaveCookTimeButton microwaveCookTimeButton)
+                {
+                    // args.Button is a MicrowaveCookTimeButton
+                    var actualButton = (MicrowaveMenu.MicrowaveCookTimeButton) args.Button;
+                    selectedCookTime = actualButton.CookTime == 0 ? 0 : actualButton.CookTime;
+                    // SendMessage(new MicrowaveSelectCookTimeMessage((int) selectedCookTime / 5, actualButton.CookTime));
+                    SendPredictedMessage(new MicrowaveSelectCookTimeMessage((int) selectedCookTime / 5, actualButton.CookTime));
+
+                    _menu.CookTimeInfoLabel.Text = Loc.GetString("microwave-bound-user-interface-cook-time-label",
+                                                                    ("time", selectedCookTime));
+                }
+                else
+                {
+                    // args.Button is a normal button aka instant cook button
+                    SendPredictedMessage(new MicrowaveSelectCookTimeMessage((int) selectedCookTime, 0));
+
+                    _menu.CookTimeInfoLabel.Text = Loc.GetString("microwave-bound-user-interface-cook-time-label",
+                                                         ("time", Loc.GetString("microwave-menu-instant-button")));
+
+                }
+
+
             };
         }
 
@@ -67,20 +89,46 @@ namespace Content.Client.Kitchen.UI
                 return;
             }
 
-            _menu?.ToggleBusyDisableOverlayPanel(cState.IsMicrowaveBusy);
+
+            _menu?.ToggleBusyDisableOverlayPanel(cState.IsMicrowaveBusy || cState.ContainedSolids.Length == 0);
 
             // TODO move this to a component state and ensure the net ids.
             RefreshContentsDisplay(_entManager.GetEntityArray(cState.ContainedSolids));
 
             if (_menu == null) return;
 
-            var currentlySelectedTimeButton = (Button) _menu.CookTimeButtonVbox.GetChild(cState.ActiveButtonIndex);
-            currentlySelectedTimeButton.Pressed = true;
+            //Set the cook time info label
             var cookTime = cState.ActiveButtonIndex == 0
                 ? Loc.GetString("microwave-menu-instant-button")
                 : cState.CurrentCookTime.ToString();
             _menu.CookTimeInfoLabel.Text = Loc.GetString("microwave-bound-user-interface-cook-time-label",
                                                          ("time", cookTime));
+            _menu.StartButton.Disabled = cState.IsMicrowaveBusy || cState.ContainedSolids.Length == 0;
+            _menu.EjectButton.Disabled = cState.IsMicrowaveBusy || cState.ContainedSolids.Length == 0;
+
+              _menu.CookTimeInfoLabel.Text = Loc.GetString("microwave-bound-user-interface-cook-time-label",
+                                                         ("time", cState.CurrentCookTime));
+
+            //Set the correct button active button
+            if (cState.ActiveButtonIndex == 0)
+            {
+                _menu.InstantCookButton.Pressed = true;
+            }
+            else
+            {
+                var currentlySelectedTimeButton = (Button) _menu.CookTimeButtonVbox.GetChild(cState.ActiveButtonIndex - 1);
+                currentlySelectedTimeButton.Pressed = true;
+            }
+
+            //Set the "micowave light" ui color to indicate if the microwave is busy or not
+            if (cState.IsMicrowaveBusy && cState.ContainedSolids.Length > 0)
+            {
+                _menu.IngredientsPanel.PanelOverride = new StyleBoxFlat { BackgroundColor = Color.FromHex("#FFB800FF") };
+            }
+            else
+            {
+                _menu.IngredientsPanel.PanelOverride = new StyleBoxFlat { BackgroundColor = Color.FromHex("#1B1B1E") };
+            }
         }
 
         private void RefreshContentsDisplay(EntityUid[] containedSolids)
