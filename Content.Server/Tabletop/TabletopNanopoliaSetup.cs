e78cc3f2a4a4b47a40857f33e9a35e30d9ca1415
Strol20 <131034301+Strol20@users.noreply.github.com>
2024-01-26 12:30:42 +0200

    This is CamelCase?

--- Content.Server/Tabletop/TabletopNanopoliaSetup.cs
+++ Content.Server/Tabletop/TabletopNanopoliaSetup.cs
@@ -7,23 +7,23 @@ namespace Content.Server.Tabletop
     public sealed partial class TabletopNanopoliaSetup : TabletopSetup
     {
 
-        [DataField("FigurineCatPrototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
-        public string FigurineCatPrototype { get; private set; } = "FigurineCat";
+        [DataField("figurineCatPrototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string figurineCatPrototype { get; private set; } = "FigurineCat";
 
-        [DataField("FigurineLaceupsPrototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
-        public string FigurineLaceupsPrototype { get; private set; } = "FigurineLaceups";
+        [DataField("figurineLaceupsPrototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string figurineLaceupsPrototype { get; private set; } = "FigurineLaceups";
 
-        [DataField("FigurineMcgriffPrototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
-        public string FigurineMcgriffPrototype { get; private set; } = "FigurineMcgriff";
+        [DataField("figurineMcgriffPrototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string figurineMcgriffPrototype { get; private set; } = "FigurineMcgriff";
 
-        [DataField("FigurineTopHatPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-        public string FigurineTopHatPrototype { get; private set; } = "FigurineTopHat";
+        [DataField("figurineTopHatPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string figurineTopHatPrototype { get; private set; } = "FigurineTopHat";
 
-        [DataField("FigurineGreenAutolatPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-        public string FigurineGreenAutolatPrototype { get; private set; } = "FigurineGreenAutolathe";
+        [DataField("figurineGreenAutolatPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string figurineGreenAutolatPrototype { get; private set; } = "FigurineGreenAutolathe";
 
-        [DataField("FigurineRedProtolatdPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-        public string FigurineRedProtolatdPrototype { get; private set; } = "FigurineRedProtolathe";
+        [DataField("figurineRedProtolatdPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+        public string figurineRedProtolatdPrototype { get; private set; } = "FigurineRedProtolathe";
 
         public override void SetupTabletop(TabletopSession session, IEntityManager entityManager)
         {
@@ -41,45 +41,45 @@ namespace Content.Server.Tabletop
             var center = session.Position;
 
             // Cat pieces.
-            EntityUid tempQualifier0 = entityManager.SpawnEntity(FigurineCatPrototype, center.Offset(-x1, y1));
+            EntityUid tempQualifier0 = entityManager.SpawnEntity(figurineCatPrototype, center.Offset(-x1, y1));
             session.Entities.Add(tempQualifier0);
 
             // Laceup pieces.
-            EntityUid tempQualifier1 = entityManager.SpawnEntity(FigurineLaceupsPrototype, center.Offset(-x1, y2));
+            EntityUid tempQualifier1 = entityManager.SpawnEntity(figurineLaceupsPrototype, center.Offset(-x1, y2));
             session.Entities.Add(tempQualifier1);
 
             // Mcgriff pieces.
-            EntityUid tempQualifier2 = entityManager.SpawnEntity(FigurineMcgriffPrototype, center.Offset(-x2, y1));
+            EntityUid tempQualifier2 = entityManager.SpawnEntity(figurineMcgriffPrototype, center.Offset(-x2, y1));
             session.Entities.Add(tempQualifier2);
 
             // TopHat pieces.
-            EntityUid tempQualifier3 = entityManager.SpawnEntity(FigurineTopHatPrototype, center.Offset(-x2, y2));
+            EntityUid tempQualifier3 = entityManager.SpawnEntity(figurineTopHatPrototype, center.Offset(-x2, y2));
             session.Entities.Add(tempQualifier3);
 
             // GreenAutolat pieces.
 
             for (float x = -5.25f; x <= -3.25f; x += 1f)
             {
-                EntityUid tempQualifier = entityManager.SpawnEntity(FigurineGreenAutolatPrototype, center.Offset(x, y3_1));
+                EntityUid tempQualifier = entityManager.SpawnEntity(figurineGreenAutolatPrototype, center.Offset(x, y3_1));
                 session.Entities.Add(tempQualifier);
             }
 
             for (float x = -5.25f; x <= -3.25f; x += 1f)
             {
-                EntityUid tempQualifier = entityManager.SpawnEntity(FigurineGreenAutolatPrototype, center.Offset(x, y3_2));
+                EntityUid tempQualifier = entityManager.SpawnEntity(figurineGreenAutolatPrototype, center.Offset(x, y3_2));
                 session.Entities.Add(tempQualifier);
             }
 
             // RedProtolat pieces.
             for (float x = 3.25f; x <= 5.25f; x += 1f)
             {
-                EntityUid tempQualifier = entityManager.SpawnEntity(FigurineRedProtolatdPrototype, center.Offset(x, y3_1));
+                EntityUid tempQualifier = entityManager.SpawnEntity(figurineRedProtolatdPrototype, center.Offset(x, y3_1));
                 session.Entities.Add(tempQualifier);
             }
 
             for (float x = 3.25f; x <= 5.25f; x += 1f)
             {
-                EntityUid tempQualifier = entityManager.SpawnEntity(FigurineRedProtolatdPrototype, center.Offset(x, y3_2));
+                EntityUid tempQualifier = entityManager.SpawnEntity(figurineRedProtolatdPrototype, center.Offset(x, y3_2));
                 session.Entities.Add(tempQualifier);
             }
         }
