85c357707ef43dc77b193b2cd1c0fb877dbf1b30
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-01-28 13:40:22 +1100

    Firestarter fixes
    - Actually networks the action.
    - Namespace fixes.

--- Content.Shared/Atmos/Components/FirestarterComponent.cs
+++ Content.Shared/Atmos/Components/FirestarterComponent.cs
@@ -1,34 +1,34 @@
-using Robust.Shared.Prototypes;
+using Content.Shared.Atmos.EntitySystems;
 using Robust.Shared.Audio;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
 
-namespace Content.Shared.Abilities.Firestarter;
+namespace Content.Shared.Atmos.Components;
 
 /// <summary>
 /// Lets its owner entity ignite flammables around it and also heal some damage.
 /// </summary>
-[RegisterComponent, NetworkedComponent, Access(typeof(SharedFirestarterSystem))]
+[RegisterComponent, NetworkedComponent, Access(typeof(SharedFirestarterSystem)), AutoGenerateComponentState]
 public sealed partial class FirestarterComponent : Component
 {
     /// <summary>
     /// Radius of objects that will be ignited if flammable.
     /// </summary>
-    [DataField("ignitionRadius")]
+    [DataField]
     public float IgnitionRadius = 4f;
 
     /// <summary>
     /// The action entity.
     /// </summary>
-    [DataField("fireStarterAction", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-    public string? FireStarterAction = "ActionFireStarter";
+    [DataField]
+    public EntProtoId? FireStarterAction = "ActionFireStarter";
 
-    [DataField("fireStarterActionEntity")] public EntityUid? FireStarterActionEntity;
+    [DataField, AutoNetworkedField] public EntityUid? FireStarterActionEntity;
 
 
     /// <summary>
     /// Radius of objects that will be ignited if flammable.
     /// </summary>
-    [DataField("igniteSound")]
+    [DataField]
     public SoundSpecifier IgniteSound = new SoundPathSpecifier("/Audio/Magic/rumble.ogg");
 }
