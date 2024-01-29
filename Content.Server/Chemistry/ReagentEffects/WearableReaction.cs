2717e47dda7125dfb51b253699a614325c2bca67
Kevin Zheng <kevinz5000@gmail.com>
2024-01-29 13:54:45 -0800

    Adjust comments

--- Content.Server/Chemistry/ReagentEffects/WearableReaction.cs
+++ Content.Server/Chemistry/ReagentEffects/WearableReaction.cs
@@ -4,32 +4,38 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Server.Chemistry.ReagentEffects;
 
+/// <summary>
+/// A reaction effect that consumes the required amount of reagent and spawns PrototypeID in the
+/// entity's Slot. Used to implement the water droplet effect for arachnids.
+/// </summary>
 public sealed partial class WearableReaction : ReagentEffect
 {
+    /// <summary>
+    /// Minimum quantity of reagent required to trigger this effect.
+    /// </summary>
     [DataField]
     public float AmountThreshold = 1f;
 
+    /// <summary>
+    /// Slot to spawn the item into.
+    /// </summary>
     [DataField(required: true)]
     public string Slot;
 
     /// <summary>
-    /// ID of item to spawn.
+    /// Prototype ID of item to spawn.
     /// </summary>
     [DataField(required: true)]
     public string PrototypeID;
 
-    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-    => null;
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys) => null;
 
-    /// <summary>
-    /// Attempts to spawn an item in the indicated slot if reagent amount is over threshold.Then substracts threshold amount of reagent.
-    /// Useful for situations where the reagent "becomes" an item entity on a player when reacting. 
-    /// </summary>
     public override void Effect(ReagentEffectArgs args)
     {
         if (args.Reagent == null || args.Quantity < AmountThreshold)
             return;
 
+        // SpawnItemInSlot returns false if slot is already occupied
         if (args.EntityManager.System<InventorySystem>().SpawnItemInSlot(args.SolutionEntity, Slot, PrototypeID))
             args.Source?.RemoveReagent(args.Reagent.ID, AmountThreshold);
     }
