bbad7092c5cafdaeae14ffb33d6706a97adff41b
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-01-29 11:00:53 +1100

    Revert "Make flare gun explode, damage and stun the player when used with the wrong ammo (#22349)"
    This reverts commit e301378114b245fa6927372f83c19868cb90b13c.
    
    # Conflicts:
    #	Content.Shared/Weapons/Ranged/Components/GunComponent.cs
    #	Resources/Prototypes/Entities/Objects/Weapons/Guns/Ammunition/Cartridges/shotgun.yml
    #	Resources/Prototypes/Entities/Objects/Weapons/Guns/Ammunition/Projectiles/shotgun.yml

--- Content.Shared/Weapons/Ranged/Components/GunComponent.cs
+++ Content.Shared/Weapons/Ranged/Components/GunComponent.cs
@@ -5,7 +5,6 @@ using Content.Shared.Weapons.Ranged.Systems;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
 using Robust.Shared.Map;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Shared.Weapons.Ranged.Components;
@@ -181,19 +180,7 @@ public sealed partial class GunComponent : Component
     public bool ResetOnHandSelected = true;
 
     /// <summary>
-    /// Type of ammo the gun can work with
-    /// </summary>
-    [DataField]
-    public List<ProtoId<TagPrototype>>? CompatibleAmmo;
-
-    /// <summary>
-    /// Damage the gun deals when used with wrong ammo
-    /// </summary>
-    [DataField]
-    public DamageSpecifier? DamageOnWrongAmmo = null;
-
-    /// <summary>
-    /// The base value for how fast the projectile moves.
+    /// How fast the projectile moves.
     /// </summary>
     [DataField]
     public float ProjectileSpeed = 25f;
