cd61272d66e5e11a73aeaa1b3777548acf665a17
SlamBamActionman <slambamactionman@gmail.com>
2024-01-29 22:35:03 +0100

    Minor fixes based on review feedback

--- Content.Server/Chemistry/ReagentEffects/WearableReaction.cs
+++ Content.Server/Chemistry/ReagentEffects/WearableReaction.cs
@@ -2,30 +2,35 @@ using Content.Shared.Inventory;
 using Content.Shared.Chemistry.Reagent;
 using Robust.Shared.Prototypes;
 
-namespace Content.Server.Chemistry.ReagentEffects
+namespace Content.Server.Chemistry.ReagentEffects;
+
+public sealed partial class WearableReaction : ReagentEffect
 {
-    public sealed partial class WearableReaction : ReagentEffect
+    [DataField]
+    public float AmountThreshold = 1f;
+
+    [DataField(required: true)]
+    public string Slot;
+
+    /// <summary>
+    /// ID of item to spawn.
+    /// </summary>
+    [DataField(required: true)]
+    public string PrototypeID;
+
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+    => null;
+
+    /// <summary>
+    /// Attempts to spawn an item in the indicated slot if reagent amount is over threshold.Then substracts threshold amount of reagent.
+    /// Useful for situations where the reagent "becomes" an item entity on a player when reacting. 
+    /// </summary>
+    public override void Effect(ReagentEffectArgs args)
     {
+        if (args.Reagent == null || args.Quantity < AmountThreshold)
+            return;
 
-        [DataField]
-        public float AmountThreshold = 1f;
-
-        [DataField(required: true)]
-        public string Slot;
-
-        [DataField(required: true)]
-        public string PrototypeID;
-
-        protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-        => null;
-
-        public override void Effect(ReagentEffectArgs args)
-        {
-            if (args.Reagent == null || args.Quantity < AmountThreshold)
-                return;
-
-            if (args.EntityManager.System<InventorySystem>().SpawnItemInSlot(args.SolutionEntity, Slot, PrototypeID))
-                args.Source?.RemoveReagent(args.Reagent.ID, AmountThreshold);
-        }
+        if (args.EntityManager.System<InventorySystem>().SpawnItemInSlot(args.SolutionEntity, Slot, PrototypeID))
+            args.Source?.RemoveReagent(args.Reagent.ID, AmountThreshold);
     }
 }
