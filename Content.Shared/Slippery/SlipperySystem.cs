43debd7e08b344341d6c501edbe9aad12dab1279
Arendian <137322659+Arendian@users.noreply.github.com>
2024-02-02 17:52:34 +0100

    count

--- Content.Shared/Slippery/SlipperySystem.cs
+++ Content.Shared/Slippery/SlipperySystem.cs
@@ -79,7 +79,7 @@ public sealed class SlipperySystem : EntitySystem
             {
                 var sliding = EnsureComp<SlidingComponent>(other);
                 sliding.CollidingEntities.Add(uid);
-                DebugTools.AssertNotEqual(sliding.CollidingEntities, 0);
+                DebugTools.AssertNotEqual(sliding.CollidingEntities.Count, 0);
             }
         }
 
