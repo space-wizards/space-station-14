f8a93cdedce96c842ef2e7b63b6bf2efeafdc080
deltanedas <@deltanedas:kde.org>
2024-02-03 06:11:23 +0000

    fuckyoufuckyou

--- Content.Server/GameTicking/GameTicker.Player.cs
+++ Content.Server/GameTicking/GameTicker.Player.cs
@@ -85,7 +85,7 @@ namespace Content.Server.GameTicking
                     if (mind == null)
                     {
                         // try to load persistence saved mind for this player when joining
-                        var query = EntityManager.AllEntityQueryEnumerator<MindComponent>(true);
+                        var query = EntityManager.AllEntityQueryEnumerator<MindComponent>();
                         while (query.MoveNext(out var mindComp))
                         {
                             if (mindComp.UserId == session.UserId)
