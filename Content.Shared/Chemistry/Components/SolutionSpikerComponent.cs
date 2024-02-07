f9319b9b7be4b513c968d3d70984923ef279986d
deltanedas <@deltanedas:kde.org>
2024-02-07 22:09:03 +0000

    move stuff to server and some refactoring

--- Content.Shared/Chemistry/Components/SolutionSpikerComponent.cs
+++ Content.Shared/Chemistry/Components/SolutionSpikerComponent.cs
@@ -1,30 +1,32 @@
-namespace Content.Server.Chemistry.Components;
+using Content.Shared.Chemistry.EntitySystems;
 
-[RegisterComponent]
+namespace Content.Shared.Chemistry.Components;
+
+[RegisterComponent, Access(typeof(SolutionSpikerSystem))]
 public sealed partial class SolutionSpikerComponent : Component
 {
     /// <summary>
     ///     The source solution to take the reagents from in order
     ///     to spike the other solution container.
     /// </summary>
-    [DataField]
-    public string SourceSolution { get; private set; } = string.Empty;
+    [DataField(required: true)]
+    public string SourceSolution = string.Empty;
 
     /// <summary>
     ///     If spiking with this entity should ignore empty containers or not.
     /// </summary>
     [DataField]
-    public bool IgnoreEmpty { get; private set; }
+    public bool IgnoreEmpty;
 
     /// <summary>
     ///     What should pop up when spiking with this entity.
     /// </summary>
     [DataField]
-    public LocId Popup { get; private set; } = "spike-solution-generic";
+    public LocId Popup = "spike-solution-generic";
 
     /// <summary>
     ///     What should pop up when spiking fails because the container was empty.
     /// </summary>
     [DataField]
-    public LocId PopupEmpty { get; private set; } = "spike-solution-empty-generic";
+    public LocId PopupEmpty = "spike-solution-empty-generic";
 }
