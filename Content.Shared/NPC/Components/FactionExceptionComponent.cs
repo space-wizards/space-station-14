a1ee38634e21311f55109c6d30e23d19b7d9bf84
deltanedas <@deltanedas:kde.org>
2024-02-12 11:44:06 +0000

    move faction exception and member stuff to shared

--- Content.Shared/NPC/Components/FactionExceptionComponent.cs
+++ Content.Shared/NPC/Components/FactionExceptionComponent.cs
@@ -1,23 +1,24 @@
-using Content.Server.NPC.Systems;
+using Content.Shared.NPC.Systems;
+using Robust.Shared.GameStates;
 
-namespace Content.Server.NPC.Components;
+namespace Content.Shared.NPC.Components;
 
 /// <summary>
 /// Prevents an NPC from attacking ignored entities from enemy factions.
 /// Can be added to if pettable, see PettableFriendComponent.
 /// </summary>
-[RegisterComponent, Access(typeof(NpcFactionSystem))]
+[RegisterComponent, NetworkedComponent, Access(typeof(NpcFactionSystem))]
 public sealed partial class FactionExceptionComponent : Component
 {
     /// <summary>
     /// Collection of entities that this NPC will refuse to attack
     /// </summary>
-    [DataField("ignored")]
+    [DataField]
     public HashSet<EntityUid> Ignored = new();
 
     /// <summary>
     /// Collection of entities that this NPC will attack, regardless of faction.
     /// </summary>
-    [DataField("hostiles")]
+    [DataField]
     public HashSet<EntityUid> Hostiles = new();
 }
---
+++ Content.Shared/NPC/Systems/NpcFactionSystem.cs
@@ -0,0 +1,265 @@
+using Content.Shared.NPC.Components;
+using Content.Shared.NPC.Prototypes;
+using Robust.Shared.Prototypes;
+using System.Collections.Frozen;
+using System.Linq;
+
+namespace Content.Shared.NPC.Systems;
+
+/// <summary>
+///     Outlines faction relationships with each other.
+/// </summary>
+public sealed partial class NpcFactionSystem : EntitySystem
+{
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!;
+
+    private ISawmill _sawmill = default!;
+
+    /// <summary>
+    /// To avoid prototype mutability we store an intermediary data class that gets used instead.
+    /// </summary>
+    private FrozenDictionary<ProtoId<NpcFactionPrototype>, FactionData> _factions = FrozenDictionary<ProtoId<NpcFactionPrototype>, FactionData>.Empty;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _sawmill = Logger.GetSawmill("faction");
+
+        SubscribeLocalEvent<NpcFactionMemberComponent, ComponentStartup>(OnFactionStartup);
+        SubscribeLocalEvent<PrototypesReloadedEventArgs>(OnProtoReload);
+
+        InitializeException();
+        RefreshFactions();
+    }
+
+    private void OnProtoReload(PrototypesReloadedEventArgs obj)
+    {
+        if (obj.WasModified<NpcFactionPrototype>())
+            RefreshFactions();
+    }
+
+    private void OnFactionStartup(Entity<NpcFactionMemberComponent> ent, ref ComponentStartup args)
+    {
+        RefreshFactions(memberComponent);
+    }
+
+    /// <summary>
+    /// Refreshes the cached factions for this component.
+    /// </summary>
+    private void RefreshFactions(Entity<NpcFactionMemberComponent> ent)
+    {
+        ent.Comp.FriendlyFactions.Clear();
+        ent.Comp.HostileFactions.Clear();
+
+        foreach (var faction in ent.Comp.Factions)
+        {
+            // YAML Linter already yells about this, don't need to log an error here
+            if (!_factions.TryGetValue(faction, out var factionData))
+                continue;
+
+            ent.Comp.FriendlyFactions.UnionWith(factionData.Friendly);
+            ent.Comp.HostileFactions.UnionWith(factionData.Hostile);
+        }
+    }
+
+    /// <summary>
+    /// Adds this entity to the particular faction.
+    /// </summary>
+    public void AddFaction(Entity<NpcFactionMemberComponent?> ent, string faction, bool dirty = true)
+    {
+        if (!_proto.HasIndex<NpcFactionPrototype>(faction))
+        {
+            _sawmill.Error($"Unable to find faction {faction}");
+            return;
+        }
+
+        ent.Comp ??= EnsureComp<NpcFactionMemberComponent>(ent);
+        if (!ent.Comp.Factions.Add(faction))
+            return;
+
+        if (dirty)
+            RefreshFactions(ent);
+    }
+
+    /// <summary>
+    /// Removes this entity from the particular faction.
+    /// </summary>
+    public void RemoveFaction(Entity<NpcFactionMemberComponent?> ent, string faction, bool dirty = true)
+    {
+        if (!_proto.HasIndex<NpcFactionPrototype>(faction))
+        {
+            _sawmill.Error($"Unable to find faction {faction}");
+            return;
+        }
+
+        if (!Resolve(ent, ref ent.Comp, false))
+            return;
+
+        if (!ent.Comp.Factions.Remove(faction))
+            return;
+
+        if (dirty)
+            RefreshFactions(ent);
+    }
+
+    /// <summary>
+    /// Remove this entity from all factions.
+    /// </summary>
+    public void ClearFactions(Entity<NpcFactionMemberComponent?> ent, bool dirty = true)
+    {
+        if (!Resolve(ent, ref ent.Comp, false))
+            return;
+
+        ent.Comp.Factions.Clear();
+
+        if (dirty)
+            RefreshFactions(ent);
+    }
+
+    public IEnumerable<EntityUid> GetNearbyHostiles(Entity<NpcFactionMemberComponent?, FactionExceptionComponent?> ent, float range)
+    {
+        if (!Resolve(ent, ref ent.Comp1, false))
+            return Array.Empty<EntityUid>();
+
+        var hostiles = GetNearbyFactions(ent, range, ent.Comp1.HostileFactions);
+        if (!Resolve(ent, ref ent.Comp2, false))
+            return hostiles;
+
+        // ignore anything from enemy faction that we are explicitly friendly towards
+        return hostiles
+            .Union(GetHostiles((ent, ent.Comp2)))
+            .Where(target => !IsIgnored(entity, target, factionException));
+    }
+
+    public IEnumerable<EntityUid> GetNearbyFriendlies(Entity<NpcFactionMemberComponent?> ent, float range)
+    {
+        if (!Resolve(ent, ref ent.Comp, false))
+            return Array.Empty<EntityUid>();
+
+        return GetNearbyFactions(ent, range, ent.Comp.FriendlyFactions);
+    }
+
+    private IEnumerable<EntityUid> GetNearbyFactions(EntityUid entity, float range, HashSet<ProtoId<NpcFactionPrototype>> factions)
+    {
+        var xform = Transform(entity);
+        foreach (var ent in _lookup.GetEntitiesInRange<NpcFactionMemberComponent>(xform.MapPosition, range))
+        {
+            if (ent.Owner == entity)
+                continue;
+
+            if (!factions.Overlaps(ent.Comp.Factions))
+                continue;
+
+            yield return ent.Owner;
+        }
+    }
+
+    /// <remarks>
+    /// 1-way and purely faction based, ignores faction exception.
+    /// </remarks>
+    public bool IsEntityFriendly(Entity<NpcFactionMemberComponent?> ent, Entity<NpcFactionMemberComponent?> other)
+    {
+        if (!Resolve(ent, ref ent.Comp, false) || !Resolve(other, ref other.Comp, false))
+            return false;
+
+        return ent.Comp.Factions.Overlaps(other.Comp.Factions) || ent.Comp.FriendlyFactions.Overlaps(other.Comp.Factions);
+    }
+
+    public bool IsFactionFriendly(string target, string with)
+    {
+        return _factions[target].Friendly.Contains(with) && _factions[with].Friendly.Contains(target);
+    }
+
+    public bool IsFactionFriendly(string target, Entity<NpcFactionMemberComponent?> with)
+    {
+        if (!Resolve(with, ref with.Comp, false))
+            return false;
+
+        return with.Comp.Factions.All(x => IsFactionFriendly(target, x)) ||
+               with.Comp.FriendlyFactions.Contains(target);
+    }
+
+    public bool IsFactionHostile(string target, string with)
+    {
+        return _factions[target].Hostile.Contains(with) && _factions[with].Hostile.Contains(target);
+    }
+
+    public bool IsFactionHostile(string target, Entity<NpcFactionMemberComponent?> with)
+    {
+        if (!Resolve(with, ref with.Comp, false))
+            return false;
+
+        return with.Comp.Factions.All(x => IsFactionHostile(target, x)) ||
+               with.Comp.HostileFactions.Contains(target);
+    }
+
+    public bool IsFactionNeutral(string target, string with)
+    {
+        return !IsFactionFriendly(target, with) && !IsFactionHostile(target, with);
+    }
+
+    /// <summary>
+    /// Makes the source faction friendly to the target faction, 1-way.
+    /// </summary>
+    public void MakeFriendly(string source, string target)
+    {
+        if (!_factions.TryGetValue(source, out var sourceFaction))
+        {
+            _sawmill.Error($"Unable to find faction {source}");
+            return;
+        }
+
+        if (!_factions.ContainsKey(target))
+        {
+            _sawmill.Error($"Unable to find faction {target}");
+            return;
+        }
+
+        sourceFaction.Friendly.Add(target);
+        sourceFaction.Hostile.Remove(target);
+        RefreshFactions();
+    }
+
+    /// <summary>
+    /// Makes the source faction hostile to the target faction, 1-way.
+    /// </summary>
+    public void MakeHostile(string source, string target)
+    {
+        if (!_factions.TryGetValue(source, out var sourceFaction))
+        {
+            _sawmill.Error($"Unable to find faction {source}");
+            return;
+        }
+
+        if (!_factions.ContainsKey(target))
+        {
+            _sawmill.Error($"Unable to find faction {target}");
+            return;
+        }
+
+        sourceFaction.Friendly.Remove(target);
+        sourceFaction.Hostile.Add(target);
+        RefreshFactions();
+    }
+
+    private void RefreshFactions()
+    {
+        _factions = _proto.EnumeratePrototypes<NpcFactionPrototype>().ToFrozenDictionary(
+            faction => faction.ID,
+            faction =>  new FactionData
+            {
+                Friendly = faction.Friendly.ToHashSet(),
+                Hostile = faction.Hostile.ToHashSet()
+            });
+
+        var query = AllEntityQuery<NpcFactionMemberComponent>();
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            comp.FriendlyFactions.Clear();
+            comp.HostileFactions.Clear();
+            RefreshFactions((uid, comp));
+        }
+    }
+}
