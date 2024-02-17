c9c5b71e7e7d214571ac729d4f9f852526378bb7
VigersRay <vigersray@gmail.com>
2024-02-18 00:48:59 +0200

    fast camera connect

--- Content.Client/SurveillanceCamera/ActiveSurveillanceCameraMonitor.cs
+++ Content.Client/SurveillanceCamera/ActiveSurveillanceCameraMonitor.cs
@@ -3,7 +3,7 @@ namespace Content.Client.SurveillanceCamera;
 [RegisterComponent]
 public sealed partial class ActiveSurveillanceCameraMonitorVisualsComponent : Component
 {
-    public float TimeLeft = 10f;
+    public float TimeLeft = 1f;
 
     public Action? OnFinish;
 }
