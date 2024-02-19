ae20328e68a31e49d26df44971610250c83ba240
k3yw <grenadiumdota@gmail.com>
2024-02-19 09:32:54 +0700

    Fixed playglobalsound autocompletion

--- Content.Server/Administration/Commands/PlayGlobalSoundCommand.cs
+++ Content.Server/Administration/Commands/PlayGlobalSoundCommand.cs
@@ -6,6 +6,12 @@ using Robust.Shared.Audio;
 using Robust.Shared.Console;
 using Robust.Shared.ContentPack;
 using Robust.Shared.Player;
+using Robust.Shared.Serialization.Manager;
+using Robust.Shared.Serialization.Markdown;
+using Robust.Shared.Serialization.Markdown.Mapping;
+using Robust.Shared.Serialization.Markdown.Sequence;
+using Robust.Shared.Utility;
+using static Content.Server.Audio.ServerGlobalSoundSystem;
 
 namespace Content.Server.Administration.Commands;
 
@@ -15,6 +21,7 @@ public sealed class PlayGlobalSoundCommand : IConsoleCommand
     [Dependency] private readonly IEntityManager _entManager = default!;
     [Dependency] private readonly IPlayerManager _playerManager = default!;
     [Dependency] private readonly IResourceManager _res = default!;
+    [Dependency] private readonly ISerializationManager _serialization = default!;
 
     public string Command => "playglobalsound";
     public string Description => Loc.GetString("play-global-sound-command-description");
@@ -94,10 +101,48 @@ public sealed class PlayGlobalSoundCommand : IConsoleCommand
         if (args.Length == 1)
         {
             var hint = Loc.GetString("play-global-sound-command-arg-path");
+            var options2 = new List<CompletionOption>();
+            try
+            {
+                var path = new ResPath(args[0]);
+                foreach (var file in _res.ContentFindFiles(path))
+                {
+                    if (file.Extension != "yml")
+                        continue;
 
+                    var yamlData = _res.ContentFileReadYaml(file);
+
+                    if (yamlData.Documents.Count == 0)
+                        continue;
+
+                    foreach (var item in yamlData.Documents[0].AllNodes)
+                    {
+                        var sequence = (SequenceDataNode) item.ToDataNode();
+                        foreach (var entry in sequence.Sequence)
+                        {
+                            if (entry is not MappingDataNode map)
+                                continue;
+                            if (!map.TryGet("files", out var value))
+                                continue;
+
+                            foreach (var item1 in (SequenceDataNode) value)
+                            {
+                                options2.Add(new CompletionOption(file.WithName(item1.ToString()).CanonPath));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception) {}
             var options = CompletionHelper.ContentFilePath(args[0], _res);
 
+#if FULL_RELEASE
+            return CompletionResult.FromHintOptions(options.Concat(options2), hint);
+#else
+            // We don't need to concat lists in debug version since it works here
             return CompletionResult.FromHintOptions(options, hint);
+#endif
+
         }
 
         if (args.Length == 2)
