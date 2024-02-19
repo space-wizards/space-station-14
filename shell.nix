d57dd85b0ec5d39d39cb3e757ed94e97fac8d085
Ioannis Eleftheriou <me@yath.xyz>
2024-02-19 12:13:52 +0000

    Pin nixpkgs per suggestion
    See https://github.com/space-wizards/space-station-14/pull/25361#discussion_r1494196038

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-{pkgs ? abort "This project is heavily dependent on flakes. Use `nix develop` instead."}:
+{ pkgs ? (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+in import (builtins.fetchTarball {
+  url =
+    "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
+  sha256 = lock.nodes.nixpkgs.locked.narHash;
+}) { }) }:
 
 let
   dependencies = with pkgs; [
