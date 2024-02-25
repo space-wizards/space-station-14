777bdd0a02dfc9b5dbb23243a0022a6136348b8e
Hannah Giovanna Dawson <karakkaraz@gmail.com>
2024-02-25 01:05:49 +0000

    Move the attempt comms console announcement message to its own file.

---
+++ Content.Shared/Communications/Messages.cs
@@ -0,0 +1,23 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Communications
+{
+    /// <summary>
+    /// Raised when an announcement is attempted by a communications console.
+    /// </summary>
+    [Serializable, NetSerializable]
+    public sealed class AttemptCommunicationConsoleAnnouncementMessage : EntityEventArgs
+    {
+        public NetEntity Console;
+        public NetEntity Sender;
+        public readonly string Message;
+
+        public AttemptCommunicationConsoleAnnouncementMessage(NetEntity console, NetEntity sender, string message)
+        {
+            Console = console;
+            Sender = sender;
+            Message = message;
+        }
+    }
+}
+
--- Content.Shared/Communications/SharedCommunicationsConsoleComponent.cs
+++ Content.Shared/Communications/SharedCommunicationsConsoleComponent.cs
@@ -68,23 +68,4 @@ namespace Content.Shared.Communications
     {
         Key
     }
-
-    /// <summary>
-    /// Raised when an announcement is attempted by a communications console.
-    /// </summary>
-    [Serializable, NetSerializable]
-    public sealed class AttemptCommunicationConsoleAnnouncementMessage : EntityEventArgs
-    {
-        public NetEntity Console;
-        public NetEntity Sender;
-        public readonly string Message;
-
-        public AttemptCommunicationConsoleAnnouncementMessage(NetEntity console, NetEntity sender, string message)
-        {
-            Console = console;
-            Sender = sender;
-            Message = message;
-        }
-    }
-
 }
