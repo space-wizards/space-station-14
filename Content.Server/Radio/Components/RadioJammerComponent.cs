b46f95826837220e0b11948412655d91db55928e
beck-thompson <beck314159@hotmail.com>
2024-02-24 22:02:55 -0800

    Added selectable power level to radio jammer

--- Content.Server/Radio/Components/RadioJammerComponent.cs
+++ Content.Server/Radio/Components/RadioJammerComponent.cs
@@ -9,12 +9,46 @@ namespace Content.Server.Radio.Components;
 [Access(typeof(JammerSystem))]
 public sealed partial class RadioJammerComponent : Component
 {
-    [DataField("range"), ViewVariables(VVAccess.ReadWrite)]
-    public float Range = 8f;
+    /// <summary>
+    /// Range of the jammer when on high power
+    /// </summary>
+    [DataField("highPowerRange"), ViewVariables(VVAccess.ReadOnly)]
+    public float HighPowerRange = 12f;
 
     /// <summary>
-    /// Power usage per second when enabled
+    /// Range of the jammer when on medium power
     /// </summary>
-    [DataField("wattage"), ViewVariables(VVAccess.ReadWrite)]
-    public float Wattage = 2f;
+    [DataField("mediumPowerRange"), ViewVariables(VVAccess.ReadOnly)]
+    public float MediumPowerRange = 6f;
+
+    /// <summary>
+    /// Range of the jammer when on low power
+    /// </summary>
+    [DataField("lowPowerRange"), ViewVariables(VVAccess.ReadOnly)]
+    public float LowPowerRange = 2.5f;
+
+    /// <summary>
+    /// Power usage per second when on high power
+    /// </summary>
+    [DataField("highPowerWattage"), ViewVariables(VVAccess.ReadOnly)]
+    public float HighPowerWattage = 12f;
+
+    /// <summary>
+    /// Power usage per second when on medium power
+    /// </summary>
+    [DataField("mediumPowerWattage"), ViewVariables(VVAccess.ReadOnly)]
+    public float MediumPowerWattage = 2f;
+
+    /// <summary>
+    /// Power usage per second when on low power
+    /// </summary>
+    [DataField("lowPowerWattage"), ViewVariables(VVAccess.ReadOnly)]
+    public float LowPowerWattage = 1f;
+
+    /// <summary>
+    /// The currently selected power level
+    /// </summary>
+    [DataField("selectedPowerLevel"), ViewVariables(VVAccess.ReadWrite)]
+    public byte SelectedPowerLevel = 1;
+
 }
--- Content.Server/Radio/EntitySystems/JammerSystem.cs
+++ Content.Server/Radio/EntitySystems/JammerSystem.cs
@@ -4,6 +4,7 @@ using Content.Server.Radio.Components;
 using Content.Shared.Examine;
 using Content.Shared.Interaction;
 using Content.Shared.PowerCell.Components;
+using Content.Shared.Verbs;
 
 namespace Content.Server.Radio.EntitySystems;
 
@@ -21,26 +22,31 @@ public sealed class JammerSystem : EntitySystem
         SubscribeLocalEvent<ActiveRadioJammerComponent, PowerCellChangedEvent>(OnPowerCellChanged);
         SubscribeLocalEvent<RadioJammerComponent, ExaminedEvent>(OnExamine);
         SubscribeLocalEvent<RadioSendAttemptEvent>(OnRadioSendAttempt);
+        SubscribeLocalEvent<RadioJammerComponent, GetVerbsEvent<Verb>>(OnGetVerb);
     }
 
     public override void Update(float frameTime)
     {
         var query = EntityQueryEnumerator<ActiveRadioJammerComponent, RadioJammerComponent>();
+
         while (query.MoveNext(out var uid, out var _, out var jam))
         {
+
             if (_powerCell.TryGetBatteryFromSlot(uid, out var battery) &&
-                !battery.TryUseCharge(jam.Wattage * frameTime))
+                !battery.TryUseCharge(GetCurrentWattage(jam) * frameTime))
             {
                 RemComp<ActiveRadioJammerComponent>(uid);
             }
+
         }
     }
 
     private void OnActivate(EntityUid uid, RadioJammerComponent comp, ActivateInWorldEvent args)
     {
+
         var activated = !HasComp<ActiveRadioJammerComponent>(uid) && 
             _powerCell.TryGetBatteryFromSlot(uid, out var battery) &&
-            battery.CurrentCharge > comp.Wattage;
+            battery.CurrentCharge > GetCurrentWattage(comp);
         if (activated)
         {
             EnsureComp<ActiveRadioJammerComponent>(uid);
@@ -78,11 +84,86 @@ public sealed class JammerSystem : EntitySystem
         var query = EntityQueryEnumerator<ActiveRadioJammerComponent, RadioJammerComponent, TransformComponent>();
         while (query.MoveNext(out _, out _, out var jam, out var transform))
         {
-            if (source.InRange(EntityManager, _transform, transform.Coordinates, jam.Range))
+            if (source.InRange(EntityManager, _transform, transform.Coordinates, GetCurrentRange(jam)))
             {
                 args.Cancelled = true;
                 return;
             }
         }
+    }
+
+    private void OnGetVerb(Entity<RadioJammerComponent> entity, ref GetVerbsEvent<Verb> args)
+    {
+        if (!args.CanAccess || !args.CanInteract)
+            return;
+
+        var user = args.User;
+        var highVerb = new Verb
+        {
+            Priority = 3,
+            Category = VerbCategory.PowerLevel,
+            Disabled = entity.Comp.SelectedPowerLevel == 2,
+            Act = () =>
+            {
+                entity.Comp.SelectedPowerLevel = 2;
+                _popup.PopupEntity(Loc.GetString("radio-jammer-component-set-high"), user, user);
+            },
+            Text = Loc.GetString("radio-jammer-component-high"),
+        };
+
+        var mediumVerb = new Verb
+        {
+            Priority = 2,
+            Category = VerbCategory.PowerLevel,
+            Disabled = entity.Comp.SelectedPowerLevel == 1,
+            Act = () =>
+            {
+                entity.Comp.SelectedPowerLevel = 1;
+                _popup.PopupEntity(Loc.GetString("radio-jammer-component-set-medium"), user, user);
+            },
+            Text = Loc.GetString("radio-jammer-component-medium"),
+        };
+
+        var lowVerb = new Verb
+        {
+            Priority = 1,
+            Category = VerbCategory.PowerLevel,
+            Disabled = entity.Comp.SelectedPowerLevel == 0,
+            Act = () =>
+            {
+                entity.Comp.SelectedPowerLevel = 0;
+                _popup.PopupEntity(Loc.GetString("radio-jammer-component-set-low"), user, user);
+            },
+            Text = Loc.GetString("radio-jammer-component-low"),
+        };
+
+        args.Verbs.Add(highVerb);
+        args.Verbs.Add(mediumVerb);
+        args.Verbs.Add(lowVerb);
+    }
+
+    private static float GetCurrentWattage(RadioJammerComponent jammer)
+    {
+        switch(jammer.SelectedPowerLevel)
+        {
+        case 2:
+            return jammer.HighPowerWattage;
+        case 1:
+            return jammer.MediumPowerWattage;
+        default:
+            return jammer.LowPowerWattage;
+        }
+    }
+    private static float GetCurrentRange(RadioJammerComponent jammer)
+    {
+        switch(jammer.SelectedPowerLevel)
+        {
+        case 2:
+            return jammer.HighPowerRange;
+        case 1:
+            return jammer.MediumPowerRange;
+        default:
+            return jammer.LowPowerRange;
+        };
     }
 }
