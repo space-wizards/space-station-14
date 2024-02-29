5e77a7520d0c98e598f43024cae3c9a1a60ab1e7
UBlueberry <161545003+UBlueberry@users.noreply.github.com>
2024-02-28 21:16:42 -0500

    (5) y'all.

--- Content.Server/Speech/EntitySystems/SouthernAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/SouthernAccentSystem.cs
@@ -3,16 +3,20 @@ using Content.Server.Speech.Components;
 
 namespace Content.Server.Speech.EntitySystems;
 
-public sealed class VerbCutoffAccentSystem : EntitySystem
-
+public sealed class SouthernAccentSystem : EntitySystem
 {
+    private static readonly Dictionary<string, string> DirectReplacements = new()
+    {
+        { "you all", "y'all"},
+    }    
+    
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<VerbCutoffAccentComponent, AccentGetEvent>(OnAccent);
+        SubscribeLocalEvent<SouthernAccentComponent, AccentGetEvent>(OnAccent);
     }
 
-    private void OnAccent(EntityUid uid, VerbCutoffAccentComponent component, AccentGetEvent args)
+    private void OnAccent(EntityUid uid, SouthernAccentComponent component, AccentGetEvent args)
     {
         var message = args.Message;
         message = Regex.Replace(message, "ing", "in'");
