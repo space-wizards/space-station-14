3840b9ddb0767067eda4d95f1d52ddb27f71c1e7
deltanedas <@deltanedas:kde.org>
2024-03-02 05:26:54 +0000

    add Name field to SpeechVerbPrototype

--- Content.Shared/Speech/SpeechVerbPrototype.cs
+++ Content.Shared/Speech/SpeechVerbPrototype.cs
@@ -43,4 +43,10 @@ public sealed partial class SpeechVerbPrototype : IPrototype
     /// </summary>
     [DataField("priority")]
     public int Priority = 0;
+
+    /// <summary>
+    /// Name shown in the voicemask UI for this verb.
+    /// </summary>
+    [DataField(required: true)]
+    public LocId Name = string.Empty;
 }
