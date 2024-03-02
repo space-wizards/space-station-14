6c411f5b0f730b261017a397664288e5806d283b
deltanedas <@deltanedas:kde.org>
2024-03-02 06:14:50 +0000

    SpeechVerb ui and handling

--- Content.Server/VoiceMask/VoiceMaskerComponent.cs
+++ Content.Server/VoiceMask/VoiceMaskerComponent.cs
@@ -1,15 +1,20 @@
+using Content.Shared.Speech;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Server.VoiceMask;
 
 [RegisterComponent]
 public sealed partial class VoiceMaskerComponent : Component
 {
-    [ViewVariables(VVAccess.ReadWrite)] public string LastSetName = "Unknown";
+    [DataField]
+    public string LastSetName = "Unknown";
 
-    [DataField("action", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-    public string Action = "ActionChangeVoiceMask";
+    [DataField]
+    public ProtoId<SpeechVerbPrototype>? LastSpeechVerb;
 
-    [DataField("actionEntity")] public EntityUid? ActionEntity;
+    [DataField]
+    public EntProtoId Action = "ActionChangeVoiceMask";
+
+    [DataField]
+    public EntityUid? ActionEntity;
 }
