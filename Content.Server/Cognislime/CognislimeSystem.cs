90794f10b0116e0a03403352ce0eac85881ca402
unknown <113810873+Interrobang01@users.noreply.github.com>
2024-03-07 05:02:37 -0800

    moved doafter stuff to shared as per review

--- Content.Server/Cognislime/CognislimeSystem.cs
+++ Content.Server/Cognislime/CognislimeSystem.cs
@@ -20,52 +20,15 @@ namespace Content.Server.Cognislime;
 /// </summary>
 public sealed class CognislimeSystem : SharedCognislimeSystem
 {
-    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<CognislimeComponent, AfterInteractEvent>(OnCognislimeAfterInteract);
         SubscribeLocalEvent<CognislimeComponent, CognislimeDoAfterEvent>(ApplyCognislime);
     }
 
-    public void OnCognislimeAfterInteract(EntityUid uid, CognislimeComponent component, AfterInteractEvent args)
-    {
-        if (args.Target == null || args.Handled || !args.CanReach)
-            return;
-
-        if (!CheckTarget(args.Target.Value, component.Whitelist, component.Blacklist))
-        {
-            _popup.PopupEntity(Loc.GetString("cognislime-invalid"), args.Target.Value, args.User);
-            return;
-        }
-
-        args.Handled = true;
-
-        TryApplyCognizine(component, args.User, args.Target.Value, uid);
-    }
-    public bool CheckTarget(EntityUid target, EntityWhitelist? whitelist, EntityWhitelist? blacklist)
-    {
-        return whitelist?.IsValid(target, EntityManager) != false &&
-            blacklist?.IsValid(target, EntityManager) != true;
-    }
-
-    public void TryApplyCognizine(CognislimeComponent component, EntityUid user, EntityUid target, EntityUid cognislime)
-    {
-        var ev = new CognislimeDoAfterEvent();
-        var args = new DoAfterArgs(EntityManager, user, component.ApplyCognislimeDuration, ev, cognislime, target: target, used: cognislime)
-        {
-            BreakOnUserMove = true,
-            BreakOnTargetMove = true,
-            BreakOnDamage = true,
-            NeedHand = true,
-        };
-
-        if (_doAfter.TryStartDoAfter(args))
-            _popup.PopupEntity(Loc.GetString("cognislime-applying"), target, user);
-    }
     public void ApplyCognislime(EntityUid uid, CognislimeComponent component, CognislimeDoAfterEvent args)
     {
         if (args.Cancelled || args.Handled || args.Target == null || args.Used == null)
