99834978da276e7e52e4534eb12aff77b8544f80
Pspritechologist <naaronn@gmail.com>
2024-03-10 03:55:17 -0400

    Removed unneeded brackets

--- Content.Server/StationEvents/Components/BasicStationEventSchedulerComponent.cs
+++ Content.Server/StationEvents/Components/BasicStationEventSchedulerComponent.cs
@@ -7,19 +7,19 @@ public sealed partial class BasicStationEventSchedulerComponent : Component
     ///     The minimum amount of time that must past before the first event can trigger.
     ///     This is in addition to the min and max times below.
     /// </summary>
-    [DataField(), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public TimeSpan MinimumTimeUntilFirstEvent = TimeSpan.FromSeconds(300);
 
     /// <summary>
     ///     The minimum amount of time that must past after the last event before the next event check will occur.
     /// </summary>
-    [DataField(), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public TimeSpan MinimumTimeBetweenEvents = TimeSpan.FromSeconds(300);
 
     /// <summary>
     ///     The maximum amount of time that must past after the last event before the next event check will occur.
     /// </summary>
-    [DataField(), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public TimeSpan MaximumTimeBetweenEvents = TimeSpan.FromSeconds(600);
 
     /// <summary>
