19f86b79cf83127a9187832f4dca6d50588d3d87
Vasilis The Pikachu <vasilis@pikachu.systems>
2024-03-10 17:04:15 +0100

    Yeah i guess i should

--- Content.Client/Players/PlayTimeTracking/JobRequirementsManager.cs
+++ Content.Client/Players/PlayTimeTracking/JobRequirementsManager.cs
@@ -88,12 +88,6 @@ public sealed class JobRequirementsManager
             return false;
         }
 
-        if (job.Requirements == null ||
-            !_cfg.GetCVar(CCVars.GameRoleTimers))
-        {
-            return true;
-        }
-
         var player = _playerManager.LocalSession;
         if (player == null)
             return true;
@@ -105,10 +99,7 @@ public sealed class JobRequirementsManager
     {
         reason = null;
 
-        if (requirements == null)
-            return true;
-
-        if (!_cfg.GetCVar(CCVars.GameRoleTimers))
+        if (requirements == null || !_cfg.GetCVar(CCVars.GameRoleTimers))
             return true;
 
         var reasons = new List<string>();
