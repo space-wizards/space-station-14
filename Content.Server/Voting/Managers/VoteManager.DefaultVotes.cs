7972fbeb3ae7e25fb9bc3666f068384cd75ca1c1
Eris <erisfiregamer1@gmail.com>
2024-03-11 20:34:43 -0400

    Any gamemode is allowed
    It's all fair game now.

--- Content.Server/Voting/Managers/VoteManager.DefaultVotes.cs
+++ Content.Server/Voting/Managers/VoteManager.DefaultVotes.cs
@@ -291,12 +291,6 @@ namespace Content.Server.Voting.Managers
                 if(!preset.ShowInVote)
                     continue;
 
-                if(_playerManager.PlayerCount < (preset.MinPlayers ?? int.MinValue))
-                    continue;
-
-                if(_playerManager.PlayerCount > (preset.MaxPlayers ?? int.MaxValue))
-                    continue;
-
                 presets[preset.ID] = preset.ModeTitle;
             }
             return presets;
