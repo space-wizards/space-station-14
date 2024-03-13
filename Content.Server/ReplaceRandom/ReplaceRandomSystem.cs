0fe69af0481dc9f6af47652cdee1f32bcfeec78e
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-03-13 19:58:28 +0300

    handle containers also

--- Content.Server/ReplaceRandom/ReplaceRandomSystem.cs
+++ Content.Server/ReplaceRandom/ReplaceRandomSystem.cs
@@ -1,3 +1,6 @@
+using Content.Shared.Buckle;
+using Robust.Server.Containers;
+using Robust.Server.GameObjects;
 using Robust.Shared.Random;
 
 namespace Content.Server.ReplaceRandom;
@@ -5,6 +8,9 @@ namespace Content.Server.ReplaceRandom;
 public sealed class ReplaceRandomSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly SharedBuckleSystem _buckle = default!;
+    [Dependency] private readonly TransformSystem _transform = default!;
+    [Dependency] private readonly ContainerSystem _container = default!;
 
     public override void Initialize()
     {
@@ -14,7 +20,21 @@ public sealed class ReplaceRandomSystem : EntitySystem
     public void OnMapInit(Entity<ReplaceRandomComponent> entity, ref MapInitEvent args)
     {
         if (!_random.Prob(entity.Comp.Chance)) return;
-        SpawnNextToOrDrop(entity.Comp.Prototype, entity);
+
+        // mostly stolen code from polymorph system
+        _buckle.TryUnbuckle(entity, entity, true);
+
+        var targetTransformComp = Transform(entity);
+        var child = Spawn(entity.Comp.Prototype, targetTransformComp.Coordinates);
+        var childXform = Transform(child);
+        _transform.SetLocalRotation(child, targetTransformComp.LocalRotation, childXform);
+
+        if (_container.TryGetContainingContainer(entity, out var cont))
+        {
+            _container.Remove(entity.Owner, cont, force: true);
+            _container.Insert(child, cont, force: true);
+        }
+
         QueueDel(entity);
     }
 }
