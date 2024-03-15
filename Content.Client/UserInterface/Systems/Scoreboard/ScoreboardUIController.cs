ebd100cc190f8c0abfff38cb23d62dea1d9d6c36
wafehling <wafehling@users.noreply.github.com>
2024-03-15 15:31:11 -0500

    Updated namespace for ScoreboardUIController.cs

--- Content.Client/UserInterface/Systems/Scoreboard/ScoreboardUIController.cs
+++ Content.Client/UserInterface/Systems/Scoreboard/ScoreboardUIController.cs
@@ -9,56 +9,56 @@ using Robust.Shared.Log;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controllers;
 
-namespace Content.Client.UserInterface.Scoreboard
+namespace Content.Client.UserInterface.Scoreboard;
+
+public sealed class ScoreboardUIController : UIController, IOnStateEntered<LobbyState>, IOnStateEntered<GameplayState>, IOnStateExited<LobbyState>, IOnStateExited<GameplayState>
 {
-    public sealed class ScoreboardUIController : UIController, IOnStateEntered<LobbyState>, IOnStateEntered<GameplayState>, IOnStateExited<LobbyState>, IOnStateExited<GameplayState>
+
+    [Dependency] private readonly IInputManager _input = default!;
+    [Dependency] private readonly IEntitySystemManager _entitySystem = default!;
+    private ClientGameTicker _gameTicker = default!;
+
+    public void OnStateEntered(LobbyState state)
     {
+        HandleStateEntered();
+    }
 
-        [Dependency] private readonly IInputManager _input = default!;
-        [Dependency] private readonly IEntitySystemManager _entitySystem = default!;
-        private ClientGameTicker _gameTicker = default!;
+    public void OnStateEntered(GameplayState state)
+    {
+        HandleStateEntered();
+    }
 
-        public void OnStateEntered(LobbyState state)
-        {
-            HandleStateEntered();
-        }
+    public void OnStateExited(LobbyState state)
+    {
+        HandleStateExited();
+    }
 
-        public void OnStateEntered(GameplayState state)
-        {
-            HandleStateEntered();
-        }
+    public void OnStateExited(GameplayState state)
+    {
+        HandleStateExited();
+    }
 
-        public void OnStateExited(LobbyState state)
-        {
-            HandleStateExited();
-        }
+    private void HandleStateEntered()
+    {
+        _gameTicker = _entitySystem.GetEntitySystem<ClientGameTicker>();
 
-        public void OnStateExited(GameplayState state)
-        {
-            HandleStateExited();
-        }
+        _input.SetInputCommand(ContentKeyFunctions.OpenScoreboardWindow,
+            InputCmdHandler.FromDelegate(_ =>
+            {
+                if (_gameTicker.Window != null)
+                    OpenScoreboardWindow(_gameTicker.Window);
+            }));
+    }
 
-        private void HandleStateEntered()
-        {
-            _gameTicker = _entitySystem.GetEntitySystem<ClientGameTicker>();
+    private void HandleStateExited()
+    {
+        CommandBinds.Unregister<ScoreboardUIController>();
+    }
 
-            _input.SetInputCommand(ContentKeyFunctions.OpenScoreboardWindow,
-                InputCmdHandler.FromDelegate(_ =>
-                {
-                    if (_gameTicker.Window != null)
-                        OpenScoreboardWindow(_gameTicker.Window);
-                }));
-        }
-
-        private void HandleStateExited()
-        {
-            CommandBinds.Unregister<ScoreboardUIController>();
-        }
-
-        private void OpenScoreboardWindow(RoundEndSummaryWindow window)
-        {
-            window.OpenCenteredRight();
-            window.MoveToFront();
-        }
+    private void OpenScoreboardWindow(RoundEndSummaryWindow window)
+    {
+        window.OpenCenteredRight();
+        window.MoveToFront();
     }
 }
+
