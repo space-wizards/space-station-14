8ce881500fb413f3939bf401dff2b8ac0eedd25e
poeMota <belochuc@gmail.com>
2024-03-16 20:32:32 +0300

    huh

--- Content.Shared/Radio/Components/HeadsetComponent.cs
+++ Content.Shared/Radio/Components/HeadsetComponent.cs
@@ -3,7 +3,7 @@ using Content.Shared.Inventory;
 namespace Content.Shared.Radio.Components;
 
 /// <summary>
-/// This component relays radio messages to the parent entity's chat when equipped.
+///     This component relays radio messages to the parent entity's chat when equipped.
 /// </summary>
 [RegisterComponent]
 public sealed partial class HeadsetComponent : Component
