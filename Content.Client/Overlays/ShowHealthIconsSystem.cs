fc235cb12d90e2de45019614a2dbbc378e35d00f
DoutorWhite <thedoctorwhite@gmail.com>
2024-03-18 02:38:18 -0300

    Minor code cleaning

--- Content.Client/Overlays/ShowHealthIconsSystem.cs
+++ Content.Client/Overlays/ShowHealthIconsSystem.cs
@@ -73,9 +73,7 @@ public sealed class ShowHealthIconsSystem : EquipmentHudSystem<ShowHealthIconsCo
             {
                 // Since there is no MobState for a rotting mob, we have to deal with this case first.
                 if (HasComp<RottingComponent>(entity) && _prototypeMan.TryIndex(damageableComponent.RottingIcon, out var rottingIcon))
-                {
                     result.Add(rottingIcon);
-                }
                 else if (damageableComponent.HealthIcons.TryGetValue(state.CurrentState, out var value) && _prototypeMan.TryIndex(value, out var icon))
                     result.Add(icon);
             }
