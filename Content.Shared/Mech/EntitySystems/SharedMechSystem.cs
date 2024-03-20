4a83c365858830e3b2ff2b94fff501256422c20e
Tayrtahn <tayrtahn@gmail.com>
2024-03-19 23:27:02 -0400

    Code cleanup: Dirty(Comp) (#26238)
    * Replaced uses of Dirty(Component) with Dirty(Uid, Component)
    Modified some systems (notably pulling-related) to use uids.
    
    * Missed a few
    
    * Revert changes to pulling
    
    * No

--- Content.Shared/Mech/EntitySystems/SharedMechSystem.cs
+++ Content.Shared/Mech/EntitySystems/SharedMechSystem.cs
@@ -194,7 +194,7 @@ public abstract class SharedMechSystem : EntitySystem
         if (_net.IsServer)
             _popup.PopupEntity(popupString, uid);
 
-        Dirty(component);
+        Dirty(uid, component);
     }
 
     /// <summary>
@@ -278,7 +278,7 @@ public abstract class SharedMechSystem : EntitySystem
             return false;
 
         component.Energy = FixedPoint2.Clamp(component.Energy + delta, 0, component.MaxEnergy);
-        Dirty(component);
+        Dirty(uid, component);
         UpdateUserInterface(uid, component);
         return true;
     }
@@ -306,7 +306,7 @@ public abstract class SharedMechSystem : EntitySystem
             UpdateAppearance(uid, component);
         }
 
-        Dirty(component);
+        Dirty(uid, component);
         UpdateUserInterface(uid, component);
     }
 
