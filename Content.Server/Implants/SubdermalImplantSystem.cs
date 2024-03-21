86528ec1c8688218048fd6c526176f72b2fb0876
nikthechampiongr <nikvog4@gmail.com>
2024-03-21 16:15:27 +0200

    Fix scram! implant keeping you pulled to someone when teleporting you

--- Content.Server/Implants/SubdermalImplantSystem.cs
+++ Content.Server/Implants/SubdermalImplantSystem.cs
@@ -19,6 +19,8 @@ using Robust.Shared.Physics;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Random;
 using System.Numerics;
+using Content.Shared.Movement.Pulling.Components;
+using Content.Shared.Movement.Pulling.Systems;
 
 namespace Content.Server.Implants;
 
@@ -34,6 +36,7 @@ public sealed class SubdermalImplantSystem : SharedSubdermalImplantSystem
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly SharedTransformSystem _xform = default!;
     [Dependency] private readonly ForensicsSystem _forensicsSystem = default!;
+    [Dependency] private readonly PullingSystem _pullingSystem = default!;
 
     private EntityQuery<PhysicsComponent> _physicsQuery;
 
@@ -97,6 +100,10 @@ public sealed class SubdermalImplantSystem : SharedSubdermalImplantSystem
 
         if (!TryComp<ScramImplantComponent>(uid, out var implant))
             return;
+
+        // If we do not stop pulling then if the user is cuffed they will teleport they will be teleporter with their captor.
+        if (TryComp<PullableComponent>(ent, out var pull) && _pullingSystem.IsPulled(ent, pull))
+            _pullingSystem.TryStopPull(ent, pull);
 
         var xform = Transform(ent);
         var entityCoords = xform.Coordinates.ToMap(EntityManager, _xform);
