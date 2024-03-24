27d5d0c7052695a90e75fcb9c67875b2724a20af
Hannah Giovanna Dawson <karakkaraz@gmail.com>
2024-03-24 17:36:16 +0000

    Imagine tests handling IOC correctly chat

--- Content.Server/Chat/Commands/DeleteChatMessageCommand.cs
+++ Content.Server/Chat/Commands/DeleteChatMessageCommand.cs
@@ -8,14 +8,14 @@ namespace Content.Server.Chat.Commands
     [AdminCommand(AdminFlags.Admin)]
     internal sealed class DeleteChatMessageCommand : IConsoleCommand
     {
-        [Dependency] private readonly ChatRepository _repo = default!;
-
         public string Command => "delmsg";
         public string Description => "Delete a specific chat message";
         public string Help => "delmsg <id>";
 
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
+            var repo = IoCManager.Resolve<ChatRepository>();
+
             if (!uint.TryParse(args[0], out var result))
             {
                 shell.WriteError("can't delete chat message: invalid number argument");
@@ -23,7 +23,7 @@ namespace Content.Server.Chat.Commands
                 return;
             }
 
-            _repo.Delete(result);
+            repo.Delete(result);
         }
     }
 }
