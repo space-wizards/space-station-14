a03ddcdcf7763435681b032a9de00ba5c6b65c53
nikthechampiongr <nikvog4@gmail.com>
2024-03-24 02:20:45 +0200

    Restrict door remotes to only being able to manipulate doors relevant to their type.

--- Content.Server/Remotes/DoorRemoteSystem.cs
+++ Content.Server/Remotes/DoorRemoteSystem.cs
@@ -81,10 +81,8 @@ namespace Content.Server.Remotes
                 return;
             }
 
-            // Holding the door remote grants you access to the relevant doors IN ADDITION to what ever access you had.
-            // This access is enforced in _doorSystem.HasAccess when it calls _accessReaderSystem.IsAllowed
             if (TryComp<AccessReaderComponent>(args.Target, out var accessComponent)
-                && !_doorSystem.HasAccess(args.Target.Value, args.User, doorComp, accessComponent))
+                && !_doorSystem.HasAccess(args.Target.Value, args.Used, doorComp, accessComponent))
             {
                 _doorSystem.Deny(args.Target.Value, doorComp, args.User);
                 ShowPopupToUser("door-remote-denied", args.User);
@@ -94,10 +92,7 @@ namespace Content.Server.Remotes
             switch (component.Mode)
             {
                 case OperatingMode.OpenClose:
-                    // Note we provide args.User here to TryToggleDoor as the "user"
-                    // This means that the door will look at all access items carryed by the player for access, including
-                    // this remote, but also including anything else they are carrying such as a PDA or ID card.
-                    if (_doorSystem.TryToggleDoor(args.Target.Value, doorComp, args.User))
+                    if (_doorSystem.TryToggleDoor(args.Target.Value, doorComp, args.Used))
                         _adminLogger.Add(LogType.Action, LogImpact.Medium, $"{ToPrettyString(args.User):player} used {ToPrettyString(args.Used)} on {ToPrettyString(args.Target.Value)}: {doorComp.State}");
                     break;
                 case OperatingMode.ToggleBolts:
@@ -105,7 +100,7 @@ namespace Content.Server.Remotes
                     {
                         if (!boltsComp.BoltWireCut)
                         {
-                            _doorSystem.SetBoltsDown((args.Target.Value, boltsComp), !boltsComp.BoltsDown, args.User);
+                            _doorSystem.SetBoltsDown((args.Target.Value, boltsComp), !boltsComp.BoltsDown, args.Used);
                             _adminLogger.Add(LogType.Action, LogImpact.Medium, $"{ToPrettyString(args.User):player} used {ToPrettyString(args.Used)} on {ToPrettyString(args.Target.Value)} to {(boltsComp.BoltsDown ? "" : "un")}bolt it");
                         }
                     }
