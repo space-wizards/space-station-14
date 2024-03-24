834a1d9445fbd9872105b5c44bb78635e401d758
Kira Bridgeton <161087999+Verbalase@users.noreply.github.com>
2024-03-24 09:19:37 -0500

    Revert "Wizard Antagonist"

--- Content.Server/Magic/Components/SpellbookComponent.cs
+++ Content.Server/Magic/Components/SpellbookComponent.cs
@@ -32,8 +32,4 @@ public sealed partial class SpellbookComponent : Component
     [DataField("learnPermanently")]
     [ViewVariables(VVAccess.ReadWrite)]
     public bool LearnPermanently;
-
-    [DataField("deleteAfterLearn")]
-    [ViewVariables(VVAccess.ReadWrite)]
-    public bool DeleteAfterLearn;
 }
