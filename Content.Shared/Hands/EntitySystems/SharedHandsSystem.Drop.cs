25588241fb390efe7c104a2c9695dcba0eeb5d71
Tayrtahn <tayrtahn@gmail.com>
2024-03-25 15:37:24 -0400

    This one seems questionable too

--- Content.Shared/Hands/EntitySystems/SharedHandsSystem.Drop.cs
+++ Content.Shared/Hands/EntitySystems/SharedHandsSystem.Drop.cs
@@ -167,22 +167,22 @@ public abstract partial class SharedHandsSystem
     private Vector2 GetFinalDropCoordinates(EntityUid user, MapCoordinates origin, MapCoordinates target)
     {
         var dropVector = target.Position - origin.Position;
-        var requestedDropDistanceSquared = dropVector.LengthSquared();
-        var dropLengthSquared = dropVector.LengthSquared();
+        var requestedDropDistance = dropVector.Length();
+        var dropLength = dropVector.Length();
 
         if (ShouldIgnoreRestrictions(user))
         {
-            if (dropLengthSquared > SharedInteractionSystem.InteractionRange)
+            if (dropVector.Length() > SharedInteractionSystem.InteractionRange)
             {
                 dropVector = dropVector.Normalized() * SharedInteractionSystem.InteractionRange;
                 target = new MapCoordinates(origin.Position + dropVector, target.MapId);
             }
 
-            dropLengthSquared = _interactionSystem.UnobstructedDistanceSquared(origin, target, predicate: e => e == user);
+            dropLength = _interactionSystem.UnobstructedDistance(origin, target, predicate: e => e == user);
         }
 
-        if (dropLengthSquared < requestedDropDistanceSquared)
-            return origin.Position + dropVector.Normalized() * MathF.Sqrt(dropLengthSquared);
+        if (dropLength < requestedDropDistance)
+            return origin.Position + dropVector.Normalized() * dropLength;
         return target.Position;
     }
 
