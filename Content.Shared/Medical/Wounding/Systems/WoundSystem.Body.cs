6cb918263c874a3de6a0b38600f0d6ae56819d01
Jezithyr <Jezithyr@gmail.com>
2024-03-27 18:22:34 -0700

    Fix merge conflicts part 2

--- Content.Shared/Medical/Wounding/Systems/WoundSystem.Body.cs
+++ Content.Shared/Medical/Wounding/Systems/WoundSystem.Body.cs
@@ -18,15 +18,14 @@ public sealed partial class WoundSystem
     private void OnWoundableRemovedFromBody(EntityUid woundableEnt, WoundableComponent woundableComp, ref BodyPartRemovedFromBodyEvent args)
     {
         var woundable = new Entity<WoundableComponent>(woundableEnt, woundableComp);
-        var body = new Entity<BodyComponent>(args.BodyUid, args.Body);
         //We don't need an insure where because we don't remove bodyparts during mapInit
         //(If we do in the future for some reason, add an ensure in here or things will break)
         woundableComp.Body = null;
         foreach (var wound in GetAllWounds(woundable))
         {
-            var ev = new WoundRemovedFromBody(body, woundable, wound);
+            var ev = new WoundRemovedFromBody(args.OldBody, woundable, wound);
             RaiseLocalEvent(wound, ref ev);
-            RaiseLocalEvent(body, ref ev);
+            RaiseLocalEvent(args.OldBody, ref ev);
         }
         Dirty(woundableEnt, woundableComp);
     }
@@ -34,17 +33,15 @@ public sealed partial class WoundSystem
     private void OnWoundableAddedToBody(EntityUid woundableEnt, WoundableComponent woundableComp, ref BodyPartAddedToBodyEvent args)
     {
         var woundable = new Entity<WoundableComponent>(woundableEnt, woundableComp);
-        var body = new Entity<BodyComponent>(args.BodyUid, args.Body);
-
         //the ensure is needed because this gets called by partAddedToBodyEvent which is raised from bodypart's mapInit
         _containerSystem.EnsureContainer<Container>(woundableEnt, WoundableComponent.WoundableContainerId);
 
-        woundableComp.Body = args.BodyUid;
+        woundableComp.Body = args.Body;
         foreach (var wound in GetAllWounds(woundable))
         {
-            var ev = new WoundAppliedToBody(body, woundable, wound);
+            var ev = new WoundAppliedToBody(args.Body, woundable, wound);
             RaiseLocalEvent(wound, ref ev);
-            RaiseLocalEvent(body, ref ev);
+            RaiseLocalEvent(args.Body, ref ev);
         }
         Dirty(woundableEnt, woundableComp);
     }
