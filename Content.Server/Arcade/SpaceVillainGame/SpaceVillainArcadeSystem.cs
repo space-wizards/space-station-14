b1ba6b5bb615615cc58f255dc9fe84c5ffe8822b
Tayrtahn <tayrtahn@gmail.com>
2024-03-28 02:28:45 -0400

    Arcade machine improvements (#24200)
    * Give 'em something to talk about
    
    * Wire panel visuals
    
    * Wire graphics tweak
    
    * More ads and thanks
    
    * More ads for a noisy arcade
    
    * New screen for space villain machines
    
    * Implement EmitSoundIntervalComponent and a bunch of arcade noises
    
    * Require power for sounds
    
    * Allow earlier startup intervals
    
    * Orange glow
    
    * Audio attributions
    
    * Include the PR link
    
    * Replace EmitSoundInterval with expanded SpamEmitSound
    
    * Remove pacman-themed arcade sounds
    
    * Documentation good.
    
    * Updated methods to use Entity<T>
    
    * Refactored SpamEmitSound to get rid of accumulator and chance.
    
    * Fixed prewarm logic
    
    * Moved stuff to Shared
    
    * Fix outdated YAML
    
    * Better prediction, auto pause handling
    
    * Make enable/disable reset the timer instead of trying to save it.

--- Content.Server/Arcade/SpaceVillainGame/SpaceVillainArcadeSystem.cs
+++ Content.Server/Arcade/SpaceVillainGame/SpaceVillainArcadeSystem.cs
@@ -1,5 +1,6 @@
 using Content.Server.Power.Components;
 using Content.Shared.UserInterface;
+using Content.Server.Advertise;
 using static Content.Shared.Arcade.SharedSpaceVillainArcadeComponent;
 using Robust.Server.GameObjects;
 using Robust.Shared.Audio;
@@ -13,6 +14,7 @@ public sealed partial class SpaceVillainArcadeSystem : EntitySystem
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
     [Dependency] private readonly UserInterfaceSystem _uiSystem = default!;
+    [Dependency] private readonly AdvertiseSystem _advertise = default!;
 
     public override void Initialize()
     {
@@ -22,6 +24,7 @@ public sealed partial class SpaceVillainArcadeSystem : EntitySystem
         SubscribeLocalEvent<SpaceVillainArcadeComponent, AfterActivatableUIOpenEvent>(OnAfterUIOpenSV);
         SubscribeLocalEvent<SpaceVillainArcadeComponent, SpaceVillainArcadePlayerActionMessage>(OnSVPlayerAction);
         SubscribeLocalEvent<SpaceVillainArcadeComponent, PowerChangedEvent>(OnSVillainPower);
+        SubscribeLocalEvent<SpaceVillainArcadeComponent, BoundUIClosedEvent>(OnBoundUIClosed);
     }
 
     /// <summary>
@@ -79,6 +82,7 @@ public sealed partial class SpaceVillainArcadeSystem : EntitySystem
             case PlayerAction.Heal:
             case PlayerAction.Recharge:
                 component.Game.ExecutePlayerAction(uid, msg.PlayerAction, component);
+                component.ShouldSayThankYou = true; // Any sort of gameplay action counts
                 break;
             case PlayerAction.NewGame:
                 _audioSystem.PlayPvs(component.NewGameSound, uid, AudioParams.Default.WithVolume(-4f));
@@ -106,5 +110,19 @@ public sealed partial class SpaceVillainArcadeSystem : EntitySystem
 
         if (_uiSystem.TryGetUi(uid, SpaceVillainArcadeUiKey.Key, out var bui))
             _uiSystem.CloseAll(bui);
+
+        component.ShouldSayThankYou = false;
+    }
+
+    private void OnBoundUIClosed(Entity<SpaceVillainArcadeComponent> ent, ref BoundUIClosedEvent args)
+    {
+        if (args.UiKey is not SpaceVillainArcadeUiKey || (SpaceVillainArcadeUiKey) args.UiKey != SpaceVillainArcadeUiKey.Key)
+            return;
+
+        if (ent.Comp.ShouldSayThankYou && TryComp<AdvertiseComponent>(ent.Owner, out var advertise))
+        {
+            _advertise.SayThankYou(ent.Owner, advertise);
+            ent.Comp.ShouldSayThankYou = false;
+        }
     }
 }
