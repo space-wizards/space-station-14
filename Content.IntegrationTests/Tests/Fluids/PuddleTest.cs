6dd8c8df649f1e44ccb9a612edc3b9010c8645bc
Jake Huxell <JakeHuxell@pm.me>
2024-03-28 01:12:09 -0400

    remove deprecated entity coordinate extension functions. Reduces warning count by approximately 50

--- Content.IntegrationTests/Tests/Fluids/PuddleTest.cs
+++ Content.IntegrationTests/Tests/Fluids/PuddleTest.cs
@@ -67,7 +67,8 @@ namespace Content.IntegrationTests.Tests.Fluids
 
             await server.WaitAssertion(() =>
             {
-                var coordinates = grid.ToCoordinates();
+                var gridOwner = grid.Owner;
+                var coordinates = gridOwner.ToCoordinates();
                 var solution = new Solution("Water", FixedPoint2.New(20));
 
                 Assert.That(spillSystem.TrySpillAt(coordinates, solution, out _), Is.False);
