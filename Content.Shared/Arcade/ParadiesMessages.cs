731f06853af30288ebaa98689144d3af1352b2ac
Simon <63975668+Simyon264@users.noreply.github.com>
2024-03-30 02:50:58 +0100

    Arcade that sends you to OpenDream Paradise station (#26476)
    * The actual april fools joke is the shitcode I produced
    
    * Make text clearer.
    
    * Spawner adjustments
    
    * Improve image size.
    Thanks .landwhale (discord)!
    
    * Fix mismatching duration

---
+++ Content.Shared/Arcade/ParadiesMessages.cs
@@ -0,0 +1,26 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Arcade;
+
+public static class ParadiesMessages
+{
+
+    [Serializable, NetSerializable]
+    public sealed class ParadiseArcadeConnectButtonPressedEvent : BoundUserInterfaceMessage
+    {
+    }
+
+    /// <summary>
+    /// Tells the client to connect to the specified destination.
+    /// </summary>
+    [Serializable, NetSerializable]
+    public sealed class ParadiseArcadeConnectEvent : BoundUserInterfaceMessage
+    {
+        public string Destination { get; }
+
+        public ParadiseArcadeConnectEvent(string destination)
+        {
+            Destination = destination;
+        }
+    }
+}
