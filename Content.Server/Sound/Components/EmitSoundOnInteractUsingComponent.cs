1de16edcb40a7a285b6564a5f506c67833ccaf94
blue3212 <luckywill339@gmail.com>
2024-03-29 20:50:54 -0400

    File-scoping

--- Content.Server/Sound/Components/EmitSoundOnInteractUsingComponent.cs
+++ Content.Server/Sound/Components/EmitSoundOnInteractUsingComponent.cs
@@ -1,14 +1,13 @@
 using Content.Shared.Sound.Components;
 
-namespace Content.Server.Sound.Components
+namespace Content.Server.Sound.Components;
+
+/// <summary>
+/// Whenever this item is used upon by a specific entity prototype in the hand of a user, play a sound
+/// </summary>
+[RegisterComponent]
+public sealed partial class EmitSoundOnInteractUsingComponent : BaseEmitSoundComponent
 {
-    /// <summary>
-    /// Whenever this item is used upon by a specific entity prototype in the hand of a user, play a sound
-    /// </summary>
-    [RegisterComponent]
-    public sealed partial class EmitSoundOnInteractUsingComponent : BaseEmitSoundComponent
-    {
-        [DataField("UsedItemID")]
-        public string UsedItemID = "";
-    }
+    [DataField("UsedItemID")]
+    public string UsedItemID = "";
 }
