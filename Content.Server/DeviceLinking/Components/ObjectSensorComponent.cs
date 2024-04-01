0a62f153f51d12eb129ce98f38963f65470776f9
I.K <tripwiregamer@gmail.com>
2024-04-01 08:27:19 -0500

    documentation

--- Content.Server/DeviceLinking/Components/ObjectSensorComponent.cs
+++ Content.Server/DeviceLinking/Components/ObjectSensorComponent.cs
@@ -7,11 +7,14 @@ using Robust.Shared.Prototypes;
 namespace Content.Server.DeviceLinking.Components;
 
 /// <summary>
-/// An anchored subfloor thing that detects entities that are on the same tile.
+/// An anchored object that detects entities that touch it.
 /// </summary>
 [RegisterComponent, Access(typeof(ObjectSensorSystem))]
 public sealed partial class ObjectSensorComponent : Component
 {
+    /// <summary>
+    ///    The source ports used for each of the outputs.
+    /// </summary>
     [DataField]
     public ProtoId<SourcePortPrototype> OutputPort1 = "ObjectSensor1Object";
 
@@ -24,12 +27,21 @@ public sealed partial class ObjectSensorComponent : Component
     [DataField]
     public ProtoId<SourcePortPrototype> OutputPort4OrMore = "ObjectSensor4OrMoreObjects";
 
+    /// <summary>
+    ///    How the mode is switched
+    /// </summary>
     [DataField]
     public ProtoId<ToolQualityPrototype> CycleQuality = "Screwing";
 
+    /// <summary>
+    ///    How many entities are contacting it
+    /// </summary>
     [DataField]
     public int Contacting;
 
+    /// <summary>
+    ///    What type of entities will be detected
+    /// </summary>
     [DataField]
     public ObjectSensorMode Mode = ObjectSensorMode.Living;
 
