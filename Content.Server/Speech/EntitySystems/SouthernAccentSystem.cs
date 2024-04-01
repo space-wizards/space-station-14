c9286a48c018aa456348c79b3c3a4f77b16b1e40
UBlueberry <cowboyscantshootslow@gmail.com>
2024-03-31 23:03:04 -0400

    I SHOULDA THOUGHT OF THIS SOONER, Y'ALL!

--- Content.Server/Speech/EntitySystems/SouthernAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/SouthernAccentSystem.cs
@@ -21,8 +21,14 @@ public sealed class SouthernAccentSystem : EntitySystem
 
         //They shoulda started runnin' an' hidin' from me!
         message = Regex.Replace(message, @"ing\b", "in'");
+        message = Regex.Replace(message, @"ING\b", "IN'");
+
         message = Regex.Replace(message, @"\band\b", "an'");
+        message = Regex.Replace(message, @"\bAND\b", "AN'");
+
         message = Regex.Replace(message, "d've", "da");
+        message = Regex.Replace(message, "D'VE", "DA");
+
         args.Message = message;
     }
 };
