d43f9ede86df5ba8cdffc14aee26f06b8df82f74
deltanedas <@deltanedas:kde.org>
2024-04-07 00:37:37 +0100

    add SealingCabinet system

---
+++ Content.Shared/Cabinet/SealingCabinetSystem.cs
@@ -0,0 +1,47 @@
+using Content.Shared.Emag.Systems;
+using Content.Shared.Nutrition.Components;
+using Content.Shared.Nutrition.EntitySystems;
+using Content.Shared.Popups;
+
+namespace Content.Shared.Cabinet;
+
+public sealed class SealingCabinetSystem : EntitySystem
+{
+    [Dependency] private readonly ItemCabinetSystem _cabinet = default!;
+    [Dependency] private readonly OpenableSystem _openable = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<SealingCabinetComponent, OpenableOpenAttemptEvent>(OnOpenAttempt);
+        SubscribeLocalEvent<SealingCabinetComponent, GotEmaggedEvent>(OnEmagged);
+    }
+
+    private void OnOpenAttempt(Entity<SealingCabinetComponent> ent, ref OpenableOpenAttemptEvent args)
+    {
+        var cabinet = (ent, Comp<ItemCabinetComponent>(ent));
+        if (!_cabinet.HasItem(cabinet))
+            return;
+
+        args.Cancelled = true;
+        if (args.User is {} user)
+            _popup.PopupClient(Loc.GetString(ent.Comp.SealedPopup, ("container", ent.Owner)), ent, user);
+    }
+
+    private void OnEmagged(Entity<SealingCabinetComponent> ent, ref GotEmaggedEvent args)
+    {
+        if (!ent.Comp.Emaggable)
+            return;
+
+        var cabinet = (ent, Comp<ItemCabinetComponent>(ent));
+        if (!_cabinet.HasItem(cabinet) || _openable.IsOpen(ent))
+            return;
+
+        _openable.SetOpen(ent, true);
+
+        args.Handled = true;
+        args.Repeatable = true;
+    }
+}
