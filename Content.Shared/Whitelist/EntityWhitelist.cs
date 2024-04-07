645fb24adbd48efbecb844d2ff8f92e5b2222c4d
Errant <35878406+Errant-4@users.noreply.github.com>
2024-04-07 13:06:32 +0200

    namespace and misc refactor

--- Content.Shared/Whitelist/EntityWhitelist.cs
+++ Content.Shared/Whitelist/EntityWhitelist.cs
@@ -5,112 +5,115 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototy
 
 namespace Content.Shared.Whitelist;
 
+/// <summary>
+///     Used to determine whether an entity fits a certain whitelist.
+///     Does not whitelist by prototypes, since that is undesirable; you're better off just adding a tag to all
+///     entity prototypes that need to be whitelisted, and checking for that.
+/// </summary>
+/// <code>
+/// whitelist:
+///   tags:
+///   - Cigarette
+///   - FirelockElectronics
+///   components:
+///   - Buckle
+///   - AsteroidRock
+///   sizes:
+///   - Tiny
+///   - Large
+/// </code>
+[DataDefinition]
+[Serializable, NetSerializable]
+public sealed partial class EntityWhitelist
+{
     /// <summary>
-    ///     Used to determine whether an entity fits a certain whitelist.
-    ///     Does not whitelist by prototypes, since that is undesirable; you're better off just adding a tag to all
-    ///     entity prototypes that need to be whitelisted, and checking for that.
+    ///     Component names that are allowed in the whitelist.
     /// </summary>
-    /// <code>
-    /// whitelist:
-    ///   tags:
-    ///   - Cigarette
-    ///   - FirelockElectronics
-    ///   components:
-    ///   - Buckle
-    ///   - AsteroidRock
-    ///   sizes:
-    ///   - Tiny
-    ///   - Large
-    /// </code>
-    [DataDefinition]
-    [Serializable, NetSerializable]
-    public sealed partial class EntityWhitelist
+    [DataField] public string[]? Components;
+    // TODO yaml validation
+
+    /// <summary>
+    ///     Item sizes that are allowed in the whitelist.
+    /// </summary>
+    [DataField("sizes", customTypeSerializer:typeof(PrototypeIdListSerializer<ItemSizePrototype>))]
+    public List<string>? Sizes;
+
+    [NonSerialized]
+    private List<ComponentRegistration>? _registrations;
+
+    /// <summary>
+    ///     Tags that are allowed in the whitelist.
+    /// </summary>
+    [DataField("tags", customTypeSerializer:typeof(PrototypeIdListSerializer<TagPrototype>))]
+    public List<string>? Tags;
+
+    /// <summary>
+    ///     If false, an entity only requires one of these components or tags to pass the whitelist. If true, an
+    ///     entity requires to have ALL of these components and tags to pass.
+    ///     The "Sizes" criteria will ignores this, since an item can only have one size.
+    /// </summary>
+    [DataField]
+    public bool RequireAll;
+
+    [Dependency] private readonly ILogManager _logMan = default!;
+
+    public void UpdateRegistrations()
     {
-        /// <summary>
-        ///     Component names that are allowed in the whitelist.
-        /// </summary>
-        [DataField] public string[]? Components = null;
-        // TODO yaml validation
+        if (Components == null)
+            return;
 
-        /// <summary>
-        ///     Item sizes that are allowed in the whitelist.
-        /// </summary>
-        [DataField("sizes", customTypeSerializer:typeof(PrototypeIdListSerializer<ItemSizePrototype>))]
-        public List<string>? Sizes = null;
-
-        [NonSerialized]
-        private List<ComponentRegistration>? _registrations = null;
-
-        /// <summary>
-        ///     Tags that are allowed in the whitelist.
-        /// </summary>
-        [DataField("tags", customTypeSerializer:typeof(PrototypeIdListSerializer<TagPrototype>))]
-        public List<string>? Tags = null;
-
-        /// <summary>
-        ///     If false, an entity only requires one of these components or tags to pass the whitelist. If true, an
-        ///     entity requires to have ALL of these components and tags to pass.
-        ///     A "Size" whitelist will ignores this, since an item can only have one size
-        /// </summary>
-        [DataField("requireAll")]
-        public bool RequireAll = false;
-
-        public void UpdateRegistrations()
+        var compFact = IoCManager.Resolve<IComponentFactory>();
+        _registrations = new List<ComponentRegistration>();
+        foreach (var name in Components)
         {
-            if (Components == null) return;
-
-            var compfact = IoCManager.Resolve<IComponentFactory>();
-            _registrations = new List<ComponentRegistration>();
-            foreach (var name in Components)
+            var availability = compFact.GetComponentAvailability(name);
+            if (compFact.TryGetRegistration(name, out var registration)
+                && availability == ComponentAvailability.Available)
             {
-                var availability = compfact.GetComponentAvailability(name);
-                if (compfact.TryGetRegistration(name, out var registration)
-                    && availability == ComponentAvailability.Available)
-                {
-                    _registrations.Add(registration);
-                }
-                else if (availability == ComponentAvailability.Unknown)
-                {
-                    Logger.Warning($"Unknown component name {name} passed to EntityWhitelist!");
-                }
+                _registrations.Add(registration);
             }
-        }
-
-        /// <summary>
-        ///     Returns whether a given entity fits the whitelist.
-        /// </summary>
-        public bool IsValid(EntityUid uid, IEntityManager? entityManager = null)
-        {
-            if (Components != null && _registrations == null)
-                UpdateRegistrations();
-
-            IoCManager.Resolve(ref entityManager);
-            if (_registrations != null)
+            else if (availability == ComponentAvailability.Unknown)
             {
-                foreach (var reg in _registrations)
-                {
-                    if (entityManager.HasComponent(uid, reg.Type))
-                    {
-                        if (!RequireAll)
-                            return true;
-                    }
-                    else if (RequireAll)
-                        return false;
-                }
+                Logger.Warning($"Unknown component name {name} passed to EntityWhitelist!");
             }
-
-            if (Sizes != null && entityManager.TryGetComponent(uid, out ItemComponent? itemComp))
-            {
-                if (Sizes.Contains(itemComp.Size))
-                    return true;
-            }
-
-            if (Tags != null && entityManager.TryGetComponent(uid, out TagComponent? tags))
-            {
-                var tagSystem = entityManager.System<TagSystem>();
-                return RequireAll ? tagSystem.HasAllTags(tags, Tags) : tagSystem.HasAnyTag(tags, Tags);
-            }
-
-            return false;
         }
     }
+
+    /// <summary>
+    ///     Returns whether a given entity fits the whitelist.
+    /// </summary>
+    public bool IsValid(EntityUid uid, IEntityManager? entityManager = null)
+    {
+        if (Components != null && _registrations == null)
+            UpdateRegistrations();
+
+        IoCManager.Resolve(ref entityManager);
+        if (_registrations != null)
+        {
+            foreach (var reg in _registrations)
+            {
+                if (entityManager.HasComponent(uid, reg.Type))
+                {
+                    if (!RequireAll)
+                        return true;
+                }
+                else if (RequireAll)
+                    return false;
+            }
+        }
+
+        if (Sizes != null && entityManager.TryGetComponent(uid, out ItemComponent? itemComp))
+        {
+            if (Sizes.Contains(itemComp.Size))
+                return true;
+        }
+
+        if (Tags != null && entityManager.TryGetComponent(uid, out TagComponent? tags))
+        {
+            var tagSystem = entityManager.System<TagSystem>();
+            return RequireAll ? tagSystem.HasAllTags(tags, Tags) : tagSystem.HasAnyTag(tags, Tags);
+        }
+
+        return false;
+    }
+}
