0e2fb002fed16dac28041beddbcb53a53fb42d94
Tainakov <136968973+Tainakov@users.noreply.github.com>
2024-04-11 23:09:38 +0000

    [Admin QOL] Adminhelp flag permissions

--- Content.Server/Chat/Commands/AdminChatCommand.cs
+++ Content.Server/Chat/Commands/AdminChatCommand.cs
@@ -6,6 +6,7 @@ using Robust.Shared.Console;
 namespace Content.Server.Chat.Commands
 {
     [AdminCommand(AdminFlags.Admin)]
+    [AdminCommand(AdminFlags.Adminhelp)]
     internal sealed class AdminChatCommand : IConsoleCommand
     {
         public string Command => "asay";
