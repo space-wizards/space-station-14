14dc5dd73a07520c2331eadd348a73b79b809760
Mr. 27 <koolthunder019@gmail.com>
2024-04-13 14:31:16 -0400

    add absorbable component

---
+++ Content.Shared/Changeling/AbsorbableComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Shared.Changeling.Components;
+
+/// <summary>
+/// Component give to people who are able to be absorbed/have DNA stolen by changelings.
+/// </summary>
+[RegisterComponent]
+public sealed partial class AbsorbableComponent : Component
+{
+}
