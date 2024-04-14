4080653d2ffe7e334bb23c2198d539002031028e
SlamBamActionman <slambamactionman@gmail.com>
2024-04-14 13:39:55 +0200

    Fixes recipe bug

--- Content.Shared/Chemistry/Components/Solution.cs
+++ Content.Shared/Chemistry/Components/Solution.cs
@@ -496,7 +496,7 @@ namespace Content.Shared.Chemistry.Components
             {
                 var (reagent, curQuantity) = Contents[i];
 
-                if(reagent != toRemove.Reagent)
+                if(reagent.Prototype != toRemove.Reagent.Prototype)
                     continue;
 
                 var newQuantity = curQuantity - toRemove.Quantity;
