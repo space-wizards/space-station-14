98cbfe673429479d25c67e67e0c91bec3dc5b183
Beck <beck314159@hotmail.com>
2024-04-13 23:46:09 -0700

    Added comments

--- Content.Server/Radio/Components/RadioJammerComponent.cs
+++ Content.Server/Radio/Components/RadioJammerComponent.cs
@@ -14,22 +14,41 @@ public sealed partial class RadioJammerComponent : SharedRadioJammerComponent
     [DataDefinition]
     public partial struct RadioJamSetting
     {
+        /// <summary>
+        /// Power usage per second when enabled.
+        /// </summary>
         [DataField(required: true)]
         public float Wattage;
 
+        /// <summary>
+        /// Range of the jammer.
+        /// </summary>
         [DataField(required: true)]
         public float Range;
 
+        /// <summary>
+        /// The message that is displayed when switched 
+        /// to this setting.
+        /// </summary>
         [DataField(required: true)]
         public LocId Message = string.Empty;
 
+        /// <summary>
+        /// Name of the setting.
+        /// </summary>
         [DataField(required: true)]
         public LocId Name = string.Empty;
     }
 
+    /// <summary>
+    /// List of all the settings for the radio jammer.
+    /// </summary>
     [DataField(required: true)]
     public List<RadioJamSetting> Settings = new();
 
+    /// <summary>
+    /// Index of the currently selected setting.
+    /// </summary>
     [DataField]
     public int SelectedPowerLevel = 1;
 }
