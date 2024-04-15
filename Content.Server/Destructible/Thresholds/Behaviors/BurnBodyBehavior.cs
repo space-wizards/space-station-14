2e642e86f9b943e11cd408d0312b5d1c7ad2d953
QuietlyWhisper <121047731+QuietlyWhisper@users.noreply.github.com>
2024-04-15 00:35:36 -0400

    fix namespace

--- Content.Server/Destructible/Thresholds/Behaviors/BurnBodyBehavior.cs
+++ Content.Server/Destructible/Thresholds/Behaviors/BurnBodyBehavior.cs
@@ -4,30 +4,29 @@ using Content.Shared.Popups;
 using JetBrains.Annotations;
 using Robust.Server.GameObjects;
 
-namespace Content.Server.Destructible.Thresholds.Behaviors
+namespace Content.Server.Destructible.Thresholds.Behaviors;
+
+[UsedImplicitly]
+[DataDefinition]
+public sealed partial class BurnBodyBehavior : IThresholdBehavior
 {
-    [UsedImplicitly]
-    [DataDefinition]
-    public sealed partial class BurnBodyBehavior : IThresholdBehavior
+
+    public void Execute(EntityUid bodyId, DestructibleSystem system, EntityUid? cause = null)
     {
+        var transformSystem = system.EntityManager.System<TransformSystem>();
+        var inventorySystem = system.EntityManager.System<InventorySystem>();
+        var sharedPopupSystem = system.EntityManager.System<SharedPopupSystem>();
 
-        public void Execute(EntityUid bodyId, DestructibleSystem system, EntityUid? cause = null)
+        if (!system.EntityManager.TryGetComponent<InventoryComponent>(bodyId, out var comp))
+            return;
+
+        foreach (var item in inventorySystem.GetHandOrInventoryEntities(bodyId))
         {
-            var transformSystem = system.EntityManager.System<TransformSystem>();
-            var inventorySystem = system.EntityManager.System<InventorySystem>();
-            var sharedPopupSystem = system.EntityManager.System<SharedPopupSystem>();
-
-            if (!system.EntityManager.TryGetComponent<InventoryComponent>(bodyId, out var comp))
-                return;
-
-            foreach (var item in inventorySystem.GetHandOrInventoryEntities(bodyId))
-            {
-                transformSystem.AttachToGridOrMap(item);
-            }
-
-            sharedPopupSystem.PopupCoordinates(Loc.GetString("bodyburn-text-others", ("name", bodyId)), transformSystem.GetMoverCoordinates(bodyId), PopupType.LargeCaution);
-
-            system.EntityManager.QueueDeleteEntity(bodyId);
+            transformSystem.AttachToGridOrMap(item);
         }
+
+        sharedPopupSystem.PopupCoordinates(Loc.GetString("bodyburn-text-others", ("name", bodyId)), transformSystem.GetMoverCoordinates(bodyId), PopupType.LargeCaution);
+
+        system.EntityManager.QueueDeleteEntity(bodyId);
     }
 }
