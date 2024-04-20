5c69031d116c3ad9c05a80525323d97d7469829e
no <165581243+pissdemon@users.noreply.github.com>
2024-04-20 08:27:38 +0200

    Admin log shuttle gibbing + Make shuttle gibbing and immovable rods drop organs and correctly put player in ghost (#27114)
    * Add admin log when someone gets gibbed by a shuttle
    
    * Make shuttle gibbing work properly again
    
    * Fix immovable rod gibbing, no longer puts you into nullspace
    
    * Update Content.Server/ImmovableRod/ImmovableRodSystem.cs
    
    * Update Content.Server/Shuttles/Systems/ShuttleSystem.FasterThanLight.cs
    
    ---------
    
    Co-authored-by: metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>

--- Content.Server/Shuttles/Systems/ShuttleSystem.FasterThanLight.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.FasterThanLight.cs
@@ -6,6 +6,7 @@ using Content.Server.Shuttles.Events;
 using Content.Server.Station.Events;
 using Content.Shared.Body.Components;
 using Content.Shared.Buckle.Components;
+using Content.Shared.Database;
 using Content.Shared.Ghost;
 using Content.Shared.Maps;
 using Content.Shared.Parallax;
@@ -867,6 +868,8 @@ public sealed partial class ShuttleSystem
 
                 if (_bodyQuery.TryGetComponent(ent, out var mob))
                 {
+                    _logger.Add(LogType.Gib, LogImpact.Extreme, $"{ToPrettyString(ent):player} got gibbed by the shuttle" +
+                                                                $" {ToPrettyString(uid)} arriving from FTL at {xform.Coordinates:coordinates}");
                     var gibs = _bobby.GibBody(ent, body: mob);
                     _immuneEnts.UnionWith(gibs);
                     continue;
