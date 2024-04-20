f25c408f1ae290298d39014e3c324444ccdd3cf3
Jezithyr <Jezithyr@gmail.com>
2024-04-20 00:27:02 -0700

    created files for respiration

---
+++ Content.Shared/Medical/Respiration/Components/RespiratorComponent.cs
@@ -0,0 +1,24 @@
+﻿using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Shared.Medical.Respiration.Components;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class RespiratorComponent  : Component
+{
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    public TimeSpan NextUpdate;
+
+    /// <summary>
+    ///     The interval between updates. CycleTime (Inhale/exhale time) is added on top of this
+    /// </summary>
+    [DataField]
+    public TimeSpan UpdateRate = TimeSpan.FromSeconds(3);
+
+    [DataField]
+    public TimeSpan CycleRate = TimeSpan.FromSeconds(1);
+
+
+}
---
+++ Content.Shared/Medical/Respiration/Systems/RespirationSystem.cs
@@ -0,0 +1,13 @@
+﻿namespace Content.Shared.Medical.Respiration.Systems;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class RespirationSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        
+    }
+}
