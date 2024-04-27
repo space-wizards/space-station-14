d8982545029f11f2a06ea2795a6ec9c587327aba
ErisWS <eris@erisws.com>
2024-04-27 09:53:17 -0400

    Did a few fixes, tried to readd popup

--- Content.Server/Chemistry/ReagentEffects/PlantMetabolism/PlantSedin.cs
+++ Content.Server/Chemistry/ReagentEffects/PlantMetabolism/PlantSedin.cs
@@ -6,44 +6,43 @@ using Content.Server.Popups;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 
-namespace Content.Server.Chemistry.ReagentEffects.PlantMetabolism
+namespace Content.Server.Chemistry.ReagentEffects.PlantMetabolism;
+
+[UsedImplicitly]
+[DataDefinition]
+public sealed partial class PlantSedin : ReagentEffect
 {
-    [UsedImplicitly]
-    [DataDefinition]
-    public sealed partial class PlantSedin : ReagentEffect
+
+    [Dependency] private readonly PopupSystem _popupSystem = default!;
+
+
+    [DataField]
+    public float SeedRestorationChance = 0.1f;
+
+    [DataField]
+    public int PotencyDropAmount = 6; // Ideallly set to 2 times the amount Robust Harvest adds
+
+    public override void Effect(ReagentEffectArgs args)
     {
+        if (!args.EntityManager.TryGetComponent(args.SolutionEntity, out PlantHolderComponent? plantHolderComp)
+                                || plantHolderComp.Seed == null || plantHolderComp.Dead ||
+                                plantHolderComp.Seed.Immutable)
+            return;
 
-        [Dependency] private readonly PopupSystem _popupSystem = default!;
 
+        var plantHolder = args.EntityManager.System<PlantHolderSystem>();
+        var random = IoCManager.Resolve<IRobustRandom>();
 
-        [DataField]
-        public float SeedRestorationChance = 0.1f;
-
-        [DataField]
-        public int PotencyDropAmount = 7; // Nerf as neccesary
-
-        public override void Effect(ReagentEffectArgs args)
+        if (random.Prob(SeedRestorationChance))
         {
-            if (!args.EntityManager.TryGetComponent(args.SolutionEntity, out PlantHolderComponent? plantHolderComp)
-                                    || plantHolderComp.Seed == null || plantHolderComp.Dead ||
-                                    plantHolderComp.Seed.Immutable)
-                return;
-
-
-            var plantHolder = args.EntityManager.System<PlantHolderSystem>();
-            var random = IoCManager.Resolve<IRobustRandom>();
-
-            if (random.Prob(SeedRestorationChance))
-            {
-                // _popupSystem.PopupEntity(Loc.GetString("botany-plant-seedsrestored"), plantHolder., plantHolderComp.Owner); // Dunno how to do this, I'll figure it out!
-                plantHolderComp.Seed.Seedless = false;
-            }
-
-
-            plantHolderComp.Seed.Potency = Math.Max(plantHolderComp.Seed.Potency - PotencyDropAmount, 1);
-
+            _popupSystem.PopupEntity(Loc.GetString("botany-plant-seedsrestored"), args.SolutionEntity);
+            plantHolderComp.Seed.Seedless = false;
         }
 
-        protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys) => Loc.GetString("reagent-effect-guidebook-missing", ("chance", Probability));
+
+        plantHolderComp.Seed.Potency = Math.Max(plantHolderComp.Seed.Potency - PotencyDropAmount, 1);
+
     }
+
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys) => Loc.GetString("reagent-effect-guidebook-missing", ("chance", Probability));
 }
--- Resources/Locale/en-US/botany/sedin.ftl
+++ Resources/Locale/en-US/botany/sedin.ftl
@@ -1,1 +1,1 @@
-botany-plant-seedsrestored = "You can hear the faint sound of shuffling leaves."
+botany-plant-seedsrestored = You can hear the faint sound of shuffling leaves.
