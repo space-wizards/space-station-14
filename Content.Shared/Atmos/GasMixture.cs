effe7654cc7e8d154fcf320a717f69ae2b338d6c
Jezithyr <Jezithyr@gmail.com>
2024-04-29 03:33:02 -0700

    Temp Fix for sandbox violation, idk why Array.Resize isn't working properly. It's already sandboxed.

--- Content.Shared/Atmos/GasMixture.cs
+++ Content.Shared/Atmos/GasMixture.cs
@@ -224,7 +224,14 @@ namespace Content.Shared.Atmos
             // TODO add fixed-length-array serializer
 
             // The arrays MUST have a specific length.
-            Array.Resize(ref Moles, Atmospherics.AdjustedNumberOfGases);
+
+            //TODO: re-enable this when the sandbox actually behaves
+            //Array.Resize(ref Moles, Atmospherics.AdjustedNumberOfGases);
+            var newMols = new float[Atmospherics.AdjustedNumberOfGases];
+            for (int i = 0; i < Moles.Length; i++)
+            {
+                newMols[i] = Moles[i];
+            }
         }
 
         public GasMixtureStringRepresentation ToPrettyString()
