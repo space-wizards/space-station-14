b8bd9dfe5ee341477faba5496947c142442fb626
Jezithyr <Jezithyr@gmail.com>
2024-04-30 14:43:05 -0700

    Fix conflict

--- Content.Server/Nutrition/EntitySystems/SmokingSystem.Vape.cs
+++ Content.Server/Nutrition/EntitySystems/SmokingSystem.Vape.cs
@@ -14,6 +14,7 @@ using Content.Shared.Interaction;
 using Content.Shared.Nutrition;
 using System.Threading;
 using Content.Shared.Atmos;
+using Content.Shared.Medical.Blood.Components;
 
 /// <summary>
 /// System for vapes
