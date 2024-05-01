1bcc82afde28907e83c834f3a08f415b4720d20d
notquitehadouken <tripwiregamer@gmail.com>
2024-05-01 09:08:19 -0500

    o shit

--- Content.Shared/Armor/AllowSuitStorageComponent.cs
+++ Content.Shared/Armor/AllowSuitStorageComponent.cs
@@ -1,5 +1,8 @@
 namespace Content.Shared.Armor;
 
+/// <summary>
+///     Used on outerclothing to allow use of suit storage
+/// </summary>
 [RegisterComponent]
 public sealed partial class AllowSuitStorageComponent : Component
 {
