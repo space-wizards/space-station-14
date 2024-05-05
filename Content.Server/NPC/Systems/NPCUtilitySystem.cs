b67e3a3a4662c71d359174a7c286581d18d2f8b1
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2024-05-05 11:10:35 +1000

    Revert "npc can no longer attack you through a locker (#27677)"
    This reverts commit 83b486b63fd23e149bb0112aac4800e63b1f33dc.

--- Content.Server/NPC/Systems/NPCUtilitySystem.cs
+++ Content.Server/NPC/Systems/NPCUtilitySystem.cs
@@ -224,7 +224,7 @@ public sealed class NPCUtilitySystem : EntitySystem
                 {
                     if (TryComp<EntityStorageComponent>(container.Owner, out var storageComponent))
                     {
-                        if (storageComponent is { Open: false })
+                        if (storageComponent is { Open: false } && _weldable.IsWelded(container.Owner))
                         {
                             return 0.0f;
                         }
