2b80314352355eff092af673381f915a2cb9a21d
Verm <vermidiathesnake@gmail.com>
2024-05-04 22:33:19 -0500

    Fix desc of component

--- Content.Shared/StepTrigger/Components/NoClothingStepTriggerImmuneComponent.cs
+++ Content.Shared/StepTrigger/Components/NoClothingStepTriggerImmuneComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.StepTrigger.Components;
 
 /// <summary>
-/// Grants the attached entity immunity to  without needing clothes.
+/// Grants the attached entity immunity to ClothingRequiredStepTriggers without needing clothes.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class NoClothingStepTriggerImmuneComponent : Component;
