31491775e597fe9906df66d7285d5b63ba92daa2
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-05-08 19:18:03 +1000

    Added new HTN operations and preconditions (#27486)
    * Added new HTN operations & preconditions
    
    * Ok I forgot about partial
    
    * Namespace pierce the skies
    
    * Some fixes, debug and new operators
    
    * Bruh git eat my files

---
+++ Content.Server/NPC/HTN/Preconditions/Math/KeyFloatGreaterPrecondition.cs
@@ -0,0 +1,17 @@
+﻿namespace Content.Server.NPC.HTN.Preconditions.Math;
+
+public sealed partial class KeyFloatGreaterPrecondition : HTNPrecondition
+{
+    [Dependency] private readonly IEntityManager _entManager = default!;
+
+    [DataField(required: true)]
+    public string Key = string.Empty;
+
+    [DataField(required: true)]
+    public float Value;
+
+    public override bool IsMet(NPCBlackboard blackboard)
+    {
+        return blackboard.TryGetValue<float>(Key, out var value, _entManager) && value > Value;
+    }
+}
