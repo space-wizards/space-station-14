1f67733775ba8acdaa7458addf1aaff0fc48b1d9
deltanedas <39013340+deltanedas@users.noreply.github.com>
2024-05-09 05:55:15 +0000

    fix mech energy display for 0 (#27828)
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Content.Client/Mech/Ui/MechMenu.xaml.cs
+++ Content.Client/Mech/Ui/MechMenu.xaml.cs
@@ -35,9 +35,17 @@ public sealed partial class MechMenu : FancyWindow
         IntegrityDisplayBar.Value = integrityPercent.Float();
         IntegrityDisplay.Text = Loc.GetString("mech-integrity-display", ("amount", (integrityPercent*100).Int()));
 
-        var energyPercent = mechComp.Energy / mechComp.MaxEnergy;
-        EnergyDisplayBar.Value = energyPercent.Float();
-        EnergyDisplay.Text = Loc.GetString("mech-energy-display", ("amount", (energyPercent*100).Int()));
+        if (mechComp.MaxEnergy != 0f)
+        {
+            var energyPercent = mechComp.Energy / mechComp.MaxEnergy;
+            EnergyDisplayBar.Value = energyPercent.Float();
+            EnergyDisplay.Text = Loc.GetString("mech-energy-display", ("amount", (energyPercent*100).Int()));
+        }
+        else
+        {
+            EnergyDisplayBar.Value = 0f;
+            EnergyDisplay.Text = Loc.GetString("mech-energy-missing");
+        }
 
         SlotDisplay.Text = Loc.GetString("mech-slot-display",
             ("amount", mechComp.MaxEquipmentAmount - mechComp.EquipmentContainer.ContainedEntities.Count));
