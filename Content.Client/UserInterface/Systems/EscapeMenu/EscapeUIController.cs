727af16008014b1d03c992bc0a86699ba659c333
lzk <124214523+lzk228@users.noreply.github.com>
2024-05-09 19:49:25 +0200

    Add discord button in escape menu

--- Content.Client/UserInterface/Systems/EscapeMenu/EscapeUIController.cs
+++ Content.Client/UserInterface/Systems/EscapeMenu/EscapeUIController.cs
@@ -98,6 +98,11 @@ public sealed class EscapeUIController : UIController, IOnStateEntered<GameplayS
             _uri.OpenUri(_cfg.GetCVar(CCVars.InfoLinksWiki));
         };
 
+        _escapeWindow.DiscordButton.OnPressed += _ =>
+        {
+            _uri.OpenUri(_cfg.GetCVar(CCVars.InfoLinksDiscord));
+        };
+
         _escapeWindow.GuidebookButton.OnPressed += _ =>
         {
             _guidebook.ToggleGuidebook();
@@ -105,6 +110,8 @@ public sealed class EscapeUIController : UIController, IOnStateEntered<GameplayS
 
         // Hide wiki button if we don't have a link for it.
         _escapeWindow.WikiButton.Visible = _cfg.GetCVar(CCVars.InfoLinksWiki) != "";
+        // Hide discord button if we don't have a link for it.
+        _escapeWindow.DiscordButton.Visible = _cfg.GetCVar(CCVars.InfoLinksDiscord) != "";
 
         CommandBinds.Builder
             .Bind(EngineKeyFunctions.EscapeMenu,
--- Resources/Locale/en-US/escape-menu/ui/escape-menu.ftl
+++ Resources/Locale/en-US/escape-menu/ui/escape-menu.ftl
@@ -4,6 +4,7 @@ ui-escape-title = Game Menu
 ui-escape-options = Options
 ui-escape-rules = Rules
 ui-escape-guidebook = Guidebook
+ui-escape-discord = Discord
 ui-escape-wiki = Wiki
 ui-escape-disconnect = Disconnect
 ui-escape-quit = Quit
