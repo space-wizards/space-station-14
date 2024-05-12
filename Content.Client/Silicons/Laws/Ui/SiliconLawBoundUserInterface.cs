07671c9c7c08227611f00a767137f1d2b6f5f2fd
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-05-12 05:46:41 -0700

    Fix UI elements being recreated when they didn't need to be

--- Content.Client/Silicons/Laws/Ui/SiliconLawBoundUserInterface.cs
+++ Content.Client/Silicons/Laws/Ui/SiliconLawBoundUserInterface.cs
@@ -1,6 +1,7 @@
+using System.Linq;
+using Content.Shared.Silicons.Laws;
 using Content.Shared.Silicons.Laws.Components;
 using JetBrains.Annotations;
-using Robust.Client.GameObjects;
 
 namespace Content.Client.Silicons.Laws.Ui;
 
@@ -10,6 +11,7 @@ public sealed class SiliconLawBoundUserInterface : BoundUserInterface
     [ViewVariables]
     private SiliconLawMenu? _menu;
     private EntityUid _owner;
+    private List<SiliconLaw>? _laws;
 
     public SiliconLawBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
@@ -40,6 +42,26 @@ public sealed class SiliconLawBoundUserInterface : BoundUserInterface
 
         if (state is not SiliconLawBuiState msg)
             return;
+
+        if (_laws != null && _laws.Count == msg.Laws.Count)
+        {
+            var isSame = true;
+            msg.Laws.Sort();
+            for (var i = 0; i < _laws.Count; i++)
+            {
+                if (_laws[i].LawString == msg.Laws[i].LawString)
+                    continue;
+
+                isSame = false;
+                break;
+            }
+
+            if (isSame)
+                return;
+        }
+
+        _laws = msg.Laws.ToList();
+        _laws.Sort();
 
         _menu?.Update(_owner, msg);
     }
