2f6827b9482fda9a5ea4c36e10a204a80065943e
Verm <vermidiathesnake@gmail.com>
2024-05-14 01:46:49 -0500

    Accidental using

--- Content.Shared/Humanoid/Prototypes/RandomHumanoidSettingsPrototype.cs
+++ Content.Shared/Humanoid/Prototypes/RandomHumanoidSettingsPrototype.cs
@@ -1,6 +1,5 @@
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Dictionary;
 
 namespace Content.Shared.Humanoid.Prototypes;
 
