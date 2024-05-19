621418afac758f4009a9c556ca81d5d4bc396b86
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-05-19 18:33:56 +1000

    Fix conveyor mispredicts
    Instead of tracking active conveyors we instead track the conveyed entities. This also handles things like stacking conveyors more gracely.

--- Content.Server/Physics/Controllers/ConveyorController.cs
+++ Content.Server/Physics/Controllers/ConveyorController.cs
@@ -55,8 +55,6 @@ public sealed class ConveyorController : SharedConveyorController
         if (MetaData(uid).EntityLifeStage >= EntityLifeStage.Terminating)
             return;
 
-        RemComp<ActiveConveyorComponent>(uid);
-
         if (!TryComp<PhysicsComponent>(uid, out var physics))
             return;
 
