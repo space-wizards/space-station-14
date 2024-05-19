9f0ea0748053b275d84645e03b52cbc199b0c5db
Flesh <62557990+PolterTzi@users.noreply.github.com>
2024-05-20 01:01:44 +0200

    Add plant metabolism effects to chemical guidebook (#28038)
    * draft one of plant metabolism guidebook
    
    * loc fix?
    
    * add attributes loc
    
    * fix loc syntax
    
    * improved appearance
    
    * last commit was undercooked, my bad
    
    * last commit was still undercooked, my worse
    
    * last commit was even still undercooked, my worst
    
    * Addressed comments?
    
    * Fix newlines
    
    * Hopefully this works
    
    * Cleanup, I think
    
    * 2xs

--- Content.Server/Chemistry/ReagentEffects/PlantMetabolism/PlantAdjustAttribute.cs
+++ Content.Server/Chemistry/ReagentEffects/PlantMetabolism/PlantAdjustAttribute.cs
@@ -16,6 +16,18 @@ namespace Content.Server.Chemistry.ReagentEffects.PlantMetabolism
         public float Prob { get; protected set; } = 1; // = (80);
 
         /// <summary>
+        /// Localisation key for the name of the adjusted attribute. Used for guidebook descriptions.
+        /// </summary>
+        [DataField]
+        public abstract string GuidebookAttributeName { get; set; }
+
+        /// <summary>
+        /// Whether the attribute in question is a good thing. Used for guidebook descriptions to determine the color of the number.
+        /// </summary>
+        [DataField]
+        public virtual bool GuidebookIsAttributePositive { get; protected set; } = true;
+
+        /// <summary>
         ///     Checks if the plant holder can metabolize the reagent or not. Checks if it has an alive plant by default.
         /// </summary>
         /// <param name="plantHolder">The entity holding the plant</param>
@@ -40,6 +52,18 @@ namespace Content.Server.Chemistry.ReagentEffects.PlantMetabolism
             return !(Prob <= 0f) && IoCManager.Resolve<IRobustRandom>().Prob(Prob);
         }
 
-        protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys) => Loc.GetString("reagent-effect-guidebook-missing", ("chance", Probability));
+        protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+        {
+            string color;
+            if (GuidebookIsAttributePositive ^ Amount < 0.0)
+            {
+                color = "green";
+            }
+            else
+            {
+                color = "red";
+            }
+            return Loc.GetString("reagent-effect-guidebook-plant-attribute", ("attribute", Loc.GetString(GuidebookAttributeName)), ("amount", Amount.ToString("0.00")), ("colorName", color), ("chance", Probability));
+        }
     }
 }
