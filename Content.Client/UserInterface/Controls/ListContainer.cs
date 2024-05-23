e579ac79db319030d33cb83d784833888d179710
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-05-23 09:31:00 -0700

    Add some documentation to ListContainer

--- Content.Client/UserInterface/Controls/ListContainer.cs
+++ Content.Client/UserInterface/Controls/ListContainer.cs
@@ -21,10 +21,26 @@ public sealed class ListContainer : Control
         set => _buttonGroup = value ? new ButtonGroup() : null;
     }
     public bool Toggle { get; set; }
+
+    /// <summary>
+    /// Called when creating a button on the UI.
+    /// The provided <see cref="ListContainerButton"/> is the generated button that Controls should be parented to.
+    /// </summary>
     public Action<ListData, ListContainerButton>? GenerateItem;
+
+    /// <inheritdoc cref="BaseButton.OnPressed"/>
     public Action<BaseButton.ButtonEventArgs, ListData>? ItemPressed;
+
+    /// <summary>
+    /// Invoked when a KeyBind is pressed on a ListContainerButton.
+    /// </summary>
     public Action<GUIBoundKeyEventArgs, ListData>? ItemKeyBindDown;
+
+    /// <summary>
+    /// Invoked when the selected item does not exist in the new data when PopulateList is called.
+    /// </summary>
     public Action? NoItemSelected;
+
     public IReadOnlyList<ListData> Data => _data;
 
     private const int DefaultSeparation = 3;
@@ -73,7 +89,7 @@ public sealed class ListContainer : Control
         _vScrollBar.OnValueChanged += ScrollValueChanged;
     }
 
-    public void PopulateList(IReadOnlyList<ListData> data)
+    public virtual void PopulateList(IReadOnlyList<ListData> data)
     {
         if ((_itemHeight == 0 || _data is {Count: 0}) && data.Count > 0)
         {
