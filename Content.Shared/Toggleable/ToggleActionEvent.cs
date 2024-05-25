ecc6f372b2a38a43490c431abb80b4099affe972
deltanedas <@deltanedas:kde.org>
2024-05-25 06:47:43 +0100

    ???

--- Content.Shared/Toggleable/ToggleActionEvent.cs
+++ Content.Shared/Toggleable/ToggleActionEvent.cs
@@ -6,7 +6,8 @@ namespace Content.Shared.Toggleable;
 /// <summary>
 ///     Generic action-event for toggle-able components.
 /// </summary>
-public sealed partial class ToggleActionEvent : InstantActionEvent { }
+[Serializable]
+public sealed partial class ToggleActionEvent : InstantActionEvent;
 
 /// <summary>
 ///     Generic enum keys for toggle-visualizer appearance data & sprite layers.
