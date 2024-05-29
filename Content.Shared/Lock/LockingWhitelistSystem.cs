6a962ace79098778625454e7d52fdb5911ec2618
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-05-29 14:15:38 -0700

    Fix wrong entity in popup when toggling another borg

--- Content.Shared/Lock/LockingWhitelistSystem.cs
+++ Content.Shared/Lock/LockingWhitelistSystem.cs
@@ -21,7 +21,7 @@ public sealed class LockingWhitelistSystem : EntitySystem
             return;
 
         if (!args.Silent)
-            _popupSystem.PopupClient(Loc.GetString("locking-whitelist-component-lock-toggle-deny"), ent.Owner);
+            _popupSystem.PopupClient(Loc.GetString("locking-whitelist-component-lock-toggle-deny"), args.User);
 
         args.Cancelled = true;
     }
