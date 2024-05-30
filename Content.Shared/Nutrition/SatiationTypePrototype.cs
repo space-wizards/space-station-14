ac813d1ea5325b730ec6707d9b24631655beb88e
Łukasz Mędrek <lukasz@lukaszm.xyz>
2024-05-30 21:48:09 +0200

    fix

--- Content.Shared/Nutrition/SatiationTypePrototype.cs
+++ Content.Shared/Nutrition/SatiationTypePrototype.cs
@@ -8,4 +8,10 @@ public sealed partial class SatiationTypePrototype : IPrototype
     [ViewVariables]
     [IdDataField]
     public string ID { get; private set; } = default!;
+
+    /// <summary>
+    /// A player-facing name used to describe this satiation.
+    /// </summary>
+    [DataField]
+    public readonly LocId Name;
 }
