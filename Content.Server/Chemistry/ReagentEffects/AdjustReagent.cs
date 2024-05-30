8bb129efc8610bd268b36d817cb3caec6d1b5084
Łukasz Mędrek <lukasz@lukaszm.xyz>
2024-05-30 20:39:01 +0200

    fix

--- Content.Server/Chemistry/ReagentEffects/AdjustReagent.cs
+++ Content.Server/Chemistry/ReagentEffects/AdjustReagent.cs
@@ -78,7 +78,7 @@ namespace Content.Server.Chemistry.ReagentEffects
                     ("amount", MathF.Abs(Amount.Float())));
             }
 
-            throw new NotImplementedException($"{Reagent}");
+            throw new NotImplementedException();
         }
     }
 }
