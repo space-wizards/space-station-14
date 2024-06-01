6ccce2d95cc0a61c3473ccbfad354d859fa586b8
Mr. 27 <koolthunder019@gmail.com>
2024-06-01 17:32:14 -0400

    add inheritance

--- Content.Shared/Roles/StartingGearPrototype.cs
+++ Content.Shared/Roles/StartingGearPrototype.cs
@@ -1,14 +1,33 @@
-using Content.Shared.Preferences;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
 
 namespace Content.Shared.Roles
 {
     [Prototype("startingGear")]
-    public sealed partial class StartingGearPrototype : IPrototype
+    public sealed partial class StartingGearPrototype : IPrototype, IInheritingPrototype
     {
+        /// <inheritdoc/>
+        [ViewVariables]
+        [IdDataField]
+        public string ID { get; private set; } = string.Empty;
+
+        /// <inheritdoc/>
+        [ParentDataField(typeof(AbstractPrototypeIdArraySerializer<StartingGearPrototype>))]
+        public string[]? Parents { get; private set; }
+
+        /// <inheritdoc/>
+        [AbstractDataField]
+        public bool Abstract { get; }
+
+        /// <summary>
+        /// The slot and entity prototype ID of the equipment that is to be spawned and equipped onto the entity.
+        /// </summary>
         [DataField]
         public Dictionary<string, EntProtoId> Equipment = new();
 
+        /// <summary>
+        /// The inhand items that are equipped when this starting gear is equipped onto an entity.
+        /// </summary>
         [DataField]
         public List<EntProtoId> Inhand = new(0);
 
@@ -18,10 +37,9 @@ namespace Content.Shared.Roles
         [DataField]
         public Dictionary<string, List<EntProtoId>> Storage = new();
 
-        [ViewVariables]
-        [IdDataField]
-        public string ID { get; private set; } = string.Empty;
-
+        /// <summary>
+        /// Gets the entity prototype ID of a slot in this starting gear.
+        /// </summary>
         public string GetGear(string slot)
         {
             return Equipment.TryGetValue(slot, out var equipment) ? equipment : string.Empty;
