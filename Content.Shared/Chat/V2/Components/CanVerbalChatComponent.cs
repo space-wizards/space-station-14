329eeee282f9aebdb25c2286c4b2ba9908f53310
Hannah Giovanna Dawson <karakkaraz@gmail.com>
2024-06-02 00:36:08 +0100

    Delete an unused component and two unused fields

--- Content.Shared/Chat/V2/Components/CanVerbalChatComponent.cs
+++ Content.Shared/Chat/V2/Components/CanVerbalChatComponent.cs
@@ -6,13 +6,4 @@ namespace Content.Shared.Chat.V2.Components;
 /// Declares that this entity can chat in local chat
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed partial class CanVerbalChatComponent : Component
-{
-    // TODO: Ensure you can't locally chat in insufficient atmosphere
-
-    /// <summary>
-    /// How far can this entity be heard from?
-    /// </summary>
-    [DataField, AutoNetworkedField]
-    public float Range = 10.0f;
-}
+public sealed partial class CanVerbalChatComponent : Component;
--- Content.Shared/Chat/V2/Components/CanVisualChatComponent.cs
+++ Content.Shared/Chat/V2/Components/CanVisualChatComponent.cs
@@ -6,11 +6,4 @@ namespace Content.Shared.Chat.V2.Components;
 /// Declares that this entity can emote.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed partial class CanVisualChatComponent : Component
-{
-    /// <summary>
-    /// How far can this emote be seen from?
-    /// </summary>
-    [DataField, AutoNetworkedField]
-    public float Range = 10.0f;
-}
+public sealed partial class CanVisualChatComponent : Component;
