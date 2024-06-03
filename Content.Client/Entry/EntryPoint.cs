8ea54022ff9cee4189dc635783aec900c851266f
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-06-03 14:44:56 -0700

    Fix client error when trying to parse the prototype

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -119,6 +119,7 @@ namespace Content.Client.Entry
             _prototypeManager.RegisterIgnore("alertLevels");
             _prototypeManager.RegisterIgnore("nukeopsRole");
             _prototypeManager.RegisterIgnore("ghostRoleRaffleDecider");
+            _prototypeManager.RegisterIgnore("censorActionGroup");
 
             _componentFactory.GenerateNetIds();
             _adminManager.Initialize();
