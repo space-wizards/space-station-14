732d6488af0ede0625b47e0e9f3d30bc784d4393
deltanedas <@deltanedas:kde.org>
2024-06-04 01:10:48 +0100

    cleanup MinMax

--- Content.Shared/Destructible/Thresholds/MinMax.cs
+++ Content.Shared/Destructible/Thresholds/MinMax.cs
@@ -1,26 +1,24 @@
 ﻿using Robust.Shared.Random;
 
-namespace Content.Shared.Destructible.Thresholds
+namespace Content.Shared.Destructible.Thresholds;
+
+[DataDefinition, Serializable]
+public partial struct MinMax
 {
-    [Serializable]
-    [DataDefinition]
-    public partial struct MinMax
+    [DataField]
+    public int Min;
+
+    [DataField]
+    public int Max;
+
+    public MinMax(int min, int max)
     {
-        [DataField("min")]
-        public int Min;
+        Min = min;
+        Max = max;
+    }
 
-        [DataField("max")]
-        public int Max;
-
-        public MinMax(int min, int max)
-        {
-            Min = min;
-            Max = max;
-        }
-
-        public int Next(IRobustRandom random)
-        {
-            return random.Next(Min, Max + 1);
-        }
+    public int Next(IRobustRandom random)
+    {
+        return random.Next(Min, Max + 1);
     }
 }
