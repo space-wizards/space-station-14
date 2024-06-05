77dc7acc4af0509702235840b439b881f5c66e19
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-06-05 02:56:05 -0700

    Add migrations

---
+++ Content.Server.Database/Migrations/Postgres/20240605091303_TextAutomod.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class TextAutomod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "text_automod",
+                columns: table => new
+                {
+                    filter_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    pattern = table.Column<string>(type: "text", nullable: false),
+                    filter_type = table.Column<byte>(type: "smallint", nullable: false),
+                    action_group = table.Column<string>(type: "text", nullable: false),
+                    target_flags = table.Column<int>(type: "integer", nullable: false),
+                    display_name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_text_automod", x => x.filter_id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_text_automod_filter_id",
+                table: "text_automod",
+                column: "filter_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "text_automod");
+        }
+    }
+}
---
+++ Content.Server.Database/Migrations/Sqlite/20240605091244_TextAutomod.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class TextAutomod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "text_automod",
+                columns: table => new
+                {
+                    filter_id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    pattern = table.Column<string>(type: "TEXT", nullable: false),
+                    filter_type = table.Column<byte>(type: "INTEGER", nullable: false),
+                    action_group = table.Column<string>(type: "TEXT", nullable: false),
+                    target_flags = table.Column<int>(type: "INTEGER", nullable: false),
+                    display_name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_text_automod", x => x.filter_id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_text_automod_filter_id",
+                table: "text_automod",
+                column: "filter_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "text_automod");
+        }
+    }
+}
