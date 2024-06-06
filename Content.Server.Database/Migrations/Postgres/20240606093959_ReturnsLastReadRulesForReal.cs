fd8a93d4ba8fd67dea77d661a21d7094bb36a6e5
AJCM <AJCM@tutanota.com>
2024-06-06 05:55:19 -0400

    Fixes client having authority over rules popup cvars

---
+++ Content.Server.Database/Migrations/Postgres/20240606093959_ReturnsLastReadRulesForReal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class ReturnsLastReadRulesForReal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
