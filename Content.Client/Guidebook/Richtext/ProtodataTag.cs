abe00603c1eb3348fb570a5ccd133bdd10e15065
Tayrtahn <tayrtahn@gmail.com>
2024-06-05 22:34:27 -0400

    Use attributes like a sane person

--- Content.Client/Guidebook/Richtext/ProtodataTag.cs
+++ Content.Client/Guidebook/Richtext/ProtodataTag.cs
@@ -28,47 +28,30 @@ public sealed class ProtodataTag : IMarkupTag
     private ISawmill Log => _log ??= _logMan.GetSawmill("protodata_tag");
     private ISawmill? _log;
 
-    private const string BadSyntaxMessage = "Bad syntax: \"{0}\". Use \"Prototype.Component.Field[:Format]\"";
-
     public string TextBefore(MarkupNode node)
     {
         // Do nothing with an empty tag
-        if (!node.Value.TryGetString(out var command))
+        if (!node.Value.TryGetString(out var prototype))
             return "";
+
+        if (!node.Attributes.TryGetValue("comp", out var component))
+            return string.Empty;
+        if (!node.Attributes.TryGetValue("member", out var member))
+            return string.Empty;
+        node.Attributes.TryGetValue("format", out var format);
 
         var guidebookData = _entMan.System<GuidebookDataSystem>();
 
-        // Split the ID and the format string
-        var parts = command.Split(':');
-        if (parts.Length < 1 || parts.Length > 2)
-        {
-            Log.Error(BadSyntaxMessage, command);
-            return "???";
-        }
-        var id = parts[0];
-        var format = parts.TryGetValue(1, out var f) ? f : string.Empty;
-
-        // Split the ID into Prototype, Component, and Field
-        var idParts = id.Split('.');
-        if (idParts.Length != 3)
-        {
-            Log.Error(BadSyntaxMessage, command);
-            return "???";
-        }
-        var prototype = idParts[0];
-        var component = idParts[1];
-        var member = idParts[2];
-
         // Try to get the value
-        if (!guidebookData.TryGetValue(prototype, component, member, out var value))
+        if (!guidebookData.TryGetValue(prototype, component.StringValue!, member.StringValue!, out var value))
         {
             Log.Error($"Failed to find protodata for {component}.{member} in {prototype}");
             return "???";
         }
 
         // If we have a format string and a formattable value, format it as requested
-        if (!string.IsNullOrEmpty(format) && value is IFormattable formattable)
-            return formattable.ToString(parts[1], CultureInfo.CurrentCulture);
+        if (!string.IsNullOrEmpty(format.StringValue) && value is IFormattable formattable)
+            return formattable.ToString(format.StringValue, CultureInfo.CurrentCulture);
 
         // No format string given, so just use default ToString
         return value?.ToString() ?? "NULL";
