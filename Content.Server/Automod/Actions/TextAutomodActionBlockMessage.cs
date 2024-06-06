16322a3e74d871d6a2a09efafa69700d1b220f91
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-06-06 04:24:55 -0700

    Add block message action

---
+++ Content.Server/Automod/Actions/TextAutomodActionBlockMessage.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared.Automod;
+using Robust.Shared.Player;
+
+namespace Content.Server.Automod.Actions;
+
+public sealed partial class TextAutomodActionBlockMessage : ITextAutomodAction
+{
+    public bool Skip(string fullText, Dictionary<string, int> patternMatches)
+    {
+        return false;
+    }
+
+    public bool RunAction(ICommonSession session,
+        string fullText,
+        Dictionary<string, int> patternMatches,
+        AutomodFilterDef filter,
+        string filterDisplayName,
+        IEntityManager entMan)
+    {
+        return false;
+    }
+}
