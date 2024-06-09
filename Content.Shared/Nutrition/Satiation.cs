c95343e5a65672c7b1bf09b9047db0fe5c63a3f1
Łukasz Mędrek <lukasz@lukaszm.xyz>
2024-06-09 22:15:08 +0200

    change: remove redundant VV

--- Content.Shared/Nutrition/Satiation.cs
+++ Content.Shared/Nutrition/Satiation.cs
@@ -11,27 +11,27 @@ public sealed partial class Satiation
     /// <summary>
     /// The current satiation amount of the entity
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public float Current;
 
     /// <summary>
     /// The actual amount at which <see cref="Current"/> decays.
     /// Affected by <seealso cref="CurrentThreshold"/>
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public float ActualDecayRate;
 
     /// <summary>
     /// The last threshold this entity was at.
     /// Stored in order to prevent recalculating
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public SatiationThreashold LastThreshold = SatiationThreashold.Okay;
 
     /// <summary>
     /// The current nutrition threshold the entity is at
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public SatiationThreashold CurrentThreshold;
 
 
@@ -43,7 +43,7 @@ public sealed partial class Satiation
     /// <summary>
     /// The current nutrition threshold the entity is at
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public ProtoId<SatiationPrototype> Prototype;
 }
 
