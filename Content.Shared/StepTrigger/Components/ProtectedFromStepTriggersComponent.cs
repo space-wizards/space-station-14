991f017a9c91d6d489a6de40c5a0c51fdcf366c8
Verm <vermidiathesnake@gmail.com>
2024-06-13 18:20:51 -0500

    Changed names of components

--- Content.Shared/StepTrigger/Components/ProtectedFromStepTriggersComponent.cs
+++ Content.Shared/StepTrigger/Components/ProtectedFromStepTriggersComponent.cs
@@ -5,11 +5,11 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.StepTrigger.Components;
 
 /// <summary>
-/// This is used for cancelling step trigger events if the user is wearing clothing in a valid slot.
+/// This is used for cancelling preventable step trigger events if the user is wearing clothing in a valid slot or if the user itself has the component.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 [Access(typeof(StepTriggerImmuneSystem))]
-public sealed partial class ClothingRequiredStepTriggerImmuneComponent : Component, IClothingSlots
+public sealed partial class ProtectedFromStepTriggersComponent : Component, IClothingSlots
 {
     [DataField]
     public SlotFlags Slots { get; set; } = SlotFlags.FEET;
