5c630db5c167077bafc31b1c9073095aae567cfe
deltanedas <@deltanedas:kde.org>
2024-06-16 13:41:26 +0100

    add LogWireAction

---
+++ Content.Server/Access/LogWireAction.cs
@@ -0,0 +1,60 @@
+using Content.Server.Wires;
+using Content.Shared.Access;
+using Content.Shared.Access.Components;
+using Content.Shared.Emag.Components;
+using Content.Shared.Wires;
+
+namespace Content.Server.Access;
+
+public sealed partial class LogWireAction : ComponentWireAction<AccessReaderComponent>
+{
+    public override Color Color { get; set; } = Color.Blue;
+    public override string Name { get; set; } = "wire-name-log";
+
+    [DataField]
+    public int PulseTimeout = 30;
+
+    public override StatusLightState? GetLightState(Wire wire, AccessReaderComponent comp)
+    {
+        return comp.LoggingDisabled ? StatusLightState.Off : StatusLightState.On;
+    }
+
+    public override object StatusKey => AccessWireActionKey.Status;
+
+    public override bool Cut(EntityUid user, Wire wire, AccessReaderComponent comp)
+    {
+        WiresSystem.TryCancelWireAction(wire.Owner, PulseTimeoutKey.Key);
+        comp.LoggingDisabled = true;
+        EntityManager.Dirty(wire.Owner, comp);
+        return true;
+    }
+
+    public override bool Mend(EntityUid user, Wire wire, AccessReaderComponent comp)
+    {
+        comp.LoggingDisabled = false;
+        return true;
+    }
+
+    public override void Pulse(EntityUid user, Wire wire, AccessReaderComponent comp)
+    {
+        comp.LoggingDisabled = true;
+        WiresSystem.StartWireAction(wire.Owner, PulseTimeout, PulseTimeoutKey.Key, new TimedWireEvent(AwaitPulseCancel, wire));
+    }
+
+    public override void Update(Wire wire)
+    {
+        if (!IsPowered(wire.Owner))
+            WiresSystem.TryCancelWireAction(wire.Owner, PulseTimeoutKey.Key);
+    }
+
+    private void AwaitPulseCancel(Wire wire)
+    {
+        if (!wire.IsCut && EntityManager.TryGetComponent<AccessReaderComponent>(wire.Owner, out var comp))
+            comp.LoggingDisabled = false;
+    }
+
+    private enum PulseTimeoutKey : byte
+    {
+        Key
+    }
+}
