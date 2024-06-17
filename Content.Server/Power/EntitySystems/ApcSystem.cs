858664f3e58df0017d322e8680f12187b923b522
WarMechanic <69510347+WarMechanic@users.noreply.github.com>
2024-06-17 18:16:00 +1000

    apc visualisation

--- Content.Server/Power/EntitySystems/ApcSystem.cs
+++ Content.Server/Power/EntitySystems/ApcSystem.cs
@@ -1,3 +1,4 @@
+using Content.Server.Emp;
 using Content.Server.Popups;
 using Content.Server.Power.Components;
 using Content.Server.Power.Pow3r;
@@ -6,6 +7,7 @@ using Content.Shared.Access.Systems;
 using Content.Shared.APC;
 using Content.Shared.Emag.Components;
 using Content.Shared.Emag.Systems;
+using Content.Shared.Emp;
 using Content.Shared.Popups;
 using Content.Shared.Rounding;
 using Robust.Server.GameObjects;
@@ -35,6 +37,9 @@ public sealed class ApcSystem : EntitySystem
         SubscribeLocalEvent<ApcComponent, ChargeChangedEvent>(OnBatteryChargeChanged);
         SubscribeLocalEvent<ApcComponent, ApcToggleMainBreakerMessage>(OnToggleMainBreaker);
         SubscribeLocalEvent<ApcComponent, GotEmaggedEvent>(OnEmagged);
+
+        SubscribeLocalEvent<ApcComponent, EmpPulseEvent>(OnEmpPulse);
+        SubscribeLocalEvent<ApcComponent, EmpDisabledRemoved>(OnEmpDisabledRemoved);
     }
 
     public override void Update(float deltaTime)
@@ -159,7 +164,7 @@ public sealed class ApcSystem : EntitySystem
 
     private ApcChargeState CalcChargeState(EntityUid uid, PowerState.Battery battery)
     {
-        if (HasComp<EmaggedComponent>(uid))
+        if (HasComp<EmaggedComponent>(uid) || HasComp<EmpDisabledComponent>(uid))
             return ApcChargeState.Emag;
 
         if (battery.CurrentStorage / battery.Capacity > ApcComponent.HighPowerThreshold)
@@ -185,6 +190,17 @@ public sealed class ApcSystem : EntitySystem
         }
 
         return ApcExternalPowerState.Good;
+    }
+    
+    private void OnEmpPulse(EntityUid uid, ApcComponent component, ref EmpPulseEvent args)
+    {
+        EnsureComp<EmpDisabledComponent>(uid, out var emp); //event calls before EmpDisabledComponent is added, ensure it to force sprite update
+        UpdateApcState(uid);
+    }
+
+    private void OnEmpDisabledRemoved(EntityUid uid, ApcComponent component, ref EmpDisabledRemoved args)
+    {
+        UpdateApcState(uid);
     }
 }
 
