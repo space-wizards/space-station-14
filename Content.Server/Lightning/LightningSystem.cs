a08409fa3a64779d26822fa04dd8b6c8d340d7d6
plykiya <plykiya@protonmail.com>
2024-06-16 21:18:41 -0700

    unobsolete lightning system

--- Content.Server/Lightning/LightningSystem.cs
+++ Content.Server/Lightning/LightningSystem.cs
@@ -82,10 +82,9 @@ public sealed class LightningSystem : SharedLightningSystem
         //To Do: Remove Hardcode LightningTargetComponent (this should be a parameter of the SharedLightningComponent)
         //To Do: This is still pretty bad for perf but better than before and at least it doesn't re-allocate
         // several hashsets every time
-
-        var targets = _lookup.GetComponentsInRange<LightningTargetComponent>(_transform.GetMapCoordinates(user), range).ToList();
+        var targets = _lookup.GetEntitiesInRange<LightningTargetComponent>(_transform.GetMapCoordinates(user), range).ToList();
         _random.Shuffle(targets);
-        targets.Sort((x, y) => y.Priority.CompareTo(x.Priority));
+        targets.Sort((x, y) => y.Comp.Priority.CompareTo(x.Comp.Priority));
 
         int shootedCount = 0;
         int count = -1;
@@ -95,14 +94,14 @@ public sealed class LightningSystem : SharedLightningSystem
 
             if (count >= targets.Count) { break; }
 
-            var curTarget = targets[count];
+            var curTarget = targets[count].Comp;
             if (!_random.Prob(curTarget.HitProbability)) //Chance to ignore target
                 continue;
 
             ShootLightning(user, targets[count].Owner, lightningPrototype, triggerLightningEvents);
-            if (arcDepth - targets[count].LightningResistance > 0)
+            if (arcDepth - targets[count].Comp.LightningResistance > 0)
             {
-                ShootRandomLightnings(targets[count].Owner, range, 1, lightningPrototype, arcDepth - targets[count].LightningResistance, triggerLightningEvents);
+                ShootRandomLightnings(targets[count].Owner, range, 1, lightningPrototype, arcDepth - targets[count].Comp.LightningResistance, triggerLightningEvents);
             }
             shootedCount++;
         }
