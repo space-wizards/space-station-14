d961a40a7eea31eef522515fa0b75da6e7fb5ca8
Tayrtahn <tayrtahn@gmail.com>
2024-06-17 11:04:37 -0400

    Access;

--- Content.Server/Speech/Components/AccentWearerNameClothingComponent.cs
+++ Content.Server/Speech/Components/AccentWearerNameClothingComponent.cs
@@ -1,7 +1,10 @@
+using Content.Server.Speech.EntitySystems;
+
 namespace Content.Server.Speech.Components;
 
 /// <summary>
 /// Applies any accent components on this item to the name of the wearer while worn.
 /// </summary>
 [RegisterComponent]
-public sealed partial class AccentWearerNameClothingComponent : Component { }
+[Access(typeof(AccentWearerNameClothingSystem))]
+public sealed partial class AccentWearerNameClothingComponent : Component;
