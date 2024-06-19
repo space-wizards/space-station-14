3f5da9021e3826786d81a3d53b97053062db0a5c
ps3moira <113228053+ps3moira@users.noreply.github.com>
2024-06-18 19:31:56 -0700

    Reverts

--- Content.Client/Power/APC/ApcVisualizerComponent.cs
+++ Content.Client/Power/APC/ApcVisualizerComponent.cs
@@ -84,10 +84,10 @@ public sealed partial class ApcVisualsComponent : Component
 
     /// <summary>
     /// The colors of the light emitted by the APC given a particular display state.
-    /// </summary>
+    /// </summary> # GreyStation - Changed colors to match current APC sprite
     [DataField("screenColors")]
     [ViewVariables(VVAccess.ReadWrite)]
-    public Color[] ScreenColors = new Color[(byte)ApcChargeState.NumStates]{Color.FromHex("#d1332e"), Color.FromHex("#dcdc28"), Color.FromHex("#82ff4c"), Color.FromHex("#ffac1c")};
+    public Color[] ScreenColors = new Color[(byte)ApcChargeState.NumStates]{Color.FromHex("#d1332e"), Color.FromHex("#2e8ad1"), Color.FromHex("#3db83b"), Color.FromHex("#ffac1c")};
 
     /// <summary>
     /// The sprite state of the unlit overlay used for the APC screen when the APC has been emagged.
