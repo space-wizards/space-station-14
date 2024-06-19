08c6ae8a31ba50fd905c4a3e0aad77f13e04e92f
Tayrtahn <tayrtahn@gmail.com>
2024-06-19 13:23:30 -0400

    Add tests for auto-internals

---
+++ Content.IntegrationTests/Tests/Internals/AutoInternalsTests.cs
@@ -0,0 +1,95 @@
+using Content.Server.Atmos.EntitySystems;
+using Content.Server.Body.Systems;
+using Content.Server.Station.Systems;
+using Content.Shared.Preferences;
+using Content.Shared.Roles.Jobs;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Map;
+
+namespace Content.IntegrationTests.Tests.Internals;
+
+[TestFixture]
+[TestOf(typeof(InternalsSystem))]
+public sealed class AutoInternalsTests
+{
+    [Test]
+    public async Task TestInternalsAutoActivateInSpaceForStationSpawn()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
+
+        var testMap = await pair.CreateTestMap();
+
+        var mapMan = server.ResolveDependency<IMapManager>();
+        var entMan = server.ResolveDependency<IEntityManager>();
+        var stationSpawning = server.System<StationSpawningSystem>();
+        var atmos = server.System<AtmosphereSystem>();
+        var internals = server.System<InternalsSystem>();
+
+        await server.WaitAssertion(() =>
+        {
+            var profile = new HumanoidCharacterProfile();
+            var dummy = stationSpawning.SpawnPlayerMob(testMap.GridCoords, new JobComponent()
+            {
+                Prototype = "TestInternalsDummy"
+            }, profile, station: null);
+
+            Assert.That(atmos.HasAtmosphere(testMap.Grid), Is.False, "Test map has atmosphere - test needs adjustment!");
+            Assert.That(internals.AreInternalsWorking(dummy), "Internals did not automatically connect!");
+
+            entMan.DeleteEntity(dummy);
+        });
+
+        await pair.CleanReturnAsync();
+    }
+
+    [Test]
+    public async Task TestInternalsAutoActivateInSpaceForEntitySpawn()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
+
+        var testMap = await pair.CreateTestMap();
+
+        var mapMan = server.ResolveDependency<IMapManager>();
+        var entMan = server.ResolveDependency<IEntityManager>();
+        var atmos = server.System<AtmosphereSystem>();
+        var internals = server.System<InternalsSystem>();
+
+        await server.WaitAssertion(() =>
+        {
+            var dummy = entMan.Spawn("TestInternalsDummyEntity", testMap.MapCoords);
+
+            Assert.That(atmos.HasAtmosphere(testMap.Grid), Is.False, "Test map has atmosphere - test needs adjustment!");
+            Assert.That(internals.AreInternalsWorking(dummy), "Internals did not automatically connect!");
+
+            entMan.DeleteEntity(dummy);
+        });
+
+        await pair.CleanReturnAsync();
+    }
+
+    [TestPrototypes]
+    private const string Prototypes = @"
+- type: playTimeTracker
+  id: PlayTimeInternalsDummy
+
+- type: startingGear
+  id: InternalsDummyGear
+  equipment:
+    mask: ClothingMaskBreath
+    suitstorage: OxygenTankFilled
+
+- type: job
+  id: TestInternalsDummy
+  playTimeTracker: PlayTimeInternalsDummy
+  startingGear: InternalsDummyGear
+
+- type: entity
+  id: TestInternalsDummyEntity
+  parent: MobHuman
+  components:
+    - type: Loadout
+      prototypes: [InternalsDummyGear]
+";
+}
