295ac2a55a4a097f0c01cd725c30f3b73732aa1c
WarMechanic <69510347+WarMechanic@users.noreply.github.com>
2024-06-20 13:25:47 +1000

    light turns off when substation runs out of power

--- Content.Client/Power/Substation/SubstationVisualizerSystem.cs
+++ Content.Client/Power/Substation/SubstationVisualizerSystem.cs
@@ -5,6 +5,7 @@ using Robust.Client.GameObjects;
 namespace Content.Client.Power.Substation;
 public sealed class SubstationVisualizerSystem : VisualizerSystem<SubstationVisualizerComponent>
 {
+    [Dependency] private readonly SharedPointLightSystem _lights = default!;
     protected override void OnAppearanceChange(EntityUid uid, SubstationVisualizerComponent comp, ref AppearanceChangeEvent args)
     {
         if (args.Sprite == null)
@@ -18,18 +19,22 @@ public sealed class SubstationVisualizerSystem : VisualizerSystem<SubstationVisu
             case SubstationChargeState.Dead:
                 args.Sprite.LayerSetState(SubstationVisualLayers.Charge, $"dead");
                 args.Sprite.LayerSetVisible(SubstationVisualLayers.Screen, false);
+                _lights.SetEnabled(uid, false);
                 break;
             case SubstationChargeState.Discharging:
                 args.Sprite.LayerSetState(SubstationVisualLayers.Charge, $"dead");
                 args.Sprite.LayerSetVisible(SubstationVisualLayers.Screen, true);
+                _lights.SetEnabled(uid, true);
                 break;
             case SubstationChargeState.Charging:
                 args.Sprite.LayerSetState(SubstationVisualLayers.Charge, $"charging");
                 args.Sprite.LayerSetVisible(SubstationVisualLayers.Screen, true);
+                _lights.SetEnabled(uid, true);
                 break;
             case SubstationChargeState.Full:
                 args.Sprite.LayerSetState(SubstationVisualLayers.Charge, $"full");
                 args.Sprite.LayerSetVisible(SubstationVisualLayers.Screen, true);
+                _lights.SetEnabled(uid, true);
                 break;
         }
     }
