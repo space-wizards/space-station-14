7fbf464e10ee6d2c793cc41295c4207de7185ac4
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-06-22 14:14:51 +1000

    review

--- Content.Server/Construction/Conditions/AmeShieldIntegrity.cs
+++ Content.Server/Construction/Conditions/AmeShieldIntegrity.cs
@@ -5,39 +5,39 @@ using Content.Shared.Examine;
 
 namespace Content.Server.Construction.Conditions;
 
-    [UsedImplicitly]
-    [DataDefinition]
-    public sealed partial class AmeShieldIntegrity : IGraphCondition
+[UsedImplicitly]
+[DataDefinition]
+public sealed partial class AmeShieldIntegrity : IGraphCondition
+{
+    [DataField]
+    public float IntegrityThreshold = 80;
+
+    /// <summary>
+    /// If true, checks for the integrity being above the threshold.
+    /// if false, checks for it being below.
+    /// </summary>
+    [DataField]
+    public bool CheckAbove = true;
+
+    public bool Condition(EntityUid uid, IEntityManager entityManager)
     {
-        [DataField]
-        public float IntegrityThreshold = 80;
+        if (!entityManager.TryGetComponent<AmeShieldComponent>(uid, out var shield))
+            return true;
 
-        /// <summary>
-        /// If true, checks for the integrity being above the threshold.
-        /// if false, checks for it being below.
-        /// </summary>
-        [DataField]
-        public bool CheckAbove = true;
-
-        public bool Condition(EntityUid uid, IEntityManager entityManager)
+        if (CheckAbove)
         {
-            if (!entityManager.TryGetComponent<AmeShieldComponent>(uid, out var shield))
-                return true;
-
-            if (CheckAbove)
-            {
-                return shield.CoreIntegrity >= IntegrityThreshold;
-            }
-            return shield.CoreIntegrity < IntegrityThreshold;
+            return shield.CoreIntegrity >= IntegrityThreshold;
         }
-
-        public bool DoExamine(ExaminedEvent args)
-        {
-            return false;
-        }
-
-        public IEnumerable<ConstructionGuideEntry> GenerateGuideEntry()
-        {
-            yield return new ConstructionGuideEntry();
-        }
+        return shield.CoreIntegrity < IntegrityThreshold;
     }
+
+    public bool DoExamine(ExaminedEvent args)
+    {
+        return false;
+    }
+
+    public IEnumerable<ConstructionGuideEntry> GenerateGuideEntry()
+    {
+        yield return new ConstructionGuideEntry();
+    }
+}
