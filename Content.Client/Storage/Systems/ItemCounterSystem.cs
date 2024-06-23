eca385ebfbaeef3dbcbeb946621858f646871bad
Crude Oil <croilbird@gmail.com>
2024-06-23 14:15:19 +0200

    move logic for open/closed containers into itemcountersystem

--- Content.Client/Storage/Systems/ItemCounterSystem.cs
+++ Content.Client/Storage/Systems/ItemCounterSystem.cs
@@ -1,5 +1,6 @@
 using Content.Shared.Rounding;
 using Content.Shared.Stacks;
+using Content.Shared.Storage;
 using Content.Shared.Storage.Components;
 using Content.Shared.Storage.EntitySystems;
 using Robust.Client.GameObjects;
@@ -32,10 +33,9 @@ public sealed class ItemCounterSystem : SharedItemCounterSystem
         if (!_appearanceSystem.TryGetData<bool>(uid, StackVisuals.Hide, out var hidden, args.Component))
             hidden = false;
 
-        // override the stack visuals hide property if the stack visuals should show regardless of whether a player
-        // is handling a container
-        if (!comp.OpenContainerOnly)
-            hidden = false;
+        // try and use the container state if there is one
+        if (_appearanceSystem.TryGetData<bool>(uid, StorageVisuals.Open, out var openContainer, args.Component))
+            hidden = !openContainer && comp.OpenContainerOnly;
 
         if (comp.IsComposite)
             ProcessCompositeSprite(uid, actual, maxCount, comp.LayerStates, hidden, sprite: args.Sprite);
