c8296e26b5540851342c1189d68e207344f352a2
JustUser <157776977+I-JustUser-I@users.noreply.github.com>
2024-06-24 10:30:18 +0300

    file scoped namespaces using

--- Content.Server/Construction/Components/TabletopComputerBoardComponent.cs
+++ Content.Server/Construction/Components/TabletopComputerBoardComponent.cs
@@ -1,12 +1,10 @@
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
-namespace Content.Server.Construction.Components
+namespace Content.Server.Construction.Components;
+[RegisterComponent, ComponentProtoName("TabletopComputerBoardComponent")]
+public sealed partial class TabletopComputerBoardComponent : Component
 {
-    [RegisterComponent, ComponentProtoName("TabletopComputerBoardComponent")]
-    public sealed partial class TabletopComputerBoardComponent : Component
-    {
-        [DataField("Prototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
-        public string? Prototype;
-    }
+    [DataField("Prototype", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string? Prototype;
 }
