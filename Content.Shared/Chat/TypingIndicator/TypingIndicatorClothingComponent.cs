4d2fc5694ebb1eba8fbda13a8eb379e79cda9893
Beck <beck314159@hotmail.com>
2024-06-26 17:18:17 -0700

    cool comments

--- Content.Shared/Chat/TypingIndicator/TypingIndicatorClothingComponent.cs
+++ Content.Shared/Chat/TypingIndicator/TypingIndicatorClothingComponent.cs
@@ -3,14 +3,25 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Chat.TypingIndicator;
 
+/// <summary>
+///     If an item is equipped to someones inventory (Anything but the pockets), and has this component
+///     the users typing indicator will be replaced by the prototype given in <c>TypingIndicatorPrototype</c>.
+/// </summary>
 [RegisterComponent, NetworkedComponent]
 [Access(typeof(SharedTypingIndicatorSystem))]
 public sealed partial class TypingIndicatorClothingComponent : Component
 {
+    /// <summary>
+    ///     The typing indicator that will override the default typing indicator when the item is equipped to a users
+    ///     inventory.
+    /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("proto", required: true)]
     public ProtoId<TypingIndicatorPrototype> TypingIndicatorPrototype = default!;
 
+    /// <summary>
+    ///     This stores the time the item was equipped in someones inventory. If null, item is currently not equipped.
+    /// </summary>
     [DataField]
     public TimeSpan? GotEquippedTime = null;
 }
