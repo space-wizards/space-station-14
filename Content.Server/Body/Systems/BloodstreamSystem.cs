ca19c6991ddf59e09843ad49cc2a483a17d9522a
Killerqu00 <killerqueen1777@gmail.com>
2024-06-29 23:48:43 +0200

    evil heat cauterize begone

--- Content.Server/Body/Systems/BloodstreamSystem.cs
+++ Content.Server/Body/Systems/BloodstreamSystem.cs
@@ -221,17 +221,18 @@ public sealed class BloodstreamSystem : EntitySystem
             _audio.PlayPvs(ent.Comp.InstantBloodSound, ent);
         }
 
-        // Heat damage will cauterize, causing the bleed rate to be reduced.
-        else if (totalFloat < 0 && oldBleedAmount > 0)
-        {
-            // Magically, this damage has healed some bleeding, likely
-            // because it's burn damage that cauterized their wounds.
+        // legacy cauterize code
 
-            // We'll play a special sound and popup for feedback.
-            _audio.PlayPvs(ent.Comp.BloodHealedSound, ent);
-            _popupSystem.PopupEntity(Loc.GetString("bloodstream-component-wounds-cauterized"), ent,
-                ent, PopupType.Medium);
-        }
+        // else if (totalFloat < 0 && oldBleedAmount > 0)
+        // {
+        //     // Magically, this damage has healed some bleeding, likely
+        //     // because it's burn damage that cauterized their wounds.
+        //
+        //     // We'll play a special sound and popup for feedback.
+        //     _audio.PlayPvs(ent.Comp.BloodHealedSound, ent);
+        //     _popupSystem.PopupEntity(Loc.GetString("bloodstream-component-wounds-cauterized"), ent,
+        //         ent, PopupType.Medium);
+        // }
     }
     /// <summary>
     ///     Shows text on health examine, based on bleed rate and blood level.
