b34901483dea2614d43db916ec70437ab29ef4fb
EmoGarbage404 <retron404@gmail.com>
2024-06-29 12:16:08 -0400

    reinforce sec intercoms

--- Content.Shared/Radio/EntitySystems/EncryptionKeySystem.cs
+++ Content.Shared/Radio/EntitySystems/EncryptionKeySystem.cs
@@ -31,6 +31,7 @@ public sealed partial class EncryptionKeySystem : EntitySystem
     [Dependency] private readonly SharedContainerSystem _container = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
+    [Dependency] private readonly SharedWiresSystem _wires = default!;
 
     public override void Initialize()
     {
@@ -150,7 +151,7 @@ public sealed partial class EncryptionKeySystem : EntitySystem
             return;
         }
 
-        if (TryComp<WiresPanelComponent>(uid, out var panel) && !panel.Open)
+        if (!_wires.IsPanelOpen(uid))
         {
             _popup.PopupClient(Loc.GetString("encryption-keys-panel-locked"), uid, args.User);
             return;
