6afdddc7902b17b99efff36fc71c20937ae9551f
Cojoke <83733158+Cojoke-dot@users.noreply.github.com>
2024-06-29 15:34:43 -0500

    press enter
    Co-authored-by: ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>

--- Content.Server/IgnitionSource/IgnitionEvent.cs
+++ Content.Server/IgnitionSource/IgnitionEvent.cs
@@ -3,4 +3,5 @@ namespace Content.Server.IgnitionSource;
 /// <summary>
 ///     Raised in order to toggle the ignitionSourceComponent on an entity on or off
 /// </summary>
-[ByRefEvent] public record struct IgnitionEvent(bool Ignite = false);
+[ByRefEvent]
+public record struct IgnitionEvent(bool Ignite = false);
