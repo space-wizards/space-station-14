9660c38af63de781f6ac72c95f4168ce5c8e277b
Gardanta Spirito <guardspirit@protonmail.com>
2024-07-01 00:34:20 +0300

    Bring up to date with master

--- Content.Server/EntityEffects/Effects/PlaySound.cs
+++ Content.Server/EntityEffects/Effects/PlaySound.cs
@@ -2,28 +2,28 @@ using JetBrains.Annotations;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Content.Shared.Chemistry.Reagent;
+using Content.Shared.EntityEffects;
 using Robust.Shared.Prototypes;
 
-namespace Content.Server.Chemistry.ReagentEffects
+namespace Content.Server.EntityEffects.Effects;
+
+/// <summary>
+/// Effect that continuously emits sound.
+/// </summary>
+[UsedImplicitly]
+[DataDefinition]
+public sealed partial class PlaySound : EntityEffect
 {
-    /// <summary>
-    /// Effect for reagents that continuously emit sound.
-    /// </summary>
-	[UsedImplicitly]
-    [DataDefinition]
-    public sealed partial class PlaySound : ReagentEffect
+    [DataField(required: true)]
+    public SoundSpecifier Sound;
+
+    // JUSTIFICATION: This is purely cosmetic.
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+        => null;
+
+    public override void Effect(EntityEffectBaseArgs args)
     {
-        [DataField(required: true)]
-        public SoundSpecifier Sound;
-
-        // JUSTIFICATION: This is purely cosmetic.
-        protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-            => null;
-
-        public override void Effect(ReagentEffectArgs args)
-        {
-            args.EntityManager.EntitySysManager.GetEntitySystem<SharedAudioSystem>()
-                .PlayPvs(Sound, args.SolutionEntity);
-        }
+        args.EntityManager.EntitySysManager.GetEntitySystem<SharedAudioSystem>()
+            .PlayPvs(Sound, args.TargetEntity);
     }
 }
