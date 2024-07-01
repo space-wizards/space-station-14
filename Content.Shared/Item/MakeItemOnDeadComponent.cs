4398579554c8444d074549ec24dce4b1f3d841de
lzk <124214523+lzk228@users.noreply.github.com>
2024-07-01 21:31:23 +0200

    add comment for component

--- Content.Shared/Item/MakeItemOnDeadComponent.cs
+++ Content.Shared/Item/MakeItemOnDeadComponent.cs
@@ -1,5 +1,8 @@
 namespace Content.Shared.Item
 {
+    /// <summary>
+    ///     Given to mobs that should be able to picked up after death. SHOULDN'T BE GIVEN TO ALREADY ITEM-MOBS
+    /// </summary>
     [RegisterComponent]
     public sealed partial class MakeItemOnDeadComponent : Component
     {
