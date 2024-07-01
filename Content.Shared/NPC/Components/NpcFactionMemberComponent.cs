b6b52787213084a8b5f8e940f8bc822f4382ebe4
lzk <124214523+lzk228@users.noreply.github.com>
2024-07-01 18:52:37 +0200

    :trollface:

--- Content.Shared/NPC/Components/NpcFactionMemberComponent.cs
+++ Content.Shared/NPC/Components/NpcFactionMemberComponent.cs
@@ -17,12 +17,24 @@ public sealed partial class NpcFactionMemberComponent : Component
     /// <summary>
     /// Cached friendly factions.
     /// </summary>
-    [DataField, ViewVariables]
-    public HashSet<ProtoId<NpcFactionPrototype>> FriendlyFactions = new();
+    [ViewVariables]
+    public readonly HashSet<ProtoId<NpcFactionPrototype>> FriendlyFactions = new();
 
     /// <summary>
     /// Cached hostile factions.
     /// </summary>
+    [ViewVariables]
+    public readonly HashSet<ProtoId<NpcFactionPrototype>> HostileFactions = new();
+
+    /// <summary>
+    /// Used to add friendly factions in prototypes.
+    /// </summary>
     [DataField, ViewVariables]
-    public HashSet<ProtoId<NpcFactionPrototype>> HostileFactions = new();
+    public HashSet<ProtoId<NpcFactionPrototype>>? AddFriendlyFactions;
+
+    /// <summary>
+    /// Used to add hostile factions in prototypes.
+    /// </summary>
+    [DataField, ViewVariables]
+    public HashSet<ProtoId<NpcFactionPrototype>>? AddHostileFactions;
 }
