840f556a420134affb2c248e582c98c49d2fcb64
lzk <124214523+lzk228@users.noreply.github.com>
2024-07-02 22:44:03 +0200

    refactor

---
+++ Content.Shared/Mobs/Components/AddCompOnMobStateChangeComponent.cs
@@ -0,0 +1,21 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Mobs.Components
+{
+    [RegisterComponent]
+    public sealed partial class AddCompOnMobStateChangeComponent : Component
+    {
+    /// <summary>
+    /// On this state the component will be given.
+    /// </summary>
+    [DataField]
+    public MobState MobState = new();
+
+    /// <summary>
+    /// Components added to the entity.
+    /// </summary>
+    [DataField]
+    public ComponentRegistry Components = new();
+    }
+}
+
