e9cec6735979b3c7ddaabe23274d236d8f97a0a7
Aidenkrz <aiden@djkraz.com>
2024-07-02 23:03:24 -0500

    MGS Change

--- Content.Server/StationEvents/Events/MassHallucinationsRule.cs
+++ Content.Server/StationEvents/Events/MassHallucinationsRule.cs
@@ -5,6 +5,7 @@ using Content.Shared.GameTicking.Components;
 using Content.Shared.Humanoid;
 using Content.Shared.Mind.Components;
 using Content.Shared.Traits.Assorted;
+using Robust.Shared.Collections;
 
 namespace Content.Server.StationEvents.Events;
 
@@ -12,28 +13,34 @@ public sealed class MassHallucinationsRule : StationEventSystem<MassHallucinatio
 {
     [Dependency] private readonly ParacusiaSystem _paracusia = default!;
 
+    private ValueList<EntityUid> _toChange = new();
+
     protected override void Started(EntityUid uid, MassHallucinationsRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
+
         base.Started(uid, component, gameRule, args);
+
+        _toChange.Clear();
         var query = EntityQueryEnumerator<MindContainerComponent, HumanoidAppearanceComponent>();
         while (query.MoveNext(out var ent, out _, out _))
-        {
             if (!HasComp<ParacusiaComponent>(ent))
-            {
-                EnsureComp<MassHallucinationsComponent>(ent);
-                var paracusia = EnsureComp<ParacusiaComponent>(ent);
-                _paracusia.SetSounds(ent, component.Sounds, paracusia);
-                _paracusia.SetTime(ent, component.MinTimeBetweenIncidents, component.MaxTimeBetweenIncidents, paracusia);
-                _paracusia.SetDistance(ent, component.MaxSoundDistance);
-            }
+                _toChange.Add(ent);
+
+        foreach (EntityUid ent in _toChange)
+        {
+            EnsureComp<MassHallucinationsComponent>(ent);
+            var paracusia = EnsureComp<ParacusiaComponent>(ent);
+            _paracusia.SetSounds(ent, component.Sounds, paracusia);
+            _paracusia.SetTime(ent, component.MinTimeBetweenIncidents, component.MaxTimeBetweenIncidents, paracusia);
+            _paracusia.SetDistance(ent, component.MaxSoundDistance);
         }
     }
 
     protected override void Ended(EntityUid uid, MassHallucinationsRuleComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
     {
         base.Ended(uid, component, gameRule, args);
-        var query = EntityQueryEnumerator<MassHallucinationsComponent>();
-        while (query.MoveNext(out var ent, out _))
+
+        foreach (EntityUid ent in _toChange)
         {
             RemComp<ParacusiaComponent>(ent);
             RemComp<MassHallucinationsComponent>(ent);
