58258a6c6950f2f26311c63c1febc42af7f3b80a
BeatusCrow <141257446+BeatusCrow@users.noreply.github.com>
2024-07-03 11:43:18 +0300

    Update DropPodSystem.cs

--- Content.Server/DropPod/DropPodSystem.cs
+++ Content.Server/DropPod/DropPodSystem.cs
@@ -82,9 +82,9 @@ namespace Content.Server.DropPod
 
         private void OnStartLandingButtonPressed(EntityUid uid, DropPodConsoleComponent component, DropPodStartMessage message)
         {
-            EntityUid end_station_uid = uid; // пока присваиваем вообще не нужное значение
-            EntityUid end_stationAlert_uid = uid; // пока присваиваем вообще не нужное значение
-            EntityUid start_station_uid = uid; // пока присваиваем вообще не нужное значение
+            EntityUid end_station_uid = uid;
+            EntityUid end_stationAlert_uid = uid;
+            EntityUid start_station_uid = uid;
             var x_form = Transform(uid);
             var Coords = x_form.Coordinates;
             int count = 0;
