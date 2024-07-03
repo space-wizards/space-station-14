b7e1ef45d836a201a65f38629d1d39e4e6035d21
bvelliquette <BenjaminVelliquette@gmail.com>
2024-07-03 02:32:22 -0400

    (probably) better sound solution.

--- Content.Client/Prying/PryingSystem.cs
+++ Content.Client/Prying/PryingSystem.cs
@@ -1,40 +1,8 @@
-using Content.Client.Storage.Components;
-using Content.Shared.Popups;
-using Content.Shared.Prying.Components;
 using Content.Shared.Prying.Systems;
-using Robust.Shared.Audio.Systems;
 
 namespace Content.Client.Prying;
 
 public sealed class PryingSystem : SharedPryingSystem
 {
-    [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
-    [Dependency] private readonly SharedPopupSystem _popup = default!;
-    public override void Initialize()
-    {
-        base.Initialize();
-        SubscribeLocalEvent<EntityStorageComponent, PryDoAfterEvent>(OnDoAfter);
-    }
 
-    private void OnDoAfter(EntityUid uid, EntityStorageComponent storage, PryDoAfterEvent args)
-    {
-        if (args.Cancelled)
-            return;
-        if (args.Target is null)
-            return;
-
-        TryComp<PryingComponent>(args.Used, out var comp);
-
-        if (!CanPry(uid, args.User, out var message, comp))
-        {
-            if (!string.IsNullOrWhiteSpace(message))
-                _popup.PopupClient(Loc.GetString(message), uid, args.User);
-            return;
-        }
-
-        if (args.Used != null && comp != null)
-        {
-            _audioSystem.PlayPredicted(comp.UseSound, args.Used.Value, args.User);
-        }
-    }
 }
--- Content.Server/Prying/PryingSystem.cs
+++ Content.Server/Prying/PryingSystem.cs
@@ -1,11 +1,13 @@
 using Content.Server.Storage.Components;
 using Content.Shared.Prying.Components;
 using Content.Shared.Prying.Systems;
+using Robust.Shared.Audio.Systems;
 
 namespace Content.Server.Prying;
 
 public sealed class PryingSystem : SharedPryingSystem
 {
+    [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
     public override void Initialize()
     {
         base.Initialize();
@@ -18,6 +20,11 @@ public sealed class PryingSystem : SharedPryingSystem
             return;
         if (args.Target is null)
             return;
+
+        if (args.Used != null && TryComp<PryingComponent>(args.Used, out var comp))
+        {
+            _audioSystem.PlayPvs(comp.UseSound, args.Used.Value);
+        }
 
         var ev = new PriedEvent(args.User);
         RaiseLocalEvent(uid, ref ev);
