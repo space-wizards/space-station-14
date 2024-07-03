4abdb494d6d8a46a8f08a2902c4c89cfa3a561fa
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-07-03 09:29:00 -0700

    File scoped namespace

--- Content.Server/Construction/Completions/GivePrototype.cs
+++ Content.Server/Construction/Completions/GivePrototype.cs
@@ -7,45 +7,44 @@ using Content.Shared.Stacks;
 using JetBrains.Annotations;
 using Robust.Shared.Prototypes;
 
-namespace Content.Server.Construction.Completions
+namespace Content.Server.Construction.Completions;
+
+[UsedImplicitly]
+[DataDefinition]
+public sealed partial class GivePrototype : IGraphAction
 {
-    [UsedImplicitly]
-    [DataDefinition]
-    public sealed partial class GivePrototype : IGraphAction
+    [DataField]
+    public ProtoId<EntityPrototype> Prototype { get; private set; } = string.Empty;
+
+    [DataField]
+    public int Amount { get; private set; } = 1;
+
+    public void PerformAction(EntityUid uid, EntityUid? userUid, IEntityManager entityManager)
     {
-        [DataField]
-        public ProtoId<EntityPrototype> Prototype { get; private set; } = string.Empty;
+        if (string.IsNullOrEmpty(Prototype))
+            return;
 
-        [DataField]
-        public int Amount { get; private set; } = 1;
-
-        public void PerformAction(EntityUid uid, EntityUid? userUid, IEntityManager entityManager)
+        if (EntityPrototypeHelpers.HasComponent<StackComponent>(Prototype))
         {
-            if (string.IsNullOrEmpty(Prototype))
+            var stackSystem = entityManager.EntitySysManager.GetEntitySystem<StackSystem>();
+            var stacks = stackSystem.SpawnMultiple(Prototype, Amount, userUid ?? uid);
+
+            if (userUid is null || !entityManager.TryGetComponent(userUid, out HandsComponent? handsComp))
                 return;
 
-            if (EntityPrototypeHelpers.HasComponent<StackComponent>(Prototype))
+            foreach (var item in stacks)
             {
-                var stackSystem = entityManager.EntitySysManager.GetEntitySystem<StackSystem>();
-                var stacks = stackSystem.SpawnMultiple(Prototype, Amount, userUid ?? uid);
-
-                if (userUid is null || !entityManager.TryGetComponent(userUid, out HandsComponent? handsComp))
-                    return;
-
-                foreach (var item in stacks)
-                {
-                    stackSystem.TryMergeToHands(item, userUid.Value, hands: handsComp);
-                }
+                stackSystem.TryMergeToHands(item, userUid.Value, hands: handsComp);
             }
-            else
+        }
+        else
+        {
+            var handsSystem = entityManager.EntitySysManager.GetEntitySystem<SharedHandsSystem>();
+            var handsComp = userUid is not null ? entityManager.GetComponent<HandsComponent>(userUid.Value) : null;
+            for (var i = 0; i < Amount; i++)
             {
-                var handsSystem = entityManager.EntitySysManager.GetEntitySystem<SharedHandsSystem>();
-                var handsComp = userUid is not null ? entityManager.GetComponent<HandsComponent>(userUid.Value) : null;
-                for (var i = 0; i < Amount; i++)
-                {
-                    var item = entityManager.SpawnNextToOrDrop(Prototype, userUid ?? uid);
-                    handsSystem.PickupOrDrop(userUid, item, handsComp: handsComp);
-                }
+                var item = entityManager.SpawnNextToOrDrop(Prototype, userUid ?? uid);
+                handsSystem.PickupOrDrop(userUid, item, handsComp: handsComp);
             }
         }
     }
