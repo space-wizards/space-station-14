b2f6e58dce9d19d76e43f5ffefb027525f876af7
Simon <63975668+Simyon264@users.noreply.github.com>
2024-07-06 12:15:51 +0200

    Explain a bit more how whitelist checking works

--- Content.Server/Connection/Whitelist/WhitelistPrototype.cs
+++ Content.Server/Connection/Whitelist/WhitelistPrototype.cs
@@ -7,6 +7,15 @@ namespace Content.Server.Connection.Whitelist;
 /// <summary>
 /// Used by the <see cref="ConnectionManager"/> to determine if a player should be allowed to join the server.
 /// Used in the whitelist.prototype_list CVar.
+///
+/// Whitelists are used to determine if a player is allowed to connect.
+/// You define a PlayerConnectionWhitelist with a list of conditions.
+/// Every condition has a type and a <see cref="ConditionAction"/> along with other parameters depending on the type.
+/// Action must either be Allow, Deny or Next.
+/// Allow means the player is instantly allowed to connect if the condition is met.
+/// Deny means the player is instantly denied to connect if the condition is met.
+/// Next means the next condition in the list is checked.
+/// If the condition doesn't match, the next condition is checked.
 /// </summary>
 [Prototype("PlayerConnectionWhitelist")]
 public sealed class PlayerConnectionWhitelistPrototype : IPrototype
