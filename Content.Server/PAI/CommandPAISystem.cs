009ac90648572ad477b5eb86c12384fda673465e
Baptr0b0t <bapt.buriot@gmail.com>
2024-07-07 14:34:28 +0200

    Cleaning

--- Content.Server/PAI/CommandPAISystem.cs
+++ Content.Server/PAI/CommandPAISystem.cs
@@ -1,14 +1,4 @@
-using Content.Server.Ghost.Roles;
-using Content.Server.Ghost.Roles.Components;
-using Content.Server.Instruments;
-using Content.Server.Kitchen.Components;
-using Content.Shared.Interaction.Events;
-using Content.Shared.Mind.Components;
 using Content.Shared.PAI;
-using Content.Shared.Popups;
-using Robust.Shared.Random;
-using System.Text;
-using Robust.Shared.Player;
 
 namespace Content.Server.PAI;
 
--- Content.Shared/PAI/SharedCommandPAISystem.cs
+++ Content.Shared/PAI/SharedCommandPAISystem.cs
@@ -2,15 +2,6 @@ using Content.Shared.Actions;
 
 namespace Content.Shared.PAI
 {
-    /// <summary>
-    /// pAIs, or Personal AIs, are essentially portable ghost role generators.
-    /// In their current implementation, they create a ghost role anyone can access,
-    /// and that a player can also "wipe" (reset/kick out player).
-    /// Theoretically speaking pAIs are supposed to use a dedicated "offer and select" system,
-    ///  with the player holding the pAI being able to choose one of the ghosts in the round.
-    /// This seems too complicated for an initial implementation, though,
-    ///  and there's not always enough players and ghost roles to justify it.
-    /// </summary>
     public abstract class SharedCommandPAISystem : EntitySystem
     {
         [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
