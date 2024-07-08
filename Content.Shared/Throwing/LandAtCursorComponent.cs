950ac4eb06785366a0d03919b1f0adac24278354
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2024-07-08 14:01:54 +1000

    Update Content.Shared/Throwing/LandAtCursorComponent.cs

--- Content.Shared/Throwing/LandAtCursorComponent.cs
+++ Content.Shared/Throwing/LandAtCursorComponent.cs
@@ -5,7 +5,7 @@ namespace Content.Shared.Throwing
     /// <summary>
     ///     Makes an item land at the cursor when thrown and slide a little further.
     ///     Without it the item lands slightly in front and stops moving at the cursor.
-    ///     Use this for throwing weapons that should pierce the oppenent, for example spears.
+    ///     Use this for throwing weapons that should pierce the opponent, for example spears.
     /// </summary>
     [RegisterComponent, NetworkedComponent]
     public sealed partial class LandAtCursorComponent : Component { }
