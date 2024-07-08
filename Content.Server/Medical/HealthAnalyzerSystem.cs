d79280a145c9ad8295cbe33c4afe4cf42679c91e
lzk <124214523+lzk228@users.noreply.github.com>
2024-07-08 09:09:53 +0200

    fix showing popup to all

--- Content.Server/Medical/HealthAnalyzerSystem.cs
+++ Content.Server/Medical/HealthAnalyzerSystem.cs
@@ -90,7 +90,7 @@ public sealed class HealthAnalyzerSystem : EntitySystem
         });
 
         var msg = Loc.GetString("health-analyzer-popup-scan-target", ("user", Identity.Entity(args.User, EntityManager)));
-        _popupSystem.PopupEntity(msg, args.Target.Value, PopupType.Medium);
+        _popupSystem.PopupEntity(msg, args.Target.Value, args.Target.Value, PopupType.Medium);
     }
 
     private void OnDoAfter(Entity<HealthAnalyzerComponent> uid, ref HealthAnalyzerDoAfterEvent args)
