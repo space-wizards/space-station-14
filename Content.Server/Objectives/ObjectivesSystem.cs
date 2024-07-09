73eecbc92cc75229b194d94eded5b58c2e95430b
lzk <124214523+lzk228@users.noreply.github.com>
2024-07-09 16:44:38 +0200

    huh!

--- Content.Server/Objectives/ObjectivesSystem.cs
+++ Content.Server/Objectives/ObjectivesSystem.cs
@@ -128,7 +128,7 @@ public sealed class ObjectivesSystem : SharedObjectivesSystem
             var agentSummary = new StringBuilder();
             agentSummary.AppendLine(Loc.GetString("objectives-with-objectives", ("custody", custody), ("title", title), ("agent", agent)));
 
-            foreach (var objectiveGroup in objectives.GroupBy(o => Comp<ObjectiveComponent>(o).Issuer))
+            foreach (var objectiveGroup in objectives.GroupBy(o => Comp<ObjectiveComponent>(o).LocIssuer))
             {
                 //TO DO:
                 //check for the right group here. Getting the target issuer is easy: objectiveGroup.Key
