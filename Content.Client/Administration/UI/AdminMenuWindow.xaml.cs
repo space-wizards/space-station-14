0838a09f3fdd04f69b2ee606a65dcebbf96f70e3
Chief-Engineer <119664036+Chief-Engineer@users.noreply.github.com>
2024-07-10 21:02:16 -0500

    Revert "Revert Baby Jail (#29891)"
    This reverts commit 24a2866747e6de37ab2c4395bee9ea44c9211c6f.

--- Content.Client/Administration/UI/AdminMenuWindow.xaml.cs
+++ Content.Client/Administration/UI/AdminMenuWindow.xaml.cs
@@ -15,14 +15,18 @@ public sealed partial class AdminMenuWindow : DefaultWindow
         MinSize = new Vector2(650, 250);
         Title = Loc.GetString("admin-menu-title");
         RobustXamlLoader.Load(this);
-        MasterTabContainer.SetTabTitle(0, Loc.GetString("admin-menu-admin-tab"));
-        MasterTabContainer.SetTabTitle(1, Loc.GetString("admin-menu-adminbus-tab"));
-        MasterTabContainer.SetTabTitle(2, Loc.GetString("admin-menu-atmos-tab"));
-        MasterTabContainer.SetTabTitle(3, Loc.GetString("admin-menu-round-tab"));
-        MasterTabContainer.SetTabTitle(4, Loc.GetString("admin-menu-server-tab"));
-        MasterTabContainer.SetTabTitle(5, Loc.GetString("admin-menu-panic-bunker-tab"));
-        MasterTabContainer.SetTabTitle(6, Loc.GetString("admin-menu-players-tab"));
-        MasterTabContainer.SetTabTitle(7, Loc.GetString("admin-menu-objects-tab"));
+        MasterTabContainer.SetTabTitle((int) TabIndex.Admin, Loc.GetString("admin-menu-admin-tab"));
+        MasterTabContainer.SetTabTitle((int) TabIndex.Adminbus, Loc.GetString("admin-menu-adminbus-tab"));
+        MasterTabContainer.SetTabTitle((int) TabIndex.Atmos, Loc.GetString("admin-menu-atmos-tab"));
+        MasterTabContainer.SetTabTitle((int) TabIndex.Round, Loc.GetString("admin-menu-round-tab"));
+        MasterTabContainer.SetTabTitle((int) TabIndex.Server, Loc.GetString("admin-menu-server-tab"));
+        MasterTabContainer.SetTabTitle((int) TabIndex.PanicBunker, Loc.GetString("admin-menu-panic-bunker-tab"));
+        /*
+         * TODO: Remove baby jail code once a more mature gateway process is established. This code is only being issued as a stopgap to help with potential tiding in the immediate future.
+         */
+        MasterTabContainer.SetTabTitle((int) TabIndex.BabyJail, Loc.GetString("admin-menu-baby-jail-tab"));
+        MasterTabContainer.SetTabTitle((int) TabIndex.Players, Loc.GetString("admin-menu-players-tab"));
+        MasterTabContainer.SetTabTitle((int) TabIndex.Objects, Loc.GetString("admin-menu-objects-tab"));
         MasterTabContainer.OnTabChanged += OnTabChanged;
     }
 
