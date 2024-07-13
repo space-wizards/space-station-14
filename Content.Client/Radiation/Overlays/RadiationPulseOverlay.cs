bf17990e71fb221d2cabcff87c81618ade8f6b6c
plykiya <plykiya@protonmail.com>
2024-07-13 02:28:47 -0700

    please have mercy

--- Content.Client/Radiation/Overlays/RadiationPulseOverlay.cs
+++ Content.Client/Radiation/Overlays/RadiationPulseOverlay.cs
@@ -26,11 +26,11 @@ namespace Content.Client.Radiation.Overlays
         private readonly ShaderInstance _baseShader;
         private readonly Dictionary<EntityUid, (ShaderInstance shd, RadiationShaderInstance instance)> _pulses = new();
 
-        public RadiationPulseOverlay(SharedTransformSystem transformSystem)
+        public RadiationPulseOverlay()
         {
             IoCManager.InjectDependencies(this);
             _baseShader = _prototypeManager.Index<ShaderPrototype>("Radiation").Instance().Duplicate();
-            _transformSystem = transformSystem;
+            _transformSystem = _entityManager.System<SharedTransformSystem>();
         }
 
         protected override bool BeforeDraw(in OverlayDrawArgs args)
