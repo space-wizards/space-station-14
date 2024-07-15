f183d784fd6fe1f23a56f6a16d14ec65d5c5daa7
Luiz Costa <luiz@mavromati.com>
2024-07-15 18:15:15 -0300

    slightly offset the typing indicator when wearing a hud

--- Content.Client/Chat/TypingIndicator/TypingIndicatorVisualizerSystem.cs
+++ Content.Client/Chat/TypingIndicator/TypingIndicatorVisualizerSystem.cs
@@ -3,6 +3,9 @@ using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
 using Robust.Shared.Prototypes;
 using Content.Shared.Inventory;
+using Content.Shared.Overlays;
+using System.Numerics;
+
 
 namespace Content.Client.Chat.TypingIndicator;
 
@@ -11,6 +14,10 @@ public sealed class TypingIndicatorVisualizerSystem : VisualizerSystem<TypingInd
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly InventorySystem _inventory = default!;
 
+    /// <summary>
+    ///     How far the typing indicator should be offset when the entity is using a HUD.
+    /// </summary>
+    private const float TypingIndicatorVerticalOffset = -0.12f;
 
     protected override void OnAppearanceChange(EntityUid uid, TypingIndicatorComponent component, ref AppearanceChangeEvent args)
     {
@@ -40,10 +47,21 @@ public sealed class TypingIndicatorVisualizerSystem : VisualizerSystem<TypingInd
         if (!layerExists)
             layer = args.Sprite.LayerMapReserveBlank(TypingIndicatorLayers.Base);
 
+        // If the entity is using eyewear that shows a HUD, offset the typing indicator slightly down so it's not obscured.
+        // TODO: Consider making the typing indicator an actual overlay instead of a sprite layer.
+        var verticalOffset = 0f;
+        if(_inventory.TryGetSlotEntity(uid, "eyes", out var slotEntity) &&
+            HasComp<ShowHealthBarsComponent>(slotEntity) && HasComp<ShowHealthIconsComponent>(slotEntity))
+        {
+            verticalOffset = TypingIndicatorVerticalOffset;
+        }
+
+        var offset = new Vector2(proto.Offset.X, proto.Offset.Y + verticalOffset);
+
         args.Sprite.LayerSetRSI(layer, proto.SpritePath);
         args.Sprite.LayerSetState(layer, proto.TypingState);
         args.Sprite.LayerSetShader(layer, proto.Shader);
-        args.Sprite.LayerSetOffset(layer, proto.Offset);
+        args.Sprite.LayerSetOffset(layer, offset);
         args.Sprite.LayerSetVisible(layer, isTyping);
     }
 }
