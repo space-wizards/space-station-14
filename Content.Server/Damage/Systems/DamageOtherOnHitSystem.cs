d7af9e8a660a59593022dcc7f939a5e2074bc8a7
plykiya <plykiya@protonmail.com>
2024-07-16 20:12:41 -0700

    FUCK YOU

--- Content.Server/Damage/Systems/DamageOtherOnHitSystem.cs
+++ Content.Server/Damage/Systems/DamageOtherOnHitSystem.cs
@@ -52,12 +52,6 @@ namespace Content.Server.Damage.Systems
                     _sharedCameraRecoil.KickCamera(args.Target, direction);
                 }
             }
-
-            // TODO: If more stuff touches this then handle it after.
-            if (TryComp<PhysicsComponent>(uid, out var physics))
-            {
-                _thrownItem.LandComponent(args.Thrown, args.Component, physics, false);
-            }
         }
 
         private void OnDamageExamine(EntityUid uid, DamageOtherOnHitComponent component, ref DamageExamineEvent args)
