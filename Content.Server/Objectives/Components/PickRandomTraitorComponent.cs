f673c1b7f412ba105f5d8d8e5b7d3b2712dc42fe
V <None>
2024-07-17 18:40:32 -0500

    small comment change

--- Content.Server/Objectives/Components/PickRandomTraitorComponent.cs
+++ Content.Server/Objectives/Components/PickRandomTraitorComponent.cs
@@ -3,7 +3,7 @@ using Content.Server.Objectives.Systems;
 namespace Content.Server.Objectives.Components;
 
 /// <summary>
-/// Sets the target for <see cref="TargetObjectiveComponent"/> to a random head.
+/// Sets the target for <see cref="TargetObjectiveComponent"/> to a random traitor.
 /// If there are no heads it will fallback to any person.
 /// </summary>
 [RegisterComponent, Access(typeof(KillPersonConditionSystem))]
