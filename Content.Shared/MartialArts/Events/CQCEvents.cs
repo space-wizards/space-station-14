e835ce8ec6bb10edf3a000ffc4823e5b9244e398
FaDeOkno <logkedr18@gmail.com>
2024-07-18 23:40:15 +0400

    fix attempt via serialization

--- Content.Shared/MartialArts/Events/CQCEvents.cs
+++ Content.Shared/MartialArts/Events/CQCEvents.cs
@@ -1,23 +1,29 @@
 using Content.Shared.Movement.Components;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.MartialArts;
 
+[Serializable, NetSerializable]
 public sealed class CQCSlamPerformedEvent : EntityEventArgs
 {
 }
 
+[Serializable, NetSerializable]
 public sealed class CQCKickPerformedEvent : EntityEventArgs
 {
 }
 
+[Serializable, NetSerializable]
 public sealed class CQCRestrainPerformedEvent : EntityEventArgs
 {
 }
 
+[Serializable, NetSerializable]
 public sealed class CQCPressurePerformedEvent : EntityEventArgs
 {
 }
 
+[Serializable, NetSerializable]
 public sealed class CQCConsecutivePerformedEvent : EntityEventArgs
 {
 }
