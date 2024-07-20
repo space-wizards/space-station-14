c9cdd0dbccfce89473b77f4de5eff9c544f8d5c9
DrSmugleaf <drsmugleaf@gmail.com>
2024-07-20 00:34:17 -0700

    Add xmldoc

--- Content.Shared/Throwing/ThrowAttemptEvent.cs
+++ Content.Shared/Throwing/ThrowAttemptEvent.cs
@@ -13,6 +13,11 @@
         public EntityUid ItemUid { get; }
     }
 
+    /// <summary>
+    ///     Raised on the item entity that is thrown.
+    /// </summary>
+    /// <param name="User">The user that threw this entity.</param>
+    /// <param name="Cancelled">Whether or not the throw should be cancelled.</param>
     [ByRefEvent]
     public record struct ThrowItemAttemptEvent(EntityUid User, bool Cancelled = false);
 
