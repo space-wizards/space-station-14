59489ee6ad59cbf0157f065a1b8bf1b12b78f8e7
pofitlo <kuzminvladislav237@gmail.com>
2024-07-20 04:29:56 +0400

    changes
    changes on demand of slarticodefast

--- Content.Server/SyndicateTeleporter/SyndicateTeleporterComponent.cs
+++ Content.Server/SyndicateTeleporter/SyndicateTeleporterComponent.cs
@@ -5,23 +5,28 @@ namespace Content.Server.SyndicateTeleporter;
 [RegisterComponent]
 public sealed partial class SyndicateTeleporterComponent : Component
 {
-    [DataField("randomValue")]
-    [ViewVariables(VVAccess.ReadWrite)]
+    /// <summary>
+    /// adds a random value to which you teleport, which is added to the guaranteed teleport value. from 0 to the set number. set 0 if you don't need randomness when teleporting
+    /// </summary>
+    [DataField]
     public int RandomValue = 4;
-
-    [DataField("teleportationValue")]
-    [ViewVariables(VVAccess.ReadWrite)]
+    /// <summary>
+    /// this is the guaranteed number of tiles that you teleport to.
+    /// </summary>
+    [DataField]
     public float TeleportationValue = 4f;
-
-    [DataField("saveAttempts")]
-    [ViewVariables(VVAccess.ReadWrite)]
+    /// <summary>
+    /// how many attempts do you have to teleport into the wall without a fatal outcome
+    /// </summary>
+    [DataField]
     public int SaveAttempts = 1;
-
-    [DataField("saveDistance")]
-    [ViewVariables(VVAccess.ReadWrite)]
+    /// <summary>
+    /// the distance to which you will be teleported when you teleport into a wall
+    /// </summary>
+    [DataField]
     public int SaveDistance = 3;
 
-    [ViewVariables(VVAccess.ReadWrite), DataField("alarm"), AutoNetworkedField]
+    [DataField("alarm"), AutoNetworkedField]
     public SoundSpecifier? AlarmSound = new SoundPathSpecifier("/Audio/Effects/beeps.ogg");
 
 }
