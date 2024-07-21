e610493f65e2a10351b0bd352db0d6d5a143cf47
Brandon H <Brandon@brandon-huu.com>
2024-07-21 17:21:09 -0500

    tweak(TransferAmountWindow): QoL stuff

--- Content.Client/Chemistry/UI/TransferAmountBoundUserInterface.cs
+++ Content.Client/Chemistry/UI/TransferAmountBoundUserInterface.cs
@@ -1,8 +1,10 @@
 using Content.Shared.Chemistry;
+using Content.Shared.Chemistry.Components;
 using Content.Shared.FixedPoint;
 using JetBrains.Annotations;
 using Robust.Client.GameObjects;
 using Robust.Client.UserInterface;
+using Robust.Shared.GameObjects;
 
 namespace Content.Client.Chemistry.UI
 {
@@ -11,15 +13,22 @@ namespace Content.Client.Chemistry.UI
     {
         [ViewVariables]
         private TransferAmountWindow? _window;
+        private IEntityManager _entManager;
+        private EntityUid _owner;
 
         public TransferAmountBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
         {
+          _owner = owner;
+          _entManager = IoCManager.Resolve<IEntityManager>();
         }
 
         protected override void Open()
         {
             base.Open();
             _window = this.CreateWindow<TransferAmountWindow>();
+
+            if(_entManager.TryGetComponent<SolutionTransferComponent>(_owner, out var comp))
+              _window.SetBounds(comp.MinimumTransferAmount.Value, comp.MaximumTransferAmount.Value);
 
             _window.ApplyButton.OnPressed += _ =>
             {
