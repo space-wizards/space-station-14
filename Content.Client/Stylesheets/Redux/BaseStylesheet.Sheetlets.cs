1e52d6b69438efe96c0dfb955858ce67148bc312
Brandon Li <sirbrandonthenerd@gmail.com>
2024-07-21 14:35:25 -0400

    Make `GetSheetletRules` not horrible

--- Content.Client/Stylesheets/Redux/BaseStylesheet.Sheetlets.cs
+++ Content.Client/Stylesheets/Redux/BaseStylesheet.Sheetlets.cs
@@ -1,4 +1,5 @@
 ﻿using Robust.Client.UserInterface;
+using Robust.Shared.Utility;
 
 namespace Content.Client.Stylesheets.Redux;
 
@@ -6,17 +7,25 @@ public abstract partial class BaseStylesheet
 {
     public StyleRule[] GetSheetletRules<TSheetTy>(Type sheetletTy)
     {
+        Sheetlet<TSheetTy>? sheetlet = null;
         try
         {
-            if (SandboxHelper.CreateInstance(sheetletTy.MakeGenericType(typeof(TSheetTy))) is Sheetlet<TSheetTy>
-                sheetlet)
-                return sheetlet.GetRules((TSheetTy)(object)this, _config);
+            if (sheetletTy.ContainsGenericParameters)
+            {
+                if (SandboxHelper.CreateInstance(sheetletTy.MakeGenericType(typeof(TSheetTy))) is Sheetlet<TSheetTy>
+                    sheetlet1)
+                    sheetlet = sheetlet1;
+            }
+            else if (SandboxHelper.CreateInstance(sheetletTy) is Sheetlet<TSheetTy> sheetlet2)
+            {
+                sheetlet = sheetlet2;
+            }
         }
-        catch (Exception)
-        {
-            // ignored
-        }
-        return [];
+        // thrown when `sheetletTy.MakeGenericType` is given a type that does not satisfy the type constraints of
+        // `sheetletTy`
+        catch (ArgumentException) { }
+
+        return sheetlet is not null ? sheetlet.GetRules((TSheetTy)(object)this, _config) : [];
     }
 
     public StyleRule[] GetSheetletRules<T, TSheetTy>()
