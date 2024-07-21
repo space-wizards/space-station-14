6a485fa451663aad93121c800110d3022d281d92
Brandon H <Brandon@brandon-huu.com>
2024-07-21 15:37:33 -0500

    remove

--- Content.Shared/Chemistry/EntitySystems/SolutionTransferSystem.cs
+++ Content.Shared/Chemistry/EntitySystems/SolutionTransferSystem.cs
@@ -72,9 +72,6 @@ public sealed class SolutionTransferSystem : EntitySystem
         var user = args.User;
         foreach (var amount in DefaultTransferAmounts)
         {
-            if (amount < comp.MinimumTransferAmount || amount > comp.MaximumTransferAmount)
-                continue;
-
             AlternativeVerb verb = new();
             verb.Text = Loc.GetString("comp-solution-transfer-verb-amount", ("amount", amount));
             verb.Category = VerbCategory.SetTransferAmount;
