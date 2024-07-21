ad12fdfbd38289ff10d1c999648c214e87dabeb2
nikthechampiongr <nikvog4@gmail.com>
2024-07-21 18:36:45 +0300

    Add functionality to most playerpanel buttons

--- Content.Client/Administration/UI/PlayerPanel/PlayerPanelEui.cs
+++ Content.Client/Administration/UI/PlayerPanel/PlayerPanelEui.cs
@@ -1,4 +1,6 @@
+using Content.Client.Administration.Managers;
 using Content.Client.Eui;
+using Content.Client.Info.PlaytimeStats;
 using Content.Shared.Administration;
 using Content.Shared.Eui;
 using JetBrains.Annotations;
@@ -11,12 +13,29 @@ public sealed class PlayerPanelEui : BaseEui
 {
     [Dependency] private readonly EuiManager _eui = default!;
     [Dependency] private readonly IClientConsoleHost _console = default!;
+    [Dependency] private readonly IClientAdminManager _adminManager = default!;
 
     private PlayerPanel PlayerPanel { get;  }
 
     public PlayerPanelEui()
     {
-        PlayerPanel = new PlayerPanel();
+        PlayerPanel = new PlayerPanel(_adminManager);
+        PlayerPanel.OnOpenNotes += id => _console.ExecuteCommand($"adminnotes {id}");
+        // Kick command does not support GUIDs
+        PlayerPanel.OnKick += username => _console.ExecuteCommand($"kick {username}");
+        PlayerPanel.OnOpenBanPanel += id => _console.ExecuteCommand($"banpanel {id}");
+        PlayerPanel.OnOpenBans += id => _console.ExecuteCommand($"banlist {id}");
+        PlayerPanel.OnOpenNotes += id => _console.ExecuteCommand($"adminnotes {id}");
+        PlayerPanel.OnWhitelistToggle += (id, whitelisted) =>
+        {
+            if (whitelisted)
+                _console.ExecuteCommand($"whitelistremove {id}");
+            else
+            {
+                _console.ExecuteCommand($"whitelistadd {id}");
+            }
+        };
+
         PlayerPanel.OnClose += () => SendMessage(new CloseEuiMessage());
     }
 
@@ -42,6 +61,6 @@ public sealed class PlayerPanelEui : BaseEui
         PlayerPanel.SetBans(s.TotalBans, s.TotalRoleBans);
         PlayerPanel.SetNotes(s.TotalNotes);
         PlayerPanel.SetWhitelisted(s.Whitelisted);
+        PlayerPanel.SetButtons();
     }
-
 }
