6bca19017fed767a661b7e6dc3eeb0d8a0fbe48f
aksenV <zendr32@gmail.com>
2024-07-25 22:55:12 +0900

    Revert "Merge pull request #4 from LogDogWhy/Z"
    This reverts commit 0d90c213045b431319fb5b2b2ea1313c3132346d, reversing
    changes made to c96a48b2dc2c73fef0571cf44f34c64af0498d3b.

--- Content.Shared/Flash/Components/FlashModifierComponent.cs
+++
@@ -1,8 +0,0 @@
- namespace Content.Shared.Flash.Components;
-
-[RegisterComponent]
-public sealed partial class FlashModifierComponent : Component
-{
-    [DataField]
-    public float Modifier = 1f;
-}
