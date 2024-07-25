3919fe439003a2de60fc8f95be5643ae6ef2e9a2
Brandon H <Brandon@brandon-huu.com>
2024-07-25 10:38:54 -0500

    Many have fallen

--- Content.Server/Antag/AntagSelectionSystem.cs
+++ Content.Server/Antag/AntagSelectionSystem.cs
@@ -328,7 +328,7 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
 
         if (session != null)
         {
-            curMind = _mind.CreateMind(session.UserId, Name(antagEnt.Value));
+            var curMind = _mind.CreateMind(session.UserId, Name(antagEnt.Value));
             _mind.SetUserId(curMind.Value, session.UserId);
 
             _mind.TransferTo(curMind.Value, antagEnt, ghostCheckOverride: true);
