6f62c03f8c9e7ff963383f60ffa3484067f8f2a9
Beck <beck314159@hotmail.com>
2024-07-28 16:34:42 -0700

    First commmit

--- Content.Server/Objectives/Systems/KillPersonConditionSystem.cs
+++ Content.Server/Objectives/Systems/KillPersonConditionSystem.cs
@@ -1,4 +1,5 @@
 using Content.Server.Objectives.Components;
+using Content.Server.GameTicking.Rules;
 using Content.Server.Shuttles.Systems;
 using Content.Shared.CCVar;
 using Content.Shared.Mind;
@@ -6,6 +7,8 @@ using Content.Shared.Objectives.Components;
 using Content.Shared.Roles.Jobs;
 using Robust.Shared.Configuration;
 using Robust.Shared.Random;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Content.Server.Objectives.Systems;
 
@@ -20,6 +23,7 @@ public sealed class KillPersonConditionSystem : EntitySystem
     [Dependency] private readonly SharedJobSystem _job = default!;
     [Dependency] private readonly SharedMindSystem _mind = default!;
     [Dependency] private readonly TargetObjectiveSystem _target = default!;
+    [Dependency] private readonly TraitorRuleSystem _traitorRule = default!;
 
     public override void Initialize()
     {
@@ -30,6 +34,8 @@ public sealed class KillPersonConditionSystem : EntitySystem
         SubscribeLocalEvent<PickRandomPersonComponent, ObjectiveAssignedEvent>(OnPersonAssigned);
 
         SubscribeLocalEvent<PickRandomHeadComponent, ObjectiveAssignedEvent>(OnHeadAssigned);
+        SubscribeLocalEvent<PickRandomTraitorComponent, ObjectiveAssignedEvent>(OnTraitorKillAssigned);
+
     }
 
     private void OnGetProgress(EntityUid uid, KillPersonConditionComponent comp, ref ObjectiveGetProgressEvent args)
@@ -97,6 +103,47 @@ public sealed class KillPersonConditionSystem : EntitySystem
             allHeads = allHumans; // fallback to non-head target
 
         _target.SetTarget(uid, _random.Pick(allHeads), target);
+    }
+
+    private void OnTraitorKillAssigned(EntityUid uid, PickRandomTraitorComponent comp, ref ObjectiveAssignedEvent args)
+    {
+        if (!TryComp<TargetObjectiveComponent>(uid, out var target))
+        {
+            Debug.Fail($"Missing components for {uid}.");
+            args.Cancelled = true;
+            return;
+        }
+
+        // target already assigned
+        if (target.Target != null)
+        {
+            Debug.Fail($"Target already assigned for {uid}.");
+            args.Cancelled = true;
+            return;
+        }
+
+        var traitors = _traitorRule.GetOtherTraitorMindsAliveAndConnected(args.Mind);
+
+        // No other traitors
+        if (traitors.Count == 0)
+        {
+            args.Cancelled = true;
+            return;
+        }
+
+        var targetTraitorMind = _random.Pick(traitors).Id;
+
+        // Make sure the current traitor doesn't have any targets targeting this traitor already.
+        foreach (var objective in args.Mind.Objectives)
+        {
+            if (TryComp<TargetObjectiveComponent>(objective, out var targetComp) && targetComp.Target == targetTraitorMind)
+            {
+                args.Cancelled = true;
+                return;
+            }
+        }
+
+        _target.SetTarget(uid, targetTraitorMind, target);
     }
 
     private float GetProgress(EntityUid target, bool requireDead)
