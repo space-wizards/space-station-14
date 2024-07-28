6ab2cdc43f4a030291d1341a8c2c737df12c02c1
TemporalOroboros <TemporalOroboros@gmail.com>
2024-07-28 02:33:09 -0700

    Network ContainerTemperatureDamageThresholdsComponent

--- Content.Shared/Temperature/Components/ContainerTemperatureDamageThresholdsComponent.cs
+++ Content.Shared/Temperature/Components/ContainerTemperatureDamageThresholdsComponent.cs
@@ -1,11 +1,15 @@
-﻿namespace Content.Shared.Temperature.Components;
+﻿using Robust.Shared.GameStates;
 
-[RegisterComponent]
+namespace Content.Shared.Temperature.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class ContainerTemperatureDamageThresholdsComponent : Component
 {
     [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [AutoNetworkedField]
     public float? HeatDamageThreshold;
 
     [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [AutoNetworkedField]
     public float? ColdDamageThreshold;
 }
