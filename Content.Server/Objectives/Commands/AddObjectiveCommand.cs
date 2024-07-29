7d342e4127d1b786322aefd72117a5aac4134e42
nikthechampiongr <nikvog4@gmail.com>
2024-07-29 19:53:24 +0300

    Add completions to addobjective and localise it

--- Content.Server/Objectives/Commands/AddObjectiveCommand.cs
+++ Content.Server/Objectives/Commands/AddObjectiveCommand.cs
@@ -1,56 +1,93 @@
-﻿using Content.Server.Administration;
+﻿using System.Linq;
+using Content.Server.Administration;
+using Content.Server.Antag;
+using Content.Server.Mind;
 using Content.Shared.Administration;
 using Content.Shared.Mind;
 using Content.Shared.Objectives.Components;
+using Content.Shared.Objectives.Systems;
+using Content.Shared.Prototypes;
 using Robust.Server.Player;
 using Robust.Shared.Console;
 using Robust.Shared.Prototypes;
 
-namespace Content.Server.Objectives.Commands
+namespace Content.Server.Objectives.Commands;
+
+[AdminCommand(AdminFlags.Admin)]
+public sealed class AddObjectiveCommand : LocalizedCommands
 {
-    [AdminCommand(AdminFlags.Admin)]
-    public sealed class AddObjectiveCommand : IConsoleCommand
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+    [Dependency] private readonly IPlayerManager _players = default!;
+    [Dependency] private readonly IPrototypeManager _prototypes = default!;
+    [Dependency] private readonly IComponentFactory _componentFactory = default!;
+
+    public override string Command => "addobjective";
+
+    private IEnumerable<string>? _objectives = null;
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IEntityManager _entityManager = default!;
-
-        public string Command => "addobjective";
-        public string Description => "Adds an objective to the player's mind.";
-        public string Help => "addobjective <username> <objectiveID>";
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        if (args.Length != 2)
         {
-            if (args.Length != 2)
-            {
-                shell.WriteLine("Expected exactly 2 arguments.");
-                return;
-            }
-
-            var mgr = IoCManager.Resolve<IPlayerManager>();
-            if (!mgr.TryGetSessionByUsername(args[0], out var data))
-            {
-                shell.WriteLine("Can't find the playerdata.");
-                return;
-            }
-
-            var minds = _entityManager.System<SharedMindSystem>();
-            if (!minds.TryGetMind(data, out var mindId, out var mind))
-            {
-                shell.WriteLine("Can't find the mind.");
-                return;
-            }
-
-            if (!IoCManager.Resolve<IPrototypeManager>()
-                .TryIndex<EntityPrototype>(args[1], out var proto) ||
-                !proto.TryGetComponent<ObjectiveComponent>(out _))
-            {
-                shell.WriteLine($"Can't find matching objective prototype {args[1]}");
-                return;
-            }
-
-            if (!minds.TryAddObjective(mindId, mind, args[1]))
-            {
-                // can fail for other reasons so dont pretend to be right
-                shell.WriteLine("Failed to add the objective. Maybe requirements dont allow that objective to be added.");
-            }
+            shell.WriteLine(Loc.GetString(Loc.GetString("cmd-addobjective-invalid-args")));
+            return;
         }
+
+        if (!_players.TryGetSessionByUsername(args[0], out var data))
+        {
+            shell.WriteLine(Loc.GetString("cmd-addobjective-player-not-found"));
+            return;
+        }
+
+        var minds = _entityManager.System<SharedMindSystem>();
+        if (!minds.TryGetMind(data, out var mindId, out var mind))
+        {
+            shell.WriteLine(Loc.GetString("cmd-addobjective-mind-not-found"));
+            return;
+        }
+
+        if (!_prototypes.TryIndex<EntityPrototype>(args[1], out var proto) ||
+            !proto.HasComponent<ObjectiveComponent>())
+        {
+            shell.WriteLine(Loc.GetString("cmd-addobjectives-objective-not-found", ("obj", args[1])));
+            return;
+        }
+
+        if (!minds.TryAddObjective(mindId, mind, args[1]))
+        {
+            // can fail for other reasons so dont pretend to be right
+            shell.WriteLine(Loc.GetString("cmd-addobjective-adding-failed"));
+        }
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        if (args.Length == 1)
+        {
+            var options = _players.Sessions.OrderBy(c => c.Name).Select(c => c.Name).ToArray();
+
+            return CompletionResult.FromHintOptions(options, "<Player>");
+        }
+
+        if (args.Length != 2)
+            return CompletionResult.Empty;
+
+        if (_objectives == null)
+        {
+            CreateCompletions();
+            _prototypes.PrototypesReloaded += _ => CreateCompletions();
+        }
+
+        return CompletionResult.FromHintOptions(
+            _objectives!,
+            "<Objective>");
+    }
+
+    private void CreateCompletions()
+    {
+        _objectives = _prototypes.EnumeratePrototypes<EntityPrototype>()
+            .Where(p => p.HasComponent<ObjectiveComponent>())
+            .Select(p => p.ID)
+            .Order();
     }
 }
