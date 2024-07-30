fde332b054ea36c6795fe426ce63e2683c36fcb1
Baa <git_78nfp05yca8p0nw7at849pbahfc56w39p@protonmail.com>
2024-07-30 12:48:37 +0100

    Adds sheep Ported from TG.

---
+++ Content.Shared/Wooly/ShearingDoAfterEvents.cs
@@ -0,0 +1,9 @@
+using Content.Shared.DoAfter;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Wooly;
+
+[Serializable, NetSerializable]
+public sealed partial class ShearingDoAfterEvent : SimpleDoAfterEvent
+{
+}
---
+++ Resources/Locale/en-US/animals/wooly/wooly-system.ftl
@@ -0,0 +1,6 @@
+### Wooly system
+
+wooly-system-success = You shear off some wool from {THE($target)}.
+wooly-system-no-wool = {THE($target)} hasn't grown enough wool to shear off.
+
+wooly-system-verb-shear = Shear
