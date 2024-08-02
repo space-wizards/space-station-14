cb7e4d33017aff82f8711ceae5a136ee05ba2ecc
EmoGarbage404 <retron404@gmail.com>
2024-08-02 17:48:26 -0400

    Small strange jackboots buff

---
+++ Content.Shared/Damage/Components/ClothingSlowOnDamageModifierComponent.cs
@@ -0,0 +1,16 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Damage.Components;
+
+/// <summary>
+/// This is used for a clothing item that modifies the slowdown from taking damage.
+/// </summary>
+[RegisterComponent, NetworkedComponent, Access(typeof(SlowOnDamageSystem))]
+public sealed partial class ClothingSlowOnDamageModifierComponent : Component
+{
+    /// <summary>
+    /// A coefficient modifier for the slowdown
+    /// </summary>
+    [DataField]
+    public float Modifier = 1;
+}
