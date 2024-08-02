8951a25c7b43f3bcbdc61877f2476f2a5322f0c6
Repo <47093363+Titian3@users.noreply.github.com>
2024-08-03 02:05:38 +1200

    Pretty sure the initial passes were false positives on this, no value keeping this on reflection.

--- Content.IntegrationTests/Tests/GameRules/AntagPreferenceTest.cs
+++ Content.IntegrationTests/Tests/GameRules/AntagPreferenceTest.cs
@@ -1,12 +1,9 @@
 #nullable enable
 using System.Collections.Generic;
 using System.Linq;
-using Content.Server.Administration.Managers;
 using Content.Server.Antag;
 using Content.Server.Antag.Components;
-using Content.Server.Database;
 using Content.Server.GameTicking;
-using Content.Shared.Database;
 using Content.Shared.GameTicking;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Player;
@@ -71,66 +68,6 @@ public sealed class AntagPreferenceTest
         Assert.That(sys.IsSessionValid(rule, pair.Player, def), Is.True);
         Assert.That(sys.IsEntityValid(client.AttachedEntity, def), Is.True);
         pool = sys.GetPlayerPool(rule, sessions, def);
-        Assert.That(pool.Count, Is.EqualTo(0));
-
-        await server.WaitPost(() => server.EntMan.DeleteEntity(uid));
-        await pair.CleanReturnAsync();
-    }
-
-    [Test]
-    public async Task TestPlayerBannedFromAntagRole()
-    {
-        await using var pair = await PoolManager.GetServerClient(new PoolSettings
-        {
-            DummyTicker = false,
-            Connected = true,
-            InLobby = true
-        });
-
-        var server = pair.Server;
-        var client = pair.Client;
-        var ticker = server.System<GameTicker>();
-        var sys = server.System<AntagSelectionSystem>();
-        var banManager = server.ResolveDependency<IBanManager>();
-
-        Assert.That(ticker.RunLevel, Is.EqualTo(GameRunLevel.PreRoundLobby));
-        Assert.That(client.AttachedEntity, Is.Null);
-        Assert.That(ticker.PlayerGameStatuses[client.User!.Value], Is.EqualTo(PlayerGameStatus.NotReadyToPlay));
-
-        EntityUid uid = default;
-        await server.WaitPost(() => uid = server.EntMan.Spawn("Traitor"));
-        var rule = new Entity<AntagSelectionComponent>(uid, server.EntMan.GetComponent<AntagSelectionComponent>(uid));
-        var def = rule.Comp.Definitions.Single();
-
-        // Ban the player from the "Traitor" role
-        await server.WaitPost(() =>
-        {
-            var banDef = new ServerRoleBanDef(
-                null,
-                client.User!.Value,
-                null,
-                null,
-                DateTimeOffset.UtcNow,
-                DateTimeOffset.UtcNow.AddMinutes(60),
-                null,
-                TimeSpan.Zero,
-                "Test ban",
-                NoteSeverity.Medium,
-                null,
-                null,
-                $"{BanManager.AntagPrefix}Traitor"
-            );
-            banManager.CreateRoleBan(client.User!.Value, "TestUser", null, null, null, "Traitor", 60, NoteSeverity.Medium, "Test ban", DateTimeOffset.UtcNow);
-        });
-
-        // Opt into the traitor role.
-        await pair.SetAntagPreference("Traitor", true);
-
-        // Check if the player is valid for the antag role
-        Assert.That(sys.IsSessionValid(rule, pair.Player, def), Is.False);
-
-        var sessions = new List<ICommonSession> { pair.Player! };
-        var pool = sys.GetPlayerPool(rule, sessions, def);
         Assert.That(pool.Count, Is.EqualTo(0));
 
         await server.WaitPost(() => server.EntMan.DeleteEntity(uid));
