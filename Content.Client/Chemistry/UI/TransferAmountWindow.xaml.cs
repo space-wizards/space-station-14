dceb562be7520950ef43ad64386727bc82f8e2ba
Brandon Hu <103440971+Brandon-Huu@users.noreply.github.com>
2024-08-02 01:20:36 +0000

    add(TransferAmountWindow): QoL stuff (#30464)
    * ugh

--- Content.Client/Chemistry/UI/TransferAmountWindow.xaml.cs
+++ Content.Client/Chemistry/UI/TransferAmountWindow.xaml.cs
@@ -8,9 +8,29 @@ namespace Content.Client.Chemistry.UI
     [GenerateTypedNameReferences]
     public sealed partial class TransferAmountWindow : DefaultWindow
     {
+        private int _max = Int32.MaxValue;
+        private int _min = 1;
+
         public TransferAmountWindow()
         {
             RobustXamlLoader.Load(this);
+            AmountLineEdit.OnTextChanged += OnValueChanged;
+        }
+
+        public void SetBounds(int min, int max)
+        {
+          _min = min;
+          _max = max;
+          MinimumAmount.Text = Loc.GetString("comp-solution-transfer-set-amount-min", ("amount", _min));
+          MaximumAmount.Text = Loc.GetString("comp-solution-transfer-set-amount-max", ("amount", _max));
+        }
+
+        private void OnValueChanged(LineEdit.LineEditEventArgs args)
+        {
+            if (!int.TryParse(AmountLineEdit.Text, out var amount)  || amount > _max || amount < _min)
+                ApplyButton.Disabled = true;
+            else 
+                ApplyButton.Disabled = false; 
         }
     }
 }
