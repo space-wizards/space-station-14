dc76c2d4075a190527a8cf572a0ecf137c9a8b90
Mervill <mervills.email@gmail.com>
2024-08-02 08:37:44 -0700

    pr feedback

--- Content.Server/Nutrition/Thirsty.cs
+++ Content.Server/Nutrition/Thirsty.cs
@@ -4,40 +4,35 @@ using Content.Shared.Nutrition.Components;
 using Content.Shared.Nutrition.EntitySystems;
 using Robust.Shared.Console;
 
-namespace Content.Server.Nutrition
+namespace Content.Server.Nutrition;
+
+[AdminCommand(AdminFlags.Debug)]
+public sealed class Thirsty : LocalizedEntityCommands
 {
-    [AdminCommand(AdminFlags.Debug)]
-    public sealed class Thirsty : IConsoleCommand
+    public override string Command => "thirsty";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IEntityManager _entities = default!;
-
-        public string Command => "thirsty";
-        public string Description => "Makes you thirsty.";
-        public string Help => $"{Command}";
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        var player = shell.Player;
+        if (player == null)
         {
-            var player = shell.Player;
-            if (player == null)
-            {
-                shell.WriteLine("You cannot use this command unless you are a player.");
-                return;
-            }
-
-            if (player.AttachedEntity is not {Valid: true} playerEntity)
-            {
-                shell.WriteLine("You cannot use this command without an entity.");
-                return;
-            }
-
-            if (!_entities.TryGetComponent(playerEntity, out ThirstComponent? thirst))
-            {
-                shell.WriteLine($"Your entity does not have a {nameof(ThirstComponent)} component.");
-                return;
-            }
-
-            var thirstyThreshold = thirst.ThirstThresholds[ThirstThreshold.Parched];
-            _entities.System<ThirstSystem>().SetThirst(playerEntity, thirst, thirstyThreshold);
+            shell.WriteError(Loc.GetString("cmd-nutrition-error-player"));
+            return;
         }
+
+        if (player.AttachedEntity is not {Valid: true} playerEntity)
+        {
+            shell.WriteError(Loc.GetString("cmd-nutrition-error-entity"));
+            return;
+        }
+
+        if (!EntityManager.TryGetComponent(playerEntity, out ThirstComponent? thirst))
+        {
+            shell.WriteError(Loc.GetString("cmd-nutrition-error-component", ("comp", nameof(ThirstComponent))));
+            return;
+        }
+
+        var thirstyThreshold = thirst.ThirstThresholds[ThirstThreshold.Parched];
+        EntityManager.System<ThirstSystem>().SetThirst(playerEntity, thirst, thirstyThreshold);
     }
 }
