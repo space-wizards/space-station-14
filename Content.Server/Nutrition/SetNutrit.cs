af5fe031929c6e9c9716f42dd4730e92889528a4
Mervill <mervills.email@gmail.com>
2024-08-02 08:49:54 -0700

    code updates

--- Content.Server/Nutrition/SetNutrit.cs
+++ Content.Server/Nutrition/SetNutrit.cs
@@ -6,106 +6,101 @@ using Content.Shared.Nutrition.EntitySystems;
 using Robust.Shared.Console;
 using System.Linq;
 
-namespace Content.Server.Nutrition
+namespace Content.Server.Nutrition;
+
+[AdminCommand(AdminFlags.Debug)]
+public sealed class SetNutrit : LocalizedEntityCommands
 {
-    [AdminCommand(AdminFlags.Debug)]
-    public sealed class SetNutrit : IConsoleCommand
+    public override string Command => "setnutrit";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IEntityManager _entities = default!;
-
-        public string Command => "setnutrit";
-        public string Description => "Modify your hunger or thirst";
-        public string Help => $"{Command}";
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        var player = shell.Player;
+        if (player == null)
         {
-            var player = shell.Player;
-            if (player == null)
-            {
-                shell.WriteLine("You cannot use this command unless you are a player.");
-                return;
-            }
-
-            if (player.AttachedEntity is not { Valid: true } playerEntity)
-            {
-                shell.WriteLine("You cannot use this command without an entity.");
-                return;
-            }
-
-            if (args.Length != 2)
-            {
-                shell.WriteLine($"Wrong number of arguments! Expected 2 got {args.Length}");
-                return;
-            }
-
-            var systemString = args[0];
-            switch (systemString)
-            {
-                case "hunger":
-                {
-                    if (!_entities.TryGetComponent(playerEntity, out HungerComponent? hunger))
-                    {
-                        shell.WriteLine($"Your entity does not have a {nameof(HungerComponent)} component.");
-                        return;
-                    }
-
-                    if (!Enum.TryParse(args[1], out HungerThreshold hungerThreshold))
-                    {
-                        shell.WriteError($"Invalid {nameof(HungerThreshold)} `{args[1]}`");
-                        return;
-                    }
-
-                    var hungerValue = hunger.Thresholds[hungerThreshold];
-                    _entities.System<HungerSystem>().SetHunger(playerEntity, hungerValue, hunger);
-                    return;
-                }
-                case "thirst":
-                {
-                    if (!_entities.TryGetComponent(playerEntity, out ThirstComponent? thirst))
-                    {
-                        shell.WriteLine($"Your entity does not have a {nameof(ThirstComponent)} component.");
-                        return;
-                    }
-
-                    if (!Enum.TryParse(args[1], out ThirstThreshold thirstThreshold))
-                    {
-                        shell.WriteError($"Invalid {nameof(ThirstThreshold)} `{args[1]}`");
-                        return;
-                    }
-
-                    var thirstValue = thirst.ThirstThresholds[thirstThreshold];
-                    _entities.System<ThirstSystem>().SetThirst(playerEntity, thirst, thirstValue);
-                    return;
-                }
-                default:
-                {
-                    shell.WriteError($"invalid nutrition system ${systemString}");
-                    return;
-                }
-            }
+            shell.WriteError(Loc.GetString("cmd-nutrition-error-player"));
+            return;
         }
 
-        public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+        if (player.AttachedEntity is not { Valid: true } playerEntity)
         {
-            switch (args.Length)
+            shell.WriteError(Loc.GetString("cmd-nutrition-error-entity"));
+            return;
+        }
+
+        if (args.Length != 2)
+        {
+            shell.WriteError($"Wrong number of arguments! Expected 2 got {args.Length}");
+            return;
+        }
+
+        var systemString = args[0];
+        switch (systemString)
+        {
+            case "hunger":
             {
-                case 1:
+                if (!EntityManager.TryGetComponent(playerEntity, out HungerComponent? hunger))
                 {
-                    string[] kinds = { "hunger", "thirst" };
-                    return CompletionResult.FromHintOptions(kinds, "nutrition system");
+                    shell.WriteError(Loc.GetString("cmd-nutrition-error-component", ("comp", nameof(HungerComponent))));
+                    return;
                 }
-                case 2:
+
+                if (!Enum.TryParse(args[1], out HungerThreshold hungerThreshold))
                 {
-                    return args[0] switch
-                    {
-                        "hunger" => CompletionResult.FromHintOptions(Enum.GetNames<HungerThreshold>(), nameof(HungerThreshold)),
-                        "thirst" => CompletionResult.FromHintOptions(Enum.GetNames<ThirstThreshold>(), nameof(ThirstThreshold)),
-                        _ => CompletionResult.Empty,
-                    };
+                    shell.WriteError($"Invalid {nameof(HungerThreshold)} `{args[1]}`");
+                    return;
                 }
-                default:
-                    return CompletionResult.Empty;
+
+                var hungerValue = hunger.Thresholds[hungerThreshold];
+                EntityManager.System<HungerSystem>().SetHunger(playerEntity, hungerValue, hunger);
+                return;
             }
+            case "thirst":
+            {
+                if (!EntityManager.TryGetComponent(playerEntity, out ThirstComponent? thirst))
+                {
+                    shell.WriteError(Loc.GetString("cmd-nutrition-error-component", ("comp", nameof(ThirstComponent))));
+                    return;
+                }
+
+                if (!Enum.TryParse(args[1], out ThirstThreshold thirstThreshold))
+                {
+                    shell.WriteError($"Invalid {nameof(ThirstThreshold)} `{args[1]}`");
+                    return;
+                }
+
+                var thirstValue = thirst.ThirstThresholds[thirstThreshold];
+                EntityManager.System<ThirstSystem>().SetThirst(playerEntity, thirst, thirstValue);
+                return;
+            }
+            default:
+            {
+                shell.WriteError($"invalid nutrition system ${systemString}");
+                return;
+            }
+        }
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        switch (args.Length)
+        {
+            case 1:
+            {
+                string[] kinds = { "hunger", "thirst" };
+                return CompletionResult.FromHintOptions(kinds, "nutrition system");
+            }
+            case 2:
+            {
+                return args[0] switch
+                {
+                    "hunger" => CompletionResult.FromHintOptions(Enum.GetNames<HungerThreshold>(), nameof(HungerThreshold)),
+                    "thirst" => CompletionResult.FromHintOptions(Enum.GetNames<ThirstThreshold>(), nameof(ThirstThreshold)),
+                    _ => CompletionResult.Empty,
+                };
+            }
+            default:
+                return CompletionResult.Empty;
         }
     }
 }
