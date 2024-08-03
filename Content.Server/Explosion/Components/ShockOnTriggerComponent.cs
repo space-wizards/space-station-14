948f9582bf1e0d419b6f681a5a611736cafdab77
chavonadelal <chavonadelal@mail.ru>
2024-08-03 04:50:56 +0300

    Cleaning the ShockCollar

--- Content.Server/Explosion/Components/ShockOnTriggerComponent.cs
+++ Content.Server/Explosion/Components/ShockOnTriggerComponent.cs
@@ -1,38 +1,39 @@
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Content.Shared.Clothing.EntitySystems;
+using Robust.Shared.GameStates;
+using Content.Server.Explosion.EntitySystems;
 
 namespace Content.Server.Explosion.Components;
 
 /// <summary>
 /// A component that electrocutes an entity having this component when a trigger is triggered.
 /// </summary>
+[NetworkedComponent]
 [RegisterComponent, AutoGenerateComponentPause]
+[Access(typeof(SelfUnremovableClothingSystem), typeof(TriggerSystem))]
 public sealed partial class ShockOnTriggerComponent : Component
 {
     /// <summary>
     /// The force of an electric shock when the trigger is triggered.
     /// </summary>
-    [ViewVariables]
     [DataField]
     public int Damage = 5;
 
     /// <summary>
     /// Duration of electric shock when the trigger is triggered.
     /// </summary>
-    [ViewVariables]
     [DataField]
     public TimeSpan Duration = TimeSpan.FromSeconds(2);
 
     /// <summary>
     /// The minimum delay between repeating triggers.
     /// </summary>
-    [ViewVariables]
     [DataField]
     public TimeSpan Cooldown = TimeSpan.FromSeconds(4);
 
     /// <summary>
     /// When can the trigger run again?
     /// </summary>
-    [ViewVariables]
     [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
     [AutoPausedField]
     public TimeSpan NextTrigger = TimeSpan.Zero;
