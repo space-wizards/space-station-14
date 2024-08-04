2a3d2b5db5bbc8e504c26dc68b23953dd2fa2b2a
Ed <edwardxperia2000@gmail.com>
2024-08-04 23:55:11 +0300

    stringbuilder is cool!

--- Content.Shared/Roles/JobRequirement/SpeciesRequirement.cs
+++ Content.Shared/Roles/JobRequirement/SpeciesRequirement.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using Content.Shared.Humanoid.Prototypes;
 using Content.Shared.Preferences;
 using JetBrains.Annotations;
@@ -29,23 +30,25 @@ public sealed partial class SpeciesRequirement : JobRequirement
         if (profile is null) //the profile could be null if the player is a ghost. In this case we don't need to block the role selection for ghostrole
             return true;
 
-        var speciesList = "[color=yellow]";
+        var sb = new StringBuilder();
+        sb.Append("[color=yellow]");
         foreach (var s in Species)
         {
-            speciesList += Loc.GetString(protoManager.Index(s).Name) + " ";
+            sb.Append(Loc.GetString(protoManager.Index(s).Name) + " ");
         }
-        speciesList += "[/color]";
+
+        sb.Append("[/color]");
 
         if (!Inverted)
         {
-            reason = FormattedMessage.FromMarkupPermissive($"{Loc.GetString("role-timer-whitelisted-species")}\n{speciesList}");
+            reason = FormattedMessage.FromMarkupPermissive($"{Loc.GetString("role-timer-whitelisted-species")}\n{sb}");
 
             if (!Species.Contains(profile.Species))
                 return false;
         }
         else
         {
-            reason = FormattedMessage.FromMarkupPermissive($"{Loc.GetString("role-timer-blacklisted-species")}\n{speciesList}");
+            reason = FormattedMessage.FromMarkupPermissive($"{Loc.GetString("role-timer-blacklisted-species")}\n{sb}");
 
             if (Species.Contains(profile.Species))
                 return false;
