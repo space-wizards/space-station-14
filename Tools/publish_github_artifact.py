e0be48a759227564a463546515e0e5b4b7af8df8
Woody <2997336+MWGMorningwood@users.noreply.github.com>
2024-08-04 19:32:04 -0400

    Update publish_github_artifact.py

--- Tools/publish_github_artifact.py
+++ Tools/publish_github_artifact.py
@@ -14,8 +14,8 @@ VERSION = os.environ['GITHUB_SHA']
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://wizards.cdn.spacestation14.com/"
-FORK_ID = "wizards"
+ROBUST_CDN_URL = "https://ss14cdn.azurewebsites.net/"
+FORK_ID = "MWGSolaris14"
 
 def main():
     print("Fetching artifact URL from API...")
