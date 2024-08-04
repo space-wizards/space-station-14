30414db46452a852a95d158c63e213ca9742ae6c
Baa <git_78nfp05yca8p0nw7at849pbahfc56w39p@protonmail.com>
2024-08-05 00:00:53 +0100

    Disable wooly layer when the mob dies.

--- Content.Server/Animals/Systems/WoolySystem.cs
+++ Content.Server/Animals/Systems/WoolySystem.cs
@@ -7,6 +7,9 @@ using Content.Shared.Nutrition.EntitySystems;
 using Robust.Shared.Timing;
 using Content.Shared.Toggleable;
 using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.Mobs;
+using Content.Shared.Chemistry.Components;
+using Content.Shared.Mobs.Components;
 
 
 namespace Content.Server.Animals.Systems;
@@ -21,6 +24,7 @@ public sealed class WoolySystem : EntitySystem
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly MobStateSystem _mobState = default!;
     [Dependency] private readonly SolutionContainerSystem _solutionContainer = default!;
+    [Dependency] private readonly SharedSolutionContainerSystem _sharedSolutionContainer = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
 
     public override void Initialize()
@@ -29,6 +33,7 @@ public sealed class WoolySystem : EntitySystem
 
         SubscribeLocalEvent<WoolyComponent, BeforeFullyEatenEvent>(OnBeforeFullyEaten);
         SubscribeLocalEvent<WoolyComponent, SolutionContainerChangedEvent>(OnSolutionChange);
+        SubscribeLocalEvent<WoolyComponent, MobStateChangedEvent>(OnMobStateChanged);
     }
 
     public override void Update(float frameTime)
@@ -72,20 +77,60 @@ public sealed class WoolySystem : EntitySystem
 
 
     /// <summary>
-    ///     Handles enabling and disabling the wooly layer (if one exists).
+    ///     Used for managing the wooly layer as the wool solution levels change..
     ///     e.g. in Sheep, it will remove the wool layer when the remaining reagent drops to 0.
-    ///     the layer is re-added when the reagent is high enough that it can be harvested.
+    ///     the layer is re-added when the reagent is above 0.
     ///     Check the sheep's Sprite and GenericVisualizer components for an example of how to add a wooly layer to your animal.
     /// </summary>
     private void OnSolutionChange(Entity<WoolyComponent> ent, ref SolutionContainerChangedEvent args)
     {
         // Only interested in wool solution, ignore the rest.
-        if (args.SolutionId != "wool")
+        if (args.SolutionId != ent.Comp.SolutionName)
             return;
 
-        TryComp<AppearanceComponent>(ent.Owner, out var appearance);
+        UpdateWoolLayer(ent, args.Solution);
+    }
 
-        if (args.Solution.Volume.Value <= 0)
+    /// <summary>
+    ///     This function checks the entity's wool solution and either disables or enables the wool layer (if one exists).
+    /// </summary>
+    /// <param name="ent">the entity containing a wooly component that will be checked.</param>
+    /// <param name="sol">a resolved solution object the prescence of which will be checked.
+    private void UpdateWoolLayer(Entity<WoolyComponent> ent, Solution? sol = null)
+    {
+        // If the sol parameter hasn't been provided, we'll try to grab the solution from inside the animal instead.
+        Solution? solution;
+        if (sol == null)
+        {
+            _sharedSolutionContainer.ResolveSolution(
+                ent.Owner,
+                ent.Comp.SolutionName,
+                ref ent.Comp.Solution,
+                out solution
+            );
+
+            // Somehow, this entity has no wool solution.
+            if (solution == null)
+            {
+                return;
+            }
+        }
+        else
+        {
+            solution = sol;
+        }
+
+        // appearance is used to disable and enable the wool layer.
+        TryComp<AppearanceComponent>(ent.Owner, out var appearance);
+        // mState is used to check if the animal is dead/critical.
+        TryComp<MobStateComponent>(ent.Owner, out var mState);
+
+        // If we couldn't resolve the mobState for some reason then just assume it's alive.
+        mState ??= new MobStateComponent();
+
+        // If there's no solution at all, or the entity is dead or critical, remove the wool layer.
+        // Otherwise, enable it.
+        if (solution.Volume.Value <= 0 || mState.CurrentState == MobState.Dead || mState.CurrentState == MobState.Critical)
         {
             // Remove wool layer
             _appearance.SetData(ent.Owner, ToggleVisuals.Toggled, false, appearance);
@@ -95,7 +140,22 @@ public sealed class WoolySystem : EntitySystem
             // Add wool layer
             _appearance.SetData(ent.Owner, ToggleVisuals.Toggled, true, appearance);
         }
+    }
 
+    /// <summary>
+    ///     This is used for checking if the wooly animal is dead or critical.
+    ///     If it is, then the wooly layer is removed.
+    private void OnMobStateChanged(Entity<WoolyComponent> ent, ref MobStateChangedEvent args)
+    {
+        if (args.NewMobState == MobState.Dead || args.NewMobState == MobState.Critical)
+        {
+            TryComp<AppearanceComponent>(ent.Owner, out var appearance);
+            _appearance.SetData(ent.Owner, ToggleVisuals.Toggled, false, appearance);
+        }
+        else
+        {
+            UpdateWoolLayer(ent);
+        }
     }
 
 }
