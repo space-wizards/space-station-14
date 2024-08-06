9d9c3a9862d722e8cb44f662c52ff92189b1aba7
themias <themias5@gmail.com>
2024-08-06 11:07:12 -0400

    Revert "Fix disposal unit flush animation"
    This reverts commit a596ecfd5f5368f87ef52cfb27884a8e000185c6.

--- Content.Client/Disposal/Systems/DisposalUnitSystem.cs
+++ Content.Client/Disposal/Systems/DisposalUnitSystem.cs
@@ -111,7 +111,7 @@ public sealed class DisposalUnitSystem : SharedDisposalUnitSystem
         {
             if (!_animationSystem.HasRunningAnimation(uid, AnimationKey))
             {
-                var flushState = sprite.LayerMapTryGet(DisposalUnitVisualLayers.BaseFlush, out var flushLayer)
+                var flushState = sprite.LayerMapTryGet(DisposalUnitVisualLayers.OverlayFlush, out var flushLayer)
                     ? sprite.LayerGetState(flushLayer)
                     : new RSI.StateId(DefaultFlushState);
 
@@ -179,7 +179,6 @@ public enum DisposalUnitVisualLayers : byte
     Unanchored,
     Base,
     BaseCharging,
-    BaseFlush,
     OverlayFlush,
     OverlayCharging,
     OverlayReady,
