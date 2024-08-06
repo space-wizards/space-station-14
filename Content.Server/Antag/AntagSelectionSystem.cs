a9bac98b519a635ee8e64d41667e498cf8e77f54
NakataRin <krllcyganov@gmail.com>
2024-08-06 21:55:17 +0400

    AntagSelectionSystemFix

--- Content.Server/Antag/AntagSelectionSystem.cs
+++ Content.Server/Antag/AntagSelectionSystem.cs
@@ -18,6 +18,7 @@ using Content.Shared.GameTicking;
 using Content.Shared.GameTicking.Components;
 using Content.Shared.Ghost;
 using Content.Shared.Humanoid;
+using Content.Shared.Mind;
 using Content.Shared.Players;
 using Content.Shared.Preferences.Loadouts;
 using Content.Shared.Roles;
@@ -231,7 +232,7 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
 
         for (var i = 0; i < count; i++)
         {
-            var session = (ICommonSession?) null;
+            var session = (ICommonSession?)null;
             if (picking)
             {
                 if (!playerPool.TryPickAndTake(RobustRandom, out session) && noSpawner)
@@ -341,18 +342,29 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
 
         if (session != null)
         {
-            var curMind = _mind.CreateMind(session.UserId, Name(antagEnt.Value));
-            _mind.SetUserId(curMind, session.UserId);
+            var curMind = session.GetMind();
+            if (curMind != null && TryComp<MindComponent>(curMind.Value, out var mindComp))
+            {
+                // Check if the mind is associated with a different entity
+                if (mindComp.OwnedEntity != antagEnt)
+                {
+                    // Create a new mind because the current one is tied to a different entity
+                    curMind = _mind.CreateMind(session.UserId, Name(antagEnt.Value));
+                    _mind.SetUserId(curMind.Value, session.UserId);
+                }
+            }
+            else
+            {
+                // Create a new mind because there was no existing mind
+                curMind = _mind.CreateMind(session.UserId, Name(antagEnt.Value));
+                _mind.SetUserId(curMind.Value, session.UserId);
+            }
 
-            _mind.TransferTo(curMind, antagEnt, ghostCheckOverride: true);
-            _role.MindAddRoles(curMind, def.MindComponents, null, true);
-            ent.Comp.SelectedMinds.Add((curMind, Name(player)));
-
+            _mind.TransferTo(curMind.Value, antagEnt, ghostCheckOverride: true);
+            _role.MindAddRoles(curMind.Value, def.MindComponents, null, true);
+            ent.Comp.SelectedMinds.Add((curMind.Value, Name(player)));
             SendBriefing(session, def.Briefing);
         }
-
-        var afterEv = new AfterAntagEntitySelectedEvent(session, player, ent, def);
-        RaiseLocalEvent(ent, ref afterEv, true);
     }
 
     /// <summary>
@@ -408,17 +420,17 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
         switch (def.MultiAntagSetting)
         {
             case AntagAcceptability.None:
-            {
-                if (_role.MindIsAntagonist(mind))
-                    return false;
-                break;
-            }
+                {
+                    if (_role.MindIsAntagonist(mind))
+                        return false;
+                    break;
+                }
             case AntagAcceptability.NotExclusive:
-            {
-                if (_role.MindIsExclusiveAntagonist(mind))
-                    return false;
-                break;
-            }
+                {
+                    if (_role.MindIsExclusiveAntagonist(mind))
+                        return false;
+                    break;
+                }
         }
 
         // todo: expand this to allow for more fine antag-selection logic for game rules.
@@ -460,7 +472,7 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
 
     private void OnObjectivesTextGetInfo(Entity<AntagSelectionComponent> ent, ref ObjectivesTextGetInfoEvent args)
     {
-        if (ent.Comp.AgentName is not {} name)
+        if (ent.Comp.AgentName is not { } name)
             return;
 
         args.Minds = ent.Comp.SelectedMinds;
