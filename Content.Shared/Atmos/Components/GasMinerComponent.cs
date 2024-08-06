4169c130da4e5b952825ae1912ebcbfaf73d4592
Mervill <mervills.email@gmail.com>
2024-08-05 20:16:38 -0700

    no need for these to be properties

--- Content.Shared/Atmos/Components/GasMinerComponent.cs
+++ Content.Shared/Atmos/Components/GasMinerComponent.cs
@@ -10,38 +10,38 @@ public sealed partial class GasMinerComponent : Component
 {
     [AutoNetworkedField]
     [ViewVariables(VVAccess.ReadOnly)]
-    public bool Enabled { get; set; } = true;
+    public bool Enabled = true;
 
     [AutoNetworkedField]
     [ViewVariables(VVAccess.ReadOnly)]
-    public bool Idle { get; set; } = false;
+    public bool Idle = false;
 
     /// <summary>
     ///      If the number of moles in the external environment exceeds this number, no gas will be mined.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("maxExternalAmount")]
-    public float MaxExternalAmount { get; set; } = float.PositiveInfinity;
+    [DataField]
+    public float MaxExternalAmount = float.PositiveInfinity;
 
     /// <summary>
     ///      If the pressure (in kPA) of the external environment exceeds this number, no gas will be mined.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("maxExternalPressure")]
-    public float MaxExternalPressure { get; set; } = Atmospherics.GasMinerDefaultMaxExternalPressure;
+    [DataField]
+    public float MaxExternalPressure = Atmospherics.GasMinerDefaultMaxExternalPressure;
 
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("spawnGas")]
-    public Gas? SpawnGas { get; set; } = null;
+    [DataField]
+    public Gas? SpawnGas = null;
 
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("spawnTemperature")]
-    public float SpawnTemperature { get; set; } = Atmospherics.T20C;
+    [DataField]
+    public float SpawnTemperature = Atmospherics.T20C;
 
     /// <summary>
     ///     Number of moles created per second when the miner is working.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("spawnAmount")]
-    public float SpawnAmount { get; set; } = Atmospherics.MolesCellStandard * 20f;
+    [DataField]
+    public float SpawnAmount = Atmospherics.MolesCellStandard * 20f;
 }
