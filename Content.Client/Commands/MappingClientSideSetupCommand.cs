9c903b209c269f85bfa828e1233f978f9fe0310e
deltanedas <@deltanedas:kde.org>
2024-08-08 20:19:08 +0100

    restore old mappingclientsidesetup command

---
+++ Content.Client/Commands/MappingClientSideSetupCommand.cs
@@ -0,0 +1,28 @@
+using Content.Client.Mapping;
+using Content.Client.Markers;
+using JetBrains.Annotations;
+using Robust.Client.Graphics;
+using Robust.Client.State;
+using Robust.Shared.Console;
+
+namespace Content.Client.Commands;
+
+[UsedImplicitly]
+    internal sealed class MappingClientSideSetupCommand : LocalizedCommands
+{
+    [Dependency] private readonly IEntitySystemManager _entitySystemManager = default!;
+    [Dependency] private readonly ILightManager _lightManager = default!;
+    [Dependency] private readonly IStateManager _stateManager = default!;
+    public override string Command => "mappingclientsidesetup";
+    public override string Help => LocalizationManager.GetString($"cmd-{Command}-help", ("command", Command));
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (!_lightManager.LockConsoleAccess)
+        {
+            _entitySystemManager.GetEntitySystem<MarkerSystem>().MarkersVisible = true;
+            _lightManager.Enabled = false;
+            shell.ExecuteCommand("showsubfloorforever");
+            _stateManager.RequestStateChange<MappingState>();
+        }
+    }
+}
