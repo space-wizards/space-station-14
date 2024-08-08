0c92a0ecf0fc3859db91a3f7c189bf87c9ebe234
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-08-08 12:40:59 +0300

    Update FoodComponent.cs

--- Content.Server/Nutrition/Components/FoodComponent.cs
+++ Content.Server/Nutrition/Components/FoodComponent.cs
@@ -17,7 +17,7 @@ public sealed partial class FoodComponent : Component
     public SoundSpecifier UseSound = new SoundCollectionSpecifier("eating");
 
     [DataField]
-    public List<EntProtoId> Trash = new();
+    public HashSet<EntProtoId> Trash = new();
 
     [DataField]
     public FixedPoint2? TransferAmount = FixedPoint2.New(5);
