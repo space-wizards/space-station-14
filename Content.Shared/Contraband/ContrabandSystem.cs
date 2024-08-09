5fb399636352e7d3f09c08a4206f155f8bd13ae5
EmoGarbage404 <retron404@gmail.com>
2024-08-08 23:10:58 -0400

    add cvar for contraband examine

--- Content.Shared/Contraband/ContrabandSystem.cs
+++ Content.Shared/Contraband/ContrabandSystem.cs
@@ -1,8 +1,10 @@
 ﻿using System.Linq;
 using Content.Shared.Access.Systems;
+using Content.Shared.CCVar;
 using Content.Shared.Examine;
 using Content.Shared.Localizations;
 using Content.Shared.Roles;
+using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Contraband;
@@ -12,17 +14,30 @@ namespace Content.Shared.Contraband;
 /// </summary>
 public sealed class ContrabandSystem : EntitySystem
 {
+    [Dependency] private readonly IConfigurationManager _configuration = default!;
     [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly SharedIdCardSystem _id = default!;
+
+    private bool _contrabandExamineEnabled;
 
     /// <inheritdoc/>
     public override void Initialize()
     {
         SubscribeLocalEvent<ContrabandComponent, ExaminedEvent>(OnExamined);
+
+        Subs.CVar(_configuration, CCVars.ContrabandExamine, SetContrabandExamine, true);
+    }
+
+    private void SetContrabandExamine(bool val)
+    {
+        _contrabandExamineEnabled = val;
     }
 
     private void OnExamined(Entity<ContrabandComponent> ent, ref ExaminedEvent args)
     {
+        if (!_contrabandExamineEnabled)
+            return;
+
         // two strings:
         // one, the actual informative 'this is restricted'
         // then, the 'you can/shouldn't carry this around' based on the ID the user is wearing
