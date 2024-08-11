c6cd91440616bbc087f59b67bbe109879ab2334a
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-08-12 03:09:32 +1000

    Added raw biopsy tool

---
+++ Content.Shared/Xenobiology/Components/CellCollectorComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Xenobiology.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CellCollectorComponent : Component
+{
+
+}
