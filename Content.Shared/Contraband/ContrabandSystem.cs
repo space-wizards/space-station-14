3028eb284caa14261599b4fbf4ce5489e3e45d87
Killerqu00 <killerqueen1777@gmail.com>
2024-08-12 12:26:56 +0200

    separate contraband examine into a different tab

--- Content.Shared/Contraband/ContrabandSystem.cs
+++ Content.Shared/Contraband/ContrabandSystem.cs
@@ -4,8 +4,10 @@ using Content.Shared.CCVar;
 using Content.Shared.Examine;
 using Content.Shared.Localizations;
 using Content.Shared.Roles;
+using Content.Shared.Verbs;
 using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
 
 namespace Content.Shared.Contraband;
 
@@ -17,15 +19,69 @@ public sealed class ContrabandSystem : EntitySystem
     [Dependency] private readonly IConfigurationManager _configuration = default!;
     [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly SharedIdCardSystem _id = default!;
+    [Dependency] private readonly ExamineSystemShared _examine = default!;
 
     private bool _contrabandExamineEnabled;
 
     /// <inheritdoc/>
     public override void Initialize()
     {
-        SubscribeLocalEvent<ContrabandComponent, ExaminedEvent>(OnExamined);
+        // SubscribeLocalEvent<ContrabandComponent, ExaminedEvent>(OnExamined);
+        SubscribeLocalEvent<ContrabandComponent, GetVerbsEvent<ExamineVerb>>(OnDetailedExamine);
 
         Subs.CVar(_configuration, CCVars.ContrabandExamine, SetContrabandExamine, true);
+    }
+
+    private void OnDetailedExamine(EntityUid ent,ContrabandComponent component, ref GetVerbsEvent<ExamineVerb> args)
+    {
+        if (!args.CanInteract || !args.CanAccess)
+            return;
+
+        var severity = _proto.Index(component.Severity);
+        var departmentExamineMessage = "";
+        if (severity.ShowDepartments && component is { AllowedDepartments: not null })
+        {
+            var list = ContentLocalizationManager.FormatList(component.AllowedDepartments.Select(p => Loc.GetString($"department-{p.Id}")).ToList());
+            departmentExamineMessage = Loc.GetString("contraband-examine-text-Restricted-department", ("departments", list));
+        }
+        else
+        {
+            departmentExamineMessage = Loc.GetString(severity.ExamineText);
+        }
+
+        // text based on ID card
+        List<ProtoId<DepartmentPrototype>>? departments = null;
+        if (_id.TryFindIdCard(args.User, out var id))
+        {
+            departments = id.Comp.JobDepartments;
+        }
+
+        var carryingMessage = "";
+        // either its fully restricted, you have no departments, or your departments dont intersect with the restricted departments
+        if (component.AllowedDepartments is null
+            || departments is null
+            || !departments.Intersect(component.AllowedDepartments).Any())
+        {
+            carryingMessage = Loc.GetString("contraband-examine-text-avoid-carrying-around");
+            return;
+        }
+
+        // otherwise fine to use :tm:
+        carryingMessage = Loc.GetString("contraband-examine-text-in-the-clear");
+
+        var examineMarkup = GetContrabandExamine(departmentExamineMessage, carryingMessage);
+        _examine.AddDetailedExamineVerb(args, component, examineMarkup,
+            Loc.GetString("armor-examinable-verb-text"), "/Textures/Interface/VerbIcons/dot.svg.192dpi.png",
+            Loc.GetString("armor-examinable-verb-message"));
+    }
+
+    private FormattedMessage GetContrabandExamine(String deptMessage, String carryMessage)
+    {
+        var msg = new FormattedMessage();
+        msg.AddMarkupOrThrow(deptMessage);
+        msg.PushNewline();
+        msg.AddMarkupOrThrow(carryMessage);
+        return msg;
     }
 
     private void SetContrabandExamine(bool val)
@@ -78,4 +134,5 @@ public sealed class ContrabandSystem : EntitySystem
             args.PushMarkup(Loc.GetString("contraband-examine-text-in-the-clear"));
         }
     }
+
 }
