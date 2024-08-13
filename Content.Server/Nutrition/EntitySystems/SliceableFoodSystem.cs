8002ab416dccf296e8e4ffcda0e734b5e391bbc1
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-08-13 13:01:43 +0300

    wtf git

--- Content.Server/Nutrition/EntitySystems/SliceableFoodSystem.cs
+++ Content.Server/Nutrition/EntitySystems/SliceableFoodSystem.cs
@@ -127,41 +127,15 @@ public sealed class SliceableFoodSystem : EntitySystem
         if (ev.Cancelled)
             return;
 
-        if (string.IsNullOrEmpty(foodComp.Trash))
-        {
-            var ev = new BeforeFullySlicedEvent
-            {
-                User = user
-            };
-            RaiseLocalEvent(uid, ev);
-            if (ev.Cancelled)
-                return;
-
-            if (foodComp.Trash.Count == 0)
-            {
-                QueueDel(uid);
-                return;
-            }
-
-            // Locate the sliced food and spawn its trash
-            foreach (var trash in foodComp.Trash)
-            {
-                var trashUid = Spawn(trash, _xformSystem.GetMapCoordinates(uid));
-
-                // try putting the trash in the food's container too, to be consistent with slice spawning?
-                _xformSystem.DropNextTo(trashUid, uid);
-                _xformSystem.SetLocalRotation(trashUid, 0);
-            }
-            QueueDel(uid);
-            return;
-        }
-
         // Locate the sliced food and spawn its trash
-        var trashUid = Spawn(foodComp.Trash, _transform.GetMapCoordinates(uid));
+        foreach (var trash in foodComp.Trash)
+        {
+            var trashUid = Spawn(trash, _transform.GetMapCoordinates(uid));
 
-        // try putting the trash in the food's container too, to be consistent with slice spawning?
-        _transform.DropNextTo(trashUid, uid);
-        _transform.SetLocalRotation(trashUid, 0);
+            // try putting the trash in the food's container too, to be consistent with slice spawning?
+            _transform.DropNextTo(trashUid, uid);
+            _transform.SetLocalRotation(trashUid, 0);
+        }
 
         QueueDel(uid);
     }
