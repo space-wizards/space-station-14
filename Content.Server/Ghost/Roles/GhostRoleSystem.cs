d6740735292f7b685bd532e8125353ce1efcb18d
Errant <35878406+Errant-4@users.noreply.github.com>
2024-08-13 23:00:31 +0200

    debug ghostrole lottery changes

--- Content.Server/Ghost/Roles/GhostRoleSystem.cs
+++ Content.Server/Ghost/Roles/GhostRoleSystem.cs
@@ -7,6 +7,7 @@ using Content.Shared.Ghost.Roles.Raffles;
 using Content.Server.Ghost.Roles.UI;
 using Content.Server.Mind.Commands;
 using Content.Shared.Administration;
+using Content.Shared.CCVar;
 using Content.Shared.Database;
 using Content.Shared.Follower;
 using Content.Shared.GameTicking;
@@ -20,6 +21,7 @@ using Content.Shared.Roles;
 using JetBrains.Annotations;
 using Robust.Server.GameObjects;
 using Robust.Server.Player;
+using Robust.Shared.Configuration;
 using Robust.Shared.Console;
 using Robust.Shared.Enums;
 using Robust.Shared.Player;
@@ -37,6 +39,7 @@ namespace Content.Server.Ghost.Roles
     [UsedImplicitly]
     public sealed class GhostRoleSystem : EntitySystem
     {
+        [Dependency] private readonly IConfigurationManager _cfg = default!;
         [Dependency] private readonly EuiManager _euiManager = default!;
         [Dependency] private readonly IPlayerManager _playerManager = default!;
         [Dependency] private readonly IAdminLogManager _adminLogger = default!;
@@ -356,7 +359,8 @@ namespace Content.Server.Ghost.Roles
 
             var raffle = ent.Comp;
             raffle.Identifier = ghostRole.Identifier;
-            raffle.Countdown = TimeSpan.FromSeconds(settings.InitialDuration);
+            var countdown = _cfg.GetCVar(CCVars.GhostQuickLottery)? 1 : settings.InitialDuration;
+            raffle.Countdown = TimeSpan.FromSeconds(countdown);
             raffle.CumulativeTime = TimeSpan.FromSeconds(settings.InitialDuration);
             // we copy these settings into the component because they would be cumbersome to access otherwise
             raffle.JoinExtendsDurationBy = TimeSpan.FromSeconds(settings.JoinExtendsDurationBy);
