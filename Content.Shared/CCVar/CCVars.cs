07ec00ed05394f0d6c8929d8d015854b8dbbc3bb
IProduceWidgets <107586145+IProduceWidgets@users.noreply.github.com>
2024-08-14 01:21:01 -0400

    refactor event schedulers to use explicit game rules (#29320)
    * works, still has testing values, im sure I did stupid shit.
    
    * shitvent crapfactor
    
    * snap extra word out of existence
    
    * shit I died of old
    
    * remove useless inaccurate design comments
    
    * Oopsie, handle requirement params in RandomRuleSystem too
    
    * I'm a slash slinging hasher
    
    * Address reviews, add admin alerts I forgor
    
    * EntityMan saves the day
    
    * address reviews 1
    
    * eh, I actually don't care about the cargo gifts thing.
    
    * started
    
    * Do reviews
    
    * you actually meant 1.2 lmao
    
    * dependency inheritance is a fickle bitch
    
    * I have no idea.
    
    * Threads are for sheets not computers.
    
    * fix traitor rule test
    
    * fix round type tattling
    
    * break things
    
    * It worky
    
    * Toolshed makes we want to drink depresso.
    
    * Finished?
    
    * remove debug values
    
    * timings
    
    * use defaults
    
    * alphabetize
    
    * bobby drop tables
    
    * Float required fr fr
    
    * continue
    
    * more continence
    
    * uno mas
    
    * obsolution
    
    * cleanup and documentations
    
    * Yell at self
    
    * use the right value defaults
    
    * housekeeping

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -110,32 +110,6 @@ namespace Content.Shared.CCVar
         public static readonly CVarDef<bool>
             EventsEnabled = CVarDef.Create("events.enabled", true, CVar.ARCHIVE | CVar.SERVERONLY);
 
-        /// <summary>
-        ///     Average time (in minutes) for when the ramping event scheduler should stop increasing the chaos modifier.
-        ///     Close to how long you expect a round to last, so you'll probably have to tweak this on downstreams.
-        /// </summary>
-        public static readonly CVarDef<float>
-            EventsRampingAverageEndTime = CVarDef.Create("events.ramping_average_end_time", 40f, CVar.ARCHIVE | CVar.SERVERONLY);
-
-        /// <summary>
-        ///     Average ending chaos modifier for the ramping event scheduler.
-        ///     Max chaos chosen for a round will deviate from this
-        /// </summary>
-        public static readonly CVarDef<float>
-            EventsRampingAverageChaos = CVarDef.Create("events.ramping_average_chaos", 6f, CVar.ARCHIVE | CVar.SERVERONLY);
-
-        /// <summary>
-        ///     Minimum time between meteor swarms in minutes.
-        /// </summary>
-        public static readonly CVarDef<float>
-            MeteorSwarmMinTime = CVarDef.Create("events.meteor_swarm_min_time", 12.5f, CVar.ARCHIVE | CVar.SERVERONLY);
-
-        /// <summary>
-        ///     Maximum time between meteor swarms in minutes.
-        /// </summary>
-        public static readonly CVarDef<float>
-            MeteorSwarmMaxTime = CVarDef.Create("events.meteor_swarm_max_time", 17.5f, CVar.ARCHIVE | CVar.SERVERONLY);
-
         /*
          * Game
          */
