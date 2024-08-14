579cd5ea88a0a89844a81996d2b686795ff6df97
Beck <beck314159@hotmail.com>
2024-08-14 15:27:41 -0700

    First commit

--- Content.Server/Nutrition/EntitySystems/SliceableFoodSystem.cs
+++ Content.Server/Nutrition/EntitySystems/SliceableFoodSystem.cs
@@ -34,6 +34,9 @@ public sealed class SliceableFoodSystem : EntitySystem
         if (args.Handled)
             return;
 
+        if (!TryComp<UtensilComponent>(args.Used, out var utensil) || (utensil.Types & UtensilType.Knife) == 0)
+            return;
+
         var doAfterArgs = new DoAfterArgs(EntityManager,
             args.User,
             entity.Comp.SliceTime,
