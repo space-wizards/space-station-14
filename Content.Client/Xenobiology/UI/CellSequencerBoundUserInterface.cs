11df8e9b00f908daeee57ffae86f71c2a4692bbb
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-08-16 04:49:59 +1000

    Added simple cell sequncer

---
+++ Content.Client/Xenobiology/UI/CellSequencerBoundUserInterface.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using Robust.Client.UserInterface;
+
+namespace Content.Client.Xenobiology.UI;
+
+[UsedImplicitly]
+public sealed class CellSequencerBoundUserInterface : BoundUserInterface
+{
+    [ViewVariables]
+    private CellSequencerWindow? _window;
+
+    public CellSequencerBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = this.CreateWindow<CellSequencerWindow>();
+    }
+}
---
+++ Content.Shared/Xenobiology/UI/CellSequencerUiKey.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Xenobiology.UI;
+
+[Serializable, NetSerializable]
+public enum CellSequencerUiKey
+{
+    Key,
+}
