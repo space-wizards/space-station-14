89018bae08dc4366646c343ba75106490521065d
Avery Dobbins <avery.dobbins@gmail.com>
2024-08-15 17:23:17 -0400

    Fix. Does this need to be in server?

--- Content.Server/Damage/Components/DamageOnLandComponent.cs
+++ Content.Server/Damage/Components/DamageOnLandComponent.cs
@@ -5,10 +5,16 @@ namespace Content.Server.Damage.Components
     [RegisterComponent]
     public sealed partial class DamageOnLandComponent : Component
     {
+        /// <summary>
+        /// Should this entity be damaged when it lands regardless of its resistances?
+        /// </summary>
         [DataField("ignoreResistances")]
         [ViewVariables(VVAccess.ReadWrite)]
         public bool IgnoreResistances = false;
 
+        /// <summary>
+        /// How much damage.
+        /// </summary>
         [DataField("damage", required: true)]
         [ViewVariables(VVAccess.ReadWrite)]
         public DamageSpecifier Damage = default!;
