c774b3d78bbea9a5cbe1006ea51c2c1f51dcedf0
Brandon H <Brandon@brandon-huu.com>
2024-08-14 22:05:50 -0500

    Revert ""
    This reverts commit 34c3ae15b00432f9d3fd75490a3c6861ab37d828.

--- Content.Server/Voting/Managers/VoteManager.DefaultVotes.cs
+++ Content.Server/Voting/Managers/VoteManager.DefaultVotes.cs
@@ -18,7 +18,6 @@ namespace Content.Server.Voting.Managers
     public sealed partial class VoteManager
     {
         [Dependency] private readonly EntityWhitelistSystem _whitelist = default!;
-
         private static readonly Dictionary<StandardVoteType, CVarDef<bool>> _voteTypesToEnableCVars = new()
         {
             {StandardVoteType.Restart, CCVars.VoteRestartEnabled},
