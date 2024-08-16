aa50aba94d7e76cf5798b01eaef278fac519de05
Milon <plmilonpl@gmail.com>
2024-08-16 19:56:28 +0200

    no more fucky wucky fucky wuckies are GONE

--- Content.Server/Medical/HealthAnalyzerSystem.cs
+++ Content.Server/Medical/HealthAnalyzerSystem.cs
@@ -92,10 +92,7 @@ public sealed class HealthAnalyzerSystem : EntitySystem
             BreakOnMove = true,
         });
 
-        if (args.Target == args.User || doAfterCancelled)
-            return;
-
-        if (uid.Comp.Silent == true)
+        if (args.Target == args.User || doAfterCancelled || uid.Comp.Silent)
             return;
 
         var msg = Loc.GetString("health-analyzer-popup-scan-target", ("user", Identity.Entity(args.User, EntityManager)));
