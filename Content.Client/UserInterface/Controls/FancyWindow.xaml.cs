b9498673d19a3e6285e1031de58fc429cfa60529
Thomas <git@aeshus.com>
2024-08-17 15:22:54 -0500

    style: Format code

--- Content.Client/UserInterface/Controls/FancyWindow.xaml.cs
+++ Content.Client/UserInterface/Controls/FancyWindow.xaml.cs
@@ -7,78 +7,79 @@ using Robust.Client.UserInterface.CustomControls;
 using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Prototypes;
 
-namespace Content.Client.UserInterface.Controls
+namespace Content.Client.UserInterface.Controls;
+
+[GenerateTypedNameReferences]
+[Virtual]
+public partial class FancyWindow : BaseWindow
 {
-    [GenerateTypedNameReferences]
-    [Virtual]
-    public partial class FancyWindow : BaseWindow
+    [Dependency] private readonly IEntitySystemManager _sysMan = default!;
+    private GuidebookSystem? _guidebookSystem;
+    private const int DragMarginSize = 7;
+    public const string StyleClassWindowHelpButton = "windowHelpButton";
+
+    public FancyWindow()
     {
-        [Dependency] private readonly IEntitySystemManager _sysMan = default!;
-        private GuidebookSystem? _guidebookSystem;
-        private const int DRAG_MARGIN_SIZE = 7;
-        public const string StyleClassWindowHelpButton = "windowHelpButton";
+        RobustXamlLoader.Load(this);
 
-        public FancyWindow()
+        CloseButton.OnPressed += _ => Close();
+        HelpButton.OnPressed += _ => Help();
+
+        XamlChildren = ContentsContainer.Children;
+    }
+
+    public string? Title
+    {
+        get => WindowTitle.Text;
+        set => WindowTitle.Text = value;
+    }
+
+    private List<ProtoId<GuideEntryPrototype>>? _helpGuidebookIds;
+
+    public List<ProtoId<GuideEntryPrototype>>? HelpGuidebookIds
+    {
+        get => _helpGuidebookIds;
+        set
         {
-            RobustXamlLoader.Load(this);
-
-            CloseButton.OnPressed += _ => Close();
-            HelpButton.OnPressed += _ => Help();
-            XamlChildren = ContentsContainer.Children;
+            _helpGuidebookIds = value;
+            HelpButton.Disabled = _helpGuidebookIds == null;
+            HelpButton.Visible = !HelpButton.Disabled;
         }
+    }
 
-        public string? Title
-        {
-            get => WindowTitle.Text;
-            set => WindowTitle.Text = value;
-        }
+    public void Help()
+    {
+        if (HelpGuidebookIds is null)
+            return;
+        _guidebookSystem ??= _sysMan.GetEntitySystem<GuidebookSystem>();
+        _guidebookSystem.OpenHelp(HelpGuidebookIds);
+    }
 
-        private List<ProtoId<GuideEntryPrototype>>? _helpGuidebookIds;
-        public List<ProtoId<GuideEntryPrototype>>? HelpGuidebookIds
-        {
-            get => _helpGuidebookIds;
-            set
-            {
-                _helpGuidebookIds = value;
-                HelpButton.Disabled = _helpGuidebookIds == null;
-                HelpButton.Visible = !HelpButton.Disabled;
-            }
-        }
+    protected override DragMode GetDragModeFor(Vector2 relativeMousePos)
+    {
+        var mode = DragMode.Move;
 
-        public void Help()
-        {
-            if (HelpGuidebookIds is null)
-                return;
-            _guidebookSystem ??= _sysMan.GetEntitySystem<GuidebookSystem>();
-            _guidebookSystem.OpenHelp(HelpGuidebookIds);
-        }
-
-        protected override DragMode GetDragModeFor(Vector2 relativeMousePos)
-        {
-            var mode = DragMode.Move;
-
-            if (Resizable)
-            {
-                if (relativeMousePos.Y < DRAG_MARGIN_SIZE)
-                {
-                    mode = DragMode.Top;
-                }
-                else if (relativeMousePos.Y > Size.Y - DRAG_MARGIN_SIZE)
-                {
-                    mode = DragMode.Bottom;
-                }
-
-                if (relativeMousePos.X < DRAG_MARGIN_SIZE)
-                {
-                    mode |= DragMode.Left;
-                }
-                else if (relativeMousePos.X > Size.X - DRAG_MARGIN_SIZE)
-                {
-                    mode |= DragMode.Right;
-                }
-            }
-
+        if (!Resizable)
             return mode;
+
+        if (relativeMousePos.Y < DragMarginSize)
+        {
+            mode = DragMode.Top;
         }
+        else if (relativeMousePos.Y > Size.Y - DragMarginSize)
+        {
+            mode = DragMode.Bottom;
+        }
+
+        if (relativeMousePos.X < DragMarginSize)
+        {
+            mode |= DragMode.Left;
+        }
+        else if (relativeMousePos.X > Size.X - DragMarginSize)
+        {
+            mode |= DragMode.Right;
+        }
+
+        return mode;
     }
 }
