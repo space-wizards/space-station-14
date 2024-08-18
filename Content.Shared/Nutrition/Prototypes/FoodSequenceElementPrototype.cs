1a285d77582d416502e69c78d1d49b3074e3b6d0
Ed <edwardxperia2000@gmail.com>
2024-08-18 17:54:46 +0300

    remove fun

---
+++ Content.Shared/Nutrition/Prototypes/FoodSequenceElementPrototype.cs
@@ -0,0 +1,25 @@
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Nutrition.Prototypes;
+
+/// <summary>
+///
+/// </summary>
+[Prototype("foodSequenceElement")]
+public sealed partial class FoodSequenceElementPrototype : IPrototype
+{
+    [IdDataField] public string ID { get; private set; } = default!;
+
+    /// <summary>
+    ///
+    /// </summary>
+    [DataField]
+    public LocId Name { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataField]
+    public SpriteSpecifier Sprite { get; private set; } = SpriteSpecifier.Invalid;
+}
