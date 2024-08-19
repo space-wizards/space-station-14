d88accfa526a30a52157c8228610dc9444a185b7
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-08-19 20:37:04 +1000

    AI upload console

---
+++ Content.Shared/Silicons/Laws/Components/SiliconLawUpdaterComponent.cs
@@ -0,0 +1,18 @@
+using Content.Shared.Whitelist;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Silicons.Laws.Components;
+
+/// <summary>
+/// Whenever an entity is inserted with silicon laws it will update the relevant entity's laws.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class SiliconLawUpdaterComponent : Component
+{
+    /// <summary>
+    /// Entities to update
+    /// </summary>
+    [DataField]
+    public ComponentRegistry Components;
+}
