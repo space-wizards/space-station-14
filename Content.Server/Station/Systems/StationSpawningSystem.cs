858faea7af57d54c16b20330f462f5e639e420d9
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-08-20 23:53:10 +0100

    apply role loadouts before starting gear

--- Content.Server/Station/Systems/StationSpawningSystem.cs
+++ Content.Server/Station/Systems/StationSpawningSystem.cs
@@ -252,13 +252,13 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
 
     private void EquipLoadout(EntityUid entity, string jobLoadout, RoleLoadout loadout, RoleLoadoutPrototype roleProto, JobPrototype? prototype, HumanoidCharacterProfile? profile)
     {
+        EquipRoleLoadout(entity, loadout, roleProto);
+
         if (prototype?.StartingGear != null)
         {
             var startingGear = _prototypeManager.Index<StartingGearPrototype>(prototype.StartingGear);
             EquipStartingGear(entity, startingGear, raiseEvent: false);
         }
-
-        EquipRoleLoadout(entity, loadout, roleProto);
 
         var gearEquippedEv = new StartingGearEquippedEvent(entity);
         RaiseLocalEvent(entity, ref gearEquippedEv);
