60a07f7179a72034d2187db6dd87c0729303da5b
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2024-08-22 10:42:37 -0400

    Update ClothingSlowOnDamageModifierComponent.cs

--- Content.Shared/Damage/Components/ClothingSlowOnDamageModifierComponent.cs
+++ Content.Shared/Damage/Components/ClothingSlowOnDamageModifierComponent.cs
@@ -4,6 +4,7 @@ namespace Content.Shared.Damage.Components;
 
 /// <summary>
 /// This is used for a clothing item that modifies the slowdown from taking damage.
+/// Used for entities with <see cref="SlowOnDamageComponent"/>
 /// </summary>
 [RegisterComponent, NetworkedComponent, Access(typeof(SlowOnDamageSystem))]
 public sealed partial class ClothingSlowOnDamageModifierComponent : Component
