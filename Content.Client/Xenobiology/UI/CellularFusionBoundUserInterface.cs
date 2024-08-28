ac9271d802cc089b96901f95f7531f0d039874f2
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-08-28 17:27:13 +1000

    Added base cellular fusion

---
+++ Content.Client/Xenobiology/UI/CellularFusionBoundUserInterface.cs
@@ -0,0 +1,8 @@
+﻿namespace Content.Client.Xenobiology.UI;
+
+public sealed class CellularFusionBoundUserInterface : BoundUserInterface
+{
+    public CellularFusionBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+}
