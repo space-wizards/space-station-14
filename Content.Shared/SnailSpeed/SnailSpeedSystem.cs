eb9c9fb1c1620114186cee802f97a60982716eaf
widgetbeck <163376292+widgetbeck@users.noreply.github.com>
2024-09-03 00:07:07 -0400

    fix to snailspeed
    snailspeed component now updates movement speed modifier whenever the modifier is changed. if it's applied post-mapinit then it will be applied the first time you walk over slime, mucin, glue etc

--- Content.Shared/SnailSpeed/SnailSpeedSystem.cs
+++ Content.Shared/SnailSpeed/SnailSpeedSystem.cs
@@ -24,6 +24,7 @@ public abstract partial class SharedSnailSpeedSystem : EntitySystem
 		SubscribeLocalEvent<SnailSpeedComponent, RefreshMovementSpeedModifiersEvent>(OnRefreshMovespeed);
     }
 
+    /// apply constant movespeed modifier as long as entity is not flying
 	private void OnRefreshMovespeed(EntityUid uid, SnailSpeedComponent component, RefreshMovementSpeedModifiersEvent args)
 	{
 	    if (_jetpack.IsUserFlying(uid))
@@ -31,5 +32,11 @@ public abstract partial class SharedSnailSpeedSystem : EntitySystem
 	
 		
 		args.ModifySpeed(component.SnailSlowdownModifier, component.SnailSlowdownModifier);
+	}
+
+    /// have to do this so that post-map init snails (e.g. urists) still get the speed mod
+	private void RefreshMovespeed(EntityUid uid, SnailSpeedComponent component)
+	{
+		_movementSpeedModifier.RefreshMovementSpeedModifiers(uid);
 	}
 }
