ef61f6d57f902b65253bc5508022676c291cf1d1
archrbx <punk.gear5260@fastmail.com>
2024-09-04 02:04:46 +0100

    fixed LoadSaveTicksSaveBagel failure

--- Content.Shared/GPS/Components/HandheldGPSComponent.cs
+++ Content.Shared/GPS/Components/HandheldGPSComponent.cs
@@ -3,19 +3,14 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.GPS.Components
 {
-    [RegisterComponent, NetworkedComponent, ComponentProtoName("HandheldGPS"), AutoGenerateComponentState]
+    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
     public sealed partial class HandheldGPSComponent : Component
     {
         [DataField("updateRate")]
         [AutoNetworkedField]
         public float UpdateRate = 1.5f;
 
-        [DataField("storedCoords")]
-        [AutoNetworkedField]
         public string StoredCoords = "Unknown";
-
-        [DataField("nextUpdate")]
-        [AutoNetworkedField]
         public TimeSpan NextCoordUpdate = TimeSpan.Zero;
     }
 }
