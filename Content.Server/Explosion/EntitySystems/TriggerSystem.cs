4940c9bf89ea5ebd896f6b7ccd8d4870c0c2ec78
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-09-05 14:25:41 +0300

    fix

--- Content.Server/Explosion/EntitySystems/TriggerSystem.cs
+++ Content.Server/Explosion/EntitySystems/TriggerSystem.cs
@@ -243,7 +243,7 @@ namespace Content.Server.Explosion.EntitySystems
         private void OnTriggerCollide(EntityUid uid, TriggerOnCollideComponent component, ref StartCollideEvent args)
         {
             if (args.OurFixtureId == component.FixtureID && (!component.IgnoreOtherNonHard || args.OtherFixture.Hard))
-                Trigger(uid);
+                Trigger(uid, args.OtherEntity);
         }
 
         private void OnSpawnTriggered(EntityUid uid, TriggerOnSpawnComponent component, MapInitEvent args)
