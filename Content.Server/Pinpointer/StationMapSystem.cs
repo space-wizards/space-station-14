c1326aa6ee7e06a05a8963cb0f8c2bb766a9c6e0
chromiumboy <chromium.boy@gmail.com>
2024-09-06 21:33:56 -0500

    Removed station map regions (to be re-added in a separate PR)

--- Content.Server/Pinpointer/StationMapSystem.cs
+++ Content.Server/Pinpointer/StationMapSystem.cs
@@ -1,10 +1,7 @@
 using Content.Server.PowerCell;
 using Content.Shared.Pinpointer;
-using static Content.Shared.Pinpointer.SharedNavMapSystem;
 using Robust.Server.GameObjects;
-using Robust.Shared.Map.Components;
 using Robust.Shared.Player;
-using Robust.Shared.Timing;
 
 namespace Content.Server.Pinpointer;
 
@@ -12,17 +9,11 @@ public sealed class StationMapSystem : EntitySystem
 {
     [Dependency] private readonly UserInterfaceSystem _ui = default!;
     [Dependency] private readonly PowerCellSystem _cell = default!;
-    [Dependency] private readonly SharedNavMapSystem _navMapSystem = default!;
-    [Dependency] private readonly SharedMapSystem _mapSystem = default!;
-    [Dependency] private readonly SharedTransformSystem _transformSystem = default!;
-    [Dependency] private readonly IGameTiming _gameTiming = default!;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<StationMapUserComponent, EntParentChangedMessage>(OnUserParentChanged);
-        SubscribeLocalEvent<NavMapRegionSeedComponent, MapInitEvent>(OnNavMapBeaconInit);
-        SubscribeLocalEvent<NavMapRegionSeedComponent, AnchorStateChangedEvent>(OnNavMapBeaconAnchor);
 
         Subs.BuiEvents<StationMapComponent>(StationMapUiKey.Key, subs =>
         {
@@ -51,57 +42,5 @@ public sealed class StationMapSystem : EntitySystem
 
         var comp = EnsureComp<StationMapUserComponent>(args.Actor);
         comp.Map = uid;
-    }
-
-    private void OnNavMapBeaconInit(EntityUid uid, NavMapRegionSeedComponent component, ref MapInitEvent ev)
-    {
-        OnNavMapBeaconChange(uid);
-    }
-
-    private void OnNavMapBeaconAnchor(EntityUid uid, NavMapRegionSeedComponent component, ref AnchorStateChangedEvent ev)
-    {
-        OnNavMapBeaconChange(uid);
-    }
-
-    private void OnNavMapBeaconChange(EntityUid uid)
-    {
-        var xform = Transform(uid);
-
-        if (!TryComp<NavMapBeaconComponent>(uid, out var beacon) ||
-            !TryComp<NavMapComponent>(xform.GridUid, out var navMap))
-            return;
-
-        if (xform.Anchored)
-        {
-            var regionProperties = GetNavMapRegionProperties(uid, beacon);
-
-            if (regionProperties.HasValue)
-                _navMapSystem.AddOrUpdateNavMapRegion(xform.GridUid.Value, navMap, GetNetEntity(uid), regionProperties.Value);
-        }
-
-        else
-        {
-            _navMapSystem.RemoveNavMapRegion(xform.GridUid.Value, navMap, GetNetEntity(uid));
-        }
-    }
-
-    private NavMapRegionProperties? GetNavMapRegionProperties(EntityUid uid, NavMapBeaconComponent component)
-    {
-        var xform = Transform(uid);
-
-        if (!TryComp<MapGridComponent>(xform.GridUid, out var mapGrid))
-            return null;
-
-        var seeds = new HashSet<Vector2i>()
-        {
-            _mapSystem.CoordinatesToTile(xform.GridUid.Value, mapGrid, _transformSystem.GetMapCoordinates(uid, xform))
-        };
-
-        var regionProperties = new NavMapRegionProperties(GetNetEntity(uid), StationMapUiKey.Key, seeds)
-        {
-            Color = component.Color,
-        };
-
-        return regionProperties;
     }
 }
