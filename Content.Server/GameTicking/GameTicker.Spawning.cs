99661c4367e4f6bda998609692228e87a6a86160
lzk <124214523+lzk228@users.noreply.github.com>
2024-09-09 00:48:30 +0200

    shame

--- Content.Server/GameTicking/GameTicker.Spawning.cs
+++ Content.Server/GameTicking/GameTicker.Spawning.cs
@@ -243,7 +243,6 @@ namespace Content.Server.GameTicking
                     _chatSystem.DispatchStationAnnouncement(station,
                         Loc.GetString("latejoin-arrival-announcement-special",
                             ("character", MetaData(mob).EntityName),
-                            ("gender", character.Gender),
                             ("job", CultureInfo.CurrentCulture.TextInfo.ToTitleCase(jobName))),
                         Loc.GetString("latejoin-arrival-sender"),
                         playDefaultSound: false,
@@ -254,7 +253,6 @@ namespace Content.Server.GameTicking
                     _chatSystem.DispatchStationAnnouncement(station,
                         Loc.GetString("latejoin-arrival-announcement",
                             ("character", MetaData(mob).EntityName),
-                            ("gender", character.Gender),
                             ("job", CultureInfo.CurrentCulture.TextInfo.ToTitleCase(jobName))),
                         Loc.GetString("latejoin-arrival-sender"),
                         playDefaultSound: false);
