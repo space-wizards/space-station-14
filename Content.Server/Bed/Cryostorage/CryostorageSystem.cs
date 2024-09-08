4c9c53b11e4427c7d6851f25e1601560a391a863
lzk <124214523+lzk228@users.noreply.github.com>
2024-09-09 00:42:54 +0200

    fix gender in locale

--- Content.Server/Bed/Cryostorage/CryostorageSystem.cs
+++ Content.Server/Bed/Cryostorage/CryostorageSystem.cs
@@ -239,6 +239,7 @@ public sealed class CryostorageSystem : SharedCryostorageSystem
             Loc.GetString(
                 "earlyleave-cryo-announcement",
                 ("character", name),
+                ("gender", ent.Owner.Gender),
                 ("job", CultureInfo.CurrentCulture.TextInfo.ToTitleCase(jobName))
             ), Loc.GetString("earlyleave-cryo-sender"),
             playDefaultSound: false
--- Content.Server/GameTicking/GameTicker.Spawning.cs
+++ Content.Server/GameTicking/GameTicker.Spawning.cs
@@ -243,6 +243,7 @@ namespace Content.Server.GameTicking
                     _chatSystem.DispatchStationAnnouncement(station,
                         Loc.GetString("latejoin-arrival-announcement-special",
                             ("character", MetaData(mob).EntityName),
+                            ("gender", character.Gender),
                             ("job", CultureInfo.CurrentCulture.TextInfo.ToTitleCase(jobName))),
                         Loc.GetString("latejoin-arrival-sender"),
                         playDefaultSound: false,
@@ -253,6 +254,7 @@ namespace Content.Server.GameTicking
                     _chatSystem.DispatchStationAnnouncement(station,
                         Loc.GetString("latejoin-arrival-announcement",
                             ("character", MetaData(mob).EntityName),
+                            ("gender", character.Gender),
                             ("job", CultureInfo.CurrentCulture.TextInfo.ToTitleCase(jobName))),
                         Loc.GetString("latejoin-arrival-sender"),
                         playDefaultSound: false);
