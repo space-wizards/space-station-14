119dd610fa30edd3927cd51acbc68601f7d0b8e0
Emisse <99158783+Emisse@users.noreply.github.com>
2024-09-09 15:58:20 -0600

    Revert "Fix spawnpref" (#32018)

--- Content.Server/Shuttles/Systems/ArrivalsSystem.cs
+++ Content.Server/Shuttles/Systems/ArrivalsSystem.cs
@@ -22,7 +22,6 @@ using Content.Shared.DeviceNetwork;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Movement.Components;
 using Content.Shared.Parallax.Biomes;
-using Content.Shared.Preferences;
 using Content.Shared.Salvage;
 using Content.Shared.Shuttles.Components;
 using Content.Shared.Tiles;
@@ -333,9 +332,6 @@ public sealed class ArrivalsSystem : EntitySystem
     public void HandlePlayerSpawning(PlayerSpawningEvent ev)
     {
         if (ev.SpawnResult != null)
-            return;
-
-        if (ev.HumanoidCharacterProfile?.SpawnPriority != SpawnPriorityPreference.Arrivals)
             return;
 
         // Only works on latejoin even if enabled.
