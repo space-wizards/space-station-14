cd0638c1fb9ae73d5221cc9c9635aa1e01ca0a57
dakimasu <dakmasuu@gmail.com>
2024-09-09 18:35:03 +0200

    Done? Need to check out if defaults are the same.

--- Content.Client/Options/UI/Tabs/AccessibilityTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/AccessibilityTab.xaml.cs
@@ -17,6 +17,8 @@ public sealed partial class AccessibilityTab : Control
         Control.AddOptionCheckBox(CCVars.ReducedMotion, ReducedMotionCheckBox);
         Control.AddOptionPercentSlider(CCVars.ChatWindowOpacity, ChatWindowOpacitySlider);
         Control.AddOptionPercentSlider(CCVars.ScreenShakeIntensity, ScreenShakeIntensitySlider);
+        Control.AddOptionPercentSlider(CCVars.SpeechBubbleOpacitySlider, SpeechBubbleOpacitySlider);
+        Control.AddOptionPercentSlider(CCVars.SpeechTextAndNameOpacitySlider, SpeechTextAndNameOpacitySlider);
 
         Control.Initialize();
     }
