1ed459840c2167968042563ff6b6458e37d568c4
chromiumboy <chromium.boy@gmail.com>
2024-09-09 23:38:07 -0500

    Added gas pipe analyzer

---
+++ Content.Shared/Atmos/Components/GasPipeAnalyzerComponent.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Atmos.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class GasPipeAnalyzerComponent : Component
+{
+
+}
