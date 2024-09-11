9cf5b5a73a4222c8454670bbf620ce27b921eaf6
Woody <2997336+MWGMorningwood@users.noreply.github.com>
2024-09-11 19:43:40 -0400

    Match MapLoadBenchmark.cs to upstream

--- Content.Benchmarks/MapLoadBenchmark.cs
+++ Content.Benchmarks/MapLoadBenchmark.cs
@@ -46,8 +46,8 @@ public class MapLoadBenchmark
         PoolManager.Shutdown();
     }
 
-    public static readonly string[] MapsSource = { "Empty", "Box", "Bagel", "Dev", "CentComm", "Atlas", "Core", "TestTeg", "MWG-Saltern", "Packed", "Omega", "Cluster", "Reach", "Origin", "Meta", "Marathon", "Europa", "MeteorArena", "Fland", "Barratry", "Oasis", "Cog" };
-    
+    public static readonly string[] MapsSource = { "Empty", "Satlern", "Box", "Bagel", "Dev", "CentComm", "Core", "TestTeg", "Packed", "Omega", "Reach", "Meta", "Marathon", "MeteorArena", "Fland", "Oasis", "Cog" };
+
     [ParamsSource(nameof(MapsSource))]
     public string Map;
 
