bfc4afa7430fe2963fcf050a39d9c271a60eeeca
chromiumboy <chromium.boy@gmail.com>
2024-09-11 00:45:12 -0500

    Renamed gas pipe analyzer to gas pipe sensor

---
+++ Content.Shared/Atmos/Components/GasPipeSensorComponent.cs
@@ -0,0 +1,19 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Atmos.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class GasPipeSensorComponent : Component
+{
+
+}
+
+[Serializable, NetSerializable]
+public enum GasPipeSensorVisuals : byte
+{
+    State,
+    Lights,
+    Blink,
+}
+
