fe2bb7046dded964b43cf572ff239e080268de20
Plykiya <58439124+Plykiya@users.noreply.github.com>
2024-09-11 13:15:31 -0700

    Radiant temp, burnable books, burn things

---
+++ Content.Server/Temperature/Components/RadiantTemperatureComponent.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.Atmos;
+
+namespace Content.Server.Temperature.Components;
+
+/// <summary>
+/// The entity will cause the surrounding air temperature to change passively without
+/// any need for power or anything else.
+/// </summary>
+[RegisterComponent]
+public sealed partial class RadiantTemperatureComponent : Component
+{
+    /// <summary>
+    /// The temperature that the entity will try to reach
+    /// </summary>
+    [DataField]
+    public float GoalTemperature = Atmospherics.T20C;
+
+    /// <summary>
+    /// How much the temperature of the surrounding air will change in Kelvin per tick
+    /// Positive means it will only heat, negative means it will only cool
+    /// </summary>
+    [DataField]
+    public float TemperatureChangePerTick = 1f;
+}
---
+++ Content.Server/Temperature/Systems/RadiantTemperatureSystem.cs
@@ -0,0 +1,40 @@
+﻿using Content.Server.Atmos.EntitySystems;
+using Content.Server.Temperature.Components;
+using Robust.Server.GameObjects;
+
+namespace Content.Server.Temperature.Systems;
+
+public sealed class RadiantTemperatureSystem : EntitySystem
+{
+    [Dependency] private readonly AtmosphereSystem _atmosphere = default!;
+    [Dependency] private readonly TransformSystem _xform = default!;
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<RadiantTemperatureComponent, TransformComponent>();
+        while (query.MoveNext(out var ent, out var comp, out var xform))
+        {
+            var grid = xform.GridUid;
+            var map = xform.MapUid;
+            var indices = _xform.GetGridTilePositionOrDefault((ent, xform));
+            var mixture = _atmosphere.GetTileMixture(grid, map, indices, true);
+
+            if (mixture is null)
+                continue;
+
+            // do not continue heating if air is hotter than goal temperature,
+            // and the entity is a radiant HEAT source (positive temp changes)
+            if (mixture.Temperature > comp.GoalTemperature && comp.TemperatureChangePerTick > 0)
+                continue;
+
+            // do not continue cooling if air is colder than goal temperature
+            // and the entity is a radiant COOLING source (negative temp changes)
+            if (mixture.Temperature < comp.GoalTemperature && comp.TemperatureChangePerTick < 0)
+                continue;
+
+            mixture.Temperature += comp.TemperatureChangePerTick * frameTime;
+        }
+    }
+}
