265f0ba80eb7922fb33f92efdf3f111abb16ddf5
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-09-12 14:57:07 +1000

    Update cell docs

--- Content.Shared/Xenobiology/Cell.cs
+++ Content.Shared/Xenobiology/Cell.cs
@@ -1,27 +1,75 @@
 ﻿using System.Linq;
+using Content.Shared.Xenobiology.Components;
+using Content.Shared.Xenobiology.Systems;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.Xenobiology;
 
+/// <summary>
+/// Displays the current information about the cell,
+/// it may differ from the prototype due to changes and other mutations,
+/// so to work with cells we allocate a separate class,
+/// which is also passed between the client and the server.
+/// </summary>
+/// <remarks>
+/// Cells are just as immutable,
+/// if you need a new cell you have to create a new one,
+/// example:
+/// <code>
+/// var cellCopy = new Cell(cellOther);
+/// </code>
+/// </remarks>
+/// <seealso cref="CellPrototype"/>
+/// <seealso cref="CellContainerComponent"/>
+/// <seealso cref="SharedCellSystem"/>
 [Serializable, NetSerializable]
 public sealed class Cell : IEquatable<Cell>
 {
+    /// <summary>
+    /// Reflects the prototype on which the cell is based,
+    /// if it is generated, this value will be null.
+    /// </summary>
     [ViewVariables]
-    public readonly ProtoId<CellPrototype> Id;
+    public readonly ProtoId<CellPrototype>? Id;
 
+    /// <summary>
+    /// The color of a cell
+    /// affecting only its display in the world or consoles.
+    /// </summary>
+    /// <seealso cref="SharedCellSystem.GetMergedColor"/>
     [ViewVariables]
     public readonly Color Color;
 
+    /// <summary>
+    /// Cell name, this can be changed by the player,
+    /// don't use this to index cells.
+    /// </summary>
+    /// <seealso cref="SharedCellSystem.GetMergedName"/>
     [ViewVariables]
     public readonly string Name;
 
+    /// <summary>
+    /// The current stability of the cell,
+    /// this affects the chance of successful splice and many other factors.
+    /// </summary>
+    ///
+    /// <seealso cref="SharedCellSystem.GetMergedStability"/>
     [ViewVariables]
     public readonly float Stability;
 
+    /// <summary>
+    /// Cost in materials of the device for printing as well as splicing of the cell.
+    /// </summary>
+    /// <seealso cref="SharedCellSystem.GetMergedCost"/>
     [ViewVariables]
     public readonly int Cost;
 
+    /// <summary>
+    /// A list of modifier prototypes,
+    /// each of which is applied to a cell when it is introduced into the <see cref="CellContainerComponent"/>,
+    /// if the <see cref="CellContainerComponent.AllowModifiers"/> field is true.
+    /// </summary>
     [ViewVariables]
     public readonly List<ProtoId<CellModifierPrototype>> Modifiers;
 
