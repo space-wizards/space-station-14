6dc4552e9012caaf54bba62db56713b3e2f8d9ee
archrbx <punk.gear5260@fastmail.com>
2024-09-15 19:17:39 +0100

    removed delay on coordinate update, removed system update loop

--- Content.Shared/GPS/Components/HandheldGPSComponent.cs
+++ Content.Shared/GPS/Components/HandheldGPSComponent.cs
@@ -1,16 +1,10 @@
-using Robust.Shared.GameStates;
 
 namespace Content.Shared.GPS.Components
 {
-    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState, AutoGenerateComponentPause]
+    [RegisterComponent]
     public sealed partial class HandheldGPSComponent : Component
     {
-        [DataField, AutoNetworkedField]
-        public TimeSpan UpdateRate = TimeSpan.FromSeconds(1.5);
-
-        public string StoredCoords;
-
-        [AutoPausedField]
-        public TimeSpan NextCoordUpdate = TimeSpan.Zero;
+        [DataField]
+        public float UpdateRate = 0.2f;
     }
 }
--- Content.Shared/GPS/Systems/HandheldGpsSystem.cs
+++ Content.Shared/GPS/Systems/HandheldGpsSystem.cs
@@ -8,46 +8,29 @@ public sealed class HandheldGpsSystem : EntitySystem
 {
     [Dependency] private readonly IGameTiming _gameTiming = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly IEntityManager _entMan = default!;
+
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<HandheldGPSComponent, ExaminedEvent>(OnExamine);
     }
-    public override void Update(float frameTime)
-    {
-        base.Update(frameTime);
-
-        var currentTime = _gameTiming.CurTime;
-
-        var query = EntityQueryEnumerator<HandheldGPSComponent>();
-
-        while (query.MoveNext(out var uid, out var gps))
-        {
-            if (Paused(uid) || gps.Deleted)
-                continue;
-
-            if (currentTime >= gps.NextCoordUpdate)
-            {
-                var xform = Transform(uid);
-
-                if (xform.GridUid is not { } gridUid)
-                    continue;
-
-                var pos = _transform.GetMapCoordinates(uid, xform);
-                var x = (int)pos.X;
-                var y = (int)pos.Y;
-                var posText = $"({x}, {y})";
-
-                gps.StoredCoords = Loc.GetString("handheld-gps-coordinates-title", ("coordinates", posText));
-
-                gps.NextCoordUpdate = currentTime + gps.UpdateRate;
-            }
-        }
-    }
 
     private void OnExamine(Entity<HandheldGPSComponent> ent, ref ExaminedEvent args)
     {
-        args.PushMarkup(ent.Comp.StoredCoords);
+        var posText = "Error";
+        var xform = _entMan.GetComponent<TransformComponent>(ent);
+
+        if (xform.GridUid is { } gridUid)
+        {
+            var pos = _transform.GetMapCoordinates(ent, xform);
+            var x = (int)pos.X;
+            var y = (int)pos.Y;
+
+            posText = $"({x}, {y})";
+        }
+
+        args.PushMarkup(Loc.GetString("handheld-gps-coordinates-title", ("coordinates", posText)));
     }
 }
