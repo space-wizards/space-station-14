364a5ca0c8fc1e923ed5336a0dd2ab3bb1f25469
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2024-09-20 17:29:25 +0200

    fix SpawnAndDeleteAllEntitiesInTheSameSpot heisentest

--- Content.Shared/Power/Generator/ActiveGeneratorRevvingComponent.cs
+++ Content.Shared/Power/Generator/ActiveGeneratorRevvingComponent.cs
@@ -3,8 +3,8 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Power.Generator;
 
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed partial class ActiveGeneratorRevvingComponent: Component
+public sealed partial class ActiveGeneratorRevvingComponent : Component
 {
-    [DataField, ViewVariables(VVAccess.ReadOnly), AutoNetworkedField]
+    [DataField, AutoNetworkedField]
     public TimeSpan CurrentTime = TimeSpan.Zero;
 }
