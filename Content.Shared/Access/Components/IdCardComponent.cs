4e1067d91213c8e13d652e64b42db9363bae69b3
chavonadelal <chavonadelal@mail.ru>
2024-09-21 00:30:47 +0300

    Job title localization

--- Content.Shared/Access/Components/IdCardComponent.cs
+++ Content.Shared/Access/Components/IdCardComponent.cs
@@ -20,7 +20,10 @@ public sealed partial class IdCardComponent : Component
     [DataField]
     [AutoNetworkedField]
     [Access(typeof(SharedIdCardSystem), typeof(SharedPdaSystem), typeof(SharedAgentIdCardSystem), Other = AccessPermissions.ReadWrite)]
-    public string? JobTitle;
+    public LocId? JobTitle;
+
+    [Access(typeof(SharedIdCardSystem), typeof(SharedPdaSystem), typeof(SharedAgentIdCardSystem), Other = AccessPermissions.ReadWriteExecute)]
+    public string? LocalizedJobTitle = Loc.GetString(JobTitle); //FIXME
 
     /// <summary>
     /// The state of the job icon rsi.
