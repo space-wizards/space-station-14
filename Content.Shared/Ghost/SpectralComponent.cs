c55bbea9583a77e2e4376c0141fe4f35fa123ed1
Ilya246 <ilyukarno@gmail.com>
2024-09-20 13:55:26 +0400

    react to revenants/AI eye

---
+++ Content.Shared/Ghost/SpectralComponent.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Ghost;
+
+/// <summary>
+/// Marker component to identify "ghostly" entities.
+/// </summary>
+[RegisterComponent]
+public sealed partial class SpectralComponent : Component {}
