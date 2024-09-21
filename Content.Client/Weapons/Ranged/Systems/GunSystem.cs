147e492a9f42c11ff1c2a7c5155dc761338b411e
NULL882 <gost6865@yandex.ru>
2024-09-21 13:22:46 +0300

    initial commit

--- Content.Client/Weapons/Ranged/Systems/GunSystem.cs
+++ Content.Client/Weapons/Ranged/Systems/GunSystem.cs
@@ -5,6 +5,7 @@ using Content.Client.Items;
 using Content.Client.Weapons.Ranged.Components;
 using Content.Shared.Camera;
 using Content.Shared.CombatMode;
+using Content.Shared.Mech.Components;
 using Content.Shared.Weapons.Ranged;
 using Content.Shared.Weapons.Ranged.Components;
 using Content.Shared.Weapons.Ranged.Events;
@@ -149,6 +150,11 @@ public sealed partial class GunSystem : SharedGunSystem
         }
 
         var entity = entityNull.Value;
+
+        if (TryComp<MechPilotComponent>(entity, out var mechPilot))
+        {
+            entity = mechPilot.Mech;
+        }
 
         if (!TryGetGun(entity, out var gunUid, out var gun))
         {
