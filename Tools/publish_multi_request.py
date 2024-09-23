f4499704e9587d5f7a2e3a96e8dce73b9eb7d3be
Woody <2997336+MWGMorningwood@users.noreply.github.com>
2024-09-22 20:30:59 -0400

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -14,7 +14,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://ss14cdn.azurewebsites.net/"
+ROBUST_CDN_URL = "https://ss14-cdn.mw-gc.com/"
 FORK_ID = "MWGSolaris14"
 
 def main():
