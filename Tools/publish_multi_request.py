8056140d0dd88af11bae6ae4bcb80d11fea83939
Kelderan <66748890+KeldWolf@users.noreply.github.com>
2024-09-25 22:12:59 -0700

    Update publish_multi_request.py
    Also removing the port from the new publish script when we start using it. 
    
    Signed-off-by: Kelderan <66748890+KeldWolf@users.noreply.github.com>

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -14,7 +14,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "http://cdn.harmony14.com:27690/"
+ROBUST_CDN_URL = "http://cdn.harmony14.com/"
 FORK_ID = "harmony"
 
 def main():
