02ca241b94e8636490b37996874d831722a49745
Ilya246 <57039557+Ilya246@users.noreply.github.com>
2024-09-27 11:03:21 +0000

    review
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Shared/Ghost/SpectralComponent.cs
+++ Content.Shared/Ghost/SpectralComponent.cs
@@ -1,9 +1,7 @@
-using Robust.Shared.Prototypes;
-
 namespace Content.Shared.Ghost;
 
 /// <summary>
 /// Marker component to identify "ghostly" entities.
 /// </summary>
 [RegisterComponent]
-public sealed partial class SpectralComponent : Component {}
+public sealed partial class SpectralComponent : Component { }
