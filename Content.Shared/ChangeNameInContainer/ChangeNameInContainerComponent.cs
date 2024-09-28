a50a6a5602ae93be1c89f582b1e9d6c255bbe828
Beck <beck314159@hotmail.com>
2024-09-28 10:57:53 -0700

    Minor fixes

--- Content.Shared/ChangeNameInContainer/ChangeNameInContainerComponent.cs
+++ Content.Shared/ChangeNameInContainer/ChangeNameInContainerComponent.cs
@@ -5,17 +5,14 @@ namespace Content.Shared.ChangeNameInContainer;
 
 /// <summary>
 ///     An entity with this component will get its name chaned to the container it's inside of. E.g, if your a
-///     pAI that has this component and are inside a backpack, your name when talking will be "backpack".
+///     pAI that has this component and are inside a lizard plushie, your name when talking will be "lizard plushie".
 /// </summary>
 [RegisterComponent, NetworkedComponent, Access(typeof(ChangeNameInContainerSystem))]
 public sealed partial class ChangeNameInContainerComponent : Component
 {
     /// <summary>
-    ///     A whitelist of containers that will change the name. This is so stuff like pockets don't change the name!
+    ///     A whitelist of containers that will change the name.
     /// </summary>
     [DataField]
-    public EntityWhitelist Whitelist = new()
-    {
-        Components = new[] {"Item"}
-    };
+    public EntityWhitelist Whitelist;
 }
