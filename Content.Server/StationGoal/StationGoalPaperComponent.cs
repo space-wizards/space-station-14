e0b79f06f0ec66642e1be116b258d65b2a34920e
AltMapper <maksimka.volkov.2008@list.ru>
2024-09-29 01:01:21 +0700

    Система выдачи цели
    Возвращена система выдачи цели

---
+++ Content.Server/StationGoal/StationGoalPaperComponent.cs
@@ -0,0 +1,11 @@
+namespace Content.Server.StationGoal
+{
+    /// <summary>
+    ///     Paper with a written station goal in it.
+    /// </summary>
+    [RegisterComponent]
+    public sealed partial class StationGoalPaperComponent : Component
+    {
+    }
+}
+
--- Content.Shared/Fax/Components/FaxMachineComponent.cs
+++ Content.Shared/Fax/Components/FaxMachineComponent.cs
@@ -58,6 +58,13 @@ public sealed partial class FaxMachineComponent : Component
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField]
     public bool ReceiveNukeCodes { get; set; } = false;
+	
+    /// <summary>
+    /// Should that fax receive station goal info
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("receiveStationGoal")]
+    public bool ReceiveStationGoal { get; set; } = false;	
 
     /// <summary>
     /// Sound to play when fax has been emagged
