317927a345f1b4fb7c974533e9c390ee875bff68
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-09-30 17:30:36 +1000

    she

--- Content.Client/Light/PlanetLightOverlay.cs
+++ Content.Client/Light/PlanetLightOverlay.cs
@@ -3,27 +3,51 @@ using Content.Shared.Light.Components;
 using Robust.Client.Graphics;
 using Robust.Shared.Enums;
 using Robust.Shared.Map.Components;
+using Robust.Shared.Prototypes;
 
 namespace Content.Client.Light;
 
 public sealed class PlanetLightOverlay : Overlay
 {
+    [Dependency] private readonly IClyde _clyde = default!;
+
     public override OverlaySpace Space => OverlaySpace.BeforeLighting;
+
+    public PlanetLightOverlay()
+    {
+        IoCManager.InjectDependencies(this);
+    }
 
     protected override void Draw(in OverlayDrawArgs args)
     {
         if (args.Viewport.Eye == null)
             return;
 
+        var protomanager = IoCManager.Resolve<IPrototypeManager>();
+        var texture = _clyde.CreateLightRenderTarget(args.Viewport.LightRenderTarget.Size, name: "planet-lighting");
+        var invMatrix = texture.GetWorldToLocalMatrix(args.Viewport.Eye, args.Viewport.RenderScale / 2f);
+        var worldHandle = args.WorldHandle;
+        var bounds = args.WorldBounds;
+
+        /*
+        args.WorldHandle.RenderInRenderTarget(texture,
+            () =>
+            {
+                worldHandle.UseShader(protomanager.Index<ShaderPrototype>("StencilMask").Instance());
+                worldHandle.SetTransform(invMatrix);
+                worldHandle.DrawTextureRect(texture1.Texture, bounds, Color.Orange);
+            }, null);
+            */
+
         var entManager = IoCManager.Resolve<IEntityManager>();
         var mapSystem = entManager.System<SharedMapSystem>();
         var viewport = args.Viewport;
-        var worldHandle = args.WorldHandle;
-        var bounds = args.WorldBounds;
+
         var lookup = entManager.System<EntityLookupSystem>();
         var xformSystem = entManager.System<SharedTransformSystem>();
 
         var query = entManager.AllEntityQueryEnumerator<PlanetLightComponent, MapGridComponent, TransformComponent>();
+        // TODO: Render to a separate texture, blur, then apply to the main texture.
 
         while (query.MoveNext(out var uid, out var comp, out var grid, out var xform))
         {
@@ -31,26 +55,43 @@ public sealed class PlanetLightOverlay : Overlay
                 continue;
 
             var gridMatrix = xformSystem.GetWorldMatrix(uid);
-            var matrix = viewport.LightRenderTarget.GetWorldToLocalMatrix(args.Viewport.Eye, args.Viewport.RenderScale / 2f);
-            var matty = Matrix3x2.Multiply(matrix, gridMatrix);
 
-            args.WorldHandle.RenderInRenderTarget(args.Viewport.LightRenderTarget,
+            var matty = Matrix3x2.Multiply(gridMatrix, invMatrix);
+
+            args.WorldHandle.RenderInRenderTarget(texture,
                 () =>
                 {
+                    worldHandle.UseShader(protomanager.Index<ShaderPrototype>("StencilMask").Instance());
                     worldHandle.SetTransform(matty);
-                    var tileEnumerator = mapSystem.GetTilesEnumerator(uid, grid, bounds);
-
-                    while (tileEnumerator.MoveNext(out var tileRef))
+                    SharedMapSystem.TilesEnumerator tileEnumerator;
                     {
-                        if (tileRef.Tile.TypeId == 126)
-                            continue;
+                        worldHandle.UseShader(null);
+                        tileEnumerator = mapSystem.GetTilesEnumerator(uid, grid, bounds);
 
-                        var local = lookup.GetLocalBounds(tileRef, grid.TileSize);
-                        worldHandle.DrawRect(local, comp.Color);
+                        while (tileEnumerator.MoveNext(out var tileRef))
+                        {
+                            if (tileRef.Tile.TypeId == 126)
+                                continue;
+
+                            var local = lookup.GetLocalBounds(tileRef, grid.TileSize);
+                            worldHandle.DrawRect(local, Color.Blue);
+                        }
                     }
-                }, null);
+                }, Color.Black);
         }
 
+        // Copy texture to lighting buffer
+        var lightTarget = args.Viewport.LightRenderTarget;
+        var lightInvMatrix = lightTarget.GetWorldToLocalMatrix(args.Viewport.Eye, args.Viewport.RenderScale / 2f);
+
+        args.WorldHandle.RenderInRenderTarget(args.Viewport.LightRenderTarget,
+            () =>
+            {
+                worldHandle.SetTransform(lightInvMatrix);
+                worldHandle.DrawTextureRect(texture.Texture, bounds);
+            }, null);
+
+        worldHandle.UseShader(null);
         args.WorldHandle.SetTransform(Matrix3x2.Identity);
     }
 }
