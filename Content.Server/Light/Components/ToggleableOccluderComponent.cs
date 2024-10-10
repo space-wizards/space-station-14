01dfe124b1882f0385ad871f4b89b7c708b670d4
lzk <124214523+lzk228@users.noreply.github.com>
2024-10-10 10:59:58 +0200

    now should be good

--- Content.Server/Light/Components/ToggleableOccluderComponent.cs
+++ Content.Server/Light/Components/ToggleableOccluderComponent.cs
@@ -1,5 +1,6 @@
 using Content.Shared.DeviceLinking;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Server.Light.Components
 {
