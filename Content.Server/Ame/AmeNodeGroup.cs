130ae1b14a206616d67f67410ed049b29d743ce7
Golinth <amh2023@gmail.com>
2024-10-14 18:03:09 -0500

    Change AME power generation

--- Content.Server/Ame/AmeNodeGroup.cs
+++ Content.Server/Ame/AmeNodeGroup.cs
@@ -169,11 +169,12 @@ public sealed class AmeNodeGroup : BaseNodeGroup
     public float CalculatePower(int fuel, int cores)
     {
         // Balanced around a single core AME with injection level 2 producing 120KW.
-        // Overclocking yields diminishing returns until it evens out at around 360KW.
+        // Two core with four injection is 147kW. Two core with two injection is 117kW. 
 
-        // The adjustment for cores make it so that a 1 core AME at 2 injections is better than a 2 core AME at 2 injections.
-        // However, for the relative amounts for each (1 core at 2 and 2 core at 4), more cores has more output.
-        return 200000f * MathF.Log10(fuel * fuel) * MathF.Pow(0.75f, cores - 1);
+        // Increasing core count creates diminishing returns, increasing injection amount increases 
+        // Unlike the previous solution, increasing fuel and cores always leads to an increase in power, even if by very small amounts.
+        // Increasing core count without increasing fuel always leads to reduced power as well.
+        return 100000f * MathF.Log10(8 * fuel * MathF.Pow(cores, -1 / 10));
     }
 
     public int GetTotalStability()
