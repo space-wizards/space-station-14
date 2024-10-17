b950ca65a56a1cd77404a9570a5739e388a6539f
unknown <wainzor1337@gmail.com>
2024-10-17 12:11:08 +0400

    jump boots

---
+++ Content.Shared/Movement/Components/JumpComponent.cs
@@ -0,0 +1,40 @@
+using Content.Shared.Actions;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+
+namespace Content.Shared.Movement.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class JumpComponent : Component
+{
+    /// <summary>
+    /// How powerful the jump will be when activated
+    /// </summary>
+    [DataField("jumpPower"), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    public float JumpPower = 5f;
+
+
+    // Logical variables
+    [DataField]
+    public EntProtoId Action = "ActionGravityJump";
+
+    [DataField]
+    public EntityUid? ActionEntity;
+
+    [DataField]
+    public bool? IsClothing;
+
+    [DataField, AutoNetworkedField]
+    public EntityUid OnClothingEntity;
+
+
+    // Sound
+    [DataField("soundJump"), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    public SoundSpecifier SoundJump = new SoundPathSpecifier("/Audio/Effects/gravity_jump.ogg");
+}
+
+
+public sealed partial class GravityJumpEvent : InstantActionEvent;
+
