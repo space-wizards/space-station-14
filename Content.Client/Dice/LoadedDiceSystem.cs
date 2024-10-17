400e00520bffdf87c4fded2f4eb86b6dec97a404
Patrick Chieppe <patrick.chieppe@hotmail.com>
2024-10-17 17:33:41 +1100

    Add client LoadedDiceSystem

---
+++ Content.Client/Dice/LoadedDiceSystem.cs
@@ -0,0 +1,10 @@
+using Content.Shared.Dice;
+
+namespace Content.Client.Dice;
+
+/// <summary>
+///     Stub system to ensure client prediction of <see cref="SharedLoadedDiceSystem"/>.
+/// </summary>
+public sealed class LoadedDiceSystem : SharedLoadedDiceSystem
+{
+}
