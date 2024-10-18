9ab578e558603e2a86d445f89fc260ae8be63ae2
Jopaglazik <wainzor1337@gmail.com>
2024-10-18 11:07:31 +0400

    reviewer changes

--- Content.Shared/Movement/Systems/SharedJumpSystem.cs
+++ Content.Shared/Movement/Systems/SharedJumpSystem.cs
@@ -1,5 +1,4 @@
 using Content.Shared.Actions;
-using Content.Shared.Clothing;
 using Content.Shared.Throwing;
 using Content.Shared.Movement.Components;
 using Robust.Shared.Audio.Systems;
@@ -17,34 +16,18 @@ public sealed partial class SharedJumpSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<JumpComponent, ClothingGotEquippedEvent>(OnEquip);
-        SubscribeLocalEvent<JumpComponent, GravityJumpEvent>(JumpAbility);
+        SubscribeLocalEvent<JumpAbilityComponent, GravityJumpEvent>(JumpAbility);
     }
 
-
-    public void OnEquip(Entity<JumpComponent> ent, ref ClothingGotEquippedEvent args)
+    private void JumpAbility(Entity<JumpAbilityComponent> entity, ref GravityJumpEvent args)
     {
-        _actions.AddAction(args.Wearer, ref ent.Comp.ActionEntity, ent.Comp.Action, ent);
-
-        ent.Comp.IsClothing = true;
-        ent.Comp.OnClothingEntity = args.Wearer;
-    }
-
-
-    public void JumpAbility(Entity<JumpComponent> entity, ref GravityJumpEvent args)
-    {
-        if (entity.Comp.IsClothing && entity.Comp.OnClothingEntity != null) // checking for wearing clothing with a jump component
-        {
-            entity.Owner = (EntityUid)entity.Comp.OnClothingEntity;
-        }
-
-        var xform = Transform(entity.Owner);
+        var xform = Transform(args.Performer);
         var throwing = xform.LocalRotation.ToWorldVec() * entity.Comp.JumpPower;
         var direction = xform.Coordinates.Offset(throwing); // to make the character jump in the direction he's looking
 
-        _throwing.TryThrow(entity.Owner, direction);
+        _throwing.TryThrow(args.Performer, direction);
 
-        _audio.PlayPredicted(entity.Comp.SoundJump, entity.Owner, entity);
+        _audio.PlayPredicted(entity.Comp.SoundJump, args.Performer, args.Performer);
 
         args.Handled = true;
     }
