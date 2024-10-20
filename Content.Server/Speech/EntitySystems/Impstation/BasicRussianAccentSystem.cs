df6f309d45d2162f449e72b29c76f911f5ec956b
Kip <tfraser520@gmail.com>
2024-10-20 13:10:02 +1100

    workin!

---
+++ Content.Server/Speech/EntitySystems/Impstation/BasicRussianAccentSystem.cs
@@ -0,0 +1,49 @@
+using System.Text;
+using Content.Server.Speech.Components;
+
+namespace Content.Server.Speech.EntitySystems;
+
+public sealed class BasicRussianAccentSystem : EntitySystem
+{
+    [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<BasicRussianAccentComponent, AccentGetEvent>(OnAccent);
+    }
+
+    public string Accentuate(string message)
+    {
+        var accentedMessage = new StringBuilder(_replacement.ApplyReplacements(message, "basicrussian"));
+
+        for (var i = 0; i < accentedMessage.Length; i++)
+        {
+            var c = accentedMessage[i];
+
+            accentedMessage[i] = c switch
+            {
+                'A' => 'Д',
+                'b' => 'в',
+                'N' => 'И',
+                'n' => 'и',
+                'K' => 'К',
+                'k' => 'к',
+                'm' => 'м',
+                'h' => 'н',
+                't' => 'т',
+                'R' => 'Я',
+                'r' => 'я',
+                'Y' => 'У',
+                'W' => 'Ш',
+                'w' => 'ш',
+                _ => accentedMessage[i]
+            };
+        }
+
+        return accentedMessage.ToString();
+    }
+
+    private void OnAccent(EntityUid uid, BasicRussianAccentComponent component, AccentGetEvent args)
+    {
+        args.Message = Accentuate(args.Message);
+    }
+}
