8a85eee4d0b1ac921b7c36754dfcebecc3add128
AirFryerBuyOneGetOneFree <jakoblondon01@gmail.com>
2024-10-20 22:38:20 -0500

    YES!! Respected new role structure!

--- Content.Shared/_DeltaV/Roles/ListeningPostRoleComponent.cs
+++ Content.Shared/_DeltaV/Roles/ListeningPostRoleComponent.cs
@@ -2,5 +2,5 @@ using Content.Shared.Roles;
 
 namespace Content.Shared.DeltaV.Roles;
 
-[RegisterComponent, ExclusiveAntagonist]
-public sealed partial class ListeningPostRoleComponent : AntagonistRoleComponent;
+[RegisterComponent]
+public sealed partial class ListeningPostRoleComponent : BaseMindRoleComponent;
