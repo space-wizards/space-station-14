ec4479d63374d01d4c0f12ace801fd2f73112aa8
FN <firenamefn@gmail.com>
2024-10-22 14:24:00 +0700

    Improved sound

--- Content.Server/Radio/EntitySystems/HeadsetSystem.cs
+++ Content.Server/Radio/EntitySystems/HeadsetSystem.cs
@@ -112,7 +112,7 @@ public sealed class HeadsetSystem : SharedHeadsetSystem
         if (!component.ToggledSoundChannels.Contains(args.Channel.ID))
             return;
 
-        _audio.PlayEntity(component.Sound, actor.PlayerSession, uid, new() { Volume = -5 });
+        _audio.PlayEntity(component.Sound, actor.PlayerSession, uid);
     }
 
     private void OnEmpPulse(EntityUid uid, HeadsetComponent component, ref EmpPulseEvent args)
