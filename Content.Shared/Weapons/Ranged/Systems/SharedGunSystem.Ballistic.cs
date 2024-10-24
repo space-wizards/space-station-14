32bfe2d48ef30ebd660d3bcae119d8453816a283
TheKittehJesus <29379890+SamGithubAccount@users.noreply.github.com>
2024-10-24 05:50:56 -0500

    Moved shotgun ammoCount fixes

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
@@ -119,6 +119,7 @@ public abstract partial class SharedGunSystem
         {
             var evInsert = new InteractUsingEvent(args.User, ammo, ammoProvider, coordinates);
             RaiseLocalEvent(ammoProvider, evInsert);
+            UpdateAmmoCount(ammoProvider);
         }
 
         List<(EntityUid? Entity, IShootable Shootable)> ammo = new();
@@ -146,7 +147,7 @@ public abstract partial class SharedGunSystem
                 // play sound to be cool
                 Audio.PlayPredicted(component.SoundInsert, uid, args.User);
                 SimulateInsertAmmo(ent.Value, args.Target.Value, Transform(args.Target.Value).Coordinates);
-                UpdateAmmoCount(args.Target.Value);
+
             }
 
             if (IsClientSide(ent.Value))
