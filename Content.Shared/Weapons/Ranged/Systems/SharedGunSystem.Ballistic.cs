b28b7ab023a019ef5c5968a9e10f731cba4986e5
TheKittehJesus <29379890+SamGithubAccount@users.noreply.github.com>
2024-10-24 06:04:13 -0500

    Moved ammoCount reload fix to inside IsClientSide

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
@@ -119,7 +119,7 @@ public abstract partial class SharedGunSystem
         {
             var evInsert = new InteractUsingEvent(args.User, ammo, ammoProvider, coordinates);
             RaiseLocalEvent(ammoProvider, evInsert);
-            UpdateAmmoCount(ammoProvider);
+
         }
 
         List<(EntityUid? Entity, IShootable Shootable)> ammo = new();
@@ -151,6 +151,7 @@ public abstract partial class SharedGunSystem
             }
 
             if (IsClientSide(ent.Value))
+                UpdateAmmoCount(args.Target.Value);
                 Del(ent.Value);
         }
 
