e99acf121292617d72413718e4b2dfc828d7dd4b
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2024-10-29 09:10:57 -0700

    Update fallsystemcomponent.cs

--- Content.Server/Falling/fallsystemcomponent.cs
+++ Content.Server/Falling/fallsystemcomponent.cs
@@ -3,5 +3,5 @@ namespace Content.Server.Falling;
 [RegisterComponent]
 public sealed partial class FallSystemComponent : Component
 {
- public float MaxRandomRadius { get; set; } = 30.0f; // Example radius of 5 units
+ public float MaxRandomRadius { get; set; } = 30.0f; // Decides the random teleport of the fallsystem
 }
