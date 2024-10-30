5cedb0274bbd70637f9b2bba0dff8ef3bc8e3c4e
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2024-10-30 10:38:27 -0700

    Create DelaminatorComponent.cs

---
+++ Content.Shared/Nuke/DelaminatorComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Nuke;
+
+/// <summary>
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class DelaminatorComponent : Component
+{
+
+}
