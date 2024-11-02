65ee2f2695046e96c81b343e2f350109c3090e63
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-11-02 11:34:06 +0300

    Update DungeonJob.PostGenBiome.cs

--- Content.Server/Procedural/DungeonJob/DungeonJob.PostGenBiome.cs
+++ Content.Server/Procedural/DungeonJob/DungeonJob.PostGenBiome.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Content.Server.Parallax;
+using Content.Shared.Maps;
 using Content.Shared.Parallax.Biomes;
 using Content.Shared.Procedural;
 using Content.Shared.Procedural.PostGeneration;
