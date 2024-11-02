f4b6c7a67967b1509048fcc09629c08d506a3d15
Rinary <rinary.super@gmail.com>
2024-11-02 21:25:22 +0200

    upd

--- Content.Server/Vampire/VampireSystem.Objectives.cs
+++ Content.Server/Vampire/VampireSystem.Objectives.cs
@@ -12,15 +12,21 @@ public sealed partial class VampireSystem
 
     private void InitializeObjectives()
     {
-
         SubscribeLocalEvent<BloodDrainConditionComponent, ObjectiveGetProgressEvent>(OnBloodDrainGetProgress);
     }
 
     private void OnBloodDrainGetProgress(EntityUid uid, BloodDrainConditionComponent comp, ref ObjectiveGetProgressEvent args)
     {
         var target = _number.GetTarget(uid);
-        if (target != 0)
-            args.Progress = MathF.Min(comp.BloodDranked / target, 1f);
-        else args.Progress = 1f;
+
+        if (target > 0)
+        {
+            args.Progress = Math.Clamp(comp.BloodDranked / target, 0f, 1f);
+        }
+        else
+        {
+            Logger.Warning($"Target for blood drain objective is 0 or invalid for entity {uid}. Setting progress to 1.");
+            args.Progress = 1f;
+        }
     }
 }
