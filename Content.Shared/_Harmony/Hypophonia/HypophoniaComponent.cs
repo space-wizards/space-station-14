a47c9880046106b0b1a2b36ce5147e8f4d2429e1
FluffMe <1780586+FluffMe@users.noreply.github.com>
2024-11-03 18:41:02 +0100

    Add Hypophonia component and system

---
+++ Content.Shared/_Harmony/Hypophonia/HypophoniaComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Harmony.Speech.Hypophonia
+{
+    [RegisterComponent, NetworkedComponent]
+    public sealed partial class HypophoniaComponent : Component
+    {
+
+    }
+}
