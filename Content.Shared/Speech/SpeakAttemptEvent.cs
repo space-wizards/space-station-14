b34d96aab8888b88aa1cb74c220e6dcc0a144db1
FluffMe <1780586+FluffMe@users.noreply.github.com>
2024-11-03 18:40:26 +0100

    Add Whisper flag to SpeakAttemptEvent, its caller CanSpeak(), and SendEntityWhisper

--- Content.Shared/Speech/SpeakAttemptEvent.cs
+++ Content.Shared/Speech/SpeakAttemptEvent.cs
@@ -2,11 +2,14 @@
 {
     public sealed class SpeakAttemptEvent : CancellableEntityEventArgs
     {
-        public SpeakAttemptEvent(EntityUid uid)
+        // Harmony change Whisper boolean added for hypophonia trait
+        public SpeakAttemptEvent(EntityUid uid, bool whisper)
         {
             Uid = uid;
+            Whisper = whisper;
         }
 
         public EntityUid Uid { get; }
+        public bool Whisper { get; }
     }
 }
