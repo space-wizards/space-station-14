782e33aa34d4906edff450801b8fccd0b1d0ce85
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2024-11-04 12:47:35 -0800

    Update ListenerSystem.cs

--- Content.Server/Store/Systems/ListenerSystem.cs
+++ Content.Server/Store/Systems/ListenerSystem.cs
@@ -2,6 +2,9 @@ using System.Linq; // Add this to use LINQ
 using Content.Shared.Components;
 using Content.Server.Popups;
 using Robust.Shared.GameObjects;
+using Content.Shared.Store.Components;
+using Content.Server.Store.Components;
+using Content.Server.Store.Systems;
 
 namespace Content.Server.Systems
 {
@@ -30,7 +33,7 @@ namespace Content.Server.Systems
         {
             // Action to unlock and open the uplink
             _popupSystem.PopupEntity($"The codeword '{codeword}' has been recognized.", uid);
-            // Implement the uplink unlock and open logic here
+            _store.ToggleUi(msg.Actor, uid);
         }
     }
 }
