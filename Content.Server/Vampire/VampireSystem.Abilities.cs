782013014e120ebf85f46104d0c67fb12d6aedb7
Rinary <rinary.super@gmail.com>
2024-11-07 20:43:26 +0200

    debaff beam
    (cherry picked from commit 6fbf799e4d03ea1b8c947f6a0588d1a632f5d727)

--- Content.Server/Vampire/VampireSystem.Abilities.cs
+++ Content.Server/Vampire/VampireSystem.Abilities.cs
@@ -404,7 +404,7 @@ public sealed partial class VampireSystem
 
                 AddBloodEssence(vampire, volumeToConsume * 0.80);
 
-                _beam.TryCreateBeam(vampire, entity, "SuperchargedLightning");
+                _beam.TryCreateBeam(vampire, entity, "VampireLightning");
 
                 _popup.PopupEntity(Loc.GetString("vampire-bloodsteal-other"), entity, entity, Shared.Popups.PopupType.LargeCaution);
             }
