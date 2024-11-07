1cab30ef000cac98cfa7026a53594cc59c4b7f51
keronshb <keronshb@live.com>
2024-11-06 19:59:18 -0500

    Adds Jaunt ECS and related prototypes

---
+++ Content.Shared/Jaunt/JauntComponent.cs
@@ -0,0 +1,16 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Jaunt;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class JauntComponent : Component
+{
+    [DataField]
+    public EntProtoId JauntAction = "ActionPolymorphJaunt";
+
+    public EntityUid? Action;
+
+    // TODO: Enter & Exit Times and Whitelist when Actions are reworked and can support it
+    // TODO: Cooldown pausing when Actions can support it
+}
---
+++ Content.Shared/Jaunt/JauntSystem.cs
@@ -0,0 +1,19 @@
+﻿using Content.Shared.Actions;
+
+namespace Content.Shared.Jaunt;
+public sealed class JauntSystem : EntitySystem
+{
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<JauntComponent, MapInitEvent>(OnInit);
+    }
+
+    private void OnInit(Entity<JauntComponent> ent, ref MapInitEvent args)
+    {
+        _actions.AddAction(ent.Owner, ref ent.Comp.Action, ent.Comp.JauntAction);
+    }
+}
+
