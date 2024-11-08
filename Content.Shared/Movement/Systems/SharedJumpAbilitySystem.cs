9c6fcf8aaeaccfbdb7e0131fba0512b0cd2f7db9
Jopaglazik <wainzor1337@gmail.com>
2024-11-08 10:41:11 +0400

    reviewer changes

--- Content.Shared/Movement/Systems/SharedJumpAbilitySystem.cs
+++ Content.Shared/Movement/Systems/SharedJumpAbilitySystem.cs
@@ -1,6 +1,7 @@
 using Content.Shared.Throwing;
 using Content.Shared.Movement.Components;
 using Robust.Shared.Audio.Systems;
+using Content.Shared.Gravity;
 
 namespace Content.Shared.Movement.Systems;
 
@@ -8,6 +9,7 @@ public sealed partial class SharedJumpAbilitySystem : EntitySystem
 {
     [Dependency] private readonly ThrowingSystem _throwing = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedGravitySystem _gravity = default!;
 
     public override void Initialize()
     {
@@ -17,14 +19,16 @@ public sealed partial class SharedJumpAbilitySystem : EntitySystem
 
     private void OnGravityJump(Entity<JumpAbilityComponent> entity, ref GravityJumpEvent args)
     {
+        if (_gravity.IsWeightless(args.Performer))
+            return;
+
         var xform = Transform(args.Performer);
         var throwing = xform.LocalRotation.ToWorldVec() * entity.Comp.JumpDistance;
         var direction = xform.Coordinates.Offset(throwing); // to make the character jump in the direction he's looking
 
-        _throwing.TryThrow(args.Performer, direction);
+        _throwing.TryThrow(args.Performer, direction, entity.Comp.JumpThrowSpeed);
 
-        _audio.PlayPredicted(entity.Comp.SoundJump, args.Performer, args.Performer);
-
+        _audio.PlayPredicted(entity.Comp.JumpSound, args.Performer, args.Performer);
         args.Handled = true;
     }
 }
