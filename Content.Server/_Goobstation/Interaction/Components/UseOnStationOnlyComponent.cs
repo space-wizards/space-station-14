1f3371d09796d4bb5f65e7534d5ff4adf272d1b4
V <97265903+formlessnameless@users.noreply.github.com>
2024-11-11 04:39:25 -0600

    remove thing

--- Content.Server/_Goobstation/Interaction/Components/UseOnStationOnlyComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Server._Goobstation.Interaction.Components;
-
-/// <summary>
-///     Allows use item with component on stations only
-/// </summary>
-[RegisterComponent]
-public sealed partial class UseOnStationOnlyComponent : Component
-{
-}
