33f36a0d2c391dcb206020c3e722a67983cffb07
keronshb <keronshb@live.com>
2024-11-12 18:47:56 -0500

    comments for jaunt component and adds on component shutdown method

--- Content.Shared/Jaunt/JauntComponent.cs
+++ Content.Shared/Jaunt/JauntComponent.cs
@@ -3,12 +3,16 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Jaunt;
 
+// Used to control various aspects of a Jaunt
+//  Can be used in-place of giving a jaunt-action directly
 [RegisterComponent, NetworkedComponent]
 public sealed partial class JauntComponent : Component
 {
+    // Which Jaunt Action the component should grant
     [DataField]
     public EntProtoId JauntAction = "ActionPolymorphJaunt";
 
+    // The jaunt action itself
     public EntityUid? Action;
 
     // TODO: Enter & Exit Times and Whitelist when Actions are reworked and can support it
--- Content.Shared/Jaunt/JauntSystem.cs
+++ Content.Shared/Jaunt/JauntSystem.cs
@@ -9,11 +9,18 @@ public sealed class JauntSystem : EntitySystem
     {
         base.Initialize();
         SubscribeLocalEvent<JauntComponent, MapInitEvent>(OnInit);
+        SubscribeLocalEvent<JauntComponent, ComponentShutdown>(OnShutdown);
     }
 
     private void OnInit(Entity<JauntComponent> ent, ref MapInitEvent args)
     {
         _actions.AddAction(ent.Owner, ref ent.Comp.Action, ent.Comp.JauntAction);
     }
+
+    private void OnShutdown(Entity<JauntComponent> ent, ref ComponentShutdown args)
+    {
+        _actions.RemoveAction(ent.Owner, ent.Comp.Action);
+    }
+
 }
 
