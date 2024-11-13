4dc9ff05013e291df5a6e435e483eaf63e7b920e
Lye <zac@doodle.co.uk>
2024-11-13 18:21:13 +0000

    Renamed, realised I should steal from barotrauma

---
+++ Content.Server/Atmos/Components/InWaterComponent.cs
@@ -0,0 +1,28 @@
+﻿using Content.Shared.Atmos;
+using Content.Shared.Damage;
+
+namespace Content.Server.Atmos.Components;
+
+/// <summary>
+/// Component to handle non-breathing gas interactions.
+/// Detects gasses around entities and applies effects. (this is currently for damage to borgs but ¯\_(ツ)_/¯)
+/// </summary>
+[RegisterComponent]
+public sealed partial class InWaterComponent : Component
+{
+
+    // amount of gas needed to trigger effect
+    [DataField("waterThreshold"), ViewVariables(VVAccess.ReadWrite)]
+    public float WaterThreshold = 0.1f;
+
+    /// <summary>
+    ///   Whether the entity is damaged by water.
+    ///   By default things are not
+    /// </summary>
+    [DataField("damagedByWater"), ViewVariables(VVAccess.ReadWrite)]
+    public bool DamagedByWater = false;
+
+    /// Damage caused by the gas
+    [DataField("damage"), ViewVariables(VVAccess.ReadWrite)]
+    public DamageSpecifier Damage = default!;
+}
---
+++ Content.Server/Atmos/EntitySystems/InWaterSystem.cs
@@ -0,0 +1,59 @@
+﻿using Content.Server.Atmos.Components;
+using Content.Shared.Damage;
+using Content.Shared.FixedPoint;
+
+
+namespace Content.Server.Atmos.EntitySystems;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class InWaterSystem : EntitySystem
+{
+    private const float UpdateTimer = 1f;
+    private float _timer = 0f;
+    [Dependency] private readonly AtmosphereSystem _atmo = default!;
+    [Dependency] private readonly DamageableSystem _damageable = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        //Subscribe to relevant events here.
+        //SubscribeLocalEvent<>();
+    }
+
+    public override void Update(float frameTime)
+    {
+        _timer += frameTime;
+
+        if (_timer < UpdateTimer)
+            return;
+
+        _timer -= UpdateTimer;
+
+        var enumerator = EntityQueryEnumerator<InWaterComponent, DamageableComponent>();
+        while (enumerator.MoveNext(out var uid, out var inWater, out var damageable))
+        {
+            if (!inWater.DamagedByWater)
+            {
+                continue;
+            }
+            var totalDamage = FixedPoint2.Zero;
+            foreach (var (barotraumaDamageType, _) in inWater.Damage.DamageDict)
+            {
+                if (!damageable.Damage.DamageDict.TryGetValue(barotraumaDamageType, out var damage))
+                    continue;
+                totalDamage += damage;
+            }
+
+            //if (totalDamage >= gasComp.MaxDamage)
+            //    continue;
+
+            if (_atmo.GetContainingMixture(uid) is { } mixture)
+            {
+
+            }
+        }
+    }
+}
