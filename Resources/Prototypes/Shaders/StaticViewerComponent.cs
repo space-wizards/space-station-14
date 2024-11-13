2108845d58c4a06fa64c0d5c9163e5e3ade600ec
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2024-11-13 11:26:15 -0800

    Update StaticViewerComponent.cs

--- Resources/Prototypes/Shaders/StaticViewerComponent.cs
+++ Resources/Prototypes/Shaders/StaticViewerComponent.cs
@@ -1,1 +1,11 @@
+using Content.Shared.GameObjects.Components;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Overlays
+{
+    public class StaticViewerComponent : Component
+    {
+
+    }
+}
 
