89715ab3f9c0d0529bdd79567f29b631664dbd7e
MendaxxDev <153332064+MendaxxDev@users.noreply.github.com>
2024-11-14 12:13:28 +0100

    asn banning panel window

---
+++ Content.Client/Administration/UI/AsnBanPanel/AsnBanPanelEui.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using Content.Client.Eui;
+using Content.Shared.Administration;
+using Content.Shared.Eui;
+
+namespace Content.Client.Administration.UI.AsnBanPanel;
+
+[UsedImplicitly]
+public sealed class AsnBanPanelEui : BaseEui
+{
+    private AsnBanPanel AsnBanPanel { get; }
+
+    public AsnBanPanelEui()
+    {
+        AsnBanPanel = new AsnBanPanel();
+        AsnBanPanel.OnClose += () => SendMessage(new CloseEuiMessage());
+        AsnBanPanel.BanSubmitted += (asn, minutes, reason, severity)
+            => SendMessage(new AsnBanPanelEuiStateMsg.CreateAsnBanRequest(asn, minutes, reason, severity));
+        AsnBanPanel.AsnChanged += asn => SendMessage(new AsnBanPanelEuiStateMsg.GetAsnInfoRequest(asn));
+    }
+
+    public override void Opened()
+    {
+        AsnBanPanel.OpenCentered();
+    }
+
+    public override void Closed()
+    {
+        AsnBanPanel.Close();
+        AsnBanPanel.Dispose();
+    }
+}
---
+++ Content.Server/Administration/AsnBanPanelEui.cs
@@ -0,0 +1,54 @@
+﻿using Content.Server.Administration.Managers;
+using Content.Server.Chat.Managers;
+using Content.Server.EUI;
+using Content.Shared.Administration;
+using Content.Shared.Eui;
+
+namespace Content.Server.Administration;
+
+public sealed class AsnBanPanelEui : BaseEui
+{
+    [Dependency] private readonly IBanManager _banManager = default!;
+    [Dependency] private readonly IEntityManager _entities = default!;
+    [Dependency] private readonly ILogManager _log = default!;
+    [Dependency] private readonly IPlayerLocator _playerLocator = default!;
+    [Dependency] private readonly IChatManager _chat = default!;
+    [Dependency] private readonly IAdminManager _admins = default!;
+
+    private readonly ISawmill _sawmill;
+
+    public AsnBanPanelEui()
+    {
+        IoCManager.InjectDependencies(this);
+
+        _sawmill = _log.GetSawmill("admin.asnbans_eui");
+    }
+
+    public override EuiStateBase GetNewState()
+    {
+        var hasBan = _admins.HasAdminFlag(Player, AdminFlags.Ban);
+        return new AsnBanPanelEuiState(hasBan);
+    }
+
+    public override async void Opened()
+    {
+        base.Opened();
+        _admins.OnPermsChanged += OnPermsChanged;
+    }
+
+    public override void Closed()
+    {
+        base.Closed();
+        _admins.OnPermsChanged -= OnPermsChanged;
+    }
+
+    private void OnPermsChanged(AdminPermsChangedEventArgs args)
+    {
+        if (args.Player != Player)
+        {
+            return;
+        }
+
+        StateDirty();
+    }
+}
