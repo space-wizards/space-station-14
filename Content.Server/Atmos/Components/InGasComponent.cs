ec99beadb45e4af0838075059be2efc43081161d
Lye <zac@doodle.co.uk>
2024-11-15 13:54:04 +0000

    Borg should work with refactor

--- Content.Server/Atmos/Components/InGasComponent.cs
+++ Content.Server/Atmos/Components/InGasComponent.cs
@@ -13,10 +13,10 @@ public sealed partial class InGasComponent : Component
 {
 
     /// <summary>
-    ///     ID of gas to check for as an int
+    ///     ID of gas to check for as an int. Defaults to water.
     /// </summary>
     [DataField("gasID"), ViewVariables(VVAccess.ReadWrite)]
-    public int GasId;
+    public int GasId = 9;
 
     // amount of gas needed to trigger effect
     [DataField("gasThreshold"), ViewVariables(VVAccess.ReadWrite)]
