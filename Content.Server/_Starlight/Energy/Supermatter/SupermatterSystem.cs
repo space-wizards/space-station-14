0fde9b7eb683dab07febbd6a542162a2bc12e681
starlight <bobopokalipsis@gmail.com>
2024-11-15 22:20:40 +0100

    Add Supermatter system and component
    Created new namespace `Content.Server.Starlight.Energy.Supermatter` and added `SupermatterSystem` class with empty `Initialize` and `Update` methods. Introduced `SupermatterComponent` in `Content.Shared.Starlight.Energy.Supermatter`, marked with `[RegisterComponent]` and `[NetworkedComponent]` attributes. Updated `supermatter.yml` to include `Supermatter` component in `SupermatterCrystal` entity.

---
+++ Content.Server/_Starlight/Energy/Supermatter/SupermatterSystem.cs
@@ -0,0 +1,11 @@
+namespace Content.Server.Starlight.Energy.Supermatter;
+
+public sealed class SupermatterSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+    }
+    public override void Update(float frameTime)
+    {
+    }
+}
---
+++ Content.Shared/_Starlight/Energy/Supermatter/SupermatterComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Starlight.Energy.Supermatter;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class SupermatterComponent : Component
+{
+}
