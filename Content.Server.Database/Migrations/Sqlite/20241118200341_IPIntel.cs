927892aa74d27412edbd63df59249789d26d38c4
Vasilis The Pikachu <vasilis@pikachu.systems>
2024-11-18 21:04:06 +0100

    Database migration

---
+++ Content.Server.Database/Migrations/Sqlite/20241118200341_IPIntel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class IPIntel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ipintel_cache",
+                columns: table => new
+                {
+                    ipintel_cache_id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    time = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    address = table.Column<string>(type: "TEXT", nullable: false),
+                    score = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ipintel_cache", x => x.ipintel_cache_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ipintel_cache");
+        }
+    }
+}
