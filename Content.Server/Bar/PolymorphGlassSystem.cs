e366df023f3792a5397a3bacd961c4c66b44282b
joshepvodka <guilherme.ornel@gmail.com>
2024-11-18 20:37:39 -0300

    fix spawn while in hands problem

--- Content.Server/Bar/PolymorphGlassSystem.cs
+++ Content.Server/Bar/PolymorphGlassSystem.cs
@@ -2,16 +2,20 @@ using Content.Shared.Bar;
 using Content.Shared.Chemistry.EntitySystems;
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Verbs;
+using Robust.Shared.Containers;
 using Robust.Server.GameObjects;
 using Robust.Shared.Prototypes;
+using FastAccessors;
+using Content.Shared.Chemistry.Components;
 
 namespace Content.Server.Bar;
 
 public sealed class TransformableContainerSystem : EntitySystem
 {
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-    [Dependency] private readonly SharedSolutionContainerSystem _solutionsSystem = default!;
     [Dependency] private readonly ItemSlotsSystem _itemSlotsSystem = default!;
+    [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
+    [Dependency] private readonly SharedSolutionContainerSystem _solutionsSystem = default!;
 
     public override void Initialize()
     {
@@ -20,54 +24,72 @@ public sealed class TransformableContainerSystem : EntitySystem
         SubscribeLocalEvent<PolymorphGlassComponent, GetVerbsEvent<Verb>>(OnGetVerb);
     }
 
-    private void ChangeGlass(EntityUid uid, PolymorphGlassComponent component, EntityPrototype prototype)
+    private void ChangeGlass(EntityUid uid, PolymorphGlassComponent component, EntityPrototype prototype, GetVerbsEvent<Verb> args)
     {
         if (!_solutionsSystem.TryGetSolution(uid, "drink", out var sourceSolutionEntity, out var sourceSolution))
             return;
 
+        //  The original glass needs to be deleted before spawning the new one because I'm using SpawnInContainer
+        //  Otherwise if you are holding the glass and change type it won't stay in your hand.
+        //  So I'm copying all the stuff that needs to be transfered from the old to the new glass to use later.
+        //  Here I copy the solution. I also cache some bools.
+        Solution solution = sourceSolution.Clone();
         var position = Transform(uid).Coordinates;
-        var targetEntity = Spawn(prototype.ID, position);
+        var inContainer = _containerSystem.IsEntityInContainer(uid);
+        var hasItemSlots = TryComp<ItemSlotsComponent>(uid, out var itemSlotsComp);
 
-        if (!_solutionsSystem.TryGetSolution(targetEntity, "drink", out var targetSolutionEntity, out var targetSolution))
+        //  Here I copy the garnishes, if there are any, and eject them so they won't get deleted with the glass.
+        EntityUid? sourceGarnish = null;
+        EntityUid? sourceDecoration = null;
+
+        if (hasItemSlots)
         {
-            Del(uid);
-            return;
+            if (_itemSlotsSystem.TryGetSlot(uid, "garnish_slot", out var sourceGarnishSlot, itemSlotsComp))
+                _itemSlotsSystem.TryEject(uid, sourceGarnishSlot, null, out sourceGarnish, true);
+            if (_itemSlotsSystem.TryGetSlot(uid, "decoration_slot", out var sourceDecorationSlot, itemSlotsComp))
+                _itemSlotsSystem.TryEject(uid, sourceDecorationSlot, null, out sourceDecoration, true);
         }
 
-        _solutionsSystem.TryTransferSolution(targetSolutionEntity.Value, sourceSolution, 30.0f);
+        //  Trying to keep my vars in scope.
+        EntityUid? targetEntity = null;
 
-        if (!TryComp<ItemSlotsComponent>(uid, out var sourceItemSlots))
+        if (inContainer)
         {
-            Del(uid);
-            return;
-        }
-
-        if (!_itemSlotsSystem.TryGetSlot(uid, "garnish_slot", out var sourceGarnishSlot) ||
-            !_itemSlotsSystem.TryGetSlot(uid, "decoration_slot", out var sourceDecorationSlot))
-        {
-            Del(uid);
-            return;
-        }
-
-        var outGarnish = _itemSlotsSystem.TryEject(uid, sourceGarnishSlot, null, out var sourceGarnish, true);
-        var outDecoration = _itemSlotsSystem.TryEject(uid, sourceDecorationSlot, null, out var sourceDecoration, true);
-
-        if (TryComp<ItemSlotsComponent>(targetEntity, out var targetItemSlots))
-        {
-            if (!_itemSlotsSystem.TryGetSlot(targetEntity, "garnish_slot", out var targetGarnishSlot) ||
-                !_itemSlotsSystem.TryGetSlot(targetEntity, "decoration_slot", out var targetDecorationSlot))
+            var containingUid = position.EntityId;
+            if (_containerSystem.TryGetContainingContainer(containingUid, uid, out var container))
             {
+                //  Deleting the entity right before spawning so it won't spawn on the ground.
                 Del(uid);
-                return;
+                targetEntity = SpawnInContainerOrDrop(prototype.ID, containingUid, container.ID);
             }
-
-            if (outGarnish && sourceGarnish.HasValue)
-                _itemSlotsSystem.TryInsert(targetEntity, "garnish_slot", sourceGarnish.Value, null, targetItemSlots, true);
-            if (outDecoration && sourceDecoration.HasValue)
-                _itemSlotsSystem.TryInsert(targetEntity, "decoration_slot", sourceDecoration.Value, null, targetItemSlots, true);
+        }
+        else
+        {
+            Del(uid);
+            targetEntity = Spawn(prototype.ID, position);
         }
 
-        Del(uid);
+        if (!targetEntity.HasValue)
+            return;
+
+        if (!_solutionsSystem.TryGetSolution(targetEntity.Value, "drink", out var targetSolutionEntity, out var targetSolution))
+            return;
+
+        //  Transfer the source solution into the new glass.
+        _solutionsSystem.AddSolution(targetSolutionEntity.Value, solution);
+
+        // Transfer the garnishes to the new glass, if there were any.
+        if (hasItemSlots)
+        {
+            if (TryComp<ItemSlotsComponent>(targetEntity, out var targetItemSlots))
+            {
+                if (sourceGarnish.HasValue && _itemSlotsSystem.TryGetSlot(targetEntity.Value, "garnish_slot", out var targetGarnishSlot, targetItemSlots))
+                    _itemSlotsSystem.TryInsert(targetEntity.Value, "garnish_slot", sourceGarnish.Value, args.User, targetItemSlots, true);
+                if (sourceDecoration.HasValue && _itemSlotsSystem.TryGetSlot(targetEntity.Value, "decoration_slot", out var targetDecorationSlot, targetItemSlots))
+                    _itemSlotsSystem.TryInsert(targetEntity.Value, "decoration_slot", sourceDecoration.Value, null, targetItemSlots, true);
+            }
+        }
+
     }
 
     private void OnGetVerb(EntityUid uid, PolymorphGlassComponent component, GetVerbsEvent<Verb> args)
@@ -91,7 +113,7 @@ public sealed class TransformableContainerSystem : EntitySystem
                 DoContactInteraction = true,
                 Act = () =>
                 {
-                    ChangeGlass(uid, component, proto);
+                    ChangeGlass(uid, component, proto, args);
                 }
             };
             args.Verbs.Add(v);
