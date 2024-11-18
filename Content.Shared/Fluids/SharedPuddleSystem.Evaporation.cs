43e1ac653319df669e2b36fef9f7d3bc9d04ee33
jajsha <corbinbinouche7@gmail.com>
2024-11-18 13:03:16 -0500

    Cleanup GetEvaporatingReagents

--- Content.Shared/Fluids/SharedPuddleSystem.Evaporation.cs
+++ Content.Shared/Fluids/SharedPuddleSystem.Evaporation.cs
@@ -9,12 +9,7 @@ public abstract partial class SharedPuddleSystem
 {
     public string[] GetEvaporatingReagents(Solution solution)
     {
-        return solution.Contents.Where(x =>
-        {
-            //Check if the prototype for the ReagentId is slippery.
-            _prototypeManager.TryIndex(x.Reagent.Prototype, out ReagentPrototype? proto);
-            return proto?.Evaporates is bool value && value;
-        }).Select(x => x.Reagent.Prototype).ToArray();
+        return solution.GetReagentPrototypes(_prototypeManager).Keys.Where(x => x.Evaporates).Select(x => x.ID).ToArray();
     }
 
     public bool CanFullyEvaporate(Solution solution)
