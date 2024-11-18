1fb454f8ee127cc7ee6e2c9dc4464ed007a7c591
jajsha <corbinbinouche7@gmail.com>
2024-11-17 23:30:44 -0500

    Make it actually work

--- Content.Shared/Fluids/SharedPuddleSystem.Evaporation.cs
+++ Content.Shared/Fluids/SharedPuddleSystem.Evaporation.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.Reagent;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Fluids;
 
@@ -8,7 +9,12 @@ public abstract partial class SharedPuddleSystem
 {
     public string[] GetEvaporatingReagents(Solution solution)
     {
-        return solution.Contents.Select(x => x.Reagent.Prototype).ToArray();
+        return solution.Contents.Where(x =>
+        {
+            _prototypeManager.TryIndex(x.Reagent.Prototype, out ReagentPrototype? y);
+            if (y == null) return false;
+            return y.Evaporates;
+        }).Select(x => x.Reagent.Prototype).ToArray();
     }
 
     public bool CanFullyEvaporate(Solution solution)
