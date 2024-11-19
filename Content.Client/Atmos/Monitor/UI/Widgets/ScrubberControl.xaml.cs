3c28a7e2a20d59406a8cecd6196abdd76aa688b6
Ignaz Kraft <ignaz.k@live.de>
2024-11-19 01:11:15 +0100

    that was on the wrong line

--- Content.Client/Atmos/Monitor/UI/Widgets/ScrubberControl.xaml.cs
+++ Content.Client/Atmos/Monitor/UI/Widgets/ScrubberControl.xaml.cs
@@ -23,8 +23,8 @@ public sealed partial class ScrubberControl : BoxContainer
     private FloatSpinBox _volumeRate => CVolumeRate;
     private FloatSpinBox _targetPressure => CTargetPressure;
     private CheckBox _wideNet => CWideNet;
-
 	private Button _copySettings => CCopySettings;
+
     private GridContainer _gases => CGasContainer;
     private Dictionary<Gas, Button> _gasControls = new();
 
