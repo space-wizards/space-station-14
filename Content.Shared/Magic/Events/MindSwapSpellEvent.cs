caa19ebca96e0708815d51f598e698c3a2f971c6
ActiveMammmoth <140334666+ActiveMammmoth@users.noreply.github.com>
2024-11-19 10:16:40 -0500

    Removing unncessary spacing
    Co-authored-by: Thomas <87614336+Aeshus@users.noreply.github.com>

--- Content.Shared/Magic/Events/MindSwapSpellEvent.cs
+++ Content.Shared/Magic/Events/MindSwapSpellEvent.cs
@@ -4,8 +4,6 @@ namespace Content.Shared.Magic.Events;
 
 public sealed partial class MindSwapSpellEvent : EntityTargetActionEvent, ISpeakSpell
 {
-
-
     [DataField]
     public string? Speech { get; private set; }
 }
