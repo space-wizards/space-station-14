6dbf7688d6d3893d958cdbd1b6bb0616caa3f33c
Rinary <rinary.super@gmail.com>
2024-11-21 18:41:57 +0200

    init

---
+++ Content.Server/_Starlight/GameTicking/GameTicker.PeacefulRoundEnd.cs
@@ -0,0 +1,36 @@
+using Content.Server.GameTicking;
+using Content.Shared.Starlight.CCVar;
+using Content.Shared.CombatMode.Pacification;
+using Content.Shared.Mindshield.Components;
+using Robust.Server.Player;
+using Robust.Shared.Configuration;
+
+namespace Content.Server.Starlight.GameTicking;
+
+public sealed class PeacefulRoundEndSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+
+    private bool _isEnabled = false;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        _cfg.OnValueChanged(StarlightCCVars.PeacefulRoundEnd, v => _isEnabled = v, true);
+        SubscribeLocalEvent<RoundEndTextAppendEvent>(OnRoundEnded);
+    }
+
+    private void OnRoundEnded(RoundEndTextAppendEvent ev)
+    {
+        if (!_isEnabled) return;
+        foreach (var session in _playerManager.Sessions)
+        {
+            if (!session.AttachedEntity.HasValue) continue;
+
+            var entityId = session.AttachedEntity.Value;
+
+            EnsureComp<PacifiedComponent>(entityId);
+        }
+    }
+}
