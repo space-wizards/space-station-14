254cce1866c739e20413611e6e6f103912615ea6
TheGrimbeeper <thegrimbeeper.11@gmail.com>
2024-11-23 10:21:36 +1100

    Added new stun trigger for artifacts
    also adds a slip trigger system but because all slips are stuns we only need to use the stun currently.

---
+++ Content.Server/_Impstation/Xenoarchaeology/XenoArtifacts/Triggers/Components/ArtifactSlipTriggerComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Content.Server.Xenoarchaeology.XenoArtifacts.Triggers.Components;
+
+/// <summary>
+/// Triggers when a nearby entity is slipped
+/// </summary>
+[RegisterComponent]
+public sealed partial class ArtifactSlipTriggerComponent : Component
+{
+    /// <summary>
+    /// How close to the slip the artifact has to be for it to trigger.
+    /// </summary>
+    [DataField("range")]
+    public float Range = 6f;
+}
