d850b2bedcbfec2d778f26e493c06d3268f2c407
TheGrimbeeper <thegrimbeeper.11@gmail.com>
2024-11-23 13:29:54 +1100

    Removed unused slip trigger system

--- Content.Server/_Impstation/Xenoarchaeology/XenoArtifacts/Triggers/Systems/ArtifactStunTriggerSystem.cs
+++ Content.Server/_Impstation/Xenoarchaeology/XenoArtifacts/Triggers/Systems/ArtifactStunTriggerSystem.cs
@@ -13,7 +13,6 @@ public sealed class ArtifactStunTriggerSystem : EntitySystem
         SubscribeLocalEvent<StunnedComponent, StunnedEvent>(OnStun);
     }
 
-
     private void OnStun(EntityUid stunned, StunnedComponent component, ref StunnedEvent args)
     {
 
@@ -27,7 +26,7 @@ public sealed class ArtifactStunTriggerSystem : EntitySystem
                 continue;
 
             if (distance > trigger.Range)
-                 continue;
+                continue;
 
             toActivate.Add((uid, trigger));
         }
