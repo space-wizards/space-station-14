0d783ff2c735757e1a7f457328e0ee7cb9c2ddd3
Winkarst <74284083+Winkarst-cpu@users.noreply.github.co>
2024-11-24 04:13:29 +0300

    Merge methods

--- Content.Server/Silicons/Laws/SiliconLawSystem.cs
+++ Content.Server/Silicons/Laws/SiliconLawSystem.cs
@@ -176,23 +176,20 @@ public sealed class SiliconLawSystem : SharedSiliconLawSystem
             LawString = Loc.GetString("law-emag-secrecy", ("faction", Loc.GetString(component.Lawset.ObeysTo))),
             Order = component.Lawset.Laws.Max(law => law.Order) + 1
         });
-    }
 
-    protected override void OnGotEmagged(EntityUid uid, EmagSiliconLawComponent component, ref GotEmaggedEvent args)
-    {
-        if (args.Handled)
+        if (!TryComp<EmagSiliconLawComponent>(uid, out var emagSiliconLaw))
             return;
 
-        if (component.RequireOpenPanel && TryComp<WiresPanelComponent>(uid, out var panel) && !panel.Open)
+        if (emagSiliconLaw.RequireOpenPanel && TryComp<WiresPanelComponent>(uid, out var panel) && !panel.Open)
             return;
 
-        base.OnGotEmagged(uid, component, ref args);
-        NotifyLawsChanged(uid, component.EmaggedSound);
-        if(_mind.TryGetMind(uid, out var mindId, out _))
+        base.OnGotEmagged(uid, emagSiliconLaw, ref args);
+        NotifyLawsChanged(uid, emagSiliconLaw.EmaggedSound);
+
+        if (_mind.TryGetMind(uid, out var mindId, out _))
             EnsureSubvertedSiliconRole(mindId);
 
-        _stunSystem.TryParalyze(uid, component.StunTime, true);
-
+        _stunSystem.TryParalyze(uid, emagSiliconLaw.StunTime, true);
     }
 
     private void EnsureSubvertedSiliconRole(EntityUid mindId)
