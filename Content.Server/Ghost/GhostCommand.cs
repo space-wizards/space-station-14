c07c926b3fede7a7c8dfc900ef72ea30df417c91
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-11-24 23:52:20 +0300

    Fix build

--- Content.Server/Ghost/GhostCommand.cs
+++ Content.Server/Ghost/GhostCommand.cs
@@ -1,7 +1,9 @@
 using Content.Server.Popups;
 using Content.Shared.Administration;
+using Content.Shared.GameTicking;
 using Content.Shared.Mind;
 using Robust.Shared.Console;
+using Content.Server.GameTicking;
 
 namespace Content.Server.Ghost
 {
