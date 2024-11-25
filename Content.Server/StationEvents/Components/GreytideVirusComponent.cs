e9b0e850d2310c608d6d365f5f3053e21c4d7b51
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2024-11-25 17:57:30 +0100

    full implementation

--- Content.Server/StationEvents/Components/GreytideVirusComponent.cs
+++ Content.Server/StationEvents/Components/GreytideVirusComponent.cs
@@ -1,5 +1,7 @@
 using Content.Server.StationEvents.Events;
 using Content.Shared.Destructible.Thresholds;
+using Content.Shared.Access;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server.StationEvents.Components;
 
@@ -11,8 +13,26 @@ public sealed partial class GreytideVirusRuleComponent : Component
 {
     /// <summary>
     ///     Range from which the severity is randomly picked from.
-    ///     Severity corresponds to the number of substations affected.
     /// </summary>
     [DataField]
     public MinMax SeverityRange = new(1, 3);
+
+    /// <summary>
+    ///     Severity corresponding to the number of access groups affected.
+    ///     Will pick randomly from the SeverityRange if not specified.
+    /// </summary>
+    [DataField]
+    public int? Severity;
+
+    /// <summary>
+    ///     Access groups to pick from.
+    /// </summary>
+    [DataField]
+    public List<ProtoId<AccessGroupPrototype>> AccessGroups = new();
+
+    /// <summary>
+    ///     Entities with this access level will be ignored.
+    /// </summary>
+    [DataField]
+    public List<ProtoId<AccessLevelPrototype>> Blacklist = new();
 }
