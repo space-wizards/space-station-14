0de6e8510ad65e4c976fc343d33397b642540175
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-11-26 23:40:52 +1100

    Fix default view top bar

--- Content.Client/UserInterface/Systems/MenuBar/Widgets/GameTopMenuBar.xaml.cs
+++ Content.Client/UserInterface/Systems/MenuBar/Widgets/GameTopMenuBar.xaml.cs
@@ -2,14 +2,13 @@
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.XAML;
 
-namespace Content.Client.UserInterface.Systems.MenuBar.Widgets
+namespace Content.Client.UserInterface.Systems.MenuBar.Widgets;
+
+[GenerateTypedNameReferences]
+public sealed partial class GameTopMenuBar : UIWidget
 {
-    [GenerateTypedNameReferences]
-    public sealed partial class GameTopMenuBar : UIWidget
+    public GameTopMenuBar()
     {
-        public GameTopMenuBar()
-        {
-            RobustXamlLoader.Load(this);
-        }
+        RobustXamlLoader.Load(this);
     }
 }
