bc55aa1b0fc87d91378089089543641035f8d566
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2024-11-26 11:54:48 -0800

    Create InWaterEvent.cs

---
+++ Content.Server/Atmos/Components/InWaterEvent.cs
@@ -0,0 +1,37 @@
+using JetBrains.Annotations;
+
+namespace Content.Shared.Interaction;
+
+/// <summary>
+/// Raised when an entity enters water in the world.
+/// </summary>
+[PublicAPI]
+public sealed class InWaterEvent : HandledEntityEventArgs
+{
+    /// <summary>
+    /// The entity that entered the water.
+    /// </summary>
+    public EntityUid User { get; }
+
+    public InWaterEvent(EntityUid user)
+    {
+        User = user;
+    }
+}
+
+/// <summary>
+/// Raised when an entity exits water in the world.
+/// </summary>
+[PublicAPI]
+public sealed class OutOfWaterEvent : HandledEntityEventArgs
+{
+    /// <summary>
+    /// The entity that exited the water.
+    /// </summary>
+    public EntityUid User { get; }
+
+    public OutOfWaterEvent(EntityUid user)
+    {
+        User = user;
+    }
+}
