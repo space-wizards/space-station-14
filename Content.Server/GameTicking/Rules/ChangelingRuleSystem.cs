f5847a81d34601734eef56d7c9030d1bc6ec1714
Rinary <rinary.super@gmail.com>
2024-11-26 20:59:06 +0200

    lings and vampires fix

--- Content.Server/GameTicking/Rules/ChangelingRuleSystem.cs
+++ Content.Server/GameTicking/Rules/ChangelingRuleSystem.cs
@@ -57,7 +57,12 @@ public sealed partial class ChangelingRuleSystem : GameRuleSystem<ChangelingRule
             var briefingShort = Loc.GetString("changeling-role-greeting-short", ("name", metaData?.EntityName ?? "Unknown"));
 
             _antag.SendBriefing(target, briefing, Color.Yellow, BriefingSound);
-            _role.MindAddRole(mindId, new RoleBriefingComponent { Briefing = briefingShort }, mind, true);
+            _role.MindHasRole<ChangelingRoleComponent>(mindId, out var changelingRole);
+            if (changelingRole is not null)
+            {
+                AddComp<RoleBriefingComponent>(changelingRole.Value.Owner);
+                Comp<RoleBriefingComponent>(changelingRole.Value.Owner).Briefing = briefing;
+            }
         }
         // hivemind stuff
         _npcFaction.RemoveFaction(target, NanotrasenFactionId, false);
