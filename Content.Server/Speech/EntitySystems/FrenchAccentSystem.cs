f8457444eed01a041c956f8db7fcbd8abf107090
obani <robin@pi-et-ro.net>
2024-11-28 21:14:23 +0100

    Improved French accent

--- Content.Server/Speech/EntitySystems/FrenchAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/FrenchAccentSystem.cs
@@ -1,5 +1,6 @@
 using Content.Server.Speech.Components;
 using System.Text.RegularExpressions;
+using Robust.Shared.Random;
 
 namespace Content.Server.Speech.EntitySystems;
 
@@ -8,6 +9,7 @@ namespace Content.Server.Speech.EntitySystems;
 /// </summary>
 public sealed class FrenchAccentSystem : EntitySystem
 {
+    [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
 
     private static readonly Regex RegexTh = new(@"th", RegexOptions.IgnoreCase);
@@ -27,14 +29,38 @@ public sealed class FrenchAccentSystem : EntitySystem
 
         msg = _replacement.ApplyReplacements(msg, "french");
 
-        // replaces th with z
-        msg = RegexTh.Replace(msg, "'z");
-
         // replaces h with ' at the start of words.
         msg = RegexStartH.Replace(msg, "'");
 
         // spaces out ! ? : and ;.
         msg = RegexSpacePunctuation.Replace(msg, " $&");
+        
+        // replaces th with 'z or 's depending on the case
+        var offset = 0;
+        foreach (Match match in RegexTh.Matches(msg))
+        {
+            var i = match.Index + offset;
+
+            var uppercase = msg.Substring(i, 2).Contains("TH");
+            var Z = uppercase ? "Z" : "z";
+            var S = uppercase ? "S" : "s";
+            var idxLetter = i + 2;
+
+            // If th is alone, just do 'zis
+            if (msg.Length <= idxLetter) {
+                msg = msg.Substring(0, i) + "'" + Z;
+            } else {
+                var c = "aeiouy".Contains(msg.Substring(idxLetter, 1).ToLower()) ? Z : S;
+
+                // french people tend to force 'ze s when talking, especially loudly
+                if (c == S && _random.Prob(uppercase ? 0.75f : 0.25f)) {
+                    c += c;
+                    offset += 1;
+                }
+
+                msg = msg.Substring(0, i) + "'" + c + msg.Substring(idxLetter);
+            }
+        }
 
         return msg;
     }
