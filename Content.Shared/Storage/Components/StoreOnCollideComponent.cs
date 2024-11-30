e453ebaf589950ee88c3373f150a700abe09ad95
keronshb <keronshb@live.com>
2024-11-30 17:55:48 -0500

    Adds IsOpen method to check if storage is open

---
+++ Content.Shared/Storage/Components/StoreOnCollideComponent.cs
@@ -0,0 +1,14 @@
+﻿using Content.Shared.Storage.EntitySystems;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Storage.Components;
+
+[RegisterComponent, NetworkedComponent, Access(typeof(StoreOnCollideSystem))]
+public sealed partial class StoreOnCollideComponent : Component
+{
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public bool BodyOnly;
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public bool LockOnCollide;
+}
