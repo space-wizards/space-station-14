51d719882cf2ef53fe312abed61379402845e6f9
Darkmajia <darkwindleaf@hotmail.co.uk>
2024-12-01 16:39:32 +0000

    duplicate entry whoopsiee

--- Content.Server/_Goobstation/Changeling/ChangelingSystem.Abilities.cs
+++ Content.Server/_Goobstation/Changeling/ChangelingSystem.Abilities.cs
@@ -20,7 +20,6 @@ using Content.Shared.Movement.Pulling.Components;
 using Content.Shared.Stealth.Components;
 using Content.Shared.Damage.Components;
 using Content.Server.Radio.Components;
-using Content.Shared.Atmos.Rotting;
 
 namespace Content.Server.Changeling;
 
