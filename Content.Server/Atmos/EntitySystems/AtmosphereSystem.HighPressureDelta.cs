1fb4e75efc200456f20e846c69dac88f685417f3
AirFryerBuyOneGetOneFree <jakoblondon01@gmail.com>
2024-11-30 20:55:08 -0600

    Turn on Space wind, seperate the minimum space wind pressure value from the SpaceWindMinimumCalculatedMass cvar, slightly increase minimum space pressure difference needed for space wind to occur.

--- Content.Server/Atmos/EntitySystems/AtmosphereSystem.HighPressureDelta.cs
+++ Content.Server/Atmos/EntitySystems/AtmosphereSystem.HighPressureDelta.cs
@@ -73,7 +73,7 @@ namespace Content.Server.Atmos.EntitySystems
 
         private void HighPressureMovements(Entity<GridAtmosphereComponent> gridAtmosphere, TileAtmosphere tile, EntityQuery<PhysicsComponent> bodies, EntityQuery<TransformComponent> xforms, EntityQuery<MovedByPressureComponent> pressureQuery, EntityQuery<MetaDataComponent> metas)
         {
-            if (tile.PressureDifference < SpaceWindMinimumCalculatedMass * SpaceWindMinimumCalculatedMass)
+            if (tile.PressureDifference < (SpaceWindMinimumCalculatedMass * SpaceWindMinimumCalculatedMass) || tile.PressureDifference < 130)
                 return;
             // TODO ATMOS finish this
 
@@ -120,7 +120,9 @@ namespace Content.Server.Atmos.EntitySystems
                 }
 
                 if (curTile != tile)
-                    tile.PressureSpecificTarget = curTile;
+                    tile.
+            if (!SpaceWind)
+                return;PressureSpecificTarget = curTile;
             }
 
             _entSet.Clear();
