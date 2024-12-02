fa1fe9375a9e0d8ec8bf146fd36f3f21e4bc6f01
keronshb <keronshb@live.com>
2024-12-01 19:33:39 -0500

    Adds locking support

--- Content.Shared/Storage/Components/StoreOnCollideComponent.cs
+++ Content.Shared/Storage/Components/StoreOnCollideComponent.cs
@@ -3,6 +3,7 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.Storage.Components;
 
+// Use where you want an entity to store other entities on collide
 [RegisterComponent, NetworkedComponent, Access(typeof(StoreOnCollideSystem))]
 public sealed partial class StoreOnCollideComponent : Component
 {
--- Content.Shared/Storage/EntitySystems/StoreOnCollideSystem.cs
+++ Content.Shared/Storage/EntitySystems/StoreOnCollideSystem.cs
@@ -1,4 +1,6 @@
 ﻿using Content.Shared.Body.Components;
+using Content.Shared.Lock;
+using Content.Shared.Projectiles;
 using Content.Shared.Storage.Components;
 using Robust.Shared.Physics.Events;
 
@@ -7,6 +9,7 @@ namespace Content.Shared.Storage.EntitySystems;
 internal sealed class StoreOnCollideSystem : EntitySystem
 {
     [Dependency] private readonly SharedEntityStorageSystem _storage = default!;
+    [Dependency] private readonly LockSystem _lock = default!;
 
     public override void Initialize()
     {
@@ -14,16 +17,23 @@ internal sealed class StoreOnCollideSystem : EntitySystem
         SubscribeLocalEvent<StoreOnCollideComponent, StartCollideEvent>(OnCollide);
     }
 
+    // We use Collide instead of Projectile to support different types of interactions
     private void OnCollide(Entity<StoreOnCollideComponent> ent, ref StartCollideEvent args)
     {
         // TODO: Add support to stop storage on collide after triggering once?
         // TODO: Add support to create storage dynamically
+        // TODO: Add support to also include projectiles, will need to think of how to include it if bodyonly is checked
 
-        if (ent.Owner == args.OtherEntity || Transform(args.OtherEntity).Anchored || _storage.IsOpen(ent.Owner) || (ent.Comp.BodyOnly && !HasComp<BodyComponent>(args.OtherEntity)))
+        var storageEnt = ent.Owner;
+        var target = args.OtherEntity;
+        var comp = ent.Comp;
+
+        if (storageEnt == target || Transform(target).Anchored || _storage.IsOpen(storageEnt) || (comp.BodyOnly && !HasComp<BodyComponent>(target)))
             return;
 
-        _storage.Insert(args.OtherEntity, ent.Owner);
+        _storage.Insert(target, storageEnt);
 
-        // TODO: Lock Support & figure out how to lock once
+        if (comp.LockOnCollide && !_lock.IsLocked(storageEnt))
+            _lock.Lock(storageEnt, storageEnt);
     }
 }
