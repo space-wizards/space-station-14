a43aa1fcb6cdac992d98a091cd34ae9e4763818b
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2024-12-02 10:42:06 -0800

    Update fallsystem.cs

--- Content.Server/Falling/fallsystem.cs
+++ Content.Server/Falling/fallsystem.cs
@@ -37,6 +37,9 @@ namespace Content.Server.Falling
             if (args.OldParent == null || args.Transform.GridUid != null || TerminatingOrDeleted(owner)) // If you came from space or are switching to another valid grid, nothing happens.
                 return;
 
+            if (!HasComp<TriesteComponent>(args.OldParent)) #Are you falling off Trieste? No? Shut up.
+            return;
+            
             if (HasComp<GhostComponent>(owner))
             return;
 
