1d4d70ea22b478d5a42b8e08f34dc09b6d51b140
GreaseMonk <1354802+GreaseMonk@users.noreply.github.com>
2024-12-03 01:40:51 +0100

    Use style property prog

--- Content.Client/Administration/UI/CustomControls/HSeparator.cs
+++ Content.Client/Administration/UI/CustomControls/HSeparator.cs
@@ -1,54 +1,36 @@
 ﻿using Robust.Client.Graphics;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
-using Robust.Shared.Maths;
 
 namespace Content.Client.Administration.UI.CustomControls;
 
 public sealed class HSeparator : Control
 {
+    public Color SeparatorColor { get; set; }
+
     private static readonly Color DefaultSeparatorColor = Color.FromHex("#3D4059");
 
-    private Color _separatorColor = DefaultSeparatorColor;
-    public Color SeparatorColor
-    {
-        get => _separatorColor;
-        set
-        {
-            _separatorColor = Color.FromHex(value.ToHex());
-            UpdateSeparatorColor();
-        }
-    }
-
-    private PanelContainer? _panelContainer = null;
+    public const string StylePropertyColor = "color";
 
     public HSeparator(Color color)
     {
         SeparatorColor = color;
-        Initialize();
+        if (TryGetStyleProperty<Color>(StylePropertyColor, out var bgColor))
+        {
+            color = bgColor;
+        }
+
+        AddChild(new PanelContainer
+            {
+                PanelOverride = new StyleBoxFlat
+                {
+                    BackgroundColor = color,
+                    ContentMarginBottomOverride = 2,
+                    ContentMarginLeftOverride = 2,
+                },
+            }
+        );
     }
 
     public HSeparator() : this(DefaultSeparatorColor) { }
-
-    private void Initialize()
-    {
-        _panelContainer = new PanelContainer
-        {
-            PanelOverride = new StyleBoxFlat
-            {
-                BackgroundColor = SeparatorColor,
-                ContentMarginBottomOverride = 2,
-                ContentMarginLeftOverride = 2
-            }
-        };
-        AddChild(_panelContainer);
-    }
-
-    private void UpdateSeparatorColor()
-    {
-        if (_panelContainer?.PanelOverride is StyleBoxFlat styleBox)
-        {
-            styleBox.BackgroundColor = SeparatorColor;
-        }
-    }
 }
