64b032291411df3dbb4a0ffdf2401ca7ae414ac5
aada <ss.adasts@gmail.com>
2024-12-04 02:20:39 -0600

    use random sprite component

--- Content.Shared/Spider/SharedSpiderSystem.cs
+++ Content.Shared/Spider/SharedSpiderSystem.cs
@@ -7,25 +7,16 @@ namespace Content.Shared.Spider;
 public abstract class SharedSpiderSystem : EntitySystem
 {
     [Dependency] private readonly SharedActionsSystem _action = default!;
-    [Dependency] private readonly IRobustRandom _robustRandom = default!;
-    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<SpiderComponent, MapInitEvent>(OnInit);
-        SubscribeLocalEvent<SpiderWebObjectComponent, ComponentStartup>(OnWebStartup);
     }
 
     private void OnInit(EntityUid uid, SpiderComponent component, MapInitEvent args)
     {
         _action.AddAction(uid, ref component.ActionEntity, component.SpawnWebAction, uid);
-    }
-
-    private void OnWebStartup(EntityUid uid, SpiderWebObjectComponent component, ComponentStartup args)
-    {
-        // TODO dont use this. use some general random appearance system
-        _appearance.SetData(uid, SpiderWebVisuals.Variant, _robustRandom.Next(1, 3));
     }
 }
