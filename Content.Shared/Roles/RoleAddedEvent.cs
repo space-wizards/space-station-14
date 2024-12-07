14db0df75c41ff7aedb86e4042d425d65d071726
Errant <35878406+Errant-4@users.noreply.github.com>
2024-12-07 17:27:40 +0100

    comment formatting
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Shared/Roles/RoleAddedEvent.cs
+++ Content.Shared/Roles/RoleAddedEvent.cs
@@ -9,5 +9,5 @@ namespace Content.Shared.Roles;
 /// <param name="MindId">The mind id associated with the player.</param>
 /// <param name="Mind">The mind component associated with the mind id.</param>
 /// <param name="RoleTypeUpdate">True if this update has changed the mind's role type</param>
-/// /// <param name="Silent">If true, Job greeting/intro will not be sent to the player's chat</param>
+/// <param name="Silent">If true, Job greeting/intro will not be sent to the player's chat</param>
 public sealed record RoleAddedEvent(EntityUid MindId, MindComponent Mind, bool RoleTypeUpdate, bool Silent = false) : RoleEvent(MindId, Mind, RoleTypeUpdate);
