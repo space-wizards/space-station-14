7b2b1a10ce420cb577461d63ca880649251d4f7c
PJB3005 <pieterjan.briers+git@gmail.com>
2024-12-09 00:29:24 +0100

    Drop SVG files from client package.
    Engine can't load SVG files, they're only in the repo as a source asset. Don't ship them to clients.

--- Content.Packaging/ClientPackaging.cs
+++ Content.Packaging/ClientPackaging.cs
@@ -61,7 +61,13 @@ public static class ClientPackaging
         var graph = new RobustClientAssetGraph();
         pass.Dependencies.Add(new AssetPassDependency(graph.Output.Name));
 
-        AssetGraph.CalculateGraph(graph.AllPasses.Append(pass).ToArray(), logger);
+        var dropSvgPass = new AssetPassFilterDrop(f => f.Path.EndsWith(".svg"))
+        {
+            Name = "DropSvgPass",
+        };
+        dropSvgPass.AddDependency(graph.Input).AddBefore(graph.PresetPasses);
+
+        AssetGraph.CalculateGraph([pass, dropSvgPass, ..graph.AllPasses], logger);
 
         var inputPass = graph.Input;
 
