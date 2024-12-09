4d37fefbf83067909bda0994881675967a6b6d76
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2024-12-09 12:47:24 -0800

    Create laddersystem.cs

---
+++ Content.Shared/Ladder/laddersystem.cs
@@ -0,0 +1,82 @@
+﻿using Content.Shared.ActionBlocker;
+using Content.Shared.Buckle.Components;
+using Content.Shared.Movement.Events;
+using Content.Shared.StepTrigger.Systems;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Network;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Timing;
+
+namespace Content.Shared.Ladder;
+
+/// <summary>
+///     Handles making entities fall into chasms when stepped on.
+/// </summary>
+public sealed class ChasmSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly ActionBlockerSystem _blocker = default!;
+    [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<LadderdownComponent, StepTriggeredOffEvent>(OnStepTriggeredDown);
+        SubscribeLocalEvent<LadderupComponent, StepTriggeredOffEvent>(OnStepTriggeredUp);
+        SubscribeLocalEvent<LadderdownComponent, StepTriggerAttemptEvent>(OnStepTriggerAttemptDown);
+        SubscribeLocalEvent<LadderupComponent, StepTriggerAttemptEvent>(OnStepTriggerAttemptUp);
+    }
+
+    public override void Update(float frameTime)
+    {
+    }
+
+    private void OnStepTriggeredDown(EntityUid owner, LadderdownComponent component, ref StepTriggeredOffEvent args)
+    {
+       if (HasComp<GhostComponent>(owner))
+           {
+            return;
+           }
+
+          if (HasComp<HandsComponent>(owner))
+            {
+           var destination = EntityManager.EntityQuery<LadderupComponent>().FirstOrDefault();
+            if (destination != null)
+            {
+                Transform(owner).Coordinates = Transform(destination.Owner).Coordinates;
+            }
+            }
+    }
+
+     private void OnStepTriggeredUp(EntityUid uid, LadderupComponent component, ref StepTriggeredOffEvent args)
+    {
+       if (HasComp<GhostComponent>(owner))
+         {
+            return;
+         }
+
+        if (HasComp<HandsComponent>(owner))
+            {
+       var destination = EntityManager.EntityQuery<LadderdownComponent>().FirstOrDefault();
+            if (destination != null)
+            {
+                Transform(owner).Coordinates = Transform(destination.Owner).Coordinates;
+            }
+            }
+    }
+
+
+    private void OnStepTriggerAttemptDown(EntityUid uid, LadderdownComponent component, ref StepTriggerAttemptEvent args)
+    {
+        args.Continue = true;
+    }
+
+     private void OnStepTriggerAttemptUp(EntityUid uid, LadderupComponent component, ref StepTriggerAttemptEvent args)
+    {
+        args.Continue = true;
+    }
+    
+}
