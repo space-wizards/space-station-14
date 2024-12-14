7ca22003e9b3951ad95e931f70e1500beb41efd4
fqqf <xzx._@outlook.com>
2024-12-15 01:56:50 +0300

    lol

---
+++ Content.Server/CloudEmotes/Commands/EmoteCommand.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using JetBrains.Annotations;
+using Robust.Shared.Console;
+using Content.Shared.Administration;
+using Robust.Shared.Map;
+
+namespace Content.Server.CloudEmotes.Commands
+{
+    [UsedImplicitly]
+    [AnyCommand]
+    public sealed class EmoteCommand : LocalizedCommands
+    {
+        [Dependency] private readonly IEntitySystemManager _entitySystems = default!;
+        [Dependency] private readonly IEntityManager _entityManager = default!;
+        protected readonly SharedTransformSystem _transformSystem;
+        public override string Command => "emote";
+        public string[] emotes = { "lenny", "mark", "nervous" };
+
+        public EmoteCommand()
+        {
+            _transformSystem = _entityManager.System<SharedTransformSystem>();
+        }
+        public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+        {
+            return args.Length switch
+            {
+                1 => CompletionResult.FromHintOptions(emotes,
+                    LocalizationManager.GetString("cmd-emote-hint-1")),
+                _ => CompletionResult.Empty,
+            };
+        }
+
+        public override void Execute(IConsoleShell shell, string argStr, string[] args)
+        {
+            if (args.Length != 1)
+            {
+                shell.WriteError(LocalizationManager.GetString("shell-wrong-arguments-number"));
+                return;
+            }
+
+            var player = shell.Player;
+            if (player?.AttachedEntity == null)
+            {
+                shell.WriteLine(LocalizationManager.GetString("shell-only-players-can-run-this-command"));
+                return;
+            }
+
+            var emote = args[0];
+            if (!emotes.Contains(emote))
+            {
+                shell.WriteLine(LocalizationManager.GetString("cmd-emote-invalid-emote"));
+                return;
+            }
+            // TODO THIS SHOULD ADD COMPONENT, AND THEN CLIENT SYSTEM WILL DRAW IT BASED ON COMPONENTS (AND WILL KEEP IT UPDATED ON NEW LOCATION DUE TO IT BEING IN COMP SAVED LIKE TETHER MOVING)
+            var emote_name = args[0] = "CloudEmote" + char.ToUpper(args[0][0]) + args[0].Substring(1);
+            var pos = _transformSystem.GetMapCoordinates(player.AttachedEntity.Value);
+            var emote_entity = _entityManager.Spawn(emote_name, pos);
+            _transformSystem.SetCoordinates(emote_entity, new EntityCoordinates(player.AttachedEntity.Value, new System.Numerics.Vector2(0f, 1f)));
+        }
+    }
+}
---
+++ Resources/Locale/en-US/_Starlight/cloud-emotes/cloud-emotes.ftl
@@ -0,0 +1,5 @@
+cmd-emote-desc = Plays emote
+cmd-emote-help = Usage: emote <emote>
+cmd-emote-invalid-emote = This emote doesn't exist.
+
+cmd-emote-hint-1 = <emote>
