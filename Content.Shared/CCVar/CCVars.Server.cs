3d1cbe05083e058ed9e4c61b957764ff2e651a7d
Errant <35878406+Errant-4@users.noreply.github.com>
2024-12-15 10:13:48 +0100

    comment format fix
    Co-authored-by: Thomas <87614336+Aeshus@users.noreply.github.com>

--- Content.Shared/CCVar/CCVars.Server.cs
+++ Content.Shared/CCVar/CCVars.Server.cs
@@ -48,9 +48,9 @@ public sealed partial class CCVars
     public static readonly CVarDef<string> ServerLobbyName =
         CVarDef.Create("server.lobby_name", "", CVar.REPLICATED | CVar.SERVER);
 
-    // <summary>
-    //  The width of the right side (chat) panel in the lobby
-    // </summary>
+    /// <summary>
+    ///     The width of the right side (chat) panel in the lobby
+    /// </summary>
     public static readonly CVarDef<int> ServerLobbyRightPanelWidth =
         CVarDef.Create("server.lobby_right_panel_width", 650, CVar.REPLICATED | CVar.SERVER);
 }
