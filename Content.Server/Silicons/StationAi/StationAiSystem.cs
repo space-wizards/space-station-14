ca78d3c18eb550eb0760f12e44fba2ab20eae6c3
ScarKy0 <scarky0@onet.eu>
2024-12-18 19:39:32 +0100

    Revert "sigh"
    This reverts commit b03c3db61749b12e9fd8479cfeaf9198fd22a6be.

--- Content.Server/Silicons/StationAi/StationAiSystem.cs
+++ Content.Server/Silicons/StationAi/StationAiSystem.cs
@@ -4,6 +4,7 @@ using Content.Server.Spawners.Components;
 using Content.Server.Station.Components;
 using Content.Server.Station.Systems;
 using Content.Server.Chat.Systems;
+using Content.Server.Chat.Systems.ChatSystem;
 using Content.Shared.Chat;
 using Content.Shared.Mind;
 using Content.Shared.Roles;
@@ -13,7 +14,6 @@ using Robust.Shared.Audio;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
-using static Content.Server.Chat.Systems.ChatSystem;
 
 namespace Content.Server.Silicons.StationAi;
 
