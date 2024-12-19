7a5aad00db8a4d800d2d837b4c234545de084962
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-12-19 18:21:57 +0300

    fix lines offset

--- Content.Client/StationTeleporter/StationTeleporterNavMapControl.cs
+++ Content.Client/StationTeleporter/StationTeleporterNavMapControl.cs
@@ -30,10 +30,10 @@ public sealed partial class StationTeleporterNavMapControl : NavMapControl
             if (_xform is null)
                 continue;
 
-            var pos1 = Vector2.Transform(link.Item1, _transformSystem.GetInvWorldMatrix(_xform)) - Offset;
+            var pos1 = Vector2.Transform(link.Item1, _transformSystem.GetInvWorldMatrix(_xform)) - GetOffset();
             pos1 = ScalePosition(new Vector2(pos1.X, -pos1.Y));
 
-            var pos2 = Vector2.Transform(link.Item2, _transformSystem.GetInvWorldMatrix(_xform)) - Offset;
+            var pos2 = Vector2.Transform(link.Item2, _transformSystem.GetInvWorldMatrix(_xform)) - GetOffset();
             pos2 = ScalePosition(new Vector2(pos2.X, -pos2.Y));
 
             handle.DrawLine(pos1, pos2, Color.Aqua);
