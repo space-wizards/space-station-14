ede1f3aa087d2793e7361534a12c87c38fd1cc22
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2024-12-20 09:44:51 -0800

    Update PlanktonScannerComponent.cs

--- Content.Server/Planktonics/PlanktonScannerComponent.cs
+++ Content.Server/Planktonics/PlanktonScannerComponent.cs
@@ -9,4 +9,7 @@ public sealed partial class PlanktonScannerComponent : Component
 
     [DataField]
     public SoundSpecifier PrintSound = new SoundPathSpecifier("/Audio/Machines/printer.ogg");
+
+    [DataField("planktonRewardEntityId", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string PlanktonRewardEntityId = "ResearchDisk5000";
 }
