09bae7841b0f32952623602d2c8a729403987964
lzk <124214523+lzk228@users.noreply.github.com>
2024-12-22 03:25:25 +0100

    Allow to paint multiple airlocks

--- Content.Shared/SprayPainter/SharedSprayPainterSystem.cs
+++ Content.Shared/SprayPainter/SharedSprayPainterSystem.cs
@@ -60,8 +60,6 @@ public abstract class SharedSprayPainterSystem : EntitySystem
 
     private void OnDoorDoAfter(Entity<SprayPainterComponent> ent, ref SprayPainterDoorDoAfterEvent args)
     {
-        ent.Comp.AirlockDoAfter = null;
-
         if (args.Handled || args.Cancelled)
             return;
 
@@ -116,9 +114,6 @@ public abstract class SharedSprayPainterSystem : EntitySystem
         if (args.Handled)
             return;
 
-        if (!TryComp<SprayPainterComponent>(args.Used, out var painter) || painter.AirlockDoAfter != null)
-            return;
-
         var group = Proto.Index<AirlockGroupPrototype>(ent.Comp.Group);
 
         var style = Styles[painter.Index];
@@ -138,9 +133,6 @@ public abstract class SharedSprayPainterSystem : EntitySystem
         if (!DoAfter.TryStartDoAfter(doAfterEventArgs, out var id))
             return;
 
-        // since we are now spraying an airlock prevent spraying more at the same time
-        // pipes ignore this
-        painter.AirlockDoAfter = id;
         args.Handled = true;
 
         // Log the attempt
