f3974aa0b6e8e1bb41434108f2d1e19408e5978f
TheGrimbeeper <thegrimbeeper.11@gmail.com>
2024-12-25 14:57:11 +1100

    New DNA scrambling artifact effect
    New DNA scrambling artifact effect based off of DNA scrambler implant

---
+++ Content.Server/_Impstation/Xenoarchaeology/XenoArtifacts/Effects/Components/ScrambleDNAArtifactComponent.cs
@@ -0,0 +1,34 @@
+﻿namespace Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
+
+/// <summary>
+/// Scrambles DNA of some number of people within some range, possibly outside of their species
+/// </summary>
+[RegisterComponent]
+public sealed partial class ScrambleDNAArtifactComponent : Component
+{
+    /// <summary>
+    /// Distance from the artifact find people to scramble DNA
+    /// </summary>
+    [DataField("range"), ViewVariables(VVAccess.ReadWrite)]
+    public float Range = 6f;
+
+    /// <summary>
+    /// Number of people to DNA scramble
+    /// </summary>
+    [DataField("count"), ViewVariables(VVAccess.ReadWrite)]
+    public int Count = 1;
+
+    /// <summary>
+    /// Force scrambling to respect initial species
+    /// </summary>
+    [DataField("withinSpecies"), ViewVariables(VVAccess.ReadWrite)]
+    public bool WithinSpecies = true;
+
+    /// <summary>
+    /// if WithinSpecies is false, what species cannot be rolled
+    /// </summary>
+    [DataField("excludedSpecies"), ViewVariables(VVAccess.ReadWrite)]
+    public HashSet<string>? ExcludedSpecies = null;
+
+
+}
