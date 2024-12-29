9460324c83f65b7e5bdca612ee8c9cecba5bcb07
Errant <35878406+Errant-4@users.noreply.github.com>
2024-12-29 22:36:11 +0100

    update RoleEvent documentation

--- Content.Shared/Roles/RoleEvent.cs
+++ Content.Shared/Roles/RoleEvent.cs
@@ -7,5 +7,5 @@ namespace Content.Shared.Roles;
 /// </summary>
 /// <param name="MindId">The mind id associated with the player.</param>
 /// <param name="Mind">The mind component associated with the mind id.</param>
-/// <param name="Antagonist">Whether or not the role makes the player an antagonist.</param>
+/// <param name="RoleTypeUpdate">True if this update has changed the mind's role type</param>
 public abstract record RoleEvent(EntityUid MindId, MindComponent Mind, bool RoleTypeUpdate);
