7293105f0ba5b858ea0f79289ceea50bfa9481b0
dylanhunter <dylan2.whittingham@live.uwe.ac.uk>
2024-12-29 19:47:32 +0000

    added basic admin logs for PDA notekeeper notes

--- Content.Server/CartridgeLoader/Cartridges/NotekeeperCartridgeSystem.cs
+++ Content.Server/CartridgeLoader/Cartridges/NotekeeperCartridgeSystem.cs
@@ -1,12 +1,14 @@
+using Content.Server.Administration.Logs;
 using Content.Shared.CartridgeLoader;
 using Content.Shared.CartridgeLoader.Cartridges;
+using Content.Shared.Database;
 
 namespace Content.Server.CartridgeLoader.Cartridges;
 
 public sealed class NotekeeperCartridgeSystem : EntitySystem
 {
     [Dependency] private readonly CartridgeLoaderSystem? _cartridgeLoaderSystem = default!;
-
+    [Dependency] private readonly IAdminLogManager _adminLogger = default!;
     public override void Initialize()
     {
         base.Initialize();
@@ -36,10 +38,14 @@ public sealed class NotekeeperCartridgeSystem : EntitySystem
         if (message.Action == NotekeeperUiAction.Add)
         {
             component.Notes.Add(message.Note);
+            _adminLogger.Add(LogType.AdminMessage, LogImpact.Low,
+                $"{ToPrettyString(args.Actor)} added a note to PDA: '{message.Note}' contained on: {ToPrettyString(uid)}");
         }
         else
         {
             component.Notes.Remove(message.Note);
+            _adminLogger.Add(LogType.AdminMessage, LogImpact.Low,
+                $"{ToPrettyString(args.Actor)} removed a note from PDA: '{message.Note}' was contained on: {ToPrettyString(uid)}");
         }
 
         UpdateUiState(uid, GetEntity(args.LoaderUid), component);
