8bd60e1219200755b5fd99feba1910682928d277
DylanWhittingham <DylanWhittingham@outlook.com>
2024-12-30 19:35:52 +0000

    added new LogType 'PdaInteract' and changed PDA notekeeper logs to it

--- Content.Server/CartridgeLoader/Cartridges/NotekeeperCartridgeSystem.cs
+++ Content.Server/CartridgeLoader/Cartridges/NotekeeperCartridgeSystem.cs
@@ -39,13 +39,13 @@ public sealed class NotekeeperCartridgeSystem : EntitySystem
         if (message.Action == NotekeeperUiAction.Add)
         {
             component.Notes.Add(message.Note);
-            _adminLogger.Add(LogType.AdminMessage, LogImpact.Low,
+            _adminLogger.Add(LogType.PdaInteract, LogImpact.Low,
                 $"{ToPrettyString(args.Actor)} added a note to PDA: '{message.Note}' contained on: {ToPrettyString(uid)}");
         }
         else
         {
             component.Notes.Remove(message.Note);
-            _adminLogger.Add(LogType.AdminMessage, LogImpact.Low,
+            _adminLogger.Add(LogType.PdaInteract, LogImpact.Low,
                 $"{ToPrettyString(args.Actor)} removed a note from PDA: '{message.Note}' was contained on: {ToPrettyString(uid)}");
         }
 
