2adb40483459b24691b49652ee429c784c3edb8e
Booblesnoot42 <108703193+Booblesnoot42@users.noreply.github.com>
2024-12-30 05:58:20 -0500

    Created blacklist marker component and added it to the AirlockExternal prototype

---
+++ Content.Shared/Doors/Components/ExternalAccessMarkerComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Doors.Components;
+
+/// <summary>
+/// Marks a door as having access to space.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ExternalAccessMarkerComponent : Component
+{
+
+}
