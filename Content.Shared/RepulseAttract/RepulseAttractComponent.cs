caad2a595ce45dce57e04efeb782f1bc74491c39
keronshb <keronshb@live.com>
2024-12-31 20:23:39 -0500

    Adds Repulse Attract ECS

---
+++ Content.Shared/RepulseAttract/RepulseAttractComponent.cs
@@ -0,0 +1,43 @@
+﻿using Content.Shared.Whitelist;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.RepulseAttract;
+
+/// <summary>
+///     Used to repulse or attract entities away from the entity this is on
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState, Access(typeof(RepulseAttractSystem))]
+public sealed partial class RepulseAttractComponent : Component
+{
+    /// <summary>
+    ///     Attracts if true, Repulse if false.
+    /// </summary>
+    [DataField]
+    public bool Attract;
+
+    /// <summary>
+    ///     How strong should the Repulse/Attract be?
+    /// </summary>
+    [DataField]
+    public float Strength = 1.0F;
+
+    /// <summary>
+    ///     How close do the entities need to be?
+    /// </summary>
+    [DataField]
+    public float Range = 1.0F;
+
+    /// <summary>
+    ///     What kind of entities should this effect only?
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public EntityWhitelist? Whitelist;
+
+    /// <summary>
+    ///     What kind of entities should be excluded from the effect?
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public EntityWhitelist? Blacklist;
+
+    // TODO: Cooldown?
+}
---
+++ Content.Shared/RepulseAttract/RepulseAttractSystem.cs
@@ -0,0 +1,60 @@
+﻿using Content.Shared.Ghost;
+using Content.Shared.Interaction;
+using Content.Shared.Throwing;
+using Content.Shared.Whitelist;
+
+namespace Content.Shared.RepulseAttract;
+
+public sealed class RepulseAttractSystem : EntitySystem
+{
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly ThrowingSystem _throw = default!;
+    [Dependency] private readonly EntityWhitelistSystem _whitelist = default!;
+    [Dependency] private readonly SharedTransformSystem _xForm = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<RepulseAttractComponent, ActivateInWorldEvent>(OnActivate);
+    }
+
+    // TODO: Internal Cooldown should be added if no usedelay
+
+    private void OnActivate(Entity<RepulseAttractComponent> ent, ref ActivateInWorldEvent args)
+    {
+        RepulseAttract(args.User, ent.Comp.Range, ent.Comp.Strength, ent.Comp.Attract, ent.Comp.Whitelist, ent.Comp.Blacklist);
+    }
+
+    // TODO: Backend code - entities in rage, check lists, do strength calculations
+    // TODO: Public API (use system to call, can be re-used in things like grav anom)
+
+    public void RepulseAttract(EntityUid caster, float range, float strength, bool attract, EntityWhitelist? whitelist = null, EntityWhitelist? blacklist = null)
+    {
+        var xForm = Transform(caster);
+
+        // Use lookup and range to check entities in range
+        var entsInRange = _lookup.GetEntitiesInRange(caster, range);
+
+        foreach (var target in entsInRange)
+        {
+            // Check white/black lists
+            if (_whitelist.IsWhitelistFail(whitelist, target) || _whitelist.IsBlacklistFail(blacklist, target))
+                continue;
+
+            var targetXForm = Transform(target);
+
+            if (HasComp<GhostComponent>(target) || targetXForm.Anchored)
+                continue;
+
+            var userWorldPos = _xForm.GetWorldPosition(xForm);
+            var targetWorldPos = _xForm.GetWorldPosition(target);
+
+            var direction = targetWorldPos - userWorldPos;
+
+            if (attract)
+                direction = userWorldPos - targetWorldPos;
+
+            _throw.TryThrow(target, direction, strength, doSpin: true);
+        }
+    }
+}
