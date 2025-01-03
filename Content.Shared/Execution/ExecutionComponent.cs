af93b3d7f107565723249f75a7f636f14431d2bb
Rinary <rinary.super@gmail.com>
2025-01-03 16:45:10 +0200

    execution system update

--- Content.Shared/Execution/ExecutionComponent.cs
+++ Content.Shared/Execution/ExecutionComponent.cs
@@ -5,73 +5,88 @@ namespace Content.Shared.Execution;
 /// <summary>
 /// Added to entities that can be used to execute another target.
 /// </summary>
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[RegisterComponent]
 public sealed partial class ExecutionComponent : Component
 {
     /// <summary>
-    /// How long the execution duration lasts.
+    /// How long the melee execution duration lasts.
     /// </summary>
-    [DataField, AutoNetworkedField]
-    public float DoAfterDuration = 5f;
+    [DataField]
+    public static float MeleeDoAfterDuration = 5f;
+    
+    /// <summary>
+    /// How long the gun execution duration lasts.
+    /// </summary>
+    [DataField]
+    public static float GunDoAfterDuration = 6f;
 
     /// <summary>
     /// Arbitrarily chosen number to multiply damage by, used to deal reasonable amounts of damage to a victim of an execution.
     /// /// </summary>
-    [DataField, AutoNetworkedField]
-    public float DamageMultiplier = 9f;
+    [DataField]
+    public static float DamageMultiplier = 9f;
 
     /// <summary>
     /// Shown to the person performing the melee execution (attacker) upon starting a melee execution.
     /// </summary>
     [DataField]
-    public LocId InternalMeleeExecutionMessage = "execution-popup-melee-initial-internal";
+    public static LocId InternalMeleeExecutionMessage = "execution-popup-melee-initial-internal";
+    [DataField]
+    public static LocId InternalGunExecutionMessage = "execution-popup-gun-initial-internal";
 
     /// <summary>
     /// Shown to bystanders and the victim of a melee execution when a melee execution is started.
     /// </summary>
     [DataField]
-    public LocId ExternalMeleeExecutionMessage = "execution-popup-melee-initial-external";
+    public static LocId ExternalMeleeExecutionMessage = "execution-popup-melee-initial-external";
+    [DataField]
+    public static LocId ExternalGunExecutionMessage = "execution-popup-gun-initial-external";
 
     /// <summary>
     /// Shown to the attacker upon completion of a melee execution.
     /// </summary>
     [DataField]
-    public LocId CompleteInternalMeleeExecutionMessage = "execution-popup-melee-complete-internal";
+    public static LocId CompleteInternalMeleeExecutionMessage = "execution-popup-melee-complete-internal";
+    [DataField]
+    public static LocId CompleteInternalGunExecutionMessage = "execution-popup-gun-complete-internal";
 
     /// <summary>
     /// Shown to bystanders and the victim of a melee execution when a melee execution is completed.
     /// </summary>
     [DataField]
-    public LocId CompleteExternalMeleeExecutionMessage = "execution-popup-melee-complete-external";
+    public static LocId CompleteExternalMeleeExecutionMessage = "execution-popup-melee-complete-external";
+    [DataField]
+    public static LocId CompleteExternalGunExecutionMessage = "execution-popup-gun-complete-external";
 
     /// <summary>
     /// Shown to the person performing the self execution when starting one.
     /// </summary>
     [DataField]
-    public LocId InternalSelfExecutionMessage = "execution-popup-self-initial-internal";
+    public static LocId InternalSelfMeleeExecutionMessage = "execution-popup-self-melee-initial-internal";
+    [DataField]
+    public static LocId InternalSelfGunExecutionMessage = "execution-popup-self-gun-initial-internal";
 
     /// <summary>
     /// Shown to bystanders near a self execution when one is started.
     /// </summary>
     [DataField]
-    public LocId ExternalSelfExecutionMessage = "execution-popup-self-initial-external";
+    public static LocId ExternalSelfMeleeExecutionMessage = "execution-popup-self-melee-initial-external";
+    [DataField]
+    public static LocId ExternalSelfGunExecutionMessage = "execution-popup-self-gun-initial-external";
 
     /// <summary>
     /// Shown to the person performing a self execution upon completion of a do-after or on use of /suicide with a weapon that has the Execution component.
     /// </summary>
     [DataField]
-    public LocId CompleteInternalSelfExecutionMessage = "execution-popup-self-complete-internal";
+    public static LocId CompleteInternalSelfMeleeExecutionMessage = "execution-popup-self-melee-complete-internal";
+    [DataField]
+    public static LocId CompleteInternalSelfGunExecutionMessage = "execution-popup-self-gun-complete-internal";
 
     /// <summary>
     /// Shown to bystanders when a self execution is completed or a suicide via execution weapon happens nearby.
     /// </summary>
     [DataField]
-    public LocId CompleteExternalSelfExecutionMessage = "execution-popup-self-complete-external";
-
-    // Not networked because this is transient inside of a tick.
-    /// <summary>
-    /// True if it is currently executing for handlers.
-    /// </summary>
+    public static LocId CompleteExternalSelfMeleeExecutionMessage = "execution-popup-self-melee-complete-external";
     [DataField]
-    public bool Executing = false;
+    public static LocId CompleteExternalSelfGunExecutionMessage = "execution-popup-self-gun-complete-external";
 }
