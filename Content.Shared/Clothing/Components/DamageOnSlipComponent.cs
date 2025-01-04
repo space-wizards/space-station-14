295e26b08db2ae4877fe8564f56822d1aaf8a3ec
Preston Smith <blackfoot03@outlook.com>
2025-01-04 15:12:31 -0600

    DamageOnSlip and destructible

---
+++ Content.Shared/Clothing/Components/DamageOnSlipComponent.cs
@@ -0,0 +1,37 @@
+﻿using Content.Shared.Damage;
+
+namespace Content.Shared.Clothing.Components;
+
+/// <summary>
+/// A component to give clothing a chance to be damaged when slipping
+/// </summary>
+/// <remarks>
+/// Pairs best with a `Destructible` component
+/// </remarks>
+[RegisterComponent]
+public sealed partial class DamageOnSlipComponent : Component
+{
+    /// <summary>
+    /// Chance the clothing will be damaged when slipped
+    /// </summary>
+    [DataField]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float DamageChance = 0.01f;
+
+    /// <summary>
+    /// Damage per instance of unlucky slip
+    /// </summary>
+    [DataField(required: true)]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public DamageSpecifier Damage;
+
+    /// <summary>
+    /// Damage multiplier maximum
+    /// </summary>
+    /// <remarks>
+    /// Will multiply the damage specifier by a random integer from 1 to maximum (non inclusive)
+    /// </remarks>
+    [DataField]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public int? MultiplierMax;
+}
