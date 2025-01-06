2bb37460e86437cabec4fd8efafc7318a8ea070c
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-01-06 13:07:08 -0800

    Create OnSweetwater.cs

---
+++ Content.Shared/Random/Rules/OnSweetwater.cs
@@ -0,0 +1,17 @@
+namespace Content.Shared.Random.Rules;
+
+/// <summary>
+/// Returns true if on Sweetwater.
+/// </summary>
+public sealed partial class OnSweetwaterRule : RulesRule
+{
+    public override bool Check(EntityManager entManager, EntityUid uid)
+    {
+        if (entManager.TryGetComponent(uid, out SweetwaterComponent? xform)
+        {
+            return !Inverted;
+        }
+
+        return Inverted;
+    }
+}
