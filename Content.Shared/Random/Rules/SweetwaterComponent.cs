998877ba2ea79abb2dec47fff2160cf53b611fd8
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-01-06 13:08:14 -0800

    Create SweetwaterComponent.cs

---
+++ Content.Shared/Random/Rules/SweetwaterComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Random.Rules;
+
+/// <summary>
+/// IDs sweetwater for ambient sound effects
+/// </summary>
+[RegisterComponent]
+public sealed partial class SweetwaterComponent : Component
+{
+
+}
