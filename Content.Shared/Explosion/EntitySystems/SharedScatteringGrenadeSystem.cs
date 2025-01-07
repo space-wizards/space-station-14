813fe5eef0cf088247d6aeb99a314456528c2c66
Tayrtahn <tayrtahn@gmail.com>
2025-01-07 17:57:09 -0500

    Limit maximum capacity of fillable cluster grenades

--- Content.Shared/Explosion/EntitySystems/SharedScatteringGrenadeSystem.cs
+++ Content.Shared/Explosion/EntitySystems/SharedScatteringGrenadeSystem.cs
@@ -49,6 +49,10 @@ public abstract class SharedScatteringGrenadeSystem : EntitySystem
         if (entity.Comp.Whitelist == null)
             return;
 
+        // Make sure there's room for another grenade to be added
+        if (GrenadeCount(entity) >= entity.Comp.Capacity)
+            return;
+
         if (args.Handled || !_whitelistSystem.IsValid(entity.Comp.Whitelist, args.Used))
             return;
 
@@ -65,6 +69,14 @@ public abstract class SharedScatteringGrenadeSystem : EntitySystem
         if (!TryComp<AppearanceComponent>(entity, out var appearanceComponent))
             return;
 
-        _appearance.SetData(entity, ClusterGrenadeVisuals.GrenadesCounter, entity.Comp.UnspawnedCount + entity.Comp.Container.ContainedEntities.Count, appearanceComponent);
+        _appearance.SetData(entity, ClusterGrenadeVisuals.GrenadesCounter, GrenadeCount(entity), appearanceComponent);
+    }
+
+    /// <summary>
+    /// Returns the number of grenades currently contained in the cluster (both spawned and unspawned)
+    /// </summary>
+    public int GrenadeCount(Entity<ScatteringGrenadeComponent> entity)
+    {
+        return entity.Comp.UnspawnedCount + entity.Comp.Container.ContainedEntities.Count;
     }
 }
