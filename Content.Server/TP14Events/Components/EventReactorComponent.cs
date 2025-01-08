0eda06e66282939542fe24258303d76218039333
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-01-08 10:23:22 -0800

    Create EventReactorComponent.cs

---
+++ Content.Server/TP14Events/Components/EventReactorComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace Content.Server.Event.Components;
+
+/// <summary>
+/// Flickers all the lights within a certain radius.
+/// </summary>
+[RegisterComponent]
+public sealed partial class EventReactorComponent : Component
+{
+    /// <summary>
+    /// Lights within this radius will be flickered on activation
+    /// </summary>
+    [DataField("radius")]
+    public float Radius = 9999;
+
+    /// <summary>
+    /// The chance that the light will flicker
+    /// </summary>
+    [DataField("flickerChance")]
+    public float FlickerChance = 1f;
+}
