3a8c92e8aa2cad26bfef1519e0c9cea72edf92f2
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-01-09 13:21:37 -0800

    Update DeathWhaleComponent.cs

--- Content.Shared/Deathwhales/DeathWhaleComponent.cs
+++ Content.Shared/Deathwhales/DeathWhaleComponent.cs
@@ -3,6 +3,9 @@ namespace Content.Server.Deathwhale
     [RegisterComponent]
     public sealed partial class DeathWhaleComponent : Component
     {
+
+         [DataField("radius")]
+         public float Radius = 50;
         
     }
 }
