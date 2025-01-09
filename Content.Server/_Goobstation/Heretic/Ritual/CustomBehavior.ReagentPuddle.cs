1ffaadbd04f4757a57e2cf3d4d41677e247b70ae
Kandiyaki <andrewepperson8@gmail.com>
2025-01-09 13:21:52 -0500

    flesh rituals that require blood now accept blood from all playable species

--- Content.Server/_Goobstation/Heretic/Ritual/CustomBehavior.ReagentPuddle.cs
+++ Content.Server/_Goobstation/Heretic/Ritual/CustomBehavior.ReagentPuddle.cs
@@ -3,6 +3,7 @@ using Content.Shared.Chemistry.Reagent;
 using Content.Shared.Fluids.Components;
 using Content.Shared.Heretic.Prototypes;
 using Robust.Shared.Prototypes;
+using System.Linq;
 
 namespace Content.Server.Heretic.Ritual;
 
@@ -10,44 +11,58 @@ public sealed partial class RitualReagentPuddleBehavior : RitualCustomBehavior
 {
     protected EntityLookupSystem _lookup = default!;
 
-    [DataField] public ProtoId<ReagentPrototype>? Reagent;
+    [DataField] public List<ProtoId<ReagentPrototype>>? Reagents;
 
     private List<EntityUid> uids = new();
 
     public override bool Execute(RitualData args, out string? outstr)
     {
-        outstr = null;
-
-        if (Reagent == null)
-            return true;
-
-        _lookup = args.EntityManager.System<EntityLookupSystem>();
-
-        var lookup = _lookup.GetEntitiesInRange(args.Platform, .75f);
-
-        foreach (var ent in lookup)
+        if (Reagents == null)
         {
-            if (!args.EntityManager.TryGetComponent<PuddleComponent>(ent, out var puddle))
-                continue;
-
-            if (puddle.Solution == null)
-                continue;
-
-            var soln = puddle.Solution.Value;
-
-            if (!soln.Comp.Solution.ContainsPrototype(Reagent))
-                continue;
-
-            uids.Add(ent);
-        }
-
-        if (uids.Count == 0)
-        {
-            outstr = Loc.GetString("heretic-ritual-fail-reagentpuddle", ("reagentname", Reagent!));
+            //should only happen if someone fucked up their ritual yaml
+            outstr = Loc.GetString("heretic-ritual-unknown");
             return false;
         }
+        string reagStrings = "";
 
-        return true;
+        foreach(var Reagent in Reagents)
+        {
+            reagStrings += (Reagent.Id + ", ");
+
+            outstr = null;
+            _lookup = args.EntityManager.System<EntityLookupSystem>();
+
+            var lookup = _lookup.GetEntitiesInRange(args.Platform, .75f);
+
+            foreach (var ent in lookup)
+            {
+                if (!args.EntityManager.TryGetComponent<PuddleComponent>(ent, out var puddle))
+                    continue;
+
+                if (puddle.Solution == null)
+                    continue;
+
+                var soln = puddle.Solution.Value;
+
+                if (!soln.Comp.Solution.ContainsPrototype(Reagent))
+                    continue;
+
+                uids.Add(ent);
+            }
+
+            if (uids.Count == 0)
+            {
+                continue;
+            }
+
+            return true;
+        }
+
+        //take off the comma + space on the end of the reagStrings
+        reagStrings = reagStrings.Substring(0, reagStrings.Length - 3);
+        outstr = Loc.GetString("heretic-ritual-fail-reagentpuddle", ("reagentname", reagStrings));
+        return false;
+
     }
 
     public override void Finalize(RitualData args)
