c36e47b66510a81fc3c23789a323b2bee3e8e854
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-01-10 10:02:38 -0800

    Update MassHallucinationsRule.cs

--- Content.Server/StationEvents/Events/MassHallucinationsRule.cs
+++ Content.Server/StationEvents/Events/MassHallucinationsRule.cs
@@ -21,11 +21,13 @@ public sealed class MassHallucinationsRule : StationEventSystem<MassHallucinatio
 
         if (component.sweetwaterOnly)
         {
-             foreach (var grid in _mapManager.GetAllGrids(playerMap.Value).OrderBy(o => o.Owner))
+            foreach (var grid in _mapManager.GetAllGrids().OrderBy(o => o.Owner))
             {
                 var map = grid.Owner;
                 if (TryComp(map, out SweetwaterComponent? gridXform))
-                ApplyOceanSound (map);
+                {
+                    ApplyOceanSound(map, component);
+                }
             }
         }
         else
@@ -44,9 +46,34 @@ public sealed class MassHallucinationsRule : StationEventSystem<MassHallucinatio
         }
     }
 
-    private void ApplyOceanSound(EntityUid map)
+    private void ApplyOceanSound(EntityUid map, MassHallucinationsRuleComponent component)
     {
-        
+        foreach (var ent in GetGridChildren(map))
+        {
+            if (HasComp<MindContainerComponent>(ent) && !HasComp<ParacusiaComponent>(ent))
+            {
+                EnsureComp<MassHallucinationsComponent>(ent);
+                var paracusia = EnsureComp<ParacusiaComponent>(ent);
+                _paracusia.SetSounds(ent, component.Sounds, paracusia);
+                _paracusia.SetTime(ent, component.MinTimeBetweenIncidents, component.MaxTimeBetweenIncidents, paracusia);
+                _paracusia.SetDistance(ent, component.MaxSoundDistance);
+            }
+        }
+    }
+
+    private IEnumerable<EntityUid> GetGridChildren(EntityUid target)
+    {
+        if (TryComp<StationDataComponent>(target, out var station))
+        {
+            foreach (var grid in station.Grids)
+            {
+                var enumerator = Transform(grid).ChildEnumerator;
+                while (enumerator.MoveNext(out var ent))
+                {
+                    yield return ent;
+                }
+            }
+        }
     }
 
     protected override void Ended(EntityUid uid, MassHallucinationsRuleComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
