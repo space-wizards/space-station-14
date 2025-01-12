bdd9f1ac353b0795c8c8c5beb786ad8436fedc06
hivehum <ketchupfaced@gmail.com>
2025-01-12 14:56:02 -0500

    dogtags

---
+++ Content.Server/_CD/Engraving/EngraveableComponent.cs
@@ -0,0 +1,32 @@
+﻿namespace Content.Server._CD.Engraving;
+
+/// <summary>
+///     Allows an items' description to be modified with an engraving
+/// </summary>
+[RegisterComponent, Access(typeof(EngraveableSystem))]
+public sealed partial class EngraveableComponent : Component
+{
+    /// <summary>
+    ///     Message given to user to notify them a message was sent
+    /// </summary>
+    [DataField]
+    public string EngravedMessage = string.Empty;
+
+    /// <summary>
+    ///     The inspect text to use when there is no engraving
+    /// </summary>
+    [DataField]
+    public LocId NoEngravingText = "engraving-dogtags-no-message";
+
+    /// <summary>
+    ///     The message to use when successfully engraving the item
+    /// </summary>
+    [DataField]
+    public LocId EngraveSuccessMessage = "engraving-dogtags-succeed";
+
+    /// <summary>
+    ///     The inspect text to use when there is an engraving. The message will be shown seperately afterwards.
+    /// </summary>
+    [DataField]
+    public LocId HasEngravingText = "engraving-dogtags-has-message";
+}
---
+++ Resources/Locale/en-US/_CD/engraving/engraving.ftl
@@ -0,0 +1,8 @@
+﻿engraving-verb-engrave = Engrave
+engraving-popup-ui-message = Description
+
+# imp edit - making this generic so engrave component is better usable in other contexts
+
+engraving-dogtags-no-message = It doesn't seem to have any kind of engraving.
+engraving-dogtags-has-message = It is engraved with a message that reads:{" "}
+engraving-dogtags-succeed = You successfully engrave your message.
