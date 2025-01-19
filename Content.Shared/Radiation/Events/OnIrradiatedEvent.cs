378f67fc5aa07313daf84940c2e57fbcba511df2
pa.pecherskij <pa.pecherskij@interfax.ru>
2025-01-19 16:51:10 +0300

    fix: removed OnIrradiatedEvent duplicate c-tor

--- Content.Shared/Radiation/Events/OnIrradiatedEvent.cs
+++ Content.Shared/Radiation/Events/OnIrradiatedEvent.cs
@@ -4,20 +4,13 @@ namespace Content.Shared.Radiation.Events;
 ///     Raised on entity when it was irradiated
 ///     by some radiation source.
 /// </summary>
-public readonly record struct OnIrradiatedEvent(float FrameTime, float RadsPerSecond)
+public readonly record struct OnIrradiatedEvent(float FrameTime, float RadsPerSecond, EntityUid Origin)
 {
     public readonly float FrameTime = FrameTime;
 
     public readonly float RadsPerSecond = RadsPerSecond;
 
-    public readonly EntityUid Origin;
+    public readonly EntityUid Origin = Origin;
 
     public float TotalRads => RadsPerSecond * FrameTime;
-
-    public OnIrradiatedEvent(float frameTime, float radsPerSecond, EntityUid origin)
-    {
-        FrameTime = frameTime;
-        RadsPerSecond = radsPerSecond;
-        Origin = origin;
-    }
 }
