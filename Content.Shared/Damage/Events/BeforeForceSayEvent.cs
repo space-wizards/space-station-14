ff8e169a6595fe288bb5771cefd8f98466bc56dd
Coolsurf6 <coolsurf24@yahoo.com.au>
2025-01-22 13:23:47 +1000

    added comment for BeforeForceSayEvent

--- Content.Shared/Damage/Events/BeforeForceSayEvent.cs
+++ Content.Shared/Damage/Events/BeforeForceSayEvent.cs
@@ -4,6 +4,9 @@ using Robust.Shared.Serialization;
 
 namespace Content.Shared.Damage.Events;
 
+/// <summary>
+///     Event for interrupting and changing the prefix for when an entity is being forced to say something
+/// </summary>
 [Serializable, NetSerializable]
 public sealed class BeforeForceSayEvent(ProtoId<LocalizedDatasetPrototype> prefixDataset) : EntityEventArgs
 {
