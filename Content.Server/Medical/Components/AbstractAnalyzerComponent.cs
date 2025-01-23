e523893c7f12abea3a3b4116110d0c7548e36dff
Ignaz Kraft <ignaz.k@live.de>
2025-01-23 02:10:44 +0100

    WIP: plant analyzer

---
+++ Content.Server/Medical/Components/AbstractAnalyzerComponent.cs
@@ -0,0 +1,54 @@
+using Robust.Shared.Audio;
+
+namespace Content.Server.Medical.Components;
+
+/// <summary>
+/// After scanning, retrieves the target Uid to use with its related UI.
+/// </summary>
+/// <remarks>
+/// Requires <c>ItemToggleComponent</c>.
+/// </remarks>
+public abstract partial class AbstractAnalyzerComponent : Component
+{
+    /// <summary>
+    /// The delay between patient health updates
+    /// </summary>
+    [DataField]
+    public TimeSpan UpdateInterval = TimeSpan.FromSeconds(1);
+
+    /// <summary>
+    /// How long it takes to scan someone.
+    /// </summary>
+    [DataField]
+    public TimeSpan ScanDelay = TimeSpan.FromSeconds(0.8);
+
+    /// <summary>
+    /// Which entity has been scanned, for continuous updates
+    /// </summary>
+    [DataField]
+    public EntityUid? ScannedEntity;
+
+    /// <summary>
+    /// The maximum range in tiles at which the analyzer can receive continuous updates, a value of null will be infinite range
+    /// </summary>
+    [DataField]
+    public float? MaxScanRange = 2.5f;
+
+    /// <summary>
+    /// Sound played on scanning begin
+    /// </summary>
+    [DataField]
+    public SoundSpecifier? ScanningBeginSound;
+
+    /// <summary>
+    /// Sound played on scanning end
+    /// </summary>
+    [DataField]
+    public SoundSpecifier ScanningEndSound = new SoundPathSpecifier("/Audio/Items/Medical/healthscanner.ogg");
+
+    /// <summary>
+    /// Whether to show up the popup
+    /// </summary>
+    [DataField]
+    public bool Silent;
+}
