511780360b3c847db2a4f5f33531a606cba7e495
Ignaz Kraft <ignaz.k@live.de>
2025-01-23 20:32:07 +0100

    it can scan!

--- Content.Shared/Botany/PlantAnalyzer/PlantAnalyzerScannedUserMessage.cs
+++ Content.Shared/Botany/PlantAnalyzer/PlantAnalyzerScannedUserMessage.cs
@@ -6,4 +6,21 @@ namespace Content.Shared.Botany.PlantAnalyzer;
 public sealed class PlantAnalyzerScannedUserMessage : BoundUserInterfaceMessage
 {
     // TODO: PA
+    public readonly NetEntity? TargetEntity;
+    public bool? ScanMode;
+    public PlantAnalyzerSeedData? SeedData;
+
+
+    public PlantAnalyzerScannedUserMessage(NetEntity? targetEntity, bool? scanMode, PlantAnalyzerSeedData? seedData)
+    {
+        TargetEntity = targetEntity;
+        ScanMode = scanMode;
+        SeedData = seedData;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class PlantAnalyzerSeedData(string displayName)
+{
+    public string DisplayName { get; private set; } = displayName;
 }
