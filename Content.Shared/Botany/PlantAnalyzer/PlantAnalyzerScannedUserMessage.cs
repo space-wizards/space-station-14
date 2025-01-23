d9a46d56d349417d927c5600b6bc5cddfb49dc86
Ignaz Kraft <ignaz.k@live.de>
2025-01-23 02:10:44 +0100

    WIP: plant analyzer

---
+++ Content.Shared/Botany/PlantAnalyzer/PlantAnalyzerScannedUserMessage.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Botany.PlantAnalyzer;
+
+[Serializable, NetSerializable]
+public sealed class PlantAnalyzerScannedUserMessage : BoundUserInterfaceMessage
+{
+    // TODO: PA
+}
