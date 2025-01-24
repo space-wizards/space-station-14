6bffe50f439fcc32b8cf7eb7590089f2f6a48808
Ruddygreat <ruddygreat1@gmail.com>
2025-01-24 00:15:21 +0000

    make briefings actually use markup'd text

--- Content.Client/UserInterface/Systems/Character/CharacterUIController.cs
+++ Content.Client/UserInterface/Systems/Character/CharacterUIController.cs
@@ -159,7 +159,7 @@ public sealed class CharacterUIController : UIController, IOnStateEntered<Gamepl
             var briefingControl = new ObjectiveBriefingControl();
             var text = new FormattedMessage();
             text.PushColor(Color.Yellow);
-            text.AddText(briefing);
+            text.TryAddMarkup(briefing, out _); //imp edit - make this recognize markup'd text as markup'd text
             briefingControl.Label.SetMessage(text);
             _window.Objectives.AddChild(briefingControl);
         }
