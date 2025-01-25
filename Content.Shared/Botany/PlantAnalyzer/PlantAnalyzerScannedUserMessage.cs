0a777cf873ac4a742a8e71401da23622f3a4fafe
Ignaz Kraft <ignaz.k@live.de>
2025-01-25 02:11:42 +0100

    plant tolerances

--- Content.Shared/Botany/PlantAnalyzer/PlantAnalyzerScannedUserMessage.cs
+++ Content.Shared/Botany/PlantAnalyzer/PlantAnalyzerScannedUserMessage.cs
@@ -1,4 +1,4 @@
-using Content.Shared.FixedPoint;
+using Content.Shared.Atmos;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.Botany.PlantAnalyzer;
@@ -9,25 +9,24 @@ public sealed class PlantAnalyzerScannedUserMessage : BoundUserInterfaceMessage
     // TODO: PA
     public readonly NetEntity? TargetEntity;
     public bool? ScanMode;
-    public PlantAnalyzerSeedData? SeedData;
+    public string? SeedDisplayName;
     public PlantAnalyzerTrayData? TrayData;
+    public PlantAnalyzerTolerancesData? TolerancesData;
 
 
-    public PlantAnalyzerScannedUserMessage(NetEntity? targetEntity, bool? scanMode, PlantAnalyzerSeedData? seedData, PlantAnalyzerTrayData? trayData)
+    public PlantAnalyzerScannedUserMessage(NetEntity? targetEntity, bool? scanMode, string? seedDisplayName, PlantAnalyzerTrayData? trayData, PlantAnalyzerTolerancesData? tolerancesData)
     {
         TargetEntity = targetEntity;
         ScanMode = scanMode;
-        SeedData = seedData;
+        SeedDisplayName = seedDisplayName;
         TrayData = trayData;
+        TolerancesData = tolerancesData;
     }
 }
 
-[Serializable, NetSerializable]
-public sealed class PlantAnalyzerSeedData(string displayName)
-{
-    public string DisplayName = displayName;
-}
-
+/// <summary>
+/// Everything that is kept independed of a given plant/seed.
+/// </summary>
 [Serializable, NetSerializable]
 public sealed class PlantAnalyzerTrayData(float waterLevel, float nutritionLevel, float toxins, float pestLevel, float weedLevel)
 {
@@ -36,4 +35,26 @@ public sealed class PlantAnalyzerTrayData(float waterLevel, float nutritionLevel
     public float Toxins = toxins;
     public float PestLevel = pestLevel;
     public float WeedLevel = weedLevel;
+}
+
+
+/// <summary>
+/// All the information to keep the plant alive.
+/// Which is most of the "Tolerances" reagion plus the gases it may need.
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class PlantAnalyzerTolerancesData(float nutrientConsumption, float waterConsumption, float idealHeat, float heatTolerance, float idealLight, float lightTolerance, float toxinsTolerance, float lowPressureTolerance, float highPressureTolerance, float pestTolerance, float weedTolerance, List<Gas> consumeGasses)
+{
+    public float WaterConsumption = waterConsumption;
+    public float NutrientConsumption = nutrientConsumption;
+    public float ToxinsTolerance = toxinsTolerance;
+    public float PestTolerance = pestTolerance;
+    public float WeedTolerance = weedTolerance;
+    public float LowPressureTolerance = lowPressureTolerance;
+    public float HighPressureTolerance = highPressureTolerance;
+    public float IdealHeat = idealHeat;
+    public float HeatTolerance = heatTolerance;
+    public float IdealLight = idealLight;
+    public float LightTolerance = lightTolerance;
+    public List<Gas> ConsumeGasses = consumeGasses;
 }
