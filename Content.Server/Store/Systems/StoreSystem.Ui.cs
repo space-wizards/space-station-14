6386d105ae72e44db8bf4c48ed94131d66442dd2
keronshb <keronshb@live.com>
2025-01-26 18:59:24 -0500

    Replaces instances of component.RefundAllowed = false with DisableRefund for more consistency and easier tracking

--- Content.Server/Store/Systems/StoreSystem.Ui.cs
+++ Content.Server/Store/Systems/StoreSystem.Ui.cs
@@ -164,7 +164,7 @@ public sealed partial class StoreSystem
         }
 
         if (!IsOnStartingMap(uid, component))
-            component.RefundAllowed = false;
+            DisableRefund(uid, component);
 
         //subtract the cash
         foreach (var (currency, amount) in cost)
@@ -332,7 +332,7 @@ public sealed partial class StoreSystem
 
         if (!IsOnStartingMap(uid, component))
         {
-            component.RefundAllowed = false;
+            DisableRefund(uid, component);
             UpdateUserInterface(buyer, uid, component);
         }
 
