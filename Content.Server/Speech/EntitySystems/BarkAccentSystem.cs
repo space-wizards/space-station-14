f31a405a2980b2056102c33a89d0cc6ac828bb8c
dayn-e <jes.ee.cook@gmail.com>
2025-01-26 13:48:55 +0200

    Rework BarkAccentSystem to sound more scooby-like; remove conversion of the letter L to R

--- Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
@@ -1,11 +1,16 @@
 using Content.Server.Speech.Components;
 using Robust.Shared.Random;
+using System.Text.RegularExpressions;
 
 namespace Content.Server.Speech.EntitySystems
 {
     public sealed class BarkAccentSystem : EntitySystem
     {
         [Dependency] private readonly IRobustRandom _random = default!;
+
+        // Regex pattern matches the first letters of words as long as they're consonants.
+        private static readonly Regex RegexLowerScoobyRs = new Regex(@"\b[bcdfghjklnpqsvw]");
+        private static readonly Regex RegexUpperScoobyRs = new Regex(@"\b[BCDFGHJKLNPQSVW]");
 
         private static readonly IReadOnlyList<string> Barks = new List<string>{
             " Woof!", " WOOF", " wof-wof"
@@ -15,12 +20,18 @@ namespace Content.Server.Speech.EntitySystems
         {
             { "ah", "arf" },
             { "Ah", "Arf" },
-            { "oh", "oof" },
-            { "Oh", "Oof" },
+            { "AH", "ARF" },
+            { "oh", "roh" },
+            { "Oh", "Roh" },
+            { "OH", "ROH" },
+            { "uh", "ruh" },
+            { "Uh", "Ruh" },
+            { "UH", "RUH" }
         };
 
         public override void Initialize()
         {
+            base.Initialize();
             SubscribeLocalEvent<BarkAccentComponent, AccentGetEvent>(OnAccent);
         }
 
@@ -31,13 +42,14 @@ namespace Content.Server.Speech.EntitySystems
                 message = message.Replace(word, repl);
             }
 
-            return message.Replace("!", _random.Pick(Barks))
-                .Replace("l", "r").Replace("L", "R");
+            return message.Replace("!", _random.Pick(Barks));
         }
 
         private void OnAccent(EntityUid uid, BarkAccentComponent component, AccentGetEvent args)
         {
             args.Message = Accentuate(args.Message);
+            args.Message = RegexLowerScoobyRs.Replace(args.Message, "r");
+            args.Message = RegexUpperScoobyRs.Replace(args.Message, "R");
         }
     }
 }
