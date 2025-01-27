87b684ff1dc6fea2b036d9fc0dc0222fe17ed1db
dayn-e <jes.ee.cook@gmail.com>
2025-01-27 19:24:44 +0100

    Add imp comments

--- Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
@@ -1,6 +1,6 @@
 using Content.Server.Speech.Components;
 using Robust.Shared.Random;
-using System.Text.RegularExpressions;
+using System.Text.RegularExpressions; // imp edit
 
 namespace Content.Server.Speech.EntitySystems
 {
@@ -8,9 +8,11 @@ namespace Content.Server.Speech.EntitySystems
     {
         [Dependency] private readonly IRobustRandom _random = default!;
 
+        // imp start
         // Regex pattern matches the first letters of words as long as they're consonants.
         private static readonly Regex RegexLowerScoobyRs = new Regex(@"\b[bcdfghjklnpqsvw]");
         private static readonly Regex RegexUpperScoobyRs = new Regex(@"\b[BCDFGHJKLNPQSVW]");
+        // imp end
 
         private static readonly IReadOnlyList<string> Barks = new List<string>{
             " Woof!", " WOOF", " wof-wof"
@@ -19,6 +21,7 @@ namespace Content.Server.Speech.EntitySystems
         private static readonly IReadOnlyDictionary<string, string> SpecialWords = new Dictionary<string, string>()
         {
             { "ah", "arf" },
+            // imp start
             { "Ah", "Arf" },
             { "AH", "ARF" },
             { "oh", "roh" },
@@ -27,11 +30,12 @@ namespace Content.Server.Speech.EntitySystems
             { "uh", "ruh" },
             { "Uh", "Ruh" },
             { "UH", "RUH" }
+            // imp end
         };
 
         public override void Initialize()
         {
-            base.Initialize();
+            base.Initialize(); // imp edit
             SubscribeLocalEvent<BarkAccentComponent, AccentGetEvent>(OnAccent);
         }
 
@@ -42,14 +46,17 @@ namespace Content.Server.Speech.EntitySystems
                 message = message.Replace(word, repl);
             }
 
-            return message.Replace("!", _random.Pick(Barks));
+            return message.Replace("!", _random.Pick(Barks)); //imp edit
         }
 
         private void OnAccent(EntityUid uid, BarkAccentComponent component, AccentGetEvent args)
         {
             args.Message = Accentuate(args.Message);
+
+            // imp start
             args.Message = RegexLowerScoobyRs.Replace(args.Message, "r");
             args.Message = RegexUpperScoobyRs.Replace(args.Message, "R");
+            // imp end
         }
     }
 }
