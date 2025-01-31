6565ba52b2ef3ec4f890f74606aa83c888b30229
keronshb <54602815+keronshb@users.noreply.github.com>
2025-01-30 19:10:35 -0500

    Void's Applause (#34591)
    * Adds single clap emote
    
    * Adds missing single clap loc
    
    * Adds void blink effects, sound, and sprite
    
    * Adds VoidApplauseEvent and implements it into the Magic System and Grimoire
    
    * Removes commented Spawn code in Shared Magic System. Adds comments to replace pointlight with a negative light in the future.
    
    * Makes spells Mono
    
    * Changes to swap positions.
    
    * Makes Void Applause target body only
    
    * Update Content.Shared/Magic/Events/VoidApplauseSpellEvent.cs
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    * Update Content.Client/Magic/MagicSystem.cs
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    * Update Resources/Audio/Effects/Emotes/attributions.yml
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    * Update Resources/Prototypes/SoundCollections/emotes.yml
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    * Update Content.Shared/Magic/Events/VoidApplauseSpellEvent.cs
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    * Removes duplicate effect spawning from server
    
    * Puts Void Applause effect on server, lets lizards hear clap
    
    * Adds single clap to diona
    
    ---------
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Client/Magic/MagicSystem.cs
+++ Content.Client/Magic/MagicSystem.cs
@@ -1,5 +1,8 @@
 ﻿using Content.Shared.Magic;
+using Content.Shared.Magic.Events;
 
 namespace Content.Client.Magic;
 
-public sealed class MagicSystem : SharedMagicSystem;
+public sealed class MagicSystem : SharedMagicSystem
+{
+}
