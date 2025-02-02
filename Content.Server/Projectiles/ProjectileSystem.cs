0d76b798abf9c1a8feca42c57cf79c09c52cee79
SlamBamActionman <slambamactionman@gmail.com>
2025-02-02 14:26:28 +0100

    LINQ BEGONETH

--- Content.Server/Projectiles/ProjectileSystem.cs
+++ Content.Server/Projectiles/ProjectileSystem.cs
@@ -9,7 +9,6 @@ using Content.Shared.FixedPoint;
 using Content.Shared.Projectiles;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Player;
-using System.Linq;
 
 namespace Content.Server.Projectiles;
 
