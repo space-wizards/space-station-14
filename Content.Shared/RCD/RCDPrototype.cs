657f252fb6b41961ed928b648ccab9c71cefe229
August Eymann <august.eymann@gmail.com>
2025-02-02 10:45:13 -0800

    this has to be a string without me rewriting more code than i want to in this moment

--- Content.Shared/RCD/RCDPrototype.cs
+++ Content.Shared/RCD/RCDPrototype.cs
@@ -42,7 +42,7 @@ public sealed class RCDPrototype : IPrototype
     /// The entity prototype that will be constructed (mode dependent)
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadOnly)]
-    public EntProtoId? Prototype { get; private set; } = string.Empty;
+    public string? Prototype { get; private set; }
 
     /// <summary>
     /// Number of charges consumed when the operation is completed
