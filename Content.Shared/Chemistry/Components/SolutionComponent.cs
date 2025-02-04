e7733fb6acfda52b454258d44d32f0896d624182
Tayrtahn <tayrtahn@gmail.com>
2025-02-04 12:30:21 -0500

    Revert "Use Solution clones when applying SolutionComponent states"
    This reverts commit 013fd111cf92b22562e00f98a7aaa49bc4b4ed62.

--- Content.Shared/Chemistry/Components/SolutionComponent.cs
+++ Content.Shared/Chemistry/Components/SolutionComponent.cs
@@ -1,7 +1,6 @@
 using Content.Shared.Chemistry.Components.SolutionManager;
 using Content.Shared.Materials;
 using Robust.Shared.GameStates;
-using Robust.Shared.Serialization;
 
 namespace Content.Shared.Chemistry.Components;
 
@@ -12,18 +11,12 @@ namespace Content.Shared.Chemistry.Components;
 /// <remarks>
 /// <para>Once reagents and materials have been merged this component should be depricated in favor of using a combination of <see cref="PhysicalCompositionComponent"/> and <see cref="Content.Server.Temperature.Components.TemperatureComponent"/>. May require minor reworks to both.</para>
 /// </remarks>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class SolutionComponent : Component
 {
     /// <summary>
     /// <para>The reagents the entity is composed of and their temperature.</para>
     /// </summary>
-    [DataField]
+    [DataField, AutoNetworkedField]
     public Solution Solution = new();
-}
-
-[Serializable, NetSerializable]
-public sealed class SolutionComponentState(Solution solution) : ComponentState
-{
-    public Solution Solution { get; } = solution;
 }
