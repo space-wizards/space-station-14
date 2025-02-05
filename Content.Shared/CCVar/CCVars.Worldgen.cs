03eb58963268b1e59488f71980c68f541041404e
TheCrimsonJupiter <54331834+TheCrimsonJupiter@users.noreply.github.com>
2025-02-05 16:12:30 -0700

    added harmony notation

--- Content.Shared/CCVar/CCVars.Worldgen.cs
+++ Content.Shared/CCVar/CCVars.Worldgen.cs
@@ -8,7 +8,7 @@ public sealed partial class CCVars
     ///     Whether or not world generation is enabled.
     /// </summary>
     public static readonly CVarDef<bool> WorldgenEnabled =
-        CVarDef.Create("worldgen.enabled", true, CVar.SERVERONLY);
+        CVarDef.Create("worldgen.enabled", true, CVar.SERVERONLY); /// Harmony change. turned from "false" to "true" 
 
     /// <summary>
     ///     The worldgen config to use.
