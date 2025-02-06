c391de9b64dd33412a40eabbaa7a6e1fa52ad90a
TheCrimsonJupiter <54331834+TheCrimsonJupiter@users.noreply.github.com>
2025-02-05 17:56:07 -0700

    added space and removed CCVar change, awaiting Kelderan doing HCCVar change.

--- Content.Shared/CCVar/CCVars.Worldgen.cs
+++ Content.Shared/CCVar/CCVars.Worldgen.cs
@@ -8,7 +8,7 @@ public sealed partial class CCVars
     ///     Whether or not world generation is enabled.
     /// </summary>
     public static readonly CVarDef<bool> WorldgenEnabled =
-        CVarDef.Create("worldgen.enabled", true, CVar.SERVERONLY); /// Harmony change. turned from "false" to "true" 
+        CVarDef.Create("worldgen.enabled", false, CVar.SERVERONLY);
 
     /// <summary>
     ///     The worldgen config to use.
