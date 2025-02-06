7022a9db2bc51354ec782393ba22da31bfa6bde6
aada <ss.adasts@gmail.com>
2025-02-05 22:41:59 -0600

    better

--- Content.Server/NPC/HTN/Preconditions/InFriendlyContainerPrecondition.cs
+++ Content.Server/NPC/HTN/Preconditions/InFriendlyContainerPrecondition.cs
@@ -28,30 +28,24 @@ public sealed partial class InFriendlyContainerPrecondition : HTNPrecondition
     {
         var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
 
-        // if (!_entManager.TryGetComponent<TransformComponent>(owner, out var xform))
-        //     return false; // If it doesn't have an xform there's bigger problems
-
         if (!_container.TryGetContainingContainer(owner, out var container))
             return !IsInFriendlyContainer;
 
-        return IsInFriendlyContainer == IsContainerFriendlyRecursive(owner, container, owner);
+        return IsInFriendlyContainer == IsContainerOrParentFriendly(owner, container);
     }
 
     /// <summary>
-    /// Recursively check a container's container f.
+    /// Recursively check if a container or any parent container is friendly.
     /// </summary>
-    /// <param name="ent">Entity that might be inside a container.</param>
-    /// <param name="owner">The NPC.</param>
-    /// <param name="container">Container containing <paramref name="ent"/>.</param>
-    /// <returns>True if container.Owner is friendly with <paramref name="owner"/>.</returns>
-    private bool IsContainerFriendlyRecursive(EntityUid ent, BaseContainer container, EntityUid owner)
+    /// <returns>True if any container is friendly.</returns>
+    private bool IsContainerOrParentFriendly(EntityUid owner, BaseContainer container)
     {
         if (_npcFaction.IsEntityFriendly(owner, container.Owner))
             return true;
 
-        if (!_container.TryGetContainingContainer(ent, out var nextContainer))
+        if (!_container.TryGetContainingContainer(container.Owner, out var nextContainer))
             return false;
 
-        return IsContainerFriendlyRecursive(container.Owner, nextContainer, owner);
+        return IsContainerOrParentFriendly(owner, nextContainer);
     }
 }
