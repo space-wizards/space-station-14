2c4b0232efcaec7449488afc38d713a82ebed9e5
Rinary <rinary.super@gmail.com>
2025-02-07 14:49:24 +0200

    Revert "fix electrified doors"
    This reverts commit a7573f6d36a719ce01cb64f1f93cd11decba6d3d.
    
    # Conflicts:
    #	Resources/Prototypes/Entities/Structures/Doors/Airlocks/base_structureairlocks.yml
    #	Resources/Prototypes/Entities/Structures/Doors/Airlocks/highsec.yml
    #	Resources/Prototypes/Entities/Structures/Doors/Windoors/base_structurewindoors.yml

--- Content.Client/Electrocution/ElectrocutionSystem.cs
+++ Content.Client/Electrocution/ElectrocutionSystem.cs
@@ -1,33 +1,7 @@
-using Robust.Shared.Utility;
-using Robust.Client.GameObjects;
 using Content.Shared.Electrocution;
-using Content.Shared.Doors.Components;
 
 namespace Content.Client.Electrocution
 {
     public sealed class ElectrocutionSystem : SharedElectrocutionSystem
-    {
-        public override void Initialize()
-        {
-            base.Initialize();
-
-            SubscribeLocalEvent<ElectrifiedComponent, ComponentInit>(OnComponentInit);
-        }
-        
-        private void OnComponentInit(EntityUid uid, ElectrifiedComponent component, ComponentInit args)
-        {
-            if (!TryComp<SpriteComponent>(uid, out var sprite) || !HasComp<DoorComponent>(uid))
-                return;
-            
-            if (sprite.BaseRSI?.TryGetState("electrified", out var state) ?? true)
-                return;
-            
-            var index = sprite.LayerMapReserveBlank("electrified");
-
-            sprite.LayerSetRSI(index, "/Textures/Interface/Misc/ai_hud.rsi");
-            sprite.LayerSetState(index, "electrified");
-            sprite.LayerSetVisible(index, false);
-            sprite.LayerMapSet(ElectrifiedLayers.HUD, index);
-        }
-    }
+    { }
 }
