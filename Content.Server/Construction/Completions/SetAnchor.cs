2642cc7edfa6e961f5688654df3df54877aa9b36
TemporalOroboros <TemporalOroboros@gmail.com>
2025-02-06 19:52:01 -0800

    Purge uses of TransformComponent.Anchored.set Switches over all existing content uses of TransformComponent.Anchored.set to use either SharedTransformSystem.AnchorEntity or SharedTransformSystem.Unanchor

--- Content.Server/Construction/Completions/SetAnchor.cs
+++ Content.Server/Construction/Completions/SetAnchor.cs
@@ -1,5 +1,6 @@
 ﻿using Content.Shared.Construction;
 using JetBrains.Annotations;
+using Robust.Shared.Map;
 
 namespace Content.Server.Construction.Completions
 {
@@ -12,7 +13,17 @@ namespace Content.Server.Construction.Completions
         public void PerformAction(EntityUid uid, EntityUid? userUid, IEntityManager entityManager)
         {
             var transform = entityManager.GetComponent<TransformComponent>(uid);
-            transform.Anchored = Value;
+
+            if (Value == transform.Anchored)
+                return;
+
+            var mapManager = IoCManager.Resolve<IMapManager>();
+            var transformSystem = entityManager.System<SharedTransformSystem>();
+
+            if (Value && mapManager.TryFindGridAt(transformSystem.GetMapCoordinates(transform), out var gridUid, out var grid))
+                transformSystem.AnchorEntity((uid, transform), (gridUid, grid));
+            else
+                transformSystem.Unanchor(uid, transform);
         }
     }
 }
