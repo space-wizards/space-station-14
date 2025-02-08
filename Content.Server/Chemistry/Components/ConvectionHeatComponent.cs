ea95b06004c5fa60380219238775a24244dced51
SG6732 <segye2376@outlook.com>
2025-02-07 21:21:21 -0800

    > Redux

---
+++ Content.Server/Chemistry/Components/ConvectionHeatComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Server.Chemistry.Components;
+
+[RegisterComponent]
+public sealed partial class ConvectionHeatComponent : Component
+{
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float TempDifference;
+}
