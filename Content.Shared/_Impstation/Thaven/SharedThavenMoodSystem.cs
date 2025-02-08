9b5ec4cdedb252f443249aa1a9700878a271160e
V <97265903+formlessnameless@users.noreply.github.com>
2025-02-08 17:39:18 -0600

    this is going to take a second

--- Content.Shared/_Impstation/Thaven/SharedThavenMoodSystem.cs
+++ Content.Shared/_Impstation/Thaven/SharedThavenMoodSystem.cs
@@ -8,15 +8,7 @@ public abstract class SharedThavenMoodSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-
-        SubscribeLocalEvent<ThavenMoodsComponent, OnAttemptEmagEvent>(OnAttemptEmag);
         SubscribeLocalEvent<ThavenMoodsComponent, GotEmaggedEvent>(OnEmagged);
-    }
-
-    private void OnAttemptEmag(EntityUid uid, ThavenMoodsComponent comp, ref OnAttemptEmagEvent args)
-    {
-        if (!comp.CanBeEmagged)
-            args.Handled = true;
     }
 
     protected virtual void OnEmagged(EntityUid uid, ThavenMoodsComponent comp, ref GotEmaggedEvent args)
