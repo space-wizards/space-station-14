25d4f6bbb2aa2b61fea445fcc51f170737870374
poklj <compgeek223@gmail.com>
2025-02-09 17:52:55 -0400

    Make UnrevivableComponent shared
    - Move UnrevivableComponent to shared
    
    - add reason messages and if the status shows up in a health analyzer

---
+++ Content.Shared/Traits/Assorted/UnrevivableComponent.cs
@@ -0,0 +1,20 @@
+namespace Content.Shared.Traits.Assorted;
+
+/// <summary>
+/// This is used for the unrevivable trait as well as generally preventing revival.
+/// </summary>
+[RegisterComponent]
+public sealed partial class UnrevivableComponent : Component
+{
+    /// <summary>
+    /// A field to define if we should display the "Genetic incompatibility" warning on health analysers
+    /// </summary>
+    [DataField]
+    public bool Analyzable { get; set; } = true;
+
+    /// <summary>
+    /// The loc string used to provide a reason for being unrevivable
+    /// </summary>
+    [DataField]
+    public string ReasonMessage = "defibrillator-unrevivable";
+}
