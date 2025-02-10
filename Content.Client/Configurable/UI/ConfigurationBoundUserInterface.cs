f467e7edcf9d3ba341414508a9af095074a8e282
Centronias <me@centronias.com>
2025-02-10 12:36:54 -0800

    namespace only changes

--- Content.Client/Configurable/UI/ConfigurationBoundUserInterface.cs
+++ Content.Client/Configurable/UI/ConfigurationBoundUserInterface.cs
@@ -3,50 +3,49 @@ using Robust.Client.GameObjects;
 using Robust.Client.UserInterface;
 using static Content.Shared.Configurable.ConfigurationComponent;
 
-namespace Content.Client.Configurable.UI
+namespace Content.Client.Configurable.UI;
+
+public sealed class ConfigurationBoundUserInterface : BoundUserInterface
 {
-    public sealed class ConfigurationBoundUserInterface : BoundUserInterface
+    [ViewVariables]
+    private ConfigurationMenu? _menu;
+
+    public ConfigurationBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
-        [ViewVariables]
-        private ConfigurationMenu? _menu;
+    }
 
-        public ConfigurationBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    protected override void Open()
+    {
+        base.Open();
+        _menu = this.CreateWindow<ConfigurationMenu>();
+        _menu.OnConfiguration += SendConfiguration;
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+
+        if (state is not ConfigurationBoundUserInterfaceState configurationState)
+            return;
+
+        _menu?.Populate(configurationState);
+    }
+
+    protected override void ReceiveMessage(BoundUserInterfaceMessage message)
+    {
+        base.ReceiveMessage(message);
+
+        if (_menu == null)
+            return;
+
+        if (message is ValidationUpdateMessage msg)
         {
+            _menu.Validation = new Regex(msg.ValidationString, RegexOptions.Compiled);
         }
+    }
 
-        protected override void Open()
-        {
-            base.Open();
-            _menu = this.CreateWindow<ConfigurationMenu>();
-            _menu.OnConfiguration += SendConfiguration;
-        }
-
-        protected override void UpdateState(BoundUserInterfaceState state)
-        {
-            base.UpdateState(state);
-
-            if (state is not ConfigurationBoundUserInterfaceState configurationState)
-                return;
-
-            _menu?.Populate(configurationState);
-        }
-
-        protected override void ReceiveMessage(BoundUserInterfaceMessage message)
-        {
-            base.ReceiveMessage(message);
-
-            if (_menu == null)
-                return;
-
-            if (message is ValidationUpdateMessage msg)
-            {
-                _menu.Validation = new Regex(msg.ValidationString, RegexOptions.Compiled);
-            }
-        }
-
-        public void SendConfiguration(Dictionary<string, string> config)
-        {
-            SendMessage(new ConfigurationUpdatedMessage(config));
-        }
+    public void SendConfiguration(Dictionary<string, string> config)
+    {
+        SendMessage(new ConfigurationUpdatedMessage(config));
     }
 }
