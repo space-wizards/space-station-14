0277f4d6afb57ab96f248f3bfa2d45fe76ab824f
poklj <compgeek223@gmail.com>
2025-02-09 21:23:26 -0400

    Revert VocalSystem Changes
    - Reverted Moving VocalSystem to shared, copy comp acomplishes it
    
    - added component.ScreamActionEntity = null; for copy comp

--- Content.Server/Speech/EntitySystems/VocalSystem.cs
+++ Content.Server/Speech/EntitySystems/VocalSystem.cs
@@ -1,12 +1,10 @@
 using Content.Server.Actions;
 using Content.Server.Chat.Systems;
 using Content.Server.Speech.Components;
-using Content.Shared.Chat;
 using Content.Shared.Chat.Prototypes;
 using Content.Shared.Humanoid;
 using Content.Shared.Speech;
 using Content.Shared.Speech.Components;
-using Content.Shared.Speech.EntitySystems;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Prototypes;
@@ -14,17 +12,45 @@ using Robust.Shared.Random;
 
 namespace Content.Server.Speech.EntitySystems;
 
-public sealed class VocalSystem : SharedVocalSystem
+public sealed class VocalSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _random = default!;
-    [Dependency] private readonly ChatSystem _chat = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly ChatSystem _chat = default!;
+    [Dependency] private readonly ActionsSystem _actions = default!;
 
     public override void Initialize()
     {
         base.Initialize();
+
+        SubscribeLocalEvent<VocalComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<VocalComponent, ComponentShutdown>(OnShutdown);
+        SubscribeLocalEvent<VocalComponent, SexChangedEvent>(OnSexChanged);
         SubscribeLocalEvent<VocalComponent, EmoteEvent>(OnEmote);
         SubscribeLocalEvent<VocalComponent, ScreamActionEvent>(OnScreamAction);
+    }
+
+    private void OnMapInit(EntityUid uid, VocalComponent component, MapInitEvent args)
+    {
+        component.ScreamActionEntity = null;
+        // try to add scream action when vocal comp added
+        _actions.AddAction(uid, ref component.ScreamActionEntity, component.ScreamAction);
+        LoadSounds(uid, component);
+    }
+
+    private void OnShutdown(EntityUid uid, VocalComponent component, ComponentShutdown args)
+    {
+        // remove scream action when component removed
+        if (component.ScreamActionEntity != null)
+        {
+            _actions.RemoveAction(uid, component.ScreamActionEntity);
+        }
+    }
+
+    private void OnSexChanged(EntityUid uid, VocalComponent component, SexChangedEvent args)
+    {
+        LoadSounds(uid, component);
     }
     private void OnEmote(EntityUid uid, VocalComponent component, ref EmoteEvent args)
     {
@@ -60,5 +86,17 @@ public sealed class VocalSystem : SharedVocalSystem
         }
 
         return _chat.TryPlayEmoteSound(uid, component.EmoteSounds, component.ScreamId);
+    }
+
+    private void LoadSounds(EntityUid uid, VocalComponent component, Sex? sex = null)
+    {
+        if (component.Sounds == null)
+            return;
+
+        sex ??= CompOrNull<HumanoidAppearanceComponent>(uid)?.Sex ?? Sex.Unsexed;
+
+        if (!component.Sounds.TryGetValue(sex.Value, out var protoId))
+            return;
+        _proto.TryIndex(protoId, out component.EmoteSounds);
     }
 }
