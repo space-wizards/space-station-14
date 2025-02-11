823aa6e546fd326a58ef4d5ede1a4370221fc984
Hannah Giovanna Dawson <karakkaraz@gmail.com>
2025-02-11 00:13:59 +0000

    Discard changes to Content.Client/Doors/Electronics/DoorElectronicsBoundUserInterface.cs

--- Content.Client/Doors/Electronics/DoorElectronicsBoundUserInterface.cs
+++ Content.Client/Doors/Electronics/DoorElectronicsBoundUserInterface.cs
@@ -1,24 +1,26 @@
 using Content.Shared.Access;
-using Content.Shared.Doors.Components;
+using Content.Shared.Doors.Electronics;
+using Robust.Client.GameObjects;
 using Robust.Client.UserInterface;
 using Robust.Shared.Prototypes;
 
 namespace Content.Client.Doors.Electronics;
 
-public sealed class DoorElectronicsBoundUserInterface(EntityUid owner, Enum uiKey) : BoundUserInterface(owner, uiKey)
+public sealed class DoorElectronicsBoundUserInterface : BoundUserInterface
 {
-
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
 
     private DoorElectronicsConfigurationMenu? _window;
 
+    public DoorElectronicsBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
     protected override void Open()
     {
         base.Open();
-
         _window = this.CreateWindow<DoorElectronicsConfigurationMenu>();
         _window.OnAccessChanged += UpdateConfiguration;
-
         Reset();
     }
 
@@ -52,14 +54,13 @@ public sealed class DoorElectronicsBoundUserInterface(EntityUid owner, Enum uiKe
     {
         base.UpdateState(state);
 
-        var castState = (DoorElectronicsConfigurationState)state;
+        var castState = (DoorElectronicsConfigurationState) state;
 
         _window?.UpdateState(castState);
     }
 
-    private void UpdateConfiguration(List<ProtoId<AccessLevelPrototype>> newAccessList)
+    public void UpdateConfiguration(List<ProtoId<AccessLevelPrototype>> newAccessList)
     {
         SendMessage(new DoorElectronicsUpdateConfigurationMessage(newAccessList));
     }
-
 }
