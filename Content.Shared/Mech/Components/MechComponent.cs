7a740d00e90d7a66ddbad58ab097daf7a35372ba
Rinary <rinary.super@gmail.com>
2025-02-11 15:22:45 +0200

    gas tank slot

--- Content.Shared/Mech/Components/MechComponent.cs
+++ Content.Shared/Mech/Components/MechComponent.cs
@@ -69,6 +69,15 @@ public sealed partial class MechComponent : Component
 
     [ViewVariables]
     public readonly string BatterySlotId = "mech-battery-slot";
+    
+    /// <summary>
+    /// The slot the gas tank is stored in.
+    /// </summary>
+    [ViewVariables]
+    public ContainerSlot GasTankSlot = default!;
+
+    [ViewVariables]
+    public readonly string GasTankSlotId = "mech-gas-tank-slot";
 
     /// <summary>
     /// A multiplier used to calculate how much of the damage done to a mech
