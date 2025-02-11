0cc71c9f09c7285ac5b72b1f8a6b6469e11e7564
Rinary <rinary.super@gmail.com>
2025-02-11 14:51:45 +0200

    light power cell drawing

--- Content.Server/Mech/Systems/MechSystem.cs
+++ Content.Server/Mech/Systems/MechSystem.cs
@@ -3,6 +3,8 @@ using Content.Server.Atmos.EntitySystems;
 using Content.Server.Mech.Components;
 using Content.Server.Power.Components;
 using Content.Server.Power.EntitySystems;
+using Content.Server.PowerCell;
+using Content.Shared.PowerCell;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Actions;
 using Content.Shared.Damage;
@@ -24,6 +26,7 @@ using Content.Server.Body.Systems;
 using Content.Shared.Tools.Systems;
 using Robust.Server.Containers;
 using Robust.Server.GameObjects;
+using Robust.Shared.Timing;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Containers;
@@ -54,6 +57,8 @@ public sealed partial class MechSystem : SharedMechSystem
     [Dependency] private readonly HandsSystem _hands = default!;
     [Dependency] private readonly NpcFactionSystem _factionSystem = default!;
     [Dependency] private readonly SharedPointLightSystem _light = default!;
+    [Dependency] private readonly PowerCellSystem _powerCell = default!;
+    [Dependency] protected readonly IGameTiming Timing = default!;
     
     /// <inheritdoc/>
     public override void Initialize()
@@ -67,6 +72,7 @@ public sealed partial class MechSystem : SharedMechSystem
         SubscribeLocalEvent<MechComponent, GetVerbsEvent<AlternativeVerb>>(OnAlternativeVerb);
         SubscribeLocalEvent<MechComponent, MechOpenUiEvent>(OnOpenUi);
         SubscribeLocalEvent<MechComponent, RemoveBatteryEvent>(OnRemoveBattery);
+        SubscribeLocalEvent<MechComponent, ChargeChangedEvent>(OnChargeChanged);
         SubscribeLocalEvent<MechComponent, MechEntryEvent>(OnMechEntry);
         SubscribeLocalEvent<MechComponent, MechExitEvent>(OnMechExit);
 
@@ -91,15 +97,66 @@ public sealed partial class MechSystem : SharedMechSystem
         #endregion
     }
     
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+        var query = EntityQueryEnumerator<PowerCellDrawComponent, MechComponent>();
+
+        while (query.MoveNext(out var uid, out var comp, out var mechComp))
+        {
+            if (!mechComp.Light)
+                continue;
+
+            if (Timing.CurTime < comp.NextUpdateTime)
+                continue;
+
+            comp.NextUpdateTime += comp.Delay;
+
+            if (mechComp.BatterySlot.ContainedEntity == null 
+                || !TryComp<BatteryComponent>(mechComp.BatterySlot.ContainedEntity.Value, out var battery) )
+                continue;
+
+            if (!_battery.TryUseCharge(mechComp.BatterySlot.ContainedEntity.Value, comp.DrawRate))
+                continue;
+            
+            var ev = new ChargeChangedEvent(battery.CurrentCharge, battery.MaxCharge);
+            RaiseLocalEvent(uid, ref ev);
+        }
+    }
+
+    
     private void OnToggleLightEvent(EntityUid uid, MechComponent component, ToggleActionEvent args)
     {
         if (args.Handled)
             return;
         
-        if (!_light.TryGetLight(uid, out var light))
+        if (component.BatterySlot.ContainedEntity == null 
+            || !TryComp<BatteryComponent>(component.BatterySlot.ContainedEntity, out var battery) 
+            || battery.CurrentCharge <= 0)
             return;
         
         args.Handled = true;
+        
+        ToggleLight(uid, component);
+    }
+    
+    private void OnChargeChanged(Entity<MechComponent> ent, ref ChargeChangedEvent args)
+    {
+        if (args.Charge == 0 && ent.Comp.Light)
+            ToggleLight(ent.Owner, ent.Comp);
+        
+        ent.Comp.Energy = args.Charge;
+        ent.Comp.MaxEnergy = args.MaxCharge;
+        
+        _actionBlocker.UpdateCanMove(ent.Owner);
+        
+        Dirty(ent.Owner, ent.Comp);
+    }
+    
+    public void ToggleLight(EntityUid uid, MechComponent component)
+    {
+        if (!_light.TryGetLight(uid, out var light))
+            return;
         
         _light.SetEnabled(uid, !component.Light, comp: light);
         
