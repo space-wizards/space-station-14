f2add9c8d84b906f2cd16b648024b95e773da7df
pheenty <fedorlukin2006@gmail.com>
2025-02-11 17:50:47 +0700

    revert that

--- Content.Server/Speech/EntitySystems/VocalSystem.cs
+++ Content.Server/Speech/EntitySystems/VocalSystem.cs
@@ -34,8 +34,7 @@ public sealed class VocalSystem : EntitySystem
     private void OnMapInit(EntityUid uid, VocalComponent component, MapInitEvent args)
     {
         // try to add scream action when vocal comp added
-        if (component.ScreamAction != null) // For borgs and others that can emote but shouldn't scream
-            _actions.AddAction(uid, ref component.ScreamActionEntity, component.ScreamAction);
+        _actions.AddAction(uid, ref component.ScreamActionEntity, component.ScreamAction);
         LoadSounds(uid, component);
     }
 
