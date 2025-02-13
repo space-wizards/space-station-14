d5187c3f7b5d8f1d0bd040308a392fc8fea61043
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-02-13 13:36:02 +0300

    refactor(src): Move public CompletionOption

--- Content.Server/Administration/Managers/IBanManager.cs
+++ Content.Server/Administration/Managers/IBanManager.cs
@@ -28,6 +28,7 @@ public interface IBanManager
     public void CreateServerBan(NetUserId? target, string? targetUsername, NetUserId? banningAdmin, (IPAddress, int)? addressRange, ImmutableTypedHwid? hwid, uint? minutes, NoteSeverity severity, string reason);
     public HashSet<string>? GetRoleBans(NetUserId playerUserId);
     public HashSet<ProtoId<JobPrototype>>? GetJobBans(NetUserId playerUserId);
+    public CompletionOption[] BanDurations { get; }
 
     /// <summary>
     /// Creates a job ban for the specified target, username or GUID
