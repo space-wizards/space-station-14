3677c7e6f129c056bb0b94e24421954b77baa04b
keronshb <keronshb@live.com>
2025-02-15 18:15:27 -0500

    Adds end of round reporting logic. Adds logic to start the survivor rule.

--- Content.Server/GameTicking/Rules/SurvivorRuleSystem.cs
+++ Content.Server/GameTicking/Rules/SurvivorRuleSystem.cs
@@ -1,9 +1,12 @@
-﻿using Content.Server.Administration.Logs;
+﻿using System.Linq;
+using Content.Server.Administration.Logs;
 using Content.Server.Antag;
 using Content.Server.GameTicking.Rules.Components;
 using Content.Server.Mind;
 using Content.Server.Roles;
+using Content.Server.RoundEnd;
 using Content.Shared.Database;
+using Content.Shared.GameTicking.Components;
 using Content.Shared.Survivor;
 using Content.Shared.Survivor.Components;
 
@@ -15,7 +18,7 @@ public sealed class SurvivorRuleSystem : GameRuleSystem<SurvivorRuleComponent>
     [Dependency] private readonly MindSystem _mind = default!;
     [Dependency] private readonly IAdminLogManager _adminLog = default!;
     [Dependency] private readonly AntagSelectionSystem _antag = default!;
-
+    [Dependency] private readonly RoundEndSystem _roundEnd = default!;
 
     public override void Initialize()
     {
@@ -26,14 +29,51 @@ public sealed class SurvivorRuleSystem : GameRuleSystem<SurvivorRuleComponent>
 
     private void OnAddSurvivorRole(ref AddSurvivorRoleEvent args)
     {
-        if (!_mind.TryGetMind(args.ToBeSurvivor, out var mind, out _) || HasComp<SurvivorComponent>(args.ToBeSurvivor))
+        const string survivorRule = "Survivor";
+
+        if (!_mind.TryGetMind(args.ToBeSurvivor, out var mind, out var mindComp) || HasComp<SurvivorComponent>(args.ToBeSurvivor))
             return;
 
         EnsureComp<SurvivorComponent>(args.ToBeSurvivor);
         _adminLog.Add(LogType.Mind, LogImpact.Medium, $"{ToPrettyString(args.ToBeSurvivor)} has become a Survivor!");
         _role.MindAddRole(mind, "MindRoleSurvivor");
         _antag.SendBriefing(args.ToBeSurvivor, Loc.GetString("survivor-role-greeting"), Color.Olive, null);
+
+        if (!GameTicker.IsGameRuleActive<SurvivorRuleComponent>())
+            GameTicker.StartGameRule(survivorRule);
     }
 
-    // TODO: Round End
+    protected override void AppendRoundEndText(EntityUid uid,
+        SurvivorRuleComponent component,
+        GameRuleComponent gameRule,
+        ref RoundEndTextAppendEvent args)
+    {
+        base.AppendRoundEndText(uid, component, gameRule, ref args);
+
+        // Using this instead of alive antagonists to make checking for centcomm easier
+        var aliveSurvivors = EntityQuery<SurvivorComponent, TransformComponent>();
+
+        args.AddLine(Loc.GetString("survivor-round-end-alive-count", ("aliveCount", aliveSurvivors.Count())));
+
+        var aliveOnCentComm = 0;
+        var centCommMapUid = _roundEnd.GetCentcomm();
+
+        if (centCommMapUid is null)
+            return;
+
+        // TODO: Detects CentComm but didn't detect the survivor ON centcomm
+        foreach (var (_, xform) in aliveSurvivors)
+        {
+            var mapUid = xform.MapUid;
+
+            if (mapUid != centCommMapUid.Value)
+                continue;
+
+            aliveOnCentComm++;
+        }
+
+        args.AddLine(Loc.GetString("survivor-round-end-alive-on-centcomm-count", ("aliveCount", aliveOnCentComm)));
+
+        // Player manifest at EOR shows who's a survivor so no need for extra info here.
+    }
 }
