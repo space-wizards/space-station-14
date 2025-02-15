5804181fb926c529b81eb461999d5d048d92988c
lzk228 <alosamarcuk@gmail.com>
2025-02-16 00:27:02 +0100

    forgor

---
+++ Content.Server/GameTicking/Prototypes/RoundStatisticPrototype.cs
@@ -0,0 +1,22 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.GameTicking.Prototypes;
+
+/// <summary>
+/// Used to create round statistics
+/// </summary>
+[Serializable, Prototype("roundStatistic")]
+public sealed class RoundStatisticPrototype : IPrototype
+{
+    /// <inheritdoc/>
+    [IdDataField]
+    public string ID { get; set; } = default!;
+
+    /// <summary>
+    /// String that will be shown in round end window
+    /// </summary>
+    [DataField(required: true)]
+    public LocId StatString;
+
+    public int StatCount;
+}
