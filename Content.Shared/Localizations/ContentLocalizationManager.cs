d20b0013a422482a8da54b2305e20733f50a90c2
Kyle Tyo <akikai297@gmail.com>
2025-02-16 14:06:36 -0500

    testing this is harder than I thought ;(

--- Content.Shared/Localizations/ContentLocalizationManager.cs
+++ Content.Shared/Localizations/ContentLocalizationManager.cs
@@ -148,7 +148,7 @@ namespace Content.Shared.Localizations
         public static string FormatPlaytime(TimeSpan time)
         {
             var hours = (int)time.TotalHours;
-            var minutes = (int)Math.Ceiling(time.TotalMinutes);
+            var minutes = (int)(time.TotalMinutes - time.TotalHours * 60);
             return Loc.GetString($"zzzz-fmt-playtime", ("hours", hours), ("minutes", minutes));
         }
 
