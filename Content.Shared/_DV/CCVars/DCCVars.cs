cca60f7faa2dad87fc6800ce954564de20b37031
hivehum <ketchupfaced@gmail.com>
2025-02-16 12:28:56 -0500

    accessibility option disabled by default

--- Content.Shared/_DV/CCVars/DCCVars.cs
+++ Content.Shared/_DV/CCVars/DCCVars.cs
@@ -121,5 +121,5 @@ public sealed class DCCVars
     /// Disables all vision filters for species like Vulpkanin or Harpies. There are good reasons someone might want to disable these.
     /// </summary>
     public static readonly CVarDef<bool> NoVisionFilters =
-        CVarDef.Create("accessibility.no_vision_filters", true, CVar.CLIENTONLY | CVar.ARCHIVE);
+        CVarDef.Create("accessibility.no_vision_filters", false, CVar.CLIENTONLY | CVar.ARCHIVE);
 }
