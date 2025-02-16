493f2c7208f572491991e8e8da5eee5d5d8f69fb
SpeltIncorrectyl <speltincorrectyl0001@gmail.com>
2025-02-16 23:16:55 +0000

    added summary to illiteracy system

--- Content.Shared/Paper/IlliteracySystem.cs
+++ Content.Shared/Paper/IlliteracySystem.cs
@@ -1,5 +1,9 @@
 namespace Content.Shared.Paper;
 
+/// <summary>
+/// A system that prevents those with the IlliterateComponent from writing on paper.
+/// Has no effect on reading ability.
+/// </summary>
 public sealed class IlliteracySystem: EntitySystem
 {
     public override void Initialize()
@@ -11,6 +15,6 @@ public sealed class IlliteracySystem: EntitySystem
     private void OnPaperWriteAttempt(Entity<IlliterateComponent> entity, ref PaperWriteAttemptEvent args)
     {
         args.FailReason = entity.Comp.FailWriteMessage;
-        args.Cancel();
+        args.Cancelled = true;
     }
 }
