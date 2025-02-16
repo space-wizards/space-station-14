ef554295361d3b1c3139ac25fafc29db2a656530
qwerltaz <msmarcinpl@gmail.com>
2025-02-16 13:09:15 +0100

    refactor AUdioFun

--- Content.Shared/Audio/AudioFun.cs
+++ Content.Shared/Audio/AudioFun.cs
@@ -5,7 +5,7 @@ namespace Content.Shared.Audio;
 
 public static class AudioFun
 {
-    private static readonly RobustRandom _random = new();
+    private static readonly RobustRandom Random = new();
 
     private const float MinPitchScale = 0.2f;
     private const float MaxPitchScale = 2.0f;
@@ -14,7 +14,7 @@ public static class AudioFun
     {
         audioParams ??= AudioParams.Default;
 
-        return audioParams.Value.WithPitchScale(_random.NextFloat() * (MaxPitchScale - MinPitchScale) + MinPitchScale)
+        return audioParams.Value.WithPitchScale(Random.NextFloat() * (MaxPitchScale - MinPitchScale) + MinPitchScale)
             .WithVariation(0);
     }
 }
