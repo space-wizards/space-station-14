3d5cbccbeb01db60efd22ba62dde7ad57eef17a4
qrtDaniil <qrtdaniil@yandex.ru>
2025-02-18 19:34:02 +0300

    add typan map to DoNotMapWhitelist

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -52,6 +52,7 @@ namespace Content.IntegrationTests.Tests
             "/Maps/Shuttles/ShuttleEvent/honki.yml", // Contains golden honker, clown's rubber stamp
             "/Maps/Shuttles/ShuttleEvent/instigator.yml", // Contains EXP-320g "Friendship"
             "/Maps/Shuttles/ShuttleEvent/syndie_evacpod.yml", // Contains syndicate rubber stamp
+            "/Maps/ds_typan.yml",
         };
 
         private static readonly string[] GameMaps =
