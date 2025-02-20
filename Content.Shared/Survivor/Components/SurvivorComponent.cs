f18fdb99c8c386b73264bcd4a9f7cd2c95e3e0c1
keronshb <keronshb@live.com>
2025-02-19 22:41:22 -0500

    Descs for Survivor Rule and Survivor comps

--- Content.Shared/Survivor/Components/SurvivorComponent.cs
+++ Content.Shared/Survivor/Components/SurvivorComponent.cs
@@ -2,5 +2,8 @@
 
 namespace Content.Shared.Survivor.Components;
 
+/// <summary>
+///     Component to keep track of which entities are a Survivor antag.
+/// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class SurvivorComponent : Component;
