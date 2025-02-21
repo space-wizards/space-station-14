101e816e81a5c9c26ef8a8f7fdcc60db1c2179f9
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-02-21 19:51:38 +0100

    let the unpaid intern do the announcements

--- Content.Server/Shuttles/Systems/EmergencyShuttleSystem.cs
+++ Content.Server/Shuttles/Systems/EmergencyShuttleSystem.cs
@@ -392,7 +392,7 @@ public sealed partial class EmergencyShuttleSystem : EntitySystem
 
         var audioFile = result.ResultType == ShuttleDockResultType.NoDock
             ? "/Audio/Misc/notice1.ogg"
-            : "/Audio/Announcements/shuttle_dock.ogg";
+            : "/Audio/Announcements/Intern/shuttledock.ogg";
 
         // TODO: Need filter extensions or something don't blame me.
         _audio.PlayGlobal(audioFile, Filter.Broadcast(), true);
