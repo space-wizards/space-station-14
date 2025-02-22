c460c07ccabd9ca837b9cfefcc3134d0f683af5e
Tayrtahn <tayrtahn@gmail.com>
2025-02-22 18:20:38 -0500

    Let's do powered airlocks too

--- Content.IntegrationTests/Tests/Doors/AirlockPryingTest.cs
+++ Content.IntegrationTests/Tests/Doors/AirlockPryingTest.cs
@@ -1,12 +1,52 @@
 using Content.IntegrationTests.Tests.Interaction;
 using Content.Server.Doors.Systems;
-using Content.Server.Power.Components;
 using Content.Shared.Doors.Components;
 
 namespace Content.IntegrationTests.Tests.Doors;
 
 public sealed class AirlockPryingTest : InteractionTest
 {
+    [Test]
+    public async Task PoweredAirlock_PryOpen_DoesNotOpen()
+    {
+        await SpawnTarget(Airlock);
+        await SpawnEntity("APCBasic", SEntMan.GetCoordinates(TargetCoords));
+
+        await RunTicks(1);
+
+        Assert.That(TryComp<AirlockComponent>(out var airlockComp), "Airlock does not have AirlockComponent?");
+        Assert.That(airlockComp.Powered, "Airlock should be powered for this test.");
+
+        Assert.That(TryComp<DoorComponent>(out var doorComp), "Airlock does not have DoorComponent?");
+        Assert.That(doorComp.State, Is.EqualTo(DoorState.Closed), "Airlock did not start closed.");
+
+        await InteractUsing(Pry);
+
+        Assert.That(doorComp.State, Is.EqualTo(DoorState.Closed), "Powered airlock was pried open.");
+    }
+
+    [Test]
+    public async Task PoweredAirlock_PryClosed_DoesNotClosed()
+    {
+        await SpawnTarget(Airlock);
+        await SpawnEntity("APCBasic", SEntMan.GetCoordinates(TargetCoords));
+
+        await RunTicks(1);
+
+        Assert.That(TryComp<AirlockComponent>(out var airlockComp), "Airlock does not have AirlockComponent?");
+        Assert.That(airlockComp.Powered, "Airlock should be powered for this test.");
+
+        var doorSys = SEntMan.System<DoorSystem>();
+        await Server.WaitPost(() => doorSys.SetState(SEntMan.GetEntity(Target.Value), DoorState.Open));
+
+        Assert.That(TryComp<DoorComponent>(out var doorComp), "Airlock does not have DoorComponent?");
+        Assert.That(doorComp.State, Is.EqualTo(DoorState.Open), "Airlock did not start open.");
+
+        await InteractUsing(Pry);
+
+        Assert.That(doorComp.State, Is.EqualTo(DoorState.Open), "Powered airlock was pried closed.");
+    }
+
     [Test]
     public async Task UnpoweredAirlock_PryOpen_Opens()
     {
@@ -20,7 +60,7 @@ public sealed class AirlockPryingTest : InteractionTest
 
         await InteractUsing(Pry);
 
-        Assert.That(doorComp.State, Is.EqualTo(DoorState.Opening), "Airlock failed to pry open.");
+        Assert.That(doorComp.State, Is.EqualTo(DoorState.Opening), "Unpowered airlock failed to pry open.");
     }
 
     [Test]
@@ -39,6 +79,6 @@ public sealed class AirlockPryingTest : InteractionTest
 
         await InteractUsing(Pry);
 
-        Assert.That(doorComp.State, Is.EqualTo(DoorState.Closing), "Airlock failed to pry closed.");
+        Assert.That(doorComp.State, Is.EqualTo(DoorState.Closing), "Unpowerd airlock failed to pry closed.");
     }
 }
