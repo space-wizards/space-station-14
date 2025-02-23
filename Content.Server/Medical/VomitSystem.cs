c3843ff5acb7f2069a41832e1a9a051e5723cc85
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-02-24 00:41:03 +0300

    Update

--- Content.Server/Medical/VomitSystem.cs
+++ Content.Server/Medical/VomitSystem.cs
@@ -31,7 +31,10 @@ namespace Content.Server.Medical
         [Dependency] private readonly ForensicsSystem _forensics = default!;
         [Dependency] private readonly BloodstreamSystem _bloodstream = default!;
 
-        private readonly SoundSpecifier _vomitSound = new SoundCollectionSpecifier("Vomit",
+        [ValidatePrototypeId<SoundCollectionPrototype>]
+        private const string VomitCollection = "Vomit";
+
+        private readonly SoundSpecifier _vomitSound = new SoundCollectionSpecifier(VomitCollection,
             AudioParams.Default.WithVariation(0.2f).WithVolume(-4f));
 
         /// <summary>
