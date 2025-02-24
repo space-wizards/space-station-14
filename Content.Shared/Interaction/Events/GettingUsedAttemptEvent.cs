3ef394fe59292a56ec20799da58822a0178fcdd5
ScarKy0 <scarky0@onet.eu>
2025-02-24 11:56:29 +0100

    doc

--- Content.Shared/Interaction/Events/GettingUsedAttemptEvent.cs
+++ Content.Shared/Interaction/Events/GettingUsedAttemptEvent.cs
@@ -1,8 +1,9 @@
 ﻿namespace Content.Shared.Interaction.Events;
 
 /// <summary>
-/// Version of UseAttemptEvent raised on the used item.
+/// Cancellable event raised on used item. Cancelling it cancels the interaction.
 /// </summary>
+/// <param name="user">The user of said item</param>
 public sealed class GettingUsedAttemptEvent(EntityUid user) : CancellableEntityEventArgs
 {
     public EntityUid User = user;
