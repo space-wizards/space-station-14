b5af4678d7a41400295559f8e1805389bee1a7d1
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-02-24 19:14:15 +0300

    Update

--- Content.Server/Bible/BibleSystem.cs
+++ Content.Server/Bible/BibleSystem.cs
@@ -46,16 +46,6 @@ namespace Content.Server.Bible
             SubscribeLocalEvent<FamiliarComponent, GhostRoleSpawnerUsedEvent>(OnSpawned);
         }
 
-        [ValidatePrototypeId<SoundCollectionPrototype>]
-        private const string FamiliarSummonCollection = "FamiliarSummon";
-
-        [ValidatePrototypeId<SoundCollectionPrototype>]
-        private const string BibleHitCollection = "BibleHit";
-
-        private readonly SoundSpecifier _familiarSummon = new SoundCollectionSpecifier(FamiliarSummonCollection, AudioParams.Default.WithVolume(-4f));
-
-        private readonly SoundSpecifier _bibleHit = new SoundCollectionSpecifier(BibleHitCollection);
-
         private readonly Queue<EntityUid> _addQueue = new();
         private readonly Queue<EntityUid> _remQueue = new();
 
@@ -94,7 +84,7 @@ namespace Content.Server.Bible
                 }
                 summonableComp.AlreadySummoned = false;
                 _popupSystem.PopupEntity(Loc.GetString("bible-summon-respawn-ready", ("book", uid)), uid, PopupType.Medium);
-                _audio.PlayPvs(_familiarSummon, uid);
+                _audio.PlayPvs(summonableComp.SummonSound, uid);
                 // Clean up the accumulator and respawn tracking component
                 summonableComp.Accumulator = 0;
                 _remQueue.Enqueue(uid);
@@ -136,7 +126,7 @@ namespace Content.Server.Bible
                     var selfFailMessage = Loc.GetString(component.LocPrefix + "-heal-fail-self", ("target", Identity.Entity(args.Target.Value, EntityManager)), ("bible", uid));
                     _popupSystem.PopupEntity(selfFailMessage, args.User, args.User, PopupType.MediumCaution);
 
-                    _audio.PlayPvs(_bibleHit, args.User);
+                    _audio.PlayPvs(component.BibleHitSound, args.User);
                     _damageableSystem.TryChangeDamage(args.Target.Value, component.DamageOnFail, true, origin: uid);
                     _delay.TryResetDelay((uid, useDelay));
                     return;
