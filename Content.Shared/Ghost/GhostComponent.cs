d76278ba92d999d674fbce81fdd2fca389f5f433
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-02-24 19:31:25 +0300

    Fix

--- Content.Shared/Ghost/GhostComponent.cs
+++ Content.Shared/Ghost/GhostComponent.cs
@@ -87,7 +87,7 @@ public sealed partial class GhostComponent : Component
     /// </summary>
     /// <remarks>Used to allow admins to change ghost colors. Should be removed if the capability to edit existing sprite colors is ever added back.</remarks>
     [DataField("color"), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
-    public Color color = Color.White;
+    public Color Color = Color.White;
 
     [DataField("canReturnToBody"), AutoNetworkedField]
     private bool _canReturnToBody;
