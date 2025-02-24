931db740f16a0c0f1ec437fd67f1406d2a4c3222
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-02-24 17:20:35 +0100

    Update Content.Shared/Interaction/Events/GettingUsedAttemptEvent.cs

--- Content.Shared/Interaction/Events/GettingUsedAttemptEvent.cs
+++ Content.Shared/Interaction/Events/GettingUsedAttemptEvent.cs
@@ -1,7 +1,7 @@
 ﻿namespace Content.Shared.Interaction.Events;
 
 /// <summary>
-/// Cancellable event raised on used item. Cancelling it cancels the interaction.
+/// Event raised on an item when attempting to use it in your hands. Cancelling it stops the interaction.
 /// </summary>
 /// <param name="user">The user of said item</param>
 public sealed class GettingUsedAttemptEvent(EntityUid user) : CancellableEntityEventArgs
