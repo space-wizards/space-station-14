4a11dfbff65a78ba042b9100eb2d529254e00fb1
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-02-24 21:59:11 +0300

    Cleanup

--- Content.IntegrationTests/Tests/Hands/HandTests.cs
+++ Content.IntegrationTests/Tests/Hands/HandTests.cs
@@ -38,7 +38,7 @@ public sealed class HandTests
 
         var entMan = server.ResolveDependency<IEntityManager>();
         var playerMan = server.ResolveDependency<IPlayerManager>();
-        var mapMan = server.ResolveDependency<IMapManager>();
+        var mapSystem = server.ResolveDependency<MapSystem>();
         var sys = entMan.System<SharedHandsSystem>();
         var tSys = entMan.System<TransformSystem>();
 
@@ -69,7 +69,7 @@ public sealed class HandTests
         await pair.RunTicksSync(5);
         Assert.That(hands.ActiveHandEntity, Is.Null);
 
-        await server.WaitPost(() => mapMan.DeleteMap(data.MapId));
+        await server.WaitPost(() => mapSystem.DeleteMap(data.MapId));
         await pair.CleanReturnAsync();
     }
 
@@ -87,7 +87,7 @@ public sealed class HandTests
 
         var entMan = server.ResolveDependency<IEntityManager>();
         var playerMan = server.ResolveDependency<IPlayerManager>();
-        var mapMan = server.ResolveDependency<IMapManager>();
+        var mapSystem = server.ResolveDependency<MapSystem>();
         var sys = entMan.System<SharedHandsSystem>();
         var tSys = entMan.System<TransformSystem>();
         var containerSystem = server.System<SharedContainerSystem>();
@@ -134,7 +134,7 @@ public sealed class HandTests
         Assert.That(hands.ActiveHandEntity, Is.Not.EqualTo(item));
         Assert.That(containerSystem.IsInSameOrNoContainer((player, xform), (item, itemXform)));
 
-        await server.WaitPost(() => mapMan.DeleteMap(map.MapId));
+        await server.WaitPost(() => mapSystem.DeleteMap(map.MapId));
         await pair.CleanReturnAsync();
     }
 }
