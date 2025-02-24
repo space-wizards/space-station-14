e8376b569fc83a6835f609984a640b6675d48272
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-02-24 22:07:42 +0300

    Update

--- Content.IntegrationTests/Tests/CargoTest.cs
+++ Content.IntegrationTests/Tests/CargoTest.cs
@@ -6,12 +6,9 @@ using Content.Server.Cargo.Systems;
 using Content.Server.Nutrition.Components;
 using Content.Server.Nutrition.EntitySystems;
 using Content.Shared.Cargo.Prototypes;
-using Content.Shared.IdentityManagement;
 using Content.Shared.Prototypes;
 using Content.Shared.Stacks;
-using Content.Shared.Tag;
 using Content.Shared.Whitelist;
-using Robust.Client.GameObjects;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
@@ -68,7 +65,7 @@ public sealed class CargoTest
         var testMap = await pair.CreateTestMap();
 
         var entManager = server.ResolveDependency<IEntityManager>();
-        var mapSystem = server.ResolveDependency<MapSystem>();
+        var mapSystem = server.ResolveDependency<SharedMapSystem>();
         var protoManager = server.ResolveDependency<IPrototypeManager>();
         var cargo = entManager.System<CargoSystem>();
 
@@ -152,7 +149,7 @@ public sealed class CargoTest
         var testMap = await pair.CreateTestMap();
 
         var entManager = server.ResolveDependency<IEntityManager>();
-        var mapSystem = server.ResolveDependency<MapSystem>();
+        var mapSystem = server.ResolveDependency<SharedMapSystem>();
         var mapManager = server.ResolveDependency<IMapManager>();
         var protoManager = server.ResolveDependency<IPrototypeManager>();
         var componentFactory = server.ResolveDependency<IComponentFactory>();
