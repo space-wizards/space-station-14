d2417f1730c60ced0d122f8cab138d8c1945fca8
keronshb <keronshb@live.com>
2025-02-26 00:21:02 -0500

    Removes wizard rule ECS, not needed

--- Content.Server/GameTicking/Rules/Components/WizardRuleComponent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Content.Server.GameTicking.Rules.Components;
-
-[RegisterComponent, Access(typeof(WizardRuleSystem))]
-public sealed partial class WizardRuleComponent : Component
-{
-    // TODO: Any rule related info like minds, Protos, Factions? Starting gear?
-    //  Death Match has starting gear, thiefs and traitors don't in these comps
-}
