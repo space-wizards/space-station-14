41c51e29052c16f092e6941f7e9ccfd14dfb3dae
SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
2025-02-27 18:57:28 +0100

    Implanter draw rework (#32136)
    * Initial commit
    
    * Clean-up
    
    * Fix ftl, new damage
    
    * ftl fix for real
    
    * Updates based on feedback
    
    * Child implant fix
    
    * Make the UI only open when implanter is in draw mode
    
    * Review fixes
    
    * shunting

--- Content.Server/Implants/ImplanterSystem.cs
+++ Content.Server/Implants/ImplanterSystem.cs
@@ -68,8 +68,6 @@ public sealed partial class ImplanterSystem : SharedImplanterSystem
         args.Handled = true;
     }
 
-
-
     /// <summary>
     /// Attempt to implant someone else.
     /// </summary>
