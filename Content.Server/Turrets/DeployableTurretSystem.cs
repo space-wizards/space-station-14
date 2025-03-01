e9c85522d82e7f893ff355d2d28eae88120785e5
chromiumboy <chromium.boy@gmail.com>
2025-03-01 12:29:39 -0600

    Removed turret control content

--- Content.Server/Turrets/DeployableTurretSystem.cs
+++ Content.Server/Turrets/DeployableTurretSystem.cs
@@ -1,25 +1,18 @@
+using Content.Server.DeviceNetwork;
+using Content.Server.DeviceNetwork.Components;
+using Content.Server.DeviceNetwork.Systems;
 using Content.Server.NPC.HTN;
+using Content.Server.NPC.HTN.PrimitiveTasks.Operators.Combat.Ranged;
 using Content.Server.Power.Components;
 using Content.Server.Repairable;
 using Content.Shared.Destructible;
+using Content.Shared.DeviceNetwork;
+using Content.Shared.Power;
 using Content.Shared.Turrets;
 using Content.Shared.Weapons.Ranged.Events;
-using Robust.Shared.Timing;
-using Content.Shared.Power;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
-using Content.Server.DeviceNetwork.Systems;
-using Content.Server.DeviceNetwork;
-using Content.Shared.DeviceNetwork;
-using Content.Server.DeviceNetwork.Components;
-using Content.Server.NPC.HTN.PrimitiveTasks.Operators.Combat.Ranged;
-using Content.Shared.Weapons.Ranged.Systems;
-
-// The following will be uncommented by the turret control panel PR
-//using Content.Server.TurretController;  
-//using Content.Shared.Weapons.Ranged.Components;
-//using Content.Shared.Access;
-//using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
 
 namespace Content.Server.Turrets;
 
@@ -29,8 +22,6 @@ public sealed partial class DeployableTurretSystem : SharedDeployableTurretSyste
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly DeviceNetworkSystem _deviceNetwork = default!;
-    [Dependency] private readonly BatteryWeaponFireModesSystem _fireModes = default!;
-    [Dependency] private readonly TurretTargetSettingsSystem _turretTargetingSettings = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
 
     public override void Initialize()
@@ -42,7 +33,6 @@ public sealed partial class DeployableTurretSystem : SharedDeployableTurretSyste
         SubscribeLocalEvent<DeployableTurretComponent, PowerChangedEvent>(OnPowerChanged);
         SubscribeLocalEvent<DeployableTurretComponent, BreakageEventArgs>(OnBroken);
         SubscribeLocalEvent<DeployableTurretComponent, RepairedEvent>(OnRepaired);
-        //SubscribeLocalEvent<DeployableTurretComponent, DeviceNetworkPacketEvent>(OnPacketReceived); // Will be uncommented by the turret control panel PR
         SubscribeLocalEvent<DeployableTurretComponent, BeforeBroadcastAttemptEvent>(OnBeforeBroadcast);
     }
 
@@ -86,40 +76,6 @@ public sealed partial class DeployableTurretSystem : SharedDeployableTurretSyste
         if (TryComp<AppearanceComponent>(ent, out var appearance))
             _appearance.SetData(ent, DeployableTurretVisuals.Broken, false, appearance);
     }
-
-    /* The following Will be uncommented by the turret control panel PR
-    private void OnPacketReceived(Entity<DeployableTurretComponent> ent, ref DeviceNetworkPacketEvent args)
-    {
-        if (!args.Data.TryGetValue(DeviceNetworkConstants.Command, out string? command))
-            return;
-
-        // Received a command to change armament state
-        if (command == DeployableTurretControllerSystem.CmdSetArmamemtState &&
-            args.Data.TryGetValue(command, out int? armamentState))
-        {
-            if (TryComp<BatteryWeaponFireModesComponent>(ent, out var batteryWeaponFireModes))
-                _fireModes.TrySetFireMode(ent, batteryWeaponFireModes, armamentState.Value);
-
-            TrySetState(ent, armamentState.Value >= 0);
-            return;
-        }
-
-        // Received a command to change access exemptions
-        if (command == DeployableTurretControllerSystem.CmdSetAccessExemptions &&
-            args.Data.TryGetValue(command, out HashSet<ProtoId<AccessLevelPrototype>>? accessExemptions) &&
-            TryComp<TurretTargetSettingsComponent>(ent, out var turretTargetSettings))
-        {
-            _turretTargetingSettings.SyncAccessLevelExemptions((ent, turretTargetSettings), accessExemptions);
-            return;
-        }
-
-        // Received a command to update the device network
-        if (command == DeviceNetworkConstants.CmdUpdatedState)
-        {
-            SendStateUpdateToDeviceNetwork(ent);
-            return;
-        }
-    }*/
 
     private void OnBeforeBroadcast(Entity<DeployableTurretComponent> ent, ref BeforeBroadcastAttemptEvent args)
     {
