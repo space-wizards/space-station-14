40fc2cde2bbc4bdc9ce3096100a40309671fed4e
Tayrtahn <tayrtahn@gmail.com>
2025-03-02 14:28:49 -0500

    Don't force disable dev config for integration tests

--- Content.IntegrationTests/PoolManager.Cvars.cs
+++ Content.IntegrationTests/PoolManager.Cvars.cs
@@ -33,7 +33,6 @@ public static partial class PoolManager
         (CVars.ReplayServerRecordingEnabled.Name, "false"),
         (CCVars.GameDummyTicker.Name, "true"),
         (CCVars.GameLobbyEnabled.Name, "false"),
-        (CCVars.ConfigPresetDevelopment.Name, "false"),
         (CCVars.AdminLogsEnabled.Name, "false"),
         (CCVars.AutosaveEnabled.Name, "false"),
         (CVars.NetBufferSize.Name, "0"),
