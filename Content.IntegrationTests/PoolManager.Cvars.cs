4fcfbc118d36ab92c9868caeca9d691877540c22
Tayrtahn <tayrtahn@gmail.com>
2025-03-02 18:14:54 -0500

    Force on instead?

--- Content.IntegrationTests/PoolManager.Cvars.cs
+++ Content.IntegrationTests/PoolManager.Cvars.cs
@@ -33,6 +33,7 @@ public static partial class PoolManager
         (CVars.ReplayServerRecordingEnabled.Name, "false"),
         (CCVars.GameDummyTicker.Name, "true"),
         (CCVars.GameLobbyEnabled.Name, "false"),
+        (CCVars.ConfigPresetDevelopment.Name, "true"),
         (CCVars.AdminLogsEnabled.Name, "false"),
         (CCVars.AutosaveEnabled.Name, "false"),
         (CVars.NetBufferSize.Name, "0"),
