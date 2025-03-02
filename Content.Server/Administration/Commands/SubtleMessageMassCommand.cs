1b4c84e86c204203f231ff4edbd73201dd5b1455
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-03-02 23:38:47 +0300

    init

---
+++ Content.Server/Administration/Commands/SubtleMessageMassCommand.cs
@@ -0,0 +1,96 @@
+using System.Linq;
+using Content.Server.Administration.Managers;
+using Content.Shared.Administration;
+using Robust.Server.Player;
+using Robust.Shared.Configuration;
+using Robust.Shared.Console;
+using Content.Server.Prayer;
+
+
+namespace Content.Server.Administration.Commands;
+
+[AdminCommand(AdminFlags.Admin)]
+public sealed class SubtleMessageMassCommand : LocalizedEntityCommands
+{
+    [Dependency] private readonly IPlayerLocator _locator = default!;
+    [Dependency] private readonly IBanManager _bans = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly PrayerSystem _prayerSystem = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+
+    public override string Command => "massmsg";
+
+    public override async void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length < 3)
+        {
+            shell.WriteLine(Loc.GetString("cmd-ban-invalid-arguments"));
+            shell.WriteLine(Help);
+            return;
+        }
+
+        var player = shell.Player;
+        if (player == null)
+        {
+            shell.WriteLine("You cannot use this command from the server console.");
+            return;
+        }
+
+        var message = args[0];
+        var popupMessage = args[1];
+
+        string[]? allTargets;
+        if (args[2] == "all")
+        {
+            allTargets = CompletionHelper.SessionNames()
+                                         .Select(option => option.Value)
+                                         .ToArray();
+        }
+        else
+        {
+            allTargets = args.Skip(2).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToArray();
+        }
+
+        foreach (var target in allTargets)
+        {
+            var trimmedTarget = target.Trim();
+            if (string.IsNullOrWhiteSpace(trimmedTarget))
+                continue;
+
+            var located = await _locator.LookupIdByNameOrIdAsync(trimmedTarget);
+
+            if (located == null)
+            {
+                shell.WriteError(Loc.GetString("cmd-ban-player"));
+                continue;
+            }
+
+            var targetSession = _playerManager.GetSessionById(located.UserId);
+            _prayerSystem.SendSubtleMessage(targetSession, player, message, popupMessage);
+        }
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        if (args.Length >= 3)
+            return CompletionResult.FromHintOptions(
+                CompletionHelper.SessionNames(),
+                "username/all"
+            );
+
+        if (args.Length == 1)
+            return CompletionResult.FromHint("message");
+
+        if (args.Length == 2)
+        {
+            var option = new CompletionOption[]
+            {
+                new(Loc.GetString("prayer-popup-subtle-default"), LocalizationManager.GetString("default")),
+            };
+
+            return CompletionResult.FromHintOptions(option, "popupMessage");
+        }
+
+        return CompletionResult.Empty;
+    }
+}
---
+++ Resources/Locale/en-US/administration/commands/subtle-message-mass-command.ftl
@@ -0,0 +1,3 @@
+# command wdwd
+massmsg-command-description = Sends a message
+massmsg-command-help-text = Usage: {$command} <message> <popurMessage> all/Nickname
