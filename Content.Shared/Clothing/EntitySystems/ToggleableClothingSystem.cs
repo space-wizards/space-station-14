a45653f2b3403ab5725f560027d6f3e3b44f27d2
Kyle Tyo <akikai297@gmail.com>
2025-03-02 04:12:44 -0500

    Revert "cleanup"
    This reverts commit e8ef2d76fef9dd7fd54b85464d1e977e83bca7db.

--- Content.Shared/Clothing/EntitySystems/ToggleableClothingSystem.cs
+++ Content.Shared/Clothing/EntitySystems/ToggleableClothingSystem.cs
@@ -56,6 +56,7 @@ public sealed class ToggleableClothingSystem : EntitySystem
     {
         if (ent.Comp.Container == null || ent.Comp.ClothingUid == null)
             return;
+        
         if (ent.Comp.Container.ContainedEntity == null)
             _inventorySystem.TryUnequip(args.Args.User, ent.Comp.Slot, force: true);
     }
