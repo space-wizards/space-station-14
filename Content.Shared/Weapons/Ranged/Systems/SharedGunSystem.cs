4d4f01acd757425a1416fc699c7795e4be0c9d38
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-03-02 16:24:34 +0100

    simplify

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -350,14 +350,7 @@ public abstract partial class SharedGunSystem : EntitySystem
             // If they're firing an existing clip then don't play anything.
             if (shots > 0)
             {
-                if (ev.Reason != null && Timing.IsFirstTimePredicted)
-                {
-                    PopupSystem.PopupCursor(ev.Reason);
-                }
-                else
-                {
-                    PopupSystem.PopupCursor(Loc.GetString("gun-magazine-fired-empty"));
-                }
+                PopupSystem.PopupCursor(ev.Reason ?? Loc.GetString("gun-magazine-fired-empty"));
 
                 // Don't spam safety sounds at gun fire rate, play it at a reduced rate.
                 // May cause prediction issues? Needs more tweaking
