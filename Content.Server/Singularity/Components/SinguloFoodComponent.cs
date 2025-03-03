6013f8acabd66ab06c9cbbc6a9785ad46bba0ab2
if23b266 <if23b266@technikum-wien.at>
2025-03-03 11:19:51 +0100

    Simplify SinguloFoodComponent and set percentageDrain to negative

--- Content.Server/Singularity/Components/SinguloFoodComponent.cs
+++ Content.Server/Singularity/Components/SinguloFoodComponent.cs
@@ -6,16 +6,14 @@ namespace Content.Server.Singularity.Components
     [RegisterComponent]
     public sealed partial class SinguloFoodComponent : Component
     {
-        [ViewVariables(VVAccess.ReadWrite)]
         [DataField("energy")]
-        public float Energy { get; set; } = 1f;
+        public float Energy = 1f;
 
         /// <summary>
         /// The percentage of the singularity's current energy that this food will drain.
         /// Only applies when Energy is negative.
         /// </summary>
-        [ViewVariables(VVAccess.ReadWrite)]
         [DataField("percentageDrain")]
-        public float PercentageDrain { get; set; }
+        public float PercentageDrain = -0.03f;
     }
 }
