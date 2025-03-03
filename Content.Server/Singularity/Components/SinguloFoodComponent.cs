62e37b98e06295b43d9638d149a3550f46b0ef4d
if23b266 <if23b266@technikum-wien.at>
2025-03-03 17:19:57 +0100

    Documentation of EnergyFactor

--- Content.Server/Singularity/Components/SinguloFoodComponent.cs
+++ Content.Server/Singularity/Components/SinguloFoodComponent.cs
@@ -13,6 +13,9 @@ namespace Content.Server.Singularity.Components
         /// Multiplier applied to singularity's energy.
         /// 1.0 = no change, 0.97 = 3% reduction, 1.05 = 5% increase
         /// </summary>
+        /// /// <remarks>
+        /// This is calculated using the singularity's energy level before <see cref="Energy"/> has been added.
+        /// </remarks>
         [DataField]
         public float EnergyFactor = 1f;
     }
