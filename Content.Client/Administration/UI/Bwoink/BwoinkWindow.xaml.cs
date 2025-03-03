d365ed8ffa875011c8e873b048d0f860f0b4d1b1
Errant <35878406+Errant-4@users.noreply.github.com>
2025-03-03 07:19:19 +0100

    Revert "[HOTFIX] - Players with unknown playtimes now are tagged as new playe…"
    This reverts commit 4dfd3e5740d316d1c37d38623b8266333bef0945.

--- Content.Client/Administration/UI/Bwoink/BwoinkWindow.xaml.cs
+++ Content.Client/Administration/UI/Bwoink/BwoinkWindow.xaml.cs
@@ -22,9 +22,12 @@ namespace Content.Client.Administration.UI.Bwoink
                     return;
                 }
 
-                Title = $"{sel.CharacterName} / {sel.Username} | {Loc.GetString("generic-playtime-title")}: ";
+                Title = $"{sel.CharacterName} / {sel.Username}";
 
-                Title += sel.OverallPlaytime != null ? sel.PlaytimeString : Loc.GetString("generic-unknown-title");
+                if (sel.OverallPlaytime != null)
+                {
+                    Title += $" | {Loc.GetString("generic-playtime-title")}: {sel.PlaytimeString}";
+                }
             };
 
             OnOpen += () =>
