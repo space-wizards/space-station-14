6f78991055d02a542c848af376eb3f15454e657a
Velken <8467292+Velken@users.noreply.github.com>
2025-03-03 22:36:19 -0300

    fix

--- Content.Server/AlertLevel/AlertLevelChangeOnTriggerComponent.cs
+++ Content.Server/AlertLevel/AlertLevelChangeOnTriggerComponent.cs
@@ -1,17 +1,15 @@
 using Content.Server.AlertLevel.Systems;
 
-namespace Content.Server.AlertLevel.Systems
+namespace Content.Server.AlertLevel;
+/// <summary>
+/// This component is for changing the alert level of the station when triggered.
+/// </summary>
+[RegisterComponent, Access(typeof(AlertLevelChangeOnTriggerSystem))]
+public sealed partial class AlertLevelChangeOnTriggerComponent : Component
 {
-    /// <summary>
-    /// This component is for changing the alert level of the station when triggered.
-    /// </summary>
-    [RegisterComponent, Access(typeof(AlertLevelChangeOnTriggerSystem))]
-    public sealed partial class AlertLevelChangeOnTriggerComponent : Component
-    {
-        ///<summary>
-        ///The alert level to change to when triggered.
-        ///</summary>
-        [DataField]
-        public string Level = "blue";
-    }
+    ///<summary>
+    ///The alert level to change to when triggered.
+    ///</summary>
+    [DataField]
+    public string Level = "blue";
 }
