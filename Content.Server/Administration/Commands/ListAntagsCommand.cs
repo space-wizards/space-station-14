7438c9671176b992e3f1d80daf1c4dd69ba351a9
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-03-05 23:51:54 +0300

    reworking. Add true/false show and not show list objectives for antags

--- Content.Server/Administration/Commands/ListAntagsCommand.cs
+++ Content.Server/Administration/Commands/ListAntagsCommand.cs
@@ -1,8 +1,14 @@
 using System.Linq;
 using Content.Shared.Administration;
 using Content.Shared.Mind;
+using Content.Shared.Objectives.Systems;
 using Content.Shared.Roles;
 using Robust.Shared.Console;
+using Content.Server.Administration;
+using Content.Shared.Administration;
+using Content.Shared.Mind;
+using Content.Shared.Objectives.Systems;
+using Robust.Server.Player;
 
 namespace Content.Server.Administration.Commands;
 
@@ -10,49 +16,86 @@ namespace Content.Server.Administration.Commands;
 public sealed class ListAntagsCommand : LocalizedEntityCommands
 {
     [Dependency] private readonly IEntityManager _entityManager = default!;
-    [Dependency] private readonly SharedRoleSystem _role = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] private readonly SharedObjectivesSystem _sharedObjectivesSystem = default!;
+    [Dependency] private readonly SharedRoleSystem _sharedRoleSystem = default!;
 
     public override string Command => "lsantags";
     public override string Description => Loc.GetString("lsantags-command-description");
 
     public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
+        bool showObjectives = args.Length > 0 && args[0].ToLower() == "true";
         var antagList = new List<string>();
-
-        // We get all players who have mind
         var allMinds = _entityManager.EntityQueryEnumerator<MindComponent>();
 
         while (allMinds.MoveNext(out var mindId, out var mind))
         {
-            if (!_role.MindIsAntagonist(mindId))
+            if (!_sharedRoleSystem.MindIsAntagonist(mindId))
                 continue;
 
-            // Getting information about the player
             var playerName = mind.Session?.Name ?? Loc.GetString("lsantags-command-unknown");
-            var icName = mind.CharacterName ?? Loc.GetString("santags-command-nameless");
-            var antagRoles = _role.MindGetAllRoleInfo(mindId)
+            var icName = mind.CharacterName ?? Loc.GetString("lsantags-command-nameless");
+            var antagRoles = _sharedRoleSystem.MindGetAllRoleInfo(mindId)
                 .Where(role => role.Antagonist)
                 .Select(role => Loc.GetString(role.Name))
                 .ToList();
-
             var antagRolesStr = string.Join(", ", antagRoles);
             var entityInfo = _entityManager.ToPrettyString(mindId);
 
-            antagList.Add(Loc.GetString(
-                "lsantags-command-list-info",
-                ("playerName", playerName),
-                ("entityInfo", entityInfo),
-                ("antagRoles", antagRolesStr)
-            ));
+            var antagInfo = $"{playerName} ({icName}) {entityInfo} - Roles: {antagRolesStr}";
+
+            if (showObjectives)
+            {
+                var objectives = mind.Objectives.ToList();
+                if (objectives.Count > 0)
+                {
+                    antagInfo += "\n  Objectives:";
+                    foreach (var obj in objectives)
+                    {
+                        var info = _sharedObjectivesSystem.GetInfo(obj, mindId, mind);
+                        if (info != null)
+                        {
+                            var progress = (int)(info.Value.Progress * 100f);
+                            antagInfo += $"\n  - {info.Value.Title} ({progress}%)";
+                        }
+                        else
+                        {
+                            antagInfo += "\n  - INVALID OBJECTIVE";
+                        }
+                    }
+                }
+                else
+                {
+                    antagInfo += "\n  No objectives.";
+                }
+            }
+
+            antagList.Add(antagInfo);
         }
 
         if (antagList.Count == 0)
         {
             shell.WriteLine(Loc.GetString("lsantags-command-no-antags"));
+            return;
         }
-        else
+
+        shell.WriteLine($"{Loc.GetString("lsantags-command-list-antags")}\n{string.Join("\n", antagList)}");
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        if (args.Length == 1)
         {
-            shell.WriteLine($"{Loc.GetString("lsantags-command-list-antags")}\n{string.Join("\n", antagList)}");
+            var option = new CompletionOption[]
+            {
+                new("true", Loc.GetString("lsantags-command-auto-completion-true")),
+                new("false", Loc.GetString("lsantags-command-auto-completion-false"))
+            };
+
+            return CompletionResult.FromHintOptions(option, Loc.GetString("lsantags-command-auto-completion"));
         }
+
+        return CompletionResult.Empty;
     }
 }
--- Resources/Locale/en-US/administration/commands/list-antags-command.ftl
+++ Resources/Locale/en-US/administration/commands/list-antags-command.ftl
@@ -6,3 +6,6 @@ lsantags-command-list-antags = List of antagonists:
 lsantags-command-unknown = Unknown
 lsantags-command-nameless = Nameless
 lsantags-command-list-info = { $playerName } { $entityInfo } - Roles: { $antagRoles }
+lsantags-command-auto-completion = Show antag targets or just list?
+lsantags-command-auto-completion-true = Provides a list of targets for each antagonist.
+lsantags-command-auto-completion-false = Will simply output a list of antags.
