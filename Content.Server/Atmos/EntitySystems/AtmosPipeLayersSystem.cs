47e4d2054845757beccadc2082a434b2eb02d9fe
chromiumboy <chromium.boy@gmail.com>
2025-03-05 22:23:48 -0600

    Moving a pipe to an overlapping layer causes unanchoring

--- Content.Server/Atmos/EntitySystems/AtmosPipeLayersSystem.cs
+++ Content.Server/Atmos/EntitySystems/AtmosPipeLayersSystem.cs
@@ -1,15 +1,20 @@
+using Content.Server.Atmos.Components;
 using Content.Server.NodeContainer;
 using Content.Server.NodeContainer.EntitySystems;
 using Content.Server.NodeContainer.NodeGroups;
 using Content.Server.NodeContainer.Nodes;
 using Content.Shared.Atmos.Components;
 using Content.Shared.Atmos.EntitySystems;
+using Content.Shared.Popups;
 
 namespace Content.Server.Atmos.EntitySystems;
 
 public sealed partial class AtmosPipeLayersSystem : SharedAtmosPipeLayersSystem
 {
     [Dependency] private readonly NodeGroupSystem _nodeGroup = default!;
+    [Dependency] private readonly PipeRestrictOverlapSystem _pipeRestrictOverlap = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedTransformSystem _xform = default!;
 
     public override void Initialize()
     {
@@ -23,7 +28,7 @@ public sealed partial class AtmosPipeLayersSystem : SharedAtmosPipeLayersSystem
         SetPipeLayer(ent, ent.Comp.CurrentPipeLayer);
     }
 
-    public override void SetPipeLayer(Entity<AtmosPipeLayersComponent> ent, int layer)
+    public override void SetPipeLayer(Entity<AtmosPipeLayersComponent> ent, int layer, EntityUid? user = null)
     {
         if (ent.Comp.PipeLayersLocked)
             return;
@@ -33,6 +38,7 @@ public sealed partial class AtmosPipeLayersSystem : SharedAtmosPipeLayersSystem
         if (!TryComp<NodeContainerComponent>(ent, out var nodeContainer))
             return;
 
+        // Update the layer values of all pipe nodes associated with the entity
         foreach (var (id, node) in nodeContainer.Nodes)
         {
             if (node is not PipeNode { } pipeNode)
@@ -45,6 +51,18 @@ public sealed partial class AtmosPipeLayersSystem : SharedAtmosPipeLayersSystem
 
             if (pipeNode.NodeGroup != null)
                 _nodeGroup.QueueRemakeGroup((BaseNodeGroup)pipeNode.NodeGroup);
+        }
+
+        // Unanchor the pipe if the new layer overlaps with an existing one
+        var xform = Transform(ent);
+
+        if (HasComp<PipeRestrictOverlapComponent>(ent) &&
+            _pipeRestrictOverlap.CheckOverlap((ent, nodeContainer, xform)))
+        {
+            if (user != null)
+                _popup.PopupEntity(Loc.GetString("pipe-restrict-overlap-popup-blocked", ("pipe", ent)), ent, user.Value);
+
+            _xform.Unanchor(ent, xform);
         }
     }
 }
