fb33c48a9518179f86b62f64fc117809fafeee7f
qwerltaz <msmarcinpl@gmail.com>
2025-03-06 19:23:51 +0100

    crew manifest

--- Content.Server/Speech/EntitySystems/RandomAccentuator.cs
+++ Content.Server/Speech/EntitySystems/RandomAccentuator.cs
@@ -7,16 +7,16 @@ public static class RandomAccentuator
 {
     private const float DefaultAccentuationChance = 1.0f;
 
-    private const float ReaccentuationChance = 0.5f;
+    private const float DefaultReaccentuationChance = 0.5f;
 
-    public static string MaybeAccentuate(string message, float chance = DefaultAccentuationChance)
+    public static string MaybeAccentuate(string message, float chance = DefaultAccentuationChance, float reaccentuationChance = DefaultReaccentuationChance)
     {
         var random = IoCManager.Resolve<IRobustRandom>();
         var singleAccentuator = new SingleAccentuator();
-        return !random.Prob(chance) ? message : MaybeAccentuateInternal(message, singleAccentuator, random);
+        return !random.Prob(chance) ? message : MaybeAccentuateInternal(message, singleAccentuator, random, reaccentuationChance);
     }
 
-    public static FormattedMessage MaybeAccentuate(FormattedMessage message, float chance = DefaultAccentuationChance)
+    public static FormattedMessage MaybeAccentuate(FormattedMessage message, float chance = DefaultAccentuationChance, float reaccentuationChance = DefaultReaccentuationChance)
     {
         var random = IoCManager.Resolve<IRobustRandom>();
         if (!random.Prob(chance))
@@ -30,7 +30,7 @@ public static class RandomAccentuator
         {
             if (node.Name is null && node.Value.TryGetString(out var text))
             {
-                var accentedText = MaybeAccentuateInternal(text, singleAccentuator, random);
+                var accentedText = MaybeAccentuateInternal(text, singleAccentuator, random, reaccentuationChance);
                 newMessage.PushTag(new MarkupNode(accentedText));
             }
             else
@@ -42,9 +42,9 @@ public static class RandomAccentuator
         return newMessage;
     }
 
-    private static string MaybeAccentuateInternal(string message, SingleAccentuator singleAccentuator, IRobustRandom random)
+    private static string MaybeAccentuateInternal(string message, SingleAccentuator singleAccentuator, IRobustRandom random, float reaccentuationChance)
     {
-        for (var i = 0; i < 3 && random.Prob(ReaccentuationChance); i++)
+        for (var i = 0; i < 3 && random.Prob(reaccentuationChance); i++)
         {
             singleAccentuator.NextSystem();
             message = singleAccentuator.Accentuate(message);
