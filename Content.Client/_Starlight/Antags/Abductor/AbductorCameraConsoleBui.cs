d89cc31629b6bda6891f357c511312a1a9c94003
starlight <ss14-Starlight@outlook.com>
2025-03-08 09:58:07 +0100

    minor fix

--- Content.Client/_Starlight/Antags/Abductor/AbductorCameraConsoleBui.cs
+++ Content.Client/_Starlight/Antags/Abductor/AbductorCameraConsoleBui.cs
@@ -13,7 +13,12 @@ public sealed class AbductorCameraConsoleBui : BoundUserInterface
     public AbductorCameraConsoleBui(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
     }
-    protected override void Open() => UpdateState(State);
+    protected override void Open()
+    {
+        base.Open();
+        UpdateState(State);
+    }
+
     protected override void UpdateState(BoundUserInterfaceState? state)
     {
         if (state is AbductorCameraConsoleBuiState s)
