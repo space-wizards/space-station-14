d3d1dd1a02ac1c1805f263c66c3c6b16c7600efa
Winkarst <74284083+Winkarst-cpu@users.noreply.github.com>
2025-03-10 17:41:11 +0300

    Revert

--- Content.Shared/Paper/PaperSystem.cs
+++ Content.Shared/Paper/PaperSystem.cs
@@ -9,7 +9,6 @@ using Content.Shared.Tag;
 using Robust.Shared.Player;
 using Robust.Shared.Audio.Systems;
 using static Content.Shared.Paper.PaperComponent;
-using Content.Shared.Hands.EntitySystems;
 
 namespace Content.Shared.Paper;
 
@@ -17,7 +16,7 @@ public sealed class PaperSystem : EntitySystem
 {
     [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
-    [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
+    [Dependency] private readonly SharedInteractionSystem _interaction = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
     [Dependency] private readonly TagSystem _tagSystem = default!;
     [Dependency] private readonly SharedUserInterfaceSystem _uiSystem = default!;
@@ -200,7 +199,7 @@ public sealed class PaperSystem : EntitySystem
 
     private void OnPaperWrite(Entity<ActivateOnPaperOpenedComponent> entity, ref PaperWriteEvent args)
     {
-        _handsSystem.TryUseItemInHand(args.User);
+        _interaction.UseInHandInteraction(args.User, entity);
     }
 
     /// <summary>
