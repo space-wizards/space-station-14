50abf655cf0fcbb784dd148c3a82ccbc83589742
Scyarah <sarah@gegner.me>
2025-03-17 20:00:41 +0100

    Fixed bug that returned true when requiredTags was an empty array

--- Content.Shared/Clothing/EntitySystems/SharedChameleonClothingSystem.cs
+++ Content.Shared/Clothing/EntitySystems/SharedChameleonClothingSystem.cs
@@ -141,7 +141,7 @@ public abstract class SharedChameleonClothingSystem : EntitySystem
         if (!proto.TryGetComponent(out TagComponent? tag, _factory) || !_tag.HasTag(tag, WhitelistChameleonTag))
             return false;
 
-        if (requiredTags != null && requiredTags.All(requiredTag => !_tag.HasTag(tag, requiredTag)))
+        if (requiredTags != null && requiredTags.Any() && requiredTags.All(requiredTag => !_tag.HasTag(tag, requiredTag)))
             return false;
 
         // check if it's valid clothing
