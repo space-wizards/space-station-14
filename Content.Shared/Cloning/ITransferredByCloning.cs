51d7bee9e73608d3bd02c28b1f5f745827921d8b
beck-thompson <beck314159@hotmail.com>
2025-03-19 21:24:49 -0700

    Inital cleanup

--- Content.Shared/Cloning/ITransferredByCloning.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Shared.Cloning;
-
-/// <summary>
-///   Indicates that this Component should be transferred to the new entity when the entity is cloned (for example, using a cloner)
-/// </summary>
-public interface ITransferredByCloning
-{
-}
-
