014a017488029c6869ab3028067ab41f7033c731
ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
2025-03-20 11:09:16 -0700

    change component to file scoped namespace

--- Content.Server/Chemistry/Components/VaporComponent.cs
+++ Content.Server/Chemistry/Components/VaporComponent.cs
@@ -1,26 +1,25 @@
 ﻿using Robust.Shared.Map;
 
-namespace Content.Server.Chemistry.Components
+namespace Content.Server.Chemistry.Components;
+
+[RegisterComponent]
+public sealed partial class VaporComponent : Component
 {
-    [RegisterComponent]
-    public sealed partial class VaporComponent : Component
-    {
-        public const string SolutionName = "vapor";
+    public const string SolutionName = "vapor";
 
-        /// <summary>
-        /// Stores data on the previously reacted tile. We only want to do reaction checks once per tile.
-        /// </summary>
-        [DataField]
-        public TileRef? PreviousTileRef;
+    /// <summary>
+    /// Stores data on the previously reacted tile. We only want to do reaction checks once per tile.
+    /// </summary>
+    [DataField]
+    public TileRef? PreviousTileRef;
 
-        /// <summary>
-        /// Percentage of the solution that is reacted with the TileReaction.
-        /// Ex: 0.5 = 50% reacts.
-        /// </summary>
-        [DataField]
-        public float TransferAmountPercentage;
+    /// <summary>
+    /// Percentage of the solution that is reacted with the TileReaction.
+    /// Ex: 0.5 = 50% reacts.
+    /// </summary>
+    [DataField]
+    public float TransferAmountPercentage;
 
-        [DataField]
-        public bool Active;
-    }
+    [DataField]
+    public bool Active;
 }
