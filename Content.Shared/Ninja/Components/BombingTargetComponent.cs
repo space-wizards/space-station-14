f388ac8239adc3be0bc44f1c3de153ebd606069a
Dinner <180707738+DinnerCalzone@users.noreply.github.com>
2025-03-22 11:12:18 -0400

    Decouple WarpPointComponent from ninja's BombingTargetComponent to remove duplicate ghost warp points (#2035)
    * decouple warp point components from bombing target
    
    * fix luna bombing targets
    
    * fixgridatmos

--- Content.Shared/Ninja/Components/BombingTargetComponent.cs
+++ Content.Shared/Ninja/Components/BombingTargetComponent.cs
@@ -1,7 +1,15 @@
-namespace Content.Shared.Ninja.Components;
-
-/// <summary>
-/// Makes this warp point a valid bombing target for ninja's spider charge.
-/// </summary>
-[RegisterComponent]
-public sealed partial class BombingTargetComponent : Component;
+namespace Content.Shared.Ninja.Components
+{
+    /// <summary>
+    /// Makes this warp point a valid bombing target for ninja's spider charge.
+    /// </summary>
+    [RegisterComponent]
+    public sealed partial class BombingTargetComponent : Component
+    {
+        /// <summary>
+        /// Imp addition. Text used in the ninja's objectives for the location of the bombing target.
+        /// </summary>
+        [ViewVariables(VVAccess.ReadWrite), DataField]
+        public string? Location;
+    }
+}
