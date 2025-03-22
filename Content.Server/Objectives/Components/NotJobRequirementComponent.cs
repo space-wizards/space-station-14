88f60595544574f45a2d4ed98e616bc7da0adbde
Dinner <180707738+DinnerCalzone@users.noreply.github.com>
2025-03-22 11:14:53 -0400

    Allow multiple jobs to be blacklisted from thief objective (#2054)
    * allow multiple jobs to be blacklisted from thief objective
    
    * array serialization

--- Content.Server/Objectives/Components/NotJobRequirementComponent.cs
+++ Content.Server/Objectives/Components/NotJobRequirementComponent.cs
@@ -1,7 +1,7 @@
 using Content.Server.Objectives.Systems;
 using Content.Shared.Roles;
 using Content.Shared.Roles.Jobs;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array; //imp edit
 
 /// <summary>
 /// Requires that the player not have a certain job to have this objective.
@@ -12,6 +12,6 @@ public sealed partial class NotJobRequirementComponent : Component
     /// <summary>
     /// ID of the job to ban from having this objective.
     /// </summary>
-    [DataField(required: true, customTypeSerializer: typeof(PrototypeIdSerializer<JobPrototype>))]
-    public string Job = string.Empty;
+    [DataField(required: true, customTypeSerializer: typeof(PrototypeIdArraySerializer<JobPrototype>))] // imp edit
+    public string[] Job; // imp edit, allows multiple jobs to be blacklisted
 }
