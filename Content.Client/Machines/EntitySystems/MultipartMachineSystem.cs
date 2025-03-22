fc60860ac7e4dbd76f4d58bbed495b34b6401cdf
BarryNorfolk <barrynorfolkman@protonmail.com>
2025-03-22 08:34:07 +0100

    Limit the number of ghosts spawnable by machine examinations

--- Content.Client/Machines/EntitySystems/MultipartMachineSystem.cs
+++ Content.Client/Machines/EntitySystems/MultipartMachineSystem.cs
@@ -1,8 +1,10 @@
 using Content.Client.Examine;
+using Content.Client.Machines.Components;
 using Content.Shared.Machines.Components;
 using Robust.Client.GameObjects;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Spawners;
 
 namespace Content.Client.Machines.EntitySystems;
 
@@ -21,17 +23,25 @@ public sealed class MultipartMachineSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<MultipartMachineComponent, ClientExaminedEvent>(OnMachineExamined);
+        SubscribeLocalEvent<MultipartMachineGhostComponent, TimedDespawnEvent>(OnGhostDespawned);
     }
 
     /// <summary>
     /// Handles spawning several ghost sprites to show where the different parts of the machine
     /// should go and the rotations they're expected to have.
-    /// Depends on the current map location of the origin machine.
+    /// Can only show one set of ghost parts at a time and their location depends on the current map/grid
+    /// location of the origin machine.
     /// </summary>
     /// <param name="ent">Entity/Component that has been inspected</param
     /// <param name="args">Args for the event</param>
     private void OnMachineExamined(Entity<MultipartMachineComponent> ent, ref ClientExaminedEvent args)
     {
+        if (ent.Comp.Ghosts.Count != 0)
+        {
+            // Already showing some part ghosts
+            return;
+        }
+
         var xformQuery = GetEntityQuery<TransformComponent>();
         foreach (var part in ent.Comp.Parts.Values)
         {
@@ -44,12 +54,31 @@ public sealed class MultipartMachineSystem : EntitySystem
 
                 xform.LocalRotation = part.Rotation + rotation;
 
+                Comp<MultipartMachineGhostComponent>(ghostEnt).LinkedMachine = ent;
+
                 if (part.Sprite != null)
                 {
                     var sprite = Comp<SpriteComponent>(ghostEnt);
                     sprite.LayerSetSprite(0, part.Sprite);
                 }
+
+                ent.Comp.Ghosts.Add(ghostEnt);
             }
         }
+    }
+
+    /// <summary>
+    /// Handles when a ghost part despawns after its short lifetime.
+    /// Will attempt to remove itself from the list of known ghost entities in the main multipart
+    /// machine component.
+    /// </summary>
+    /// <param name="ent">Ghost entity that has been despawned</param>
+    /// <param name="args">Args for the event</param>
+    private void OnGhostDespawned(Entity<MultipartMachineGhostComponent> ent, ref TimedDespawnEvent args)
+    {
+        if (!TryComp<MultipartMachineComponent>(ent.Comp.LinkedMachine, out var machine))
+            return;
+
+        machine.Ghosts.Remove(ent);
     }
 }
