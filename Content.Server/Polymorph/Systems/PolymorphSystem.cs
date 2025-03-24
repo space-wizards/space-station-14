4f3f05cbef3555be3ab4a343e97a83162d069156
Tayrtahn <tayrtahn@gmail.com>
2025-03-24 19:05:41 -0400

    AsNullable

--- Content.Server/Polymorph/Systems/PolymorphSystem.cs
+++ Content.Server/Polymorph/Systems/PolymorphSystem.cs
@@ -168,7 +168,7 @@ public sealed partial class PolymorphSystem : EntitySystem
     private void OnPolymorphedTerminating(Entity<PolymorphedEntityComponent> ent, ref EntityTerminatingEvent args)
     {
         if (ent.Comp.Configuration.RevertOnDelete)
-            Revert((ent, ent));
+            Revert(ent.AsNullable());
 
         // Remove our original entity too
         // Note that Revert will set Parent to null, so reverted entities will not be deleted
