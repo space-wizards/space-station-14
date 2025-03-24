f5881a6c5b87cd57b68d72034547c7a7cff6775f
Patrick Chieppe <patrick.chieppe@hotmail.com>
2025-03-24 23:19:34 +1100

    Final cleanup

--- Content.Client/Dice/LoadedDiceSystem.cs
+++ Content.Client/Dice/LoadedDiceSystem.cs
@@ -5,6 +5,4 @@ namespace Content.Client.Dice;
 /// <summary>
 ///     Stub system to ensure client prediction of <see cref="SharedLoadedDiceSystem"/>.
 /// </summary>
-public sealed class LoadedDiceSystem : SharedLoadedDiceSystem
-{
-}
+public sealed class LoadedDiceSystem : SharedLoadedDiceSystem;
--- Content.Shared/Dice/SharedLoadedDiceSystem.cs
+++ Content.Shared/Dice/SharedLoadedDiceSystem.cs
@@ -1,12 +1,10 @@
 using Content.Shared.Administration.Logs;
 using Content.Shared.Database;
-using Content.Shared.Hands;
 using Content.Shared.Inventory;
 using Content.Shared.Popups;
 using Content.Shared.Verbs;
 using Content.Shared.Whitelist;
 using Robust.Shared.Containers;
-using Robust.Shared.GameObjects;
 using Robust.Shared.Player;
 using Robust.Shared.Utility;
 
@@ -24,9 +22,6 @@ public abstract class SharedLoadedDiceSystem : EntitySystem
     [Dependency] private readonly EntityWhitelistSystem _whitelist = default!;
     [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
 
-    // TODO REMOVE
-    [Dependency] private readonly ILogManager _logManager = default!;
-
     public override void Initialize()
     {
         base.Initialize();
@@ -34,9 +29,6 @@ public abstract class SharedLoadedDiceSystem : EntitySystem
         SubscribeLocalEvent<LoadedDiceComponent, GetVerbsEvent<AlternativeVerb>>(OnAlternativeVerb);
         SubscribeLocalEvent<LoadedDiceComponent, LoadedDiceSideSelectedMessage>(OnSelected);
     }
-
-    // TODO REMOVE
-    private ISawmill _sawmill = default!;
 
     /// <summary>
     ///     If the entity is held in the hand or pocket, and the holder also has a loaded dice bag in the hand or in a pocket, return the holder
@@ -79,14 +71,14 @@ public abstract class SharedLoadedDiceSystem : EntitySystem
         if (user != args.User)
             return;
 
-        if (!TryComp(entity.Owner, out DiceComponent? die))
+        if (!TryComp(entity, out DiceComponent? die))
             return;
 
         args.Verbs.Add(new InteractionVerb()
         {
             Text = Loc.GetString("loaded-dice-set-verb-text"),
             Icon = new SpriteSpecifier.Texture(new("/Textures/Interface/VerbIcons/settings.svg.192dpi.png")),
-            Act = () => TryOpenUi(entity.Owner, die, entity.Comp, user)
+            Act = () => TryOpenUi((entity.Owner, die, entity.Comp), user)
         });
     }
 
@@ -95,10 +87,7 @@ public abstract class SharedLoadedDiceSystem : EntitySystem
         if (!args.CanAccess || !args.CanInteract)
             return;
 
-        if (!GetCapableUser(entity, out var user))
-            return;
-
-        if (user != args.User)
+        if (!GetCapableUser(entity, out var user) || user != args.User)
             return;
 
         args.Verbs.Add(new AlternativeVerb()
@@ -106,29 +95,29 @@ public abstract class SharedLoadedDiceSystem : EntitySystem
             Priority = 1, // Make this the alt-click verb
             Text = Loc.GetString("loaded-dice-unset-verb-text"),
             Icon = new SpriteSpecifier.Texture(new("/Textures/Interface/VerbIcons/die.svg.192dpi.png")),
-            Act = () => SetSelectedSide(entity.Owner, null, user, null, entity.Comp)
+            Act = () => SetSelectedSide((entity, null, entity.Comp), null, user)
         });
     }
 
     private void OnSelected(Entity<LoadedDiceComponent> entity, ref LoadedDiceSideSelectedMessage args)
     {
-        SetSelectedSide(entity.Owner, args.SelectedSide, args.Actor, null, entity.Comp);
+        SetSelectedSide((entity.Owner, null, entity.Comp), args.SelectedSide, args.Actor);
     }
 
-    private void TryOpenUi(EntityUid uid, DiceComponent die, LoadedDiceComponent loadedDie, EntityUid user)
+    private void TryOpenUi(Entity<DiceComponent, LoadedDiceComponent> entity, EntityUid user)
     {
         if (!TryComp(user, out ActorComponent? actor))
             return;
 
-        _uiSystem.TryToggleUi(uid, LoadedDiceUiKey.Key, actor.PlayerSession);
-        UpdateUi(uid, die, loadedDie);
+        _uiSystem.TryToggleUi(entity.Owner, LoadedDiceUiKey.Key, actor.PlayerSession);
+        UpdateUi(entity);
     }
 
-    private void UpdateUi(EntityUid uid, DiceComponent die, LoadedDiceComponent loadedDie)
+    private void UpdateUi(Entity<DiceComponent, LoadedDiceComponent> entity)
     {
-        var state = new LoadedDiceBoundUserInterfaceState(die, loadedDie.SelectedSide);
+        var state = new LoadedDiceBoundUserInterfaceState(entity.Comp1, entity.Comp2.SelectedSide);
 
-        _uiSystem.SetUiState(uid, LoadedDiceUiKey.Key, state);
+        _uiSystem.SetUiState(entity.Owner, LoadedDiceUiKey.Key, state);
     }
 
     /// <summary>
@@ -136,13 +125,10 @@ public abstract class SharedLoadedDiceSystem : EntitySystem
     ///     If selectedSide is null, this unsets the die, and makes it behave like a normal die.
     ///     If user is not null, this will also display a popup and log an admin message.
     /// </summary>
-    public void SetSelectedSide(
-            EntityUid uid,
-            int? selectedSide,
-            EntityUid? user = null,
-            DiceComponent? die = null,
-            LoadedDiceComponent? loadedDie = null)
+    public void SetSelectedSide( Entity<DiceComponent?, LoadedDiceComponent?> entity, int? selectedSide, EntityUid? user = null)
     {
+        entity.Deconstruct(out var uid, out var die, out var loadedDie);
+
         if (!Resolve(uid, ref die, false))
             return;
 
@@ -156,11 +142,12 @@ public abstract class SharedLoadedDiceSystem : EntitySystem
         // There may not be a user if the change is not done by a player
         if (user != null)
         {
-            if (!GetCapableUser((uid, loadedDie), out var capableUser))
+            if (!GetCapableUser((uid, loadedDie), out var capableUser) || capableUser != user)
+            {
+                // The user currently trying to set the die can't actually do that, close the UI
+                _uiSystem.CloseUi((uid, null), LoadedDiceUiKey.Key, user);
                 return;
-
-            if (capableUser != user)
-                return;
+            }
 
             string message;
             LogStringHandler adminMessage;
@@ -177,13 +164,13 @@ public abstract class SharedLoadedDiceSystem : EntitySystem
                 adminMessage = $"{ToPrettyString(user):player} set the loaded {ToPrettyString(uid):target} to roll {sideValue}";
             }
 
-            _popup.PopupEntity(message, uid, user.Value, PopupType.Small);
+            _popup.PopupPredicted(message, null, uid, user.Value, PopupType.Small);
             _adminLogger.Add(LogType.Action, LogImpact.Low, ref adminMessage);
         }
 
         loadedDie.SelectedSide = selectedSide;
 
-        UpdateUi(uid, die, loadedDie);
+        UpdateUi((uid, die, loadedDie));
         Dirty(uid, loadedDie);
     }
 }
