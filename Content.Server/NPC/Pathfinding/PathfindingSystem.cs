87e8bad08389438b2c7a31a1ac682a91df78a46e
Tayrtahn <tayrtahn@gmail.com>
2025-03-25 16:13:19 -0400

    EntityCoordinates.ToMapPos -> SharedTransformSystem.ToMapCoordinates().Position

--- Content.Server/NPC/Pathfinding/PathfindingSystem.cs
+++ Content.Server/NPC/Pathfinding/PathfindingSystem.cs
@@ -405,7 +405,7 @@ namespace Content.Server.NPC.Pathfinding
                 return null;
             }
 
-            var localPos = Vector2.Transform(coordinates.ToMapPos(EntityManager, _transform), _transform.GetInvWorldMatrix(xform));
+            var localPos = Vector2.Transform(_transform.ToMapCoordinates(coordinates).Position, _transform.GetInvWorldMatrix(xform));
             var origin = GetOrigin(localPos);
 
             if (!TryGetChunk(origin, comp, out var chunk))
