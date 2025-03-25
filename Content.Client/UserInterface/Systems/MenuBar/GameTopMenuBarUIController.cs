7cc7a6be9839c15e962b84a4ccb41df57d58140d
Kyle Tyo <akikai297@gmail.com>
2025-03-25 14:46:07 -0400

    add visibility manager for admin button

--- Content.Client/UserInterface/Systems/MenuBar/GameTopMenuBarUIController.cs
+++ Content.Client/UserInterface/Systems/MenuBar/GameTopMenuBarUIController.cs
@@ -1,7 +1,25 @@
+using Content.Client.Administration.Managers;
+using Content.Client.UserInterface.Systems.MenuBar.Widgets;
+using Robust.Client.Console;
 using Robust.Client.UserInterface.Controllers;
 
 namespace Content.Client.UserInterface.Systems.MenuBar;
 
 public sealed class GameTopMenuBarUIController : UIController
 {
+    [Dependency] private readonly IClientAdminManager _admin = default!;
+    [Dependency] private readonly IClientConGroupController _conGroups = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _admin.AdminStatusUpdated += OnAdminStatusUpdated;
+    }
+
+    private void OnAdminStatusUpdated()
+    {
+        var button = UIManager.GetActiveUIWidget<GameTopMenuBar>().AdminButton;
+        button.Visible = _conGroups.CanAdminMenu();
+    }
 }
