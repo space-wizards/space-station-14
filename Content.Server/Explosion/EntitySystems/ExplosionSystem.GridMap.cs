1dba8f6d88e5835f7c81a0720ae4d2c501489bd6
Tayrtahn <tayrtahn@gmail.com>
2025-03-26 11:52:55 -0400

    Fix 1 warning in ExplosionSystem.GridMap

--- Content.Server/Explosion/EntitySystems/ExplosionSystem.GridMap.cs
+++ Content.Server/Explosion/EntitySystems/ExplosionSystem.GridMap.cs
@@ -258,7 +258,7 @@ public sealed partial class ExplosionSystem
             {
                 var neighbourIndex = tileRef.GridIndices + NeighbourVectors[i];
 
-                if (grid.TryGetTileRef(neighbourIndex, out var neighbourTile) && !neighbourTile.Tile.IsEmpty)
+                if (_mapSystem.TryGetTileRef(ev.Entity, grid, neighbourIndex, out var neighbourTile) && !neighbourTile.Tile.IsEmpty)
                 {
                     var oppositeDirection = (NeighborFlag) (1 << ((i + 4) % 8));
                     edges[neighbourIndex] = edges.GetValueOrDefault(neighbourIndex) | oppositeDirection;
