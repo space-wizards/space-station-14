df75c66668d3943db986106be8ec2093fe321c5f
Lmorgan89 <billsmith116@gmail.com>
2025-03-29 19:42:38 +0100

    Async doafter removal
    The doafter system looks fairly complex and I'm not 100% if I've understood everything correctly. The change is a bit more complex than a simple "use another API" like most of the rest of the warning cleanup.

---
+++ Content.Shared/Engineering/EntitySystems/DisassembleOnAltVerbDoAfterEvent.cs
@@ -0,0 +1,10 @@
+using Content.Shared.DoAfter;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Engineering.EntitySystems
+{
+    [Serializable, NetSerializable]
+    public sealed partial class DisassembleOnAltVerbDoAfterEvent : SimpleDoAfterEvent
+    {
+    }
+}
