6a79c247e39112c63a72957fca1bc92867510e18
J <billsmith116@gmail.com>
2025-03-30 01:12:14 +0000

    Fix some atmos warnings (#36157)

--- Content.Client/Atmos/UI/GasPressurePumpBoundUserInterface.cs
+++ Content.Client/Atmos/UI/GasPressurePumpBoundUserInterface.cs
@@ -1,8 +1,7 @@
-﻿using Content.Shared.Atmos;
+using Content.Shared.Atmos;
 using Content.Shared.Atmos.Components;
 using Content.Shared.Atmos.Piping.Binary.Components;
 using Content.Shared.IdentityManagement;
-using Content.Shared.Localizations;
 using JetBrains.Annotations;
 using Robust.Client.UserInterface;
 
