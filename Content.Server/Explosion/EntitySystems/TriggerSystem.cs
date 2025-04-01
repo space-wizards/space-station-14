6df54cfe6412b369beab03c3d38d30459cdf7cf3
Rinary <rinary.super@gmail.com>
2025-04-01 16:36:56 +0300

    upstream fixes

--- Content.Server/Explosion/EntitySystems/TriggerSystem.cs
+++ Content.Server/Explosion/EntitySystems/TriggerSystem.cs
@@ -33,6 +33,7 @@ using Robust.Shared.Physics.Events;
 using Robust.Shared.Physics.Systems;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
+using Robust.Shared.Timing;
 using Robust.Shared.Utility;
 
 namespace Content.Server.Explosion.EntitySystems
