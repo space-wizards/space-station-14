6be2de712168fc3f6230f967b21141b71c0fb802
Tayrtahn <tayrtahn@gmail.com>
2025-04-05 12:41:48 -0400

    Add test of R&D server hacking

--- Content.IntegrationTests/Tests/GameRules/SpaceNinjaInteractionTest.cs
+++ Content.IntegrationTests/Tests/GameRules/SpaceNinjaInteractionTest.cs
@@ -3,9 +3,11 @@ using Content.Server.Antag;
 using Content.Server.Antag.Components;
 using Content.Server.CriminalRecords.Systems;
 using Content.Server.GameTicking;
+using Content.Server.Research.Systems;
 using Content.Shared.Inventory;
 using Content.Shared.Mind;
 using Content.Shared.Ninja.Components;
+using Content.Shared.Research.Prototypes;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Prototypes;
 
@@ -13,28 +15,34 @@ namespace Content.IntegrationTests.Tests.GameRules;
 
 public sealed partial class SpaceNinjaInteractionTest : InteractionTest
 {
-    public sealed class CriminalRecordsHackTestSystem : EntitySystem
+    public abstract class HackTestSystem<TComp, TEvent> : EntitySystem
+        where TComp : Component
     {
         public bool Hacked { get; private set; }
 
         public override void Initialize()
         {
-            SubscribeLocalEvent<CriminalRecordsHackTestComponent, CriminalRecordsHackedEvent>(OnHack);
+            base.Initialize();
+
+            SubscribeLocalEvent<TComp, TEvent>(OnHack);
         }
 
-        private void OnHack(Entity<CriminalRecordsHackTestComponent> entity, ref CriminalRecordsHackedEvent args)
+        private void OnHack(Entity<TComp> entity, ref TEvent args)
         {
             Hacked = true;
         }
     }
 
-    // This exists just so the test system has a way of subscribing to the event on the ninja entity.
-    // We can't SpaceNinjaComponent because it's already subscribed.
-    [RegisterComponent]
-    public sealed partial class CriminalRecordsHackTestComponent : Component;
+    [RegisterComponent] public sealed partial class CriminalRecordsHackTestComponent : Component;
+    [RegisterComponent] public sealed partial class ResearchServerHackTestComponent : Component;
+    public sealed class CriminalRecordsHackTestSystem : HackTestSystem<CriminalRecordsHackTestComponent, CriminalRecordsHackedEvent>;
+    public sealed class ResearchServerHackTestSystem : HackTestSystem<ResearchServerHackTestComponent, ResearchStolenEvent>;
+
 
     // ProtoIds of things we need to spawn, check for, etc.
     private static readonly EntProtoId CriminalRecordsConsoleProtoId = "ComputerCriminalRecords";
+    private static readonly EntProtoId ResearchServerProtoId = "ResearchAndDevelopmentServer";
+
     private static readonly EntProtoId NinjaSpawnGameRuleProtoId = "NinjaSpawn";
     private static readonly EntProtoId NinjaGlovesProtoId = "ClothingHandsGlovesSpaceNinja";
     private static readonly EntProtoId NinjaSuitProtoId = "ClothingOuterSuitSpaceNinja";
@@ -90,6 +98,56 @@ public sealed partial class SpaceNinjaInteractionTest : InteractionTest
         await Interact();
 
         // Make sure the records were hacked
+        Assert.That(testSys.Hacked);
+    }
+
+    [Test]
+    public async Task ResearchServerHackTest()
+    {
+        var testSys = Server.System<ResearchServerHackTestSystem>();
+        Assert.That(testSys.Hacked, Is.False, "Reseach server was already hacked?");
+
+        // Ninjitize me, Cap'n
+        await MakeNinja(Player);
+
+        // Add our tracker component to the ninja
+        await Server.WaitPost(() =>
+        {
+            SEntMan.AddComponent<ResearchServerHackTestComponent>(SEntMan.GetEntity(Player));
+        });
+
+        // Drop the jetpack so they have an empty hand to use the console
+        await Drop();
+
+        // Spawn the R&D server
+        var researchServer = await SpawnTarget(ResearchServerProtoId);
+
+        // Grant all technologies to the server so we have something to steal
+        var researchSys = Server.System<ResearchSystem>();
+        var technologies = ProtoMan.EnumeratePrototypes<TechnologyPrototype>();
+        await Server.WaitPost(() =>
+        {
+            foreach (var technology in technologies)
+            {
+                researchSys.AddTechnology(ToServer(researchServer), technology);
+            }
+        });
+
+        // Interact with the server - gloves are not active, so this should not hack
+        await Interact();
+        Assert.That(testSys.Hacked, Is.False, "Ninja hacked research server without activating gloves.");
+
+        // Activate the gloves
+        await Server.WaitPost(() =>
+        {
+            Assert.That(ItemToggleSys.TryActivate(ToServer(_gloves).Value, user: ToServer(Player)),
+                "Failed to activate ninja gloves.");
+        });
+
+        // Interact with the server again with active gloves
+        await Interact();
+
+        // Make sure the server was hacked
         Assert.That(testSys.Hacked);
     }
 
