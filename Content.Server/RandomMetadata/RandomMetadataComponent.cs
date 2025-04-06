fc576b5cdbaa6ce0d2529cefb6d8d4e190100b77
Tayrtahn <tayrtahn@gmail.com>
2025-04-06 00:04:16 -0400

    Docs

--- Content.Server/RandomMetadata/RandomMetadataComponent.cs
+++ Content.Server/RandomMetadata/RandomMetadataComponent.cs
@@ -15,9 +15,17 @@ public sealed partial class RandomMetadataComponent : Component
     [DataField]
     public List<ProtoId<LocalizedDatasetPrototype>>? NameSegments;
 
+    /// <summary>
+    /// LocId of the formatting string to use to assemble the <see cref="NameSegments"/> into the entity's name.
+    /// Segments will be passed to the localization system with this string as variables named $part0, $part1, $part2, etc.
+    /// </summary>
     [DataField]
     public LocId NameFormat = "random-metadata-name-format-default";
 
+    /// <summary>
+    /// LocId of the formatting string to use to assemble the <see cref="DescriptionSegments"/> into the entity's description.
+    /// Segments will be passed to the localization system with this string as variables named $part0, $part1, $part2, etc.
+    /// </summary>
     [DataField]
     public LocId DescriptionFormat = "random-metadata-description-format-default";
 }
