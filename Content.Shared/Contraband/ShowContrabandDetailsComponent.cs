da8541feed15ba2663778d2468b330af95219d5a
qrwas <aleksandr.vernigora93@gmail.com>
2025-04-09 16:21:06 +0300

    fix component description

--- Content.Shared/Contraband/ShowContrabandDetailsComponent.cs
+++ Content.Shared/Contraband/ShowContrabandDetailsComponent.cs
@@ -2,7 +2,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Contraband;
 
 /// <summary>
-///     This component allows you to see Controband details on examine items
+///     This component allows you to see Contraband details on examine items
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class ShowContrabandDetailsComponent : Component
