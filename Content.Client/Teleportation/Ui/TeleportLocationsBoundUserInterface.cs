dd56b2e146802783a6f16eeb4a9ba94f2b9e81f6
keronshb <keronshb@live.com>
2025-04-08 21:19:21 -0400

    Adds Teleport Locations UI class and BUI. Adds Teleport point collection and button generation

---
+++ Content.Client/Teleportation/Ui/TeleportLocationsBoundUserInterface.cs
@@ -0,0 +1,39 @@
+﻿using Content.Shared.Teleportation;
+using Content.Shared.Teleportation.Components;
+using JetBrains.Annotations;
+using Robust.Client.UserInterface;
+
+namespace Content.Client.Teleportation.Ui;
+
+[UsedImplicitly]
+public sealed class TeleportLocationsBoundUserInterface : BoundUserInterface
+{
+    [ViewVariables]
+    private TeleportMenu? _menu;
+
+    [ViewVariables]
+    private string _search = "";
+
+    public TeleportLocationsBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _menu = this.CreateWindow<TeleportMenu>();
+        SendMessage(new TeleportLocationRequestPointsMessage());
+
+        if (!EntMan.TryGetComponent<TeleportLocationsComponent>(Owner, out var teleComp))
+            return;
+
+        _menu._warps = teleComp.AvailableWarps;
+        _menu.AddTeleportButtons();
+
+        _menu.TeleportClicked += netEnt =>
+        {
+            SendMessage(new TeleportLocationRequestTeleportMessage(netEnt));
+        };
+    }
+}
---
+++ Content.Shared/Teleportation/TeleportLocationsUi.cs
@@ -0,0 +1,21 @@
+﻿using Content.Shared.Teleportation.Components;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Teleportation;
+
+[Serializable, NetSerializable]
+public enum TeleportLocationUiKey : byte
+{
+    Key
+}
+
+[Serializable, NetSerializable]
+public sealed class TeleportLocationRequestPointsMessage() : BoundUserInterfaceMessage
+{
+}
+
+[Serializable, NetSerializable]
+public sealed class TeleportLocationRequestTeleportMessage(NetEntity netEnt) : BoundUserInterfaceMessage
+{
+    public NetEntity NetEnt = netEnt;
+}
