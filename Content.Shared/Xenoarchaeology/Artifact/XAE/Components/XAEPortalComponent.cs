343227db524fcdf7946ecf0bfb7dd88ea19d8ee6
pa.pecherskij <pa.pecherskij@interfax.ru>
2025-04-11 21:40:39 +0300

    feat: networking for shared XAE components + xml-doc leftovers

--- Content.Shared/Xenoarchaeology/Artifact/XAE/Components/XAEPortalComponent.cs
+++ Content.Shared/Xenoarchaeology/Artifact/XAE/Components/XAEPortalComponent.cs
@@ -1,13 +1,17 @@
+using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Xenoarchaeology.Artifact.XAE.Components;
 
 /// <summary>
-///     When activated artifact will spawn an pair portals. First - right in artifact, Second - at random point of station.
+///     When activated artifact will spawn a pair of portals. First - right in artifact, Second - at random point of station.
 /// </summary>
-[RegisterComponent, Access(typeof(XAEPortalSystem))]
+[RegisterComponent, Access(typeof(XAEPortalSystem)), NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class XAEPortalComponent : Component
 {
-    [DataField]
+    /// <summary>
+    /// Entity that should be spawned as portal.
+    /// </summary>
+    [DataField, AutoNetworkedField]
     public EntProtoId PortalProto = "PortalArtifact";
 }
--- Content.Shared/Xenoarchaeology/Artifact/XAE/Components/XAERandomTeleportInvokerComponent.cs
+++ Content.Shared/Xenoarchaeology/Artifact/XAE/Components/XAERandomTeleportInvokerComponent.cs
@@ -1,21 +1,23 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.Xenoarchaeology.Artifact.XAE.Components;
 
 /// <summary>
 /// When activated, will teleport the artifact
 /// to a random position within a certain radius
 /// </summary>
-[RegisterComponent, Access(typeof(XAERandomTeleportInvokerSystem))]
+[RegisterComponent, Access(typeof(XAERandomTeleportInvokerSystem)), NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class XAERandomTeleportInvokerComponent : Component
 {
     /// <summary>
     /// The max distance that the artifact will teleport.
     /// </summary>
-    [DataField("maxRange")]
+    [DataField, AutoNetworkedField]
     public float MaxRange = 15f;
 
     /// <summary>
     /// The min distance that the artifact will teleport.
     /// </summary>
-    [DataField("minRange")]
+    [DataField, AutoNetworkedField]
     public float MinRange = 6f;
 }
