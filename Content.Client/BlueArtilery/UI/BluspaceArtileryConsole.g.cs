d85184df5cc835dace817439e428a017cac30281
samir20210 <samirchik.21022000@gmail.com>
2025-04-13 19:56:51 +0300

    Fixed bug and created by this new one. This is suck as a starting developer :(

--- Content.Client/BlueArtilery/UI/BluspaceArtileryConsole.g.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Client.UserInterface;
-using Robust.Client.UserInterface.Controls;
-namespace Content.Client.BlueArtilery.UI
-{
-    public partial class BluespaceArtileryConsoleWindow
-    {
-        private global::Robust.Client.UserInterface.Controls.Button FireBSA => this.FindControl<global::Robust.Client.UserInterface.Controls.Button>("FireBSA");
-    }
-}
