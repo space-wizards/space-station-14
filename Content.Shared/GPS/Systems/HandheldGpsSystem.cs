cb3239229a749cf33ebd6651ce04b9d5428c8487
Milon <milonpl.git@proton.me>
2025-04-13 17:11:46 +0200

    review

--- Content.Shared/GPS/Systems/HandheldGpsSystem.cs
+++ Content.Shared/GPS/Systems/HandheldGpsSystem.cs
@@ -1,14 +1,14 @@
 using Content.Shared.GPS.Components;
 using Content.Shared.Examine;
-using Robust.Shared.Timing;
+using Robust.Shared.Map;
 
 namespace Content.Shared.GPS.Systems;
 
 public sealed class HandheldGpsSystem : EntitySystem
 {
     [Dependency] private readonly SharedTransformSystem _transform = default!;
-    [Dependency] private readonly IEntityManager _entMan = default!;
 
+    /// <inheritdoc/>
     public override void Initialize()
     {
         base.Initialize();
@@ -16,17 +16,19 @@ public sealed class HandheldGpsSystem : EntitySystem
         SubscribeLocalEvent<HandheldGPSComponent, ExaminedEvent>(OnExamine);
     }
 
+    /// <summary>
+    /// Handles showing the coordinates when a GPS is examined.
+    /// </summary>
     private void OnExamine(Entity<HandheldGPSComponent> ent, ref ExaminedEvent args)
     {
         var posText = "Error";
-        var xform = _entMan.GetComponent<TransformComponent>(ent);
 
-        if (xform.GridUid is { } gridUid)
+        var pos = _transform.GetMapCoordinates(ent);
+
+        if (pos.MapId != MapId.Nullspace)
         {
-            var pos = _transform.GetMapCoordinates(ent, xform);
-            var x = (int)pos.X;
-            var y = (int)pos.Y;
-
+            var x = (int) pos.Position.X;
+            var y = (int) pos.Position.Y;
             posText = $"({x}, {y})";
         }
 
