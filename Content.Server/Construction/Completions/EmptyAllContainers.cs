c5bdf561bf6852639f86617d442c0bdd565c8113
Lmorgan89 <billsmith116@gmail.com>
2025-04-15 23:33:50 +0200

    Reusing shared system call

--- Content.Server/Construction/Completions/EmptyAllContainers.cs
+++ Content.Server/Construction/Completions/EmptyAllContainers.cs
@@ -2,7 +2,6 @@ using Content.Server.Hands.Systems;
 using Content.Shared.Construction;
 using Content.Shared.Hands.Components;
 using JetBrains.Annotations;
-using Robust.Server.Containers;
 using Robust.Server.GameObjects;
 using Robust.Shared.Containers;
 
@@ -29,15 +28,14 @@ namespace Content.Server.Construction.Completions
             if (!entityManager.TryGetComponent(uid, out ContainerManagerComponent? containerManager))
                 return;
 
-            var containerSys = entityManager.EntitySysManager.GetEntitySystem<ContainerSystem>();
+            var containerSys = entityManager.EntitySysManager.GetEntitySystem<SharedContainerSystem>();
             var handSys = entityManager.EntitySysManager.GetEntitySystem<HandsSystem>();
             var transformSys = entityManager.EntitySysManager.GetEntitySystem<TransformSystem>();
 
             HandsComponent? hands = null;
             var pickup = Pickup && entityManager.TryGetComponent(userUid, out hands);
 
-            var containerEnumerable = entityManager.EntitySysManager.GetEntitySystem<SharedContainerSystem>().GetAllContainers(uid);
-            foreach (var container in containerEnumerable)
+            foreach (var container in containerSys.GetAllContainers(uid))
             {
                 foreach (var ent in containerSys.EmptyContainer(container, true, reparent: !pickup))
                 {
