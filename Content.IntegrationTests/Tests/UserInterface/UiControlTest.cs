d47f810d542441b8e4c6126733c1905de4a506d9
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-04-15 14:52:40 -0400

    basic structure of a late join gui with character selection

--- Content.IntegrationTests/Tests/UserInterface/UiControlTest.cs
+++ Content.IntegrationTests/Tests/UserInterface/UiControlTest.cs
@@ -1,5 +1,5 @@
 using System.Linq;
-using Content.Client.LateJoin;
+using Content.Client.Lobby.UI;
 using Robust.Client.UserInterface.CustomControls;
 using Robust.Shared.ContentPack;
 using Robust.Shared.IoC;
