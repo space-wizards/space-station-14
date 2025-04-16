695eb8188d27b8328a6e1846faae9acee1022fdb
metalgearsloth <comedian_vs_clown@hotmail.com>
2025-04-16 19:11:25 +1000

    Blindness networking
    Nothing actually sets this.

--- Content.Shared/Traits/Assorted/PermanentBlindnessComponent.cs
+++ Content.Shared/Traits/Assorted/PermanentBlindnessComponent.cs
@@ -5,10 +5,10 @@ namespace Content.Shared.Traits.Assorted;
 /// <summary>
 /// This is used for making something blind forever.
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class PermanentBlindnessComponent : Component
 {
-    [ViewVariables(VVAccess.ReadWrite), DataField]
+    [DataField, AutoNetworkedField]
     public int Blindness = 0; // How damaged should their eyes be. Set 0 for maximum damage.
 }
 
