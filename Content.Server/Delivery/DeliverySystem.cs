28daf6f964b551a519ca28d91375202317d24744
Lmorgan89 <billsmith116@gmail.com>
2025-04-18 02:12:08 +0200

    Basic delivery penalization

--- Content.Server/Delivery/DeliverySystem.cs
+++ Content.Server/Delivery/DeliverySystem.cs
@@ -1,4 +1,5 @@
 using Content.Server.Cargo.Systems;
+using Content.Server.Chat.Systems;
 using Content.Server.Station.Systems;
 using Content.Server.StationRecords.Systems;
 using Content.Shared.Cargo.Components;
@@ -8,6 +9,7 @@ using Content.Shared.Labels.EntitySystems;
 using Content.Shared.StationRecords;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server.Delivery;
 
@@ -25,6 +27,13 @@ public sealed partial class DeliverySystem : SharedDeliverySystem
     [Dependency] private readonly FingerprintReaderSystem _fingerprintReader = default!;
     [Dependency] private readonly LabelSystem _label = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
+    [Dependency] private readonly ChatSystem _chat = default!;
+    [Dependency] private readonly IPrototypeManager _protoMan = default!;
+
+    /// <summary>
+    /// Name to use if the <see cref="DeliveryComponent.PenaltyBankAccount"/> is not set
+    /// </summary>
+    private const string UnknownAccount = "Unknown Account";
 
     public override void Initialize()
     {
@@ -66,13 +75,49 @@ public sealed partial class DeliverySystem : SharedDeliverySystem
         if (!Resolve(ent, ref ent.Comp))
             return;
 
-        if (!TryComp<StationBankAccountComponent>(ent.Comp.RecipientStation, out var account))
+        var delivery = ent.Comp;
+
+        if (!TryComp<StationBankAccountComponent>(delivery.RecipientStation, out var account))
             return;
 
+        var stationAccountEnt = (delivery.RecipientStation.Value, account);
+
+        if (delivery.WasPenalized)
+            HandlePenalization(ent!, stationAccountEnt); // After the Resolve above, we know it's not null
+
         _cargo.UpdateBankAccount(
-            (ent.Comp.RecipientStation.Value, account),
-            ent.Comp.BaseSpesoReward,
+            stationAccountEnt,
+            delivery.BaseSpesoReward,
             _cargo.CreateAccountDistribution(account.PrimaryAccount, account, account.PrimaryCut));
+    }
+
+    /// <summary>
+    /// Allows the delivery to speak with the message and handles the charging of the penalized account
+    /// </summary>
+    /// <param name="ent"><see cref="DeliveryComponent"/> entity.</param>
+    /// <param name="stationAccountEnt"><see cref="StationBankAccountComponent"/> entity.</param>
+    private void HandlePenalization(Entity<DeliveryComponent> ent, Entity<StationBankAccountComponent> stationAccountEnt)
+    {
+        var delivery = ent.Comp;
+        var accountName = UnknownAccount;
+        if (_protoMan.TryIndex(delivery.PenaltyBankAccount, out var accountInfo))
+            accountName = Loc.GetString(accountInfo.Name);
+
+        // Extract reason from components...
+        _chat.TrySendInGameICMessage(ent, GetMessage("wah", delivery.BaseSpesoPenalty, accountName), InGameICChatType.Speak, hideChat: true);
+
+        _cargo.UpdateBankAccount(
+            // Recasting as nullable, it's this or using .Comp! for CreateAccountDistribution and trusting it's always resolved before calling.
+            // I'm not Resolving again in here
+            (stationAccountEnt, stationAccountEnt.Comp),
+            -delivery.BaseSpesoPenalty, // Subtracting
+            _cargo.CreateAccountDistribution(delivery.PenaltyBankAccount, stationAccountEnt.Comp)
+            );
+    }
+
+    private string GetMessage(string reason, int penalty, string accountName)
+    {
+        return Loc.GetString("delivery-penalty-message", ("reason", reason), ("spesos", penalty), ("account", accountName));
     }
 
     public override void Update(float frameTime)
