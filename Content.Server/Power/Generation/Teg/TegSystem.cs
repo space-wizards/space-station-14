63dfd21b146eae00c696ae561da6f40a223af0ad
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2025-04-19 16:20:40 +1000

    Predict dumping (#32394)
    * Predict dumping
    
    - This got soaped really fucking hard.
    - Dumping is predicted, this required disposals to be predicte.d
    - Disposals required mailing (because it's tightly coupled), and a smidge of other content systems.
    - I also had to fix a compnetworkgenerator issue at the same time so it wouldn't mispredict.
    
    * Fix a bunch of stuff
    
    * nasty merge
    
    * Some reviews
    
    * Some more reviews while I stash
    
    * Fix merge
    
    * Fix merge
    
    * Half of review
    
    * Review
    
    * re(h)f
    
    * lizards
    
    * feexes
    
    * feex

--- Content.Server/Power/Generation/Teg/TegSystem.cs
+++ Content.Server/Power/Generation/Teg/TegSystem.cs
@@ -9,6 +9,7 @@ using Content.Server.NodeContainer.Nodes;
 using Content.Server.Power.Components;
 using Content.Shared.Atmos;
 using Content.Shared.DeviceNetwork;
+using Content.Shared.DeviceNetwork.Events;
 using Content.Shared.Examine;
 using Content.Shared.Power;
 using Content.Shared.Power.EntitySystems;
