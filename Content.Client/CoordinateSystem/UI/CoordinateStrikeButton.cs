673db44b9d9c0f6b0f00fe4e342faee89f1c86f7
samir20210 <samirchik.21022000@gmail.com>
2025-04-20 10:43:56 +0300

    started developing UI for coordinates settings.

---
+++ Content.Client/CoordinateSystem/UI/CoordinateStrikeButton.cs
@@ -0,0 +1,28 @@
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.IoC;
+using Robust.Shared.Localization;
+
+namespace Content.Client.CoordinateSystem.UI
+{
+    /// <summary>
+    ///     LITERALLY just a button that opens the vote call menu.
+    ///     Automatically disables itself if the client cannot call votes.
+    /// </summary>
+    public sealed class CoordinateStrikeButton : Button
+    {
+
+        public CoordinateStrikeButton()
+        {
+            IoCManager.InjectDependencies(this);
+
+            Text = "Aim";
+            OnPressed += OnOnPressed;
+        }
+
+        private void OnOnPressed(ButtonEventArgs obj)
+        {
+            var menu = new CoordinateWindow();
+            menu.OpenCentered();
+        }
+    }
+}
