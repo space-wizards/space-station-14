43ef60e44312e5de04b4e25de1e09fde97699049
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-04-21 16:56:20 -0400

    file scoped namespace

--- Content.Server/Destructible/Thresholds/Behaviors/DamageContainerContentsBehavior.cs
+++ Content.Server/Destructible/Thresholds/Behaviors/DamageContainerContentsBehavior.cs
@@ -2,55 +2,54 @@ using Content.Shared.Damage;
 using Robust.Server.Containers;
 using Robust.Shared.Containers;
 
-namespace Content.Server.Destructible.Thresholds.Behaviors
+namespace Content.Server.Destructible.Thresholds.Behaviors;
+
+/// <summary>
+///     Damage all items in specified containers
+/// </summary>
+[DataDefinition]
+public sealed partial class DamageContainerContentsBehavior : IThresholdBehavior
 {
     /// <summary>
-    ///     Damage all items in specified containers
+    /// List of containers to apply damage to contents
     /// </summary>
-    [DataDefinition]
-    public sealed partial class DamageContainerContentsBehavior : IThresholdBehavior
+    [DataField("containers")]
+    public List<string> Containers = new();
+
+    /// <summary>
+    /// Damage specifier to apply to container contents.
+    /// If null, apply the current damage of the owner entity to all the contents
+    /// </summary>
+    [DataField]
+    public DamageSpecifier? Damage = null;
+
+    [DataField]
+    public bool IgnoreResistances = false;
+
+    public void Execute(EntityUid owner, DestructibleSystem system, EntityUid? cause = null)
     {
-        /// <summary>
-        /// List of containers to apply damage to contents
-        /// </summary>
-        [DataField("containers")]
-        public List<string> Containers = new();
+        if (!system.EntityManager.TryGetComponent<ContainerManagerComponent>(owner, out var containerManager))
+            return;
 
-        /// <summary>
-        /// Damage specifier to apply to container contents.
-        /// If null, apply the current damage of the owner entity to all the contents
-        /// </summary>
-        [DataField]
-        public DamageSpecifier? Damage = null;
+        var containerSys = system.EntityManager.System<ContainerSystem>();
+        var damageSys = system.EntityManager.System<DamageableSystem>();
 
-        [DataField]
-        public bool IgnoreResistances = false;
-
-        public void Execute(EntityUid owner, DestructibleSystem system, EntityUid? cause = null)
+        foreach (var containerId in Containers)
         {
-            if (!system.EntityManager.TryGetComponent<ContainerManagerComponent>(owner, out var containerManager))
-                return;
+            if (!containerSys.TryGetContainer(owner, containerId, out var container, containerManager))
+                continue;
 
-            var containerSys = system.EntityManager.System<ContainerSystem>();
-            var damageSys = system.EntityManager.System<DamageableSystem>();
-
-            foreach (var containerId in Containers)
+            if (Damage == null)
             {
-                if (!containerSys.TryGetContainer(owner, containerId, out var container, containerManager))
+                if (!system.EntityManager.TryGetComponent<DamageableComponent>(owner, out var damageable))
+                    return;
+                Damage = damageable.Damage;
+            }
+            foreach (var ent in container.ContainedEntities)
+            {
+                if(!system.EntityManager.TryGetComponent<DamageableComponent>(ent, out var damageable))
                     continue;
-
-                if (Damage == null)
-                {
-                    if (!system.EntityManager.TryGetComponent<DamageableComponent>(owner, out var damageable))
-                        return;
-                    Damage = damageable.Damage;
-                }
-                foreach (var ent in container.ContainedEntities)
-                {
-                    if(!system.EntityManager.TryGetComponent<DamageableComponent>(ent, out var damageable))
-                        continue;
-                    damageSys.TryChangeDamage(ent, Damage, IgnoreResistances, true, damageable);
-                }
+                damageSys.TryChangeDamage(ent, Damage, IgnoreResistances, true, damageable);
             }
         }
     }
