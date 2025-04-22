07423a4d559741ed37b4ec1045c37fae54ca5763
Kyle Tyo <akikai297@gmail.com>
2025-04-22 14:25:25 -0400

    convert AmbientOverlayCommand

--- Content.Client/Audio/AmbientOverlayCommand.cs
+++ Content.Client/Audio/AmbientOverlayCommand.cs
@@ -4,12 +4,14 @@ namespace Content.Client.Audio;
 
 public sealed class AmbientOverlayCommand : IConsoleCommand
 {
+    [Dependency] private readonly IEntityManager _ent = default!;
+
     public string Command => "showambient";
     public string Description => "Shows all AmbientSoundComponents in the viewport";
     public string Help => $"{Command}";
     public void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        var system = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<AmbientSoundSystem>();
+        var system = _ent.System<AmbientSoundSystem>();
         system.OverlayEnabled ^= true;
 
         shell.WriteLine($"Ambient sound overlay set to {system.OverlayEnabled}");
