5b5182bea2852d5bd62b88cafdb02a2cf299639a
Samuel <samuelcamilo8@gmail.com>
2025-04-22 20:36:31 -0300

    add Xenoborg and Mothership components

---
+++ Content.Shared/Xenoborgs/Components/XenoborgComponent.cs
@@ -0,0 +1,13 @@
+namespace Content.Shared.Xenoborgs.Components;
+
+/// <summary>
+/// This component does nothing for now. It will be used for something in the future.
+/// </summary>
+[RegisterComponent]
+public sealed partial class Xenoborg : Component;
+
+/// <summary>
+/// This component for now is gonna be used for the pinpointer, but I suppose is also gonna more stuff in the future.
+/// </summary>
+[RegisterComponent]
+public sealed partial class Mothership : Component;
