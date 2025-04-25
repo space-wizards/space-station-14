0d83461e3eb307fc5d612fc034c337e8e1318f6d
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-04-25 12:45:34 -0400

    Factor out the profile sprite preview for re-use with character picker, late join, and profile editor. Add back job descriptions to the character pickers. Add non-crew antags to the profile previews.
    There might be a couple weird hacky things in here, but I'm planning a big profile editor refactor after this gets merged.

---
+++ Content.Client/Lobby/UI/ProfileEditorControls/ProfilePreviewSpriteView.cs
@@ -0,0 +1,70 @@
+﻿using Content.Shared.Preferences;
+using Content.Shared.Roles;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client.Lobby.UI.ProfileEditorControls;
+
+public sealed partial class ProfilePreviewSpriteView : SpriteView
+{
+
+    private IClientPreferencesManager _preferencesManager = default!;
+    private IPrototypeManager _prototypeManager = default!;
+    private IEntityManager _entManager = default!;
+    private ISharedPlayerManager _playerManager = default!;
+
+    public string? JobName { get; private set; }
+
+    public EntityUid PreviewDummy { get; private set; } = EntityUid.Invalid;
+
+    public void Initialize(IClientPreferencesManager prefMan,
+        IPrototypeManager protoMan,
+        IEntityManager entMan,
+        ISharedPlayerManager playerMan)
+    {
+        _preferencesManager = prefMan;
+        _prototypeManager = protoMan;
+        _entManager = entMan;
+        _playerManager = playerMan;
+    }
+
+    public void LoadPreview(ICharacterProfile profile, JobPrototype? jobOverride = null, bool showClothes = true)
+    {
+        _entManager.DeleteEntity(PreviewDummy);
+        PreviewDummy = EntityUid.Invalid;
+
+        switch (profile)
+        {
+            case HumanoidCharacterProfile humanoid:
+                LoadHumanoidEntity(humanoid, jobOverride, showClothes);
+                break;
+            default:
+                return;
+        }
+
+        SetEntity(PreviewDummy);
+        InvalidateMeasure();
+        _entManager.System<MetaDataSystem>().SetEntityName(PreviewDummy, profile.Name);
+    }
+
+    public void ReloadProfilePreview(ICharacterProfile profile)
+    {
+        switch (profile)
+        {
+            case HumanoidCharacterProfile humanoid:
+                ReloadHumanoidEntity(humanoid);
+                break;
+        }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (!disposing)
+            return;
+
+        _entManager.DeleteEntity(PreviewDummy);
+        PreviewDummy = EntityUid.Invalid;
+    }
+}
