99e3b7398bf7bedaf8d497b6bdd692745427a919
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-04-26 15:20:48 -0400

    remove dead includes

--- Content.Client/Lobby/LobbyUIController.cs
+++ Content.Client/Lobby/LobbyUIController.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using Content.Client.Guidebook;
 using Content.Client.Humanoid;
 using Content.Client.Inventory;
@@ -6,9 +5,6 @@ using Content.Client.Lobby.UI;
 using Content.Client.Players.PlayTimeTracking;
 using Content.Client.Station;
 using Content.Shared.CCVar;
-using Content.Shared.Clothing;
-using Content.Shared.GameTicking;
-using Content.Shared.Humanoid;
 using Content.Shared.Humanoid.Markings;
 using Content.Shared.Humanoid.Prototypes;
 using Content.Shared.Preferences;
@@ -21,7 +17,6 @@ using Robust.Client.State;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controllers;
 using Robust.Shared.Configuration;
-using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
