a7dbb7970ae6a36923145441693fb9190075f94f
ScarKy0 <scarky0@onet.eu>
2025-04-27 03:14:24 +0200

    doc

--- Content.Shared/ComponentTable/ComponentTableComponent.cs
+++ Content.Shared/ComponentTable/ComponentTableComponent.cs
@@ -10,6 +10,10 @@ namespace Content.Shared.ComponentTable;
 [Access(typeof(SharedComponentTableSystem))]
 public sealed partial class ComponentTableComponent : Component
 {
+    /// <summary>
+    /// The table from which to grab entities.
+    /// ALL components of the grabbed entities will be added to the holder of this component.
+    /// </summary>
     [DataField(required: true)]
     public EntityTableSelector Table = default!;
 }
