cf9bbbe298eb5ad0bd583ffab4b45e0ec3e55808
BIGZi0348 <svalker0348@gmail.com>
2025-04-28 21:09:49 +0300

    edits

--- Content.Shared/Weapons/Reflect/ReflectComponent.cs
+++ Content.Shared/Weapons/Reflect/ReflectComponent.cs
@@ -1,6 +1,6 @@
+using Content.Shared.Inventory;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
-using Content.Shared.Inventory;
 
 namespace Content.Shared.Weapons.Reflect;
 
@@ -21,19 +21,19 @@ public sealed partial class ReflectComponent : Component
     /// Select in which inventory slots it will reflect.
     /// By default, it will reflect in any inventory position, except pockets.
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite), DataField]
+    [DataField]
     public SlotFlags SlotFlags = SlotFlags.WITHOUT_POCKET;
 
     /// <summary>
     /// Is it allowed to reflect while being in hands.
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [DataField, AutoNetworkedField]
     public bool ReflectingInHands = true;
 
     /// <summary>
     /// Can only reflect when placed correctly.
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [DataField, ViewVariables(VVAccess.ReadOnly), AutoNetworkedField]
     public bool InRightPlace = true;
 
     /// <summary>
