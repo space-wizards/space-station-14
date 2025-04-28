3d8e319c4cc12f81d42aa552b4fed9512bb2092f
Kyle Tyo <36606155+VerinSenpai@users.noreply.github.com>
2025-04-28 11:23:50 -0400

    Update Content.Server/Salvage/SalvageSystem.Runner.cs
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Server/Salvage/SalvageSystem.Runner.cs
+++ Content.Server/Salvage/SalvageSystem.Runner.cs
@@ -74,7 +74,7 @@ public sealed partial class SalvageSystem
             ChatChannel.Radio,
             text,
             text,
-            _mapSystem.GetMap(mapId),
+            _mapSystem.GetMapOrInvalid(mapId),
             false,
             true,
             null);
