327e86984a5e3adb6ed8a618fb068199c40e91cd
Kyle Tyo <akikai297@gmail.com>
2025-05-03 16:44:55 -0400

    Revert "localized help and desc"
    This reverts commit d060d8a4ea6065f8e445012ac6216660fef649a9.

--- Content.Client/Access/Commands/ShowAccessReadersCommand.cs
+++ Content.Client/Access/Commands/ShowAccessReadersCommand.cs
@@ -13,8 +13,15 @@ public sealed class ShowAccessReadersCommand : LocalizedEntityCommands
 
     public override string Command => "showaccessreaders";
 
-    public override string Description => Loc.GetString($"cmd-show-access-readers-desc");
-    public override string Help => Loc.GetString($"cmd-show-access-readers-help");
+    public override string Description => "Toggles showing access reader permissions on the map";
+    public override string Help => """
+        Overlay Info:
+        -Disabled | The access reader is disabled
+        +Unrestricted | The access reader has no restrictions
+        +Set [Index]: [Tag Name]| A tag in an access set (accessor needs all tags in the set to be allowed by the set)
+        +Key [StationUid]: [StationRecordKeyId] | A StationRecordKey that is allowed
+        -Tag [Tag Name] | A tag that is not allowed (takes priority over other allows)
+        """;
     public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         if (_overlay.RemoveOverlay<AccessOverlay>())
