5922352e775ef46aea165a110402af325778b3cd
Kyle Tyo <36606155+VerinSenpai@users.noreply.github.com>
2025-05-03 17:07:06 -0400

    Update Content.Client/Parallax/ParallaxOverlay.cs
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Client/Parallax/ParallaxOverlay.cs
+++ Content.Client/Parallax/ParallaxOverlay.cs
@@ -34,7 +34,7 @@ public sealed class ParallaxOverlay : Overlay
 
     protected override bool BeforeDraw(in OverlayDrawArgs args)
     {
-        if (args.MapId == MapId.Nullspace || _entManager.HasComponent<BiomeComponent>(_mapSystem.GetMap(args.MapId)))
+        if (args.MapId == MapId.Nullspace || _entManager.HasComponent<BiomeComponent>(_mapSystem.GetMapOrInvalid(args.MapId)))
             return false;
 
         return true;
