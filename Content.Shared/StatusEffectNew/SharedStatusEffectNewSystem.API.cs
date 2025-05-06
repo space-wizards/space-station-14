fed1a1a4742147a6c164e8fdcab64fa1878b1da2
Ed <edwardxperia2000@gmail.com>
2025-05-06 21:51:05 +0300

    new system

---
+++ Content.Shared/StatusEffectNew/SharedStatusEffectNewSystem.API.cs
@@ -0,0 +1,147 @@
+using Content.Shared.StatusEffectNew.Components;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.StatusEffectNew;
+
+public sealed partial class SharedStatusEffectNewSystem
+{
+    /// <summary>
+    /// Attempts to add a status effect to the specified entity. Returns True if the effect is added or exists
+    /// and has been successfully extended in time, returns False if the status effect cannot be applied to this entity,
+    /// or for any other reason
+    /// </summary>
+    /// <param name="uid">The target entity on which the effect is added</param>
+    /// <param name="effectProto">ProtoId of the status effect entity. Make sure it has CP14StatusEffectComponent on it</param>
+    /// <param name="duration">Duration of status effect. Leave null and the effect will be permanent until it is removed using <c>TryRemoveStatusEffect</c></param>
+    /// <param name="resetCooldown">if True, the effect duration time will be reset and reapplied. If False, the effect duration time will be overlaid with the existing one.
+    /// In the other case, the effect will either be added for the specified time or its time will be extended for the specified time.</param>
+    /// <returns></returns>
+    public bool TryAddStatusEffect(EntityUid uid,
+        EntProtoId effectProto,
+        TimeSpan? duration = null,
+        bool resetCooldown = false)
+    {
+        if (TryGetStatusEffect(uid, effectProto, out var existedEffect))
+        {
+            //We don't need to add the effect if it already exists
+            if (duration is null)
+                return true;
+
+            if (existedEffect != null)
+            {
+                if (resetCooldown)
+                    SetStatusEffectTime(existedEffect.Value, duration.Value);
+                else
+                    AddStatusEffectTime(existedEffect.Value, duration.Value);
+                return true;
+            }
+        }
+
+        // We make this checks before status effect entity spawned
+        if (!_proto.TryIndex(effectProto, out var effectProtoData))
+            return false;
+
+        if (!effectProtoData.TryGetComponent<StatusEffectNewComponent>(out var effectProtoComp,
+                _compFactory))
+        {
+            Log.Error(
+                $"Entity {effectProto} does not have a {nameof(StatusEffectNewComponent)} component, but tried to apply it as a status effect on {ToPrettyString(uid)}.");
+            return false;
+        }
+
+        if (!_whitelist.CheckBoth(uid, effectProtoComp.Blacklist, effectProtoComp.Whitelist))
+            return false;
+
+        //Technically, on the client, all checks have been successful and we do not need to execute further code related to entity spawning, as this is the server's responsibility
+        if (_net.IsClient)
+            return true;
+        EnsureComp<StatusEffectContainerComponent>(uid, out var container);
+
+        //And only if all checks passed we spawn the effect
+        var effect = SpawnAttachedTo(effectProto, Transform(uid).Coordinates);
+        _transform.SetParent(effect, uid);
+        if (!_effectQuery.TryComp(effect, out var effectComp))
+            return false;
+
+        if (duration != null)
+            effectComp.EndEffectTime = _timing.CurTime + duration;
+
+        container.ActiveStatusEffects.Add(effect);
+        effectComp.AppliedTo = uid;
+
+        var ev = new StatusEffectApplied(uid, (effect, effectComp));
+        RaiseLocalEvent(uid, ref ev);
+        RaiseLocalEvent(effect, ref ev);
+
+        return true;
+    }
+
+    /// <summary>
+    /// Attempting to remove a status effect from an entity. Returns True if the status effect existed on the entity and was successfully removed, and False in any other case.
+    /// </summary>
+    public bool TryRemoveStatusEffect(EntityUid uid, EntProtoId effectProto)
+    {
+        if (!_containerQuery.TryComp(uid, out var container))
+            return false;
+
+        foreach (var effect in container.ActiveStatusEffects)
+        {
+            var meta = MetaData(effect);
+            if (meta.EntityPrototype is not null && meta.EntityPrototype == effectProto)
+            {
+                if (!_effectQuery.TryComp(effect, out var effectComp))
+                    return false;
+
+                var ev = new StatusEffectRemoved(uid, (effect, effectComp));
+                RaiseLocalEvent(uid, ref ev);
+                RaiseLocalEvent(effect, ref ev);
+
+                QueueDel(effect);
+                container.ActiveStatusEffects.Remove(effect);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Checks whether the specified entity is under a specific status effect.
+    /// </summary>
+    public bool HasStatusEffect(EntityUid uid, EntProtoId effectProto)
+    {
+        if (!_containerQuery.TryComp(uid, out var container))
+            return false;
+
+        foreach (var effect in container.ActiveStatusEffects)
+        {
+            var meta = MetaData(effect);
+            if (meta.EntityPrototype is not null && meta.EntityPrototype == effectProto)
+                return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Attempting to retrieve the EntityUid of a status effect from an entity.
+    /// </summary>
+    public bool TryGetStatusEffect(EntityUid uid, EntProtoId effectProto, out EntityUid? effect)
+    {
+        effect = null;
+        if (!_containerQuery.TryComp(uid, out var container))
+            return false;
+
+        foreach (var e in container.ActiveStatusEffects)
+        {
+            var meta = MetaData(e);
+            if (meta.EntityPrototype is not null && meta.EntityPrototype == effectProto)
+            {
+                effect = e;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Content.Shared/StatusEffectNew/SharedStatusEffectNewSystem.cs
@@ -0,0 +1,145 @@
+using Content.Shared.Alert;
+using Content.Shared.StatusEffectNew.Components;
+using Content.Shared.Whitelist;
+using Robust.Shared.Network;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
+
+namespace Content.Shared.StatusEffectNew;
+
+public sealed partial class SharedStatusEffectNewSystem : EntitySystem
+{
+    [Dependency] private readonly AlertsSystem _alerts = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly EntityWhitelistSystem _whitelist = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!;
+    [Dependency] private readonly IComponentFactory _compFactory = default!;
+    [Dependency] private readonly INetManager _net = default!;
+
+    private EntityQuery<StatusEffectContainerComponent> _containerQuery;
+    private EntityQuery<StatusEffectNewComponent> _effectQuery;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<StatusEffectContainerComponent, ComponentShutdown>(OnContainerShutdown);
+
+        SubscribeLocalEvent<StatusEffectNewComponent, StatusEffectApplied>(OnStatusEffectApplied);
+        SubscribeLocalEvent<StatusEffectNewComponent, StatusEffectRemoved>(OnStatusEffectRemoved);
+
+        _containerQuery = GetEntityQuery<StatusEffectContainerComponent>();
+        _effectQuery = GetEntityQuery<StatusEffectNewComponent>();
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<StatusEffectNewComponent>();
+        while (query.MoveNext(out var ent, out var effect))
+        {
+            if (effect.EndEffectTime is null)
+                continue;
+
+            if (!(_timing.CurTime >= effect.EndEffectTime))
+                continue;
+
+            if (effect.AppliedTo is null)
+                continue;
+
+            var meta = MetaData(ent);
+
+            if (meta.EntityPrototype is null)
+                continue;
+
+            TryRemoveStatusEffect(effect.AppliedTo.Value, meta.EntityPrototype);
+        }
+    }
+
+    private void OnContainerShutdown(Entity<StatusEffectContainerComponent> ent, ref ComponentShutdown args)
+    {
+        foreach (var effect in ent.Comp.ActiveStatusEffects)
+        {
+            QueueDel(effect);
+        }
+    }
+
+    private void AddStatusEffectTime(EntityUid effect, TimeSpan duration)
+    {
+        if (!_effectQuery.TryComp(effect, out var effectComp))
+            return;
+
+        if (effectComp.AppliedTo is null)
+            return;
+
+        if (effectComp.Alert is not null)
+        {
+            effectComp.EndEffectTime += duration;
+            _alerts.ShowAlert(
+                effectComp.AppliedTo.Value,
+                effectComp.Alert.Value,
+                cooldown: effectComp.EndEffectTime is null ? null : (_timing.CurTime, effectComp.EndEffectTime.Value));
+        }
+    }
+
+    private void SetStatusEffectTime(EntityUid effect, TimeSpan duration)
+    {
+        if (!_effectQuery.TryComp(effect, out var effectComp))
+            return;
+
+        if (effectComp.AppliedTo is null)
+            return;
+
+        if (effectComp.Alert is not null)
+        {
+            effectComp.EndEffectTime = _timing.CurTime + duration;
+            _alerts.ShowAlert(
+                effectComp.AppliedTo.Value,
+                effectComp.Alert.Value,
+                cooldown: effectComp.EndEffectTime is null ? null : (_timing.CurTime, effectComp.EndEffectTime.Value));
+        }
+    }
+
+    private void OnStatusEffectApplied(Entity<StatusEffectNewComponent> ent, ref StatusEffectApplied args)
+    {
+        if (ent.Comp.AppliedTo is null)
+            return;
+
+        if (ent.Comp.Alert is not null)
+        {
+            _alerts.ShowAlert(
+                ent.Comp.AppliedTo.Value,
+                ent.Comp.Alert.Value,
+                cooldown: ent.Comp.EndEffectTime is null ? null : (_timing.CurTime, ent.Comp.EndEffectTime.Value));
+        }
+
+        EntityManager.AddComponents(args.Target, ent.Comp.Components);
+    }
+
+    private void OnStatusEffectRemoved(Entity<StatusEffectNewComponent> ent, ref StatusEffectRemoved args)
+    {
+        if (ent.Comp.AppliedTo is null)
+            return;
+
+        if (ent.Comp.Alert is not null)
+        {
+            _alerts.ClearAlert(ent.Comp.AppliedTo.Value, ent.Comp.Alert.Value);
+        }
+
+        EntityManager.RemoveComponents(args.Target, ent.Comp.Components);
+    }
+}
+
+/// <summary>
+/// Calls on both effect entity and target entity, when a status effect is applied.
+/// </summary>
+[ByRefEvent]
+public readonly record struct StatusEffectApplied(EntityUid Target, Entity<StatusEffectNewComponent> Effect);
+
+/// <summary>
+/// Calls on both effect entity and target entity, when a status effect is removed.
+/// </summary>
+[ByRefEvent]
+public readonly record struct StatusEffectRemoved(EntityUid Target, Entity<StatusEffectNewComponent> Effect);
