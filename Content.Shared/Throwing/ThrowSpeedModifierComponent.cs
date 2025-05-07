d4b9454562ab8e4f06c713a295560c6680b4f70f
Thinbug <ringingsince1997@gmail.com>
2025-05-07 17:37:50 -0300

    small fixies

--- Content.Shared/Throwing/ThrowSpeedModifierComponent.cs
+++ Content.Shared/Throwing/ThrowSpeedModifierComponent.cs
@@ -8,14 +8,14 @@ public sealed partial class ThrowSpeedModifierComponent : Component
 {
     /// <summary>
     /// Modifies the speed at which the entity is thrown by adding its value to it.
-    /// It will not take effect if the result would be or go below 0.
+    /// It will default to 0 if the result would be lower than that.
     /// </summary>
     [DataField, AutoNetworkedField]
     public float FlatModifier;
 
     /// <summary>
     /// Modifies the speed at which the entity is thrown by multiplying its value.
-    /// It will not take effect if the result would be or go below 0.
+    /// It will default to 0 if the result would be lower than that.
     /// </summary>
     [DataField, AutoNetworkedField]
     public float Multiplier = 1;
--- Content.Shared/Throwing/ThrowSpeedModifierSystem.cs
+++ Content.Shared/Throwing/ThrowSpeedModifierSystem.cs
@@ -11,10 +11,9 @@ public sealed class ThrowSpeedModifierSystem : EntitySystem
 
     private void ApplyModifier(Entity<ThrowSpeedModifierComponent> ent, ref BeforeThrowEvent args)
     {
-        //Apply speed modifiers
         args.ThrowSpeed += ent.Comp.FlatModifier;
         args.ThrowSpeed *= ent.Comp.Multiplier;
-        //Make sure it's at least 0
-        if (args.ThrowSpeed < 0) { args.ThrowSpeed = 0; }
+
+        if (args.ThrowSpeed < 0) { args.ThrowSpeed = 0; }  //Make sure it's at least 0
     }
 }
