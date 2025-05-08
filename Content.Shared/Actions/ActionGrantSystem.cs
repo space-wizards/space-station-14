9a83deb5cb0ccdca42d8034980f37c2c2135d499
Голубь <124601871+Golubgik@users.noreply.github.com>
2025-05-09 01:14:29 +0700

    I'm dumb, but I get it.

--- Content.Shared/Actions/ActionGrantSystem.cs
+++ Content.Shared/Actions/ActionGrantSystem.cs
@@ -17,7 +17,7 @@ public sealed class ActionGrantSystem : EntitySystem
 
     private void OnItemGet(Entity<ItemActionGrantComponent> ent, ref GetItemActionsEvent args)
     {
-        if (itemActionGrant.ActiveIfWorn && getitemActions.InHand)
+        if (ent.ActiveIfWorn && args.InHand)
             return;
 
         if (!TryComp(ent.Owner, out ActionGrantComponent? grant))
