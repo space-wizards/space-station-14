394b5e71f6c75032714cfa9583ec764d9ab372c9
Tayrtahn <tayrtahn@gmail.com>
2025-05-09 11:18:10 -0400

    Add test that mob damage thresholds have alerts

---
+++ Content.IntegrationTests/Tests/Damageable/MobThresholdsTest.cs
@@ -0,0 +1,35 @@
+using Content.Shared.Mobs.Components;
+
+namespace Content.IntegrationTests.Tests.Damageable;
+
+public sealed class MobThresholdsTest
+{
+    [Test]
+    public async Task ValidateMobThresholds()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
+
+        var entMan = server.EntMan;
+        var protoMan = server.ProtoMan;
+
+        var protos = pair.GetPrototypesWithComponent<MobThresholdsComponent>();
+
+        Assert.Multiple(() =>
+        {
+            foreach (var (proto, comp) in protos)
+            {
+                // See which mob states are mapped to alerts
+                var alertStates = comp.StateAlertDict.Keys;
+                // Check each mob state that this mob can be in
+                foreach (var (_, state) in comp.Thresholds)
+                {
+                    // Make sure that an alert exists for each possible mob state
+                    Assert.That(alertStates, Does.Contain(state), $"{proto.ID} does not have an alert state for mob state {state}");
+                }
+            }
+        });
+
+        await pair.CleanReturnAsync();
+    }
+}
