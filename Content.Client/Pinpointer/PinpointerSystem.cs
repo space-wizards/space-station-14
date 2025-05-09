fc7c7dac783448a1644b779b4023e271638e1291
psykana <36602558+psykana@users.noreply.github.com>
2025-05-09 21:32:25 +0100

    Fix pinpointer showing the wrong way in a backpack

--- Content.Client/Pinpointer/PinpointerSystem.cs
+++ Content.Client/Pinpointer/PinpointerSystem.cs
@@ -1,11 +1,15 @@
 using Content.Shared.Pinpointer;
+using Content.Shared.Storage.EntitySystems;
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
+using Robust.Shared.Containers;
 
 namespace Content.Client.Pinpointer;
 
 public sealed class PinpointerSystem : SharedPinpointerSystem
 {
+    [Dependency] private readonly SharedContainerSystem _container = default!;
+    [Dependency] private readonly SharedStorageSystem _storage = default!;
     [Dependency] private readonly IEyeManager _eyeManager = default!;
 
     public override void Update(float frameTime)
@@ -18,12 +22,22 @@ public sealed class PinpointerSystem : SharedPinpointerSystem
         // because eye can change it rotation anytime
         // we need to update this arrow in a update loop
         var query = EntityQueryEnumerator<PinpointerComponent, SpriteComponent>();
-        while (query.MoveNext(out var _, out var pinpointer, out var sprite))
+        while (query.MoveNext(out var uid, out var pinpointer, out var sprite))
         {
             if (!pinpointer.HasTarget)
                 continue;
             var eye = _eyeManager.CurrentEye;
             var angle = pinpointer.ArrowAngle + eye.Rotation;
+
+            // Pinpointer may be flipped in a backpack etc, adjust for it
+            if (_container.TryGetContainingContainer((uid, null, null), out var container))
+            {
+                _storage.TryGetStorageLocation(uid, out var _, out var _, out var itemStorageLocation);
+                if (itemStorageLocation.Direction == Direction.East || itemStorageLocation.Direction == Direction.West)
+                {
+                    angle += Angle.FromDegrees(180);
+                }
+            }
 
             switch (pinpointer.DistanceToTarget)
             {
