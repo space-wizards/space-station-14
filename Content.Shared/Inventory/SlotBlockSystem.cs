6ef8ab15a4b8e92f5c472477fb722fca25d1c29a
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-05-10 19:38:28 +0300

    Beck T, my beloved

--- Content.Shared/Inventory/SlotBlockSystem.cs
+++ Content.Shared/Inventory/SlotBlockSystem.cs
@@ -5,7 +5,7 @@ namespace Content.Shared.Inventory;
 /// <summary>
 /// Handles prevention of items being unequipped and equipped from slots that are blocked by <see cref="SlotBlockComponent"/>.
 /// </summary>
-public sealed class SlotBlockSystem : EntitySystem
+public sealed partial class SlotBlockSystem : EntitySystem
 {
     public override void Initialize()
     {
