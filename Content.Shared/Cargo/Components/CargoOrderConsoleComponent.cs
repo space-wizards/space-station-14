35b2086c59b2a605c27d5e47d9cded7b5cfac804
imatsoup <93290208+imatsoup@users.noreply.github.com>
2025-05-10 13:11:17 -0400

    fixed unused fields in cargoorderconsolecomponent

--- Content.Shared/Cargo/Components/CargoOrderConsoleComponent.cs
+++ Content.Shared/Cargo/Components/CargoOrderConsoleComponent.cs
@@ -98,36 +98,6 @@ public sealed partial class CargoOrderConsoleComponent : Component
     public static readonly ProtoId<RadioChannelPrototype> BaseAnnouncementChannel = "Supply";
 
     /// <summary>
-    /// If set to true, restricts this console from ordering and has it print slips instead
-    /// </summary>
-    [DataField]
-    public bool SlipPrinter;
-
-    /// <summary>
-    /// The time at which the console will be able to print a slip again.
-    /// </summary>
-    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField]
-    public TimeSpan NextPrintTime = TimeSpan.Zero;
-
-    /// <summary>
-    /// The time between prints.
-    /// </summary>
-    [DataField]
-    public TimeSpan PrintDelay = TimeSpan.FromSeconds(5);
-
-    /// <summary>
-    /// The sound made when printing occurs
-    /// </summary>
-    [DataField]
-    public SoundSpecifier PrintSound = new SoundCollectionSpecifier("PrinterPrint");
-
-    /// <summary>
-    /// The sound made when an order slip is scanned
-    /// </summary>
-    [DataField]
-    public SoundSpecifier ScanSound = new SoundCollectionSpecifier("CargoBeep");
-
-    /// <summary>
     /// The time at which the console will be able to play the deny sound.
     /// </summary>
     [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField]
