32a9843ea7708b01b0042e36b806c3c63ba37b79
Kyle Tyo <akikai297@gmail.com>
2025-05-12 02:09:15 -0400

    localize ambientoverlaycommand properly

--- Content.Client/Audio/AmbientOverlayCommand.cs
+++ Content.Client/Audio/AmbientOverlayCommand.cs
@@ -7,11 +7,11 @@ public sealed class AmbientOverlayCommand : LocalizedEntityCommands
     [Dependency] private readonly AmbientSoundSystem _ambient = default!;
 
     public override string Command => "showambient";
-    public override string Description => "Shows all AmbientSoundComponents in the viewport";
+    public override string Description => Loc.GetString($"cmd-show-ambient-overlay-desc");
     public override string Help => $"{Command}";
     public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         _ambient.OverlayEnabled ^= true;
-        shell.WriteLine($"Ambient sound overlay set to {_ambient.OverlayEnabled}");
+        shell.WriteLine(Loc.GetString($"cmd-ambient-overlay-status", ("status", _ambient.OverlayEnabled)));
     }
 }
---
+++ Resources/Locale/en-US/commands/ambient-overlay-command.ftl
@@ -0,0 +1,2 @@
+﻿cmd-ambient-overlay-desc = Shows all AmbientSoundComponents in the viewport
+cmd-ambient-overlay-status = Ambient sound overlay set to {$status}
