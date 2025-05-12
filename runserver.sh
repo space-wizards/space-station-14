cf0d2f993dc255ae87691a319bfda0b85350425d
Shmeegoid <shmeegoid@gmail.com>
2025-05-12 16:03:40 +0100

    I'M STILL DOING IT

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-dotnet run --project Content.Server --configuration Release -- --config-file ../server_config.toml
+dotnet run --project Content.Server --configuration Release -- --config-file /home/shmeegoid/shmeeg-station/shmeeg-station-14/server_config.toml
 read -p "Press enter to continue"
 
