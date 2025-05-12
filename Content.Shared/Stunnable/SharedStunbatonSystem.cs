d7ad2c4af89785a51d20cdec278283ea69f932f5
Krunklehorn <krunklehornvector@gmail.com>
2025-05-12 05:09:15 -0400

    Prevent toggling stun batons

--- Content.Shared/Stunnable/SharedStunbatonSystem.cs
+++ Content.Shared/Stunnable/SharedStunbatonSystem.cs
@@ -1,9 +1,33 @@
+using Content.Shared.ActionBlocker;
+using Content.Shared.Item.ItemToggle.Components;
+
 namespace Content.Shared.Stunnable;
 
 public abstract class SharedStunbatonSystem : EntitySystem
 {
+    [Dependency] private readonly ActionBlockerSystem _actionBlocker = default!;
+
     public override void Initialize()
     {
         base.Initialize();
+
+        SubscribeLocalEvent<StunbatonComponent, ItemToggleActivateAttemptEvent>(TryTurnOn);
+        SubscribeLocalEvent<StunbatonComponent, ItemToggleDeactivateAttemptEvent>(TryTurnOff);
+    }
+
+    protected virtual void TryTurnOn(Entity<StunbatonComponent> entity, ref ItemToggleActivateAttemptEvent args)
+    {
+        if (args.User != null && !_actionBlocker.CanComplexInteract(args.User.Value)) {
+            args.Cancelled = true;
+            return;
+        }
+    }
+
+    protected virtual void TryTurnOff(Entity<StunbatonComponent> entity, ref ItemToggleDeactivateAttemptEvent args)
+    {
+        if (args.User != null && !_actionBlocker.CanComplexInteract(args.User.Value)) {
+            args.Cancelled = true;
+            return;
+        }
     }
 }
