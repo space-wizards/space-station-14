0afb298906530f5c1b1a92abbc03d5644fdc1888
Qerd <73325910+BigfootBravo@users.noreply.github.com>
2025-05-13 21:31:08 -0700

    Update PaperSystem.cs
    Didn't mean to change this in this pr.

--- Content.Shared/Paper/PaperSystem.cs
+++ Content.Shared/Paper/PaperSystem.cs
@@ -122,7 +122,7 @@ public sealed class PaperSystem : EntitySystem
                 if (entity.Comp.EditingDisabled)
                 {
                     var paperEditingDisabledMessage = Loc.GetString("paper-tamper-proof-modified-message");
-                    _popupSystem.PopupPredicted(paperEditingDisabledMessage, entity, args.User);
+                    _popupSystem.PopupEntity(paperEditingDisabledMessage, entity, args.User);
 
                     args.Handled = true;
                     return;
