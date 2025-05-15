88d3262f47584d4db55ba2aad88d12ebe752dcc6
Lanedon <matth.galvez@gmail>
2025-05-15 18:01:31 +0200

    trying audio stuff...

--- Content.Server/Clothing/Systems/SecurityHailerSystem.cs
+++ Content.Server/Clothing/Systems/SecurityHailerSystem.cs
@@ -6,6 +6,8 @@ using Content.Shared.Humanoid;
 using Content.Shared.Stealth.Components;
 using Robust.Shared.Physics;
 using Content.Shared.Coordinates;
+using Robust.Server.Audio;
+using Robust.Shared.Audio;
 
 namespace Content.Server.Clothing.Systems
 {
@@ -14,6 +16,7 @@ namespace Content.Server.Clothing.Systems
         [Dependency] private readonly ChatSystem _chat = default!;
         [Dependency] private readonly EntityLookupSystem _entityLookup = default!;
         [Dependency] private readonly SharedTransformSystem _transform = default!;
+        [Dependency] private readonly AudioSystem _audio = default!;
 
         public override void Initialize()
         {
@@ -25,22 +28,34 @@ namespace Content.Server.Clothing.Systems
         {
             Log.Debug("OnHailOrder servers side reached !");
 
-            //if (args.Handled)
-            //    return;
+            if (ev.Handled)
+                return;
+            bool exclamation = ExclamateHumanoidsAround(ent);
 
+            int index = PlaySoundEffect(ent);
+
+            //Play the voice line
             _chat.TrySendInGameICMessage(ev.Performer, "HALT!!", InGameICChatType.Speak, true, true, checkRadioPrefix: false);  //Speech that isn't sent to chat or adminlogs
-            //Exclamation point code here so it can be predicted
-            var (uid, comp) = ent;
-            if (!Resolve(uid, ref comp, false) || comp.Distance <= 0)
-                ev.Handled = false;
 
-            ExclamateHumanoidsAround(ent);
-            ev.Handled = true;
+
+            ev.Handled = exclamation;
         }
 
-        private void ExclamateHumanoidsAround(Entity<SecurityHailerComponent> ent)
+        private int PlaySoundEffect(Entity<SecurityHailerComponent> ent)
+        {
+            var resolver = _audio.ResolveSound(ent.Comp.LowAggressionSounds);
+            if (resolver is not ResolvedCollectionSpecifier collectionResolver)
+                return -1;
+
+            return collectionResolver.Index;
+        }
+
+        private bool ExclamateHumanoidsAround(Entity<SecurityHailerComponent> ent)
         {
             var (uid, comp) = ent;
+            if (!Resolve(uid, ref comp, false) || comp.Distance <= 0)
+                return false;
+
             StealthComponent? stealth = null;
             foreach (var iterator in
                 _entityLookup.GetEntitiesInRange<HumanoidAppearanceComponent>(_transform.GetMapCoordinates(uid), comp.Distance))
@@ -55,6 +70,8 @@ namespace Content.Server.Clothing.Systems
 
                 SpawnAttachedTo(comp.ExclamationEffect, iterator.Owner.ToCoordinates());
             }
+
+            return true;
         }
     }
 }
