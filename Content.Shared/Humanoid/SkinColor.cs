6437c136a218d08a89c47025c60fe2fa2656dabb
Killer Tamashi <tyler443344@outlook.com>
2025-05-15 16:12:02 -0400

    guh

--- Content.Shared/Humanoid/SkinColor.cs
+++ Content.Shared/Humanoid/SkinColor.cs
@@ -3,19 +3,19 @@ using Microsoft.VisualBasic.CompilerServices;
 
 namespace Content.Shared.Humanoid;
 
-public static class SkinColor
+public static class SkinColor 
 {
-    public const float MaxTintedHuesSaturation = 1;
-    public const float MinTintedHuesLightness = 0;
+    public const float MaxTintedHuesSaturation = 1; //starlight
+    public const float MinTintedHuesLightness = 0; //starlight
 
-    public const float MinHuesLightness = 0;
+    public const float MinHuesLightness = 0; //starlight
 
-    public const float MinFeathersHue = 0 / 360;
-    public const float MaxFeathersHue = 360 / 360;
-    public const float MinFeathersSaturation = 0 / 100;
-    public const float MaxFeathersSaturation = 100 / 100;
-    public const float MinFeathersValue = 0 / 100;
-    public const float MaxFeathersValue = 100 / 100;
+    public const float MinFeathersHue = 0 / 360; //starlight
+    public const float MaxFeathersHue = 360 / 360; //starlight
+    public const float MinFeathersSaturation = 0 / 100; //starlight
+    public const float MaxFeathersSaturation = 100 / 100; //starlight
+    public const float MinFeathersValue = 0 / 100; //starlight
+    public const float MaxFeathersValue = 100 / 100; //starlight, color limits is cringe
 
     public static Color ValidHumanSkinTone => Color.FromHsv(new Vector4(0.07f, 0.2f, 1f, 1f));
 
