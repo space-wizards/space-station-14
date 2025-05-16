4df7da4d581d251dabdd8219646d9afdcc7f019f
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2025-05-15 18:46:44 -0700

    Fix comment

--- Content.Shared/Friction/TileFrictionOverwrittenComponent.cs
+++ Content.Shared/Friction/TileFrictionOverwrittenComponent.cs
@@ -1,11 +1,15 @@
 ﻿namespace Content.Shared.Friction;
 
 /// <summary>
-/// An entity with this component will have their tileFriction overwritten with these value
+/// An entity with this component will have their tileFriction overwritten with these values
 /// </summary>
 [RegisterComponent]
 public sealed partial class TileFrictionOverwrittenComponent : Component
 {
+    /// <summary>
+    /// These three datafields correspond to the three friction datafields tiles have.
+    /// If defined they will take precedent over the values given by tileDef
+    /// </summary>
     [DataField]
     public float? Friction;
 
