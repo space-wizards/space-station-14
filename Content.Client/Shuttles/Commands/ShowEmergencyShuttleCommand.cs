e9feb333cbfd8adca0deba07556d2a8e7c108ee9
Kyle Tyo <akikai297@gmail.com>
2025-05-17 16:57:33 -0400

    revise showemergencyshuttle command

--- Content.Client/Shuttles/Commands/ShowEmergencyShuttleCommand.cs
+++ Content.Client/Shuttles/Commands/ShowEmergencyShuttleCommand.cs
@@ -8,11 +8,9 @@ public sealed class ShowEmergencyShuttleCommand : LocalizedEntityCommands
     [Dependency] private readonly ShuttleSystem _shuttle = default!;
 
     public override string Command => "showemergencyshuttle";
-    public override string Description => Loc.GetString($"cmd-show-emergency-shuttle-desc");
-    public override string Help => $"{Command}";
     public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         _shuttle.EnableShuttlePosition ^= true;
-        shell.WriteLine(Loc.GetString($"cmd-show-emergency-shuttle-status", ("status", _shuttle.EnableShuttlePosition)));
+        shell.WriteLine(Loc.GetString($"cmd-showemergencyshuttle-status", ("status", _shuttle.EnableShuttlePosition)));
     }
 }
--- Resources/Locale/en-US/commands/show-emergency-shuttle-command.ftl
+++ Resources/Locale/en-US/commands/show-emergency-shuttle-command.ftl
@@ -1,2 +1,2 @@
-﻿cmd-show-emergency-shuttle-desc = Shows the expected position of the emergency shuttle.
-cmd-show-emergency-shuttle-status = Set emergency shuttle debug to {$status}.
+﻿cmd-showemergencyshuttle-desc = Shows the expected position of the emergency shuttle.
+cmd-showemergencyshuttle-status = Set emergency shuttle debug to {$status}.
