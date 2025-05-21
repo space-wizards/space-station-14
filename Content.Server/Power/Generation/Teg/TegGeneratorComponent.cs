66007f9e0d2e0ea49bbd2bfd4e648900deac4a1d
mq <113324899+mqole@users.noreply.github.com>
2025-05-21 18:38:08 +1000

    early merge (#2540)

--- Content.Server/Power/Generation/Teg/TegGeneratorComponent.cs
+++ Content.Server/Power/Generation/Teg/TegGeneratorComponent.cs
@@ -75,4 +75,10 @@ public sealed partial class TegGeneratorComponent : Component
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("volumeMax")]
     public float VolumeMax = -4;
+
+    /// <summary>
+    /// Smoothing factor used to smooth out power generation.
+    /// </summary>
+    [DataField]
+    public float PowerSmoothingFactor = 0.2f;
 }
