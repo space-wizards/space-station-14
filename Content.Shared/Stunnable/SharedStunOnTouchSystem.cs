afd1398ad1020bd0d47b76bd6cc0b73144e8c4a1
imatsoup <93290208+imatsoup@users.noreply.github.com>
2025-05-22 01:08:36 -0400

    As per results of poll, removed the Immovable Rod changes. I also liked Arena's suggestion to increase the smite damage, so I did that

--- Content.Shared/Stunnable/SharedStunOnTouchSystem.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Shared.Stunnable;
-
-public abstract class SharedStunOnTouchSystem : EntitySystem
-{
-    public override void Initialize()
-    {
-        base.Initialize();
-    }
-}
