f4be490653dff6ccff9daa2296fe6c9d239efb8e
KarmaKitsuna <130065372+KarmaKitsuna@users.noreply.github.com>
2025-05-22 21:46:46 -0400

    Teach them a lesson

---
+++ Content.Server/Objectives/Components/TeachALessonTargetComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace Content.Server.Objectives.Components;
+
+/// <summary>
+/// 
+/// </summary>
+
+[RegisterComponent ]
+public sealed partial class TeachALessonTargetComponent : Component
+{ 
+   [DataField]
+   public HashSet<EntityUid> Teachers = new HashSet<EntityUid>();
+}
