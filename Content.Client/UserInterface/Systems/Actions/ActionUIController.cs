29806583fe27845577742f899b9647f8cc8d441c
Tayrtahn <tayrtahn@gmail.com>
2025-05-24 17:16:17 -0400

    Update UIControllers to use Log

--- Content.Client/UserInterface/Systems/Actions/ActionUIController.cs
+++ Content.Client/UserInterface/Systems/Actions/ActionUIController.cs
@@ -192,7 +192,7 @@ public sealed class ActionUIController : UIController, IOnStateChanged<GameplayS
                 return TryTargetEntityWorld(args, actionId, entMapTarget, user, comp) || !entMapTarget.InteractOnMiss;
 
             default:
-                Logger.Error($"Unknown targeting action: {actionId.GetType()}");
+                Log.Error($"Unknown targeting action: {actionId.GetType()}");
                 return false;
         }
     }
