7867cbec00e5157696ebd9f09b283c539e9a0551
Simon <63975668+Simyon264@users.noreply.github.com>
2025-05-24 20:49:52 +0200

    Add basic discord client integration with ooc and admin chat support (#33840)
    * Add basic discord client integration with ooc and admin chat support
    
    * Use username instead of global name
    
    WHY IS GLOBAL NAME NOT JUST THE USERNAME??? WHY ARE THERE NO DOC COMMENTS???
    
    * stuff
    
    * Reviews, methinks.
    
    * Reviews
    
    * reviews

---
+++ Content.Server/Discord/DiscordLink/DiscordChatLink.cs
@@ -0,0 +1,96 @@
+﻿using System.Threading.Tasks;
+using Content.Server.Chat.Managers;
+using Content.Shared.CCVar;
+using Content.Shared.Chat;
+using Discord.WebSocket;
+using Robust.Shared.Asynchronous;
+using Robust.Shared.Configuration;
+
+namespace Content.Server.Discord.DiscordLink;
+
+public sealed class DiscordChatLink
+{
+    [Dependency] private readonly DiscordLink _discordLink = default!;
+    [Dependency] private readonly IConfigurationManager _configurationManager = default!;
+    [Dependency] private readonly IChatManager _chatManager = default!;
+    [Dependency] private readonly ITaskManager _taskManager = default!;
+
+    private ulong? _oocChannelId;
+    private ulong? _adminChannelId;
+
+    public void Initialize()
+    {
+        _discordLink.OnMessageReceived += OnMessageReceived;
+
+        _configurationManager.OnValueChanged(CCVars.OocDiscordChannelId, OnOocChannelIdChanged, true);
+        _configurationManager.OnValueChanged(CCVars.AdminChatDiscordChannelId, OnAdminChannelIdChanged, true);
+    }
+
+    public void Shutdown()
+    {
+        _discordLink.OnMessageReceived -= OnMessageReceived;
+
+        _configurationManager.UnsubValueChanged(CCVars.OocDiscordChannelId, OnOocChannelIdChanged);
+        _configurationManager.UnsubValueChanged(CCVars.AdminChatDiscordChannelId, OnAdminChannelIdChanged);
+    }
+
+    private void OnOocChannelIdChanged(string channelId)
+    {
+        if (string.IsNullOrEmpty(channelId))
+        {
+            _oocChannelId = null;
+            return;
+        }
+
+        _oocChannelId = ulong.Parse(channelId);
+    }
+
+    private void OnAdminChannelIdChanged(string channelId)
+    {
+        if (string.IsNullOrEmpty(channelId))
+        {
+            _adminChannelId = null;
+            return;
+        }
+
+        _adminChannelId = ulong.Parse(channelId);
+    }
+
+    private void OnMessageReceived(SocketMessage message)
+    {
+        if (message.Author.IsBot)
+            return;
+
+        var contents = message.Content.ReplaceLineEndings(" ");
+
+        if (message.Channel.Id == _oocChannelId)
+        {
+            _taskManager.RunOnMainThread(() => _chatManager.SendHookOOC(message.Author.Username, contents));
+        }
+        else if (message.Channel.Id == _adminChannelId)
+        {
+            _taskManager.RunOnMainThread(() => _chatManager.SendHookAdmin(message.Author.Username, contents));
+        }
+    }
+
+    public async Task SendMessage(string message, string author, ChatChannel channel)
+    {
+        var channelId = channel switch
+        {
+            ChatChannel.OOC => _oocChannelId,
+            ChatChannel.AdminChat => _adminChannelId,
+            _ => throw new InvalidOperationException("Channel not linked to Discord."),
+        };
+
+        if (channelId == null)
+        {
+            // Configuration not set up. Ignore.
+            return;
+        }
+
+        // @ and < are both problematic for discord due to pinging. / is sanitized solely to kneecap links to murder embeds via blunt force
+        message = message.Replace("@", "\\@").Replace("<", "\\<").Replace("/", "\\/");
+
+        await _discordLink.SendMessageAsync(channelId.Value, $"**{channel.GetString()}**: `{author}`: {message}");
+    }
+}
