9a0c0a74f65a9f4af04c3a0dc6afc48f963aef93
PJB3005 <pieterjan.briers+git@gmail.com>
2025-05-24 16:42:54 +0200

    Make file dialog API usages read-only
    Requires https://github.com/space-wizards/RobustToolbox/commit/4e7de2f272cafc687c775fceb16b88681fcfd7e3
    
    Fixes #37762

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -1580,7 +1580,7 @@ namespace Content.Client.Lobby.UI
                 return;
 
             StartExport();
-            await using var file = await _dialogManager.OpenFile(new FileDialogFilters(new FileDialogFilters.Group("yml")));
+            await using var file = await _dialogManager.OpenFile(new FileDialogFilters(new FileDialogFilters.Group("yml")), FileAccess.Read);
 
             if (file == null)
             {
