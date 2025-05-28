0c756b579d0d94e3d6ea5a76808c2bf35488be46
TemporalOroboros <TemporalOroboros@gmail.com>
2025-05-28 01:29:38 -0700

    Make PryTile use Entity<T> Converts the old parameters into an obsolete overload

--- Content.Server/Atmos/EntitySystems/AtmosphereSystem.Utils.cs
+++ Content.Server/Atmos/EntitySystems/AtmosphereSystem.Utils.cs
@@ -28,7 +28,8 @@ public partial class AtmosphereSystem
 
         // Pay more for gas canisters that are more pure
         float purity = 1;
-        if (totalMoles > 0) {
+        if (totalMoles > 0)
+        {
             purity = maxComponent / totalMoles;
         }
 
@@ -88,7 +89,7 @@ public partial class AtmosphereSystem
 
             fixVacuum |= airtight.FixVacuum;
 
-            if(!airtight.AirBlocked)
+            if (!airtight.AirBlocked)
                 continue;
 
             blockedDirs |= airtight.AirBlockedDirection;
@@ -106,11 +107,18 @@ public partial class AtmosphereSystem
     /// </summary>
     /// <param name="mapGrid">The grid in question.</param>
     /// <param name="tile">The indices of the tile.</param>
-    private void PryTile(MapGridComponent mapGrid, Vector2i tile)
+    private void PryTile(Entity<MapGridComponent> mapGrid, Vector2i tile)
     {
-        if (!mapGrid.TryGetTileRef(tile, out var tileRef))
+        if (!_mapSystem.TryGetTileRef(mapGrid.Owner, mapGrid.Comp, tile, out var tileRef))
             return;
 
         _tile.PryTile(tileRef);
+    }
+
+    /// <inheritdoc cref="PryTile(Entity{MapGridComponent}, Vector2i)"/>
+    [Obsolete("please use the Entity<MapGridComponent> override")]
+    private void PryTile(MapGridComponent mapGrid, Vector2i tile)
+    {
+        PryTile((mapGrid.Owner, mapGrid), tile);
     }
 }
