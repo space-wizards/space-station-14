87924d133a073d6550f6be012216fd40e2868f82
Jessica M <jessica@maybe.sh>
2025-05-30 13:36:55 -0700

    Pathfinidng to bins w/target.. figuring out inserting

--- Content.Server/NPC/HTN/PrimitiveTasks/Operators/Specific/DisposalInsertOperator.cs
+++ Content.Server/NPC/HTN/PrimitiveTasks/Operators/Specific/DisposalInsertOperator.cs
@@ -1,27 +1,41 @@
+using Content.Server.Chat.Systems;
 using Content.Server.Disposal.Unit;
 using Content.Server.Power.Components;
 using Content.Server.Power.EntitySystems;
+using Content.Shared.Containers;
 using Content.Shared.Disposal.Components;
 using Content.Shared.Disposal.Tube;
 using Content.Shared.Disposal.Unit;
+using Content.Shared.Interaction;
+using Robust.Shared.Containers;
 
 namespace Content.Server.NPC.HTN.PrimitiveTasks.Operators.Specific;
 
 public sealed partial class DisposalInsertOperator : HTNOperator
 {
     [Dependency] private readonly IEntityManager _entManager = default!;
+    private ChatSystem _chat = default!;
     private DisposalUnitSystem _disposalSystem = default!;
+    private SharedInteractionSystem _interaction = default!;
+    [Dependency] private readonly SharedContainerSystem _container = default!;
 
     /// <summary>
-    /// Target entity to dispose of
+    /// Target entity to flush
     /// </summary>
     [DataField("targetKey", required: true)]
     public string TargetKey = string.Empty;
+
+    /// <summary>
+    /// Target disposal bin entity
+    /// </summary>
+    [DataField("disposalTargetKey", required: true)]
+    public string DisposalTargetKey = string.Empty;
 
     public override void Initialize(IEntitySystemManager sysManager)
     {
         base.Initialize(sysManager);
         _disposalSystem = sysManager.GetEntitySystem<DisposalUnitSystem>();
+        _interaction = sysManager.GetEntitySystem<SharedInteractionSystem>();
     }
 
     public override void TaskShutdown(NPCBlackboard blackboard, HTNOperatorStatus status)
@@ -33,12 +47,27 @@ public sealed partial class DisposalInsertOperator : HTNOperator
     public override HTNOperatorStatus Update(NPCBlackboard blackboard, float frameTime)
     {
         var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
+        var disposalUnitQuery = _entManager.GetEntityQuery<DisposalUnitComponent>();
+
 
         if (!blackboard.TryGetValue<EntityUid>(TargetKey, out var target, _entManager) || _entManager.Deleted(target))
             return HTNOperatorStatus.Failed;
 
-        if (_disposalSystem.CanInsert(target.Id
+        if (!blackboard.TryGetValue<EntityUid>(DisposalTargetKey, out var disposalTarget, _entManager) || _entManager.Deleted(target))
+            return HTNOperatorStatus.Failed;
+
+        if (!disposalUnitQuery.TryGetComponent(disposalTarget, out var disposalComp))
+            return HTNOperatorStatus.Failed;
+
+        var container = _container.GetContainer(disposalTarget, disposalComp.Container.ID);
+
+        _container.Insert(target, container);
+
+        //_disposalSystem.TryInsert(disposalTarget, target, null);
+
+        //_disposalSystem.DoInsertDisposalUnit(disposalTarget, target, owner);
 
         return HTNOperatorStatus.Finished;
+
     }
 }
---
+++ Content.Server/NPC/HTN/PrimitiveTasks/Operators/Specific/PickNearbyDisposableOperator.cs
@@ -0,0 +1,96 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Content.Server.Chat.Systems;
+using Content.Server.Disposal.Unit;
+using Content.Shared.NPC.Components;
+using Content.Server.NPC.Pathfinding;
+using Content.Shared.Interaction;
+using Content.Server.Disposal.Unit;
+using Content.Shared.Disposal.Components;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
+
+namespace Content.Server.NPC.HTN.PrimitiveTasks.Operators.Specific;
+
+public sealed partial class PickNearbyDisposableOperator : HTNOperator
+{
+    [Dependency] private readonly IEntityManager _entManager = default!;
+    private EntityLookupSystem _lookup = default!;
+    private ChatSystem _chat = default!;
+    private DisposalUnitSystem _disposalSystem = default!;
+    private PathfindingSystem _pathfinding = default!;
+
+    [DataField("rangeKey")] public string RangeKey = NPCBlackboard.MedibotInjectRange;
+
+    /// <summary>
+    /// Target entity to flush
+    /// </summary>
+    [DataField("targetKey", required: true)]
+    public string TargetKey = string.Empty;
+
+    /// <summary>
+    /// Target disposal bin entity
+    /// </summary>
+    [DataField("disposalTargetKey", required: true)]
+    public string DisposalTargetKey = string.Empty;
+
+    /// <summary>
+    /// Target entitycoordinates to move to.
+    /// </summary>
+    [DataField("targetMoveKey", required: true)]
+    public string TargetMoveKey = string.Empty;
+
+    public override void Initialize(IEntitySystemManager sysManager)
+    {
+        base.Initialize(sysManager);
+        _lookup = sysManager.GetEntitySystem<EntityLookupSystem>();
+        _disposalSystem = sysManager.GetEntitySystem<DisposalUnitSystem>();
+        _pathfinding = sysManager.GetEntitySystem<PathfindingSystem>();
+    }
+
+    public override async Task<(bool Valid, Dictionary<string, object>? Effects)> Plan(NPCBlackboard blackboard,
+        CancellationToken cancelToken)
+    {
+        var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
+
+        if (!blackboard.TryGetValue<float>(RangeKey, out var range, _entManager))
+            return (false, null);
+
+        var disposalUnitQuery = _entManager.GetEntityQuery<DisposalUnitComponent>();
+        var mailingUnitQuery = _entManager.GetEntityQuery<MailingUnitComponent>();
+        var mobState = _entManager.GetEntityQuery<MobStateComponent>();
+
+        foreach (var entity in _lookup.GetEntitiesInRange(owner, range))
+        {
+            if (!disposalUnitQuery.TryGetComponent(entity, out var disposalComp))
+                continue;
+            // dont want the mail boxes
+            if (mailingUnitQuery.TryGetComponent(entity, out var mailingComp))
+                continue;
+
+
+            foreach (var flushable in _lookup.GetEntitiesInRange(entity, 3))
+            {
+                if (mobState.TryGetComponent(flushable, out var state))
+                {
+                    var pathRange = SharedInteractionSystem.InteractionRange;
+                    var path = await _pathfinding.GetPath(owner, flushable, pathRange, cancelToken);
+
+                    if (path.Result == PathResult.NoPath)
+                        continue;
+
+                    return (true, new Dictionary<string, object>()
+                    {
+                        {TargetKey, entity},
+                        {DisposalTargetKey, entity},
+                        {TargetMoveKey, _entManager.GetComponent<TransformComponent>(flushable).Coordinates},
+                        {NPCBlackboard.PathfindKey, path},
+                    });
+                }
+            }
+
+        }
+
+        return (false, null);
+    }
+}
