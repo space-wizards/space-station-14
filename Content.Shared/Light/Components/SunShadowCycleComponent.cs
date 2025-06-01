0baf3c8d6b52b8092759234b2c4b5dcc1b1c6d9a
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-06-01 11:22:33 -0400

    Use a struct to hold the items for `Directions` in `SunShadowComponent`, fix serialization.

--- Content.Shared/Light/Components/SunShadowCycleComponent.cs
+++ Content.Shared/Light/Components/SunShadowCycleComponent.cs
@@ -25,11 +25,27 @@ public sealed partial class SunShadowCycleComponent : Component
     /// Time to have each direction applied. Will lerp from the current value to the next one.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public List<(float Ratio, Vector2 Direction, float Alpha)> Directions = new()
+    public List<SunShadowCycleDirection> Directions = new()
     {
-        (0f, new Vector2(0f, 3f), 0f),
-        (0.25f, new Vector2(-3f, -0.1f), 0.5f),
-        (0.5f, new Vector2(0f, -3f), 0.8f),
-        (0.75f, new Vector2(3f, -0.1f), 0.5f),
+        new SunShadowCycleDirection(0f, new Vector2(0f, 3f), 0f),
+        new SunShadowCycleDirection(0.25f, new Vector2(-3f, -0.1f), 0.5f),
+        new SunShadowCycleDirection(0.5f, new Vector2(0f, -3f), 0.8f),
+        new SunShadowCycleDirection(0.75f, new Vector2(3f, -0.1f), 0.5f),
     };
-}
+};
+
+[DataDefinition]
+[Serializable]
+public partial record struct SunShadowCycleDirection
+{
+    public SunShadowCycleDirection(float ratio, Vector2 direction, float alpha)
+    {
+        Ratio = ratio;
+        Direction = direction;
+        Alpha = alpha;
+    }
+
+    public float Ratio;
+    public Vector2 Direction;
+    public float Alpha;
+};
