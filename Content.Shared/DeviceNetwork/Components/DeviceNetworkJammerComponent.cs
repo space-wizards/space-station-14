22dfa54b7bb1b1a74968b2a6f79a3f874d722244
Samuel <samuelcamilo8@gmail.com>
2025-06-01 20:19:52 -0300

    add summary

--- Content.Shared/DeviceNetwork/Components/DeviceNetworkJammerComponent.cs
+++ Content.Shared/DeviceNetwork/Components/DeviceNetworkJammerComponent.cs
@@ -23,6 +23,9 @@ public sealed partial class DeviceNetworkJammerComponent : Component
     [DataField, AutoNetworkedField]
     public HashSet<string> JammableNetworks = [];
 
+    /// <summary>
+    /// Device networks frequencies that wont be jammed.
+    /// </summary>
     [DataField]
     public HashSet<uint> FrequenciesExcluded = [];
 
