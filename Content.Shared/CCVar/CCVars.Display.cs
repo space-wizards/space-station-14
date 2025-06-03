1dfb5b090ebaf1f998a134c2d1e4637e13aa6796
B_Kirill <cool.bkirill@yandex.ru>
2025-06-03 13:57:07 +1000

    Fix whitespaces

--- Content.Shared/CCVar/CCVars.Display.cs
+++ Content.Shared/CCVar/CCVars.Display.cs
@@ -5,6 +5,6 @@ namespace Content.Shared.CCVar;
 public sealed partial class CCVars
 {
     // Sharpening shader
-    public static readonly CVarDef<int> DisplaySharpening = 
+    public static readonly CVarDef<int> DisplaySharpening =
         CVarDef.Create("display.sharpening", 0, CVar.ARCHIVE | CVar.CLIENTONLY);
 }
