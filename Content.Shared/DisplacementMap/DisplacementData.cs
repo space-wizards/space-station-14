7b06e34d00488fe467867c785b8a543987085775
PJB3005 <pieterjan.briers+git@gmail.com>
2025-06-03 15:26:57 +0200

    Fix displacement map rendering
    918709cb4742d02e9822a651cf8e74727ba997de changed the way humans work to completely remove the stencil mask thing for clothing cutouts, in favor of displacement maps. Except it didn't actually remove the stencil parameters from the shader prototype, so this caused the shader to render incorrectly in SpriteView instances.
    
    I introduced a new DisplacedDraw shader that doesn't have the stencil in use, as removing the old one would probably be a more annoying breaking change.
    
    Fixes #37629

--- Content.Shared/DisplacementMap/DisplacementData.cs
+++ Content.Shared/DisplacementMap/DisplacementData.cs
@@ -10,5 +10,5 @@ public sealed partial class DisplacementData
     public Dictionary<int, PrototypeLayerData> SizeMaps = new();
 
     [DataField]
-    public string? ShaderOverride = "DisplacedStencilDraw";
+    public string? ShaderOverride = "DisplacedDraw";
 }
