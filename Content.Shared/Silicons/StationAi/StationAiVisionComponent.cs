b8995dff2b12bf39b856927541206490bb531c57
chromiumboy <chromium.boy@gmail.com>
2025-06-04 10:08:03 -0500

    Initial commit

--- Content.Shared/Silicons/StationAi/StationAiVisionComponent.cs
+++ Content.Shared/Silicons/StationAi/StationAiVisionComponent.cs
@@ -6,14 +6,26 @@ namespace Content.Shared.StationAi;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState, Access(typeof(SharedStationAiSystem))]
 public sealed partial class StationAiVisionComponent : Component
 {
+    /// <summary>
+    /// Determines whether the entity is actively providing vision to the station AI.
+    /// </summary>
     [DataField, AutoNetworkedField]
     public bool Enabled = true;
 
+    /// <summary>
+    /// Determines whether the entity's vision is blocked by walls.
+    /// </summary>
     [DataField, AutoNetworkedField]
     public bool Occluded = true;
 
     /// <summary>
-    /// Range in tiles
+    /// Determines whether the entity needs to be receiving power to provide vision to the station AI.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool NeedsPower = false;
+
+    /// <summary>
+    /// Vision range in tiles.
     /// </summary>
     [DataField, AutoNetworkedField]
     public float Range = 7.5f;
