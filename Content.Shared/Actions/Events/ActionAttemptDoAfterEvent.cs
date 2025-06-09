f981b75b875a76d45176abc2646b6fdb0b39eef6
keronshb <keronshb@live.com>
2025-06-08 23:24:51 -0400

    Adds Action DoAfter Events

---
+++ Content.Shared/Actions/Events/ActionAttemptDoAfterEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Shared.Actions.Events;
+
+/// <summary>
+/// Raised when attempting to run a DoAfter on an Action, used to trigger a DoAfter on an Action (if it has the DoAfter component)
+/// </summary>
+[ByRefEvent]
+public record struct ActionAttemptDoAfterEvent(EntityUid User, RequestPerformActionEvent requestEvent);
---
+++ Content.Shared/Actions/Events/ActionDoAfterEvent.cs
@@ -0,0 +1,20 @@
+﻿using Content.Shared.DoAfter;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Actions.Events;
+
+/// <summary>
+/// The event that triggers when an action doafter is completed or cancelled
+/// </summary>
+[Serializable, NetSerializable]
+public sealed partial class ActionDoAfterEvent : DoAfterEvent
+{
+    public readonly RequestPerformActionEvent RequestEvent;
+
+    public ActionDoAfterEvent(RequestPerformActionEvent requestEvent)
+    {
+        RequestEvent = requestEvent;
+    }
+
+    public override DoAfterEvent Clone() => this;
+}
