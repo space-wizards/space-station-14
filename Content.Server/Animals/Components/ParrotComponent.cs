0106cad1804d5b6cfe7830da767e9c2176485167
Crude Oil <crude@croil.net>
2025-06-10 23:45:41 +0200

    high tech parrot functionality

---
+++ Content.Server/Animals/Components/ParrotComponent.cs
@@ -0,0 +1,73 @@
+namespace Content.Server.Animals.Components;
+
+/// <summary>
+/// Component for parroting behavior
+/// </summary>
+[RegisterComponent, AutoGenerateComponentState]
+public sealed partial class ParrotComponent : Component
+{
+    /// <summary>
+    /// List of speech entries this entity has learned
+    /// </summary>
+    public List<string> SpeechMemory = new();
+
+    /// <summary>
+    /// Whether or not this parrot is listening for new entries
+    /// </summary>
+    [DataField]
+    public bool Listening = false;
+
+    /// <summary>
+    /// Range of hearing for the entity
+    /// </summary>
+    [DataField]
+    public int ListenRange = 10;
+
+    /// <summary>
+    /// The % chance an entity with this component learns a phrase when learning is off cooldown
+    /// </summary>
+    [DataField]
+    public float LearnChance = 0.5f;
+
+    /// <summary>
+    /// Time in seconds before another attempt can be made at learning a phrase
+    /// </summary>
+    [DataField]
+    public float LearnCooldown = 60f;
+
+    [DataField]
+    public TimeSpan NextLearnInterval = TimeSpan.FromSeconds(0.0f);
+
+    /// <summary>
+    /// The number of speech entries that are remembered
+    /// </summary>
+    [DataField]
+    public int MaxSpeechMemory = 10;
+
+    /// <summary>
+    /// Minimum length of a speech entry
+    /// </summary>
+    [DataField]
+    public int MinEntryLength = 4;
+
+    /// <summary>
+    /// Maximum length of a speech entry
+    /// </summary>
+    [DataField]
+    public int MaxEntryLength = 50;
+
+    /// <summary>
+    /// Minimum time to wait after speaking to speak again
+    /// </summary>
+    [DataField]
+    public float MinSpeakInterval = 4.0f * 60f;
+
+    /// <summary>
+    /// Maximum time to wait after speaking to speak again
+    /// </summary>
+    [DataField]
+    public float MaxSpeakInterval = 8.0f * 60f;
+
+    [DataField]
+    public TimeSpan NextSpeakInterval = TimeSpan.FromSeconds(0.0f);
+}
---
+++ Content.Server/Animals/Systems/ParrotSystem.cs
@@ -0,0 +1,222 @@
+using Content.Server.Administration.Logs;
+using Content.Server.Animals.Components;
+using Content.Server.Chat.Systems;
+using Content.Server.Radio;
+using Content.Server.Radio.Components;
+using Content.Server.Radio.EntitySystems;
+using Content.Server.Speech;
+using Content.Server.Speech.Components;
+using Content.Server.Speech.EntitySystems;
+using Content.Shared.Clothing.EntitySystems;
+using Content.Shared.Database;
+using Content.Shared.Inventory;
+using Content.Shared.Radio;
+using Content.Shared.Speech;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+using Robust.Shared.Timing;
+
+namespace Content.Server.Animals.Systems;
+
+public sealed partial class ParrotSystem : EntitySystem
+{
+    [Dependency] private readonly IAdminLogManager _adminLogger = default!;
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!;
+    [Dependency] private readonly ChatSystem _chat = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
+    [Dependency] private readonly RadioSystem _radio = default!;
+    [Dependency] private readonly ParrotAccentSystem _parrotAccent = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ParrotComponent, ComponentInit>(OnParrotInit);
+
+        SubscribeLocalEvent<ParrotComponent, ListenEvent>(OnListen);
+        SubscribeLocalEvent<ParrotComponent, RadioReceiveEvent>(OnRadioReceive);
+
+        SubscribeLocalEvent<ParrotComponent, ParrotLearnEvent>(OnLearn);
+        SubscribeLocalEvent<ParrotComponent, ParrotSpeakEvent>(OnSpeak);
+    }
+
+    private void OnParrotInit(Entity<ParrotComponent> entity, ref ComponentInit args)
+    {
+        StartListening(entity);
+    }
+
+    private void StartListening(Entity<ParrotComponent> entity)
+    {
+        if (entity.Comp.Listening)
+            return;
+
+        entity.Comp.Listening = true;
+
+        EnsureComp<ActiveListenerComponent>(entity);
+    }
+
+    private void StartListening(EntityUid uid, ParrotComponent parrot)
+    {
+        if (parrot.Listening)
+            return;
+
+        parrot.Listening = true;
+
+        EnsureComp<ActiveListenerComponent>(uid);
+    }
+
+    private void StopListening(Entity<ParrotComponent> entity)
+    {
+        if (!entity.Comp.Listening)
+            return;
+
+        entity.Comp.Listening = false;
+
+        RemCompDeferred<ActiveListenerComponent>(entity);
+    }
+
+    private void OnListen(Entity<ParrotComponent> entity, ref ListenEvent args)
+    {
+        Learn(entity, args.Message, args.Source);
+    }
+
+    private void OnRadioReceive(Entity<ParrotComponent> entity, ref RadioReceiveEvent args)
+    {
+        Learn(entity, args.Message, args.MessageSource);
+    }
+
+    private void Learn(Entity<ParrotComponent> entity, string incomingMessage, EntityUid source)
+    {
+        // too early to learn
+        if (_random.NextFloat() > entity.Comp.LearnChance)
+            return;
+
+        // ignore speakers with ParrotComponent or things get silly
+        if (HasComp<ParrotComponent>(source))
+            return;
+
+        var message = incomingMessage.Trim();
+
+        if (string.IsNullOrWhiteSpace(message))
+            return;
+
+        // message length out of bounds
+        if (message.Length < entity.Comp.MinEntryLength || message.Length > entity.Comp.MaxEntryLength)
+            return;
+
+        // actually fire the learning event
+        var learnEvent = new ParrotLearnEvent(message, source);
+        RaiseLocalEvent(entity, ref learnEvent);
+    }
+
+    private void OnLearn(Entity<ParrotComponent> entity, ref ParrotLearnEvent args)
+    {
+        // set new time for learn interval
+        entity.Comp.NextLearnInterval = _gameTiming.CurTime + TimeSpan.FromSeconds(entity.Comp.LearnCooldown);
+
+        // reset next speak interval if this is the first thing the parrot learnt
+        if (entity.Comp.SpeechMemory.Count == 0)
+        {
+            var intervalSeconds = _random.NextFloat(entity.Comp.MinSpeakInterval, entity.Comp.MaxSpeakInterval);
+            var randomSpeakInterval = TimeSpan.FromSeconds(intervalSeconds);
+
+            entity.Comp.NextSpeakInterval = _gameTiming.CurTime + randomSpeakInterval;
+        }
+
+        var tryLearnEvent = new ParrotTryLearnEvent(args.Message, args.Teacher);
+        RaiseLocalEvent(entity, ref tryLearnEvent);
+
+        // expect to be canceled if dead or something
+        // potentiall also if poly has more elaborate learning stuff
+        if (tryLearnEvent.Cancelled)
+            return;
+
+        _adminLogger.Add(LogType.Chat, LogImpact.Low, $"Parroting entity {ToPrettyString(entity):entity} learned the phrase \"{args.Message}\" from {ToPrettyString(args.Teacher):speaker}");
+
+        // can fit more in peanut brain :)
+        if (entity.Comp.SpeechMemory.Count < entity.Comp.MaxSpeechMemory)
+        {
+            entity.Comp.SpeechMemory.Add(args.Message);
+        }
+        // too much in brain. remove something :(
+        else
+        {
+            var replaceIdx = _random.Next(entity.Comp.SpeechMemory.Count);
+            entity.Comp.SpeechMemory[replaceIdx] = args.Message;
+        }
+
+
+        if (entity.Comp.LearnCooldown == 0.0f)
+            return;
+
+        StopListening(entity);
+    }
+
+    private void OnSpeak(Entity<ParrotComponent> entity, ref ParrotSpeakEvent args)
+    {
+        if (!HasComp<SpeechComponent>(entity))
+            return;
+
+        var intervalSeconds = _random.NextFloat(entity.Comp.MinSpeakInterval, entity.Comp.MaxSpeakInterval);
+        var randomSpeakInterval = TimeSpan.FromSeconds(intervalSeconds);
+
+        entity.Comp.NextSpeakInterval = _gameTiming.CurTime + randomSpeakInterval;
+
+        var message = _random.Pick(entity.Comp.SpeechMemory);
+
+        _chat.TrySendInGameICMessage(entity, message, InGameICChatType.Speak, ChatTransmitRange.Normal);
+
+        TrySpeakRadio(entity, message);
+    }
+
+    private bool TrySpeakRadio(Entity<ParrotComponent> entity, string message)
+    {
+        if (!_inventory.TryGetSlotEntity(entity, "ears", out var item))
+            return false;
+
+        if (!TryComp<ActiveRadioComponent>(item.Value, out var radio))
+            return false;
+
+        foreach (var channel in radio.Channels)
+        {
+            _radio.SendRadioMessage(entity, message, _proto.Index<RadioChannelPrototype>(channel), item.Value);
+        }
+
+        return true;
+    }
+
+    public override void Update(float frameTime)
+    {
+        var currentGameTime = _gameTiming.CurTime;
+
+        var query = EntityQueryEnumerator<ParrotComponent>();
+        while (query.MoveNext(out var uid, out var parrot))
+        {
+            if (!parrot.Listening && currentGameTime > parrot.NextLearnInterval)
+                StartListening(uid, parrot);
+
+            // no need to continue to speak if there is nothing to say
+            if (parrot.SpeechMemory.Count == 0)
+                continue;
+
+            // return if too early to speak
+            if (currentGameTime < parrot.NextSpeakInterval)
+                continue;
+
+            var speakEvent = new ParrotSpeakEvent();
+            RaiseLocalEvent<ParrotSpeakEvent>(uid, ref speakEvent);
+        }
+    }
+}
+
+[ByRefEvent]
+public record struct ParrotTryLearnEvent(string Message, EntityUid Teacher)
+{
+    public bool Cancelled = false;
+};
+
+[ByRefEvent]
+public readonly record struct ParrotLearnEvent(string Message, EntityUid Teacher);
+
+[ByRefEvent]
+public readonly record struct ParrotSpeakEvent();
