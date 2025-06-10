de351bd866577a1bf3e406dfebc0ea87d5f39690
beck-thompson <beck314159@hotmail.com>
2025-06-10 15:41:07 -0700

    telemetry

--- Content.Server.Database/Migrations/Sqlite/20250610212733_telemetry_migration1.Designer.cs
+++ Content.Server.Database/Migrations/Sqlite/20250610212733_telemetry_migration1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Content.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Content.Server.Database.Migrations.Sqlite
 {
     [DbContext(typeof(SqliteServerDbContext))]
-    partial class SqliteServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250610212733_telemetry_migration1")]
+    partial class telemetry_migration1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
