4cac0ef5b46cc25b2a2c5f58525cd84776c825c2
Samuel <samuelcamilo8@gmail.com>
2025-06-13 13:11:46 -0300

    zero missing

--- Content.Client/Robotics/UI/RoboticsConsoleWindow.xaml.cs
+++ Content.Client/Robotics/UI/RoboticsConsoleWindow.xaml.cs
@@ -123,7 +123,7 @@ public sealed partial class RoboticsConsoleWindow : FancyWindow
         BorgSprite.Texture = _sprite.Frame0(data.ChassisSprite!);
 
         var batteryColor = data.Charge switch {
-            < 0.2f => "#FF000", // red
+            < 0.2f => "#FF0000", // red
             < 0.4f => "#FF7F00", // orange
             < 0.6f => "#FFFF00", // yellow
             < 0.8f => "#7FFF00", // lime?
@@ -131,7 +131,7 @@ public sealed partial class RoboticsConsoleWindow : FancyWindow
         };
 
         var hpPercentColor = data.HpPercent switch {
-            < 0.2f => "#FF000", // red
+            < 0.2f => "#FF0000", // red
             < 0.4f => "#FF7F00", // orange
             < 0.6f => "#FFFF00", // yellow
             < 0.8f => "#7FFF00", // lime?
