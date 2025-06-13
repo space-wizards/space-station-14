49121a770a8cd743d650ec0877168c4eca21a3f3
PJB3005 <pieterjan.briers+git@gmail.com>
2025-06-13 15:57:25 +0200

    Enable parallel processing on pow3r again
    Originally disabled due to a theory of it causing bugs, it was re-enabled on Vulture, and I'm not aware of it having caused any issues there.

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -36,5 +36,5 @@ public sealed partial class CCVars : CVars
     /// Set to true to disable parallel processing in the pow3r solver.
     /// </summary>
     public static readonly CVarDef<bool> DebugPow3rDisableParallel =
-        CVarDef.Create("debug.pow3r_disable_parallel", true, CVar.SERVERONLY);
+        CVarDef.Create("debug.pow3r_disable_parallel", false, CVar.SERVERONLY);
 }
