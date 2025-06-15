ce9c0d7bf30365db72a2786508b2496bca2610a2
Stonylogray <clarkbradleym@gmail.com>
2025-06-15 13:21:08 -0500

    keb tuoy

--- Content.Server/Pointing/EntitySystems/RoguePointingSystem.cs
+++ Content.Server/Pointing/EntitySystems/RoguePointingSystem.cs
@@ -32,7 +32,7 @@ namespace Content.Server.Pointing.EntitySystems
             var angering = _random.Pick(targets);
             angering.Comp.RemainingAnger -= 1;
             if (angering.Comp.RemainingAnger <= 0)
-                RemComp<PointingArrowAngeringComponent>(angering);
+                RemComp<PointingArrowAngeringComponent>(uid);
 
             return angering.Owner;
         }
