2abf4983a9e749799771cbbe71554b58785a60cf
Kyle Tyo <akikai297@gmail.com>
2025-06-15 14:59:02 -0400

    :/

--- Content.Client/Decals/ToggleDecalCommand.cs
+++ Content.Client/Decals/ToggleDecalCommand.cs
@@ -2,11 +2,14 @@
 
 namespace Content.Client.Decals;
 
-public sealed class ToggleDecalCommand : LocalizedCommands
+public sealed class ToggleDecalCommand : LocalizedEntityCommands
 {
+    [Dependency] private readonly DecalSystem _decal = default!;
+
     public override string Command => "toggledecals";
 
     public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
+        _decal.ToggleOverlay();
     }
 }
