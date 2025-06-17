a969f3a956dc95b3706e14cd53a848c55b3d82b9
Thinbug <ringingsince1997@gmail.com>
2025-06-17 16:47:38 -0300

    small fixie push

--- Content.Server/GhostTypes/BeforeBodyDestructionEvent.cs
+++ Content.Server/GhostTypes/BeforeBodyDestructionEvent.cs
@@ -2,6 +2,7 @@ namespace Content.Server.GhostTypes;
 
 /// <summary>
 /// Raised right before a body is deleted (gibbed or otherwise)
+/// small change so i can try pushing again
 /// </summary>
 [ByRefEvent]
 public readonly record struct BeforeBodyDestructionEvent;
