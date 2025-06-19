9e18a4c68b9005036984cd24eea186fea5755afb
Merrokitsune <xudcii9i@duck.com>
2025-06-19 12:21:58 +0700

    Preparing for cherry-pick.

--- Content.Shared/_Impstation/Containers/AntiTamper/AntiTamperDisarmDoAfterEvent.cs
+++
@@ -1,9 +0,0 @@
-using Content.Shared.DoAfter;
-using Robust.Shared.Serialization;
-
-namespace Content.Shared.Containers.AntiTamper;
-
-[Serializable, NetSerializable]
-public sealed partial class AntiTamperDisarmDoAfterEvent : SimpleDoAfterEvent
-{
-}
