545df4cbb8d70373372eb42a580c12db3d3d7c97
TrixxedHeart <46364955+TrixxedBit@users.noreply.github.com>
2025-06-19 18:57:25 -0500

    Revert "Added dropbox fix, potentially cursed"
    This reverts commit a709883366fbee813e839742125e70844672af29.

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -241,14 +241,6 @@ namespace Content.Client.Lobby.UI
 
             RgbSkinColorContainer.AddChild(_rgbSkinColorSelector = new ColorSelectorSliders());
             _rgbSkinColorSelector.SelectorType = ColorSelectorSliders.ColorSelectorType.Hsv; // defaults color selector to HSV
-            // this is most certainly not the right way to do this but this was the only way I could get it to find the dropbox reliably.
-            if (_rgbSkinColorSelector.GetChild(0) is BoxContainer rootBox &&
-                rootBox.GetChild(0) is BoxContainer headerBox &&
-                headerBox.GetChild(0) is OptionButton typeSelector)
-            {
-                typeSelector.Select((int) ColorSelectorSliders.ColorSelectorType.Hsv);
-            }
-
             _rgbSkinColorSelector.OnColorChanged += _ =>
             {
                 OnSkinColorOnValueChanged();
