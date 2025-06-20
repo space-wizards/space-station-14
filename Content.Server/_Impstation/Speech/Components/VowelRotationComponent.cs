3b66f5fcc341d0601d54a8ab0ffaffed90ad05ad
beck <163376292+widgetbeck@users.noreply.github.com>
2025-06-20 01:35:09 -0400

    vowel rotation accent + trait (#2817)
    * add the vowel rotation accent + trait
    
    * significantly improve the vowel rotation accent + fix EIOUE + fix whitespace
    
    * clarify description

---
+++ Content.Server/_Impstation/Speech/Components/VowelRotationComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server.Speech.Components;
+
+/// <summary>
+///     Rotates each vowel to the right by one.
+/// </summary>
+[RegisterComponent]
+public sealed partial class VowelRotationComponent : Component
+{
+
+}
