a0544fdbf202fbbe8193edd63ef9c4e2043f086c
Perry Fraser <perryprog@users.noreply.github.com>
2025-06-21 05:06:49 -0400

    Make gas tank UI a bit more network-happy (#38184)
    * fix: network gas tank output pressure
    
    * fix: don't overwrite gas tank output pressure during editing

--- Content.Shared/Atmos/Components/GasTankComponent.cs
+++ Content.Shared/Atmos/Components/GasTankComponent.cs
@@ -44,7 +44,7 @@ public sealed partial class GasTankComponent : Component, IGasMixtureHolder
     /// <summary>
     ///     Distributed pressure.
     /// </summary>
-    [DataField]
+    [DataField, AutoNetworkedField]
     public float OutputPressure = DefaultOutputPressure;
 
     /// <summary>
