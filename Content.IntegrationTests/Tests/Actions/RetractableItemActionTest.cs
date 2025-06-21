7e21ddc51ba6b52d2efa15eb04c94cdb1536dbb3
Tayrtahn <tayrtahn@gmail.com>
2025-06-20 20:57:13 -0400

    Combine WaitAssertion blocks

--- Content.IntegrationTests/Tests/Actions/RetractableItemActionTest.cs
+++ Content.IntegrationTests/Tests/Actions/RetractableItemActionTest.cs
@@ -3,7 +3,6 @@ using Content.IntegrationTests.Tests.Interaction;
 using Content.Shared.Actions;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.RetractableItemAction;
-using Robust.Shared.GameObjects;
 using Robust.Shared.Prototypes;
 
 namespace Content.IntegrationTests.Tests.Actions;
@@ -23,43 +22,35 @@ public sealed class RetractableItemActionTest : InteractionTest
         var handsSystem = Server.System<SharedHandsSystem>();
         var playerUid = SEntMan.GetEntity(Player);
 
-        // Make sure the player's hand starts empty
-        var heldItem = Hands.ActiveHandEntity;
-        Assert.That(heldItem, Is.Null, $"Player is holding an item ({SEntMan.ToPrettyString(heldItem)}) at start of test.");
-
-        // Inspect the action prototype to find the item it spawns
-        var armBladeActionProto = ProtoMan.Index(ArmBladeActionProtoId);
-        EntProtoId? spawnedProtoId = null;
         await Server.WaitAssertion(() =>
         {
+            // Make sure the player's hand starts empty
+            var heldItem = Hands.ActiveHandEntity;
+            Assert.That(heldItem, Is.Null, $"Player is holding an item ({SEntMan.ToPrettyString(heldItem)}) at start of test.");
+
+            // Inspect the action prototype to find the item it spawns
+            var armBladeActionProto = ProtoMan.Index(ArmBladeActionProtoId);
+
             // Find the component
             Assert.That(armBladeActionProto.TryGetComponent<RetractableItemActionComponent>(out var actionComp, SEntMan.ComponentFactory));
             // Get the item protoId from the component
-            spawnedProtoId = actionComp!.SpawnedPrototype;
-        });
-        Assert.That(spawnedProtoId, Is.Not.Null);
+            var spawnedProtoId = actionComp!.SpawnedPrototype;
 
-        // Add the action to the player
-        EntityUid? actionUid = null;
-        await Server.WaitAssertion(() =>
-        {
-            actionUid = actionsSystem.AddAction(playerUid, ArmBladeActionProtoId);
-        });
-        // Make sure the player has the action now
-        Assert.That(actionUid, Is.Not.Null, "Failed to add action to player.");
-        var actionEnt = actionsSystem.GetAction(actionUid);
+            // Add the action to the player
+            var actionUid = actionsSystem.AddAction(playerUid, ArmBladeActionProtoId);
+            // Make sure the player has the action now
+            Assert.That(actionUid, Is.Not.Null, "Failed to add action to player.");
+            var actionEnt = actionsSystem.GetAction(actionUid);
 
-        // Make sure the player's hand is still empty
-        heldItem = Hands.ActiveHandEntity;
-        Assert.That(heldItem, Is.Null, $"Player is holding an item ({SEntMan.ToPrettyString(heldItem)}) after adding action.");
+            // Make sure the player's hand is still empty
+            heldItem = Hands.ActiveHandEntity;
+            Assert.That(heldItem, Is.Null, $"Player is holding an item ({SEntMan.ToPrettyString(heldItem)}) after adding action.");
 
-        await Server.WaitAssertion(() =>
-        {
             // Activate the arm blade
             actionsSystem.PerformAction(ToServer(Player), actionEnt!.Value);
 
             // Make sure the player is now holding the expected item
-            var heldItem = Hands.ActiveHandEntity;
+            heldItem = Hands.ActiveHandEntity;
             Assert.That(heldItem, Is.Not.Null, $"Expected player to be holding {spawnedProtoId} but was holding nothing.");
             AssertPrototype(spawnedProtoId, SEntMan.GetNetEntity(heldItem));
 
