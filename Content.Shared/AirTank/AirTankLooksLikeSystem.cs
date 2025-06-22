225ec43ad8023a52fe03caf8aaf80744a2d7bab1
notquitehadouken <tripwiregamer@gmail.com>
2025-06-22 14:36:16 -0500

    Add examine text

--- Content.Shared/AirTank/AirTankLooksLikeSystem.cs
+++ Content.Shared/AirTank/AirTankLooksLikeSystem.cs
@@ -1,4 +1,5 @@
-﻿using Content.Shared.Popups;
+﻿using Content.Shared.Examine;
+using Content.Shared.Popups;
 using Robust.Shared.Network;
 using Robust.Shared.Timing;
 
@@ -11,8 +12,15 @@ public sealed class AirTankLooksLikeSystem : EntitySystem
     [Dependency] private readonly INetManager _netMan = default!;
 
     public readonly LocId PopupText = "air-tank-looks-like";
-
+    public readonly LocId DontText = "air-tank-looks-like-dont";
     public readonly LocId ConfirmText = "air-tank-looks-like-confirm";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<AirTankLooksLikeComponent, ExaminedEvent>(OnExamine);
+    }
 
     public void CreateShouldntBreathePopup(EntityUid user, EntityUid tank)
     {
@@ -25,13 +33,27 @@ public sealed class AirTankLooksLikeSystem : EntitySystem
         _popupSystem.PopupPredicted(PopupTextLoc + "\n" + ConfirmTextLoc, null, user, user, PopupType.MediumCaution);
     }
 
+    public void OnExamine(Entity<AirTankLooksLikeComponent> uid, ref ExaminedEvent args)
+    {
+        if (!TryComp(args.Examiner, out AirTankShouldBreatheComponent? shouldBreathe))
+            return;
+
+        if (!CanBreathe(shouldBreathe, uid.Comp))
+            args.PushMarkup(Loc.GetString(DontText));
+    }
+
+    public bool CanBreathe(AirTankShouldBreatheComponent user, AirTankLooksLikeComponent tank)
+    {
+        return user.TankTypes.Contains(tank.Contains);
+    }
+
     public bool CheckShouldBreathe(EntityUid user, EntityUid tank)
     {
         if (!TryComp(user, out AirTankShouldBreatheComponent? shouldBreathe) ||
             !TryComp(tank, out AirTankLooksLikeComponent? looksLike))
             return true;
 
-        if (shouldBreathe.TankTypes.Contains(looksLike.Contains))
+        if (CanBreathe(shouldBreathe, looksLike))
         {
             shouldBreathe.LastAttempted = null;
             return true;
--- Resources/Locale/en-US/airtank/airtank.ftl
+++ Resources/Locale/en-US/airtank/airtank.ftl
@@ -1,12 +1,14 @@
 ﻿-air-tank-looks-like-states = { $state ->
-    [notair] This air tank looks like it doesn't actually contain air.
-    [regularair] This tank looks like it has regular air, which you don't breathe.
-    [oxygen] This tank looks like it has oxygen, which you don't breathe.
-    [nitrogen] This tank looks like it has nitrogen, which you don't breathe.
-    [plasma] This tank looks like it has highly toxic plasma gas.
+    [notair] This probably doesn't actually contain air.
+    [regularair] This probably has regular air, which you don't breathe.
+    [oxygen] This probably has oxygen, which you don't breathe.
+    [nitrogen] This probably has nitrogen, which you don't breathe.
+    [plasma] This looks toxic.
    *[invalid] This tank has broken the laws of reality.
 }
 
 air-tank-looks-like = {-air-tank-looks-like-states(state:$state)}
+air-tank-looks-like-examine = {-air-tank-looks-like-examine-states(state:$state)}
+air-tank-looks-like-dont = You shouldn't breathe from this.
 
 air-tank-looks-like-confirm = Use it again to breathe from the tank anyway.
