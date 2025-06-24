cc1317daa9e5ea86cdc4628dd5a7fc9eb252a4dd
Bartek <bartek.szmajda@appvinio.com>
2025-06-24 19:32:52 +0200

    Fix(Construction): Add Archive modifier to construction ghost setting CCVar

--- Content.Shared/_Starlight/CCVar/StarlightCCVars.Construction.cs
+++ Content.Shared/_Starlight/CCVar/StarlightCCVars.Construction.cs
@@ -8,5 +8,5 @@ public sealed partial class StarlightCCVars
     ///     The maximum number of construction ghosts allowed per tile.
     /// </summary>
     public static readonly CVarDef<int> ConstructionMaxGhostsPerTile =
-        CVarDef.Create("construction.max_ghosts_per_tile", 6, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("construction.max_ghosts_per_tile", 6, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
 } 
