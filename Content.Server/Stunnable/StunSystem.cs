a379d914129924c0096b2033488b90bb10a6d3c2
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2025-06-24 19:57:57 -0700

    File scoped namespaces

--- Content.Server/Stunnable/StunSystem.cs
+++ Content.Server/Stunnable/StunSystem.cs
@@ -1,22 +1,22 @@
 using Content.Shared.Stunnable;
 
-namespace Content.Server.Stunnable
+namespace Content.Server.Stunnable;
+
+public sealed class StunSystem : SharedStunSystem
 {
-    public sealed class StunSystem : SharedStunSystem
+    public override void Update(float frameTime)
     {
-        public override void Update(float frameTime)
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<KnockedDownComponent>();
+
+        while (query.MoveNext(out var uid, out var knockedDown))
         {
-            base.Update(frameTime);
+            if (!knockedDown.AutoStand || knockedDown.DoAfter.HasValue || knockedDown.NextUpdate > GameTiming.CurTime)
+                continue;
 
-            var query = EntityQueryEnumerator<KnockedDownComponent>();
-
-            while (query.MoveNext(out var uid, out var knockedDown))
-            {
-                if (!knockedDown.AutoStand || knockedDown.DoAfter.HasValue || knockedDown.NextUpdate > GameTiming.CurTime)
-                    continue;
-
-                TryStanding(uid, out knockedDown.DoAfter);
-            }
+            TryStanding(uid, out knockedDown.DoAfter);
         }
     }
 }
+
