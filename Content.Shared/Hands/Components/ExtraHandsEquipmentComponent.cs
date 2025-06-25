32e163338db9dc3a2d2698f4e6b34aef21ddf266
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-06-25 17:20:29 +0200

    RIG

---
+++ Content.Shared/Hands/Components/ExtraHandsEquipmentComponent.cs
@@ -0,0 +1,16 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Hands.Components;
+
+/// <summary>
+/// An entity with this component will give you extra hands when you quip it in your inventory.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ExtraHandsEquipmentComponent : Component
+{
+    /// <summary>
+    /// Dictionary relating a unique hand ID corresponding to a container slot on the attached entity to a struct containing information about the Hand itself.
+    /// </summary>
+    [DataField]
+    public Dictionary<string, Hand> Hands = new();
+}
