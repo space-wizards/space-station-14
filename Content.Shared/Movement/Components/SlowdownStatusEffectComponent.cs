e1975a7552d01493c57478e7ac6d69fc98607733
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2025-06-27 02:20:31 -0700

    Documentation yum

--- Content.Shared/Movement/Components/SlowdownStatusEffectComponent.cs
+++ Content.Shared/Movement/Components/SlowdownStatusEffectComponent.cs
@@ -2,12 +2,23 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.Movement.Components;
 
+/// <summary>
+/// This is used to store a movement speed modifier attached to a status effect entity so it can be applied via statuses.
+/// To be used in conjunction with <see cref="SlowedStatusSystem"/>.
+/// See <see cref="SlowedDownComponent"/> for the component applied to the entity.
+/// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class SlowdownStatusEffectComponent : Component
 {
-    [ViewVariables, DataField("sprintSpeedModifier"), AutoNetworkedField]
+    /// <summary>
+    /// Multiplicative sprint modifier, with bounds of [0, 1)
+    /// </summary>
+    [DataField, AutoNetworkedField]
     public float SprintSpeedModifier = 0.5f;
 
-    [ViewVariables, DataField("walkSpeedModifier"), AutoNetworkedField]
+    /// <summary>
+    /// Multiplicative walk modifier, with bounds of [0, 1)
+    /// </summary>
+    [DataField, AutoNetworkedField]
     public float WalkSpeedModifier = 0.5f;
 }
