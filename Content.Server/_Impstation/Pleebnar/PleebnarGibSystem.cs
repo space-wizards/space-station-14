58f28b810960639627429682c5d85dc47987682d
sev7ves <58966116+sev7ves@users.noreply.github.com>
2025-06-27 15:41:23 -0400

    stopped pleebnars from gibbing anything (#2883)
    * the telepathy works
    
    * gibbing and telepathy imporvements
    
    * forgot to add the sfx
    
    * dwawd
    
    * Final push
    
    * Update speech_emote_sounds.yml
    
    * Update attributions.yml
    
    * Update meta.json
    
    * fixed typo
    
    * had to change the license in the rsi meta because it was getting mad
    
    * add imp comment
    
    * Update pleebnar.ftl
    
    * Update Pleebnars.yml
    
    * merged hascomp if statements
    
    * cleaned up ui code
    
    * got tooltips working (in its current version) will see about using prototypes instead tomorrow
    
    * changed ui to use prototypes instead of strings
    
    * dealt with some of the requested changes, need clarification for rest of them.
    
    * grammar fixes
    
    * more grammar
    
    * Additional cleanup and comments
    
    * Update pleebnar-vision.ftl
    
    missed a piskun typo.
    
    * grammar suggestions from jane + a vision
    
    * i fucking hate github
    
    * stupid fucking trailing white space bullshit failing my pr
    
    * works with the new action systems yay
    
    * adjusted hunger rate
    
    * fuck thui baka life
    
    * added to calm pest table
    
    * when you put an and instead of an or.
    
    * okay no longer instant, but i made it really fast because thats better
    
    ---------
    
    Co-authored-by: sev7ves <soapedzone@gmail.com>

--- Content.Server/_Impstation/Pleebnar/PleebnarGibSystem.cs
+++ Content.Server/_Impstation/Pleebnar/PleebnarGibSystem.cs
@@ -4,9 +4,7 @@ using Content.Shared._Impstation.Pleebnar;
 using Content.Shared._Impstation.Pleebnar.Components;
 using Content.Shared.Body.Components;
 using Content.Shared.DoAfter;
-using Content.Shared.Gibbing.Systems;
 using Content.Shared.Popups;
-using Robust.Shared.Physics.Components;
 
 namespace Content.Server._Impstation.Pleebnar;
 /// <summary>
@@ -28,15 +26,13 @@ public sealed class PleebnarGibSystem : SharedPleebnarGibSystem
     //function called when an entity is targeted by the action
     public void PleebnarGib(Entity<PleebnarGibActionComponent> ent, ref PleebnarGibEvent args)
     {
-        if ((!HasComp<PleebnarGibbableComponent>(args.Target))&&(!HasComp<BodyComponent>(args.Target)))//check if it has a body and is gibbable by pleebnars, else return
+        if ((!HasComp<PleebnarGibbableComponent>(args.Target))||(!HasComp<BodyComponent>(args.Target)))//check if it has a body and is gibbable by pleebnars, else return
         {
             return;
         }
-        if (!TryComp<PhysicsComponent>(args.Target, out var targetPhysics))//try to get physics componenet, if not present return
-            return;
 
         _popupSystem.PopupEntity(Loc.GetString("pleebnar-focus"),ent.Owner,PopupType.Small);
-        var doargs = new DoAfterArgs(EntityManager, ent, targetPhysics.Mass/10, new SharedPleebnarGibSystem.PleebnarGibDoAfterEvent(), ent, args.Target)
+        var doargs = new DoAfterArgs(EntityManager, ent, 0.5f, new SharedPleebnarGibSystem.PleebnarGibDoAfterEvent(), ent, args.Target)
         {
             DistanceThreshold = 15f,
             BreakOnDamage = true,
@@ -44,14 +40,14 @@ public sealed class PleebnarGibSystem : SharedPleebnarGibSystem
             BreakOnMove = true,
             BreakOnWeightlessMove = true,
             AttemptFrequency = AttemptFrequency.StartAndEnd
-        };//lifted from baby kode code, basically takes longer to gib the more mass there is,
+        };
         _doAfter.TryStartDoAfter(doargs);
         args.Handled = true;
     }
     //actually handle the gibbing
     public void PleebnarGibDoafter(Entity<PleebnarGibActionComponent> ent, ref PleebnarGibDoAfterEvent args)
     {
-        if (args.Target == null)//if the target somehow dissapears then return
+        if (args.Target == null||args.Cancelled )//if the target somehow dissapears or the action was cancelled then return
         {
             return;
         }
