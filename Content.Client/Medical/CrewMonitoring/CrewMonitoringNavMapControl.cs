8d6efe3834979d1fabd0c6575ea304ebc160e03d
Centronias <me@centronias.com>
2025-06-29 11:15:36 -0700

    coordination

--- Content.Client/Medical/CrewMonitoring/CrewMonitoringNavMapControl.cs
+++ Content.Client/Medical/CrewMonitoring/CrewMonitoringNavMapControl.cs
@@ -7,6 +7,8 @@ namespace Content.Client.Medical.CrewMonitoring;
 
 public sealed partial class CrewMonitoringNavMapControl : NavMapControl
 {
+    private readonly SharedTransformSystem _transform;
+
     public NetEntity? Focus;
     public Dictionary<NetEntity, string> LocalizedNames = new();
 
@@ -15,6 +17,8 @@ public sealed partial class CrewMonitoringNavMapControl : NavMapControl
 
     public CrewMonitoringNavMapControl() : base()
     {
+        _transform = EntManager.System<SharedTransformSystem>();
+
         WallColor = new Color(192, 122, 196);
         TileColor = new(71, 42, 72);
         BackgroundColor = Color.FromSrgb(TileColor.WithAlpha(BackgroundOpacity));
@@ -64,7 +68,8 @@ public sealed partial class CrewMonitoringNavMapControl : NavMapControl
             if (!LocalizedNames.TryGetValue(netEntity, out var name))
                 name = "Unknown";
 
-            var message = name + "\nLocation: [x = " + MathF.Round(blip.Coordinates.X) + ", y = " + MathF.Round(blip.Coordinates.Y) + "]";
+            var mapCoords = _transform.ToMapCoordinates(blip.Coordinates);
+            var message = name + "\nLocation: [x = " + MathF.Floor(mapCoords.X) + ", y = " + MathF.Floor(mapCoords.Y) + "]";
 
             _trackedEntityLabel.Text = message;
             _trackedEntityPanel.Visible = true;
