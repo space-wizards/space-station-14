cfdb69d42f03647c161fceabb003104e6008c6e3
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-06-30 19:33:49 +0300

    Fix

--- Content.Shared/Stealth/SharedStealthSystem.cs
+++ Content.Shared/Stealth/SharedStealthSystem.cs
@@ -135,7 +135,7 @@ public abstract class SharedStealthSystem : EntitySystem
     /// <param name="delta">The delta to be used in visibility calculation.</param>
     public void ModifyVisibility(EntityUid uid, float delta, StealthComponent? component = null)
     {
-        if (delta == 0 || !Resolve(uid, ref component))
+        if (delta == 0 || !Resolve(uid, ref component, false))
             return;
 
         if (component.LastUpdated != null)
