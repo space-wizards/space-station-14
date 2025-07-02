579b38b92b6dfc800b2d8dbfe1e2fc0c79c003b2
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2025-07-02 10:46:30 -0700

    Improvements and fixups for New Status Effect API (#38660)

--- Content.Server/Drowsiness/DrowsinessSystem.cs
+++ Content.Server/Drowsiness/DrowsinessSystem.cs
@@ -1,4 +1,4 @@
-﻿using Content.Server.StatusEffectNew;
+using Content.Server.StatusEffectNew;
 using Content.Shared.Bed.Sleep;
 using Content.Shared.Drowsiness;
 using Content.Shared.StatusEffectNew;
@@ -47,7 +47,7 @@ public sealed class DrowsinessSystem : SharedDrowsinessSystem
             // Make sure the sleep time doesn't cut into the time to next incident.
             drowsiness.NextIncidentTime += duration;
 
-            _statusEffects.TryAddStatusEffect(statusEffect.AppliedTo.Value, SleepingSystem.StatusEffectForcedSleeping, duration);
+            _statusEffects.TryAddStatusEffectDuration(statusEffect.AppliedTo.Value, SleepingSystem.StatusEffectForcedSleeping, duration);
         }
     }
 }
