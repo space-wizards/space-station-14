4ea730bdce3d957019e1a8767889b5e2a9e92dff
Perry Fraser <perry@frasers.org>
2025-07-03 01:15:27 -0400

    style: convert PickupAttemptEvent to primary constructors
    I tried to resist the temptation but it's just too great. It's
     so much neater...

--- Content.Shared/Item/PickupAttemptEvent.cs
+++ Content.Shared/Item/PickupAttemptEvent.cs
@@ -3,32 +3,21 @@ namespace Content.Shared.Item;
 /// <summary>
 ///     Raised on a *mob* when it tries to pickup something
 /// </summary>
-public sealed class PickupAttemptEvent : BasePickupAttemptEvent
-{
-    public PickupAttemptEvent(EntityUid user, EntityUid item, bool physicalAttempt = false) : base(user,
-        item,
-        physicalAttempt)
-    {
-    }
-}
+public sealed class PickupAttemptEvent(EntityUid user, EntityUid item, bool physicalAttempt = false)
+    : BasePickupAttemptEvent(user, item, physicalAttempt);
 
 /// <summary>
 ///     Raised directed at entity being picked up when someone tries to pick it up
 /// </summary>
-public sealed class GettingPickedUpAttemptEvent : BasePickupAttemptEvent
-{
-    public GettingPickedUpAttemptEvent(EntityUid user, EntityUid item, bool physicalAttempt = false) : base(user,
-        item,
-        physicalAttempt)
-    {
-    }
-}
+public sealed class GettingPickedUpAttemptEvent(EntityUid user, EntityUid item, bool physicalAttempt = false)
+    : BasePickupAttemptEvent(user, item, physicalAttempt);
 
 [Virtual]
-public class BasePickupAttemptEvent : CancellableEntityEventArgs
+public class BasePickupAttemptEvent(EntityUid user, EntityUid item, bool physicalAttempt = false)
+    : CancellableEntityEventArgs
 {
-    public readonly EntityUid User;
-    public readonly EntityUid Item;
+    public readonly EntityUid User = user;
+    public readonly EntityUid Item = item;
 
     /// <summary>
     /// String describing the reason the pickup failed.
@@ -41,12 +30,5 @@ public class BasePickupAttemptEvent : CancellableEntityEventArgs
     /// Whether or not the event is raised in response to a physical action taken.
     /// If false, in-game side effects should not occur in response to this event.
     /// </summary>
-    public bool PhysicalAttempt;
-
-    public BasePickupAttemptEvent(EntityUid user, EntityUid item, bool physicalAttempt = false)
-    {
-        User = user;
-        Item = item;
-        PhysicalAttempt = physicalAttempt;
-    }
+    public bool PhysicalAttempt = physicalAttempt;
 }
