27657e098d962d58cb5855e8a5b749885b3e4445
Perry Fraser <perry@frasers.org>
2025-07-04 16:15:18 -0400

    style: Basic SolutionPurgeComponent field cleanup

--- Content.Shared/Chemistry/Components/SolutionPurgeComponent.cs
+++ Content.Shared/Chemistry/Components/SolutionPurgeComponent.cs
@@ -16,32 +16,31 @@ public sealed partial class SolutionPurgeComponent : Component
     /// <summary>
     /// The name of the solution to detract from.
     /// </summary>
-    [DataField("solution", required: true), ViewVariables(VVAccess.ReadWrite)]
+    [DataField(required: true)]
     public string Solution = string.Empty;
 
     /// <summary>
     /// The reagent(s) to be ignored when purging the solution
     /// </summary>
-    [DataField("preserve", customTypeSerializer: typeof(PrototypeIdListSerializer<ReagentPrototype>))]
-    [ViewVariables(VVAccess.ReadWrite)]
-    public List<string> Preserve = new();
+    [DataField(customTypeSerializer: typeof(PrototypeIdListSerializer<ReagentPrototype>))]
+    public List<string> Preserve = [];
 
     /// <summary>
     /// Amount of reagent(s) that are purged
     /// </summary>
-    [DataField("quantity", required: true), ViewVariables(VVAccess.ReadWrite)]
-    public FixedPoint2 Quantity = default!;
+    [DataField(required: true)]
+    public FixedPoint2 Quantity;
 
     /// <summary>
     /// How long it takes to purge once.
     /// </summary>
-    [DataField("duration"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public TimeSpan Duration = TimeSpan.FromSeconds(1);
 
     /// <summary>
     /// The time when the next purge will occur.
     /// </summary>
-    [DataField("nextPurgeTime", customTypeSerializer: typeof(TimeOffsetSerializer)), ViewVariables(VVAccess.ReadWrite)]
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
     [AutoPausedField]
-    public TimeSpan NextPurgeTime = TimeSpan.FromSeconds(0);
+    public TimeSpan NextPurgeTime;
 }
