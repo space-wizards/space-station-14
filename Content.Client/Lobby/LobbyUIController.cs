01bc80b7c72f3646b65d8497c863718ed77412eb
Robert Cross <7065792+Quantum-cross@users.noreply.github.com>
2025-07-04 18:12:26 -0400

    refresh the job priority editor if cvar "game.role_timers" is changed.

--- Content.Client/Lobby/LobbyUIController.cs
+++ Content.Client/Lobby/LobbyUIController.cs
@@ -73,9 +73,9 @@ public sealed class LobbyUIController : UIController, IOnStateEntered<LobbyState
             _profileEditor?.RefreshFlavorText();
         });
 
-        _configurationManager.OnValueChanged(CCVars.GameRoleTimers, _ => RefreshProfileEditor());
+        _configurationManager.OnValueChanged(CCVars.GameRoleTimers, _ => RefreshEditors());
 
-        _configurationManager.OnValueChanged(CCVars.GameRoleWhitelist, _ => RefreshProfileEditor());
+        _configurationManager.OnValueChanged(CCVars.GameRoleWhitelist, _ => RefreshEditors());
     }
 
     private LobbyCharacterPreviewPanel? GetLobbyPreview()
@@ -180,11 +180,12 @@ public sealed class LobbyUIController : UIController, IOnStateEntered<LobbyState
         PreviewPanel?.Refresh();
     }
 
-    private void RefreshProfileEditor()
+    private void RefreshEditors()
     {
         _profileEditor?.RefreshAntags();
         _profileEditor?.RefreshJobs();
         _profileEditor?.RefreshLoadouts();
+        _jobPriorityEditor?.RefreshJobs();
     }
 
     /// <summary>
