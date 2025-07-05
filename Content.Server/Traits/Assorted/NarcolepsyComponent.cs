28459df38971637391a1563f7e664ec20b789b8a
Perry Fraser <perry@frasers.org>
2025-07-04 22:01:48 -0400

    fix: improve narcolepsy logic
    Also refactors it to use timespans and other minor tweaks.

--- Content.Server/Traits/Assorted/NarcolepsyComponent.cs
+++ Content.Server/Traits/Assorted/NarcolepsyComponent.cs
@@ -6,19 +6,41 @@ namespace Content.Server.Traits.Assorted;
 /// This is used for the narcolepsy trait.
 /// </summary>
 [RegisterComponent, Access(typeof(NarcolepsySystem))]
+[AutoGenerateComponentPause]
 public sealed partial class NarcolepsyComponent : Component
 {
     /// <summary>
-    /// The random time between incidents, (min, max).
+    /// The minimum time between naps.
     /// </summary>
-    [DataField("timeBetweenIncidents", required: true)]
-    public Vector2 TimeBetweenIncidents { get; private set; }
+    [DataField]
+    public TimeSpan MinTimeBetweenIncidents = TimeSpan.FromMinutes(5);
 
     /// <summary>
-    /// The duration of incidents, (min, max).
+    /// The maximum time between naps.
     /// </summary>
-    [DataField("durationOfIncident", required: true)]
-    public Vector2 DurationOfIncident { get; private set; }
+    [DataField]
+    public TimeSpan MaxTimeBetweenIncidents = TimeSpan.FromMinutes(10);
 
-    public float NextIncidentTime;
+    /// <summary>
+    /// The minimum duration that the entity will be unable to wake.
+    /// </summary>
+    [DataField]
+    public TimeSpan MinIncidentDuration = TimeSpan.FromSeconds(10);
+
+    /// <summary>
+    /// The maximum duration the entity will be unable to wake.
+    /// </summary>
+    [DataField]
+    public TimeSpan MaxIncidentDuration = TimeSpan.FromSeconds(30);
+
+    /// <summary>
+    /// The next time a forced sleep will occur.
+    /// </summary>
+    [AutoPausedField]
+    public TimeSpan NextIncidentTime;
+
+    /// <summary>
+    /// Whether the entity is asleep due to narcolepsy. (As opposed to normal sleep or chemically induced sleep.)
+    /// </summary>
+    public bool NarcolepsyInducedSleep;
 }
