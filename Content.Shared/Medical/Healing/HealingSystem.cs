5fe409e471fc815ddc06821e54265d73bee121a2
iaada <iaada@users.noreply.github.com>
2025-07-04 23:24:26 -0500

    small misc change

--- Content.Shared/Medical/Healing/HealingSystem.cs
+++ Content.Shared/Medical/Healing/HealingSystem.cs
@@ -89,7 +89,7 @@ public sealed class HealingSystem : EntitySystem
         {
             _stacks.LowerCount((args.Used.Value, stackComp), 1);
 
-            if (stackComp.Count <= 0)
+            if (_stacks.GetCount((args.Used.Value, stackComp)) <= 0)
                 dontRepeat = true;
         }
         else
