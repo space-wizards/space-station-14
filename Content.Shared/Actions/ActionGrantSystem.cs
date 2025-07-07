3eba9fbbab3c418caad0e853138261bd592f8ac4
ScarKy0 <scarky0@onet.eu>
2025-07-07 22:31:27 +0200

    handn't

--- Content.Shared/Actions/ActionGrantSystem.cs
+++ Content.Shared/Actions/ActionGrantSystem.cs
@@ -24,7 +24,7 @@ public sealed class ActionGrantSystem : EntitySystem
             return;
 
         // If slot we are equipping into isn't allowed then don't give the action
-        if ((ent.Comp.AllowedSlots & args.SlotFlags) != args.SlotFlags)
+        if (args.SlotFlags == null || (ent.Comp.AllowedSlots & args.SlotFlags) != args.SlotFlags)
             return;
 
         foreach (var action in grant.ActionEntities)
