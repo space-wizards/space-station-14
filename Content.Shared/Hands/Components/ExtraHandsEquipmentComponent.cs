fdf6df32e02a6d92d2ec48b5b4ff7a6d604a70a7
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-07-07 17:28:01 +0200

    review

--- Content.Shared/Hands/Components/ExtraHandsEquipmentComponent.cs
+++ Content.Shared/Hands/Components/ExtraHandsEquipmentComponent.cs
@@ -1,11 +1,13 @@
 using Robust.Shared.GameStates;
+using Content.Shared.Hands.EntitySystems;
 
 namespace Content.Shared.Hands.Components;
 
 /// <summary>
-/// An entity with this component will give you extra hands when you quip it in your inventory.
+/// An entity with this component will give you extra hands when you equip it in your inventory.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
+[Access(typeof(ExtraHandsEquipmentSystem))]
 public sealed partial class ExtraHandsEquipmentComponent : Component
 {
     /// <summary>
