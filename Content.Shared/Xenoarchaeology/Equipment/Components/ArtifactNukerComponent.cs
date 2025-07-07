2df25f711a5d131922a6245387d38e9c1255da13
kosticia <kosticia46@gmail.com>
2025-07-07 17:26:02 +0300

    start

---
+++ Content.Shared/Xenoarchaeology/Equipment/Components/ArtifactNukerComponent.cs
@@ -0,0 +1,5 @@
+namespace Content.Shared.Xenoarchaeology.Equipment.Components;
+
+[RegisterComponent]
+[Access(typeof(ArtifactNukerSystem))]
+public sealed partial class ArtifactNukerComponent : Component;
