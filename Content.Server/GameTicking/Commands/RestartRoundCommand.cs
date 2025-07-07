4662aec94b0ac6db80b514a4f04d31588af5ffa5
Kyle Tyo <akikai297@gmail.com>
2025-07-06 20:56:32 -0400

    commit-progress

--- Content.Server/GameTicking/Commands/RestartRoundCommand.cs
+++ Content.Server/GameTicking/Commands/RestartRoundCommand.cs
@@ -3,43 +3,37 @@ using Content.Server.RoundEnd;
 using Content.Shared.Administration;
 using Robust.Shared.Console;
 
-namespace Content.Server.GameTicking.Commands
+namespace Content.Server.GameTicking.Commands;
+
+[AdminCommand(AdminFlags.Round)]
+public sealed class RestartRoundCommand : LocalizedEntityCommands
 {
-    [AdminCommand(AdminFlags.Round)]
-    public sealed class RestartRoundCommand : IConsoleCommand
+    [Dependency] private readonly GameTicker _gameTicker = default!;
+    [Dependency] private readonly RoundEndSystem _roundEndSystem = default!;
+
+    public override string Command => "restartround";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IEntityManager _e = default!;
-
-        public string Command => "restartround";
-        public string Description => "Ends the current round and starts the countdown for the next lobby.";
-        public string Help => string.Empty;
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        if (_gameTicker.RunLevel != GameRunLevel.InRound)
         {
-            var ticker = _e.System<GameTicker>();
-
-            if (ticker.RunLevel != GameRunLevel.InRound)
-            {
-                shell.WriteLine("This can only be executed while the game is in a round - try restartroundnow");
-                return;
-            }
-
-            _e.System<RoundEndSystem>().EndRound();
+            shell.WriteLine(Loc.GetString("shell-can-only-run-while-round-is-active"));
+            return;
         }
+
+        _roundEndSystem.EndRound();
     }
+}
 
-    [AdminCommand(AdminFlags.Round)]
-    public sealed class RestartRoundNowCommand : IConsoleCommand
+[AdminCommand(AdminFlags.Round)]
+public sealed class RestartRoundNowCommand : LocalizedEntityCommands
+{
+    [Dependency] private readonly GameTicker _gameTicker = default!;
+
+    public override string Command => "restartroundnow";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IEntityManager _e = default!;
-
-        public string Command => "restartroundnow";
-        public string Description => "Moves the server from PostRound to a new PreRoundLobby.";
-        public string Help => String.Empty;
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
-        {
-            _e.System<GameTicker>().RestartRound();
-        }
+        _gameTicker.RestartRound();
     }
 }
---
+++ Resources/Locale/en-US/commands/delaystart-command.ftl
@@ -0,0 +1,6 @@
+﻿cmd-delaystart-desc = Delays the round start.
+cmd-delaystart-help = Usage: delaystart [seconds]
+                      If no arguments are passed, the round will be paused or resumed accordingly.
+cmd-delaystart-invalid-seconds = {$value} isn't a valid amount of seconds.
+cmd-delaystart-paused = Paused the countdown.
+cmd-delaystart-unpaused = Resumed the countdown.
