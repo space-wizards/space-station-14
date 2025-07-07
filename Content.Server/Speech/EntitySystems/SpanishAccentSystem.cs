b4fd8a1acf405b3618ccb1adca1bd918f5314220
Centronias <me@centronias.com>
2025-07-07 11:27:54 -0700

    Kill word replacements, reformat some stuff.

--- Content.Server/Speech/EntitySystems/SpanishAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/SpanishAccentSystem.cs
@@ -6,11 +6,9 @@ namespace Content.Server.Speech.EntitySystems
 {
     public sealed class SpanishAccentSystem : EntitySystem
     {
-        [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
-
-        private static readonly Regex RegexWordInitialLowerCaseS = new(@"(^| )s");
-        private static readonly Regex RegexWordInitialUpperCaseSNotFollowedByCapital = new(@"(^| )S([^A-Z])");
-        private static readonly Regex RegexWordInitialUpperCaseSFollowedByCapital = new(@" (^| )([A-Z])");
+        private static readonly Regex RegexWordInitialLowerCaseS = new("(^| )s");
+        private static readonly Regex RegexWordInitialUpperCaseSNotFollowedByCapital = new("(^| )S([^A-Z])");
+        private static readonly Regex RegexWordInitialUpperCaseSFollowedByCapital = new("(^| )S([A-Z])");
 
         private static readonly Regex RegexHaha = new(@"\b(ha)+\b");
 
@@ -19,23 +17,20 @@ namespace Content.Server.Speech.EntitySystems
             SubscribeLocalEvent<SpanishAccentComponent, AccentGetEvent>(OnAccent);
         }
 
-        public string Accentuate(string message)
+        public static string Accentuate(string message)
         {
             // Insert E before every S
             message = ReplaceWordInitialSWithEs(message);
+
             // Replace things like "haha"s with "jaja"s.
             message = ReplaceHaWithJa(message);
-
-            // Replace words after the accent application so that the spnish words aren't "accented".
-            // The downside of this is that the words-to-replace look for the "es-" modified words.
-            message = _replacement.ApplyReplacements(message, "spanish");
 
             // If a sentence ends with ?, insert a reverse ? at the beginning of the sentence
             message = ReplacePunctuation(message);
             return message;
         }
 
-        private string ReplaceWordInitialSWithEs(string message)
+        private static string ReplaceWordInitialSWithEs(string message)
         {
             // stun -> estun
             message = RegexWordInitialLowerCaseS.Replace(message, "$1es");
@@ -47,17 +42,18 @@ namespace Content.Server.Speech.EntitySystems
             return message;
         }
 
-        private string ReplaceHaWithJa(string message)
+        private static string ReplaceHaWithJa(string message)
         {
             if (RegexHaha.IsMatch(message))
             {
                 // Preserve case and number of "ha"s by replacing individual letters.
                 message = message.Replace('h', 'j').Replace('H', 'j');
             }
+
             return message;
         }
 
-        private string ReplacePunctuation(string message)
+        private static string ReplacePunctuation(string message)
         {
             var sentences = AccentSystem.SentenceRegex.Split(message);
             var msg = new StringBuilder();
@@ -71,22 +67,17 @@ namespace Content.Server.Speech.EntitySystems
                         '?' => '¿',
                         '!' => '¡',
                         '‽' => '⸘',
-                        _ => ' '
+                        _ => ' ',
                     });
                 }
-                if (toInsert.Length == 0)
-                {
-                    msg.Append(s);
-                }
-                else
-                {
-                    msg.Append(s.Insert(s.Length - s.TrimStart().Length, toInsert.ToString()));
-                }
+
+                msg.Append(toInsert.Length == 0 ? s : s.Insert(s.Length - s.TrimStart().Length, toInsert.ToString()));
             }
+
             return msg.ToString();
         }
 
-        private void OnAccent(EntityUid uid, SpanishAccentComponent component, AccentGetEvent args)
+        private static void OnAccent(Entity<SpanishAccentComponent> entity, ref AccentGetEvent args)
         {
             args.Message = Accentuate(args.Message);
         }
