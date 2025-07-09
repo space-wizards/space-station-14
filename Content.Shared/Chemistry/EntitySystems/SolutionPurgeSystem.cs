c802b8bbb2322f89ad897f2d12823692e06c3fef
Perry Fraser <perryprog@users.noreply.github.com>
2025-07-08 23:17:55 -0400

    Make mopping predicted (and some other stuff) (#38749)
    * refactor: move puddle evaporation + absorbents to shared
    
    * refactor: move SolutionRegeneration to shared
    
    * refactor: make AbsorbentSystem visuals clientside
    
    * style: general formatting/cleanup on touched files
    
    - Few logical simplifications
    - Add field for hard-coded sparkle effect ent
    - Switch stuff to Entity<T>
    
    No actual prediction fixes in this commit (though in
    retrospect I should've done this commit last).
    
    * fix: use predicted variants for predicted code
    
    * fix: average out evaporation rates in mixtures
    
    * refactor: move SolutionPurge to shared
    
    * style: Basic SolutionPurgeComponent field cleanup
    
    * fix: general prediction + timing + networking fixes
    
    - Moves client side visuals back to shared because other
      players exist
    - Don't accumulate CurTime in Purge/RegenerationSystem
    - Network the next update field in Purge/RegenerationSystem to
      deal with UI mispredictions???
    
    * fix: add udder bug workaround
    
    Not needed for SolutionPurgeSystem which doesn't resolve
    solutions (probably fine that SolutionPurgeSystem doesn't
    cache since it's much rarer, though it probably should), and
    likely not needed for AbsorbentSystem since it only resolves
    against puddles which, I don't think can be in containers.
    
    * fix: don't divide by zero for evaporation speed = 0.
    
    * refactor: revert evaporation changes
    
    Will cherry-pick these out in another PR.
    
    Also reverting the evaporation speed bugfix since it's easier
    to revert all at once. :)
    
    * fix: component cleanup; autopause fields, use ProtoID
    
    * fix: remove unused AbsorbentComponentState
    
    * fix: ProtoId is not string
    
    * refactor: move PuddleSystem.UpdateAppearance to shared
    
    * style: general PuddleSystem.UpdateAppearance tweaks
    
    - Switch to Entity<T>
    - Use ProtoIds
    - Minor simplifications
    
    * fix: add udderly silly PVS workaround
    
    * cleanup
    
    * fix
    
    ---------
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Shared/Chemistry/EntitySystems/SolutionPurgeSystem.cs
+++ Content.Shared/Chemistry/EntitySystems/SolutionPurgeSystem.cs
@@ -1,14 +1,27 @@
-using Content.Server.Chemistry.Components;
-using Content.Shared.Chemistry.EntitySystems;
+using System.Linq;
+using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.Components.SolutionManager;
 using Robust.Shared.Timing;
 
-namespace Content.Server.Chemistry.EntitySystems;
+namespace Content.Shared.Chemistry.EntitySystems;
 
 public sealed class SolutionPurgeSystem : EntitySystem
 {
     [Dependency] private readonly SharedSolutionContainerSystem _solutionContainer = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<SolutionPurgeComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(Entity<SolutionPurgeComponent> ent, ref MapInitEvent args)
+    {
+        ent.Comp.NextPurgeTime = _timing.CurTime + ent.Comp.Duration;
+        Dirty(ent);
+    }
 
     public override void Update(float frameTime)
     {
@@ -22,8 +35,15 @@ public sealed class SolutionPurgeSystem : EntitySystem
 
             // timer ignores if it's empty, it's just a fixed cycle
             purge.NextPurgeTime += purge.Duration;
+            // Needs to be networked and dirtied so that the client can reroll it during prediction
+            Dirty(uid, purge);
+
             if (_solutionContainer.TryGetSolution((uid, manager), purge.Solution, out var solution))
-                _solutionContainer.SplitSolutionWithout(solution.Value, purge.Quantity, purge.Preserve.ToArray());
+            {
+                _solutionContainer.SplitSolutionWithout(solution.Value,
+                    purge.Quantity,
+                    purge.Preserve.Select(proto => proto.Id).ToArray());
+            }
         }
     }
 }
