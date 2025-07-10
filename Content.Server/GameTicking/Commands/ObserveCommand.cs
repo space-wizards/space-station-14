433c6758d5a4bd33dcd8b1655feb0f4b257b03e7
Kyle Tyo <akikai297@gmail.com>
2025-07-10 03:37:44 -0400

    commit

--- Content.Server/GameTicking/Commands/ObserveCommand.cs
+++ Content.Server/GameTicking/Commands/ObserveCommand.cs
@@ -3,50 +3,37 @@ using Content.Shared.Administration;
 using Content.Shared.GameTicking;
 using Robust.Shared.Console;
 
-namespace Content.Server.GameTicking.Commands
+namespace Content.Server.GameTicking.Commands;
+
+[AnyCommand]
+public sealed class ObserveCommand : LocalizedEntityCommands
 {
-    [AnyCommand]
-    sealed class ObserveCommand : IConsoleCommand
+    [Dependency] private readonly IAdminManager _adminManager = default!;
+    [Dependency] private readonly GameTicker _gameTicker = default!;
+
+    public override string Command => "observe";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IEntityManager _e = default!;
-        [Dependency] private readonly IAdminManager _adminManager = default!;
-
-        public string Command => "observe";
-        public string Description => "";
-        public string Help => "";
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        if (shell.Player is not { } player)
         {
-            if (shell.Player is not { } player)
-            {
-                shell.WriteError(Loc.GetString("shell-cannot-run-command-from-server"));
-                return;
-            }
-
-            var ticker = _e.System<GameTicker>();
-
-            if (ticker.RunLevel == GameRunLevel.PreRoundLobby)
-            {
-                shell.WriteError("Wait until the round starts.");
-                return;
-            }
-
-            var isAdminCommand = args.Length > 0 && args[0].ToLower() == "admin";
-
-            if (!isAdminCommand && _adminManager.IsAdmin(player))
-            {
-                _adminManager.DeAdmin(player);
-            }
-
-            if (ticker.PlayerGameStatuses.TryGetValue(player.UserId, out var status) &&
-                status != PlayerGameStatus.JoinedGame)
-            {
-                ticker.JoinAsObserver(player);
-            }
-            else
-            {
-                shell.WriteError($"{player.Name} is not in the lobby.   This incident will be reported.");
-            }
+            shell.WriteError(Loc.GetString("shell-cannot-run-command-from-server"));
+            return;
         }
+
+        if ((_gameTicker.PlayerGameStatuses.TryGetValue(player.UserId, out var status) &&
+             status != PlayerGameStatus.JoinedGame) ||
+            _gameTicker.RunLevel == GameRunLevel.PreRoundLobby)
+        {
+            shell.WriteError(Loc.GetString("shell-can-only-run-from-in-round-lobby"));
+            return;
+        }
+
+        var isAdminCommand = args.Length > 0 && args[0].ToLower() == "admin";
+
+        if (!isAdminCommand && _adminManager.IsAdmin(player))
+            _adminManager.DeAdmin(player);
+
+        _gameTicker.JoinAsObserver(player);
     }
 }
