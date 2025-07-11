bf96082d2d425bd4698338b8261f4135038d5242
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-07-11 05:52:49 +0200

    cleanup

--- Content.Client/Overlays/ShowJobIconsSystem.cs
+++ Content.Client/Overlays/ShowJobIconsSystem.cs
@@ -1,16 +1,6 @@
 using Content.Shared.Overlays;
-using Content.Shared.StatusIcon;
-using Content.Shared.StatusIcon.Components;
 
 namespace Content.Client.Overlays;
 
-public sealed class ShowJobIconsSystem : EquipmentHudSystem<ShowJobIconsComponent>
-{
-    public void TryShowIcon(JobIconPrototype iconPrototype, ref GetStatusIconsEvent ev)
-    {
-        if (!IsActive)
-            return;
-
-        ev.StatusIcons.Add(iconPrototype);
-    }
-}
+// The GetStatusIconsEvent subscription is handled in JobStatusSystem
+public sealed class ShowJobIconsSystem : EquipmentHudSystem<ShowJobIconsComponent>;
