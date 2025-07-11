a64fe298d0550f1988c9b54b1254ac028e3ec031
Whatstone <166147148+whatston3@users.noreply.github.com>
2025-07-10 23:43:56 -0400

    Add Serializable, NetSerializable attributes to DecalPaintMode (#38921)
    Serializable, Net" on DecalPaintMode

--- Content.Shared/SprayPainter/Components/SprayPainterComponent.cs
+++ Content.Shared/SprayPainter/Components/SprayPainterComponent.cs
@@ -2,6 +2,7 @@ using Content.Shared.Decals;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.SprayPainter.Components;
 
@@ -109,6 +110,7 @@ public sealed partial class SprayPainterComponent : Component
 /// <summary>
 /// A set of operating modes for decal painting.
 /// </summary>
+[Serializable, NetSerializable]
 public enum DecalPaintMode : byte
 {
     /// <summary>
