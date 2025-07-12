7bf90fe7e53c13c2d93be92be83846487f9c720c
kosticia <kosticia46@gmail.com>
2025-07-12 21:47:39 +0300

    aaaaaaaaaaaaaaaaaaaaa

--- Content.Shared/Xenoarchaeology/Equipment/Components/ArtifactNukerComponent.cs
+++ Content.Shared/Xenoarchaeology/Equipment/Components/ArtifactNukerComponent.cs
@@ -1,7 +1,6 @@
 namespace Content.Shared.Xenoarchaeology.Equipment.Components;
 
 [RegisterComponent]
-[Access(typeof(SharedArtifactNukerSystem))]
 public sealed partial class ArtifactNukerComponent : Component
 {
     /// <summary>
@@ -15,4 +14,10 @@ public sealed partial class ArtifactNukerComponent : Component
     /// </summary>
     [DataField]
     public float EnergyDrain = 50;
+
+    /// <summary>
+    ///     Index of the artifact node that will be attempted to nuke.
+    /// </summary>
+    [ViewVariables]
+    public int? Index;
 }
