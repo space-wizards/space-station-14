a3ba93f4797dfc1fa9141a980b89271a3a9aa376
kosticia <kosticia46@gmail.com>
2025-07-13 01:40:29 +0300

    aw

--- Content.Shared/Xenoarchaeology/Equipment/Components/ArtifactNukerComponent.cs
+++ Content.Shared/Xenoarchaeology/Equipment/Components/ArtifactNukerComponent.cs
@@ -1,6 +1,15 @@
+using Robust.Shared.Serialization;
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.Xenoarchaeology.Equipment.Components;
 
+/// <summary>
+///     Component that allows objects delete nodes in artifacts on interaction.
+///     Related logic lies in the <see cref="SharedArtifactNukerSystem"/>
+/// </summary>
 [RegisterComponent]
+[NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SharedArtifactNukerSystem))]
 public sealed partial class ArtifactNukerComponent : Component
 {
     /// <summary>
@@ -19,5 +28,12 @@ public sealed partial class ArtifactNukerComponent : Component
     ///     Index of the artifact node that will be attempted to nuke.
     /// </summary>
     [ViewVariables]
+    [AutoNetworkedField]
     public int? Index;
+}
+
+[Serializable, NetSerializable]
+public enum ArtifactNukerUiKey : byte
+{
+    Key
 }
