676e40606976f32f1cbb722b78e714a626bcc95e
Kyle Tyo <akikai297@gmail.com>
2025-07-13 14:01:28 -0400

    Update SharedDevourSystem.cs

--- Content.Shared/Devour/SharedDevourSystem.cs
+++ Content.Shared/Devour/SharedDevourSystem.cs
@@ -14,15 +14,15 @@ using Robust.Shared.Serialization;
 
 namespace Content.Shared.Devour;
 
-public abstract class SharedDevourSystem : EntitySystem
+public sealed class SharedDevourSystem : EntitySystem
 {
+    [Dependency] private readonly EntityWhitelistSystem _whitelistSystem = default!;
     [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
     [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
     [Dependency] private readonly SharedBloodstreamSystem _bloodstreamSystem = default!;
     [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
     [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
-    [Dependency] private readonly EntityWhitelistSystem _whitelistSystem = default!;
 
     public override void Initialize()
     {
@@ -34,21 +34,21 @@ public abstract class SharedDevourSystem : EntitySystem
         SubscribeLocalEvent<DevourerComponent, BeingGibbedEvent>(OnGibContents);
     }
 
-    private void OnInit(EntityUid uid, DevourerComponent component, MapInitEvent args)
+    private void OnInit(Entity<DevourerComponent> ent, ref MapInitEvent args)
     {
         //Devourer doesn't actually chew, since he sends targets right into his stomach.
         //I did it mom, I added ERP content into upstream. Legally!
-        component.Stomach = _containerSystem.EnsureContainer<Container>(uid, "stomach");
+        ent.Comp.Stomach = _containerSystem.EnsureContainer<Container>(ent.Owner, "stomach");
 
-        _actionsSystem.AddAction(uid, ref component.DevourActionEntity, component.DevourAction);
+        _actionsSystem.AddAction(ent.Owner, ref ent.Comp.DevourActionEntity, ent.Comp.DevourAction);
     }
 
     /// <summary>
     /// The devour action
     /// </summary>
-    private void OnDevourAction(EntityUid uid, DevourerComponent component, DevourActionEvent args)
+    private void OnDevourAction(Entity<DevourerComponent> ent, ref DevourActionEvent args)
     {
-        if (args.Handled || _whitelistSystem.IsWhitelistFailOrNull(component.Whitelist, args.Target))
+        if (args.Handled || _whitelistSystem.IsWhitelistFailOrNull(ent.Comp.Whitelist, args.Target))
             return;
 
         args.Handled = true;
@@ -62,47 +62,49 @@ public abstract class SharedDevourSystem : EntitySystem
                 case MobState.Critical:
                 case MobState.Dead:
 
-                    _doAfterSystem.TryStartDoAfter(new DoAfterArgs(EntityManager, uid, component.DevourTime, new DevourDoAfterEvent(), uid, target: target, used: uid)
+                    _doAfterSystem.TryStartDoAfter(new DoAfterArgs(EntityManager, ent.Owner, ent.Comp.DevourTime, new DevourDoAfterEvent(), ent.Owner, target: target, used: ent.Owner)
                     {
                         BreakOnMove = true,
                     });
                     break;
+                case MobState.Invalid:
+                case MobState.Alive:
                 default:
-                    _popupSystem.PopupClient(Loc.GetString("devour-action-popup-message-fail-target-alive"), uid,uid);
+                    _popupSystem.PopupClient(Loc.GetString("devour-action-popup-message-fail-target-alive"), ent.Owner, ent.Owner);
                     break;
             }
 
             return;
         }
 
-        _popupSystem.PopupClient(Loc.GetString("devour-action-popup-message-structure"), uid, uid);
+        _popupSystem.PopupClient(Loc.GetString("devour-action-popup-message-structure"), ent.Owner, ent.Owner);
 
-        if (component.SoundStructureDevour != null)
-            _audioSystem.PlayPredicted(component.SoundStructureDevour, uid, uid, component.SoundStructureDevour.Params);
+        if (ent.Comp.SoundStructureDevour != null)
+            _audioSystem.PlayPredicted(ent.Comp.SoundStructureDevour, ent.Owner, ent.Owner, ent.Comp.SoundStructureDevour.Params);
 
-        _doAfterSystem.TryStartDoAfter(new DoAfterArgs(EntityManager, uid, component.StructureDevourTime, new DevourDoAfterEvent(), uid, target: target, used: uid)
+        _doAfterSystem.TryStartDoAfter(new DoAfterArgs(EntityManager, ent.Owner, ent.Comp.StructureDevourTime, new DevourDoAfterEvent(), ent.Owner, target: target, used: ent.Owner)
         {
             BreakOnMove = true,
         });
     }
 
-    private void OnDoAfter(EntityUid uid, DevourerComponent component, DevourDoAfterEvent args)
+    private void OnDoAfter(Entity<DevourerComponent> ent, ref DevourDoAfterEvent args)
     {
         if (args.Handled || args.Cancelled)
             return;
 
-        var ichorInjection = new Solution(component.Chemical, component.HealRate);
+        var ichorInjection = new Solution(ent.Comp.Chemical, ent.Comp.HealRate);
 
         // Grant ichor if the devoured thing meets the dragon's food preference
-        if (args.Args.Target != null && _whitelistSystem.IsWhitelistPassOrNull(component.FoodPreferenceWhitelist, (EntityUid)args.Args.Target))
+        if (args.Args.Target != null && _whitelistSystem.IsWhitelistPassOrNull(ent.Comp.FoodPreferenceWhitelist, (EntityUid)args.Args.Target))
         {
-            _bloodstreamSystem.TryAddToChemicals(uid, ichorInjection);
+            _bloodstreamSystem.TryAddToChemicals(ent.Owner, ichorInjection);
         }
 
         // If the devoured thing meets the stomach whitelist criteria, add it to the stomach
-        if (args.Args.Target != null && _whitelistSystem.IsWhitelistPass(component.StomachStorageWhitelist, (EntityUid)args.Args.Target))
+        if (args.Args.Target != null && _whitelistSystem.IsWhitelistPass(ent.Comp.StomachStorageWhitelist, (EntityUid)args.Args.Target))
         {
-            _containerSystem.Insert(args.Args.Target.Value, component.Stomach);
+            _containerSystem.Insert(args.Args.Target.Value, ent.Comp.Stomach);
         }
         //TODO: Figure out a better way of removing structures via devour that still entails standing still and waiting for a DoAfter. Somehow.
         //If it's not alive, it must be a structure.
@@ -112,17 +114,18 @@ public abstract class SharedDevourSystem : EntitySystem
             QueueDel(args.Args.Target.Value);
         }
 
-        _audioSystem.PlayPvs(component.SoundDevour, uid);
+        var xform = Transform(ent.Owner);
+        _audioSystem.PlayPredicted(ent.Comp.SoundDevour, xform.Coordinates, ent.Owner);
     }
 
-    private void OnGibContents(EntityUid uid, DevourerComponent component, ref BeingGibbedEvent args)
+    private void OnGibContents(Entity<DevourerComponent> ent, ref BeingGibbedEvent args)
     {
-        if (component.StomachStorageWhitelist == null)
+        if (ent.Comp.StomachStorageWhitelist == null)
             return;
 
         // For some reason we have two different systems that should handle gibbing,
         // and for some another reason GibbingSystem, which should empty all containers, doesn't get involved in this process
-        _containerSystem.EmptyContainer(component.Stomach);
+        _containerSystem.EmptyContainer(ent.Comp.Stomach);
     }
 }
 
