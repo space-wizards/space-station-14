50abf48421cd17c4e5f0de406c1b9ab619492375
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2025-07-13 10:29:18 +0200

    Don't compile EF Core designer files on release builds (#38927)

--- Content.Server.Database/Migrations/Sqlite/20220509060717_FlavorText.Designer.cs
+++ Content.Server.Database/Migrations/Sqlite/20220509060717_FlavorText.Designer.cs
@@ -14,6 +14,7 @@ namespace Content.Server.Database.Migrations.Sqlite
     [Migration("20220509060717_FlavorText")]
     partial class FlavorText
     {
+#if EF_DESIGNER
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
@@ -1195,5 +1196,6 @@ namespace Content.Server.Database.Migrations.Sqlite
                 });
 #pragma warning restore 612, 618
         }
+#endif
     }
 }
