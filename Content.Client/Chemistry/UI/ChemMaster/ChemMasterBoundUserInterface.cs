4be28a29dadeff70e45cb4c58554248cde550864
Perry Fraser <perry@frasers.org>
2025-07-14 17:31:37 -0400

    style: clean-up pass for ChemMaster UI stuff
    Remove a few docs that I felt were redundant, primary constructors,
    de-filenamespaceing, a method extraction.

--- Content.Client/Chemistry/UI/ChemMaster/ChemMasterBoundUserInterface.cs
+++ Content.Client/Chemistry/UI/ChemMaster/ChemMasterBoundUserInterface.cs
@@ -3,61 +3,48 @@ using Content.Shared.Containers.ItemSlots;
 using JetBrains.Annotations;
 using Robust.Client.UserInterface;
 
-namespace Content.Client.Chemistry.UI.ChemMaster
+namespace Content.Client.Chemistry.UI.ChemMaster;
+
+/// <summary>
+/// Initializes a <see cref="ChemMasterWindow"/> and updates it when new server messages are received.
+/// </summary>
+[UsedImplicitly]
+public sealed class ChemMasterBoundUserInterface(EntityUid owner, Enum uiKey) : BoundUserInterface(owner, uiKey)
 {
+    [ViewVariables]
+    private ChemMasterWindow? _window;
+
     /// <summary>
-    /// Initializes a <see cref="ChemMasterWindow"/> and updates it when new server messages are received.
+    /// Called each time a chem master UI instance is opened. Generates the window and fills it with
+    /// relevant info. Sets up necessary action subscriptions.
     /// </summary>
-    [UsedImplicitly]
-    public sealed class ChemMasterBoundUserInterface : BoundUserInterface
+    protected override void Open()
     {
-        [ViewVariables]
-        private ChemMasterWindow? _window;
+        base.Open();
 
-        public ChemMasterBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
-        {
-        }
+        // Setup window layout/elements
+        _window = this.CreateWindow<ChemMasterWindow>();
+        _window.Title = EntMan.GetComponent<MetaDataComponent>(Owner).EntityName;
 
-        /// <summary>
-        /// Called each time a chem master UI instance is opened. Generates the window and fills it with
-        /// relevant info. Sets the actions for static buttons.
-        /// </summary>
-        protected override void Open()
-        {
-            base.Open();
+        // Setup static button actions.
+        _window.OnReagentButton += args =>
+            SendPredictedMessage(new ChemMasterReagentAmountButtonMessage(args.Id, args.Amount, args.IsBuffer));
+        _window.OnModeButton += mode => SendPredictedMessage(new ChemMasterSetModeMessage(mode));
+        _window.OnSortButton += () => SendPredictedMessage(new ChemMasterSortingTypeCycleMessage());
+        _window.OnEjectButton += slot => SendPredictedMessage(new ItemSlotButtonPressedEvent(slot));
 
-            // Setup window layout/elements
-            _window = this.CreateWindow<ChemMasterWindow>();
-            _window.Title = EntMan.GetComponent<MetaDataComponent>(Owner).EntityName;
+        _window.OnPillButton += index => SendPredictedMessage(new ChemMasterSetPillTypeMessage(index));
+        _window.OnCreatePill += args =>
+            SendPredictedMessage(new ChemMasterCreatePillsMessage(args.Dosage, args.Count, args.Label));
+        _window.OnCreateBottle += args =>
+            SendPredictedMessage(new ChemMasterOutputToBottleMessage(args.Dosage, args.Label));
+    }
 
-            // Setup static button actions.
-            _window.OnReagentButton += args =>
-                SendPredictedMessage(new ChemMasterReagentAmountButtonMessage(args.Id, args.Amount, args.IsBuffer));
-            _window.OnModeButton += mode => SendPredictedMessage(new ChemMasterSetModeMessage(mode));
-            _window.OnSortButton += () => SendPredictedMessage(new ChemMasterSortingTypeCycleMessage());
-            _window.OnEjectButton += slot => SendPredictedMessage(new ItemSlotButtonPressedEvent(slot));
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        if (state is not ChemMasterBoundUserInterfaceState chemState)
+            return;
 
-            _window.OnPillButton += index => SendPredictedMessage(new ChemMasterSetPillTypeMessage(index));
-            _window.OnCreatePill += args =>
-                SendPredictedMessage(new ChemMasterCreatePillsMessage(args.Dosage, args.Count, args.Label));
-            _window.OnCreateBottle += args =>
-                SendPredictedMessage(new ChemMasterOutputToBottleMessage(args.Dosage, args.Label));
-        }
-
-        /// <summary>
-        /// Update the ui each time new state data is sent from the server.
-        /// </summary>
-        /// <param name="state">
-        /// Data of the <see cref="ChemMasterComponent"/> that this ui represents.
-        /// Sent from the server.
-        /// </param>
-        protected override void UpdateState(BoundUserInterfaceState state)
-        {
-            base.UpdateState(state);
-
-            var castState = (ChemMasterBoundUserInterfaceState) state;
-
-            _window?.UpdateState(castState); // Update window state
-        }
+        _window?.UpdateState(chemState);
     }
 }
