ed3dfa87d6e0a1c309db49b76c2280afc3274027
lzk <124214523+lzk228@users.noreply.github.com>
2025-07-18 19:26:15 +0200

    sure

--- Content.Shared/NameIdentifier/NameIdentifierGroupPrototype.cs
+++ Content.Shared/NameIdentifier/NameIdentifierGroupPrototype.cs
@@ -11,25 +11,25 @@ public sealed partial class NameIdentifierGroupPrototype : IPrototype
     /// <summary>
     ///     Should the identifier become the full name, or just append?
     /// </summary>
-    [DataField("fullName")]
+    [DataField]
     public bool FullName = false;
 
     /// <summary>
     /// Optional format identifier. If set, the name will be formatted using it (e.g., "MK-500").
     /// If not set, only the numeric part will be used (e.g., "500").
     /// </summary>
-    [DataField("format")]
+    [DataField]
     public LocId? Format;
 
     /// <summary>
     ///     The maximal value appearing in an identifier.
     /// </summary>
-    [DataField("maxValue")]
+    [DataField]
     public int MaxValue = 1000;
 
     /// <summary>
     ///     The minimal value appearing in an identifier.
     /// </summary>
-    [DataField("minValue")]
+    [DataField]
     public int MinValue = 0;
 }
