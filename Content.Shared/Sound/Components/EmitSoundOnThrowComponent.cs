efbf30fc8845a57728b18b3ef3de3603b5649c45
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-07-20 06:16:51 +0200

    content changes for engine PR

--- Content.Shared/Sound/Components/EmitSoundOnThrowComponent.cs
+++ Content.Shared/Sound/Components/EmitSoundOnThrowComponent.cs
@@ -5,5 +5,5 @@ namespace Content.Shared.Sound.Components;
 /// <summary>
 /// Simple sound emitter that emits sound on ThrowEvent
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class EmitSoundOnThrowComponent : BaseEmitSoundComponent;
