39bef788797256dba96d0bb18e9888c846c5d423
Kyle Tyo <akikai297@gmail.com>
2025-07-20 04:17:00 -0400

    doc doc doc doc doc

--- Content.Shared/Glue/GluedComponent.cs
+++ Content.Shared/Glue/GluedComponent.cs
@@ -10,11 +10,16 @@ namespace Content.Shared.Glue;
 [Access(typeof(GlueSystem))]
 public sealed partial class GluedComponent : Component
 {
-
+    /// <summary>
+    /// The TimeSpan this effect expires at.
+    /// </summary>
     [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
     [AutoNetworkedField, AutoPausedField]
     public TimeSpan Until;
 
+    /// <summary>
+    /// The duration this effect will last. Determined by the quantity of the reagent that is applied.
+    /// </summary>
     [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
     [AutoNetworkedField]
     public TimeSpan Duration;
