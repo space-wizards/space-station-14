04dd6e470806dc4546e24eec540b3ae598b10022
pa.pecherskij <pa.pecherskij@interfax.ru>
2025-07-24 23:05:48 +0300

    refactor: cleanup for artifact generation code, preparation for balanced graph generation

--- Content.Shared/Xenoarchaeology/Artifact/Prototypes/XenoArchTriggerPrototype.cs
+++ Content.Shared/Xenoarchaeology/Artifact/Prototypes/XenoArchTriggerPrototype.cs
@@ -1,7 +1,5 @@
-using Content.Shared.Random;
 using Content.Shared.Whitelist;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Dictionary;
 
 namespace Content.Shared.Xenoarchaeology.Artifact.Prototypes;
 
@@ -30,17 +28,4 @@ public sealed partial class XenoArchTriggerPrototype : IPrototype
     /// </summary>
     [DataField]
     public ComponentRegistry Components = new();
-}
-
-/// <summary>
-/// Container for list of xeno artifact triggers and their respective weights to be used in case randomly rolling trigger is required.
-/// </summary>
-[Prototype]
-public sealed partial class WeightedRandomXenoArchTriggerPrototype : IWeightedRandomPrototype
-{
-    [IdDataField]
-    public string ID { get; private set; } = default!;
-
-    [DataField(customTypeSerializer: typeof(PrototypeIdDictionarySerializer<float, XenoArchTriggerPrototype>))]
-    public Dictionary<string, float> Weights { get; private set; } = new();
 }
