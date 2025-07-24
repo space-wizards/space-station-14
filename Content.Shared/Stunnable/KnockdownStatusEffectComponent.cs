d14531a0de978e080bf288ead7f8605a57146dfb
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2025-07-23 17:35:01 -0700

    How the fuck did this not get pushed???

--- Content.Shared/Stunnable/KnockdownStatusEffectComponent.cs
+++ Content.Shared/Stunnable/KnockdownStatusEffectComponent.cs
@@ -3,25 +3,7 @@
 namespace Content.Shared.Stunnable;
 
 /// <summary>
-/// Crawling as a status effect
+/// Knockdown as a status effect.
 /// </summary>
 [RegisterComponent, NetworkedComponent, Access(typeof(SharedStunSystem))]
-public sealed partial class KnockdownStatusEffectComponent : Component
-{
-    /// <summary>
-    /// Should this knockdown only affect crawlers?
-    /// </summary>
-    /// <remarks>
-    /// If your status effect doesn't come paired with <see cref="StunnedStatusEffectComponent"/>
-    /// Or if your status effect doesn't whitelist itself to only those with <see cref="CrawlerComponent"/>
-    /// Then you need to set this to true.
-    /// </remarks>
-    [ViewVariables]
-    public bool Crawl;
-
-    /// <summary>
-    /// Should we drop items when we fall?
-    /// </summary>
-    [ViewVariables]
-    public bool Drop = true;
-}
+public sealed partial class KnockdownStatusEffectComponent : Component;
