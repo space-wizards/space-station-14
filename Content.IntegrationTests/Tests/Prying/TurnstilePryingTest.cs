da3b2ffb2d693370176b5448ced61e25b6ae9304
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-07-27 19:23:25 -0400

    remove bolting from turnstile tests

--- Content.IntegrationTests/Tests/Prying/TurnstilePryingTest.cs
+++ Content.IntegrationTests/Tests/Prying/TurnstilePryingTest.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Content.IntegrationTests.Tests.Interaction;
-using Content.Server.Doors;
-using Content.Server.Doors.Systems;
+using Content.Server.Doors.WireActions;
 using Content.Server.Wires;
 using Content.Shared.Doors.Components;
 
@@ -21,27 +20,18 @@ public sealed class TurnstilePryingTest : InteractionTest
     public async Task GenericTurnstilePryingTest(
         [Values(null, Pry, PryPowered, ForcedPryer)]
         string? pryingToolProtoId,
-        [Values] bool bolted,
         [Values] bool solenoidBypassed)
     {
-        var expectedState = GetExpectedValue(pryingToolProtoId, bolted, solenoidBypassed);
+        var expectedState = GetExpectedValue(pryingToolProtoId, solenoidBypassed);
         await SpawnTarget(Turnstile);
         await RunTicks(1);
 
         Assert.That(TryComp<TurnstileComponent>(out var turnstileComponent), "Turnstile does not have TurnstileComponent?");
 
-        var boltSys = SEntMan.System<BoltSystem>();
         var wiresSys = SEntMan.System<WiresSystem>();
 
         // Make sure the door does not auto close for long DoAfters on powered doors
         await RunTicks(1);
-
-        if (bolted)
-        {
-            Assert.That(TryComp<DoorBoltComponent>(out var doorBoltComp), "Turnstile does not have DoorBoltComponent?");
-            await Server.WaitPost(() => boltSys.TrySetBoltsDown((STarget.Value, doorBoltComp!), true));
-            await RunTicks(1);
-        }
 
         if (solenoidBypassed)
         {
@@ -59,7 +49,6 @@ public sealed class TurnstilePryingTest : InteractionTest
         // Initial condition checks
         Assert.That(turnstileComponent, Is.Not.Null);
         Assert.That(turnstileComponent.SolenoidBypassed, Is.EqualTo(solenoidBypassed), "Turnstile has incorrect solenoid bypass state");
-        Assert.That(boltSys.IsBolted(STarget.Value), Is.EqualTo(bolted), "Turnstile has incorrect bolted state.");
         Assert.That(turnstileComponent.PriedExceptions, Is.Empty, "Turnstile pried exceptions are not empty");
 
         // Attempt prying, use hands if null
@@ -79,22 +68,22 @@ public sealed class TurnstilePryingTest : InteractionTest
     /// <param name="bolted"></param>
     /// <param name="solenoidBypassed"></param>
     /// <returns></returns>
-    private static bool GetExpectedValue(string? pryingToolProtoId, bool bolted, bool solenoidBypassed)
+    private static bool GetExpectedValue(string? pryingToolProtoId, bool solenoidBypassed)
     {
         switch (pryingToolProtoId)
         {
             // Hand pry
             case null:
-                // Can't hand pry turnstile
+                // Can't hand pry turnstile.
                 return false;
             case Pry:
-                // Basic tool can pry the turnstile only if the solenoid is bypassed and it's not bolted.
-                return !bolted && solenoidBypassed;
+                // Basic tool can pry the turnstile only if the solenoid is bypassed.
+                return solenoidBypassed;
             case PryPowered:
-                // Powered tool can only pry if the turnstile is not bolted.
-                return !bolted;
+                // Powered tool can always bypass a turnstile.
+                return true;
             case ForcedPryer:
-                // Forced pry can always bypass a turnstile
+                // Forced pry can always bypass a turnstile.
                 return true;
         }
 
