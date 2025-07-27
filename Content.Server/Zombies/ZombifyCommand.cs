8f1249305ed29f6c6b00076dd0a6abb216c9a39a
Kyle Tyo <akikai297@gmail.com>
2025-07-27 04:16:53 -0400

    haha whoops

--- Content.Server/Zombies/ZombifyCommand.cs
+++ Content.Server/Zombies/ZombifyCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Content.Server.Administration;
 using Content.Shared.Administration;
+using Content.Shared.Body.Components;
 using Content.Shared.Zombies;
 using Robust.Shared.Console;
 using Robust.Shared.Toolshed;
