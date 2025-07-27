f631a3017bdad7df7f847da56d083214130325c9
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-07-27 19:27:44 -0400

    cherrypick fixup

--- Content.Server/Doors/Systems/TurnstileSystem.cs
+++ Content.Server/Doors/Systems/TurnstileSystem.cs
@@ -1,29 +1,6 @@
-using System.Linq;
-using Content.Server.Wires;
-using Content.Shared.Doors.Components;
 using Content.Shared.Doors.Systems;
 
 namespace Content.Server.Doors.Systems;
 
 /// <inheritdoc/>
-public sealed class TurnstileSystem : SharedTurnstileSystem
-{
-    [Dependency] private readonly WiresSystem _wires = default!;
-
-    public override void SetSolenoidBypassed(Entity<TurnstileComponent> ent, bool value)
-    {
-        base.SetSolenoidBypassed(ent, value);
-
-        // If the solenoid becomes un-bypassed, bolt the turnstile if the bolt wire is already cut.
-
-        if (value)
-            return;
-
-        if (!TryComp<DoorBoltComponent>(ent, out var doorBolt))
-            return;
-
-        var boltCut = _wires.TryGetWires<DoorBoltWireAction>(ent).Any(w => w.IsCut);
-        if (boltCut)
-            Bolt.TrySetBoltsDown((ent, doorBolt), true);
-    }
-}
+public sealed class TurnstileSystem : SharedTurnstileSystem;
