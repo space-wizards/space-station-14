81aee5baad63f0ab7cfed4d732a72aa30f43ede9
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-07-27 20:23:25 +0200

    initial commit

--- Content.Client/Inventory/ClientInventorySystem.cs
+++ Content.Client/Inventory/ClientInventorySystem.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Content.Client.Clothing;
 using Content.Client.Examine;
 using Content.Client.Verbs.UI;
@@ -11,6 +12,7 @@ using Robust.Client.UserInterface;
 using Robust.Shared.Containers;
 using Robust.Shared.Input.Binding;
 using Robust.Shared.Player;
+using Robust.Shared.Timing;
 
 namespace Content.Client.Inventory
 {
@@ -19,7 +21,7 @@ namespace Content.Client.Inventory
     {
         [Dependency] private readonly IPlayerManager _playerManager = default!;
         [Dependency] private readonly IUserInterfaceManager _ui = default!;
-
+        [Dependency] private readonly IGameTiming _timing = default!;
         [Dependency] private readonly ClientClothingSystem _clothingVisualsSystem = default!;
         [Dependency] private readonly ExamineSystem _examine = default!;
 
@@ -91,6 +93,14 @@ namespace Content.Client.Inventory
 
         private void OnShutdown(EntityUid uid, InventoryComponent component, ComponentShutdown args)
         {
+            if (TryComp(uid, out InventorySlotsComponent? inventorySlots))
+            {
+                foreach (var slot in component.Slots)
+                {
+                    TryRemoveSlotData((uid, inventorySlots), (SlotData)slot);
+                }
+            }
+
             if (uid == _playerManager.LocalEntity)
                 OnUnlinkInventory?.Invoke();
         }
@@ -128,18 +138,11 @@ namespace Content.Client.Inventory
             base.Shutdown();
         }
 
-        protected override void OnInit(EntityUid uid, InventoryComponent component, ComponentInit args)
+        protected override void OnInit(Entity<InventoryComponent> ent, ref ComponentInit args)
         {
-            base.OnInit(uid, component, args);
-            _clothingVisualsSystem.InitClothing(uid, component);
+            base.OnInit(ent, ref args);
 
-            if (!TryComp(uid, out InventorySlotsComponent? inventorySlots))
-                return;
-
-            foreach (var slot in component.Slots)
-            {
-                TryAddSlotDef(uid, inventorySlots, slot);
-            }
+            _clothingVisualsSystem.InitClothing(ent.Owner, ent.Comp);
         }
 
         public void ReloadInventory(InventorySlotsComponent? component = null)
@@ -165,6 +168,9 @@ namespace Content.Client.Inventory
         public void UpdateSlot(EntityUid owner, InventorySlotsComponent component, string slotName,
             bool? blocked = null, bool? highlight = null)
         {
+            if (!_timing.IsFirstTimePredicted)
+                return;
+
             var oldData = component.SlotData[slotName];
             var newHighlight = oldData.Highlighted;
             var newBlocked = oldData.Blocked;
@@ -181,14 +187,25 @@ namespace Content.Client.Inventory
                 EntitySlotUpdate?.Invoke(newData);
         }
 
-        public bool TryAddSlotDef(EntityUid owner, InventorySlotsComponent component, SlotDefinition newSlotDef)
+        public bool TryAddSlotData(Entity<InventorySlotsComponent> ent, SlotData newSlotData)
         {
-            SlotData newSlotData = newSlotDef; //convert to slotData
-            if (!component.SlotData.TryAdd(newSlotDef.Name, newSlotData))
+            if (!ent.Comp.SlotData.TryAdd(newSlotData.SlotName, newSlotData))
                 return false;
 
-            if (owner == _playerManager.LocalEntity)
+            if (ent.Owner == _playerManager.LocalEntity)
                 OnSlotAdded?.Invoke(newSlotData);
+
+            return true;
+        }
+
+        public bool TryRemoveSlotData(Entity<InventorySlotsComponent> ent, SlotData removedSlotData)
+        {
+            if (!ent.Comp.SlotData.Remove(removedSlotData.SlotName))
+                return false;
+
+            if (ent.Owner == _playerManager.LocalEntity)
+                OnSlotRemoved?.Invoke(removedSlotData);
+
             return true;
         }
 
@@ -239,13 +256,30 @@ namespace Content.Client.Inventory
         {
             base.UpdateInventoryTemplate(ent);
 
-            if (TryComp(ent, out InventorySlotsComponent? inventorySlots))
+            if (!TryComp(ent, out InventorySlotsComponent? inventorySlots))
+                return;
+
+            List<SlotData> slotDataToRemove = new(); // don't modify dict while iterating
+
+            foreach (var slotData in inventorySlots.SlotData.Values)
             {
-                foreach (var slot in ent.Comp.Slots)
-                {
-                    if (inventorySlots.SlotData.TryGetValue(slot.Name, out var slotData))
-                        slotData.SlotDef = slot;
-                }
+                if (!ent.Comp.Slots.Any(s => s.Name == slotData.SlotName))
+                    slotDataToRemove.Add(slotData);
+            }
+
+            // remove slots that are no longer in the new template
+            foreach (var slotData in slotDataToRemove)
+            {
+                TryRemoveSlotData((ent.Owner, inventorySlots), slotData);
+            }
+
+            // update existing slots or add them if they don't exist yet
+            foreach (var slot in ent.Comp.Slots)
+            {
+                if (inventorySlots.SlotData.TryGetValue(slot.Name, out var slotData))
+                    slotData.SlotDef = slot;
+                else
+                    TryAddSlotData((ent.Owner, inventorySlots), (SlotData)slot);
             }
         }
 
