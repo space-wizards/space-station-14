ca8670c7e06e33296051abf44be6b86f02b0378a
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-07-28 20:05:40 +0200

    some more cleanup

--- Content.Shared/Abilities/Mime/MimePowersComponent.cs
+++ Content.Shared/Abilities/Mime/MimePowersComponent.cs
@@ -1,81 +1,76 @@
 using Content.Shared.Alert;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
-namespace Content.Shared.Abilities.Mime
+namespace Content.Shared.Abilities.Mime;
+
+/// <summary>
+/// Lets its owner entity use mime powers, like placing invisible walls.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[AutoGenerateComponentPause]
+public sealed partial class MimePowersComponent : Component
 {
     /// <summary>
-    /// Lets its owner entity use mime powers, like placing invisible walls.
+    /// Whether this component is active or not.
+    /// </summarY>
+    [DataField, AutoNetworkedField]
+    public bool Enabled = true;
+
+    /// <summary>
+    /// The wall prototype to use.
     /// </summary>
-    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-    [AutoGenerateComponentPause]
-    public sealed partial class MimePowersComponent : Component
-    {
-        /// <summary>
-        /// Whether this component is active or not.
-        /// </summarY>
-        [DataField]
-        [AutoNetworkedField]
-        public bool Enabled = true;
+    [DataField, AutoNetworkedField]
+    public EntProtoId WallPrototype = "WallInvisible";
 
-        /// <summary>
-        /// The wall prototype to use.
-        /// </summary>
-        [DataField]
-        public EntProtoId WallPrototype = "WallInvisible";
+    [DataField]
+    public EntProtoId? InvisibleWallAction = "ActionMimeInvisibleWall";
 
-        [DataField]
-        public EntProtoId? InvisibleWallAction = "ActionMimeInvisibleWall";
+    [DataField, AutoNetworkedField]
+    public EntityUid? InvisibleWallActionEntity;
 
-        [DataField]
-        [AutoNetworkedField]
-        public EntityUid? InvisibleWallActionEntity;
+    // The vow zone lies below
+    [DataField, AutoNetworkedField]
+    public bool VowBroken = false;
 
-        // The vow zone lies below
-        [ViewVariables]
-        [AutoNetworkedField]
-        public bool VowBroken = false;
+    /// <summary>
+    /// Whether this mime is ready to take the vow again.
+    /// Note that if they already have the vow, this is also false.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool ReadyToRepent = false;
 
-        /// <summary>
-        /// Whether this mime is ready to take the vow again.
-        /// Note that if they already have the vow, this is also false.
-        /// </summary>
-        [ViewVariables]
-        [AutoNetworkedField]
-        public bool ReadyToRepent = false;
+    /// <summary>
+    /// Time when the mime can repent their vow
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    [AutoNetworkedField, AutoPausedField]
+    public TimeSpan VowRepentTime = TimeSpan.Zero;
 
-        /// <summary>
-        /// Time when the mime can repent their vow
-        /// </summary>
-        [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
-        [AutoNetworkedField, AutoPausedField]
-        public TimeSpan VowRepentTime = TimeSpan.Zero;
+    /// <summary>
+    /// How long it takes the mime to get their powers back
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public TimeSpan VowCooldown = TimeSpan.FromMinutes(5);
 
-        /// <summary>
-        /// How long it takes the mime to get their powers back
-        /// </summary>
-        [DataField]
-        public TimeSpan VowCooldown = TimeSpan.FromMinutes(5);
+    [DataField]
+    public ProtoId<AlertPrototype> VowAlert = "VowOfSilence";
 
-        [DataField]
-        public ProtoId<AlertPrototype> VowAlert = "VowOfSilence";
+    [DataField]
+    public ProtoId<AlertPrototype> VowBrokenAlert = "VowBroken";
 
-        [DataField]
-        public ProtoId<AlertPrototype> VowBrokenAlert = "VowBroken";
+    /// <summary>
+    /// Does this component prevent the mime from writing on paper while their vow is active?
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool PreventWriting = false;
 
-        /// <summary>
-        /// Does this component prevent the mime from writing on paper while their vow is active?
-        /// </summary>
-        [DataField]
-        public bool PreventWriting = false;
+    /// <summary>
+    /// What message is displayed when the mime fails to write?
+    /// </summary>
+    [DataField]
+    public LocId FailWriteMessage = "paper-component-illiterate-mime";
 
-        /// <summary>
-        /// What message is displayed when the mime fails to write?
-        /// </summary>
-        [DataField]
-        public LocId FailWriteMessage = "paper-component-illiterate-mime";
-
-        public override bool SendOnlyToOwner => true;
-    }
+    public override bool SendOnlyToOwner => true;
 }
