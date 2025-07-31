544614112f57226a8d24803a66b61e0b13b80de5
Pok245 <113675512+Pok27@users.noreply.github.com>
2025-07-31 16:48:01 +0300

    TODO execution

--- Content.Server/Botany/Components/BasicGrowthComponent.cs
+++ Content.Server/Botany/Components/BasicGrowthComponent.cs
@@ -3,13 +3,15 @@ namespace Content.Server.Botany.Components;
 [RegisterComponent]
 public sealed partial class BasicGrowthComponent : PlantGrowthComponent
 {
-    //Remaining TODOs/TO CHECKS
-    //Update all of seeds.yml to have condensed component set? or set prototype to have them?
-    //Ensure mutations work as expected for non-default growth components
-    //ensure components / values are copied over when seeds are made/clipped.
+    /// <summary>
+    /// Amount of water consumed per growth tick.
+    /// </summary>
     [DataField]
     public float WaterConsumption = 0.5f;
 
+    /// <summary>
+    /// Amount of nutrients consumed per growth tick.
+    /// </summary>
     [DataField]
     public float NutrientConsumption = 0.75f;
 }
