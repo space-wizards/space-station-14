ff3dca355ce7d191e554dade58b2caa1923de591
Token <56667933+TokenStyle@users.noreply.github.com>
2025-08-03 03:52:19 +0500

    bible client made smal

--- Content.Client/Bible/BibleSystem.cs
+++ Content.Client/Bible/BibleSystem.cs
@@ -1,12 +1,5 @@
 using Content.Shared.Bible;
-using Content.Shared.Bible.Components;
 
 namespace Content.Client.Bible;
 
-public sealed class BibleSystem : SharedBibleSystem
-{
-    protected override void AttemptSummon(Entity<SummonableComponent> ent, EntityUid user, TransformComponent? position)
-    {
-        base.AttemptSummon(ent, user, position);
-    }
-}
+public sealed class BibleSystem : SharedBibleSystem;
