962471cdebdb228fe362b1a95b5509eb47890b5f
AsnDen <asnden@gmail.com>
2025-08-03 12:06:32 +0300

    Revert "string"
    This reverts commit 0148e4193dafce43f793da1f3306b6246d602824.

--- Content.Shared/Warps/WarpPointComponent.cs
+++ Content.Shared/Warps/WarpPointComponent.cs
@@ -10,7 +10,7 @@ namespace Content.Shared.Warps;
 public sealed partial class WarpPointComponent : Component
 {
     [ViewVariables(VVAccess.ReadWrite), DataField]
-    public string? Location;
+    public LocId? Location;
 
     /// <summary>
     /// If true, ghosts warping to this entity will begin following it.
