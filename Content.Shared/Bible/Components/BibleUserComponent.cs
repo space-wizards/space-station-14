ab75cbc8bad925c7265b423b5bd4efdcb7b0662d
Token <56667933+TokenStyle@users.noreply.github.com>
2025-08-03 17:38:27 +0500

    SendOnlyToOwner for bibleUsers

--- Content.Shared/Bible/Components/BibleUserComponent.cs
+++ Content.Shared/Bible/Components/BibleUserComponent.cs
@@ -5,5 +5,11 @@ namespace Content.Shared.Bible.Components;
 /// <summary>
 /// For humanoids/entities that can interact with bibles, religion related things. i.e., summon entity with bible or pray on bible.
 /// </summary>
+/// <remarks>
+/// Only chapelain's client get networked and informed as bibleUser. No other entity get informed.
+/// </remarks>
 [RegisterComponent, NetworkedComponent]
-public sealed partial class BibleUserComponent : Component;
+public sealed partial class BibleUserComponent : Component
+{
+    public override bool SendOnlyToOwner => true;
+}
