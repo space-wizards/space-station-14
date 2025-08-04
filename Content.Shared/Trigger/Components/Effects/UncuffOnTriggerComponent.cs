645c5d2ff45e39a17c40225d7994af8f406c4e5c
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-08-04 13:15:48 +0200

    fix comment

--- Content.Shared/Trigger/Components/Effects/UncuffOnTriggerComponent.cs
+++ Content.Shared/Trigger/Components/Effects/UncuffOnTriggerComponent.cs
@@ -5,7 +5,6 @@ namespace Content.Shared.Trigger.Components.Effects;
 /// <summary>
 /// Removes a pair of handcuffs from the entity.
 /// If TargetUser is true the user will be uncuffed instead.
-/// Used for dna scrambler implants.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class UncuffOnTriggerComponent : BaseXOnTriggerComponent;
