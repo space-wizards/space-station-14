1d683a17d936e04f31501a2c300efd5e78925396
Perry Fraser <perry@frasers.org>
2025-08-04 11:57:14 -0400

    refactor: kill AfterAutoHandleState for Follower

--- Content.Shared/Follower/Components/FollowerComponent.cs
+++ Content.Shared/Follower/Components/FollowerComponent.cs
@@ -4,7 +4,7 @@ namespace Content.Shared.Follower.Components;
 
 [RegisterComponent]
 [Access(typeof(FollowerSystem))]
-[NetworkedComponent, AutoGenerateComponentState(true)]
+[NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class FollowerComponent : Component
 {
     [AutoNetworkedField, DataField("following")]
