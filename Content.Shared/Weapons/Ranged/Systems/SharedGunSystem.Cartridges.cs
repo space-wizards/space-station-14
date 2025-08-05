e5e547ba4a1096c2a6c7bd0576d0b57f2f015f2e
Kyle Tyo <akikai297@gmail.com>
2025-08-05 08:51:42 -0400

    commit

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Cartridges.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Cartridges.cs
@@ -1,13 +1,54 @@
+using Content.Shared.Damage;
+using Content.Shared.Damage.Events;
+using Content.Shared.Damage.Systems;
+using Content.Shared.Examine;
+using Content.Shared.Projectiles;
 using Content.Shared.Weapons.Ranged.Components;
-using Robust.Shared.GameStates;
-using Robust.Shared.Serialization;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Weapons.Ranged.Systems;
 
 public abstract partial class SharedGunSystem
 {
+    [Dependency] private readonly DamageExamineSystem _damageExamine = default!;
+
     // needed for server system
     protected virtual void InitializeCartridge()
     {
+        SubscribeLocalEvent<CartridgeAmmoComponent, ExaminedEvent>(OnCartridgeExamine);
+        SubscribeLocalEvent<CartridgeAmmoComponent, DamageExamineEvent>(OnCartridgeDamageExamine);
+    }
+
+    private void OnCartridgeExamine(Entity<CartridgeAmmoComponent> ent, ref ExaminedEvent args)
+    {
+        args.PushMarkup(ent.Comp.Spent
+            ? Loc.GetString("gun-cartridge-spent")
+            : Loc.GetString("gun-cartridge-unspent"));
+    }
+
+    private void OnCartridgeDamageExamine(EntityUid uid, CartridgeAmmoComponent component, ref DamageExamineEvent args)
+    {
+        var damageSpec = GetProjectileDamage(component.Prototype);
+
+        if (damageSpec == null)
+            return;
+
+        _damageExamine.AddDamageExamine(args.Message, Damageable.ApplyUniversalAllModifiers(damageSpec), Loc.GetString("damage-projectile"));
+    }
+
+    private DamageSpecifier? GetProjectileDamage(string proto)
+    {
+        if (!ProtoManager.TryIndex<EntityPrototype>(proto, out var entityProto))
+            return null;
+
+        if (!entityProto.Components.TryGetValue(Factory.GetComponentName<ProjectileComponent>(), out var projectile))
+            return null;
+
+        var p = (ProjectileComponent) projectile.Component;
+
+        if (!p.Damage.Empty)
+            return p.Damage * Damageable.UniversalProjectileDamageModifier;
+
+        return null;
     }
 }
