1f57b820ac23a613c08546f0532235b3151329cf
PubliclyExecutedPig <orangyfingy@gmail.com>
2025-08-05 15:52:24 +0100

    Update ZombieComponent.cs

--- Content.Shared/Zombies/ZombieComponent.cs
+++ Content.Shared/Zombies/ZombieComponent.cs
@@ -30,7 +30,7 @@ public sealed partial class ZombieComponent : Component
     public float MinZombieInfectionChance = 0.25f;
 
     [ViewVariables(VVAccess.ReadWrite)]
-    public float ZombieMovementSpeedDebuff = 0.90f;
+    public float ZombieMovementSpeedDebuff = 0.80f;
 
     /// <summary>
     /// The skin color of the zombie
