d39d8e3e65a40f77b296de04038d418f398a8191
Studio.FaeWilds <studio.faewilds@gmail.com>
2025-08-05 13:52:38 +1000

    Removes unnecessary lines from comment

--- Content.Shared/Trigger/Components/Triggers/TriggerOnLandComponent.cs
+++ Content.Shared/Trigger/Components/Triggers/TriggerOnLandComponent.cs
@@ -2,9 +2,7 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.Trigger.Components.Triggers;
 
-/// <summary>
-/// Triggers when the component parent object lands.
-/// </summary>
+// Triggers when the component parent object lands.
 
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class TriggerOnLandComponent : BaseTriggerOnXComponent;
