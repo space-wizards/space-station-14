ae689010f54d298b990d9bcd61114739266e4098
Studio Fae-Wilds <studio.faewilds@gmail.com>
2025-08-06 13:37:01 +1000

    Update Content.Shared/Trigger/Components/Triggers/TriggerOnUnstrappedComponent.cs
    Increases comment clarity
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Shared/Trigger/Components/Triggers/TriggerOnUnstrappedComponent.cs
+++ Content.Shared/Trigger/Components/Triggers/TriggerOnUnstrappedComponent.cs
@@ -3,9 +3,9 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Trigger.Components.Triggers;
 
 /// <summary>
-/// Triggers when the component parent is unstrapped.
+/// Triggers when something is unstrapped from the entity.
 /// This is intended to be used on objects like chairs or beds.
-/// The parent object should be the object "containing" the strap so to speak.
+/// The user is the entity unstrapped from the component owner.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class TriggerOnUnstrappedComponent : BaseTriggerOnXComponent;
