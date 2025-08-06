44aa29c4666c7ed0997f38419fe22f89c47ce197
Studio.FaeWilds <studio.faewilds@gmail.com>
2025-08-06 18:58:14 +1000

    refactored TriggerOnStrappedOrBuckledSystem.cs removed TriggerOnExaminedSystem.cs and moved it into TriggerSystem.Interaction.cs
    Changes currently untested, not sure how to make it so modders can change what method they want sending out the appropriate trigger key but want to save progress working on it and get feedback from maintainers

--- Content.Shared/Trigger/Components/Triggers/TriggerOnExaminedComponent.cs
+++ Content.Shared/Trigger/Components/Triggers/TriggerOnExaminedComponent.cs
@@ -4,7 +4,7 @@ namespace Content.Shared.Trigger.Components.Triggers;
 
 /// <summary>
 /// Triggers when the receiving object/entity/thing is examined.
-/// Triggers on the thing being examined, not on the one doing the examining.
+/// Triggers on the thing being examined, not on the player doing the examining.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class TriggerOnExaminedComponent : BaseTriggerOnXComponent;
--- Content.Shared/Trigger/Systems/TriggerOnStrappedOrBuckledSystem.cs
+++ Content.Shared/Trigger/Systems/TriggerOnStrappedOrBuckledSystem.cs
@@ -4,11 +4,11 @@ using Content.Shared.Trigger.Components.Triggers;
 namespace Content.Shared.Trigger.Systems;
 
 /// <summary>
-/// This is a system covering all trigger interactions involving strapping or unstrapping objects.
-/// It is used by several separated components, but they all share this same system.
-/// This method of implementing the trigger system was requested by a maintainer.
+/// This is a system covering all trigger interactions involving strapping or buckling objects.
+/// The users of strap components are the objects having an entity strapped to them (IE: Chairs)
+/// The users of buckle components are entities being buckled to an object. (IE: Mobs and players)
 /// </summary>
-public sealed partial class TriggerOnStrappedSystem : EntitySystem
+public sealed partial class TriggerOnStrappedOrBuckledSystem : EntitySystem
 {
     [Dependency] private readonly TriggerSystem _trigger = default!;
 
@@ -17,61 +17,33 @@ public sealed partial class TriggerOnStrappedSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<TriggerOnStrappedComponent, StrappedEvent>(OnStrapped);
+        SubscribeLocalEvent<TriggerOnUnstrappedComponent, UnstrappedEvent>(OnUnstrapped);
+        SubscribeLocalEvent<TriggerOnBuckledComponent, BuckledEvent>(OnBuckled);
+        SubscribeLocalEvent<TriggerOnUnbuckledComponent, UnbuckledEvent>(OnUnbuckled);
     }
 
+
+    #region Class Methods
+    // Called by objects entities can be buckled to. (Chairs, surgical tables/)
     private void OnStrapped(Entity<TriggerOnStrappedComponent> ent, ref StrappedEvent args)
     {
         _trigger.Trigger(ent.Owner, args.Strap, ent.Comp.KeyOut);
-    }
-}
-
-public sealed partial class TriggerOnUnstrappedSystem : EntitySystem
-{
-    [Dependency] private readonly TriggerSystem _trigger = default!;
-
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SubscribeLocalEvent<TriggerOnUnstrappedComponent, UnstrappedEvent>(OnUnstrapped);
     }
 
     private void OnUnstrapped(Entity<TriggerOnUnstrappedComponent> ent, ref UnstrappedEvent args)
     {
         _trigger.Trigger(ent.Owner, args.Strap, ent.Comp.KeyOut);
     }
-}
 
-public sealed partial class TriggerOnBuckledSystem : EntitySystem
-{
-    [Dependency] private readonly TriggerSystem _trigger = default!;
-
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SubscribeLocalEvent<TriggerOnBuckledComponent, BuckledEvent>(OnBuckled);
-    }
-
+    // Called by entities that are buckled to an object. (Mobs, players.)
     private void OnBuckled(Entity<TriggerOnBuckledComponent> ent, ref BuckledEvent args)
     {
         _trigger.Trigger(ent.Owner, args.Buckle, ent.Comp.KeyOut);
-    }
-}
-
-public sealed partial class TriggerOnUnbuckledSystem : EntitySystem
-{
-    [Dependency] private readonly TriggerSystem _trigger = default!;
-
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SubscribeLocalEvent<TriggerOnUnbuckledComponent, UnbuckledEvent>(OnUnbuckled);
     }
 
     private void OnUnbuckled(Entity<TriggerOnUnbuckledComponent> ent, ref UnbuckledEvent args)
     {
         _trigger.Trigger(ent.Owner, args.Buckle, ent.Comp.KeyOut);
     }
+    #endregion
 }
