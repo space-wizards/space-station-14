77be0943f97be8346da27ccf19b594c36fa65aa9
Crude Oil <crude@croil.net>
2025-08-07 12:45:31 +0200

    add AnimalVocalizerComponent, simplify system

---
+++ Content.Server/Vocalization/Components/AnimalVocalizerComponent.cs
@@ -0,0 +1,24 @@
+namespace Content.Server.Vocalization.Components;
+
+/// <summary>
+/// Used on animals that vocalize randomly
+/// </summary>
+[RegisterComponent]
+public sealed partial class AnimalVocalizerComponent : Component
+{
+    /// <summary>
+    /// Minimum length of the random string used to generate a random-length string for vocalization
+    /// </summary>
+    [DataField]
+    public int MinRandomStringLength = 2;
+
+    /// <summary>
+    /// Maximum length of the random string used to generate a random-length string for vocalization
+    /// </summary>
+    [DataField]
+    public int MaxRandomStringLength = 8;
+
+
+    [DataField]
+    public char RandomStringChar = 'a';
+}
