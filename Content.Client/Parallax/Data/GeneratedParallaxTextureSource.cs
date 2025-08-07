b89dd69437462da3192ca4a9c426bb19642654e1
DariusWhite <etoanosono569@gmail.com>
2025-08-07 15:22:13 -0700

    Attempting to fix YAML Linter issue

--- Content.Client/Parallax/Data/GeneratedParallaxTextureSource.cs
+++ Content.Client/Parallax/Data/GeneratedParallaxTextureSource.cs
@@ -37,5 +37,6 @@ public sealed partial class GeneratedParallaxTextureSource : IParallaxTextureSou
         var cache = dependencies.Resolve<GeneratedParallaxCache>();
         cache.Unload(Identifier);
     }
+	public float Slowness { get; private set; }
 }
 
