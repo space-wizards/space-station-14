1374ceea4758db71c7c3cbff9d4b128f38662b82
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2025-08-08 11:22:34 -0400

    Move some Station methods into shared (#38976)

--- Content.Shared/Station/Components/StationDataComponent.cs
+++ Content.Shared/Station/Components/StationDataComponent.cs
@@ -1,26 +1,23 @@
-﻿using Content.Server.Shuttles.Systems;
-using Content.Server.Station.Systems;
-using Robust.Shared.Serialization.TypeSerializers.Implementations;
-using Robust.Shared.Utility;
+﻿using Robust.Shared.GameStates;
 
-namespace Content.Server.Station.Components;
+namespace Content.Shared.Station.Components;
 
 /// <summary>
 /// Stores core information about a station, namely its config and associated grids.
 /// All station entities will have this component.
 /// </summary>
-[RegisterComponent, Access(typeof(StationSystem))]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState, Access(typeof(SharedStationSystem))]
 public sealed partial class StationDataComponent : Component
 {
     /// <summary>
     /// The game map prototype, if any, associated with this station.
     /// </summary>
-    [DataField("stationConfig")]
-    public StationConfig? StationConfig = null;
+    [DataField]
+    public StationConfig? StationConfig;
 
     /// <summary>
     /// List of all grids this station is part of.
     /// </summary>
-    [DataField("grids")]
+    [DataField, AutoNetworkedField]
     public HashSet<EntityUid> Grids = new();
 }
