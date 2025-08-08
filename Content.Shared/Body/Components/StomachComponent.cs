9747a04c6ff5b3338bf815e5e5351d88aaf30b2a
Perry Fraser <perry@frasers.org>
2025-08-08 16:47:58 -0400

    feat: rework stomach reagent tracking

--- Content.Shared/Body/Components/StomachComponent.cs
+++ Content.Shared/Body/Components/StomachComponent.cs
@@ -6,87 +6,81 @@ using Content.Shared.Whitelist;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
-namespace Content.Shared.Body.Components
+namespace Content.Shared.Body.Components;
+
+[RegisterComponent, NetworkedComponent, Access(typeof(StomachSystem), typeof(FoodSystem))]
+public sealed partial class StomachComponent : Component
 {
-    [RegisterComponent, NetworkedComponent, Access(typeof(StomachSystem), typeof(FoodSystem))]
-    public sealed partial class StomachComponent : Component
-    {
-        /// <summary>
-        ///     The next time that the stomach will try to digest its contents.
-        /// </summary>
-        [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
-        public TimeSpan NextUpdate;
+    /// <summary>
+    ///     The next time that the stomach will try to digest its contents.
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    public TimeSpan NextUpdate;
 
-        /// <summary>
-        ///     The interval at which this stomach digests its contents.
-        /// </summary>
-        [DataField]
-        public TimeSpan UpdateInterval = TimeSpan.FromSeconds(1);
+    /// <summary>
+    ///     The interval at which this stomach digests its contents.
+    /// </summary>
+    [DataField]
+    public TimeSpan UpdateInterval = TimeSpan.FromSeconds(1);
 
-        /// <summary>
-        /// Multiplier applied to <see cref="UpdateInterval"/> for adjusting based on metabolic rate multiplier.
-        /// </summary>
-        [DataField]
-        public float UpdateIntervalMultiplier = 1f;
+    /// <summary>
+    /// Multiplier applied to <see cref="UpdateInterval"/> for adjusting based on metabolic rate multiplier.
+    /// </summary>
+    [DataField]
+    public float UpdateIntervalMultiplier = 1f;
 
-        /// <summary>
-        /// Adjusted update interval based off of the multiplier value.
-        /// </summary>
-        [ViewVariables]
-        public TimeSpan AdjustedUpdateInterval => UpdateInterval * UpdateIntervalMultiplier;
+    /// <summary>
+    /// Adjusted update interval based off of the multiplier value.
+    /// </summary>
+    [ViewVariables]
+    public TimeSpan AdjustedUpdateInterval => UpdateInterval * UpdateIntervalMultiplier;
 
-        /// <summary>
-        ///     The solution inside of this stomach this transfers reagents to the body.
-        /// </summary>
-        [ViewVariables]
-        public Entity<SolutionComponent>? Solution;
+    /// <summary>
+    ///     The solution inside of this stomach this transfers reagents to the body.
+    /// </summary>
+    [ViewVariables]
+    public Entity<SolutionComponent>? Solution;
 
-        /// <summary>
-        ///     What solution should this stomach push reagents into, on the body?
-        /// </summary>
-        [DataField]
-        public string BodySolutionName = "chemicals";
+    /// <summary>
+    ///     What solution should this stomach push reagents into, on the body?
+    /// </summary>
+    [DataField]
+    public string BodySolutionName = "chemicals";
 
-        /// <summary>
-        ///     Time between reagents being ingested and them being
-        ///     transferred to <see cref="BloodstreamComponent"/>
-        /// </summary>
-        [DataField]
-        public TimeSpan DigestionDelay = TimeSpan.FromSeconds(20);
+    /// <summary>
+    ///     Time between reagents being ingested and them being
+    ///     transferred to <see cref="BloodstreamComponent"/>
+    /// </summary>
+    [DataField]
+    public TimeSpan DigestionDelay = TimeSpan.FromSeconds(20);
 
-        /// <summary>
-        ///     A whitelist for what special-digestible-required foods this stomach is capable of eating.
-        /// </summary>
-        [DataField]
-        public EntityWhitelist? SpecialDigestible = null;
+    /// <summary>
+    ///     A whitelist for what special-digestible-required foods this stomach is capable of eating.
+    /// </summary>
+    [DataField]
+    public EntityWhitelist? SpecialDigestible = null;
 
-        /// <summary>
-        /// Controls whitelist behavior. If true, this stomach can digest <i>only</i> food that passes the whitelist. If false, it can digest normal food <i>and</i> any food that passes the whitelist.
-        /// </summary>
-        [DataField]
-        public bool IsSpecialDigestibleExclusive = true;
+    /// <summary>
+    /// Controls whitelist behavior. If true, this stomach can digest <i>only</i> food that passes the whitelist. If false, it can digest normal food <i>and</i> any food that passes the whitelist.
+    /// </summary>
+    [DataField]
+    public bool IsSpecialDigestibleExclusive = true;
 
-        /// <summary>
-        ///     Used to track how long each reagent has been in the stomach
-        /// </summary>
-        [ViewVariables]
-        public readonly List<ReagentDelta> ReagentDeltas = new();
-
-        /// <summary>
-        ///     Used to track quantity changes when ingesting & digesting reagents
-        /// </summary>
-        public sealed class ReagentDelta
-        {
-            public readonly ReagentQuantity ReagentQuantity;
-            public TimeSpan Lifetime { get; private set; }
-
-            public ReagentDelta(ReagentQuantity reagentQuantity)
-            {
-                ReagentQuantity = reagentQuantity;
-                Lifetime = TimeSpan.Zero;
-            }
-
-            public void Increment(TimeSpan delta) => Lifetime += delta;
-        }
-    }
+    /// <summary>
+    /// Used to track how long each reagent has been in the stomach
+    /// </summary>
+    [DataField]
+    public Dictionary<ReagentId, List<ReagentDelta>> ReagentDeltas = new();
 }
+
+/// <summary>
+/// A simple pair of a reagent's quantity and the time it's been present in the
+/// stomach.
+/// </summary>
+/// <remarks>
+/// We can't have it be the timespan the reagent should be digested since the
+/// amount the lifetime it gets incremented by is actually dependent on
+/// <see cref="StomachComponent.AdjustedUpdateInterval"/>, which can change at
+/// any time.
+/// </remarks>
+public record struct ReagentDelta(ReagentQuantity Reagent, TimeSpan Lifetime);
