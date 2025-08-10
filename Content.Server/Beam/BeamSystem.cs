f3db16862e628a0ccbf4aa31597ca4470014c8e5
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-08-10 11:49:51 +0300

    Fix

--- Content.Server/Beam/BeamSystem.cs
+++ Content.Server/Beam/BeamSystem.cs
@@ -4,10 +4,8 @@ using Content.Shared.Beam;
 using Content.Shared.Beam.Components;
 using Content.Shared.Physics;
 using Robust.Server.GameObjects;
-using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Map;
-using Robust.Shared.Physics;
 using Robust.Shared.Physics.Collision.Shapes;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Systems;
@@ -20,7 +18,6 @@ public sealed class BeamSystem : SharedBeamSystem
     [Dependency] private readonly TransformSystem _transform = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly SharedBroadphaseSystem _broadphase = default!;
-    [Dependency] private readonly SharedPhysicsSystem _physics = default!;
 
     public override void Initialize()
     {
@@ -83,7 +80,6 @@ public sealed class BeamSystem : SharedBeamSystem
         if (!TryComp<PhysicsComponent>(ent, out var physics) || !TryComp<BeamComponent>(ent, out var beam))
             return;
 
-        FixturesComponent? manager = null;
         _fixture.TryCreateFixture(
             ent,
             shape,
@@ -91,12 +87,9 @@ public sealed class BeamSystem : SharedBeamSystem
             hard: false,
             collisionMask: (int)CollisionGroup.ItemMask,
             collisionLayer: (int)CollisionGroup.MobLayer,
-            manager: manager,
             body: physics);
 
-        _physics.SetBodyType(ent, BodyType.Dynamic, manager: manager, body: physics);
-        _physics.SetCanCollide(ent, true, manager: manager, body: physics);
-        _broadphase.RegenerateContacts((ent, physics, manager));
+        _broadphase.RegenerateContacts((ent, physics));
 
         var distanceLength = distanceCorrection.Length();
 
