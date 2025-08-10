5a1be1ac8ba16af0cbda9bcb92dd4511c12ff5f8
imatsoup <93290208+imatsoup@users.noreply.github.com>
2025-08-10 00:43:38 -0400

    Fix check failures

--- Content.Shared/Silicons/StationAi/SharedStationAiSystem.cs
+++ Content.Shared/Silicons/StationAi/SharedStationAiSystem.cs
@@ -306,10 +306,13 @@ public abstract partial class SharedStationAiSystem : EntitySystem
         // Corvax-Next-AiRemoteControl-Start
         if (TryComp<StationAiHeldComponent>(held, out var heldComp))
             if (heldComp.CurrentConnectedEntity != null)
-                AnnounceIntellicardUsage(heldComp.CurrentConnectedEntity.Value, intelliComp.WarningSound);
+            {
+                var ev = new ChatNotificationEvent(_downloadChatNotificationPrototype, args.Used, args.User);
+                RaiseLocalEvent(held, ref ev);
+            }
         // Corvax-Next-AiRemoteControl-End
 
-        var doAfterArgs = new DoAfterArgs(EntityManager, args.User, cardHasAi ? intelliComp.UploadTime : intelliComp.DownloadTime, new IntellicardDoAfterEvent(), args.Target, ent.Owner)
+                var doAfterArgs = new DoAfterArgs(EntityManager, args.User, cardHasAi ? intelliComp.UploadTime : intelliComp.DownloadTime, new IntellicardDoAfterEvent(), args.Target, ent.Owner)
         {
             BreakOnDamage = true,
             BreakOnMove = true,
