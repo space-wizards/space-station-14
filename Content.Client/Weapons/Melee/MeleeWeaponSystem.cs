43051068eed41a5fd8226a721ed1fdd89b9e945c
Luxeator <Luxeator>
2025-08-11 12:59:23 -0400

    Re-add dependencies

--- Content.Client/Weapons/Melee/MeleeWeaponSystem.cs
+++ Content.Client/Weapons/Melee/MeleeWeaponSystem.cs
@@ -26,10 +26,12 @@ public sealed partial class MeleeWeaponSystem : SharedMeleeWeaponSystem
     [Dependency] private readonly IInputManager _inputManager = default!;
     [Dependency] private readonly IPlayerManager _player = default!;
     [Dependency] private readonly IStateManager _stateManager = default!;
+    [Dependency] private readonly AnimationPlayerSystem _animation = default!;
     [Dependency] private readonly InputSystem _inputSystem = default!;
     [Dependency] private readonly SharedColorFlashEffectSystem _color = default!;
     [Dependency] private readonly MapSystem _map = default!;
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
+    [Dependency] private readonly SpriteSystem _sprite = default!;
 
     private EntityQuery<TransformComponent> _xformQuery;
 
