abf6bb63292033767ca43fe9c0da93f1bd40a2b0
chromiumboy <chromium.boy@gmail.com>
2025-08-13 12:27:16 -0500

    Allow MMIs used in the construction of AI cores to take them over

--- Content.Server/Silicons/StationAi/StationAiSystem.cs
+++ Content.Server/Silicons/StationAi/StationAiSystem.cs
@@ -1,10 +1,17 @@
 using Content.Server.Chat.Systems;
+using Content.Server.Construction;
+using Content.Server.Mind;
+using Content.Server.Roles;
+using Content.Server.Station.Systems;
 using Content.Shared.Chat.Prototypes;
+using Content.Shared.Containers.ItemSlots;
 using Content.Shared.DeviceNetwork.Components;
+using Content.Shared.Roles;
 using Content.Shared.Silicons.StationAi;
 using Content.Shared.StationAi;
 using Content.Shared.Turrets;
 using Content.Shared.Weapons.Ranged.Events;
+using Robust.Server.Containers;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
@@ -16,10 +23,20 @@ public sealed class StationAiSystem : SharedStationAiSystem
 {
     [Dependency] private readonly EntityLookupSystem _lookup = default!;
     [Dependency] private readonly SharedTransformSystem _xforms = default!;
+    [Dependency] private readonly ContainerSystem _container = default!;
+    [Dependency] private readonly MindSystem _mind = default!;
+    [Dependency] private readonly RoleSystem _roles = default!;
+    [Dependency] private readonly StationSystem _station = default!;
+    [Dependency] private readonly StationJobsSystem _stationJobs = default!;
+    [Dependency] private readonly ItemSlotsSystem _slots = default!;
 
     private readonly HashSet<Entity<StationAiCoreComponent>> _stationAiCores = new();
     private readonly ProtoId<ChatNotificationPrototype> _turretIsAttackingChatNotificationPrototype = "TurretIsAttacking";
     private readonly ProtoId<ChatNotificationPrototype> _aiWireSnippedChatNotificationPrototype = "AiWireSnipped";
+
+    private readonly ProtoId<JobPrototype> _stationAiJob = "StationAi";
+    private readonly EntProtoId _defaultAi = "StationAiBrain";
+    private readonly string _stationAiMMIContainer = "mmi";
 
     public override void Initialize()
     {
@@ -27,6 +44,36 @@ public sealed class StationAiSystem : SharedStationAiSystem
 
         SubscribeLocalEvent<ExpandICChatRecipientsEvent>(OnExpandICChatRecipients);
         SubscribeLocalEvent<StationAiTurretComponent, AmmoShotEvent>(OnAmmoShot);
+        SubscribeLocalEvent<StationAiCoreComponent, AfterConstructionChangeEntityEvent>(AfterConstructionChangeEntity);
+    }
+
+    private void AfterConstructionChangeEntity(Entity<StationAiCoreComponent> ent, ref AfterConstructionChangeEntityEvent args)
+    {
+        var station = _station.GetOwningStation(ent);
+
+        if (!_container.TryGetContainer(ent, _stationAiMMIContainer, out var container) ||
+            container.Count == 0)
+        {
+            return;
+        }
+
+        var mmi = container.ContainedEntities[0];
+
+        if (_mind.TryGetMind(mmi, out var mindId, out var mind))
+        {
+            // Found an existing mind to transfer into the AI core
+            var aiBrain = Spawn(_defaultAi, Transform(ent.Owner).Coordinates);
+            _roles.MindAddJobRole(mindId, mind, false, _stationAiJob);
+            _mind.TransferTo(mindId, aiBrain);
+
+            if (!TryComp<StationAiHolderComponent>(ent, out var targetHolder) ||
+                !_slots.TryInsert(ent, targetHolder.Slot, aiBrain, null))
+            {
+                QueueDel(aiBrain);
+            }
+        }
+
+        QueueDel(mmi);
     }
 
     private void OnExpandICChatRecipients(ExpandICChatRecipientsEvent ev)
