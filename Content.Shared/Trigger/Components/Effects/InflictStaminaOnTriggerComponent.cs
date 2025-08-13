0872ddc8b0d1c7f11b25927ac67a53ee56b9bc6d
Hannah Giovanna Dawson <karakkaraz@gmail.com>
2025-08-13 12:06:54 +0100

    InflictStaminaOnTriggerSystem
    Surprised this wasn't done alongside the damage one.

---
+++ Content.Shared/Trigger/Components/Effects/InflictStaminaOnTriggerComponent.cs
@@ -0,0 +1,26 @@
+using Content.Shared.Damage;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Trigger.Components.Effects;
+
+/// <summary>
+/// Will damage an entity when triggered.
+/// If TargetUser is true it the user will take damage instead.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class InflictStaminaOnTriggerComponent : BaseXOnTriggerComponent
+{
+    /// <summary>
+    /// Should the damage ignore resistances?
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool IgnoreResistances;
+
+
+    /// <summary>
+    /// The stamina amount that is dealt.
+    /// May be further modified by <see cref="Systems.BeforeDamageOnTriggerEvent"/> subscriptions.
+    /// </summary>
+    [DataField(required: true), AutoNetworkedField]
+    public float Stamina;
+}
