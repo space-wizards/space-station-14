3daa134a5d2799a5ced66ea03c6057890aae9e2c
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2025-08-13 18:56:03 +0800

    kill .net comments

--- Content.Shared/Atmos/Components/GasMinerComponent.cs
+++ Content.Shared/Atmos/Components/GasMinerComponent.cs
@@ -9,72 +9,72 @@ namespace Content.Shared.Atmos.Components;
 public sealed partial class GasMinerComponent : Component
 {
     /// <summary>
-    ///     Operational state of the miner.
+    /// Operational state of the miner.
     /// </summary>
     [AutoNetworkedField]
     [ViewVariables(VVAccess.ReadOnly)]
     public GasMinerState MinerState = GasMinerState.Disabled;
 
     /// <summary>
-    ///      If the number of moles in the external environment exceeds this number, no gas will be released.
+    /// If the number of moles in the external environment exceeds this number, no gas will be released.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField]
     public float MaxExternalAmount = float.PositiveInfinity;
 
     /// <summary>
-    ///      If the pressure (in kPA) of the external environment exceeds this number, no gas will be released.
+    /// If the pressure (in kPA) of the external environment exceeds this number, no gas will be released.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField]
     public float MaxExternalPressure = Atmospherics.GasMinerDefaultMaxExternalPressure;
 
     /// <summary>
-    ///     Gas to spawn.
+    /// Gas to spawn.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField(required: true)]
     public Gas SpawnGas;
 
     /// <summary>
-    ///     Temperature in Kelvin.
+    /// Temperature in Kelvin.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField]
     public float SpawnTemperature = Atmospherics.T20C;
 
     /// <summary>
-    ///     The number of moles released from the miner's internal storage, per second, when the miner is working,
-    ///     if it has enough gas stored to do so.
+    /// The number of moles released from the miner's internal storage, per second, when the miner is working,
+    /// if it has enough gas stored to do so.
     /// </summary>
     [DataField("releaseAmount")]
     public float ReleaseRate = Atmospherics.MolesCellStandard * 20f;
 
     /// <summary>
-    ///     The maximum number of moles that can be stored within the miner's internal storage at once.
+    /// The maximum number of moles that can be stored within the miner's internal storage at once.
     /// </summary>
     /// <remarks>
-    ///     This should not be infinite. Instead, <see cref="ReleaseRate"/> and <see cref="MiningRate"/>
-    ///     should both be of a lower or equal value compared to this.
+    /// This should not be infinite. Instead, <see cref="ReleaseRate"/> and <see cref="MiningRate"/>
+    /// should both be of a lower or equal value compared to this.
     /// </remarks>
     [DataField]
     public float MaxStoredAmount = 1200f;
 
     /// <summary>
-    ///     The number of moles that are currently stored within the miner's internal storage, to be released later at the rate of <see cref="ReleaseRate">.
+    /// The number of moles that are currently stored within the miner's internal storage, to be released later at the rate of <see cref="ReleaseRate">.
     /// </summary>
     [DataField, AutoNetworkedField]
     public float StoredAmount;
 
     /// <summary>
-    ///     The <see cref="StoredAmount"/>, the last time which it was replicated. This is used so that continuous very small changes in <see cref="StoredAmount"/> being
-    ///     intentionally not replicated, will not adversly affect anyone who examines this.
+    /// The <see cref="StoredAmount"/>, the last time which it was replicated. This is used so that continuous very small changes in <see cref="StoredAmount"/> being
+    /// intentionally not replicated, will not adversly affect anyone who examines this.
     /// </summary>
     [ViewVariables(VVAccess.ReadOnly)]
     public float LastReplicatedStoredAmount;
 
     /// <summary>
-    ///     The number of moles that are mined, per second, into the miner's internal storage, not released.
+    /// The number of moles that are mined, per second, into the miner's internal storage, not released.
     /// </summary>
     [DataField("spawnAmount")]
     public float MiningRate = Atmospherics.MolesCellStandard * 20f;
