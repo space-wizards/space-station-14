c5487c67f29a09e30d66d74c00a57e85efc55ebf
Hannah Giovanna Dawson <karakkaraz@gmail.com>
2025-08-13 12:16:01 +0100

    Moar docstring changes!!!

--- Content.Shared/Trigger/Components/Effects/InflictStaminaOnTriggerComponent.cs
+++ Content.Shared/Trigger/Components/Effects/InflictStaminaOnTriggerComponent.cs
@@ -1,24 +1,23 @@
-using Content.Shared.Damage;
 using Robust.Shared.GameStates;
 
 namespace Content.Shared.Trigger.Components.Effects;
 
 /// <summary>
-/// Will add stamina to an entity when triggered.
-/// If TargetUser is true it the user will have stamina added instead.
+/// Will inflict stamina to an entity when triggered.
+/// If TargetUser is true it the user will have stamina inflicted instead.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class InflictStaminaOnTriggerComponent : BaseXOnTriggerComponent
 {
     /// <summary>
-    /// Should the damage ignore resistances?
+    /// Should the inflicted stamina ignore resistances?
     /// </summary>
     [DataField, AutoNetworkedField]
     public bool IgnoreResistances;
 
     /// <summary>
-    /// The stamina amount that is added to the target.
-    /// May be further modified by <see cref="Systems.BeforeDamageOnTriggerEvent"/> subscriptions.
+    /// The stamina amount that is inflicted to the target.
+    /// May be further modified by <see cref="Systems.BeforeInflictStaminaOnTriggerEvent"/> subscriptions.
     /// </summary>
     [DataField(required: true), AutoNetworkedField]
     public float Stamina;
