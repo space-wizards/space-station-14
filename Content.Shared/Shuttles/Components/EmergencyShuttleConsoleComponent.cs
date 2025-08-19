28273afb5921e4ae8f8cada2a955ce1100d993ce
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-08-19 13:54:13 +0300

    Yes

--- Content.Shared/Shuttles/Components/EmergencyShuttleConsoleComponent.cs
+++ Content.Shared/Shuttles/Components/EmergencyShuttleConsoleComponent.cs
@@ -3,7 +3,6 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Shuttles.Components;
 
 [RegisterComponent, NetworkedComponent]
-[AutoGenerateComponentState]
 public sealed partial class EmergencyShuttleConsoleComponent : Component
 {
     // TODO: Okay doing it by string is kinda suss but also ID card tracking doesn't seem to be robust enough
@@ -11,9 +10,9 @@ public sealed partial class EmergencyShuttleConsoleComponent : Component
     /// <summary>
     /// ID cards that have been used to authorize an early launch.
     /// </summary>
-    [DataField("authorized"), AutoNetworkedField]
-    public HashSet<string> AuthorizedEntities = [];
+    [ViewVariables(VVAccess.ReadWrite), DataField("authorized")]
+    public HashSet<string> AuthorizedEntities = new();
 
-    [DataField, AutoNetworkedField]
+    [ViewVariables(VVAccess.ReadWrite), DataField("authorizationsRequired")]
     public int AuthorizationsRequired = 3;
 }
