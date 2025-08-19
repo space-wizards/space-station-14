26c838b4e201f9c9c81fc8fcdae5b33a719398d5
Samuel <samuelcamilo8@gmail.com>
2025-08-19 09:59:27 -0300

    use GenericVisualizer

--- Content.Client/Silicons/Laws/SiliconLawSystem.cs
+++ Content.Client/Silicons/Laws/SiliconLawSystem.cs
@@ -1,32 +1,9 @@
 ﻿using Content.Shared.Silicons.Laws;
-using Content.Shared.Silicons.Laws.Components;
-using Robust.Client.GameObjects;
 
 namespace Content.Client.Silicons.Laws;
 
 /// <inheritdoc/>
 public sealed class SiliconLawSystem : SharedSiliconLawSystem
 {
-    [Dependency] private readonly AppearanceSystem _appearance = default!;
-    [Dependency] private readonly SpriteSystem _sprite = default!;
 
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SubscribeLocalEvent<SiliconLawOverriderComponent, AppearanceChangeEvent>(OnLawOverriderAppearanceChanged);
-    }
-
-    public void OnLawOverriderAppearanceChanged(EntityUid uid, SiliconLawOverriderComponent component, ref AppearanceChangeEvent args)
-    {
-        if (args.Sprite == null)
-            return;
-        var sprite = args.Sprite;
-
-        if (!_appearance.TryGetData(uid, LawOverriderVisuals.LawBoardInserted, out bool lawboardInserted))
-            lawboardInserted = false;
-
-        _sprite.LayerSetVisible((uid, sprite), LawOverriderVisualLayers.LawBoard, lawboardInserted);
-        _sprite.LayerSetVisible((uid, sprite), LawOverriderVisualLayers.Light, lawboardInserted);
-    }
 }
