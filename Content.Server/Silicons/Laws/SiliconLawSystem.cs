9e4d15b6fa4bad87e44c9090f5c4179488b5ff37
Samuel <samuelcamilo8@gmail.com>
2025-08-19 10:52:50 -0300

    swapp GenericVisualizer for ItemMapper

--- Content.Server/Silicons/Laws/SiliconLawSystem.cs
+++ Content.Server/Silicons/Laws/SiliconLawSystem.cs
@@ -50,9 +50,6 @@ public sealed class SiliconLawSystem : SharedSiliconLawSystem
         SubscribeLocalEvent<SiliconLawProviderComponent, MindAddedMessage>(OnLawProviderMindAdded);
         SubscribeLocalEvent<SiliconLawProviderComponent, MindRemovedMessage>(OnLawProviderMindRemoved);
         SubscribeLocalEvent<SiliconLawProviderComponent, SiliconEmaggedEvent>(OnEmagLawsAdded);
-
-        SubscribeLocalEvent<SiliconLawOverriderComponent, EntInsertedIntoContainerMessage>(OnLawOverriderEntityInserted);
-        SubscribeLocalEvent<SiliconLawOverriderComponent, EntRemovedFromContainerMessage>(OnLawOverriderEntityRemoved);
     }
 
     private void OnMapInit(EntityUid uid, SiliconLawBoundComponent component, MapInitEvent args)
@@ -289,16 +286,6 @@ public sealed class SiliconLawSystem : SharedSiliconLawSystem
         {
             SetLaws(lawset, update, provider.LawUploadSound);
         }
-    }
-
-    public void OnLawOverriderEntityInserted(EntityUid uid, SiliconLawOverriderComponent component, EntInsertedIntoContainerMessage args)
-    {
-        _appearance.SetData(uid, LawOverriderVisuals.LawBoardInserted, true);
-    }
-
-    public void OnLawOverriderEntityRemoved(EntityUid uid, SiliconLawOverriderComponent component, EntRemovedFromContainerMessage args)
-    {
-        _appearance.SetData(uid, LawOverriderVisuals.LawBoardInserted, false);
     }
 }
 
