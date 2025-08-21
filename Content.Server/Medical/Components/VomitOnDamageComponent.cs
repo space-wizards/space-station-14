d8fd480229dc2697d7e2fd969996c5226bcade41
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-08-21 20:04:53 +0300

    Cleanup

--- Content.Server/Medical/Components/VomitOnDamageComponent.cs
+++ Content.Server/Medical/Components/VomitOnDamageComponent.cs
@@ -5,26 +5,28 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Server.Medical.Components;
 
+/// <summary>
+/// Makes an entity vomit after receiving certain damage types.
+/// </summary>
 [RegisterComponent, AutoGenerateComponentPause]
 [Access(typeof(VomitOnDamageSystem))]
 public sealed partial class VomitOnDamageComponent : Component
 {
     /// <summary>
-    ///
+    /// Key - damage type. Value - damage threshold.
     /// </summary>
     [DataField(required: true)]
     public Dictionary<ProtoId<DamageTypePrototype>, FixedPoint2?> Damage;
 
     /// <summary>
-    ///
+    /// Cooldown between vomits.
     /// </summary>
     [DataField]
-    public TimeSpan VomitCooldown = TimeSpan.FromMinutes(5);
+    public TimeSpan VomitCooldown = TimeSpan.FromMinutes(10);
 
     /// <summary>
-    ///
+    /// Next time when an entity will be able to vomit.
     /// </summary>
-    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
-    [AutoPausedField]
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField]
     public TimeSpan NextVomitTime = TimeSpan.Zero;
 }
