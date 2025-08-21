70c277033c042816f9e5a50a08108811b10116e5
WreakHavocOnTheMiddleClass <179751402+WreakHavocOnTheMiddleClass@users.noreply.github.com>
2025-08-20 22:24:24 -0500

    fix misprediction?
    Uses current game tick + entity ID to generate a seed, similar to workaround used in ClumsySystem. Addressing misprediction caused by client and server not sharing a seed for RNG.

--- Content.Shared/Damage/Systems/RequireProjectileTargetSystem.cs
+++ Content.Shared/Damage/Systems/RequireProjectileTargetSystem.cs
@@ -1,18 +1,20 @@
 using Content.Shared.CCVar;
 using Content.Shared.Projectiles;
+using Content.Shared.Random.Helpers;
 using Content.Shared.Weapons.Ranged.Components;
 using Content.Shared.Standing;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Configuration;
 using Robust.Shared.Containers;
-using Robust.Shared.Random;
+using Robust.Shared.Timing;
+
 
 namespace Content.Shared.Damage.Components;
 
 public sealed class RequireProjectileTargetSystem : EntitySystem
 {
     [Dependency] private readonly IConfigurationManager _cfgManager = default!;
-    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
 
     public override void Initialize()
@@ -44,7 +46,7 @@ public sealed class RequireProjectileTargetSystem : EntitySystem
             if (TerminatingOrDeleted(shooter.Value))
                 return;
 
-            if (!_container.IsEntityOrParentInContainer(shooter.Value)) 
+            if (!_container.IsEntityOrParentInContainer(shooter.Value))
             {
                 var hitChance = _cfgManager.GetCVar(CCVars.ProneMobHitChance);
 
@@ -52,7 +54,11 @@ public sealed class RequireProjectileTargetSystem : EntitySystem
                 // Skip if false or hit chance is 0
                 if ((hitChance > 0) && HasComp<StandingStateComponent>(ent))
                 {
-                    if ((hitChance < 100) && (hitChance <= _random.Next(100)))
+                    // TODO: Replace with RandomPredicted once the engine PR is merged
+                    var seed = SharedRandomExtensions.HashCodeCombine(new() { (int)_timing.CurTick.Value, GetNetEntity(ent).Id });
+                    var rand = new System.Random(seed);
+
+                    if ((hitChance < 100) && (hitChance <= rand.Next(100)))
                     {
                         args.Cancelled = true;
                     }
