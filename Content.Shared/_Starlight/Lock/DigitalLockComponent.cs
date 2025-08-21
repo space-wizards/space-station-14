66f1780cc9273a0219effe0c4dbfb7051897cd20
Rinary1 <rinary.super@gmail.com>
2025-08-21 21:41:21 +0300

    remove auto network, commentaries

--- Content.Shared/_Starlight/Lock/DigitalLockComponent.cs
+++ Content.Shared/_Starlight/Lock/DigitalLockComponent.cs
@@ -14,29 +14,53 @@ public sealed partial class DigitalLockComponent : Component
     /// <summary>
     /// Password of the digital lock
     /// </summary>
-    [AutoNetworkedField]
     public string Code = "";
+
+    /// <summary>
+    /// Determines, is the service panel open
+    /// </summary>
 
     [AutoNetworkedField]
     public bool MaintenanceOpen = false;
 
+    /// <summary>
+    /// Entered code by user
+    /// </summary>
     [AutoNetworkedField]
     public string EnteredCode = "";
 
+    /// <summary>
+    /// Current lock status
+    /// </summary>
     [AutoNetworkedField]
     public DigitalLockStatus Status = DigitalLockStatus.AWAIT_CODE;
 
+    /// <summary>
+    /// Max code lenght which can be setuped
+    /// </summary>
     [DataField]
     public int MaxCodeLength = 6;
 
+    /// <summary>
+    /// Stores last played note of keypad button
+    /// </summary>
     public int LastPlayedKeypadSemitones = 0;
 
+    /// <summary>
+    /// Sound which played when press number keypad button
+    /// </summary>
     [DataField("keypadPressSound")]
     public SoundSpecifier KeypadPressSound = new SoundPathSpecifier("/Audio/Machines/Nuke/general_beep.ogg");
 
+    /// <summary>
+    /// Sound which played when access granted
+    /// </summary>
     [DataField("accessGrantedSound")]
     public SoundSpecifier AccessGrantedSound = new SoundPathSpecifier("/Audio/Machines/Nuke/confirm_beep.ogg");
 
+    /// <summary>
+    /// Sound which played when password wrong
+    /// </summary>
     [DataField("accessDeniedSound")]
     public SoundSpecifier AccessDeniedSound = new SoundPathSpecifier("/Audio/Machines/Nuke/angry_beep.ogg");
 }
