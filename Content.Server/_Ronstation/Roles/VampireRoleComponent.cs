993def9b8978c17cbd8ac7da5550b1df2c843f31
imatsoup <93290208+imatsoup@users.noreply.github.com>
2025-08-21 15:42:16 -0400

    another miscomment

--- Content.Server/_Ronstation/Roles/VampireRoleComponent.cs
+++ Content.Server/_Ronstation/Roles/VampireRoleComponent.cs
@@ -4,7 +4,7 @@ using Robust.Shared.GameStates;
 namespace Content.Server._Ronstation.Roles;
 
 /// <summary>
-/// Added to mind role entities to tag that they are a blood brother.
+/// Added to mind role entities to tag that they are a vampire.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class VampireRoleComponent : BaseMindRoleComponent
