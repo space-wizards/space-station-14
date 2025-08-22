da71c930e2ed5e7a0abc1e976d9e6954394641f8
FungiFellow <151778459+FungiFellow@users.noreply.github.com>
2025-08-22 15:11:27 -0400

    Add files via upload

---
+++ Content.Shared/Ghost/DeadHearingComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Shared.Ghost;
+
+/// <summary>
+/// This is used for marking entities which should receive messages from the ghosts.
+/// </summary>
+[RegisterComponent]
+public sealed partial class DeadHearingComponent : Component
+{
+}
