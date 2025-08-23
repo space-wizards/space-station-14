d776b2ec3710246798133a152a196968f218e3dd
beck-thompson <beck314159@hotmail.com>
2025-08-23 11:58:19 -0700

    meow

--- Content.Server/GameTicking/Commands/DelayStartCommand.cs
+++ Content.Server/GameTicking/Commands/DelayStartCommand.cs
@@ -2,50 +2,45 @@ using Content.Server.Administration;
 using Content.Shared.Administration;
 using Robust.Shared.Console;
 
-namespace Content.Server.GameTicking.Commands
+namespace Content.Server.GameTicking.Commands;
+
+[AdminCommand(AdminFlags.Round)]
+sealed class DelayStartCommand : LocalizedEntityCommands
 {
-    [AdminCommand(AdminFlags.Round)]
-    sealed class DelayStartCommand : IConsoleCommand
+    public override string Command => "adjustroundstart";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IEntityManager _e = default!;
-
-        public string Command => "delaystart";
-        public string Description => Loc.GetString("delaystart-description");
-        public string Help => Loc.GetString("delaystart-help");
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        var ticker = EntityManager.System<GameTicker>();
+        if (ticker.RunLevel != GameRunLevel.PreRoundLobby)
         {
-            var ticker = _e.System<GameTicker>();
-            if (ticker.RunLevel != GameRunLevel.PreRoundLobby)
-            {
-                shell.WriteLine(Loc.GetString("delaystart-preround-only"));
-                return;
-            }
+            shell.WriteLine(Loc.GetString("adjustroundstart-preround-only"));
+            return;
+        }
 
-            if (args.Length == 0)
-            {
-                var paused = ticker.TogglePause();
-                shell.WriteLine(paused ? Loc.GetString("delaystart-paused") : Loc.GetString("delaystart-resumed"));
-                return;
-            }
+        if (args.Length == 0)
+        {
+            var paused = ticker.TogglePause();
+            shell.WriteLine(paused ? Loc.GetString("adjustroundstart-paused") : Loc.GetString("adjustroundstart-resumed"));
+            return;
+        }
 
-            if (args.Length != 1)
-            {
-                shell.WriteLine(Loc.GetString("shell-need-between-arguments", ("lower", 0), ("upper", 1)));
-                return;
-            }
+        if (args.Length != 1)
+        {
+            shell.WriteLine(Loc.GetString("shell-need-between-arguments", ("lower", 0), ("upper", 1)));
+            return;
+        }
 
-            if (!int.TryParse(args[0], out var seconds) || seconds == 0)
-            {
-                shell.WriteLine(Loc.GetString("delaystart-invalid-seconds", ("seconds", args[0])));
-                return;
-            }
+        if (!int.TryParse(args[0], out var seconds) || seconds == 0)
+        {
+            shell.WriteLine(Loc.GetString("adjustroundstart-invalid-seconds", ("seconds", args[0])));
+            return;
+        }
 
-            var time = TimeSpan.FromSeconds(seconds);
-            if (!ticker.DelayStart(time))
-            {
-                shell.WriteLine(Loc.GetString("shell-unknown-error"));
-            }
+        var time = TimeSpan.FromSeconds(seconds);
+        if (!ticker.DelayStart(time))
+        {
+            shell.WriteLine(Loc.GetString("shell-unknown-error"));
         }
     }
 }
--- Resources/Locale/en-US/game-ticking/delaystart-command.ftl
+++ Resources/Locale/en-US/game-ticking/delaystart-command.ftl
@@ -1,6 +1,7 @@
-delaystart-description = Adjusts the roundstart timer (accepts positive and negative integers).
-delaystart-help = Usage: delaystart <seconds> Accepts +/- integers, Pauses/Resumes the countdown if no argument is provided.
-delaystart-preround-only = This can only be executed while the game is in the pre-round lobby.
-delaystart-paused = Paused the countdown.
-delaystart-resumed = Resumed the countdown.
-delaystart-invalid-seconds = {$seconds} isn't a valid amount of seconds. Accepts nonzero integers.
+cmd-adjustroundstart-desc = Adjusts the roundstart timer (accepts positive and negative integers).
+cmd-adjustroundstart-help = Usage: adjustroundstart <seconds> Accepts +/- integers, Pauses/Resumes the countdown if no argument is provided.
+
+adjustroundstart-preround-only = This can only be executed while the game is in the pre-round lobby.
+adjustroundstart-paused = Paused the countdown.
+adjustroundstart-resumed = Resumed the countdown.
+adjustroundstart-invalid-seconds = {$seconds} isn't a valid amount of seconds. Accepts nonzero integers.
