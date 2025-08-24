257836de5dfcf8886bf9ef458021af36a3830324
imatsoup <93290208+imatsoup@users.noreply.github.com>
2025-08-24 17:03:12 -0400

    Possibly fixed the test failure by un-networking this?

--- Content.Server/_Ronstation/Roles/VampireRoleComponent.cs
+++ Content.Server/_Ronstation/Roles/VampireRoleComponent.cs
@@ -6,5 +6,5 @@ namespace Content.Server._Ronstation.Roles;
 /// <summary>
 /// Added to mind role entities to tag that they are a vampire.
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent]
 public sealed partial class VampireRoleComponent : BaseMindRoleComponent;
