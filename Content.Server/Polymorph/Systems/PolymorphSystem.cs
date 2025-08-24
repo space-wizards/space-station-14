91a4cee6e1b8b9810ad59eab8e9a944b24ad22a2
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2025-08-24 06:34:56 -0700

    [Bugfix] Lizard smite fix (#39842)
    Fix
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.Server/Polymorph/Systems/PolymorphSystem.cs
+++ Content.Server/Polymorph/Systems/PolymorphSystem.cs
@@ -259,7 +259,7 @@ public sealed partial class PolymorphSystem : EntitySystem
 
         if (configuration.TransferHumanoidAppearance)
         {
-            _humanoid.CloneAppearance(uid, child);
+            _humanoid.CloneAppearance(child, uid);
         }
 
         if (_mindSystem.TryGetMind(uid, out var mindId, out var mind))
