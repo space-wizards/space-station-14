25200e6d7e67d152abb48bbb163bfb8b123eef66
kosticia <kosticia46@gmail.com>
2025-08-25 04:21:17 +0300

    why git is lazy

---
+++ Content.Client/Tools/StatusControls/ToolsStatusControlSystem.cs
@@ -0,0 +1,20 @@
+using Content.Client.Items;
+using Content.Shared.Tools.Components;
+using Content.Shared.Tools.Systems;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Tools;
+
+public sealed class ToolsStatusControlSystem : EntitySystem
+{
+    [Dependency] private readonly SpriteSystem _sprite = default!;
+    [Dependency] private readonly SharedToolSystem _tool = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        Subs.ItemStatus<WelderComponent>(ent => new WelderStatusControl(ent, EntityManager, _tool));
+        Subs.ItemStatus<MultipleToolComponent>(ent => new MultipleToolStatusControl(ent));
+    }
+}
