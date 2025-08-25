5d1d02945977d7883d43bee2532c8cf800b4c600
kosticia <kosticia46@gmail.com>
2025-08-25 04:38:10 +0300

    this one unrelated

--- Content.Client/Tools/StatusControls/ToolsStatusControlSystem.cs
+++ Content.Client/Tools/StatusControls/ToolsStatusControlSystem.cs
@@ -1,13 +1,11 @@
 using Content.Client.Items;
 using Content.Shared.Tools.Components;
 using Content.Shared.Tools.Systems;
-using Robust.Client.GameObjects;
 
 namespace Content.Client.Tools;
 
 public sealed class ToolsStatusControlSystem : EntitySystem
 {
-    [Dependency] private readonly SpriteSystem _sprite = default!;
     [Dependency] private readonly SharedToolSystem _tool = default!;
 
     public override void Initialize()
