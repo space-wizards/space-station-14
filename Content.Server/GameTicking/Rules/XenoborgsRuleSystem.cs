519fbbb1af58bf0a5b546874d79a51dc5c9ab7c9
Samuel <samuelcamilo8@gmail.com>
2025-08-27 00:26:28 -0300

    round end summary for xenoborgs

--- Content.Server/GameTicking/Rules/XenoborgsRuleSystem.cs
+++ Content.Server/GameTicking/Rules/XenoborgsRuleSystem.cs
@@ -1,13 +1,23 @@
 using Content.Server.Antag;
 using Content.Server.GameTicking.Rules.Components;
 using Content.Shared.GameTicking.Components;
+using Content.Shared.Humanoid;
+using Content.Shared.Mind;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Mobs.Systems;
+using Content.Server.RoundEnd;
 using Content.Shared.Xenoborgs.Components;
+using Robust.Shared.Player;
+using System.Globalization;
 
 namespace Content.Server.GameTicking.Rules;
 
 public sealed class XenoborgsRuleSystem : GameRuleSystem<XenoborgsRuleComponent>
 {
     [Dependency] private readonly AntagSelectionSystem _antag = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!;
+    [Dependency] private readonly SharedMindSystem _mindSystem = default!;
+    [Dependency] private readonly RoundEndSystem _roundEnd = default!;
 
     public override void Initialize()
     {
@@ -20,7 +30,7 @@ public sealed class XenoborgsRuleSystem : GameRuleSystem<XenoborgsRuleComponent>
         GameRuleComponent gameRule,
         GameRuleStartedEvent args)
     {
-
+        base.Started(uid, component, gameRule, args);
     }
 
     private void OnAfterAntagEntSelected(Entity<XenoborgsRuleComponent> ent, ref AfterAntagEntitySelectedEvent args)
@@ -39,6 +49,110 @@ public sealed class XenoborgsRuleSystem : GameRuleSystem<XenoborgsRuleComponent>
                 Color.BlueViolet,
                 ent.Comp.GreetSoundNotification);
         }
+    }
+
+    protected override void AppendRoundEndText(EntityUid uid,
+        XenoborgsRuleComponent component,
+        GameRuleComponent gameRule,
+        ref RoundEndTextAppendEvent args)
+    {
+        base.AppendRoundEndText(uid, component, gameRule, ref args);
+
+        var numXenoborgs = GetNumberXenoborgs();
+        var numHumans = GetNumberHumans();
+
+        if (numXenoborgs < 5)
+            args.AddLine(Loc.GetString("xenoborgs-crewmajor"));
+        else if (4 * numXenoborgs < numHumans)
+            args.AddLine(Loc.GetString("xenoborgs-crewmajor"));
+        else if (2 * numXenoborgs < numHumans)
+            args.AddLine(Loc.GetString("xenoborgs-crewminor"));
+        else if (1.5 * numXenoborgs < numHumans)
+            args.AddLine(Loc.GetString("xenoborgs-neutral"));
+        else if (numXenoborgs < numHumans)
+            args.AddLine(Loc.GetString("xenoborgs-borgsminor"));
+        else
+            args.AddLine(Loc.GetString("xenoborgs-borgsmajor"));
+
+        var numMothershipCores = GetNumberMothershipCores();
+
+        if (numMothershipCores == 0)
+            args.AddLine(Loc.GetString("xenoborgs-cond-all-xenoborgs-dead-core-dead"));
+        else if (numXenoborgs == 0)
+            args.AddLine(Loc.GetString("xenoborgs-cond-all-xenoborgs-dead-core-alive"));
+        else
+            args.AddLine(Loc.GetString("xenoborgs-cond-xenoborgs-alive", ("count", numXenoborgs)));
+
+        args.AddLine(Loc.GetString("xenoborgs-list-start"));
+
+        var antags = _antag.GetAntagIdentifiers(uid);
+
+        foreach (var (_, sessionData, name) in antags)
+        {
+            args.AddLine(Loc.GetString("xenoborgs-list", ("name", name), ("user", sessionData.UserName)));
+        }
+    }
+
+    /// <summary>
+    /// Get the number of xenoborgs
+    /// </summary>
+    /// <param name="playerControlled">if it should only include xenoborgs with a mind</param>
+    /// <param name="alive">if it should only include xenoborgs that are alive</param>
+    /// <returns></returns>
+    private int GetNumberXenoborgs(bool playerControlled = true, bool alive = true)
+    {
+        var numberXenoborgs = 0;
+
+        var query = AllEntityQuery<XenoborgComponent>();
+        while (query.MoveNext(out var xenoborg, out _))
+        {
+            if (playerControlled && !_mindSystem.TryGetMind(xenoborg, out _, out _))
+                continue;
+
+            if (alive && !_mobState.IsAlive(xenoborg))
+                continue;
+
+            numberXenoborgs++;
+        }
+
+        return numberXenoborgs;
+    }
+
+    /// <summary>
+    /// Gets the number of humans who are alive
+    /// </summary>
+    /// <returns></returns>
+    private int GetNumberHumans()
+    {
+        var humans = 0;
+
+        var players = AllEntityQuery<HumanoidAppearanceComponent, ActorComponent, MobStateComponent>();
+        while (players.MoveNext(out var uid, out _, out _, out var mob))
+        {
+            if (!_mobState.IsAlive(uid, mob))
+                continue;
+
+            humans++;
+        }
+
+        return humans;
+    }
+
+    /// <summary>
+    /// Gets the number of xenoborg cores
+    /// </summary>
+    /// <returns></returns>
+    private int GetNumberMothershipCores()
+    {
+        var numberMothershipCores = 0;
+
+        var mothershipCoreQuery = AllEntityQuery<MothershipCoreComponent>();
+        while (mothershipCoreQuery.MoveNext(out _, out _))
+        {
+            numberMothershipCores++;
+        }
+
+        return numberMothershipCores;
     }
 
 }
--- Resources/Locale/en-US/game-ticking/game-presets/preset-xenoborgs.ftl
+++ Resources/Locale/en-US/game-ticking/game-presets/preset-xenoborgs.ftl
@@ -4,3 +4,20 @@ xenoborgs-description = A Xenoborg Mothership was detected near the station. Sto
 xenoborgs-welcome = You're a xenoborg. Protect and help the mothership core to make more xenoborgs. and eventually turn all carbon-based life form into silicon.
 
 mothership-welcome = You're the mothership core. Guide the xeonborgs so they can bring your materials and sentient brains so you can grow the xenoborg army and turn all carbon-based life form into silicon.
+
+xenoborgs-borgsmajor = [color=blue]Xenoborg major victory![/color]
+xenoborgs-borgsminor = [color=blue]Xenoborg minor victory![/color]
+xenoborgs-neutral = [color=white]Neutral outcome![/color]
+xenoborgs-crewminor = [color=yellow]Crew minor victory![/color]
+xenoborgs-crewmajor = [color=yellow]Crew major victory![/color]
+
+xenoborgs-cond-all-xenoborgs-dead-core-alive = All xenoborgs were destroyed. The mothership core remains adrift in space.
+xenoborgs-cond-all-xenoborgs-dead-core-dead = The mothership core was destroyed and there are no xenoborgs left.
+xenoborgs-cond-xenoborgs-alive = {$count ->
+    [one] Only one xenoborg survived.
+    *[other] There were {$count} xenoborgs in the end.
+}
+
+xenoborgs-list-start = The starting xenoborg team were:
+xenoborgs-list = - [color=White]{$name}[/color] ([color=gray]{$user}[/color])
+
