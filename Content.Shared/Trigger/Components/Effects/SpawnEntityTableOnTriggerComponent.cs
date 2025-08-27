aaa9f70bf1e49d95f6464b0a1397da0c259e0851
iaada <iaada@users.noreply.github.com>
2025-08-26 19:05:02 -0500

    commit

--- Content.Shared/Trigger/Components/Effects/SpawnEntityTableOnTriggerComponent.cs
+++ Content.Shared/Trigger/Components/Effects/SpawnEntityTableOnTriggerComponent.cs
@@ -1,21 +1,21 @@
+using Content.Shared.EntityTable.EntitySelectors;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Trigger.Components.Effects;
 
 /// <summary>
-/// Spawns a prototype when triggered.
+/// Spawns an entity table at this entity when triggered.
 /// If TargetUser is true it will be spawned at their location.
 /// </summary>
-/// <seealso cref="SpawnEntityTableOnTriggerComponent"/>
+/// <seealso cref="SpawnOnTriggerComponent"/>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed partial class SpawnOnTriggerComponent : BaseXOnTriggerComponent
+public sealed partial class SpawnEntityTableOnTriggerComponent : BaseXOnTriggerComponent
 {
     /// <summary>
-    /// The prototype to spawn.
+    /// The table to spawn.
     /// </summary>
     [DataField(required: true), AutoNetworkedField]
-    public EntProtoId Proto = string.Empty;
+    public EntityTableSelector Table;
 
     /// <summary>
     /// Use MapCoordinates for spawning?
@@ -25,7 +25,7 @@ public sealed partial class SpawnOnTriggerComponent : BaseXOnTriggerComponent
     public bool UseMapCoords;
 
     /// <summary>
-    /// Whether or not to use predicted spawning.
+    /// Whether to use predicted spawning.
     /// </summary>
     [DataField, AutoNetworkedField]
     public bool Predicted;
