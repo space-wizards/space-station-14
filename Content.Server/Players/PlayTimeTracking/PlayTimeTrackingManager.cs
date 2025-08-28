d0fc52b67965b99ec32c500789d955cd694e58bb
ss14-Starlight <ss14-Starlight@outlook.com>
2025-08-28 16:53:57 +0200

    Apply suggestions from code review
    Co-authored-by: Rinary <rinary.super@gmail.com>

--- Content.Server/Players/PlayTimeTracking/PlayTimeTrackingManager.cs
+++ Content.Server/Players/PlayTimeTracking/PlayTimeTrackingManager.cs
@@ -1,10 +1,10 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
+using System.Linq; // NullLink
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
-using Content.Server._NullLink.Core;
-using Content.Server._NullLink.Helpers;
+using Content.Server._NullLink.Core; // NullLink
+using Content.Server._NullLink.Helpers; // NullLink
 using Content.Server.Database;
 using Content.Shared.CCVar;
 using Content.Shared.Players.PlayTimeTracking;
@@ -16,8 +16,8 @@ using Robust.Shared.Network;
 using Robust.Shared.Player;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
-using Starlight.NullLink;
-using PlayTime = Starlight.NullLink.PlayTime;
+using Starlight.NullLink; // NullLink
+using PlayTime = Starlight.NullLink.PlayTime; // NullLink
 
 namespace Content.Server.Players.PlayTimeTracking;
 
