05fef55a0f2aaca6df9d1f88d5e2a22d1fdbf4ea
Jezithyr <Jezithyr@gmail.com>
2025-08-31 06:16:40 -0700

    one more

--- Content.Server/_Starlight/Character/SLCharacterInfoSystem.cs
+++ Content.Server/_Starlight/Character/SLCharacterInfoSystem.cs
@@ -1,7 +1,4 @@
-﻿// SPDX-FileCopyrightText: 2025 Afterlight RnD
-// SPDX-License-Identifier: ASL-1.2
-
-using Content.Shared._Starlight.Character.Info;
+﻿using Content.Shared._Starlight.Character.Info;
 
 namespace Content.Server._Starlight.Character;
 
