8bf18daa734bbe1f3fee024345506a4246629c8e
M4rchy-S <alex12345suslik@gmail.com>
2025-08-31 23:35:15 +0300

    Fix Radiation Vomit for dead mobs

--- Content.Server/Destructible/Thresholds/Behaviors/VomitBehavior.cs
+++ Content.Server/Destructible/Thresholds/Behaviors/VomitBehavior.cs
@@ -12,7 +12,7 @@ public sealed partial class VomitBehavior : IThresholdBehavior
         if (!system.EntityManager.TryGetComponent(uid, out MobStateComponent? mobState))
             return;
 
-        if (mobState.CurrentState == MobState.Alive)
+        if (mobState.CurrentState is MobState.Alive or MobState.Critical)
             system.EntityManager.System<VomitSystem>().Vomit(uid);
     }
 }
