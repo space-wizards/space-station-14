589e5c448199cc2ec84f7531e5d9b749abb0176b
Matthew Herber <matthewherber@sbcglobal.net>
2025-09-01 18:04:51 -0400

    oof I was authing incorrectly

--- Tools/_Starlight/wait_for_actions.py
+++ Tools/_Starlight/wait_for_actions.py
@@ -4,15 +4,14 @@ import os
 import time
 
 # using an access token
-github_token = os.getenv("GITHUB_TOKEN")
-repo_name = os.getenv("GITHUB_REPOSITORY")
-workflow_run_id = int(os.getenv("WORKFLOW_RUN_ID"))
-workflow_name = os.getenv("WORKFLOW_NAME")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+REPO_NAME = os.getenv("GITHUB_REPOSITORY")
+RUN_ID = int(os.getenv("WORKFLOW_RUN_ID"))
+WORKFLOW_NAME = os.getenv("WORKFLOW_NAME")
 
-auth = Auth.Token(github_token)
-g = Github(auth=auth)
-repo = g.get_repo(repo_name)
-print(f"Authenticated to repo {repo.full_name}")
+g = Github(GITHUB_TOKEN)
+repo = g.get_repo(REPO_NAME)
+print(f"Authenticated to repo {repo.full_name}", flush=True)
 
 #now find the workflow run list for the que workflow, and see if there is any before us
 workflow_runs = repo.get_workflow_runs()
@@ -21,12 +20,12 @@ workflow_runs = repo.get_workflow_runs()
 while True:
     workflow_runs = repo.get_workflow_runs()
     #get all the ones running the specific workflow
-    que_workflow_runs = [wr for wr in workflow_runs if wr.name == workflow_name and wr.status in ["queued", "in_progress"]]
+    que_workflow_runs = [wr for wr in workflow_runs if wr.name == WORKFLOW_NAME and wr.status in ["queued", "in_progress"]]
     #check them all for if any of them have an ID less than us
-    any_before_us = any(wr.id < workflow_run_id for wr in que_workflow_runs)
+    any_before_us = any(wr.id < RUN_ID for wr in que_workflow_runs)
     if not any_before_us:
-        print("No workflow runs queued or running before us, proceeding...")
-        break
+        print("No workflow runs queued or running before us, proceeding...", flush=True)
+        exit(0)
 
     print("There are still workflow runs queued or running before us, waiting 10 seconds...", flush=True)
     time.sleep(10)
