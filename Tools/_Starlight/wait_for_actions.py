1168ad2949e929333b6769ad62d8f5463a757ed2
Matthew Herber <matthewherber@sbcglobal.net>
2025-09-01 18:52:27 -0400

    fixes for que

--- Tools/_Starlight/wait_for_actions.py
+++ Tools/_Starlight/wait_for_actions.py
@@ -6,7 +6,7 @@ import time
 # using an access token
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 REPO_NAME = os.getenv("GITHUB_REPOSITORY")
-RUN_ID = int(os.getenv("WORKFLOW_RUN_ID"))
+RUN_NUMBER = int(os.getenv("WORKFLOW_RUN_NUMBER"))
 WORKFLOW_NAME = os.getenv("WORKFLOW_NAME")
 
 g = Github(GITHUB_TOKEN)
@@ -16,12 +16,12 @@ print(f"Authenticated to repo {repo.full_name}", flush=True)
 def checkIfSafeToProceed(status):
     #check if there are any other workflow runs for this workflow that are queued or in progress with an ID less than us
     workflow_runs = repo.get_workflow_runs(status=status, exclude_pull_requests=False)
+    #print all runs
+    for wr in workflow_runs:
+        print(f"Found workflow run {wr.name} with ID {wr.run_number} and status {wr.status}", flush=True)
     que_workflow_runs = [wr for wr in workflow_runs if wr.name == WORKFLOW_NAME]
-    any_before_us = any(wr.id < RUN_ID for wr in que_workflow_runs)
+    any_before_us = any(wr.run_number < RUN_NUMBER for wr in que_workflow_runs)
     return not any_before_us
-
-#now find the workflow run list for the que workflow, and see if there is any before us
-workflow_runs = repo.get_workflow_runs()
 
 #loop and get all their IDs, and check if ANY are less than us
 while True:
