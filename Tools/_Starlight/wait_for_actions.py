9d9f49a4c3d5b0b7765f14e077c52dfe31cfc360
Matthew Herber <matthewherber@sbcglobal.net>
2025-09-01 18:07:47 -0400

    change to filter on the get request

--- Tools/_Starlight/wait_for_actions.py
+++ Tools/_Starlight/wait_for_actions.py
@@ -13,17 +13,19 @@ g = Github(GITHUB_TOKEN)
 repo = g.get_repo(REPO_NAME)
 print(f"Authenticated to repo {repo.full_name}", flush=True)
 
+def checkIfSafeToProceed(status):
+    #check if there are any other workflow runs for this workflow that are queued or in progress with an ID less than us
+    workflow_runs = repo.get_workflow_runs(status=status)
+    que_workflow_runs = [wr for wr in workflow_runs if wr.name == WORKFLOW_NAME]
+    any_before_us = any(wr.id < RUN_ID for wr in que_workflow_runs)
+    return not any_before_us
+
 #now find the workflow run list for the que workflow, and see if there is any before us
 workflow_runs = repo.get_workflow_runs()
 
 #loop and get all their IDs, and check if ANY are less than us
 while True:
-    workflow_runs = repo.get_workflow_runs()
-    #get all the ones running the specific workflow
-    que_workflow_runs = [wr for wr in workflow_runs if wr.name == WORKFLOW_NAME and wr.status in ["queued", "in_progress"]]
-    #check them all for if any of them have an ID less than us
-    any_before_us = any(wr.id < RUN_ID for wr in que_workflow_runs)
-    if not any_before_us:
+    if checkIfSafeToProceed("queued") and checkIfSafeToProceed("in_progress"):
         print("No workflow runs queued or running before us, proceeding...", flush=True)
         exit(0)
 
