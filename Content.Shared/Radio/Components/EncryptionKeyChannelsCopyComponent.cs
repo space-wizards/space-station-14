fbef28a94c7dade00ecf0137e9a243768076c01e
Anri <elyasin.ane@mail.ru>
2025-09-01 20:31:39 +0300

    init-commit

---
+++ Content.Shared/Radio/Components/EncryptionKeyChannelsCopyComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared.Radio.Components;
+
+/// <summary>
+/// This component provides ability to copy channels from others Encryption keys or entire headsets.
+/// </summary>
+[RegisterComponent]
+public sealed partial class EncryptionKeyChannelsCopyComponent : Component { }
---
+++ Resources/Locale/en-US/radio/components/encryption-key-channels-copy-component.ftl
@@ -0,0 +1,2 @@
+encryption-key-channel-copy-successful = {CAPITALIZE($used)} received new encryption from {THE($target)}
+encryption-key-channel-copy-failed = {CAPITALIZE($used)} didn't receive encryption from {THE($target)}
