4a5f3a7d2918cc8386600f69c85ecc06d7c0d88c
SuperGDPWYL <supergdphone@gmail.com>
2025-09-02 12:03:28 +0100

    multi exclusion!

--- Content.Server/Objectives/Components/NotJobRequirementComponent.cs
+++ Content.Server/Objectives/Components/NotJobRequirementComponent.cs
@@ -2,6 +2,7 @@ using Content.Server.Objectives.Systems;
 using Content.Shared.Roles;
 using Content.Shared.Roles.Jobs;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Prototypes;
 
 /// <summary>
 /// Requires that the player not have a certain job to have this objective.
@@ -10,8 +11,17 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototy
 public sealed partial class NotJobRequirementComponent : Component
 {
     /// <summary>
-    /// ID of the job to ban from having this objective.
+    /// ID of the job to ban from having this objective, use for singular job exclusions (i.e. steal tropico for atmos tech).
     /// </summary>
-    [DataField(required: true, customTypeSerializer: typeof(PrototypeIdSerializer<JobPrototype>))]
-    public string Job = string.Empty;
+    /// <remarks>
+    /// May be worth phasing this out later, but that would be a breaking change.
+    /// </remarks>
+    [DataField]
+    public ProtoId<JobPrototype> Job = string.Empty;
+
+    /// <summary>
+    /// List of IDs to ban from having this objective, for multi-job exclusions (i.e. steal paramed's voidsuit for paramedic, chemist, doctor, psychologist)
+    /// </summary>
+    [DataField]
+    public List<ProtoId<JobPrototype>> Jobs = new List<ProtoId<JobPrototype>>();
 }
