bcaa14d05aa10b847d4b13c9e1e4ce3af1e0335c
AndrewFenriz <78079974+AndrewFenriz@users.noreply.github.com>
2025-09-02 17:38:55 +0300

    Расширение UI новых/обновлённых консолей, перестановка в UI музыкальных инструментов (#472)

--- Content.Client/Instruments/UI/InstrumentMenu.xaml.cs
+++ Content.Client/Instruments/UI/InstrumentMenu.xaml.cs
@@ -48,7 +48,7 @@ namespace Content.Client.Instruments.UI
             PlaybackSlider.OnValueChanged += PlaybackSliderSeek;
             PlaybackSlider.OnKeyBindUp += PlaybackSliderKeyUp;
 
-            MinSize = SetSize = new Vector2(400, 150);
+            MinSize = SetSize = new Vector2(500, 150); // DS14: expanding the interface size for ru localization
         }
 
         public void SetInstrument(Entity<InstrumentComponent> entity)
