2b2c5ba55acf324372c52a36a098256d9e41d014
Janet Blackquill <uhhadd@gmail.com>
2025-09-03 13:14:19 -0400

    fix profile editing & loading

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -483,6 +483,11 @@ namespace Content.Client.Lobby.UI
 
             _pronounWindow = new PronounWindow(Profile, pronouns, gender, _playerManager.LocalSession, collection);
             _pronounWindow.OpenCentered();
+            _pronounWindow.OnProfileChanged += profile =>
+            {
+                Profile = profile;
+                SetDirty();
+            };
 
             if (Profile is null)
                 return;
