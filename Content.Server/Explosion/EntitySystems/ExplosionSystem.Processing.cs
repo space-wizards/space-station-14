29a33d25cc1436c4e3fe040a7a9f95f1bcfc4b47
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2025-09-03 13:15:20 -0700

    Explosiv nerf

--- Content.Server/Explosion/EntitySystems/ExplosionSystem.Processing.cs
+++ Content.Server/Explosion/EntitySystems/ExplosionSystem.Processing.cs
@@ -461,7 +461,7 @@ public sealed partial class ExplosionSystem
                 }
 
                 // TODO EXPLOSIONS turn explosions into entities, and pass the the entity in as the damage origin.
-                _damageableSystem.TryChangeDamage(entity, damage * _damageableSystem.UniversalExplosionDamageModifier, ignoreResistances: true);
+                _damageableSystem.TryChangeDamage(entity, damage * _damageableSystem.UniversalExplosionDamageModifier);
 
             }
         }
