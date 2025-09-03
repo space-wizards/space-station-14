120fc514744040afcbba106b26d559cf966880d2
Samuel <samuelcamilo8@gmail.com>
2025-09-02 22:28:36 -0300

    documentation

--- Content.Shared/Xenoborgs/Components/MothershipCoreComponent.cs
+++ Content.Shared/Xenoborgs/Components/MothershipCoreComponent.cs
@@ -2,6 +2,9 @@ using Robust.Shared.Audio;
 
 namespace Content.Shared.Xenoborgs.Components;
 
+/// <summary>
+/// Defines what is a xenoborg core for the intentions of the xenoborg rule. if all xenoborg cores are destroyed. all xenoborgs will self-destruct.
+/// </summary>
 [RegisterComponent]
 public sealed partial class MothershipCoreComponent : Component
 {
