8b43e45eef4c9ce2e88eb05b47d93a20a7c5a0bf
pa.pecherskij <pa.pecherskij@interfax.ru>
2025-09-04 00:32:17 +0300

    fix: make color actually apply

--- Content.Client/Administration/UI/CustomControls/HSeparator.cs
+++ Content.Client/Administration/UI/CustomControls/HSeparator.cs
@@ -10,27 +10,47 @@ public sealed class HSeparator : Control
 
     private static readonly Color DefaultSeparatorColor = Color.FromHex("#3D4059");
 
+    private readonly PanelContainer _panelContainer;
+
     public HSeparator(Color color)
     {
-        SeparatorColor = color;
         if (TryGetStyleProperty<Color>(StylePropertyColor, out var bgColor))
         {
             color = bgColor;
         }
 
-        AddChild(new PanelContainer
+        _panelContainer = new PanelContainer
+        {
+            PanelOverride = new StyleBoxFlat
             {
-                PanelOverride = new StyleBoxFlat
-                {
-                    BackgroundColor = color,
-                    ContentMarginBottomOverride = 2,
-                    ContentMarginLeftOverride = 2,
-                },
-            }
-        );
+                BackgroundColor = color,
+                ContentMarginBottomOverride = 2,
+                ContentMarginLeftOverride = 2,
+            },
+        };
+        AddChild(_panelContainer);
+        SeparatorColor = color;
     }
 
     public HSeparator() : this(DefaultSeparatorColor) { }
 
-    public Color SeparatorColor { get; set; }
+    public Color SeparatorColor
+    {
+        get
+        {
+            if(_panelContainer?.PanelOverride is StyleBoxFlat styleBox)
+            {
+                return styleBox.BackgroundColor;
+            }
+
+            return DefaultSeparatorColor;
+        }
+        set
+        {
+            if (_panelContainer?.PanelOverride is StyleBoxFlat styleBox)
+            {
+                styleBox.BackgroundColor = value;
+            }
+        }
+    }
 }
