b78da591f5276fe9261c20ba49f3182d7ae9b525
nkokic <nikola1.kokic@gmail.com>
2025-09-04 22:33:52 +0200

    documented MetabolismExclusionEvent

--- Content.Shared/Body/Events/MetabolismExclusionEvent.cs
+++ Content.Shared/Body/Events/MetabolismExclusionEvent.cs
@@ -2,5 +2,9 @@ using Content.Shared.Chemistry.Reagent;
 
 namespace Content.Shared.Body.Events;
 
+/// <summary>
+/// Event called by <see cref="Content.Server.Body.Systems.MetabolizerSystem"/> to get a list excluded of
+/// reagents that Bloodstream deems as blood reagents.
+/// </summary>
 [ByRefEvent]
 public readonly record struct MetabolismExclusionEvent(List<ReagentQuantity> ReagentList);
