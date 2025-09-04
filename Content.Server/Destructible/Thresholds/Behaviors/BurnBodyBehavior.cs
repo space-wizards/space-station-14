01c9adbda050550e9f252585a1a728e34ba95112
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2025-09-04 23:05:21 +0200

    spread items of burned body on burning to ash

--- Content.Server/Destructible/Thresholds/Behaviors/BurnBodyBehavior.cs
+++ Content.Server/Destructible/Thresholds/Behaviors/BurnBodyBehavior.cs
@@ -11,19 +11,34 @@ namespace Content.Server.Destructible.Thresholds.Behaviors;
 [DataDefinition]
 public sealed partial class BurnBodyBehavior : IThresholdBehavior
 {
-
     public void Execute(EntityUid bodyId, DestructibleSystem system, EntityUid? cause = null)
     {
         var transformSystem = system.EntityManager.System<TransformSystem>();
         var inventorySystem = system.EntityManager.System<InventorySystem>();
         var sharedPopupSystem = system.EntityManager.System<SharedPopupSystem>();
+        var physicsSystem = system.EntityManager.System<PhysicsSystem>();
+        var random = system.Random;
 
-        if (system.EntityManager.TryGetComponent<InventoryComponent>(bodyId, out var comp))
+        const float maxWornItemThrowSpeed = 40.0f;
+
+        foreach (var item in inventorySystem.GetHandOrInventoryEntities(bodyId))
         {
-            foreach (var item in inventorySystem.GetHandOrInventoryEntities(bodyId))
+            if (!inventorySystem.TryGetContainingSlot(item, out var itemSlot))
             {
-                transformSystem.DropNextTo(item, bodyId);
+                continue;
             }
+
+            if (!inventorySystem.TryUnequip(bodyId, bodyId, itemSlot.Name, force: true))
+            {
+                continue;
+            }
+
+            var throwDirection = random.NextAngle().ToVec();
+            var throwSpeed = random.NextFloat() * maxWornItemThrowSpeed;
+            var throwRotationSpeed = random.NextFloat() * maxWornItemThrowSpeed / 10.0f;
+
+            physicsSystem.ApplyLinearImpulse(item, throwDirection * throwSpeed);
+            physicsSystem.ApplyAngularImpulse(item, throwRotationSpeed);
         }
 
         var bodyIdentity = Identity.Entity(bodyId, system.EntityManager);
