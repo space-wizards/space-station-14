e8cfc135f4760d702e07a7100e1a7b8d7a2f358e
Sir Warock <67167466+SirWarock@users.noreply.github.com>
2025-09-05 12:11:54 +0200

    Add StabilizingComponent & System

---
+++ Content.Shared/Bed/Components/StabilizeOnBuckleComponent.cs
@@ -0,0 +1,24 @@
+﻿namespace Content.Shared.Bed.Components
+{
+    /// <summary>
+    ///     Entities in the critical state strapped to this bed will receive stabilizing effects.
+    /// </summary>
+    [RegisterComponent]
+    public sealed partial class StabilizeOnBuckleComponent : Component
+    {
+        /// <summary>
+        ///     How much Asphyxiation and Bloodloss are prevented in the critical state.
+        ///     1 = All damage prevented, 0 = no stabilization.
+        /// </summary>
+        [DataField(required: true)]
+        public float Efficiency;
+
+        /// <summary>
+        ///     How much bleeding it stops.
+        /// </summary>
+        [DataField]
+        public float ReducesBleeding = 0f;
+
+        [DataField] public EntityUid? SleepAction;
+    }
+}
---
+++ Content.Shared/Bed/Systems/SharedStabilizeOnBuckleSystem.cs
@@ -0,0 +1,53 @@
+﻿using Content.Shared.Actions;
+using Content.Shared.Bed.Components;
+using Content.Shared.Bed.Sleep;
+using Content.Shared.Buckle.Components;
+using Content.Shared.Examine;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Bed.Systems
+{
+    public sealed class SharedStabilizeOnBuckleSystem : EntitySystem
+    {
+        [Dependency] private readonly SleepingSystem _sleepingSystem = null!;
+        [Dependency] private readonly SharedActionsSystem _actionsSystem = null!;
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<StabilizeOnBuckleComponent, StrappedEvent>(OnStrapped);
+            SubscribeLocalEvent<StabilizeOnBuckleComponent, UnstrappedEvent>(OnUnstrapped);
+            SubscribeLocalEvent<StabilizeOnBuckleComponent, ExaminedEvent>(OnExamine);
+        }
+
+        private void OnStrapped(Entity<StabilizeOnBuckleComponent> bed, ref StrappedEvent args)
+        {
+            _actionsSystem.AddAction(args.Buckle, ref bed.Comp.SleepAction, SleepingSystem.SleepActionId, bed);
+            CopyComp(args.Strap.Owner, args.Buckle.Owner, bed.Comp);
+            // Single action entity, cannot strap multiple entities to the same rollerbed.
+            DebugTools.AssertEqual(args.Strap.Comp.BuckledEntities.Count, 1);
+        }
+
+        private void OnUnstrapped(Entity<StabilizeOnBuckleComponent> bed, ref UnstrappedEvent args)
+        {
+            _actionsSystem.RemoveAction(args.Buckle.Owner, bed.Comp.SleepAction);
+            _sleepingSystem.TryWaking(args.Buckle.Owner);
+            RemComp<StabilizeOnBuckleComponent>(args.Buckle.Owner);
+        }
+
+        private void OnExamine(Entity<StabilizeOnBuckleComponent> ent, ref ExaminedEvent args)
+        {
+            if (!HasComp<StrapComponent>(ent))
+            {
+                return;
+            }
+            using (args.PushGroup(nameof(StabilizeOnBuckleComponent)))
+            {
+                var comp = ent.Comp;
+                var value = MathF.Round((comp.Efficiency) * 100, 1);
+                args.PushMarkup(Loc.GetString("stabilizing-efficiency-value", ("value", value)));
+                if (comp.ReducesBleeding > 0)
+                    args.PushMarkup(Loc.GetString("stabilizing-bleeding"));
+            }
+        }
+    }
+}
