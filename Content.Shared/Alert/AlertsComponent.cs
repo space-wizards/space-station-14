fe7c7251c530974337f3cc6c99038c3bede7176b
Pok245 <113675512+Pok27@users.noreply.github.com>
2025-09-06 19:15:30 +0300

    relay-alerts

--- Content.Shared/Alert/AlertsComponent.cs
+++ Content.Shared/Alert/AlertsComponent.cs
@@ -18,6 +18,30 @@ public sealed partial class AlertsComponent : Component
     public override bool SendOnlyToOwner => true;
 }
 
+/// <summary>
+/// When present on a controlled entity, indicates that its HUD should display alerts
+/// of another source entity (e.g., the pilot while controlling a vehicle), and clicks should
+/// be proxied back to that source.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class AlertsDisplayRelayComponent : Component
+{
+    public override bool SendOnlyToOwner => true;
+
+    /// <summary>
+    /// The entity whose alerts should be displayed is the local entity.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public EntityUid? Source;
+
+    /// <summary>
+    /// If true and this entity is displaying alerts for <see cref="Source"/>, clicking alerts will activate them
+    /// as if the click originated from <see cref="Source"/>.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool InteractAsSource = false;
+}
+
 [Serializable, NetSerializable]
 public sealed class AlertComponentState : ComponentState
 {
