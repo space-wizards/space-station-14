e927e8a0da7fa6935495bfbc77d5e38519499e59
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-09-06 19:12:44 +0200

    .

--- Content.Shared/Damage/Components/DamageExaminableComponent.cs
+++ Content.Shared/Damage/Components/DamageExaminableComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Damage.Components;
 
 /// <summary>
-/// Shows a detailed examine window with this entity's damage stats when examined..
+/// Shows a detailed examine window with this entity's damage stats when examined.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class DamageExaminableComponent : Component;
