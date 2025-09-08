8ef58a80a467f12c8c2cd9d166accc25498ba3fb
Sir Warock <67167466+SirWarock@users.noreply.github.com>
2025-09-08 09:00:17 +0200

    Clean System

--- Content.Shared/Bed/Systems/SharedStabilizeOnBuckleSystem.cs
+++ Content.Shared/Bed/Systems/SharedStabilizeOnBuckleSystem.cs
@@ -1,16 +1,11 @@
-﻿using Content.Shared.Actions;
-using Content.Shared.Bed.Components;
-using Content.Shared.Bed.Sleep;
+﻿using Content.Shared.Bed.Components;
 using Content.Shared.Buckle.Components;
 using Content.Shared.Examine;
-using Robust.Shared.Utility;
 
 namespace Content.Shared.Bed.Systems
 {
     public sealed class SharedStabilizeOnBuckleSystem : EntitySystem
     {
-        [Dependency] private readonly SleepingSystem _sleepingSystem = null!;
-        [Dependency] private readonly SharedActionsSystem _actionsSystem = null!;
         public override void Initialize()
         {
             base.Initialize();
@@ -21,16 +16,11 @@ namespace Content.Shared.Bed.Systems
 
         private void OnStrapped(Entity<StabilizeOnBuckleComponent> bed, ref StrappedEvent args)
         {
-            _actionsSystem.AddAction(args.Buckle, ref bed.Comp.SleepAction, SleepingSystem.SleepActionId, bed);
             CopyComp(args.Strap.Owner, args.Buckle.Owner, bed.Comp);
-            // Single action entity, cannot strap multiple entities to the same rollerbed.
-            DebugTools.AssertEqual(args.Strap.Comp.BuckledEntities.Count, 1);
         }
 
         private void OnUnstrapped(Entity<StabilizeOnBuckleComponent> bed, ref UnstrappedEvent args)
         {
-            _actionsSystem.RemoveAction(args.Buckle.Owner, bed.Comp.SleepAction);
-            _sleepingSystem.TryWaking(args.Buckle.Owner);
             RemComp<StabilizeOnBuckleComponent>(args.Buckle.Owner);
         }
 
