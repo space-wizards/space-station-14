6cb2693754b3bca19a66c0af0849b495991b11f0
Rinary1 <rinary.super@gmail.com>
2025-09-08 07:33:05 +0300

    add comments + blacklist/whitelist

--- Content.Shared/_Starlight/Chemistry/Components/InjectOnCollideComponent.cs
+++ Content.Shared/_Starlight/Chemistry/Components/InjectOnCollideComponent.cs
@@ -6,12 +6,28 @@ namespace Content.Shared._Starlight.Chemistry.Components;
 [RegisterComponent]
 public sealed partial class InjectOnCollideComponent : Component
 {
+    /// <summary>
+    /// Reagent(s) to inject on collision.
+    /// </summary>
     [DataField("reagents")]
     public List<ReagentQuantity> Reagents;
-    
+
+    /// <summary>
+    /// Limit of reagents that can be injected. When null, no limit is applied.
+    /// </summary>
     [DataField("limit")]
     public float? ReagentLimit;
 
-    [DataField("sound")]
-    public SoundSpecifier? Sound;
+    /// <summary>
+    /// A blacklist of entities that should be ignored by this component's speed modifiers.
+    /// </summary>
+    [DataField]
+    public EntityWhitelist? Blacklist;
+    
+    
+    /// <summary>
+    /// A whitelist of entities that should be targeted by this component's speed modifiers.
+    /// </summary>
+    [DataField]
+    public EntityWhitelist? Whitelist;
 }
--- Content.Shared/_Starlight/Chemistry/SharedOnCollideSystem.cs
+++ Content.Shared/_Starlight/Chemistry/SharedOnCollideSystem.cs
@@ -1,9 +1,10 @@
 using Content.Shared.Chemistry;
 using Content.Shared.Chemistry.EntitySystems;
 using Content.Shared._Starlight.Chemistry.Components;
-using Robust.Shared.Physics.Events;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.FixedPoint;
+using Content.Shared.Whitelist;
+using Robust.Shared.Physics.Events;
 
 namespace Content.Shared._Starlight.Chemistry;
 
@@ -11,6 +12,8 @@ public sealed class SharedOnCollideSystem : EntitySystem
 {
     [Dependency] protected readonly ReactiveSystem _reactiveSystem = default!;
     [Dependency] protected readonly SharedSolutionContainerSystem _solutionContainers = default!;
+    [Dependency] private readonly EntityWhitelistSystem _whitelistSystem = default!;
+
     public override void Initialize()
     {
         SubscribeLocalEvent<InjectOnCollideComponent, StartCollideEvent>(OnCollide);
@@ -20,6 +23,9 @@ public sealed class SharedOnCollideSystem : EntitySystem
     private void OnCollide(EntityUid uid, InjectOnCollideComponent component, ref StartCollideEvent args)
     {
         var target = args.OtherEntity;
+        if (!_whitelistSystem.CheckBoth(target, component.Blacklist, component.Whitelist))
+            return;
+
         if (_solutionContainers.TryGetInjectableSolution(target, out var targetSoln, out _))
         {
             var solution = new Solution(component.Reagents);
