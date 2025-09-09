86a81420ae136bcf58c6a67b49480b438b666176
Sir Warock <67167466+SirWarock@users.noreply.github.com>
2025-09-09 08:06:24 +0200

    Scope Filename

--- Content.Shared/Bed/Components/StabilizeOnBuckleComponent.cs
+++ Content.Shared/Bed/Components/StabilizeOnBuckleComponent.cs
@@ -1,22 +1,22 @@
-﻿namespace Content.Shared.Bed.Components
+﻿namespace Content.Shared.Bed.Components;
+
+/// <summary>
+///     Entities in the critical state strapped to this bed will receive stabilizing effects.
+/// </summary>
+[RegisterComponent]
+public sealed partial class StabilizeOnBuckleComponent : Component
 {
     /// <summary>
-    ///     Entities in the critical state strapped to this bed will receive stabilizing effects.
+    ///     How much Asphyxiation and Bloodloss are prevented in the critical state.
+    ///     1 = All damage prevented, 0 = no stabilization.
     /// </summary>
-    [RegisterComponent]
-    public sealed partial class StabilizeOnBuckleComponent : Component
-    {
-        /// <summary>
-        ///     How much Asphyxiation and Bloodloss are prevented in the critical state.
-        ///     1 = All damage prevented, 0 = no stabilization.
-        /// </summary>
-        [DataField(required: true)]
-        public float Efficiency;
+    [DataField(required: true)]
+    public float Efficiency;
 
-        /// <summary>
-        ///     How much bleeding it stops.
-        /// </summary>
-        [DataField]
-        public float ReducesBleeding = 0f;
-    }
+    /// <summary>
+    ///     How much bleeding it stops.
+    /// </summary>
+    [DataField]
+    public float ReducesBleeding = 0f;
 }
+
