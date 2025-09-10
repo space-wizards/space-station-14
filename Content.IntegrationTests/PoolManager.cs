abc2a6133dfe616d2efed8e5c7c1d261caed3d24
ElectroJr <leonsfriedrich@gmail.com>
2025-09-10 20:34:08 +1200

    Readd cvars partial class

--- Content.IntegrationTests/PoolManager.cs
+++ Content.IntegrationTests/PoolManager.cs
@@ -8,40 +8,10 @@ using Robust.UnitTesting;
 namespace Content.IntegrationTests;
 
 // The static class exist to avoid breaking changes
-public static class PoolManager
+public static partial class PoolManager
 {
     public static readonly ContentPoolManager Instance = new();
     public const string TestMap = "Empty";
-
-    public static readonly (string cvar, string value)[] TestCvars =
-    {
-        // @formatter:off
-        (CCVars.DatabaseSynchronous.Name,     "true"),
-        (CCVars.DatabaseSqliteDelay.Name,     "0"),
-        (CCVars.HolidaysEnabled.Name,         "false"),
-        (CCVars.GameMap.Name,                 TestMap),
-        (CCVars.AdminLogsQueueSendDelay.Name, "0"),
-        (CCVars.NPCMaxUpdates.Name,           "999999"),
-        (CCVars.GameRoleTimers.Name,          "false"),
-        (CCVars.GameRoleLoadoutTimers.Name,   "false"),
-        (CCVars.GameRoleWhitelist.Name,       "false"),
-        (CCVars.GridFill.Name,                "false"),
-        (CCVars.PreloadGrids.Name,            "false"),
-        (CCVars.ArrivalsShuttles.Name,        "false"),
-        (CCVars.EmergencyShuttleEnabled.Name, "false"),
-        (CCVars.ProcgenPreload.Name,          "false"),
-        (CCVars.WorldgenEnabled.Name,         "false"),
-        (CCVars.GatewayGeneratorEnabled.Name, "false"),
-        (CCVars.GameDummyTicker.Name,         "true"),
-        (CCVars.GameLobbyEnabled.Name,        "false"),
-        (CCVars.ConfigPresetDevelopment.Name, "false"),
-        (CCVars.AdminLogsEnabled.Name,        "false"),
-        (CCVars.AutosaveEnabled.Name,         "false"),
-        (CCVars.MovementMobPushing.Name,      "false"),
-        (CCVars.InteractionRateLimitCount.Name, "9999999"),
-        (CCVars.InteractionRateLimitPeriod.Name, "0.1"),
-        (CCVars.MovementMobPushing.Name, "false"),
-    };
 
     /// <summary>
     /// Runs a server, or a client until a condition is true
