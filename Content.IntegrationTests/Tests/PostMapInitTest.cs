2882fd289f502b9f0b394ee067766191bdc3bf0c
IProduceWidgets <107586145+IProduceWidgets@users.noreply.github.com>
2025-09-10 21:01:28 -0400

    Add admin shuttles (#32139)
    * dis da shuttles tho
    
    * power checking
    
    * rerun tests
    
    * purge invalids
    
    * attributions
    
    * Update shuttle save files for new serialization.
    
    * get regexed nerd
    
    * fix shuttle yml guh
    
    * Kill actions

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -73,6 +73,7 @@ namespace Content.IntegrationTests.Tests
         private static readonly string[] DoNotMapWhitelist =
         {
             "/Maps/centcomm.yml",
+            "/Maps/Shuttles/AdminSpawn/**" // admin gaming
         };
 
         /// <summary>
