604bbff64b15e6c78df914bb0e68d1a5b73c7494
Arkanic <50847107+Arkanic@users.noreply.github.com>
2025-09-13 16:04:59 +1200

    unequip, proper checks

--- Content.Shared/_Starlight/Inventory/SwitchableEquipModeSystem.cs
+++ Content.Shared/_Starlight/Inventory/SwitchableEquipModeSystem.cs
@@ -1,13 +1,21 @@
 using Content.Shared.Storage;
 using Content.Shared.Storage.EntitySystems;
+using Content.Shared.Inventory;
+using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Verbs;
 using Robust.Shared.Utility;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.GameObjects;
 
 namespace Content.Shared._Starlight.Inventory;
 
 public sealed class SwitchableEquipModeSystem : EntitySystem
 {
-    [Dependency] private readonly SharedStorageSystem _sharedStorage = default!;
+    [Dependency] private readonly SharedStorageSystem _storage = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
+    [Dependency] private readonly SharedHandsSystem _hands = default!;
+    [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
 
     private readonly Dictionary<EquipMode, string> _equipModeLocaleMapping = new() {
         { EquipMode.Remove, "equipmode-mode-remove" },
@@ -19,39 +27,72 @@ public sealed class SwitchableEquipModeSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<SwitchableEquipModeComponent, InventoryUseSlotEvent>(OnInventoryUseSlot);
-        SubscribeLocalEvent<SwitchableEquipModeComponent, GetVerbsEvent<InteractionVerb>>(OnGetVerbs);
+        SubscribeLocalEvent<SwitchableEquipModeComponent, GetVerbsEvent<ActivationVerb>>(OnGetVerbs);
     }
 
-    private void OnInventoryUseSlot(Entity<SwitchableEquipModeComponent> ent, ref InventoryUseSlotEvent args)
+    private void OnInventoryUseSlot(EntityUid uid, SwitchableEquipModeComponent switchModeComp, ref InventoryUseSlotEvent args)
     {
-        var comp = ent.Comp;
+        if (!TryComp<StorageComponent>(uid, out var storageComp)) return;
 
-        switch (comp.Mode)
+        switch (switchModeComp.Mode)
         {
             case EquipMode.Remove:
                 return; // pass to next
 
             case EquipMode.Open:
-                _sharedStorage.OpenStorageUI(args.Target, args.Actor);
                 args.Handled = true;
+                if (_ui.IsUiOpen(uid, StorageComponent.StorageUiKey.Key, args.Actor)) return; // stop sound spam
+
+                _storage.OpenStorageUI(args.Target, args.Actor);
+                _audio.PlayPredicted(storageComp.StorageOpenSound, uid, args.Actor);
+
                 break;
         }
     }
 
-    private void OnGetVerbs(EntityUid uid, SwitchableEquipModeComponent switchModeComp, ref GetVerbsEvent<InteractionVerb> args)
+    private void OnGetVerbs(EntityUid uid, SwitchableEquipModeComponent switchModeComp, ref GetVerbsEvent<ActivationVerb> args)
     {
         if (!args.CanInteract) return;
-        if (!TryComp<StorageComponent>(uid, out var storageComp)) return; // not a bag
+        if (!TryComp<StorageComponent>(uid, out var storageComp)) return;
 
         var modeCount = Enum.GetNames<EquipMode>().Length;
         EquipMode nextMode = (EquipMode)(((int)switchModeComp.Mode + 1) % modeCount);
 
-        InteractionVerb switchVerb = new()
+        ActivationVerb switchVerb = new()
         {
             Text = Loc.GetString("equipmode-switch", ("type", Loc.GetString(new(_equipModeLocaleMapping[nextMode])))),
-            Icon = new SpriteSpecifier.Texture(new("/Textures/Interface/VerbIcons/eject.svg.192dpi.png")),
+            Icon = new SpriteSpecifier.Texture(new("/Textures/Interface/VerbIcons/outfit.svg.192dpi.png")),
+            Priority = -1,
             Act = () => switchModeComp.Mode = nextMode
         };
         args.Verbs.Add(switchVerb);
+
+        // misc verbs
+
+        EntityUid user = args.User;
+
+        ActivationVerb unequipVerb = new()
+        {
+            Text = Loc.GetString("equipmode-remove"),
+            Icon = new SpriteSpecifier.Texture(new("/Textures/Interface/VerbIcons/eject.svg.192dpi.png")),
+            Priority = 0,
+            Act = () =>
+            {
+                if (!_inventory.TryGetContainingSlot(uid, out var slot)) return;
+                _inventory.TryUnequip(user, slot.Name, true, true);
+                _hands.TryPickup(user, uid);
+            }
+        };
+
+        switch (switchModeComp.Mode)
+        {
+            // we want an unequip option if the default click option isn't to remove
+            case EquipMode.Open:
+                args.Verbs.Add(unequipVerb);
+                break;
+
+            default:
+                break;
+        };
     }
 }
--- Resources/Locale/en-US/_Starlight/inventory/equipmode.ftl
+++ Resources/Locale/en-US/_Starlight/inventory/equipmode.ftl
@@ -1,6 +1,6 @@
-equipmode-switch = Switch interaction to {$type} on interact
+equipmode-switch = Switch to {$type} on interact
 
 equipmode-mode-remove = unequip
 equipmode-mode-open = open
 
-equipmode-unequip = Unequip
+equipmode-remove = Unequip
