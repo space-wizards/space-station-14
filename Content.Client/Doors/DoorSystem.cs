ac4cdd8030ec539ef83dca5d619603d90435de1b
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2025-09-14 14:29:07 +0300

    Comment

--- Content.Client/Doors/DoorSystem.cs
+++ Content.Client/Doors/DoorSystem.cs
@@ -92,7 +92,7 @@ public sealed class DoorSystem : SharedDoorSystem
         if (_animationSystem.HasRunningAnimation(entity, DoorComponent.AnimationKey))
             _animationSystem.Stop(entity.Owner, DoorComponent.AnimationKey);
 
-        // We are checking beforehand since some doors may not have an emagging animation layer, and we don't want LayerSetVisible to throw an error.
+        // We are checking beforehand since some doors may not have an emagging visual layer, and we don't want LayerSetVisible to throw an error.
         if (_sprite.TryGetLayer(entity.Owner, DoorVisualLayers.BaseEmagging, out var _, false))
             _sprite.LayerSetVisible(entity.Owner, DoorVisualLayers.BaseEmagging, state == DoorState.Emagging);
 
@@ -138,7 +138,7 @@ public sealed class DoorSystem : SharedDoorSystem
 
                 return;
             case DoorState.Emagging:
-                // We are checking beforehand since some doors may not have an emagging animation layer.
+                // We are checking beforehand since some doors may not have an emagging visual layer.
                 if (_sprite.TryGetLayer(entity.Owner, DoorVisualLayers.BaseEmagging, out var _, false))
                     _animationSystem.Play(entity, (Animation)entity.Comp.EmaggingAnimation, DoorComponent.AnimationKey);
 
