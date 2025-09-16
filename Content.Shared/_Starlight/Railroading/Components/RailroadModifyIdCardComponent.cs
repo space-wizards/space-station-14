d2889e8ad461bb211f1b54138992bfd661b7b3af
Arkanic <50847107+Arkanic@users.noreply.github.com>
2025-09-17 09:18:43 +1200

    access add/remove

--- Content.Shared/_Starlight/Railroading/Components/RailroadModifyIdCardComponent.cs
+++ Content.Shared/_Starlight/Railroading/Components/RailroadModifyIdCardComponent.cs
@@ -1,4 +1,5 @@
 using Content.Shared.StatusIcon;
+using Content.Shared.Access;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Starlight.Railroading;
@@ -15,4 +16,18 @@ public sealed partial class RailroadModifyIdCardComponent : Component
     [DataField]
     public string? Name;
 
+    /// <summary>
+    /// List of accesses to add to the ID card if not already held
+    /// </summary>
+    [DataField]
+    public IReadOnlyCollection<ProtoId<AccessLevelPrototype>> AccessAdd = Array.Empty<ProtoId<AccessLevelPrototype>>();
+
+    /// <summary>
+    /// List of accesses to remove from the ID card if they are currently held
+    /// </summary>
+    [DataField]
+    public IReadOnlyCollection<ProtoId<AccessLevelPrototype>> AccessRemove = Array.Empty<ProtoId<AccessLevelPrototype>>();
+
+    [DataField]
+    public string DefaultIdPrototypeIfNoneFound = "AssistantIdCard";
 }
