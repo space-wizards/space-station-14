1ed808e0f66327870ae553fb75919817491886b5
kretschs <77357653+SurrealShibe@users.noreply.github.com>
2025-09-18 21:41:17 -0700

    revert code, remove DrainableSolution from towel

--- Content.Shared/Nutrition/EntitySystems/IngestionSystem.Blockers.cs
+++ Content.Shared/Nutrition/EntitySystems/IngestionSystem.Blockers.cs
@@ -139,12 +139,6 @@ public sealed partial class IngestionSystem
     /// </remarks>
     private void OnDrainableIsDigestible(Entity<DrainableSolutionComponent> ent, ref IsDigestibleEvent args)
     {
-        // Check for EdibleComponent or FoodComponent. Do not make universally digestible if special digestion is required.
-        if (TryComp<EdibleComponent>(ent.Owner, out var edibleComp) && edibleComp.RequiresSpecialDigestion)
-            return;
-        if (TryComp<FoodComponent>(ent.Owner, out var foodComp) && foodComp.RequiresSpecialDigestion)
-            return;
-
         args.UniversalDigestion();
     }
 
