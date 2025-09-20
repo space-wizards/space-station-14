312733609bffd777e35838f8671340a479b2428a
AndrewFenriz <78079974+AndrewFenriz@users.noreply.github.com>
2025-09-20 14:10:51 +0300

    Правка режима Блоба: Требование онлайна (в целом требования режима) (#401)

--- Content.Shared/Backmen/CCVar/CCVars.cs
+++ Content.Shared/Backmen/CCVar/CCVars.cs
@@ -7,17 +7,6 @@ namespace Content.Shared.Backmen.CCVar;
 [CVarDefs]
 public sealed class CCVars
 {
-    /*
-     * Blob
-     */
-
-    public static readonly CVarDef<int> BlobMax =
-        CVarDef.Create("blob.max", 3, CVar.SERVERONLY);
-
-    public static readonly CVarDef<int> BlobPlayersPer =
-        CVarDef.Create("blob.players_per", 20, CVar.SERVERONLY);
-
-
     public static readonly CVarDef<bool>
         EconomyWagesEnabled = CVarDef.Create("economy.wages_enabled", true, CVar.SERVERONLY);
 }
