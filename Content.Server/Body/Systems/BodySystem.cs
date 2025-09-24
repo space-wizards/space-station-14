6b7e1d72405f25f6adb97c92b337ae48307f4bbf
Thinbug <ringingsince1997@gmail.com>
2025-09-24 16:07:58 -0300

    small bit of progress yknow may as well yeah

--- Content.Server/Body/Systems/BodySystem.cs
+++ Content.Server/Body/Systems/BodySystem.cs
@@ -1,7 +1,5 @@
 using System.Numerics;
 using Content.Server.Ghost;
-using Content.Server.GhostTypes;
-using Content.Server.Body.Components;
 using Content.Server.Humanoid;
 using Content.Shared.Body.Components;
 using Content.Shared.Body.Events;
@@ -106,9 +104,6 @@ public sealed class BodySystem : SharedBodySystem
         SoundSpecifier? gibSoundOverride = null
     )
     {
-        //var beforeEvent = new BeforeBodyDestructionEvent();
-        //RaiseLocalEvent(bodyId, ref beforeEvent);
-
         if (!Resolve(bodyId, ref body, logMissing: false)
             || TerminatingOrDeleted(bodyId)
             || EntityManager.IsQueuedForDeletion(bodyId))
