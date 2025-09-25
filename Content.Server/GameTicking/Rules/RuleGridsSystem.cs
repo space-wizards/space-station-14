e963d0af6765ba5f5300d8ea0d93a5476fd7b4ff
Samuel <samuelcamilo8@gmail.com>
2025-09-24 21:03:10 -0300

    not needed anymore

--- Content.Server/GameTicking/Rules/RuleGridsSystem.cs
+++ Content.Server/GameTicking/Rules/RuleGridsSystem.cs
@@ -54,7 +54,7 @@ public sealed class RuleGridsSystem : GameRuleSystem<RuleGridsComponent>
     private void OnSelectLocation(Entity<RuleGridsComponent> ent, ref AntagSelectLocationEvent args)
     {
         var query = EntityQueryEnumerator<SpawnPointComponent, TransformComponent>();
-        while (query.MoveNext(out var uid, out var spawnPointComp, out var xform))
+        while (query.MoveNext(out var uid, out _, out var xform))
         {
             if (xform.MapID != ent.Comp.Map)
                 continue;
