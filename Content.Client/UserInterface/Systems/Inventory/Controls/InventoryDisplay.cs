0d7b1ce924044b1f55a2fd9101d91f514fd129df
David <david.owen.dev@gmail.com>
2025-09-26 11:32:25 -0600

    Reverted changes for Inventory Display

--- Content.Client/UserInterface/Systems/Inventory/Controls/InventoryDisplay.cs
+++ Content.Client/UserInterface/Systems/Inventory/Controls/InventoryDisplay.cs
@@ -7,10 +7,6 @@ namespace Content.Client.UserInterface.Systems.Inventory.Controls;
 
 public sealed class InventoryDisplay : LayoutContainer
 {
-    [Dependency] private readonly ILogManager _logManager = default!;
-
-    private readonly ISawmill _sawmill = default!;
-
     private int Columns = 0;
     private int Rows = 0;
     private const int MarginThickness = 10;
@@ -22,8 +18,6 @@ public sealed class InventoryDisplay : LayoutContainer
 
     public InventoryDisplay()
     {
-        _sawmill = _logManager.GetSawmill("inventory.display");
-
         resizer = new Control();
         AddChild(resizer);
     }
@@ -35,7 +29,7 @@ public sealed class InventoryDisplay : LayoutContainer
         VerticalExpand = true;
         InheritChildMeasure = true;
         if (!_buttons.TryAdd(newButton.SlotName, (newButton, buttonOffset)))
-            _sawmill.Warning("Tried to add button without a slot!");
+            Logger.Warning("Tried to add button without a slot!");
         SetPosition(newButton, buttonOffset * ButtonSize + new Vector2(ButtonSpacing, ButtonSpacing));
         UpdateSizeData(buttonOffset);
         return newButton;
