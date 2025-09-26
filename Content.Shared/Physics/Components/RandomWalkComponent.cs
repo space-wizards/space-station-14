42ddf82fd2e40c0cc9615bbd3e045a1e4ac41a2e
Avery Dobbins <avery.dobbins@gmail.com>
2025-09-26 16:40:47 -0400

    oops unyoink DataFields

--- Content.Shared/Physics/Components/RandomWalkComponent.cs
+++ Content.Shared/Physics/Components/RandomWalkComponent.cs
@@ -11,33 +11,37 @@ public sealed partial class RandomWalkComponent : Component
     /// <summary>
     /// The minimum speed at which this entity will move.
     /// </summary>
+    [DataField]
     public float MinSpeed = 7.5f;
 
     /// <summary>
     /// The maximum speed at which this entity will move.
     /// </summary>
+    [DataField]
     public float MaxSpeed = 10f;
 
     /// <summary>
     /// The amount of speed carried over when the speed updates.
     /// </summary>
+    [DataField]
     public float AccumulatorRatio = 0.0f;
 
     /// <summary>
     /// The vector by which the random walk direction is biased.
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public Vector2 BiasVector = new Vector2(0f, 0f);
 
     /// <summary>
     /// Whether to set BiasVector to (0, 0) every random walk update.
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public bool ResetBiasOnWalk = true;
 
     /// <summary>
     /// Whether this random walker should take a step immediately when it starts up.
     /// </summary>
+    [DataField]
     public bool StepOnStartup = false;
 
     #region Update Timing
@@ -45,11 +49,13 @@ public sealed partial class RandomWalkComponent : Component
     /// <summary>
     /// The minimum amount of time between speed updates.
     /// </summary>
+    [DataField]
     public TimeSpan MinStepCooldown { get; internal set; } = TimeSpan.FromSeconds(2.0);
 
     /// <summary>
     /// The maximum amount of time between speed updates.
     /// </summary>
+    [DataField]
     public TimeSpan MaxStepCooldown { get; internal set; } = TimeSpan.FromSeconds(5.0);
 
     /// <summary>
