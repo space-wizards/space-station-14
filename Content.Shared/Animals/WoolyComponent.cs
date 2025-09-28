3a1354aeb05b13d6a38139a50b2ff9fd036409ba
Baa14453 <git_78nfp05yca8p0nw7at849pbahfc56w39p@protonmail.com>
2025-09-28 20:17:29 +0100

    One more change removed.

--- Content.Shared/Animals/WoolyComponent.cs
+++ Content.Shared/Animals/WoolyComponent.cs
@@ -3,7 +3,6 @@ using Content.Shared.Chemistry.Reagent;
 using Content.Shared.FixedPoint;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Shared.Animals;
 
