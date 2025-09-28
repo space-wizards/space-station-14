22d981fc677fce167e50ebd045c2040cf71a7964
CrazyPhantom779 <izayussedwards@gmail.com>
2025-09-28 16:29:14 +1000

    Starlight Comments

--- Content.Client/Access/UI/IdCardConsoleBoundUserInterface.cs
+++ Content.Client/Access/UI/IdCardConsoleBoundUserInterface.cs
@@ -31,7 +31,7 @@ namespace Content.Client.Access.UI
         {
             _idCardConsoleSystem = EntMan.System<SharedIdCardConsoleSystem>();
 
-            _maxNameLength = _cfgManager.GetCVar(CCVars.MaxNameLength);
+            _maxNameLength =_cfgManager.GetCVar(CCVars.MaxNameLength);
             _maxIdJobLength = _cfgManager.GetCVar(CCVars.MaxIdJobLength);
         }
 
@@ -63,7 +63,7 @@ namespace Content.Client.Access.UI
         protected override void UpdateState(BoundUserInterfaceState state)
         {
             base.UpdateState(state);
-            var castState = (IdCardConsoleBoundUserInterfaceState)state;
+            var castState = (IdCardConsoleBoundUserInterfaceState) state;
             _window?.UpdateState(castState);
         }
 
