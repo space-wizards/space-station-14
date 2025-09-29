b6472ce5f4b3a6906f2cd4d407c39e460a51c90c
Kowlin <git@wyvern.blue>
2025-09-29 03:11:48 +0200

    Update butchering with some more conditionals

--- Content.Shared/Kitchen/SharedKitchenSpikeSystem.cs
+++ Content.Shared/Kitchen/SharedKitchenSpikeSystem.cs
@@ -7,6 +7,7 @@ using Content.Shared.DoAfter;
 using Content.Shared.DragDrop;
 using Content.Shared.Examine;
 using Content.Shared.Hands;
+using Content.Shared.Humanoid;
 using Content.Shared.IdentityManagement;
 using Content.Shared.Interaction;
 using Content.Shared.Interaction.Events;
@@ -238,8 +239,15 @@ public sealed class SharedKitchenSpikeSystem : EntitySystem
                 args.Target.Value,
                 ent);
 
+            // normally medium severity, but for humanoids high severity, so new players get relay'd to admin alerts.
+            var logSeverity = LogImpact.Medium;
+            if (HasComp<HumanoidAppearanceComponent>(args.Target))
+            {
+                logSeverity = LogImpact.High;
+            }
+
             _logger.Add(LogType.Action,
-                LogImpact.High,
+                logSeverity,
                 $"{ToPrettyString(args.User):user} put {ToPrettyString(args.Target):target} on the {ToPrettyString(ent):spike}");
 
             _audioSystem.PlayPredicted(ent.Comp.SpikeSound, ent, args.User);
@@ -316,8 +324,14 @@ public sealed class SharedKitchenSpikeSystem : EntitySystem
         {
             _bodySystem.GibBody(args.Target.Value, true);
 
+            var logSeverity = LogImpact.High;
+            if (HasComp<HumanoidAppearanceComponent>(args.Target))
+            {
+                logSeverity = LogImpact.Extreme;
+            }
+
             _logger.Add(LogType.Gib,
-                LogImpact.Extreme,
+                logSeverity,
                 $"{ToPrettyString(args.User):user} finished butchering {ToPrettyString(args.Target):target} on the {ToPrettyString(ent):spike}");
         }
         else
@@ -325,8 +339,10 @@ public sealed class SharedKitchenSpikeSystem : EntitySystem
             EnsureComp<KitchenSpikeVictimComponent>(args.Target.Value);
 
             _damageableSystem.TryChangeDamage(args.Target, ent.Comp.ButcherDamage, true);
+
+            // Log severity for damaging other entities is normally medium.
             _logger.Add(LogType.Action,
-                LogImpact.High,
+                LogImpact.Medium,
                 $"{ToPrettyString(args.User):user} butchered {ToPrettyString(args.Target):target} on the {ToPrettyString(ent):spike}");
         }
 
