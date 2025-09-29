97f4c5e4ddd253cc3258a609820f6a3e3482903d
David <david.owen.dev@gmail.com>
2025-09-29 17:06:14 -0600

    Added comments to crayoncomponent

--- Content.Server/Crayon/CrayonComponent.cs
+++ Content.Server/Crayon/CrayonComponent.cs
@@ -6,17 +6,32 @@ namespace Content.Server.Crayon
     [RegisterComponent]
     public sealed partial class CrayonComponent : SharedCrayonComponent
     {
+        /// <summary>
+        /// Play a sound when using if specified
+        /// </summary>
         [DataField]
         public SoundSpecifier? UseSound;
 
+        /// <summary>
+        /// Is the color valid for use
+        /// </summary>
         [DataField]
         public bool SelectableColor;
 
-        public int Charges { get; set; }
+        /// <summary>
+        /// How many crayon usages are left
+        /// </summary>
+        public int Charges;
 
+        /// <summary>
+        /// Max number of charges
+        /// </summary>
         [DataField]
-        public int Capacity { get; set; } = 30;
+        public int Capacity = 30;
 
+        /// <summary>
+        /// Should the crayon be deleted when all charges are consumed
+        /// </summary>
         [DataField]
         public bool DeleteEmpty = true;
 
