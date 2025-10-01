8de1caafb254515202aa21d2b96029a1fce24df2
kosticia <kosticia46@gmail.com>
2025-10-01 18:37:07 +0300

    slarti best

--- Content.Server/Anomaly/Effects/ShuffleParticlesAnomalySystem.cs
+++ Content.Server/Anomaly/Effects/ShuffleParticlesAnomalySystem.cs
@@ -4,6 +4,7 @@ using Robust.Shared.Physics.Events;
 using Robust.Shared.Random;
 
 namespace Content.Server.Anomaly.Effects;
+
 public sealed class ShuffleParticlesAnomalySystem : EntitySystem
 {
     [Dependency] private readonly AnomalySystem _anomaly = default!;
@@ -12,7 +13,7 @@ public sealed class ShuffleParticlesAnomalySystem : EntitySystem
     public override void Initialize()
     {
         SubscribeLocalEvent<ShuffleParticlesAnomalyComponent, AnomalyPulseEvent>(OnPulse);
-        SubscribeLocalEvent<ShuffleParticlesAnomalyComponent, StartCollideEvent>(OnStartCollide);
+        SubscribeLocalEvent<ShuffleParticlesAnomalyComponent, StartCollideEvent>(OnStartCollide, after: [typeof(AnomalySystem)]);
     }
 
     private void OnStartCollide(Entity<ShuffleParticlesAnomalyComponent> ent, ref StartCollideEvent args)
