79c6d44cdc42ae23df14b6c593043fae156e0636
Hannah Giovanna Dawson <karakkaraz@gmail.com>
2025-10-01 22:52:26 +0100

    Resolve @ScarKy0 comments

--- Content.Shared/Trigger/Components/Effects/LaunchOnTriggerComponent.cs
+++ Content.Shared/Trigger/Components/Effects/LaunchOnTriggerComponent.cs
@@ -3,12 +3,12 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Trigger.Components.Effects;
 
 /// <summary>
-/// Launches the owner on trigger.
-/// If TargetUser is true, launches the target instead.
+/// Launches the owner of this component when triggered.
+/// If TargetUser is true, this launches the entity that was collided with instead (because the "user" is the thing that's caused the collision, i.e. the other object).
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class LaunchOnTriggerComponent : BaseXOnTriggerComponent
 {
-    [DataField]
-    public float Speed;
+    [DataField, AutoNetworkedField]
+    public float Speed = 10.0f;
 }
