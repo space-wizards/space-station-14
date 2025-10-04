043f5d5a1fde6141149df28e253ccd8228a00eba
Andrew Montagne <andrew@montagne.uk>
2025-10-04 18:37:47 +0100

    Store Fire Alarm's state and restore it when power returns
    Fix for #12841.
    
    Also changes fire alarm so it asserts a "Normal" state when not triggered, so it can work with other fire alarms and air alarms without stomping on them.

--- Content.Server/Atmos/Monitor/Components/FireAlarmComponent.cs
+++ Content.Server/Atmos/Monitor/Components/FireAlarmComponent.cs
@@ -3,4 +3,6 @@ namespace Content.Server.Atmos.Monitor.Components;
 [RegisterComponent]
 public sealed partial class FireAlarmComponent : Component
 {
+    [DataField("alarmTriggered")]
+    public bool AlarmTriggered = false;
 }
