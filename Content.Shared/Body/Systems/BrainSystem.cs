5fdab9a14cdd0acb380edd9a9b518141e5edee79
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-10-07 14:46:38 +0200

    Update Content.Shared/Body/Systems/BrainSystem.cs

--- Content.Shared/Body/Systems/BrainSystem.cs
+++ Content.Shared/Body/Systems/BrainSystem.cs
@@ -15,7 +15,6 @@ public sealed class BrainSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-        
         SubscribeLocalEvent<BrainComponent, OrganAddedToBodyEvent>((uid, _, args) => HandleMind(args.Body, uid));
         SubscribeLocalEvent<BrainComponent, OrganRemovedFromBodyEvent>((uid, _, args) => HandleMind(uid, args.OldBody));
         SubscribeLocalEvent<BrainComponent, PointAttemptEvent>(OnPointAttempt);
