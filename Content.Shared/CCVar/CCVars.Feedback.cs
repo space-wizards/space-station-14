53c5a9711f192224a9504f26f52ff1d37cedf66e
beck-thompson <beck314159@hotmail.com>
2025-10-08 08:29:39 -0700

    Add better comment

--- Content.Shared/CCVar/CCVars.Feedback.cs
+++ Content.Shared/CCVar/CCVars.Feedback.cs
@@ -5,7 +5,9 @@ namespace Content.Shared.CCVar;
 public sealed partial class CCVars
 {
     /// <summary>
-    /// Used to set what popups are shown. Can accept multiple origins, just use spaces!
+    /// Used to set what popups are shown. Can accept multiple origins, just use spaces! See
+    /// <see cref="Content.Shared.FeedbackSystem.FeedbackPopupPrototype">FeedbackPopupPrototype</see>'s <see cref="Content.Shared.FeedbackSystem.FeedbackPopupPrototype.PopupOrigin">PopupOrigin</see> field.
+    /// Only prototypes who's PopupOrigin matches one of the FeedbackValidOrigins will be shown to players.
     /// </summary>
     /// <example>
     /// wizden deltav
