b2c6be6fea702a045feb148cb27a703da8fbf56f
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-10-09 21:01:14 +0200

    prototype whitelists

--- Content.IntegrationTests/Tests/Utility/EntityWhitelistTest.cs
+++ Content.IntegrationTests/Tests/Utility/EntityWhitelistTest.cs
@@ -1,26 +1,31 @@
 using System.Linq;
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Whitelist;
-using Robust.Shared.GameObjects;
 
 namespace Content.IntegrationTests.Tests.Utility
 {
-    [TestFixture]
     [TestOf(typeof(EntityWhitelist))]
     public sealed class EntityWhitelistTest
     {
         private const string InvalidComponent = "Sprite";
         private const string ValidComponent = "Physics";
 
+        private const string WhitelistDummyId = "WhitelistDummy";
+        private const string ValidComponentDummyId = "ValidComponentDummy";
+        private const string WhitelistTestValidTagDummyId = "WhitelistTestValidTagDummy";
+        private const string InvalidComponentDummyId = "InvalidComponentDummy";
+        private const string WhitelistTestInvalidTagDummyId = "WhitelistTestInvalidTagDummy";
+
         [TestPrototypes]
         private const string Prototypes = $@"
 - type: Tag
   id: WhitelistTestValidTag
+
 - type: Tag
   id: WhitelistTestInvalidTag
 
 - type: entity
-  id: WhitelistDummy
+  id: {WhitelistDummyId}
   components:
   - type: ItemSlots
     slots:
@@ -34,22 +39,24 @@ namespace Content.IntegrationTests.Tests.Utility
           - WhitelistTestValidTag
 
 - type: entity
-  id: InvalidComponentDummy
+  id: {InvalidComponentDummyId}
   components:
   - type: {InvalidComponent}
+
 - type: entity
-  id: WhitelistTestInvalidTagDummy
+  id: {WhitelistTestInvalidTagDummyId}
   components:
   - type: Tag
     tags:
     - WhitelistTestInvalidTag
 
 - type: entity
-  id: ValidComponentDummy
+  id: {ValidComponentDummyId}
   components:
   - type: {ValidComponent}
+
 - type: entity
-  id: WhitelistTestValidTagDummy
+  id: {WhitelistTestValidTagDummyId}
   components:
   - type: Tag
     tags:
@@ -69,11 +76,11 @@ namespace Content.IntegrationTests.Tests.Utility
 
             await server.WaitAssertion(() =>
             {
-                var validComponent = sEntities.SpawnEntity("ValidComponentDummy", mapCoordinates);
-                var WhitelistTestValidTag = sEntities.SpawnEntity("WhitelistTestValidTagDummy", mapCoordinates);
+                var validComponent = sEntities.SpawnEntity(ValidComponentDummyId, mapCoordinates);
+                var whitelistTestValidTag = sEntities.SpawnEntity(WhitelistTestValidTagDummyId, mapCoordinates);
 
-                var invalidComponent = sEntities.SpawnEntity("InvalidComponentDummy", mapCoordinates);
-                var WhitelistTestInvalidTag = sEntities.SpawnEntity("WhitelistTestInvalidTagDummy", mapCoordinates);
+                var invalidComponent = sEntities.SpawnEntity(InvalidComponentDummyId, mapCoordinates);
+                var whitelistTestInvalidTag = sEntities.SpawnEntity(WhitelistTestInvalidTagDummyId, mapCoordinates);
 
                 // Test instantiated on its own
                 var whitelistInst = new EntityWhitelist
@@ -85,14 +92,18 @@ namespace Content.IntegrationTests.Tests.Utility
                 Assert.Multiple(() =>
                 {
                     Assert.That(sys.IsValid(whitelistInst, validComponent), Is.True);
-                    Assert.That(sys.IsValid(whitelistInst, WhitelistTestValidTag), Is.True);
+                    Assert.That(sys.IsValid(whitelistInst, ValidComponentDummyId), Is.True);
+                    Assert.That(sys.IsValid(whitelistInst, whitelistTestValidTag), Is.True);
+                    Assert.That(sys.IsValid(whitelistInst, WhitelistTestValidTagDummyId), Is.True);
 
                     Assert.That(sys.IsValid(whitelistInst, invalidComponent), Is.False);
-                    Assert.That(sys.IsValid(whitelistInst, WhitelistTestInvalidTag), Is.False);
+                    Assert.That(sys.IsValid(whitelistInst, InvalidComponentDummyId), Is.False);
+                    Assert.That(sys.IsValid(whitelistInst, whitelistTestInvalidTag), Is.False);
+                    Assert.That(sys.IsValid(whitelistInst, WhitelistTestInvalidTagDummyId), Is.False);
                 });
 
                 // Test from serialized
-                var dummy = sEntities.SpawnEntity("WhitelistDummy", mapCoordinates);
+                var dummy = sEntities.SpawnEntity(WhitelistDummyId, mapCoordinates);
                 var whitelistSer = sEntities.GetComponent<ItemSlotsComponent>(dummy).Slots.Values.First().Whitelist;
                 Assert.That(whitelistSer, Is.Not.Null);
 
@@ -105,10 +116,14 @@ namespace Content.IntegrationTests.Tests.Utility
                 Assert.Multiple(() =>
                 {
                     Assert.That(sys.IsValid(whitelistSer, validComponent), Is.True);
-                    Assert.That(sys.IsValid(whitelistSer, WhitelistTestValidTag), Is.True);
+                    Assert.That(sys.IsValid(whitelistInst, ValidComponentDummyId), Is.True);
+                    Assert.That(sys.IsValid(whitelistSer, whitelistTestValidTag), Is.True);
+                    Assert.That(sys.IsValid(whitelistInst, WhitelistTestValidTagDummyId), Is.True);
 
                     Assert.That(sys.IsValid(whitelistSer, invalidComponent), Is.False);
-                    Assert.That(sys.IsValid(whitelistSer, WhitelistTestInvalidTag), Is.False);
+                    Assert.That(sys.IsValid(whitelistInst, InvalidComponentDummyId), Is.False);
+                    Assert.That(sys.IsValid(whitelistSer, whitelistTestInvalidTag), Is.False);
+                    Assert.That(sys.IsValid(whitelistInst, WhitelistTestInvalidTagDummyId), Is.False);
                 });
             });
             await pair.CleanReturnAsync();
--- Content.Shared/Whitelist/EntityWhitelistSystem.cs
+++ Content.Shared/Whitelist/EntityWhitelistSystem.cs
@@ -1,11 +1,13 @@
 using System.Diagnostics.CodeAnalysis;
 using Content.Shared.Item;
 using Content.Shared.Tag;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Whitelist;
 
-public sealed class EntityWhitelistSystem : EntitySystem
+public sealed partial class EntityWhitelistSystem : EntitySystem
 {
+    [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly TagSystem _tag = default!;
 
     private EntityQuery<ItemComponent> _itemQuery;
@@ -13,30 +15,17 @@ public sealed class EntityWhitelistSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
+
         _itemQuery = GetEntityQuery<ItemComponent>();
     }
 
-    /// <inheritdoc cref="IsValid(Content.Shared.Whitelist.EntityWhitelist,Robust.Shared.GameObjects.EntityUid)"/>
+    /// <summary>
+    /// Checks whether a given entity satisfies a whitelist.
+    /// Returns false if the entity is null.
+    /// </summary>
     public bool IsValid(EntityWhitelist list, [NotNullWhen(true)] EntityUid? uid)
     {
         return uid != null && IsValid(list, uid.Value);
-    }
-
-    /// <summary>
-    /// Checks whether a given entity is allowed by a whitelist and not blocked by a blacklist.
-    /// If a blacklist is provided and it matches then this returns false.
-    /// If a whitelist is provided and it does not match then this returns false.
-    /// If either list is null it does not get checked.
-    /// </summary>
-    public bool CheckBoth([NotNullWhen(true)] EntityUid? uid, EntityWhitelist? blacklist = null, EntityWhitelist? whitelist = null)
-    {
-        if (uid == null)
-            return false;
-
-        if (blacklist != null && IsValid(blacklist, uid))
-            return false;
-
-        return whitelist == null || IsValid(whitelist, uid);
     }
 
     /// <summary>
@@ -83,13 +72,31 @@ public sealed class EntityWhitelistSystem : EntitySystem
 
         return list.RequireAll;
     }
+
     /// The following are a list of "helper functions" that are basically the same as each other
     /// to help make code that uses EntityWhitelist a bit more readable because at the moment
     /// it is quite clunky having to write out component.Whitelist == null ? true : _whitelist.IsValid(component.Whitelist, uid)
     /// several times in a row and makes comparisons easier to read
 
     /// <summary>
-    /// Helper function to determine if Whitelist is not null and entity is on list
+    /// Checks whether a given entity is allowed by a whitelist and not blocked by a blacklist.
+    /// If a blacklist is provided and it matches then this returns false.
+    /// If a whitelist is provided and it does not match then this returns false.
+    /// If either list is null it does not get checked.
+    /// </summary>
+    public bool CheckBoth([NotNullWhen(true)] EntityUid? uid, EntityWhitelist? blacklist = null, EntityWhitelist? whitelist = null)
+    {
+        if (uid == null)
+            return false;
+
+        if (blacklist != null && IsValid(blacklist, uid))
+            return false;
+
+        return whitelist == null || IsValid(whitelist, uid);
+    }
+
+    /// <summary>
+    /// Helper function to determine if a whitelist is not null and the entity is on list.
     /// </summary>
     public bool IsWhitelistPass(EntityWhitelist? whitelist, EntityUid uid)
     {
@@ -100,7 +107,7 @@ public sealed class EntityWhitelistSystem : EntitySystem
     }
 
     /// <summary>
-    /// Helper function to determine if Whitelist is not null and entity is not on the list
+    /// Helper function to determine if a whitelist is not null and the entity is not on the list.
     /// </summary>
     public bool IsWhitelistFail(EntityWhitelist? whitelist, EntityUid uid)
     {
@@ -111,7 +118,7 @@ public sealed class EntityWhitelistSystem : EntitySystem
     }
 
     /// <summary>
-    /// Helper function to determine if Whitelist is either null or the entity is on the list
+    /// Helper function to determine if a whitelist is either null or the entity is on the list.
     /// </summary>
     public bool IsWhitelistPassOrNull(EntityWhitelist? whitelist, EntityUid uid)
     {
@@ -122,7 +129,7 @@ public sealed class EntityWhitelistSystem : EntitySystem
     }
 
     /// <summary>
-    /// Helper function to determine if Whitelist is either null or the entity is not on the list
+    /// Helper function to determine if a whitelist is either null or the entity is not on the list.
     /// </summary>
     public bool IsWhitelistFailOrNull(EntityWhitelist? whitelist, EntityUid uid)
     {
@@ -133,8 +140,8 @@ public sealed class EntityWhitelistSystem : EntitySystem
     }
 
     /// <summary>
-    /// Helper function to determine if Blacklist is not null and entity is on list
-    /// Duplicate of equivalent Whitelist function
+    /// Helper function to determine if a blacklist is not null and the entity is on list.
+    /// Duplicate of equivalent whitelist function.
     /// </summary>
     public bool IsBlacklistPass(EntityWhitelist? blacklist, EntityUid uid)
     {
@@ -142,8 +149,8 @@ public sealed class EntityWhitelistSystem : EntitySystem
     }
 
     /// <summary>
-    /// Helper function to determine if Blacklist is not null and entity is not on the list
-    /// Duplicate of equivalent Whitelist function
+    /// Helper function to determine if a blacklist is not null and the entity is not on the list.
+    /// Duplicate of equivalent whitelist function.
     /// </summary>
     public bool IsBlacklistFail(EntityWhitelist? blacklist, EntityUid uid)
     {
@@ -151,8 +158,8 @@ public sealed class EntityWhitelistSystem : EntitySystem
     }
 
     /// <summary>
-    /// Helper function to determine if Blacklist is either null or the entity is on the list
-    /// Duplicate of equivalent Whitelist function
+    /// Helper function to determine if a blacklist is either null or the entity is on the list.
+    /// Duplicate of equivalent whitelist function.
     /// </summary>
     public bool IsBlacklistPassOrNull(EntityWhitelist? blacklist, EntityUid uid)
     {
@@ -160,8 +167,8 @@ public sealed class EntityWhitelistSystem : EntitySystem
     }
 
     /// <summary>
-    /// Helper function to determine if Blacklist is either null or the entity is not on the list
-    /// Duplicate of equivalent Whitelist function
+    /// Helper function to determine if a blacklist is either null or the entity is not on the list.
+    /// Duplicate of equivalent whitelist function.
     /// </summary>
     public bool IsBlacklistFailOrNull(EntityWhitelist? blacklist, EntityUid uid)
     {
