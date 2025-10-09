56cdb94f257911bf4cee13fdd6d99b1a1ece7bb2
ScarKy0 <scarky0@onet.eu>
2025-10-09 14:44:56 +0200

    review

--- Content.Shared/ParcelWrap/Systems/ParcelWrappingSystem.cs
+++ Content.Shared/ParcelWrap/Systems/ParcelWrappingSystem.cs
@@ -51,7 +51,6 @@ public sealed partial class ParcelWrappingSystem : EntitySystem
             wrapper.Owner != target &&
             // Wrapper should never be empty, but may as well make sure.
             !_charges.IsEmpty(wrapper.Owner) &&
-            _whitelist.IsWhitelistPassOrNull(wrapper.Comp.Whitelist, target) &&
-            _whitelist.IsBlacklistFailOrNull(wrapper.Comp.Blacklist, target);
+            _whitelist.CheckBoth(target, wrapper.Comp.Blacklist, wrapper.Comp.Whitelist);
     }
 }
