8f0b2c0bf8b39d69979e88ea66ac3256dad43746
UpAndLeaves <92269094+Alpha-Two@users.noreply.github.com>
2025-10-10 13:33:49 +0100

    Add inventory and storage query toolshed commands

--- Content.Server/Inventory/InventoryCommand.cs
+++ Content.Server/Inventory/InventoryCommand.cs
@@ -12,6 +12,27 @@ public sealed class InventoryCommand : ToolshedCommand
 {
     private InventorySystem? _inventorySystem;
 
+    [CommandImplementation("query")]
+    public IEnumerable<EntityUid> InventoryQuery([PipedArgument] IEnumerable<EntityUid> entities) =>
+        entities.SelectMany(InventoryQuery);
+
+    private IEnumerable<EntityUid> InventoryQuery(EntityUid entity)
+    {
+        _inventorySystem ??= GetSys<InventorySystem>();
+
+        if (!EntityManager.TryGetComponent<InventoryComponent>(entity, out var inventory))
+            return [];
+
+        List<EntityUid> result = new();
+
+        foreach (var slot in inventory.Slots)
+        {
+            if (_inventorySystem.TryGetSlotEntity(entity, slot.Name, out var item, inventory))
+                result.Add(item.Value);
+        }
+        return result;
+    }
+
     [CommandImplementation("getflags")]
     public IEnumerable<EntityUid> InventoryGetFlags([PipedArgument] IEnumerable<EntityUid> ents, SlotFlags slotFlag)
     {
