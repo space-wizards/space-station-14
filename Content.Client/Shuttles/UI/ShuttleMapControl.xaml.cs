f75435752bebf347043440590799cc60d4ae567f
PJB3005 <pieterjan.briers+git@gmail.com>
2025-10-15 20:21:13 +0200

    Fix shuttle FTL with UI scale
    Fixed click inputs being broken
    
    Fixed rendering of background parallax.

--- Content.Client/Shuttles/UI/ShuttleMapControl.xaml.cs
+++ Content.Client/Shuttles/UI/ShuttleMapControl.xaml.cs
@@ -124,7 +124,7 @@ public sealed partial class ShuttleMapControl : BaseShuttleControl
                 else
                 {
                     // We'll send the "adjusted" position and server will adjust it back when relevant.
-                    var mapCoords = new MapCoordinates(InverseMapPosition(args.RelativePosition), ViewingMap);
+                    var mapCoords = new MapCoordinates(InverseMapPosition(args.RelativePixelPosition), ViewingMap);
                     RequestFTL?.Invoke(mapCoords, _ftlAngle);
                 }
             }
@@ -180,7 +180,7 @@ public sealed partial class ShuttleMapControl : BaseShuttleControl
 
         // Remove offset so we can floor.
         var botLeft = new Vector2(0f, 0f);
-        var topRight = botLeft + Size;
+        var topRight = botLeft + PixelSize;
 
         var flooredBL = botLeft - originBL;
 
