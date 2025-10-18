3e5ad1e12b69377ecb9dfbabdc75a10fa64db982
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-10-18 22:15:39 +0200

    fix

--- Content.Shared/Wieldable/SharedWieldableSystem.cs
+++ Content.Shared/Wieldable/SharedWieldableSystem.cs
@@ -259,7 +259,7 @@ public abstract class SharedWieldableSystem : EntitySystem
             return false;
         }
 
-        if (_hands.CountFreeableHands((user, hands)) < component.FreeHandsRequired)
+        if (_hands.CountFreeableHandsExcept((user, hands), uid) < component.FreeHandsRequired)
         {
             if (!quiet)
             {
