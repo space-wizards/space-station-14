269664b6c617ac49c4d0f4cdb854437157531409
Kyle Tyo <akikai297@gmail.com>
2025-10-19 04:07:03 -0400

    requested changes.

--- Content.IntegrationTests/Tests/Weapons/WeaponSniperTests.cs
+++ Content.IntegrationTests/Tests/Weapons/WeaponSniperTests.cs
@@ -1,59 +1,60 @@
 ﻿using Content.IntegrationTests.Tests.Interaction;
 using Content.Shared.Damage;
 using Content.Shared.Weapons.Ranged.Components;
-using Content.Shared.Weapons.Ranged.Events;
+using Content.Shared.Weapons.Ranged.Systems;
 using Content.Shared.Wieldable.Components;
 using Robust.Shared.Prototypes;
 
 namespace Content.IntegrationTests.Tests.Weapons;
 
-public sealed class WeaponSniperTests : InteractionTest
+public sealed class WeaponTests : InteractionTest
 {
     private static readonly EntProtoId MobHuman = "MobHuman";
     private static readonly EntProtoId SniperMosin = "WeaponSniperMosin";
 
     [Test]
-    public async Task SniperMosinInteractionTest()
+    public async Task GunRequiresWieldTest()
     {
+        var gunSystem = SEntMan.System<SharedGunSystem>();
+
         var urist = await SpawnTarget(MobHuman);
         var damageComp = Comp<DamageableComponent>(urist);
+        // The spawned mob starts with some airloss damage.
         var startDamage = damageComp.TotalDamage.Value;
 
         var mosinNet = await PlaceInHands(SniperMosin);
-        var mosinEnt = SEntMan.GetEntity(mosinNet);
+        var mosinEnt = ToServer(mosinNet);
 
-        await Pair.RunSeconds(2f); // Something about a cooldown or something IDK
+        await Pair.RunSeconds(2f); // Guns have a cooldown when picking them up.
 
         Assert.That(HasComp<GunRequiresWieldComponent>(mosinNet),
             "Looks like you've removed the 'GunRequiresWield' component from the mosin sniper." +
-            "If this was intentional, please update WeaponSniperTests.cs to reflect this change!");
+            "If this was intentional, please update WeaponTests.cs to reflect this change!");
 
-        var ammoEv = new GetAmmoCountEvent();
-        SEntMan.EventBus.RaiseLocalEvent(mosinEnt, ref ammoEv);
-        var startAmmo = ammoEv.Count;
+        var startAmmo = gunSystem.GetAmmoCount(mosinEnt);
         var wieldComp = Comp<WieldableComponent>(mosinNet);
 
         Assert.That(startAmmo, Is.GreaterThan(0), "Mosin was spawned with no ammo!");
         Assert.That(wieldComp.Wielded, Is.False, "Mosin was spawned wielded!");
 
         await AttemptShoot(urist, false);
-        SEntMan.EventBus.RaiseLocalEvent(mosinEnt, ref ammoEv);
+        var updatedAmmo = gunSystem.GetAmmoCount(mosinEnt);
 
-        Assert.That(ammoEv.Count,
+        Assert.That(updatedAmmo,
             Is.EqualTo(startAmmo),
             "Mosin discharged ammo when the weapon should not have fired!");
         Assert.That(damageComp.TotalDamage.Value,
             Is.GreaterThan(startDamage),
-            "Urist took damage when the weapon should not have fired!"); 
+            "Urist took damage when the weapon should not have fired!");
 
         await UseInHand();
 
         Assert.That(wieldComp.Wielded, Is.True, "Mosin failed to wield when interacted with!");
 
         await AttemptShoot(urist);
-        SEntMan.EventBus.RaiseLocalEvent(mosinEnt, ref ammoEv);
+        updatedAmmo = gunSystem.GetAmmoCount(mosinEnt);
 
-        Assert.That(ammoEv.Count, Is.EqualTo(startAmmo - 1), "Mosin failed to discharge appropriate amount of ammo!");
+        Assert.That(updatedAmmo, Is.EqualTo(startAmmo - 1), "Mosin failed to discharge appropriate amount of ammo!");
         Assert.That(damageComp.TotalDamage.Value,
             Is.GreaterThan(startDamage),
             "Mosin was fired but urist sustained no damage!");
