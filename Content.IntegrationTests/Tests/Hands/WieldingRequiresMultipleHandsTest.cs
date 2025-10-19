e26a94322350b0efacfeccd17106602eba9d8f08
Kyle Tyo <akikai297@gmail.com>
2025-10-19 07:23:38 -0400

    Create WieldingRequiresMultipleHandsTest.cs

---
+++ Content.IntegrationTests/Tests/Hands/WieldingRequiresMultipleHandsTest.cs
@@ -0,0 +1,29 @@
+﻿using Content.IntegrationTests.Tests.Interaction;
+using Content.Shared.Wieldable.Components;
+using Robust.Shared.Prototypes;
+
+namespace Content.IntegrationTests.Tests.Hands;
+
+public sealed class WieldingRequiresMultipleHandsTest : InteractionTest
+{
+    private static readonly EntProtoId SniperMosin = "WeaponSniperMosin";
+
+    [Test]
+    public async Task TestOneHandedWielding()
+    {
+        var mosinNet = await PlaceInHands(SniperMosin);
+        var wieldComp = Comp<WieldableComponent>(mosinNet);
+
+        // The player entity by default should only have one hand.
+        // We're checking to make sure that is still the case.
+        var handCount = HandSys.GetHandCount(ToServer(Player));
+
+        Assert.That(handCount,
+            Is.EqualTo(1),
+            "Player entity has more than one hand! If this is intentional, " +
+            "WieldingRequiresMultipleHandsTest should be removed!");
+        Assert.That(wieldComp.Wielded, Is.False, "Mosin spawned in wielded!");
+        await UseInHand();
+        Assert.That(wieldComp.Wielded, Is.False, "Mosin was wielded but player only has one hand!");
+    }
+}
