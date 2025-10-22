d98cbf3210e81934ad8bd3a06d67d7947ba88188
Uberration <jimboedeedler@gmail.com>
2025-10-22 16:39:32 -0400

    RE-FIX: Localize default deny reason in LockSystem.HasUserAccess
    The default deny reason was being passed as a localization key string instead of being localized before display.

--- Content.Shared/Access/Systems/AccessReaderSystem.cs
+++ Content.Shared/Access/Systems/AccessReaderSystem.cs
@@ -186,8 +186,6 @@ public sealed class AccessReaderSystem : EntitySystem
         // If the user has access to this lock, we pass it into the event.
         if (IsAllowed(args.User, ent))
             args.HasAccess |= LockTypes.Access;
-        else
-            args.DenyReason ??= Loc.GetString("lock-comp-has-user-access-fail");
     }
 
     /// <summary>
