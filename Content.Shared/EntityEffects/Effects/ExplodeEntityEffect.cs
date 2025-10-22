16e5856457355a41c4f90bbb9506439b5253b4a0
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-10-22 20:11:33 +0200

    first batch

---
+++ Content.Shared/EntityEffects/Effects/ExplodeEntityEffect.cs
@@ -0,0 +1,34 @@
+﻿using Content.Shared.Database;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.EntityEffects.Effects.Transform;
+
+/// <inheritdoc cref="EntityEffect"/>
+/// <seealso cref="ExplosionEffect">
+public sealed partial class ExplodeEffect : EntityEffectBase<ExplodeEffect>
+{
+    /// <summary>
+    /// Optional override for the explosion intensity.
+    /// </summary>
+    [DataField]
+    public float? Intensity;
+
+    /// <summary>
+    /// Optional override for the explosion radius.
+    /// </summary>
+    [DataField]
+    public float? Radius;
+
+    /// <summary>
+    /// Delete the entity with the explosion?
+    /// </summary>
+    [DataField]
+    public bool Delete = true;
+
+    public override string EntityEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+        => Loc.GetString("entity-effect-guidebook-explosion", ("chance", Probability));
+
+    public override bool ShouldLog => true;
+
+    public override LogImpact LogImpact => LogImpact.High;
+}
