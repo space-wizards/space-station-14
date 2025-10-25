5c12f86192dd82e814870f1e6bcb6e18defa01f3
NexusNull <p.wellershaus@googlemail.com>
2025-10-25 11:51:27 +0200

    empty out slots

--- Content.Server/Construction/ConstructionSystem.Initial.cs
+++ Content.Server/Construction/ConstructionSystem.Initial.cs
@@ -220,7 +220,13 @@ namespace Content.Server.Construction
                             {
                                 _container.EmptyContainer(storage.Container);
                             }
-
+                            if (TryComp<ContainerManagerComponent>(entity, out var slotContainers))
+                            {
+                                foreach (var slot in slotContainers.Containers)
+                                {
+                                    _container.EmptyContainer(slot.Value);
+                                }
+                            }
                             if (string.IsNullOrEmpty(arbitraryStep.Store))
                             {
                                 if (!_container.Insert(entity, container))
