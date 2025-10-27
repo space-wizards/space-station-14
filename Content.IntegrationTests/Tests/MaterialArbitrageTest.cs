fbfcffab75a3edf6f89b062ffa5afe5f6e4a478c
iaada <iaada@users.noreply.github.com>
2025-10-26 19:30:56 -0500

    plates n stuff

--- Content.IntegrationTests/Tests/MaterialArbitrageTest.cs
+++ Content.IntegrationTests/Tests/MaterialArbitrageTest.cs
@@ -33,8 +33,8 @@ public sealed class MaterialArbitrageTest
 
     private readonly HashSet<string> _compositionArbitrageIgnore =
     [
-        "FoodPlateSmall", "AirTank", "FoodPlateTin", "FoodPlateMuffinTin", "WeaponCapacitorRechargerCircuitboard",
-        "WeaponCapacitorRechargerCircuitboard", "BorgChargerCircuitboard", "BorgChargerCircuitboard", "FoodPlate",
+        "AirTank", "WeaponCapacitorRechargerCircuitboard",
+        "WeaponCapacitorRechargerCircuitboard", "BorgChargerCircuitboard", "BorgChargerCircuitboard",
         "CellRechargerCircuitboard", "CellRechargerCircuitboard",
     ];
 
