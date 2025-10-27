300376088890c66d2278edd83321b03620a38769
Prole <172158352+Prole0@users.noreply.github.com>
2025-10-27 13:37:11 -0700

    Conflict Fix Thrice

--- Content.Server/Administration/Systems/AdminVerbSystem.Smites.cs
+++ Content.Server/Administration/Systems/AdminVerbSystem.Smites.cs
@@ -58,7 +58,7 @@ using Robust.Shared.Spawners;
 using Robust.Shared.Utility;
 using System.Numerics;
 using System.Threading;
-using Content.Shared.Body.Systems;
+
 using Timer = Robust.Shared.Timing.Timer;
 
 namespace Content.Server.Administration.Systems;
