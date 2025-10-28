4a0dae3a152ac7ec2ef6bd4937227f4c57eccb47
Kyle Tyo <akikai297@gmail.com>
2025-10-28 08:26:20 -0400

    Update GasPassiveGateComponent.cs

--- Content.Shared/Atmos/Piping/Binary/Components/GasPassiveGateComponent.cs
+++ Content.Shared/Atmos/Piping/Binary/Components/GasPassiveGateComponent.cs
@@ -5,14 +5,28 @@ namespace Content.Shared.Atmos.Piping.Binary.Components;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class GasPassiveGateComponent : Component
 {
+    /// <summary>
+    /// Specifies the pipe node name to be treated as the inlet.
+    /// </summary>
     [DataField("inlet")]
     public string InletName = "inlet";
+
+    /// <summary>
+    /// Specifies the pipe node name to be treated as the outlet.
+    /// </summary>
     [DataField("outlet")]
     public string OutletName = "outlet";
 
+    /// <summary>
+    /// The current flow rate of the passive gate.
+    /// </summary>
     [ViewVariables(VVAccess.ReadOnly)]
     [DataField, AutoNetworkedField]
     public float FlowRate;
 
+    /// <summary>
+    /// The previous flow rate of the passive gate.
+    /// This is used to determine if the FlowRate field should be dirtied.
+    /// </summary>
     public float OldFlowRate;
 }
