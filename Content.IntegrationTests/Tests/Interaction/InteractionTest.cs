4656eabc68b5af77b89f3c8940e02ed13c1a4a53
Connor Huffine <chuffine@gmail.com>
2025-11-01 17:47:12 -0400

    Change connected default

--- Content.IntegrationTests/Tests/Interaction/InteractionTest.cs
+++ Content.IntegrationTests/Tests/Interaction/InteractionTest.cs
@@ -167,7 +167,7 @@ public abstract partial class InteractionTest
   - type: CombatMode
 ";
 
-    protected static PoolSettings Default => new() { Connected = true, Dirty = true };
+    protected static PoolSettings Default => new() { Connected = false, Dirty = true };
     protected virtual PoolSettings Settings => Default;
 
     [SetUp]
