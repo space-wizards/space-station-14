8fb92f108a5b2e5129864aaeda17d0c8ec5c0518
portfiend <109661617+portfiend@users.noreply.github.com>
2025-11-03 10:54:29 -0500

    add arcade game completion events

--- Content.Server/Arcade/ArcadeSystem.cs
+++ Content.Server/Arcade/ArcadeSystem.cs
@@ -1,9 +1,63 @@
 using Content.Shared.Arcade;
+using JetBrains.Annotations;
 
 namespace Content.Server.Arcade;
 
 public sealed partial class ArcadeSystem : SharedArcadeSystem
 {
+    /// <summary>
+    ///     Ends the arcade game on a win.
+    /// </summary>
+    /// <param name="player">The entity playing the game.</param>
+    /// <param name="machine">The arcade machine entity.</param>
+    [PublicAPI]
+    public void WinGame(EntityUid? player, EntityUid machine)
+    {
+        FinishGame(player, machine, ArcadeGameResult.Win);
+    }
+
+    /// <summary>
+    ///     Ends the arcade game on a loss.
+    /// </summary>
+    /// <param name="player">The entity playing the game.</param>
+    /// <param name="machine">The arcade machine entity.</param>
+    [PublicAPI]
+    public void LoseGame(EntityUid? player, EntityUid machine)
+    {
+        FinishGame(player, machine, ArcadeGameResult.Fail);
+    }
+
+    /// <summary>
+    ///     Ends the arcade game without finishing it (i.e. quitting early).
+    /// </summary>
+    /// <param name="player">The entity playing the game.</param>
+    /// <param name="machine">The arcade machine entity.</param>
+    [PublicAPI]
+    public void LeaveGame(EntityUid? player, EntityUid machine)
+    {
+        FinishGame(player, machine, ArcadeGameResult.Forfeit);
+    }
+
+    /// <summary>
+    ///     Ends the arcade game on a draw (game finished, neither win nor lose).
+    /// </summary>
+    /// <param name="player">The entity playing the game.</param>
+    /// <param name="machine">The arcade machine entity.</param>
+    [PublicAPI]
+    public void DrawGame(EntityUid? player, EntityUid machine)
+    {
+        FinishGame(player, machine, ArcadeGameResult.Draw);
+    }
+
+    private void FinishGame(EntityUid? player, EntityUid machine, ArcadeGameResult result)
+    {
+        var endedEvent = new ArcadeGameEndedEvent(player, result);
+        var finishEvent = new FinishedArcadeGameEvent(result);
+
+        RaiseLocalEvent(machine, endedEvent);
+        if (player != null)
+            RaiseLocalEvent(player.Value, finishEvent);
+    }
 }
 
 /// <summary>
