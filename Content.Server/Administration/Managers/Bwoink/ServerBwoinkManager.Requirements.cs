fdb50d00e8332a3a2eb1466118956894ac58f706
Simon <63975668+Simyon264@users.noreply.github.com>
2025-11-04 16:41:27 +0100

    add events for requirement checks

--- Content.Server/Administration/Managers/Bwoink/ServerBwoinkManager.Requirements.cs
+++ Content.Server/Administration/Managers/Bwoink/ServerBwoinkManager.Requirements.cs
@@ -1,5 +1,6 @@
 ﻿using Content.Shared.Administration.Managers.Bwoink;
 using Content.Shared.Administration.Managers.Bwoink.Requirements;
+using JetBrains.Annotations;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
@@ -19,6 +20,34 @@ public sealed partial class ServerBwoinkManager
     {
         SyncChannels(PlayerManager.GetSessionByChannel(message.MsgChannel));
     }
+
+    /// <summary>
+    /// Event that gets raised when CanManageChannel is called.
+    /// This is to be used for conditions that do not exactly fit into this file to avoid making a mega class.
+    /// </summary>
+    /// <remarks>
+    /// Called *after* the other access checks are done.
+    /// </remarks>
+    [PublicAPI]
+    public event EventHandler<BwoinkRequirementCheckEventArgs>? OnManageCheck;
+    /// <summary>
+    /// Event that gets raised when CanReadChannel is called.
+    /// This is to be used for conditions that do not exactly fit into this file to avoid making a mega class.
+    /// </summary>
+    /// <remarks>
+    /// Called after the other access checks are done.
+    /// </remarks>
+    [PublicAPI]
+    public event EventHandler<BwoinkRequirementCheckEventArgs>? OnReadCheck;
+    /// <summary>
+    /// Event that gets raised when CanWriteChannel is called.
+    /// This is to be used for conditions that do not exactly fit into this file to avoid making a mega class.
+    /// </summary>
+    /// <remarks>
+    /// Called after the other access checks are done.
+    /// </remarks>
+    [PublicAPI]
+    public event EventHandler<BwoinkRequirementCheckEventArgs>? OnWriteCheck;
 
     /// <summary>
     /// Sends over the up-to-date list of channel condition flags to the session.
@@ -58,8 +87,12 @@ public sealed partial class ServerBwoinkManager
     /// <inheritdoc cref="CanManageChannel(Robust.Shared.Prototypes.ProtoId{Content.Shared.Administration.Managers.Bwoink.BwoinkChannelPrototype},Robust.Shared.Player.ICommonSession)"/>
     public override bool CanManageChannel(BwoinkChannelPrototype channel, ICommonSession session)
     {
-        return channel.ManageRequirement == null
+        var result = channel.ManageRequirement == null
                || CheckConditions(channel, channel.ManageRequirement.Requirements, channel.ManageRequirement.OperationMode, session);
+
+        var eventCall = new BwoinkRequirementCheckEventArgs(channel, session, result);
+        OnManageCheck?.Invoke(this, eventCall);
+        return eventCall.CanAccess;
     }
 
 
@@ -78,8 +111,12 @@ public sealed partial class ServerBwoinkManager
         if (CanManageChannel(channel, session))
             return true;
 
-        return channel.ReadRequirement == null
+        var result = channel.ReadRequirement == null
                || CheckConditions(channel, channel.ReadRequirement.Requirements, channel.ReadRequirement.OperationMode, session);
+
+        var eventCall = new BwoinkRequirementCheckEventArgs(channel, session, result);
+        OnReadCheck?.Invoke(this, eventCall);
+        return eventCall.CanAccess;
     }
     /// <summary>
     /// Checks if a given session is able to write to a channel.
@@ -96,8 +133,12 @@ public sealed partial class ServerBwoinkManager
         if (CanManageChannel(channel, session))
             return true;
 
-        return channel.WriteRequirement == null
+        var result = channel.WriteRequirement == null
                || CheckConditions(channel, channel.WriteRequirement.Requirements, channel.WriteRequirement.OperationMode, session);
+
+        var eventCall = new BwoinkRequirementCheckEventArgs(channel, session, result);
+        OnWriteCheck?.Invoke(this, eventCall);
+        return eventCall.CanAccess;
     }
 
     private bool CheckConditions(BwoinkChannelPrototype prototype, List<BwoinkChannelCondition> requirements, RequirementOperationMode mode, ICommonSession session)
@@ -158,4 +199,14 @@ public sealed partial class ServerBwoinkManager
     }
 
     #endregion
+}
+
+/// <summary>
+/// Event args for when an access check for a bwoink channel is performed.
+/// </summary>
+public sealed class BwoinkRequirementCheckEventArgs(BwoinkChannelPrototype proto, ICommonSession session, bool canAccess) : EventArgs
+{
+    public BwoinkChannelPrototype Prototype { get; } = proto;
+    public ICommonSession Session { get; } = session;
+    public bool CanAccess { get; set; } = canAccess;
 }
