dc1db82470989d11b242bf89aac6142374de272b
shibe <swiftshibe@gmail.com>
2025-11-04 18:12:23 -0500

    ..

--- Content.Server/GameTicking/Rules/VariationPass/Components/MisplacementMarker/MisplacementMarkerComponent.cs
+++ Content.Server/GameTicking/Rules/VariationPass/Components/MisplacementMarker/MisplacementMarkerComponent.cs
@@ -1,7 +1,7 @@
 ﻿namespace Content.Server.GameTicking.Rules.VariationPass.Components.MisplacementMarker;
 
 /// <summary>
-/// This component marks replaceable reinforced walls for use with fast queries in variation passes.
+/// This component is used to query entities to be misplaced during the variation pass.
 /// </summary>
 [RegisterComponent]
 public sealed partial class MisplacementMarkerComponent : Component
