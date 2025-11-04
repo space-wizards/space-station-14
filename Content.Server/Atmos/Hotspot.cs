e09ae3406cf0f9a83842e46c7b5c9f8a6ca0278c
ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
2025-11-03 20:34:37 -0800

    Finalize docs

--- Content.Server/Atmos/Hotspot.cs
+++ Content.Server/Atmos/Hotspot.cs
@@ -2,12 +2,12 @@
 
 /// <summary>
 /// Internal Atmospherics struct that stores data about a hotspot in a tile.
-/// Hotspots are used to model flammable gas ignition and fires.
+/// Hotspots are used to model (slow-spreading) fires and firestarters.
 /// </summary>
 public struct Hotspot
 {
     /// <summary>
-    /// Whether this hotspot is currently representing fire.
+    /// Whether this hotspot is currently representing fire and needs to be processed.
     /// Set when the hotspot "becomes alight". This is never set to false
     /// because Atmospherics will just assign <see cref="TileAtmosphere"/>
     /// a new <see cref="Hotspot"/> struct when the fire goes out.
@@ -36,14 +36,21 @@ public struct Hotspot
     [ViewVariables]
     public bool Bypassing;
 
+    /// <summary>
+    /// Current temperature of the hotspot's volume, in Kelvin.
+    /// </summary>
     [ViewVariables]
     public float Temperature;
 
+    /// <summary>
+    /// Current volume of the hotspot, in liters.
+    /// You can think of this as the volume of the current fire in the tile.
+    /// </summary>
     [ViewVariables]
     public float Volume;
 
     /// <summary>
-    ///     State for the fire sprite.
+    /// State for the fire sprite.
     /// </summary>
     [ViewVariables]
     public byte State;
