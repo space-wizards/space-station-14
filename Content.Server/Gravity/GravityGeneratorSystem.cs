4338fc6df3a678d2a9af6eac8a92608e5fc1f61f
Justin Pfeifler <jrpl101998@gmail.com>
2025-11-05 01:37:33 -0600

    Dirty GravityActive whenever changed

--- Content.Server/Gravity/GravityGeneratorSystem.cs
+++ Content.Server/Gravity/GravityGeneratorSystem.cs
@@ -1,9 +1,8 @@
 using Content.Server.Power.Components;
 using Content.Server.Power.EntitySystems;
-using Content.Server.Popups;
+using Content.Shared.Popups;
 using Content.Shared.Gravity;
 using Content.Shared.Construction.Components;
-using Content.Shared.Popups;
 
 namespace Content.Server.Gravity;
 
@@ -11,22 +10,22 @@ public sealed class GravityGeneratorSystem : EntitySystem
 {
     [Dependency] private readonly GravitySystem _gravitySystem = default!;
     [Dependency] private readonly SharedPointLightSystem _lights = default!;
-    [Dependency] private readonly PopupSystem _popupSystem = default!;
+    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<GravityGeneratorComponent, EntParentChangedMessage>(OnParentChanged);
-        SubscribeLocalEvent<GravityGeneratorComponent, UnanchorAttemptEvent>(OnUnanchorAttempt);
-        SubscribeLocalEvent<GravityGeneratorComponent, ChargedMachineActivatedEvent>(OnActivated);
-        SubscribeLocalEvent<GravityGeneratorComponent, ChargedMachineDeactivatedEvent>(OnDeactivated);
+        SubscribeLocalEvent<SharedGravityGeneratorComponent, EntParentChangedMessage>(OnParentChanged);
+        SubscribeLocalEvent<SharedGravityGeneratorComponent, UnanchorAttemptEvent>(OnUnanchorAttempt);
+        SubscribeLocalEvent<SharedGravityGeneratorComponent, ChargedMachineActivatedEvent>(OnActivated);
+        SubscribeLocalEvent<SharedGravityGeneratorComponent, ChargedMachineDeactivatedEvent>(OnDeactivated);
     }
 
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
-        var query = EntityQueryEnumerator<GravityGeneratorComponent, PowerChargeComponent>();
+        var query = EntityQueryEnumerator<SharedGravityGeneratorComponent, PowerChargeComponent>();
         while (query.MoveNext(out var uid, out var grav, out var charge))
         {
             if (!_lights.TryGetLight(uid, out var pointLight))
@@ -38,9 +37,10 @@ public sealed class GravityGeneratorSystem : EntitySystem
         }
     }
 
-    private void OnActivated(Entity<GravityGeneratorComponent> ent, ref ChargedMachineActivatedEvent args)
+    private void OnActivated(Entity<SharedGravityGeneratorComponent> ent, ref ChargedMachineActivatedEvent args)
     {
         ent.Comp.GravityActive = true;
+        DirtyField(ent, ent.Comp, nameof(SharedGravityGeneratorComponent.GravityActive));
 
         var xform = Transform(ent);
 
@@ -50,9 +50,10 @@ public sealed class GravityGeneratorSystem : EntitySystem
         }
     }
 
-    private void OnDeactivated(Entity<GravityGeneratorComponent> ent, ref ChargedMachineDeactivatedEvent args)
+    private void OnDeactivated(Entity<SharedGravityGeneratorComponent> ent, ref ChargedMachineDeactivatedEvent args)
     {
         ent.Comp.GravityActive = false;
+        DirtyField(ent, ent.Comp, nameof(SharedGravityGeneratorComponent.GravityActive));
 
         var xform = Transform(ent);
 
@@ -65,21 +66,17 @@ public sealed class GravityGeneratorSystem : EntitySystem
     /// <summary>
     /// Prevent unanchoring when gravity is active
     /// </summary>
-    private void OnUnanchorAttempt(Entity<GravityGeneratorComponent> ent, ref UnanchorAttemptEvent args)
+    private void OnUnanchorAttempt(Entity<SharedGravityGeneratorComponent> ent, ref UnanchorAttemptEvent args)
     {
         if (!ent.Comp.GravityActive)
             return;
 
-        _popupSystem.PopupEntity(
-            Loc.GetString("gravity-generator-unanchoring-failed"),
-            ent,
-            args.User,
-            PopupType.Medium);
+        _popupSystem.PopupEntity(Loc.GetString("gravity-generator-unanchoring-failed"), ent.Owner, args.User, PopupType.Medium);
 
         args.Cancel();
     }
 
-    private void OnParentChanged(EntityUid uid, GravityGeneratorComponent component, ref EntParentChangedMessage args)
+    private void OnParentChanged(EntityUid uid, SharedGravityGeneratorComponent component, ref EntParentChangedMessage args)
     {
         if (component.GravityActive && TryComp(args.OldParent, out GravityComponent? gravity))
         {
