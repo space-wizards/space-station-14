5d22509f4917b545ee1c1de8a5543ef96ab9ccc2
Errant <35878406+Errant-4@users.noreply.github.com>
2025-11-07 09:04:52 +0100

    cleanup

--- Content.Server/Administration/Systems/RerouteSpawningSystem.cs
+++ Content.Server/Administration/Systems/RerouteSpawningSystem.cs
@@ -1,8 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using Content.Server.GameTicking.Rules;
 using Content.Server.GameTicking.Rules.Components;
-using Content.Server.Maps;
 using Content.Server.Mind;
 using Content.Server.Station.Components;
 using Content.Server.Station.Systems;
@@ -22,7 +20,6 @@ public sealed class RerouteSpawningSystem : GameRuleSystem<RerouteSpawningRuleCo
     [Dependency] private readonly SharedMapSystem _map = default!;
     [Dependency] private readonly MindSystem _mind = default!;
     [Dependency] private readonly IPrototypeManager _proto = default!;
-    // [Dependency] private readonly SolitaryManager _solitary = default!;
     [Dependency] private readonly StationSpawningSystem _stationSpawning = default!;
 
     private Dictionary<ICommonSession, EntityUid> _catalog = [];
@@ -33,17 +30,15 @@ public sealed class RerouteSpawningSystem : GameRuleSystem<RerouteSpawningRuleCo
         base.Initialize();
 
         SubscribeLocalEvent<PlayerBeforeSpawnEvent>(OnBeforeSpawn);
-        SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnSpawnComplete);
     }
 
     private void OnBeforeSpawn(PlayerBeforeSpawnEvent ev)
     {
         var session = ev.Player;
 
-        // Copied verbatim from DeathMatchRuleSystem
         // Check if any Reroute Spawning rules are running
-        var query = EntityQueryEnumerator<RerouteSpawningRuleComponent,GameRuleComponent>();
-        while (query.MoveNext(out var uid, out var reroute, out var rule))
+        var rules = EntityQueryEnumerator<RerouteSpawningRuleComponent,GameRuleComponent>();
+        while (rules.MoveNext(out var uid, out var reroute, out var rule))
         {
             if (!GameTicker.IsGameRuleActive(uid, rule))
                 continue;
@@ -55,67 +50,33 @@ public sealed class RerouteSpawningSystem : GameRuleSystem<RerouteSpawningRuleCo
                 break;
             }
 
-            // TODO Ask the Manager for a map prototype that the player pricked pre-join or even pre-round
-
-            // TODO This whole stuff should probably work through Gameticker.Roundflow
+            // TODO Ask the Manager for a map prototype that the player pricked pre-join or even pre-round?
 
             if (!_proto.TryIndex(reroute.Map, out var map))
             {
-                Log.Warning($"Invalid map prototype: {reroute.Map}");
+                Log.Error($"Invalid map prototype: {reroute.Map}");
                 continue;
             }
 
-            // Create new map
-            // var newMap = _map.CreateMap(out var mapId);
-            // _loader.TryLoadGrid(mapId,
-            //     ev.GameMap.MapPath,
-            //     out var grid,
-            //     ev.Options,
-            //     ev.Offset,
-            //     ev.Rotation))
-
-            var stationGrids = GameTicker.LoadGameMap(map, out var mapId);
-
-            // TODO give this map a custom name
-
-
-
-
-            //TODO:ERRANT I think this is not actually the station, but the map or the grid.
-            // THis does not work, and is only here to get the code to continue to Spawn
-            // var stationGrid = stationGrids.First();
-
-
+            // Create the new map
+            GameTicker.LoadGameMap(map, out var mapId, stationName: ev.Profile.Name);
             _map.InitializeMap(mapId);
 
-            // DebugTools.Assert(!_map.IsInitialized(mapId));
-
-
-
-            ///
+            // Identify the new station grid that was created
             var spawns = EntityQueryEnumerator<StationJobsComponent, StationSpawningComponent>();
             EntityUid? stationEnt = null;
-            var targets = new List<EntityUid>();
-            StationSpawningComponent? spawnTarget;
-            while (spawns.MoveNext(out var station, out _, out var spawn))
+            while (spawns.MoveNext(out var station, out _, out _))
             {
-                // var comp1 = Transform(station);
-                // if (comp1.MapID == mapId)
-                // {
-                    targets.Add(station);
-                // }
-
-                var c = 3;
+                stationEnt = station;
             }
-            stationEnt = targets.Last();
-            var stationTarget = stationEnt.Value;
-            // var stationTarget = stationGrid;
 
-            if (!TryComp<StationSpawningComponent>(stationEnt.Value, out var comp))
+            if (stationEnt is null)
             {
                 Log.Error($"Reroute failed for {session} - no target station");
-                break;
+                continue;
             }
+
+            var stationTarget = stationEnt.Value;
 
             //store the newly created station entity for this session so we can put the player back on reconnect
             _catalog.Add(session, stationTarget);
@@ -129,16 +90,10 @@ public sealed class RerouteSpawningSystem : GameRuleSystem<RerouteSpawningRuleCo
         }
     }
 
-    // TODO:ERRANT this is kinda a duplication for the code in GameTicker. Very much not ideal
-    private void LoadMap(PlayerBeforeSpawnEvent ev, GameMapPrototype map)
-    {
-
-
-    }
-
     private void SpawnPlayer(PlayerBeforeSpawnEvent ev, EntityUid station)
     {
-        //TODO:ERRANT this was copied from deathmatchsystem, might be bad/obsolete!
+        //TODO This needs a full review, it might be missing modern stuff/steps. Ideally it should call an existing spawningsystem?
+
         var newMind = _mind.CreateMind(ev.Player.UserId, ev.Profile.Name);
         _mind.SetUserId(newMind, ev.Player.UserId);
         var mobMaybe = _stationSpawning.SpawnPlayerCharacterOnStation(station, null, ev.Profile);
@@ -163,10 +118,4 @@ public sealed class RerouteSpawningSystem : GameRuleSystem<RerouteSpawningRuleCo
         station = stored;
         return true;
     }
-
-    private void OnSpawnComplete(PlayerSpawnCompleteEvent ev)
-    {
-        // Do I even need this one?
-    }
-
 }
