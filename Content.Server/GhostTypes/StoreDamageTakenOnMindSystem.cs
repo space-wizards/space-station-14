8a6cbb162618a0c5c0068133cd856170921d3bb5
Thinbug <ringingsince1997@gmail.com>
2025-11-08 20:03:12 -0300

    more fixies

--- Content.Server/GhostTypes/StoreDamageTakenOnMindSystem.cs
+++ Content.Server/GhostTypes/StoreDamageTakenOnMindSystem.cs
@@ -1,9 +1,9 @@
-using Content.Server.Destructible;
 using Content.Shared.Damage.Components;
 using Content.Shared.GhostTypes;
 using Content.Shared.Gibbing.Events;
 using Content.Shared.Mind;
 using Content.Shared.Mind.Components;
+using Content.Shared.Mobs;
 using Robust.Server.Containers;
 
 namespace Content.Server.GhostTypes;
@@ -14,7 +14,7 @@ public sealed class StoreDamageTakenOnMindSystem : EntitySystem
     public override void Initialize()
     {
         SubscribeLocalEvent<StoreDamageTakenOnMindComponent, AttemptEntityGibEvent>(SaveBodyOnGib);
-        SubscribeLocalEvent<StoreDamageTakenOnMindComponent, DamageThresholdReached>(SaveBodyOnThreshold);
+        SubscribeLocalEvent<StoreDamageTakenOnMindComponent, MobStateChangedEvent>(SaveBodyOnThreshold);
     }
 
     /// <summary>
@@ -31,7 +31,7 @@ public sealed class StoreDamageTakenOnMindSystem : EntitySystem
     /// <summary>
     /// Saves the damage of a player body inside their MindComponent after a damage threshold event
     /// </summary>
-    private void SaveBodyOnThreshold(Entity<StoreDamageTakenOnMindComponent> ent, ref DamageThresholdReached args)
+    private void SaveBodyOnThreshold(Entity<StoreDamageTakenOnMindComponent> ent, ref MobStateChangedEvent args)
     {
         SaveBody(ent.Owner);
     }
--- Content.Shared/GhostTypes/StoreDamageTakenOnMindComponent.cs
+++ Content.Shared/GhostTypes/StoreDamageTakenOnMindComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.GhostTypes;
 
 /// <summary>
-/// Stores the damage an entity took before that body is destroyed in their mind component
+/// Stores the damage an entity took before their body is destroyed inside it's mind LastBodyDamageComponent
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class StoreDamageTakenOnMindComponent : Component;
