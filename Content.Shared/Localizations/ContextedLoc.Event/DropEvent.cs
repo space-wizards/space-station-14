1bd9cc121e10f51d7f68245372f1945b4a9fdb3c
Łukasz Mędrek <lukasz@lukaszm.xyz>
2025-11-15 09:14:48 +0100

    add: missing comments

--- Content.Shared/Localizations/ContextedLoc.Event/DropEvent.cs
+++ Content.Shared/Localizations/ContextedLoc.Event/DropEvent.cs
@@ -5,23 +5,31 @@ namespace Content.Shared.Localizations;
 
 public static partial class ContextedLoc
 {
+    /// <inheritdoc cref="GetString(String, object)"/>
     public static string GetString(string messageId, DroppedEvent context)
         => _loc.GetString(messageId, ("user", Identity.Entity(context.User, _entity)));
+    /// <inheritdoc cref="GetStringWithEntity(String, EntityUid, object)"/>
     public static string GetStringWithEntity(string messageId, EntityUid entity, DroppedEvent context)
         => _loc.GetString(messageId, ("entity", Identity.Entity(entity, _entity)), ("user", Identity.Entity(context.User, _entity)));
 
+    /// <inheritdoc cref="GetString(String, object)"/>
     public static string GetString(string messageId, DroppedEvent context, (string, object) arg0)
         => _loc.GetString(messageId, ("user", Identity.Entity(context.User, _entity)), arg0);
+    /// <inheritdoc cref="GetStringWithEntity(String, EntityUid, object)"/>
     public static string GetStringWithEntity(string messageId, EntityUid entity, DroppedEvent context, (string, object) arg0)
         => _loc.GetString(messageId, ("entity", Identity.Entity(entity, _entity)), ("user", Identity.Entity(context.User, _entity)), arg0);
 
+    /// <inheritdoc cref="GetString(String, object)"/>
     public static string GetString(string messageId, DroppedEvent context, (string, object) arg0, (string, object) arg1)
         => _loc.GetString(messageId, ("user", Identity.Entity(context.User, _entity)), arg0, arg1);
+    /// <inheritdoc cref="GetStringWithEntity(String, EntityUid, object)"/>
     public static string GetStringWithEntity(string messageId, EntityUid entity, DroppedEvent context, (string, object) arg0, (string, object) arg1)
         => _loc.GetString(messageId, ("entity", Identity.Entity(entity, _entity)), ("user", Identity.Entity(context.User, _entity)), arg0, arg1);
 
+    /// <inheritdoc cref="GetString(String, object)"/>
     public static string GetString(string messageId, DroppedEvent context, (string, object) arg0, (string, object) arg1, (string, object) arg2)
         => _loc.GetString(messageId, ("user", Identity.Entity(context.User, _entity)), arg0, arg1, arg2);
+    /// <inheritdoc cref="GetStringWithEntity(String, EntityUid, object)"/>
     public static string GetStringWithEntity(string messageId, EntityUid entity, DroppedEvent context, (string, object) arg0, (string, object) arg1, (string, object) arg2)
         => _loc.GetString(messageId, ("entity", Identity.Entity(entity, _entity)), ("user", Identity.Entity(context.User, _entity)), arg0, arg1, arg2);
 }
--- Content.Shared/Localizations/ContextedLoc.cs
+++ Content.Shared/Localizations/ContextedLoc.cs
@@ -1,7 +1,56 @@
 namespace Content.Shared.Localizations;
 
+/// <summary>
+/// Static wrapper for the <see cref="ILocalizationManager"/>, that provides
+/// general use parameters for Localization use.
+/// </summary>
+/// <seealso cref="Robust.Shared.Localization.Loc"/>
 public static partial class ContextedLoc
 {
     private static ILocalizationManager _loc => IoCManager.Resolve<ILocalizationManager>();
     private static EntityManager _entity => IoCManager.Resolve<EntityManager>();
+
+    /// <summary>
+    ///     Gets a language appropriate string represented by the supplied messageId.
+    /// </summary>
+    /// <param name="messageId">Unique Identifier for a translated message.</param>
+    /// <param name="context">Context to pass into translation system</param>
+    /// <returns>
+    ///     The language appropriate message if available, otherwise the messageId is returned.
+    /// </returns>
+    public static string GetString(string messageId, object context)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    ///     Gets a language appropriate string represented by the supplied messageId.
+    /// </summary>
+    /// <param name="messageId">Unique Identifier for a translated message.</param>
+    /// <param name="entity">Entity that cause message to appear, entity system's ent.Owner should be passed here</param>
+    /// <param name="context">Context to pass into translation system</param>
+    /// <returns>
+    ///     The language appropriate message if available, otherwise the messageId is returned.
+    /// </returns>
+    public static string GetStringWithEntity(string messageId, EntityUid entity, object context)
+        => throw new NotImplementedException();
+
+    /// <inheritdoc cref="GetString(String, object)"/>
+    public static string GetString(string messageId, object context, (string, object) arg0)
+        => throw new NotImplementedException();
+    /// <inheritdoc cref="GetStringWithEntity(String, EntityUid, object)"/>
+    public static string GetStringWithEntity(string messageId, EntityUid entity, object context, (string, object) arg0)
+        => throw new NotImplementedException();
+
+    /// <inheritdoc cref="GetString(String, object)"/>
+    public static string GetString(string messageId, object context, (string, object) arg0, (string, object) arg1)
+        => throw new NotImplementedException();
+    /// <inheritdoc cref="GetStringWithEntity(String, EntityUid, object)"/>
+    public static string GetStringWithEntity(string messageId, EntityUid entity, object context, (string, object) arg0, (string, object) arg1)
+        => throw new NotImplementedException();
+
+    /// <inheritdoc cref="GetString(String, object)"/>
+    public static string GetString(string messageId, object context, (string, object) arg0, (string, object) arg1, (string, object) arg2)
+        => throw new NotImplementedException();
+    /// <inheritdoc cref="GetStringWithEntity(String, EntityUid, object)"/>
+    public static string GetStringWithEntity(string messageId, EntityUid entity, object context, (string, object) arg0, (string, object) arg1, (string, object) arg2)
+        => throw new NotImplementedException();
 }
