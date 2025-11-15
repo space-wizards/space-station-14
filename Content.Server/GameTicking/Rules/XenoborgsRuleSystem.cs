b24a3f66d50a40f7097caedf5d2360f9834f1cf5
Samuel-C <samuelcamilo8@gmail.com>
2025-11-15 00:42:19 -0300

    no longer calls evac if evac is called or if the round is over

--- Content.Server/GameTicking/Rules/XenoborgsRuleSystem.cs
+++ Content.Server/GameTicking/Rules/XenoborgsRuleSystem.cs
@@ -92,7 +92,7 @@ public sealed class XenoborgsRuleSystem : GameRuleSystem<XenoborgsRuleComponent>
         var numXenoborgs = GetNumberXenoborgs();
         var numHumans = _mindSystem.GetAliveHumans().Count;
 
-        if ((float)numXenoborgs / numHumans > xenoborgsRuleComponent.XenoborgShuttleCallPercentage)
+        if ((float)numXenoborgs / numHumans > xenoborgsRuleComponent.XenoborgShuttleCallPercentage && !_roundEnd.IsRoundEndRequested())
         {
             foreach (var station in _station.GetStations())
             {
