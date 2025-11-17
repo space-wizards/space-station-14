243f2f35224fd484a086d35a73352c6bb8e3b207
iaada <iaada@users.noreply.github.com>
2025-11-17 01:27:49 -0600

    ideas

---
+++ Content.Shared/Trigger/Components/Effects/EmptyContentsOnTriggerComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Trigger.Components.Effects;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class EmptyContentsOnTriggerComponent : Component
+{
+    
+}
---
+++ Content.Shared/Trigger/Components/Triggers/TriggerOnThrowDoHitComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Trigger.Components.Triggers;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class TriggerOnThrowDoHitComponent : Component
+{
+    
+}
