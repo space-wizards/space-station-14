b2eee2ad25d488f4fa1d168d13762f6ec1027a89
JrInventor05Next <dorokhovartem2010@gmail.com>
2025-11-18 17:33:22 +0800

    fix

--- Content.Client/Weather/WeatherSystem.cs
+++ Content.Client/Weather/WeatherSystem.cs
@@ -130,6 +130,18 @@ public sealed class WeatherSystem : SharedWeatherSystem
         if (!Timing.IsFirstTimePredicted)
             return true;
 
+        if (_playerManager.LocalEntity is not {} ent)
+            return false;
+
+        var map = Transform(uid).MapUid;
+        var entMap = Transform(ent).MapUid;
+
+        if (map == null || entMap != map)
+        {
+            weather.Stream = _audio.Stop(weather.Stream);
+            return false;
+        }
+
         // TODO: Fades (properly)
         weather.Stream = _audio.Stop(weather.Stream);
         weather.Stream = _audio.PlayGlobal(weatherProto.Sound, Filter.Local(), true)?.Entity;
