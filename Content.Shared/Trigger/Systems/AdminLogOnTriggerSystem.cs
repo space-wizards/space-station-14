0d435a825ae70a77011994b4f63fbab967dff085
iaada <iaada@users.noreply.github.com>
2025-11-18 19:07:03 -0600

    requested

--- Content.Shared/Trigger/Systems/AdminLogOnTriggerSystem.cs
+++ Content.Shared/Trigger/Systems/AdminLogOnTriggerSystem.cs
@@ -12,6 +12,8 @@ public sealed class AdminLogOnTriggerSystem : XOnTriggerSystem<AdminLogOnTrigger
         _adminLogger.Add(
             ent.Comp.LogType,
             ent.Comp.LogImpact,
-            $"{ToPrettyString(args.User):actor} sent a trigger using {ToPrettyString(ent):tool}: {Loc.GetString(ent.Comp.Message)}");
+            $"{ToPrettyString(args.User)} sent a trigger using {ToPrettyString(ent)}: {Loc.GetString(ent.Comp.Message)}"
+            );
+        // Intentionally does not handle the event since this shouldn't affect the gamestate.
     }
 }
