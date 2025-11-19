ee8f5bfa5f0c0e0bab3d2a42527fddac58ceb9e9
iaada <iaada@users.noreply.github.com>
2025-11-18 19:45:14 -0600

    comment

--- Content.Shared/Trigger/Components/Effects/SwapLocationOnTriggerComponent.cs
+++ Content.Shared/Trigger/Components/Effects/SwapLocationOnTriggerComponent.cs
@@ -4,6 +4,7 @@ namespace Content.Shared.Trigger.Components.Effects;
 
 /// <summary>
 /// Swaps the location of the target and the user of the trigger when triggered.
+/// <see cref="BaseXOnTriggerComponent.TargetUser"/> is ignored.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class SwapLocationOnTriggerComponent : BaseXOnTriggerComponent;
