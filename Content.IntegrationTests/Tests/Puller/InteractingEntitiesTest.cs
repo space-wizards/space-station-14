bc9566072fa17fef634b51d43b5f4268e400f3a8
Ciarán Walsh <git@ciaranwal.sh>
2025-11-21 08:40:52 +0000

    Move new test into separate (InteractionTest) test file

---
+++ Content.IntegrationTests/Tests/Puller/InteractingEntitiesTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Content.IntegrationTests.Tests.Interaction;
+using Content.Shared.Interaction;
+using Content.Shared.Movement.Pulling.Systems;
+using Robust.Shared.GameObjects;
+
+namespace Content.IntegrationTests.Tests.Puller;
+
+#nullable enable
+
+[TestFixture]
+public sealed class InteractingEntitiesTest : InteractionTest
+{
+    [Test]
+    public async Task PullerIsConsideredInteractingTest()
+    {
+        await SpawnTarget("MobHuman");
+        var puller = await SpawnEntity("MobHuman", SEntMan.GetCoordinates(TargetCoords));
+
+        var pullSys = SEntMan.System<PullingSystem>();
+        await Server.WaitPost(() => pullSys.TryStartPull(puller, SEntMan.GetEntity(Target.Value)));
+
+        var list = new HashSet<EntityUid>();
+        Server.System<SharedInteractionSystem>()
+            .GetEntitiesInteractingWithTarget(SEntMan.GetEntity(Target.Value), list);
+        Assert.That(list, Is.EquivalentTo([puller]));
+    }
+}
--- Content.IntegrationTests/Tests/Puller/PullerTest.cs
+++ Content.IntegrationTests/Tests/Puller/PullerTest.cs
@@ -1,9 +1,5 @@
-using System.Collections.Generic;
-using Content.IntegrationTests.Tests.Interaction;
 using Content.Shared.Hands.Components;
-using Content.Shared.Interaction;
 using Content.Shared.Movement.Pulling.Components;
-using Content.Shared.Movement.Pulling.Systems;
 using Content.Shared.Prototypes;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Prototypes;
@@ -13,7 +9,7 @@ namespace Content.IntegrationTests.Tests.Puller;
 #nullable enable
 
 [TestFixture]
-public sealed class PullerTest : InteractionTest
+public sealed class PullerTest
 {
     /// <summary>
     /// Checks that needsHands on PullerComponent is not set on mobs that don't even have hands.
@@ -45,20 +41,5 @@ public sealed class PullerTest : InteractionTest
         });
 
         await pair.CleanReturnAsync();
-    }
-
-    [Test]
-    public async Task PullerIsConsideredInteractingTest()
-    {
-        await SpawnTarget("MobHuman");
-        var puller = await SpawnEntity("MobHuman", SEntMan.GetCoordinates(TargetCoords));
-
-        var pullSys = SEntMan.System<PullingSystem>();
-        await Server.WaitPost(() => pullSys.TryStartPull(puller, SEntMan.GetEntity(Target.Value)));
-
-        var list = new HashSet<EntityUid>();
-        Server.System<SharedInteractionSystem>()
-            .GetEntitiesInteractingWithTarget(SEntMan.GetEntity(Target.Value), list);
-        Assert.That(list, Is.EquivalentTo([puller]));
     }
 }
