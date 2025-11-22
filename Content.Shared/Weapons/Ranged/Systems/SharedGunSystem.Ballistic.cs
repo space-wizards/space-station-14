1642270cbac5f62c408f959319d324383531c121
Samuka <47865393+Samuka-C@users.noreply.github.com>
2025-11-22 01:33:20 -0300

    update todo
    Co-authored-by: āda <ss.adasts@gmail.com>

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
@@ -59,7 +59,7 @@ public abstract partial class SharedGunSystem
         {
             _stack.ReduceCount((args.Used, stackComp), 1);
 
-            // TODO: call a method from the stack system when it gets predicted
+            // TODO: call a spawn method from StackSystem when it gets predicted
             if (!ProtoManager.Resolve(stackComp.StackTypeId, out var stackType))
                 return;
 
