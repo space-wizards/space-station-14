35d92127d28b8eb349b7c8f9a8364a744aed4493
alexalexmax <alexalexmax123@gmail.com>
2025-11-24 12:15:19 -0800

    new method for downing + dropping held items in mobstatesystem

--- Content.Shared/Mobs/Systems/MobStateSystem.Subscribers.cs
+++ Content.Shared/Mobs/Systems/MobStateSystem.Subscribers.cs
@@ -59,6 +59,13 @@ public partial class MobStateSystem
             args.Cancelled = true;
     }
 
+    private void Down(EntityUid target)
+    {
+        _standing.Down(target);
+        var ev = new DropHandItemsEvent();
+        RaiseLocalEvent(target, ref ev, false);
+    }
+
     private void CheckConcious(Entity<MobStateComponent> ent, ref ConsciousAttemptEvent args)
     {
         switch (ent.Comp.CurrentState)
@@ -110,18 +117,14 @@ public partial class MobStateSystem
             }
             case MobState.Critical:
             {
-                _standing.Down(target);
-                var ev = new DropHandItemsEvent();
-                RaiseLocalEvent(target, ref ev, false);
+                Down(target);
                 _appearance.SetData(target, MobStateVisuals.State, MobState.Critical);
                 break;
             }
             case MobState.Dead:
             {
                 EnsureComp<CollisionWakeComponent>(target);
-                _standing.Down(target);
-                var ev = new DropHandItemsEvent();
-                RaiseLocalEvent(target, ref ev, false);
+                Down(target);
                 _appearance.SetData(target, MobStateVisuals.State, MobState.Dead);
                 break;
             }
