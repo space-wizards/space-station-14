a954cd95ce262aed3e893963224c4ce860cfe45c
Pok245 <113675512+Pok27@users.noreply.github.com>
2025-11-27 19:03:18 +0200

    get-charge-percent

--- Content.Shared/Power/EntitySystems/PredictedBatterySystem.API.cs
+++ Content.Shared/Power/EntitySystems/PredictedBatterySystem.API.cs
@@ -175,6 +175,18 @@ public sealed partial class PredictedBatterySystem
     }
 
     /// <summary>
+    /// Gets the fraction of charge remaining (0–1).
+    /// </summary>
+    [PublicAPI]
+    public float GetChargeLevel(Entity<PredictedBatteryComponent?> ent)
+    {
+        if (!Resolve(ent, ref ent.Comp, false))
+            return 0f;
+
+        return GetCharge(ent) / ent.Comp.MaxCharge;
+    }
+
+    /// <summary>
     /// Gets number of remaining uses for the given charge cost.
     /// </summary>
     [PublicAPI]
