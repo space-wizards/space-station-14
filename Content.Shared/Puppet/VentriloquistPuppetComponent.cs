7a63de8c8c74c32243d394dc4e4d6626e6b89a66
Pok245 <113675512+Pok27@users.noreply.github.com>
2025-11-28 20:51:25 +0200

    VentriloquistPuppetSystem-move-to-shared

--- Content.Shared/Puppet/VentriloquistPuppetComponent.cs
+++ Content.Shared/Puppet/VentriloquistPuppetComponent.cs
@@ -2,7 +2,16 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.Puppet;
 
-[RegisterComponent, NetworkedComponent]
+/// <summary>
+/// This component allows an entity to be a ventriloquist puppet.
+/// When used in hand, the entity will speak and the user will be able to hear the entity's voice.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class VentriloquistPuppetComponent : Component
 {
+    /// <summary>
+    /// Whether a player's hand is currently inserted into the puppet.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool HasHandInserted = false;
 }
