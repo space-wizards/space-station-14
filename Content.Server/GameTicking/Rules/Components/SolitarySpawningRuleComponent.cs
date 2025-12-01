0f6f2898e03a3410967415457a2dcf13c765a6d4
Errant <35878406+Errant-4@users.noreply.github.com>
2025-12-01 16:51:15 +0100

    Update Content.Server/GameTicking/Rules/Components/SolitarySpawningRuleComponent.cs
    Co-authored-by: Nikita (Nick) <174215049+nikitosych@users.noreply.github.com>

--- Content.Server/GameTicking/Rules/Components/SolitarySpawningRuleComponent.cs
+++ Content.Server/GameTicking/Rules/Components/SolitarySpawningRuleComponent.cs
@@ -14,7 +14,7 @@ public sealed partial class SolitarySpawningRuleComponent : Component
     /// The list of spawn profiles available. The lobby can be configured to allow the player to pick one of the options.
     /// If no player choice is made (or possible), the first prototoype will be chosen.
     /// </summary>
-    [DataField(rquired: true)]
+    [DataField(required: true)]
     public List<ProtoId<SolitarySpawningPrototype>> Prototypes;
 
     //TODO blacklist/whitelist for which player is covered by this rule
