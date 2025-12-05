6f9cce644177725f35a70f0ae968b24130adf1cc
sleepyyapril <flyingkarii@gmail.com>
2025-12-04 21:51:47 -0400

    feat: StopEmbedEvent

--- Content.Shared/Projectiles/EmbedEvents.cs
+++ Content.Shared/Projectiles/EmbedEvents.cs
@@ -13,3 +13,17 @@ public readonly record struct EmbedEvent(EntityUid? Shooter, EntityUid Embedded)
     /// </summary>
     public readonly EntityUid Embedded = Embedded;
 }
+
+/// <summary>
+/// Raised directed on an entity when it stops embedding in another entity.
+/// </summary>
+[ByRefEvent]
+public readonly record struct StopEmbedEvent(EntityUid? Detacher, EntityUid Embedded)
+{
+    public readonly EntityUid? Detacher = Detacher;
+
+    /// <summary>
+    /// Entity that it is embedded in.
+    /// </summary>
+    public readonly EntityUid Embedded = Embedded;
+}
