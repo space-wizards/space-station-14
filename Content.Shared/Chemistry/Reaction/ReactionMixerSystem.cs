87cd0e88d0981deb7b0710f527e60d7086e10ce0
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-12-05 19:38:52 +0100

    documentation

--- Content.Shared/Chemistry/Reaction/ReactionMixerSystem.cs
+++ Content.Shared/Chemistry/Reaction/ReactionMixerSystem.cs
@@ -1,3 +1,4 @@
+using Content.Shared.Chemistry.Components;
 using Content.Shared.DoAfter;
 using Content.Shared.IdentityManagement;
 using Content.Shared.Interaction;
@@ -10,8 +11,8 @@ namespace Content.Shared.Chemistry.Reaction;
 public sealed partial class ReactionMixerSystem : EntitySystem
 {
     [Dependency] private readonly SharedPopupSystem _popup = default!;
-    [Dependency] private readonly SharedSolutionContainerSystem _solutionContainers = default!;
-    [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
+    [Dependency] private readonly SharedSolutionContainerSystem _solutionContainer = default!;
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
 
     public override void Initialize()
     {
@@ -32,7 +33,7 @@ public sealed partial class ReactionMixerSystem : EntitySystem
 
         var doAfterArgs = new DoAfterArgs(EntityManager, args.User, ent.Comp.TimeToMix, new ReactionMixDoAfterEvent(), ent, args.Target.Value, ent);
 
-        _doAfterSystem.TryStartDoAfter(doAfterArgs);
+        _doAfter.TryStartDoAfter(doAfterArgs);
         args.Handled = true;
     }
 
@@ -56,6 +57,11 @@ public sealed partial class ReactionMixerSystem : EntitySystem
         TryMix(ent.AsNullable(), ent);
     }
 
+    /// <summary>
+    /// Returns if the given reaction mixer is able to able to mix the solution inside the target entity.
+    /// </summary>
+    /// <param name="ent">The reaction mixer used to cause the reaction.</param>
+    /// <param name="target">The target solution container with a <see cref="MixableSolutionComponent"/>.</param>
     public bool CanMix(Entity<ReactionMixerComponent?> ent, EntityUid target)
     {
         if (!Resolve(ent, ref ent.Comp, false)) // The used entity needs the component to be able to mix a solution
@@ -66,12 +72,18 @@ public sealed partial class ReactionMixerSystem : EntitySystem
         if (mixAttemptEvent.Cancelled)
             return false;
 
-        if (!_solutionContainers.TryGetMixableSolution(target, out _, out _))
+        if (!_solutionContainer.TryGetMixableSolution(target, out _, out _))
             return false;
 
         return true;
     }
 
+    /// <summary>
+    /// Attempts to mix the solution inside the target entity using the given reaction mixer.
+    /// </summary>
+    /// <param name="ent">The reaction mixer used to cause the reaction.</param>
+    /// <param name="target">The target solution container with a <see cref="MixableSolutionComponent"/>.</param>
+    /// <returns>If the reaction mixer was able to mix the solution. This does not necessarily mean a reaction took place.</returns>
     public bool TryMix(Entity<ReactionMixerComponent?> ent, EntityUid target)
     {
         if (!Resolve(ent, ref ent.Comp))
@@ -82,10 +94,10 @@ public sealed partial class ReactionMixerSystem : EntitySystem
         if (mixAttemptEvent.Cancelled)
             return false;
 
-        if (!_solutionContainers.TryGetMixableSolution(target, out var solutionEnt, out _))
+        if (!_solutionContainer.TryGetMixableSolution(target, out var solutionEnt, out _))
             return false;
 
-        _solutionContainers.UpdateChemicals(solutionEnt.Value, true, ent.Comp);
+        _solutionContainer.UpdateChemicals(solutionEnt.Value, true, ent.Comp);
 
         var afterMixingEvent = new AfterMixingEvent(ent, target);
         RaiseLocalEvent(ent, ref afterMixingEvent);
