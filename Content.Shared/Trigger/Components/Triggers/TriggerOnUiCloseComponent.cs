083502caa19aacedec6f8a2ca0bc3912ac6397f1
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-12-05 21:10:19 -0400

    Update Content.Shared/Trigger/Components/Triggers/TriggerOnUiCloseComponent.cs
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Shared/Trigger/Components/Triggers/TriggerOnUiCloseComponent.cs
+++ Content.Shared/Trigger/Components/Triggers/TriggerOnUiCloseComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Trigger.Components.Triggers;
 
 /// <summary>
-/// Triggers when a user closes a UI.
+/// Triggers when a user closes a UI belonging to the owning entity.
 /// The user is the actor that tries to open a UI.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
