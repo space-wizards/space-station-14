89ecb6764f9e7b0992e812e2fe4a007ac75d4832
UpAndLeaves <92269094+Alpha-Two@users.noreply.github.com>
2025-12-07 18:17:54 +0000

    Requested Changes

--- Content.Server/GameTicking/GameTicker.RoundFlow.cs
+++ Content.Server/GameTicking/GameTicker.RoundFlow.cs
@@ -393,7 +393,9 @@ namespace Content.Server.GameTicking
                 }
                 else
                 {
-                    profile = HumanoidCharacterProfile.Random(new HashSet<string>(_cfg.GetCVar(CCVars.ICNewAccountSpeciesBlacklist).Split(",")));
+                    var speciesToBlacklist =
+                        new HashSet<string>(_cfg.GetCVar(CCVars.ICNewAccountSpeciesBlacklist).Split(","));
+                    profile = HumanoidCharacterProfile.Random(speciesToBlacklist);
                 }
                 readyPlayerProfiles.Add(userId, profile);
             }
