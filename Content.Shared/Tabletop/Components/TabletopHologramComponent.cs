9a16215f83b5030676a18731d202e79f11ef5721
Pok245 <113675512+Pok27@users.noreply.github.com>
2025-12-08 14:16:42 +0200

    Update TabletopHologramComponent.cs

--- Content.Shared/Tabletop/Components/TabletopHologramComponent.cs
+++ Content.Shared/Tabletop/Components/TabletopHologramComponent.cs
@@ -1,6 +1,6 @@
 using Robust.Shared.GameStates;
 
-namespace Content.Shared.Tabletop;
+namespace Content.Shared.Tabletop.Components;
 
 /// <summary>
 /// This is used for tracking pieces that are simply "holograms" shown on the tabletop
