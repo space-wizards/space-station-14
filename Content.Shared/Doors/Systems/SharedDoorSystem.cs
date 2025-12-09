c7d1b87d30ae0470ce3d4d0c5697e461e7301498
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2025-12-09 02:26:58 -0800

    One line bugefix

--- Content.Shared/Doors/Systems/SharedDoorSystem.cs
+++ Content.Shared/Doors/Systems/SharedDoorSystem.cs
@@ -368,7 +368,7 @@ public abstract partial class SharedDoorSystem : EntitySystem
             Audio.PlayPvs(door.OpenSound, uid, AudioParams.Default.WithVolume(-5));
 
         if (lastState == DoorState.Emagging && TryComp<DoorBoltComponent>(uid, out var doorBoltComponent))
-            SetBoltsDown((uid, doorBoltComponent), !doorBoltComponent.BoltsDown, user, true);
+            SetBoltsDown((uid, doorBoltComponent), true, user, true);
     }
 
     /// <summary>
