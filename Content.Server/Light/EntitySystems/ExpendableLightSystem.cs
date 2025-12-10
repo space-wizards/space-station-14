44401d9cba4f05eef039af4cf12120507b718d04
murphyneko <killerqueen1777@gmail.com>
2025-12-10 20:17:59 +0100

    fix merge conflict

--- Content.Server/Light/EntitySystems/ExpendableLightSystem.cs
+++ Content.Server/Light/EntitySystems/ExpendableLightSystem.cs
@@ -150,14 +150,12 @@ namespace Content.Server.Light.EntitySystems
                 return;
 
             component.StateExpiryTime += (float)component.RefuelMaterialTime.TotalSeconds;
-            _stackSystem.SetCount(args.Used, stack.Count - 1, stack);
-
             switch (component.CurrentState)
             {
                 case ExpendableLightState.Dead:
                     component.CurrentState = ExpendableLightState.BrandNew;
                     _nameModifier.RefreshNameModifiers(uid);
-                    _stackSystem.SetCount(args.Used, stack.Count - 1, stack);
+                    _stackSystem.ReduceCount((args.Used, stack), 1);
                     return;
                 case ExpendableLightState.Fading:
                     component.CurrentState = ExpendableLightState.Lit;
