454f97765e165c238dbc14cd81f9766cd37d3db7
alexalexmax <alexalexmax123@gmail.com>
2025-12-26 06:19:32 -0800

    predict TradeStationComponent

--- Content.Shared/Cargo/Components/TradeStationComponent.cs
+++ Content.Shared/Cargo/Components/TradeStationComponent.cs
@@ -1,9 +1,11 @@
-namespace Content.Server.Cargo.Components;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Cargo.Components;
 
 /// <summary>
 /// Target for approved orders to spawn at.
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class TradeStationComponent : Component
 {
 
