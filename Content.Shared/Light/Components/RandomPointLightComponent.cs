8b96ada7c05651ad1763f7e096dcd2bc4a945f97
ActiveMammmoth <kmcsmooth@gmail.com>
2025-12-27 09:45:22 -0500

    guard statement

--- Content.Shared/Light/Components/RandomPointLightComponent.cs
+++ Content.Shared/Light/Components/RandomPointLightComponent.cs
@@ -3,32 +3,32 @@ using Robust.Shared.GameStates;
 namespace Content.Shared.Light.Components;
 
 /// <summary>
-/// Gives a point light random radius, energy and color when added to an entity with a point light
+/// Gives a point light random radius, energy and color when added to an entity with a <see cref="SharedPointLightComponent"/>
 /// </summary>
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class RandomPointLightComponent : Component
 {
     /// <summary>
     /// Maximum value for the random radius
     /// </summary>
-    [DataField, AutoNetworkedField]
+    [DataField]
     public float MaxRadius = 6;
 
     /// <summary>
     /// Minimum value for the random radius
     /// </summary>
-    [DataField, AutoNetworkedField]
-    public float MinRadius = 1;
+    [DataField]
+    public float MinRadius = 3;
 
     /// <summary>
     /// Maximum value for the random energy
     /// </summary>
-    [DataField, AutoNetworkedField]
+    [DataField]
     public float MaxEnergy = 5;
 
     /// <summary>
     /// Minimum value for the random energy
     /// </summary>
-    [DataField, AutoNetworkedField]
+    [DataField]
     public float MinEnergy = 1;
 }
--- Content.Shared/Light/EntitySystems/RandomPointLightSystem.cs
+++ Content.Shared/Light/EntitySystems/RandomPointLightSystem.cs
@@ -1,26 +1,32 @@
 using System.Numerics;
 using Robust.Shared.Random;
 using Content.Shared.Light.Components;
+using Robust.Shared.Timing;
 
 namespace Content.Shared.Light.EntitySystems;
 
 /// <summary>
-/// System for assigning random values to point light variables when given RandomPointLightComponent
+/// System for assigning random values to <see cref="SharedPointLightComponent"/> variables when given <see cref="RandomPointLightComponent"/>
 /// </summary>
 public sealed class RandomPointLightSystem : EntitySystem
 {
+    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly SharedPointLightSystem _light = default!;
+
 
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<RandomPointLightComponent, MapInitEvent>(RandomLight);
+        SubscribeLocalEvent<RandomPointLightComponent, ComponentStartup>(RandomLight);
     }
 
-    private void RandomLight(Entity<RandomPointLightComponent> ent, ref MapInitEvent args)
+    private void RandomLight(Entity<RandomPointLightComponent> ent, ref ComponentStartup args)
     {
+        if (_timing.ApplyingState)
+            return;
+
         var rpl = ent.Comp;
 
         // Keeping value between 0.5 and 1.0 so that it's always bright
