abebc83138d7ef6d9738cedcf00f0b56f440971b
Drywink <43855731+Drywink@users.noreply.github.com>
2025-12-27 19:47:27 +0100

    Update BiteSystem.cs

--- Content.Server/Species/Systems/BiteSystem.cs
+++ Content.Server/Species/Systems/BiteSystem.cs
@@ -1,8 +1,3 @@
-// SPDX-FileCopyrightText: 2025 Drywink <43855731+Drywink@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Drywink <hugogrethen@gmail.com>
-//
-// SPDX-License-Identifier: MIT
-
 using Content.Server.Popups;
 using Content.Shared.Actions;
 using Content.Shared.Chemistry.Components;
