dcf7769dab3c44a3bc8a4a494acbf3aa6ea8d1ec
EchoOfNothing <52498373+EchoOfNothing@users.noreply.github.com>
2025-12-30 23:53:01 +0200

    Made starting thirst no longer hardcoded

--- Content.Shared/Nutrition/Components/ThirstComponent.cs
+++ Content.Shared/Nutrition/Components/ThirstComponent.cs
@@ -20,6 +20,20 @@ public sealed partial class ThirstComponent : Component
     [AutoNetworkedField]
     public float ActualDecayRate;
 
+    /// <summary>
+    /// The minimum possible starting thirst value.
+    /// If it is less than 0, it will be set in <see cref="ThirstSystem.OnMapInit"/>
+    /// </summary>
+    [DataField]
+    public float MinStartingThirst = -1;
+
+    /// <summary>
+    /// The maximum possible starting thirst value.
+    /// If it is less than 0, it will be set in <see cref="ThirstSystem.OnMapInit"/>
+    /// </summary>
+    [DataField]
+    public float MaxStartingThirst = -1;
+
     [DataField, AutoNetworkedField, ViewVariables(VVAccess.ReadWrite)]
     public ThirstThreshold CurrentThirstThreshold;
 
--- Content.Shared/Nutrition/EntitySystems/ThirstSystem.cs
+++ Content.Shared/Nutrition/EntitySystems/ThirstSystem.cs
@@ -38,12 +38,20 @@ public sealed class ThirstSystem : EntitySystem
 
     private void OnMapInit(EntityUid uid, ThirstComponent component, MapInitEvent args)
     {
+        if (component.MinStartingThirst < 0)
+        {
+            component.MinStartingThirst = component.ThirstThresholds[ThirstThreshold.Thirsty] + 10;
+        }
+        if (component.MaxStartingThirst < 0)
+        {
+            component.MaxStartingThirst = component.ThirstThresholds[ThirstThreshold.Okay] - 1;
+        }
         // Do not change behavior unless starting value is explicitly defined
         if (component.CurrentThirst < 0)
         {
             component.CurrentThirst = _random.Next(
-                (int) component.ThirstThresholds[ThirstThreshold.Thirsty] + 10,
-                (int) component.ThirstThresholds[ThirstThreshold.Okay] - 1);
+                (int) component.MinStartingThirst,
+                (int) component.MaxStartingThirst);
 
             DirtyField(uid, component, nameof(ThirstComponent.CurrentThirst));
         }
