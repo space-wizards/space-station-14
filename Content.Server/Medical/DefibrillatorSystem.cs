249fc0ca8d994c0109d7f1b5bccef21bb0b194ec
Connor Huffine <chuffine@gmail.com>
2025-12-30 21:09:49 -0500

    CS0105

--- Content.Server/Medical/DefibrillatorSystem.cs
+++ Content.Server/Medical/DefibrillatorSystem.cs
@@ -5,7 +5,6 @@ using Content.Server.Electrocution;
 using Content.Server.EUI;
 using Content.Server.Ghost;
 using Content.Server.Popups;
-using Content.Shared.PowerCell;
 using Content.Shared.Traits.Assorted;
 using Content.Shared.Chat;
 using Content.Shared.Damage.Components;
