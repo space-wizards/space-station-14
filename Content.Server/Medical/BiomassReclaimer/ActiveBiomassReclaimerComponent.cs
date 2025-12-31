2e74d9b86c43a6842a84856395e41cc7d7737b43
Connor Huffine <chuffine@gmail.com>
2025-12-30 23:00:09 -0500

    Biomass reclaimer

--- Content.Server/Medical/BiomassReclaimer/ActiveBiomassReclaimerComponent.cs
+++ Content.Server/Medical/BiomassReclaimer/ActiveBiomassReclaimerComponent.cs
@@ -1,6 +1,13 @@
+using Robust.Shared.Audio;
+
 namespace Content.Server.Medical.BiomassReclaimer
 {
     [RegisterComponent]
     public sealed partial class ActiveBiomassReclaimerComponent : Component
-    {}
+    {
+        /// <summary>
+        /// Sound to be played when the reclaimer is activated.
+        /// </summary>
+        public readonly SoundSpecifier StartupSound = new SoundPathSpecifier("/Audio/Machines/reclaimer_startup.ogg");
+    }
 }
