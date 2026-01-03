b08d1ef33585b93a8ce94be2b187b4b12260d9f8
shab00m <tobias.sjobom@gmail.com>
2026-01-03 12:50:52 +0100

    rng device initial commit ported from frontier

---
+++ Content.Shared/Dice/D20DieComponent.cs
@@ -0,0 +1,4 @@
+namespace Content.Shared.Dice;
+
+[RegisterComponent]
+public sealed partial class D20DieComponent : Component; //Frontier: This is here in order to be able to use a specific type of die in construction graphs.
