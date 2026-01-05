b395e3fe4ee086a35a3c531333701e2e38d687e3
SlamBamActionman <slambamactionman@gmail.com>
2026-01-05 16:09:40 +0100

    Rename to cooldown since that is more accurate than "delay"

--- Content.Shared/Weapons/Ranged/Components/AmmoProviderInsertionCooldownComponent.cs
+++ Content.Shared/Weapons/Ranged/Components/AmmoProviderInsertionCooldownComponent.cs
@@ -5,18 +5,18 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 namespace Content.Shared.Weapons.Ranged.Components;
 
 /// <summary>
-/// Ensures a gun can not be reloaded faster than the given <see cref="InsertDelay"/>.
+/// Ensures a gun can not be reloaded faster than the given <see cref="InsertCooldown"/>.
 /// Currently compatible with <see cref="BallisticAmmoProviderComponent"/> and <see cref="RevolverAmmoProviderComponent"/>.
 /// Is ignored by <see cref="BallisticAmmoSelfRefillerComponent"/>.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState(fieldDeltas: true), Access(typeof(SharedGunSystem)), AutoGenerateComponentPause]
-public sealed partial class AmmoProviderInsertionDelayComponent : Component
+public sealed partial class AmmoProviderInsertionCooldownComponent : Component
 {
     /// <summary>
     /// The minimum time in between insertions.
     /// </summary>
     [DataField]
-    public TimeSpan InsertDelay = TimeSpan.FromSeconds(0.5f);
+    public TimeSpan InsertCooldown = TimeSpan.FromSeconds(0.5f);
 
     /// <summary>
     /// Last time ammo was inserted into this provider.
--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.InsertionCooldown.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.InsertionCooldown.cs
@@ -7,7 +7,7 @@ public abstract partial class SharedGunSystem
     /// <summary>
     /// Returns true if the insertion is allowed to go ahead, and updates the insertion delay if it does.
     /// </summary>
-    public bool ValidateInsertionSpeed(Entity<AmmoProviderInsertionDelayComponent?> entity)
+    public bool ValidateInsertionSpeed(Entity<AmmoProviderInsertionCooldownComponent?> entity)
     {
         if (!Resolve(entity.Owner, ref entity.Comp, logMissing: false)) // No comp? No problem!
             return true;
@@ -26,23 +26,23 @@ public abstract partial class SharedGunSystem
     /// <summary>
     /// Returns true if the insertion is happening too fast and should fail.
     /// </summary>
-    public bool IsInsertionTooFast(Entity<AmmoProviderInsertionDelayComponent?> entity)
+    public bool IsInsertionTooFast(Entity<AmmoProviderInsertionCooldownComponent?> entity)
     {
         if (!Resolve(entity.Owner, ref entity.Comp))
             return false;
 
-        return entity.Comp.LastInsertion + entity.Comp.InsertDelay > Timing.CurTime;
+        return entity.Comp.LastInsertion + entity.Comp.InsertCooldown > Timing.CurTime;
     }
 
     /// <summary>
     /// Updates the last insertion value to the current time.
     /// </summary>
-    public void UpdateLastInsertion(Entity<AmmoProviderInsertionDelayComponent?> entity)
+    public void UpdateLastInsertion(Entity<AmmoProviderInsertionCooldownComponent?> entity)
     {
         if (!Resolve(entity.Owner, ref entity.Comp))
             return;
 
         entity.Comp.LastInsertion = Timing.CurTime;
-        DirtyField(entity.AsNullable(), nameof(AmmoProviderInsertionDelayComponent.LastInsertion));
+        DirtyField(entity.AsNullable(), nameof(AmmoProviderInsertionCooldownComponent.LastInsertion));
     }
 }
