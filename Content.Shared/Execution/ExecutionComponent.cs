8838bdf1a91899fc24998a6f140737a49481f3cc
SpeltIncorrectyl <speltincorrectyl0001@gmail.com>
2026-01-09 02:07:51 +0000

    fixed tests fr this time

--- Content.Shared/Execution/ExecutionComponent.cs
+++ Content.Shared/Execution/ExecutionComponent.cs
@@ -26,47 +26,47 @@ public sealed partial class ExecutionComponent : Component
     /// Shown to the person performing the melee execution (attacker) upon starting a melee execution.
     /// </summary>
     [DataField]
-    public LocId InternalMeleeExecutionMessage = string.Empty;
+    public LocId InternalMeleeExecutionMessage = "execution-popup-melee-slash-initial-internal";
 
     /// <summary>
     /// Shown to bystanders and the victim of a melee execution when a melee execution is started.
     /// </summary>
     [DataField]
-    public LocId ExternalMeleeExecutionMessage = string.Empty;
+    public LocId ExternalMeleeExecutionMessage = "execution-popup-melee-slash-initial-external";
 
     /// <summary>
     /// Shown to the attacker upon completion of a melee execution.
     /// </summary>
     [DataField]
-    public LocId CompleteInternalMeleeExecutionMessage = string.Empty;
+    public LocId CompleteInternalMeleeExecutionMessage = "execution-popup-melee-slash-complete-internal";
 
     /// <summary>
     /// Shown to bystanders and the victim of a melee execution when a melee execution is completed.
     /// </summary>
     [DataField]
-    public LocId CompleteExternalMeleeExecutionMessage = string.Empty;
+    public LocId CompleteExternalMeleeExecutionMessage = "execution-popup-melee-slash-complete-external";
 
     /// <summary>
     /// Shown to the person performing the self execution when starting one.
     /// </summary>
     [DataField]
-    public LocId InternalSelfExecutionMessage = string.Empty;
+    public LocId InternalSelfExecutionMessage = "execution-popup-melee-slash-self-initial-internal";
 
     /// <summary>
     /// Shown to bystanders near a self execution when one is started.
     /// </summary>
     [DataField]
-    public LocId ExternalSelfExecutionMessage = string.Empty;
+    public LocId ExternalSelfExecutionMessage = "execution-popup-melee-slash-self-initial-external";
 
     /// <summary>
     /// Shown to the person performing a self execution upon completion of a do-after or on use of /suicide with a weapon that has the Execution component.
     /// </summary>
     [DataField]
-    public LocId CompleteInternalSelfExecutionMessage = string.Empty;
+    public LocId CompleteInternalSelfExecutionMessage = "execution-popup-melee-slash-self-complete-internal";
 
     /// <summary>
     /// Shown to bystanders when a self execution is completed or a suicide via execution weapon happens nearby.
     /// </summary>
     [DataField]
-    public LocId CompleteExternalSelfExecutionMessage = string.Empty;
+    public LocId CompleteExternalSelfExecutionMessage = "execution-popup-melee-slash-self-complete-external";
 }
