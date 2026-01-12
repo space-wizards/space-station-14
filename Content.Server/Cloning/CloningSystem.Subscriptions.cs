dedcea1243dd2b60b965acf89c864bec635776c0
Sir Warock <67167466+SirWarock@users.noreply.github.com>
2026-01-12 14:04:20 +0100

    One commit ops

--- Content.Server/Cloning/CloningSystem.Subscriptions.cs
+++ Content.Server/Cloning/CloningSystem.Subscriptions.cs
@@ -1,7 +1,8 @@
-using Content.Server.Forensics;
 using Content.Server.Speech.EntitySystems;
 using Content.Shared.Cloning.Events;
 using Content.Shared.FixedPoint;
+using Content.Shared.Forensics.Components;
+using Content.Shared.Forensics.Systems;
 using Content.Shared.Inventory;
 using Content.Shared.Labels.Components;
 using Content.Shared.Labels.EntitySystems;
--- Content.Shared/Forensics/Events.cs
+++ Content.Shared/Forensics/Events.cs
@@ -5,14 +5,13 @@ using Robust.Shared.Serialization;
 namespace Content.Shared.Forensics;
 
 [Serializable, NetSerializable]
-public sealed partial class ForensicScannerDoAfterEvent : SimpleDoAfterEvent
-{
-}
+public sealed partial class ForensicScannerDoAfterEvent : SimpleDoAfterEvent;
 
 [Serializable, NetSerializable]
 public sealed partial class ForensicPadDoAfterEvent : DoAfterEvent
 {
-    [DataField("sample", required: true)] public  string Sample = default!;
+    [DataField(required: true)]
+    public string Sample = default!;
 
     private ForensicPadDoAfterEvent()
     {
@@ -27,31 +26,7 @@ public sealed partial class ForensicPadDoAfterEvent : DoAfterEvent
 }
 
 [Serializable, NetSerializable]
-public sealed partial class CleanForensicsDoAfterEvent : SimpleDoAfterEvent
-{
-}
-
-/// <summary>
-/// An event to apply DNA evidence from a donor onto some recipient.
-/// </summary>
-[ByRefEvent]
-public record struct TransferDnaEvent()
-{
-    /// <summary>
-    /// The entity donating the DNA.
-    /// </summary>
-    public EntityUid Donor;
-
-    /// <summary>
-    /// The entity receiving the DNA.
-    /// </summary>
-    public EntityUid Recipient;
-
-    /// <summary>
-    /// Can the DNA be cleaned off?
-    /// </summary>
-    public bool CanDnaBeCleaned = true;
-}
+public sealed partial class CleanForensicsDoAfterEvent : SimpleDoAfterEvent;
 
 /// <summary>
 /// Raised on an entity when its DNA has been changed.
