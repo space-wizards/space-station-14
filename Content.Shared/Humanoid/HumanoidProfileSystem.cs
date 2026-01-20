8dea8e440ef82928733a043c8b1f59bd7b2f769d
Janet Blackquill <uhhadd@gmail.com>
2026-01-20 12:34:21 -0500

    Fix humanoid appearance voice being broken

--- Content.Shared/Humanoid/HumanoidProfileSystem.cs
+++ Content.Shared/Humanoid/HumanoidProfileSystem.cs
@@ -30,6 +30,9 @@ public sealed class HumanoidProfileSystem : EntitySystem
         ent.Comp.Sex = profile.Sex;
         Dirty(ent);
 
+        var sexChanged = new SexChangedEvent(ent.Comp.Sex, profile.Sex);
+        RaiseLocalEvent(ent, ref sexChanged);
+
         if (TryComp<GrammarComponent>(ent, out var grammar))
         {
             _grammar.SetGender((ent, grammar), profile.Gender);
