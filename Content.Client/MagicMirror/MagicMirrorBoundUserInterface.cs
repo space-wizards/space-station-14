7f7f3b6ef105cfff28265c0e51e651d618dcb006
B_Kirill <153602297+B-Kirill@users.noreply.github.com>
2026-01-20 21:59:58 +1000

    Fix MagicMirror UI (#42544)

--- Content.Client/MagicMirror/MagicMirrorBoundUserInterface.cs
+++ Content.Client/MagicMirror/MagicMirrorBoundUserInterface.cs
@@ -20,6 +20,9 @@ public sealed class MagicMirrorBoundUserInterface : BoundUserInterface
         base.Open();
 
         _window = this.CreateWindow<MagicMirrorWindow>();
+
+        _window.Title = EntMan.GetComponent<MetaDataComponent>(Owner).EntityName;
+
         _window.MarkingsPicker.SetModel(_markingsModel);
 
         _markingsModel.MarkingsChanged += (_, _) =>
