b57c506b33a3dde059a19f58560deebb9562e0cb
Janet Blackquill <uhhadd@gmail.com>
2026-01-22 13:12:51 -0500

    fix crash on master

--- Content.Client/Construction/ConstructionSystem.cs
+++ Content.Client/Construction/ConstructionSystem.cs
@@ -306,6 +306,7 @@ namespace Content.Client.Construction
                 var targetSprite = EnsureComp<SpriteComponent>(dummy);
                 EntityManager.System<AppearanceSystem>().OnChangeData(dummy, targetSprite);
 
+                var addedIndex = 0;
                 for (var i = 0; i < targetSprite.AllLayers.Count(); i++)
                 {
                     if (!targetSprite[i].Visible || !targetSprite[i].RsiState.IsValid)
@@ -316,10 +317,11 @@ namespace Content.Client.Construction
                         state.StateId.Name is null)
                         continue;
 
-                    _sprite.AddBlankLayer((ghost.Value, sprite), i);
-                    _sprite.LayerSetSprite((ghost.Value, sprite), i, new SpriteSpecifier.Rsi(rsi.Path, state.StateId.Name));
-                    sprite.LayerSetShader(i, "unshaded");
-                    _sprite.LayerSetVisible((ghost.Value, sprite), i, true);
+                    _sprite.AddBlankLayer((ghost.Value, sprite), addedIndex);
+                    _sprite.LayerSetSprite((ghost.Value, sprite), addedIndex, new SpriteSpecifier.Rsi(rsi.Path, state.StateId.Name));
+                    sprite.LayerSetShader(addedIndex, "unshaded");
+                    _sprite.LayerSetVisible((ghost.Value, sprite), addedIndex, true);
+                    addedIndex++;
                 }
 
                 Del(dummy);
