263fe107a4ade14c42c9f93a8f16580c19e7b20f
DigitalThaumaturge <247493542+DigitalThaumaturge@users.noreply.github.com>
2026-01-22 23:24:33 -0500

    Implemented

--- Content.Shared/Revenant/EntitySystems/SharedCorporealSystem.cs
+++ Content.Shared/Revenant/EntitySystems/SharedCorporealSystem.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Content.Shared.Movement.Systems;
 using Content.Shared.Revenant.Components;
 using Robust.Shared.Physics.Systems;
+using Content.Shared.Movement.Pulling.Systems;
 
 namespace Content.Shared.Revenant.EntitySystems;
 
@@ -17,6 +18,7 @@ public abstract class SharedCorporealSystem : EntitySystem
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
     [Dependency] private readonly MovementSpeedModifierSystem _movement = default!;
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
+    [Dependency] protected readonly PullingSystem pulling = default!;
 
     public override void Initialize()
     {
@@ -48,14 +50,17 @@ public abstract class SharedCorporealSystem : EntitySystem
 
     public virtual void OnShutdown(EntityUid uid, CorporealComponent component, ComponentShutdown args)
     {
-        _appearance.SetData(uid, RevenantVisuals.Corporeal, false);
-
-        if (TryComp<FixturesComponent>(uid, out var fixtures) && fixtures.FixtureCount >= 1)
+        if (!pulling.IsPulling(uid))
         {
-            var fixture = fixtures.Fixtures.First();
+            _appearance.SetData(uid, RevenantVisuals.Corporeal, false);
 
-            _physics.SetCollisionMask(uid, fixture.Key, fixture.Value, (int) CollisionGroup.GhostImpassable, fixtures);
-            _physics.SetCollisionLayer(uid, fixture.Key, fixture.Value, 0, fixtures);
+            if (TryComp<FixturesComponent>(uid, out var fixtures) && fixtures.FixtureCount >= 1)
+            {
+                var fixture = fixtures.Fixtures.First();
+
+                _physics.SetCollisionMask(uid, fixture.Key, fixture.Value, (int)CollisionGroup.GhostImpassable, fixtures);
+                _physics.SetCollisionLayer(uid, fixture.Key, fixture.Value, 0, fixtures);
+            }
         }
         component.MovementSpeedDebuff = 1; //just so we can avoid annoying code elsewhere
         _movement.RefreshMovementSpeedModifiers(uid);
