c225e6d62fb4d3b07fca3356dd7c88a53b2fefcd
Łukasz Lindert <lukasz.lindert@protonmail.com>
2026-01-23 03:08:28 +0100

    final cleanup

--- Content.Shared/Silicons/Borgs/SharedBorgSystem.cs
+++ Content.Shared/Silicons/Borgs/SharedBorgSystem.cs
@@ -297,17 +297,12 @@ public abstract partial class SharedBorgSystem : EntitySystem
 
     private void OnBeingGibbed(Entity<BorgChassisComponent> chassis, ref BeingGibbedEvent args)
     {
-        List<EntityUid> gibblets = new();
-
         // Don't use the ItemSlotsSystem eject method since we don't want to play a sound and want we to eject the battery even if the slot is locked.
         if (TryComp<PowerCellSlotComponent>(chassis, out var slotComp) &&
             _container.TryGetContainer(chassis, slotComp.CellSlotId, out var slotContainer))
-            gibblets.AddRange(_container.EmptyContainer(slotContainer));
-        gibblets.AddRange(_container.EmptyContainer(chassis.Comp.BrainContainer));
-        gibblets.AddRange(_container.EmptyContainer(chassis.Comp.ModuleContainer));
-
-        foreach (var gibblet in gibblets)
-            args.Giblets.Add(gibblet);
+            args.Giblets.UnionWith(_container.EmptyContainer(slotContainer));
+        args.Giblets.UnionWith(_container.EmptyContainer(chassis.Comp.BrainContainer));
+        args.Giblets.UnionWith(_container.EmptyContainer(chassis.Comp.ModuleContainer));
     }
 
     private void OnGetDeadIC(Entity<BorgChassisComponent> chassis, ref GetCharactedDeadIcEvent args)
