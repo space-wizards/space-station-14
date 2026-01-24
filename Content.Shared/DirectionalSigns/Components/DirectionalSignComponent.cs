427b769b5e5e82bd37e5cb37cb54e16528992554
OctomusPrime <128234130+OctomusPrime@users.noreply.github.com>
2026-01-24 01:35:22 -0500

    Adds/Corrects functionality for Directional Signs as follows: -New Placement Mode (AlignDirectionalSign) to place signs uniformly with equal spacing and overlaps     -Includes halfway spaced positions to allow centering of odd or even amounts of signs -Implements new DirectionalSignSystem and DirectionalSignComponent to rotate directional signs about their tile center as the camera rotates -Solves one issue from https://github.com/space-wizards/space-station-14/issues/7187     -Signs no longer overlap at 90 degree angle
    Known issues:
    -Rotation system does not work Entity Spawn Panel (F5) is open
    -Placement Mode only functions in default view (Relative camera rotation of 0 Degrees to grid)

---
+++ Content.Shared/DirectionalSigns/Components/DirectionalSignComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.DirectionalSigns.Components;
+
+/// <summary>
+/// Component that marks a sign for sprite rotation and offset correction when the 'eye' rotates.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class DirectionalSignComponent : Component;
