def98ea0cf9a3955991eb6e25064a440a0c97201
Dmitry <57028746+DIMMoon1@users.noreply.github.com>
2026-01-27 04:54:11 +0700

    doc

--- Content.Server/Body/Systems/RespiratorSystem.cs
+++ Content.Server/Body/Systems/RespiratorSystem.cs
@@ -201,6 +201,7 @@ public sealed class RespiratorSystem : EntitySystem
     /// Checks if it's safe for a given entity to breathe the air from the environment it is currently situated in.
     /// </summary>
     /// <param name="ent">The entity attempting to metabolize the gas.</param>
+    /// <param name="logMissing">Do I need to get a log if he can't breathe the gas.</param>
     /// <returns>Returns true only if the air is not toxic, and it wouldn't suffocate.</returns>
     public bool CanMetabolizeInhaledAir(Entity<RespiratorComponent?> ent, bool logMissing = true)
     {
