cc45249a7edbc1ad3ad939b2a958b0f97fe68660
Jen Pollock <jen@jenpollock.ca>
2026-01-29 19:59:10 -0700

    Remove unused using statements

--- Content.Client/Lobby/LobbyUIController.cs
+++ Content.Client/Lobby/LobbyUIController.cs
@@ -1,13 +1,9 @@
-using System.Linq;
-using Content.Client.Body;
 using Content.Client.Guidebook;
 using Content.Client.Inventory;
 using Content.Client.Lobby.UI;
 using Content.Client.Players.PlayTimeTracking;
 using Content.Client.Station;
 using Content.Shared.CCVar;
-using Content.Shared.Clothing;
-using Content.Shared.GameTicking;
 using Content.Shared.Humanoid.Markings;
 using Content.Shared.Humanoid.Prototypes;
 using Content.Shared.Preferences;
@@ -20,7 +16,6 @@ using Robust.Client.State;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controllers;
 using Robust.Shared.Configuration;
-using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
