b8011830b4a96d620db13621f534f49961e87a2c
TheGrimbeeper <thegrimbeeper.11@gmail.com>
2026-01-31 21:22:41 +1100

    Fix #42713 - refactor UnlockStateIncrementPerNode to allow sibling nodes to count towards time adding

--- Content.Shared/Xenoarchaeology/Artifact/SharedXenoArtifactSystem.XAT.cs
+++ Content.Shared/Xenoarchaeology/Artifact/SharedXenoArtifactSystem.XAT.cs
@@ -73,24 +73,48 @@ public abstract partial class SharedXenoArtifactSystem
             if (_net.IsServer)
                 _popup.PopupEntity(Loc.GetString("artifact-unlock-state-begin"), ent);
             Dirty(ent);
+            if (node != null && unlockingComp.TriggeredNodeIndexes.Add(GetIndex(ent, node.Value)))
+            {
+                Dirty(ent, unlockingComp);
+            }
         }
         else if (node != null)
         {
             var index = GetIndex(ent, node.Value);
 
-            var predecessorNodeIndices = GetPredecessorNodes((ent, ent), index);
-            var successorNodeIndices = GetSuccessorNodes((ent, ent), index);
-            if (unlockingComp.TriggeredNodeIndexes.Count == 0
-                || unlockingComp.TriggeredNodeIndexes.All(
-                    x => predecessorNodeIndices.Contains(x) || successorNodeIndices.Contains(x)
-                )
-               )
-                // we add time on each new trigger, if it is not going to fail us
-                unlockingComp.EndTime += ent.Comp.UnlockStateIncrementPerNode;
-        }
+            // We need to add time, UNLESS the unlocking process is in a failed state after adding the new trigger.
+            // An unlockable node will fail to unlock if there is a trigger other than its required triggers.
+            // A failing unlocking state is one where there exists no unlockable nodes that have not failed.
 
-        if (node != null && unlockingComp.TriggeredNodeIndexes.Add(GetIndex(ent, node.Value)))
-        {
+            if (unlockingComp.TriggeredNodeIndexes.Add(index))
+            {
+                var unlockableNodes = new List<Entity<XenoArtifactNodeComponent>>();
+
+                var allnodes = GetAllNodes((ent, ent));
+                foreach (var nodeEnt in allnodes)
+                {
+                    if (!nodeEnt.Comp.Locked)
+                        continue;
+                    var directPredecessorNodes = GetDirectPredecessorNodes((ent, ent), nodeEnt);
+                    if (directPredecessorNodes.Count == 0 || directPredecessorNodes.All(x => !x.Comp.Locked))
+                    {
+                        unlockableNodes.Add(nodeEnt);
+                    }
+                }
+
+                var viableUnlockFound = false;
+                foreach (var unlockable in unlockableNodes)
+                {
+                    var predecessorNodeIndices = GetPredecessorNodes((ent, ent), GetIndex(ent, unlockable));
+                    if (unlockingComp.TriggeredNodeIndexes.All(x => predecessorNodeIndices.Contains(x)))
+                    {
+                        viableUnlockFound = true; // We have found an unlockable node that is still possible to unlock - it contains all triggers in its predecessors
+                        break;
+                    }
+                }
+                if (viableUnlockFound)
+                    unlockingComp.EndTime += ent.Comp.UnlockStateIncrementPerNode;
+            }
             Dirty(ent, unlockingComp);
         }
     }
