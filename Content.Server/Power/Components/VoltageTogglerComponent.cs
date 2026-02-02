9caadea711e326101ed85a4f0005ba14c5655454
Samuel-C <samuelcamilo8@gmail.com>
2026-02-02 17:41:34 -0300

    voltage toggle comp

---
+++ Content.Server/Power/Components/VoltageTogglerComponent.cs
@@ -0,0 +1,44 @@
+﻿using Content.Shared.Power;
+
+namespace Content.Server.Power.Components;
+
+/// <summary>
+///     Changes the voltage of a device with <see cref="PowerConsumerComponent"/>
+/// </summary>
+public sealed partial class VoltageTogglerComponent : Component
+{
+    [DataDefinition]
+    public partial struct VoltageSetting
+    {
+        /// <summary>
+        /// Voltage.
+        /// </summary>
+        [DataField(required: true)]
+        public Voltage Voltage;
+
+        /// <summary>
+        /// Power usage in that voltage.
+        /// </summary>
+        [DataField(required: true)]
+        public float Wattage;
+
+        /// <summary>
+        /// Name of the setting.
+        /// </summary>
+        [DataField(required: true)]
+        public LocId Name = string.Empty;
+    }
+
+    /// <summary>
+    /// List of all voltage settings.
+    /// </summary>
+    [DataField]
+    public VoltageSetting[] Settings;
+
+    /// <summary>
+    /// Index of the currently selected setting.
+    /// </summary>
+    [DataField]
+    [AutoNetworkedField]
+    public int SelectedVoltageLevel;
+}
---
+++ Content.Server/Power/EntitySystems/VoltageTogglerSystem.cs
@@ -0,0 +1,43 @@
+﻿using Content.Server.Power.Components;
+using Content.Shared.Verbs;
+
+namespace Content.Server.Power.EntitySystems;
+
+public sealed class VoltageTogglerSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<VoltageTogglerComponent, GetVerbsEvent<Verb>>(OnGetVerb);
+    }
+
+    private void OnGetVerb(Entity<VoltageTogglerComponent> entity, ref GetVerbsEvent<Verb> args)
+    {
+        if (!args.CanAccess || !args.CanInteract)
+            return;
+
+        var index = 0;
+        foreach (var setting in entity.Comp.Settings)
+        {
+            // This is because Act wont work with index.
+            // Needs it to be saved in the loop.
+            var currIndex = index;
+            var verb = new Verb
+            {
+                Priority = currIndex,
+                Category = VerbCategory.VoltageLevel,
+                Disabled = entity.Comp.SelectedVoltageLevel == currIndex,
+                Text = Loc.GetString(setting.Name),
+                Act = () =>
+                {
+                    entity.Comp.SelectedVoltageLevel = currIndex;
+                    Dirty(entity);
+
+                    // TODO: add stuff here
+                }
+            };
+            args.Verbs.Add(verb);
+            index++;
+        }
+    }
+}
