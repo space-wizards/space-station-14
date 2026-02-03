728de4c0db78f70a831fd7279fd804d4b2a7b3cc
Samuel-C <samuelcamilo8@gmail.com>
2026-02-03 10:05:17 -0300

    move stuff

--- Content.Server/Power/Components/VoltageTogglerComponent.cs
+++ Content.Server/Power/Components/VoltageTogglerComponent.cs
@@ -1,39 +1,19 @@
-﻿using Content.Shared.Power;
+﻿using Robust.Shared.GameStates;
+using Content.Shared.Power;
 
 namespace Content.Server.Power.Components;
 
 /// <summary>
 ///     Changes the voltage of a device with <see cref="PowerConsumerComponent"/>
 /// </summary>
+[RegisterComponent, NetworkedComponent]
 public sealed partial class VoltageTogglerComponent : Component
 {
-    [DataDefinition]
-    public partial struct VoltageSetting
-    {
-        /// <summary>
-        /// Voltage.
-        /// </summary>
-        [DataField(required: true)]
-        public Voltage Voltage;
-
-        /// <summary>
-        /// Power usage in that voltage.
-        /// </summary>
-        [DataField(required: true)]
-        public float Wattage;
-
-        /// <summary>
-        /// Name of the setting.
-        /// </summary>
-        [DataField(required: true)]
-        public LocId Name = string.Empty;
-    }
-
     /// <summary>
     /// List of all voltage settings.
     /// </summary>
-    [DataField]
-    public VoltageSetting[] Settings;
+    [DataField(required: true), ViewVariables(VVAccess.ReadOnly)]
+    public VoltageSetting[] Settings = [];
 
     /// <summary>
     /// Index of the currently selected setting.
@@ -41,4 +21,26 @@ public sealed partial class VoltageTogglerComponent : Component
     [DataField]
     [AutoNetworkedField]
     public int SelectedVoltageLevel;
+}
+
+[DataDefinition]
+public partial struct VoltageSetting
+{
+    /// <summary>
+    /// Voltage.
+    /// </summary>
+    [DataField(required: true)]
+    public Voltage Voltage;
+
+    /// <summary>
+    /// Power usage in that voltage.
+    /// </summary>
+    [DataField(required: true)]
+    public float Wattage;
+
+    /// <summary>
+    /// Name of the setting.
+    /// </summary>
+    [DataField(required: true)]
+    public LocId Name;
 }
