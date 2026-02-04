46aa1084daf68d64e6cf7ca996f31e1b729052fc
Samuel-C <samuelcamilo8@gmail.com>
2026-02-04 16:10:26 -0300

    can only anchor the extractor on the station

---
+++ Content.Shared/Construction/Components/AnchorOnlyOnStationComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Shared.Construction.Components;
+
+/// <summary>
+/// If a entity has this component it can only be anchored to the station
+/// </summary>
+[RegisterComponent]
+public sealed partial class AnchorOnlyOnStationComponent : Component;
