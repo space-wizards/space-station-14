bfe2e73a8456dff17107d5dec1a58ee30471bb1f
Samuel-C <samuelcamilo8@gmail.com>
2026-02-04 16:59:58 -0300

    commentary

--- Content.Shared/Construction/Components/AnchorOnlyOnStationComponent.cs
+++ Content.Shared/Construction/Components/AnchorOnlyOnStationComponent.cs
@@ -6,6 +6,9 @@
 [RegisterComponent]
 public sealed partial class AnchorOnlyOnStationComponent : Component
 {
+    /// <summary>
+    /// Pop up message when you try to anchor the entity on any grid that isn't the station grid
+    /// </summary>
     [DataField]
     public LocId PopupMessageAnchorFail = "anchorable-fail-not-on-station";
 }
