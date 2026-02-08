9c23b4a6d8d3188b8027ae5c0042a31455e31e03
pathetic meowmeow <uhhadd@gmail.com>
2026-02-08 13:30:09 -0500

    Documents all the public APIs for Nubody & markings code (#42857)

--- Content.Shared/Body/InitialBodyComponent.cs
+++ Content.Shared/Body/InitialBodyComponent.cs
@@ -4,11 +4,15 @@ namespace Content.Shared.Body;
 
 /// <summary>
 /// On map initialization, spawns the given organs into the body.
+/// Liable to change as the body becomes more complex.
 /// </summary>
 [RegisterComponent]
 [Access(typeof(InitialBodySystem))]
 public sealed partial class InitialBodyComponent : Component
 {
+    /// <summary>
+    /// The organs to spawn based on their category.
+    /// </summary>
     [DataField(required: true)]
     public Dictionary<ProtoId<OrganCategoryPrototype>, EntProtoId<OrganComponent>> Organs;
 }
