d3bbef260c4926ff9b0521ce1756d9fe83398355
portfiend <109661617+portfiend@users.noreply.github.com>
2026-02-20 17:53:35 -0600

    make high score entries VVable

--- Content.Shared/Arcade/Systems/SharedArcadeSystem.cs
+++ Content.Shared/Arcade/Systems/SharedArcadeSystem.cs
@@ -5,10 +5,22 @@ namespace Content.Shared.Arcade.Systems;
 public abstract partial class SharedArcadeSystem : EntitySystem
 { }
 
+/// <summary>
+/// Represents a single entry on the scoreboard of an arcade game.
+/// </summary>
 [Serializable, NetSerializable]
 public sealed class ArcadeHighScoreEntry : IComparable
 {
+    /// <summary>
+    /// The name of the player associated with this high score entry.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
     public string Name;
+
+    /// <summary>
+    /// The score associated with this high score entry.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
     public int Score;
 
     public ArcadeHighScoreEntry(string name, int score)
