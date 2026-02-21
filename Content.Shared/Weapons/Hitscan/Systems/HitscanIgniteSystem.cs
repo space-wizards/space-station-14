41b4ab839f97645705c733ca62c19851992caea1
august-sun <45527070+august.sun@users.noreply.github.com>
2026-02-21 16:35:20 -0700

    Initial System and Comp implementation, moves FlammableSystem to Shared

---
+++ Content.Shared/Weapons/Hitscan/Systems/HitscanIgniteSystem.cs
@@ -0,0 +1,37 @@
+﻿using Content.Shared.Atmos.EntitySystems;
+using Content.Shared.Random.Helpers;
+using Content.Shared.Weapons.Hitscan.Components;
+using Content.Shared.Weapons.Hitscan.Events;
+using Robust.Shared.Timing;
+using Robust.Shared.Random;
+
+namespace Content.Shared.Weapons.Hitscan.Systems;
+
+public sealed class HitscanIgniteSystem : EntitySystem
+{
+
+    [Dependency] private readonly SharedFlammableSystem _flammable = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<HitscanIgniteComponent, HitscanRaycastFiredEvent>(OnHitscanHit);
+    }
+
+    private void OnHitscanHit(Entity<HitscanIgniteComponent> ent, ref HitscanRaycastFiredEvent args)
+    {
+        if (args.Data.HitEntity == null)
+            return;
+
+        var seed = SharedRandomExtensions.HashCodeCombine((int)_timing.CurTick.Value, GetNetEntity(args.Gun).Id);
+        var rand = new System.Random(seed);
+        if (!rand.Prob(ent.Comp.IgniteChance))
+            return;
+
+        _flammable.Ignite(args.Data.HitEntity);
+
+
+    }
+}
