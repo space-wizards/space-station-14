be6bbd32160089d807518b6ad2e4944994295fd9
august-sun <45527070+august.sun@users.noreply.github.com>
2026-02-22 12:36:44 -0700

    Switching up method used

--- Content.Shared/Atmos/EntitySystems/SharedFlammableSystem.cs
+++ Content.Shared/Atmos/EntitySystems/SharedFlammableSystem.cs
@@ -1,6 +1,47 @@
-﻿namespace Content.Shared.Atmos.EntitySystems;
+﻿using Content.Shared.Atmos.Components;
+using Content.Shared.Database;
+using Content.Shared.IgnitionSource;
+using Content.Shared.Toggleable;
+
+namespace Content.Shared.Atmos.EntitySystems;
 
 public abstract class SharedFlammableSystem : EntitySystem
 {
+    [Dependency] private readonly SharedIgnitionSourceSystem _ignitionSourceSystem = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
 
+    public void UpdateAppearance(EntityUid uid, FlammableComponent? flammable = null, AppearanceComponent? appearance = null)
+    {
+        if (!Resolve(uid, ref flammable, ref appearance))
+            return;
+
+        _appearance.SetData(uid, FireVisuals.OnFire, flammable.OnFire, appearance);
+        _appearance.SetData(uid, FireVisuals.FireStacks, flammable.FireStacks, appearance);
+
+        // Also enable toggleable-light visuals
+        // This is intended so that matches & candles can re-use code for un-shaded layers on in-hand sprites.
+        // However, this could cause conflicts if something is ACTUALLY both a toggleable light and flammable.
+        // if that ever happens, then fire visuals will need to implement their own in-hand sprite management.
+        _appearance.SetData(uid, ToggleableVisuals.Enabled, flammable.OnFire, appearance);
+    }
+    public void Ignite(EntityUid uid, EntityUid ignitionSource, FlammableComponent? flammable = null,
+        EntityUid? ignitionSourceUser = null)
+    {
+        if (!Resolve(uid, ref flammable))
+            return;
+
+        if (flammable.AlwaysCombustible)
+        {
+            flammable.FireStacks = Math.Max(flammable.FirestacksOnIgnite, flammable.FireStacks);
+        }
+
+        if (flammable.FireStacks > 0 && !flammable.OnFire)
+        {
+            var extinguished = new IgnitedEvent();
+            RaiseLocalEvent(uid, ref extinguished);
+        }
+
+        UpdateAppearance(uid, flammable);
+
+    }
 }
--- Content.Shared/Weapons/Hitscan/Systems/HitscanIgniteSystem.cs
+++ Content.Shared/Weapons/Hitscan/Systems/HitscanIgniteSystem.cs
@@ -1,4 +1,7 @@
-﻿using Content.Shared.Atmos.EntitySystems;
+﻿using Content.Shared.Atmos.Components;
+using Content.Shared.Atmos.EntitySystems;
+using Content.Shared.IgnitionSource.SharedIgnitionSourceSystem;
+using Content.Shared.IgnitionSource;
 using Content.Shared.Random.Helpers;
 using Content.Shared.Weapons.Hitscan.Components;
 using Content.Shared.Weapons.Hitscan.Events;
@@ -9,7 +12,7 @@ namespace Content.Shared.Weapons.Hitscan.Systems;
 
 public sealed class HitscanIgniteSystem : EntitySystem
 {
-
+    [Dependency] private readonly SharedIgnitionSourceSystem _ignitionSourceSystem = default!;
     [Dependency] private readonly SharedFlammableSystem _flammable = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
 
@@ -20,17 +23,21 @@ public sealed class HitscanIgniteSystem : EntitySystem
         SubscribeLocalEvent<HitscanIgniteComponent, HitscanRaycastFiredEvent>(OnHitscanHit);
     }
 
-    private void OnHitscanHit(Entity<HitscanIgniteComponent> ent, ref HitscanRaycastFiredEvent args)
+    private void OnHitscanHit(EntityUid uid, Entity<HitscanIgniteComponent> ent, ref HitscanRaycastFiredEvent args)
     {
         if (args.Data.HitEntity == null)
             return;
 
-        var seed = SharedRandomExtensions.HashCodeCombine((int)_timing.CurTick.Value, GetNetEntity(args.Gun).Id);
+        var seed = SharedRandomExtensions.HashCodeCombine((int)_timing.CurTick.Value, GetNetEntity(args.Data.Gun).Id);
         var rand = new System.Random(seed);
         if (!rand.Prob(ent.Comp.IgniteChance))
             return;
 
-        _flammable.Ignite(args.Data.HitEntity);
+        if (!TryComp<FlammableComponent>(uid, out var flammable))
+            return;
+
+        _ignitionSourceSystem.SetIgnited((uid, flammable));
+        //_flammable.Ignite(args.Data.HitEntity);
 
     }
 }
