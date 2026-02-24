a3fa55d9a196d2385e43105db8b445e43422917f
august-sun <45527070+august.sun@users.noreply.github.com>
2026-02-24 15:01:18 -0700

    Code cleanup, moving everything to Server

---
+++ Content.Server/Weapons/Hitscan/Components/HitscanIgniteComponent.cs
@@ -0,0 +1,21 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Server.Weapons.Hitscan.Components;
+
+/// <summary>
+/// Allows for hitscan entities to ignite their targets. This component modifies the ignition chance, as well as how many stacks are added once ignited.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class HitscanIgniteComponent : Component
+{
+    /// <summary>
+    /// The chance the target has to ignite when hit by a hitscan.
+    /// </summary>
+    [DataField]
+    public float IgniteChance = 0.25f;
+    /// <summary>
+    /// How many fire stacks are added if ignition occurs.
+    /// </summary>
+    [DataField]
+    public float FireStacks = 1f;
+}
--- Content.Server/Weapons/Hitscan/Systems/HitscanIgniteSystem.cs
+++ Content.Server/Weapons/Hitscan/Systems/HitscanIgniteSystem.cs
@@ -1,37 +1,26 @@
 ﻿using Content.Server.Atmos.EntitySystems;
-using Content.Shared.Atmos;
-using Content.Shared.Atmos.Components;
-using Content.Shared.IgnitionSource;
-using Content.Shared.Random.Helpers;
-using Content.Shared.Weapons.Hitscan.Components;
+using Content.Server.Weapons.Hitscan.Components;
 using Content.Shared.Weapons.Hitscan.Events;
-using Robust.Shared.Timing;
 using Robust.Shared.Random;
 
-namespace Content.Shared.Weapons.Hitscan.Systems;
+namespace Content.Server.Weapons.Hitscan.Systems;
 
 public sealed class HitscanIgniteSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _robustRandom = default!;
-    [Dependency] private readonly SharedIgnitionSourceSystem _ignitionSourceSystem = default!;
     [Dependency] private readonly FlammableSystem _flammable = default!;
-    [Dependency] private readonly IGameTiming _timing = default!;
-    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<HitscanIgniteComponent, HitscanRaycastFiredEvent>(OnHitscanHit);
+
     }
+
     //The hitscan has hit the target, rolls a chance to ignite and ignite if it succeeds.
     private void OnHitscanHit(Entity<HitscanIgniteComponent> ent, ref HitscanRaycastFiredEvent args)
     {
-
-        //Rolls a chance for the laser to ignite the target. Cancels if it fails.
-        if (!_robustRandom.Prob(ent.Comp.IgniteChance))
-            return;
-
         //If the roll succeeds, the target is set on fire.
         var target = args.Data.HitEntity;
         var stackAmount = ent.Comp.FireStacks;
@@ -39,7 +28,11 @@ public sealed class HitscanIgniteSystem : EntitySystem
         if (target == null)
             return;
 
-        _flammable.AdjustFireStacks(target.Value, stackAmount, null, true);
+        //Rolls a chance for the laser to ignite the target. Cancels if it fails.
+        if (!_robustRandom.Prob(ent.Comp.IgniteChance))
+            return;
 
+        _flammable.AdjustFireStacks(target.Value, stackAmount, null, true);
     }
+
 }
