ec5f910fbbc9779b45247dc83ac6135cdaaefc62
Samuel-C <samuelcamilo8@gmail.com>
2026-02-27 00:40:14 -0300

    remove commentary

--- Content.Server/FeedbackSystem/FeedbackSystem.cs
+++ Content.Server/FeedbackSystem/FeedbackSystem.cs
@@ -28,17 +28,5 @@ public sealed partial class FeedbackSystem : EntitySystem
             .ToList();
 
         _feedbackManager.SendToAllSessions(showFeedbackPrototypes);
-
-        // TODO: find a better way to remove the feedback popup from rounds where the rule is not present
-        /*
-        var notShowFeedbackPrototypes = _feedbackManager.GetOriginFeedbackPrototypes(true, true)
-            .Select(x => _prototypeManager.Index(x))
-            .Where(x => !_gameTicker.IsGameRuleAdded(x.RuleWhitelist!))
-            .Select(x => new ProtoId<FeedbackPopupPrototype>(x.ID))
-            .OrderBy(x => x.Id)
-            .ToList();
-
-        _feedbackManager.SendToAllSessions(notShowFeedbackPrototypes, true);
-        */
     }
 }
