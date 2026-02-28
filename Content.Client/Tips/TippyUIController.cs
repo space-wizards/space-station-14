3d0f36b3b7199f67dcb03ee929ac0434cbd89cbc
neomoth <69372252+neomoth@users.noreply.github.com>
2026-02-28 16:18:00 -0500

    better tippy checking

--- Content.Client/Tips/TippyUIController.cs
+++ Content.Client/Tips/TippyUIController.cs
@@ -127,18 +127,16 @@ public sealed class TippyUIController : UIController
                 if (!_queuedMessages.TryDequeue(out var next))
                     return;
 
-                if (next.Proto != null && next.Proto != _cfg.GetCVar(CCVars.TippyEntity))
-                {
-                    _entity = EntityManager.SpawnEntity(next.Proto, MapCoordinates.Nullspace);
-                    tippy.ModifyLayers = false;
-                }
-                else
-                {
-                    _entity = EntityManager.SpawnEntity(_cfg.GetCVar(CCVars.TippyEntity), MapCoordinates.Nullspace);
-                    tippy.ModifyLayers = true;
-                }
+                _entity = next.Proto is null
+                    ? EntityManager.SpawnEntity(_cfg.GetCVar(CCVars.TippyEntity), MapCoordinates.Nullspace)
+                    : EntityManager.SpawnEntity(next.Proto, MapCoordinates.Nullspace);
+
                 if (!EntityManager.TryGetComponent(_entity, out sprite))
                     return;
+                // Only modify layers if they have all of the required ones.
+                tippy.ModifyLayers = _sprite.TryGetLayer(_entity, "revealing", out _, false) &&
+                                     _sprite.TryGetLayer(_entity, "speaking", out _, false) &&
+                                     _sprite.TryGetLayer(_entity, "hiding", out _, false);
                 if (!EntityManager.HasComponent<PaperVisualsComponent>(_entity))
                 {
                     var paper = EntityManager.AddComponent<PaperVisualsComponent>(_entity);
