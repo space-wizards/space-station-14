bf74698e6c72846d49cb4f7965f023b121ef029b
Samuel-C <samuelcamilo8@gmail.com>
2026-03-01 17:46:17 -0300

    important detail in the commentary

--- Content.Shared/Weapons/Ranged/Events/GetAmmoEvent.cs
+++ Content.Shared/Weapons/Ranged/Events/GetAmmoEvent.cs
@@ -3,7 +3,7 @@
 namespace Content.Shared.Weapons.Ranged.Events;
 
 /// <summary>
-/// Raised before a entity is loaded into a gun as ammo
+/// Raised before a entity is loaded into a gun (or a magazine) as ammo
 /// </summary>
 [ByRefEvent]
 public struct GetAmmoEvent(Entity<BallisticAmmoProviderComponent> ballisticProvider)
