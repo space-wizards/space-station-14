a530fb6104260987c845dda9653eb89641fa0d4e
Tayrtahn <tayrtahn@gmail.com>
2026-03-01 14:25:49 -0500

    Update Content.Shared/Audio/Jukebox/SharedJukeboxSystem.cs
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Shared/Audio/Jukebox/SharedJukeboxSystem.cs
+++ Content.Shared/Audio/Jukebox/SharedJukeboxSystem.cs
@@ -6,6 +6,9 @@ public abstract class SharedJukeboxSystem : EntitySystem
 {
     [Dependency] protected readonly SharedAudioSystem Audio = default!;
 
+    /// <summary>
+    /// Returns whether or not the given jukebox is currently playing a song.
+    /// </summary>
     public bool IsPlaying(Entity<JukeboxComponent?> entity)
     {
         if (!Resolve(entity, ref entity.Comp))
