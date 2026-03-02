53c90ffa1751bb10b25848dc1c4a6d54e2c19711
Samuel-C <samuelcamilo8@gmail.com>
2026-03-01 22:18:57 -0300

    commentary

--- Content.Shared/Weapons/Ranged/Events/GetAmmoEvent.cs
+++ Content.Shared/Weapons/Ranged/Events/GetAmmoEvent.cs
@@ -8,6 +8,14 @@ namespace Content.Shared.Weapons.Ranged.Events;
 [ByRefEvent]
 public struct GetAmmoEvent
 {
+    /// <summary>
+    /// if the entity can be used to load the gun or magazine
+    /// </summary>
     public bool CanLoad => true;
+
+    /// <summary>
+    /// If null the entity itself is used to load a gun or magazine,
+    /// if not null, the entity provided is used to load the gun or magazine
+    /// </summary>
     public EntityUid? AmmoOverride;
 }
