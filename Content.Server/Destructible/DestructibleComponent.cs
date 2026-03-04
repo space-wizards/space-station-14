5f33fb96e1a1108110cbd67c07043bf23836c9c6
Connor Huffine <chuffine@gmail.com>
2026-03-03 20:41:34 -0500

    Change to file scoped namespace

--- Content.Server/Destructible/DestructibleComponent.cs
+++ Content.Server/Destructible/DestructibleComponent.cs
@@ -1,31 +1,30 @@
 using Content.Server.Destructible.Thresholds;
 
-namespace Content.Server.Destructible
+namespace Content.Server.Destructible;
+
+/// <summary>
+///     When attached to an <see cref="Robust.Shared.GameObjects.EntityUid"/>, allows it to take damage
+///     and triggers thresholds when reached.
+/// </summary>
+[RegisterComponent]
+public sealed partial class DestructibleComponent : Component
 {
     /// <summary>
-    ///     When attached to an <see cref="Robust.Shared.GameObjects.EntityUid"/>, allows it to take damage
-    ///     and triggers thresholds when reached.
+    /// A list of damage thresholds for the entity;
+    /// includes their triggers and resultant behaviors.
     /// </summary>
-    [RegisterComponent]
-    public sealed partial class DestructibleComponent : Component
-    {
-        /// <summary>
-        /// A list of damage thresholds for the entity;
-        /// includes their triggers and resultant behaviors.
-        /// </summary>
-        [DataField]
-        public List<DamageThreshold> Thresholds = new();
+    [DataField]
+    public List<DamageThreshold> Thresholds = new();
 
-        /// <summary>
-        /// Specifies whether the entity has passed a damage threshold that causes it to break.
-        /// </summary>
-        [DataField]
-        public bool IsBroken = false;
+    /// <summary>
+    /// Specifies whether the entity has passed a damage threshold that causes it to break.
+    /// </summary>
+    [DataField]
+    public bool IsBroken = false;
 
-        /// <summary>
-        /// Specifies whether the entity should ignore excess damage and handle destruction normally.
-        /// </summary>
-        [DataField(readOnly: true)]
-        public bool IgnoreExcess = false;
-    }
+    /// <summary>
+    /// Specifies whether the entity should ignore excess damage and handle destruction normally.
+    /// </summary>
+    [DataField(readOnly: true)]
+    public bool IgnoreExcess = false;
 }
