9e65a7e4808c54edad446a3ab2939fc9bc6c2f69
kaylie <moony@hellomouse.net>
2026-03-09 11:23:02 +0100

    Add 204 more tests.

--- Content.IntegrationTests/Tests/Guidebook/GuideEntryPrototypeTests.cs
+++ Content.IntegrationTests/Tests/Guidebook/GuideEntryPrototypeTests.cs
@@ -2,8 +2,9 @@ using Content.Client.Guidebook;
 using Content.Client.Guidebook.Richtext;
 using Robust.Shared.ContentPack;
 using Robust.Shared.Prototypes;
-using System.Linq;
+using Content.IntegrationTests.Utility;
 using Content.Shared.Guidebook;
+using Robust.Shared.Localization;
 
 namespace Content.IntegrationTests.Tests.Guidebook;
 
@@ -13,29 +14,33 @@ namespace Content.IntegrationTests.Tests.Guidebook;
 [TestOf(typeof(DocumentParsingManager))]
 public sealed class GuideEntryPrototypeTests
 {
+    private static string[] _guideEntries = GameDataScrounger.PrototypesOfKind<GuideEntryPrototype>();
+
     [Test]
-    public async Task ValidatePrototypeContents()
+    [TestCaseSource(nameof(_guideEntries))]
+    [Description("Ensures a given guidebook entry is valid, checking the document/etc.")]
+    public async Task Validate(string protoKey)
     {
         await using var pair = await PoolManager.GetServerClient(new PoolSettings { Connected = true });
         var client = pair.Client;
         await client.WaitIdleAsync();
         var protoMan = client.ResolveDependency<IPrototypeManager>();
         var resMan = client.ResolveDependency<IResourceManager>();
+        var locMan = client.ResolveDependency<ILocalizationManager>();
         var parser = client.ResolveDependency<DocumentParsingManager>();
-        var prototypes = protoMan.EnumeratePrototypes<GuideEntryPrototype>().ToList();
+        var proto = protoMan.Index<GuideEntryPrototype>(protoKey);
 
-        foreach (var proto in prototypes)
+        await client.WaitAssertion(() =>
         {
-            await client.WaitAssertion(() =>
+            using var reader = resMan.ContentFileReadText(proto.Text);
+            var text = reader.ReadToEnd();
+            using (Assert.EnterMultipleScope())
             {
-                using var reader = resMan.ContentFileReadText(proto.Text);
-                var text = reader.ReadToEnd();
-                Assert.That(parser.TryAddMarkup(new Document(), text), $"Failed to parse guidebook: {proto.Id}");
-            });
+                Assert.That(parser.TryAddMarkup(new Document(), text), $"Failed to parse the guide entry's document.");
 
-            // Avoid styleguide update limit
-            await client.WaitRunTicks(1);
-        }
+                Assert.That(locMan.TryGetString(proto.Name, out _), $"The entry's name, {proto.Name}, is missing a locale key.");
+            }
+        });
 
         await pair.CleanReturnAsync();
     }
