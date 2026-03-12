f3558597a3f5e078f710a45a70c2a0214860a2e7
kaylie <moony@hellomouse.net>
2026-03-12 15:03:00 +0100

    TrackingIssue attribute.

--- Content.IntegrationTests/Tests/GameTestTests/RunOnSideTests.cs
+++ Content.IntegrationTests/Tests/GameTestTests/RunOnSideTests.cs
@@ -27,7 +27,8 @@ public sealed class RunOnSideTests : GameTest
     [Test]
     [RunOnSide(Side.Server)]
     [Description("Ensures that RunOnSide appropriately adds a property.")]
-    [Ignore("TestContext on the game threads is broken. https://github.com/space-wizards/RobustToolbox/issues/6449")]
+    [Ignore("TestContext on the game threads is broken.")]
+    [TrackingIssue("https://github.com/space-wizards/RobustToolbox/issues/6449")]
     public void TestProperty()
     {
         Assert.That(TestContext.CurrentContext.Test.Properties.Get(RunOnSideAttribute.RunOnSideProperty), Is.Not.Null);
