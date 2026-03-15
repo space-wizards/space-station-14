437775eaeb2b00ef384c586ca9fba2926ecc3b90
ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
2026-03-14 23:33:59 -0700

    modifying a lot anyways

--- Content.Client/Atmos/EntitySystems/GasTileOverlaySystem.cs
+++ Content.Client/Atmos/EntitySystems/GasTileOverlaySystem.cs
@@ -4,83 +4,82 @@ using Content.Shared.Atmos.EntitySystems;
 using JetBrains.Annotations;
 using Robust.Shared.GameStates;
 
-namespace Content.Client.Atmos.EntitySystems
+namespace Content.Client.Atmos.EntitySystems;
+
+[UsedImplicitly]
+public sealed class GasTileOverlaySystem : SharedGasTileOverlaySystem
 {
-    [UsedImplicitly]
-    public sealed class GasTileOverlaySystem : SharedGasTileOverlaySystem
+    public override void Initialize()
     {
-        public override void Initialize()
+        base.Initialize();
+        SubscribeNetworkEvent<GasOverlayUpdateEvent>(HandleGasOverlayUpdate);
+        SubscribeLocalEvent<GasTileOverlayComponent, ComponentHandleState>(OnHandleState);
+    }
+
+    private void OnHandleState(EntityUid gridUid, GasTileOverlayComponent comp, ref ComponentHandleState args)
+    {
+        Dictionary<Vector2i, GasOverlayChunk> modifiedChunks;
+
+        switch (args.Current)
         {
-            base.Initialize();
-            SubscribeNetworkEvent<GasOverlayUpdateEvent>(HandleGasOverlayUpdate);
-            SubscribeLocalEvent<GasTileOverlayComponent, ComponentHandleState>(OnHandleState);
+            // is this a delta or full state?
+            case GasTileOverlayDeltaState delta:
+            {
+                modifiedChunks = delta.ModifiedChunks;
+                foreach (var index in comp.Chunks.Keys)
+                {
+                    if (!delta.AllChunks.Contains(index))
+                        comp.Chunks.Remove(index);
+                }
+
+                break;
+            }
+            case GasTileOverlayState state:
+            {
+                modifiedChunks = state.Chunks;
+                foreach (var index in comp.Chunks.Keys)
+                {
+                    if (!state.Chunks.ContainsKey(index))
+                        comp.Chunks.Remove(index);
+                }
+
+                break;
+            }
+            default:
+                return;
         }
 
-        private void OnHandleState(EntityUid gridUid, GasTileOverlayComponent comp, ref ComponentHandleState args)
+        foreach (var (index, data) in modifiedChunks)
         {
-            Dictionary<Vector2i, GasOverlayChunk> modifiedChunks;
+            comp.Chunks[index] = data;
+        }
+    }
 
-            switch (args.Current)
+    private void HandleGasOverlayUpdate(GasOverlayUpdateEvent ev)
+    {
+        foreach (var (nent, removedIndicies) in ev.RemovedChunks)
+        {
+            var grid = GetEntity(nent);
+
+            if (!TryComp(grid, out GasTileOverlayComponent? comp))
+                continue;
+
+            foreach (var index in removedIndicies)
             {
-                // is this a delta or full state?
-                case GasTileOverlayDeltaState delta:
-                {
-                    modifiedChunks = delta.ModifiedChunks;
-                    foreach (var index in comp.Chunks.Keys)
-                    {
-                        if (!delta.AllChunks.Contains(index))
-                            comp.Chunks.Remove(index);
-                    }
-
-                    break;
-                }
-                case GasTileOverlayState state:
-                {
-                    modifiedChunks = state.Chunks;
-                    foreach (var index in comp.Chunks.Keys)
-                    {
-                        if (!state.Chunks.ContainsKey(index))
-                            comp.Chunks.Remove(index);
-                    }
-
-                    break;
-                }
-                default:
-                    return;
-            }
-
-            foreach (var (index, data) in modifiedChunks)
-            {
-                comp.Chunks[index] = data;
+                comp.Chunks.Remove(index);
             }
         }
 
-        private void HandleGasOverlayUpdate(GasOverlayUpdateEvent ev)
+        foreach (var (nent, gridData) in ev.UpdatedChunks)
         {
-            foreach (var (nent, removedIndicies) in ev.RemovedChunks)
+            var grid = GetEntity(nent);
+
+            if (!TryComp(grid, out GasTileOverlayComponent? comp))
+                continue;
+
+            foreach (var chunkData in gridData)
             {
-                var grid = GetEntity(nent);
-
-                if (!TryComp(grid, out GasTileOverlayComponent? comp))
-                    continue;
-
-                foreach (var index in removedIndicies)
-                {
-                    comp.Chunks.Remove(index);
-                }
-            }
-
-            foreach (var (nent, gridData) in ev.UpdatedChunks)
-            {
-                var grid = GetEntity(nent);
-
-                if (!TryComp(grid, out GasTileOverlayComponent? comp))
-                    continue;
-
-                foreach (var chunkData in gridData)
-                {
-                    comp.Chunks[chunkData.Index] = chunkData;
-                }
+                comp.Chunks[chunkData.Index] = chunkData;
             }
         }
     }
