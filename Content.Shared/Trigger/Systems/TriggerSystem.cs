3b38ea086524434c09489c9ab92af262a3e10c44
Bea Shakow <beashakow@gmail.com>
2026-03-16 22:22:31 -0400

    didn't end up needing the bugfix in triggersystem

--- Content.Shared/Trigger/Systems/TriggerSystem.cs
+++ Content.Shared/Trigger/Systems/TriggerSystem.cs
@@ -90,9 +90,6 @@ public sealed partial class TriggerSystem : EntitySystem
         if (!Resolve(ent, ref ent.Comp))
             return false;
 
-        if (Terminating(ent))
-            return false; // Stop trying to resurrect a dead horse.
-
         if (HasComp<ActiveTimerTriggerComponent>(ent))
             return false; // already activated
 
