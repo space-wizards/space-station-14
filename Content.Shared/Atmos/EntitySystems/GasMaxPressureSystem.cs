f5e1131077ce23a4a78641d5620dc7e8e695e046
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2026-03-17 01:47:38 -0700

    move comments

--- Content.Shared/Atmos/EntitySystems/GasMaxPressureSystem.cs
+++ Content.Shared/Atmos/EntitySystems/GasMaxPressureSystem.cs
@@ -2,6 +2,7 @@
 using Content.Shared.CCVar;
 using Content.Shared.Destructible;
 using Content.Shared.Explosion.EntitySystems;
+using Content.Shared.Jittering;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Configuration;
