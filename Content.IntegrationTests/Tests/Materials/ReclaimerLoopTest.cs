1a4edaaa8be8b146604955d82b02b35d4cefe744
Bea Shakow <beashakow@gmail.com>
2026-03-19 11:46:02 -0400

    Proper Indentation for Assertions

--- Content.IntegrationTests/Tests/Materials/ReclaimerLoopTest.cs
+++ Content.IntegrationTests/Tests/Materials/ReclaimerLoopTest.cs
@@ -47,6 +47,7 @@ public sealed class ReclaimerLoopTest : InteractionTest
         await SpawnTarget(reclaimerID, PlayerCoords);
         Assert.That(STarget, Is.Not.Null,
             "STarget was null, did the reclaimer spawn correctly?");
+
         var reclaimComp = Comp<MaterialReclaimerComponent>(Target);
         //If the reclaimer can produce materials
         var reclaimsMaterials = reclaimComp.ReclaimMaterials;
@@ -91,8 +92,7 @@ public sealed class ReclaimerLoopTest : InteractionTest
 
         //Assert that reclaimer enabled
         Assert.That(reclaimComp.Enabled,
-        "The reclaimer did not get or stay enabled");
-
+            "The reclaimer did not get or stay enabled");
 
         // //put a floor tile down
         await InteractUsing(FloorTileID);
@@ -102,20 +102,20 @@ public sealed class ReclaimerLoopTest : InteractionTest
         {
             EntProtoId? matStack = ProtoMan.Index(material).StackEntity;
             Assert.That(matStack, Is.Not.Null,
-            $"The material, {material}, did not have a stackentity associated with it. You may need to add a stackEntity to its Reagents/Materials yml file.");
+                $"The material, {material}, did not have a stackentity associated with it. You may need to add a stackEntity to its Reagents/Materials yml file.");
 
             var matInHands = await PlaceInHands(matStack);
             var matInHandsUid = SEntMan.GetEntity(matInHands);
 
             //Assert we're holding material
             Assert.That(HandSys.GetActiveItem((SPlayer, Hands)), Is.EqualTo(matInHandsUid),
-            $"The material, {matStack}, never got put in our hands.");
+                $"The material, {matStack}, never got put in our hands.");
 
             await Interact();
 
             //Assert Hands not empty
             Assert.That(HandSys.GetActiveItem((ToServer(Player), Hands)), Is.Not.EqualTo(null),
-            $"The material that should not have been reclaimed, {matStack}, is no longer in our hands. The reclaimer was {reclaimerID}");
+                $"The material that should not have been reclaimed, {matStack}, is no longer in our hands. The reclaimer was {reclaimerID}");
         }
     }
 }
