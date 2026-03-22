7a2329ae493f483d8f182089dfbe9254afbca8e7
nomdéraisonnablementlong <you@example.com>
2026-03-22 18:18:50 -0400

    support for colors

--- Content.Server/Photography/PictureTakerComponent.cs
+++ Content.Server/Photography/PictureTakerComponent.cs
@@ -1,9 +1,13 @@
+using Robust.Shared.Prototypes;
+
 namespace Content.Server.Photography;
 // since camera is taken...
 /// <summary>
 /// marks an entity as able to take pictures (when you smash other entities with it)
 /// </summary>
 [RegisterComponent]
-public sealed partial class PictureTakerComponent: Component{
-
+public sealed partial class PictureTakerComponent: Component
+{
+    [DataField]
+    public List<EntProtoId> Photographs;
 }
