64fc5adb4ccfa84ecddcc91814a8e427de339930
nomdéraisonnablementlong <you@example.com>
2026-03-22 18:18:43 -0400

    charges, description order, fixed typo

--- Content.Server/Photography/PhotographySystem.cs
+++ Content.Server/Photography/PhotographySystem.cs
@@ -1,12 +1,15 @@
-using Content.Server.Administration.Logs;
+using System.Linq;
+using Content.Shared.Charges.Components;
 using Content.Shared.Examine;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Weapons.Melee.Events;
+using Robust.Shared.Random;
+
 namespace Content.Server.Photography;
 public sealed class PhotographySystem: EntitySystem {
     [Dependency] private readonly ExamineSystemShared _examine = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
-    [Dependency] private readonly IAdminLogManager _log = default!;
+    [Dependency] private readonly IRobustRandom _rng = default!;
     public override void Initialize() {
         base.Initialize();
         SubscribeLocalEvent<PictureTakerComponent, MeleeHitEvent>(OnCameraMeleeHit);
@@ -17,24 +20,47 @@ public sealed class PhotographySystem: EntitySystem {
     {
         if (!args.IsInDetailsRange)
         {
-            // cant see details from far away!
+            // can't see details from far away!
             return;
         }
-        args.PushMessage(component.Text);
-        args.PushText(Loc.GetString("photography-description", ("entity", component.Name)));
+        // writes the description since we are close enough
+        using (args.PushGroup("photographDescription"))
+        {
+            args.PushText(Loc.GetString("photograph-description", ("entity", component.Name)));
+            args.PushMessage(component.Text);
+        }
 
     }
 
     private void OnCameraMeleeHit(Entity<PictureTakerComponent> ent, ref MeleeHitEvent args) {
+        // nothing was hit
         if (!args.IsHit)
         {
             return;
+        }
+
+        // no photographs...
+        if (!ent.Comp.Photographs.Any())
+        {
+            return;
+        }
+        {
+            if (TryComp(ent.Owner, out LimitedChargesComponent? comp))
+            {
+                if (comp.LastCharges == 0)
+                {
+                    // no charges, we can't print anymore
+                    return;
+                }
+            }
         }
         foreach (var entity in args.HitEntities)
         {
             var text = _examine.GetExamineText(entity, ent.Owner);
             var name = Name(entity);
-            var spawned = Spawn("Photography");
+
+
+            var spawned = Spawn(ent.Comp.Photographs[_rng.Next(0, ent.Comp.Photographs.Count)]);
             var metadata = MetaData(spawned);
             var comp = new PhotographyComponent(name, text);
             AddComp(spawned, comp);
