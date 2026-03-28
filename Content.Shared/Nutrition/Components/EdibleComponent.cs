05ac406f20cc5469ab1ce04a0284f6b2fec7307e
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2026-03-28 09:13:28 -0700

    mmm yummy

--- Content.Shared/Nutrition/Components/EdibleComponent.cs
+++ Content.Shared/Nutrition/Components/EdibleComponent.cs
@@ -37,7 +37,7 @@ public sealed partial class EdibleComponent : Component
     /// How much of our solution is eaten on a do-after completion. Set to null to eat the whole thing.
     /// </summary>
     [DataField]
-    public FixedPoint2? TransferAmount = FixedPoint2.New(5);
+    public FixedPoint2? TransferAmount = FixedPoint2.New(10);
 
     /// <summary>
     /// Acceptable utensils to use
