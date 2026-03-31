7f4b15c4ecc4c83bab76bcd02b283ae4db602b64
Kyle Tyo <akikai297@gmail.com>
2026-03-31 09:30:33 -0400

    Update DeAdminCommand.cs

--- Content.Server/Administration/Commands/DeAdminCommand.cs
+++ Content.Server/Administration/Commands/DeAdminCommand.cs
@@ -4,20 +4,19 @@ using Content.Shared.Administration;
 using JetBrains.Annotations;
 using Robust.Shared.Console;
 
-namespace Content.Server.Administration.Commands
+namespace Content.Server.Administration.Commands;
+
+[UsedImplicitly]
+[AdminCommand(AdminFlags.None)]
+public sealed class DeAdminCommand : LocalizedCommands
 {
-    [UsedImplicitly]
-    [AdminCommand(AdminFlags.None)]
-    public sealed class DeAdminCommand : LocalizedCommands
+    [Dependency] private readonly IAdminManager _admin = default!;
+
+    public override string Command => "deadmin";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IAdminManager _admin = default!;
-
-        public override string Command => "deadmin";
-
-        public override void Execute(IConsoleShell shell, string argStr, string[] args)
-        {
-            if (CommandChecks.MustNotBeServer(shell, out var player))
-                _admin.DeAdmin(player);
-        }
+        if (CommandChecks.MustNotBeServer(shell, out var player))
+            _admin.DeAdmin(player);
     }
 }
