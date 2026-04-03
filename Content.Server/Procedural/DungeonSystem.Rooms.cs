aa178bbfef8ddf69c16f7327047ef4e05d9b8eea
RainyGale <rainygale@protonmail.com>
2026-04-03 19:27:31 -0400

    Use IRobustRandom instead of System.Random

--- Content.Server/Procedural/DungeonSystem.Rooms.cs
+++ Content.Server/Procedural/DungeonSystem.Rooms.cs
@@ -19,7 +19,7 @@ public sealed partial class DungeonSystem
     /// <summary>
     /// Gets a random dungeon room matching the specified area, whitelist and size.
     /// </summary>
-    public DungeonRoomPrototype? GetRoomPrototype(Vector2i size, Random random, EntityWhitelist? whitelist = null)
+    public DungeonRoomPrototype? GetRoomPrototype(Vector2i size, IRobustRandom random, EntityWhitelist? whitelist = null)
     {
         return GetRoomPrototype(random, whitelist, minSize: size, maxSize: size);
     }
@@ -27,7 +27,7 @@ public sealed partial class DungeonSystem
     /// <summary>
     /// Gets a random dungeon room matching the specified area and whitelist and size range
     /// </summary>
-    public DungeonRoomPrototype? GetRoomPrototype(Random random,
+    public DungeonRoomPrototype? GetRoomPrototype(IRobustRandom random,
         EntityWhitelist? whitelist = null,
         Vector2i? minSize = null,
         Vector2i? maxSize = null)
@@ -77,7 +77,7 @@ public sealed partial class DungeonSystem
         MapGridComponent grid,
         Vector2i origin,
         DungeonRoomPrototype room,
-        Random random,
+        IRobustRandom random,
         HashSet<Vector2i>? reservedTiles,
         bool clearExisting = false,
         bool rotation = false)
@@ -96,7 +96,7 @@ public sealed partial class DungeonSystem
         SpawnRoom(gridUid, grid, finalTransform, room, reservedTiles, clearExisting);
     }
 
-    public Angle GetRoomRotation(DungeonRoomPrototype room, Random random)
+    public Angle GetRoomRotation(DungeonRoomPrototype room, IRobustRandom random)
     {
         var roomRotation = Angle.Zero;
 
