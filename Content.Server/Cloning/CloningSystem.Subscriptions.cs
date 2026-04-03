f970377964e166996eb0c9b4be8314142d7651a4
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2026-04-03 23:32:02 +0200

    fix tailwag action for changelings

--- Content.Server/Cloning/CloningSystem.Subscriptions.cs
+++ Content.Server/Cloning/CloningSystem.Subscriptions.cs
@@ -53,7 +53,7 @@ public sealed partial class CloningSystem
         SubscribeLocalEvent<VocalComponent, CloningEvent>(OnCloneVocal);
         SubscribeLocalEvent<StorageComponent, CloningEvent>(OnCloneStorage);
         SubscribeLocalEvent<InventoryComponent, CloningEvent>(OnCloneInventory);
-        SubscribeLocalEvent<MovementSpeedModifierComponent, CloningEvent>(OnCloneInventory);
+        SubscribeLocalEvent<MovementSpeedModifierComponent, CloningEvent>(OnCloneMovementSpeedModifier);
     }
 
     private void OnCloneItemStack(Entity<StackComponent> ent, ref CloningItemEvent args)
@@ -120,7 +120,7 @@ public sealed partial class CloningSystem
         _inventory.CopyComponent(ent.AsNullable(), args.CloneUid);
     }
 
-    private void OnCloneInventory(Entity<MovementSpeedModifierComponent> ent, ref CloningEvent args)
+    private void OnCloneMovementSpeedModifier(Entity<MovementSpeedModifierComponent> ent, ref CloningEvent args)
     {
         if (!args.Settings.EventComponents.Contains(Factory.GetRegistration(ent.Comp.GetType()).Name))
             return;
