6468754bb2459c351f648d6bc78c8d3e7e2b954d
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2026-04-05 11:51:20 -0700

    add fallback behavior

--- Content.Server/Traitor/Uplink/UplinkSystem.cs
+++ Content.Server/Traitor/Uplink/UplinkSystem.cs
@@ -52,6 +52,7 @@ public sealed class UplinkSystem : EntitySystem
             return;
         }
 
+        SetUplink(args.Implanted, Spawn(TraitorUplinkStore, MapCoordinates.Nullspace), 0, false);
         Log.Error($"{ToPrettyString(args.Implanted)} did not have an uplink when they were implanted.");
     }
 
