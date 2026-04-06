bbfa0e1af8250a8de98f731540e2b934c85366b4
ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
2026-04-06 12:55:43 -0700

    second pass

--- Content.Server/Xenoarchaeology/Artifact/XAT/XATMagnetSystem.cs
+++ Content.Server/Xenoarchaeology/Artifact/XAT/XATMagnetSystem.cs
@@ -54,7 +54,7 @@ public sealed class XATMagnetSystem : BaseQueryUpdateXATSystem<XATMagnetComponen
             if (node.Attached == null)
                 continue;
 
-            var artifact = _xenoArtifactQuery.Get(node.Attached.Value);
+            var artifact = XenoArtifactQuery.Get(node.Attached.Value);
 
             if (!CanTrigger(artifact, (uid, node)))
                 continue;
