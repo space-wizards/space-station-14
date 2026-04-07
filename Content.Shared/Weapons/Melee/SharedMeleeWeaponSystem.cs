64ca5e195a39f389a1fa3e1b6a0d8f77a828be88
geekyhobo <geekyhobo321@gmail.com>
2026-04-07 09:16:55 -0400

    automerge borked this

--- Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
+++ Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
@@ -723,6 +723,9 @@ public abstract partial class SharedMeleeWeaponSystem : EntitySystem
                 appliedDamage += damageResult;
                 hitEntities.Add(entity);
             }
+
+            if (TerminatingOrDeleted(entity))
+                targets.RemoveAt(i);
         }
 
         // Emit a single compound log for the entire wide attack.
@@ -758,9 +761,6 @@ public abstract partial class SharedMeleeWeaponSystem : EntitySystem
                     LogImpact.Medium,
                     $"{user:actor} melee attacked (heavy) {hitEntities.Count} targets using {meleeUid:tool}, hitting {names} for {appliedDamage.GetTotal():damage} total damage");
             }
-
-            if (TerminatingOrDeleted(entity))
-                targets.RemoveAt(i);
         }
 
         if (entities.Count != 0)
