9c9379ee99ee6eead8edc35bb78fcfdcb2277b3a
Samuel-C <samuelcamilo8@gmail.com>
2026-04-08 12:35:35 -0300

    custom sprite ammo counter comp

---
+++ Content.Shared/Weapons/Ranged/Components/CustomSpriteAmmoCounterComponent.cs
@@ -0,0 +1,20 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Weapons.Ranged.Components;
+
+[NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class CustomSpriteAmmoCounterComponent : Component
+{
+    /// <summary>
+    /// The sprite that will be used to count the ammo
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public SpriteSpecifier LoadedAmmoSprite;
+
+    /// <summary>
+    /// The sprite that will be used to count the ammo when the ammo is spent
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public SpriteSpecifier SpentAmmoSprite;
+}
