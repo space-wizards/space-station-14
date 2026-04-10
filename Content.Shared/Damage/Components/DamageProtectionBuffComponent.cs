3db1fb5d4e9c4a67229b7e950cd4b7a421a9c0fd
iaada <iaada@users.noreply.github.com>
2026-04-10 13:33:52 -0500

    remove

--- Content.Shared/Damage/Components/DamageProtectionBuffComponent.cs
+++ Content.Shared/Damage/Components/DamageProtectionBuffComponent.cs
@@ -1,20 +1,6 @@
-﻿using Content.Shared.Damage.Prototypes;
-using Robust.Shared.GameStates;
+﻿using Robust.Shared.GameStates;
 
 namespace Content.Shared.Damage.Components;
-
-/// <summary>
-///     Applies the specified DamageModifierSets when the entity takes damage.
-/// </summary>
-[RegisterComponent, NetworkedComponent]
-public sealed partial class DamageProtectionBuffComponent : Component
-{
-    /// <summary>
-    ///     The damage modifiers for entities with this component.
-    /// </summary>
-    [DataField]
-    public Dictionary<string, DamageModifierSetPrototype> Modifiers = new();
-}
 
 /// <summary>
 /// Component used on a status effect entity to modify damage taken.
--- Content.Shared/Damage/Systems/DamageProtectionBuffSystem.cs
+++ Content.Shared/Damage/Systems/DamageProtectionBuffSystem.cs
@@ -9,18 +9,11 @@ public sealed class DamageProtectionBuffSystem : EntitySystem
     {
         base.Initialize();
 
-        SubscribeLocalEvent<DamageProtectionBuffComponent, DamageModifyEvent>(OnDamageModify);
         SubscribeLocalEvent<DamageModifierStatusEffectComponent, StatusEffectRelayedEvent<DamageModifyEvent>>(OnDamageModifyStatus);
-    }
-
-    private void OnDamageModify(EntityUid uid, DamageProtectionBuffComponent component, DamageModifyEvent args)
-    {
-        foreach (var modifier in component.Modifiers.Values)
-            args.Damage = DamageSpecifier.ApplyModifierSet(args.Damage, modifier);
     }
 
     private void OnDamageModifyStatus(Entity<DamageModifierStatusEffectComponent> status, ref StatusEffectRelayedEvent<DamageModifyEvent> args)
     {
-        args.Args.Damage = DamageSpecifier.ApplyModifierSet(args.Args.Damage, status.Comp.Modifier);
+        args.Args.Damage = DamageSpecifier.ApplyModifierSet(args.Args.Damage, status.Comp.Modifiers);
     }
 }
