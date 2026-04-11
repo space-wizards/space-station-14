e25ffe9b7d700b50d33ba6016ced050e3b4c0560
ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
2026-04-10 20:22:31 -0700

    whoops

--- Content.Server/Lathe/LatheSystem.cs
+++ Content.Server/Lathe/LatheSystem.cs
@@ -54,7 +54,6 @@ namespace Content.Server.Lathe
         [Dependency] private readonly ReagentSpeedSystem _reagentSpeed = default!;
         [Dependency] private readonly SharedSolutionContainerSystem _solution = default!;
         [Dependency] private readonly StackSystem _stack = default!;
-        [Dependency] private readonly TransformSystem _transform = default!;
         [Dependency] private readonly RadioSystem _radio = default!;
 
         public override void Initialize()
