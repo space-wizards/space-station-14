c0dc66885219941393e5e4de627198555bd6d4ca
Samuel-C <samuelcamilo8@gmail.com>
2026-04-18 17:21:29 -0300

    PowerConsumerBatteryCharger

---
+++ Content.Server/Power/Components/PowerConsumerBatteryChargerComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace Content.Server.Power.Components;
+
+/// <summary>
+/// Used to charge a battery with <see cref="PowerConsumerComponent"/>
+/// instead of <see cref="BatteryChargerComponent"/> and <see cref="PowerNetworkBatteryComponent"/>
+/// </summary>
+[RegisterComponent]
+public sealed partial class PowerConsumerBatteryChargerComponent : Component
+{
+    /// <summary>
+    /// Out of much power consumed by the <see cref="PowerConsumerComponent"/>
+    /// will actually charge the battery
+    /// </summary>
+    [DataField]
+    public float Efficiency = 1f;
+}
---
+++ Content.Server/Power/EntitySystems/PowerConsumerBatteryChargerSystem.cs
@@ -0,0 +1,23 @@
+﻿using Content.Server.Power.Components;
+using Content.Shared.Power.Components;
+
+namespace Content.Server.Power.EntitySystems;
+
+public sealed class PowerConsumerBatteryChargerSystem : EntitySystem
+{
+    [Dependency] private readonly BatterySystem _battery = null!;
+
+    public override void Update(float frameTime)
+    {
+        var query = EntityQueryEnumerator<PowerConsumerBatteryChargerComponent, PowerConsumerComponent, BatteryComponent>();
+        while (query.MoveNext(out var entityUid, out var powerConsumerBatteryCharger, out var powerConsumer, out var battery))
+        {
+            var energyConsumed = powerConsumer.ReceivedPower * frameTime;
+
+            if (energyConsumed == 0)
+                continue;
+
+            _battery.ChangeCharge((entityUid, battery), energyConsumed * powerConsumerBatteryCharger.Efficiency);
+        }
+    }
+}
