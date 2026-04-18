dcf359e1bb1fca10aeb76ab0b44c573652b3c881
Samuel-C <samuelcamilo8@gmail.com>
2026-04-18 18:31:17 -0300

    PowerConsumerBatteryChargerEfficiencyVoltageTogglerComponent

---
+++ Content.Server/Power/Components/PowerConsumerBatteryChargerEfficiencyVoltageTogglerComponent.cs
@@ -0,0 +1,18 @@
+﻿using Content.Shared.Power;
+using Content.Shared.Power.Components;
+
+namespace Content.Server.Power.Components;
+
+/// <summary>
+/// Changes the efficiency of <see cref="PowerConsumerBatteryChargerComponent"/>
+/// when the voltage is changed via the <see cref="VoltageTogglerComponent"/>
+/// </summary>
+[RegisterComponent]
+public sealed partial class PowerConsumerBatteryChargerEfficiencyVoltageTogglerComponent : Component
+{
+    /// <summary>
+    /// Different efficiencies per voltage
+    /// </summary>
+    [DataField(required: true)]
+    public Dictionary<Voltage, float> EfficiencyPerVoltage;
+}
--- Content.Server/Power/EntitySystems/PowerConsumerBatteryChargerSystem.cs
+++ Content.Server/Power/EntitySystems/PowerConsumerBatteryChargerSystem.cs
@@ -7,6 +7,37 @@ public sealed class PowerConsumerBatteryChargerSystem : EntitySystem
 {
     [Dependency] private readonly BatterySystem _battery = null!;
 
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PowerConsumerBatteryChargerEfficiencyVoltageTogglerComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<PowerConsumerBatteryChargerEfficiencyVoltageTogglerComponent, VoltageChangeEvent>(OnVoltageChanged);
+    }
+
+    private void OnMapInit(
+        Entity<PowerConsumerBatteryChargerEfficiencyVoltageTogglerComponent> entity,
+        ref MapInitEvent args)
+    {
+        if (!TryComp<PowerConsumerBatteryChargerComponent>(entity, out var powerConsumerBatteryCharger))
+            return;
+
+        if (!TryComp<PowerConsumerComponent>(entity, out var powerConsumer))
+            return;
+
+        powerConsumerBatteryCharger.Efficiency = entity.Comp.EfficiencyPerVoltage[powerConsumer.Voltage];
+    }
+
+    private void OnVoltageChanged(
+        Entity<PowerConsumerBatteryChargerEfficiencyVoltageTogglerComponent> entity,
+        ref VoltageChangeEvent args)
+    {
+        if (!TryComp<PowerConsumerBatteryChargerComponent>(entity, out var powerConsumerBatteryCharger))
+            return;
+
+        powerConsumerBatteryCharger.Efficiency = entity.Comp.EfficiencyPerVoltage[args.NewVoltage.Voltage];
+    }
+
     public override void Update(float frameTime)
     {
         var query = EntityQueryEnumerator<PowerConsumerBatteryChargerComponent, PowerConsumerComponent, BatteryComponent>();
