9b23ce203415630560ee604017f98575556451b5
Prole <172158352+Prole0@users.noreply.github.com>
2026-04-20 14:56:47 -0700

    Better to make a seperate branch

---
+++ Content.Client/Storage/Components/ItemSlotVisualsComponent.cs
@@ -0,0 +1,32 @@
+﻿namespace Content.Client.Storage.Components;
+
+[RegisterComponent]
+public sealed partial class ItemSlotVisualsComponent : Component
+{
+    [DataField]
+    public int MaxFillLevels;
+
+    [DataField]
+    public string? FillBaseName;
+
+    [DataField]
+    public string? InHandsFillBaseName;
+
+    [DataField]
+    public int InHandsMaxFillLevels;
+
+    [DataField]
+    public string? EquippedFillBaseName;
+
+    [DataField]
+    public int EquippedMaxFillLevels;
+
+    [DataField("layer")]
+    public ItemSlotVisualLayers FillLayer = ItemSlotVisualLayers.Fill;
+}
+
+public enum ItemSlotVisualLayers : byte
+{
+    ContainsItem,
+    Fill
+}
---
+++ Content.Client/Storage/Systems/ItemSlotVisualsSystem.cs
@@ -0,0 +1,9 @@
+﻿using Content.Client.Storage.Components;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Storage.Systems;
+
+public sealed class ItemSlotVisualsSystem : VisualizerSystem<ItemSlotVisualsComponent>
+{
+
+}
