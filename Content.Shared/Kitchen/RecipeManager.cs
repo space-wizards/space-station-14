0b58788ff1b5065346782b83d8e5a533a18ecd19
Fenriz <kastonbag552@gmail.com>
2026-04-23 10:14:08 +0300

    small refactor RecipeManager and fix prototype reloading

--- Content.Shared/Kitchen/RecipeManager.cs
+++ Content.Shared/Kitchen/RecipeManager.cs
@@ -1,46 +1,40 @@
-﻿using System.Linq;
+using System.Linq;
 using Robust.Shared.Prototypes;
 
-namespace Content.Shared.Kitchen
+namespace Content.Shared.Kitchen;
+
+public sealed class RecipeManager
 {
-    public sealed class RecipeManager
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+
+    public List<FoodRecipePrototype> Recipes { get; private set; } = new();
+
+    public void Initialize()
     {
-        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+        ReloadRecipes();
+        _prototypeManager.PrototypesReloaded += OnPrototypesReloaded;
+    }
 
-        public List<FoodRecipePrototype> Recipes { get; private set; } = new();
+    private void OnPrototypesReloaded(PrototypesReloadedEventArgs args)
+    {
+        if (args.WasModified<FoodRecipePrototype>())
+            ReloadRecipes();
+    }
 
-        public void Initialize()
-        {
-            Recipes = new List<FoodRecipePrototype>();
-            foreach (var item in _prototypeManager.EnumeratePrototypes<FoodRecipePrototype>())
-            {
-                if (!item.SecretRecipe)
-                    Recipes.Add(item);
-            }
+    private void ReloadRecipes()
+    {
+        Recipes = _prototypeManager
+            .EnumeratePrototypes<FoodRecipePrototype>()
+            .Where(x => !x.SecretRecipe)
+            .OrderByDescending(x => x.IngredientCount())
+            .ToList();
+    }
 
-            Recipes.Sort(new RecipeComparer());
-        }
-        /// <summary>
-        /// Check if a prototype ids appears in any of the recipes that exist.
-        /// </summary>
-        public bool SolidAppears(string solidId)
-        {
-            return Recipes.Any(recipe => recipe.IngredientsSolids.ContainsKey(solidId));
-        }
-
-        private sealed class RecipeComparer : Comparer<FoodRecipePrototype>
-        {
-            public override int Compare(FoodRecipePrototype? x, FoodRecipePrototype? y)
-            {
-                if (x == null || y == null)
-                {
-                    return 0;
-                }
-
-                var nx = x.IngredientCount();
-                var ny = y.IngredientCount();
-                return -nx.CompareTo(ny);
-            }
-        }
+    /// <summary>
+    /// Check if a prototype ids appears in any of the recipes that exist.
+    /// </summary>
+    public bool SolidAppears(string solidId)
+    {
+        return Recipes.Any(recipe => recipe.IngredientsSolids.ContainsKey(solidId));
     }
 }
