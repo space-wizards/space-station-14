655e66d21e8681153e4287cb0f75a789b4b91800
Samuel-C <samuelcamilo8@gmail.com>
2026-04-24 20:27:43 -0300

    shared stuff

---
+++ Content.Shared/Power/Components/VoltageTogglerComponent.cs
@@ -0,0 +1,76 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Power.Components;
+
+/// <summary>
+/// Changes the voltage of a device that has some field related to voltage
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class VoltageTogglerComponent : Component
+{
+    /// <summary>
+    /// List of all voltage settings.
+    /// </summary>
+    [DataField(required: true)]
+    public VoltageSetting[] Settings = [];
+
+    /// <summary>
+    /// Index of the currently selected setting.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public int SelectedVoltageLevel;
+
+    /// <summary>
+    /// Locale id for the popup shown when switching voltages.
+    /// It is given "voltage" as a colored voltage string.
+    /// </summary>
+    [DataField]
+    public LocId? SwitchText;
+
+    /// <summary>
+    /// Locale id for text shown when examined.
+    /// It is given "voltage" as a colored voltage string.
+    /// </summary>
+    [DataField]
+    public LocId ExamineText = "voltage-toggler-examine";
+
+    /// <summary>
+    /// Sound that plays when the cable is switched.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier? SwitchSound = new SoundPathSpecifier("/Audio/Machines/button.ogg");
+}
+
+/// <summary>
+/// A voltage setting from which a device can be set to change to.
+/// </summary>
+[DataDefinition]
+public partial struct VoltageSetting
+{
+    /// <summary>
+    /// The voltage of the setting
+    /// </summary>
+    [DataField(required: true)]
+    public Voltage Voltage;
+
+    /// <summary>
+    /// Name of the node for the cable.
+    /// Must be a <c>CableDeviceNode</c>
+    /// </summary>
+    [DataField(required: true)]
+    public string Node = string.Empty;
+
+    /// <summary>
+    /// Name of the setting.
+    /// </summary>
+    [DataField(required: true)]
+    public LocId Name;
+}
+
+/// <summary>
+/// Event called when the voltage of a device with <see cref="VoltageTogglerComponent"/> is toggled
+/// </summary>
+/// <param name="NewVoltage">the new voltage setting</param>
+[ByRefEvent]
+public record struct VoltageChangeEvent(VoltageSetting NewVoltage);
---
+++ Content.Shared/Power/EntitySystems/SharedVoltageTogglerSystem.cs
@@ -0,0 +1,97 @@
+﻿using Content.Shared.Examine;
+using Content.Shared.Power.Components;
+using Content.Shared.Verbs;
+
+namespace Content.Shared.Power.EntitySystems;
+
+public abstract class SharedVoltageTogglerSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<VoltageTogglerComponent, ExaminedEvent>(OnExamined);
+        SubscribeLocalEvent<VoltageTogglerComponent, GetVerbsEvent<Verb>>(OnGetVerb);
+    }
+
+    private void OnExamined(Entity<VoltageTogglerComponent> entity, ref ExaminedEvent args)
+    {
+        var voltage = entity.Comp.Settings[entity.Comp.SelectedVoltageLevel].Voltage;
+        args.PushMarkup(Loc.GetString(entity.Comp.ExamineText, ("voltage", VoltageString(voltage, true))));
+    }
+
+    private void OnGetVerb(Entity<VoltageTogglerComponent> entity, ref GetVerbsEvent<Verb> args)
+    {
+        if (!args.CanAccess || !args.CanInteract)
+            return;
+
+        var user = args.User;
+
+        var index = 0;
+        foreach (var setting in entity.Comp.Settings)
+        {
+            // This is because Act won't work with index.
+            // Needs it to be saved in the loop.
+            var currIndex = index;
+            var verb = new Verb
+            {
+                Priority = currIndex,
+                Category = VerbCategory.VoltageLevel,
+                Disabled = entity.Comp.SelectedVoltageLevel == currIndex,
+                Text = Loc.GetString(setting.Name),
+                Act = () =>
+                {
+                    ChangeVoltage(entity, currIndex, user);
+                }
+            };
+
+            var ev = new ToggleVoltageCheckEvent();
+            RaiseLocalEvent(entity, ref ev);
+            if (ev.DisableMessage != null)
+            {
+                verb.Message = ev.DisableMessage;
+                verb.Disabled = true;
+            }
+
+            args.Verbs.Add(verb);
+            index++;
+        }
+    }
+
+    public string VoltageString(Voltage voltage, bool color=false)
+    {
+        var voltageNoColor = voltage switch
+        {
+            Voltage.High => "HV",
+            Voltage.Medium => "MV",
+            Voltage.Apc => "LV",
+            _ => "Unknown",
+        };
+
+        if (color)
+            return Loc.GetString("power-voltage", ("voltage", voltageNoColor));
+
+        return voltageNoColor;
+    }
+
+    /// <summary>
+    /// This is used by the GeneretorWindow.xaml.cs
+    /// </summary>
+    public Voltage GetVoltage(Entity<VoltageTogglerComponent> entity)
+    {
+        return entity.Comp.Settings[entity.Comp.SelectedVoltageLevel].Voltage;
+    }
+
+    /// <summary>
+    /// This is used by the GeneretorWindow.xaml.cs
+    /// </summary>
+    public Voltage GetNextVoltage(Entity<VoltageTogglerComponent> entity)
+    {
+        var nextVoltageLevel = (entity.Comp.SelectedVoltageLevel + 1) % entity.Comp.Settings.Length;
+        return entity.Comp.Settings[nextVoltageLevel].Voltage;
+    }
+
+    protected virtual void ChangeVoltage(Entity<VoltageTogglerComponent> entity, int settingIndex, EntityUid? user) {}
+}
+
+[ByRefEvent]
+public record struct ToggleVoltageCheckEvent(string? DisableMessage = null);
