410bc7e330db094c54bc912875b69e7227bbbc16
psykana <36602558+psykana@users.noreply.github.com>
2026-04-24 12:53:03 +0100

    mixer

--- Content.Shared/Atmos/Piping/Trinary/Components/GasMixerComponent.cs
+++ Content.Shared/Atmos/Piping/Trinary/Components/GasMixerComponent.cs
@@ -1,42 +1,33 @@
-using Content.Server.Atmos.Piping.Trinary.EntitySystems;
 using Content.Shared.Atmos;
+using Robust.Shared.GameStates;
 
-namespace Content.Server.Atmos.Piping.Trinary.Components
+namespace Content.Server.Atmos.Piping.Trinary.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
+//[Access(typeof(GasMixerSystem))]
+public sealed partial class GasMixerComponent : Component
 {
-    [RegisterComponent]
-    [Access(typeof(GasMixerSystem))]
-    public sealed partial class GasMixerComponent : Component
-    {
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("enabled")]
-        public bool Enabled = true;
+    [DataField, AutoNetworkedField]
+    public bool Enabled = true;
 
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("inletOne")]
-        public string InletOneName = "inletOne";
+    [ViewVariables]
+    public string InletOneName = "inletOne";
 
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("inletTwo")]
-        public string InletTwoName = "inletTwo";
+    [ViewVariables]
+    public string InletTwoName = "inletTwo";
 
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("outlet")]
-        public string OutletName = "outlet";
+    [ViewVariables]
+    public string OutletName = "outlet";
 
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("targetPressure")]
-        public float TargetPressure = Atmospherics.OneAtmosphere;
+    [DataField, AutoNetworkedField]
+    public float TargetPressure = Atmospherics.OneAtmosphere;
 
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("maxTargetPressure")]
-        public float MaxTargetPressure = Atmospherics.MaxOutputPressure;
+    [DataField, AutoNetworkedField]
+    public float MaxTargetPressure = Atmospherics.MaxOutputPressure;
 
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("inletOneConcentration")]
-        public float InletOneConcentration = 0.5f;
+    [DataField, AutoNetworkedField]
+    public float InletOneConcentration = 0.5f;
 
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("inletTwoConcentration")]
-        public float InletTwoConcentration = 0.5f;
-    }
+    [DataField, AutoNetworkedField]
+    public float InletTwoConcentration = 0.5f;
 }
