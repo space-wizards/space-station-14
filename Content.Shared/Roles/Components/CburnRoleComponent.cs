d20505355641f3959df5848f8e29e01d5310ad17
nomdéraisonnablementlong <you@example.com>
2026-04-24 08:26:24 -0400

    removal

--- Content.Shared/Roles/Components/CburnRoleComponent.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared.Roles.Components;
-
-/// <summary>
-/// Added to mind role entities to tag that they are a CBURN agent.
-/// </summary>
-[RegisterComponent, NetworkedComponent]
-public sealed partial class CburnRoleComponent : BaseMindRoleComponent;
