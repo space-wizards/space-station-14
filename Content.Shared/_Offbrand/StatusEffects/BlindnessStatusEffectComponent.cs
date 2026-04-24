7ecdace8a46e1acc0ac01bcf6c8c19bdcb888990
Janet Blackquill <uhhadd@gmail.com>
2026-04-24 19:47:37 -0400

    ok then

--- Content.Shared/_Offbrand/StatusEffects/BlindnessStatusEffectComponent.cs
+++
@@ -1,5 +0,0 @@
-namespace Content.Shared._Offbrand.StatusEffects;
-
-[RegisterComponent]
-[Access(typeof(BlindnessStatusEffectSystem))]
-public sealed partial class BlindnessStatusEffectComponent : Component;
