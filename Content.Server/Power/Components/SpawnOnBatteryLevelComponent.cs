b7fb9d197fdea926410c314e19d94af5de784510
Samuel-C <samuelcamilo8@gmail.com>
2026-04-25 11:47:01 -0300

    code stuff

---
+++ Content.Server/Power/Components/SpawnOnBatteryLevelComponent.cs
@@ -0,0 +1,32 @@
+﻿using Content.Shared.EntityTable.EntitySelectors;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.Power.Components;
+
+/// <summary>
+/// Spawns a entity when the battery reaches a certain amount of power.
+/// It also consumes that much power when spawning the entity.
+/// </summary>
+[RegisterComponent]
+public sealed partial class SpawnOnBatteryLevelComponent : Component
+{
+    /// <summary>
+    /// A entity proto to spawn.
+    /// </summary>
+    /// <remarks>Takes priority over <see cref="Table"/></remarks>
+    [DataField]
+    public EntProtoId? Proto;
+
+    /// <summary>
+    /// A entity table to spawn stuff from.
+    /// </summary>
+    [DataField]
+    public EntityTableSelector? Table;
+
+    /// <summary>
+    /// Amount of power in the battery (in joules) to spawn entity
+    /// </summary>
+    /// <remarks>It's initialized to have the value of the max battery charge if 0.0f</remarks>
+    [DataField]
+    public float Charge;
+}
---
+++ Content.Server/Power/EntitySystems/SpawnOnBatteryLevelSystem.cs
@@ -0,0 +1,50 @@
+﻿using Content.Server.Power.Components;
+using Content.Shared.EntityTable;
+using Content.Shared.Power;
+using Content.Shared.Power.Components;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.Power.EntitySystems;
+
+public sealed class SpawnOnBatteryLevelSystem : EntitySystem
+{
+    [Dependency] private readonly BatterySystem _battery = null!;
+    [Dependency] private readonly EntityTableSystem _entityTable =  null!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<SpawnOnBatteryLevelComponent, ComponentInit>(OnComponentInit);
+        SubscribeLocalEvent<SpawnOnBatteryLevelComponent, ChargeChangedEvent>(OnBatteryChargeChange);
+    }
+
+    private void OnComponentInit(Entity<SpawnOnBatteryLevelComponent> entity, ref ComponentInit args)
+    {
+        if (entity.Comp.Charge != 0f || !TryComp<BatteryComponent>(entity, out var battery))
+            return;
+
+        entity.Comp.Charge = battery.MaxCharge;
+    }
+
+    private void OnBatteryChargeChange(Entity<SpawnOnBatteryLevelComponent> entity, ref ChargeChangedEvent args)
+    {
+        // only cares about battery charging, not discharging
+        if (args.Delta < 0)
+            return;
+
+        if (!TryComp<BatteryComponent>(entity, out var battery))
+            return;
+
+        if (battery.LastCharge < entity.Comp.Charge)
+            return;
+
+        var spawns = entity.Comp.Proto == null ? _entityTable.GetSpawns(entity.Comp.Table) : new List<EntProtoId>{entity.Comp.Proto.Value};
+        foreach (var spawn in spawns)
+        {
+            Spawn(spawn, Transform(entity).Coordinates);
+        }
+
+        _battery.ChangeCharge((entity, battery), -entity.Comp.Charge);
+    }
+}
