c5e5f72d9daabe31e0f7c107ebc1ebbed9ea0b9d
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2026-04-26 11:43:30 -0700

    two more instances

--- Content.Server/StationEvents/EventManagerSystem.cs
+++ Content.Server/StationEvents/EventManagerSystem.cs
@@ -163,6 +163,9 @@ public sealed class EventManagerSystem : EntitySystem
 
         foreach (var eventid in selectedEvents)
         {
+            if (GameTicker.IgnoredPresets.Contains(eventid))
+                continue;
+
             if (!_prototype.Resolve(eventid, out var eventproto))
             {
                 Log.Warning("An event ID has no prototype index!");
