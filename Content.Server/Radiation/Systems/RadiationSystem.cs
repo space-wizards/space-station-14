a89a71c1275b57a12109add9bc0c2c31a3ec4569
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2026-04-26 13:50:55 -0700

    dependencies

--- Content.Server/Radiation/Systems/RadiationSystem.cs
+++ Content.Server/Radiation/Systems/RadiationSystem.cs
@@ -17,10 +17,9 @@ public sealed partial class RadiationSystem : SharedRadiationSystem
     [Dependency] private readonly SharedStackSystem _stack = default!;
     [Dependency] private readonly SharedMapSystem _maps = default!;
 
-    private EntityQuery<RadiationBlockingContainerComponent> _blockerQuery;
-    private EntityQuery<RadiationGridResistanceComponent> _resistanceQuery;
-    private EntityQuery<MapGridComponent> _gridQuery;
-    private EntityQuery<StackComponent> _stackQuery;
+    [Dependency] private readonly EntityQuery<RadiationBlockingContainerComponent> _blockerQuery = default!;
+    [Dependency] private readonly EntityQuery<RadiationGridResistanceComponent> _resistanceQuery = default!;
+    [Dependency] private readonly EntityQuery<MapGridComponent> _gridQuery = default!;
 
     private float _accumulator;
     private List<SourceData> _sources = new();
@@ -30,11 +29,6 @@ public sealed partial class RadiationSystem : SharedRadiationSystem
         base.Initialize();
         SubscribeCvars();
         InitRadBlocking();
-
-        _blockerQuery = GetEntityQuery<RadiationBlockingContainerComponent>();
-        _resistanceQuery = GetEntityQuery<RadiationGridResistanceComponent>();
-        _gridQuery = GetEntityQuery<MapGridComponent>();
-        _stackQuery = GetEntityQuery<StackComponent>();
     }
 
     public override void Update(float frameTime)
