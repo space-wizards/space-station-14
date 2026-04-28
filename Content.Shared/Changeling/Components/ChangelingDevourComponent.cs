9beeac0663f2ad68baa0e7795747b20d2327c7a5
ScarKy0 <scarky0@onet.eu>
2026-04-28 16:39:00 +0200

    yeah

--- Content.Shared/Changeling/Components/ChangelingDevourComponent.cs
+++ Content.Shared/Changeling/Components/ChangelingDevourComponent.cs
@@ -133,7 +133,7 @@ public sealed partial class ChangelingDevourComponent : Component
     [DataField, AutoNetworkedField]
     public Dictionary<string, FixedPoint2> DevourDnaReward = new()
     {
-        { "ChangelingDNA", 10 }
+        { "ChangelingDNA", 2 }
     };
 
     public override bool SendOnlyToOwner => true;
