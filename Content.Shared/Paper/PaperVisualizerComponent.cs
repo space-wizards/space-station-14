fef5de5d64e4a440b614fd1ea8348d990b35c425
SlamBamActionman <slambamactionman@gmail.com>
2026-05-01 16:40:05 +0200

    minor tweak

--- Content.Shared/Paper/PaperVisualizerComponent.cs
+++ Content.Shared/Paper/PaperVisualizerComponent.cs
@@ -4,6 +4,4 @@ namespace Content.Shared.Paper;
 /// Visualizer adjusting the written and stamps states for a paper sprite.
 /// </summary>
 [RegisterComponent]
-public sealed partial class PaperVisualizerComponent : Component
-{
-}
+public sealed partial class PaperVisualizerComponent : Component;
