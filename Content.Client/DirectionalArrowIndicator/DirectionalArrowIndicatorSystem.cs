504813c553a06422f94e3e2bba0f0f6e78d50d46
Łukasz Lindert <lukasz.lindert@protonmail.com>
2026-05-04 15:23:57 +0200

    arrow

---
+++ Content.Client/DirectionalArrowIndicator/DirectionalArrowIndicatorSystem.cs
@@ -0,0 +1,23 @@
+using Content.Shared.DirectionalArrowIndicator;
+using Content.Shared.Examine;
+using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client.DirectionalArrowIndicator;
+
+public sealed class DirectionalArrowIndicatorSystem : EntitySystem
+{
+    private static readonly EntProtoId ExamineArrow = "TurnstileArrow";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<DirectionalArrowIndicatorComponent, ExaminedEvent>(OnExamined);
+    }
+
+    private void OnExamined(Entity<DirectionalArrowIndicatorComponent> ent, ref ExaminedEvent args)
+    {
+        Spawn(ExamineArrow, new EntityCoordinates(ent, 0, 0));
+    }
+}
---
+++ Content.Shared/DirectionalArrowIndicator/DirectionalArrowIndicatorComponent.cs
@@ -0,0 +1,8 @@
+
+namespace Content.Shared.DirectionalArrowIndicator;
+
+[RegisterComponent]
+public sealed partial class DirectionalArrowIndicatorComponent : Component
+{
+
+}
