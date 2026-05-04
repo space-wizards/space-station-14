9bec7bd685facb03cab568b60c7fae1507d6f8b0
Łukasz Lindert <lukasz.lindert@protonmail.com>
2026-05-04 21:28:31 +0200

    done

--- Content.Client/DirectionalArrowIndicator/DirectionalArrowIndicatorSystem.cs
+++ Content.Client/DirectionalArrowIndicator/DirectionalArrowIndicatorSystem.cs
@@ -2,6 +2,7 @@ using Content.Shared.DirectionalArrowIndicator;
 using Content.Shared.Examine;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Spawners;
 
 namespace Content.Client.DirectionalArrowIndicator;
 
@@ -12,12 +13,34 @@ public sealed class DirectionalArrowIndicatorSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-
         SubscribeLocalEvent<DirectionalArrowIndicatorComponent, ExaminedEvent>(OnExamined);
     }
 
     private void OnExamined(Entity<DirectionalArrowIndicatorComponent> ent, ref ExaminedEvent args)
     {
-        Spawn(ExamineArrow, new EntityCoordinates(ent, 0, 0));
+        var arrows = ent.Comp.Arrows;
+        var lifetime = ent.Comp.Lifetime;
+
+        if (arrows.Count == 0)
+        {
+            SpawnArrow(new EntityCoordinates(ent, 0, 0), lifetime, Angle.Zero);
+            return;
+        }
+        foreach (var arrowData in ent.Comp.Arrows)
+        {
+            SpawnArrow(new EntityCoordinates(ent, arrowData.Offset), lifetime, arrowData.Rotation);
+        }
+    }
+
+    private void SpawnArrow(EntityCoordinates coords, float lifetime, Angle rotation)
+    {
+        var spawnedEnt = Spawn(ExamineArrow, coords);
+
+        Transform(spawnedEnt).LocalRotation = rotation;
+
+        if (TryComp<TimedDespawnComponent>(spawnedEnt, out var timedDespawn))
+        {
+            timedDespawn.Lifetime = lifetime;
+        }
     }
 }
--- Content.Shared/DirectionalArrowIndicator/DirectionalArrowIndicatorComponent.cs
+++ Content.Shared/DirectionalArrowIndicator/DirectionalArrowIndicatorComponent.cs
@@ -1,8 +1,24 @@
 
+using System.Numerics;
+
 namespace Content.Shared.DirectionalArrowIndicator;
 
 [RegisterComponent]
 public sealed partial class DirectionalArrowIndicatorComponent : Component
 {
+    [DataField]
+    public float Lifetime = 2f;
 
+    [DataField]
+    public List<ArrowSpawnData> Arrows = new();
+}
+
+[DataDefinition]
+public sealed partial class ArrowSpawnData
+{
+    [DataField]
+    public Vector2 Offset = Vector2.Zero;
+
+    [DataField]
+    public Angle Rotation = Angle.Zero;
 }
