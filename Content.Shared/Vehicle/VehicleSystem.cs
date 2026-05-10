076b67f149edc6f27279be0575d749fc0503c1a8
Fenriz <kastonbag552@gmail.com>
2026-05-10 21:56:14 +0300

    fix

--- Content.Shared/Vehicle/VehicleSystem.cs
+++ Content.Shared/Vehicle/VehicleSystem.cs
@@ -21,13 +21,13 @@ namespace Content.Shared.Vehicle;
 /// </summary>
 public sealed partial class VehicleSystem : EntitySystem
 {
-    [Dependency] private readonly ActionBlockerSystem _actionBlocker = default!;
-    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
-    [Dependency] private readonly SharedContainerSystem _container = default!;
-    [Dependency] private readonly DamageableSystem _damageable = default!;
-    [Dependency] private readonly EntityWhitelistSystem _entityWhitelist = default!;
-    [Dependency] private readonly SharedMoverController _mover = default!;
-    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private ActionBlockerSystem _actionBlocker = default!;
+    [Dependency] private SharedAppearanceSystem _appearance = default!;
+    [Dependency] private SharedContainerSystem _container = default!;
+    [Dependency] private DamageableSystem _damageable = default!;
+    [Dependency] private EntityWhitelistSystem _entityWhitelist = default!;
+    [Dependency] private SharedMoverController _mover = default!;
+    [Dependency] private IGameTiming _timing = default!;
 
     private EntityQuery<VehicleComponent> _vehicleQuery;
     private EntityQuery<VehicleOperatorComponent> _operatorQuery;
