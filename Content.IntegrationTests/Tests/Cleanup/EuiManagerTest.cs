8d2b7cd48bb9934e0ce36ce1d4a8c11e0662c0f2
Tayrtahn <tayrtahn@gmail.com>
2026-05-13 12:18:31 -0400

    EuiManagerTest

--- Content.IntegrationTests/Tests/Cleanup/EuiManagerTest.cs
+++ Content.IntegrationTests/Tests/Cleanup/EuiManagerTest.cs
@@ -1,5 +1,7 @@
+#nullable enable
 using System.Linq;
 using Content.IntegrationTests.Fixtures;
+using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Server.Administration.UI;
 using Content.Server.EUI;
 using Robust.Server.Player;
@@ -8,28 +10,25 @@ namespace Content.IntegrationTests.Tests.Cleanup;
 
 public sealed class EuiManagerTest : GameTest
 {
-    public override PoolSettings PoolSettings => new PoolSettings
+    public override PoolSettings PoolSettings => new()
     {
         Connected = true,
         Dirty = true
     };
+
+    [SidedDependency(Side.Server)] private IPlayerManager _sPlayerManager = null!;
+    [SidedDependency(Side.Server)] private EuiManager _euiManager = null!;
 
     [Test]
     [Retry(2)]
     // Even though we are using the server EUI here, we actually want to see if the client EUIManager crashes
     public async Task EuiManagerRecycleWithOpenWindowTest()
     {
-        var pair = Pair;
-        var server = pair.Server;
-
-        var sPlayerManager = server.ResolveDependency<IPlayerManager>();
-        var eui = server.ResolveDependency<EuiManager>();
-
-        await server.WaitAssertion(() =>
+        await Server.WaitAssertion(() =>
         {
-            var clientSession = sPlayerManager.Sessions.Single();
+            var clientSession = _sPlayerManager.Sessions.Single();
             var ui = new AdminAnnounceEui();
-            eui.OpenEui(ui, clientSession);
+            _euiManager.OpenEui(ui, clientSession);
         });
 
         await RunUntilSynced();
