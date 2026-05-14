606a352c6bf37a53c49041954e7bb8bba47fea86
Tayrtahn <tayrtahn@gmail.com>
2026-05-14 11:54:50 -0400

    CharacterCreationTest

--- Content.IntegrationTests/Tests/Lobby/CharacterCreationTest.cs
+++ Content.IntegrationTests/Tests/Lobby/CharacterCreationTest.cs
@@ -1,5 +1,7 @@
+#nullable enable
 using Content.Client.Lobby;
 using Content.IntegrationTests.Fixtures;
+using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Server.Preferences.Managers;
 using Content.Shared.Humanoid;
 using Content.Shared.Preferences;
@@ -7,82 +9,85 @@ using Robust.Client.State;
 
 namespace Content.IntegrationTests.Tests.Lobby;
 
-[TestFixture]
 [TestOf(typeof(ClientPreferencesManager))]
 [TestOf(typeof(ServerPreferencesManager))]
 public sealed class CharacterCreationTest : GameTest
 {
-    public override PoolSettings PoolSettings => new() { InLobby = true };
+    public override PoolSettings PoolSettings => new()
+    {
+        InLobby = true,
+    };
+
+    [SidedDependency(Side.Client)] private IClientPreferencesManager _cPrefManager = null!;
+    [SidedDependency(Side.Client)] private IStateManager _cStateManager = null!;
+    [SidedDependency(Side.Server)] private IServerPreferencesManager _sPrefManager = null!;
 
     [Test]
     public async Task CreateDeleteCreateTest()
     {
-        var pair = Pair;
-        var server = pair.Server;
-        var client = pair.Client;
-        var user = pair.Client.User!.Value;
-        var clientPrefManager = client.Resolve<IClientPreferencesManager>();
-        var serverPrefManager = server.Resolve<IServerPreferencesManager>();
+        Assert.That(Client.User, Is.Not.Null);
+        var user = Client.User.Value;
 
-        Assert.That(client.Resolve<IStateManager>().CurrentState, Is.TypeOf<LobbyState>());
-        await client.WaitPost(() => clientPrefManager.SelectCharacter(0));
-        await pair.RunTicksSync(5);
+        Assert.That(_cStateManager.CurrentState, Is.TypeOf<LobbyState>());
+        await Client.WaitPost(() => _cPrefManager.SelectCharacter(0));
 
-        var clientCharacters = clientPrefManager.Preferences?.Characters;
+        await RunTicksSync(5);
+
+        var clientCharacters = _cPrefManager.Preferences?.Characters;
         Assert.That(clientCharacters, Is.Not.Null);
         Assert.That(clientCharacters, Has.Count.EqualTo(1));
 
-        HumanoidCharacterProfile profile = null;
-        await client.WaitPost(() =>
+        HumanoidCharacterProfile profile = null!;
+        await Client.WaitPost(() =>
         {
             profile = HumanoidCharacterProfile.Random();
-            clientPrefManager.CreateCharacter(profile);
+            _cPrefManager.CreateCharacter(profile);
         });
-        await pair.RunTicksSync(5);
+        await RunTicksSync(5);
 
-        clientCharacters = clientPrefManager.Preferences?.Characters;
+        clientCharacters = _cPrefManager.Preferences?.Characters;
         Assert.That(clientCharacters, Is.Not.Null);
         Assert.That(clientCharacters, Has.Count.EqualTo(2));
         AssertEqual(clientCharacters[1], profile);
 
-        await PoolManager.WaitUntil(server, () => serverPrefManager.GetPreferences(user).Characters.Count == 2, maxTicks: 60);
+        await PoolManager.WaitUntil(Server, () => _sPrefManager.GetPreferences(user).Characters.Count == 2, maxTicks: 60);
 
-        var serverCharacters = serverPrefManager.GetPreferences(user).Characters;
+        var serverCharacters = _sPrefManager.GetPreferences(user).Characters;
         Assert.That(serverCharacters, Has.Count.EqualTo(2));
         AssertEqual(serverCharacters[1], profile);
 
-        await client.WaitAssertion(() => clientPrefManager.DeleteCharacter(1));
-        await pair.RunTicksSync(5);
-        Assert.That(clientPrefManager.Preferences?.Characters.Count, Is.EqualTo(1));
-        await PoolManager.WaitUntil(server, () => serverPrefManager.GetPreferences(user).Characters.Count == 1, maxTicks: 60);
-        Assert.That(serverPrefManager.GetPreferences(user).Characters.Count, Is.EqualTo(1));
+        await Client.WaitAssertion(() => _cPrefManager.DeleteCharacter(1));
+        await RunTicksSync(5);
+        Assert.That(_cPrefManager.Preferences?.Characters, Has.Count.EqualTo(1));
+        await PoolManager.WaitUntil(Server, () => _sPrefManager.GetPreferences(user).Characters.Count == 1, maxTicks: 60);
+        Assert.That(_sPrefManager.GetPreferences(user).Characters, Has.Count.EqualTo(1));
 
-        await client.WaitIdleAsync();
+        await Client.WaitIdleAsync();
 
-        await client.WaitAssertion(() =>
+        await Client.WaitAssertion(() =>
         {
             profile = HumanoidCharacterProfile.Random();
-            clientPrefManager.CreateCharacter(profile);
+            _cPrefManager.CreateCharacter(profile);
         });
-        await pair.RunTicksSync(5);
+        await RunTicksSync(5);
 
-        clientCharacters = clientPrefManager.Preferences?.Characters;
+        clientCharacters = _cPrefManager.Preferences?.Characters;
         Assert.That(clientCharacters, Is.Not.Null);
         Assert.That(clientCharacters, Has.Count.EqualTo(2));
         AssertEqual(clientCharacters[1], profile);
 
-        await PoolManager.WaitUntil(server, () => serverPrefManager.GetPreferences(user).Characters.Count == 2, maxTicks: 60);
-        serverCharacters = serverPrefManager.GetPreferences(user).Characters;
+        await PoolManager.WaitUntil(Server, () => _sPrefManager.GetPreferences(user).Characters.Count == 2, maxTicks: 60);
+        serverCharacters = _sPrefManager.GetPreferences(user).Characters;
         Assert.That(serverCharacters, Has.Count.EqualTo(2));
         AssertEqual(serverCharacters[1], profile);
     }
 
-    private void AssertEqual(HumanoidCharacterProfile a, HumanoidCharacterProfile b)
+    private static void AssertEqual(HumanoidCharacterProfile a, HumanoidCharacterProfile b)
     {
         if (a.MemberwiseEquals(b))
             return;
 
-        Assert.Multiple(() =>
+        using (Assert.EnterMultipleScope())
         {
             Assert.That(a.Name, Is.EqualTo(b.Name));
             Assert.That(a.Age, Is.EqualTo(b.Age));
@@ -98,17 +103,20 @@ public sealed class CharacterCreationTest : GameTest
             Assert.That(a.Loadouts, Is.EquivalentTo(b.Loadouts));
             AssertEqual(a.Appearance, b.Appearance);
             Assert.Fail("Profile not equal");
-        });
+        }
     }
 
-    private void AssertEqual(HumanoidCharacterAppearance a, HumanoidCharacterAppearance b)
+    private static void AssertEqual(HumanoidCharacterAppearance a, HumanoidCharacterAppearance b)
     {
         if (a.Equals(b))
             return;
 
-        Assert.That(a.EyeColor, Is.EqualTo(b.EyeColor));
-        Assert.That(a.SkinColor, Is.EqualTo(b.SkinColor));
-        Assert.That(a.Markings, Is.EquivalentTo(b.Markings));
-        Assert.Fail("Appearance not equal");
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(a.EyeColor, Is.EqualTo(b.EyeColor));
+            Assert.That(a.SkinColor, Is.EqualTo(b.SkinColor));
+            Assert.That(a.Markings, Is.EquivalentTo(b.Markings));
+            Assert.Fail("Appearance not equal");
+        }
     }
 }
