44ff5c50d2150679f4fab493c0d0d2710620c705
Tayrtahn <tayrtahn@gmail.com>
2026-05-15 11:44:26 -0400

    PvsCommandTest

--- Content.IntegrationTests/Tests/Networking/PvsCommandTest.cs
+++ Content.IntegrationTests/Tests/Networking/PvsCommandTest.cs
@@ -1,52 +1,52 @@
+#nullable enable
 using Content.IntegrationTests.Fixtures;
-using Robust.Shared.GameObjects;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Prototypes;
 
 namespace Content.IntegrationTests.Tests.Networking;
 
-[TestFixture]
 public sealed class PvsCommandTest : GameTest
 {
     private static readonly EntProtoId TestEnt = "MobHuman";
 
-    public override PoolSettings PoolSettings => new() { Connected = true, DummyTicker = false };
+    public override PoolSettings PoolSettings => new()
+    {
+        Connected = true,
+        DummyTicker = false,
+    };
 
     [Test]
     public async Task TestPvsCommands()
     {
-        var pair = Pair;
-        var (server, client) = pair;
-
         // Spawn a complex entity.
-        EntityUid entity = default;
-        await server.WaitPost(() => entity = server.EntMan.Spawn(TestEnt));
-        await pair.RunTicksSync(5);
+        var entity = await Spawn(TestEnt);
+        await RunUntilSynced();
 
-        // Check that the client has a variety pf entities.
-        Assert.That(client.EntMan.EntityCount, Is.GreaterThan(0));
-        Assert.That(client.EntMan.Count<MapComponent>, Is.GreaterThan(0));
-        Assert.That(client.EntMan.Count<MapGridComponent>, Is.GreaterThan(0));
+        using (Assert.EnterMultipleScope())
+        {
+            // Check that the client has a variety of entities.
+            Assert.That(CEntMan.EntityCount, Is.GreaterThan(0));
+            Assert.That(CEntMan.Count<MapComponent>(), Is.GreaterThan(0));
+            Assert.That(CEntMan.Count<MapGridComponent>(), Is.GreaterThan(0));
+        }
 
-        var meta = client.MetaData(pair.ToClientUid(entity));
+        var meta = Client.MetaData(ToClientUid(entity));
         var lastApplied = meta.LastStateApplied;
 
         // Dirty all entities
-        await server.ExecuteCommand("dirty");
-        await pair.RunTicksSync(5);
+        await Server.ExecuteCommand("dirty");
+        await RunUntilSynced();
         Assert.That(meta.LastStateApplied, Is.GreaterThan(lastApplied));
-        await pair.RunTicksSync(5);
+        await RunUntilSynced();
 
         // Do a client-side full state reset
-        await client.ExecuteCommand("resetallents");
-        await pair.RunTicksSync(5);
+        await Client.ExecuteCommand("resetallents");
+        await RunUntilSynced();
 
         // Request a full server state.
         lastApplied = meta.LastStateApplied;
-        await client.ExecuteCommand("fullstatereset");
-        await pair.RunTicksSync(10);
+        await Client.ExecuteCommand("fullstatereset");
+        await RunUntilSynced();
         Assert.That(meta.LastStateApplied, Is.GreaterThan(lastApplied));
-
-        await server.WaitPost(() => server.EntMan.DeleteEntity(entity));
     }
 }
