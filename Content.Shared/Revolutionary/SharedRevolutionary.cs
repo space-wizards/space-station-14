cc3845fbe08eb92d810d0761b6ce41b677b0a1c1
themias <themias5@gmail.com>
2026-05-18 13:40:12 -0400

    fix

--- Content.Shared/Revolutionary/SharedRevolutionary.cs
+++ Content.Shared/Revolutionary/SharedRevolutionary.cs
@@ -1,8 +1,5 @@
 namespace Content.Shared.Revolutionary;
 
-public sealed class AttemptConvertRevolutionaryEvent : CancellableEntityEventArgs
-{
-
 /// <summary>
 /// Raised when a revolutionary conversion is being attempted on an entity.
 /// </summary>
