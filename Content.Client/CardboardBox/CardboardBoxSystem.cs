9a49ac931ca5b7ad509a36c0cd9b66d62dc7871f
Pok245 <113675512+Pok27@users.noreply.github.com>
2026-05-19 18:00:16 +0300

    CardboardBox-System-Predict

--- Content.Client/CardboardBox/CardboardBoxSystem.cs
+++ Content.Client/CardboardBox/CardboardBoxSystem.cs
@@ -11,10 +11,11 @@ namespace Content.Client.CardboardBox;
 public sealed partial class CardboardBoxSystem : SharedCardboardBoxSystem
 {
     [Dependency] private EntityLookupSystem _entityLookup = default!;
-    [Dependency] private TransformSystem _transform = default!;
     [Dependency] private ExamineSystemShared _examine = default!;
     [Dependency] private SpriteSystem _sprite = default!;
-    [Dependency] private EntityQuery<MobStateComponent> _mobStateQuery = default!;
+    [Dependency] private TransformSystem _transform = default!;
+
+    [Dependency] private EntityQuery<MobStateComponent> _mobStateQuery;
 
     public override void Initialize()
     {
@@ -33,13 +34,13 @@ public sealed partial class CardboardBoxSystem : SharedCardboardBoxSystem
         var xform = Transform(source);
         var sourcePos = _transform.GetMapCoordinates(source, xform);
 
-        //Any mob that can move should be surprised?
-        //God mind rework needs to come faster so it can just check for mind
-        //TODO: Replace with Mind Query when mind rework is in.
+        // Any mob that can move should be surprised?
+        // God mind rework needs to come faster so it can just check for mind
+        // TODO: Replace with Mind Query when mind rework is in.
         var mobMoverEntities = new List<EntityUid>();
         var mover = GetEntity(msg.Mover);
 
-        //Filter out entities in range to see that they're a mob and add them to the mobMoverEntities hash for faster lookup
+        // Filter out entities in range to see that they're a mob and add them to the mobMoverEntities hash for faster lookup
         var movers = new HashSet<Entity<MobMoverComponent>>();
         _entityLookup.GetEntitiesInRange(xform.Coordinates, box.Distance, movers);
 
@@ -52,7 +53,7 @@ public sealed partial class CardboardBoxSystem : SharedCardboardBoxSystem
             mobMoverEntities.Add(uid);
         }
 
-        //Play the effect for the mobs as long as they can see the box and are in range.
+        // Play the effect for the mobs as long as they can see the box and are in range.
         foreach (var mob in mobMoverEntities)
         {
             var mapPos = _transform.GetMapCoordinates(mob);
@@ -64,13 +65,11 @@ public sealed partial class CardboardBoxSystem : SharedCardboardBoxSystem
                 continue;
 
             var ent = Spawn(box.Effect, mapPos);
-
             if (!TryComp(ent, out TransformComponent? entTransform) || !TryComp<SpriteComponent>(ent, out var sprite))
                 continue;
 
             _sprite.SetOffset((ent, sprite), new Vector2(0, 1));
             _transform.SetParent(ent, entTransform, mob);
         }
-
     }
 }
