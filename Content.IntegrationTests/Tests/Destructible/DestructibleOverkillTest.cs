44cc6174eeca485c2956f15517f30b349107c999
Connor Huffine <chuffine@gmail.com>
2026-05-23 18:32:17 -0400

    Update DestructibleOverkillTest.cs

--- Content.IntegrationTests/Tests/Destructible/DestructibleOverkillTest.cs
+++ Content.IntegrationTests/Tests/Destructible/DestructibleOverkillTest.cs
@@ -14,10 +14,10 @@ namespace Content.IntegrationTests.Tests.Destructible;
 /// <summary>
 /// Tests ensuring the correct operation of <see cref="SharedDestructibleSystem"/>.
 /// </summary>
-[TestFixture]
 public sealed class DestructibleOverkillTest : GameTest
 {
-    [SidedDependency(Side.Server)] private readonly IEntitySystemManager _sEntSysManager = default!;
+    [SidedDependency(Side.Server)] private TestDestructibleListenerSystem _sDestructibleListenerSystem = default!;
+    [SidedDependency(Side.Server)] private DamageableSystem _sDamageableSystem = default!;
 
     /// <summary>
     /// Test that an entity with consequences is destroyed cleanly when overkilled.
@@ -31,14 +31,12 @@ public sealed class DestructibleOverkillTest : GameTest
         var baseEntityCount = SEntMan.EntityCount;
 
         EntityUid sDestructibleEntity = default;
-        TestDestructibleListenerSystem sTestThresholdListenerSystem = null;
 
         // Spawn our test entity and threshold listener
         await Server.WaitPost(() =>
         {
-            sDestructibleEntity = SEntMan.SpawnEntity(DestructibleDestructionEntityId, testMap.GridCoords);
-            sTestThresholdListenerSystem = _sEntSysManager.GetEntitySystem<TestDestructibleListenerSystem>();
-            sTestThresholdListenerSystem.ThresholdsReached.Clear();
+            sDestructibleEntity = SSpawnAtPosition(DestructibleDestructionEntityId, testMap.GridCoords);
+            _sDestructibleListenerSystem.ThresholdsReached.Clear();
         });
 
         await Server.WaitAssertion(() =>
@@ -49,28 +47,25 @@ public sealed class DestructibleOverkillTest : GameTest
             // Hit the destructible with enough damage to overkill
             Assert.DoesNotThrow(() =>
             {
-                SEntMan.System<DamageableSystem>().TryChangeDamage(sDestructibleEntity, bruteDamage, true);
+                _sDamageableSystem.TryChangeDamage(sDestructibleEntity, bruteDamage, true);
             });
 
             // We now verify that our component has the properties we expect
 
             // Our first threshold should be the overkill destruction
-            var threshold = sTestThresholdListenerSystem.ThresholdsReached[0].Threshold;
+            var threshold = _sDestructibleListenerSystem.ThresholdsReached[0].Threshold;
 
             // Ensure that the threshold triggered and only has one behavior
-            Assert.Multiple(() =>
+            using (Assert.EnterMultipleScope())
             {
                 Assert.That(threshold.Triggered, Is.True);
                 Assert.That(threshold.Behaviors, Has.Count.EqualTo(1));
-            });
+            }
 
             var doActsBehavior = (DoActsBehavior)threshold.Behaviors.Single(b => b is DoActsBehavior);
 
             // Ensure that the one act in this behavior is destruction
-            Assert.Multiple(() =>
-            {
-                Assert.That(doActsBehavior.HasAct(ThresholdActs.Destruction));
-            });
+            Assert.That(doActsBehavior.HasAct(ThresholdActs.Destruction));
         });
 
         await Server.WaitRunTicks(1);   // Wait for predicted delete
