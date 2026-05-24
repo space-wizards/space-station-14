45731bf6e1a8c6acd4943cf073a5585be4a4bb23
riccardi48 <richardballard00@gmail.com>
2026-05-24 14:17:09 +0100

    comment

--- Content.Shared/Weapons/Misc/SharedTetherGunSystem.cs
+++ Content.Shared/Weapons/Misc/SharedTetherGunSystem.cs
@@ -139,7 +139,7 @@ public abstract partial class SharedTetherGunSystem : EntitySystem
 
     private void OnTetherGunDropped(EntityUid uid, TetherGunComponent component, DroppedEvent args)
     {
-        if (component.Tethered == null || component.TetherEntity == null || component.TetherMirrorEntity == null)
+       if (component.Tethered == null || component.TetherEntity == null || component.TetherMirrorEntity == null)
             return;
         var tetheredCoords = TransformSystem.GetMapCoordinates(component.Tethered.Value);
         EntityCoordinates coords;
@@ -151,6 +151,8 @@ public abstract partial class SharedTetherGunSystem : EntitySystem
         {
             coords = TransformSystem.ToCoordinates(_mapSystem.GetMap(tetheredCoords.MapId), tetheredCoords);
         }
+        // TODO: Based on gun location before dropping
+        // Should be from after dropping
         if (TryComp(uid, out TransformComponent? gunXform))
         {
             if (coords.TryDelta(EntityManager, TransformSystem, gunXform.Coordinates, out Vector2 delta))
