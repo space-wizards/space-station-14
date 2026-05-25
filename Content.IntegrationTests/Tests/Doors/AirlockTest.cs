e31a696d9e3ddce8018ff6d21f04dc553a343605
Tayrtahn <tayrtahn@gmail.com>
2026-05-25 18:24:38 -0400

    AirlockTest

--- Content.IntegrationTests/Tests/Doors/AirlockTest.cs
+++ Content.IntegrationTests/Tests/Doors/AirlockTest.cs
@@ -1,24 +1,27 @@
+#nullable enable
 using System.Numerics;
 using Content.IntegrationTests.Fixtures;
+using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Server.Doors.Systems;
 using Content.Shared.Doors.Components;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
-using Robust.Shared.Maths;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Systems;
 
-namespace Content.IntegrationTests.Tests.Doors
+namespace Content.IntegrationTests.Tests.Doors;
+
+[TestOf(typeof(AirlockComponent))]
+public sealed class AirlockTest : GameTest
 {
-    [TestFixture]
-    [TestOf(typeof(AirlockComponent))]
-    public sealed class AirlockTest : GameTest
-    {
-        [TestPrototypes]
-        private const string Prototypes = @"
+    private const string AirlockPhysicsDummy = "AirlockPhysicsDummy";
+    private const string AirlockDummy = "AirlockDummy";
+
+    [TestPrototypes]
+    private const string Prototypes = $@"
 - type: entity
-  name: AirlockPhysicsDummy
-  id: AirlockPhysicsDummy
+  name: {AirlockPhysicsDummy}
+  id: {AirlockPhysicsDummy}
   components:
   - type: Physics
     bodyType: Dynamic
@@ -33,8 +36,8 @@ namespace Content.IntegrationTests.Tests.Doors
         - Impassable
 
 - type: entity
-  name: AirlockDummy
-  id: AirlockDummy
+  name: {AirlockDummy}
+  id: {AirlockDummy}
   components:
   - type: Door
   - type: Airlock
@@ -52,130 +55,117 @@ namespace Content.IntegrationTests.Tests.Doors
         mask:
         - Impassable
 ";
-        [Test]
-        public async Task OpenCloseDestroyTest()
+
+    [SidedDependency(Side.Server)] private DoorSystem _sDoorSystem = null!;
+    [SidedDependency(Side.Server)] private SharedPhysicsSystem _sharedPhysics = null!;
+    [SidedDependency(Side.Server)] private SharedTransformSystem _sXformSystem = null!;
+
+    [Test]
+    [Description("Check that door state transitions work correctly.")]
+    public async Task OpenCloseDestroyTest()
+    {
+        Entity<DoorComponent> airlock = default;
+
+        await Server.WaitAssertion(() =>
         {
-            var pair = Pair;
-            var server = pair.Server;
+            var uid = SSpawn(AirlockDummy);
+            airlock = SEntity<DoorComponent>(uid);
 
-            var entityManager = server.ResolveDependency<IEntityManager>();
-            var doors = entityManager.EntitySysManager.GetEntitySystem<DoorSystem>();
+            AssertDoorState(airlock, DoorState.Closed);
+        });
 
-            EntityUid airlock = default;
-            DoorComponent doorComponent = null;
+        await RunUntilSynced();
 
-            await server.WaitAssertion(() =>
+        await Server.WaitAssertion(() =>
+        {
+            _sDoorSystem.StartOpening(airlock);
+            AssertDoorState(airlock, DoorState.Opening);
+        });
+
+        await RunUntilSynced();
+
+        await WaitForDoorState(airlock, DoorState.Open);
+
+        await Server.WaitAssertion(() =>
+        {
+            _sDoorSystem.TryClose(airlock);
+            AssertDoorState(airlock, DoorState.Closing);
+        });
+
+        await WaitForDoorState(airlock, DoorState.Closed);
+    }
+
+    [Test]
+    [Description("Check that a closed airlock blocks the movement of a physics object.")]
+    public async Task AirlockBlockTest()
+    {
+        PhysicsComponent? physBody = null;
+        EntityUid airlockPhysicsDummy = default;
+        Entity<DoorComponent> airlock = default;
+
+        const int airlockPhysicsDummyStartingX = -1;
+
+        await Pair.CreateTestMap();
+
+        await Server.WaitAssertion(() =>
+        {
+            var dummyCoordinates = new EntityCoordinates(TestMap!.Grid, new Vector2(airlockPhysicsDummyStartingX, 0));
+            airlockPhysicsDummy = SSpawnAtPosition(AirlockPhysicsDummy, dummyCoordinates);
+
+            var uid = SSpawnAtPosition(AirlockDummy, TestMap.GridCoords);
+            airlock = SEntity<DoorComponent>(uid);
+
+            Assert.That(STryComp(airlockPhysicsDummy, out physBody), Is.True);
+            AssertDoorState(airlock, DoorState.Closed);
+        });
+
+        await RunUntilSynced();
+
+        // Push the dummy towards the airlock
+        await Server.WaitAssertion(() => Assert.That(physBody, Is.Not.Null));
+        await Server.WaitPost(() =>
+        {
+            _sharedPhysics.SetLinearVelocity(airlockPhysicsDummy, new Vector2(0.5f, 0f), body: physBody);
+        });
+
+        for (var i = 0; i < 240; i += 10)
+        {
+            // Keep the airlock awake so they collide
+            await Server.WaitPost(() =>
             {
-                airlock = entityManager.SpawnEntity("AirlockDummy", MapCoordinates.Nullspace);
-
-#pragma warning disable NUnit2045 // Interdependent assertions.
-                Assert.That(entityManager.TryGetComponent(airlock, out doorComponent), Is.True);
-                Assert.That(doorComponent.State, Is.EqualTo(DoorState.Closed));
-#pragma warning restore NUnit2045
+                _sharedPhysics.WakeBody(airlock);
             });
+            AssertDoorState(airlock, DoorState.Closed);
 
-            await server.WaitIdleAsync();
-
-            await server.WaitAssertion(() =>
-            {
-                doors.StartOpening(airlock);
-                Assert.That(doorComponent.State, Is.EqualTo(DoorState.Opening));
-            });
-
-            await server.WaitIdleAsync();
-
-            await PoolManager.WaitUntil(server, () => doorComponent.State == DoorState.Open);
-
-            Assert.That(doorComponent.State, Is.EqualTo(DoorState.Open));
-
-            await server.WaitAssertion(() =>
-            {
-                doors.TryClose(airlock);
-                Assert.That(doorComponent.State, Is.EqualTo(DoorState.Closing));
-            });
-
-            await PoolManager.WaitUntil(server, () => doorComponent.State == DoorState.Closed);
-
-            Assert.That(doorComponent.State, Is.EqualTo(DoorState.Closed));
-
-            await server.WaitAssertion(() =>
-            {
-                Assert.DoesNotThrow(() =>
-                {
-                    entityManager.DeleteEntity(airlock);
-                });
-            });
+            await RunTicksSync(10);
         }
 
-        [Test]
-        public async Task AirlockBlockTest()
-        {
-            var pair = Pair;
-            var server = pair.Server;
+        // Sanity check
+        // Sloth: Okay I'm sorry but I hate having to rewrite tests for every refactor
+        // If you see this yell at me in discord so I can continue to pretend this didn't happen.
+        // REMINDER THAT I STILL HAVE TO FIX THIS TEST EVERY OTHER PHYSICS PR
+        // _transform.GetMapCoordinates(UID HERE, xform: Assert.That(AirlockPhysicsDummy.Transform).X, Is.GreaterThan(AirlockPhysicsDummyStartingX));
 
-            await server.WaitIdleAsync();
 
-            var mapManager = server.ResolveDependency<IMapManager>();
-            var entityManager = server.ResolveDependency<IEntityManager>();
-            var physicsSystem = entityManager.System<SharedPhysicsSystem>();
-            var xformSystem = entityManager.System<SharedTransformSystem>();
+        var airlockPhysicsDummyFinalX = _sXformSystem.GetWorldPosition(airlockPhysicsDummy).X;
+        // The dummy moved, but was blocked by the airlock
+        Assert.That(airlockPhysicsDummyFinalX, Is.GreaterThan(airlockPhysicsDummyStartingX).And.LessThanOrEqualTo(0));
+    }
 
-            PhysicsComponent physBody = null;
-            EntityUid airlockPhysicsDummy = default;
-            EntityUid airlock = default;
-            DoorComponent doorComponent = null;
+    /// <summary>
+    /// Assert that the door is in a specific state.
+    /// </summary>
+    private static void AssertDoorState(Entity<DoorComponent> door, DoorState state)
+    {
+        Assert.That(door.Comp.State, Is.EqualTo(state));
+    }
 
-            var airlockPhysicsDummyStartingX = -1;
-
-            var map = await pair.CreateTestMap();
-
-            await server.WaitAssertion(() =>
-            {
-                var humanCoordinates = new MapCoordinates(new Vector2(airlockPhysicsDummyStartingX, 0), map.MapId);
-                airlockPhysicsDummy = entityManager.SpawnEntity("AirlockPhysicsDummy", humanCoordinates);
-
-                airlock = entityManager.SpawnEntity("AirlockDummy", new MapCoordinates(new Vector2(0, 0), map.MapId));
-
-                Assert.Multiple(() =>
-                {
-                    Assert.That(entityManager.TryGetComponent(airlockPhysicsDummy, out physBody), Is.True);
-                    Assert.That(entityManager.TryGetComponent(airlock, out doorComponent), Is.True);
-                });
-                Assert.That(doorComponent.State, Is.EqualTo(DoorState.Closed));
-            });
-
-            await server.WaitIdleAsync();
-
-            // Push the human towards the airlock
-            await server.WaitAssertion(() => Assert.That(physBody, Is.Not.EqualTo(null)));
-            await server.WaitPost(() =>
-            {
-                physicsSystem.SetLinearVelocity(airlockPhysicsDummy, new Vector2(0.5f, 0f), body: physBody);
-            });
-
-            for (var i = 0; i < 240; i += 10)
-            {
-                // Keep the airlock awake so they collide
-                await server.WaitPost(() =>
-                {
-                    physicsSystem.WakeBody(airlock);
-                });
-
-                await server.WaitRunTicks(10);
-                await server.WaitIdleAsync();
-            }
-
-            // Sanity check
-            // Sloth: Okay I'm sorry but I hate having to rewrite tests for every refactor
-            // If you see this yell at me in discord so I can continue to pretend this didn't happen.
-            // REMINDER THAT I STILL HAVE TO FIX THIS TEST EVERY OTHER PHYSICS PR
-            // _transform.GetMapCoordinates(UID HERE, xform: Assert.That(AirlockPhysicsDummy.Transform).X, Is.GreaterThan(AirlockPhysicsDummyStartingX));
-
-            // Blocked by the airlock
-            await server.WaitAssertion(() =>
-            {
-                Assert.That(Math.Abs(xformSystem.GetWorldPosition(airlockPhysicsDummy).X - 1), Is.GreaterThan(0.01f));
-            });
-        }
+    /// <summary>
+    /// Wait for the door to be in a specific state.
+    /// </summary>
+    private async Task WaitForDoorState(Entity<DoorComponent> door, DoorState state)
+    {
+        await PoolManager.WaitUntil(Server, () => door.Comp.State == state);
+        AssertDoorState(door, state);
     }
 }
