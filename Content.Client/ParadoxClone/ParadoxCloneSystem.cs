6d36208d3ba3550e87dd4fd97adf840af55b6890
KeTuFaisPiKiNut <85769816+ketufaispikinut@users.noreply.github.com>
2026-05-25 14:35:50 -0400

    Update Content.Client/ParadoxClone/ParadoxCloneSystem.cs
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Client/ParadoxClone/ParadoxCloneSystem.cs
+++ Content.Client/ParadoxClone/ParadoxCloneSystem.cs
@@ -3,6 +3,4 @@ using Content.Shared.ParadoxClone;
 namespace Content.Client.ParadoxClone;
 
 // literally just here for the alerts to work
-public sealed partial class ParadoxCloneSystem : SharedParadoxCloneSystem
-{
-}
+public sealed partial class ParadoxCloneSystem : SharedParadoxCloneSystem;
