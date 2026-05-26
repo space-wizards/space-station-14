103d113c02a0bcc1ea885d41d01b0e3461416ba1
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2026-05-26 14:29:07 -0700

    Revert "Destructible Automatic Overkill (#43113)"
    This reverts commit 76277b737df94b02cb3fd439d8cda0f63964b85d.

--- Content.Server/Destructible/DestructibleComponent.cs
+++ Content.Server/Destructible/DestructibleComponent.cs
@@ -1,31 +1,25 @@
 using Content.Server.Destructible.Thresholds;
 
-namespace Content.Server.Destructible;
-
-/// <summary>
-///     When attached to an <see cref="Robust.Shared.GameObjects.EntityUid"/>, allows it to take damage
-///     and triggers thresholds when reached.
-/// </summary>
-[RegisterComponent]
-public sealed partial class DestructibleComponent : Component
+namespace Content.Server.Destructible
 {
     /// <summary>
-    /// A list of damage thresholds for the entity;
-    /// includes their triggers and resultant behaviors.
+    ///     When attached to an <see cref="Robust.Shared.GameObjects.EntityUid"/>, allows it to take damage
+    ///     and triggers thresholds when reached.
     /// </summary>
-    [DataField]
-    public List<DamageThreshold> Thresholds = new();
+    [RegisterComponent]
+    public sealed partial class DestructibleComponent : Component
+    {
+        /// <summary>
+        /// A list of damage thresholds for the entity;
+        /// includes their triggers and resultant behaviors
+        /// </summary>
+        [DataField]
+        public List<DamageThreshold> Thresholds = new();
 
-    /// <summary>
-    /// Specifies whether the entity has passed a damage threshold that causes it to break.
-    /// </summary>
-    [DataField]
-    public bool IsBroken = false;
-
-    /// <summary>
-    /// Specifies if the entity should be silently destroyed when receiving damage significantly in excess of
-    /// its normal destructible threshold.
-    /// </summary>
-    [DataField(readOnly: true)]
-    public bool GenerateOverkillThreshold = true;
+        /// <summary>
+        /// Specifies whether the entity has passed a damage threshold that causes it to break
+        /// </summary>
+        [DataField]
+        public bool IsBroken = false;
+    }
 }
