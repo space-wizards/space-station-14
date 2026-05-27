03a62da5610480fbc92e744ae7de91d56b99319b
nomdéraisonnablementlong <you@example.com>
2026-05-27 09:14:07 -0400

    cleanup

--- Content.Server/Mindshield/MindShieldSystem.cs
+++ Content.Server/Mindshield/MindShieldSystem.cs
@@ -1,15 +1,4 @@
-using Content.Server.Administration.Logs;
-using Content.Server.Mind;
-using Content.Server.Popups;
-using Content.Server.Roles;
-using Content.Shared.Database;
-using Content.Shared.Implants;
 using Content.Shared.Mindshield;
-using Content.Shared.Mindshield.Components;
-using Content.Shared.Revolutionary;
-using Content.Shared.Revolutionary.Components;
-using Content.Shared.Roles.Components;
-using Robust.Shared.Containers;
 
 namespace Content.Server.Mindshield;
 
