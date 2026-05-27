e6350886f2a246801a2816e719f9db7f9adb8236
Ed <edwardxperia2000@gmail.com>
2026-05-27 17:38:03 +0300

    Update JobPrototype.cs

--- Content.Shared/Roles/JobPrototype.cs
+++ Content.Shared/Roles/JobPrototype.cs
@@ -3,12 +3,11 @@ using Content.Shared.Guidebook;
 using Content.Shared.Players.PlayTimeTracking;
 using Content.Shared.StatusIcon;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Shared.Roles;
 
 /// <summary>
-///     Describes information for a single job on the station.
+/// Describes information for a single job on the station.
 /// </summary>
 [Prototype]
 public sealed partial class JobPrototype : IPrototype
@@ -17,76 +16,76 @@ public sealed partial class JobPrototype : IPrototype
     [IdDataField]
     public string ID { get; private set; } = default!;
 
-    [DataField(required: true, customTypeSerializer: typeof(PrototypeIdSerializer<PlayTimeTrackerPrototype>))]
-    public string PlayTimeTracker { get; private set; } = string.Empty;
+    [DataField(required: true)]
+    public ProtoId<PlayTimeTrackerPrototype> PlayTimeTracker = string.Empty;
 
     /// <summary>
-    ///     Who is the supervisor for this job.
+    /// Who is the supervisor for this job.
     /// </summary>
     [DataField]
     public LocId Supervisors = "job-supervisors-nobody";
 
     /// <summary>
-    ///     The name of this job as displayed to players.
+    /// The name of this job as displayed to players.
     /// </summary>
     [DataField]
-    public string Name { get; private set; } = string.Empty;
+    public string Name = string.Empty;
 
     [ViewVariables(VVAccess.ReadOnly)]
     public string LocalizedName => Loc.GetString(Name);
 
     /// <summary>
-    ///     The name of this job as displayed to players.
+    /// The name of this job as displayed to players.
     /// </summary>
     [DataField]
-    public string? Description { get; private set; }
+    public string? Description;
 
     [ViewVariables(VVAccess.ReadOnly)]
     public string? LocalizedDescription => Description is null ? null : Loc.GetString(Description);
 
     /// <summary>
-    ///     Requirements for the job.
+    /// Requirements for the job.
     /// </summary>
     [DataField, Access(typeof(SharedRoleSystem), Other = AccessPermissions.None)]
     public HashSet<JobRequirement>? Requirements;
 
     /// <summary>
-    ///     When true - the station will have anouncement about arrival of this player.
+    /// When true - the station will have announcement about arrival of this player.
     /// </summary>
     [DataField]
-    public bool JoinNotifyCrew { get; private set; } = false;
+    public bool JoinNotifyCrew;
 
     /// <summary>
-    ///     When true - the player will recieve a message about importancy of their job.
+    /// When true - the player will recieve a message about importancy of their job.
     /// </summary>
     [DataField]
-    public bool RequireAdminNotify { get; private set; } = false;
+    public bool RequireAdminNotify;
 
     /// <summary>
-    ///     Should this job appear in preferences menu?
+    /// Should this job appear in preferences menu?
     /// </summary>
     [DataField]
-    public bool SetPreference { get; private set; } = true;
+    public bool SetPreference = true;
 
     /// <summary>
-    ///     Should the selected traits be applied for this job?
+    /// Should the selected traits be applied for this job?
     /// </summary>
     [DataField]
-    public bool ApplyTraits { get; private set; } = true;
+    public bool ApplyTraits = true;
 
     /// <summary>
-    ///     Whether this job should show in the ID Card Console.
-    ///     If set to null, it will default to SetPreference's value.
+    /// Whether this job should show in the ID Card Console.
+    /// If set to null, it will default to SetPreference's value.
     /// </summary>
     [DataField]
-    public bool? OverrideConsoleVisibility { get; private set; } = null;
+    public bool? OverrideConsoleVisibility;
 
     /// <summary>
-    ///     The "weight" or importance of this job. If this number is large, the job system will assign this job
-    ///     before assigning other jobs.
+    /// The "weight" or importance of this job. If this number is large, the job system will assign this job
+    /// before assigning other jobs.
     /// </summary>
     [DataField]
-    public int Weight { get; private set; }
+    public int Weight;
 
     /// <summary>
     /// How to sort this job relative to other jobs in the UI.
@@ -94,16 +93,16 @@ public sealed partial class JobPrototype : IPrototype
     /// If not set, <see cref="Weight"/> is used as a fallback.
     /// </summary>
     [DataField]
-    public int? DisplayWeight { get; private set; }
+    public int? DisplayWeight;
 
     public int RealDisplayWeight => DisplayWeight ?? Weight;
 
     /// <summary>
-    ///     A numerical score for how much easier this job is for antagonists.
-    ///     For traitors, reduces starting TC by this amount. Other gamemodes can use it for whatever they find fitting.
+    /// A numerical score for how much easier this job is for antagonists.
+    /// For traitors, reduces starting TC by this amount. Other gamemodes can use it for whatever they find fitting.
     /// </summary>
     [DataField]
-    public int AntagAdvantage = 0;
+    public int AntagAdvantage;
 
     [DataField]
     public ProtoId<StartingGearPrototype>? StartingGear { get; private set; }
@@ -113,33 +112,33 @@ public sealed partial class JobPrototype : IPrototype
     /// Starting gear will be ignored.
     /// If you want to just add special attributes to a humanoid, use AddComponentSpecial instead.
     /// </summary>
-    [DataField(customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-    public string? JobEntity = null;
+    [DataField]
+    public EntProtoId? JobEntity;
 
     /// <summary>
     /// Entity to use as a preview in the lobby/character editor.
     /// Same restrictions as <see cref="JobEntity"/> apply.
     /// </summary>
     [DataField]
-    public EntProtoId? JobPreviewEntity = null;
+    public EntProtoId? JobPreviewEntity;
 
     [DataField]
-    public ProtoId<JobIconPrototype> Icon { get; private set; } = "JobIconUnknown";
+    public ProtoId<JobIconPrototype> Icon = "JobIconUnknown";
 
     [DataField(serverOnly: true)]
     public JobSpecial[] Special { get; private set; } = Array.Empty<JobSpecial>();
 
     [DataField]
-    public IReadOnlyCollection<ProtoId<AccessLevelPrototype>> Access { get; private set; } = Array.Empty<ProtoId<AccessLevelPrototype>>();
+    public IReadOnlyCollection<ProtoId<AccessLevelPrototype>> Access = Array.Empty<ProtoId<AccessLevelPrototype>>();
 
     [DataField]
-    public IReadOnlyCollection<ProtoId<AccessGroupPrototype>> AccessGroups { get; private set; } = Array.Empty<ProtoId<AccessGroupPrototype>>();
+    public IReadOnlyCollection<ProtoId<AccessGroupPrototype>> AccessGroups = Array.Empty<ProtoId<AccessGroupPrototype>>();
 
     [DataField]
-    public IReadOnlyCollection<ProtoId<AccessLevelPrototype>> ExtendedAccess { get; private set; } = Array.Empty<ProtoId<AccessLevelPrototype>>();
+    public IReadOnlyCollection<ProtoId<AccessLevelPrototype>> ExtendedAccess = Array.Empty<ProtoId<AccessLevelPrototype>>();
 
     [DataField]
-    public IReadOnlyCollection<ProtoId<AccessGroupPrototype>> ExtendedAccessGroups { get; private set; } = Array.Empty<ProtoId<AccessGroupPrototype>>();
+    public IReadOnlyCollection<ProtoId<AccessGroupPrototype>> ExtendedAccessGroups = Array.Empty<ProtoId<AccessGroupPrototype>>();
 
     [DataField]
     public bool Whitelisted;
