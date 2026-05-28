ed9597d5dca2dd6a3b6ea7192f756edcef9fced0
iaada <iaada@users.noreply.github.com>
2026-05-28 15:47:49 -0500

    commit

--- Content.Server/Medical/BiomassReclaimer/BiomassReclaimerSystem.cs
+++ Content.Server/Medical/BiomassReclaimer/BiomassReclaimerSystem.cs
@@ -12,6 +12,7 @@ using Content.Shared.Climbing.Events;
 using Content.Shared.Construction.Components;
 using Content.Shared.Database;
 using Content.Shared.DoAfter;
+using Content.Shared.FixedPoint;
 using Content.Shared.Humanoid;
 using Content.Shared.Interaction;
 using Content.Shared.Interaction.Events;
@@ -211,7 +212,7 @@ namespace Content.Server.Medical.BiomassReclaimer
                 _solution.ResolveSolution(toProcess, stream.BloodSolutionName, ref stream.BloodSolution, out var solution))
             {
                 component.BloodReagents = solution.Clone();
-                component.BloodReagents.ScaleSolution(50 / component.BloodReagents.Volume);
+                component.BloodReagents.ScaleSolution(component.BloodReagents.Volume == FixedPoint2.Zero ? 0 : 50 / component.BloodReagents.Volume);
             }
             if (TryComp<ButcherableComponent>(toProcess, out var butcherableComponent))
             {
