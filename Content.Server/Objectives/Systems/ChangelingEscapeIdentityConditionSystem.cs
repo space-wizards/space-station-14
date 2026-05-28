d9f1a4ca6d9c29f2b81cf36f98ca45b552f6acf4
Pok245 <113675512+Pok27@users.noreply.github.com>
2026-05-28 23:09:16 +0300

    review 2

--- Content.Server/Objectives/Systems/ChangelingEscapeIdentityConditionSystem.cs
+++ Content.Server/Objectives/Systems/ChangelingEscapeIdentityConditionSystem.cs
@@ -3,7 +3,8 @@ using Content.Server.Objectives.Components;
 using Content.Server.Shuttles.Systems;
 using Content.Shared.Access.Systems;
 using Content.Shared.Changeling.Components;
-using Content.Shared.Cuffs.Components;
+using Content.Shared.Changeling.Systems;
+using Content.Shared.Cuffs;
 using Content.Shared.Mind;
 using Content.Shared.Objectives.Components;
 
@@ -12,6 +13,8 @@ namespace Content.Server.Objectives.Systems;
 public sealed partial class ChangelingEscapeIdentityConditionSystem : EntitySystem
 {
     [Dependency] private EmergencyShuttleSystem _emergencyShuttle = default!;
+    [Dependency] private SharedChangelingIdentitySystem _changelingIdentity = default!;
+    [Dependency] private SharedCuffableSystem _cuffable = default!;
     [Dependency] private SharedIdCardSystem _idCard = default!;
     [Dependency] private SharedMindSystem _mind = default!;
     [Dependency] private TargetObjectiveSystem _target = default!;
@@ -47,18 +50,15 @@ public sealed partial class ChangelingEscapeIdentityConditionSystem : EntitySyst
             return 0f;
 
         // Check 1: Must have transformed to the target person.
-        if (!TryComp<ChangelingIdentityComponent>(ownedEntity, out var identityComp))
-            return 0f;
-
-        var currentData = identityComp.ConsumedIdentities.FirstOrDefault(d => d.Identity == identityComp.CurrentIdentity);
-        if (currentData == null || currentData.OriginalName != ent.Comp.TargetName)
+        if (!_changelingIdentity.TryGetCurrentIdentityData(ownedEntity.Value, out var identityData)
+            || identityData.OriginalName != ent.Comp.TargetName)
             return 0f;
 
         // Check 2: Must escape alive.
         if (!_emergencyShuttle.IsTargetEscaping(ownedEntity.Value))
             return 0.5f;
 
-        if (TryComp<CuffableComponent>(ownedEntity, out var cuffed) && cuffed.CuffedHandCount > 0)
+        if (_cuffable.TryGetAllCuffs(ownedEntity.Value, out _))
             return 0.5f;
 
         // Check 3: Must wear an ID card with the target's name on it.
