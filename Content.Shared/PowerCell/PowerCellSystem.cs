9d11261319ba14a0f1a2921b7c2e719adfb55d45
Samuel-C <samuelcamilo8@gmail.com>
2026-05-27 21:45:21 -0300

    ops

--- Content.Shared/PowerCell/PowerCellSystem.cs
+++ Content.Shared/PowerCell/PowerCellSystem.cs
@@ -135,13 +135,12 @@ public sealed partial class PowerCellSystem : EntitySystem
 
     private void OnDrawRefreshChargeRate(Entity<PowerCellDrawComponent> ent, ref RefreshChargeRateEvent args)
     {
-if (!ent.Comp.Enabled)
-     return;
+        if (!ent.Comp.Enabled)
+            return;
 
-args.NewChargeRate -= ent.Comp.DrawRate;
+        args.NewChargeRate -= ent.Comp.DrawRate;
 
         // If PowerCellDraw is enabled. then it should set the charge cooldown on the self recharging battery
-
         if (!TryGetBatteryFromSlot(ent.Owner, out var battery))
             return;
 
