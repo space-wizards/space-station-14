fc949c9e09c5c733c1b6251c7d1215a0f374a7df
Samuel-C <samuelcamilo8@gmail.com>
2026-05-28 19:24:23 -0300

    attack whitelist comp + system

---
+++ Content.Shared/CombatMode/AttackWhitelistComponent.cs
@@ -0,0 +1,28 @@
+﻿using Content.Shared.Whitelist;
+
+namespace Content.Shared.CombatMode;
+
+/// <summary>
+/// A entity with this component can only attack entities that pass the white/blacklist from this component
+/// </summary>
+[RegisterComponent]
+public sealed partial class AttackWhitelistComponent : Component
+{
+    /// <summary>
+    /// Popup message that is displayed when the entity fails to attack a entity because of this component
+    /// </summary>
+    [DataField]
+    public LocId? FailedMessage;
+
+    /// <summary>
+    /// whitelist of entities that can be attack.
+    /// </summary>
+    [DataField]
+    public EntityWhitelist? Whitelist;
+
+    /// <summary>
+    /// whitelist of entities that can't be attack.
+    /// </summary>
+    [DataField]
+    public EntityWhitelist? Blacklist;
+}
---
+++ Content.Shared/CombatMode/AttackWhitelistSystem.cs
@@ -0,0 +1,50 @@
+﻿using Content.Shared.Interaction.Events;
+using Content.Shared.Popups;
+using Content.Shared.Weapons.Ranged.Events;
+using Content.Shared.Whitelist;
+
+namespace Content.Shared.CombatMode;
+
+public sealed partial class AttackWhitelistSystem : EntitySystem
+{
+    [Dependency] private EntityWhitelistSystem _whitelist = default!;
+    [Dependency] private SharedPopupSystem _popup = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<AttackWhitelistComponent, AttackAttemptEvent>(OnAttackAttempt);
+        SubscribeLocalEvent<AttackWhitelistComponent, ShotAttemptedEvent>(OnShotAttempt);
+    }
+
+    private void OnAttackAttempt(Entity<AttackWhitelistComponent> entity, ref AttackAttemptEvent args)
+    {
+        if (CheckCancelAttack(entity, args.Target))
+            args.Cancel();
+    }
+
+    private void OnShotAttempt(Entity<AttackWhitelistComponent> entity, ref ShotAttemptedEvent args)
+    {
+        if (CheckCancelAttack(entity, args.Target))
+            args.Cancel();
+    }
+
+    /// <summary>
+    /// Checks if a attacker with <see cref="AttackWhitelistComponent"/> can attack a target
+    /// </summary>
+    /// <param name="attacker">the attacker entity with <see cref="AttackWhitelistComponent"/></param>
+    /// <param name="target">the target</param>
+    /// <returns>true if it should cancel the attack, false it should continue the attack</returns>
+    private bool CheckCancelAttack(Entity<AttackWhitelistComponent> attacker, EntityUid? target)
+    {
+        if (target == null)
+            return false;
+
+        if (_whitelist.CheckBoth(target, attacker.Comp.Blacklist, attacker.Comp.Whitelist))
+            return false;
+
+        if (attacker.Comp.FailedMessage != null)
+            _popup.PopupClient(Loc.GetString(attacker.Comp.FailedMessage, ("target", target)), attacker);
+
+        return true;
+    }
+}
