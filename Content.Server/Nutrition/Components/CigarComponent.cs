784616d83188c390afdfeab1f1c61dc9a23e5462
Jonas Lorincz <jblorincz@gmail.com>
2026-05-28 23:01:03 -0400

    Refactor cigar/ette dipping into a helper, add DipAmount DataField

--- Content.Server/Nutrition/Components/CigarComponent.cs
+++ Content.Server/Nutrition/Components/CigarComponent.cs
@@ -1,4 +1,5 @@
 using Content.Server.Nutrition.EntitySystems;
+using Content.Shared.FixedPoint;
 
 namespace Content.Server.Nutrition.Components
 {
@@ -8,5 +9,10 @@ namespace Content.Server.Nutrition.Components
     [RegisterComponent, Access(typeof(SmokingSystem))]
     public sealed partial class CigarComponent : Component
     {
+        /// <summary>
+        ///     Amount of solution drawn from a container when the cigar is dipped, in units.
+        /// </summary>
+        [DataField]
+        public FixedPoint2 DipAmount = FixedPoint2.New(10);
     }
 }
---
+++ Content.Server/Nutrition/EntitySystems/SmokingSystem.CigarDip.cs
@@ -0,0 +1,48 @@
+using Content.Server.Nutrition.Components;
+using Content.Shared.Chemistry.Components;
+using Content.Shared.FixedPoint;
+using Content.Shared.Interaction;
+using Content.Shared.Nutrition.Components;
+
+namespace Content.Server.Nutrition.EntitySystems
+{
+    public sealed partial class SmokingSystem
+    {
+        private bool TryDipCigar(Entity<CigarComponent> entity, SmokableComponent smokable, ref AfterInteractEvent args)
+        {
+            if (args.Target is not { } target)
+                return false;
+
+            if (!TryComp(target, out DrainableSolutionComponent? drainable))
+                return false;
+
+            if (!_solutionContainerSystem.TryGetSolution(target, drainable.Solution, out var containerSoln, out var containerSolution))
+                return false;
+
+            if (containerSolution.Volume <= FixedPoint2.Zero)
+            {
+                _popupSystem.PopupEntity(Loc.GetString("cigar-component-dip-empty", ("cigar", entity.Owner)), target, args.User);
+                return true;
+            }
+
+            if (!_solutionContainerSystem.TryGetSolution(entity.Owner, smokable.Solution, out var cigSoln, out var cigSolution))
+                return false;
+
+            if (cigSolution.Volume >= cigSolution.MaxVolume)
+            {
+                _popupSystem.PopupEntity(Loc.GetString("cigar-component-dip-full", ("cigar", entity.Owner)), target, args.User);
+                return true;
+            }
+
+            var amountToDrain = FixedPoint2.Min(cigSolution.AvailableVolume, entity.Comp.DipAmount);
+
+            var drawn = _solutionContainerSystem.SplitSolution(containerSoln.Value, amountToDrain);
+
+            _solutionContainerSystem.TryAddSolution(cigSoln.Value, drawn);
+
+            _popupSystem.PopupEntity(Loc.GetString("cigar-component-dip-success", ("cigar", entity.Owner), ("target", target)), target, args.User);
+
+            return true;
+        }
+    }
+}
