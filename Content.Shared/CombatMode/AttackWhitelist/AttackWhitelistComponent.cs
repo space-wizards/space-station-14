e248877723c4f74397c2904f2d58c07d3c59e653
Samuel-C <samuelcamilo8@gmail.com>
2026-05-29 11:21:19 -0300

    make fields networked to avoid misspredictions

--- Content.Shared/CombatMode/AttackWhitelist/AttackWhitelistComponent.cs
+++ Content.Shared/CombatMode/AttackWhitelist/AttackWhitelistComponent.cs
@@ -6,24 +6,24 @@ namespace Content.Shared.CombatMode.AttackWhitelist;
 /// <summary>
 /// A entity with this component can only attack entities that pass the white/blacklist from this component
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class AttackWhitelistComponent : Component
 {
     /// <summary>
     /// Popup message that is displayed when the entity fails to attack a entity because of this component
     /// </summary>
-    [DataField]
+    [DataField, AutoNetworkedField]
     public LocId? FailedMessage;
 
     /// <summary>
     /// whitelist of entities that can be attack.
     /// </summary>
-    [DataField]
+    [DataField, AutoNetworkedField]
     public EntityWhitelist? Whitelist;
 
     /// <summary>
     /// whitelist of entities that can't be attack.
     /// </summary>
-    [DataField]
+    [DataField, AutoNetworkedField]
     public EntityWhitelist? Blacklist;
 }
