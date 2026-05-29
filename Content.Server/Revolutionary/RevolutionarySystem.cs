3566b6dac056c88954ea89b82c00fa6084c095f4
KeTuFaisPiKiNut <85769816+ketufaispikinut@users.noreply.github.com>
2026-05-29 17:56:41 -0400

    Update Content.Server/Revolutionary/RevolutionarySystem.cs
    Co-authored-by: ScarKy0 <106310278+ScarKy0@users.noreply.github.com>

--- Content.Server/Revolutionary/RevolutionarySystem.cs
+++ Content.Server/Revolutionary/RevolutionarySystem.cs
@@ -25,7 +25,6 @@ public sealed partial class RevolutionarySystem : SharedRevolutionarySystem
     {
         base.Initialize();
         SubscribeLocalEvent<MindShieldImplantComponent, ImplantImplantedEvent>(MindShieldImplanted);
-
     }
 
     /// <summary>
