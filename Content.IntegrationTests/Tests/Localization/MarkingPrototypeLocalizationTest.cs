e60d28437503c181009e880c84dcf04947c83ee0
portfiend <109661617+portfiend@users.noreply.github.com>
2026-05-30 15:08:16 -0500

    use nunit attributes

--- Content.IntegrationTests/Tests/Localization/MarkingPrototypeLocalizationTest.cs
+++ Content.IntegrationTests/Tests/Localization/MarkingPrototypeLocalizationTest.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Content.IntegrationTests.Utility;
 using Content.Shared.Humanoid;
 using Content.Shared.Humanoid.Markings;
 using Robust.Shared.Localization;
@@ -8,43 +9,47 @@ namespace Content.IntegrationTests.Tests.Localization;
 
 public sealed class MarkingPrototypeLocalizationTest
 {
+    private static string[] _markings = GameDataScrounger.PrototypesOfKind<MarkingPrototype>();
+
     [Test]
-    public async Task AllMarkingsLocalized()
+    [TestCaseSource(nameof(_markings))]
+    [TestOf(typeof(MarkingPrototype))]
+    [Description("Ensures that all markings would be properly localized in the marking editor UI.")]
+    public async Task AllMarkingsLocalized(string marking)
     {
         await using var pair = await PoolManager.GetServerClient();
 
         var server = pair.Server;
         var protoMan = server.ProtoMan;
         var locMan = server.ResolveDependency<ILocalizationManager>();
+        var proto = protoMan.Index<MarkingPrototype>(marking);
+        var statesExcluded = proto.ForcedColoring; // markings with forced coloring will not show their layer states anyway
 
-        Assert.Multiple(() =>
+        // markings with empty group whitelists won't show up in the marking picker anyway
+        if (proto.GroupWhitelist?.Count == 0)
+            return;
+
+        await server.WaitAssertion(() =>
         {
-            foreach (var proto in protoMan.EnumeratePrototypes<MarkingPrototype>())
-            {
-                // markings with empty group whitelists won't show up in the marking picker anyway
-                if (proto.GroupWhitelist?.Count == 0)
-                    continue;
+            var missingLocales = new List<string>();
 
-                var missingLocales = new List<string>();
+            // marking name locales
+            var nameId = proto.GetNameLocale();
+            if (!locMan.HasString(nameId))
+                missingLocales.Add(nameId);
 
-                var nameId = proto.GetNameLocale();
-                if (!locMan.HasString(nameId))
-                    missingLocales.Add(nameId);
+            // marking layer locales
+            if (!statesExcluded)
+                GetMissingStateLocales(proto, ref missingLocales, locMan);
 
-                // markings with forced coloring will not show their layer states anyway
-                var statesExcluded = proto.ForcedColoring;
-                if (!statesExcluded)
-                    GetMissingStateLocales(proto, ref missingLocales, locMan);
-
-                Assert.That(missingLocales, Is.Empty,
-                    $"Marking {proto.ID} is missing the following localization strings: {string.Join(", ", missingLocales)}");
-            }
+            Assert.That(missingLocales, Is.Empty,
+                $"Marking {proto.ID} is missing the following localization strings: {string.Join(", ", missingLocales)}");
         });
-
-        await pair.CleanReturnAsync();
     }
 
-    private void GetMissingStateLocales(MarkingPrototype marking, ref List<string> missingLocales, ILocalizationManager locMan)
+    private static void GetMissingStateLocales(MarkingPrototype marking,
+        ref List<string> missingLocales,
+        ILocalizationManager locMan)
     {
         foreach (var state in marking.Sprites)
         {
