a3a8938b22186a8427cee7a98323d3c5206ba79a
riccardi48 <richardballard00@gmail.com>
2026-06-03 18:42:19 +0100

    comments

--- Content.Shared/Cargo/CargoOrderData.cs
+++ Content.Shared/Cargo/CargoOrderData.cs
@@ -9,7 +9,8 @@ namespace Content.Shared.Cargo
     public sealed partial class CargoOrderData
     {
         /// <summary>
-        /// A unique (arbitrary) ID which identifies this order.
+        /// A unique ID which identifies this order.
+        /// Counts up from 1 with each order placed
         /// </summary>
         [DataField]
         public int OrderId { get; private set; }
@@ -18,21 +19,25 @@ namespace Content.Shared.Cargo
         /// The ID of the cargo product ordered.
         /// </summary>
         [DataField]
-        public ProtoId<CargoProductPrototype> Product;
+        public ProtoId<CargoProductPrototype> Product { get; private set; }
 
         /// <summary>
         /// The number of items in the order. Not readonly, as it might change
         /// due to caps on the amount of orders that can be placed.
         /// </summary>
         [DataField]
-        public int OrderQuantity;
+        public int OrderQuantity { get; set; }
 
         /// <summary>
         /// How many instances of this order that we've already dispatched
         /// </summary>
         [DataField]
-        public int NumDispatched = 0;
+        public int NumDispatched { get; set; } = 0;
 
+        /// <summary>
+        /// Requester field on the order menu
+        /// Defaults to the name of the orderer
+        /// </summary>
         [DataField]
         public string Requester { get; private set; }
 
@@ -40,28 +45,47 @@ namespace Content.Shared.Cargo
         // public int RequesterId;
         [DataField]
         public string Reason { get; private set; }
-        public bool Approved;
 
+        /// <summary>
+        /// Wether this order has been approved for delivery
+        /// </summary>
+        public bool Approved { get; set; }
+
+        /// <summary>
+        /// If this order has been assigned to be delivered differently from the
+        /// </summary>
         [ViewVariables]
-        public bool Assigned;
+        public bool Assigned { get; set; }
 
+        /// <summary>
+        /// The entity assigned to deliver, only not null if not the ATS
+        /// </summary>
         [ViewVariables]
-        public NetEntity? AssignedEntity;
+        public NetEntity? AssignedEntity { get; set; }
 
+        /// <summary>
+        /// The console which approved the order, used for telepad linking
+        /// </summary>
         [ViewVariables]
-        public NetEntity? ApprovingConsole;
+        public NetEntity? ApprovingConsole { get; set; }
 
+        /// <summary>
+        /// The ID of the person who approved the order
+        /// </summary>
         [DataField]
-        public string? Approver;
+        public string? Approver { get; set; }
 
         /// <summary>
         /// Which account to deduct funds from when ordering
         /// </summary>
         [DataField]
-        public ProtoId<CargoAccountPrototype> Account;
+        public ProtoId<CargoAccountPrototype> Account { get; private set; }
 
+        /// <summary>
+        /// If the order should be visible in any UI which displays orders
+        /// </summary>
         [DataField]
-        public bool Visible = true;
+        public bool Visible { get; set; } = true;
 
         public CargoOrderData(
             int orderId,
