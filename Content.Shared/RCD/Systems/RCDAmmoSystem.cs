c15cb549c236ecaa379a07c262afe747cab21eba
CawsForConcern <kopczynski.zesty@gmail.com>
2026-06-09 15:16:32 -0700

    - fix: RCD ammo no longer throws an error when all of its charges are depleted.

--- Content.Shared/RCD/Systems/RCDAmmoSystem.cs
+++ Content.Shared/RCD/Systems/RCDAmmoSystem.cs
@@ -4,6 +4,7 @@ using Content.Shared.Examine;
 using Content.Shared.Interaction;
 using Content.Shared.Popups;
 using Content.Shared.RCD.Components;
+using Robust.Shared.Network;
 using Robust.Shared.Timing;
 
 namespace Content.Shared.RCD.Systems;
@@ -13,6 +14,7 @@ public sealed partial class RCDAmmoSystem : EntitySystem
     [Dependency] private SharedChargesSystem _sharedCharges = default!;
     [Dependency] private SharedPopupSystem _popup = default!;
     [Dependency] private IGameTiming _timing = default!;
+    [Dependency] private INetManager _net = default!;
 
     public override void Initialize()
     {
@@ -57,7 +59,7 @@ public sealed partial class RCDAmmoSystem : EntitySystem
         Dirty(uid, comp);
 
         // prevent having useless ammo with 0 charges
-        if (comp.Charges <= 0)
+        if (comp.Charges <= 0 && _net.IsServer)
             QueueDel(uid);
     }
 }
