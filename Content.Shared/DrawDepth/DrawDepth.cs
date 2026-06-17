c17093bffc605925b0a25ab919e090607216eb82
Whatstone <whatston3@gmail.com>
2026-06-17 01:06:20 -0400

    return the namespace block for the diff's sake

--- Content.Shared/DrawDepth/DrawDepth.cs
+++ Content.Shared/DrawDepth/DrawDepth.cs
@@ -1,137 +1,138 @@
 using Robust.Shared.Serialization;
 using DrawDepthTag = Robust.Shared.GameObjects.DrawDepth;
 
-namespace Content.Shared.DrawDepth;
-
-[ConstantsFor(typeof(DrawDepthTag))]
-public enum DrawDepth
+namespace Content.Shared.DrawDepth
 {
-    /// <summary>
-    ///     This is for sub-floors, the floors you see after prying off a tile.
-    /// </summary>
-    LowFloors = DrawDepthTag.Default - 22,
 
-    // various entity types that require different
-    // draw depths, as to avoid hiding
-    // If updating this set, update the gaps above Puddles and Overdoors.
-    #region SubfloorEntities
-    ThickPipe = DrawDepthTag.Default - 21,
-    ThickWire = DrawDepthTag.Default - 20,
-    ThinPipeAlt2 = DrawDepthTag.Default - 19,
-    ThinPipeAlt1 = DrawDepthTag.Default - 18,
-    ThinPipe = DrawDepthTag.Default - 17,
-    ThinWire = DrawDepthTag.Default - 16,
-    #endregion
+    [ConstantsFor(typeof(DrawDepthTag))]
+    public enum DrawDepth
+    {
+        /// <summary>
+        ///     This is for sub-floors, the floors you see after prying off a tile.
+        /// </summary>
+        LowFloors = DrawDepthTag.Default - 22,
 
-    /// <summary>
-    ///     Things that are beneath regular floors.
-    /// </summary>
-    BelowFloor = DrawDepthTag.Default - 15,
+        // various entity types that require different
+        // draw depths, as to avoid hiding
+        // If updating this set, update the gaps above Puddles and Overdoors.
+        #region SubfloorEntities
+        ThickPipe = DrawDepthTag.Default - 21,
+        ThickWire = DrawDepthTag.Default - 20,
+        ThinPipeAlt2 = DrawDepthTag.Default - 19,
+        ThinPipeAlt1 = DrawDepthTag.Default - 18,
+        ThinPipe = DrawDepthTag.Default - 17,
+        ThinWire = DrawDepthTag.Default - 16,
+        #endregion
 
-    /// <summary>
-    ///     Used for entities like carpets.
-    /// </summary>
-    FloorTiles = DrawDepthTag.Default - 14,
+        /// <summary>
+        ///     Things that are beneath regular floors.
+        /// </summary>
+        BelowFloor = DrawDepthTag.Default - 15,
 
-    /// <summary>
-    ///     Things that are actually right on the floor, like ice crust or atmos devices. This does not mean objects like
-    ///     tables, even though they are technically "on the floor".
-    /// </summary>
-    FloorObjects = DrawDepthTag.Default - 13,
+        /// <summary>
+        ///     Used for entities like carpets.
+        /// </summary>
+        FloorTiles = DrawDepthTag.Default - 14,
 
-    /// <summary>
-    //     Discrete drawdepth to avoid z-fighting with other FloorObjects but also above floor entities.
-    /// </summary>
-    Puddles = DrawDepthTag.Default - 12,
+        /// <summary>
+        ///     Things that are actually right on the floor, like ice crust or atmos devices. This does not mean objects like
+        ///     tables, even though they are technically "on the floor".
+        /// </summary>
+        FloorObjects = DrawDepthTag.Default - 13,
 
-    // NOTE: There's a gap for subfloor entities to retain relative draw depth when revealed by a t-ray scanner (need 6 layers in between).
+        /// <summary>
+        //     Discrete drawdepth to avoid z-fighting with other FloorObjects but also above floor entities.
+        /// </summary>
+        Puddles = DrawDepthTag.Default - 12,
 
-    /// <summary>
-    //     Objects that are on the floor, but should render above puddles. This includes kudzu, holopads, telepads and levers.
-    /// </summary>
-    HighFloorObjects = DrawDepthTag.Default - 5,
+        // NOTE: There's a gap for subfloor entities to retain relative draw depth when revealed by a t-ray scanner (need 6 layers in between).
 
-    DeadMobs = DrawDepthTag.Default - 4,
+        /// <summary>
+        //     Objects that are on the floor, but should render above puddles. This includes kudzu, holopads, telepads and levers.
+        /// </summary>
+        HighFloorObjects = DrawDepthTag.Default - 5,
 
-    /// <summary>
-    ///     Allows small mobs like mice and drones to render under tables and chairs but above puddles and vents
-    /// </summary>
-    SmallMobs = DrawDepthTag.Default - 3,
+        DeadMobs = DrawDepthTag.Default - 4,
 
-    Walls = DrawDepthTag.Default - 2,
+        /// <summary>
+        ///     Allows small mobs like mice and drones to render under tables and chairs but above puddles and vents
+        /// </summary>
+        SmallMobs = DrawDepthTag.Default - 3,
 
-    /// <summary>
-    ///     Used for windows (grilles use walls) and misc signage. Useful if you want to have an APC in the middle
-    ///     of some wall-art or something.
-    /// </summary>
-    WallTops = DrawDepthTag.Default - 1,
+        Walls = DrawDepthTag.Default - 2,
 
-    /// <summary>
-    ///     Furniture, crates, tables. etc. If an entity should be drawn on top of a table, it needs a draw depth
-    ///     that is higher than this.
-    /// </summary>
-    Objects = DrawDepthTag.Default,
+        /// <summary>
+        ///     Used for windows (grilles use walls) and misc signage. Useful if you want to have an APC in the middle
+        ///     of some wall-art or something.
+        /// </summary>
+        WallTops = DrawDepthTag.Default - 1,
 
-    /// <summary>
-    ///     In-between an furniture and an item. Useful for entities that need to appear on top of tables, but are
-    ///     not items. E.g., power cell chargers. Also useful for pizza boxes, which appear above crates, but not
-    ///     above the pizza itself.
-    /// </summary>
-    SmallObjects = DrawDepthTag.Default + 1,
+        /// <summary>
+        ///     Furniture, crates, tables. etc. If an entity should be drawn on top of a table, it needs a draw depth
+        ///     that is higher than this.
+        /// </summary>
+        Objects = DrawDepthTag.Default,
 
-    /// <summary>
-    ///     Posters, APCs, air alarms, etc. This also includes most lights & lamps.
-    /// </summary>
-    WallMountedItems = DrawDepthTag.Default + 2,
+        /// <summary>
+        ///     In-between an furniture and an item. Useful for entities that need to appear on top of tables, but are
+        ///     not items. E.g., power cell chargers. Also useful for pizza boxes, which appear above crates, but not
+        ///     above the pizza itself.
+        /// </summary>
+        SmallObjects = DrawDepthTag.Default + 1,
 
-    /// <summary>
-    ///     To use for objects that would usually fall under SmallObjects, but appear taller than 1 tile. For example: Reagent Grinder
-    /// </summary>
-    LargeObjects = DrawDepthTag.Default + 3,
+        /// <summary>
+        ///     Posters, APCs, air alarms, etc. This also includes most lights & lamps.
+        /// </summary>
+        WallMountedItems = DrawDepthTag.Default + 2,
 
-    /// <summary>
-    ///     Generic items. Things that should be above crates & tables, but underneath mobs.
-    /// </summary>
-    Items = DrawDepthTag.Default + 4,
-    /// <summary>
-    /// Stuff that should be drawn below mobs, but on top of items. Like muzzle flash.
-    /// </summary>
-    BelowMobs = DrawDepthTag.Default + 5,
+        /// <summary>
+        ///     To use for objects that would usually fall under SmallObjects, but appear taller than 1 tile. For example: Reagent Grinder
+        /// </summary>
+        LargeObjects = DrawDepthTag.Default + 3,
 
-    Mobs = DrawDepthTag.Default + 6,
+        /// <summary>
+        ///     Generic items. Things that should be above crates & tables, but underneath mobs.
+        /// </summary>
+        Items = DrawDepthTag.Default + 4,
+        /// <summary>
+        /// Stuff that should be drawn below mobs, but on top of items. Like muzzle flash.
+        /// </summary>
+        BelowMobs = DrawDepthTag.Default + 5,
 
-    OverMobs = DrawDepthTag.Default + 7,
+        Mobs = DrawDepthTag.Default + 6,
 
-    Doors = DrawDepthTag.Default + 8,
+        OverMobs = DrawDepthTag.Default + 7,
 
-    /// <summary>
-    /// Blast doors and shutters which go over the usual doors.
-    /// </summary>
-    BlastDoors = DrawDepthTag.Default + 9,
+        Doors = DrawDepthTag.Default + 8,
 
-    /// <summary>
-    /// Stuff that needs to draw over most things, but not effects, like Kudzu.
-    /// </summary>
-    Overdoors = DrawDepthTag.Default + 10,
+        /// <summary>
+        /// Blast doors and shutters which go over the usual doors.
+        /// </summary>
+        BlastDoors = DrawDepthTag.Default + 9,
 
-    // NOTE: There's a gap here for subfloor layers in mapping mode (need 6 layers in between)
+        /// <summary>
+        /// Stuff that needs to draw over most things, but not effects, like Kudzu.
+        /// </summary>
+        Overdoors = DrawDepthTag.Default + 10,
 
-    /// <summary>
-    ///     Visible atmos gas.
-    /// </summary>
-    Gasses = DrawDepthTag.Default + 17,
+        // NOTE: There's a gap here for subfloor layers in mapping mode (need 6 layers in between)
 
-    /// <summary>
-    ///     Explosions, fire, melee swings. Whatever.
-    /// </summary>
-    Effects = DrawDepthTag.Default + 18,
+        /// <summary>
+        ///     Visible atmos gas.
+        /// </summary>
+        Gasses = DrawDepthTag.Default + 17,
 
-    Ghosts = DrawDepthTag.Default + 19,
+        /// <summary>
+        ///     Explosions, fire, melee swings. Whatever.
+        /// </summary>
+        Effects = DrawDepthTag.Default + 18,
 
-    /// <summary>
-    ///    Use this selectively if it absolutely needs to be drawn above (almost) everything else. Examples include
-    ///    the pointing arrow, the drag & drop ghost-entity, and some debug tools.
-    /// </summary>
-    Overlays = DrawDepthTag.Default + 20,
+        Ghosts = DrawDepthTag.Default + 19,
+
+        /// <summary>
+        ///    Use this selectively if it absolutely needs to be drawn above (almost) everything else. Examples include
+        ///    the pointing arrow, the drag & drop ghost-entity, and some debug tools.
+        /// </summary>
+        Overlays = DrawDepthTag.Default + 20,
+    }
 }
-
