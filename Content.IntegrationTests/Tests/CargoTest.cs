b2ca31ced07afe8610aad8caf066f3fa4a5b0a0d
riccardi48 <richardballard00@gmail.com>
2026-06-18 19:06:13 +0100

    oops

--- Content.IntegrationTests/Tests/CargoTest.cs
+++ Content.IntegrationTests/Tests/CargoTest.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using Content.IntegrationTests.Fixtures;
 using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Server.Cargo.Components;
