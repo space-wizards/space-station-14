15c6931bd13c4fe05548000a5af1bdedc14fc1fb
Axionyx <nyx@axionyx.sh>
2026-06-20 18:42:24 +0200

    Fix local connection check
    I feel like I'm overlooking an obvious way to more easily determine this but whatever

--- Content.Server/GameTicking/GameTicker.Player.cs
+++ Content.Server/GameTicking/GameTicker.Player.cs
@@ -52,7 +52,7 @@ namespace Content.Server.GameTicking
                     {
                         var userData = session.Channel.UserData;
                         var trust = userData.Trust;
-                        if (IPAddress.IsLoopback(session.Channel.RemoteEndPoint.Address))
+                        if (session.Name.StartsWith("localhost@")) // Lazy :3
                         {
                             trust = 1.0f; // Localhost should be inherently trusted.
                         }
