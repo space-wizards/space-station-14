c2cf2802ae380e724ddbe0b1344ab1e75fbfe215
Roudenn <romabond091@gmail.com>
2026-06-26 15:29:51 +0300

    Add buffers

---
+++ Content.Client/Buffers/ClientRobustArrayPool.cs
@@ -0,0 +1,23 @@
+﻿using Content.Shared.Buffers;
+
+namespace Content.Client.Buffers;
+
+public sealed class ClientRobustArrayPool<T> : SharedRobustArrayPool<T>
+{
+    private readonly RobustArrayPool<T> _arrayPool;
+
+    public ClientRobustArrayPool(int startArraySize, int startBucketSize, Func<T>? factory = null, bool init = false)
+    {
+        _arrayPool = new(startArraySize, startBucketSize, factory, init);
+    }
+
+    public override T[] Rent(int minSize)
+    {
+        return _arrayPool.Rent(minSize);
+    }
+
+    public override void Return(T[] obj)
+    {
+        _arrayPool.Return(obj);
+    }
+}
---
+++ Content.Server/Buffers/ServerRobustArrayPool.cs
@@ -0,0 +1,17 @@
+﻿using System.Buffers;
+using Content.Shared.Buffers;
+
+namespace Content.Server.Buffers;
+
+public sealed class ServerRobustArrayPool<T> : SharedRobustArrayPool<T>
+{
+    public override T[] Rent(int minSize)
+    {
+        return ArrayPool<T>.Shared.Rent(minSize);
+    }
+
+    public override void Return(T[] obj)
+    {
+        ArrayPool<T>.Shared.Return(obj);
+    }
+}
