903781a51418e1d2b6696d3708521a12efcdf1b1
tkirkby <trevor.p.kirkby@gmail.com>
2026-06-26 03:38:55 -0700

    boombot dancing animation activates when playing music regardless of whether it is controlled by a player

---
+++ Content.Shared/Silicons/Bots/BoombotVisualsComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Shared.Silicons.Bots;
+
+/// <summary>
+/// Makes boombots update their visuals depending on whether they're playing music or not.
+/// </summary>
+[RegisterComponent]
+public sealed partial class BoombotVisualsComponent : Component
+{
+}
