830548d3bf301fb442395a3c6805c9f44f1a6cb7
tkirkby <trevor.p.kirkby@gmail.com>
2026-06-26 13:20:30 -0700

    made the instrument toggle visuals a generic component

---
+++ Content.Shared/Instruments/InstrumentToggleVisualsComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Shared.Instruments;
+
+/// <summary>
+/// Makes instruments toggle their appearance depending on whether they're playing music or not.
+/// </summary>
+/// <see cref="ToggleableVisuals"/>
+[RegisterComponent]
+public sealed partial class InstrumentToggleVisualsComponent : Component;
--- Content.Shared/Silicons/Bots/BoombotVisualsComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Shared.Silicons.Bots;
-
-/// <summary>
-/// Makes boombots update their visuals depending on whether they're playing music or not.
-/// </summary>
-[RegisterComponent]
-public sealed partial class BoombotVisualsComponent : Component
-{
-}
