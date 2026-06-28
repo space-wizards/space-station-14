494655a74ff112b5dd7c4b611b30c693479419cd
jessicamaybe <jessica@jessicamaybe.com>
2026-06-28 11:28:51 -0700

    networked

--- Content.Shared/Audio/AmbientMusic/AmbientMusicMarkerMedicalComponent.cs
+++ Content.Shared/Audio/AmbientMusic/AmbientMusicMarkerMedicalComponent.cs
@@ -1,9 +1,11 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.Audio.AmbientMusic;
 
 /// <summary>
 /// Marks an entity as contributing to Medical ambient music
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class AmbientMusicMarkerMedicalComponent : Component
 {
 
