bd5ae7384d2aafef6420149bd7d2598b7c1fa257
SlamBamActionman <slambamactionman@gmail.com>
2026-06-30 01:08:22 +0200

    Initial pass on making graffiti into an entity effect

---
+++ Content.Shared/EntityEffects/Effects/VariationPass/EntitySpawnVariationPassEntityEffect.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using Content.Shared.Storage;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Random;
+
+namespace Content.Shared.EntityEffects.Effects.VariationPass;
+
+/// <summary>
+/// Used for spawning entities randomly dotted around the grid.
+/// </summary>
+/// <inheritdoc cref="EntityEffectSystem{T,TEffect}"/>
+public sealed partial class EntitySpawnVariationPassEntityEffectSystem : EntityEffectSystem<MapGridComponent, EntitySpawnVariationPass>
+{
+    [Dependency] private SharedMapSystem _map = default!;
+    [Dependency] private IRobustRandom _random = default!;
+
+    protected override void Effect(Entity<MapGridComponent> entity, ref EntityEffectEvent<EntitySpawnVariationPass> args)
+    {
+        var totalTiles = _map.GetAllTiles(entity, entity).Count();
+
+        var dirtyMod = _random.NextGaussian(args.Effect.TilesPerEntityAverage, args.Effect.TilesPerEntityStdDev);
+        var trashTiles = Math.Max((int) (totalTiles * (1 / dirtyMod)), 0);
+
+        for (var i = 0; i < trashTiles; i++)
+        {
+            FindRandomTileOnStation(entity, out var coords);
+
+            var ents = EntitySpawnCollection.GetSpawns(args.Effect.Entities, _random);
+            foreach (var spawn in ents)
+            {
+                SpawnAtPosition(spawn, coords);
+            }
+        }
+    }
+
+    private void FindRandomTileOnStation(Entity<MapGridComponent> grid,
+        out EntityCoordinates targetCoords)
+    {
+        targetCoords = EntityCoordinates.Invalid;
+        var aabb = grid.Comp.LocalAABB;
+
+        // TODO: Check for atmospherics, which isn't available in Shared. Until that is done, this method isn't the same as the old one
+
+        var randomX = _random.Next((int) aabb.Left, (int) aabb.Right);
+        var randomY = _random.Next((int) aabb.Bottom, (int) aabb.Top);
+
+        var tile = new Vector2i(randomX, randomY);
+
+        targetCoords = _map.GridTileToLocal(grid, grid.Comp, tile);
+    }
+}
+
+/// <inheritdoc cref="EntityEffect"/>
+public sealed partial class EntitySpawnVariationPass : EntityEffectBase<EntitySpawnVariationPass>
+{
+    /// <summary>
+    ///     Number of tiles before we spawn one entity on average.
+    /// </summary>
+    [DataField]
+    public float TilesPerEntityAverage = 120f;
+
+    [DataField]
+    public float TilesPerEntityStdDev = 5f;
+
+    /// <summary>
+    ///     Spawn entries for each chosen location.
+    /// </summary>
+    [DataField(required: true)]
+    public List<EntitySpawnEntry> Entities = default!;
+}
---
+++ Content.Shared/EntityEffects/EntityEffectOnInitComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace Content.Shared.EntityEffects;
+
+/// <summary>
+/// Applies a set of EntityEffects to the entity upon map initialization.
+/// <remarks>Useful for e.g. station spawning.</remarks>
+/// </summary>
+[RegisterComponent]
+public sealed partial class EntityEffectOnInitComponent : Component
+{
+    /// <summary>
+    /// Effects that should be applied upon the map being initiated.
+    /// </summary>
+    [DataField(required: true)]
+    public EntityEffect[] Effects { get; set; } = default!;
+}
