662c0ab1f2c059956ebe08101d601ddac5e4bf38
Rami Alanbagy <ramialanbagy@gmail.com>
2026-06-30 12:20:21 -0700

    sorry

--- Content.Shared/Buckle/Components/PacifismDisallowedBuckleComponent.cs
+++ Content.Shared/Buckle/Components/PacifismDisallowedBuckleComponent.cs
@@ -1,6 +1,5 @@
 using Robust.Shared.GameStates;
 namespace Content.Shared.Buckle.Components;
-public sealed partial class BuckleComponent : Component;
 /// <summary>
 /// Makes so a entity with <see cref="StrapComponent"/> don't allow pacifists to buckle other people to it
 /// </summary>
