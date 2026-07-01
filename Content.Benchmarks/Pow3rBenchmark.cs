c465abc4f4950a363d96e46f7ab4b7060be081a9
Roudenn <romabond091@gmail.com>
2026-07-01 20:49:20 +0300

    Create Pow3rBenchmark.cs

---
+++ Content.Benchmarks/Pow3rBenchmark.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Content.Server.Power.Pow3r;
+using Robust.Shared.Analyzers;
+using Robust.Shared.Threading;
+using Robust.UnitTesting;
+
+namespace Content.Benchmarks;
+
+[Virtual]
+[SimpleJob]
+public class Pow3rBenchmark
+{
+    private BatteryRampPegSolver _solver = new();
+    private PowerState _state = new();
+    private IParallelManager _parallel = new TestingParallelManager();
+    private PowerState.Network _chargeNetwork = new();
+    private PowerState.Network _dischargeNetwork = new();
+    private List<PowerState.NodeId> _loads = new();
+
+    public int SupplyCount { get; set; } = 100;
+    public int LoadCount { get; set; } = 2000;
+    public int BatteryCount { get; set; } = 100;
+
+    public int TicksAmount { get; set; } = 60000;
+
+    [Params(true, false)] public bool ChargeDischarge { get; set; }
+
+    public float TickRate = 1f / 30f;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _chargeNetwork = new PowerState.Network();
+        _dischargeNetwork = new PowerState.Network();
+        _state.Networks.Allocate(out var chargeNetId) = _chargeNetwork;
+        _state.Networks.Allocate(out var dischargeNetId) = _dischargeNetwork;
+
+        for (int i = 0; i < SupplyCount; i++)
+        {
+            var supply = new PowerState.Supply();
+            _state.Supplies.Allocate(out var supplyId) = supply;
+            supply.Id = supplyId;
+            _dischargeNetwork.Supplies.Add(supplyId);
+            supply.AvailableSupply = 5000;
+            supply.LinkedNetwork = dischargeNetId;
+        }
+
+        for (int i = 0; i < LoadCount; i++)
+        {
+            var load = new PowerState.Load();
+            _state.Loads.Allocate(out var loadId) = load;
+            load.Id = loadId;
+            _chargeNetwork.Loads.Add(load.Id);
+            load.DesiredPower = ChargeDischarge ? 0 : 500;
+            load.LinkedNetwork = chargeNetId;
+            _loads.Add(loadId);
+        }
+
+        for (int i = 0; i < BatteryCount; i++)
+        {
+            var battery = new PowerState.Battery();
+            _state.Batteries.Allocate(out var batteryId) = battery;
+            battery.Id = batteryId;
+            battery.Capacity = 500000;
+            _chargeNetwork.BatteryLoads.Add(batteryId);
+            _dischargeNetwork.BatteryLoads.Add(batteryId);
+            battery.LinkedNetworkCharging = chargeNetId;
+            battery.LinkedNetworkDischarging = dischargeNetId;
+        }
+    }
+
+    [Benchmark(Description = "Run Pow3r Idle")]
+    public void RunPowerIdle()
+    {
+        var ticks = ChargeDischarge ? TicksAmount / 2 : TicksAmount;
+        for (int i = 0; i < ticks; i++)
+        {
+            _solver.Tick(TickRate, _state, _parallel);
+        }
+
+        if (!ChargeDischarge)
+            return;
+
+        for (int i = 0; i < LoadCount; i++)
+        {
+            _state.Loads[_loads[i]].DesiredPower = 1000;
+        }
+
+        for (int i = 0; i < ticks; i++)
+        {
+            _solver.Tick(TickRate, _state, _parallel);
+        }
+    }
+}
