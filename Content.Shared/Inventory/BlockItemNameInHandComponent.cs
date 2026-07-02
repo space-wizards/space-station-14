d78f94da7c415d849475104ed13b80ca4911b930
Samuel-C <samuelcamilo8@gmail.com>
2026-07-02 14:28:54 -0300

    component to hide the name

---
+++ Content.Shared/Inventory/BlockItemNameInHandComponent.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Inventory;
+
+/// <summary>
+/// Makes so a item doesn't show the item name in the hand UI
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class BlockItemNameInHandComponent : Component;
+
