21ba560b2c03fd9b0f9c56b374c9f0f624d838f0
Samuel-C <samuelcamilo8@gmail.com>
2026-07-02 17:12:24 -0300

    comment good

--- Content.Shared/Inventory/BlockItemNameInHandComponent.cs
+++ Content.Shared/Inventory/BlockItemNameInHandComponent.cs
@@ -4,6 +4,7 @@ namespace Content.Shared.Inventory;
 
 /// <summary>
 /// Makes so a item doesn't show the item name in the hand UI
+/// Also centers the vertical alignment of the item status panel
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class BlockItemNameInHandComponent : Component;
