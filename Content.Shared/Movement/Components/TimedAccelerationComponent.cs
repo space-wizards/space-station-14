b2230629b24f9f14e7d559219f87038a280ee15c
notquitehadouken <tripwiregamer@gmail.com>
2026-07-01 20:08:09 -0500

    fleelf

---
+++ Content.Shared/Movement/Components/TimedAccelerationComponent.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Shared.Movement.Components;
+
+/// <summary>
+/// Makes an entity accelerate over time.
+/// </summary>
+[RegisterComponent, AutoGenerateComponentPause]
+public sealed partial class TimedAccelerationComponent : Component
+{
+    /// <summary>
+    /// When acceleration should start (inclusive)
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField]
+    public TimeSpan StartAcceleration;
+
+    /// <summary>
+    /// When acceleration should end (inclusive)
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField]
+    public TimeSpan EndAcceleration;
+
+    /// <summary>
+    /// How long acceleration will last.
+    /// </summary>
+    public TimeSpan AccelerationTime
+    {
+        get => EndAcceleration - StartAcceleration;
+        set => EndAcceleration = StartAcceleration + value;
+    }
+
+    /// <summary>
+    /// After the acceleration, does this component get removed?
+    /// </summary>
+    [DataField]
+    public bool RemoveOnEnd;
+
+    /// <summary>
+    /// Not affected by entity rotation.
+    /// </summary>
+    [DataField]
+    public Vector2 WorldAcceleration;
+
+    /// <summary>
+    /// Affected by entity rotation.
+    /// </summary>
+    [DataField]
+    public Vector2 LocalAcceleration;
+}
---
+++ Content.Shared/Movement/Systems/TimedMovementSystem.cs
@@ -0,0 +1,8 @@
+﻿using Robust.Shared.Physics.Controllers;
+
+namespace Content.Shared.Movement.Systems;
+
+public sealed class TimedMovementSystem : VirtualController
+{
+
+}
