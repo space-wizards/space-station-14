5ce27ea84ab5246cb4b495b402bcd28dfa7d98da
AJCM-git <ajcm_mail@disroot.org>
2026-07-04 18:33:57 -0400

    reviews

--- Content.Client/Flash/FlashOverlay.cs
+++ Content.Client/Flash/FlashOverlay.cs
@@ -9,101 +9,97 @@ using Robust.Shared.Enums;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 
-namespace Content.Client.Flash
+namespace Content.Client.Flash;
+
+public sealed partial class FlashOverlay : Overlay
 {
-    public sealed partial class FlashOverlay : Overlay
+    private static readonly ProtoId<ShaderPrototype> FlashedEffect = "FlashedEffect";
+    private static readonly ProtoId<ShaderPrototype> FlashedEffectReducedMotion = "FlashedEffectReducedMotion";
+
+    [Dependency] private IPrototypeManager _prototypeManager = default!;
+    [Dependency] private IEntityManager _entityManager = default!;
+    [Dependency] private IPlayerManager _playerManager = default!;
+    [Dependency] private IGameTiming _timing = default!;
+    [Dependency] private IConfigurationManager _configManager = default!;
+
+    private readonly SharedFlashSystem _flash;
+    private readonly StatusEffectsSystem _statusSys;
+
+    public override OverlaySpace Space => OverlaySpace.WorldSpace;
+    private ShaderInstance _shader;
+    public float PercentComplete;
+    public Texture? ScreenshotTexture;
+
+    public FlashOverlay()
     {
-        private static readonly ProtoId<ShaderPrototype> FlashedEffectShader = "FlashedEffect";
+        IoCManager.InjectDependencies(this);
+        _flash = _entityManager.System<SharedFlashSystem>();
+        _statusSys = _entityManager.System<StatusEffectsSystem>();
 
-        [Dependency] private IPrototypeManager _prototypeManager = default!;
-        [Dependency] private IEntityManager _entityManager = default!;
-        [Dependency] private IPlayerManager _playerManager = default!;
-        [Dependency] private IGameTiming _timing = default!;
-        [Dependency] private IConfigurationManager _configManager = default!;
+        // Default to the normal flashed effect so that _shader can be initialized in the constructor
+        // and does not need to be nullable
+        _shader = _prototypeManager.Index(FlashedEffect).InstanceUnique();
+        // Set the shader according to the CVar when it is changed and also now.
+        _configManager.OnValueChanged(CCVars.ReducedMotion, OnReducedMotionChanged, invokeImmediately: true);
+    }
 
-        private readonly SharedFlashSystem _flash;
-        private readonly StatusEffectsSystem _statusSys;
+    protected override void FrameUpdate(FrameEventArgs args)
+    {
+        var playerEntity = _playerManager.LocalEntity;
 
-        public override OverlaySpace Space => OverlaySpace.WorldSpace;
-        private readonly ShaderInstance _shader;
-        private bool _reducedMotion;
-        public float PercentComplete;
-        public Texture? ScreenshotTexture;
+        if (playerEntity == null)
+            return;
 
-        public FlashOverlay()
+        if (!_entityManager.HasComponent<FlashedComponent>(playerEntity)
+            || !_entityManager.TryGetComponent<StatusEffectsComponent>(playerEntity, out var status))
+            return;
+
+        if (!_statusSys.TryGetTime(playerEntity.Value, _flash.FlashedKey, out var time, status))
+            return;
+
+        var curTime = _timing.CurTime;
+        var lastsFor = (float)(time.Value.Item2 - time.Value.Item1).TotalSeconds;
+        var timeDone = (float)(curTime - time.Value.Item1).TotalSeconds;
+
+        PercentComplete = timeDone / lastsFor;
+    }
+
+    protected override bool BeforeDraw(in OverlayDrawArgs args)
+    {
+        if (!_entityManager.TryGetComponent(_playerManager.LocalEntity, out EyeComponent? eyeComp))
+            return false;
+        if (args.Viewport.Eye != eyeComp.Eye)
+            return false;
+
+        return PercentComplete < 1.0f;
+    }
+
+    protected override void Draw(in OverlayDrawArgs args)
+    {
+        if (RequestScreenTexture && ScreenTexture != null)
         {
-            IoCManager.InjectDependencies(this);
-            _shader = _prototypeManager.Index(FlashedEffectShader).InstanceUnique();
-            _flash = _entityManager.System<SharedFlashSystem>();
-            _statusSys = _entityManager.System<StatusEffectsSystem>();
-
-            _configManager.OnValueChanged(CCVars.ReducedMotion, (b) => { _reducedMotion = b; }, invokeImmediately: true);
+            ScreenshotTexture = ScreenTexture;
+            RequestScreenTexture = false; // we only need the first frame, so we can stop the request now for performance reasons
         }
+        if (ScreenshotTexture == null)
+            return;
 
-        protected override void FrameUpdate(FrameEventArgs args)
-        {
-            var playerEntity = _playerManager.LocalEntity;
+        var worldHandle = args.WorldHandle;
+        _shader.SetParameter("percentComplete", PercentComplete);
+        worldHandle.UseShader(_shader);
+        worldHandle.DrawTextureRectRegion(ScreenshotTexture, args.WorldBounds);
+        worldHandle.UseShader(null);
+    }
 
-            if (playerEntity == null)
-                return;
+    protected override void DisposeBehavior()
+    {
+        base.DisposeBehavior();
+        ScreenshotTexture = null;
+    }
 
-            if (!_entityManager.HasComponent<FlashedComponent>(playerEntity)
-                || !_entityManager.TryGetComponent<StatusEffectsComponent>(playerEntity, out var status))
-                return;
-
-            if (!_statusSys.TryGetTime(playerEntity.Value, _flash.FlashedKey, out var time, status))
-                return;
-
-            var curTime = _timing.CurTime;
-            var lastsFor = (float)(time.Value.Item2 - time.Value.Item1).TotalSeconds;
-            var timeDone = (float)(curTime - time.Value.Item1).TotalSeconds;
-
-            PercentComplete = timeDone / lastsFor;
-        }
-
-        protected override bool BeforeDraw(in OverlayDrawArgs args)
-        {
-            if (!_entityManager.TryGetComponent(_playerManager.LocalEntity, out EyeComponent? eyeComp))
-                return false;
-            if (args.Viewport.Eye != eyeComp.Eye)
-                return false;
-
-            return PercentComplete < 1.0f;
-        }
-
-        protected override void Draw(in OverlayDrawArgs args)
-        {
-            if (RequestScreenTexture && ScreenTexture != null)
-            {
-                ScreenshotTexture = ScreenTexture;
-                RequestScreenTexture = false; // we only need the first frame, so we can stop the request now for performance reasons
-            }
-            if (ScreenshotTexture == null)
-                return;
-
-            var worldHandle = args.WorldHandle;
-            if (_reducedMotion)
-            {
-                // TODO: This is a very simple placeholder.
-                // Replace it with a proper shader once we come up with something good.
-                // Turns out making an effect that is supposed to be a bright, sudden, and disorienting flash
-                // not do any of that while also being equivalent in terms of game balance is hard.
-                var alpha = 1 - MathF.Pow(PercentComplete, 8f); // similar falloff curve to the flash shader
-                worldHandle.DrawRect(args.WorldBounds, new Color(0f, 0f, 0f, alpha));
-            }
-            else
-            {
-                _shader.SetParameter("percentComplete", PercentComplete);
-                worldHandle.UseShader(_shader);
-                worldHandle.DrawTextureRectRegion(ScreenshotTexture, args.WorldBounds);
-                worldHandle.UseShader(null);
-            }
-        }
-
-        protected override void DisposeBehavior()
-        {
-            base.DisposeBehavior();
-            ScreenshotTexture = null;
-        }
+    private void OnReducedMotionChanged(bool reducedMotion)
+    {
+        var effectName = reducedMotion ? FlashedEffectReducedMotion : FlashedEffect;
+        _shader = _prototypeManager.Index<ShaderPrototype>(effectName).InstanceUnique();
     }
 }
