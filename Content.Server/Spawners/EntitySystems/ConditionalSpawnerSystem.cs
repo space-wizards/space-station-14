01b5a83ceb2ed6ef3d6da5fc128c5ca7c5e067a1
Fildrance <fildrance@gmail.com>
2026-07-06 15:00:03 +0300

    refactor: update after merge

--- Content.Server/Spawners/EntitySystems/ConditionalSpawnerSystem.cs
+++ Content.Server/Spawners/EntitySystems/ConditionalSpawnerSystem.cs
@@ -4,45 +4,42 @@ using Content.Server.Stack;
 using Content.Shared.EntityTable;
 using Content.Shared.GameTicking.Components;
 using Content.Shared.Stacks;
-using JetBrains.Annotations;
+using Robust.Server.GameObjects;
 using Robust.Shared.Collections;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
+using System.Numerics;
 
 namespace Content.Server.Spawners.EntitySystems;
 
-[UsedImplicitly]
-public sealed class ConditionalSpawnerSystem : EntitySystem
+// TODO: This whole system is a mess. A lot of this should be marked obsolete.
+// TODO: It should probably use interfaces with entity tables *if* more than one component is needed.
+// TODO: Remove the TransformSystem Dependency when engine SpawnAtPosition EntityCoordinates override is fixed.
+public sealed partial class ConditionalSpawnerSystem : EntitySystem
 {
-    [Dependency] private readonly IRobustRandom _robustRandom = default!;
-    [Dependency] private readonly GameTicker _ticker = default!;
-    [Dependency] private readonly EntityTableSystem _entityTable = default!;
-    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-    [Dependency] private readonly StackSystem _stack = default!;
+    [Dependency] private IRobustRandom _robustRandom = default!;
+    [Dependency] private GameTicker _ticker = default!;
+    [Dependency] private EntityTableSystem _entityTable = default!;
+    [Dependency] private IPrototypeManager _prototypeManager = default!;
+    [Dependency] private StackSystem _stack = default!;
+    [Dependency] private TransformSystem _xform = default!;
 
-    public override void Initialize()
+    [SubscribeLocalEvent]
+    private void OnCondSpawnMapInit(Entity<ConditionalSpawnerComponent> ent, ref MapInitEvent args)
     {
-        base.Initialize();
-
-        SubscribeLocalEvent<GameRuleStartedEvent>(OnRuleStarted);
-        SubscribeLocalEvent<ConditionalSpawnerComponent, MapInitEvent>(OnCondSpawnMapInit);
-        SubscribeLocalEvent<RandomSpawnerComponent, MapInitEvent>(OnRandSpawnMapInit);
-        SubscribeLocalEvent<EntityTableSpawnerComponent, MapInitEvent>(OnEntityTableSpawnMapInit);
+        TrySpawn(ent, ent);
     }
 
-    private void OnCondSpawnMapInit(EntityUid uid, ConditionalSpawnerComponent component, MapInitEvent args)
+    [SubscribeLocalEvent]
+    private void OnRandSpawnMapInit(Entity<RandomSpawnerComponent> ent, ref MapInitEvent args)
     {
-        TrySpawn(uid, component);
+        Spawn(ent, ent);
+        if (ent.Comp.DeleteSpawnerAfterSpawn)
+            QueueDel(ent);
     }
 
-    private void OnRandSpawnMapInit(EntityUid uid, RandomSpawnerComponent component, MapInitEvent args)
-    {
-        Spawn(uid, component);
-        if (component.DeleteSpawnerAfterSpawn)
-            QueueDel(uid);
-    }
-
+    [SubscribeLocalEvent]
     private void OnEntityTableSpawnMapInit(Entity<EntityTableSpawnerComponent> ent, ref MapInitEvent args)
     {
         Spawn(ent);
@@ -50,6 +47,7 @@ public sealed class ConditionalSpawnerSystem : EntitySystem
             QueueDel(ent);
     }
 
+    [SubscribeLocalEvent]
     private void OnRuleStarted(ref GameRuleStartedEvent args)
     {
         var query = EntityQueryEnumerator<ConditionalSpawnerComponent>();
@@ -93,84 +91,138 @@ public sealed class ConditionalSpawnerSystem : EntitySystem
             return;
         }
 
-            if (Deleted(uid))
-                return;
+        if (Deleted(uid))
+            return;
 
-            var xform = Transform(uid);
-            var coords = _xform.GetMapCoordinates(uid, xform);
-            var rotation = _xform.GetWorldRotation(xform);
+        var xform = Transform(uid);
+        var coords = _xform.GetMapCoordinates(uid, xform);
+        var rotation = _xform.GetWorldRotation(xform);
 
-            Spawn(_robustRandom.Pick(component.Prototypes), coords, rotation: rotation);
-        }
+        var toSpawn = _robustRandom.Pick(component.Prototypes);
+        Spawn(toSpawn, coords, rotation: rotation);
+    }
 
-        private void Spawn(EntityUid uid, RandomSpawnerComponent component)
-        {
-            if (Deleted(uid))
-                return;
+    private void Spawn(EntityUid uid, RandomSpawnerComponent component)
+    {
+        if (Deleted(uid))
+            return;
 
-            if (GetPrototype((uid, component)) is not { } proto)
-                return;
+        if (GetPrototype((uid, component)) is not { } proto)
+            return;
 
-            var offset = component.Offset;
-            var vOffset = _robustRandom.NextVector2Box(-offset, offset);
+        var xform = Transform(uid);
+        var coords = _xform.GetMapCoordinates(uid, xform);
+        var coordinates = GetRandomOffset(coords, component.Offset);
+        var rotation = _xform.GetWorldRotation(xform);
 
-            var xform = Transform(uid);
-            var coords = _xform.GetMapCoordinates(uid, xform).Offset(vOffset);
-            var rotation = _xform.GetWorldRotation(xform);
-
-            Spawn(proto, coords, rotation: rotation);
-        }
-
-        private EntProtoId? GetPrototype(Entity<RandomSpawnerComponent> spawner)
-        {
-            if (GetPrototypes(spawner) is not { } list)
-                return null;
-
-            return _robustRandom.Pick(list);
-        }
-
-        private List<EntProtoId>? GetPrototypes(Entity<RandomSpawnerComponent> spawner)
-        {
-            // ReSharper disable once CompareOfFloatsByEqualityOperator
-            if (spawner.Comp.RarePrototypes.Count > 0 &&
-                (spawner.Comp.RareChance == 1.0f || _robustRandom.Prob(spawner.Comp.RareChance)))
-            {
-                return spawner.Comp.RarePrototypes;
-            }
-
-            if (spawner.Comp.Prototypes.Count == 0)
-            {
-                Log.Warning($"Prototype list in RandomSpawnerComponent is empty! Entity: {ToPrettyString(spawner)}");
-                return null;
-            }
-
-            // ReSharper disable once CompareOfFloatsByEqualityOperator
-            if (spawner.Comp.Chance == 1.0f || !_robustRandom.Prob(spawner.Comp.Chance))
-            {
-                return spawner.Comp.Prototypes;
-            }
-
-            return null;
-        }
+        Spawn(_robustRandom.Pick(component.Prototypes), coordinates, rotation: rotation);
+    }
 
     private void Spawn(Entity<EntityTableSpawnerComponent> ent)
     {
         if (TerminatingOrDeleted(ent) || !Exists(ent))
             return;
 
-            var xform = Transform(ent);
-            var coords = _xform.GetMapCoordinates(ent, xform);
-            var rotation = _xform.GetWorldRotation(xform);
-            var offset = ent.Comp.Offset;
+        var xform = Transform(ent);
+        var coords = _xform.GetMapCoordinates(ent, xform);
+        var rotation = _xform.GetWorldRotation(xform);
 
-            var spawns = _entityTable.GetSpawns(ent.Comp.Table);
-            foreach (var proto in spawns)
-            {
-                var vOffset = _robustRandom.NextVector2(-offset, offset);
-                var trueCoords = coords.Offset(vOffset);
-
-                Spawn(proto, trueCoords, rotation: rotation);
-            }
+        EntityTableSpawnerComponent comp = ent;
+        var spawns = _entityTable.GetSpawns(comp.Table);
+        if (comp.AutoStack)
+        {
+            SpawnStackedWhenPossible(spawns, ent, coords, comp.Offset, rotation);
         }
+        else
+        {
+            SpawnAtRandomOffset(spawns, coords, comp.Offset, rotation);
+        }
+    }
+
+    private void SpawnStackedWhenPossible(IEnumerable<EntProtoId> spawns,
+        Entity<EntityTableSpawnerComponent> ent,
+        MapCoordinates coords,
+        float offset,
+        Angle rotation)
+    {
+        Dictionary<ProtoId<StackPrototype>, (EntProtoId Proto, int Count)> prototypeStacks = new();
+        ValueList<EntProtoId> nonStackable = [];
+        foreach (var protoId in spawns)
+        {
+            var prototype = _prototypeManager.Index(protoId);
+            if (!prototype.Components.TryGetComponent<StackComponent>(Factory, out var stack))
+            {
+                nonStackable.Add(protoId);
+                continue;
+            }
+
+            prototypeStacks[stack.StackTypeId] = prototypeStacks.TryGetValue(stack.StackTypeId, out var found)
+                ? (protoId, found.Count + 1)
+                : (protoId, 1);
+        }
+
+        SpawnAtRandomOffset(nonStackable, coords, offset, rotation);
+
+        foreach (var (protoId, count) in prototypeStacks.Values)
+        {
+            var trueCoords = GetRandomOffset(coords, offset);
+            var entCoordinates = _xform.ToCoordinates((ent, null), trueCoords);
+            _stack.SpawnMultipleAtPosition(protoId, count, entCoordinates);
+        }
+    }
+
+    private void SpawnAtRandomOffset(IEnumerable<EntProtoId> spawns, MapCoordinates coords, float offset, Angle rotation)
+    {
+        foreach (var proto in spawns)
+        {
+            SpawnAtRandomOffset(proto, coords, offset, rotation);
+        }
+    }
+
+    private EntityUid SpawnAtRandomOffset(EntProtoId proto, MapCoordinates coords, float offset, Angle rotation)
+    {
+        var trueCoords = GetRandomOffset(coords, offset);
+
+        return Spawn(proto, trueCoords, rotation: rotation);
+    }
+
+    private EntProtoId? GetPrototype(Entity<RandomSpawnerComponent> spawner)
+    {
+        if (GetPrototypes(spawner) is not { } list)
+            return null;
+
+        return _robustRandom.Pick(list);
+    }
+
+    private List<EntProtoId>? GetPrototypes(Entity<RandomSpawnerComponent> spawner)
+    {
+        // ReSharper disable once CompareOfFloatsByEqualityOperator
+        if (spawner.Comp.RarePrototypes.Count > 0 &&
+            (spawner.Comp.RareChance == 1.0f || _robustRandom.Prob(spawner.Comp.RareChance)))
+        {
+            return spawner.Comp.RarePrototypes;
+
+        }
+
+        if (spawner.Comp.Prototypes.Count == 0)
+        {
+            Log.Warning($"Prototype list in RandomSpawnerComponent is empty! Entity: {ToPrettyString(spawner)}");
+            return null;
+        }
+
+
+        // ReSharper disable once CompareOfFloatsByEqualityOperator
+        if (spawner.Comp.Chance == 1.0f || !_robustRandom.Prob(spawner.Comp.Chance))
+        {
+            return spawner.Comp.Prototypes;
+        }
+
+        return null;
+    }
+
+    private MapCoordinates GetRandomOffset(MapCoordinates coords, float offset)
+    {
+        var vOffset = _robustRandom.NextVector2(-offset, offset);
+        return coords.Offset(vOffset);
     }
 }
