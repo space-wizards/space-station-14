4b709137047a1f4f5597018c0e85f9c8192fda64
Ataman <Ataman@users.noreply.github.com>
2026-07-07 20:26:02 +0200

    Added condition for checking equipped items.

---
+++ Content.Client/Animus/Conditions/AnimusConditionHasEquipped.cs
@@ -0,0 +1,37 @@
+﻿using Content.Shared.Inventory;
+using Robust.Client.Animus.Conditions;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client.Animus.Conditions;
+
+public sealed partial class AnimusConditionHasEquipped : AnimusConditionBase
+{
+    [DataField]
+    public string Slot;
+
+    [DataField]
+    public ProtoId<IPrototype> Prototype;
+
+    private InventorySystem _inventorySystem = null!;
+    private EntityManager _entities = null!;
+    public override void Initialize(EntityManager entityManager)
+    {
+        base.Initialize(entityManager);
+        _entities = entityManager;
+        _inventorySystem = entityManager.System<InventorySystem>();
+    }
+
+    protected override bool Evaluate(EntityUid entity)
+    {
+        if (!_inventorySystem.TryGetSlotEntity(entity, Slot, out var slotEntity))
+            return false;
+
+        if (!_entities.TryGetComponent<MetaDataComponent>(slotEntity, out var metaData))
+            return false;
+
+        if (metaData.EntityPrototype == null)
+            return false;
+
+        return metaData.EntityPrototype.ID == Prototype;
+    }
+}
