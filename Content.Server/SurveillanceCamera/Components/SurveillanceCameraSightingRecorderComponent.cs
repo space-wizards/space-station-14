5b3942f9fdb170abab05baa8ad26ba1d79df818c
andreademurtas <56048157+andreademurtas@users.noreply.github.com>
2026-07-07 23:11:19 +0200

    Add sighting recorder scaffolding to surveillance cameras

---
+++ Content.Server/SurveillanceCamera/Components/SurveillanceCameraSightingRecorderComponent.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Server.SurveillanceCamera;
+
+[RegisterComponent, AutoGenerateComponentPause]
+public sealed partial class SurveillanceCameraSightingRecorderComponent : Component
+{
+    [DataField]
+    public int DetectionRange { get; set; } = 10;
+
+    [DataField]
+    public TimeSpan UpdateInterval { get; set; } = TimeSpan.FromSeconds(5f);
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    [AutoPausedField]
+    public TimeSpan NextUpdate = TimeSpan.Zero;
+}
---
+++ Content.Server/SurveillanceCamera/Systems/SurveillanceCameraSightingRecorderSystem.cs
@@ -0,0 +1,45 @@
+﻿using Content.Server.Power.Components;
+using Content.Shared.SurveillanceCamera.Components;
+using Robust.Shared.Random;
+using Robust.Shared.Timing;
+
+namespace Content.Server.SurveillanceCamera;
+
+public sealed class SurveillanceCameraSightingRecorderSystem : EntitySystem
+{
+    [Dependency] private IGameTiming _gameTiming = default!;
+    [Dependency] private IRobustRandom _random = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<SurveillanceCameraSightingRecorderComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(Entity<SurveillanceCameraSightingRecorderComponent> ent, ref MapInitEvent args)
+    {
+        ent.Comp.NextUpdate = _gameTiming.CurTime + ent.Comp.UpdateInterval * _random.NextFloat();
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var curTime = _gameTiming.CurTime;
+        var cameras = EntityQueryEnumerator<SurveillanceCameraSightingRecorderComponent, SurveillanceCameraComponent>();
+
+        while (cameras.MoveNext(out var uid, out var recorder, out var camera))
+        {
+            if (curTime < recorder.NextUpdate)
+                continue;
+
+            recorder.NextUpdate = curTime + recorder.UpdateInterval;
+
+            if (!camera.Active || CompOrNull<ApcPowerReceiverComponent>(uid)?.Powered != true)
+                continue;
+
+            Log.Info($"{camera.CameraId} @ {Transform(uid).Coordinates}");
+        }
+    }
+}
