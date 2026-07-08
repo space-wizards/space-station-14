320ef85721ef94a960d98bb33a30395ec0001ffb
metalgearsloth <comedian_vs_clown@hotmail.com>
2026-07-09 00:42:26 +1000

    Fix ExpireIdCard
    - The fields logically conflict with each other. If it expires immediately it should either be expired.
    - UI doesn't account for paused.
    - Breaking change because now it's assumed to be permanent (it's that or we set it to expired and this seemed like the lesser evil).

--- Content.IntegrationTests/Tests/Access/ExpireIdCardTest.cs
+++ Content.IntegrationTests/Tests/Access/ExpireIdCardTest.cs
@@ -7,6 +7,7 @@ using Content.Shared.Access.Systems;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Localization;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
 using System.Collections.Generic;
 
 namespace Content.IntegrationTests.Tests.Access
@@ -34,6 +35,8 @@ namespace Content.IntegrationTests.Tests.Access
 ";
 
         [SidedDependency(Side.Server)] private readonly SharedIdCardSystem _sharedIdCardSystem = null!;
+        [SidedDependency(Side.Server)] private readonly IGameTiming _gameTiming = null!;
+        [SidedDependency(Side.Server)] private readonly MetaDataSystem _metaDataSystem = null!;
 
         [Test]
         public async Task TestExpireIdCardResetsAccessTagsWhenExpiring()
@@ -55,20 +58,21 @@ namespace Content.IntegrationTests.Tests.Access
             using (Assert.EnterMultipleScope())
             {
                 Assert.That(expireComp.Expired, Is.False);
-                Assert.That(expireComp.Permanent, Is.False);
-                Assert.That(expireComp.ExpireTime, Is.EqualTo(TimeSpan.Zero));
+                Assert.That(expireComp.Permanent, Is.True);
+                Assert.That(expireComp.ExpireTime, Is.Null);
                 Assert.That(accessComp.Tags, Is.EqualTo(new HashSet<ProtoId<AccessLevelPrototype>> { GenpopEnter }));
                 Assert.That(expireComp.ExpiredAccess, Is.EqualTo(new HashSet<ProtoId<AccessLevelPrototype>> { GenpopLeave }));
                 Assert.That(expireComp.ExpireMessage, Is.EqualTo(new LocId("genpop-prisoner-id-expire")));
             }
 
             // Set the expire time to the future
-            _sharedIdCardSystem.SetExpireTime(ent, expireTime);
+            var absoluteExpireTime = _gameTiming.CurTime + expireTime;
+            _sharedIdCardSystem.SetExpireTime(ent, absoluteExpireTime);
             using (Assert.EnterMultipleScope())
             {
                 Assert.That(expireComp.Expired, Is.False);
                 Assert.That(expireComp.Permanent, Is.False);
-                Assert.That(expireComp.ExpireTime, Is.EqualTo(expireTime));
+                Assert.That(expireComp.ExpireTime, Is.EqualTo(absoluteExpireTime));
                 Assert.That(accessComp.Tags, Is.EqualTo(new HashSet<ProtoId<AccessLevelPrototype>> { GenpopEnter }));
             }
 
@@ -88,6 +92,42 @@ namespace Content.IntegrationTests.Tests.Access
                 Assert.That(expireComp.Permanent, Is.False);
                 Assert.That(accessComp.Tags, Is.EqualTo(new HashSet<ProtoId<AccessLevelPrototype>> { GenpopLeave }));
             }
+        }
+
+        [Test]
+        public async Task TestExpireIdCardExpiryUsesPauseDuration()
+        {
+            EntityUid ent = default;
+            ExpireIdCardComponent expireComp = default!;
+            AccessComponent accessComp = default!;
+
+            await Pair.Server.WaitPost(() =>
+            {
+                ent = SSpawn(TestExpireIdCard);
+                expireComp = SComp<ExpireIdCardComponent>(ent);
+                accessComp = SComp<AccessComponent>(ent);
+
+                _sharedIdCardSystem.SetExpireTime(ent, _gameTiming.CurTime + TimeSpan.FromSeconds(1));
+                _metaDataSystem.SetEntityPaused(ent, true);
+            });
+
+            await Pair.RunSeconds(2.0f);
+
+            await Pair.Server.WaitPost(() =>
+            {
+                Assert.That(expireComp.Expired, Is.False);
+                Assert.That(accessComp.Tags, Is.EqualTo(new HashSet<ProtoId<AccessLevelPrototype>> { GenpopEnter }));
+
+                _metaDataSystem.SetEntityPaused(ent, false);
+            });
+
+            await Pair.RunSeconds(1.1f);
+
+            await Pair.Server.WaitPost(() =>
+            {
+                Assert.That(expireComp.Expired, Is.True);
+                Assert.That(accessComp.Tags, Is.EqualTo(new HashSet<ProtoId<AccessLevelPrototype>> { GenpopLeave }));
+            });
         }
     }
 }
