4d90c515069dde6de34f45768f7f801b10bc74fd
Connor Huffine <chuffine@gmail.com>
2026-07-07 21:05:58 -0400

    Multiply by 1600

--- Content.Shared/CCVar/CCVars.Shuttle.cs
+++ Content.Shared/CCVar/CCVars.Shuttle.cs
@@ -112,7 +112,7 @@ public sealed partial class CCVars
     ///     Any value equal to or less than zero will disable this check.
     /// </summary>
     public static readonly CVarDef<float> FTLMassLimit =
-        CVarDef.Create("shuttle.mass_limit", 300f, CVar.SERVERONLY);
+        CVarDef.Create("shuttle.mass_limit", 48000f, CVar.SERVERONLY);
 
     /// <summary>
     ///     How long to knock down entities for if they aren't buckled when FTL starts and stops.
