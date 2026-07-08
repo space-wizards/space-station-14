b734db3f41d2c445cb35874b0bf3b5028c0dfc42
Connor Huffine <chuffine@gmail.com>
2026-07-07 21:13:50 -0400

    Add admin levels to the shuttle mass variable

--- Content.Shared/CCVar/CCVars.Shuttle.cs
+++ Content.Shared/CCVar/CCVars.Shuttle.cs
@@ -111,6 +111,7 @@ public sealed partial class CCVars
     ///     The maximum <see cref="PhysicsComponent.Mass"/> a grid can have before it becomes unable to FTL.
     ///     Any value equal to or less than zero will disable this check.
     /// </summary>
+    [CVarControl(AdminFlags.VarEdit)]
     public static readonly CVarDef<float> FTLMassLimit =
         CVarDef.Create("shuttle.mass_limit", 48000f, CVar.SERVERONLY);
 
