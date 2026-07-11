fcb544bebfbcf4ccbfc3bc7b52c979670ead5dc7
Thomas (Aeshus) <git@aeshus.com>
2026-07-11 18:00:24 -0500

    Update ItemStatusSheetlet

--- Content.Client/Stylesheets/Sheetlets/Hud/ItemStatusSheetlet.cs
+++ Content.Client/Stylesheets/Sheetlets/Hud/ItemStatusSheetlet.cs
@@ -1,24 +1,27 @@
 ﻿using Content.Client.Stylesheets.Fonts;
+using Content.Client.Stylesheets.SheetletConfigs;
+using Content.Client.Stylesheets.Stylesheets;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using static Content.Client.Stylesheets.StylesheetHelpers;
 
 namespace Content.Client.Stylesheets.Sheetlets.Hud;
 
-[Sheetlet]
-public sealed class ItemStatusSheetlet : ISheetlet<PalettedStylesheet>
+[Sheetlet(typeof(CommonStylesheetFactory))]
+public sealed class ItemStatusSheetlet<T> : ISheetlet<T>
+    where T : IFontConfig
 {
-    public StyleRule[] GetRules(PalettedStylesheet sheet, object config)
+    public StyleRule[] GetRules(StylesheetFactory factory, T config)
     {
         return
         [
             E()
                 .Class(StyleClass.ItemStatus)
-                .Prop("font", sheet.BaseFont.GetFont(10)),
+                .Prop("font", config.BaseFont.GetFont(10)),
 
             E()
                 .Class(StyleClass.ItemStatusNotHeld)
-                .Prop("font", sheet.BaseFont.GetFont(10, FontKind.Italic))
+                .Prop("font", config.BaseFont.GetFont(10, FontKind.Italic))
                 .Prop("font-color", Color.Gray),
 
             E<RichTextLabel>()
