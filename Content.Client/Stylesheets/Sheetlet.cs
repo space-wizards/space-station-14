788c4f6992c04e0e9f12c94a79dbd032409a737a
Thomas (Aeshus) <git@aeshus.com>
2026-07-11 13:40:09 -0500

    Refactor ISheetlet, change parameters
    I forgot to add the ISheetletConfig constraint earlier, so I added it
    here. The marker interface isn't really needed, but it makes mistakes
    harder and that's valuable in my book.
    
    The main real change is to the GetRules function.
    
    I have split the old "sheet" generic into two fields, passing the
    generic ISheetletConfig stuff into the (then unused) config argument and
    using the base StyleResolver class (all *StylesheetFactory classes inherit
    from it).
    
    The old approach of adding the marker stylesheet class to the same
    generic argument and passing it in as "sheet" broke encapsulation, as
    they include ALL sheetlet configs in that type, not just the ones
    specified.
    
    This just better helps differentiate concerns. If it's related to
    resolving of style config properties, then you use the resolver, which
    has stylesheet builder-specific implementation details (such as what
    paths to resolve from), while we can keep config type for only constants
    from the configs.

--- Content.Client/Stylesheets/Sheetlet.cs
+++ Content.Client/Stylesheets/Sheetlet.cs
@@ -1,9 +1,20 @@
-﻿using Robust.Client.ResourceManagement;
-using Robust.Client.UserInterface;
+﻿using Robust.Client.UserInterface;
 
 namespace Content.Client.Stylesheets;
 
+/// <summary>
+/// A sheetlet is a class that takes in SheetletConfigs as type constraints and returns style rules that are collected
+/// to form a stylesheet.
+/// </summary>
+/// <typeparam name="T">Sheetlet configs</typeparam>
 public interface ISheetlet<in T>
+    where T : ISheetletConfig
 {
-    StyleRule[] GetRules(T sheet, object config);
+    /// <summary>
+    /// Generates the style rules for the sheetlet.
+    /// </summary>
+    /// <param name="resolver">Resolves stylesheet-dependent style properties (e.g. paths, textures, resources)</param>
+    /// <param name="config">Sheetlet configs</param>
+    /// <returns>Style rules</returns>
+    StyleRule[] GetRules(StyleResolver resolver, T config);
 }
