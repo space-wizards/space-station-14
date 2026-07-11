4188d15b763e36b5a68365ee1e687c5268b88b6c
Thomas (Aeshus) <git@aeshus.com>
2026-07-11 17:17:44 -0500

    Bring back Texture StylesheetFactory helpers

--- Content.Client/Stylesheets/StylesheetFactory.Resources.cs
+++ Content.Client/Stylesheets/StylesheetFactory.Resources.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Content.Client.Stylesheets.Stylesheets;
+using Robust.Client.Graphics;
 using Robust.Client.ResourceManagement;
 using Robust.Shared.Utility;
 
@@ -51,6 +52,35 @@ public abstract partial class StylesheetFactory
         return TryGetResource(target, out T? res)
             ? res
             : throw new MissingStyleResourceException(this, target.ToString());
+    }
+
+        /// <summary>
+    ///     Attempts to locate a texture within the stylesheet's roots.
+    /// </summary>
+    /// <param name="target">The relative path of the target resource.</param>
+    /// <param name="texture">The retrieved texture, if any.</param>
+    /// <returns>Whether <paramref name="texture"/> is null.</returns>
+    public bool TryGetTexture(ResPath target, [NotNullWhen(true)] out Texture? texture)
+    {
+        if (TryGetResource(target, out TextureResource? resource))
+        {
+            texture = resource.Texture;
+            return true;
+        }
+
+        texture = null;
+        return false;
+    }
+
+    /// <summary>
+    ///     Retrieves a texture, or throws.
+    /// </summary>
+    /// <param name="target">The relative path of the target texture.</param>
+    /// <returns>The retrieved texture</returns>
+    /// <exception cref="MissingStyleResourceException">Thrown if the texture does not exist within the stylesheet's roots.</exception>
+    public Texture GetTexture(ResPath target)
+    {
+        return GetResource<TextureResource>(target).Texture;
     }
 }
 
