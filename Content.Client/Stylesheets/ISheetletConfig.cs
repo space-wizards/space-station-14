770f8fbd644e68cc5937bb289e224b7ce45d9cb6
Thomas (Aeshus) <git@aeshus.com>
2026-07-11 11:58:38 -0500

    Document ISheetletConfig

--- Content.Client/Stylesheets/ISheetletConfig.cs
+++ Content.Client/Stylesheets/ISheetletConfig.cs
@@ -1,3 +1,6 @@
 ﻿namespace Content.Client.Stylesheets;
 
+/// <summary>
+/// A config that defines the required properties for a sheetlet to be executed.
+/// </summary>
 public interface ISheetletConfig;
