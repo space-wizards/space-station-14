cc6f7de2ddd7b3b0c37751c27a55eecc9618034c
Thomas (Aeshus) <git@aeshus.com>
2026-07-11 17:51:38 -0500

    Update DividersSheetlet

--- Content.Client/Stylesheets/Sheetlets/DividersSheetlet.cs
+++ Content.Client/Stylesheets/Sheetlets/DividersSheetlet.cs
@@ -1,4 +1,6 @@
 ﻿using System.Numerics;
+using Content.Client.Stylesheets.SheetletConfigs;
+using Content.Client.Stylesheets.Stylesheets;
 using Robust.Client.Graphics;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
@@ -6,19 +8,20 @@ using static Content.Client.Stylesheets.StylesheetHelpers;
 
 namespace Content.Client.Stylesheets.Sheetlets;
 
-[Sheetlet]
-public sealed class DividersSheetlet : ISheetlet<PalettedStylesheet>
+[Sheetlet(typeof(CommonStylesheetFactory))]
+public sealed class DividersSheetlet<T> : ISheetlet<T>
+    where T : IPaletteConfig
 {
-    public StyleRule[] GetRules(PalettedStylesheet sheet, object config)
+    public StyleRule[] GetRules(StylesheetFactory factory, T config)
     {
         var boxHighDivider = new StyleBoxFlat
         {
-            BackgroundColor = sheet.HighlightPalette.Base,
+            BackgroundColor = config.HighlightPalette.Base,
             ContentMarginBottomOverride = 2,
             ContentMarginLeftOverride = 2,
         };
 
-        var boxLowDivider = new StyleBoxFlat(sheet.SecondaryPalette.TextDark);
+        var boxLowDivider = new StyleBoxFlat(config.SecondaryPalette.TextDark);
 
         // high divider and low divider styles are VERY inconsistent but its too much of a pain to change right now (also HighDivider has its own Control ???)
         // i dont think theres a good resolution to this besides just deleting HighDivider. HighDivider is barely used but LowDivider is used everywhere.
