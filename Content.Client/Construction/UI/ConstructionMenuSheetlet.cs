243ee30f0e71fbf061a33c469fe871c95164ffb9
Thomas (Aeshus) <git@aeshus.com>
2026-07-11 18:11:25 -0500

    Update ConstructionMenuSheetlet

--- Content.Client/Construction/UI/ConstructionMenuSheetlet.cs
+++ Content.Client/Construction/UI/ConstructionMenuSheetlet.cs
@@ -1,25 +1,29 @@
 using Content.Client.Stylesheets;
+using Content.Client.Stylesheets.SheetletConfigs;
+using Content.Client.Stylesheets.Stylesheets;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using static Content.Client.Stylesheets.StylesheetHelpers;
 
 namespace Content.Client.Construction.UI;
-[Sheetlet]
-public sealed class ConstructionMenuSheetlet : ISheetlet<PalettedStylesheet>
+
+[Sheetlet(typeof(CommonStylesheetFactory))]
+public sealed class ConstructionMenuSheetlet<T> : ISheetlet<T>
+    where T : IFontConfig
 {
-    public StyleRule[] GetRules(PalettedStylesheet sheet, object config)
+    public StyleRule[] GetRules(StylesheetFactory resolver, T config)
     {
         return
         [
             E<Label>()
                 .Identifier("RecipeHistoryNavButtonLabel")
-                .Font(sheet.BaseFont.GetFont(8))
+                .Font(config.BaseFont.GetFont(8))
                 .FontColor(Color.White),
 
             E<Label>()
                 .Identifier("RecipeHistoryNavButtonLabel")
                 .PseudoDisabled()
-                .Font(sheet.BaseFont.GetFont(8))
+                .Font(config.BaseFont.GetFont(8))
                 .FontColor(Color.Gray),
         ];
     }
