acd584aa090e68ccd7560314c9aeed7e03815442
B_Kirill <153602297+B-Kirill@users.noreply.github.com>
2026-07-11 11:34:55 +1000

    Fix client mispredict on some storage operations

--- Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
+++ Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
@@ -568,7 +568,7 @@ public abstract partial class SharedStorageSystem : EntitySystem
             _entityLookupSystem.GetEntitiesInRange(args.ClickLocation, storageComp.AreaInsertRadius, _entSet, LookupFlags.Dynamic | LookupFlags.Sundries);
             var delay = 0f;
 
-            foreach (var entity in _entSet)
+            foreach (var entity in _entSet.OrderBy(e => GetNetEntity(e).Id))
             {
                 if (entity == args.User
                     || !_itemQuery.TryGetComponent(entity, out var itemComp) // Need comp to get item size to get weight
@@ -1009,7 +1009,7 @@ public abstract partial class SharedStorageSystem : EntitySystem
             || Resolve(target, ref targetLock, false) && targetLock.Locked)
             return;
 
-        foreach (var entity in entities.ToArray())
+        foreach (var entity in entities.ToArray().OrderBy(e => GetNetEntity(e).Id))
         {
             Insert(target, entity, out _, user: user, targetComp, playSound: false);
         }
@@ -1208,7 +1208,7 @@ public abstract partial class SharedStorageSystem : EntitySystem
 
         var toInsertCount = insertStack.Count;
 
-        foreach (var ent in storageComp.Container.ContainedEntities)
+        foreach (var ent in storageComp.Container.ContainedEntities.OrderBy(e => GetNetEntity(e).Id))
         {
             if (!_stackQuery.TryGetComponent(ent, out var containedStack))
                 continue;
