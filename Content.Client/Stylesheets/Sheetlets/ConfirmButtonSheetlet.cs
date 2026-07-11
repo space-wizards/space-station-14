51783dd53aaba307a35b2ea4dd4e0c65413ab4e5
Thomas (Aeshus) <git@aeshus.com>
2026-07-11 17:52:08 -0500

    Update ConfirmButtonSheetlet

--- Content.Client/Stylesheets/Sheetlets/ConfirmButtonSheetlet.cs
+++ Content.Client/Stylesheets/Sheetlets/ConfirmButtonSheetlet.cs
@@ -1,32 +1,35 @@
+using Content.Client.Stylesheets.SheetletConfigs;
+using Content.Client.Stylesheets.Stylesheets;
+using Content.Client.UserInterface.Controls;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
-using Content.Client.Stylesheets;
-using Content.Client.Stylesheets.Stylesheets;
 using static Content.Client.Stylesheets.StylesheetHelpers;
 
-namespace Content.Client.UserInterface.Controls;
+namespace Content.Client.Stylesheets.Sheetlets;
 
-[Sheetlet]
-public sealed class ConfirmButtonSheetlet : ISheetlet<NanotrasenStylesheetFactory>
+[Sheetlet(typeof(CommonStylesheetFactory))]
+public sealed class ConfirmButtonSheetlet<T> : ISheetlet<T>
+    where T : IPaletteConfig
 {
-    public StyleRule[] GetRules(NanotrasenStylesheetFactory sheet, object config)
+    public StyleRule[] GetRules(StylesheetFactory factory, T config)
     {
-        return [
+        return
+        [
             E<ConfirmButton>()
                 .Pseudo(ConfirmButton.ConfirmPrefix + ContainerButton.StylePseudoClassNormal)
-                .Prop(Control.StylePropertyModulateSelf, sheet.NegativePalette.Element),
+                .Prop(Control.StylePropertyModulateSelf, config.NegativePalette.Element),
 
             E<ConfirmButton>()
                 .Pseudo(ConfirmButton.ConfirmPrefix + ContainerButton.StylePseudoClassHover)
-                .Prop(Control.StylePropertyModulateSelf, sheet.NegativePalette.HoveredElement),
+                .Prop(Control.StylePropertyModulateSelf, config.NegativePalette.HoveredElement),
 
             E<ConfirmButton>()
                 .Pseudo(ConfirmButton.ConfirmPrefix + ContainerButton.StylePseudoClassPressed)
-                .Prop(Control.StylePropertyModulateSelf, sheet.NegativePalette.PressedElement),
+                .Prop(Control.StylePropertyModulateSelf, config.NegativePalette.PressedElement),
 
             E<ConfirmButton>()
                 .Pseudo(ConfirmButton.ConfirmPrefix + ContainerButton.StylePseudoClassDisabled)
-                .Prop(Control.StylePropertyModulateSelf, sheet.NegativePalette.DisabledElement),
+                .Prop(Control.StylePropertyModulateSelf, config.NegativePalette.DisabledElement),
         ];
     }
 }
