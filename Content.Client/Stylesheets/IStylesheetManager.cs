e9ea35d4acb711e8d6e67817d562cb89996a4aff
Thomas (Aeshus) <git@aeshus.com>
2026-07-12 17:56:05 -0500

    Pull in PJB's new StylesheetManager code for hot reloading/updating
    Co-authored-by: Pieter-Jan Briers <pieterjan.briers+git@gmail.com>

--- Content.Client/Stylesheets/IStylesheetManager.cs
+++ Content.Client/Stylesheets/IStylesheetManager.cs
@@ -8,14 +8,18 @@ namespace Content.Client.Stylesheets;
 /// </summary>
 public interface IStylesheetManager
 {
+    #region Obsolete APIs
+
     /// <summary>
     /// Default UI style, used for in-game/in-context UIs, but used basically everywhere anyways.
     /// </summary>
+    [Obsolete("Access via UseStylesheet/IStylesheetAccessor instead")]
     Stylesheet SheetNanotrasen { get; }
 
     /// <summary>
     /// For heavily out-of-context UIs, such as admin UIs/debug UIs/changelog.
     /// </summary>
+    [Obsolete("Access via UseStylesheet/IStylesheetAccessor instead")]
     Stylesheet SheetSystem { get; }
 
     /// <summary>
@@ -33,10 +37,74 @@ public interface IStylesheetManager
     /// <summary>
     /// Resolve a stylesheet by name
     /// </summary>
+    [Obsolete("Access via UseStylesheet/IStylesheetAccessor instead")]
     bool TryGetStylesheet(string name, [MaybeNullWhen(false)] out Stylesheet stylesheet);
+
+    #endregion
+
+    /// <summary>
+    /// Apply a stylesheet to a control and automatically subscribe to updates.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This will automatically update the stylesheet on the control if the backing stylesheet changed,
+    /// for example due to user preferences.
+    /// </para>
+    /// <para>
+    /// A call to <see cref="UseStylesheet"/> should always be paired with a call to <see cref="StopStylesheet"/>,
+    /// otherwise memory leaks will ensue! The best way to do this is to call <see cref="UseStylesheet"/> in
+    /// <see cref="Control.EnteredTree"/>, and call <see cref="StopStylesheet"/> in <see cref="Control.ExitedTree"/>.
+    /// </para>
+    /// <para>
+    /// If this method gets called twice on the same control, it will simply replace the previous
+    /// <paramref name="getStylesheet"/> method. In this scenario, <see cref="StopStylesheet"/> does <b>not</b> need to
+    /// be called another time for cleanup, in this scenario.
+    /// </para>
+    /// </remarks>
+    /// <param name="control">The control to apply the stylesheet to.</param>
+    /// <param name="getStylesheet">
+    /// A function used to select the stylesheet from the <see cref="IStylesheetAccessor"/>.
+    /// </param>
+    void UseStylesheet(Control control, Func<IStylesheetAccessor, Stylesheet> getStylesheet);
+
+    /// <summary>
+    /// Stop stylesheet update subscription from <see cref="UseStylesheet"/>.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This does not (currently) unset the stylesheet on <paramref name="control"/>, as a performance optimization.
+    /// Do not rely on this.
+    /// </para>
+    /// </remarks>
+    /// <param name="control">The control to unsubscribe.</param>
+    void StopStylesheet(Control control);
 
     /// <summary>
     /// Initalize the stylesheet manager.
     /// </summary>
     void Initialize();
+}
+
+/// <summary>
+/// Provides access to stylesheets on the <see cref="IStylesheetManager"/>.
+/// </summary>
+public interface IStylesheetAccessor
+{
+    /// <summary>
+    /// Nanotrasen style sheet: should be used for IC UIs like machines.
+    /// </summary>
+    /// <remarks>
+    /// Is currently default for legacy reasons.
+    /// </remarks>
+    Stylesheet SheetNanotrasen { get; }
+
+    /// <summary>
+    /// System stylesheet: used for OOC UIs.
+    /// </summary>
+    Stylesheet SheetSystem { get; }
+
+    /// <summary>
+    /// Get a stylesheet by name.
+    /// </summary>
+    bool TryGetStylesheet(string name, [MaybeNullWhen(false)] out Stylesheet stylesheet);
 }
--- Content.Client/Stylesheets/StylesheetManager.cs
+++ Content.Client/Stylesheets/StylesheetManager.cs
@@ -1,70 +1,168 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using Content.Client.Stylesheets.StylesheetFactories;
 using Robust.Client.ResourceManagement;
 using Robust.Client.UserInterface;
-using Robust.Shared.Reflection;
 
 namespace Content.Client.Stylesheets;
 
-/// <summary>
-/// Creates and provides access to stylesheets.
-/// </summary>
-public sealed partial class StylesheetManager : IStylesheetManager
+/// <inheritdoc cref="IStylesheetManager"/>
+public sealed partial class StylesheetManager : IStylesheetManager, IPostInjectInit
 {
     [Dependency] private ILogManager _logManager = default!;
     [Dependency] private IUserInterfaceManager _userInterfaceManager = default!;
-    [Dependency] private IReflectionManager _reflection = default!;
 
     // TODO: REMOVE (obsolete; used to construct StyleNano/StyleSpace)
     [Dependency] private IResourceCache _resCache = default!;
 
+    private readonly Dictionary<Control, Func<IStylesheetAccessor, Stylesheet>> _controlStylesheetSubs = [];
+    private readonly Dictionary<string, Stylesheet> _stylesheets = [];
+    private readonly StylesheetAccessorImpl _accessor;
+
+    private ISawmill _sawmill = null!;
+
+    private bool _initialized;
+
+    private Stylesheet? _sheetNanotrasen;
+    private Stylesheet? _sheetSystem;
+    private Stylesheet? _sheetNanoLegacy;
+    private Stylesheet? _sheetSpaceLegacy;
+
+    public StylesheetManager()
+    {
+        _accessor = new StylesheetAccessorImpl(this);
+    }
+
+    public void Initialize()
+    {
+        _sawmill.Debug("Initializing Stylesheets...");
+        var sw = Stopwatch.StartNew();
+
+        RegenerateStylesheets();
+
+        _sawmill.Debug(
+            $"Initialized {_sheetNanotrasen?.Rules.Count + _sheetSystem?.Rules.Count} style rules in {sw.Elapsed}");
+        _initialized = true;
+    }
+
     /// <inheritdoc/>
-    public Stylesheet SheetNanotrasen { get; private set; } = default!;
+    public void UseStylesheet(Control control, Func<IStylesheetAccessor, Stylesheet> getStylesheet)
+    {
+        _controlStylesheetSubs[control] = getStylesheet;
+        control.Stylesheet = getStylesheet(_accessor);
+    }
+
     /// <inheritdoc/>
-    public Stylesheet SheetSystem { get; private set; } = default!;
+    public void StopStylesheet(Control control)
+    {
+        // Not unsetting the stylesheet here (which would make it resolve to the default) for performance reasons.
+        _controlStylesheetSubs.Remove(control);
+    }
+
+    private void RegenerateStylesheets()
+    {
+        _sawmill.Debug("Regenerating stylesheets...");
+
+        _stylesheets.Clear();
+
+        // TODO: these factories can be saved/cached, but it's so infrequently used that it doesn't feel necessary.
+        _sheetNanotrasen = new NanotrasenStylesheetFactory().Build();
+        _sheetSystem = new SystemStylesheetFactory().Build();
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        _sheetNanoLegacy = new StyleNano(_resCache).Stylesheet;
+        _sheetSpaceLegacy = new StyleSpace(_resCache).Stylesheet;
+#pragma warning restore CS0618 // Type or member is obsolete
+
+        _stylesheets.Add("Nanotrasen", _sheetNanotrasen);
+        _stylesheets.Add("System", _sheetSystem);
+
+        // Default stylesheet (which will automatically propogate and update any UIs without a specific Stylesheet set)
+        _userInterfaceManager.Stylesheet = _sheetNanotrasen;
+
+        UpdateControls();
+    }
+
+    /// <summary>
+    /// Updates all controls that have specifically selected a stylesheet.
+    /// </summary>
+    private void UpdateControls()
+    {
+        foreach (var (control, getStylesheet) in _controlStylesheetSubs)
+        {
+            try
+            {
+                control.Stylesheet = getStylesheet(_accessor);
+            }
+            catch (Exception e)
+            {
+                _sawmill.Error($"Caught exception while updating stylesheets on controls! {e}");
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    void IPostInjectInit.PostInject()
+    {
+        _sawmill = _logManager.GetSawmill("style");
+    }
+
+    /// <inheritdoc/>
+    private sealed class StylesheetAccessorImpl(StylesheetManager owner) : IStylesheetAccessor
+    {
+        /// <inheritdoc/>
+        public Stylesheet SheetNanotrasen => GetOrThrow(owner._sheetNanotrasen);
+
+        /// <inheritdoc/>
+        public Stylesheet SheetSystem => GetOrThrow(owner._sheetSystem);
+
+        /// <inheritdoc/>
+        public bool TryGetStylesheet(string name, [MaybeNullWhen(false)] out Stylesheet stylesheet)
+        {
+            if (!owner._initialized)
+                ThrowNotInitialized();
+
+            return owner._stylesheets.TryGetValue(name, out stylesheet);
+        }
+
+        private static Stylesheet GetOrThrow(Stylesheet? sheet)
+        {
+            return sheet ?? ThrowNotInitialized();
+        }
+
+        [DoesNotReturn]
+        private static Stylesheet ThrowNotInitialized()
+        {
+            throw new InvalidOperationException("Stylesheets not initialized yet!");
+        }
+    }
+
+    #region Obsolete
+
+    /// <inheritdoc/>
+    [Obsolete("Access through UseStylesheet/IStylesheetAccessor instead")]
+    public Stylesheet SheetNanotrasen => _accessor.SheetNanotrasen;
+
+    /// <inheritdoc/>
+    [Obsolete("Access through UseStylesheet/IStylesheetAccessor instead")]
+    public Stylesheet SheetSystem => _accessor.SheetSystem;
 
     /// <inheritdoc/>
     [Obsolete("Update to use SheetNanotrasen instead")]
-    public Stylesheet SheetNano { get; private set; } = default!;
+    public Stylesheet SheetNano =>
+        _sheetNanoLegacy ?? throw new InvalidOperationException("Stylesheets not initialized yet!");
 
     /// <inheritdoc/>
     [Obsolete("Update to use SheetSystem instead")]
-    public Stylesheet SheetSpace { get; private set; } = default!;
-
-    private Dictionary<string, Stylesheet> Stylesheets { get; set; } = [];
-
-    /// <inheritdoc/>
-    public void Initialize()
-    {
-        var sawmill = _logManager.GetSawmill("style");
-        sawmill.Debug("Initializing Stylesheets...");
-        var sw = Stopwatch.StartNew();
-
-        Stylesheets = new Dictionary<string, Stylesheet>();
-
-        SheetNanotrasen = new NanotrasenStylesheetFactory().Build();
-        Stylesheets.Add("Nanotrasen", SheetNanotrasen);
-
-        SheetSystem = new SystemStylesheetFactory().Build();
-        Stylesheets.Add("System", SheetSystem);
-
-#pragma warning disable CS0618 // Type or member is obsolete
-        // NOTE: Please delete.
-        SheetNano = new StyleNano(_resCache).Stylesheet;
-        SheetSpace = new StyleSpace(_resCache).Stylesheet;
-#pragma warning restore CS0618 // Type or member is obsolete
-
-        // Set the default stylesheet
-        _userInterfaceManager.Stylesheet = SheetNanotrasen;
-
-        sawmill.Debug($"Initialized {Stylesheets.Values.Sum(s => s.Rules.Count)} style rules in {sw.Elapsed}");
-    }
+    public Stylesheet SheetSpace =>
+        _sheetSpaceLegacy ?? throw new InvalidOperationException("Stylesheets not initialized yet!");
 
     /// <inheritdoc/>
+    [Obsolete("Access through UseStylesheet/IStylesheetAccessor instead")]
     public bool TryGetStylesheet(string name, [MaybeNullWhen(false)] out Stylesheet stylesheet)
     {
-        return Stylesheets.TryGetValue(name, out stylesheet);
+        return _accessor.TryGetStylesheet(name, out stylesheet);
     }
+
+    #endregion
 }
