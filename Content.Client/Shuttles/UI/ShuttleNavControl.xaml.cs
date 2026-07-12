f7410723562d64558b42a2191cc08349c0d67cb0
Łukasz Lindert <lukasz.lindert@protonmail.com>
2026-07-12 23:03:48 +0200

    cleanup

--- Content.Client/Shuttles/UI/ShuttleNavControl.xaml.cs
+++ Content.Client/Shuttles/UI/ShuttleNavControl.xaml.cs
@@ -298,11 +298,16 @@ public sealed partial class ShuttleNavControl : BaseShuttleControl
         if (!ShowDocks)
             return;
 
-        const float DockScale = 0.6f;
+        const float dockScale = 0.6f;
+        var topLeft = new Vector2(-dockScale, -dockScale);
+        var topRight = new Vector2(dockScale, -dockScale);
+        var bottomRight = new Vector2(dockScale, dockScale);
+        var bottomLeft = new Vector2(-dockScale, dockScale);
+
         var nent = EntManager.GetNetEntity(uid);
 
         const float sqrt2 = 1.41421356f;
-        const float dockRadius = DockScale * sqrt2;
+        const float dockRadius = dockScale * sqrt2;
         // Worst-case bounds used to cull a dock:
         Box2 viewBounds = new Box2(
             -dockRadius * UIScale,
@@ -325,10 +330,10 @@ public sealed partial class ShuttleNavControl : BaseShuttleControl
 
                 var color = Color.ToSrgb(state.HighlightedColor);
 
-                verts[0] = Vector2.Transform(position + new Vector2(-DockScale, -DockScale), gridToView);
-                verts[1] = Vector2.Transform(position + new Vector2(DockScale, -DockScale), gridToView);
-                verts[2] = Vector2.Transform(position + new Vector2(DockScale, DockScale), gridToView);
-                verts[3] = Vector2.Transform(position + new Vector2(-DockScale, DockScale), gridToView);
+                verts[0] = Vector2.Transform(position + topLeft, gridToView);
+                verts[1] = Vector2.Transform(position + topRight, gridToView);
+                verts[2] = Vector2.Transform(position + bottomRight, gridToView);
+                verts[3] = Vector2.Transform(position + bottomLeft, gridToView);
 
                 handle.DrawPrimitives(DrawPrimitiveTopology.TriangleFan, verts, color.WithAlpha(0.8f));
                 handle.DrawPrimitives(DrawPrimitiveTopology.LineStrip, verts, color);
