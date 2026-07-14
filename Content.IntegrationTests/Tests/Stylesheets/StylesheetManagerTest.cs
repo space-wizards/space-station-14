419843d8164d9608d04e9858756e4601d7891aa0
Thomas (Aeshus) <git@aeshus.com>
2026-07-14 17:23:43 -0500

    Throw some tests together, idk
    Also threw in Serilog there as I need it to access properties of things
    but when it moved to RT's namespace, I couldn't access it anymore in
    rider so idk.
    
     It should be picked up but rider said to do this so I listen

---
+++ Content.IntegrationTests/Tests/Stylesheets/StylesheetManagerTest.cs
@@ -0,0 +1,50 @@
+using Content.Client.Stylesheets;
+using Content.IntegrationTests.Fixtures;
+using Content.IntegrationTests.Fixtures.Attributes;
+using Robust.Client.UserInterface;
+
+namespace Content.IntegrationTests.Tests.Stylesheets;
+
+[TestOf(typeof(StylesheetManager))]
+public sealed class StylesheetManagerTest : GameTest
+{
+    [SidedDependency(Side.Client)] private IStylesheetManager _stylesheet = default!;
+
+    [Test]
+    [Description("Checks the basic functionality of Stylesheet Manager")]
+    [RunOnSide(Side.Client)]
+    public void TestStylesheetManager()
+    {
+        // This test should also fail if there's a missing constraint for the stylesheets provided by the manager
+
+#pragma warning disable CS0618
+        // Check the old obsolete direct accessors still work
+        var nanotrasen = _stylesheet.SheetNanotrasen;
+        var system = _stylesheet.SheetSystem;
+
+        // Test the string accessor method works
+        // Remember to update if you change any of the string names
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(_stylesheet.TryGetStylesheet("Nanotrasen", out _), Is.True);
+            Assert.That(_stylesheet.TryGetStylesheet("System", out _), Is.True);
+            Assert.That(_stylesheet.TryGetStylesheet("GoodUI", out _), Is.False);
+        }
+#pragma warning restore CS0618
+
+        var control = new Control();
+        _stylesheet.UseStylesheet(control, a => a.SheetNanotrasen);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(control.Stylesheet, Is.Not.Null);
+            Assert.That(control.Stylesheet, Is.SameAs(nanotrasen));
+        }
+
+        _stylesheet.UseStylesheet(control, a => a.SheetSystem);
+        Assert.That(control.Stylesheet, Is.SameAs(system));
+
+        // We don't currently reset to defaults right now.
+        _stylesheet.StopStylesheet(control);
+        Assert.That(control.Stylesheet, Is.SameAs(system));
+    }
+}
