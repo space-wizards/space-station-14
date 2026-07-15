95b3b3c6bbbfb46eab975a09a2b9ef7bafa6e507
DrSmugleaf <drsmugleaf@gmail.com>
2026-07-14 23:21:57 -0700

    Add components to require uncovered mouth and specific mob states for actions

---
+++ Content.Shared/Nutrition/Components/ActionRequireMouthUncoveredComponent.cs
@@ -0,0 +1,8 @@
+﻿using Content.Shared.Nutrition.EntitySystems;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Nutrition.Components;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(IngestionSystem))]
+public sealed partial class ActionRequireMouthUncoveredComponent : Component;
