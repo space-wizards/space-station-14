74bc29f2a48a4ba4107f2581fde3bff3c0db7133
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2026-07-15 16:52:06 -0700

    wokies

--- Content.Shared/Blocking/Components/BlockingComponent.cs
+++ Content.Shared/Blocking/Components/BlockingComponent.cs
@@ -24,10 +24,10 @@ public sealed partial class BlockingComponent : Component
     public EntityUid? User;
 
     /// <summary>
-    /// Is it currently blocking?
+    /// Is it currently raised and using its <see cref="ActiveBlockFraction"/>?
     /// </summary>
     [DataField, AutoNetworkedField]
-    public bool IsBlocking;
+    public bool IsRaised;
 
     /// <summary>
     /// The shape of the blocking fixture that will be dynamically spawned
