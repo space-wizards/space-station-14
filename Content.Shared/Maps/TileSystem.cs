441ff76d47fd96ac9d644c3507db66968cfd1e22
Whatstone <whatston3@gmail.com>
2026-07-15 12:19:51 -0400

    nullable placementVariants

--- Content.Shared/Maps/TileSystem.cs
+++ Content.Shared/Maps/TileSystem.cs
@@ -122,6 +122,10 @@ public sealed partial class TileSystem : EntitySystem
     /// </summary>
     public byte PickVariant(ContentTileDefinition tile, IRobustRandom random)
     {
+        // Null variants? Uniform distribution.
+        if (tile.PlacementVariants == null)
+            return random.NextByte(tile.Variants);
+
         var variants = tile.PlacementVariants;
 
         var sum = variants.Sum();
