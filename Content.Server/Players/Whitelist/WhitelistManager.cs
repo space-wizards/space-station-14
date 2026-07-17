3337217698d636b1ff884d0cd0143541b6d0cd8a
SlamBamActionman <slambamactionman@gmail.com>
2026-07-17 18:50:59 +0200

    Initial commit

---
+++ Content.Server/Players/Whitelist/WhitelistManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Content.Server.Database;
+using Robust.Shared.Network;
+using Robust.Shared.Player;
+
+namespace Content.Server.Players.Whitelist;
+
+public sealed partial class WhitelistManager : IPostInjectInit
+{
+    [Dependency] private IServerDbManager _db = default!;
+    [Dependency] private UserDbDataManager _userDb = default!;
+    [Dependency] private ILogManager _logManager = default!;
+
+    private readonly Dictionary<NetUserId, bool> _whitelistStatus = new();
+    private ISawmill _sawmill = default!;
+
+    private async Task LoadData(ICommonSession session, CancellationToken cancel)
+    {
+        var whitelistStatus = await _db.GetWhitelistStatusAsync(session.UserId);
+        _whitelistStatus[session.UserId] = whitelistStatus;
+    }
+
+    private void ClientDisconnected(ICommonSession session)
+    {
+        _whitelistStatus.Remove(session.UserId);
+    }
+
+    /// <inheritdoc cref="IsWhitelisted(Robust.Shared.Network.NetUserId)"/>
+    public bool IsWhitelisted(ICommonSession session)
+    {
+        return IsWhitelisted(session.UserId);
+    }
+
+    /// <summary>
+    /// Returns true if the player has whitelist status on the server.
+    /// Returns false if the player doesn't, or if the database failed to load them.
+    /// </summary>
+    public bool IsWhitelisted(NetUserId userId)
+    {
+        if (!_whitelistStatus.TryGetValue(userId, out var whitelistStatus))
+        {
+            _sawmill.Error("Unable to check if player {Player} is whitelisted for the server in the database. Stack trace:\\n{StackTrace}",
+                userId,
+                Environment.StackTrace);
+            return false;
+        }
+
+        return whitelistStatus;
+    }
+
+    /// <summary>
+    /// Adds a player to the whitelist and the whitelist tracker.
+    /// </summary>
+    public async void AddWhitelist(NetUserId player)
+    {
+        _whitelistStatus[player] = true;
+
+        await _db.AddToWhitelistAsync(player);
+    }
+
+    /// <summary>
+    /// Removes a player from the whitelist and the whitelist tracker.
+    /// </summary>
+    public async void RemoveWhitelist(NetUserId player)
+    {
+        if (_whitelistStatus.ContainsKey(player))
+            _whitelistStatus[player] = false;
+
+        await _db.RemoveFromWhitelistAsync(player);
+    }
+
+    void IPostInjectInit.PostInject()
+    {
+        _userDb.AddOnLoadPlayer(LoadData);
+        _userDb.AddOnPlayerDisconnect(ClientDisconnected);
+        _sawmill = _logManager.GetSawmill("server_whitelist");
+    }
+}
--- Content.Server/Whitelist/WhitelistCommands.cs
+++ Content.Server/Whitelist/WhitelistCommands.cs
@@ -1,5 +1,6 @@
 using Content.Server.Administration;
 using Content.Server.Database;
+using Content.Server.Players.Whitelist;
 using Content.Shared.Administration;
 using Content.Shared.CCVar;
 using Robust.Server.Player;
@@ -13,7 +14,7 @@ namespace Content.Server.Whitelist;
 public sealed partial class AddWhitelistCommand : LocalizedCommands
 {
     [Dependency] private IPlayerLocator _locator = default!;
-    [Dependency] private IServerDbManager _dbManager = default!;
+    [Dependency] private WhitelistManager _whitelistManager = default!;
     public override string Command => "whitelistadd";
 
     public override async void Execute(IConsoleShell shell, string argStr, string[] args)
@@ -31,14 +32,13 @@ public sealed partial class AddWhitelistCommand : LocalizedCommands
         if (data != null)
         {
             var guid = data.UserId;
-            var isWhitelisted = await _dbManager.GetWhitelistStatusAsync(guid);
-            if (isWhitelisted)
+            if (_whitelistManager.IsWhitelisted(guid))
             {
                 shell.WriteLine(Loc.GetString("cmd-whitelistadd-existing", ("username", data.Username)));
                 return;
             }
 
-            await _dbManager.AddToWhitelistAsync(guid);
+            _whitelistManager.AddWhitelist(guid);
             shell.WriteLine(Loc.GetString("cmd-whitelistadd-added", ("username", data.Username)));
             return;
         }
@@ -61,7 +61,7 @@ public sealed partial class AddWhitelistCommand : LocalizedCommands
 public sealed partial class RemoveWhitelistCommand : LocalizedCommands
 {
     [Dependency] private IPlayerLocator _locator = default!;
-    [Dependency] private IServerDbManager _dbManager = default!;
+    [Dependency] private WhitelistManager _whitelistManager = default!;
 
     public override string Command => "whitelistremove";
 
@@ -80,14 +80,13 @@ public sealed partial class RemoveWhitelistCommand : LocalizedCommands
         if (data != null)
         {
             var guid = data.UserId;
-            var isWhitelisted = await _dbManager.GetWhitelistStatusAsync(guid);
-            if (!isWhitelisted)
+            if (!_whitelistManager.IsWhitelisted(guid))
             {
                 shell.WriteLine(Loc.GetString("cmd-whitelistremove-existing", ("username", data.Username)));
                 return;
             }
 
-            await _dbManager.RemoveFromWhitelistAsync(guid);
+            _whitelistManager.RemoveWhitelist(guid);
             shell.WriteLine(Loc.GetString("cmd-whitelistremove-removed", ("username", data.Username)));
             return;
         }
@@ -133,6 +132,8 @@ public sealed partial class KickNonWhitelistedCommand : LocalizedCommands
             if (await _dbManager.GetAdminDataForAsync(session.UserId) is not null)
                 continue;
 
+            // We let this one query the whitelist to be 100% certain it's kicking out non-whitelisted players +
+            // it's mostly adding/removing whitelists that needs to go through the cache.
             if (!await _dbManager.GetWhitelistStatusAsync(session.UserId))
                 _netManager.DisconnectChannel(session.Channel, Loc.GetString("whitelist-not-whitelisted"));
         }
