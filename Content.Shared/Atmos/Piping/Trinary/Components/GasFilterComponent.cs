90445dc663aa22e00926c291e725da713368c155
psykana <36602558+psykana@users.noreply.github.com>
2026-07-19 18:38:53 +0100

    docs

--- Content.Shared/Atmos/Piping/Trinary/Components/GasFilterComponent.cs
+++ Content.Shared/Atmos/Piping/Trinary/Components/GasFilterComponent.cs
@@ -2,10 +2,16 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.Atmos.Piping.Trinary.Components;
 
+/// <summary>
+/// Defines a gas filter, which removes a specific gas out of the input mixture, and outputs that to a secondary outlet
+/// </summary>
 [RegisterComponent, NetworkedComponent]
 [AutoGenerateComponentState(raiseAfterAutoHandleState: true, fieldDeltas: true)]
 public sealed partial class GasFilterComponent : Component
 {
+    /// <summary>
+    /// Indicates whether this filter is currently operational
+    /// </summary>
     [DataField, AutoNetworkedField]
     public bool Enabled = true;
 
@@ -27,12 +33,21 @@ public sealed partial class GasFilterComponent : Component
     [DataField]
     public string Filter = "filter";
 
+    /// <summary>
+    /// Inlet node gas transfer rate, in L\s
+    /// </summary>
     [DataField, AutoNetworkedField]
     public float TransferRate = Atmospherics.MaxTransferRate;
 
+    /// <summary>
+    /// Maximum allowed inlet node gas transfer rate, in L\s
+    /// </summary>
     [DataField, AutoNetworkedField]
     public float MaxTransferRate = Atmospherics.MaxTransferRate;
 
+    /// <summary>
+    /// Indicates gas type to be filtered out into the secondary outlet
+    /// </summary>
     [DataField, AutoNetworkedField]
     public Gas? FilteredGas;
 }
