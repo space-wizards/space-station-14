26f1c631a49de2e7cebb9e6d18b3b40fa08c8010
Velken <8467292+Velken@users.noreply.github.com>
2026-07-23 17:42:58 -0300

    added comments

--- Content.Shared/Mobs/Components/ActionRequireMobStateComponent.cs
+++ Content.Shared/Mobs/Components/ActionRequireMobStateComponent.cs
@@ -8,9 +8,15 @@ namespace Content.Shared.Mobs.Components;
 [Access(typeof(MobStateSystem))]
 public sealed partial class ActionRequireMobStateComponent : Component
 {
+    /// <summary>
+    /// The required MobState for the action.
+    /// </summary>
     [DataField(required: true), AutoNetworkedField]
     public HashSet<MobState> States = new();
 
+    /// <summary>
+    /// The type of Popup to be used.
+    /// </summary>
     [DataField, AutoNetworkedField]
     public PopupType? Popup = PopupType.SmallCaution;
 }
--- Content.Shared/Nutrition/Components/ActionRequireMouthUncoveredComponent.cs
+++ Content.Shared/Nutrition/Components/ActionRequireMouthUncoveredComponent.cs
@@ -8,6 +8,9 @@ namespace Content.Shared.Nutrition.Components;
 [Access(typeof(IngestionSystem))]
 public sealed partial class ActionRequireMouthUncoveredComponent : Component
 {
+    /// <summary>
+    /// The slots that must be free for the action to be used.
+    /// </summary>
     [DataField, AutoNetworkedField]
     public SlotFlags Slots = SlotFlags.HEAD | SlotFlags.MASK;
 }
