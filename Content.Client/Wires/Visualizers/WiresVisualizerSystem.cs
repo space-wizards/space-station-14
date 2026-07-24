ceeaba63a44caec15b3c18c5f56b2273d9a0683a
Whatstone <whatston3@gmail.com>
2026-07-24 10:04:18 -0400

    Add Inverted field to WiresPanel for default viz

--- Content.Client/Wires/Visualizers/WiresVisualizerSystem.cs
+++ Content.Client/Wires/Visualizers/WiresVisualizerSystem.cs
@@ -1,32 +1,35 @@
 using Content.Shared.Wires;
 using Robust.Client.GameObjects;
 
-namespace Content.Client.Wires.Visualizers
+namespace Content.Client.Wires.Visualizers;
+
+public sealed class WiresVisualizerSystem : VisualizerSystem<WiresVisualsComponent>
 {
-    public sealed class WiresVisualizerSystem : VisualizerSystem<WiresVisualsComponent>
+    protected override void OnAppearanceChange(EntityUid uid, WiresVisualsComponent component, ref AppearanceChangeEvent args)
     {
-        protected override void OnAppearanceChange(EntityUid uid, WiresVisualsComponent component, ref AppearanceChangeEvent args)
+        if (args.Sprite == null)
+            return;
+
+        var layer = SpriteSystem.LayerMapReserve((uid, args.Sprite), WiresVisualLayers.MaintenancePanel);
+
+        if (args.AppearanceData.TryGetValue(WiresVisuals.MaintenancePanelState, out var panelStateObject) &&
+            panelStateObject is bool panelState)
         {
-            if (args.Sprite == null)
-                return;
-
-            var layer = SpriteSystem.LayerMapReserve((uid, args.Sprite), WiresVisualLayers.MaintenancePanel);
-
-            if (args.AppearanceData.TryGetValue(WiresVisuals.MaintenancePanelState, out var panelStateObject) &&
-                panelStateObject is bool panelState)
-            {
-                SpriteSystem.LayerSetVisible((uid, args.Sprite), layer, panelState);
-            }
-            else
-            {
-                //Mainly for spawn window
-                SpriteSystem.LayerSetVisible((uid, args.Sprite), layer, false);
-            }
+            SpriteSystem.LayerSetVisible((uid, args.Sprite), layer, panelState ^ component.Inverted);
+        }
+        else
+        {
+            //Mainly for spawn window
+            SpriteSystem.LayerSetVisible((uid, args.Sprite), layer, !component.Inverted);
         }
     }
-
-    public enum WiresVisualLayers : byte
-    {
-        MaintenancePanel
-    }
 }
+
+/// <summary>
+/// Layers for the maintenance panel.
+/// </summary>
+public enum WiresVisualLayers : byte
+{
+    MaintenancePanel
+}
+
--- Content.Client/Wires/WiresVisualsComponent.cs
+++ Content.Client/Wires/WiresVisualsComponent.cs
@@ -1,7 +1,16 @@
-namespace Content.Client.Wires
+namespace Content.Client.Wires;
+
+/// <summary>
+/// Denotes an entity with an openable maintenance panel.
+/// The sprite layer with a map of "enum.WiresVisualLayers.MaintenancePanel" will be hidden and shown when the maintenance panel is open.
+/// </summary>
+/// <seealso cref="WiresVisuals.MaintenancePanelState"/>
+[RegisterComponent]
+public sealed partial class WiresVisualsComponent : Component
 {
-    [RegisterComponent]
-    public sealed partial class WiresVisualsComponent : Component
-    {
-    }
+    /// <summary>
+    /// If true, the maintenance panel should be visible normally.
+    /// </summary>
+    [DataField]
+    public bool Inverted;
 }
