68b415d8537508118149d71f5d0b2a9b19642741
SlamBamActionman <slambamactionman@gmail.com>
2026-07-25 18:01:15 +0200

    Review changes

--- Content.Shared/Ghost/Roles/SharedGhostRoleSystem.cs
+++ Content.Shared/Ghost/Roles/SharedGhostRoleSystem.cs
@@ -20,36 +20,70 @@ public sealed class GhostRole
 [DataDefinition]
 public sealed partial class GhostRoleSettings
 {
+    /// <summary>
+    /// Whether the MakeSentientCommand should run on the mob.
+    /// </summary>
     [DataField]
     public bool MakeSentient = true;
 
+    /// <summary>
+    /// The probability that this ghost role will be available after init.
+    /// Used mostly for takeover roles that want some probability of being takeover, but not 100%.
+    /// </summary>
     [DataField]
     public float Probability = 1f;
 
+    /// <summary>
+    /// Name of the role to display in the ghost role UI.
+    /// </summary>
     [DataField]
     public string RoleName = "Unknown";
 
+    /// <summary>
+    /// Description of the role to display in the ghost role UI.
+    /// </summary>
     [DataField]
     public string RoleDescription = "Unknown";
 
+    /// <summary>
+    /// Rules for the role to display in the ghost role UI when selected.
+    /// </summary>
     [DataField]
     public string RoleRules = "ghost-role-component-default-rules";
 
+    /// <summary>
+    /// The mind roles that will be added to the mob's mind entity
+    /// </summary>
     [DataField]
     public List<EntProtoId> MindRoles = new() { "MindRoleGhostRoleNeutral" };
 
+    /// <summary>
+    /// Can the ghost role speak?
+    /// </summary>
     [DataField]
     public bool AllowSpeech = true;
 
+    /// <summary>
+    /// Can the ghost role move?
+    /// </summary>
     [DataField]
     public bool AllowMovement = false;
 
+    /// <summary>
+    /// Reregisters the ghost role when the current player ghosts.
+    /// </summary>
     [DataField]
     public bool ReregisterOnGhost = true;
 
+    /// <summary>
+    /// If set, ghost role is raffled, otherwise it is first-come-first-serve.
+    /// </summary>
     [DataField]
     public GhostRoleRaffleConfig? RaffleConfig = null;
 
+    /// <summary>
+    /// Job the entity will receive after adding the mind.
+    /// </summary>
     [DataField]
     public ProtoId<JobPrototype>? JobProto = null;
 }
