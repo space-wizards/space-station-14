1e54fb8decdbb3ebd8cf7c1b5a20075a22d353e5
riccardi48 <richardballard00@gmail.com>
2026-07-26 13:37:39 +0100

    bugfix

--- Content.Server/Cargo/Systems/CargoSystem.Telepad.cs
+++ Content.Server/Cargo/Systems/CargoSystem.Telepad.cs
@@ -45,11 +45,10 @@ public sealed partial class CargoSystem
                 console.Value.Owner != args.OrderConsole.Owner)
                 continue;
 
-            for (var i = 0; i < args.Order.OrderQuantity; i++)
-            {
-                tele.CurrentOrders.Add(args.Order);
-            }
+            tele.CurrentOrders.Add(args.Order);
+
             tele.Accumulator = tele.Delay;
+
             args.Handled = true;
             args.FulfillmentEntity = uid;
             return;
@@ -98,6 +97,8 @@ public sealed partial class CargoSystem
                 continue;
             }
 
+            comp.CurrentOrders.RemoveAll(order => order.NumDispatched >= order.OrderQuantity);
+
             if (comp.CurrentOrders.Count == 0 || !TryGetLinkedConsole((uid, comp), out var console))
             {
                 comp.Accumulator += comp.Delay;
@@ -107,6 +108,8 @@ public sealed partial class CargoSystem
             var currentOrder = comp.CurrentOrders.First();
             if (FulfillOrder(currentOrder, currentOrder.Account, xform.Coordinates, comp.PrinterOutput))
             {
+                currentOrder.NumDispatched++;
+
                 var teleportSound = comp.TeleportSound;
                 var audioParams = teleportSound?.Params ?? AudioParams.Default;
                 audioParams = audioParams.AddVolume(-8f);
@@ -115,7 +118,6 @@ public sealed partial class CargoSystem
                 if (_station.GetOwningStation(uid) is { } station)
                     UpdateOrders(station);
 
-                comp.CurrentOrders.Remove(currentOrder);
                 comp.CurrentState = CargoTelepadState.Teleporting;
                 _appearance.SetData(uid, CargoTelepadVisuals.State, CargoTelepadState.Teleporting, appearance);
             }
