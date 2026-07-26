8e5d3deada46221fba88b83f044c5b274620f838
riccardi48 <richardballard00@gmail.com>
2026-07-26 20:53:54 +0100

    Event handler to top of file

--- Content.Client/Cards/CardSystem.cs
+++ Content.Client/Cards/CardSystem.cs
@@ -16,60 +16,6 @@ public sealed partial class CardSystem : SharedCardSystem
 
     [Dependency] private IPlayerManager _playerManager = default!;
 
-    /// <summary>
-    /// Calculates the local position of a card on a fanned arc, given its angle from center. It is shifted downwards 3/4 of radius to center it.
-    /// </summary>
-    /// <param name="angle">The angle of the card along the fan, in radians, where 0 is centered.</param>
-    /// <param name="radius">The radius of the fan's arc.</param>
-    /// <returns>The local position offset for the card. In pixel coordinates scale.</returns>
-    public static Vector2 FanPosition(double angle, float radius) =>
-        new((float)Math.Sin(angle) * radius, (float)Math.Cos(angle) * radius - radius * (3f / 4f));
-
-    /// <summary>
-    /// Calculates the radius of the fan arc based on the number of cards.
-    /// </summary>
-    /// <param name="count">The total number of cards in the fan.</param>
-    /// <returns>The fan radius, or 0 if there is only one card, since a single card cannot be fanned.</returns>
-    public static float FanRadius(int count) => count <= 1 ? 0f : (float)Math.Sqrt(count / 20f);
-
-    /// <summary>
-    /// Calculates the position and rotation of a card at a given index within a fanned hand,
-    /// arranging cards in a semi-circle from left to right.
-    /// </summary>
-    /// <param name="idx">The index of the card of those to be fanned.</param>
-    /// <param name="count">The total number of cards in the hand.</param>
-    /// <returns>A tuple containing the card's position and rotation.</returns>
-    public static (Vector2, Angle) GetCardPosRot(int idx, int count)
-    {
-        var radius = FanRadius(count);
-        return GetCardPosRot(idx, count, radius);
-    }
-
-    /// <summary>
-    /// Calculates the position and rotation of a card at a given index within a fanned hand,
-    /// arranging cards in a semi-circle from left to right.
-    /// </summary>
-    /// <param name="idx">The index of the card of those to be fanned.</param>
-    /// <param name="count">The total number of cards in the hand.</param>
-    /// <param name="radius">The radius of the fan's arc.</param>
-    /// <returns>A tuple containing the card's position and rotation.</returns>
-    public static (Vector2, Angle) GetCardPosRot(int idx, int count, float radius)
-    {
-        // Semi-circle from left to right
-        float angle = (idx - count / 2.0f + 0.5f) / count * (float)Math.PI;
-        var position = FanPosition(angle, radius);
-        var rotation = new Angle(-angle);
-        return (position, rotation);
-    }
-
-    /// <summary>
-    /// Gets the layer names for the 3 layers sprite layers used for a card.
-    /// </summary>
-    /// <param name="idx">The index of the card of those to be fanned.</param>
-    /// <returns>The three layer names</returns>
-    private static (string Base, string LayerOne, string LayerTwo) CardLayers(int idx) =>
-        ($"card_{idx}_base", $"card_{idx}_layerOne", $"card_{idx}_layerTwo");
-
     [SubscribeLocalEvent]
     private void OnAppearanceChanged(EntityUid uid, CardsComponent _, ref AppearanceChangeEvent args)
     {
@@ -148,6 +94,60 @@ public sealed partial class CardSystem : SharedCardSystem
                 TransformLayer("base", position, rotation, (uid, sprite));
         }
     }
+
+    /// <summary>
+    /// Calculates the local position of a card on a fanned arc, given its angle from center. It is shifted downwards 3/4 of radius to center it.
+    /// </summary>
+    /// <param name="angle">The angle of the card along the fan, in radians, where 0 is centered.</param>
+    /// <param name="radius">The radius of the fan's arc.</param>
+    /// <returns>The local position offset for the card. In pixel coordinates scale.</returns>
+    public static Vector2 FanPosition(double angle, float radius) =>
+        new((float)Math.Sin(angle) * radius, (float)Math.Cos(angle) * radius - radius * (3f / 4f));
+
+    /// <summary>
+    /// Calculates the radius of the fan arc based on the number of cards.
+    /// </summary>
+    /// <param name="count">The total number of cards in the fan.</param>
+    /// <returns>The fan radius, or 0 if there is only one card, since a single card cannot be fanned.</returns>
+    public static float FanRadius(int count) => count <= 1 ? 0f : (float)Math.Sqrt(count / 20f);
+
+    /// <summary>
+    /// Calculates the position and rotation of a card at a given index within a fanned hand,
+    /// arranging cards in a semi-circle from left to right.
+    /// </summary>
+    /// <param name="idx">The index of the card of those to be fanned.</param>
+    /// <param name="count">The total number of cards in the hand.</param>
+    /// <returns>A tuple containing the card's position and rotation.</returns>
+    public static (Vector2, Angle) GetCardPosRot(int idx, int count)
+    {
+        var radius = FanRadius(count);
+        return GetCardPosRot(idx, count, radius);
+    }
+
+    /// <summary>
+    /// Calculates the position and rotation of a card at a given index within a fanned hand,
+    /// arranging cards in a semi-circle from left to right.
+    /// </summary>
+    /// <param name="idx">The index of the card of those to be fanned.</param>
+    /// <param name="count">The total number of cards in the hand.</param>
+    /// <param name="radius">The radius of the fan's arc.</param>
+    /// <returns>A tuple containing the card's position and rotation.</returns>
+    public static (Vector2, Angle) GetCardPosRot(int idx, int count, float radius)
+    {
+        // Semi-circle from left to right
+        float angle = (idx - count / 2.0f + 0.5f) / count * (float)Math.PI;
+        var position = FanPosition(angle, radius);
+        var rotation = new Angle(-angle);
+        return (position, rotation);
+    }
+
+    /// <summary>
+    /// Gets the layer names for the 3 layers sprite layers used for a card.
+    /// </summary>
+    /// <param name="idx">The index of the card of those to be fanned.</param>
+    /// <returns>The three layer names</returns>
+    private static (string Base, string LayerOne, string LayerTwo) CardLayers(int idx) =>
+        ($"card_{idx}_base", $"card_{idx}_layerOne", $"card_{idx}_layerTwo");
 
     // Adds sprites to sprite layers and colours them.
     private void BuildCard(
