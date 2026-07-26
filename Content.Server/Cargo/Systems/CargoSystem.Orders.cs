12f2ab2ba48898f59ebe27fb55017d130f640bc4
riccardi48 <richardballard00@gmail.com>
2026-07-26 14:26:20 +0100

    oops

--- Content.Server/Cargo/Systems/CargoSystem.Orders.cs
+++ Content.Server/Cargo/Systems/CargoSystem.Orders.cs
@@ -233,6 +233,7 @@ namespace Content.Server.Cargo.Systems
                 {
                     _popup.PopupCursor(Loc.GetString("cargo-console-unfulfilled"), args.Actor);
                     PlayDenySound(uid, component);
+                    order.Approver = null;
                     order.ApprovingConsole = null;
                     order.Approved = false;
                     return;
