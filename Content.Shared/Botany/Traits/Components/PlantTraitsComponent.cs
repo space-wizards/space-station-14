20d1dab120ce68a49a84d5f89e5e45edba551581
Pok245 <113675512+Pok27@users.noreply.github.com>
2026-07-27 13:58:26 +0300

    Update PlantTraitsComponent.cs

--- Content.Shared/Botany/Traits/Components/PlantTraitsComponent.cs
+++ Content.Shared/Botany/Traits/Components/PlantTraitsComponent.cs
@@ -1,15 +1,13 @@
-using Robust.Shared.GameStates;
-
 namespace Content.Shared.Botany.Traits.Components;
 
 /// <summary>
-/// Base component for plant trait components.
+/// Base class for plant trait components.
 /// </summary>
-[RegisterComponent, NetworkedComponent]
 public abstract partial class PlantTraitsComponent : Component
 {
     /// <summary>
     /// Localization key describing the plant trait state.
     /// </summary>
+    [DataField]
     public abstract LocId TraitState { get; set; }
 }
