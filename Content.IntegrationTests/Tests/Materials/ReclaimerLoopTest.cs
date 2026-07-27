483c30e9977c8cff6acd9edd165c2d38d29b45e4
Sapphic_bee <beashakow@gmail.com>
2026-07-27 05:26:21 -0400

    Reclaimer Loop Prevention Integration Test (#43268)
    * Started Making Test
    
    * made reclaimMaterials false for the industral reagent grinder, to mimic the bug that #39690 fixed
    
    * hopefully set up parameterization ok?
    
    * un-parameterized Recyclables
    
    * Changed name of file, have main loops
    
    * in a better state, but still having issues with glass
    
    * Updated to include stackEntities for Silk, gunpowder, bones, cotton, and meatearial. They were just missing bc they presumeably never interacted with Materials storage.
    
    * Marked WebSilk as a material
    
    * Made Gunpowder a material too
    
    * A bugfix from moonheart08, already in #43207 but made it so my test could run
    
    * Have it working, but still falls into a loop if it fails. which is just the same bug
    
    * realized problem with my test paradigm
    
    * made things smoother if reclaimsMaterials is false
    
    * didn't end up needing the bugfix in triggersystem
    
    * Ran into Heisentest with EnsureValidGroupAndSex
    
    * Again ran into issue with EnsureValidGroupAndSex
    
    * Made  produceableMaterials a HashSet instead of a list (im gonna love HashSets i can tell)
    
    * ID strings moved to static readonly EntProtoIDs
    
    * typo fix
    
    * Using STarget better
    
    * Cleaned up checking for spawners
    
    * Fixed how I deal with filtering out spawners and random items
    
    * Using ToServer now
    
    * now enable the reclaimer in a server.waitPost
    
    * Proper Indentation for Assertions
    
    * Changes to comments and description
    
    * Fixed spawner-exclusive logic
    
    * Using protomanager instead of gamescrounger in test
    
    * took StackEntity away from bones
    
    * updated _reclaimers to readonly Reclaimers
    
    * Removed redundnacy in hashet declaration
    
    * removed redudant typing for MatStack
    
    * got matInHandsUid via ToServer now, fixed apcid name
    
    * using Is.Not.Null instead of is.not.equalto(null)
    
    * removed reliance on strings to filter the prototypes
    
    * Minor Comment Change
    
    * Re-ordered imports
    
    * Tiny cleanup
    
    * Using SPlayer better now.
    
    * refactor: fix test problems!
    
    * refactor: modernize test (where possible
    
    ---------
    
    Co-authored-by: Tayrtahn <tayrtahn@gmail.com>
    Co-authored-by: Fildrance <fildrance@gmail.com>

---
+++ Content.IntegrationTests/Tests/Materials/ReclaimerLoopTest.cs
@@ -0,0 +1,127 @@
+using Content.IntegrationTests.Fixtures.Attributes;
+using Content.IntegrationTests.Tests.Interaction;
+using Content.IntegrationTests.Utility;
+using Content.Server.Materials;
+using Content.Server.Spawners.Components;
+using Content.Shared.Materials;
+using Content.Shared.Sprite;
+using Content.Shared.Whitelist;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Prototypes;
+using System.Collections.Generic;
+
+namespace Content.IntegrationTests.Tests.Materials;
+
+/// <summary>
+/// Tests to prevent Recycler loops, where the product of one recycling can be recycled again.
+/// </summary>
+[TestOf(typeof(MaterialReclaimerSystem))]
+[TestOf(typeof(MaterialReclaimerComponent))]
+public sealed class ReclaimerLoopTest : InteractionTest
+{
+    //ProtoIDs we need
+    private static readonly EntProtoId ApcId = "APCBasic";
+    private static readonly EntProtoId FloorTileId = "FloorTileItemSteelCheckerDark";
+
+    private static readonly string[] Reclaimers = GameDataScrounger.EntitiesWithComponent("MaterialReclaimer");
+
+    [SidedDependency(Side.Server)] private readonly SharedMaterialReclaimerSystem _materialReclaimerSystem = null!;
+    [SidedDependency(Side.Server)] private readonly EntityWhitelistSystem _entityWhitelistSystem = null!;
+    [SidedDependency(Side.Server)] private readonly IComponentFactory _compFactory = null!;
+
+    [Test]
+    [TestCaseSource(nameof(Reclaimers))]
+    [TestOf(typeof(MaterialReclaimerSystem))]
+    [TestOf(typeof(MaterialReclaimerComponent))]
+    [Description("For every material that a reclaimer can spawn, make sure that it cannot get stuck in a loop of spawning then recycling.")]
+    [TrackingIssue("https://github.com/space-wizards/space-station-14/issues/39691")]
+    public async Task MaterialSpawnLoopTest(string reclaimerId)
+    {
+        //Spawn the reclaimer
+        await SpawnTarget(reclaimerId, PlayerCoords);
+        Assert.That(STarget, Is.Not.Null, "STarget was null, did the reclaimer spawn correctly?");
+
+        var reclaimComp = Comp<MaterialReclaimerComponent>(Target);
+        //If the reclaimer can produce materials
+        var reclaimsMaterials = reclaimComp.ReclaimMaterials;
+
+        //if the reclaimer has reclaimsMaterials and is able to produce materials
+        //go through all recyclable items, compile a HashSet of producible materials
+        HashSet<ProtoId<MaterialPrototype>> producibleMaterials = []; //If reclaimMaterials is false, this will stay empty
+        if (reclaimsMaterials)
+        {
+            foreach (var proto in ProtoMan.EnumeratePrototypes<EntityPrototype>())
+            {
+                //we don't care about items that don't recycle into anything physical
+                if (!proto.HasComp<PhysicalCompositionComponent>(_compFactory))
+                    continue;
+
+                //spawners and random items mess things up quickly, avoid them too
+                if (proto.HasComp<RandomSpriteComponent>(_compFactory) || proto.HasComp<EntityTableSpawnerComponent>(_compFactory))
+                    continue;
+
+                var currentScrap = await Spawn(proto.ID);
+                var currentScrapUid = ToServer(currentScrap);
+                var currentScrapCompositionComp = Comp<PhysicalCompositionComponent>(currentScrap);
+
+                //If it's on the whitelist for the reclaimer, and not on its blacklist.
+                if (_entityWhitelistSystem.CheckBoth(currentScrapUid, reclaimComp.Blacklist, reclaimComp.Whitelist))
+                {
+                    //for each material it produces, add it to the HashSet
+                    foreach (var (mat, _) in currentScrapCompositionComp.MaterialComposition)
+                    {
+                        ProtoId<MaterialPrototype> matAsProto = ProtoMan.Index<MaterialPrototype>(mat);
+                        producibleMaterials.Add(matAsProto);
+                    }
+                }
+
+                await Delete(currentScrapUid);
+            }
+        }
+
+        // Power the reclaimer
+        await SpawnEntity(ApcId, SEntMan.GetCoordinates(TargetCoords));
+        await RunTicks(1);
+        //Set reclaimer to enabled
+        await Server.WaitPost(() =>
+        {
+            _materialReclaimerSystem.SetReclaimerEnabled((EntityUid)STarget, true);
+        });
+
+        //Assert that reclaimer enabled
+        Assert.That(reclaimComp.Enabled, "The reclaimer did not get or stay enabled");
+
+        //put a floor tile down
+        await InteractUsing(FloorTileId);
+
+        using (Assert.EnterMultipleScope())
+        {
+            //For each producible Material, assert that it is not recyclable (and would thus cause a recycling loop)
+            foreach (ProtoId<MaterialPrototype> material in producibleMaterials)
+            {
+                var matStack = ProtoMan.Index(material).StackEntity;
+                Assert.That(
+                    matStack,
+                    Is.Not.Null,
+                    $"The material, {material}, did not have a stackentity associated with it. You may need to add a stackEntity to its Reagents/Materials yml file.");
+
+                var matInHands = await PlaceInHands(matStack);
+                var matInHandsUid = ToServer(matInHands);
+
+                //Assert we're holding material
+                Assert.That(
+                    HandSys.GetActiveItem((SPlayer, Hands)),
+                    Is.EqualTo(matInHandsUid),
+                    $"The material, {matStack}, never got put in our hands.");
+
+                await Interact();
+
+                //Assert Hands not empty
+                Assert.That(
+                    HandSys.GetActiveItem((SPlayer, Hands)),
+                    Is.Not.Null,
+                    $"The material that should not have been reclaimed, {matStack}, is no longer in our hands. The reclaimer was {reclaimerId}");
+            }
+        }
+    }
+}
