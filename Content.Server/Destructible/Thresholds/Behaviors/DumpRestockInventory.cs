8cc49ef8fdec49dd693db503dabd423786caacb1
Fenriz <kastonbag552@gmail.com>
2026-07-29 14:34:11 +0300

    fix

--- Content.Server/Destructible/Thresholds/Behaviors/DumpRestockInventory.cs
+++ Content.Server/Destructible/Thresholds/Behaviors/DumpRestockInventory.cs
@@ -1,6 +1,5 @@
 using Robust.Shared.Random;
 using Content.Shared.Stacks;
-using Content.Shared.Prototypes;
 using Content.Shared.VendingMachines;
 using Content.Shared.VendingMachines.Components;
 
