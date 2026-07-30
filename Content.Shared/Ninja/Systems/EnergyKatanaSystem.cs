20e632d7ef374a3dd095ca880dcaf7f902d13c5a
Fenriz <kastonbag552@gmail.com>
2026-07-30 13:30:10 +0300

    katana use ActionGrant/ItemActionGrant

--- Content.Shared/Ninja/Systems/EnergyKatanaSystem.cs
+++ Content.Shared/Ninja/Systems/EnergyKatanaSystem.cs
@@ -3,33 +3,13 @@ using Content.Shared.Ninja.Components;
 
 namespace Content.Shared.Ninja.Systems;
 
-/// <summary>
-/// System for katana binding and dash events. Recalling is handled by the suit.
-/// </summary>
 public sealed partial class EnergyKatanaSystem : EntitySystem
 {
     [Dependency] private SharedSpaceNinjaSystem _ninja = default!;
 
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SubscribeLocalEvent<EnergyKatanaComponent, GotEquippedEvent>(OnEquipped);
-        SubscribeLocalEvent<EnergyKatanaComponent, CheckDashEvent>(OnCheckDash);
-    }
-
-    /// <summary>
-    /// When equipped by a ninja, try to bind it.
-    /// </summary>
+    [SubscribeLocalEvent]
     private void OnEquipped(Entity<EnergyKatanaComponent> ent, ref GotEquippedEvent args)
     {
         _ninja.BindKatana(args.EquipTarget, ent);
-    }
-
-    private void OnCheckDash(Entity<EnergyKatanaComponent> ent, ref CheckDashEvent args)
-    {
-        // Just use a whitelist fam
-        if (!_ninja.IsNinja(args.User))
-            args.Cancelled = true;
     }
 }
