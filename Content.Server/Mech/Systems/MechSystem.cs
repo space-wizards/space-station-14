41ee1df27d8e6f80bd72d8b100f05387919e2f71
NopeOfAllNopes <78011942+NopeOfAllNopes@users.noreply.github.com>
2026-07-30 02:10:00 -0700

    Added line spaces back to maintain formatting.

--- Content.Server/Mech/Systems/MechSystem.cs
+++ Content.Server/Mech/Systems/MechSystem.cs
@@ -213,6 +213,7 @@ public sealed partial class MechSystem : SharedMechSystem
             _popup.PopupEntity(Loc.GetString("mech-no-enter", ("item", uid)), Identity.Entity(args.User, EntityManager));
             return;
         }
+
         TryInsert(uid, args.User, component);
         args.Handled = true;
     }
@@ -222,8 +223,10 @@ public sealed partial class MechSystem : SharedMechSystem
     {
         if (args.Cancelled || args.Handled)
             return;
+
         if (!TryEject(uid, component))
             return;
+
         args.Handled = true;
     }
 
