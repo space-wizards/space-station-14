5c1ceea7dcbded107648ffb8a80cb60dbfe53b09
riccardi48 <richardballard00@gmail.com>
2026-07-30 13:42:41 +0100

    rename

--- Content.Server/Cards/CardSystem.cs
+++ Content.Server/Cards/CardSystem.cs
@@ -8,7 +8,7 @@ namespace Content.Server.Cards;
 public sealed partial class CardSystem : SharedCardSystem
 {
     // Server-side index counter. Needed so that all cards have unique ids. Basically a EntityUid but for cards.
-    private int _inxCounter = 0;
+    private int _indexCounter = 0;
 
     protected override void OnCardsInit(Entity<CardsComponent> ent, ref ComponentInit args)
     {
@@ -18,8 +18,8 @@ public sealed partial class CardSystem : SharedCardSystem
             var card = ent.Comp.Cards[i];
             if (card.CardIndex != 0)
                 continue;
-            card.CardIndex = _inxCounter;
-            _inxCounter++;
+            card.CardIndex = _indexCounter;
+            _indexCounter++;
             ent.Comp.Cards[i] = card;
         }
         Dirty(ent.Owner, ent.Comp);
