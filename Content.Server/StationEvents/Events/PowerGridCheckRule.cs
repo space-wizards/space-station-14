79ff81c71d6e15e94ebfd4af1f152206f5383c80
Krunklehorn <krunklehornvector@gmail.com>
2026-07-31 09:53:33 -0400

    Use GetLargestGrid

--- Content.Server/StationEvents/Events/PowerGridCheckRule.cs
+++ Content.Server/StationEvents/Events/PowerGridCheckRule.cs
@@ -4,6 +4,7 @@ using Content.Server.Power.EntitySystems;
 using Content.Server.Station.Components;
 using Content.Server.StationEvents.Components;
 using Content.Shared.GameTicking.Components;
+using Content.Shared.Station;
 using Content.Shared.Station.Components;
 using JetBrains.Annotations;
 using Robust.Shared.Audio;
@@ -18,6 +19,7 @@ namespace Content.Server.StationEvents.Events
     public sealed partial class PowerGridCheckRule : StationEventSystem<PowerGridCheckRuleComponent>
     {
         [Dependency] private ApcSystem _apcSystem = default!;
+        [Dependency] private SharedStationSystem _stationSystem = default!;
 
         public override void Initialize()
         {
@@ -35,16 +37,21 @@ namespace Content.Server.StationEvents.Events
 
             component.AffectedStation = chosenStation.Value;
 
+            var largestGrid = _stationSystem.GetLargestGrid(chosenStation.Value);
+
+            if (largestGrid == null)
+                return;
+
             var query = AllEntityQuery<ApcComponent, TransformComponent>();
             while (query.MoveNext(out var apcUid, out var apc, out var transform))
             {
                 if (!apc.MainBreakerEnabled)
                     continue;
 
-                if (!TryComp<GridEventEligibleComponent>(transform.GridUid, out var gridEventEligibleComp) || !gridEventEligibleComp.PowerGridChecks)
+                if (CompOrNull<StationMemberComponent>(transform.GridUid)?.Station != chosenStation)
                     continue;
 
-                if (CompOrNull<StationMemberComponent>(transform.GridUid)?.Station != chosenStation)
+                if (transform.GridUid != largestGrid.Value)
                     continue;
 
                 component.Powered.Add(apcUid);
@@ -90,11 +97,6 @@ namespace Content.Server.StationEvents.Events
                 return null;
             }
 
-            if (!TryComp<GridEventEligibleComponent>(xform.GridUid, out var gridEventEligibleComp) || !gridEventEligibleComp.PowerGridChecks)
-            {
-                return null;
-            }
-
             if (!TryComp<StationMemberComponent>(xform.GridUid, out var stationMemberComp))
             {
                 return null;
@@ -103,10 +105,18 @@ namespace Content.Server.StationEvents.Events
             var activeRules = AllEntityQuery<PowerGridCheckRuleComponent, ActiveGameRuleComponent>();
             while (activeRules.MoveNext(out var _entity, out var powerGridRule, out var _activeGameRule))
             {
-                if (stationMemberComp.Station == powerGridRule.AffectedStation)
-                {
-                    return powerGridRule;
-                }
+                if (stationMemberComp.Station != powerGridRule.AffectedStation)
+                    continue;
+
+                var largestGrid = _stationSystem.GetLargestGrid(powerGridRule.AffectedStation);
+
+                if (largestGrid == null)
+                    continue;
+
+                if (xform.GridUid != largestGrid.Value)
+                    continue;
+
+                return powerGridRule;
             }
 
             return null;
