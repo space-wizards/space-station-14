254910c66990b3c1fd891be0d056653d44d8c3e3
Krunklehorn <krunklehornvector@gmail.com>
2026-07-31 09:47:45 -0400

    Revert GridEventEligible

--- Content.Server/Station/Components/StationEventEligibleComponent.cs
+++ Content.Server/Station/Components/StationEventEligibleComponent.cs
@@ -1,23 +1,10 @@
 ﻿namespace Content.Server.Station.Components;
 
 /// <summary>
-/// Marks a station for event eligibility.
+/// This is used for event eligibility.
 /// </summary>
 [RegisterComponent]
 public sealed partial class StationEventEligibleComponent : Component
 {
 
-}
-
-/// <summary>
-/// Marks a grid, allowing events to configure their behavior for specific grids of a station.
-/// </summary>
-[RegisterComponent]
-public sealed partial class GridEventEligibleComponent : Component
-{
-    /// <summary>
-    /// See <see cref="PowerGridCheckRule"/>.
-    /// </summary>
-    [DataField, AutoNetworkedField]
-    public bool PowerGridChecks = true;
 }
