62ffe77fdb3668819da3a545f1e3a1e82829e1a4
MNX <56132549+Mehnix@users.noreply.github.com>
2026-08-02 16:12:30 +0100

    smol changes

--- Content.Shared/Xenoarchaeology/Artifact/XAT/Components/XATInteractAttackComponent.cs
+++ Content.Shared/Xenoarchaeology/Artifact/XAT/Components/XATInteractAttackComponent.cs
@@ -8,7 +8,7 @@ namespace Content.Shared.Xenoarchaeology.Artifact.XAT.Components;
 /// This is used for an artifact trigger that activates when an entity collides or is used to attack the artifact.
 /// EG: A user attacks the artifact whilst holding a knife. Or, a anomaly particle collides with the artifact.
 /// </summary>
-[RegisterComponent, NetworkedComponent, Access(typeof(XATInteractAttackSystem)), AutoGenerateComponentState(fieldDeltas: true)]
+[RegisterComponent, NetworkedComponent, Access(typeof(XATInteractAttackSystem)), AutoGenerateComponentState]
 public sealed partial class XATInteractAttackComponent : Component
 {
     /// <summary>
@@ -21,17 +21,17 @@ public sealed partial class XATInteractAttackComponent : Component
     /// Number of interactions required to trigger.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public MinMax InteractionCount = new(1, 4);
+    public MinMax InteractionCount = new(1, 1);
 
     /// <summary>
     /// Number of interactions required to trigger, set after initiation
     /// </summary>
-    [AutoNetworkedField]
-    public int MaxCount = 0;
+    [DataField, AutoNetworkedField]
+    public int? MaxCount;
 
     /// <summary>
     /// Number of interactions to go.
     /// </summary>
-    [AutoNetworkedField]
-    public int Count = 0;
+    [DataField, AutoNetworkedField]
+    public int? Count;
 }
--- Content.Shared/Xenoarchaeology/Artifact/XAT/XATInteractAttackSystem.cs
+++ Content.Shared/Xenoarchaeology/Artifact/XAT/XATInteractAttackSystem.cs
@@ -1,4 +1,5 @@
 using Content.Shared.Item.ItemToggle.Components;
+using Content.Shared.Popups;
 using Content.Shared.Weapons.Melee.Events;
 using Content.Shared.Weapons.Ranged.Events;
 using Content.Shared.Weapons.Ranged.Components;
@@ -16,13 +17,13 @@ namespace Content.Shared.Xenoarchaeology.Artifact.XAT;
 /// </summary>
 public sealed partial class XATInteractAttackSystem : BaseXATSystem<XATInteractAttackComponent>
 {
+    [Dependency] private SharedPopupSystem _popup = default!;
     [Dependency] private IRobustRandom _random = default!;
     [Dependency] private EntityWhitelistSystem _whitelistSystem = default!;
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<XATInteractAttackComponent, MapInitEvent>(OnMapInit);
         XATSubscribeDirectEvent<StartCollideEvent>(OnStartCollide);
         XATSubscribeDirectEvent<AttackedEvent>(OnAttacked);
         XATSubscribeDirectEvent<HitScanReflectAttemptEvent>(OnHitscan);
@@ -31,11 +32,10 @@ public sealed partial class XATInteractAttackSystem : BaseXATSystem<XATInteractA
     /// <summary>
     /// Randomly decide initial count of interactions for node.
     /// </summary>
+    [SubscribeLocalEvent]
     private void OnMapInit(Entity<XATInteractAttackComponent> ent, ref MapInitEvent args)
     {
-        ent.Comp.MaxCount = ent.Comp.InteractionCount.Next(_random); //randomly decide count to decrement.
-        ent.Comp.Count = ent.Comp.MaxCount; //define count amount.
-        Dirty(ent);
+        SetMaxCount(ent);
     }
 
     /// <summary>
@@ -43,10 +43,10 @@ public sealed partial class XATInteractAttackSystem : BaseXATSystem<XATInteractA
     /// </summary>
     private void OnAttacked(Entity<XenoArtifactComponent> artifact, Entity<XATInteractAttackComponent, XenoArtifactNodeComponent> node, ref AttackedEvent args)
     {
-        if (!TryComp<ItemToggleComponent>(args.Used, out var itemComp) || itemComp.Activated == false) //make sure it's on
+        if (!TryComp<ItemToggleComponent>(args.Used, out var itemComp) || !itemComp.Activated) //make sure it's on
             return;
 
-        if (_whitelistSystem.IsWhitelistPassOrNull(node.Comp1.Whitelist, args.Used) && TriggerCountdown(node) == true)
+        if (_whitelistSystem.IsWhitelistPassOrNull(node.Comp1.Whitelist, args.Used) && TriggerCountdown(node, artifact.Owner, args.User))
             Trigger(artifact, node);
     }
 
@@ -55,7 +55,7 @@ public sealed partial class XATInteractAttackSystem : BaseXATSystem<XATInteractA
     /// </summary>
     private void OnStartCollide(Entity<XenoArtifactComponent> artifact, Entity<XATInteractAttackComponent, XenoArtifactNodeComponent> node, ref StartCollideEvent args)
     {
-        if (_whitelistSystem.IsWhitelistPassOrNull(node.Comp1.Whitelist, args.OtherEntity) && TriggerCountdown(node) == true)
+        if (_whitelistSystem.IsWhitelistPassOrNull(node.Comp1.Whitelist, args.OtherEntity) && TriggerCountdown(node, artifact.Owner, args.OtherEntity))
             Trigger(artifact, node);
     }
 
@@ -67,26 +67,47 @@ public sealed partial class XATInteractAttackSystem : BaseXATSystem<XATInteractA
         if (!TryComp<BatteryAmmoProviderComponent>(args.SourceItem, out var batteryComp))
             return;
 
-        if (_whitelistSystem.IsWhitelistPassOrNull(node.Comp1.Whitelist, batteryComp.Prototype) && TriggerCountdown(node))
+        if (_whitelistSystem.IsWhitelistPassOrNull(node.Comp1.Whitelist, batteryComp.Prototype) && TriggerCountdown(node, artifact.Owner, args.Shooter))
             Trigger(artifact, node);
     }
 
     /// <summary>
-    /// Count down the number of interactions needed to trigger
+    /// Count down the number of interactions needed to trigger.
     /// <returns>true if enough interactions have been made, false if not</returns>
-    private bool TriggerCountdown(Entity<XATInteractAttackComponent> node)
+    private bool TriggerCountdown(Entity<XATInteractAttackComponent> ent, EntityUid artifact, EntityUid? user)
     {
-        if (node.Comp.Count <= 1)
+        if (ent.Comp.MaxCount == null || ent.Comp.Count == null) //ensure countdown isn't null
+            SetMaxCount(ent);
+
+        ent.Comp.Count--;
+
+        if (ent.Comp.Count > 0)
         {
-            node.Comp.Count = node.Comp.MaxCount;
-            Dirty(node);
-            return true;
+            _popup.PopupEntity(Loc.GetString("interact-actifact-more"), artifact, user);
+            Dirty(ent);
+            return false;
         }
         else
         {
-            node.Comp.Count--;
-            Dirty(node);
-            return false;
+            ent.Comp.Count = ent.Comp.MaxCount;
+            Dirty(ent);
+            return true;
         }
+    }
+
+    /// <summary>
+    /// Sets the maximum value to count down to a random number if not otherwise defined
+    /// Sets the count value if not defined
+    /// </summary>
+    /// <param name="ent"></param>
+    private void SetMaxCount(Entity<XATInteractAttackComponent> ent)
+    {
+        if (ent.Comp.MaxCount == null)
+            ent.Comp.MaxCount = ent.Comp.InteractionCount.Next(_random); // randomly decide count to decrement.
+
+        if (ent.Comp.Count == null)
+            ent.Comp.Count = ent.Comp.MaxCount.Value; // define count amount.
+
+        Dirty(ent);
     }
 }
