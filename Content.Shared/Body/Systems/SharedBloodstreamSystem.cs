9e8b0e7fab920d9ba29d1129f228599aa83cc46c
Prole <172158352+Prole0@users.noreply.github.com>
2026-08-02 21:58:26 -0700

    maybe?

--- Content.Shared/Body/Systems/SharedBloodstreamSystem.cs
+++ Content.Shared/Body/Systems/SharedBloodstreamSystem.cs
@@ -27,7 +27,7 @@ using Robust.Shared.Timing;
 
 namespace Content.Shared.Body.Systems;
 
-public abstract partial class SharedBloodstreamSystem : EntitySystem
+public partial class SharedBloodstreamSystem : EntitySystem
 {
     public static readonly EntProtoId Bloodloss = "StatusEffectBloodloss";
 
