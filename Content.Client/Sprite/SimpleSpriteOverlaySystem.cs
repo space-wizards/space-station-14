0337ae243c780a4b08e126d8f8044319a6d10e66
beck-thompson <beck314159@hotmail.com>
2026-08-03 21:14:18 -0700

    Fixes

---
+++ Content.Client/Sprite/SimpleSpriteOverlaySystem.cs
@@ -0,0 +1,37 @@
+﻿using Content.Shared.Sprite;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.Sprite;
+
+public sealed class SimpleSpriteOverlaySystem : EntitySystem
+{
+    [Dependency] private readonly SpriteSystem _sprite = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<SimpleSpriteOverlayComponent, AfterAutoHandleStateEvent>(OnAfterHandleState);
+        SubscribeLocalEvent<SimpleSpriteOverlayComponent, ComponentShutdown>(OnCompShutdown);
+    }
+
+
+    private void OnAfterHandleState(Entity<SimpleSpriteOverlayComponent> ent, ref AfterAutoHandleStateEvent args)
+    {
+        var sprite = Comp<SpriteComponent>(ent);
+
+        var index = _sprite.LayerMapReserve((ent.Owner, sprite), ent.Comp.LayerMap);
+
+        _sprite.LayerSetSprite((ent.Owner, sprite), index, ent.Comp.OverlaySprite);
+        _sprite.LayerSetVisible((ent.Owner, sprite), index, true);
+
+        if (ent.Comp.Shader is not null)
+            sprite.LayerSetShader(index, ent.Comp.Shader);
+    }
+
+    private void OnCompShutdown(Entity<SimpleSpriteOverlayComponent> ent, ref ComponentShutdown args)
+    {
+        var sprite = Comp<SpriteComponent>(ent);
+
+        if (_sprite.LayerMapTryGet((ent.Owner, sprite), ent.Comp.LayerMap, out var index, true))
+            _sprite.LayerSetVisible((ent.Owner, sprite), index, false);
+    }
+}
---
+++ Content.Shared/Sprite/SimpleSpriteOverlayComponent.cs
@@ -0,0 +1,29 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Sprite;
+
+/// <summary>
+/// Add a simple overlay to an entity
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
+public sealed partial class SimpleSpriteOverlayComponent : Component
+{
+    /// <summary>
+    /// Rsi of the sprite we want to overlay.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public SpriteSpecifier OverlaySprite;
+
+    /// <summary>
+    /// The key of the entity layer into which the sprite will be inserted.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public string LayerMap = "simple_visual_overlay";
+
+    /// <summary>
+    /// The shader (if any) that will be applied to the sprite layer.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public string? Shader;
+}
