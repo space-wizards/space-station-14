477a45d4a283ec367eac8561bb42fdf7f7c7d969
Pok245 <113675512+Pok27@users.noreply.github.com>
2026-08-07 14:43:03 +0300

    Update PlantTraitsComponent.cs

--- Content.Shared/Botany/Traits/Components/PlantTraitsComponent.cs
+++ Content.Shared/Botany/Traits/Components/PlantTraitsComponent.cs
@@ -1,11 +1,8 @@
-using Robust.Shared.GameStates;
-
 namespace Content.Shared.Botany.Traits.Components;
 
 /// <summary>
-/// Base component for plant trait components.
+/// Base class for plant trait components.
 /// </summary>
-[RegisterComponent, NetworkedComponent]
 public abstract partial class PlantTraitsComponent : Component
 {
     /// <summary>
