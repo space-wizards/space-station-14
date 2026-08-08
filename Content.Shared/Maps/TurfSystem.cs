bf21b8eec5fcf0dfe06ba1b05aaa68f12d52ff20
metalgearsloth <comedian_vs_clown@hotmail.com>
2026-08-08 16:49:12 +1000

    Update engine to 288.0.0 and fix tests.

--- Content.Shared/Maps/TurfSystem.cs
+++ Content.Shared/Maps/TurfSystem.cs
@@ -1,4 +1,3 @@
-using System.Buffers;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
 using System.Linq;
@@ -25,7 +24,7 @@ public sealed partial class TurfSystem : EntitySystem
 
     [Dependency] private EntityQuery<FixturesComponent> _fixtureQuery = default!;
 
-    private bool[] _tileHasMapAtmosphere = ArrayPool<bool>.Shared.Rent(0);
+    private bool[] _tileHasMapAtmosphere = [];
 
     public override void Initialize()
     {
@@ -54,9 +53,7 @@ public sealed partial class TurfSystem : EntitySystem
             maxTileId = Math.Max(maxTileId, tileDef.TileId);
         }
 
-        ArrayPool<bool>.Shared.Return(_tileHasMapAtmosphere);
-        var cache = ArrayPool<bool>.Shared.Rent(maxTileId + 1);
-        Array.Clear(cache);
+        var cache = new bool[maxTileId + 1];
 
         foreach (var tileDef in _tileDefinitions)
         {
@@ -73,7 +70,6 @@ public sealed partial class TurfSystem : EntitySystem
     {
         base.Shutdown();
 
-        ArrayPool<bool>.Shared.Return(_tileHasMapAtmosphere);
         _tileHasMapAtmosphere = [];
     }
 
