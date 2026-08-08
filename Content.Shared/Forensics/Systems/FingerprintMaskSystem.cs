39f54ed17ba6ffaeb8b6f2cc24d3454a4612e844
Sir Warock <67167466+SirWarock@users.noreply.github.com>
2026-08-08 03:31:16 +0200

    whatson the absolute gigachad found the problem
    Co-Authored-By: Whatstone <166147148+whatston3@users.noreply.github.com>

--- Content.Shared/Forensics/Systems/FingerprintMaskSystem.cs
+++ Content.Shared/Forensics/Systems/FingerprintMaskSystem.cs
@@ -4,7 +4,7 @@ using Content.Shared.Inventory;
 
 namespace Content.Shared.Forensics.Systems;
 
-public sealed class FingerprintMaskSystem : EntitySystem
+public sealed partial class FingerprintMaskSystem : EntitySystem
 {
     [SubscribeLocalEvent]
     private void OnTryAccessFingerprint(Entity<FingerprintMaskComponent> gloves, ref InventoryRelayedEvent<TryAccessFingerprintEvent> args)
