37853beee97d6a35ef4560c8df0870dfc0ed6d08
Whatstone <whatston3@gmail.com>
2026-08-10 17:24:39 -0400

    copy all the things

--- Content.Shared/Cloning/CloningContext.cs
+++ Content.Shared/Cloning/CloningContext.cs
@@ -20,14 +20,7 @@ namespace Content.Shared.Cloning;
 /// </summary>
 public sealed partial class CloningContext :
     ISerializationContext,
-    ITypeCopier<BloodstreamComponent>,
-    ITypeCopier<InventoryComponent>,
-    ITypeCopier<JumpAbilityComponent>,
-    ITypeCopier<PullerComponent>,
-    ITypeCopier<RootableComponent>,
-    ITypeCopier<SericultureComponent>,
-    ITypeCopier<StorageComponent>,
-    ITypeCopier<WaggingComponent>
+    ITypeCopier<Component>
 {
     /// <inheritdoc />
     public SerializationManager.SerializerProvider SerializerProvider { get; }
@@ -43,133 +36,52 @@ public sealed partial class CloningContext :
         SerializerProvider.RegisterSerializer(this);
     }
 
-    #region Bloodstream
     public void CopyTo(
         ISerializationManager serializationManager,
-        BloodstreamComponent source,
-        ref BloodstreamComponent target,
+        Component source,
+        ref Component target,
         IDependencyCollection dependencies,
         SerializationHookContext hookCtx,
         ISerializationContext? context = null)
     {
         serializationManager.CopyTo(source, ref target, notNullableOverride: true);
 
-        target.NextUpdate = TimeSpan.Zero;
-        target.BleedAmount = 0.0f;
-        target.BloodData = null;
-        target.BloodSolution = null;
-        target.TemporarySolution = null;
-        target.MetabolitesSolution = null;
+        switch (target)
+        {
+            case BloodstreamComponent bloodTarget:
+                bloodTarget.NextUpdate = TimeSpan.Zero;
+                bloodTarget.BleedAmount = 0.0f;
+                bloodTarget.BloodData = null;
+                bloodTarget.BloodSolution = null;
+                bloodTarget.TemporarySolution = null;
+                bloodTarget.MetabolitesSolution = null;
+                break;
+            case InventoryComponent invTarget:
+                invTarget.Slots = Array.Empty<SlotDefinition>();
+                invTarget.Containers = Array.Empty<ContainerSlot>();
+                break;
+            case JumpAbilityComponent jumpTarget:
+                jumpTarget.ActionEntity = null;
+                break;
+            case PullerComponent pullTarget:
+                pullTarget.Pulling = null;
+                pullTarget.NextThrow = TimeSpan.Zero;
+                break;
+            case RootableComponent rootTarget:
+                rootTarget.ActionEntity = null;
+                rootTarget.PuddleEntity = null;
+                break;
+            case SericultureComponent seriTarget:
+                seriTarget.ActionEntity = null;
+                break;
+            case StorageComponent storageTarget:
+                storageTarget.StoredItems.Clear();
+                storageTarget.SavedLocations.Clear();
+                break;
+            case WaggingComponent wagTarget:
+                wagTarget.ActionEntity = null;
+                wagTarget.Wagging = false;
+                break;
+        }
     }
-    #endregion Bloodstream
-
-    #region Inventory
-    public void CopyTo(
-        ISerializationManager serializationManager,
-        InventoryComponent source,
-        ref InventoryComponent target,
-        IDependencyCollection dependencies,
-        SerializationHookContext hookCtx,
-        ISerializationContext? context = null)
-    {
-        serializationManager.CopyTo(source, ref target, notNullableOverride: true);
-
-        target.Slots = Array.Empty<SlotDefinition>();
-        target.Containers = Array.Empty<ContainerSlot>();
-    }
-    #endregion Inventory
-
-    #region JumpAbility
-    public void CopyTo(
-        ISerializationManager serializationManager,
-        JumpAbilityComponent source,
-        ref JumpAbilityComponent target,
-        IDependencyCollection dependencies,
-        SerializationHookContext hookCtx,
-        ISerializationContext? context = null)
-    {
-        serializationManager.CopyTo(source, ref target, notNullableOverride: true);
-
-        target.ActionEntity = null;
-    }
-    #endregion JumpAbility
-
-    #region Puller
-    public void CopyTo(
-        ISerializationManager serializationManager,
-        PullerComponent source,
-        ref PullerComponent target,
-        IDependencyCollection dependencies,
-        SerializationHookContext hookCtx,
-        ISerializationContext? context = null)
-    {
-        serializationManager.CopyTo(source, ref target, notNullableOverride: true);
-
-        target.Pulling = null;
-        target.NextThrow = TimeSpan.Zero;
-    }
-    #endregion Puller
-
-    #region Rootable
-    public void CopyTo(
-        ISerializationManager serializationManager,
-        RootableComponent source,
-        ref RootableComponent target,
-        IDependencyCollection dependencies,
-        SerializationHookContext hookCtx,
-        ISerializationContext? context = null)
-    {
-        serializationManager.CopyTo(source, ref target, notNullableOverride: true);
-
-        target.ActionEntity = null;
-        target.PuddleEntity = null;
-    }
-    #endregion Rootable
-
-    #region Sericulture
-    public void CopyTo(
-        ISerializationManager serializationManager,
-        SericultureComponent source,
-        ref SericultureComponent target,
-        IDependencyCollection dependencies,
-        SerializationHookContext hookCtx,
-        ISerializationContext? context = null)
-    {
-        serializationManager.CopyTo(source, ref target, notNullableOverride: true);
-
-        target.ActionEntity = null;
-    }
-    #endregion Sericulture
-
-    #region Storage
-    public void CopyTo(
-        ISerializationManager serializationManager,
-        StorageComponent source,
-        ref StorageComponent target,
-        IDependencyCollection dependencies,
-        SerializationHookContext hookCtx,
-        ISerializationContext? context = null)
-    {
-        serializationManager.CopyTo(source, ref target, notNullableOverride: true);
-
-        target.StoredItems.Clear();
-        target.SavedLocations.Clear();
-    }
-    #endregion Storage
-
-    #region Wagging
-    public void CopyTo(
-        ISerializationManager serializationManager,
-        WaggingComponent source,
-        ref WaggingComponent target,
-        IDependencyCollection dependencies,
-        SerializationHookContext hookCtx,
-        ISerializationContext? context = null)
-    {
-        serializationManager.CopyTo(source, ref target, notNullableOverride: true);
-
-        target.ActionEntity = null;
-        target.Wagging = false;
-    }
-    #endregion Wagging
 }
