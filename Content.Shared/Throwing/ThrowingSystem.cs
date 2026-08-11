5c1997c525a1a1f4be168b784c8b172d2b1f5eff
Winkarst <74284083+Winkarst-cpu@users.noreply.github.com>
2026-08-11 21:08:43 +0000

    Fix TryThrow returns from #45279 (#45286)
    * fix
    
    * I'm dumb
    
    * submodule?
    
    ---------
    
    Co-authored-by: AJCM-git <ajcm_mail@disroot.org>

--- Content.Shared/Throwing/ThrowingSystem.cs
+++ Content.Shared/Throwing/ThrowingSystem.cs
@@ -225,7 +225,7 @@ public sealed partial class ThrowingSystem : EntitySystem
         }
 
         if (user == null)
-            return false;
+            return true;
 
         if (recoil)
             _recoil.KickCamera(user.Value, -direction * 0.04f);
@@ -234,12 +234,12 @@ public sealed partial class ThrowingSystem : EntitySystem
         if (pushbackRatio == 0.0f ||
             physics.Mass == 0f ||
             !TryComp(user.Value, out PhysicsComponent? userPhysics))
-            return false;
+            return true;
         var msg = new ThrowPushbackAttemptEvent();
         RaiseLocalEvent(uid, msg);
 
         if (msg.Cancelled)
-            return false;
+            return true;
 
         var pushEv = new ThrowerImpulseEvent();
         RaiseLocalEvent(user.Value, ref pushEv);
