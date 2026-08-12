12ceb08c1ef1748ba90ea59e11a4def69a2765ed
Roudenn <romabond091@gmail.com>
2026-08-12 09:23:21 +0300

    Update FaxMachineComponent.cs

--- Content.Shared/Fax/Components/FaxMachineComponent.cs
+++ Content.Shared/Fax/Components/FaxMachineComponent.cs
@@ -3,6 +3,7 @@ using Content.Shared.Paper;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.Fax.Components;
 
