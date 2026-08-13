d1a2e7f9d3e6b8297bffd9b8d5d6e2a8bad501db
Errant <35878406+Errant-4@users.noreply.github.com>
2026-08-13 16:09:35 +0200

    make command be able to target specific map, make system work on preinit maps

--- Content.Server/Administration/Commands/SatanCommand.cs
+++ Content.Server/Administration/Commands/SatanCommand.cs
@@ -20,14 +20,30 @@ public sealed partial class SatanCommand : IConsoleCommand
 
     public void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        if (args.Length != 0)
+        if (args.Length > 1)
         {
             shell.WriteError(Loc.GetString("shell-wrong-arguments-number"));
             return;
         }
 
+        MapId? map = null;
+
+        if (args.Length > 0)
+        {
+            if (!int.TryParse(args[0], out var intMapId))
+            {
+                shell.WriteError(Loc.GetString("cmd-mapping-failure-integer", ("arg", args[0])));
+                return;
+            }
+
+            map = new MapId(intMapId);
+        }
+
+
         var sat = _entManager.System<SharedSatanSystem>();
 
-        sat.AlignAll();
+        var response = sat.AlignAll(map);
+        if (!string.IsNullOrEmpty(response))
+            shell.WriteLine(response);
     }
 }
--- Content.Shared/Mapping/SharedSatanSystem.cs
+++ Content.Shared/Mapping/SharedSatanSystem.cs
@@ -1,5 +1,8 @@
 using Content.Shared.Examine;
 using Content.Shared.Pinpointer;
+using Robust.Shared;
+using Robust.Shared.Configuration;
+using Robust.Shared.Map;
 
 namespace Content.Shared.Mapping;
 
@@ -12,83 +15,112 @@ namespace Content.Shared.Mapping;
 /// </remarks>>
 public sealed partial class SharedSatanSystem : EntitySystem // StructureAlignmentTAgNonsmoothing
 {
+    [Dependency] private IConfigurationManager _cfg = default!;
+    [Dependency] private IEntityManager _entMan = default!;
     [Dependency] private SharedTransformSystem _trans = default!;
 
-    public override void Initialize() { }
+    private static bool _mint; //TODO:ERRANT rename
+
+    public override void Initialize()
+    {
+        // _cfg.OnValueChanged(CVars.SatanOnMapInit, _mint, true);
+    }
+
 
     //TODO:ERRANT For testing only
     [SubscribeLocalEvent]
     private void OnExamined(Entity<SatanAlignComponent> entity, ref ExaminedEvent args)
     {
-        Align(entity.AsNullable());
+
+            Align(entity.AsNullable());
+    }
+
+    [SubscribeLocalEvent]
+    private void OnMapInit(Entity<SatanAlignComponent> entity, ref MapInitEvent args)
+    {
+        if (_mint)
+            Align(entity.AsNullable());
     }
 
     /// <summary>
     /// Runs align on every alignable entity.
     /// This is very expensive and should never be ran after players are in the game!
     /// </summary>
-    public void AlignAll()
+    public string? AlignAll(MapId? map = null)
     {
-        var query = EntityQueryEnumerator<SatanAlignComponent, TransformComponent>();
+        // Needs to be an All Entity Query so it works on pre-init maps
+        var query = _entMan.AllEntityQueryEnumerator<SatanAlignComponent, TransformComponent>();
 
-        var i = 0; //TODO:ERRANT testing only
-        foreach (var (ent, comp, transform) in query)
+        var countAll = 0;
+        var countFixed = 0;
+
+        foreach (var (ent, comp, trans) in query)
         {
-            if (!transform.Anchored)
+            if (map is not null && trans.MapID != map)
                 continue;
 
-            Align((ent, comp));
-            i++;
+            if (Align((ent, comp)))
+                countFixed++;
+            countAll++;
         }
+
+        return ($"Found {countAll} alignable entities, of which {countFixed} were rotated.");  // TODO:ERRANT localize?
     }
 
     /// <summary>
     /// Makes the target entity align to adjacent compatible entities
     /// </summary>
     /// <remarks> Only works for entities which are symmetrical, so north-south and east-west headings are interchangeable</remarks>
-    private void Align(Entity<SatanAlignComponent?> entity)
+    private bool Align(Entity<SatanAlignComponent?> entity)
     {
         if (!Resolve(entity, ref entity.Comp))
-            return;
-
-        if (!TryComp<SatanKindComponent>(entity, out var kind))
-            return;
+            return false;
 
         var trans = Transform(entity); //TODO:ERRANT get this from the calling function
 
         if (!trans.Anchored)
-            return;
+            return false;
 
-        // var list = new List<EntityUid>();
+        if (!TryComp<SatanKindComponent>(entity, out var kind))
+            return false;
 
         // locate nearby walls and doors
-        var query = EntityQueryEnumerator<SatanKindComponent>();
+        var query = _entMan.AllEntityQueryEnumerator<SatanKindComponent>();
 
         var ns = 0d;
         var ew = 0d;
         var neighbors = 0;
 
-        if (!HasComp<NavMapDoorComponent>(entity))
+        if (!HasComp<NavMapDoorComponent>(entity)) //TODO:ERRANT testing only
         {
             var a = entity.Owner;
         }
 
         foreach (var (ent, comp) in query)
         {
-            var t = Transform(ent);
+            if (entity.Owner == ent)
+                continue;
 
             if (kind.AlignType != comp.AlignType)
                 continue;
 
+            var t = Transform(ent);
+
             // They must be anchored to the same parent to matter
             if (t.ParentUid != trans.ParentUid)
                 continue;
+
             if (!t.Anchored)
                 continue;
 
             trans.Coordinates.TryDistance(EntityManager, t.Coordinates, out var dist); //TODO:ERRANT use _trans.InRange ?
             if (dist > entity.Comp.ProximityMax || dist < entity.Comp.ProximityMin)
                 continue;
+
+            var inMinRange = _trans.InRange(t.Coordinates, trans.Coordinates, entity.Comp.ProximityMax);
+            var inMaxRange = _trans.InRange(t.Coordinates, trans.Coordinates, entity.Comp.ProximityMin);
+            // if(!(inMaxRange && !inMinRange))
+            //     continue;
 
             neighbors++;
 
@@ -109,7 +141,7 @@ public sealed partial class SharedSatanSystem : EntitySystem // StructureAlignme
 
         if (ew == ns) //TODO:ERRANT fix this shit
         {
-            Log.Warning($"Airlock {entity} was not rotated");
+            // Log.Warning($"Airlock {entity} was not rotated");
         }
 
 
@@ -122,10 +154,11 @@ public sealed partial class SharedSatanSystem : EntitySystem // StructureAlignme
             && !MathHelper.CloseTo(locRot, targetAngle + Angle.FromDegrees(180), 0.01f))
         {
             _trans.SetLocalRotation(entity, trans.LocalRotation + Angle.FromDegrees(90));
+
+            Log.Debug($"Satan aligned entity: {entity.Owner}"); //TODO:ERRANT testing only
+            return true;
         }
-        else
-        {
-            // _popup.PopupEntity("Rotation ok", entity);
-        }
+
+        return false;
     }
 }
