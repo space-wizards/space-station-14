7389733a0f55c0e19ecc93d387b958bf96c2e183
Tayrtahn <tayrtahn@gmail.com>
2026-08-13 10:18:25 +0000

    Fix `GuidebookDataSystem` late MsgEntity warning (#45298)
    Send GuidebookData automatically to new clients.

--- Content.Shared/Guidebook/Events.cs
+++ Content.Shared/Guidebook/Events.cs
@@ -3,14 +3,8 @@ using Robust.Shared.Serialization;
 namespace Content.Shared.Guidebook;
 
 /// <summary>
-/// Raised by the client on GuidebookDataSystem Initialize to request a
-/// full set of guidebook data from the server.
-/// </summary>
-[Serializable, NetSerializable]
-public sealed class RequestGuidebookDataEvent : EntityEventArgs { }
-
-/// <summary>
-/// Raised by the server at a specific client in response to <see cref="RequestGuidebookDataEvent"/>.
+/// Sends all extracted prototype data needed by GuidebookDataSystem.
+/// Raised by the server directed at newly-connected clients.
 /// Also raised by the server at ALL clients when prototype data is hot-reloaded.
 /// </summary>
 [Serializable, NetSerializable]
