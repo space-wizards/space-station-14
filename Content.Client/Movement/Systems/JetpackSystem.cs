6c7d27d48fec90540783bf071337792327bc805e
Fenriz <kastonbag552@gmail.com>
2026-08-13 23:48:17 +0300

    upd SubscribeLocalEvent

--- Content.Client/Movement/Systems/JetpackSystem.cs
+++ Content.Client/Movement/Systems/JetpackSystem.cs
@@ -10,18 +10,13 @@ public sealed partial class JetpackSystem : SharedJetpackSystem
 {
     [Dependency] private ClothingSystem _clothing = default!;
 
-    public override void Initialize()
-    {
-        base.Initialize();
-        SubscribeLocalEvent<JetpackComponent, AppearanceChangeEvent>(OnJetpackAppearance);
-    }
-
     protected override bool CanEnable(EntityUid uid, JetpackComponent component)
     {
         // No predicted atmos so you'd have to do a lot of funny to get this working.
         return false;
     }
 
+    [SubscribeLocalEvent]
     private void OnJetpackAppearance(EntityUid uid, JetpackComponent component, ref AppearanceChangeEvent args)
     {
         Appearance.TryGetData<bool>(uid, JetpackVisuals.Enabled, out var enabled, args.Component);
