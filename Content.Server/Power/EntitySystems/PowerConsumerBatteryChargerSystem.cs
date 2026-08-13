1dbcaf1b73b5cca329c69f45c045ed9bf109487f
ScarKy0 <scarky0@onet.eu>
2026-08-13 02:14:17 +0200

    cleanup

--- Content.Server/Power/EntitySystems/PowerConsumerBatteryChargerSystem.cs
+++ Content.Server/Power/EntitySystems/PowerConsumerBatteryChargerSystem.cs
@@ -9,10 +9,13 @@ public sealed partial class PowerConsumerBatteryChargerSystem : EntitySystem
 
     public override void Update(float frameTime)
     {
-        var query = EntityQueryEnumerator<PowerConsumerComponent, PowerConsumerBatteryChargerComponent, BatteryComponent>();
+        var query = EntityQueryEnumerator<PowerConsumerComponent, PowerConsumerBatteryChargerComponent, BatteryComponent, TransformComponent>();
 
-        while (query.MoveNext(out var uid, out var powerConsumer, out var batteryCharger, out _))
+        while (query.MoveNext(out var uid, out var powerConsumer, out var batteryCharger, out _, out var transformComp))
         {
+            if (!transformComp.Anchored)
+                continue;
+
             var powerConsumed = powerConsumer.ReceivedPower * frameTime;
             _battery.ChangeCharge(uid, powerConsumed * batteryCharger.Efficiency);
         }
