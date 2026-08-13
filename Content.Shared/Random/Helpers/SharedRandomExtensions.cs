a5ad3c1bb4454442dea09edf2ad7549081cdb3e9
SnappingOpossum <snappingopossum@outlook.com>
2026-08-13 14:31:46 +1000

    Filescope namespace

--- Content.Shared/Random/Helpers/SharedRandomExtensions.cs
+++ Content.Shared/Random/Helpers/SharedRandomExtensions.cs
@@ -6,228 +6,227 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
 
-namespace Content.Shared.Random.Helpers
+namespace Content.Shared.Random.Helpers;
+
+public static class SharedRandomExtensions
 {
-    public static class SharedRandomExtensions
+    public static string Pick(this IRobustRandom random, DatasetPrototype prototype)
     {
-        public static string Pick(this IRobustRandom random, DatasetPrototype prototype)
+        return random.Pick(prototype.Values);
+    }
+
+    /// <summary>
+    /// Randomly selects an entry from <paramref name="prototype"/>, attempts to localize it, and returns the result.
+    /// </summary>
+    public static string Pick(this IRobustRandom random, LocalizedDatasetPrototype prototype)
+    {
+        var index = random.Next(prototype.Values.Count);
+        return Loc.GetString(prototype.Values[index]);
+    }
+
+    public static string Pick(this IWeightedRandomPrototype prototype, IRobustRandom? random = null)
+    {
+        IoCManager.Resolve(ref random);
+        var picks = prototype.Weights;
+        var sum = picks.Values.Sum();
+        var accumulated = 0f;
+
+        var rand = random.NextFloat() * sum;
+
+        foreach (var (key, weight) in picks)
         {
-            return random.Pick(prototype.Values);
-        }
+            accumulated += weight;
 
-        /// <summary>
-        /// Randomly selects an entry from <paramref name="prototype"/>, attempts to localize it, and returns the result.
-        /// </summary>
-        public static string Pick(this IRobustRandom random, LocalizedDatasetPrototype prototype)
-        {
-            var index = random.Next(prototype.Values.Count);
-            return Loc.GetString(prototype.Values[index]);
-        }
-
-        public static string Pick(this IWeightedRandomPrototype prototype, IRobustRandom? random = null)
-        {
-            IoCManager.Resolve(ref random);
-            var picks = prototype.Weights;
-            var sum = picks.Values.Sum();
-            var accumulated = 0f;
-
-            var rand = random.NextFloat() * sum;
-
-            foreach (var (key, weight) in picks)
+            if (accumulated >= rand)
             {
-                accumulated += weight;
-
-                if (accumulated >= rand)
-                {
-                    return key;
-                }
+                return key;
             }
-
-            // Shouldn't happen
-            throw new InvalidOperationException($"Invalid weighted pick for {prototype.ID}!");
         }
 
-        public static ProtoId<T> Pick<T>(this IWeightedRandomPrototype<T> prototype, IRobustRandom? random = null)
+        // Shouldn't happen
+        throw new InvalidOperationException($"Invalid weighted pick for {prototype.ID}!");
+    }
+
+    public static ProtoId<T> Pick<T>(this IWeightedRandomPrototype<T> prototype, IRobustRandom? random = null)
         where
-            T: class, IPrototype
+        T: class, IPrototype
+    {
+        IoCManager.Resolve(ref random);
+        return random.Pick(prototype.Weights);
+    }
+
+    public static T Pick<T>(this IRobustRandom random, Dictionary<T, float> weights)
+        where T: notnull
+    {
+        var sum = weights.Values.Sum();
+        var accumulated = 0f;
+
+        var rand = random.NextFloat() * sum;
+
+        foreach (var (key, weight) in weights)
         {
-            IoCManager.Resolve(ref random);
-            return random.Pick(prototype.Weights);
-        }
+            accumulated += weight;
 
-        public static T Pick<T>(this IRobustRandom random, Dictionary<T, float> weights)
-            where T: notnull
-        {
-            var sum = weights.Values.Sum();
-            var accumulated = 0f;
-
-            var rand = random.NextFloat() * sum;
-
-            foreach (var (key, weight) in weights)
+            if (accumulated >= rand)
             {
-                accumulated += weight;
-
-                if (accumulated >= rand)
-                {
-                    return key;
-                }
+                return key;
             }
-
-            throw new InvalidOperationException("Invalid weighted pick");
         }
 
-        public static T PickAndTake<T>(this IRobustRandom random, Dictionary<T, float> weights)
-            where T : notnull
-        {
-            var pick = Pick(random, weights);
-            weights.Remove(pick);
-            return pick;
-        }
+        throw new InvalidOperationException("Invalid weighted pick");
+    }
 
-        public static bool TryPickAndTake<T>(this IRobustRandom random, Dictionary<T, float> weights, [NotNullWhen(true)] out T? pick)
-            where T : notnull
+    public static T PickAndTake<T>(this IRobustRandom random, Dictionary<T, float> weights)
+        where T : notnull
+    {
+        var pick = Pick(random, weights);
+        weights.Remove(pick);
+        return pick;
+    }
+
+    public static bool TryPickAndTake<T>(this IRobustRandom random, Dictionary<T, float> weights, [NotNullWhen(true)] out T? pick)
+        where T : notnull
+    {
+        if (weights.Count == 0)
         {
-            if (weights.Count == 0)
+            pick = default;
+            return false;
+        }
+        pick = PickAndTake(random, weights);
+        return true;
+    }
+
+    public static T Pick<T>(Dictionary<T, float> weights, IRobustRandom random)
+        where T : notnull
+    {
+        var sum = weights.Values.Sum();
+        var accumulated = 0f;
+
+        var rand = random.NextFloat() * sum;
+
+        foreach (var (key, weight) in weights)
+        {
+            accumulated += weight;
+
+            if (accumulated >= rand)
             {
-                pick = default;
-                return false;
+                return key;
             }
-            pick = PickAndTake(random, weights);
-            return true;
         }
 
-        public static T Pick<T>(Dictionary<T, float> weights, IRobustRandom random)
-            where T : notnull
+        throw new InvalidOperationException("Invalid weighted pick");
+    }
+
+    public static (string reagent, FixedPoint2 quantity) Pick(this WeightedRandomFillSolutionPrototype prototype, IRobustRandom? random = null)
+    {
+        var randomFill = prototype.PickRandomFill(random);
+
+        IoCManager.Resolve(ref random);
+
+        var sum = randomFill.Reagents.Count;
+        var accumulated = 0f;
+
+        var rand = random.NextFloat() * sum;
+
+        foreach (var reagent in randomFill.Reagents)
         {
-            var sum = weights.Values.Sum();
-            var accumulated = 0f;
+            accumulated += 1f;
 
-            var rand = random.NextFloat() * sum;
-
-            foreach (var (key, weight) in weights)
+            if (accumulated >= rand)
             {
-                accumulated += weight;
-
-                if (accumulated >= rand)
-                {
-                    return key;
-                }
+                return (reagent, randomFill.Quantity);
             }
-
-            throw new InvalidOperationException("Invalid weighted pick");
         }
 
-        public static (string reagent, FixedPoint2 quantity) Pick(this WeightedRandomFillSolutionPrototype prototype, IRobustRandom? random = null)
+        // Shouldn't happen
+        throw new InvalidOperationException($"Invalid weighted pick for {prototype.ID}!");
+    }
+
+    public static RandomFillSolution PickRandomFill(this WeightedRandomFillSolutionPrototype prototype, IRobustRandom? random = null)
+    {
+        IoCManager.Resolve(ref random);
+
+        var fills = prototype.Fills;
+        Dictionary<RandomFillSolution, float> picks = new();
+
+        foreach (var fill in fills)
         {
-            var randomFill = prototype.PickRandomFill(random);
+            picks[fill] = fill.Weight;
+        }
 
-            IoCManager.Resolve(ref random);
+        var sum = picks.Values.Sum();
+        var accumulated = 0f;
 
-            var sum = randomFill.Reagents.Count;
-            var accumulated = 0f;
+        var rand = random.NextFloat() * sum;
 
-            var rand = random.NextFloat() * sum;
+        foreach (var (randSolution, weight) in picks)
+        {
+            accumulated += weight;
 
-            foreach (var reagent in randomFill.Reagents)
+            if (accumulated >= rand)
             {
-                accumulated += 1f;
-
-                if (accumulated >= rand)
-                {
-                    return (reagent, randomFill.Quantity);
-                }
+                return randSolution;
             }
-
-            // Shouldn't happen
-            throw new InvalidOperationException($"Invalid weighted pick for {prototype.ID}!");
         }
 
-        public static RandomFillSolution PickRandomFill(this WeightedRandomFillSolutionPrototype prototype, IRobustRandom? random = null)
+        // Shouldn't happen
+        throw new InvalidOperationException($"Invalid weighted pick for {prototype.ID}!");
+    }
+
+    /// <inheritdoc cref="HashCodeCombine(IReadOnlyCollection{int})"/>
+    public static int HashCodeCombine(params int[] values)
+    {
+        return HashCodeCombine((IReadOnlyCollection<int>)values);
+    }
+
+    /// <summary>
+    /// A very simple, deterministic djb2 hash function for generating a combined seed for the random number generator.
+    /// We can't use HashCode.Combine because that is initialized with a random value, creating different results on the server and client.
+    /// </summary>
+    /// <example>
+    /// Combine the current game tick with a NetEntity Id in order to not get the same random result if this is called multiple times in the same tick.
+    /// <code>
+    /// var seed = SharedRandomExtensions.HashCodeCombine((int)_timing.CurTick.Value, GetNetEntity(ent).Id);
+    /// </code>
+    /// </example>
+    public static int HashCodeCombine(IReadOnlyCollection<int> values)
+    {
+        int hash = 5381;
+        foreach (var value in values)
         {
-            IoCManager.Resolve(ref random);
-
-            var fills = prototype.Fills;
-            Dictionary<RandomFillSolution, float> picks = new();
-
-            foreach (var fill in fills)
-            {
-                picks[fill] = fill.Weight;
-            }
-
-            var sum = picks.Values.Sum();
-            var accumulated = 0f;
-
-            var rand = random.NextFloat() * sum;
-
-            foreach (var (randSolution, weight) in picks)
-            {
-                accumulated += weight;
-
-                if (accumulated >= rand)
-                {
-                    return randSolution;
-                }
-            }
-
-            // Shouldn't happen
-            throw new InvalidOperationException($"Invalid weighted pick for {prototype.ID}!");
+            hash = (hash << 5) + hash + value;
         }
+        return hash;
+    }
 
-        /// <inheritdoc cref="HashCodeCombine(IReadOnlyCollection{int})"/>
-        public static int HashCodeCombine(params int[] values)
-        {
-            return HashCodeCombine((IReadOnlyCollection<int>)values);
-        }
+    // TODO: REPLACE ALL OF THIS WITH PREDICTED RANDOM WHEN ENGINE PR IS MERGED
+    /// <summary>
+    /// Creates an instance of IRobustRandom that will be the same for both the server and client.
+    /// This allows for the client and server to roll the same results when determining things randomly, preventing mispredictions.
+    /// We generate a unique seed by getting 2-3 unique but predictable integers into a Hashcode.
+    /// </summary>
+    /// <param name="timing">An instance if IGameTiming.
+    /// We use the integer value of the current tick to ensure a different seed every tick.</param>
+    /// <param name="netEnt">The relevant net entity to our seed.
+    /// This allows different entities to have different seeds and therefore different results on the same game-tick.</param>
+    /// <param name="netEnt2">An optional relevant net entity to our seed.
+    /// Typically used if we have an entity checking random potentially multiple times per tick, to ensure we get a unique seed each time.
+    /// This entity should not be the same entity as <see cref="netEnt"/>.</param>
+    public static IRobustRandom PredictedRandom(IGameTiming timing, NetEntity netEnt, NetEntity? netEnt2 = null)
+    {
+        var seed = HashCodeCombine((int)timing.CurTick.Value, netEnt.Id, netEnt2?.Id ?? 0);
+        var random = new RobustRandom();
+        random.SetSeed(seed);
+        return random;
+    }
 
-        /// <summary>
-        /// A very simple, deterministic djb2 hash function for generating a combined seed for the random number generator.
-        /// We can't use HashCode.Combine because that is initialized with a random value, creating different results on the server and client.
-        /// </summary>
-        /// <example>
-        /// Combine the current game tick with a NetEntity Id in order to not get the same random result if this is called multiple times in the same tick.
-        /// <code>
-        /// var seed = SharedRandomExtensions.HashCodeCombine((int)_timing.CurTick.Value, GetNetEntity(ent).Id);
-        /// </code>
-        /// </example>
-        public static int HashCodeCombine(IReadOnlyCollection<int> values)
-        {
-            int hash = 5381;
-            foreach (var value in values)
-            {
-                hash = (hash << 5) + hash + value;
-            }
-            return hash;
-        }
-
-        // TODO: REPLACE ALL OF THIS WITH PREDICTED RANDOM WHEN ENGINE PR IS MERGED
-        /// <summary>
-        /// Creates an instance of IRobustRandom that will be the same for both the server and client.
-        /// This allows for the client and server to roll the same results when determining things randomly, preventing mispredictions.
-        /// We generate a unique seed by getting 2-3 unique but predictable integers into a Hashcode.
-        /// </summary>
-        /// <param name="timing">An instance if IGameTiming.
-        /// We use the integer value of the current tick to ensure a different seed every tick.</param>
-        /// <param name="netEnt">The relevant net entity to our seed.
-        /// This allows different entities to have different seeds and therefore different results on the same game-tick.</param>
-        /// <param name="netEnt2">An optional relevant net entity to our seed.
-        /// Typically used if we have an entity checking random potentially multiple times per tick, to ensure we get a unique seed each time.
-        /// This entity should not be the same entity as <see cref="netEnt"/>.</param>
-        public static IRobustRandom PredictedRandom(IGameTiming timing, NetEntity netEnt, NetEntity? netEnt2 = null)
-        {
-            var seed = HashCodeCombine((int)timing.CurTick.Value, netEnt.Id, netEnt2?.Id ?? 0);
-            var random = new RobustRandom();
-            random.SetSeed(seed);
-            return random;
-        }
-
-        /// <summary>
-        /// Checks a probability against a <see cref="PredictedRandom"/> instance.
-        /// Returns true if the amount rolled is below the probability.
-        /// </summary>
-        public static bool PredictedProb(IGameTiming timing, float probability, NetEntity netEnt1, NetEntity? netEnt2 = null)
-        {
-            var rand = PredictedRandom(timing, netEnt1, netEnt2);
-            return rand.Prob(probability);
-        }
+    /// <summary>
+    /// Checks a probability against a <see cref="PredictedRandom"/> instance.
+    /// Returns true if the amount rolled is below the probability.
+    /// </summary>
+    public static bool PredictedProb(IGameTiming timing, float probability, NetEntity netEnt1, NetEntity? netEnt2 = null)
+    {
+        var rand = PredictedRandom(timing, netEnt1, netEnt2);
+        return rand.Prob(probability);
     }
 }
