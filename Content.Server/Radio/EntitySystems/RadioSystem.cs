2eb7185e4f0707d9563ff0db6b498c3eae62f911
cashew <wackerlinus@gmail.com>
2026-08-15 04:29:38 +0200

    fix name color

--- Content.Server/Radio/EntitySystems/RadioSystem.cs
+++ Content.Server/Radio/EntitySystems/RadioSystem.cs
@@ -62,10 +62,7 @@ public sealed partial class RadioSystem : SharedRadioSystem
         RaiseLocalEvent(messageSource, evt);
 
         var name = evt.VoiceName;
-        name = FormattedMessage.EscapeText(name);
-
-        if (_chat.ChatNameLinks)
-            name = $"[chatlink=\"{name}\" ent=\"{GetNetEntity(messageSource)}\"]";
+        name = _chat.ChatNameLinks ? $"[chatlink=\"{FormattedMessage.EscapeStringParameter(name)}\" ent=\"{GetNetEntity(messageSource)}\"]" : FormattedMessage.EscapeText(name);
 
         SpeechVerbPrototype speech;
         if (evt.SpeechVerb != null && ProtoMan.Resolve(evt.SpeechVerb, out var evntProto))
