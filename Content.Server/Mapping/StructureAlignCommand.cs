65b0682920a384c0ac82eef79b07ad06f6553609
Errant <35878406+Errant-4@users.noreply.github.com>
2026-08-16 00:53:16 +0200

    finish aligncommand

--- Content.Server/Mapping/StructureAlignCommand.cs
+++ Content.Server/Mapping/StructureAlignCommand.cs
@@ -1,22 +1,19 @@
+using Content.Server.Administration;
 using Content.Shared.Administration;
 using Content.Shared.Mapping;
 using Robust.Shared.Console;
 using Robust.Shared.Map;
 
-namespace Content.Server.Administration.Commands;
+namespace Content.Server.Mapping;
 
 [AdminCommand(AdminFlags.Mapping)]
-public sealed partial class StructureAlignCommand : IConsoleCommand
+public sealed partial class StructureAlignCommand : LocalizedEntityCommands
 {
     [Dependency] private IEntityManager _entManager = default!;
 
-    public string Command => "align";
+    public override string Command => "align";
 
-    public string Description => Loc.GetString("align-command-description");
-
-    public string Help => Loc.GetString("align-command-help-text"); //TODO:ERRANT add locales
-
-    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         if (args.Length > 1)
         {
@@ -24,26 +21,31 @@ public sealed partial class StructureAlignCommand : IConsoleCommand
             return;
         }
 
-        //TODO:ERRANT require a safety bool when in Release conf?
-
         MapId? map = null;
 
         if (args.Length > 0)
         {
             if (!int.TryParse(args[0], out var intMapId))
             {
-                shell.WriteError(Loc.GetString("cmd-mapping-failure-integer", ("arg", args[0])));
+                shell.WriteError(Loc.GetString("shell-argument-must-be-number"));
                 return;
             }
 
             map = new MapId(intMapId);
         }
 
-
         var sat = _entManager.System<SharedStructureAlignerSystem>();
 
         var response = sat.AlignAll(map);
         if (!string.IsNullOrEmpty(response))
             shell.WriteLine(response);
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        if (args.Length == 1)
+            return CompletionResult.FromHintOptions(CompletionHelper.MapIds(_entManager), Loc.GetString("cmd-hint-align-id"));
+
+        return CompletionResult.Empty;
     }
 }
---
+++ Resources/Locale/en-US/mapping/align-command.ftl
@@ -0,0 +1,4 @@
+cmd-align-desc = Automatically snap the alignment of all anchored airlocks, doors, firelocks etc. to line up with adjacent structures.
+cmd-align-help = Usage: {$command} [MapID]
+cmd-align-no-release = You can't use this command if the game is running in RELEASE configuration.
+cmd-hint-align-id = MapID
