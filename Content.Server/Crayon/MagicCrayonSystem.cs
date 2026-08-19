605b75927c38c4350629d440649232f4b6f94635
Weryfrate <w3ryfrate@gmail.com>
2026-08-20 01:35:01 +0200

    Added DoAfter for MagicCrayon

--- Content.Server/Crayon/MagicCrayonSystem.cs
+++ Content.Server/Crayon/MagicCrayonSystem.cs
@@ -1,7 +1,9 @@
 using Content.Server.Popups;
 using Content.Shared.Charges.Systems;
 using Content.Shared.Crayon;
+using Content.Shared.DoAfter;
 using Content.Shared.Interaction;
+using System.ComponentModel;
 
 namespace Content.Server.Crayon;
 
@@ -9,29 +11,56 @@ public sealed partial class MagicCrayonSystem : EntitySystem
 {
     [Dependency] private SharedChargesSystem _charges = default!;
     [Dependency] private PopupSystem _popup = default!;
+    [Dependency] private SharedDoAfterSystem _doAfter = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<MagicCrayonComponent, AfterInteractEvent>(OnAfterInteract);
+        SubscribeLocalEvent<MagicCrayonComponent, MagicCrayonDoAfterEvent>(OnDoAfter);
     }
 
     private void OnAfterInteract(EntityUid uid, MagicCrayonComponent component, ref AfterInteractEvent args)
     {
-        if (args.Handled || !args.CanReach || args.Target != null)
+        if (!args.CanReach || args.Handled || args.Target != null)
             return;
 
         if (_charges.IsEmpty(uid))
-        {
-            _popup.PopupEntity(Loc.GetString("crayon-interact-used-up-text", ("owner", uid)), uid, args.User);
-            QueueDel(uid);
-            args.Handled = true;
             return;
-        }
 
-        Spawn(component.SpawnProto, args.ClickLocation);
-        _charges.TryUseCharge(uid);
+        var doAfterArgs = new DoAfterArgs(EntityManager, args.User, TimeSpan.FromSeconds(2.0f),
+            new MagicCrayonDoAfterEvent(), uid, used: uid)
+        {
+            BreakOnMove = true,
+            BreakOnDamage = true,
+            NeedHand = true,
+        };
+
+        _doAfter.TryStartDoAfter(doAfterArgs);
         args.Handled = true;
+    }
+
+    private void OnDoAfter(EntityUid uid, MagicCrayonComponent component, ref MagicCrayonDoAfterEvent args)
+    {
+        if (args.Handled || args.Cancelled || args.Target != null)
+            return;
+
+        if (_charges.IsEmpty(uid))
+            return;
+
+        Spawn(component.SpawnProto, Transform(args.User).Coordinates);
+        _charges.TryUseCharge(uid);
+
+        if (_charges.IsEmpty(uid))
+            UseUp(uid, args.User);
+
+        args.Handled = true;
+    }
+
+    private void UseUp(EntityUid uid, EntityUid user)
+    {
+        _popup.PopupEntity("The magic crayon has been used up.", user);
+        QueueDel(uid);
     }
 }
