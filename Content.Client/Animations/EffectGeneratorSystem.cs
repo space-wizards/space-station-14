71f57eeae771fb5a14aa9676a9f5a4cf809a68c2
Samuel-C <samuelcamilo8@gmail.com>
2026-08-19 13:35:05 -0300

    effect generation system

---
+++ Content.Client/Animations/EffectGeneratorSystem.cs
@@ -0,0 +1,75 @@
+using System.Numerics;
+using Content.Shared.Animation;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Physics.Systems;
+using Robust.Shared.Random;
+using Robust.Shared.Timing;
+
+namespace Content.Client.Animations;
+
+public sealed partial class EffectGeneratorSystem : EntitySystem
+{
+    [Dependency] private SharedMapSystem _mapSystem = default!;
+    [Dependency] private SharedPhysicsSystem _physics = default!;
+    [Dependency] private IGameTiming _timing = default!;
+    [Dependency] private SharedTransformSystem _transform = default!;
+    [Dependency] private IRobustRandom _random = default!;
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        if (!_timing.IsFirstTimePredicted)
+            return;
+
+        var query = EntityQueryEnumerator<EffectGeneratorComponent, TransformComponent>();
+
+        while (query.MoveNext(out var uid, out var comp, out var xform))
+        {
+            if (_transform.InRange(xform.Coordinates, comp.LastCoordinates, comp.MaxDistance))
+            {
+                if (_timing.CurTime < comp.NextEffectSpawnTime)
+                    continue;
+            }
+
+            comp.LastCoordinates = _transform.GetMoverCoordinates(xform.Coordinates);
+            comp.NextEffectSpawnTime = _timing.CurTime + comp.EffectCooldown;
+
+            SpawnEffect((uid, comp));
+        }
+    }
+
+    private void SpawnEffect(Entity<EffectGeneratorComponent> entity)
+    {
+        var entityXform = Transform(entity);
+        var entitySpeed = _physics.GetLinearVelocity(entity, Vector2.Zero);
+
+        // Don't show particles unless the user is moving.
+        if (entitySpeed.LengthSquared() < 1f)
+            return;
+
+        var coordinates = entityXform.Coordinates;
+        var gridUid = _transform.GetGrid(coordinates);
+
+        if (TryComp<MapGridComponent>(gridUid, out var grid))
+            coordinates = new EntityCoordinates(gridUid.Value, _mapSystem.WorldToLocal(gridUid.Value, grid, _transform.ToMapCoordinates(coordinates).Position));
+        else if (entityXform.MapUid != null)
+            coordinates = new EntityCoordinates(entityXform.MapUid.Value, _transform.GetWorldPosition(entityXform));
+        else
+            return;
+
+        var effect = Spawn(entity.Comp.EffectPrototype, coordinates);
+
+        switch (entity.Comp.RotationPolicy)
+        {
+            case RotationPolicy.FollowMotionDirection:
+                _transform.SetWorldRotation(effect, entitySpeed.ToAngle());
+                break;
+
+            case RotationPolicy.Random:
+                _transform.SetWorldRotation(effect, _random.NextAngle());
+                break;
+        }
+    }
+}
---
+++ Content.Shared/Animation/EffectGeneratorComponent.cs
@@ -0,0 +1,70 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Shared.Animation;
+
+/// <summary>
+/// Makes a entity spawn visual effects (particles) on movement.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class EffectGeneratorComponent : Component
+{
+    /// <summary>
+    /// Prototype of the effect to be spawned
+    /// </summary>
+    [DataField]
+    public EntProtoId EffectPrototype = "JetpackEffect";
+
+    /// <summary>
+    /// How it should rotate the effect entity after it spawns
+    /// </summary>
+    [DataField]
+    public RotationPolicy RotationPolicy = RotationPolicy.NoRotation;
+
+    /// <summary>
+    /// Cooldown time between spawning effects
+    /// </summary>
+    [DataField]
+    public TimeSpan EffectCooldown = TimeSpan.FromSeconds(0.3);
+
+    /// <summary>
+    /// Next time to spawn a effect after the cooldown.
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    public TimeSpan NextEffectSpawnTime;
+
+    /// <summary>
+    /// Max distance between effects.
+    ///
+    /// If the entity gets too far from the last point where it spawned a effect.
+    /// it will spawn a new effect entity regardless of the cooldown.
+    /// </summary>
+    [DataField]
+    public float MaxDistance = 0.7f;
+
+    /// <summary>
+    /// Last point where the effect was spawned.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    public EntityCoordinates LastCoordinates;
+}
+
+public enum RotationPolicy
+{
+    /// <summary>
+    /// Doesn't rotate the effect entity
+    /// </summary>
+    NoRotation,
+
+    /// <summary>
+    /// Rotate the effect entity to match the direction of motion
+    /// </summary>
+    FollowMotionDirection,
+
+    /// <summary>
+    /// Rotate the effect randomly
+    /// </summary>
+    Random,
+}
