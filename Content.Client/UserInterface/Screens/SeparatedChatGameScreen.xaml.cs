6d8adf86a78ee6fd562d691eeda0fcfc319e99a2
joules <pacifist.jules@gmail.com>
2026-08-21 01:04:56 +0300

    mad

--- Content.Client/UserInterface/Screens/SeparatedChatGameScreen.xaml.cs
+++ Content.Client/UserInterface/Screens/SeparatedChatGameScreen.xaml.cs
@@ -27,7 +27,7 @@ public sealed partial class SeparatedChatGameScreen : InGameScreen
         SetAnchorAndMarginPreset(Ghost, LayoutPreset.BottomWide, margin: 80);
         SetAnchorAndMarginPreset(Hotbar, LayoutPreset.BottomWide, margin: 5);
         SetAnchorAndMarginPreset(Alerts, LayoutPreset.CenterRight, margin: 10);
-        SetAnchorAndMarginPreset(Clocks, LayoutPreset.BottomWide);
+        SetAnchorPreset(Clocks, LayoutPreset.BottomRight, true);
 
         ScreenContainer.OnSplitResizeFinished += () =>
             OnChatResized?.Invoke(new Vector2(ScreenContainer.SplitFraction, 0));
