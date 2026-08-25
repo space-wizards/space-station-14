40070e948efccebb405fa625f77c3059cd809ef1
Whatstone <whatston3@gmail.com>
2026-08-25 12:12:18 -0400

    Add SpawnTrashOnRefined to EdibleComponent

--- Content.Shared/Nutrition/EntitySystems/IngestionSystem.cs
+++ Content.Shared/Nutrition/EntitySystems/IngestionSystem.cs
@@ -514,7 +514,8 @@ public sealed partial class IngestionSystem : EntitySystem
 
     private void OnBeforeToolRefined(Entity<EdibleComponent> entity, ref BeforeToolRefinedEvent args)
     {
-        SpawnTrash(entity, args.User);
+        if (entity.Comp.SpawnTrashOnRefine)
+            SpawnTrash(entity, args.User);
     }
 
     private void AddEdibleVerbs(Entity<EdibleComponent> entity, ref GetVerbsEvent<AlternativeVerb> args)
