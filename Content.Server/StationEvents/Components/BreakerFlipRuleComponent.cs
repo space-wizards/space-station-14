6715136f189171a88f6bc1b66bc4de0c74552972
Whatstone <whatston3@gmail.com>
2026-08-25 00:49:36 -0400

    finish your thought challenge: diffic. impossible

--- Content.Server/StationEvents/Components/BreakerFlipRuleComponent.cs
+++ Content.Server/StationEvents/Components/BreakerFlipRuleComponent.cs
@@ -4,8 +4,9 @@ using Content.Shared.Whitelist;
 namespace Content.Server.StationEvents.Components;
 
 /// <summary>
-/// A component for the breaker flip rule.
-///
+/// A component for a game rule that disables breakers for active APCs.
+/// When it runs, it selects a random station and disables some random number of APCs,
+/// optionally checking blacklists for the stations/grids that they're a part of.
 /// </summary>
 [RegisterComponent, Access(typeof(BreakerFlipRule))]
 public sealed partial class BreakerFlipRuleComponent : Component
