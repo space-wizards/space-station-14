8fcad935389f1532d6c33a380b8e8f420479cc52
Whatstone <whatston3@gmail.com>
2026-08-26 14:53:09 -0400

    peaceful-joules' comments + MMIVisuals comments

--- Content.Shared/Silicons/Borgs/Components/MMIComponent.cs
+++ Content.Shared/Silicons/Borgs/Components/MMIComponent.cs
@@ -44,29 +44,53 @@ public sealed partial class MMIComponent : Component
     public string NoBrainState = "mmi_off";
 
     /// <summary>
-    /// The sprite state when the brain inserted has a mind.
+    /// The color of the <see cref="MMIVisualLayers.Unshaded"/> layer when the brain inserted has a mind.
     /// </summary>
     [DataField]
     public Color HasMindLightColor = Color.FromHex("#0094ff");
 
     /// <summary>
-    /// The sprite state when the brain inserted doesn't have a mind.
+    /// The color of the <see cref="MMIVisualLayers.Unshaded"/> layer when the brain inserted doesn't have a mind.
     /// </summary>
     [DataField]
     public Color NoMindLightColor = Color.FromHex("#ff3033");
 }
 
+/// <summary>
+/// AppearanceData keys for the MMI.
+/// </summary>
 [Serializable, NetSerializable]
 public enum MMIVisuals : byte
 {
+    /// <summary>
+    /// bool: Whether or not there is a brain in the MMI.
+    /// </summary>
     BrainPresent,
+
+    /// <summary>
+    /// bool: Whether or not there is an active mind (a player) in the MMI.
+    /// </summary>
     HasMind
 }
 
+/// <summary>
+/// Sprite map keys for MMI visuals.
+/// </summary>
 [Serializable, NetSerializable]
 public enum MMIVisualLayers : byte
 {
+    /// <summary>
+    /// The layer of the brain.
+    /// </summary>
     Brain,
+
+    /// <summary>
+    /// The layer of the housing.
+    /// </summary>
     Base,
-    Unlit,
+
+    /// <summary>
+    /// The optional layer of an indicator light.
+    /// </summary>
+    Unshaded,
 }
