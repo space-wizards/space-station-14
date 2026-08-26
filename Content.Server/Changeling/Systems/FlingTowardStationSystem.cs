573ed7608ee0c7d862b254254c91e6b9bb6fe33b
nomdéraisonnablementlong <you@example.com>
2026-08-25 20:45:08 -0400

    shitcode!!!!

--- Content.Server/Changeling/Systems/FlingTowardStationSystem.cs
+++ Content.Server/Changeling/Systems/FlingTowardStationSystem.cs
@@ -3,6 +3,7 @@ using Robust.Shared.Physics.Systems;
 using Robust.Shared.Random;
 using Content.Server.Station.Systems;
 using Robust.Shared.Physics.Components;
+using Content.Shared.Station.Components;
 
 namespace Content.Server.Changeling.Systems;
 
@@ -18,13 +19,22 @@ public sealed partial class FlingTowardStationSystem : EntitySystem
         if (_station.GetStations().Count == 0)
             return;
 
+        // fetch the station's grid
         var random = new RobustRandom();
         var station = random.Pick(_station.GetStations());
+        if (!TryComp<StationDataComponent>(station, out var stationComp))
+            return;
 
-        var stationPosition = _transform.GetMapCoordinates(station);
+        if (stationComp.Grids.Count == 0)
+            return;
+
+        var grid = random.Pick(stationComp.Grids);
+        var stationPosition = _transform.GetMapCoordinates(grid);
         var entPosition = _transform.GetMapCoordinates(ent.Owner);
+        var gridPhys = Comp<PhysicsComponent>(grid);
 
-        var offset = entPosition.Position - stationPosition.Position;
+        // calculate the offset from the center of mass of the station to the body
+        var offset = entPosition.Position - _transform.GetWorldRotation(grid).RotateVec(gridPhys.LocalCenter) - stationPosition.Position;
 
         var physics = Comp<PhysicsComponent>(ent.Owner);
         _physics.ApplyLinearImpulse(ent.Owner, -offset.Normalized() * ent.Comp.Speed * physics.Mass, body: physics);
