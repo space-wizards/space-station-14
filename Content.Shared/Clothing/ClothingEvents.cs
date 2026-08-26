7b67d5a705a128f3b7389439f141caf515c70a78
Whatstone <whatston3@gmail.com>
2026-08-25 22:20:54 -0400

    ClientClothing/ToggleableVisuals: copy Layers

--- Content.Shared/Clothing/ClothingEvents.cs
+++ Content.Shared/Clothing/ClothingEvents.cs
@@ -7,17 +7,22 @@ namespace Content.Shared.Clothing;
 /// <summary>
 /// Raised directed at a piece of clothing to get the set of layers to show on the wearer's sprite.
 /// </summary>
-public sealed class GetEquipmentVisualsEvent : EntityEventArgs
+public sealed class GetEquipmentVisualsEvent(EntityUid equipee, string slot) : EntityEventArgs
 {
     /// <summary>
     /// Entity that is wearing the item.
     /// </summary>
-    public readonly EntityUid Equipee;
+    public readonly EntityUid Equipee = equipee;
 
-    public readonly string Slot;
+    /// <summary>
+    /// The name of slot that the item is being worn in.
+    /// </summary>
+    public readonly string Slot = slot;
 
     /// <summary>
     /// The layers that will be added to the entity that is wearing this item.
+    /// PrototypeLayerData objects passed may be modified, do not pass your component state directly!
+    ///
     /// NOTE: any layers will be checked for species-specific layers automatically inside ClothingSystem.
     /// If you return a state of "equipped-HEAD" and "equipped-HEAD-reptilian" exists,
     /// it will be automatically used by entities with InventoryComponent.SpeciesId set to "reptilian".
@@ -26,12 +31,6 @@ public sealed class GetEquipmentVisualsEvent : EntityEventArgs
     /// Note that the actual ordering of the layers depends on the order in which they are added to this list.
     /// </remarks>
     public List<(string, PrototypeLayerData)> Layers = new();
-
-    public GetEquipmentVisualsEvent(EntityUid equipee, string slot)
-    {
-        Equipee = equipee;
-        Slot = slot;
-    }
 }
 
 /// <summary>
@@ -40,29 +39,25 @@ public sealed class GetEquipmentVisualsEvent : EntityEventArgs
 /// <remarks>
 /// Useful for systems/components that modify the visual layers that an item adds to a player. (e.g. RGB memes)
 /// </remarks>
-public sealed class EquipmentVisualsUpdatedEvent : EntityEventArgs
+public sealed class EquipmentVisualsUpdatedEvent(
+    EntityUid equipee,
+    string slot,
+    HashSet<string> revealedLayers) : EntityEventArgs
 {
     /// <summary>
     /// Entity that is wearing the item.
     /// </summary>
-    public readonly EntityUid Equipee;
+    public readonly EntityUid Equipee = equipee;
 
     /// <summary>
     /// The slot that the equipment is being worn in.
     /// </summary>
-    public readonly string Slot;
+    public readonly string Slot = slot;
 
     /// <summary>
     /// The layers that this item is now revealing.
     /// </summary>
-    public HashSet<string> RevealedLayers;
-
-    public EquipmentVisualsUpdatedEvent(EntityUid equipee, string slot, HashSet<string> revealedLayers)
-    {
-        Equipee = equipee;
-        Slot = slot;
-        RevealedLayers = revealedLayers;
-    }
+    public HashSet<string> RevealedLayers = revealedLayers;
 }
 
 public sealed partial class ToggleMaskEvent : InstantActionEvent;
