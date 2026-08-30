443bc4efd65f30730271926ae801c5552b889c78
nomdéraisonnablementlong <you@example.com>
2026-08-29 21:30:56 -0400

    c# speedrun any%

---
+++ Content.Shared/EntityEffects/Effects/Body/SpecialDeathCauseEntityEffectSystem.cs
@@ -0,0 +1,31 @@
+using Content.Shared.GhostTypes;
+using Content.Shared.Medical;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.EntityEffects.Effects.Body;
+
+/// <summary>
+/// Sets a death cause to the entity
+/// </summary>
+/// <inheritdoc cref="EntityEffectSystem{T,TEffect}"/>
+public sealed partial class SpecialDeathCauseEffectSystem : EntityEffectSystem<StoreDamageTakenOnMindComponent, SpecialDeathCause>
+{
+    [Dependency] private StoreDamageTakenOnMindSystem _storeSystem = default!;
+    protected override void Effect(Entity<StoreDamageTakenOnMindComponent> entity, ref EntityEffectEvent<SpecialDeathCause> args)
+    {
+        _storeSystem.SaveSpecialCauseOfDeath(ent, args.Effect.DeathCause);
+    }
+}
+
+/// <inheritdoc cref="EntityEffect"/>
+public sealed partial class SpecialDeathCause : EntityEffectBase<SpecialDeathCause>
+{
+    /// <summary>
+    /// The special death cause that will be added to the entity affected by this entity effect
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<SpecialCauseOfDeathPrototype> DeathCause;
+
+    public override string EntityEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+        => Loc.GetString("entity-effect-guidebook-death-cause", ("cause", DeathCause));
+}
