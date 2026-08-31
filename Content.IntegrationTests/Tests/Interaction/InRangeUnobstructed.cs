c42bc10e4e94d15b9968211d73ec4848979a76fa
Tayrtahn <tayrtahn@gmail.com>
2026-08-31 15:25:43 -0400

    most of Interaction

--- Content.IntegrationTests/Tests/Interaction/InRangeUnobstructed.cs
+++ Content.IntegrationTests/Tests/Interaction/InRangeUnobstructed.cs
@@ -1,114 +1,105 @@
+#nullable enable
 using System.Numerics;
 using Content.IntegrationTests.Fixtures;
+using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Shared.Interaction;
 using Robust.Server.GameObjects;
 using Robust.Shared.Containers;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
-using Robust.Shared.Maths;
+using Robust.Shared.Prototypes;
 
-namespace Content.IntegrationTests.Tests.Interaction
+namespace Content.IntegrationTests.Tests.Interaction;
+
+[TestOf(typeof(SharedInteractionSystem))]
+public sealed class InRangeUnobstructed : GameTest
 {
-    [TestFixture]
-    [TestOf(typeof(SharedInteractionSystem))]
-    public sealed class InRangeUnobstructed : GameTest
+    private static readonly EntProtoId HumanId = "MobHuman";
+
+    private const float InteractionRange = SharedInteractionSystem.InteractionRange;
+    private const float InteractionRangeDivided15 = InteractionRange / 1.5f;
+    private static readonly Vector2 InteractionRangeDivided15X = new(InteractionRangeDivided15, 0f);
+    private const float InteractionRangeDivided15Times3 = InteractionRangeDivided15 * 3;
+    private const float HumanRadius = 0.35f;
+
+    [SidedDependency(Side.Server)] private SharedContainerSystem _sContainerSystem = default!;
+    [SidedDependency(Side.Server)] private SharedInteractionSystem _sInteractionSystem = default!;
+    [SidedDependency(Side.Server)] private TransformSystem _sTransformSystem = default!;
+
+    [Test]
+    public async Task EntityEntityTest()
     {
-        private const string HumanId = "MobHuman";
+        EntityUid origin = default;
+        EntityUid other = default;
+        MapCoordinates mapCoordinates = default;
 
-        private const float InteractionRange = SharedInteractionSystem.InteractionRange;
+        await Pair.CreateTestMap();
+        Assert.That(TestMap, Is.Not.Null);
 
-        private const float InteractionRangeDivided15 = InteractionRange / 1.5f;
-
-        private static readonly Vector2 InteractionRangeDivided15X = new(InteractionRangeDivided15, 0f);
-
-        private const float InteractionRangeDivided15Times3 = InteractionRangeDivided15 * 3;
-
-        private const float HumanRadius = 0.35f;
-
-        [Test]
-        public async Task EntityEntityTest()
+        await Server.WaitAssertion(() =>
         {
-            var pair = Pair;
-            var server = pair.Server;
+            var coordinates = TestMap.GridCoords;
 
-            var sEntities = server.ResolveDependency<IEntityManager>();
-            var conSystem = sEntities.EntitySysManager.GetEntitySystem<SharedContainerSystem>();
-            var tSystem = sEntities.EntitySysManager.GetEntitySystem<TransformSystem>();
+            origin = SSpawnAtPosition(HumanId, coordinates);
+            other = SSpawnAtPosition(HumanId, coordinates);
+            _sContainerSystem.EnsureContainer<Container>(other, "InRangeUnobstructedTestOtherContainer");
+            mapCoordinates = _sTransformSystem.GetMapCoordinates(other);
+        });
 
-            EntityUid origin = default;
-            EntityUid other = default;
-            MapCoordinates mapCoordinates = default;
+        await Server.WaitIdleAsync();
 
-            var map = await pair.CreateTestMap();
+        var xform = SComp<TransformComponent>(origin);
 
-            await server.WaitAssertion(() =>
+        await Server.WaitAssertion(() =>
+        {
+            using (Assert.EnterMultipleScope())
             {
-                var coordinates = map.MapCoords;
+                // Entity <-> Entity
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(origin, other));
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(other, origin));
 
-                origin = sEntities.SpawnEntity(HumanId, coordinates);
-                other = sEntities.SpawnEntity(HumanId, coordinates);
-                conSystem.EnsureContainer<Container>(other, "InRangeUnobstructedTestOtherContainer");
-                mapCoordinates = tSystem.GetMapCoordinates(other);
+                // Entity <-> MapCoordinates
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(origin, mapCoordinates));
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(mapCoordinates, origin));
+            }
+
+            // Move them slightly apart
+            _sTransformSystem.SetLocalPosition(origin, xform.LocalPosition + InteractionRangeDivided15X, xform);
+
+            Assert.Multiple(() =>
+            {
+                // Entity <-> Entity
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(origin, other));
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(other, origin));
+
+                // Entity <-> MapCoordinates
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(origin, mapCoordinates));
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(mapCoordinates, origin));
             });
 
-            await server.WaitIdleAsync();
+            // Move them out of range
+            _sTransformSystem.SetLocalPosition(origin, xform.LocalPosition + new Vector2(InteractionRangeDivided15 + HumanRadius * 2f, 0f), xform);
 
-            var interactionSys = sEntities.System<SharedInteractionSystem>();
-            var xformSys = sEntities.System<SharedTransformSystem>();
-            var xform = sEntities.GetComponent<TransformComponent>(origin);
-
-            await server.WaitAssertion(() =>
+            using (Assert.EnterMultipleScope())
             {
-                Assert.Multiple(() =>
-                {
-                    // Entity <-> Entity
-                    Assert.That(interactionSys.InRangeUnobstructed(origin, other));
-                    Assert.That(interactionSys.InRangeUnobstructed(other, origin));
+                // Entity <-> Entity
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(origin, other), Is.False);
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(other, origin), Is.False);
 
-                    // Entity <-> MapCoordinates
-                    Assert.That(interactionSys.InRangeUnobstructed(origin, mapCoordinates));
-                    Assert.That(interactionSys.InRangeUnobstructed(mapCoordinates, origin));
-                });
+                // Entity <-> MapCoordinates
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(origin, mapCoordinates), Is.False);
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(mapCoordinates, origin), Is.False);
 
-                // Move them slightly apart
-                xformSys.SetLocalPosition(origin, xform.LocalPosition + InteractionRangeDivided15X, xform);
+                // Checks with increased range
 
-                Assert.Multiple(() =>
-                {
-                    // Entity <-> Entity
-                    // Entity <-> Entity
-                    Assert.That(interactionSys.InRangeUnobstructed(origin, other));
-                    Assert.That(interactionSys.InRangeUnobstructed(other, origin));
+                // Entity <-> Entity
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(origin, other, InteractionRangeDivided15Times3));
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(other, origin, InteractionRangeDivided15Times3));
 
-                    // Entity <-> MapCoordinates
-                    Assert.That(interactionSys.InRangeUnobstructed(origin, mapCoordinates));
-                    Assert.That(interactionSys.InRangeUnobstructed(mapCoordinates, origin));
-                });
-
-                // Move them out of range
-                xformSys.SetLocalPosition(origin, xform.LocalPosition + new Vector2(InteractionRangeDivided15 + HumanRadius * 2f, 0f), xform);
-
-                Assert.Multiple(() =>
-                {
-                    // Entity <-> Entity
-                    Assert.That(interactionSys.InRangeUnobstructed(origin, other), Is.False);
-                    Assert.That(interactionSys.InRangeUnobstructed(other, origin), Is.False);
-
-                    // Entity <-> MapCoordinates
-                    Assert.That(interactionSys.InRangeUnobstructed(origin, mapCoordinates), Is.False);
-                    Assert.That(interactionSys.InRangeUnobstructed(mapCoordinates, origin), Is.False);
-
-                    // Checks with increased range
-
-                    // Entity <-> Entity
-                    Assert.That(interactionSys.InRangeUnobstructed(origin, other, InteractionRangeDivided15Times3));
-                    Assert.That(interactionSys.InRangeUnobstructed(other, origin, InteractionRangeDivided15Times3));
-
-                    // Entity <-> MapCoordinates
-                    Assert.That(interactionSys.InRangeUnobstructed(origin, mapCoordinates, InteractionRangeDivided15Times3));
-                    Assert.That(interactionSys.InRangeUnobstructed(mapCoordinates, origin, InteractionRangeDivided15Times3));
-                });
-            });
-        }
+                // Entity <-> MapCoordinates
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(origin, mapCoordinates, InteractionRangeDivided15Times3));
+                Assert.That(_sInteractionSystem.InRangeUnobstructed(mapCoordinates, origin, InteractionRangeDivided15Times3));
+            }
+        });
     }
 }
