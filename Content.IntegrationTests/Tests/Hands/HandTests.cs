a2c6e426cd224d8c71a9b7950106283b52baa927
Tayrtahn <tayrtahn@gmail.com>
2026-08-31 14:37:13 -0400

    Hands

--- Content.IntegrationTests/Tests/Hands/HandTests.cs
+++ Content.IntegrationTests/Tests/Hands/HandTests.cs
@@ -1,5 +1,6 @@
-using System.Linq;
+#nullable enable
 using Content.IntegrationTests.Fixtures;
+using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Server.Storage.EntitySystems;
 using Content.Shared.Hands.Components;
 using Content.Shared.Hands.EntitySystems;
@@ -7,16 +8,19 @@ using Robust.Server.GameObjects;
 using Robust.Server.Player;
 using Robust.Shared.Containers;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Prototypes;
 
 namespace Content.IntegrationTests.Tests.Hands;
 
-[TestFixture]
 public sealed class HandTests : GameTest
 {
+    private const string TestPickUpThenDropInContainerTestBox = "TestPickUpThenDropInContainerTestBox";
+    private static readonly EntProtoId Crowbar = "Crowbar";
+
     [TestPrototypes]
-    private const string Prototypes = @"
+    private const string Prototypes = $@"
 - type: entity
-  id: TestPickUpThenDropInContainerTestBox
+  id: {TestPickUpThenDropInContainerTestBox}
   name: box
   components:
   - type: EntityStorage
@@ -25,69 +29,52 @@ public sealed class HandTests : GameTest
       entity_storage: !type:Container
 ";
 
-
     public override PoolSettings PoolSettings => new()
     {
         Connected = true,
         DummyTicker = false
     };
 
+    [SidedDependency(Side.Server)] private SharedContainerSystem _sContainerSystem = default!;
+    [SidedDependency(Side.Server)] private EntityStorageSystem _sEntityStorageSystem = default!;
+    [SidedDependency(Side.Server)] private SharedHandsSystem _sHandsSystem = default!;
+    [SidedDependency(Side.Server)] private TransformSystem _sTransformSystem = default!;
+
     [Test]
     public async Task TestPickupDrop()
     {
-        var pair = Pair;
-        var server = pair.Server;
-
-        var entMan = server.ResolveDependency<IEntityManager>();
-        var playerMan = server.ResolveDependency<IPlayerManager>();
-        var mapSystem = server.System<SharedMapSystem>();
-        var sys = entMan.System<SharedHandsSystem>();
-        var tSys = entMan.System<TransformSystem>();
-
-        var data = await pair.CreateTestMap();
-        await pair.RunTicksSync(5);
+        await Pair.CreateTestMap();
 
         EntityUid item = default;
         EntityUid player = default;
         HandsComponent hands = default!;
-        await server.WaitPost(() =>
+        await Server.WaitPost(() =>
         {
-            player = playerMan.Sessions.First().AttachedEntity!.Value;
-            var xform = entMan.GetComponent<TransformComponent>(player);
-            item = entMan.SpawnEntity("Crowbar", tSys.GetMapCoordinates(player, xform: xform));
-            hands = entMan.GetComponent<HandsComponent>(player);
-            sys.TryPickup(player, item, hands.ActiveHandId!);
+            player = ServerSession!.AttachedEntity!.Value;
+            var xform = SComp<TransformComponent>(player);
+            item = SSpawnAtPosition(Crowbar, xform.Coordinates);
+            hands = SComp<HandsComponent>(player);
+            _sHandsSystem.TryPickup(player, item, hands.ActiveHandId!);
         });
 
         // run ticks here is important, as errors may happen within the container system's frame update methods.
-        await pair.RunTicksSync(5);
-        Assert.That(sys.GetActiveItem((player, hands)), Is.EqualTo(item));
+        await RunTicksSync(5);
+        Assert.That(_sHandsSystem.GetActiveItem((player, hands)), Is.EqualTo(item));
 
-        await server.WaitPost(() =>
+        await Server.WaitPost(() =>
         {
-            sys.TryDrop(player, item);
+            _sHandsSystem.TryDrop(player, item);
         });
 
-        await pair.RunTicksSync(5);
-        Assert.That(sys.GetActiveItem((player, hands)), Is.Null);
-
-        await server.WaitPost(() => mapSystem.DeleteMap(data.MapId));
+        await RunTicksSync(5);
+        Assert.That(_sHandsSystem.GetActiveItem((player, hands)), Is.Null);
     }
 
     [Test]
     public async Task TestPickUpThenDropInContainer()
     {
-        var pair = Pair;
-        var server = pair.Server;
-        var map = await pair.CreateTestMap();
-        await pair.RunTicksSync(5);
-
-        var entMan = server.ResolveDependency<IEntityManager>();
-        var playerMan = server.ResolveDependency<IPlayerManager>();
-        var mapSystem = server.System<SharedMapSystem>();
-        var sys = entMan.System<SharedHandsSystem>();
-        var tSys = entMan.System<TransformSystem>();
-        var containerSystem = server.System<SharedContainerSystem>();
+        await Pair.CreateTestMap();
+        Assert.That(TestMap, Is.Not.Null);
 
         EntityUid item = default;
         EntityUid box = default;
@@ -95,42 +82,41 @@ public sealed class HandTests : GameTest
         HandsComponent hands = default!;
 
         // spawn the elusive box and crowbar at the coordinates
-        await server.WaitPost(() => box = server.EntMan.SpawnEntity("TestPickUpThenDropInContainerTestBox", map.GridCoords));
-        await server.WaitPost(() => item = server.EntMan.SpawnEntity("Crowbar", map.GridCoords));
+        await Server.WaitPost(() => box = SSpawnAtPosition(TestPickUpThenDropInContainerTestBox, TestMap.GridCoords));
+        await Server.WaitPost(() => item = SSpawnAtPosition(Crowbar, TestMap.GridCoords));
         // place the player at the exact same coordinates and have them grab the crowbar
-        await server.WaitPost(() =>
+        await Server.WaitPost(() =>
         {
-            player = playerMan.Sessions.First().AttachedEntity!.Value;
-            tSys.PlaceNextTo(player, item);
-            hands = entMan.GetComponent<HandsComponent>(player);
-            sys.TryPickup(player, item, hands.ActiveHandId!);
+            player = ServerSession!.AttachedEntity!.Value;
+            _sTransformSystem.PlaceNextTo(player, item);
+            hands = SComp<HandsComponent>(player);
+            _sHandsSystem.TryPickup(player, item, hands.ActiveHandId!);
         });
-        await pair.RunTicksSync(5);
-        Assert.That(sys.GetActiveItem((player, hands)), Is.EqualTo(item));
+
+        await RunTicksSync(5);
+        Assert.That(_sHandsSystem.GetActiveItem((player, hands)), Is.EqualTo(item));
 
         // Open then close the box to place the player, who is holding the crowbar, inside of it
-        var storage = server.System<EntityStorageSystem>();
-        await server.WaitPost(() =>
+        await Server.WaitPost(() =>
         {
-            storage.OpenStorage(box);
-            storage.CloseStorage(box);
+            _sEntityStorageSystem.OpenStorage(box);
+            _sEntityStorageSystem.CloseStorage(box);
         });
-        await pair.RunTicksSync(5);
-        Assert.That(containerSystem.IsEntityInContainer(player), Is.True);
+        await RunTicksSync(5);
+        Assert.That(_sContainerSystem.IsEntityInContainer(player), Is.True);
 
         // Dropping the item while the player is inside the box should cause the item
         // to also be inside the same container the player is in now,
         // with the item not being in the player's hands
-        await server.WaitPost(() =>
+        await Server.WaitPost(() =>
         {
-            sys.TryDrop(player, item);
+            _sHandsSystem.TryDrop(player, item);
         });
-        await pair.RunTicksSync(5);
-        var xform = entMan.GetComponent<TransformComponent>(player);
-        var itemXform = entMan.GetComponent<TransformComponent>(item);
-        Assert.That(sys.GetActiveItem((player, hands)), Is.Not.EqualTo(item));
-        Assert.That(containerSystem.IsInSameOrNoContainer((player, xform), (item, itemXform)));
 
-        await server.WaitPost(() => mapSystem.DeleteMap(map.MapId));
+        await RunTicksSync(5);
+        var xform = SComp<TransformComponent>(player);
+        var itemXform = SComp<TransformComponent>(item);
+        Assert.That(_sHandsSystem.GetActiveItem((player, hands)), Is.Not.EqualTo(item));
+        Assert.That(_sContainerSystem.IsInSameOrNoContainer((player, xform), (item, itemXform)));
     }
 }
