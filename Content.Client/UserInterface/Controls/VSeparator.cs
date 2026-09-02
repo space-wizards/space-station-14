a1ad0896d37312051f0dde68adc8db6e46a0c140
Fenriz <kastonbag552@gmail.com>
2026-09-03 01:13:26 +0300

    move VSeparator

--- Content.Client/UserInterface/Controls/VSeparator.cs
+++ Content.Client/UserInterface/Controls/VSeparator.cs
@@ -2,7 +2,7 @@
 using Robust.Client.Graphics;
 using Robust.Client.UserInterface.Controls;
 
-namespace Content.Client.Administration.UI.CustomControls;
+namespace Content.Client.UserInterface.Controls;
 
 public sealed class VSeparator : PanelContainer
 {
