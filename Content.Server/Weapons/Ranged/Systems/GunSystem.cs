c568e4481263e04d1bfad4e4262ccd072592ac8e
mycobiota <154991750+mycobiota@users.noreply.github.com>
2026-09-02 23:55:48 +1000

    make sure guns dont shoot in invalid places

--- Content.Server/Weapons/Ranged/Systems/GunSystem.cs
+++ Content.Server/Weapons/Ranged/Systems/GunSystem.cs
@@ -76,6 +76,12 @@ public sealed partial class GunSystem : SharedGunSystem
             ? TransformSystem.WithEntityId(fromCoordinates, gridUid)
             : new EntityCoordinates(_map.GetMapOrInvalid(fromMap.MapId), fromMap.Position);
 
+        if (fromEnt.EntityId == EntityUid.Invalid)
+        {
+            Log.Warning($"Tried to fire gun {ToPrettyString(gun)} from an invalid parent EntityUid.");
+            return;
+        }
+
         // Update shot based on the recoil
         toMap = fromMap.Position + angle.ToVec() * mapDirection.Length();
         mapDirection = toMap - fromMap.Position;
