0b103299e845667838905c260ce577c4eebb7ca8
11BelowStudio <60523642+11BelowStudio@users.noreply.github.com>
2026-09-04 15:39:20 +0100

    tested force-dropping all items

--- Content.IntegrationTests/Tests/Hands/HandForceDropTests.cs
+++ Content.IntegrationTests/Tests/Hands/HandForceDropTests.cs
@@ -37,7 +37,6 @@ public sealed class HandForceDropTests: InteractionTest
     [Test]
     public async Task TestTryDropUnremovable()
     {
-        
         var initialFreeHands = HandSys.CountFreeHands(SPlayer);
         Assume.That(initialFreeHands, Is.GreaterThan(0), "SPlayer must have at least one free hand");
 
@@ -53,5 +52,59 @@ public sealed class HandForceDropTests: InteractionTest
         Assert.That(HandSys.CountFreeHands(SPlayer), Is.EqualTo(initialFreeHands), "SPlayer did not return to initial free hand count after forcibly dropping the item!");
     }
 
+    [Test]
+    public async Task TryDropAll()
+    {
+        // make sure we have at least two hands
+        await HandDuplicationHelper(2);
+        
+        var initialFreeHands = HandSys.CountFreeHands(SPlayer);
+        Assume.That(initialFreeHands, Is.GreaterThan(0), "SPlayer must have at least one free hand");
+
+        // fill hands with unremovable items
+        while (HandSys.TryGetEmptyHand(SPlayer, out var emptyHand))
+        {
+            HandSys.SetActiveHand(SPlayer, emptyHand);
+            await PlaceInHands(UnremovableCrowbar);
+        }
+        var holdingItemsFreeHands = HandSys.CountFreeHands(SPlayer);
+        Assume.That(holdingItemsFreeHands, Is.LessThan(initialFreeHands), "SPlayer somehow has more free hands after being given the item");
+        
+        // attempting to drop all normally, should fail.
+        HandSys.DropAll(SPlayer);
+        Assert.That(HandSys.CountFreeHands(SPlayer), Is.EqualTo(holdingItemsFreeHands), "SPlayer somehow freed a hand after unforced DropAll of unremovable items.");
+        
+        // attempting to force drop all, should free all hands.
+        HandSys.DropAll(SPlayer, force: true);
+        Assert.That(HandSys.CountFreeHands(SPlayer), Is.EqualTo(initialFreeHands), "SPlayer did not free all hands after force dropping all");
+    }
+
+    /// <summary>
+    /// helper method which duplicates the active hand of SPlayer, so player will have at least the desired hand quantity
+    /// (assumes that the player has hands)
+    /// </summary>
+    /// <param name="targetHandCount">how many hands do we want?<br/>
+    /// function will ensure player has at least this many hands.<br/>
+    /// surplus hands will <b>not</b> be removed if player already has more hands than target.</param>
+    private async Task HandDuplicationHelper(int targetHandCount)
+    {
+        Assume.That(Hands, Is.Not.Null, "SPlayer has no HandsComponent :(");
+        var initialHands = HandSys.GetHandCount(SPlayer);
+        if (initialHands >= targetHandCount)
+            return;
+        Assume.That(initialHands, Is.GreaterThan(0));
+        var handName = HandSys.GetActiveHand(SPlayer);
+        Assume.That(handName, Is.Not.Null, "SPlayer active hand has no ID");
+        var hand = Hands!.Hands[HandSys.GetActiveHand(SPlayer)!];
+        await Server.WaitPost(() =>
+        {
+            for (var i = initialHands; i < targetHandCount; i++)
+            {
+                HandSys.AddHand(SPlayer, $"{handName}_{i}", hand);
+            }
+        });
+        await RunTicks(1);
+        Assert.That(HandSys.GetHandCount(SPlayer), Is.EqualTo(targetHandCount), "Unable to update hand count to meet target hand count :(");
+    }
     
 }
