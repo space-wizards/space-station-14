be2b9ad95d1a91930ea69b928c512ff3bd9e48c9
Fenriz <kastonbag552@gmail.com>
2026-09-04 19:18:53 +0300

    move pinball to effect

--- Content.Server/EntityEffects/Effects/Smite/PinballEntityEffectSystem.cs
+++ Content.Server/EntityEffects/Effects/Smite/PinballEntityEffectSystem.cs
@@ -1,12 +1,19 @@
+using Content.Shared.EntityEffects;
 using Robust.Shared.Physics;
 using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics.Systems;
+using Robust.Shared.Random;
 
-namespace Content.Server.Administration.Verbs.Operations;
+namespace Content.Server.EntityEffects.Effects.Smite;
 
-public sealed partial class AdminOperationSystem
+public sealed partial class PinballEntityEffectSystem : EntityEffectSystem<PhysicsComponent, Pinball>
 {
-    [SubscribeLocalEvent]
-    private void OnPinball(Entity<PhysicsComponent> entity, ref AdminOperationEvent<PinballOperation> args)
+    [Dependency] private FixtureSystem _fixtures = default!;
+    [Dependency] private SharedPhysicsSystem _physics = default!;
+    [Dependency] private IRobustRandom _random = default!;
+    [Dependency] private SharedTransformSystem _transform = default!;
+
+    protected override void Effect(Entity<PhysicsComponent> entity, ref EntityEffectEvent<Pinball> args)
     {
         if (!TryComp<FixturesComponent>(entity, out var fixtures))
             return;
@@ -18,14 +25,11 @@ public sealed partial class AdminOperationSystem
 
         foreach (var fixture in fixtures.Fixtures.Values)
         {
-            if (!fixture.Hard)
-                continue;
-
-            _physics.SetRestitution(entity, fixture, 1.1f, false, fixtures);
+            if (fixture.Hard)
+                _physics.SetRestitution(entity, fixture, 1.1f, false, fixtures);
         }
 
         _fixtures.FixtureUpdate(entity, manager: fixtures, body: entity.Comp);
-
         _physics.SetLinearVelocity(entity, _random.NextVector2(1.5f, 1.5f), manager: fixtures, body: entity.Comp);
         _physics.SetAngularVelocity(entity, MathF.PI * 12, manager: fixtures, body: entity.Comp);
         _physics.SetLinearDamping(entity, entity.Comp, 0f);
@@ -33,4 +37,4 @@ public sealed partial class AdminOperationSystem
     }
 }
 
-public sealed partial class PinballOperation : AdminOperationBase<PinballOperation>;
+public sealed partial class Pinball : EntityEffectBase<Pinball>;
