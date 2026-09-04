acd96d9bc4462881d0002ed9d94e19f960ddd65b
joules <pacifist.jules@gmail.com>
2026-09-05 02:49:35 +0300

    IMSOOOOOOSTUPID

--- Content.Shared/Storage/EntitySystems/SpawnItemsOnUseSystem.cs
+++ Content.Shared/Storage/EntitySystems/SpawnItemsOnUseSystem.cs
@@ -2,9 +2,10 @@ using Content.Shared.Administration.Logs;
 using Content.Shared.Database;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Interaction.Events;
+using Content.Shared.Random.Helpers;
 using Content.Shared.Storage.Components;
 using Robust.Shared.Audio.Systems;
-using Robust.Shared.Random;
+using Robust.Shared.Timing;
 using static Content.Shared.Storage.EntitySpawnCollection;
 
 namespace Content.Shared.Storage.EntitySystems;
@@ -14,10 +15,10 @@ namespace Content.Shared.Storage.EntitySystems;
 /// </summary>
 public abstract partial class SpawnItemsOnUseSystem : EntitySystem
 {
-    [Dependency] private IRobustRandom _random = default!;
     [Dependency] private ISharedAdminLogManager _adminLogger = default!;
     [Dependency] private SharedHandsSystem _hands = default!;
     [Dependency] private SharedAudioSystem _audio = default!;
+    [Dependency] private IGameTiming _timing = default!;
 
     [SubscribeLocalEvent]
     private void OnUseInHand(Entity<SpawnItemsOnUseComponent> ent, ref UseInHandEvent args)
@@ -25,28 +26,9 @@ public abstract partial class SpawnItemsOnUseSystem : EntitySystem
         if (args.Handled)
             return;
 
-        // If starting with zero or fewer uses, this component is a no-op
-        if (ent.Comp.Uses <= 0)
-            return;
-
-        var spawnEntities = GetSpawns(ent.Comp.Items, _random);
-
-        var spawned = new List<EntityUid>();
-        foreach (var proto in spawnEntities)
-        {
-            var spawn = PredictedSpawnNextToOrDrop(proto, args.User);
-            spawned.Add(spawn);
-
-            _adminLogger.Add(LogType.EntitySpawn, LogImpact.Low, $"{ToPrettyString(args.User)} used {ToPrettyString(ent)} which spawned {ToPrettyString(spawn)}");
-        }
-
-        // The entity is often deleted, so play the sound at its position rather than parenting
-        if (ent.Comp.Sound != null)
-            _audio.PlayPredicted(ent.Comp.Sound, ent, args.User, ent.Comp.Sound.Params);
+        var spawnEntities = GetSpawns(ent.Comp.Items, SharedRandomExtensions.PredictedRandom(_timing, GetNetEntity(ent)));
 
         ent.Comp.Uses--;
-
-        // Delete entity only if component was successfully used
         var remove = false;
         if (ent.Comp.Uses <= 0)
         {
@@ -54,10 +36,17 @@ public abstract partial class SpawnItemsOnUseSystem : EntitySystem
             remove = true;
         }
 
-        foreach (var spawn in spawned)
+        foreach (var proto in spawnEntities)
         {
+            var spawn = PredictedSpawnNextToOrDrop(proto, args.User);
+
             _hands.TryPickupAnyHand(args.User, spawn);
+            _adminLogger.Add(LogType.EntitySpawn, LogImpact.Low, $"{ToPrettyString(args.User)} used {ToPrettyString(ent)} which spawned {ToPrettyString(spawn)}");
         }
+
+        // The entity is often deleted, so play the sound at its position rather than parenting
+        if (ent.Comp.Sound != null)
+            _audio.PlayPredicted(ent.Comp.Sound, ent, args.User);
 
         if (remove)
             PredictedQueueDel(ent);
