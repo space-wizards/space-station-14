d03fe2f5838f8381bf69e845508b117271ef0912
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2026-09-04 15:18:09 -0700

    asfsafsafsafsafas

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -129,7 +129,6 @@ namespace Content.Client.Entry
             _prototypeManager.RegisterIgnore("nukeopsRole");
             _prototypeManager.RegisterIgnore("ghostRoleRaffleDecider");
             _prototypeManager.RegisterIgnore("codewordGenerator");
-            _prototypeManager.RegisterIgnore("codewordFaction");
 
             _componentFactory.GenerateNetIds();
             _adminManager.Initialize();
