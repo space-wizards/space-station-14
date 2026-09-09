75ee8fe29f35ec5e7b05570516ec0769e417a44b
joules <pacifist.jules@gmail.com>
2026-09-09 23:48:37 +0300

    init

--- Content.Server/Engineering/EntitySystems/SpawnAfterInteractSystem.cs
+++ Content.Server/Engineering/EntitySystems/SpawnAfterInteractSystem.cs
@@ -1,4 +1,4 @@
-using Content.Server.Engineering.Components;
+using Content.Shared.Storage.Components;
 using Content.Server.Stack;
 using Content.Shared.Coordinates.Helpers;
 using Content.Shared.DoAfter;
@@ -7,72 +7,96 @@ using Content.Shared.Maps;
 using Content.Shared.Physics;
 using Content.Shared.Stacks;
 using JetBrains.Annotations;
+using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
+using Robust.Shared.Serialization;
 
-namespace Content.Server.Engineering.EntitySystems
+namespace Content.Server.Engineering.EntitySystems;
+
+[UsedImplicitly]
+public sealed partial class SpawnAfterInteractSystem : EntitySystem
 {
-    [UsedImplicitly]
-    public sealed partial class SpawnAfterInteractSystem : EntitySystem
+    [Dependency] private SharedDoAfterSystem _doAfterSystem = default!;
+    [Dependency] private StackSystem _stackSystem = default!;
+    [Dependency] private TurfSystem _turfSystem = default!;
+    [Dependency] private SharedTransformSystem _transform = default!;
+    [Dependency] private SharedMapSystem _maps = default!;
+
+    [SubscribeLocalEvent]
+    private void OnInteract(Entity<SpawnAfterInteractComponent> ent, ref AfterInteractEvent args)
     {
-        [Dependency] private SharedDoAfterSystem _doAfterSystem = default!;
-        [Dependency] private StackSystem _stackSystem = default!;
-        [Dependency] private TurfSystem _turfSystem = default!;
-        [Dependency] private SharedTransformSystem _transform = default!;
-        [Dependency] private SharedMapSystem _maps = default!;
+        if (!args.CanReach && !ent.Comp.IgnoreDistance)
+            return;
 
-        public override void Initialize()
+        if (string.IsNullOrEmpty(ent.Comp.Prototype))
+            return;
+
+        var gridUid = _transform.GetGrid(args.ClickLocation);
+
+        if (!TryComp<MapGridComponent>(gridUid, out var grid))
+            return;
+
+        if (!_maps.TryGetTileRef(gridUid.Value, grid, args.ClickLocation, out var tileRef))
+            return;
+
+        if (!IsTileClear())
+            return;
+
+        if (ent.Comp.DoAfterTime <= 0)
         {
-            base.Initialize();
-
-            SubscribeLocalEvent<SpawnAfterInteractComponent, AfterInteractEvent>(HandleAfterInteract);
+            var ev = new SpawnAfterInteractEvent(args.ClickLocation.SnapToGrid(grid));
+            RaiseLocalEvent(ent, ev);
         }
 
-        private async void HandleAfterInteract(EntityUid uid, SpawnAfterInteractComponent component, AfterInteractEvent args)
+        var doAfterArgs = new DoAfterArgs(EntityManager, args.User, ent.Comp.DoAfterTime, new SpawnAfterInteractEvent(args.ClickLocation.SnapToGrid(grid)), ent, used: ent)
         {
-            if (!args.CanReach && !component.IgnoreDistance)
-                return;
-            if (string.IsNullOrEmpty(component.Prototype))
-                return;
+            BreakOnDamage = true,
+            BreakOnMove = true,
+            BreakOnWeightlessMove = false,
+        };
+        _doAfterSystem.TryStartDoAfter(doAfterArgs);
 
-            var gridUid = _transform.GetGrid(args.ClickLocation);
-            if (!TryComp<MapGridComponent>(gridUid, out var grid))
-                return;
-            if (!_maps.TryGetTileRef(gridUid.Value, grid, args.ClickLocation, out var tileRef))
-                return;
+        return;
 
-            bool IsTileClear()
-            {
-                return tileRef.Tile.IsEmpty == false && !_turfSystem.IsTileBlocked(tileRef, CollisionGroup.MobMask);
-            }
-
-            if (!IsTileClear())
-                return;
-
-            if (component.DoAfterTime > 0)
-            {
-                var doAfterArgs = new DoAfterArgs(EntityManager, args.User, component.DoAfterTime, new AwaitedDoAfterEvent(), null)
-                {
-                    BreakOnMove = true,
-                };
-                var result = await _doAfterSystem.WaitDoAfter(doAfterArgs);
-
-                if (result != DoAfterStatus.Finished)
-                    return;
-            }
-
-            if (component.Deleted || !IsTileClear())
-                return;
-
-            if (TryComp<StackComponent>(uid, out var stackComp)
-                && component.RemoveOnInteract && !_stackSystem.TryUse((uid, stackComp), 1))
-            {
-                return;
-            }
-
-            Spawn(component.Prototype, args.ClickLocation.SnapToGrid(grid));
-
-            if (component.RemoveOnInteract && stackComp == null)
-                TryQueueDel(uid);
+        bool IsTileClear()
+        {
+            return !tileRef.Tile.IsEmpty && !_turfSystem.IsTileBlocked(tileRef, CollisionGroup.MobMask);
         }
+    }
+
+    [SubscribeLocalEvent]
+    private void AfterDoafter(Entity<SpawnAfterInteractComponent> ent, ref SpawnAfterInteractEvent args)
+    {
+        if (TryComp<StackComponent>(ent, out var stackComp)
+            && ent.Comp.RemoveOnInteract && !_stackSystem.TryUse((ent, stackComp), 1))
+        {
+            return;
+        }
+
+        PredictedSpawnAtPosition(ent.Comp.Prototype, args.Coordinates);
+
+        if (ent.Comp.RemoveOnInteract && stackComp == null)
+            PredictedQueueDel(ent);
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed partial class SpawnAfterInteractEvent : SimpleDoAfterEvent
+{
+    [DataField(required:true)]
+    public EntityCoordinates Coordinates;
+
+    private SpawnAfterInteractEvent()
+    {
+    }
+
+    public SpawnAfterInteractEvent(EntityCoordinates coordinates)
+    {
+        Coordinates = coordinates;
+    }
+
+    public override DoAfterEvent Clone()
+    {
+        return this;
     }
 }
