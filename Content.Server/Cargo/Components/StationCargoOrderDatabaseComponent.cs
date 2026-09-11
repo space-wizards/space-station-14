d27ddeba582ecf5f8bec09165efd085fcbdee0e9
Connor Huffine <chuffine@gmail.com>
2026-09-10 20:48:04 -0400

    Doc up
    Signed-off-by: Connor Huffine <chuffine@gmail.com>

--- Content.Server/Cargo/Components/StationCargoOrderDatabaseComponent.cs
+++ Content.Server/Cargo/Components/StationCargoOrderDatabaseComponent.cs
@@ -18,14 +18,20 @@ public sealed partial class StationCargoOrderDatabaseComponent : Component
     [DataField]
     public int Capacity = 20;
 
+    /// <summary>
+    /// Every outstanding order across every account.
+    /// </summary>
     [ViewVariables]
     public IEnumerable<CargoOrderData> AllOrders => Orders.SelectMany(p => p.Value);
 
+    /// <summary>
+    /// A dictionary containing every outstanding order on the system, indexed by account.
+    /// </summary>
     [DataField]
     public Dictionary<ProtoId<CargoAccountPrototype>, List<CargoOrderData>> Orders = new();
 
     /// <summary>
-    /// Used to determine unique order IDs
+    /// Used to determine unique order IDs.
     /// </summary>
     [ViewVariables]
     public int NumOrdersCreated;
@@ -48,7 +54,7 @@ public sealed partial class StationCargoOrderDatabaseComponent : Component
     public EntityUid? Shuttle;
 
     /// <summary>
-    ///     The paper-type prototype to spawn with the order information.
+    /// The paper-type prototype to spawn with the order information.
     /// </summary>
     [DataField]
     public EntProtoId PrinterOutput = "PaperCargoInvoice";
@@ -60,9 +66,23 @@ public sealed partial class StationCargoOrderDatabaseComponent : Component
 [ByRefEvent]
 public record struct FulfillCargoOrderEvent(Entity<StationDataComponent> Station, CargoOrderData Order)
 {
+    /// <summary>
+    /// The station that placed the order.
+    /// </summary>
     public readonly Entity<StationDataComponent> Station = Station;
+
+    /// <summary>
+    /// The <see cref="CargoOrderData"/> representing the order.
+    /// </summary>
     public readonly CargoOrderData Order = Order;
+
+    /// <summary>
+    /// The entity that is fulfilling the order, e.g. the telepad where an order will arrive.
+    /// </summary>
     public EntityUid? FulfillmentEntity;
+
+    /// <summary>
+    /// If this event has already been handled.
+    /// </summary>
     public bool Handled = false;
 }
-
