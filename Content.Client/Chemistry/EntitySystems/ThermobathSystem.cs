816aefda67f2be3eb5ace4593629eb0a33304946
Fenriz <kastonbag552@gmail.com>
2026-09-13 16:20:15 +0300

    hm?

--- Content.Client/Chemistry/EntitySystems/ThermobathSystem.cs
+++ Content.Client/Chemistry/EntitySystems/ThermobathSystem.cs
@@ -1,8 +1,6 @@
 using Content.Shared.Chemistry.Components;
-using Content.Shared.Chemistry.Components.SolutionManager;
 using Content.Shared.Chemistry.EntitySystems;
 using Content.Shared.Temperature.Components;
-using Robust.Shared.Containers;
 
 namespace Content.Client.Chemistry.EntitySystems;
 
@@ -10,9 +8,7 @@ namespace Content.Client.Chemistry.EntitySystems;
 public sealed partial class ThermobathSystem : SharedThermobathSystem
 {
     [Dependency] private SharedUserInterfaceSystem _ui = default!;
-    [Dependency] private SharedContainerSystem _container = default!;
 
-    [Dependency] private EntityQuery<ContainedSolutionComponent> _containedSolutionQuery;
     [Dependency] private EntityQuery<ThermobathComponent> _thermobathQuery;
 
     [SubscribeLocalEvent]
@@ -20,18 +16,6 @@ public sealed partial class ThermobathSystem : SharedThermobathSystem
     {
         if (_thermobathQuery.TryComp(ent, out var thermobath))
             UpdateUi((ent, thermobath));
-    }
-
-    [SubscribeLocalEvent]
-    private void OnSolutionState(Entity<SolutionComponent> ent, ref AfterAutoHandleStateEvent args)
-    {
-        if (!_containedSolutionQuery.TryComp(ent, out var contained) ||
-            !_container.TryGetContainingContainer(contained.Container, out var container) ||
-            container.ID != ThermobathComponent.BeakerSlotId ||
-            !_thermobathQuery.TryComp(container.Owner, out var thermobath))
-            return;
-
-        UpdateUi((container.Owner, thermobath));
     }
 
     protected override void UpdateUi(Entity<ThermobathComponent> ent)
