3cc9f0859b1d0ff4b3a101b9d0cca9b91d586db9
cashew <wackerlinus@gmail.com>
2026-09-14 04:24:46 +0200

    forgor newline

--- Content.Server/Chat/Managers/ChatManager.cs
+++ Content.Server/Chat/Managers/ChatManager.cs
@@ -312,6 +312,7 @@ internal sealed partial class ChatManager : IChatManager
             _adminLogger.Add(LogType.Chat, LogImpact.Extreme, $"{player:Player} attempted to send admin message but was not admin");
             return;
         }
+
         var playerName = SharedChatSystem.ChatNameLinks ? $"[textlink=\"{FormattedMessage.EscapeStringParameter(player.Name)}\" entity=\"{_entityManager.GetNetEntity(player.AttachedEntity)}\" color=\"{ChatChannel.Admin.TextColor().ToHex()}\"]" : FormattedMessage.EscapeText(player.Name);
         var clients = _adminManager.ActiveAdmins.Select(p => p.Channel);
         var wrappedMessage = Loc.GetString("chat-manager-send-admin-chat-wrap-message",
