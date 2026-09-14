6d7e134760ccb829982e24bd3411c95a8c4523f1
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2026-09-14 18:42:01 +0200

    borgs cant send, AI can.

---
+++ Content.Shared/Radio/EntitySystems/ActiveRadioSystem.cs
@@ -0,0 +1,19 @@
+using Content.Shared.Popups;
+using Content.Shared.Radio.Components;
+
+namespace Content.Shared.Radio.EntitySystems;
+
+public sealed partial class ActiveRadioSystem : EntitySystem
+{
+    [Dependency] private SharedPopupSystem _popup = default!;
+
+    [SubscribeLocalEvent]
+    private void OnRadioSendAttempt(Entity<ActiveRadioComponent> ent, ref RadioSendAttemptEvent args)
+    {
+        if (!ent.Comp.CanSendInReceiveOnlyChannels)
+        {
+            _popup.PopupEntity(Loc.GetString("chat-manager-radio-channel-forbidden-for-headset"), ent, ent);
+            args.Cancelled = true;
+        }
+    }
+}
