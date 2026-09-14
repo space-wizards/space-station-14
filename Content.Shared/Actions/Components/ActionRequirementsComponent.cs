3f1586881d103a7664a679d4ba4fb8be8bd69331
ScarKy0 <scarky0@onet.eu>
2026-09-14 22:51:44 +0200

    actually this is how its supposed to be yeah

--- Content.Shared/Actions/Components/ActionRequirementsComponent.cs
+++ Content.Shared/Actions/Components/ActionRequirementsComponent.cs
@@ -59,5 +59,5 @@ public enum ActionRequirementTarget : byte
     /// <summary>
     /// The target is both the performer and the target.
     /// </summary>
-    Both = 1 << 1,
+    Both = Performer | Target,
 }
