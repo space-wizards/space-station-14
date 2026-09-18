53e29ab9624b9f13fd7e667017c07389aabb7276
AndrewFenriz <78079974+AndrewFenriz@users.noreply.github.com>
2026-09-18 19:01:16 +0000

    Move admin smites to prototypes (#45550)
    * Add prototype-backed admin verb framework
    
    * Add reusable admin verb operations
    
    * Add smite-specific admin operations
    
    * Migrate admin smites to prototypes
    
    * More docs for god of docs
    
    * We dont have prediction here, so...
    
    * Final cleanup
    
    * Fix adminSmite
    
    * Add segment display control
    
    * Add generic thermoregulator
    
    * Add thermobath systems
    
    * Replace hotplate with thermobath
    
    * Refine thermobath layout
    
    * move simple smites to effects
    
    * move comps to effects
    
    * move add action to effect
    
    * move add mind role to effect
    
    * move add user interfaces to effect
    
    * move godmode to effect
    
    * move eye zoom to effect
    
    * move speed swap to effect
    
    * move spill bloodstream to effect
    
    * move remove organs to effect
    
    * move cream pie to effect
    
    * move slip to effect
    
    * fix server effect usings
    
    * move homing rod to effect
    
    * move locker stuff to effect
    
    * move tabletop dimension to effect
    
    * move pinball to effect
    
    * move yeet to effect
    
    * move silicon laws to effect
    
    * move ghost kick to effect
    
    * remove admin operations
    
    * revert sht
    
    * remove BOM
    
    * use admin verb prototypes for smites
    
    * add heart attack smite prototype
    
    * docs update
    
    * fix AdminSmiteVomitOrgans
    
    * split and move to shared
    
    * move to shared
    
    * move to shared and predict
    
    * PredictedQueueDel
    
    * fix Popup
    
    * fix HomingRod
    
    * port DeleteEntityEntityEffectSystem
    
    * fix StuffIntoLockerEntityEffectSystem
    
    * upd
    
    * fix eye
    
    * fix movement
    
    * fix!!
    
    * fix and doc
    
    * Add admin verb category prototype
    
    * upd AdminVerbPrototype
    
    * split verbs
    
    * SuperSuperSuperBonk
    
    * Update PopupMessageEntityEffectSystem.cs
    
    * fields
    
    ---------
    
    Co-authored-by: Pok245 <113675512+Pok27@users.noreply.github.com>

--- Content.Server/EntityEffects/Effects/PolymorphEntityEffectSystem.cs
+++ Content.Server/EntityEffects/Effects/PolymorphEntityEffectSystem.cs
@@ -8,12 +8,15 @@ namespace Content.Server.EntityEffects.Effects;
 /// Polymorphs this entity into another entity.
 /// </summary>
 /// <inheritdoc cref="EntityEffectSystem{T,TEffect}"/>
-public sealed partial class PolymorphEntityEffectSystem : EntityEffectSystem<PolymorphableComponent, Shared.EntityEffects.Effects.Polymorph>
+public sealed partial class PolymorphEntityEffectSystem : EntityEffectSystem<MetaDataComponent, Shared.EntityEffects.Effects.Polymorph>
 {
     [Dependency] private PolymorphSystem _polymorph = default!;
 
-    protected override void Effect(Entity<PolymorphableComponent> entity, ref EntityEffectEvent<Shared.EntityEffects.Effects.Polymorph> args)
+    protected override void Effect(Entity<MetaDataComponent> entity, ref EntityEffectEvent<Shared.EntityEffects.Effects.Polymorph> args)
     {
+        if (!args.Effect.Force && !HasComp<PolymorphableComponent>(entity))
+            return;
+
         _polymorph.PolymorphEntity(entity, args.Effect.Prototype);
     }
 }
