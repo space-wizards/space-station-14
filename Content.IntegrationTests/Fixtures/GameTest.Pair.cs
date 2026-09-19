d1ca261ce9c73b5b8bbf43756a83f280daf2c5a7
Tayrtahn <tayrtahn@gmail.com>
2026-09-19 11:46:06 -0400

    xmldoc improvements

--- Content.IntegrationTests/Fixtures/GameTest.Pair.cs
+++ Content.IntegrationTests/Fixtures/GameTest.Pair.cs
@@ -37,13 +37,15 @@ public abstract partial class GameTest
     }
 
     /// <summary>
-    ///     Creates a test map for use during the test.
+    ///     Creates a map with a single grid consisting of one tile for use during the test.
     /// </summary>
     /// <remarks>
-    ///     The map will be deleted automatically during test cleanup.
+    ///     The map will be deleted automatically during test cleanup (see <see cref="TestPair{TServer,TClient}.Cleanup"/>).
+    ///     Use this method instead of Pair.CreateTestMap to ensure that <see cref="TestMap"/> is not null.
     ///     Data about the map can be referenced using <see cref="TestMap"/>.
     /// </remarks>
     /// <returns>Data about the test map. Can also be accessed via <see cref="TestMap"/>.</returns>
+    /// <seealso cref="Pair.TestPair.CreateTestMap"/>
     [MemberNotNull(nameof(TestMap))]
     public async Task<TestMapData> CreateTestMap()
     {
