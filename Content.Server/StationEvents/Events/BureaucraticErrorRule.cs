384d3f3095444ebba1acd35da7df888272067f70
Whatstone <whatston3@gmail.com>
2026-09-20 10:31:09 -0400

    Resolve StationEvent warnings

--- Content.Server/StationEvents/Events/BureaucraticErrorRule.cs
+++ Content.Server/StationEvents/Events/BureaucraticErrorRule.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Content.Server.Station.Systems;
 using Content.Server.StationEvents.Components;
 using Content.Shared.GameTicking.Components;
+using Content.Shared.Station.Components;
 using JetBrains.Annotations;
 using Robust.Shared.Random;
 
@@ -12,16 +13,16 @@ public sealed partial class BureaucraticErrorRule : StationEventSystem<Bureaucra
 {
     [Dependency] private ServerStationJobsSystem _stationJobs = default!;
 
-    protected override void Started(EntityUid uid, BureaucraticErrorRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+    protected override void Started(Entity<BureaucraticErrorRuleComponent, GameRuleComponent> ent, ref GameRuleStartedEvent args)
     {
-        base.Started(uid, component, gameRule, args);
+        base.Started(ent, ref args);
 
-        if (!Station.TryGetRandomStation(out var chosenStation, HasComp<Shared.Station.Components.StationJobsComponent>))
+        if (!Station.TryGetRandomStation<StationEventEligibleComponent>(out var chosenStation, HasComp<StationJobsComponent>))
             return;
 
         var jobList = _stationJobs.GetJobs(chosenStation.Value).Keys.ToList();
 
-        foreach(var job in component.IgnoredJobs)
+        foreach (var job in ent.Comp1.IgnoredJobs)
             jobList.Remove(job);
 
         if (jobList.Count == 0)
--- Content.Server/StationEvents/Events/RandomEntityStorageSpawnRule.cs
+++ Content.Server/StationEvents/Events/RandomEntityStorageSpawnRule.cs
@@ -1,28 +1,35 @@
 using Content.Server.StationEvents.Components;
 using Content.Server.Storage.EntitySystems;
 using Content.Shared.GameTicking.Components;
+using Content.Shared.Station.Components;
 using Content.Shared.Storage.Components;
 using Robust.Shared.Map;
 using Robust.Shared.Random;
 
 namespace Content.Server.StationEvents.Events;
 
+/// <summary>
+/// Handler for events that spawn an entity inside of another random entity with <see cref="EntityStorageComponent"/>.
+/// </summary>
+/// <seealso cref="RandomEntityStorageSpawnRuleComponent"/>
 public sealed partial class RandomEntityStorageSpawnRule : StationEventSystem<RandomEntityStorageSpawnRuleComponent>
 {
     [Dependency] private EntityStorageSystem _entityStorage = default!;
 
-    protected override void Started(EntityUid uid, RandomEntityStorageSpawnRuleComponent comp, GameRuleComponent gameRule, GameRuleStartedEvent args)
+    protected override void Started(Entity<RandomEntityStorageSpawnRuleComponent, GameRuleComponent> ent, ref GameRuleStartedEvent args)
     {
-        base.Started(uid, comp, gameRule, args);
+        base.Started(ent, ref args);
 
-        if (!Station.TryGetRandomStation(out var station))
+        if (!Station.TryGetRandomStation<StationEventEligibleComponent>(out var station))
             return;
 
+        var spawnRule = ent.Comp1;
+
         var validLockers = new List<(EntityUid, EntityStorageComponent)>();
-        var spawn = Spawn(comp.Prototype, MapCoordinates.Nullspace);
+        var spawn = Spawn(spawnRule.Prototype, MapCoordinates.Nullspace);
 
         var query = EntityQueryEnumerator<EntityStorageComponent, TransformComponent>();
-        while (query.MoveNext(out var ent, out var storage, out var xform))
+        while (query.MoveNext(out var storageUid, out var storage, out var xform))
         {
             if (Station.GetOwningStation(ent, xform) != station.Value.Owner)
                 continue;
@@ -30,7 +37,7 @@ public sealed partial class RandomEntityStorageSpawnRule : StationEventSystem<Ra
             if (!_entityStorage.CanInsert(spawn, ent, storage))
                 continue;
 
-            validLockers.Add((ent, storage));
+            validLockers.Add((storageUid, storage));
         }
 
         if (validLockers.Count == 0)
