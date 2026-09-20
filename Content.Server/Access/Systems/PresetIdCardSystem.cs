2dc8895efa95579d16fc7c01eb74dc5b90b31734
joules <pacifist.jules@gmail.com>
2026-09-21 01:51:20 +0300

    pun

--- Content.Server/Access/Systems/PresetIdCardSystem.cs
+++ Content.Server/Access/Systems/PresetIdCardSystem.cs
@@ -2,7 +2,6 @@ using Content.Server.Access.Components;
 using Content.Server.Station.Systems;
 using Content.Shared.Access.Systems;
 using Content.Shared.GameTicking;
-using Content.Shared.Roles;
 using Content.Shared.Station.Components;
 
 namespace Content.Server.Access.Systems;
@@ -13,13 +12,7 @@ public sealed partial class PresetIdCardSystem : EntitySystem
     [Dependency] private SharedAccessSystem _accessSystem = default!;
     [Dependency] private ServerStationSystem _stationSystem = default!;
 
-    public override void Initialize()
-    {
-        SubscribeLocalEvent<PresetIdCardComponent, MapInitEvent>(OnMapInit);
-
-        SubscribeLocalEvent<RulePlayerJobsAssignedEvent>(PlayerJobsAssigned);
-    }
-
+    [SubscribeLocalEvent]
     private void PlayerJobsAssigned(RulePlayerJobsAssignedEvent ev)
     {
         // Go over all ID cards and make sure they're correctly configured for extended access.
@@ -33,53 +26,55 @@ public sealed partial class PresetIdCardSystem : EntitySystem
             if (station == null || !TryComp<StationJobsComponent>(station.Value, out var jobsComp) || !jobsComp.ExtendedAccess)
                 continue;
 
-            SetupIdAccess(uid, card, true);
-            SetupIdName(uid, card);
+            SetupIdAccess((uid, card), true);
+            SetupIdName((uid, card));
         }
     }
 
-    private void OnMapInit(EntityUid uid, PresetIdCardComponent id, MapInitEvent args)
+    [SubscribeLocalEvent]
+    private void OnMapInit(Entity<PresetIdCardComponent> ent, ref MapInitEvent args)
     {
         // If a preset ID card is spawned on a station at setup time,
         // the station may not exist,
         // or may not yet know whether it is on extended access (players not spawned yet).
         // PlayerJobsAssigned makes sure extended access is configured correctly in that case.
 
-        var station = _stationSystem.GetOwningStation(uid);
+        var station = _stationSystem.GetOwningStation(ent);
         var extended = false;
 
         // Station not guaranteed to have jobs (e.g. nukie outpost).
         if (TryComp(station, out StationJobsComponent? stationJobs))
             extended = stationJobs.ExtendedAccess;
 
-        SetupIdAccess(uid, id, extended);
-        SetupIdName(uid, id);
+        SetupIdAccess(ent, extended);
+        SetupIdName(ent);
     }
 
-    private void SetupIdName(EntityUid uid, PresetIdCardComponent id)
+    private void SetupIdName(Entity<PresetIdCardComponent> ent)
     {
-        if (id.IdName == null)
+        if (ent.Comp.IdName == null)
             return;
-        _cardSystem.TryChangeFullName(uid, id.IdName);
+
+        _cardSystem.TryChangeFullName(ent, Loc.GetString(ent.Comp.IdName));
     }
 
-    private void SetupIdAccess(EntityUid uid, PresetIdCardComponent id, bool extended)
+    private void SetupIdAccess(Entity<PresetIdCardComponent> ent, bool extended)
     {
-        if (id.JobName == null)
+        if (ent.Comp.JobName == null)
             return;
 
-        if (!ProtoMan.TryIndex(id.JobName, out JobPrototype? job))
+        if (!ProtoMan.TryIndex(ent.Comp.JobName, out var job))
         {
-            Log.Error($"Invalid job id ({id.JobName}) for preset card");
+            Log.Error($"Invalid job id ({ent.Comp.JobName}) for preset card");
             return;
         }
 
-        _accessSystem.SetAccessToJob(uid, job, extended);
+        _accessSystem.SetAccessToJob(ent, job, extended);
 
-        _cardSystem.TryChangeJobTitle(uid, job.LocalizedName);
-        _cardSystem.TryChangeJobDepartment(uid, job);
+        _cardSystem.TryChangeJobTitle(ent, job.LocalizedName);
+        _cardSystem.TryChangeJobDepartment(ent, job);
 
         if (ProtoMan.Resolve(job.Icon, out var jobIcon))
-            _cardSystem.TryChangeJobIcon(uid, jobIcon);
+            _cardSystem.TryChangeJobIcon(ent, jobIcon);
     }
 }
