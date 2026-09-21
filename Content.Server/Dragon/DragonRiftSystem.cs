bb097619b05e33ce5f65c6a1d1d69179c8f71791
Pok245 <113675512+Pok27@users.noreply.github.com>
2026-09-21 14:53:24 +0300

    Formatting

--- Content.Server/Dragon/DragonRiftSystem.cs
+++ Content.Server/Dragon/DragonRiftSystem.cs
@@ -1,18 +1,18 @@
+using System.Numerics;
 using Content.Server.Chat.Systems;
 using Content.Server.NPC;
 using Content.Server.NPC.Systems;
 using Content.Server.Pinpointer;
+using Content.Shared.Damage.Components;
 using Content.Shared.Dragon;
 using Content.Shared.EntityTable;
 using Content.Shared.Examine;
 using Content.Shared.Sprite;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.GameStates;
 using Robust.Shared.Map;
 using Robust.Shared.Player;
 using Robust.Shared.Serialization.Manager;
-using System.Numerics;
-using Content.Shared.Damage.Components;
-using Robust.Shared.Audio.Systems;
-using Robust.Shared.GameStates;
 using Robust.Shared.Utility;
 
 namespace Content.Server.Dragon;
