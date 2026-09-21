f2f5df9be7e26d0e61fdea42bedff1c9180b7c08
AftrLite <61218133+AftrLite@users.noreply.github.com>
2026-09-21 23:13:46 +1200

    Requested Changes - replace random with PredictedRandom

--- Content.Shared/Speech/EntitySystems/SpeakOnActionSystem.cs
+++ Content.Shared/Speech/EntitySystems/SpeakOnActionSystem.cs
@@ -3,8 +3,10 @@ using Content.Shared.Actions.Events;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Chat;
 using Content.Shared.Dataset;
+using Content.Shared.Random.Helpers;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
+using Robust.Shared.Timing;
 
 namespace Content.Shared.Speech.EntitySystems;
 
@@ -12,6 +14,7 @@ public sealed partial class SpeakOnActionSystem : EntitySystem
 {
     [Dependency] private ActionBlockerSystem _actionBlocker = default!;
     [Dependency] private SharedChatSystem _chat = default!;
+    [Dependency] private IGameTiming _timing = default!;
     [Dependency] private IPrototypeManager _proto = default!;
     [Dependency] private IRobustRandom _random = default!;
 
@@ -19,15 +22,16 @@ public sealed partial class SpeakOnActionSystem : EntitySystem
     private void OnActionPerformed(Entity<SpeakOnActionComponent> ent, ref ActionPerformedEvent args)
     {
         var user = args.Performer;
+        var random = SharedRandomExtensions.PredictedRandom(_timing, GetNetEntity(ent));
 
         // If we can't speak, we can't speak.
         if (!HasComp<SpeechComponent>(user) || !_actionBlocker.CanSpeak(user))
             return;
 
-        if (ent.Comp.SpeakChance != null && !_random.Prob(ent.Comp.SpeakChance.Value))
+        if (ent.Comp.SpeakChance != null && !random.Prob(ent.Comp.SpeakChance.Value))
             return;
 
-        var randomSentence = GetDialogue(ent.Comp.Sentences);
+        var randomSentence = GetDialogue(ent.Comp.Sentences, random);
 
         if (!string.IsNullOrWhiteSpace(randomSentence))
             _chat.TrySendInGameICMessage(user, Loc.GetString(randomSentence), InGameICChatType.Speak, false);
@@ -35,11 +39,11 @@ public sealed partial class SpeakOnActionSystem : EntitySystem
             _chat.TrySendInGameICMessage(user, Loc.GetString(ent.Comp.Sentence), InGameICChatType.Speak, false);
     }
 
-    private string? GetDialogue(ProtoId<LocalizedDatasetPrototype>? dialogue)
+    private string? GetDialogue(ProtoId<LocalizedDatasetPrototype>? dialogue, IRobustRandom random)
     {
         if (!_proto.TryIndex(dialogue, out var proto))
             return null;
 
-        return _random.Pick(proto.Values);
+        return random.Pick(proto.Values);
     }
 }
