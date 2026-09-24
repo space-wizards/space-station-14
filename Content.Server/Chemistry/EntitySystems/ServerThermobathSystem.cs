22395524ee142a80c51b7d9a89e6836c44d1a1d0
Fenriz <kastonbag552@gmail.com>
2026-09-24 08:12:58 +0300

    fix Solution

--- Content.Server/Chemistry/EntitySystems/ServerThermobathSystem.cs
+++ Content.Server/Chemistry/EntitySystems/ServerThermobathSystem.cs
@@ -19,7 +19,11 @@ public sealed partial class ServerThermobathSystem : ThermobathSystem
     private void OnThermoregulatorUpdated(Entity<ThermobathComponent> ent, ref ThermoregulatorUpdatedEvent args)
     {
         var thermoregulator = args.Thermoregulator;
-        if (!TryGetSolutionFromContainer(ent, out var soln, out var solution) || solution.Volume <= 0)
+        if (!TryGetSolutionFromContainer(ent, out var soln))
+            return;
+
+        var solution = soln.Value.Comp.Solution;
+        if (solution.Volume <= 0)
             return;
 
         // TODO: Replace this with HeatContainerQuerySystem once #45554 is merged.
@@ -36,15 +40,13 @@ public sealed partial class ServerThermobathSystem : ThermobathSystem
 
     private bool TryGetSolutionFromContainer(
         Entity<ThermobathComponent> ent,
-        [NotNullWhen(true)] out Entity<SolutionComponent>? soln,
-        [NotNullWhen(true)] out Solution? solution)
+        [NotNullWhen(true)] out Entity<SolutionComponent>? soln)
     {
         var beaker = _itemSlots.GetItemOrNull(ent.Owner, ThermobathComponent.BeakerSlotId);
         if (beaker != null)
-            return _solutionContainer.TryGetFitsInDispenser(beaker.Value, out soln, out solution);
+            return _solutionContainer.TryGetFitsInDispenser(beaker.Value, out soln, out _);
 
         soln = null;
-        solution = null;
         return false;
     }
 }
